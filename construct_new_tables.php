5571b385325a428a3b8c5de273b633f67d18109c
ashoat <ashoat@gmail.com>
2016-09-14 23:44:28 -0400

    Some necessary changes

--- construct_new_tables.php
+++ construct_new_tables.php
@@ -3,7 +3,7 @@
 require_once('config.php');
 
 $result = $conn->query(
-  "SELECT d.id AS day_id, d.text, d.last_update, d.session_id, ".
+  "SELECT d.id AS day_id, ".
     "e.id AS entry_id FROM days d LEFT JOIN entries e ON e.day = d.id "
 );
 while ($row = $result->fetch_assoc()) {
@@ -14,8 +14,7 @@ while ($row = $result->fetch_assoc()) {
     $entry_id = $conn->insert_id;
     $conn->query(
       "INSERT INTO entries(id, day, text, creator, creation_time, last_update) ".
-        "VALUES ($entry_id, $row[day_id], '$row[text]', 256, $row[last_update], ".
-        "$row[last_update])"
+        "VALUES ($entry_id, $row[day_id], '', 256, 0, 0)"
     );
     echo "Created entry #".$entry_id." for day #".$row['day_id']."\n";
   }
@@ -29,7 +28,7 @@ while ($row = $result->fetch_assoc()) {
     $conn->query(
       "INSERT INTO revisions(id, entry, author, text, creation_time, ".
         "session_id, last_update) VALUES ($revision_id, $entry_id, 256, ".
-        "'$row[text]', $row[last_update], '$row[session_id]', $row[last_update])"
+        "'', 0, '', 0)"
     );
     echo "Created revision #".$revision_id." for day #".$row['day_id']."\n";
   }
