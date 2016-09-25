ca79e24000358738f48d78a4582ea272e4904448
ashoat <ashoat@gmail.com>
2016-09-26 03:38:55 +0700

    Minor

--- wipe_empty_entries.php
+++ wipe_empty_entries.php
@@ -3,7 +3,7 @@
 require_once('config.php');
 
 $result = $conn->query(
-  "SELECT r.id, r.entry FROM revisions r "
+  "SELECT r.id, r.entry FROM revisions r ".
     "LEFT JOIN entries e ON e.id = r.entry WHERE TRIM(e.text) = ''"
 );
 $entries = array();
@@ -12,11 +12,11 @@ while ($row = $result->fetch_assoc()) {
   $entries[$entry_id] = $entry_id;
   $revision_id = intval($row['id']);
   echo "Deleting revision #$revision_id...\n";
-  $conn->query("DELETE FROM revisions WHERE id = " . $revision_id);
-  $conn->query("DELETE FROM ids WHERE id = " . $revision_id);
+  $conn->query("DELETE FROM revisions WHERE id = ".$revision_id);
+  $conn->query("DELETE FROM ids WHERE id = ".$revision_id);
 }
 foreach ($entries as $entry_id) {
-  echo "Deleting revision #$entry_id...\n";
-  $conn->query("DELETE FROM entries WHERE id = " . $entry_id);
-  $conn->query("DELETE FROM ids WHERE id = " . $entry_id);
+  echo "Deleting entry #$entry_id...\n";
+  $conn->query("DELETE FROM entries WHERE id = ".$entry_id);
+  $conn->query("DELETE FROM ids WHERE id = ".$entry_id);
 }
