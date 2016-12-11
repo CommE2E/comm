26263fea7db626182a72e650bdb425c6dfd6f75c
ashoat <ashoat@gmail.com>
2016-12-11 18:34:02 -0500

    bcrypt for verify table, breaking SQL changes (and extant verification)

--- verify_lib.php
+++ verify_lib.php
@@ -43,38 +43,43 @@ EMAIL;
 }
 
 // generates row in verifications table with hash of code
-// returns verification code
+// returns hex containing verification ID and code
 function generate_verification_code($user, $field) {
   global $conn;
   $code = bin2hex(openssl_random_pseudo_bytes(4));
-  $hash = hash('sha512', $code);
+  $hash = password_hash($code, PASSWORD_BCRYPT);
+  $conn->query("INSERT INTO ids(table_name) VALUES('cookies')");
+  $id = $conn->insert_id;
   $time = round(microtime(true) * 1000); // in milliseconds
   $conn->query(
-    "INSERT INTO verifications(user, field, hash, creation_time) ".
-      "VALUES($user, $field, UNHEX('$hash'), $time)"
+    "INSERT INTO verifications(id, user, field, hash, creation_time) ".
+      "VALUES($id, $user, $field, '$hash', $time)"
   );
-  return $code;
+  return $code . dechex($id);
 }
 
 // returns array(user id, field)
 // if code doesn't work then returns null
-function verify_code($code) {
+function verify_code($hex) {
   global $conn, $verify_code_lifetime;
 
-  $hash = hash('sha512', $code);
+  $code = substr($hex, 0, 8);
+  $id = hexdec(substr($hex, 8));
+
   $result = $conn->query(
-    "SELECT user, field, creation_time ".
-      "FROM verifications WHERE hash = UNHEX('$hash')"
+    "SELECT hash, user, field, creation_time ".
+      "FROM verifications WHERE id = $id"
   );
   $row = $result->fetch_assoc();
-  if (!$row) {
+  if (!$row || !password_verify($code, $row['hash'])) {
     return null;
   }
 
   $time = round(microtime(true) * 1000); // in milliseconds
   if ($row['creation_time'] + $verify_code_lifetime * 1000 < $time) {
     // Code is expired. Delete it...
-    $conn->query("DELETE FROM verifications WHERE hash = UNHEX('$hash')");
+    $conn->query("DELETE FROM verifications WHERE id = $id");
+    $conn->query("DELETE FROM ids WHERE id = $id");
     return null;
   }
 
@@ -84,6 +89,16 @@ function verify_code($code) {
 // Call this function after a successful verification
 function clear_verify_codes($user, $field) {
   global $conn;
+  $result = $conn->query(
+    "SELECT id FROM verifications WHERE user = $user AND field = $field"
+  );
+  $delete_ids = array();
+  while ($row = $result->fetch_assoc()) {
+    $delete_ids[] = "id = ".$row['id'];
+  }
+  if ($delete_ids) {
+    $conn->query("DELETE FROM ids WHERE ".implode(' OR ', $delete_ids));
+  }
   $conn->query(
     "DELETE FROM verifications WHERE user = $user AND field = $field"
   );
