bb724d82a8d162bf3354686948e256adb8a18a0a
ashoat <ashoat@gmail.com>
2017-04-14 16:09:15 -0400

    Ping on app foregrounding to preemptively catch cookie invalidations and make sure Redux state is updated and consistent

--- native/app.react.js
+++ native/app.react.js
@@ -5,14 +5,26 @@ import { PropTypes as ReactNavigationPropTypes } from 'react-navigation';
 import type { Dispatch } from 'lib/types/redux-types';
 import type { AppState } from './redux-setup';
 import type { Action } from './navigation-setup';
+import type { PingResult } from 'lib/actions/ping-actions';
+import type { DispatchActionPromise } from 'lib/utils/action-utils';
 
 import React from 'react';
 import { Provider, connect } from 'react-redux';
-import { AppRegistry, Platform, UIManager } from 'react-native';
+import {
+  AppRegistry,
+  Platform,
+  UIManager,
+  AppState as NativeAppState,
+} from 'react-native';
 import { addNavigationHelpers } from 'react-navigation';
 import invariant from 'invariant';
 
 import { registerConfig } from 'lib/utils/config';
+import {
+  includeDispatchActionProps,
+  bindServerCalls,
+} from 'lib/utils/action-utils';
+import { pingActionType, ping } from 'lib/actions/ping-actions';
 
 import { RootNavigator } from './navigation-setup';
 import { store } from './redux-setup';
@@ -48,14 +60,47 @@ if (Platform.OS === "android") {
 
 class AppWithNavigationState extends React.PureComponent {
 
-  static propTypes = {
-    navigationState: ReactNavigationPropTypes.navigationState,
-    dispatch: React.PropTypes.func.isRequired,
-  };
   props: {
+    // Redux state
+    cookie: ?string,
     navigationState: NavigationState,
+    loggedIn: bool,
+    // Redux dispatch functions
     dispatch: Dispatch<AppState, Action>,
+    dispatchActionPromise: DispatchActionPromise,
+    // async functions that hit server APIs
+    ping: () => Promise<PingResult>,
   };
+  static propTypes = {
+    cookie: React.PropTypes.string,
+    navigationState: ReactNavigationPropTypes.navigationState,
+    loggedIn: React.PropTypes.bool.isRequired,
+    dispatch: React.PropTypes.func.isRequired,
+    dispatchActionPromise: React.PropTypes.func.isRequired,
+    ping: React.PropTypes.func.isRequired,
+  };
+  currentState: ?string = NativeAppState.currentState;
+
+  componentDidMount() {
+    NativeAppState.addEventListener('change', this.handleAppStateChange);
+  }
+
+  componentWillUnmount() {
+    NativeAppState.removeEventListener('change', this.handleAppStateChange);
+  }
+
+  handleAppStateChange = (nextAppState: ?string) => {
+    if (
+      this.currentState &&
+      this.currentState.match(/inactive|background/) &&
+      nextAppState === "active" &&
+      (this.props.loggedIn ||
+        (this.props.cookie && this.props.cookie.startsWith("user=")))
+    ) {
+      this.props.dispatchActionPromise(pingActionType, this.props.ping());
+    }
+    this.currentState = nextAppState;
+  }
 
   render() {
     const navigation = addNavigationHelpers({
@@ -69,8 +114,12 @@ class AppWithNavigationState extends React.PureComponent {
 
 const ConnectedAppWithNavigationState = connect(
   (state: AppState) => ({
+    cookie: state.cookie,
     navigationState: state.navInfo.navigationState,
+    loggedIn: !!state.userInfo,
   }),
+  includeDispatchActionProps({ dispatchActionPromise: true }),
+  bindServerCalls({ ping }),
 )(AppWithNavigationState);
 const App = (props: {}) =>
   <Provider store={store}>
