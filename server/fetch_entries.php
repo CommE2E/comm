e5fcc1e0c2f0f0d2559596ebf6e6f067bb0b2add
ashoat <ashoat@gmail.com>
2017-04-18 20:33:33 -0400

    Replace day_history.php and month_entries.php with fetch_entries.php

--- server/fetch_entries.php
+++ server/fetch_entries.php
@@ -7,9 +7,12 @@ require_once('calendar_lib.php');
 
 async_start();
 
+// Be careful with the regex below; bad validation could lead to SQL injection
 if (
-  !isset($_POST['month']) ||
-  !isset($_POST['year']) ||
+  !isset($_POST['start_date']) ||
+  !isset($_POST['end_date']) ||
+  !preg_match("/^[0-9]{4}-[0-1][0-9]-[0-3][0-9]$/", $_POST['start_date']) ||
+  !preg_match("/^[0-9]{4}-[0-1][0-9]-[0-3][0-9]$/", $_POST['end_date']) ||
   !isset($_POST['nav'])
 ) {
   async_end(array(
@@ -17,8 +20,9 @@ if (
   ));
 }
 
-$month = intval($_POST['month']);
-$year = intval($_POST['year']);
+$start_date = $_POST['start_date'];
+$end_date = $_POST['end_date'];
+$include_deleted = !empty($_POST['include_deleted']);
 
 $home = null;
 $calendar = null;
@@ -30,17 +34,19 @@ if ($_POST['nav'] === "home") {
 }
 
 $additional_condition = $home ? "r.subscribed = 1" : "d.calendar = $calendar";
+$deleted_condition = $include_deleted ? "" : "AND e.deleted = 0 ";
 $viewer_id = get_viewer_id();
 $result = $conn->query(
-  "SELECT e.id, DAY(d.date) AS day, e.text, e.creation_time AS creationTime, ".
-    "d.calendar AS calendarID, e.deleted, u.username AS creator ".
+  "SELECT DAY(d.date) AS day, MONTH(d.date) AS month, YEAR(d.date) AS year, ".
+    "e.id, e.text, e.creation_time AS creationTime, d.calendar AS calendarID, ".
+    "e.deleted, u.username AS creator ".
     "FROM entries e ".
     "LEFT JOIN days d ON d.id = e.day ".
     "LEFT JOIN calendars c ON c.id = d.calendar ".
     "LEFT JOIN roles r ON r.calendar = d.calendar AND r.user = $viewer_id ".
     "LEFT JOIN users u ON u.id = e.creator ".
-    "WHERE MONTH(d.date) = $month AND YEAR(d.date) = $year AND ".
-    "e.deleted = 0 AND (c.visibility_rules < ".VISIBILITY_CLOSED." OR ".
+    "WHERE d.date BETWEEN '$start_date' AND '$end_date' $deleted_condition".
+    "AND (c.visibility_rules < ".VISIBILITY_CLOSED." OR ".
     "(r.calendar IS NOT NULL AND r.role >= ".ROLE_SUCCESSFUL_AUTH.")) AND ".
     $additional_condition." ".
     "ORDER BY e.creation_time DESC"
@@ -62,8 +68,8 @@ if ($calendar !== null) {
 $entries = array();
 while ($row = $result->fetch_assoc()) {
   $row['day'] = intval($row['day']);
-  $row['year'] = $year;
-  $row['month'] = $month;
+  $row['year'] = intval($row['year']);
+  $row['month'] = intval($row['month']);
   $row['creationTime'] = intval($row['creationTime']);
   $row['deleted'] = (bool)$row['deleted'];
   $row['creator'] = $row['creator'] ?: null;
