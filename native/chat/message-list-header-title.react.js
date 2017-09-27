8274160365841dac0f66f7d7f1ad94f5f7ef0e0e
ashoat <ashoat@gmail.com>
2017-09-27 15:52:13 -0400

    Simplify MessageListHeaderTitle logic and make sure forward caret always appears on Android

--- native/chat/message-list-header-title.react.js
+++ native/chat/message-list-header-title.react.js
@@ -98,24 +98,13 @@ const styles = StyleSheet.create({
     bottom: 0,
   },
   container: {
-    backgroundColor: 'transparent',
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
+    flex: 1,
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: Platform.select({
-      android: 'flex-start',
-      default: 'center',
-    }),
+    justifyContent: 'center',
   },
   forwardIcon: {
-    flex: Platform.select({
-      android: undefined,
-      default: 1,
-    }),
+    flex: 1,
     minWidth: 25,
   },
   fakeIcon: {
