5ea574faecef7fc6c6cfd6d8dcca64aad551974e
ashoat <ashoat@gmail.com>
2017-11-12 00:10:19 -0500

    Minor

--- native/chat/settings/thread-settings.react.js
+++ native/chat/settings/thread-settings.react.js
@@ -39,7 +39,6 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import _every from 'lodash/fp/every';
 import _omit from 'lodash/fp/omit';
 import shallowequal from 'shallowequal';
-import 'react-native-popover-tooltip';
 
 import { visibilityRules } from 'lib/types/thread-types';
 import {
