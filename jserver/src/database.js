412e68d5bad9c7d73fd7ba52343a8371dc5710c9
ashoat <ashoat@gmail.com>
2017-12-27 14:15:37 -0500

    Fetch unread counts from MySQL in node.js notifs logic

---
+++ jserver/src/database.js
@@ -0,0 +1,25 @@
+// @flow
+
+import mysql from 'mysql2/promise';
+import SQL from 'sql-template-strings';
+
+import dbConfig from '../secrets/db_config';
+
+export type QueryResult = [
+  any[],
+  any[],
+];
+
+export type Connection = {
+  query(query: string): Promise<QueryResult>;
+  end(): Promise<void>;
+};
+
+async function connect(): Promise<Connection> {
+  return await mysql.createConnection(dbConfig);
+}
+
+export {
+  connect,
+  SQL,
+};
--- jserver/src/push/ios_notifs.js
+++ jserver/src/push/ios_notifs.js
@@ -5,7 +5,8 @@ import type { RawMessageInfo } from 'lib/types/message-types';
 
 import apn from 'apn';
 
-import apnConfig from '../../keys/apn_config';
+import apnConfig from '../../secrets/apn_config';
+import { connect, SQL } from '../database';
 
 type IOSPushUserInfo = {
   deviceTokens: string[],
@@ -17,20 +18,48 @@ const apnProvider = new apn.Provider(apnConfig);
 
 async function sendIOSPushNotifs(req: $Request, res: $Response) {
   const pushInfo: IOSPushInfo = req.body;
+  const unreadCounts = await getUnreadCounts(Object.keys(pushInfo));
+
   const promises = [];
   for (let userID in pushInfo) {
     for (let messageInfo of pushInfo[userID].messageInfos) {
       const notification = new apn.Notification();
       notification.alert = "Hello, world!";
       notification.topic = "org.squadcal.app";
-      notification.badge = 0;
+      notification.badge = unreadCounts[userID];
       promises.push(apnProvider.send(
         notification,
         pushInfo[userID].deviceTokens,
       ));
     }
   }
+
   return await Promise.all(promises);
+}
+
+async function getUnreadCounts(
+  userIDs: string[],
+): Promise<{ [userID: string]: number }> {
+  const conn = await connect();
+  const intUserIDs = userIDs.map(parseInt);
+  const query = SQL`
+    SELECT user, COUNT(thread) AS unread_count
+    FROM memberships
+    WHERE user IN (${intUserIDs}) AND unread = 1 AND role != 0
+    GROUP BY user
+  `;
+  const [ result ] = await conn.query(query);
+  conn.end();
+  const usersToUnreadCounts = {};
+  for (let row of result) {
+    usersToUnreadCounts[row.user.toString()] = row.unread_count;
+  }
+  for (let userID of userIDs) {
+    if (usersToUnreadCounts[userID] === undefined) {
+      usersToUnreadCounts[userID] = 0;
+    }
+  }
+  return usersToUnreadCounts;
 }
 
 export {
