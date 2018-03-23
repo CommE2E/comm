2ae105054d4f420e9b458345ae52b17e5714c084
ashoat <ashoat@gmail.com>
2018-03-22 22:53:32 -0400

    Update to code version.... 3

--- native/persist.js
+++ native/persist.js
@@ -34,7 +34,7 @@ const persistConfig = {
   migrate: createMigrate(migrations, { debug: __DEV__ }),
 };
 
-const codeVersion = 0;
+const codeVersion = 3;
 
 // This local exists to avoid a circular dependency where redux-setup needs to
 // import all the navigation and screen stuff, but some of those screens want to
