2c0c382660fa205b84b5ee8a9411aea6c2cb5abc
ashoat <ashoat@gmail.com>
2018-03-27 10:27:14 -0400

    If a keyboard event occurs right before the app is foregrounded, trigger it
    We'll accept a 500ms delay

--- native/keyboard.js
+++ native/keyboard.js
@@ -12,10 +12,32 @@ export type KeyboardEvent = {
   },
 };
 type KeyboardCallback = (event: KeyboardEvent) => void;
+type IgnoredKeyboardEvent = {|
+  callback: KeyboardCallback,
+  event: KeyboardEvent,
+  time: number,
+|};
+// If the app becomes active within 500ms after a keyboard event is triggered,
+// we will call the relevant keyboard callbacks.
+const appStateChangeDelay = 500;
 
 let currentState = AppState.currentState;
+let recentIgnoredKeyboardEvents: IgnoredKeyboardEvent[] = [];
 function handleAppStateChange(nextAppState: ?string) {
   currentState = nextAppState;
+
+  const time = Date.now();
+  const ignoredEvents = recentIgnoredKeyboardEvents;
+  recentIgnoredKeyboardEvents = [];
+
+  if (currentState !== "active") {
+    return;
+  }
+  for (let ignoredEvent of ignoredEvents) {
+    if (ignoredEvent.time + appStateChangeDelay > time) {
+      ignoredEvent.callback(ignoredEvent.event);
+    }
+  }
 }
 
 let listenersEnabled = 0;
@@ -40,6 +62,8 @@ function callCallbackIfAppActive(callback: KeyboardCallback): KeyboardCallback {
   return (event: KeyboardEvent) => {
     if (currentState === "active") {
       callback(event);
+    } else {
+      recentIgnoredKeyboardEvents.push({ callback, event, time: Date.now() });
     }
   }
 }
