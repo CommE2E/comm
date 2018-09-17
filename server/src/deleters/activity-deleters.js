ab7a296c91e5bee8efbd664f4878062b785bc377
Ashoat Tevosyan <ashoat@gmail.com>
2018-09-17 11:14:17 -0400

    [server] Delete expired rows from the focused table in the nightly cronjob

--- server/src/deleters/activity-deleters.js
+++ server/src/deleters/activity-deleters.js
@@ -1,15 +1,18 @@
 // @flow
 
+import { earliestTimeConsideredExpired } from 'lib/shared/ping-utils';
+
 import { dbQuery, SQL } from '../database';
 
 async function deleteOrphanedFocused(): Promise<void> {
+  const time = earliestTimeConsideredExpired();
   await dbQuery(SQL`
     DELETE f
     FROM focused f
     LEFT JOIN threads t ON t.id = f.thread
     LEFT JOIN users u ON u.id = f.user
     LEFT JOIN sessions s ON s.id = f.session
-    WHERE t.id IS NULL OR u.id IS NULL OR s.id IS NULL
+    WHERE t.id IS NULL OR u.id IS NULL OR s.id IS NULL OR f.time <= ${time}
   `);
 }
 
