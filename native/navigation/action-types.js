fd0f8ec680c908631d7ac8e25e87fb215adb9276
Ashoat Tevosyan <ashoat@gmail.com>
2018-10-21 15:56:35 -0400

    Redux state for queuing activity updates

--- native/navigation/action-types.js
+++ native/navigation/action-types.js
@@ -5,6 +5,8 @@ export const navigateToAppActionType = "NAVIGATE_TO_APP";
 export const backgroundActionType = "BACKGROUND";
 export const foregroundActionType = "FOREGROUND";
 export const resetUserStateActionType = "RESET_USER_STATE";
-export const recordNotifPermissionAlertActionType = "RECORD_NOTIF_PERMISSION_ALERT";
-export const recordAndroidNotificationActionType = "RECORD_ANDROID_NOTIFICATION";
+export const recordNotifPermissionAlertActionType =
+  "RECORD_NOTIF_PERMISSION_ALERT";
+export const recordAndroidNotificationActionType =
+  "RECORD_ANDROID_NOTIFICATION";
 export const clearAndroidNotificationActionType = "CLEAR_ANDROID_NOTIFICATION";
