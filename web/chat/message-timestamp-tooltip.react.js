6ba8bbffaeaa69abdd09104afe295f7fe32ad98b
Ashoat Tevosyan <ashoat@gmail.com>
2019-02-02 21:28:08 -0500

    [web] Add an arrow to MessageTimestampTooltip

--- web/chat/message-timestamp-tooltip.react.js
+++ web/chat/message-timestamp-tooltip.react.js
@@ -1,11 +1,15 @@
 // @flow
 
 import type { MessagePositionInfo } from './message.react';
+import { isComposableMessageType } from 'lib/types/message-types';
 
 import * as React from 'react';
+import invariant from 'invariant';
+import classNames from 'classnames';
 
 import { longAbsoluteDate } from 'lib/utils/date-utils';
 
+import { calculateTextWidth } from '../utils/text-utils';
 import css from './chat-message-list.css';
 
 type Props = {|
@@ -16,9 +20,64 @@ function MessageTimestampTooltip(props: Props) {
     return null;
   }
   const { item, messagePosition } = props.messagePositionInfo;
-  const style = { left: messagePosition.left, top: messagePosition.top };
+  const text = longAbsoluteDate(item.messageInfo.time);
+
+  const font =
+    '14px -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", ' +
+    '"Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", ' +
+    '"Helvetica Neue", sans-serif';
+  const textWidth = calculateTextWidth(text, font);
+  const width = textWidth + 10; // 10px padding
+  const widthWithArrow = width + 10; // 7px arrow + 3px extra
+  const height = 27; // 17px line-height + 10px padding
+  const heightWithArrow = height + 10; // 7px arrow + 3px extra
+
+  const { isViewer } = item.messageInfo.creator;
+  const isComposed = isComposableMessageType(item.messageInfo.type);
+  let align = isComposed && isViewer ? "right" : "left";
+  if (align === "right" && messagePosition.right + width > window.innerWidth) {
+    align = "top-right";
+  } else if (align === "left" && messagePosition.left - width < 0) {
+    align = "top-left";
+  }
+
+  let style, className;
+  if (align === "left") {
+    const centerOfMessage = messagePosition.top + messagePosition.height / 2;
+    const topOfTooltip = centerOfMessage - height / 2;
+    style = {
+      left: messagePosition.left - widthWithArrow,
+      top: topOfTooltip,
+    };
+    className = css.messageTimestampLeftTooltip;
+  } else if (align === "right") {
+    const centerOfMessage = messagePosition.top + messagePosition.height / 2;
+    const topOfTooltip = centerOfMessage - height / 2;
+    style = {
+      right: messagePosition.right + widthWithArrow,
+      top: topOfTooltip,
+    };
+    className = css.messageTimestampRightTooltip;
+  } else if (align === "top-left") {
+    style = {
+      left: messagePosition.left,
+      top: messagePosition.top - heightWithArrow,
+    };
+    className = css.messageTimestampTopLeftTooltip;
+  } else if (align === "top-right") {
+    style = {
+      left: messagePosition.right - width,
+      top: messagePosition.top - heightWithArrow,
+    };
+    className = css.messageTimestampTopRightTooltip;
+  }
+  invariant(style, "should be set");
+
   return (
-    <div className={css.messageTimestampTooltip} style={style}>
+    <div
+      className={classNames(css.messageTimestampTooltip, className)}
+      style={style}
+    >
       {longAbsoluteDate(item.messageInfo.time)}
     </div>
   );
---
+++ web/utils/text-utils.js
@@ -0,0 +1,17 @@
+// @flow
+
+let canvas;
+
+function calculateTextWidth(text: string, font: string): number {
+  if (!canvas) {
+    canvas = document.createElement("canvas");
+  }
+  const context = canvas.getContext("2d");
+  context.font = font;
+  const { width } = context.measureText(text);
+  return width;
+}
+
+export {
+  calculateTextWidth,
+};
