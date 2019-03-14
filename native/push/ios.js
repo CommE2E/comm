4e1e5c28ffa19a79b7855cc7309c59e99c36be7d
Ashoat Tevosyan <ashoat@gmail.com>
2019-03-14 16:53:03 -0400

    [native] Always request iOS device token on first run
    This can change without getting updated in Redux, such as if an iOS backup is restored to a new phone.

--- native/push/ios.js
+++ native/push/ios.js
@@ -5,14 +5,18 @@ import NotificationsIOS from 'react-native-notifications';
 type PushPermissions = { alert?: bool, badge?: bool, sound?: bool };
 
 let currentlyActive = false;
+let firstRun = true;
 
 async function requestIOSPushPermissions(missingDeviceToken: bool) {
-  let permissionNeeded = missingDeviceToken;
+  let permissionNeeded = firstRun || missingDeviceToken;
+  firstRun = false;
+
   if (!permissionNeeded) {
     const permissions: PushPermissions =
       await NotificationsIOS.checkPermissions();
     permissionNeeded = permissionMissing(permissions);
   }
+
   if (permissionNeeded) {
     if (currentlyActive) {
       return;
@@ -20,6 +24,7 @@ async function requestIOSPushPermissions(missingDeviceToken: bool) {
     currentlyActive = true;
     await NotificationsIOS.requestPermissions();
   }
+
   NotificationsIOS.consumeBackgroundQueue();
 }
 
