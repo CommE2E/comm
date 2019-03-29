97d37ad0a941a6ea39b7eee19497ae0806831715
Ashoat Tevosyan <ashoat@gmail.com>
2019-03-29 16:57:23 -0400

    [native] Pinching and panning the MultimediaModal

--- native/media/multimedia-modal.react.js
+++ native/media/multimedia-modal.react.js
@@ -26,6 +26,11 @@ import {
 } from 'react-native';
 import PropTypes from 'prop-types';
 import Icon from 'react-native-vector-icons/Feather';
+import {
+  PinchGestureHandler,
+  PanGestureHandler,
+  State as GestureState,
+} from 'react-native-gesture-handler';
 
 import { connect } from 'lib/utils/redux-utils';
 
@@ -82,6 +87,140 @@ class MultimediaModal extends React.PureComponent<Props> {
     screenDimensions: dimensionsPropType.isRequired,
   };
 
+  pinchScale = new Animated.Value(1);
+  pinchFocalX = new Animated.Value(0);
+  pinchFocalY = new Animated.Value(0);
+  pinchEvent = Animated.event(
+    [{
+      nativeEvent: {
+        scale: this.pinchScale,
+        focalX: this.pinchFocalX,
+        focalY: this.pinchFocalY,
+      },
+    }],
+    { useNativeDriver: true },
+  );
+
+  panX = new Animated.Value(0);
+  panY = new Animated.Value(0);
+  panEvent = Animated.event(
+    [{
+      nativeEvent: {
+        translationX: this.panX,
+        translationY: this.panY,
+      },
+    }],
+    { useNativeDriver: true },
+  );
+
+  curScaleNum = 1;
+  curXNum = 0;
+  curYNum = 0;
+  curScale = new Animated.Value(1);
+  curX = new Animated.Value(0);
+  curY = new Animated.Value(0);
+
+  progress: Animated.Value;
+  scale: Animated.Value;
+  pinchX: Animated.Value;
+  pinchY: Animated.Value;
+  x: Animated.Value;
+  y: Animated.Value;
+  imageContainerOpacity: Animated.Value;
+
+  constructor(props: Props) {
+    super(props);
+
+    const { height, width } = this.imageDimensions;
+    const { height: screenHeight, width: screenWidth } = this.screenDimensions;
+    const top = (screenHeight - height) / 2 + contentVerticalOffset;
+    const left = (screenWidth - width) / 2;
+
+    const { initialCoordinates } = props.navigation.state.params;
+    const initialScale = new Animated.Value(initialCoordinates.width / width);
+    const initialTranslateX = new Animated.Value(
+      (initialCoordinates.x + initialCoordinates.width / 2)
+        - (left + width / 2),
+    );
+    const initialTranslateY = new Animated.Value(
+      (initialCoordinates.y + initialCoordinates.height / 2)
+        - (top + height / 2),
+    );
+
+    const { position } = props.transitionProps;
+    const { index } = props.scene;
+    this.progress = position.interpolate({
+      inputRange: [ index - 1, index ],
+      outputRange: ([ 0, 1 ]: number[]),
+      extrapolate: 'clamp',
+    });
+    this.imageContainerOpacity = this.progress.interpolate({
+      inputRange: [ 0, 0.1 ],
+      outputRange: ([ 0, 1 ]: number[]),
+      extrapolate: 'clamp',
+    });
+
+    const reverseProgress = Animated.subtract(1, this.progress);
+    this.scale = Animated.add(
+      Animated.multiply(reverseProgress, initialScale),
+      Animated.multiply(
+        this.progress,
+        Animated.multiply(this.curScale, this.pinchScale),
+      ),
+    );
+
+    this.pinchX = Animated.multiply(
+      Animated.subtract(1, this.pinchScale),
+      Animated.subtract(
+        Animated.subtract(
+          this.pinchFocalX,
+          this.curX,
+        ),
+        Animated.divide(
+          this.props.transitionProps.layout.width,
+          2,
+        ),
+      ),
+    );
+    this.x = Animated.add(
+      Animated.multiply(reverseProgress, initialTranslateX),
+      Animated.multiply(
+        this.progress,
+        Animated.add(
+          this.curX,
+          Animated.add(this.pinchX, this.panX),
+        ),
+      ),
+    );
+
+    this.pinchY = Animated.multiply(
+      Animated.subtract(1, this.pinchScale),
+      Animated.subtract(
+        Animated.subtract(
+          this.pinchFocalY,
+          this.curY,
+        ),
+        Animated.divide(
+          Animated.add(
+            this.props.transitionProps.layout.height,
+            contentVerticalOffset - contentBottomOffset,
+          ),
+          2,
+        ),
+      ),
+    );
+    this.y = Animated.add(
+      Animated.multiply(reverseProgress, initialTranslateY),
+      Animated.multiply(
+        this.progress,
+        Animated.add(
+          this.curY,
+          Animated.add(this.pinchY, this.panY),
+        ),
+      ),
+    );
+  }
+
   get screenDimensions(): Dimensions {
     const { screenDimensions } = this.props;
     if (contentVerticalOffset === 0) {
@@ -118,58 +257,17 @@ class MultimediaModal extends React.PureComponent<Props> {
     const { height: screenHeight, width: screenWidth } = this.screenDimensions;
     const top = (screenHeight - height) / 2 + contentVerticalOffset;
     const left = (screenWidth - width) / 2;
-
-    const { position } = this.props.transitionProps;
-    const { index } = this.props.scene;
-    const opacity = position.interpolate({
-      inputRange: [ index - 1, index - 0.9 ],
-      outputRange: ([ 0, 1 ]: number[]),
-      extrapolate: 'clamp',
-    });
-
-    const { initialCoordinates } = this.props.navigation.state.params;
-    const initialScaleX = initialCoordinates.width / width;
-    const scaleX = position.interpolate({
-      inputRange: [ index - 1, index ],
-      outputRange: ([ initialScaleX, 1 ]: number[]),
-      extrapolate: 'clamp',
-    });
-    const initialScaleY = initialCoordinates.height / height;
-    const scaleY = position.interpolate({
-      inputRange: [ index - 1, index ],
-      outputRange: ([ initialScaleY, 1 ]: number[]),
-      extrapolate: 'clamp',
-    });
-
-    const initialTranslateX =
-      (initialCoordinates.x + initialCoordinates.width / 2)
-      - (left + width / 2);
-    const translateX = position.interpolate({
-      inputRange: [ index - 1, index ],
-      outputRange: ([ initialTranslateX, 0 ]: number[]),
-      extrapolate: 'clamp',
-    });
-    const initialTranslateY =
-      (initialCoordinates.y + initialCoordinates.height / 2)
-      - (top + height / 2);
-    const translateY = position.interpolate({
-      inputRange: [ index - 1, index ],
-      outputRange: ([ initialTranslateY, 0 ]: number[]),
-      extrapolate: 'clamp',
-    });
-
     const { verticalBounds } = this.props.navigation.state.params;
     return {
       height,
       width,
       marginTop: top - verticalBounds.y,
       marginLeft: left,
-      opacity,
+      opacity: this.imageContainerOpacity,
       transform: [
-        { translateX },
-        { translateY },
-        { scaleX },
-        { scaleY },
+        { translateX: this.x },
+        { translateY: this.y },
+        { scale: this.scale },
       ],
     };
   }
@@ -194,26 +292,16 @@ class MultimediaModal extends React.PureComponent<Props> {
     return [ styles.contentContainer, verticalStyle ];
   }
 
-  get backdropStyle() {
-    const { position } = this.props.transitionProps;
-    const { index } = this.props.scene;
-    return {
-      opacity: position.interpolate({
-        inputRange: [ index - 1, index ],
-        outputRange: ([ 0, 1 ]: number[]),
-      }),
-    };
-  }
-
   render() {
     const { media } = this.props.navigation.state.params;
     const statusBar = this.isActive
       ? <ConnectedStatusBar barStyle="light-content" />
       : null;
-    return (
-      <View style={styles.container}>
+    const backdropStyle = { opacity: this.progress };
+    const view = (
+      <Animated.View style={styles.container}>
         {statusBar}
-        <Animated.View style={[ styles.backdrop, this.backdropStyle ]} />
+        <Animated.View style={[ styles.backdrop, backdropStyle ]} />
         <View style={this.contentContainerStyle}>
           <TouchableWithoutFeedback onPress={this.close}>
             <View style={styles.cover} />
@@ -222,20 +310,83 @@ class MultimediaModal extends React.PureComponent<Props> {
             <Multimedia media={media} spinnerColor="white" />
           </Animated.View>
         </View>
-        <Animated.View style={[
-          styles.closeButtonContainer,
-          this.backdropStyle,
-        ]}>
+        <Animated.View style={[ styles.closeButtonContainer, backdropStyle ]}>
           <TouchableOpacity onPress={this.close}>
             <Icon name="x-circle" style={styles.closeButton} />
           </TouchableOpacity>
         </Animated.View>
-      </View>
+      </Animated.View>
+    );
+    return (
+      <PinchGestureHandler
+        onGestureEvent={this.pinchEvent}
+        onHandlerStateChange={this.onPinchHandlerStateChange}
+      >
+        <Animated.View style={styles.container}>
+          <PanGestureHandler
+            onGestureEvent={this.panEvent}
+            onHandlerStateChange={this.onPanHandlerStateChange}
+          >
+            {view}
+          </PanGestureHandler>
+        </Animated.View>
+      </PinchGestureHandler>
     );
   }
 
   close = () => {
     this.props.navigation.goBack();
+  }
+
+  onPinchHandlerStateChange = (
+    event: { nativeEvent: {
+      state: number,
+      oldState: number,
+      scale: number,
+      focalX: number,
+      focalY: number,
+    } },
+  ) => {
+    const { state, oldState, scale, focalX, focalY } = event.nativeEvent;
+    if (state === GestureState.ACTIVE || oldState !== GestureState.ACTIVE) {
+      return;
+    }
+    this.curScaleNum *= event.nativeEvent.scale;
+    this.curScale.setValue(this.curScaleNum);
+
+    // Keep this logic in sync with pinchX/pinchY definitions in constructor.
+    // Note however that this.screenDimensions.height is not the same as
+    // this.props.transitionProps.layout.height. The latter includes both
+    // contentVerticalOffset and contentBottomOffset.
+    const { height: screenHeight, width: screenWidth } = this.screenDimensions;
+    this.curXNum += (1 - scale)
+      * (focalX - this.curXNum - screenWidth / 2);
+    this.curYNum += (1 - scale)
+      * (focalY - this.curYNum - screenHeight / 2 - contentVerticalOffset);
+    this.curX.setValue(this.curXNum);
+    this.curY.setValue(this.curYNum);
+
+    this.pinchScale.setValue(1);
+  }
+
+  onPanHandlerStateChange = (
+    event: { nativeEvent: {
+      state: number,
+      oldState: number,
+      translationX: number,
+      translationY: number,
+    } },
+  ) => {
+    const { state, oldState } = event.nativeEvent;
+    if (state === GestureState.ACTIVE || oldState !== GestureState.ACTIVE) {
+      return;
+    }
+    this.curXNum += event.nativeEvent.translationX;
+    this.curYNum += event.nativeEvent.translationY;
+    this.curX.setValue(this.curXNum);
+    this.curY.setValue(this.curYNum);
+    this.panX.setValue(0);
+    this.panY.setValue(0);
   }
 
 }
