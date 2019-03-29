72af7b9f71f25679ad96ce7545aac16e467178e3
Ashoat Tevosyan <ashoat@gmail.com>
2019-03-29 19:21:03 -0400

    [native] Fix react-native-gesture-handler on Android

--- native/android/app/src/main/java/org/squadcal/MainActivity.java
+++ native/android/app/src/main/java/org/squadcal/MainActivity.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.content.res.Configuration;
 
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
 public class MainActivity extends ReactFragmentActivity {
 
   /**
@@ -35,6 +39,16 @@ public class MainActivity extends ReactFragmentActivity {
       Intent intent = new Intent("onConfigurationChanged");
       intent.putExtra("newConfig", newConfig);
       this.sendBroadcast(intent);
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+       return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
   }
 
 }
