CommE2E/comm

commit 6ef25752b36aac5f3c69b2532ac259896b71e768

[native] Convert business logic to use Redux dimensions

native/account/log-in-panel-container.react.js
@@ -6,8 +6,10 @@ import {
   stateContainerPropType,
 } from '../utils/state-container';
 import type { LogInState } from './log-in-panel.react';
+import type { AppState } from '../redux-setup';
+import { type Dimensions, dimensionsPropType } from '../types/dimensions';
 
-import React from 'react';
+import * as React from 'react';
 import {
   View,
   Animated,
@@ -20,25 +22,28 @@ import invariant from 'invariant';
 import PropTypes from 'prop-types';
 
 import sleep from 'lib/utils/sleep';
+import { connect } from 'lib/utils/redux-utils';
 
-import { windowWidth } from '../dimensions';
+import { dimensionsSelector } from '../selectors/dimension-selectors';
 import LogInPanel from './log-in-panel.react';
 import ForgotPasswordPanel from './forgot-password-panel.react';
 
 type LogInMode = "log-in" | "forgot-password" | "forgot-password-success";
 
-type Props = {
+type Props = {|
   onePasswordSupported: bool,
   setActiveAlert: (activeAlert: bool) => void,
   opacityValue: Animated.Value,
   forgotPasswordLinkOpacity: Animated.Value,
   logInState: StateContainer<LogInState>,
-};
-type State = {
+  // Redux state
+  dimensions: Dimensions,
+|};
+type State = {|
   panelTransition: Animated.Value,
   logInMode: LogInMode,
   nextLogInMode: LogInMode,
-};
+|};
 class LogInPanelContainer extends React.PureComponent<Props, State> {
 
   static propTypes = {
@@ -47,6 +52,7 @@ class LogInPanelContainer extends React.PureComponent<Props, State> {
     opacityValue: PropTypes.object.isRequired,
     forgotPasswordLinkOpacity: PropTypes.object.isRequired,
     logInState: stateContainerPropType.isRequired,
+    dimensions: dimensionsPropType.isRequired,
   };
   state = {
     panelTransition: new Animated.Value(0),
@@ -56,6 +62,7 @@ class LogInPanelContainer extends React.PureComponent<Props, State> {
   logInPanel: ?InnerLogInPanel = null;
 
   render() {
+    const windowWidth = this.props.dimensions.width;
     const logInPanelDynamicStyle = {
       left: this.state.panelTransition.interpolate({
         inputRange: [0, 2],
@@ -284,4 +291,8 @@ const styles = StyleSheet.create({
   },
 });
 
-export default LogInPanelContainer;
+export default connect(
+  (state: AppState) => ({
+    dimensions: dimensionsSelector(state),
+  }),
+)(LogInPanelContainer);

native/account/logged-out-modal.react.js
@@ -12,6 +12,8 @@ import type { KeyboardEvent, EmitterSubscription } from '../keyboard';
 import type { LogInState } from './log-in-panel.react';
 import type { RegisterState } from './register-panel.react';
 import type { LogInExtraInfo } from 'lib/types/account-types';
+import { type Dimensions, dimensionsPropType } from '../types/dimensions';
+import type { ImageStyle } from '../types/styles';
 
 import * as React from 'react';
 import {
@@ -44,10 +46,9 @@ import sleep from 'lib/utils/sleep';
 import { connect } from 'lib/utils/redux-utils';
 
 import {
-  windowHeight,
-  windowWidth,
+  dimensionsSelector,
   contentVerticalOffset,
-} from '../dimensions';
+} from '../selectors/dimension-selectors';
 import LogInPanelContainer from './log-in-panel-container.react';
 import RegisterPanel from './register-panel.react';
 import ConnectedStatusBar from '../connected-status-bar.react';
@@ -57,7 +58,7 @@ import {
   resetUserStateActionType,
 } from '../navigation/action-types';
 import { splashBackgroundURI } from './background-info';
-import { splashStyle } from '../splash';
+import { splashStyleSelector } from '../splash';
 import {
   addKeyboardShowListener,
   addKeyboardDismissListener,
@@ -83,6 +84,8 @@ type Props = {
   urlPrefix: string,
   loggedIn: bool,
   isForeground: bool,
+  dimensions: Dimensions,
+  splashStyle: ImageStyle,
   // Redux dispatch functions
   dispatch: Dispatch,
   dispatchActionPayload: DispatchActionPayload,
@@ -100,7 +103,7 @@ type State = {
   registerState: StateContainer<RegisterState>;
 };
 
-class InnerLoggedOutModal extends React.PureComponent<Props, State> {
+class LoggedOutModal extends React.PureComponent<Props, State> {
 
   static propTypes = {
     navigation: PropTypes.shape({
@@ -112,6 +115,7 @@ class InnerLoggedOutModal extends React.PureComponent<Props, State> {
     urlPrefix: PropTypes.string.isRequired,
     loggedIn: PropTypes.bool.isRequired,
     isForeground: PropTypes.bool.isRequired,
+    dimensions: dimensionsPropType.isRequired,
     dispatch: PropTypes.func.isRequired,
     dispatchActionPayload: PropTypes.func.isRequired,
     dispatchActionPromise: PropTypes.func.isRequired,
@@ -172,10 +176,10 @@ class InnerLoggedOutModal extends React.PureComponent<Props, State> {
     this.state = {
       mode: props.rehydrateConcluded ? "prompt" : "loading",
       panelPaddingTop: new Animated.Value(
-        InnerLoggedOutModal.calculatePanelPaddingTop("prompt", 0),
+        this.calculatePanelPaddingTop("prompt", 0),
       ),
       footerPaddingTop: new Animated.Value(
-        InnerLoggedOutModal.calculateFooterPaddingTop(0),
+        this.calculateFooterPaddingTop(0),
       ),
       panelOpacity: new Animated.Value(0),
       forgotPasswordLinkOpacity: new Animated.Value(0),
@@ -351,10 +355,11 @@ class InnerLoggedOutModal extends React.PureComponent<Props, State> {
     return false;
   }
 
-  static calculatePanelPaddingTop(
+  calculatePanelPaddingTop(
     mode: LoggedOutMode,
     keyboardHeight: number,
   ) {
+    const windowHeight = this.props.dimensions.height;
     let containerSize = Platform.OS === "ios" ? 62 : 59; // header height
     if (mode === "log-in") {
       // We need to make space for the reset password button on smaller devices
@@ -373,7 +378,8 @@ class InnerLoggedOutModal extends React.PureComponent<Props, State> {
     return (contentHeight - containerSize - keyboardHeight) / 2;
   }
 
-  static calculateFooterPaddingTop(keyboardHeight: number) {
+  calculateFooterPaddingTop(keyboardHeight: number) {
+    const windowHeight = this.props.dimensions.height;
     const textHeight = Platform.OS === "ios" ? 17 : 19;
     if (DeviceInfo.isIPhoneX_deprecated) {
       keyboardHeight -= 34;
@@ -391,7 +397,7 @@ class InnerLoggedOutModal extends React.PureComponent<Props, State> {
     }
     const animations = [];
     const newPanelPaddingTopValue =
-      InnerLoggedOutModal.calculatePanelPaddingTop(
+      this.calculatePanelPaddingTop(
         this.state.mode,
         this.keyboardHeight,
       );
@@ -414,7 +420,7 @@ class InnerLoggedOutModal extends React.PureComponent<Props, State> {
         {
           duration,
           easing: Easing.out(Easing.ease),
-          toValue: InnerLoggedOutModal.calculateFooterPaddingTop(
+          toValue: this.calculateFooterPaddingTop(
             realKeyboardHeight,
           ),
         },
@@ -467,7 +473,7 @@ class InnerLoggedOutModal extends React.PureComponent<Props, State> {
   animateKeyboardDownOrBackToPrompt(inputDuration: ?number) {
     const duration = inputDuration ? inputDuration : 250;
     const newLastPanelPaddingTopValue =
-      InnerLoggedOutModal.calculatePanelPaddingTop(
+      this.calculatePanelPaddingTop(
         this.nextMode,
         0,
       );
@@ -486,7 +492,7 @@ class InnerLoggedOutModal extends React.PureComponent<Props, State> {
         {
           duration,
           easing: Easing.out(Easing.ease),
-          toValue: InnerLoggedOutModal.calculateFooterPaddingTop(
+          toValue: this.calculateFooterPaddingTop(
             this.keyboardHeight,
           ),
         },
@@ -571,7 +577,7 @@ class InnerLoggedOutModal extends React.PureComponent<Props, State> {
     const background = (
       <Image
         source={{ uri: splashBackgroundURI }}
-        style={styles.modalBackground}
+        style={[ styles.modalBackground, this.props.splashStyle ]}
       />
     );
 
@@ -651,13 +657,18 @@ class InnerLoggedOutModal extends React.PureComponent<Props, State> {
       );
     }
 
+    const windowWidth = this.props.dimensions.width;
+    const buttonStyle = {
+      opacity: this.state.panelOpacity,
+      left: windowWidth < 360 ? 28 : 40,
+    };
     const padding = { paddingTop: this.state.panelPaddingTop };
-    const opacity = { opacity: this.state.panelOpacity };
+
     const animatedContent = (
       <Animated.View style={[styles.animationContainer, padding]}>
         <View>
           <Text style={styles.header}>SquadCal</Text>
-          <Animated.View style={[styles.backButton, opacity]}>
+          <Animated.View style={[ styles.backButton, buttonStyle ]}>
             <TouchableOpacity activeOpacity={0.6} onPress={this.hardwareBack}>
               <Icon
                 name="arrow-circle-o-left"
@@ -688,8 +699,10 @@ class InnerLoggedOutModal extends React.PureComponent<Props, State> {
         </React.Fragment>
       );
     } else {
+      // https://github.com/facebook/react-native/issues/6785
+      const topContainerStyle = { height: this.props.dimensions.height };
       return (
-        <View style={styles.topContainer}>
+        <View style={[ styles.androidTopContainer, topContainerStyle ]}>
           {background}
 					<View style={styles.container}>
 						{statusBar}
@@ -746,12 +759,9 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     right: 0,
-    ...splashStyle,
   },
-  topContainer: {
+  androidTopContainer: {
     backgroundColor: 'transparent',
-		// https://github.com/facebook/react-native/issues/6785
-    height: windowHeight,
   },
   container: {
     flex: 1,
@@ -768,7 +778,6 @@ const styles = StyleSheet.create({
   },
   backButton: {
     position: 'absolute',
-    left: windowWidth < 360 ? 28 : 40,
     top: 13,
   },
   buttonContainer: {
@@ -811,7 +820,7 @@ const styles = StyleSheet.create({
 
 const isForegroundSelector =
   createIsForegroundSelector(LoggedOutModalRouteName);
-const LoggedOutModal = connect(
+export default connect(
   (state: AppState) => ({
     rehydrateConcluded: state._persist && state._persist.rehydrated,
     cookie: state.cookie,
@@ -819,9 +828,9 @@ const LoggedOutModal = connect(
     loggedIn: !!(state.currentUserInfo &&
       !state.currentUserInfo.anonymous && true),
     isForeground: isForegroundSelector(state),
+    dimensions: dimensionsSelector(state),
+    splashStyle: splashStyleSelector(state),
   }),
   null,
   true,
-)(InnerLoggedOutModal);
-
-export default LoggedOutModal;
+)(LoggedOutModal);

native/components/tag-input.react.js
@@ -1,6 +1,8 @@
 // @flow
 
 import type { ViewStyle, TextStyle } from '../types/styles';
+import { type Dimensions, dimensionsPropType } from '../types/dimensions';
+import type { AppState } from '../redux-setup';
 
 import * as React from 'react';
 import PropTypes from 'prop-types';
@@ -10,7 +12,6 @@ import {
   TextInput,
   StyleSheet,
   TouchableOpacity,
-  Dimensions,
   TouchableWithoutFeedback,
   ScrollView,
   ViewPropTypes,
@@ -18,7 +19,9 @@ import {
 } from 'react-native';
 import invariant from 'invariant';
 
-const windowWidth = Dimensions.get('window').width;
+import { connect } from 'lib/utils/redux-utils';
+
+import { dimensionsSelector } from '../selectors/dimension-selectors';
 
 type Props<T> = {
   /**
@@ -86,6 +89,8 @@ type Props<T> = {
    * value and the new value, which looks sketchy.
    */
   defaultInputWidth: number,
+  // Redux state
+  dimensions: Dimensions,
 };
 type State = {
   inputWidth: number,
@@ -109,8 +114,9 @@ class TagInput<T> extends React.PureComponent<Props<T>, State> {
     maxHeight: PropTypes.number,
     onHeightChange: PropTypes.func,
     defaultInputWidth: PropTypes.number,
+    dimensions: dimensionsPropType.isRequired,
   };
-  wrapperWidth = windowWidth;
+  wrapperWidth: number;
   spaceLeft = 0;
   // scroll to bottom
   contentHeight = 0;
@@ -150,6 +156,7 @@ class TagInput<T> extends React.PureComponent<Props<T>, State> {
       inputWidth: props.defaultInputWidth,
       wrapperHeight: 36,
     };
+    this.wrapperWidth = props.dimensions.width;
   }
 
   componentWillReceiveProps(nextProps: Props<T>) {
@@ -466,4 +473,8 @@ const styles = StyleSheet.create({
   },
 });
 
-export default TagInput;
+export default connect(
+  (state: AppState) => ({
+    dimensions: dimensionsSelector(state),
+  }),
+)(TagInput);

native/account/forgot-password-panel.react.js
@@ -28,7 +28,10 @@ import {
   validEmailRegex,
 } from 'lib/shared/account-regexes';
 
-import { TextInput, usernamePlaceholder } from './modal-components.react';
+import {
+  TextInput,
+  usernamePlaceholderSelector,
+} from './modal-components.react';
 import { PanelButton, Panel } from './panel-components.react';
 
 type Props = {
@@ -37,6 +40,7 @@ type Props = {
   onSuccess: () => void,
   // Redux state
   loadingStatus: LoadingStatus,
+  usernamePlaceholder: string,
   // Redux dispatch functions
   dispatchActionPromise: DispatchActionPromise,
   // async functions that hit server APIs
@@ -52,6 +56,7 @@ class ForgotPasswordPanel extends React.PureComponent<Props, State> {
     opacityValue: PropTypes.object.isRequired,
     onSuccess: PropTypes.func.isRequired,
     loadingStatus: PropTypes.string.isRequired,
+    usernamePlaceholder: PropTypes.string.isRequired,
     dispatchActionPromise: PropTypes.func.isRequired,
     forgotPassword: PropTypes.func.isRequired,
   };
@@ -69,7 +74,7 @@ class ForgotPasswordPanel extends React.PureComponent<Props, State> {
             style={styles.input}
             value={this.state.usernameOrEmailInputText}
             onChangeText={this.onChangeUsernameOrEmailInputText}
-            placeholder={usernamePlaceholder}
+            placeholder={this.props.usernamePlaceholder}
             autoFocus={true}
             autoCorrect={false}
             autoCapitalize="none"
@@ -183,6 +188,7 @@ const loadingStatusSelector
 export default connect(
   (state: AppState) => ({
     loadingStatus: loadingStatusSelector(state),
+    usernamePlaceholder: usernamePlaceholderSelector(state),
   }),
   { forgotPassword },
 )(ForgotPasswordPanel);

native/account/log-in-panel.react.js
@@ -39,7 +39,10 @@ import {
 } from 'lib/actions/user-actions';
 import { createLoadingStatusSelector } from 'lib/selectors/loading-selectors';
 
-import { TextInput, usernamePlaceholder } from './modal-components.react';
+import {
+  TextInput,
+  usernamePlaceholderSelector,
+} from './modal-components.react';
 import {
   PanelButton,
   PanelOnePasswordButton,
@@ -64,6 +67,7 @@ type Props = {
   // Redux state
   loadingStatus: LoadingStatus,
   logInExtraInfo: () => LogInExtraInfo,
+  usernamePlaceholder: string,
   // Redux dispatch functions
   dispatchActionPromise: DispatchActionPromise,
   // async functions that hit server APIs
@@ -79,6 +83,7 @@ class LogInPanel extends React.PureComponent<Props> {
     state: stateContainerPropType.isRequired,
     loadingStatus: PropTypes.string.isRequired,
     logInExtraInfo: PropTypes.func.isRequired,
+    usernamePlaceholder: PropTypes.string.isRequired,
     dispatchActionPromise: PropTypes.func.isRequired,
     logIn: PropTypes.func.isRequired,
   };
@@ -117,7 +122,7 @@ class LogInPanel extends React.PureComponent<Props> {
             style={styles.input}
             value={this.props.state.state.usernameOrEmailInputText}
             onChangeText={this.onChangeUsernameOrEmailInputText}
-            placeholder={usernamePlaceholder}
+            placeholder={this.props.usernamePlaceholder}
             autoFocus={true}
             autoCorrect={false}
             autoCapitalize="none"
@@ -357,6 +362,7 @@ export default connect(
   (state: AppState) => ({
     loadingStatus: loadingStatusSelector(state),
     logInExtraInfo: nativeLogInExtraInfoSelector(state),
+    usernamePlaceholder: usernamePlaceholderSelector(state),
   }),
   { logIn },
 )(LogInPanel);

native/account/modal-components.react.js
@@ -1,6 +1,8 @@
 // @flow
 
-import React from 'react';
+import type { Dimensions } from '../types/dimensions';
+
+import * as React from 'react';
 import {
   TextInput as BaseTextInput,
   View,
@@ -8,8 +10,9 @@ import {
   Platform,
 } from 'react-native';
 import invariant from 'invariant';
+import { createSelector } from 'reselect';
 
-import { windowWidth } from '../dimensions';
+import { dimensionsSelector } from '../selectors/dimension-selectors';
 
 class TextInput extends React.PureComponent<*> {
   
@@ -54,11 +57,14 @@ const styles = StyleSheet.create({
   },
 });
 
-const usernamePlaceholder = windowWidth < 360
-  ? "Username or email"
-  : "Username or email address";
+const usernamePlaceholderSelector = createSelector<*, *, *, *>(
+  dimensionsSelector,
+  (dimensions: Dimensions): string => dimensions.width < 360
+    ? "Username or email"
+    : "Username or email address",
+);
 
 export {
   TextInput,
-  usernamePlaceholder,
+  usernamePlaceholderSelector,
 };

native/account/panel-components.react.js
@@ -4,6 +4,8 @@ import type { LoadingStatus } from 'lib/types/loading-types';
 import { loadingStatusPropType } from 'lib/types/loading-types';
 import type { ViewStyle } from '../types/styles';
 import type { KeyboardEvent, EmitterSubscription } from '../keyboard';
+import { type Dimensions, dimensionsPropType } from '../types/dimensions';
+import type { AppState } from '../redux-setup';
 
 import * as React from 'react';
 import {
@@ -14,13 +16,16 @@ import {
   Animated,
   ScrollView,
   LayoutAnimation,
+  ViewPropTypes,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import PropTypes from 'prop-types';
 
+import { connect } from 'lib/utils/redux-utils';
+
 import Button from '../components/button.react';
 import OnePasswordButton from '../components/one-password-button.react';
-import { windowHeight } from '../dimensions';
+import { dimensionsSelector } from '../selectors/dimension-selectors';
 import {
   addKeyboardShowListener,
   addKeyboardDismissListener,
@@ -86,12 +91,19 @@ type PanelProps = {|
   opacityValue: Animated.Value,
   children: React.Node,
   style?: ViewStyle,
+  dimensions: Dimensions,
 |};
 type PanelState = {|
   keyboardHeight: number,
 |};
-class Panel extends React.PureComponent<PanelProps, PanelState> {
+class InnerPanel extends React.PureComponent<PanelProps, PanelState> {
 
+  static propTypes = {
+    opacityValue: PropTypes.instanceOf(Animated.Value).isRequired,
+    children: PropTypes.node.isRequired,
+    style: ViewPropTypes.style,
+    dimensions: dimensionsPropType.isRequired,
+  };
   state = {
     keyboardHeight: 0,
   };
@@ -116,6 +128,7 @@ class Panel extends React.PureComponent<PanelProps, PanelState> {
   }
 
   keyboardHandler = (event: ?KeyboardEvent) => {
+    const windowHeight = this.props.dimensions.height;
     const keyboardHeight = event
       ? windowHeight - event.endCoordinates.screenY
       : 0;
@@ -139,9 +152,17 @@ class Panel extends React.PureComponent<PanelProps, PanelState> {
   }
 
   render() {
-    const opacityStyle = { opacity: this.props.opacityValue };
+    const windowHeight = this.props.dimensions.height;
+    const containerStyle = {
+      opacity: this.props.opacityValue,
+      marginTop: windowHeight < 600 ? 15 : 40,
+    };
     const content = (
-      <Animated.View style={[styles.container, opacityStyle, this.props.style]}>
+      <Animated.View style={[
+        styles.container,
+        containerStyle,
+        this.props.style,
+      ]}>
         {this.props.children}
       </Animated.View>
     );
@@ -161,6 +182,12 @@ class Panel extends React.PureComponent<PanelProps, PanelState> {
   }
 
 }
+
+const Panel = connect(
+  (state: AppState) => ({
+    dimensions: dimensionsSelector(state),
+  }),
+)(InnerPanel);
 
 const styles = StyleSheet.create({
   loadingIndicatorContainer: {
@@ -201,7 +228,6 @@ const styles = StyleSheet.create({
     paddingRight: 18,
     marginLeft: 20,
     marginRight: 20,
-    marginTop: windowHeight < 600 ? 15 : 40,
     borderRadius: 6,
     backgroundColor: '#FFFFFFAA',
   },

native/account/verification-modal.react.js
@@ -15,6 +15,8 @@ import {
   type HandleVerificationCodeResult,
 } from 'lib/types/verify-types';
 import type { KeyboardEvent } from '../keyboard';
+import { type Dimensions, dimensionsPropType } from '../types/dimensions';
+import type { ImageStyle } from '../types/styles';
 
 import * as React from 'react';
 import {
@@ -45,13 +47,13 @@ import {
 } from 'lib/actions/user-actions';
 import sleep from 'lib/utils/sleep';
 
-import { windowHeight } from '../dimensions';
+import { dimensionsSelector } from '../selectors/dimension-selectors';
 import ConnectedStatusBar from '../connected-status-bar.react';
 import ResetPasswordPanel from './reset-password-panel.react';
 import { createIsForegroundSelector } from '../selectors/nav-selectors';
 import { navigateToAppActionType } from '../navigation/action-types';
 import { splashBackgroundURI } from './background-info';
-import { splashStyle } from '../splash';
+import { splashStyleSelector } from '../splash';
 import {
   addKeyboardShowListener,
   addKeyboardDismissListener,
@@ -68,6 +70,8 @@ type Props = {
     & NavigationScreenProp<NavigationLeafRoute>,
   // Redux state
   isForeground: bool,
+  dimensions: Dimensions,
+  splashStyle: ImageStyle,
   // Redux dispatch functions
   dispatchActionPayload: DispatchActionPayload,
   dispatchActionPromise: DispatchActionPromise,
@@ -96,6 +100,7 @@ class InnerVerificationModal extends React.PureComponent<Props, State> {
       goBack: PropTypes.func.isRequired,
     }).isRequired,
     isForeground: PropTypes.bool.isRequired,
+    dimensions: dimensionsPropType.isRequired,
     dispatchActionPayload: PropTypes.func.isRequired,
     dispatchActionPromise: PropTypes.func.isRequired,
     handleVerificationCode: PropTypes.func.isRequired,
@@ -104,7 +109,7 @@ class InnerVerificationModal extends React.PureComponent<Props, State> {
   state = {
     mode: "simple-text",
     paddingTop: new Animated.Value(
-      InnerVerificationModal.currentPaddingTop("simple-text", 0),
+      this.currentPaddingTop("simple-text", 0),
     ),
     verifyField: null,
     errorMessage: null,
@@ -160,7 +165,7 @@ class InnerVerificationModal extends React.PureComponent<Props, State> {
       this.setState({
         mode: "simple-text",
         paddingTop: new Animated.Value(
-          InnerVerificationModal.currentPaddingTop("simple-text", 0),
+          this.currentPaddingTop("simple-text", 0),
         ),
         verifyField: null,
         errorMessage: null,
@@ -272,10 +277,11 @@ class InnerVerificationModal extends React.PureComponent<Props, State> {
     }
   }
 
-  static currentPaddingTop(
+  currentPaddingTop(
     mode: VerificationModalMode,
     keyboardHeight: number,
   ) {
+    const windowHeight = this.props.dimensions.height;
     let containerSize = 0;
     if (mode === "simple-text") {
       containerSize = 90;
@@ -293,7 +299,7 @@ class InnerVerificationModal extends React.PureComponent<Props, State> {
         {
           duration,
           easing: Easing.out(Easing.ease),
-          toValue: InnerVerificationModal.currentPaddingTop(
+          toValue: this.currentPaddingTop(
             this.state.mode,
             this.keyboardHeight,
           ),
@@ -338,7 +344,7 @@ class InnerVerificationModal extends React.PureComponent<Props, State> {
         {
           duration,
           easing: Easing.out(Easing.ease),
-          toValue: InnerVerificationModal.currentPaddingTop(this.nextMode, 0),
+          toValue: this.currentPaddingTop(this.nextMode, 0),
         },
       ),
     ];
@@ -385,7 +391,7 @@ class InnerVerificationModal extends React.PureComponent<Props, State> {
     const background = (
       <Image
         source={{ uri: splashBackgroundURI }}
-        style={styles.modalBackground}
+        style={[ styles.modalBackground, this.props.splashStyle ]}
       />
     );
     const closeButton = (
@@ -483,7 +489,6 @@ const styles = StyleSheet.create({
     position: 'absolute',
     width: ('100%': number | string),
     height: ('100%': number | string),
-    ...splashStyle,
   },
   container: {
     flex: 1,
@@ -529,6 +534,8 @@ const isForegroundSelector =
 const VerificationModal = connect(
   (state: AppState) => ({
     isForeground: isForegroundSelector(state),
+    dimensions: dimensionsSelector(state),
+    splashStyle: splashStyleSelector(state),
   }),
   { handleVerificationCode },
 )(InnerVerificationModal);

commit 81d383cf67a1d35d86d54b6b1378921c173f282c

[native] Fix some ref cases broken by earlier commit
6ef25752b36aac5f3c69b2532ac259896b71e768 was the culprit

native/account/log-in-panel-container.react.js
@@ -36,6 +36,7 @@ type Props = {|
   opacityValue: Animated.Value,
   forgotPasswordLinkOpacity: Animated.Value,
   logInState: StateContainer<LogInState>,
+  innerRef: (container: ?LogInPanelContainer) => void,
   // Redux state
   dimensions: Dimensions,
 |};
@@ -52,6 +53,7 @@ class LogInPanelContainer extends React.PureComponent<Props, State> {
     opacityValue: PropTypes.object.isRequired,
     forgotPasswordLinkOpacity: PropTypes.object.isRequired,
     logInState: stateContainerPropType.isRequired,
+    innerRef: PropTypes.func.isRequired,
     dimensions: dimensionsPropType.isRequired,
   };
   state = {
@@ -60,6 +62,14 @@ class LogInPanelContainer extends React.PureComponent<Props, State> {
     nextLogInMode: "log-in",
   };
   logInPanel: ?InnerLogInPanel = null;
+
+  componentDidMount() {
+    this.props.innerRef(this);
+  }
+
+  componentWillUnmount() {
+    this.props.innerRef(null);
+  }
 
   render() {
     const windowWidth = this.props.dimensions.width;

native/account/logged-out-modal.react.js
@@ -596,7 +596,7 @@ class LoggedOutModal extends React.PureComponent<Props, State> {
           opacityValue={this.state.panelOpacity}
           forgotPasswordLinkOpacity={this.state.forgotPasswordLinkOpacity}
           logInState={this.state.logInState}
-          ref={this.logInPanelContainerRef}
+          innerRef={this.logInPanelContainerRef}
         />
       );
     } else if (this.state.mode === "register") {

native/chat/compose-thread.react.js
@@ -328,7 +328,7 @@ class InnerComposeThread extends React.PureComponent<Props, State> {
               onChangeText={this.setUsernameInputText}
               labelExtractor={this.tagDataLabelExtractor}
               inputProps={inputProps}
-              ref={this.tagInputRef}
+              innerRef={this.tagInputRef}
             />
           </View>
         </View>

native/chat/settings/add-users-modal.react.js
@@ -240,7 +240,7 @@ class AddUsersModal extends React.PureComponent<Props, State> {
           defaultInputWidth={160}
           maxHeight={36}
           inputProps={tagInputProps}
-          ref={this.tagInputRef}
+          innerRef={this.tagInputRef}
         />
         <UserList
           userInfos={this.state.userSearchResults}

native/components/tag-input.react.js
@@ -89,6 +89,7 @@ type Props<T> = {
    * value and the new value, which looks sketchy.
    */
   defaultInputWidth: number,
+  innerRef?: (tagInput: ?TagInput<T>) => void,
   // Redux state
   dimensions: Dimensions,
 };
@@ -114,6 +115,7 @@ class TagInput<T> extends React.PureComponent<Props<T>, State> {
     maxHeight: PropTypes.number,
     onHeightChange: PropTypes.func,
     defaultInputWidth: PropTypes.number,
+    innerRef: PropTypes.func,
     dimensions: dimensionsPropType.isRequired,
   };
   wrapperWidth: number;
@@ -157,6 +159,18 @@ class TagInput<T> extends React.PureComponent<Props<T>, State> {
       wrapperHeight: 36,
     };
     this.wrapperWidth = props.dimensions.width;
+  }
+
+  componentDidMount() {
+    if (this.props.innerRef) {
+      this.props.innerRef(this);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.props.innerRef) {
+      this.props.innerRef(null);
+    }
   }
 
   componentWillReceiveProps(nextProps: Props<T>) {