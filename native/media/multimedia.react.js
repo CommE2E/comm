0caada4ec8935c70235ec7aca7673480042c6ba6
Ashoat Tevosyan <ashoat@gmail.com>
2019-04-04 17:22:46 -0400

    [native] Center Multimedia loading spinner

--- native/media/multimedia.react.js
+++ native/media/multimedia.react.js
@@ -93,6 +93,10 @@ const styles = StyleSheet.create({
   },
   spinnerContainer: {
     position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
     justifyContent: 'center',
     alignItems: 'center',
   },
