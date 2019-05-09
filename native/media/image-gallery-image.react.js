0799994895207ac799369e789cc0cd9b113743b5
Ashoat Tevosyan <ashoat@gmail.com>
2019-05-08 23:16:44 -0400

    [native] Sticky opacity style for ImageGalleryImage

--- native/media/image-gallery-image.react.js
+++ native/media/image-gallery-image.react.js
@@ -4,7 +4,7 @@ import type { Dimensions } from 'lib/types/media-types';
 
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import { Image, TouchableOpacity } from 'react-native';
+import { Image, TouchableOpacity, StyleSheet } from 'react-native';
 
 export type GalleryImageInfo = {|
   ...Dimensions,
@@ -16,7 +16,10 @@ type Props = {|
   containerHeight: number,
   onSelect: (imageInfo: GalleryImageInfo) => void,
 |};
-class ImageGalleryImage extends React.PureComponent<Props> {
+type State = {|
+  selected: bool,
+|};
+class ImageGalleryImage extends React.PureComponent<Props, State> {
 
   static propTypes = {
     imageInfo: PropTypes.shape({
@@ -27,26 +30,60 @@ class ImageGalleryImage extends React.PureComponent<Props> {
     containerHeight: PropTypes.number.isRequired,
     onSelect: PropTypes.func.isRequired,
   };
+  state = {
+    selected: false,
+  };
+  button: ?TouchableOpacity;
+
+  componentDidUpdate(prevProps: Props, prevState: State) {
+    const { button } = this;
+    if (!button) {
+      return;
+    }
+    if (this.state.selected && !prevState.selected) {
+      button.setOpacityTo(0.2, 0);
+    } else if (!this.state.selected && prevState.selected) {
+      button.setOpacityTo(1, 0);
+    }
+  }
 
   render() {
     const { imageInfo, containerHeight } = this.props;
     const { uri, width, height } = imageInfo;
     const source = { uri };
-    const style = {
+    const imageStyle = {
       height: containerHeight,
       width: width / height * containerHeight,
     };
+    const buttonStyle = {
+      opacity: this.state.selected ? 0.2 : 1,
+    };
     return (
-      <TouchableOpacity onPress={this.onPress}>
-        <Image source={source} style={style} />
+      <TouchableOpacity
+        onPress={this.onPress}
+        style={[ styles.button, buttonStyle ]}
+        ref={this.buttonRef}
+      >
+        <Image source={source} style={imageStyle} />
       </TouchableOpacity>
     );
   }
 
+  buttonRef = (button: ?TouchableOpacity) => {
+    this.button = button;
+  }
+
   onPress = () => {
-    this.props.onSelect(this.props.imageInfo);
+    //this.props.onSelect(this.props.imageInfo);
+    this.setState({ selected: true });
   }
 
 }
+
+const styles = StyleSheet.create({
+  button: {
+    flex: 1,
+  },
+});
 
 export default ImageGalleryImage;
