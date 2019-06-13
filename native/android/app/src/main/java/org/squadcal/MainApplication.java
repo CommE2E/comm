abb60379214c59bd22f04154210e307b33292c7c
Ashoat Tevosyan <ashoat@gmail.com>
2019-06-12 23:27:19 -0400

    [native] react-native-fs

--- native/android/app/src/main/java/org/squadcal/MainApplication.java
+++ native/android/app/src/main/java/org/squadcal/MainApplication.java
@@ -3,6 +3,7 @@ package org.squadcal;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rnfs.RNFSPackage;
 import fr.bamlab.rnimageresizer.ImageResizerPackage;
 import com.horcrux.svg.SvgPackage;
 import com.airbnb.android.react.lottie.LottiePackage;
@@ -42,6 +43,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFSPackage(),
             new ImageResizerPackage(),
             new SvgPackage(),
             new LottiePackage(),
