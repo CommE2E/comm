29de915765c903365d18ed0d26c558920da6e52d
Ashoat Tevosyan <ashoat@gmail.com>
2019-06-12 22:55:53 -0400

    [server] Convert large size files down

--- server/src/uploads/media-utils.js
+++ server/src/uploads/media-utils.js
@@ -2,22 +2,55 @@
 
 import type { UploadInput } from '../creators/upload-creator';
 
+import sharp from 'sharp';
+
 import { fileInfoFromData } from 'lib/utils/media-utils';
 
+const fiveMegabytes = 5 * 1024 * 1024;
+
 async function validateAndConvert(
-  initialData: Buffer,
+  initialBuffer: Buffer,
   initialName: string,
   size: number, // in bytes
 ): Promise<?UploadInput> {
-  const fileInfo = fileInfoFromData(initialData, initialName);
+  const fileInfo = fileInfoFromData(initialBuffer, initialName);
   if (!fileInfo) {
     return null;
   }
-  if (fileInfo.mime === "image/heic") {
+
+  const { mime } = fileInfo;
+  if (mime === "image/heic") {
     // This should've gotten converted on the client
     return null;
   }
-  return { ...fileInfo, buffer: initialData };
+  if (size < fiveMegabytes && (mime === "image/png" || mime === "image/jpeg")) {
+    return { ...fileInfo, buffer: initialBuffer };
+  }
+
+  let sharpImage;
+  try {
+    sharpImage = sharp(initialBuffer);
+  } catch (e) {
+    return null;
+  }
+  sharpImage = sharpImage.resize(
+    3000,
+    2000,
+    { fit: 'inside', withoutEnlargement: true },
+  );
+
+  if (mime === "image/png" || mime === "image/gif") {
+    sharpImage = sharpImage.png();
+  } else {
+    sharpImage = sharpImage.jpeg({ quality: 92 });
+  }
+
+  const convertedBuffer = await sharpImage.toBuffer();
+  const convertedFileInfo = fileInfoFromData(convertedBuffer, initialName);
+  if (!convertedFileInfo) {
+    return null;
+  }
+  return { ...convertedFileInfo, buffer: convertedBuffer };
 }
 
 export {
