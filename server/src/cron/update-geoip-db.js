1ec8e47595f60091f5e88df9197dd3b3b32b8a31
Ashoat Tevosyan <ashoat@gmail.com>
2019-09-16 12:26:07 -0400

    [server] Reload GeoIP on all cluster members after update

--- server/src/cron/update-geoip-db.js
+++ server/src/cron/update-geoip-db.js
@@ -2,6 +2,9 @@
 
 import childProcess from 'child_process';
 import geoip from 'geoip-lite';
+import cluster from 'cluster';
+
+import { handleAsyncPromise } from '../responders/handlers';
 
 function updateGeoipDB(): Promise<void> {
   const spawned = childProcess.spawn(
@@ -18,9 +21,29 @@ function reloadGeoipDB(): Promise<void> {
   return new Promise(resolve => geoip.reloadData(resolve));
 }
 
+type IPCMessage = {|
+  type: 'geoip_reload',
+|};
+const reloadMessage: IPCMessage = { type: 'geoip_reload' };
+
 async function updateAndReloadGeoipDB(): Promise<void> {
   await updateGeoipDB();
   await reloadGeoipDB();
+
+  if (!cluster.isMaster) {
+    return;
+  }
+  for (const id in cluster.workers) {
+    cluster.workers[id].send(reloadMessage);
+  }
+}
+
+if (!cluster.isMaster) {
+  process.on('message', (ipcMessage: IPCMessage) => {
+    if (ipcMessage.type === 'geoip_reload') {
+      handleAsyncPromise(reloadGeoipDB());
+    }
+  });
 }
 
 export {
