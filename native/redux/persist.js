b97a1cf1640ff68cccadf658eda9ed9920d31ab9
Ashoat Tevosyan <ashoat@gmail.com>
2019-10-08 15:20:58 -0400

    [native] codeVersion -> 35
    Won't release this one to the people. Just for long-term internal testing

--- native/redux/persist.js
+++ native/redux/persist.js
@@ -142,7 +142,7 @@ const persistConfig = {
   migrate: createMigrate(migrations, { debug: __DEV__ }),
 };
 
-const codeVersion = 34;
+const codeVersion = 35;
 
 // This local exists to avoid a circular dependency where redux-setup needs to
 // import all the navigation and screen stuff, but some of those screens want to
