6f6e113dd6d923fb51b981d7838083f2bf6d9284
Ashoat Tevosyan <ashoat@gmail.com>
2019-10-11 17:44:25 -0400

    [native] codeVersion -> 38

--- native/redux/persist.js
+++ native/redux/persist.js
@@ -143,7 +143,7 @@ const persistConfig = {
   timeout: __DEV__ ? 0 : undefined,
 };
 
-const codeVersion = 37;
+const codeVersion = 38;
 
 // This local exists to avoid a circular dependency where redux-setup needs to
 // import all the navigation and screen stuff, but some of those screens want to
