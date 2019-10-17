f581b0af86a8ba491d5a9c16094adccf96b580f2
Ashoat Tevosyan <ashoat@gmail.com>
2019-10-17 14:45:01 -0400

    [native] DisconnectedBar should show below Header on Android

--- native/navigation/header.react.js
+++ native/navigation/header.react.js
@@ -2,14 +2,15 @@
 
 import * as React from 'react';
 import { Header } from 'react-navigation-stack';
+import { View } from 'react-native';
 
 import DisconnectedBar from './disconnected-bar.react';
 
 export default function CustomHeader(props: React.ElementProps<typeof Header>) {
   return (
-    <>
+    <View>
       <Header {...props} />
       <DisconnectedBar />
-    </>
+    </View>
   );
 }
