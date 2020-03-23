CommE2E/comm

commit 6a467123b192751f2410fb6a4a334eb1bd56868b

[native] codeVersion -> 43

native/android/app/build.gradle
@@ -131,8 +131,8 @@ android {
         applicationId "org.squadcal"
         minSdkVersion rootProject.ext.minSdkVersion
         targetSdkVersion rootProject.ext.targetSdkVersion
-        versionCode 42
-        versionName "0.0.42"
+        versionCode 43
+        versionName "0.0.43"
         missingDimensionStrategy 'react-native-camera', 'general'
         multiDexEnabled true
     }

native/ios/SquadCal/Info.debug.plist
@@ -17,11 +17,11 @@
 	<key>CFBundlePackageType</key>
 	<string>APPL</string>
 	<key>CFBundleShortVersionString</key>
-	<string>0.0.42</string>
+	<string>0.0.43</string>
 	<key>CFBundleSignature</key>
 	<string>????</string>
 	<key>CFBundleVersion</key>
-	<string>42</string>
+	<string>43</string>
 	<key>LSApplicationQueriesSchemes</key>
 	<array>
 		<string>org-appextension-feature-password-management</string>

native/ios/SquadCal/Info.release.plist
@@ -17,11 +17,11 @@
 	<key>CFBundlePackageType</key>
 	<string>APPL</string>
 	<key>CFBundleShortVersionString</key>
-	<string>0.0.42</string>
+	<string>0.0.43</string>
 	<key>CFBundleSignature</key>
 	<string>????</string>
 	<key>CFBundleVersion</key>
-	<string>42</string>
+	<string>43</string>
 	<key>LSApplicationQueriesSchemes</key>
 	<array>
 		<string>org-appextension-feature-password-management</string>

native/redux/persist.js
@@ -177,7 +177,7 @@ const persistConfig = {
   timeout: __DEV__ ? 0 : undefined,
 };
 
-const codeVersion = 42;
+const codeVersion = 43;
 
 // This local exists to avoid a circular dependency where redux-setup needs to
 // import all the navigation and screen stuff, but some of those screens want to

commit a9559a26cb0f717e85b7c9f4e3b872acd9a0baf5

[native] codeVersion -> 44
This is an iOS-only build to test if 6a467123b192751f2410fb6a4a334eb1bd56868b introduced a regression in media library order on iOS

native/android/app/build.gradle
@@ -131,8 +131,8 @@ android {
         applicationId "org.squadcal"
         minSdkVersion rootProject.ext.minSdkVersion
         targetSdkVersion rootProject.ext.targetSdkVersion
-        versionCode 43
-        versionName "0.0.43"
+        versionCode 44
+        versionName "0.0.44"
         missingDimensionStrategy 'react-native-camera', 'general'
         multiDexEnabled true
     }

native/ios/SquadCal/Info.debug.plist
@@ -17,11 +17,11 @@
 	<key>CFBundlePackageType</key>
 	<string>APPL</string>
 	<key>CFBundleShortVersionString</key>
-	<string>0.0.43</string>
+	<string>0.0.44</string>
 	<key>CFBundleSignature</key>
 	<string>????</string>
 	<key>CFBundleVersion</key>
-	<string>43</string>
+	<string>44</string>
 	<key>LSApplicationQueriesSchemes</key>
 	<array>
 		<string>org-appextension-feature-password-management</string>

native/ios/SquadCal/Info.release.plist
@@ -17,11 +17,11 @@
 	<key>CFBundlePackageType</key>
 	<string>APPL</string>
 	<key>CFBundleShortVersionString</key>
-	<string>0.0.43</string>
+	<string>0.0.44</string>
 	<key>CFBundleSignature</key>
 	<string>????</string>
 	<key>CFBundleVersion</key>
-	<string>43</string>
+	<string>44</string>
 	<key>LSApplicationQueriesSchemes</key>
 	<array>
 		<string>org-appextension-feature-password-management</string>

native/redux/persist.js
@@ -167,7 +167,7 @@ const persistConfig = {
   timeout: __DEV__ ? 0 : undefined,
 };
 
-const codeVersion = 43;
+const codeVersion = 44;
 
 // This local exists to avoid a circular dependency where redux-setup needs to
 // import all the navigation and screen stuff, but some of those screens want to