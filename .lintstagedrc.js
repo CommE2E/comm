52b0fd6bb429561887cc958170b4db5803815f21
Ashoat Tevosyan <ashoat@gmail.com>
2020-03-28 01:09:41 -0400

    [native] Fix lint-staged to not report eslint ignore files

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,7 +3,7 @@ const { CLIEngine } = require('eslint');
 const cli = new CLIEngine({ });
 
 module.exports = {
-  "*.js": "eslint --cache --fix",
+  '*.js': files => 'eslint --cache --fix --max-warnings=0 ' + files.filter(file => !cli.isPathIgnored(file)).join(' '),
   "lib/**/*.js": function libFlow(files) { return "yarn workspace lib flow --quiet" },
   "web/**/*.js": function webFlow(files) { return "yarn workspace web flow --quiet" },
   "native/**/*.js": function nativeFlow(files) { return "yarn workspace native flow --quiet" },
