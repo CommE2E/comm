b936c93f1b44c79944f97b07323aedfba2bc37e1
Ashoat Tevosyan <ashoat@gmail.com>
2020-04-01 10:20:51 -0400

    [native] Only count a thread MessageList as active thread
    For clearing notifs and badge count

--- native/navigation/route-names.js
+++ native/navigation/route-names.js
@@ -61,3 +61,9 @@ export const chatRootModals = [
   ColorPickerModalRouteName,
   ComposeSubthreadModalRouteName,
 ];
+
+export const threadRoutes = [
+  MessageListRouteName,
+  ThreadSettingsRouteName,
+  DeleteThreadRouteName,
+];
