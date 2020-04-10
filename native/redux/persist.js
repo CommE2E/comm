f10564d2e1286e35ff2c9ecc9a07285f395e01e2
Ashoat Tevosyan <ashoat@gmail.com>
2020-04-10 01:16:22 -0400

    [native] codeVersion -> 47
    Last build had a major regression, this is the real one to test RN0.61

--- native/redux/persist.js
+++ native/redux/persist.js
@@ -178,7 +178,7 @@ const persistConfig = {
   timeout: __DEV__ ? 0 : undefined,
 };
 
-const codeVersion = 46;
+const codeVersion = 47;
 
 // This local exists to avoid a circular dependency where redux-setup needs to
 // import all the navigation and screen stuff, but some of those screens want to
