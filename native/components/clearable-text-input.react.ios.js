be5607801378717be088349a9fb619fff4b8d22d
Ashoat Tevosyan <ashoat@gmail.com>
2020-04-23 17:28:47 -0400

    [native] ClearableTextInput for Android

---
+++ native/components/clearable-text-input.react.ios.js
@@ -0,0 +1,157 @@
+// @flow
+
+import type { KeyPressEvent } from '../types/react-native';
+
+import * as React from 'react';
+import { TextInput, View, StyleSheet } from 'react-native';
+
+import invariant from 'invariant';
+
+type Props = {|
+  ...React.ElementConfig<typeof TextInput>,
+  textInputRef: (textInput: ?TextInput) => mixed,
+|};
+type State = {|
+  textInputKey: number,
+|};
+class ClearableTextInput extends React.PureComponent<Props, State> {
+  state = {
+    textInputKey: 0,
+  };
+  pendingMessage: ?{| value: ?string, resolve: (value: ?string) => void |};
+  lastKeyPressed: ?string;
+  lastTextInputSent = -1;
+
+  onChangeText = (text: string) => {
+    this.props.onChangeText && this.props.onChangeText(text);
+  };
+
+  sendMessage() {
+    if (this.pendingMessageSent) {
+      return;
+    }
+    const { pendingMessage } = this;
+    invariant(pendingMessage, 'cannot send an empty message');
+    pendingMessage.resolve(pendingMessage.value);
+
+    const textInputSent = this.state.textInputKey - 1;
+    if (textInputSent > this.lastTextInputSent) {
+      this.lastTextInputSent = textInputSent;
+    }
+  }
+
+  get pendingMessageSent() {
+    return this.lastTextInputSent >= this.state.textInputKey - 1;
+  }
+
+  onOldInputChangeText = (text: string) => {
+    const { pendingMessage, lastKeyPressed } = this;
+    invariant(
+      pendingMessage,
+      'onOldInputChangeText should have a pendingMessage',
+    );
+
+    if (
+      !this.pendingMessageSent &&
+      lastKeyPressed &&
+      lastKeyPressed.length > 1
+    ) {
+      // This represents an autocorrect event on blur
+      pendingMessage.value = text;
+    }
+    this.lastKeyPressed = null;
+
+    this.sendMessage();
+    this.updateTextFromOldInput(text);
+  };
+
+  updateTextFromOldInput(text: string) {
+    const { pendingMessage } = this;
+    invariant(
+      pendingMessage,
+      'updateTextFromOldInput should have a pendingMessage',
+    );
+    const pendingValue = pendingMessage.value;
+    if (!pendingValue || !text.startsWith(pendingValue)) {
+      return;
+    }
+    const newValue = text.substring(pendingValue.length);
+    if (this.props.value === newValue) {
+      return;
+    }
+    this.onChangeText(newValue);
+  }
+
+  onOldInputKeyPress = (event: KeyPressEvent) => {
+    const { key } = event.nativeEvent;
+    if (this.lastKeyPressed && this.lastKeyPressed.length > key.length) {
+      return;
+    }
+    this.lastKeyPressed = key;
+    this.props.onKeyPress && this.props.onKeyPress(event);
+  };
+
+  onOldInputBlur = () => {
+    this.sendMessage();
+  };
+
+  textInputRef = (textInput: ?TextInput) => {
+    if (this.state.textInputKey > 0 && textInput) {
+      textInput.focus();
+    }
+    this.props.textInputRef(textInput);
+  };
+
+  getValueAndReset(): Promise<?string> {
+    this.onChangeText('');
+    return new Promise(resolve => {
+      this.pendingMessage = {
+        value: this.props.value,
+        resolve,
+      };
+      this.setState(prevState => ({
+        textInputKey: prevState.textInputKey + 1,
+      }));
+    });
+  }
+
+  render() {
+    const { textInputRef, ...props } = this.props;
+
+    const textInputs = [];
+    if (this.state.textInputKey > 0) {
+      textInputs.push(
+        <TextInput
+          {...props}
+          style={[props.style, styles.invisibleTextInput]}
+          onChangeText={this.onOldInputChangeText}
+          onKeyPress={this.onOldInputKeyPress}
+          onBlur={this.onOldInputBlur}
+          key={this.state.textInputKey - 1}
+        />,
+      );
+    }
+    textInputs.push(
+      <TextInput
+        {...props}
+        onChangeText={this.onChangeText}
+        key={this.state.textInputKey}
+        ref={this.textInputRef}
+      />,
+    );
+
+    return <View style={styles.textInputContainer}>{textInputs}</View>;
+  }
+}
+
+const styles = StyleSheet.create({
+  invisibleTextInput: {
+    opacity: 0,
+    position: 'absolute',
+  },
+  textInputContainer: {
+    flex: 1,
+  },
+});
+
+export default ClearableTextInput;
--- native/components/clearable-text-input.react.js
+++ native/components/clearable-text-input.react.js
@@ -1,155 +1,61 @@
 // @flow
 
-import type { KeyPressEvent } from '../types/react-native';
-
 import * as React from 'react';
-import { TextInput, View, Platform, StyleSheet } from 'react-native';
-
-import invariant from 'invariant';
+import { TextInput, View, StyleSheet } from 'react-native';
 
 type Props = {|
   ...React.ElementConfig<typeof TextInput>,
   textInputRef: (textInput: ?TextInput) => mixed,
 |};
-type State = {|
-  textInputKey: number,
-|};
-class ClearableTextInput extends React.PureComponent<Props, State> {
-  state = {
-    textInputKey: 0,
-  };
-  pendingMessage: ?{| value: ?string, resolve: (value: ?string) => void |};
-  lastKeyPressed: ?string;
-  lastTextInputSent = -1;
+class ClearableTextInput extends React.PureComponent<Props> {
+  textInput: ?TextInput;
+  lastMessageSent: ?string;
 
-  onChangeText = (text: string) => {
-    this.props.onChangeText && this.props.onChangeText(text);
-  };
-
-  sendMessage() {
-    if (this.pendingMessageSent) {
+  onChangeText = (inputText: string) => {
+    const { onChangeText } = this.props;
+    if (!onChangeText) {
       return;
     }
-    const { pendingMessage } = this;
-    invariant(pendingMessage, 'cannot send an empty message');
-    pendingMessage.resolve(pendingMessage.value);
 
-    const textInputSent = this.state.textInputKey - 1;
-    if (textInputSent > this.lastTextInputSent) {
-      this.lastTextInputSent = textInputSent;
+    let text;
+    if (this.lastMessageSent && inputText.startsWith(this.lastMessageSent)) {
+      text = inputText.substring(this.lastMessageSent.length);
+    } else {
+      text = inputText;
+      this.lastMessageSent = null;
     }
-  }
-
-  get pendingMessageSent() {
-    return this.lastTextInputSent >= this.state.textInputKey - 1;
-  }
-
-  onOldInputChangeText = (text: string) => {
-    const { pendingMessage, lastKeyPressed } = this;
-    invariant(
-      pendingMessage,
-      'onOldInputChangeText should have a pendingMessage',
-    );
-
-    if (
-      Platform.OS === 'ios' &&
-      !this.pendingMessageSent &&
-      lastKeyPressed &&
-      lastKeyPressed.length > 1
-    ) {
-      // This represents an autocorrect event on blur
-      pendingMessage.value = text;
-    }
-    this.lastKeyPressed = null;
-
-    this.sendMessage();
-    this.updateTextFromOldInput(text);
+    onChangeText(text);
   };
 
-  updateTextFromOldInput(text: string) {
-    const { pendingMessage } = this;
-    invariant(
-      pendingMessage,
-      'updateTextFromOldInput should have a pendingMessage',
-    );
-    const pendingValue = pendingMessage.value;
-    if (!pendingValue || !text.startsWith(pendingValue)) {
-      return;
+  async getValueAndReset(): Promise<?string> {
+    this.lastMessageSent = this.props.value;
+    this.props.onChangeText && this.props.onChangeText('');
+    if (this.textInput) {
+      this.textInput.clear();
     }
-    const newValue = text.substring(pendingValue.length);
-    if (this.props.value === newValue) {
-      return;
-    }
-    this.onChangeText(newValue);
-  }
-
-  onOldInputKeyPress = (event: KeyPressEvent) => {
-    const { key } = event.nativeEvent;
-    if (this.lastKeyPressed && this.lastKeyPressed.length > key.length) {
-      return;
-    }
-    this.lastKeyPressed = key;
-    this.props.onKeyPress && this.props.onKeyPress(event);
-  };
-
-  onOldInputBlur = () => {
-    this.sendMessage();
-  };
-
-  textInputRef = (textInput: ?TextInput) => {
-    if (this.state.textInputKey > 0 && textInput) {
-      textInput.focus();
-    }
-    this.props.textInputRef(textInput);
-  };
-
-  getValueAndReset(): Promise<?string> {
-    this.onChangeText('');
-    return new Promise(resolve => {
-      this.pendingMessage = {
-        value: this.props.value,
-        resolve,
-      };
-      this.setState(prevState => ({
-        textInputKey: prevState.textInputKey + 1,
-      }));
-    });
+    return this.props.value;
   }
 
   render() {
     const { textInputRef, ...props } = this.props;
-
-    const textInputs = [];
-    if (this.state.textInputKey > 0) {
-      textInputs.push(
+    return (
+      <View style={styles.textInputContainer}>
         <TextInput
           {...props}
-          style={[props.style, styles.invisibleTextInput]}
-          onChangeText={this.onOldInputChangeText}
-          onKeyPress={this.onOldInputKeyPress}
-          onBlur={this.onOldInputBlur}
-          key={this.state.textInputKey - 1}
-        />,
-      );
-    }
-    textInputs.push(
-      <TextInput
-        {...props}
-        onChangeText={this.onChangeText}
-        key={this.state.textInputKey}
-        ref={this.textInputRef}
-      />,
+          onChangeText={this.onChangeText}
+          ref={this.textInputRef}
+        />
+      </View>
     );
-
-    return <View style={styles.textInputContainer}>{textInputs}</View>;
   }
+
+  textInputRef = (textInput: ?TextInput) => {
+    this.textInput = textInput;
+    this.props.textInputRef(textInput);
+  };
 }
 
 const styles = StyleSheet.create({
-  invisibleTextInput: {
-    opacity: 0,
-    position: 'absolute',
-  },
   textInputContainer: {
     flex: 1,
   },
