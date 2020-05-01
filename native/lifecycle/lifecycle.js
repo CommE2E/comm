8802c176ab16a5020b3d21274bf9ef7fc9ef4a1d
Ashoat Tevosyan <ashoat@gmail.com>
2020-04-30 22:23:31 -0400

    [native] Use AndroidLifecycleModule in lifecycle.js

--- native/lifecycle/lifecycle.js
+++ native/lifecycle/lifecycle.js
@@ -1,8 +1,19 @@
 // @flow
 
-import { AppState as NativeAppState } from 'react-native';
+import {
+  Platform,
+  AppState as NativeAppState,
+  DeviceEventEmitter,
+  NativeModules,
+} from 'react-native';
 
 function addLifecycleListener(listener: (state: ?string) => mixed) {
+  if (Platform.OS === 'android') {
+    return DeviceEventEmitter.addListener('LIFECYCLE_CHANGE', event => {
+      listener(event.status);
+    });
+  }
+
   NativeAppState.addEventListener('change', listener);
   return {
     remove: () => {
@@ -11,8 +22,19 @@ function addLifecycleListener(listener: (state: ?string) => mixed) {
   };
 }
 
+let currentAndroidLifecycle;
+if (Platform.OS === 'android') {
+  currentAndroidLifecycle = NativeModules.AndroidLifecycle.getConstants()
+    .initialStatus;
+  addLifecycleListener(state => {
+    currentAndroidLifecycle = state;
+  });
+}
+
 function getCurrentLifecycleState() {
-  return NativeAppState.currentState;
+  return Platform.OS === 'android'
+    ? currentAndroidLifecycle
+    : NativeAppState.currentState;
 }
 
 export { addLifecycleListener, getCurrentLifecycleState };
