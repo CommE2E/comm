0b4b8147d29e48eb942163583cc3391d5b84359b
Ashoat Tevosyan <ashoat@gmail.com>
2020-05-01 16:29:26 -0400

    [native] Extend ActionResultModal lifespan if its text changes

--- native/navigation/action-result-modal.react.js
+++ native/navigation/action-result-modal.react.js
@@ -9,7 +9,6 @@ import type { AppState } from '../redux/redux-setup';
 
 import * as React from 'react';
 import { View, Text } from 'react-native';
-import PropTypes from 'prop-types';
 import Animated from 'react-native-reanimated';
 
 import { connect } from 'lib/utils/redux-utils';
@@ -37,60 +36,39 @@ type Props = {|
   // Redux state
   styles: StyleSheetOf<typeof styles>,
 |};
-class ActionResultModal extends React.PureComponent<Props> {
-  static propTypes = {
-    navigation: PropTypes.shape({
-      state: PropTypes.shape({
-        params: PropTypes.shape({
-          message: PropTypes.string.isRequired,
-        }).isRequired,
-      }).isRequired,
-      goBack: PropTypes.func.isRequired,
-    }).isRequired,
-    scene: PropTypes.object.isRequired,
-    position: PropTypes.instanceOf(Value).isRequired,
-    styles: PropTypes.objectOf(PropTypes.object).isRequired,
+function ActionResultModal(props: Props) {
+  const { position, scene } = props;
+  const { index } = scene;
+  const progress = React.useMemo(
+    () =>
+      interpolate(position, {
+        inputRange: [index - 1, index],
+        outputRange: [0, 1],
+        extrapolate: Extrapolate.CLAMP,
+      }),
+    [position, index],
+  );
+
+  // Timer resets whenever message updates
+  const { state, goBack } = props.navigation;
+  const { message } = state.params;
+  React.useEffect(() => {
+    const timeoutID = setTimeout(goBack, 2000);
+    return () => clearTimeout(timeoutID);
+  }, [message, goBack]);
+
+  const containerStyle = {
+    ...props.styles.container,
+    opacity: progress,
   };
-  progress: Value;
-
-  constructor(props: Props) {
-    super(props);
-
-    const { position } = props;
-    const { index } = props.scene;
-    this.progress = interpolate(position, {
-      inputRange: [index - 1, index],
-      outputRange: [0, 1],
-      extrapolate: Extrapolate.CLAMP,
-    });
-  }
-
-  componentDidMount() {
-    setTimeout(this.goBack, 2000);
-  }
-
-  goBack = () => {
-    this.props.navigation.goBack();
-  };
-
-  get containerStyle() {
-    return {
-      ...this.props.styles.container,
-      opacity: this.progress,
-    };
-  }
-
-  render() {
-    const { message } = this.props.navigation.state.params;
-    return (
-      <Animated.View style={this.containerStyle}>
-        <View style={this.props.styles.message}>
-          <View style={this.props.styles.backdrop} />
-          <Text style={this.props.styles.text}>{message}</Text>
-        </View>
-      </Animated.View>
-    );
-  }
+  return (
+    <Animated.View style={containerStyle}>
+      <View style={props.styles.message}>
+        <View style={props.styles.backdrop} />
+        <Text style={props.styles.text}>{message}</Text>
+      </View>
+    </Animated.View>
+  );
 }
 
 const styles = {
