7f7c5083ccd930c187841d2fab95f56e5ff1e442
Ashoat Tevosyan <ashoat@gmail.com>
2020-05-11 23:25:06 -0400

    [native] Add notice explaining themes/fonts.js existence

--- native/themes/fonts.js
+++ native/themes/fonts.js
@@ -6,6 +6,8 @@ import IonIcon from 'react-native-vector-icons/Ionicons';
 import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 
+// This is only necessary on iOS if we switch to use_frameworks
+// We're keeping it here in case we ever do that again
 FeatherIcon.loadFont();
 FontAwesomeIcon.loadFont();
 IonIcon.loadFont();
