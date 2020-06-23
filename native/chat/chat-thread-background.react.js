7c162402a48525fc0a7257ad4d6039b3600c4202
KatPo <kasko.po@gmail.com>
2020-06-23 09:41:44 +0200

    [native] After review changes

--- native/chat/chat-thread-background.react.js
+++ native/chat/chat-thread-background.react.js
@@ -6,10 +6,7 @@ import * as React from 'react';
 import { useSelector } from 'react-redux';
 
 import { threadInBackgroundChatList } from 'lib/shared/thread-utils';
-import {
-  type ChatThreadItem,
-  chatListData,
-} from 'lib/selectors/chat-selectors';
+import { unreadBackgroundCount } from 'lib/selectors/thread-selectors';
 
 import ChatThreadList from './chat-thread-list.react';
 
@@ -20,34 +17,25 @@ type BackgroundChatThreadListProps = {
 export default function BackgroundChatThreadList(
   props: BackgroundChatThreadListProps,
 ) {
-  const chatThreadListData = useSelector(state => chatListData(state));
-
-  const findUnreadBackgroundThreadsNumber = (
-    chatThreadsListData: Array<ChatThreadItem>,
-  ): number => {
-    return chatThreadsListData
-      .map(info => ({
-        home: info.threadInfo.currentUser.subscription.home,
-        unread: info.threadInfo.currentUser.unread,
-      }))
-      .filter(item => !item.home && item.unread).length;
-  };
+  const unreadBackgroundThreadsCount = useSelector(state =>
+    unreadBackgroundCount(state),
+  );
 
   const prevUnreadNumber = React.useRef(0);
   const unreadBackgroundThreadsNumber = React.useMemo(() => {
-    return findUnreadBackgroundThreadsNumber(chatThreadListData);
-  }, [chatThreadListData]);
+    return unreadBackgroundThreadsCount;
+  }, [unreadBackgroundThreadsCount]);
 
   React.useEffect(() => {
-    if (unreadBackgroundThreadsNumber == prevUnreadNumber.current) return;
+    if (unreadBackgroundThreadsNumber === prevUnreadNumber.current) {
+      return;
+    }
     prevUnreadNumber.current = unreadBackgroundThreadsNumber;
     let title = 'Background';
     if (unreadBackgroundThreadsNumber !== 0)
-      title = title.concat(' (', unreadBackgroundThreadsNumber.toString(), ')');
-    props.navigation.setOptions({
-      title: title,
-    });
-  });
+      title += ` (${unreadBackgroundThreadsNumber})`;
+    props.navigation.setOptions({ title });
+  }, [props.navigation, unreadBackgroundThreadsNumber]);
 
   return (
     <ChatThreadList
