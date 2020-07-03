b7c08d43592b68be7277e51b9608f10ca3ac8689
KatPo <kasko.po@gmail.com>
2020-07-03 15:48:31 +0200

    [web] Add unread threads number to background tab (#100)
    * [web] Add unread threads number to background tab
    
    * [web] After review changes
    
    * [web] Some nits

--- web/chat/chat-tabs.react.js
+++ web/chat/chat-tabs.react.js
@@ -1,43 +1,61 @@
 // @flow
 
+import type { AppState } from '../redux-setup';
+
 import * as React from 'react';
+import PropTypes from 'prop-types';
+
+import { connect } from 'lib/utils/redux-utils';
+import { unreadBackgroundCount } from 'lib/selectors/thread-selectors';
 
 import ChatThreadTab from './chat-thread-tab.react';
 import css from './chat-tabs.css';
 import ChatThreadHome from './chat-thread-home.react';
 import ChatThreadBackground from './chat-thread-background.react';
 
-type Props = {||};
+type Props = {|
+  unreadBackgroundCount: ?number,
+|};
 type State = {|
   activeTab: string,
 |};
-
 class ChatTabs extends React.PureComponent<Props, State> {
+  static propTypes = {
+    unreadBackgroundCount: PropTypes.number,
+  };
   state = {
     activeTab: 'HOME',
   };
 
-  onClickTabItem = (tab: string) => {
-    this.setState({ activeTab: tab });
+  onClickHome = () => {
+    this.setState({ activeTab: 'HOME' });
+  };
+
+  onClickBackground = () => {
+    this.setState({ activeTab: 'BACKGROUND' });
   };
 
   render() {
     const { activeTab } = this.state;
     const threadList =
       activeTab === 'HOME' ? <ChatThreadHome /> : <ChatThreadBackground />;
+    let backgroundTitle = 'BACKGROUND';
+    if (this.props.unreadBackgroundCount) {
+      backgroundTitle += ` (${this.props.unreadBackgroundCount})`;
+    }
 
     return (
       <div className={css.container}>
         <div className={css.tabs}>
           <ChatThreadTab
-            activeTab={this.state.activeTab}
             title="HOME"
-            onClick={this.onClickTabItem}
+            tabIsActive={this.state.activeTab === 'HOME'}
+            onClick={this.onClickHome}
           />
           <ChatThreadTab
-            activeTab={this.state.activeTab}
-            title="BACKGROUND"
-            onClick={this.onClickTabItem}
+            title={backgroundTitle}
+            tabIsActive={this.state.activeTab === 'BACKGROUND'}
+            onClick={this.onClickBackground}
           />
         </div>
         <div className={css.threadList}>{threadList}</div>
@@ -46,4 +64,6 @@ class ChatTabs extends React.PureComponent<Props, State> {
   }
 }
 
-export default ChatTabs;
+export default connect((state: AppState) => ({
+  unreadBackgroundCount: unreadBackgroundCount(state),
+}))(ChatTabs);
--- web/chat/chat-thread-tab.react.js
+++ web/chat/chat-thread-tab.react.js
@@ -1,41 +1,26 @@
 // @flow
 
 import * as React from 'react';
-import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 import css from './chat-tabs.css';
 
 type Props = {|
-  activeTab: string,
   title: string,
   onClick: (title: string) => void,
+  tabIsActive: boolean,
 |};
-
-class ChatThreadTab extends React.PureComponent<Props> {
-  static propTypes = {
-    activeTab: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    onClick: PropTypes.func.isRequired,
-  };
-
-  onClick = () => {
-    this.props.onClick(this.props.title);
-  };
-
-  render() {
-    const isActive = this.props.activeTab === this.props.title;
-    const className = classNames({
-      [css.tabItem]: true,
-      [css.tabItemActive]: isActive,
-      [css.tabItemInactive]: !isActive,
-    });
-    return (
-      <div className={className} onClick={this.onClick}>
-        {this.props.title}
-      </div>
-    );
-  }
+function ChatThreadTab(props: Props) {
+  const className = classNames({
+    [css.tabItem]: true,
+    [css.tabItemActive]: props.tabIsActive,
+    [css.tabItemInactive]: !props.tabIsActive,
+  });
+  return (
+    <div className={className} onClick={props.onClick}>
+      {props.title}
+    </div>
+  );
 }
 
 export default ChatThreadTab;
