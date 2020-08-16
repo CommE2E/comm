db496033cee7a9bb99ecc305be86ee23786e1868
Ashoat Tevosyan <ashoat@gmail.com>
2020-08-16 10:47:18 -0400

    Revert "[server] Still include newThreadInfo for create_thread"
    This reverts commit 7a9207f73c8db056ffc0ba380c18a00450b6d40a.

--- server/src/creators/thread-creator.js
+++ server/src/creators/thread-creator.js
@@ -206,7 +206,7 @@ async function createThread(
   ]);
   const { threadInfos, viewerUpdates } = commitResult;
 
-  if (hasMinCodeVersion(viewer.platformDetails, 64)) {
+  if (hasMinCodeVersion(viewer.platformDetails, 62)) {
     return {
       newThreadID: id,
       updatesResult: {
@@ -217,7 +217,6 @@ async function createThread(
   }
 
   return {
-    newThreadID: id,
     newThreadInfo: threadInfos[id],
     updatesResult: {
       newUpdates: viewerUpdates,
