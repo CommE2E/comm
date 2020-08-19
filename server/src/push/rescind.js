46201365f388fc221a3ecd3ad865a42e1359dd25
Ashoat Tevosyan <ashoat@gmail.com>
2020-08-19 18:28:41 -0400

    [server] Log notifs rescinds to notifications table

--- server/src/push/rescind.js
+++ server/src/push/rescind.js
@@ -4,9 +4,13 @@ import { threadPermissions } from 'lib/types/thread-types';
 import { threadSubscriptions } from 'lib/types/subscription-types';
 
 import apn from 'apn';
+import invariant from 'invariant';
+
+import { promiseAll } from 'lib/utils/promises';
 
 import { dbQuery, SQL, SQLStatement } from '../database';
 import { apnPush, fcmPush } from './utils';
+import createIDs from '../creators/id-creator';
 
 async function rescindPushNotifs(
   notifCondition: SQLStatement,
@@ -15,7 +19,7 @@ async function rescindPushNotifs(
   const notificationExtractString = `$.${threadSubscriptions.home}`;
   const visPermissionExtractString = `$.${threadPermissions.VISIBLE}.value`;
   const fetchQuery = SQL`
-    SELECT n.id, n.delivery, n.collapse_key, n.thread, COUNT(
+    SELECT n.id, n.user, n.thread, n.message, n.delivery, n.collapse_key, COUNT(
   `;
   fetchQuery.append(inputCountCondition ? inputCountCondition : SQL`m.thread`);
   fetchQuery.append(SQL`
@@ -30,12 +34,20 @@ async function rescindPushNotifs(
   fetchQuery.append(SQL` GROUP BY n.id, m.user`);
   const [fetchResult] = await dbQuery(fetchQuery);
 
-  const promises = [];
+  const deliveryPromises = {};
+  const notifInfo = {};
   const rescindedIDs = [];
   for (let row of fetchResult) {
     const deliveries = Array.isArray(row.delivery)
       ? row.delivery
       : [row.delivery];
+    const id = row.id.toString();
+    const threadID = row.thread.toString();
+    notifInfo[id] = {
+      userID: row.user.toString(),
+      threadID,
+      messageID: row.message.toString(),
+    };
     for (let delivery of deliveries) {
       if (delivery.iosID && delivery.iosDeviceTokens) {
         // Old iOS
@@ -43,36 +55,44 @@ async function rescindPushNotifs(
           delivery.iosID,
           row.unread_count,
         );
-        promises.push(apnPush(notification, delivery.iosDeviceTokens));
+        deliveryPromises[id] = apnPush(notification, delivery.iosDeviceTokens);
       } else if (delivery.androidID) {
         // Old Android
         const notification = prepareAndroidNotification(
-          row.collapse_key ? row.collapse_key : row.id.toString(),
+          row.collapse_key ? row.collapse_key : id,
           row.unread_count,
-          row.thread.toString(),
+          threadID,
           null,
         );
-        promises.push(
-          fcmPush(notification, delivery.androidDeviceTokens, null),
+        deliveryPromises[id] = fcmPush(
+          notification,
+          delivery.androidDeviceTokens,
+          null,
         );
       } else if (delivery.deviceType === 'ios') {
         // New iOS
         const { iosID, deviceTokens } = delivery;
         const notification = prepareIOSNotification(iosID, row.unread_count);
-        promises.push(apnPush(notification, deviceTokens));
+        deliveryPromises[id] = apnPush(notification, deviceTokens);
       } else if (delivery.deviceType === 'android') {
         // New Android
         const { deviceTokens, codeVersion } = delivery;
         const notification = prepareAndroidNotification(
-          row.collapse_key ? row.collapse_key : row.id.toString(),
+          row.collapse_key ? row.collapse_key : id,
           row.unread_count,
-          row.thread.toString(),
+          threadID,
           codeVersion,
         );
-        promises.push(fcmPush(notification, deviceTokens, null));
+        deliveryPromises[id] = fcmPush(notification, deviceTokens, null);
       }
     }
     rescindedIDs.push(row.id);
+  }
+
+  const numRescinds = Object.keys(deliveryPromises).length;
+  const promises = [promiseAll(deliveryPromises)];
+  if (numRescinds > 0) {
+    promises.push(createIDs('notifications', numRescinds));
   }
   if (rescindedIDs.length > 0) {
     const rescindQuery = SQL`
@@ -81,7 +101,39 @@ async function rescindPushNotifs(
     promises.push(dbQuery(rescindQuery));
   }
 
-  await Promise.all(promises);
+  const [deliveryResults, dbIDs] = await Promise.all(promises);
+  const newNotifRows = [];
+  if (numRescinds > 0) {
+    invariant(dbIDs, 'dbIDs should be set');
+    for (const rescindedID in deliveryResults) {
+      const delivery = {};
+      delivery.type = 'rescind';
+      delivery.rescindedID = rescindedID;
+      const { errors } = deliveryResults[rescindedID];
+      if (errors) {
+        delivery.errors = errors;
+      }
+      const dbID = dbIDs.shift();
+      const { userID, threadID, messageID } = notifInfo[rescindedID];
+      newNotifRows.push([
+        dbID,
+        userID,
+        threadID,
+        messageID,
+        null,
+        JSON.stringify([delivery]),
+        1,
+      ]);
+    }
+  }
+  if (newNotifRows.length > 0) {
+    const insertQuery = SQL`
+      INSERT INTO notifications
+        (id, user, thread, message, collapse_key, delivery, rescinded)
+      VALUES ${newNotifRows}
+    `;
+    await dbQuery(insertQuery);
+  }
 }
 
 function prepareIOSNotification(
