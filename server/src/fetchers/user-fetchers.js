54c01931d7effbd60064073de23b0c238b4698b7
Dominik Źrebiec <zrebcu@gmail.com>
2020-08-26 16:27:36 +0200

    [server] Add relationshipStatus to userInfo
    Reviewers: ashoat
    
    Reviewed By: ashoat
    
    Subscribers: Adrian, KatPo, palys-swm
    
    Differential Revision: https://phabricator.ashoat.com/D24

--- server/src/fetchers/user-fetchers.js
+++ server/src/fetchers/user-fetchers.js
@@ -5,6 +5,11 @@ import type {
   CurrentUserInfo,
   LoggedInUserInfo,
 } from 'lib/types/user-types';
+import {
+  undirectedStatus,
+  directedStatus,
+  userRelationshipStatus,
+} from 'lib/types/relationship-types';
 import type { Viewer } from '../session/viewer';
 
 import { ServerError } from 'lib/utils/errors';
@@ -53,28 +58,88 @@ async function fetchKnownUserInfos(
   }
 
   const query = SQL`
-    SELECT DISTINCT u.id, u.username FROM relationships_undirected r 
-    LEFT JOIN users u ON r.user1 = u.id OR r.user2 = u.id 
+    SELECT ru.user1, ru.user2, ru.status AS undirected_status,
+      rd1.status AS user1_directed_status, rd2.status AS user2_directed_status,
+      u.username
+    FROM relationships_undirected ru
+    LEFT JOIN relationships_directed rd1
+      ON rd1.user1 = ru.user1 AND rd1.user2 = ru.user2
+    LEFT JOIN relationships_directed rd2
+      ON rd2.user1 = ru.user2 AND rd2.user2 = ru.user1
+    LEFT JOIN users u
+      ON u.id != ${viewer.userID} AND (u.id = ru.user1 OR u.id = ru.user2)
   `;
   if (userIDs) {
     query.append(SQL`
-      WHERE (r.user1 = ${viewer.userID} AND r.user2 IN (${userIDs})) OR
-        (r.user1 IN (${userIDs}) AND r.user2 = ${viewer.userID})
+      WHERE (ru.user1 = ${viewer.userID} AND ru.user2 IN (${userIDs})) OR
+        (ru.user1 IN (${userIDs}) AND ru.user2 = ${viewer.userID})
     `);
   } else {
     query.append(SQL`
-      WHERE r.user1 = ${viewer.userID} OR r.user2 = ${viewer.userID}
+      WHERE ru.user1 = ${viewer.userID} OR ru.user2 = ${viewer.userID}
     `);
   }
+  query.append(SQL`
+    UNION SELECT id AS user1, NULL AS user2, NULL AS undirected_status,
+      NULL AS user1_directed_status, NULL AS user2_directed_status,
+      username
+    FROM users
+    WHERE id = ${viewer.userID}
+  `);
   const [result] = await dbQuery(query);
 
   const userInfos = {};
-  for (let row of result) {
-    const id = row.id.toString();
-    userInfos[id] = {
+  for (const row of result) {
+    const user1 = row.user1.toString();
+    const user2 = row.user2 ? row.user2.toString() : null;
+    const id = user1 === viewer.userID && user2 ? user2 : user1;
+    const userInfo = {
       id,
       username: row.username,
     };
+
+    if (!user2) {
+      userInfos[id] = userInfo;
+      continue;
+    }
+
+    let viewerDirectedStatus;
+    let targetDirectedStatus;
+    if (user1 === viewer.userID) {
+      viewerDirectedStatus = row.user1_directed_status;
+      targetDirectedStatus = row.user2_directed_status;
+    } else {
+      viewerDirectedStatus = row.user2_directed_status;
+      targetDirectedStatus = row.user1_directed_status;
+    }
+
+    const viewerBlockedTarget = viewerDirectedStatus === directedStatus.BLOCKED;
+    const targetBlockedViewer = targetDirectedStatus === directedStatus.BLOCKED;
+    const friendshipExists = row.undirected_status === undirectedStatus.FRIEND;
+    const viewerRequestedTargetFriendship =
+      viewerDirectedStatus === directedStatus.PENDING_FRIEND;
+    const targetRequestedViewerFriendship =
+      targetDirectedStatus === directedStatus.PENDING_FRIEND;
+
+    let relationshipStatus;
+    if (viewerBlockedTarget && targetBlockedViewer) {
+      relationshipStatus = userRelationshipStatus.BOTH_BLOCKED;
+    } else if (targetBlockedViewer) {
+      relationshipStatus = userRelationshipStatus.BLOCKED_VIEWER;
+    } else if (viewerBlockedTarget) {
+      relationshipStatus = userRelationshipStatus.BLOCKED_BY_VIEWER;
+    } else if (friendshipExists) {
+      relationshipStatus = userRelationshipStatus.FRIEND;
+    } else if (targetRequestedViewerFriendship) {
+      relationshipStatus = userRelationshipStatus.REQUEST_RECEIVED;
+    } else if (viewerRequestedTargetFriendship) {
+      relationshipStatus = userRelationshipStatus.REQUEST_SENT;
+    }
+
+    userInfos[id] = userInfo;
+    if (relationshipStatus) {
+      userInfos[id].relationshipStatus = relationshipStatus;
+    }
   }
 
   return userInfos;
