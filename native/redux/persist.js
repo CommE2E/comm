687716be7be2e290d124149344a93ae24012b6c1
Ashoat Tevosyan <ashoat@gmail.com>
2020-10-09 11:47:41 -0400

    [native] codeVersion -> 68
    Had to throw away 67 after uploading an Android build to test if the `targetSdkVersion` would work and then realizing I had a couple bugs I still had to fix.

--- native/redux/persist.js
+++ native/redux/persist.js
@@ -190,7 +190,7 @@ const persistConfig = {
   timeout: __DEV__ ? 0 : undefined,
 };
 
-const codeVersion = 66;
+const codeVersion = 68;
 
 // This local exists to avoid a circular dependency where redux-setup needs to
 // import all the navigation and screen stuff, but some of those screens want to
