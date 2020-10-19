CommE2E/comm

commit 4422581fa4e904035938ad9b4fbfccdc47c2b5b4

Fix log out

native/account/logged-out-modal.react.js
@@ -78,10 +78,12 @@ import {
 import {
   type SimpleStateSetter,
   type StateContainer,
+  type StateChange,
   setStateForContainer,
 } from '../utils/state-container';
 
 const forceInset = { top: 'always', bottom: 'always' };
+let initialAppLoad = true;
 
 type LoggedOutMode = "loading" | "prompt" | "log-in" | "register";
 type Props = {
@@ -155,9 +157,8 @@ class InnerLoggedOutModal extends React.PureComponent<Props, State> {
 
     // Man, this is a lot of boilerplate just to containerize some state.
     // Mostly due to Flow typing requirements...
-    const setState = this.setState.bind(this);
     const setLogInState = setStateForContainer(
-      setState,
+      this.guardedSetState,
       (change: $Shape<LogInState>) => (fullState: State) => ({
         logInState: {
           ...fullState.logInState,
@@ -166,7 +167,7 @@ class InnerLoggedOutModal extends React.PureComponent<Props, State> {
       }),
     );
     const setRegisterState = setStateForContainer(
-      setState,
+      this.guardedSetState,
       (change: $Shape<RegisterState>) => (fullState: State) => ({
         registerState: {
           ...fullState.registerState,
@@ -183,19 +184,6 @@ class InnerLoggedOutModal extends React.PureComponent<Props, State> {
       emailInputText: "",
       passwordInputText: "",
       confirmPasswordInputText: "",
-    };
-    const clearState = async () => {
-      await sleep(500);
-      setState((fullState: State) => ({
-        logInState: {
-          ...fullState.logInState,
-          state: initialLogInState,
-        },
-        registerState: {
-          ...fullState.registerState,
-          state: initialRegisterState,
-        },
-      }));
     };
 
     this.state = {
@@ -216,7 +204,6 @@ class InnerLoggedOutModal extends React.PureComponent<Props, State> {
           passwordInputText: "",
         },
         setState: setLogInState,
-        clearState,
       },
       registerState: {
         state: {
@@ -226,11 +213,16 @@ class InnerLoggedOutModal extends React.PureComponent<Props, State> {
           confirmPasswordInputText: "",
         },
         setState: setRegisterState,
-        clearState,
       },
     };
     if (props.rehydrateConcluded) {
       this.nextMode = "prompt";
+    }
+  }
+
+  guardedSetState = (change: StateChange<State>) => {
+    if (this.mounted) {
+      this.setState(change);
     }
   }
 
@@ -241,9 +233,7 @@ class InnerLoggedOutModal extends React.PureComponent<Props, State> {
     } catch (e) {
       onePasswordSupported = false;
     }
-    if (this.mounted) {
-      this.setState({ onePasswordSupported });
-    }
+    this.guardedSetState({ onePasswordSupported });
   }
 
   componentDidMount() {
@@ -295,11 +285,16 @@ class InnerLoggedOutModal extends React.PureComponent<Props, State> {
   // This gets triggered when an app is killed and restarted
   // Not when it is returned from being backgrounded
   async onInitialAppLoad(nextProps: Props) {
+    if (!initialAppLoad) {
+      return;
+    }
+    initialAppLoad = false;
+
     let cookie = nextProps.cookie;
     const urlPrefix = nextProps.urlPrefix;
     const showPrompt = () => {
       this.nextMode = "prompt";
-      this.setState({ mode: "prompt" });
+      this.guardedSetState({ mode: "prompt" });
       Animated.timing(
         this.state.buttonOpacity,
         {
@@ -588,7 +583,7 @@ class InnerLoggedOutModal extends React.PureComponent<Props, State> {
     let opacityListenerID: ?string = null;
     const opacityListener = (animatedUpdate: { value: number }) => {
       if (animatedUpdate.value === 0) {
-        this.setState({ mode: this.nextMode });
+        this.guardedSetState({ mode: this.nextMode });
         invariant(opacityListenerID, "should be set");
         this.state.panelOpacity.removeListener(opacityListenerID);
       }
@@ -750,7 +745,7 @@ class InnerLoggedOutModal extends React.PureComponent<Props, State> {
   onPressLogIn = () => {
     this.opacityChangeQueued = true;
     this.nextMode = "log-in";
-    this.setState({ mode: "log-in" });
+    this.guardedSetState({ mode: "log-in" });
     if (this.activeKeyboard) {
       // If keyboard isn't currently active, keyboardShow will handle the
       // animation. This is so we can run all animations in parallel
@@ -763,7 +758,7 @@ class InnerLoggedOutModal extends React.PureComponent<Props, State> {
   onPressRegister = () => {
     this.opacityChangeQueued = true;
     this.nextMode = "register";
-    this.setState({ mode: "register" });
+    this.guardedSetState({ mode: "register" });
     if (this.activeKeyboard) {
       // If keyboard isn't currently active, keyboardShow will handle the
       // animation. This is so we can run all animations in parallel

native/account/log-in-panel.react.js
@@ -233,7 +233,6 @@ class LogInPanel extends React.PureComponent<Props> {
         deviceTokenUpdateRequest,
       });
       this.props.setActiveAlert(false);
-      this.props.state.clearState();
       await setNativeCredentials({
         username: result.currentUserInfo.username,
         password: this.props.state.state.passwordInputText,

native/account/register-panel.react.js
@@ -316,7 +316,6 @@ class RegisterPanel extends React.PureComponent<Props> {
         this.props.state.state.passwordInputText,
       );
       this.props.setActiveAlert(false);
-      this.props.state.clearState();
       await setNativeCredentials({
         username: result.currentUserInfo.username,
         password: this.props.state.state.passwordInputText,

native/navigation-setup.js
@@ -476,12 +476,7 @@ function removeModals(
 }
 
 function resetNavInfoAndEnsureLoggedOutModalPresence(state: NavInfo): NavInfo {
-  const navigationState = {
-    ...state.navigationState,
-    routes: [
-      ...state.navigationState.routes,
-    ],
-  };
+  const navigationState = { ...state.navigationState };
   navigationState.routes[0] = defaultNavInfo.navigationState.routes[0];
   const currentModalIndex =
     _findIndex(['routeName', LoggedOutModalRouteName])(navigationState.routes);

native/utils/state-container.js
@@ -7,7 +7,7 @@ export type SimpleStateSetter<S: {}> = (
   callback?: () => mixed,
 ) => void;
 
-type StateChange<S: {}> = $Shape<S> | S => $Shape<S>;
+export type StateChange<S: {}> = $Shape<S> | S => $Shape<S>;
 type StateSetter<S: {}> = (
   newState: StateChange<S>,
   callback?: () => mixed,
@@ -16,13 +16,11 @@ type StateSetter<S: {}> = (
 export type StateContainer<S: {}> = {
   state: S,
   setState: SimpleStateSetter<S>,
-  clearState: () => mixed,
 };
 
 const stateContainerPropType = PropTypes.shape({
   state: PropTypes.object.isRequired,
   setState: PropTypes.func.isRequired,
-  clearState: PropTypes.func.isRequired,
 });
 
 function setStateForContainer<FullState: {}, OurContainer: {}>(

server/src/responders/report-responders.js
@@ -19,7 +19,6 @@ import {
   fetchErrorReportInfos,
   fetchReduxToolsImport,
 } from '../fetchers/report-fetchers';
-import { fetchViewerForJSONRequest } from '../session/cookies';
 
 const errorReportCreationRequestInputValidator = tShape({
   deviceType: t.enums.of(['ios', 'android']),

server/src/responders/user-responders.js
@@ -38,7 +38,7 @@ import {
   createNewAnonymousCookie,
   createNewUserCookie,
   deleteCookie,
-  deleteCookiesWithDeviceTokens,
+  deleteCookiesOnLogOut,
 } from '../session/cookies';
 import { deleteAccount } from '../deleters/account-deleters';
 import createAccount from '../creators/account-creator';
@@ -47,7 +47,6 @@ import { verifyThreadID } from '../fetchers/thread-fetchers';
 import { dbQuery, SQL } from '../database';
 import { fetchMessageInfos } from '../fetchers/message-fetchers';
 import { fetchEntryInfos } from '../fetchers/entry-fetchers';
-import { fetchDeviceTokensForCookie } from '../fetchers/device-token-fetchers';
 import { deviceTokenUpdater } from '../updaters/device-token-updaters';
 import { deviceTokenUpdateRequestInputValidator } from './device-responders';
 
@@ -112,12 +111,9 @@ async function logOutResponder(
 ): Promise<LogOutResponse> {
   const cookieID = viewer.getData().cookieID;
   if (viewer.loggedIn) {
-    const deviceTokens = await fetchDeviceTokensForCookie(cookieID);
     const [ anonymousViewerData ] = await Promise.all([
       createNewAnonymousCookie(),
-      deleteCookiesWithDeviceTokens(viewer.userID, deviceTokens),
-      // deleteCookiesWithDeviceTokens should delete it, but just in case...
-      deleteCookie(cookieID),
+      deleteCookiesOnLogOut(viewer.userID, cookieID),
     ]);
     viewer.setNewCookie(anonymousViewerData);
   }
@@ -201,7 +197,10 @@ async function logInResponder(
   }
   const id = userRow.id.toString();
 
-  const userViewerData = await createNewUserCookie(id);
+  const [ userViewerData ] = await Promise.all([
+    createNewUserCookie(id),
+    deleteCookie(viewer.getData().cookieID),
+  ]);
   viewer.setNewCookie(userViewerData);
   const newPingTime = Date.now();
 

server/src/session/cookies.js
@@ -18,6 +18,7 @@ import { fetchThreadInfos } from '../fetchers/thread-fetchers';
 import urlFacts from '../../facts/url';
 import createIDs from '../creators/id-creator';
 import { assertSecureRequest } from '../utils/security-utils';
+import { fetchDeviceTokensForCookie } from '../fetchers/device-token-fetchers';
 
 const { baseDomain, basePath, https } = urlFacts;
 
@@ -284,20 +285,20 @@ async function deleteCookie(cookieID: string): Promise<void> {
   `);
 }
 
-async function deleteCookiesWithDeviceTokens(
+async function deleteCookiesOnLogOut(
   userID: string,
-  deviceTokens: DeviceTokens,
+  cookieID: string,
 ): Promise<void> {
-  const conditions = [];
+  const deviceTokens = await fetchDeviceTokensForCookie(cookieID);
+
+  const conditions = [ SQL`c.id = ${cookieID}` ];
   if (deviceTokens.ios) {
-    conditions.push(SQL`ios_device_token = ${deviceTokens.ios}`);
+    conditions.push(SQL`c.ios_device_token = ${deviceTokens.ios}`);
   }
   if (deviceTokens.android) {
-    conditions.push(SQL`android_device_token = ${deviceTokens.android}`);
+    conditions.push(SQL`c.android_device_token = ${deviceTokens.android}`);
   }
-  if (conditions.length === 0) {
-    return;
-  }
+
   const query = SQL`
     DELETE c, i
     FROM cookies c
@@ -305,6 +306,7 @@ async function deleteCookiesWithDeviceTokens(
     WHERE c.user = ${userID} AND
   `;
   query.append(mergeOrConditions(conditions));
+
   await dbQuery(query);
 }
 
@@ -384,7 +386,7 @@ export {
   fetchViewerForHomeRequest,
   createNewAnonymousCookie,
   deleteCookie,
-  deleteCookiesWithDeviceTokens,
+  deleteCookiesOnLogOut,
   createNewUserCookie,
   addCookieToJSONResponse,
   addCookieToHomeResponse,

commit 8d983c682ee7bea23d5bc9d8becfb9896ef534bb

[native] Fix callbacks on StateContainer.setState
Summary: We use these callbacks to focus specific input elements after errors. I broke them in 4422581fa4e904035938ad9b4fbfccdc47c2b5b4.

Test Plan: I tested triggering error conditions from `RegisterPanel` and making sure the correct input elements got focused after I closed the `Alert`s.

Reviewers: palys-swm

Reviewed By: palys-swm

Subscribers: KatPo, zrebcu411, Adrian

Differential Revision: https://phabricator.ashoat.com/D252

native/account/logged-out-modal.react.js
@@ -217,9 +217,9 @@ class LoggedOutModal extends React.PureComponent<Props, State> {
     this.forgotPasswordLinkOpacityValue = this.forgotPasswordLinkOpacity();
   }
 
-  guardedSetState = (change: StateChange<State>) => {
+  guardedSetState = (change: StateChange<State>, callback?: () => mixed) => {
     if (this.mounted) {
-      this.setState(change);
+      this.setState(change, callback);
     }
   };
 