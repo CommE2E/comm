6f65d2aa45adb856780477a39d3f39d9f7fc7b9a
Ashoat Tevosyan <ashoat@gmail.com>
2020-11-08 18:34:34 -0500

    [native] Get rid of withNavContext and connectNav
    Summary:
    They're no longer used. Going forward, let's use hooks for data binding.
    
    I also got rid of `navContextPropType`.
    
    Test Plan: Flow
    
    Reviewers: palys-swm
    
    Reviewed By: palys-swm
    
    Subscribers: KatPo, zrebcu411, Adrian
    
    Differential Revision: https://phabricator.ashoat.com/D378

--- native/navigation/navigation-context.js
+++ native/navigation/navigation-context.js
@@ -9,7 +9,6 @@ import type { ChatRouterNavigationAction } from '../chat/chat-router';
 import type { OverlayRouterNavigationAction } from './overlay-router';
 
 import * as React from 'react';
-import PropTypes from 'prop-types';
 
 export type NavAction =
   | CommonAction
@@ -17,71 +16,10 @@ export type NavAction =
   | ChatRouterNavigationAction
   | OverlayRouterNavigationAction;
 export type NavContextType = {|
-  state: PossiblyStaleNavigationState,
-  dispatch: (action: NavAction) => void,
+  +state: PossiblyStaleNavigationState,
+  +dispatch: (action: NavAction) => void,
 |};
-
-const navContextPropType = PropTypes.shape({
-  state: PropTypes.object.isRequired,
-  dispatch: PropTypes.func.isRequired,
-});
 
 const NavContext = React.createContext<?NavContextType>(null);
 
-function withNavContext<
-  AllProps: {},
-  ComponentType: React.ComponentType<AllProps>,
->(
-  Component: ComponentType,
-): React.ComponentType<
-  $Diff<React.ElementConfig<ComponentType>, { navContext: ?NavContextType }>,
-> &
-  ComponentType {
-  function NavContextHOC(
-    props: $Diff<
-      React.ElementConfig<ComponentType>,
-      { navContext: ?NavContextType },
-    >,
-  ) {
-    return (
-      <NavContext.Consumer>
-        {(value) => <Component {...props} navContext={value} />}
-      </NavContext.Consumer>
-    );
-  }
-  // $FlowFixMe React.memo typing fixed in later version of Flow
-  return React.memo(NavContextHOC);
-}
-
-function connectNav<
-  AllProps: {},
-  ConnectedProps: AllProps,
-  ComponentType: React.ComponentType<$Exact<AllProps>>,
->(
-  connectFunc: (
-    navContext: ?NavContextType,
-    ownProps: $Diff<$Exact<AllProps>, $Exact<ConnectedProps>>,
-  ) => $Exact<ConnectedProps>,
-): (
-  Component: ComponentType,
-) => React.ComponentType<$Diff<$Exact<AllProps>, $Exact<ConnectedProps>>> &
-  ComponentType {
-  return (Component: ComponentType) => {
-    function NavConnectedComponent(props: {|
-      ...$Diff<React.ElementConfig<ComponentType>, $Exact<ConnectedProps>>,
-      navContext: ?NavContextType,
-    |}) {
-      const { navContext, ...rest } = props;
-      const connectedProps = connectFunc(navContext, rest);
-      return <Component {...rest} {...connectedProps} />;
-    }
-    NavConnectedComponent.displayName = Component.displayName
-      ? `NavConnected(${Component.displayName})`
-      : `NavConnectedComponent`;
-    const MemoizedNavConnectedComponent = React.memo(NavConnectedComponent);
-    // $FlowFixMe React.memo typing fixed in later version of Flow
-    return withNavContext(MemoizedNavConnectedComponent);
-  };
-}
-
-export { NavContext, withNavContext, connectNav, navContextPropType };
+export { NavContext };
