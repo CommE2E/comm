65510917a3c3b89790bc8be547e947ee5104b179
Ashoat Tevosyan <ashoat@gmail.com>
2020-11-23 20:02:00 -0500

    [native] Convert TextMessageTooltipButton to hook
    Summary: I want to call `useTextMessageRulesFunc` in `TextMessageTooltipButton` so I can set a `MessageListContext`. We need this so the `InnerTextMessage` inside `TextMessageTooltipButton` has access to Markdown rules.
    
    Test Plan: Flow, will test in combination with later diffs
    
    Reviewers: KatPo, palys-swm
    
    Reviewed By: KatPo
    
    Subscribers: zrebcu411, Adrian
    
    Differential Revision: https://phabricator.ashoat.com/D438

--- native/chat/text-message-tooltip-button.react.js
+++ native/chat/text-message-tooltip-button.react.js
@@ -1,22 +1,14 @@
 // @flow
 
-import {
-  verticalBoundsPropType,
-  layoutCoordinatesPropType,
-} from '../types/layout-types';
-import { chatMessageItemPropType } from 'lib/selectors/chat-selectors';
-import type { AppState } from '../redux/redux-setup';
 import type { AppNavigationProp } from '../navigation/app-navigator.react';
 import type { TooltipRoute } from '../navigation/tooltip.react';
 
 import * as React from 'react';
 import Animated from 'react-native-reanimated';
-import PropTypes from 'prop-types';
-
-import { connect } from 'lib/utils/redux-utils';
 
 import { InnerTextMessage } from './inner-text-message.react';
 import { MessageHeader } from './message-header.react';
+import { useSelector } from '../redux/redux-utils';
 
 /* eslint-disable import/no-named-as-default-member */
 const { Value } = Animated;
@@ -26,56 +18,32 @@ type Props = {|
   +navigation: AppNavigationProp<'TextMessageTooltipModal'>,
   +route: TooltipRoute<'TextMessageTooltipModal'>,
   +progress: Value,
-  // Redux state
-  +windowWidth: number,
 |};
-class TextMessageTooltipButton extends React.PureComponent<Props> {
-  static propTypes = {
-    navigation: PropTypes.shape({
-      goBackOnce: PropTypes.func.isRequired,
-    }).isRequired,
-    route: PropTypes.shape({
-      params: PropTypes.shape({
-        initialCoordinates: layoutCoordinatesPropType.isRequired,
-        verticalBounds: verticalBoundsPropType.isRequired,
-        location: PropTypes.oneOf(['above', 'below']),
-        margin: PropTypes.number,
-        item: chatMessageItemPropType.isRequired,
-      }).isRequired,
-    }).isRequired,
-    progress: PropTypes.object.isRequired,
-    windowWidth: PropTypes.number.isRequired,
-  };
-
-  get headerStyle() {
-    const { initialCoordinates } = this.props.route.params;
+function TextMessageTooltipButton(props: Props) {
+  const { progress } = props;
+  const windowWidth = useSelector((state) => state.dimensions.width);
+  const { initialCoordinates } = props.route.params;
+  const headerStyle = React.useMemo(() => {
     const bottom = initialCoordinates.height;
     return {
-      opacity: this.props.progress,
+      opacity: progress,
       position: 'absolute',
       left: -initialCoordinates.x,
-      width: this.props.windowWidth,
+      width: windowWidth,
       bottom,
     };
-  }
+  }, [progress, windowWidth, initialCoordinates]);
 
-  render() {
-    const { item } = this.props.route.params;
-    return (
-      <React.Fragment>
-        <Animated.View style={this.headerStyle}>
-          <MessageHeader item={item} focused={true} display="modal" />
-        </Animated.View>
-        <InnerTextMessage item={item} onPress={this.onPress} />
-      </React.Fragment>
-    );
-  }
-
-  onPress = () => {
-    this.props.navigation.goBackOnce();
-  };
+  const { navigation } = props;
+  const { item } = props.route.params;
+  return (
+    <>
+      <Animated.View style={headerStyle}>
+        <MessageHeader item={item} focused={true} display="modal" />
+      </Animated.View>
+      <InnerTextMessage item={item} onPress={navigation.goBackOnce} />
+    </>
+  );
 }
 
-export default connect((state: AppState) => ({
-  windowWidth: state.dimensions.width,
-}))(TextMessageTooltipButton);
+export default TextMessageTooltipButton;
