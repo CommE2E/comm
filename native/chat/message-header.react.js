7b6a31fb3b9745b9cd8b5d5f441bba56c37aca9c
KatPo <kasko.po@gmail.com>
2020-12-03 15:19:15 +0100

    [native] Use hook instead of connect functions and HOC in MessageHeader
    Test Plan: Flow
    
    Reviewers: ashoat, palys-swm
    
    Reviewed By: ashoat
    
    Subscribers: zrebcu411, Adrian
    
    Differential Revision: https://phabricator.ashoat.com/D480

--- native/chat/message-header.react.js
+++ native/chat/message-header.react.js
@@ -1,13 +1,11 @@
 // @flow
 
 import { stringForUser } from 'lib/shared/user-utils';
-import { connect } from 'lib/utils/redux-utils';
 import * as React from 'react';
 import { View } from 'react-native';
 
 import { SingleLine } from '../components/single-line.react';
-import type { AppState } from '../redux/redux-setup';
-import { styleSelector } from '../themes/colors';
+import { useStyles } from '../themes/colors';
 
 import { clusterEndHeight } from './composed-message.react';
 import type { ChatMessageInfoItemWithHeight } from './message.react';
@@ -15,13 +13,12 @@ import type { DisplayType } from './timestamp.react';
 import { Timestamp, timestampHeight } from './timestamp.react';
 
 type Props = {|
-  item: ChatMessageInfoItemWithHeight,
-  focused: boolean,
-  display: DisplayType,
-  // Redux state
-  styles: typeof styles,
+  +item: ChatMessageInfoItemWithHeight,
+  +focused: boolean,
+  +display: DisplayType,
 |};
 function MessageHeader(props: Props) {
+  const styles = useStyles(unboundStyles);
   const { item, focused, display } = props;
   const { creator, time } = item.messageInfo;
   const { isViewer } = creator;
@@ -29,9 +26,9 @@ function MessageHeader(props: Props) {
 
   let authorName = null;
   if (!isViewer && (modalDisplay || item.startsCluster)) {
-    const style = [props.styles.authorName];
+    const style = [styles.authorName];
     if (modalDisplay) {
-      style.push(props.styles.modal);
+      style.push(styles.modal);
     }
     authorName = (
       <SingleLine style={style}>{stringForUser(creator)}</SingleLine>
@@ -65,7 +62,7 @@ function MessageHeader(props: Props) {
 
 const authorNameHeight = 25;
 
-const styles = {
+const unboundStyles = {
   authorName: {
     bottom: 0,
     color: 'listBackgroundSecondaryLabel',
@@ -81,10 +78,5 @@ const styles = {
     color: 'white',
   },
 };
-const stylesSelector = styleSelector(styles);
 
-const ConnectedMessageHeader = connect((state: AppState) => ({
-  styles: stylesSelector(state),
-}))(MessageHeader);
-
-export { ConnectedMessageHeader as MessageHeader, authorNameHeight };
+export { MessageHeader, authorNameHeight };
