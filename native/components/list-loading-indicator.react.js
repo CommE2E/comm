6f3cd5a14d32b86bec9c0892a75d367e89420a05
KatPo <kasko.po@gmail.com>
2020-12-03 15:30:10 +0100

    [native] Use hook instead of connect functions and HOC in ListLoadingIndicator
    Test Plan: Flow
    
    Reviewers: ashoat, palys-swm
    
    Reviewed By: ashoat
    
    Subscribers: zrebcu411, Adrian
    
    Differential Revision: https://phabricator.ashoat.com/D483

--- native/components/list-loading-indicator.react.js
+++ native/components/list-loading-indicator.react.js
@@ -1,39 +1,29 @@
 // @flow
 
-import { connect } from 'lib/utils/redux-utils';
 import * as React from 'react';
 import { ActivityIndicator } from 'react-native';
 
-import type { AppState } from '../redux/redux-setup';
-import type { Colors } from '../themes/colors';
-import { colorsSelector, styleSelector } from '../themes/colors';
+import { useStyles, useColors } from '../themes/colors';
 
-type Props = {|
-  // Redux state
-  colors: Colors,
-  styles: typeof styles,
-|};
-function ListLoadingIndicator(props: Props) {
-  const { listBackgroundLabel } = props.colors;
+function ListLoadingIndicator() {
+  const styles = useStyles(unboundStyles);
+  const colors = useColors();
+  const { listBackgroundLabel } = colors;
   return (
     <ActivityIndicator
       color={listBackgroundLabel}
       size="large"
-      style={props.styles.loadingIndicator}
+      style={styles.loadingIndicator}
     />
   );
 }
 
-const styles = {
+const unboundStyles = {
   loadingIndicator: {
     backgroundColor: 'listBackground',
     flex: 1,
     padding: 10,
   },
 };
-const stylesSelector = styleSelector(styles);
 
-export default connect((state: AppState) => ({
-  colors: colorsSelector(state),
-  styles: stylesSelector(state),
-}))(ListLoadingIndicator);
+export default ListLoadingIndicator;
