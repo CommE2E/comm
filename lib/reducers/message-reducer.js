CommE2E/comm

commit 46403debd4cd7d64552483f77d7956c3fadda65e

RN0.63 11/16: Fix Flow bug when merging Media
Summary: Flow is worried about `Video` getting merged with an `Image`

Test Plan: Flow

Reviewers: palys-swm

Subscribers: KatPo, zrebcu411, Adrian

Differential Revision: https://phabricator.ashoat.com/D321

lib/reducers/message-reducer.js
@@ -771,8 +771,22 @@ function reduceMessageStore(
     for (let singleMedia of message.media) {
       if (singleMedia.id !== currentMediaID) {
         media.push(singleMedia);
-      } else {
+      } else if (singleMedia.type === 'photo') {
         replaced = true;
+        invariant(
+          mediaUpdate.type === 'photo',
+          'media with matching IDs should have same media types',
+        );
+        media.push({
+          ...singleMedia,
+          ...mediaUpdate,
+        });
+      } else if (singleMedia.type === 'video') {
+        replaced = true;
+        invariant(
+          mediaUpdate.type === 'video',
+          'media with matching IDs should have same media types',
+        );
         media.push({
           ...singleMedia,
           ...mediaUpdate,

commit c15f4bf08066f0c863822a8e3815bd2912a2f045

[lib] Don't require type parameter in mediaUpdate
Summary:
This diff fixes a bug on web where if you select an image for upload and immediately send the message, the message does not successfully send.

The bug was introduced in 46403debd4cd7d64552483f77d7956c3fadda65e. That diff expects `mediaUpdate` to always have a `type` parameter.

But we allow `$Shape<Media>` in `UpdateMultimediaMessageMediaPayload`, which means that a `type` parameter does not need to be set. We call it without `type` on web [here](https://github.com/Ashoat/squadcal/blob/master/web/input/input-state-container.react.js#L587-L589). That's what causes the bug.

Turns out we don't need the `invariant` to avoid the Flow error. In fact I think Flow is missing a type error, but I don't think the work necessary to fix the types here is really worth it.

Test Plan: Make sure the bug is fixed

Reviewers: palys-swm

Reviewed By: palys-swm

Subscribers: KatPo, zrebcu411, Adrian

Differential Revision: https://phabricator.ashoat.com/D489

lib/reducers/message-reducer.js
@@ -773,20 +773,12 @@ function reduceMessageStore(
         media.push(singleMedia);
       } else if (singleMedia.type === 'photo') {
         replaced = true;
-        invariant(
-          mediaUpdate.type === 'photo',
-          'media with matching IDs should have same media types',
-        );
         media.push({
           ...singleMedia,
           ...mediaUpdate,
         });
       } else if (singleMedia.type === 'video') {
         replaced = true;
-        invariant(
-          mediaUpdate.type === 'video',
-          'media with matching IDs should have same media types',
-        );
         media.push({
           ...singleMedia,
           ...mediaUpdate,