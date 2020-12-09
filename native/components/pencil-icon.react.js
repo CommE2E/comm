77efdf52be5cf70b8ea190fb0c98730c9e123346
KatPo <kasko.po@gmail.com>
2020-12-09 13:10:53 +0100

    [native] Use hook instead of connect functions and HOC in PencilIcon
    Test Plan: Flow
    
    Reviewers: ashoat, palys-swm
    
    Reviewed By: ashoat
    
    Subscribers: zrebcu411, Adrian
    
    Differential Revision: https://phabricator.ashoat.com/D496

--- native/components/pencil-icon.react.js
+++ native/components/pencil-icon.react.js
@@ -4,20 +4,14 @@ import * as React from 'react';
 import { Platform } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-import { connect } from 'lib/utils/redux-utils';
+import { useStyles } from '../themes/colors';
 
-import type { AppState } from '../redux/redux-setup';
-import { styleSelector } from '../themes/colors';
-
-type Props = {|
-  // Redux state
-  styles: typeof styles,
-|};
-function PencilIcon(props: Props) {
-  return <Icon name="pencil" size={16} style={props.styles.editIcon} />;
+function PencilIcon() {
+  const styles = useStyles(unboundStyles);
+  return <Icon name="pencil" size={16} style={styles.editIcon} />;
 }
 
-const styles = {
+const unboundStyles = {
   editIcon: {
     color: 'link',
     lineHeight: 20,
@@ -25,8 +19,5 @@ const styles = {
     textAlign: 'right',
   },
 };
-const stylesSelector = styleSelector(styles);
 
-export default connect((state: AppState) => ({
-  styles: stylesSelector(state),
-}))(PencilIcon);
+export default PencilIcon;
