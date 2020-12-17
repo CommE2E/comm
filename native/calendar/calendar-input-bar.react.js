5d0a6b1591cf9fae6f182176d563bb284b9e493d
KatPo <kasko.po@gmail.com>
2020-12-17 14:41:07 +0100

    [native] Use hook instead of connect functions and HOC in CalendarInputBar
    Test Plan: Flow
    
    Reviewers: ashoat, palys-swm
    
    Reviewed By: ashoat
    
    Subscribers: zrebcu411, Adrian
    
    Differential Revision: https://phabricator.ashoat.com/D518

--- native/calendar/calendar-input-bar.react.js
+++ native/calendar/calendar-input-bar.react.js
@@ -3,35 +3,29 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
 
-import { connect } from 'lib/utils/redux-utils';
-
 import Button from '../components/button.react';
-import type { AppState } from '../redux/redux-setup';
-import { styleSelector } from '../themes/colors';
+import { useStyles } from '../themes/colors';
 
 type Props = {|
-  onSave: () => void,
-  disabled: boolean,
-  // Redux state
-  styles: typeof styles,
+  +onSave: () => void,
+  +disabled: boolean,
 |};
 function CalendarInputBar(props: Props) {
-  const inactiveStyle = props.disabled
-    ? props.styles.inactiveContainer
-    : undefined;
+  const styles = useStyles(unboundStyles);
+  const inactiveStyle = props.disabled ? styles.inactiveContainer : undefined;
   return (
     <View
-      style={[props.styles.container, inactiveStyle]}
+      style={[styles.container, inactiveStyle]}
       pointerEvents={props.disabled ? 'none' : 'auto'}
     >
       <Button onPress={props.onSave} iosActiveOpacity={0.5}>
-        <Text style={props.styles.saveButtonText}>Save</Text>
+        <Text style={styles.saveButtonText}>Save</Text>
       </Button>
     </View>
   );
 }
 
-const styles = {
+const unboundStyles = {
   container: {
     alignItems: 'flex-end',
     backgroundColor: 'listInputBar',
@@ -47,8 +41,5 @@ const styles = {
     padding: 8,
   },
 };
-const stylesSelector = styleSelector(styles);
 
-export default connect((state: AppState) => ({
-  styles: stylesSelector(state),
-}))(CalendarInputBar);
+export default CalendarInputBar;
