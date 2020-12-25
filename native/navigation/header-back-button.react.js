9481589559286b04d911fd206adff2b0916c1714
Ashoat Tevosyan <ashoat@gmail.com>
2020-12-24 22:07:54 -0500

    [native] Convert HeaderBackButton into Hook
    Test Plan: Flow
    
    Reviewers: palys-swm
    
    Reviewed By: palys-swm
    
    Subscribers: KatPo, zrebcu411, Adrian
    
    Differential Revision: https://phabricator.ashoat.com/D560

--- native/navigation/header-back-button.react.js
+++ native/navigation/header-back-button.react.js
@@ -3,26 +3,14 @@
 import { HeaderBackButton as BaseHeaderBackButton } from '@react-navigation/stack';
 import * as React from 'react';
 
-import { connect } from 'lib/utils/redux-utils';
+import { useColors } from '../themes/colors';
 
-import type { AppState } from '../redux/redux-setup';
-import type { Colors } from '../themes/colors';
-import { colorsSelector } from '../themes/colors';
-
-type Props = {
-  ...React.ElementConfig<typeof BaseHeaderBackButton>,
-  // Redux state
-  colors: Colors,
-};
+type Props = React.ElementConfig<typeof BaseHeaderBackButton>;
 function HeaderBackButton(props: Props) {
+  const { link: tintColor } = useColors();
   if (!props.canGoBack) {
     return null;
   }
-  const { colors, ...rest } = props;
-  const { link: tintColor } = colors;
-  return <BaseHeaderBackButton {...rest} tintColor={tintColor} />;
+  return <BaseHeaderBackButton {...props} tintColor={tintColor} />;
 }
-
-export default connect((state: AppState) => ({
-  colors: colorsSelector(state),
-}))(HeaderBackButton);
+export default HeaderBackButton;
