fbcd65535d2a4f400fb67423b7c36d813c2dc116
Ashoat Tevosyan <ashoat@gmail.com>
2020-12-29 01:09:28 -0500

    [server] Script to rename user
    Summary: This will change Atul's SquadCal username to `atul`
    
    Test Plan: I ran the script with a test user in my local environment
    
    Reviewers: atul, palys-swm
    
    Reviewed By: palys-swm
    
    Subscribers: KatPo, zrebcu411, Adrian
    
    Differential Revision: https://phabricator.ashoat.com/D563

---
+++ server/src/scripts/rename-user.js
@@ -0,0 +1,40 @@
+// @flow
+
+import { updateTypes } from 'lib/types/update-types';
+
+import { createUpdates } from '../creators/update-creator';
+import { dbQuery, SQL } from '../database/database';
+import { fetchKnownUserInfos } from '../fetchers/user-fetchers';
+import { createScriptViewer } from '../session/scripts';
+import { main } from './utils';
+
+const userID = '518252';
+const newUsername = 'atul';
+
+async function renameUser() {
+  const [adjacentUsers] = await Promise.all([
+    fetchKnownUserInfos(createScriptViewer(userID)),
+    dbQuery(
+      SQL`UPDATE users SET username = ${newUsername} WHERE id = ${userID}`,
+    ),
+  ]);
+
+  const updateDatas = [];
+  const time = Date.now();
+  updateDatas.push({
+    type: updateTypes.UPDATE_CURRENT_USER,
+    userID,
+    time,
+  });
+  for (const adjacentUserID in adjacentUsers) {
+    updateDatas.push({
+      type: updateTypes.UPDATE_USER,
+      userID: adjacentUserID,
+      time,
+      updatedUserID: userID,
+    });
+  }
+  await createUpdates(updateDatas);
+}
+
+main([renameUser]);
