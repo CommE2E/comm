0cdb5adac8a8b5707c9b979f0e14e95f5a9e4f00
Ashoat Tevosyan <ashoat@comm.app>
2021-01-13 10:09:08 -0800

    Include sourceMessageID in all ThreadInfo structs
    Summary: I actually don't think this should cause any issues with older clients, and since we don't have sidebars on prod yet, I think we can avoid having to run a script to update `ThreadStore`s. Should be pretty easy!
    
    Test Plan: Will test this together with the next diff, which will make the `sourceMessageID` field required when creating new sidebars
    
    Reviewers: palys-swm, KatPo
    
    Subscribers: zrebcu411, Adrian, atul, subnub
    
    Differential Revision: https://phabricator.ashoat.com/D593

--- server/src/fetchers/thread-fetchers.js
+++ server/src/fetchers/thread-fetchers.js
@@ -19,9 +19,9 @@ async function fetchServerThreadInfos(
 
   const query = SQL`
     SELECT t.id, t.name, t.parent_thread_id, t.color, t.description,
-      t.type, t.creation_time, t.default_role, r.id AS role,
+      t.type, t.creation_time, t.default_role, t.source_message, r.id AS role,
       r.name AS role_name, r.permissions AS role_permissions, m.user,
-      m.permissions, m.subscription, 
+      m.permissions, m.subscription,
       m.last_read_message < m.last_message AS unread
     FROM threads t
     LEFT JOIN (
@@ -53,6 +53,7 @@ async function fetchServerThreadInfos(
           : null,
         members: [],
         roles: {},
+        sourceMessageID: row.source_message,
       };
     }
     const role = row.role.toString();
