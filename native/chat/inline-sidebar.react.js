a1ec80be6ceb4c366a8ee2178bf7954d2533c72b
Tomasz Palys <tomasz.palys@swmansion.com>
2021-02-04 17:26:55 +0100

    [native] Display the senders
    Test Plan: Open a parent thread and check if senders were displayed correctly
    
    Reviewers: ashoat
    
    Reviewed By: ashoat
    
    Subscribers: KatPo, zrebcu411, Adrian, atul, subnub, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D698

--- native/chat/inline-sidebar.react.js
+++ native/chat/inline-sidebar.react.js
@@ -5,10 +5,14 @@ import * as React from 'react';
 import { Text, View } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 
+import { relativeMemberInfoSelectorForMembersOfThread } from 'lib/selectors/user-selectors';
+import { stringForUser } from 'lib/shared/user-utils';
 import type { ThreadInfo } from 'lib/types/thread-types';
+import { pluralizeAndTrim } from 'lib/utils/text-utils';
 
 import Button from '../components/button.react';
 import { MessageListRouteName } from '../navigation/route-names';
+import { useSelector } from '../redux/redux-utils';
 import { useStyles } from '../themes/colors';
 import type { ChatNavigationProp } from './chat.react';
 
@@ -50,11 +54,25 @@ function InlineSidebar(props: Props) {
   const repliesText = `${repliesCount} ${
     repliesCount > 1 ? 'replies' : 'reply'
   }`;
+
+  const threadMembers = useSelector(
+    relativeMemberInfoSelectorForMembersOfThread(threadInfo.id),
+  );
+  const sendersText = React.useMemo(() => {
+    const senders = threadMembers
+      .filter((member) => member.isSender)
+      .map(stringForUser);
+    return senders.length > 0 ? `${pluralizeAndTrim(senders, 25)} sent ` : '';
+  }, [threadMembers]);
+
   return (
     <View style={[styles.content, alignStyle]}>
       <Button style={styles.sidebar} onPress={onPress}>
         {nonViewerIcon}
-        <Text style={[styles.name, unreadStyle]}>{repliesText}</Text>
+        <Text style={[styles.name, unreadStyle]}>
+          {sendersText}
+          {repliesText}
+        </Text>
         {viewerIcon}
       </Button>
     </View>
