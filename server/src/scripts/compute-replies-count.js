e60b9f6590011858f668e6610a05962fd0e7647d
Tomasz Palys <tomasz.palys@swmansion.com>
2021-02-05 15:41:12 +0100

    [server] Generate updates in replies count migration script
    Test Plan: I set replies_count to 0 for all the threads, logged out and in on the device and noticed that 1 reply was displayed for every sidebar. Then I ran the script and the count was immediately increased to the correct value.
    
    Reviewers: ashoat
    
    Reviewed By: ashoat
    
    Subscribers: KatPo, zrebcu411, Adrian, atul, subnub, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D702

--- server/src/scripts/compute-replies-count.js
+++ server/src/scripts/compute-replies-count.js
@@ -3,7 +3,9 @@
 import { messageSpecs } from 'lib/shared/messages/message-specs';
 import { messageTypes } from 'lib/types/message-types';
 import { threadTypes } from 'lib/types/thread-types';
+import { updateTypes } from 'lib/types/update-types';
 
+import { createUpdates } from '../creators/update-creator';
 import { dbQuery, SQL } from '../database/database';
 import { main } from './utils';
 
@@ -12,7 +14,14 @@ async function computeRepliesCount() {
     .map((key) => messageTypes[key])
     .filter((type) => messageSpecs[type].includedInRepliesCount);
 
-  const query = SQL`
+  const sidebarMembersQuery = SQL`
+    SELECT t.id AS threadID, m.user AS userID
+    FROM threads t
+    INNER JOIN memberships m ON t.id = m.thread
+    WHERE t.type = ${threadTypes.SIDEBAR}
+      AND m.role >= 0
+  `;
+  const readCountUpdate = SQL`
     UPDATE threads t
     INNER JOIN (
       SELECT thread AS threadID, COUNT(*) AS count
@@ -23,7 +32,19 @@ async function computeRepliesCount() {
     SET t.replies_count = c.count 
     WHERE t.type = ${threadTypes.SIDEBAR}
   `;
-  await dbQuery(query);
+  const [[sidebarMembers]] = await Promise.all([
+    dbQuery(sidebarMembersQuery),
+    dbQuery(readCountUpdate),
+  ]);
+
+  const time = Date.now();
+  const updates = sidebarMembers.map(({ threadID, userID }) => ({
+    userID,
+    time,
+    threadID,
+    type: updateTypes.UPDATE_THREAD,
+  }));
+  await createUpdates(updates);
 }
 
 main([computeRepliesCount]);
