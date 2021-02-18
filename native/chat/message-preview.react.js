bd81a863ad9451d04f349545ea494839a692637f
Kyle Hoell <kylehoell@Kyles-Mac-mini.local>
2021-02-18 12:57:04 -0500

    [native] Use hook instead of connect function and HOC in MessagePreview
    Summary: Switched to hooks instead of the connect function the MessagePreview class.
    
    Test Plan: Made sure that message previews still are displayed properly.
    
    Reviewers: ashoat, palys-swm
    
    Reviewed By: ashoat
    
    Subscribers: KatPo, zrebcu411, Adrian, atul, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D742

--- native/chat/message-preview.react.js
+++ native/chat/message-preview.react.js
@@ -14,78 +14,62 @@ import {
   type RobotextMessageInfo,
 } from 'lib/types/message-types';
 import { type ThreadInfo } from 'lib/types/thread-types';
-import { connect } from 'lib/utils/redux-utils';
 
 import { SingleLine } from '../components/single-line.react';
 import { getDefaultTextMessageRules } from '../markdown/rules.react';
-import type { AppState } from '../redux/redux-setup';
-import { styleSelector } from '../themes/colors';
+import { useStyles } from '../themes/colors';
 
 type Props = {|
   +messageInfo: MessageInfo,
   +threadInfo: ThreadInfo,
-  // Redux state
-  +styles: typeof styles,
 |};
-class MessagePreview extends React.PureComponent<Props> {
-  render() {
-    const messageInfo: ComposableMessageInfo | RobotextMessageInfo =
-      this.props.messageInfo.type === messageTypes.SIDEBAR_SOURCE
-        ? this.props.messageInfo.sourceMessage
-        : this.props.messageInfo;
-    const unreadStyle = this.props.threadInfo.currentUser.unread
-      ? this.props.styles.unread
-      : null;
-    const messageTitle = getMessageTitle(
-      messageInfo,
-      this.props.threadInfo,
-      getDefaultTextMessageRules().simpleMarkdownRules,
-    );
-    if (messageInfo.type === messageTypes.TEXT) {
-      let usernameText = null;
-      if (
-        threadIsGroupChat(this.props.threadInfo) ||
-        this.props.threadInfo.name !== '' ||
-        messageInfo.creator.isViewer
-      ) {
-        const userString = stringForUser(messageInfo.creator);
-        const username = `${userString}: `;
-        usernameText = (
-          <Text style={[this.props.styles.username, unreadStyle]}>
-            {username}
-          </Text>
-        );
-      }
-      return (
-        <Text
-          style={[this.props.styles.lastMessage, unreadStyle]}
-          numberOfLines={1}
-        >
-          {usernameText}
-          {messageTitle}
-        </Text>
-      );
-    } else {
-      invariant(
-        messageInfo.type !== messageTypes.SIDEBAR_SOURCE,
-        'Sidebar source should not be handled here',
-      );
-      return (
-        <SingleLine
-          style={[
-            this.props.styles.lastMessage,
-            this.props.styles.preview,
-            unreadStyle,
-          ]}
-        >
-          {messageTitle}
-        </SingleLine>
+function MessagePreview(props: Props) {
+  const styles = useStyles(unboundStyles);
+  const messageInfo: ComposableMessageInfo | RobotextMessageInfo =
+    props.messageInfo.type === messageTypes.SIDEBAR_SOURCE
+      ? props.messageInfo.sourceMessage
+      : props.messageInfo;
+  const unreadStyle = props.threadInfo.currentUser.unread
+    ? styles.unread
+    : null;
+  const messageTitle = getMessageTitle(
+    messageInfo,
+    props.threadInfo,
+    getDefaultTextMessageRules().simpleMarkdownRules,
+  );
+  if (messageInfo.type === messageTypes.TEXT) {
+    let usernameText = null;
+    if (
+      threadIsGroupChat(props.threadInfo) ||
+      props.threadInfo.name !== '' ||
+      messageInfo.creator.isViewer
+    ) {
+      const userString = stringForUser(messageInfo.creator);
+      const username = `${userString}: `;
+      usernameText = (
+        <Text style={[styles.username, unreadStyle]}>{username}</Text>
       );
     }
+    return (
+      <Text style={[styles.lastMessage, unreadStyle]} numberOfLines={1}>
+        {usernameText}
+        {messageTitle}
+      </Text>
+    );
+  } else {
+    invariant(
+      messageInfo.type !== messageTypes.SIDEBAR_SOURCE,
+      'Sidebar source should not be handled here',
+    );
+    return (
+      <SingleLine style={[styles.lastMessage, styles.preview, unreadStyle]}>
+        {messageTitle}
+      </SingleLine>
+    );
   }
 }
 
-const styles = {
+const unboundStyles = {
   lastMessage: {
     color: 'listForegroundTertiaryLabel',
     flex: 1,
@@ -102,8 +86,5 @@ const styles = {
     color: 'listForegroundQuaternaryLabel',
   },
 };
-const stylesSelector = styleSelector(styles);
 
-export default connect((state: AppState) => ({
-  styles: stylesSelector(state),
-}))(MessagePreview);
+export default MessagePreview;
