1905d3cb8d7a12cf1251ba629d801bf658bc398b
Kyle Hoell <kylehoell@Kyles-Mac-mini.local>
2021-02-25 11:21:16 -0500

    [native] Use hook instead of connect functions and HOC in ConnectedStatusBar
    Summary: Switched to hooks instead of the connect function for the ConnectedStatusBar class
    
    Test Plan: Made sure the statusbar still showed up and functioned normally.
    
    Reviewers: ashoat, palys-swm
    
    Reviewed By: ashoat
    
    Subscribers: KatPo, zrebcu411, Adrian, atul, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D771

--- native/connected-status-bar.react.js
+++ native/connected-status-bar.react.js
@@ -1,60 +1,37 @@
 // @flow
 
-import PropTypes from 'prop-types';
 import React from 'react';
 import { StatusBar, Platform } from 'react-native';
 
 import { globalLoadingStatusSelector } from 'lib/selectors/loading-selectors';
-import type { LoadingStatus } from 'lib/types/loading-types';
-import { connect } from 'lib/utils/redux-utils';
 
-import type { AppState } from './redux/redux-setup';
-import { type GlobalTheme, globalThemePropType } from './types/themes';
+import { useSelector } from './redux/redux-utils';
 
 type Props = {|
-  barStyle?: 'default' | 'light-content' | 'dark-content',
-  animated?: boolean,
-  // Redux state
-  globalLoadingStatus: LoadingStatus,
-  activeTheme: ?GlobalTheme,
+  +barStyle?: 'default' | 'light-content' | 'dark-content',
+  +animated?: boolean,
+  +hidden?: boolean,
 |};
-class ConnectedStatusBar extends React.PureComponent<Props> {
-  static propTypes = {
-    barStyle: PropTypes.oneOf(['default', 'light-content', 'dark-content']),
-    animated: PropTypes.bool,
-    globalLoadingStatus: PropTypes.string.isRequired,
-    activeTheme: globalThemePropType,
-  };
+export default function ConnectedStatusBar(props: Props) {
+  const globalLoadingStatus = useSelector(globalLoadingStatusSelector);
+  const activeTheme = useSelector((state) => state.globalThemeInfo.activeTheme);
+  const { barStyle: inBarStyle, ...statusBarProps } = props;
 
-  render() {
-    const {
-      barStyle: inBarStyle,
-      activeTheme,
-      globalLoadingStatus,
-      ...statusBarProps
-    } = this.props;
-
-    let barStyle = inBarStyle;
-    if (!barStyle) {
-      if (Platform.OS !== 'android' && this.props.activeTheme === 'light') {
-        barStyle = 'dark-content';
-      } else {
-        barStyle = 'light-content';
-      }
+  let barStyle = inBarStyle;
+  if (!barStyle) {
+    if (Platform.OS !== 'android' && activeTheme === 'light') {
+      barStyle = 'dark-content';
+    } else {
+      barStyle = 'light-content';
     }
-
-    const fetchingSomething = this.props.globalLoadingStatus === 'loading';
-    return (
-      <StatusBar
-        {...statusBarProps}
-        barStyle={barStyle}
-        networkActivityIndicatorVisible={fetchingSomething}
-      />
-    );
   }
-}
 
-export default connect((state: AppState) => ({
-  globalLoadingStatus: globalLoadingStatusSelector(state),
-  activeTheme: state.globalThemeInfo.activeTheme,
-}))(ConnectedStatusBar);
+  const fetchingSomething = globalLoadingStatus === 'loading';
+  return (
+    <StatusBar
+      {...statusBarProps}
+      barStyle={barStyle}
+      networkActivityIndicatorVisible={fetchingSomething}
+    />
+  );
+}
