7e82fd73953180574519f00fff86b9924e596a49
Atul Madhugiri <atulm@berkeley.edu>
2021-04-08 14:19:06 -0400

    [landing] Recreate parallax star effect with `react-particles-js`
    Summary:
    Introduce `react-particle-js`: https://github.com/Wufe/react-particles-js
    
    Here's what it looks like: https://blob.sh/atul/parallax.mov
    
    Test Plan: NA
    
    Reviewers: ashoat
    
    Reviewed By: ashoat
    
    Subscribers: KatPo, zrebcu411, palys-swm, Adrian, subnub
    
    Differential Revision: https://phabricator.ashoat.com/D978

--- landing/landing.react.js
+++ landing/landing.react.js
@@ -1,10 +1,18 @@
 // @flow
 
 import * as React from 'react';
+import Particles from 'react-particles-js';
+
 import './landing.css';
+import particlesConfig from './particles-config.json';
 
 function Landing(): React.Node {
-  return <h1>Hello World!</h1>;
+  return (
+    <div>
+      <h1>Comm</h1>
+      <Particles height="100vh" width="100vw" params={particlesConfig} />
+    </div>
+  );
 }
 
 export default Landing;
