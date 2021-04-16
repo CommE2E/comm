5ed34fe28f04c511c7756797cfa95c45dfed0b1b
Atul Madhugiri <atulm@berkeley.edu>
2021-04-16 12:27:20 -0400

    [landing] Add copy to landing page
    Summary: NA
    
    Test Plan: NA
    
    Reviewers: ashoat
    
    Reviewed By: ashoat
    
    Subscribers: KatPo, palys-swm, Adrian
    
    Differential Revision: https://phabricator.ashoat.com/D1012

--- landing/landing.react.js
+++ landing/landing.react.js
@@ -3,14 +3,48 @@
 import * as React from 'react';
 import Particles from 'react-particles-js';
 
-import './landing.css';
+import css from './landing.css';
 import particlesConfig from './particles-config.json';
 
 function Landing(): React.Node {
   return (
     <div>
+      <Particles canvasClassName={css.particles} params={particlesConfig} />
       <h1>Comm</h1>
-      <Particles height="100vh" width="100vw" params={particlesConfig} />
+      <div>
+        <h1>Reclaim your digital identity.</h1>
+        <p>
+          The internet is broken today. Private user data is owned by
+          mega-corporations and farmed for their benefit.
+        </p>
+        <p>
+          E2E encryption has the potential to change this equation. But
+          it&apos;s constrained by a crucial limitation.
+        </p>
+      </div>
+
+      <div>
+        <h2>Apps need servers.</h2>
+        <p>
+          Sophisticated applications rely on servers to do things that your
+          devices simply can&apos;t.
+        </p>
+        <p>
+          That&apos;s why E2E encryption only works for simple chat apps today.
+          There&apos;s no way to build a robust server layer that has access to
+          your data without leaking that data to corporations.
+        </p>
+      </div>
+
+      <div>
+        <h2>Comm is the keyserver company.</h2>
+        <p>In the future, people have their own servers.</p>
+        <p>
+          Your keyserver is the home of your digital identity. It owns your
+          private keys and your personal data. It&apos;s your password manager,
+          your crypto wallet, your digital surrogate, and your second brain.
+        </p>
+      </div>
     </div>
   );
 }
