357eddbd489aafc2b120631edba3cc4dc9ccd1a8
Atul Madhugiri <atulm@berkeley.edu>
2021-04-29 11:36:21 -0400

    [landing] Call `subscribe_email` endpoint on form submission
    Summary: Hooked up the landing page email form to the `subscribe_email` endpoint we created on the backend.
    
    Test Plan: Was able to successfully hit endpoint and get responses back as expected. Was also, thanks to this WeWork Wifi, able to confirm that the emails look as expected.
    
    Reviewers: ashoat
    
    Reviewed By: ashoat
    
    Subscribers: KatPo, palys-swm, Adrian
    
    Differential Revision: https://phabricator.ashoat.com/D1075

--- landing/subscription-form.react.js
+++ landing/subscription-form.react.js
@@ -2,13 +2,71 @@
 
 import * as React from 'react';
 
+import { validEmailRegex } from 'lib/shared/account-utils';
+
 import css from './subscription-form.css';
 
 function SubscriptionForm(): React.Node {
+  const [email, setEmail] = React.useState('');
+  // eslint-disable-next-line
+  const [success, setSuccess] = React.useState(null);
+  // eslint-disable-next-line
+  const [errorMsg, setErrorMsg] = React.useState(null);
+
+  const onEmailSubmitted = React.useCallback(
+    async (e: Event) => {
+      e.preventDefault();
+      if (email.search(validEmailRegex) === -1) {
+        setSuccess(false);
+        setErrorMsg('Please enter a valid email address!');
+        return;
+      }
+
+      try {
+        const response = await fetch('subscribe_email', {
+          method: 'POST',
+          credentials: 'same-origin',
+          body: JSON.stringify({ email }),
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+          },
+        });
+
+        const respJson = await response.json();
+        setSuccess(respJson.success);
+
+        if (!respJson.success) {
+          setErrorMsg('Could not add to email list, please try again later!');
+        }
+      } catch {
+        setSuccess(false);
+        setErrorMsg('Network request failed, please try again later!');
+      }
+    },
+    [email],
+  );
+
+  React.useEffect(() => {
+    setSuccess(null);
+  }, [email]);
+
+  const onEmailChange = React.useCallback((e) => {
+    setEmail(e.target.value);
+  }, []);
+
   return (
     <form>
-      <input className={css.email_input} placeholder="Enter your email" />
-      <button className={css.button}>Subscribe for updates</button>
+      <input
+        type="text"
+        value={email}
+        onChange={onEmailChange}
+        className={css.email_input}
+        placeholder="Enter your email"
+      />
+      <button className={css.button} onClick={onEmailSubmitted}>
+        Subscribe for updates
+      </button>
     </form>
   );
 }
