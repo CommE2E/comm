c54f727b07137e30f4f19d6196de4dca9ac75d52
Ashoat Tevosyan <ashoat@comm.app>
2021-05-28 18:40:39 -0400

    [server] Update containing_thread_id to set values for new column
    Test Plan: Run the script
    
    Reviewers: atul
    
    Reviewed By: atul
    
    Subscribers: KatPo, palys-swm, Adrian
    
    Differential Revision: https://phabricator.ashoat.com/D1301

--- server/src/scripts/add-containing-thread-id-column.js
+++ server/src/scripts/add-containing-thread-id-column.js
@@ -1,6 +1,11 @@
 // @flow
 
+import invariant from 'invariant';
+
+import { threadTypes, type ServerThreadInfo } from 'lib/types/thread-types';
+
 import { dbQuery, SQL } from '../database/database';
+import { fetchServerThreadInfos } from '../fetchers/thread-fetchers';
 import { main } from './utils';
 
 async function addColumnAndIndexes() {
@@ -12,4 +17,70 @@ async function addColumnAndIndexes() {
   `);
 }
 
-main([addColumnAndIndexes]);
+async function setColumn() {
+  const stack = [[null, SQL`t.parent_thread_id IS NULL`]];
+
+  while (stack.length > 0) {
+    const [parentThreadInfo, predicate] = stack.shift();
+    const { threadInfos } = await fetchServerThreadInfos(predicate);
+    const updatedThreadInfos = await setColumnForLayer(
+      parentThreadInfo,
+      threadInfos,
+    );
+    for (const threadInfo of updatedThreadInfos) {
+      stack.push([threadInfo, SQL`t.parent_thread_id = ${threadInfo.id}`]);
+    }
+  }
+}
+
+async function setColumnForLayer(
+  parentThreadInfo: ?ServerThreadInfo,
+  threadInfos: { [id: string]: ServerThreadInfo },
+): Promise<ServerThreadInfo[]> {
+  const updatedThreadInfos = [];
+  for (const threadID in threadInfos) {
+    const threadInfo = threadInfos[threadID];
+    const containingThreadID = getContainingThread(
+      threadInfo,
+      parentThreadInfo,
+    );
+    if (!containingThreadID) {
+      console.log(`containingThreadID is null for ${threadID}, skipping...`);
+      updatedThreadInfos.push(threadInfo);
+      continue;
+    }
+    console.log(
+      `setting containingThreadID to ${containingThreadID} for ${threadID}`,
+    );
+    await dbQuery(SQL`
+      UPDATE threads
+      SET containing_thread_id = ${containingThreadID}
+      WHERE id = ${threadID}
+    `);
+    updatedThreadInfos.push({
+      ...threadInfo,
+      containingThreadID,
+    });
+  }
+  return updatedThreadInfos;
+}
+
+function getContainingThread(
+  threadInfo: ServerThreadInfo,
+  parentThreadInfo: ?ServerThreadInfo,
+) {
+  const { type, parentThreadID } = threadInfo;
+  if (!parentThreadID) {
+    return null;
+  }
+  if (type === threadTypes.SIDEBAR) {
+    return parentThreadID;
+  }
+  invariant(parentThreadInfo, 'parentThreadInfo should be set');
+  if (!parentThreadInfo.containingThreadID) {
+    return parentThreadID;
+  }
+  return parentThreadInfo.containingThreadID;
+}
+
+main([addColumnAndIndexes, setColumn]);
