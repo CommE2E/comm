bd0686a9c0bf7892d47dee5f8d61294997160a2e
Atul Madhugiri <atulm@berkeley.edu>
2021-06-03 19:07:19 -0400

    [native] Extract `thread-ancestors` component out of `thread-settings-ancestors`
    Summary: Simple cut/paste refactor, sets things up for subsequent diffs.
    
    Test Plan: `thread-ancestors` continued to look and work as expected. should be simple cut/paste diff
    
    Reviewers: ashoat
    
    Reviewed By: ashoat
    
    Subscribers: KatPo, palys-swm, Adrian
    
    Differential Revision: https://phabricator.ashoat.com/D1343

--- native/chat/settings/thread-settings-ancestors.react.js
+++ native/chat/settings/thread-settings-ancestors.react.js
@@ -1,13 +1,11 @@
 // @flow
 
 import * as React from 'react';
-import { Text, View, ScrollView } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome5';
+import { Text, View } from 'react-native';
 
-import { memberHasAdminPowers } from 'lib/shared/thread-utils';
-import { type ThreadInfo, type MemberInfo } from 'lib/types/thread-types';
+import { type ThreadInfo } from 'lib/types/thread-types';
 
-import { useSelector } from '../../redux/redux-utils';
+import ThreadAncestors from '../../components/thread-ancestors.react';
 import { useStyles } from '../../themes/colors';
 
 type Props = {|
@@ -16,77 +14,16 @@ type Props = {|
 function ThreadSettingsAncestors(props: Props) {
   const { ancestorThreads } = props;
   const styles = useStyles(unboundStyles);
-  const userInfos = useSelector((state) => state.userStore.userInfos);
-
-  const parentAdmin: ?string = React.useMemo(() => {
-    for (const member: MemberInfo of ancestorThreads[0].members) {
-      if (memberHasAdminPowers(member)) {
-        return userInfos[member.id].username;
-      }
-    }
-  }, [ancestorThreads, userInfos]);
-
-  const pathElements = [];
-  for (const [idx, threadInfo] of ancestorThreads.entries()) {
-    if (idx === ancestorThreads.length - 1) {
-      pathElements.push(
-        <View key={threadInfo.id} style={styles.pathItem}>
-          <Text style={styles.pathLabel}>{threadInfo.uiName}</Text>
-        </View>,
-      );
-    } else {
-      pathElements.push(
-        <View key={threadInfo.id} style={styles.pathItem}>
-          <Text style={styles.pathLabel}>{threadInfo.uiName}</Text>
-          <Icon
-            name="chevron-right"
-            size={12}
-            color="white"
-            style={styles.arrowIcon}
-          />
-        </View>,
-      );
-    }
-  }
-
-  let adminLabel;
-  if (parentAdmin) {
-    adminLabel = (
-      <View style={styles.adminBadge}>
-        <Icon name="cloud" size={12} color="white" style={styles.cloudIcon} />
-        <Text style={styles.adminName}>{parentAdmin}</Text>
-      </View>
-    );
-  }
 
   return (
     <View style={styles.cell}>
       <Text style={styles.label}>Path</Text>
-      <ScrollView horizontal={true}>
-        <View style={styles.pathItem}>
-          {adminLabel}
-          {pathElements}
-        </View>
-      </ScrollView>
+      <ThreadAncestors ancestorThreads={ancestorThreads} />
     </View>
   );
 }
 
 const unboundStyles = {
-  adminBadge: {
-    fontSize: 16,
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: 'codeBackground',
-    paddingHorizontal: 8,
-    paddingVertical: 4,
-    borderRadius: 8,
-  },
-  adminName: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    color: 'panelForegroundLabel',
-  },
   cell: {
     flexDirection: 'row',
     alignItems: 'center',
@@ -97,23 +34,6 @@ const unboundStyles = {
     fontSize: 16,
     width: 96,
     color: 'panelForegroundTertiaryLabel',
-  },
-  pathItem: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    height: 40,
-  },
-  pathLabel: {
-    fontSize: 16,
-    paddingLeft: 8,
-    fontWeight: 'bold',
-    color: 'panelForegroundSecondaryLabel',
-  },
-  cloudIcon: {
-    paddingRight: 8,
-  },
-  arrowIcon: {
-    paddingLeft: 8,
   },
 };
 
