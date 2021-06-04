bf662af826c7e0abbc08b06dcd9e71cb8a725d02
Atul Madhugiri <atulm@berkeley.edu>
2021-06-04 16:45:02 -0400

    [native] Use `pill` component in `thread-ancestors`
    Test Plan: Continues to look/work as expected
    
    Reviewers: ashoat
    
    Reviewed By: ashoat
    
    Subscribers: KatPo, palys-swm, Adrian
    
    Differential Revision: https://phabricator.ashoat.com/D1357

--- native/components/thread-ancestors.react.js
+++ native/components/thread-ancestors.react.js
@@ -1,22 +1,24 @@
 // @flow
 
 import * as React from 'react';
-import { Text, View, ScrollView } from 'react-native';
+import { View, ScrollView } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 
 import { memberHasAdminPowers } from 'lib/shared/thread-utils';
 import { type ThreadInfo, type MemberInfo } from 'lib/types/thread-types';
 
 import { useSelector } from '../redux/redux-utils';
-import { useStyles } from '../themes/colors';
+import { useStyles, useColors } from '../themes/colors';
+import Pill from './pill.react';
 
 type Props = {|
   +ancestorThreads: $ReadOnlyArray<ThreadInfo>,
 |};
 
-function ThreadAncestors(props: Props) {
+function ThreadAncestors(props: Props): React.Node {
   const { ancestorThreads } = props;
   const styles = useStyles(unboundStyles);
+  const colors = useColors();
   const userInfos = useSelector((state) => state.userStore.userInfos);
 
   const parentAdmin: ?string = React.useMemo(() => {
@@ -27,80 +29,64 @@ function ThreadAncestors(props: Props) {
     }
   }, [ancestorThreads, userInfos]);
 
+  const adminLabel: ?React.Node = React.useMemo(() => {
+    if (!parentAdmin) {
+      return undefined;
+    }
+    return (
+      <Pill
+        backgroundColor={colors.codeBackground}
+        roundCorners={{ left: true, right: false }}
+        label={parentAdmin}
+        faIcon="cloud"
+      />
+    );
+  }, [colors.codeBackground, parentAdmin]);
+
+  const rightArrow: React.Node = React.useMemo(
+    () => (
+      <Icon
+        name="chevron-right"
+        size={12}
+        color="white"
+        style={styles.arrowIcon}
+      />
+    ),
+    [styles.arrowIcon],
+  );
+
   const pathElements = [];
   for (const [idx, threadInfo] of ancestorThreads.entries()) {
-    if (idx === ancestorThreads.length - 1) {
-      pathElements.push(
-        <View key={threadInfo.id} style={styles.pathItem}>
-          <Text style={styles.pathLabel}>{threadInfo.uiName}</Text>
-        </View>,
-      );
-    } else {
-      pathElements.push(
-        <View key={threadInfo.id} style={styles.pathItem}>
-          <Text style={styles.pathLabel}>{threadInfo.uiName}</Text>
-          <Icon
-            name="chevron-right"
-            size={12}
-            color="white"
-            style={styles.arrowIcon}
-          />
-        </View>,
-      );
-    }
-  }
+    const isLastThread = idx === ancestorThreads.length - 1;
+    const backgroundColor = `#${threadInfo.color}`;
 
-  let adminLabel;
-  if (parentAdmin) {
-    adminLabel = (
-      <View style={styles.adminBadge}>
-        <Icon name="cloud" size={12} color="white" style={styles.cloudIcon} />
-        <Text style={styles.adminName}>{parentAdmin}</Text>
-      </View>
+    pathElements.push(
+      <View key={threadInfo.id} style={styles.pathItem}>
+        {idx === 0 ? adminLabel : null}
+        <Pill
+          backgroundColor={backgroundColor}
+          roundCorners={{ left: !(idx === 0), right: true }}
+          label={threadInfo.uiName}
+        />
+        {!isLastThread ? rightArrow : null}
+      </View>,
     );
   }
-
   return (
     <ScrollView horizontal={true}>
-      <View style={styles.pathItem}>
-        {adminLabel}
-        {pathElements}
-      </View>
+      <View style={styles.pathItem}>{pathElements}</View>
     </ScrollView>
   );
 }
 
 const unboundStyles = {
-  adminBadge: {
-    fontSize: 16,
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: 'codeBackground',
-    paddingHorizontal: 8,
-    paddingVertical: 4,
-    borderRadius: 8,
-  },
-  adminName: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    color: 'panelForegroundLabel',
-  },
   pathItem: {
     flexDirection: 'row',
     alignItems: 'center',
     height: 40,
   },
-  pathLabel: {
-    fontSize: 16,
-    paddingLeft: 8,
-    fontWeight: 'bold',
-    color: 'panelForegroundSecondaryLabel',
-  },
-  cloudIcon: {
-    paddingRight: 8,
-  },
   arrowIcon: {
-    paddingLeft: 8,
+    paddingHorizontal: 8,
   },
 };
 
