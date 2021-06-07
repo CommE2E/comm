4d1a496f847b1e65ed27854938bb2c25048a5410
Atul Madhugiri <atulm@berkeley.edu>
2021-06-07 13:45:12 -0400

    [native] Navigate to ancestor thread on tap in `thread-ancestors` component
    Test Plan: Works and looks as expected
    
    Reviewers: ashoat
    
    Reviewed By: ashoat
    
    Subscribers: KatPo, palys-swm, Adrian
    
    Differential Revision: https://phabricator.ashoat.com/D1358

--- native/components/thread-ancestors.react.js
+++ native/components/thread-ancestors.react.js
@@ -1,14 +1,17 @@
 // @flow
 
+import { useNavigation } from '@react-navigation/native';
 import * as React from 'react';
-import { View, ScrollView } from 'react-native';
+import { View, ScrollView, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 
 import { memberHasAdminPowers } from 'lib/shared/thread-utils';
 import { type ThreadInfo, type MemberInfo } from 'lib/types/thread-types';
 
+import { MessageListRouteName } from '../navigation/route-names';
 import { useSelector } from '../redux/redux-utils';
-import { useStyles, useColors } from '../themes/colors';
+import { useColors } from '../themes/colors';
+import Button from './button.react';
 import Pill from './pill.react';
 
 type Props = {|
@@ -17,9 +20,20 @@ type Props = {|
 
 function ThreadAncestors(props: Props): React.Node {
   const { ancestorThreads } = props;
-  const styles = useStyles(unboundStyles);
+  const navigation = useNavigation();
   const colors = useColors();
   const userInfos = useSelector((state) => state.userStore.userInfos);
+
+  const navigateToThread = React.useCallback(
+    (threadInfo) => {
+      navigation.navigate({
+        name: MessageListRouteName,
+        params: { threadInfo },
+        key: `${MessageListRouteName}${threadInfo.id}`,
+      });
+    },
+    [navigation],
+  );
 
   const parentAdmin: ?string = React.useMemo(() => {
     for (const member: MemberInfo of ancestorThreads[0].members) {
@@ -43,51 +57,51 @@ function ThreadAncestors(props: Props): React.Node {
     );
   }, [colors.codeBackground, parentAdmin]);
 
-  const rightArrow: React.Node = React.useMemo(
-    () => (
-      <Icon
-        name="chevron-right"
-        size={12}
-        color="white"
-        style={styles.arrowIcon}
-      />
-    ),
-    [styles.arrowIcon],
-  );
+  const pathElements = React.useMemo(() => {
+    const elements = [];
+    for (const [idx, threadInfo] of ancestorThreads.entries()) {
+      const isLastThread = idx === ancestorThreads.length - 1;
+      const backgroundColor = `#${threadInfo.color}`;
 
-  const pathElements = [];
-  for (const [idx, threadInfo] of ancestorThreads.entries()) {
-    const isLastThread = idx === ancestorThreads.length - 1;
-    const backgroundColor = `#${threadInfo.color}`;
+      elements.push(
+        <View key={threadInfo.id} style={styles.pathItem}>
+          <Button
+            style={styles.row}
+            onPress={() => navigateToThread(threadInfo)}
+          >
+            {idx === 0 ? adminLabel : null}
+            <Pill
+              backgroundColor={backgroundColor}
+              roundCorners={{ left: !(idx === 0), right: true }}
+              label={threadInfo.uiName}
+            />
+          </Button>
+          {!isLastThread ? rightArrow : null}
+        </View>,
+      );
+    }
+    return <View style={styles.pathItem}>{elements}</View>;
+  }, [adminLabel, ancestorThreads, navigateToThread]);
 
-    pathElements.push(
-      <View key={threadInfo.id} style={styles.pathItem}>
-        {idx === 0 ? adminLabel : null}
-        <Pill
-          backgroundColor={backgroundColor}
-          roundCorners={{ left: !(idx === 0), right: true }}
-          label={threadInfo.uiName}
-        />
-        {!isLastThread ? rightArrow : null}
-      </View>,
-    );
-  }
-  return (
-    <ScrollView horizontal={true}>
-      <View style={styles.pathItem}>{pathElements}</View>
-    </ScrollView>
-  );
+  return <ScrollView horizontal={true}>{pathElements}</ScrollView>;
 }
 
-const unboundStyles = {
-  pathItem: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    height: 40,
-  },
+const styles = StyleSheet.create({
   arrowIcon: {
     paddingHorizontal: 8,
   },
-};
+  pathItem: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    height: 40,
+  },
+  row: {
+    flexDirection: 'row',
+  },
+});
+
+const rightArrow: React.Node = (
+  <Icon name="chevron-right" size={12} color="white" style={styles.arrowIcon} />
+);
 
 export default ThreadAncestors;
