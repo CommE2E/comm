8da14570de325463fa9c2625cd17cf801882ad52
Atul Madhugiri <atulm@berkeley.edu>
2021-06-09 16:39:11 -0400

    [native] Introduce `ThreadAncestorsLabel` component
    Summary: Introduce `ThreadAncestorsLabel` which displays a non-interactive ancestor path that may be shortened with ellipsis if full path exceeds the given space.
    
    Test Plan: Looks as expected when added to `ChatThreadListItem` (next diff)
    
    Reviewers: ashoat
    
    Reviewed By: ashoat
    
    Subscribers: KatPo, palys-swm, Adrian
    
    Differential Revision: https://phabricator.ashoat.com/D1392

---
+++ native/components/thread-ancestors-label.react.js
@@ -0,0 +1,40 @@
+// @flow
+
+import * as React from 'react';
+
+import { ancestorThreadInfos } from 'lib/selectors/thread-selectors';
+import { type ThreadInfo } from 'lib/types/thread-types';
+
+import { useSelector } from '../redux/redux-utils';
+import { useStyles } from '../themes/colors';
+import { SingleLine } from './single-line.react';
+
+type Props = {|
+  +threadInfo: ThreadInfo,
+|};
+function ThreadAncestorsLabel(props: Props): React.Node {
+  const { threadInfo } = props;
+  const styles = useStyles(unboundStyles);
+  const ancestorThreads: $ReadOnlyArray<ThreadInfo> = useSelector(
+    ancestorThreadInfos(threadInfo.id),
+  );
+
+  const ancestorPath: string = React.useMemo(() => {
+    const path = ancestorThreads.map((each) => each.uiName);
+    return path.slice(0, -1).join(' > ').concat(' > ');
+  }, [ancestorThreads]);
+
+  return <SingleLine style={styles.pathText}>{ancestorPath}</SingleLine>;
+}
+
+const unboundStyles = {
+  pathText: {
+    opacity: 0.8,
+    fontSize: 12,
+    paddingLeft: 10,
+    paddingVertical: 2,
+    color: 'listForegroundTertiaryLabel',
+  },
+};
+
+export default ThreadAncestorsLabel;
