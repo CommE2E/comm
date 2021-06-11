b5d54e09f81f55bd952c879e12b4c6690fa588fe
Tomasz Palys <tomasz.palys@swmansion.com>
2021-06-11 12:21:23 +0200

    [native] Create chat context
    Summary: Created an empty context which currently doesn't do anything useful
    
    Test Plan: Tested with other diffs
    
    Reviewers: ashoat
    
    Reviewed By: ashoat
    
    Subscribers: KatPo, Adrian, atul
    
    Differential Revision: https://phabricator.ashoat.com/D1401

---
+++ native/chat/chat-context-provider.react.js
@@ -0,0 +1,48 @@
+// @flow
+
+import * as React from 'react';
+
+import type { ChatMessageItem } from 'lib/selectors/chat-selectors';
+import type { ThreadInfo } from 'lib/types/thread-types';
+
+import { ChatContext } from './chat-context';
+import type { ChatMessageItemWithHeight } from './message-list-container.react';
+
+type BaseProps = {|
+  +children: React.Node,
+|};
+type Props = {|
+  ...BaseProps,
+|};
+
+class ChatContextProvider extends React.PureComponent<Props> {
+  registerMeasurer = () => ({
+    measure: (
+      // eslint-disable-next-line no-unused-vars
+      messages: $ReadOnlyArray<ChatMessageItem>,
+      // eslint-disable-next-line no-unused-vars
+      threadInfo: ThreadInfo,
+      // eslint-disable-next-line no-unused-vars
+      onMessagesMeasured: ($ReadOnlyArray<ChatMessageItemWithHeight>) => mixed,
+    ) => {},
+    unregister: () => {},
+  });
+
+  contextValue = {
+    registerMeasurer: this.registerMeasurer,
+  };
+
+  render() {
+    return (
+      <ChatContext.Provider value={this.contextValue}>
+        {this.props.children}
+      </ChatContext.Provider>
+    );
+  }
+}
+
+export default React.memo<BaseProps>(function ConnectedChatContextProvider(
+  props: BaseProps,
+) {
+  return <ChatContextProvider {...props} />;
+});
