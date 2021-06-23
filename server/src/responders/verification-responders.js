58186b5970d1a8c7039d93f69c93fe0198db18e8
Ashoat Tevosyan <ashoat@comm.app>
2021-06-23 19:38:05 -0400

    [server] Deprecate codeVerificationResponder
    Summary: (This endpoint would get triggered when you press a verification link in an email.)
    
    Test Plan: Flow
    
    Reviewers: atul, palys-swm
    
    Reviewed By: palys-swm
    
    Subscribers: KatPo, Adrian
    
    Differential Revision: https://phabricator.ashoat.com/D1477

--- server/src/responders/verification-responders.js
+++ server/src/responders/verification-responders.js
@@ -2,14 +2,9 @@
 
 import t from 'tcomb';
 
-import type {
-  CodeVerificationRequest,
-  HandleVerificationCodeResult,
-} from 'lib/types/verify-types';
-import { verifyField } from 'lib/types/verify-types';
+import type { HandleVerificationCodeResult } from 'lib/types/verify-types';
 import { ServerError } from 'lib/utils/errors';
 
-import { handleCodeVerificationRequest } from '../models/verification';
 import type { Viewer } from '../session/viewer';
 import { validateInput, tShape } from '../utils/validation-utils';
 
@@ -17,23 +12,15 @@ const codeVerificationRequestInputValidator = tShape({
   code: t.String,
 });
 
+/* eslint-disable no-unused-vars */
 async function codeVerificationResponder(
   viewer: Viewer,
   input: any,
 ): Promise<HandleVerificationCodeResult> {
-  const request: CodeVerificationRequest = input;
-  await validateInput(viewer, codeVerificationRequestInputValidator, request);
-
-  const result = await handleCodeVerificationRequest(viewer, request.code);
-  if (result.field === verifyField.EMAIL) {
-    return { verifyField: result.field };
-  } else if (result.field === verifyField.RESET_PASSWORD) {
-    return {
-      verifyField: result.field,
-      resetPasswordUsername: result.username,
-    };
-  }
-  throw new ServerError('invalid_code');
+  /* eslint-enable no-unused-vars */
+  await validateInput(viewer, codeVerificationRequestInputValidator, input);
+  // We have no way to handle this request anymore
+  throw new ServerError('deprecated');
 }
 
 export { codeVerificationResponder };
