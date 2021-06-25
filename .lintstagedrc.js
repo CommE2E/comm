67c0950df61537151d838036b43ec65f003f060c
Atul Madhugiri <atulm@berkeley.edu>
2021-06-25 12:21:37 -0400

    [eslint] Remove `*.cjs` from `lintstaged`
    Summary: na
    
    Test Plan: na
    
    Reviewers: ashoat
    
    Reviewed By: ashoat
    
    Subscribers: KatPo, palys-swm, Adrian
    
    Differential Revision: https://phabricator.ashoat.com/D1501

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,7 +3,7 @@ const { CLIEngine } = require('eslint');
 const cli = new CLIEngine({});
 
 module.exports = {
-  '*.{js,mjs,cjs}': function eslint(files) {
+  '*.{js,mjs}': function eslint(files) {
     return (
       'eslint --cache --fix --max-warnings=0 ' +
       files.filter((file) => !cli.isPathIgnored(file)).join(' ')
