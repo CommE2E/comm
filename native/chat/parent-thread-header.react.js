5452472eb96eb54f76f7a73b0c179aea767aade7
Ashoat Tevosyan <ashoat@comm.app>
2021-06-28 17:10:50 -0400

    [native] Make ParentThreadHeader horizontally scrollable
    Test Plan: Play around with it
    
    Reviewers: atul, palys-swm
    
    Reviewed By: atul, palys-swm
    
    Subscribers: KatPo, Adrian
    
    Differential Revision: https://phabricator.ashoat.com/D1528

--- native/chat/parent-thread-header.react.js
+++ native/chat/parent-thread-header.react.js
@@ -1,7 +1,7 @@
 // @flow
 
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 
 import type { ThreadInfo, ThreadType } from 'lib/types/thread-types';
 
@@ -28,36 +28,40 @@ function ParentThreadHeader(props: Props): React.Node {
   }, [parentThreadInfo, navigateToThread]);
 
   return (
-    <View style={styles.parentThreadRow}>
-      <ThreadVisibility
-        threadType={childThreadType}
-        color={threadVisibilityColor}
-      />
-      <Text style={styles.parentThreadLabel}>within</Text>
-      <Button onPress={onPressParentThread}>
-        <CommunityPill community={parentThreadInfo} />
-      </Button>
+    <View style={styles.container}>
+      <ScrollView
+        contentContainerStyle={styles.contentContainer}
+        showsHorizontalScrollIndicator={false}
+        horizontal={true}
+      >
+        <ThreadVisibility
+          threadType={childThreadType}
+          color={threadVisibilityColor}
+        />
+        <Text style={styles.within}>within</Text>
+        <Button onPress={onPressParentThread}>
+          <CommunityPill community={parentThreadInfo} />
+        </Button>
+      </ScrollView>
     </View>
   );
 }
 
+const height = 48;
 const unboundStyles = {
-  parentThreadRow: {
-    alignItems: 'center',
+  container: {
+    height,
     backgroundColor: 'modalSubtext',
-    flexDirection: 'row',
-    paddingLeft: 12,
-    paddingVertical: 6,
   },
-  parentThreadLabel: {
+  contentContainer: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    paddingHorizontal: 12,
+  },
+  within: {
     color: 'modalSubtextLabel',
     fontSize: 16,
     paddingHorizontal: 6,
-  },
-  parentThreadName: {
-    color: 'modalForegroundLabel',
-    fontSize: 16,
-    paddingLeft: 6,
   },
 };
 
