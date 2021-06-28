338dab186a8460aae4fb7c96445e96fd417dae71
Ashoat Tevosyan <ashoat@comm.app>
2021-06-28 16:49:36 -0400

    [native] Make ParentThreadHeader pressable
    Test Plan: Press it
    
    Reviewers: atul, palys-swm
    
    Reviewed By: atul, palys-swm
    
    Subscribers: KatPo, Adrian
    
    Differential Revision: https://phabricator.ashoat.com/D1527

--- native/chat/parent-thread-header.react.js
+++ native/chat/parent-thread-header.react.js
@@ -5,9 +5,11 @@ import { View, Text } from 'react-native';
 
 import type { ThreadInfo, ThreadType } from 'lib/types/thread-types';
 
+import Button from '../components/button.react';
 import CommunityPill from '../components/community-pill.react';
 import ThreadVisibility from '../components/thread-visibility.react';
 import { useColors, useStyles } from '../themes/colors';
+import { useNavigateToThread } from './message-list-types';
 
 type Props = {|
   +parentThreadInfo: ThreadInfo,
@@ -16,10 +18,15 @@ type Props = {|
 function ParentThreadHeader(props: Props): React.Node {
   const colors = useColors();
   const threadVisibilityColor = colors.modalForegroundLabel;
-
   const styles = useStyles(unboundStyles);
 
   const { parentThreadInfo, childThreadType } = props;
+
+  const navigateToThread = useNavigateToThread();
+  const onPressParentThread = React.useCallback(() => {
+    navigateToThread({ threadInfo: parentThreadInfo });
+  }, [parentThreadInfo, navigateToThread]);
+
   return (
     <View style={styles.parentThreadRow}>
       <ThreadVisibility
@@ -27,7 +34,9 @@ function ParentThreadHeader(props: Props): React.Node {
         color={threadVisibilityColor}
       />
       <Text style={styles.parentThreadLabel}>within</Text>
-      <CommunityPill community={parentThreadInfo} />
+      <Button onPress={onPressParentThread}>
+        <CommunityPill community={parentThreadInfo} />
+      </Button>
     </View>
   );
 }
