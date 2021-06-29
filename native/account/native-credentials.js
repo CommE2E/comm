d5cf12f165de0ad192a6bf6b941d5a36293f628d
Ashoat Tevosyan <ashoat@comm.app>
2021-06-29 18:57:31 -0400

    [native] Get rid of Shared Web Credentials on iOS
    Test Plan: Flow
    
    Reviewers: atul
    
    Reviewed By: atul
    
    Subscribers: KatPo, palys-swm, Adrian
    
    Differential Revision: https://phabricator.ashoat.com/D1535

--- native/account/native-credentials.js
+++ native/account/native-credentials.js
@@ -3,9 +3,7 @@
 import { Platform } from 'react-native';
 import {
   getInternetCredentials,
-  requestSharedWebCredentials,
   setInternetCredentials,
-  setSharedWebCredentials,
   resetInternetCredentials,
 } from 'react-native-keychain';
 
@@ -22,7 +20,7 @@ let storedNativeKeychainCredentials: StoredCredentials = {
   state: 'undetermined',
   credentials: null,
 };
-let storedSharedWebCredentials: StoredCredentials = {
+const storedSharedWebCredentials: StoredCredentials = {
   state: Platform.OS === 'ios' ? 'undetermined' : 'unsupported',
   credentials: null,
 };
@@ -55,33 +53,12 @@ function getNativeSharedWebCredentials(): ?UserCredentials {
   return storedSharedWebCredentials.credentials;
 }
 
-async function fetchNativeSharedWebCredentials(): Promise<?UserCredentials> {
-  if (Platform.OS !== 'ios') {
-    return null;
-  }
-  if (storedSharedWebCredentials.state === 'determined') {
-    return storedSharedWebCredentials.credentials;
-  }
-  try {
-    const result = await requestSharedWebCredentials();
-    const credentials = result
-      ? { username: result.username, password: result.password }
-      : undefined;
-    storedSharedWebCredentials = { state: 'determined', credentials };
-    return credentials;
-  } catch (e) {
-    const credentials = null;
-    storedSharedWebCredentials = { state: 'unsupported', credentials };
-    return credentials;
-  }
-}
-
 async function fetchNativeCredentials(): Promise<?UserCredentials> {
   const keychainCredentials = await fetchNativeKeychainCredentials();
   if (keychainCredentials) {
     return keychainCredentials;
   }
-  return await fetchNativeSharedWebCredentials();
+  return null;
 }
 
 async function setNativeKeychainCredentials(credentials: UserCredentials) {
@@ -108,54 +85,8 @@ async function setNativeKeychainCredentials(credentials: UserCredentials) {
   }
 }
 
-async function setNativeSharedWebCredentials(credentials: UserCredentials) {
-  if (Platform.OS !== 'ios') {
-    return;
-  }
-  const currentKeychainCredentials = await fetchNativeKeychainCredentials();
-  // If the password entered is the same as what we've got in the native
-  // keychain, then assume that nothing has been changed. We exit early here
-  // because if there are already shared web credentials, the
-  // setSharedWebCredentials call below will pop up an alert regardless of
-  // whether the credentials we pass it are the same as what it already has.
-  if (
-    currentKeychainCredentials &&
-    credentials.username === currentKeychainCredentials.username &&
-    credentials.password === currentKeychainCredentials.password
-  ) {
-    return;
-  }
-  // You might think we should just check fetchNativeSharedWebCredentials to
-  // see if the new shared web credentials we are about to pass to
-  // setSharedWebCredentials are the same as what it already has. But it turns
-  // out that that will trigger an alert too, which isn't worth it because we're
-  // only checking it to prevent an unnecessary alert. The smart thing we can do
-  // is check our internal cache.
-  const cachedSharedWebCredentials = getNativeSharedWebCredentials();
-  if (
-    cachedSharedWebCredentials &&
-    credentials.username === cachedSharedWebCredentials.username &&
-    credentials.password === cachedSharedWebCredentials.password
-  ) {
-    return;
-  }
-  try {
-    await setSharedWebCredentials(
-      'squadcal.org',
-      credentials.username,
-      credentials.password,
-    );
-    storedSharedWebCredentials = { state: 'determined', credentials };
-  } catch (e) {
-    storedSharedWebCredentials = { state: 'unsupported', credentials: null };
-  }
-}
-
 function setNativeCredentials(credentials: UserCredentials) {
-  return Promise.all([
-    setNativeKeychainCredentials(credentials),
-    setNativeSharedWebCredentials(credentials),
-  ]);
+  return setNativeKeychainCredentials(credentials);
 }
 
 async function deleteNativeKeychainCredentials() {
@@ -173,31 +104,9 @@ async function deleteNativeKeychainCredentials() {
   }
 }
 
-async function deleteNativeSharedWebCredentialsFor(username: string) {
-  if (Platform.OS !== 'ios') {
-    return;
-  }
-  try {
-    // This native call will display a modal iff credentials are non-null,
-    // so we don't need to worry about checking our current state
-    await setSharedWebCredentials('squadcal.org', username, undefined);
-    storedSharedWebCredentials = {
-      state: 'determined',
-      credentials: undefined,
-    };
-  } catch (e) {
-    storedSharedWebCredentials = {
-      state: 'unsupported',
-      credentials: null,
-    };
-  }
-}
-
-async function deleteNativeCredentialsFor(username: string) {
-  await Promise.all([
-    deleteNativeKeychainCredentials(),
-    deleteNativeSharedWebCredentialsFor(username),
-  ]);
+// eslint-disable-next-line no-unused-vars
+function deleteNativeCredentialsFor(username: string) {
+  return deleteNativeKeychainCredentials();
 }
 
 export {
