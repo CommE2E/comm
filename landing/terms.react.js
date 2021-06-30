5d0ca4fa25894abea680f7c3a4c10e4422fa0337
Atul Madhugiri <atulm@berkeley.edu>
2021-06-30 09:16:21 -0400

    [landing] Add Terms of Service copy
    Test Plan: Skimmed through alongside the PDF provided by the lawyers and made sure the formatting matched up
    
    Reviewers: ashoat
    
    Reviewed By: ashoat
    
    Subscribers: KatPo, palys-swm, Adrian
    
    Differential Revision: https://phabricator.ashoat.com/D1544

--- landing/terms.react.js
+++ landing/terms.react.js
@@ -3,7 +3,274 @@
 import * as React from 'react';
 
 function Terms(): React.Node {
-  return <h1>Terms of Service</h1>;
+  return (
+    <div>
+      <h1>Terms of Use</h1>
+      <p>
+        Effective date: <strong>June 29, 2021</strong>
+      </p>
+
+      <p>
+        These Terms of Use (the “Terms”) are a binding contract between you and{' '}
+        <strong>COMM TECHNOLOGIES, INC.</strong> (“Comm Technologies,” “we” and
+        “us”) and govern your use of our website, products, services and
+        applications (the “Services”). Your use of the Services in any way means
+        that you agree to all of these Terms, and these Terms will remain in
+        effect while you use the Services. These Terms include the provisions in
+        this document as well as those in the{' '}
+        <a href="https://comm.app/privacy">Privacy Policy</a>.
+      </p>
+
+      <h2>Will these Terms ever change?</h2>
+
+      <p>
+        We may change the Terms at any time, but if we do, we will place a
+        notice on our site located at{' '}
+        <a href="https://comm.app">https://comm.app</a>. If you don’t agree with
+        the new Terms, you are free to reject them; unfortunately, that means
+        you will no longer be able to use the Services. If you use the Services
+        in any way after a change to the Terms is effective, that means you
+        agree to all of the changes.
+      </p>
+
+      <h2>What about my privacy?</h2>
+
+      <p>
+        For the current Comm Technologies Privacy Policy, please{' '}
+        <a href="https://comm.app/privacy">click here</a>.
+      </p>
+
+      <h2>Minimum Age</h2>
+
+      <p>
+        You must be at least 13 years old to use our Services. The minimum age
+        to use our services without parental approval may be higher in your home
+        country.
+      </p>
+
+      <h2>What are the basics of using Comm?</h2>
+
+      <p>
+        In order to access Comm, you are required to sign up for an account and
+        select a user name and password. You will not share your user name,
+        account or password with anyone, and you must protect the security of
+        your user name, account, password and any other access tools or
+        credentials. You’re responsible for any activity associated with your
+        user name and account.
+      </p>
+
+      <h2>Who will have access to my Content?</h2>
+
+      <p>
+        <strong>
+          Comm Technologies does not have the ability to access, view or control
+          anything you post, upload, share, store, or otherwise provide through
+          the Services (your “Content”).
+        </strong>{' '}
+        However, other Comm users may have access to and control over your
+        Content. For more information on how Comm works, please{' '}
+        <a href="https://comm.app/how-it-works">click here</a>.
+      </p>
+
+      <h2>Are there restrictions in how I can use the Services?</h2>
+
+      <p>
+        You agree that you will not provide or contribute anything to the
+        Services, or otherwise use or interact with the Services, in a manner
+        that: (a) infringes or violates the right of Comm Technologies, or our
+        users, or others, including intellectual property or other proprietary
+        rights; or (b) violates any law or regulation, including without
+        limitation, any applicable export control laws, privacy laws or any
+        other purpose not reasonably intended by Comm Technologies.
+      </p>
+
+      <h2>What are my rights in the Services?</h2>
+
+      <p>
+        The Content displayed or performed or available on or through the
+        Services may be protected by copyright and/or other intellectual
+        property laws. You promise to abide by all copyright notices, trademark
+        rules, information, and restrictions contained in any Content you access
+        through the Services, and you won’t use, copy, reproduce, modify,
+        translate, publish, broadcast, transmit, distribute, perform, upload,
+        display, license, sell, commercialize or otherwise exploit for any
+        purpose any Content not owned by you, (i) without the prior consent of
+        the owner of that Content or (ii) in a way that violates someone else’s
+        (including Comm Technologies&apos;) rights. You further agree to comply
+        with all applicable open source licenses.
+      </p>
+
+      <p>
+        Subject to these Terms, we grant each user of the Services a worldwide,
+        non-exclusive, non-sublicensable and non-transferable license to use the
+        Services.
+      </p>
+
+      <h2>Who is responsible for what is contributed to the Services?</h2>
+
+      <p>
+        Any information or Content posted or transmitted through the Services is
+        the sole responsibility of the person from whom such Content originated,
+        and you access all such information and Content at your own risk, and we
+        aren’t liable for any errors or omissions in that information or Content
+        or for any damages or loss you might suffer in connection with it.
+      </p>
+
+      <h2>
+        What if I see something on the Services that infringes my copyright?
+      </h2>
+
+      <p>
+        Comm Technologies does not host and has no access to or control over any
+        Content provided through the Services and has no ability to remove any
+        such Content. If you have any further questions, please contact{' '}
+        <a href="mailto:support@comm.app">support@comm.app</a>.
+      </p>
+
+      <h2>Who is responsible for third-party services?</h2>
+
+      <p>
+        The Services may contain links or connections to third-party websites or
+        services that are not owned or controlled by Comm Technologies. When you
+        access third-party websites or use third-party services, you accept that
+        there are risks in doing so, and that Comm Technologies is not
+        responsible for such risks. We encourage you to be aware when you leave
+        the Services and to read the terms and conditions and privacy policy of
+        each third-party website or service that you visit or utilize.
+      </p>
+
+      <h2>Will Comm Technologies ever change the Services?</h2>
+
+      <p>
+        We’re always trying to improve our Services, so they may change over
+        time. We may suspend or discontinue any part of the Services, or we may
+        introduce new features or impose limits on certain features or restrict
+        access to parts or all of the Services.
+      </p>
+
+      <h2>What if I want to stop using the Services?</h2>
+
+      <p>
+        You’re free to do that at any time, either by deleting your account
+        within the app, or by contacting us at{' '}
+        <a href="mailto:support@comm.app">support@comm.app</a>; please refer to
+        our <a href="https://comm.app/privacy">Privacy Policy</a>, as well as
+        the licenses above, to understand how we treat information you provide
+        to us after you have stopped using our Services. Termination of your
+        account may or may not result in destruction of Content associated with
+        your account, and other Comm users may continue to have access to and
+        control over your Content.
+      </p>
+
+      <p>
+        Comm Technologies is also free to terminate (or suspend access to) your
+        use of the Services or your account for any reason at our discretion,
+        including your breach of these Terms. Comm Technologies has the sole
+        right to decide whether you are in violation of any of the restrictions
+        set forth in these Terms.
+      </p>
+
+      <p>
+        Provisions that, by their nature, should survive termination of these
+        Terms shall survive termination. By way of example, all of the following
+        will survive termination: any limitations on our liability, any terms
+        regarding ownership or intellectual property rights, and terms regarding
+        disputes between us.
+      </p>
+
+      <h2>What else do I need to know?</h2>
+
+      <h2>Warranty Disclaimer.</h2>
+
+      <p>
+        Comm Technologies and its licensors, suppliers, partners, parent,
+        subsidiaries or affiliated entities, and each of their respective
+        officers, directors, members, employees, consultants, contract
+        employees, representatives and agents, and each of their respective
+        successors and assigns (Comm Technologies and all such parties together,
+        the “Comm Technologies Parties”) make no representations or warranties
+        concerning the Services, including without limitation regarding any
+        content contained in or accessed through the Services, and the Comm
+        Technologies Parties will not be responsible or liable for the accuracy,
+        copyright compliance, legality, or decency of material contained in or
+        accessed through the Services or any claims, actions, suits procedures,
+        costs, expenses, damages or liabilities arising out of use of, or in any
+        way related to your participation in, the Services. The Comm
+        Technologies Parties make no representations or warranties regarding
+        suggestions or recommendations of services or products offered or
+        purchased through or in connection with the Services. THE SERVICES AND
+        CONTENT ARE PROVIDED BY COMM TECHNOLOGIES (AND ITS LICENSORS AND
+        SUPPLIERS) ON AN “AS-IS” BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER
+        EXPRESS OR IMPLIED, INCLUDING, WITHOUT LIMITATION, IMPLIED WARRANTIES OF
+        MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, NON- INFRINGEMENT, OR
+        THAT USE OF THE SERVICES WILL BE UNINTERRUPTED OR ERROR-FREE. SOME
+        STATES DO NOT ALLOW LIMITATIONS ON HOW LONG AN IMPLIED WARRANTY LASTS,
+        SO THE ABOVE LIMITATIONS MAY NOT APPLY TO YOU. WE DO NOT CONTROL, AND
+        ARE NOT RESPONSIBLE FOR, CONTROLLING HOW OR WHEN OUR USERS USE OUR
+        SERVICES. WE ARE NOT RESPONSIBLE FOR THE ACTIONS OR INFORMATION
+        (INCLUDING CONTENT) OF OUR USERS OR OTHER THIRD PARTIES.
+      </p>
+
+      <h2>Limitation of Liability.</h2>
+
+      <p>
+        TO THE FULLEST EXTENT ALLOWED BY APPLICABLE LAW, UNDER NO CIRCUMSTANCES
+        AND UNDER NO LEGAL THEORY (INCLUDING, WITHOUT LIMITATION, TORT,
+        CONTRACT, STRICT LIABILITY, OR OTHERWISE) SHALL ANY OF THE COMM
+        TECHNOLOGIES PARTIES BE LIABLE TO YOU OR TO ANY OTHER PERSON FOR (A) ANY
+        INDIRECT, SPECIAL, INCIDENTAL, PUNITIVE OR CONSEQUENTIAL DAMAGES OF ANY
+        KIND, INCLUDING DAMAGES FOR LOST PROFITS, BUSINESS INTERRUPTION, LOSS OF
+        DATA, LOSS OF GOODWILL, WORK STOPPAGE, ACCURACY OF RESULTS, OR COMPUTER
+        FAILURE OR MALFUNCTION, (B) ANY SUBSTITUTE GOODS, SERVICES OR
+        TECHNOLOGY, (C) ANY AMOUNT, IN THE AGGREGATE, IN EXCESS OF THE GREATER
+        OF (I) ONE-HUNDRED ($100) DOLLARS OR (II) THE AMOUNTS PAID AND/OR
+        PAYABLE BY YOU TO COMM TECHNOLOGIES THE IN CONNECTION WITH THE SERVICES
+        IN THE TWELVE (12) MONTH PERIOD PRECEDING THIS APPLICABLE CLAIM OR (D)
+        ANY MATTER BEYOND OUR REASONABLE CONTROL. SOME STATES DO NOT ALLOW THE
+        EXCLUSION OR LIMITATION OF INCIDENTAL OR CONSEQUENTIAL OR CERTAIN OTHER
+        DAMAGES, SO ABOVE LIMITATION AND EXCLUSIONS MAY NOT APPLY TO YOU.
+      </p>
+
+      <h2>Assignment.</h2>
+
+      <p>
+        You may not assign, delegate or transfer these Terms or your rights or
+        obligations hereunder, or your Services account, in any way (by
+        operation of law or otherwise) without Comm Technologies&apos; prior
+        written consent. We may transfer, assign, or delegate these Terms and
+        our rights and obligations without consent.
+      </p>
+
+      <h2>Dispute Resolution.</h2>
+
+      <p>
+        These Terms are governed by and will be construed under the laws of the
+        State of New York, without regard to the conflict of laws provisions
+        thereof. Any legal action or proceeding relating to these Terms shall be
+        brought exclusively in the state or federal courts located in New York
+        County, New York, and each party consents to the jurisdiction thereof.
+      </p>
+
+      <h2>Miscellaneous</h2>
+
+      <p>
+        The failure of either you or us to exercise, in any way, any right
+        herein shall not be deemed a waiver of any further rights hereunder. If
+        any provision of these Terms are found to be unenforceable or invalid,
+        that provision will be limited or eliminated, to the minimum extent
+        necessary, so that these Terms shall otherwise remain in full force and
+        effect and enforceable. You and Comm Technologies agree that these Terms
+        are the complete and exclusive statement of the mutual understanding
+        between you and Comm Technologies, and that these Terms supersede and
+        cancel all previous written and oral agreements, communications and
+        other understandings relating to the subject matter of these Terms. You
+        hereby acknowledge and agree that you are not an employee, agent,
+        partner, or joint venture of Comm Technologies, and you do not have any
+        authority of any kind to bind Comm Technologies in any respect
+        whatsoever.
+      </p>
+    </div>
+  );
 }
 
 export default Terms;
