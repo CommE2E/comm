0e7450c88b76b673ad192e26d93780ce1fd62b95
Ashoat Tevosyan <ashoat@comm.app>
2021-07-07 19:39:39 -0400

    [native] Keep messageListVerticalBounds updated while keyboard is up
    Summary:
    As part of [this task](https://www.notion.so/commapp/Make-it-possible-to-interact-with-MessageList-while-keyboard-is-up-1b4c5589b5914297b35629147a1e13df), I made it so the user can open overlays while the keyboard is up. But because we weren't updating `messageListVerticalBounds` while the keyboard was up, tooltips were getting rendered in the wrong direction.
    
    This diff makes it so we keep `messageListVerticalBounds` updated, even while the keyboard is up.
    
    Test Plan: Make sure that tooltips open in the correct directions while the keyboard is up. Test with message scrolled near top and message scrolled near bottom
    
    Reviewers: palys-swm
    
    Reviewed By: palys-swm
    
    Subscribers: KatPo, Adrian, atul
    
    Differential Revision: https://phabricator.ashoat.com/D1629

--- native/chat/message-list.react.js
+++ native/chat/message-list.react.js
@@ -259,12 +259,6 @@ class MessageList extends React.PureComponent<Props, State> {
     if (!flatListContainer) {
       return;
     }
-
-    const { keyboardState } = this.props;
-    if (!keyboardState || keyboardState.keyboardShowing) {
-      return;
-    }
-
     flatListContainer.measure((x, y, width, height, pageX, pageY) => {
       if (
         height === null ||
