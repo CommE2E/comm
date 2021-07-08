CommE2E/comm

commit 56da3f7bd1d94a9c593976657285be64020f4fce

[web] console.warn when we choose not to set an active, unread thread to read
Summary: Adding this logging to help investigate [this issue](https://www.notion.so/commapp/Long-lived-web-instance-stopped-keeping-active-threads-as-read-99dc5afa58014d7abc51d0af4146393f) that I'm only seeing on prod.

Test Plan: Run prod build, open two windows logged in as different users. Have user 1 open a thread, and then switch to the other window and have user 2 send a message to user 1 in that thread

Reviewers: atul

Reviewed By: atul

Subscribers: KatPo, palys-swm, Adrian

Differential Revision: https://phabricator.ashoat.com/D1587

web/redux/redux-setup.js
@@ -152,6 +152,29 @@ function validateState(oldState: AppState, state: AppState): AppState {
   const activeThread = activeThreadSelector(state);
   if (
     activeThread &&
+    state.threadStore.threadInfos[activeThread].currentUser.unread &&
+    getVisibility().hidden()
+  ) {
+    console.warn(
+      `thread ${activeThread} is active and unread, ` +
+        'but visibilityjs reports the window is not visible',
+    );
+  }
+  if (
+    activeThread &&
+    state.threadStore.threadInfos[activeThread].currentUser.unread &&
+    typeof document !== 'undefined' &&
+    document &&
+    document.hasFocus &&
+    !document.hasFocus()
+  ) {
+    console.warn(
+      `thread ${activeThread} is active and unread, ` +
+        'but document.hasFocus() is false',
+    );
+  }
+  if (
+    activeThread &&
     !getVisibility().hidden() &&
     typeof document !== 'undefined' &&
     document &&

commit 4847ae61afbdf829d891cf0d26a04a50adea1903

[web] Fix pending sidebars on web
Summary: I discovered that nothing happens on prod when you press the "Create sidebar" button on web. I investigated and realized I had broken this in D1587 because I had failed to check `state.navInfo.pendingThread`.

Test Plan: Test pending sidebar creation flow on web

Reviewers: palys-swm, atul

Subscribers: KatPo, Adrian

Differential Revision: https://phabricator.ashoat.com/D1647

web/redux/redux-setup.js
@@ -152,6 +152,7 @@ function validateState(oldState: AppState, state: AppState): AppState {
   const activeThread = activeThreadSelector(state);
   if (
     activeThread &&
+    !state.navInfo.pendingThread &&
     state.threadStore.threadInfos[activeThread].currentUser.unread &&
     getVisibility().hidden()
   ) {
@@ -162,6 +163,7 @@ function validateState(oldState: AppState, state: AppState): AppState {
   }
   if (
     activeThread &&
+    !state.navInfo.pendingThread &&
     state.threadStore.threadInfos[activeThread].currentUser.unread &&
     typeof document !== 'undefined' &&
     document &&