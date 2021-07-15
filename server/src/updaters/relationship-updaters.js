CommE2E/comm

commit 5e6bc99e3dd0e9e18fae3cfdf05f5e14f077a309

[server] Avoiding dispatching UPDATE_USER if relationships already exist
Summary: In `commitMembershipChangeset` we create `UPDATE_USER` updates for any relationships we set in the DB, even if we didn't change the row. This is wasteful since those updates get propagated to individual clients. We should check if the row needs to be changed before creating these updates.

Test Plan: I tested this diff without D1679 and found this decreased the time it took for the relationship logic to be handled from several minutes to just 20s. It still took 20s to run the huge `SELECT` query in this diff, but that was way faster than doing the corresponding `DELETE FROM updates` query in `createUpdates` (for the update replacement logic)

Reviewers: palys-swm

Reviewed By: palys-swm

Subscribers: KatPo, Adrian, atul

Differential Revision: https://phabricator.ashoat.com/D1680

server/src/updaters/relationship-updaters.js
@@ -21,7 +21,7 @@ import { promiseAll } from 'lib/utils/promises';
 import createMessages from '../creators/message-creator';
 import { createThread } from '../creators/thread-creator';
 import { createUpdates } from '../creators/update-creator';
-import { dbQuery, SQL } from '../database/database';
+import { dbQuery, SQL, mergeOrConditions } from '../database/database';
 import { fetchFriendRequestRelationshipOperations } from '../fetchers/relationship-fetchers';
 import { fetchUserInfos } from '../fetchers/user-fetchers';
 import type { Viewer } from '../session/viewer';
@@ -252,6 +252,49 @@ async function updateUndirectedRelationships(
   await dbQuery(query);
 }
 
+async function updateChangedUndirectedRelationships(
+  changeset: UndirectedRelationshipRow[],
+): Promise<UpdateData[]> {
+  if (!changeset.length) {
+    return [];
+  }
+
+  const selectQuery = SQL`
+    SELECT user1, user2, status
+    FROM relationships_undirected
+    WHERE
+  `;
+  const conditions = [];
+  for (const row of changeset) {
+    conditions.push(SQL`(user1 = ${row.user1} AND user2 = ${row.user2})`);
+  }
+  selectQuery.append(mergeOrConditions(conditions));
+
+  const [result] = await dbQuery(selectQuery);
+  const existingStatuses = new Map();
+  for (const row of result) {
+    existingStatuses.set(`${row.user1}|${row.user2}`, row.status);
+  }
+
+  const insertRows = [];
+  for (const row of changeset) {
+    const existingStatus = existingStatuses.get(`${row.user1}|${row.user2}`);
+    if (existingStatus && existingStatus < row.status) {
+      insertRows.push([row.user1, row.user2, row.status]);
+    }
+  }
+  const insertQuery = SQL`
+    INSERT INTO relationships_undirected (user1, user2, status)
+    VALUES ${insertRows}
+    ON DUPLICATE KEY UPDATE status = GREATEST(status, VALUES(status))
+  `;
+  await dbQuery(insertQuery);
+
+  return updateDatasForUserPairs(
+    insertRows.map(([user1, user2]) => [user1, user2]),
+  );
+}
+
 async function createPersonalThreads(
   viewer: Viewer,
   request: RelationshipRequest,
@@ -312,4 +355,5 @@ export {
   updateRelationships,
   updateDatasForUserPairs,
   updateUndirectedRelationships,
+  updateChangedUndirectedRelationships,
 };

server/src/updaters/thread-permission-updaters.js
@@ -37,10 +37,7 @@ import { createScriptViewer } from '../session/scripts';
 import type { Viewer } from '../session/viewer';
 import DepthQueue from '../utils/depth-queue';
 import RelationshipChangeset from '../utils/relationship-changeset';
-import {
-  updateDatasForUserPairs,
-  updateUndirectedRelationships,
-} from './relationship-updaters';
+import { updateChangedUndirectedRelationships } from './relationship-updaters';
 
 export type MembershipRowToSave = {|
   +operation: 'save',
@@ -1019,10 +1016,10 @@ async function commitMembershipChangeset(
   }
   const relationshipRows = relationshipChangeset.getRows();
 
-  await Promise.all([
+  const [updateDatas] = await Promise.all([
+    updateChangedUndirectedRelationships(relationshipRows),
     saveMemberships(toSave),
     deleteMemberships(toDelete),
-    updateUndirectedRelationships(relationshipRows),
     rescindPushNotifsForMemberDeletion(toRescindPushNotifs),
   ]);
 
@@ -1033,9 +1030,6 @@ async function commitMembershipChangeset(
   const { threadInfos: serverThreadInfos } = serverThreadInfoFetchResult;
 
   const time = Date.now();
-  const updateDatas = updateDatasForUserPairs(
-    relationshipRows.map(({ user1, user2 }) => [user1, user2]),
-  );
   for (const changedThreadID of changedThreadIDs) {
     const serverThreadInfo = serverThreadInfos[changedThreadID];
     for (const memberInfo of serverThreadInfo.members) {

commit b363c27f47cb170579cd3291aa4f952a4f02abc0

[server] Fix relationship creation for new threads
Summary: I accidentally broke this in D1680. The code currently only updates the relationship if it already exists but has a lower `status` than requested. This diff makes sure that the relationship is always created when it doesn't yet exist.

Test Plan: Manually deployed this fix to prod and verified it solved the issue

Reviewers: atul

Reviewed By: atul

Subscribers: KatPo, palys-swm, Adrian

Differential Revision: https://phabricator.ashoat.com/D1695

server/src/updaters/relationship-updaters.js
@@ -279,7 +279,7 @@ async function updateChangedUndirectedRelationships(
   const insertRows = [];
   for (const row of changeset) {
     const existingStatus = existingStatuses.get(`${row.user1}|${row.user2}`);
-    if (existingStatus && existingStatus < row.status) {
+    if (!existingStatus || existingStatus < row.status) {
       insertRows.push([row.user1, row.user2, row.status]);
     }
   }