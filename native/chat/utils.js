e7fd4338cb5ebf2de87def4ed6962f6edd2dfccb
Tomasz Palys <tomasz.palys@swmansion.com>
2021-07-21 13:41:12 +0200

    [native] Create a hook which handles source message animation
    Summary: The hook provides message container style and handles making source message visible again.
    
    Test Plan: Tested with the next diff
    
    Reviewers: ashoat
    
    Reviewed By: ashoat
    
    Subscribers: KatPo, Adrian, atul
    
    Differential Revision: https://phabricator.ashoat.com/D1773

--- native/chat/utils.js
+++ native/chat/utils.js
@@ -1,12 +1,15 @@
 // @flow
 
+import invariant from 'invariant';
 import * as React from 'react';
+import Animated from 'react-native-reanimated';
 
 import { useMessageListData } from 'lib/selectors/chat-selectors';
 
 import { useSelector } from '../redux/redux-utils';
 import type { LayoutCoordinates, VerticalBounds } from '../types/layout-types';
-import { useHeightMeasurer } from './chat-context';
+import type { AnimatedViewStyle } from '../types/styles';
+import { ChatContext, useHeightMeasurer } from './chat-context';
 import type { ChatMessageItemWithHeight } from './message-list-container.react';
 import {
   type ChatMessageInfoItemWithHeight,
@@ -14,6 +17,10 @@ import {
 } from './message.react';
 import { getSidebarThreadInfo } from './sidebar-navigation';
 import { timestampHeight } from './timestamp.react';
+
+/* eslint-disable import/no-named-as-default-member */
+const { Node, Extrapolate, interpolateNode } = Animated;
+/* eslint-enable import/no-named-as-default-member */
 
 function chatMessageItemHeight(item: ChatMessageItemWithHeight): number {
   if (item.itemType === 'loader') {
@@ -88,4 +95,52 @@ function useMessageTargetPosition(
   return targetDistanceFromBottom - currentDistanceFromBottom;
 }
 
-export { chatMessageItemHeight, useMessageTargetPosition };
+function useAnimatedMessageTooltipButton(
+  sourceMessage: ChatMessageInfoItemWithHeight,
+  initialCoordinates: LayoutCoordinates,
+  messageListVerticalBounds: VerticalBounds,
+  progress: Node,
+): { +style: AnimatedViewStyle } {
+  const targetPosition = useMessageTargetPosition(
+    sourceMessage,
+    initialCoordinates,
+    messageListVerticalBounds,
+  );
+
+  const chatContext = React.useContext(ChatContext);
+  invariant(chatContext, 'chatContext should be set');
+  const {
+    currentTransitionSidebarSourceID,
+    setCurrentTransitionSidebarSourceID,
+  } = chatContext;
+
+  React.useEffect(() => {
+    return () => setCurrentTransitionSidebarSourceID(null);
+  }, [setCurrentTransitionSidebarSourceID]);
+
+  const bottom = React.useMemo(
+    () =>
+      interpolateNode(progress, {
+        inputRange: [0, 1],
+        outputRange: [targetPosition, 0],
+        extrapolate: Extrapolate.CLAMP,
+      }),
+    [progress, targetPosition],
+  );
+
+  const messageContainerStyle = React.useMemo(() => {
+    return {
+      bottom: currentTransitionSidebarSourceID ? bottom : 0,
+    };
+  }, [bottom, currentTransitionSidebarSourceID]);
+
+  return {
+    style: messageContainerStyle,
+  };
+}
+
+export {
+  getSidebarThreadInfo,
+  chatMessageItemHeight,
+  useAnimatedMessageTooltipButton,
+};
