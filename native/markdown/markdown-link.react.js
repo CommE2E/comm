93b57d2a115aa7cfce79b46cb826e814d3efa510
Ashoat Tevosyan <ashoat@comm.app>
2021-07-21 22:41:00 -0400

    [native] Make Markdown link presses work on Android
    Summary:
    @atul's friend reported [an issue](https://www.notion.so/commapp/Android-Issue-with-links-in-messages-f4098a529d3049f5aaf724c9c3ddecd9) where links in messages were not pressable on Android. This diff solves it.
    
    [This react-native-gesture-handler issue](https://github.com/software-mansion/react-native-gesture-handler/issues/867) is the root cause. I found a way around this using arcane React Native magic. The docs cover [some](https://reactnative.dev/docs/text#onstartshouldsetrespondercapture) of [this](https://reactnative.dev/docs/gesture-responder-system#capture-shouldset-handlers), but they are largely inaccurate. Mostly I figured this one by adding logs to all the callbacks referenced in the docs, and seeing which ones got triggered in which scenarios.
    
    Test Plan: I tested various combinations of link presses, message swipes, and message presses, in various orders, on an Android device
    
    Reviewers: palys-swm, atul
    
    Reviewed By: palys-swm
    
    Subscribers: KatPo, Adrian, atul
    
    Differential Revision: https://phabricator.ashoat.com/D1777

--- native/markdown/markdown-link.react.js
+++ native/markdown/markdown-link.react.js
@@ -1,14 +1,19 @@
 // @flow
 
 import * as React from 'react';
-import { Text, Linking, Alert } from 'react-native';
+import { Text, Linking, Alert, Platform } from 'react-native';
 
 import { normalizeURL } from 'lib/utils/url-utils';
 
-import { MarkdownLinkContext } from './markdown-link-context';
+import {
+  MarkdownLinkContext,
+  type MarkdownLinkContextType,
+} from './markdown-link-context';
 
-function useDisplayLinkPrompt(inputURL: string) {
-  const markdownLinkContext = React.useContext(MarkdownLinkContext);
+function useDisplayLinkPrompt(
+  inputURL: string,
+  markdownLinkContext: ?MarkdownLinkContextType,
+) {
   const setLinkModalActive = markdownLinkContext?.setLinkModalActive;
   const onDismiss = React.useCallback(() => {
     setLinkModalActive?.(false);
@@ -45,9 +50,33 @@ type Props = {
   ...TextProps,
 };
 function MarkdownLink(props: Props): React.Node {
+  const markdownLinkContext = React.useContext(MarkdownLinkContext);
+
   const { target, ...rest } = props;
-  const onPressLink = useDisplayLinkPrompt(target);
-  return <Text onPress={onPressLink} {...rest} />;
+  const onPressLink = useDisplayLinkPrompt(target, markdownLinkContext);
+
+  const setLinkPressActive = markdownLinkContext?.setLinkPressActive;
+  const androidOnStartShouldSetResponderCapture = React.useCallback(() => {
+    setLinkPressActive?.(true);
+    return true;
+  }, [setLinkPressActive]);
+
+  const androidOnResponderTerminate = React.useCallback(() => {
+    onPressLink();
+    setLinkPressActive?.(false);
+  }, [onPressLink, setLinkPressActive]);
+
+  if (Platform.OS !== 'android') {
+    return <Text onPress={onPressLink} {...rest} />;
+  }
+
+  // The Flow type for Text's props is missing onStartShouldSetResponderCapture
+  const gestureProps: any = {
+    onStartShouldSetResponderCapture: androidOnStartShouldSetResponderCapture,
+    onResponderTerminate: androidOnResponderTerminate,
+  };
+
+  return <Text {...gestureProps} {...rest} />;
 }
 
 export default MarkdownLink;
