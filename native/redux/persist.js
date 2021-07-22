81a483481e42a11ef96170e79f77806e9379030e
Ashoat Tevosyan <ashoat@comm.app>
2021-07-22 16:50:14 -0400

    [native] codeVersion -> 92
    Android-only release to solve notifications. (See [Notion task](https://www.notion.so/commapp/Android-fails-to-start-JS-runtime-from-notification-bb01f808f0e44201be941a6fbbc1a4a7).)

--- native/redux/persist.js
+++ native/redux/persist.js
@@ -280,7 +280,7 @@ const persistConfig = {
   timeout: ((__DEV__ ? 0 : undefined): number | void),
 };
 
-const codeVersion = 91;
+const codeVersion = 92;
 
 // This local exists to avoid a circular dependency where redux-setup needs to
 // import all the navigation and screen stuff, but some of those screens want to
