eaa6483c02273a069d2d64d0a8b4d65dfa7a7832
Atul Madhugiri <atulm@berkeley.edu>
2021-07-27 12:17:15 -0400

    [native] Introduce `MessageStoreOperation` and `processMessageStoreOperations` to `CommCoreModuleSchema`
    Summary: Introducing types + adding `processMessageStoreOperations` function to spec on js side
    
    Test Plan: Will be tested w/ subsequent diffs
    
    Reviewers: ashoat
    
    Reviewed By: ashoat
    
    Subscribers: palys-swm, Adrian, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D1812

--- native/schema/CommCoreModuleSchema.js
+++ native/schema/CommCoreModuleSchema.js
@@ -5,7 +5,35 @@
 import { TurboModuleRegistry } from 'react-native';
 import type { TurboModule } from 'react-native/Libraries/TurboModule/RCTExport';
 
-import type { SQLiteMessageInfo, SQLiteDraftInfo } from '../types/core-module';
+export type SQLiteMessageInfo = {
+  +id: string,
+  +thread: string,
+  +user: string,
+  +type: string,
+  +futureType: string,
+  +content: string,
+  +time: string,
+  +creation: string,
+};
+
+export type SQLiteDraftInfo = {
+  +key: string,
+  +text: string,
+};
+
+export type RemoveMessageOperation = {
+  +type: 'remove',
+  +payload: { +id: string },
+};
+
+export type ReplaceMessageOperation = {
+  +type: 'replace',
+  +payload: SQLiteMessageInfo,
+};
+
+export type MessageStoreOperation =
+  | RemoveMessageOperation
+  | ReplaceMessageOperation;
 
 export interface Spec extends TurboModule {
   +getDraft: (key: string) => Promise<string>;
@@ -15,6 +43,9 @@ export interface Spec extends TurboModule {
   +removeAllDrafts: () => Promise<void>;
   +removeAllMessages: () => Promise<void>;
   +getAllMessages: () => Promise<$ReadOnlyArray<SQLiteMessageInfo>>;
+  +processMessageStoreOperations: (
+    operations: $ReadOnlyArray<MessageStoreOperation>,
+  ) => Promise<void>;
 }
 
 export default (TurboModuleRegistry.getEnforcing<Spec>(
