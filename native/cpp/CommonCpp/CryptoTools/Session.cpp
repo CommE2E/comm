97d632b53a3d2d35880d8cbe31d1bba882fda8f2
karol-bisztyga <karol.bisztyga@swmansion.com>
2021-08-03 12:16:20 +0200

    [native] Generate Keys - add Olm session wrapper
    Summary:
    Adding olm session wrapper to the comm repo.
    The original code can be found [here](https://github.com/karol-bisztyga/olm_tests/tree/main/karol_tests)
    
    Test Plan: none
    
    Reviewers: palys-swm, ashoat
    
    Reviewed By: palys-swm, ashoat
    
    Subscribers: ashoat, palys-swm, Adrian, atul
    
    Differential Revision: https://phabricator.ashoat.com/D1841

---
+++ native/cpp/CommonCpp/CryptoTools/Session.cpp
@@ -0,0 +1,109 @@
+#include "Session.h"
+
+#include "Tools.h"
+
+namespace comm {
+namespace crypto {
+
+std::unique_ptr<Session> Session::createSessionAsInitializer(
+    OlmAccount *account,
+    std::uint8_t *ownerIdentityKeys,
+    const OlmBuffer &idKeys,
+    const OlmBuffer &oneTimeKeys,
+    size_t keyIndex) {
+  std::unique_ptr<Session> session(new Session(account, ownerIdentityKeys));
+
+  session->olmSessionBuffer.resize(::olm_session_size());
+  session->olmSession = ::olm_session(session->olmSessionBuffer.data());
+
+  OlmBuffer randomBuffer;
+  Tools::getInstance().generateRandomBytes(
+      randomBuffer,
+      ::olm_create_outbound_session_random_length(session->olmSession));
+
+  if (-1 == ::olm_create_outbound_session(
+                session->olmSession,
+                session->ownerUserAccount,
+                idKeys.data() + ID_KEYS_PREFIX_OFFSET,
+                KEYSIZE,
+                oneTimeKeys.data() + ONE_TIME_KEYS_PREFIX_OFFSET +
+                    (KEYSIZE + ONE_TIME_KEYS_MIDDLE_OFFSET) * keyIndex,
+                KEYSIZE,
+                randomBuffer.data(),
+                randomBuffer.size())) {
+    throw std::runtime_error(
+        "error createOutbound => ::olm_create_outbound_session");
+  }
+  return std::move(session);
+}
+
+std::unique_ptr<Session> Session::createSessionAsResponder(
+    OlmAccount *account,
+    std::uint8_t *ownerIdentityKeys,
+    const OlmBuffer &encryptedMessage,
+    const OlmBuffer &idKeys) {
+  std::unique_ptr<Session> session(new Session(account, ownerIdentityKeys));
+
+  OlmBuffer tmpEncryptedMessage(encryptedMessage);
+  session->olmSessionBuffer.resize(::olm_session_size());
+  session->olmSession = ::olm_session(session->olmSessionBuffer.data());
+  if (-1 == ::olm_create_inbound_session(
+                session->olmSession,
+                session->ownerUserAccount,
+                tmpEncryptedMessage.data(),
+                encryptedMessage.size())) {
+    throw std::runtime_error(
+        "error createInbound => ::olm_create_inbound_session");
+  }
+  return std::move(session);
+}
+
+OlmBuffer Session::storeAsB64(const std::string &secretKey) {
+  size_t pickleLength = ::olm_pickle_session_length(this->olmSession);
+  OlmBuffer pickle(pickleLength);
+  size_t res = ::olm_pickle_session(
+      this->olmSession,
+      secretKey.data(),
+      secretKey.size(),
+      pickle.data(),
+      pickleLength);
+  if (pickleLength != res) {
+    throw std::runtime_error("error pickleSession => ::olm_pickle_session");
+  }
+  return pickle;
+}
+
+std::unique_ptr<Session> Session::restoreFromB64(
+    OlmAccount *account,
+    std::uint8_t *ownerIdentityKeys,
+    const std::string &secretKey,
+    OlmBuffer &b64) {
+  std::unique_ptr<Session> session(new Session(account, ownerIdentityKeys));
+
+  session->olmSessionBuffer.resize(::olm_session_size());
+  session->olmSession = ::olm_session(session->olmSessionBuffer.data());
+  if (-1 == ::olm_unpickle_session(
+                session->olmSession,
+                secretKey.data(),
+                secretKey.size(),
+                b64.data(),
+                b64.size())) {
+    throw std::runtime_error("error pickleSession => ::olm_unpickle_session");
+  }
+  if (b64.size() != ::olm_pickle_session_length(session->olmSession)) {
+    throw std::runtime_error(
+        "error pickleSession => ::olm_pickle_session_length");
+  }
+  return session;
+}
+
+OlmSession *Session::getOlmSession() {
+  if (this->olmSession == nullptr) {
+    throw std::runtime_error(
+        "trying to obtain a session pointer of uninitialized session");
+  }
+  return this->olmSession;
+}
+
+} // namespace crypto
+} // namespace comm
---
+++ native/cpp/CommonCpp/CryptoTools/Session.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <memory>
+#include <string>
+
+#include "Tools.h"
+
+namespace comm {
+namespace crypto {
+
+class Session {
+  OlmAccount *ownerUserAccount;
+  std::uint8_t *ownerIdentityKeys;
+
+  OlmSession *olmSession = nullptr;
+  OlmBuffer olmSessionBuffer;
+
+  Session(OlmAccount *account, std::uint8_t *ownerIdentityKeys)
+      : ownerUserAccount(account), ownerIdentityKeys(ownerIdentityKeys) {
+  }
+
+public:
+  static std::unique_ptr<Session> createSessionAsInitializer(
+      OlmAccount *account,
+      std::uint8_t *ownerIdentityKeys,
+      const OlmBuffer &idKeys,
+      const OlmBuffer &oneTimeKeys,
+      size_t keyIndex = 0);
+  static std::unique_ptr<Session> createSessionAsResponder(
+      OlmAccount *account,
+      std::uint8_t *ownerIdentityKeys,
+      const OlmBuffer &encryptedMessage,
+      const OlmBuffer &idKeys);
+  OlmBuffer storeAsB64(const std::string &secretKey);
+  static std::unique_ptr<Session> restoreFromB64(
+      OlmAccount *account,
+      std::uint8_t *ownerIdentityKeys,
+      const std::string &secretKey,
+      OlmBuffer &b64);
+  OlmSession *getOlmSession();
+};
+
+} // namespace crypto
+} // namespace comm
