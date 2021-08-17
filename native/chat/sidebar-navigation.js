CommE2E/comm

commit 4273a4b05b34a4e647b3af98bf24810e33493bf7

[native] Navigate to sidebar from SwipeableMessage
Summary: Actually performs the action when the user swipes

Test Plan: Test the threshold on a real phone

Reviewers: palys-swm, atul

Subscribers: Adrian, karol-bisztyga

Differential Revision: https://phabricator.ashoat.com/D1806

native/chat/sidebar-navigation.js
@@ -1,6 +1,7 @@
 // @flow
 
 import invariant from 'invariant';
+import * as React from 'react';
 
 import { createPendingSidebar } from 'lib/shared/thread-utils';
 import type { ThreadInfo } from 'lib/types/thread-types';
@@ -11,8 +12,12 @@ import { getDefaultTextMessageRules } from '../markdown/rules.react';
 import type { AppNavigationProp } from '../navigation/app-navigator.react';
 import type { MessageTooltipRouteNames } from '../navigation/route-names';
 import type { TooltipRoute } from '../navigation/tooltip.react';
+import { useSelector } from '../redux/redux-utils';
 import type { ChatContextType } from './chat-context';
-import { createNavigateToThreadAction } from './message-list-types';
+import {
+  createNavigateToThreadAction,
+  useNavigateToThread,
+} from './message-list-types';
 import type { ChatMessageInfoItemWithHeight } from './message.react';
 
 function getSidebarThreadInfo(
@@ -52,4 +57,18 @@ function navigateToSidebar<RouteName: MessageTooltipRouteNames>(
   navigation.navigate(createNavigateToThreadAction({ threadInfo }));
 }
 
-export { navigateToSidebar, getSidebarThreadInfo };
+function useNavigateToSidebar(item: ChatMessageInfoItemWithHeight): () => void {
+  const viewerID = useSelector(
+    state => state.currentUserInfo && state.currentUserInfo.id,
+  );
+  const threadInfo = React.useMemo(() => getSidebarThreadInfo(item, viewerID), [
+    item,
+    viewerID,
+  ]);
+  const navigateToThread = useNavigateToThread();
+  return React.useCallback(() => {
+    navigateToThread({ threadInfo });
+  }, [navigateToThread, threadInfo]);
+}
+
+export { navigateToSidebar, getSidebarThreadInfo, useNavigateToSidebar };

native/chat/composed-message.react.js
@@ -19,6 +19,7 @@ import {
 } from './inline-sidebar.react';
 import { MessageHeader } from './message-header.react';
 import type { ChatMessageInfoItemWithHeight } from './message.react';
+import { useNavigateToSidebar } from './sidebar-navigation';
 import SwipeableMessage from './swipeable-message.react';
 
 const clusterEndHeight = 7;
@@ -38,6 +39,7 @@ type Props = {
   +colors: Colors,
   // withInputState
   +inputState: ?InputState,
+  +navigateToSidebar: () => void,
 };
 class ComposedMessage extends React.PureComponent<Props> {
   render() {
@@ -51,6 +53,7 @@ class ComposedMessage extends React.PureComponent<Props> {
       composedMessageMaxWidth,
       colors,
       inputState,
+      navigateToSidebar,
       ...viewProps
     } = this.props;
     const { id, creator } = item.messageInfo;
@@ -94,7 +97,8 @@ class ComposedMessage extends React.PureComponent<Props> {
       messageBox = (
         <View style={styles.messageBox}>
           <SwipeableMessage
-            onSwipeableWillOpen={this.reply}
+            triggerReply={this.reply}
+            triggerSidebar={navigateToSidebar}
             isViewer={isViewer}
             messageBoxStyle={messageBoxStyle}
             threadColor={item.threadInfo.color}
@@ -181,12 +185,14 @@ const ConnectedComposedMessage: React.ComponentType<BaseProps> = React.memo<Base
     const composedMessageMaxWidth = useComposedMessageMaxWidth();
     const colors = useColors();
     const inputState = React.useContext(InputStateContext);
+    const navigateToSidebar = useNavigateToSidebar(props.item);
     return (
       <ComposedMessage
         {...props}
         composedMessageMaxWidth={composedMessageMaxWidth}
         colors={colors}
         inputState={inputState}
+        navigateToSidebar={navigateToSidebar}
       />
     );
   },

native/chat/swipeable-message.react.js
@@ -139,14 +139,15 @@ function SwipeSnake<IconGlyphs: string>(
 }
 
 type Props = {
-  +onSwipeableWillOpen: () => void,
+  +triggerReply: () => void,
+  +triggerSidebar: () => void,
   +isViewer: boolean,
   +messageBoxStyle: ViewStyle,
   +threadColor: string,
   +children: React.Node,
 };
 function SwipeableMessage(props: Props): React.Node {
-  const { isViewer, onSwipeableWillOpen } = props;
+  const { isViewer, triggerReply, triggerSidebar } = props;
   const onPassThreshold = React.useCallback(() => {
     if (Platform.OS === 'ios') {
       TapticFeedback.impact();
@@ -186,14 +187,16 @@ function SwipeableMessage(props: Props): React.Node {
       },
       onEnd: event => {
         const absValue = Math.abs(translateX.value);
-        if (absValue >= replyThreshold) {
-          runOnJS(onSwipeableWillOpen)();
+        if (absValue >= sidebarThreshold) {
+          runOnJS(triggerSidebar)();
+        } else if (absValue >= replyThreshold) {
+          runOnJS(triggerReply)();
         }
 
         translateX.value = withSpring(0, makeSpringConfig(event.velocityX));
       },
     },
-    [isViewer, onSwipeableWillOpen],
+    [isViewer, triggerReply, triggerSidebar],
   );
 
   const transformMessageBoxStyle = useAnimatedStyle(

commit 82b537a4b568bb53ce006e7df200dffe582c32e5

[native] Fix crash on log out if MessageList is open in Chat tab
Summary:
Issue described in [the Notion issue](https://www.notion.so/commapp/Crash-on-log-out-if-MessageList-is-open-in-Chat-tab-8117fbb3b5924c6899ad52a5c101458f):

> The `viewerID` `invariant` in `getSidebarThreadInfo` triggers as a result of being called by `useNavigateToSidebar` in `ConnectedComposedMessage`. Caused by D1806.

Test Plan: Tested log out while `MessageList` was open on my local environment

Reviewers: palys-swm

Reviewed By: palys-swm

Subscribers: Adrian, atul, karol-bisztyga

Differential Revision: https://phabricator.ashoat.com/D1903

native/chat/sidebar-navigation.js
@@ -23,13 +23,15 @@ import {
 function getSidebarThreadInfo(
   sourceMessage: ChatMessageInfoItemWithHeight,
   viewerID?: ?string,
-): ThreadInfo {
+): ?ThreadInfo {
   const threadCreatedFromMessage = sourceMessage.threadCreatedFromMessage;
   if (threadCreatedFromMessage) {
     return threadCreatedFromMessage;
   }
 
-  invariant(viewerID, 'viewerID should be set');
+  if (!viewerID) {
+    return null;
+  }
 
   const { messageInfo, threadInfo } = sourceMessage;
   return createPendingSidebar(
@@ -49,7 +51,9 @@ function navigateToSidebar<RouteName: MessageTooltipRouteNames>(
   viewerID: ?string,
   chatContext: ?ChatContextType,
 ) {
+  invariant(viewerID, 'viewerID should be set');
   const threadInfo = getSidebarThreadInfo(route.params.item, viewerID);
+  invariant(threadInfo, 'threadInfo should be set');
 
   chatContext?.setCurrentTransitionSidebarSourceID(
     route.params.item.messageInfo.id,
@@ -67,6 +71,7 @@ function useNavigateToSidebar(item: ChatMessageInfoItemWithHeight): () => void {
   ]);
   const navigateToThread = useNavigateToThread();
   return React.useCallback(() => {
+    invariant(threadInfo, 'threadInfo should be set');
     navigateToThread({ threadInfo });
   }, [navigateToThread, threadInfo]);
 }

native/chat/utils.js
@@ -109,7 +109,10 @@ function useMessageTargetParameters(
     state => state.currentUserInfo && state.currentUserInfo.id,
   );
   const sidebarThreadInfo = React.useMemo(() => {
-    return getSidebarThreadInfo(sourceMessage, viewerID);
+    invariant(viewerID, 'viewerID should be set');
+    const threadInfo = getSidebarThreadInfo(sourceMessage, viewerID);
+    invariant(threadInfo, 'threadInfo should be set');
+    return threadInfo;
   }, [sourceMessage, viewerID]);
 
   const messageListData = useMessageListData({
@@ -271,7 +274,6 @@ function getMessageTooltipKey(item: ChatMessageInfoItemWithHeight): string {
 }
 
 export {
-  getSidebarThreadInfo,
   chatMessageItemHeight,
   useAnimatedMessageTooltipButton,
   messageItemHeight,