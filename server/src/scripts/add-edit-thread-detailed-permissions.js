9afbfba4cc0812f7555e3ebbbf04fc9c2006323c
Jacek Nitychoruk <jacek.nitychoruk@swmansion.com>
2021-09-10 09:52:15 +0200

    [server] Add database migration with new thread edit permissions
    Summary: Added database migration script. It adds `thread_edit_color` and `thread_edit_description` permissions if thread is private or user has `thread_edit` permission.
    
    Test Plan: I run the script on database and new permissions were added succesfully and correctly.
    
    Reviewers: palys-swm, ashoat
    
    Reviewed By: palys-swm, ashoat
    
    Subscribers: ashoat, palys-swm, Adrian, atul, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D1979

---
+++ server/src/scripts/add-edit-thread-detailed-permissions.js
@@ -0,0 +1,51 @@
+// @flow
+import bots from 'lib/facts/bots.json';
+import { assertThreadType } from 'lib/types/thread-types';
+
+import { dbQuery, SQL } from '../database/database';
+import { createScriptViewer } from '../session/scripts';
+import { updateRoles } from '../updaters/role-updaters';
+import {
+  recalculateThreadPermissions,
+  commitMembershipChangeset,
+} from '../updaters/thread-permission-updaters';
+import RelationshipChangeset from '../utils/relationship-changeset';
+import { main } from './utils';
+
+async function addEditThreadDetailedPermissions() {
+  const batchSize = 10;
+
+  const fetchThreads = SQL`SELECT id, type FROM threads`;
+  const [result] = await dbQuery(fetchThreads);
+  const threads = result.map(row => {
+    return { id: row.id.toString(), type: assertThreadType(row.type) };
+  });
+
+  const viewer = createScriptViewer(bots.commbot.userID);
+
+  while (threads.length > 0) {
+    const batch = threads.splice(0, batchSize);
+    const membershipRows = [];
+    const relationshipChangeset = new RelationshipChangeset();
+    await Promise.all(
+      batch.map(async thread => {
+        console.log(`updating roles for ${thread.id}`);
+        await updateRoles(viewer, thread.id, thread.type);
+        console.log(`recalculating permissions for ${thread.id}`);
+        const {
+          membershipRows: threadMembershipRows,
+          relationshipChangeset: threadRelationshipChangeset,
+        } = await recalculateThreadPermissions(thread.id);
+        membershipRows.push(...threadMembershipRows);
+        relationshipChangeset.addAll(threadRelationshipChangeset);
+      }),
+    );
+    console.log(`committing batch ${JSON.stringify(batch)}`);
+    await commitMembershipChangeset(viewer, {
+      membershipRows,
+      relationshipChangeset,
+    });
+  }
+}
+
+main([addEditThreadDetailedPermissions]);
