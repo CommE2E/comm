df4e4a7362fb57c43babeeb8a974035019fe0e0d
karol-bisztyga <karol.bisztyga@swmansion.com>
2021-09-27 16:58:33 +0200

    [native] Automatically detect abis for Android
    Summary: This is my script for automating detection of active android devices' architectures. More info [here](https://www.notion.so/commapp/Detect-target-architecture-in-Android-debug-build-e2baf471598b45639e5b10c6c3f67ba1#ed6fa8753bf8438987652185dc113be9)
    
    Test Plan: Plug in one or two devices and see that it builds only for necessary architectures.
    
    Reviewers: palys-swm, ashoat, geekbrother
    
    Reviewed By: palys-swm, ashoat
    
    Subscribers: ashoat, palys-swm, Adrian, atul
    
    Differential Revision: https://phabricator.ashoat.com/D2010

---
+++ native/android/app/bash/detect_abis.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+IDS=$(adb devices -l | tail -n +2 | cut -d ' ' -f 1)
+ABIS=()
+
+for ID in ${IDS}
+do
+  ABI="$(adb -s $ID shell getprop ro.product.cpu.abi)"
+  # check if we already have this ABI
+  if [[ " ${ABIS[*]} " =~ " ${ABI} " ]]; then
+    continue
+  fi
+	ABIS+="${ABI} "
+done
+
+echo $ABIS
