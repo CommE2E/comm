fc95c84ed0e4d6a7f70dc96d8b5988cab7423d12
Atul Madhugiri <atulm@berkeley.edu>
2021-09-28 14:21:44 -0400

    [landing] Create `tile_grid` grid
    Summary: CSS grid for `AppLanding` component
    
    Test Plan: Here's what it looks like: https://blob.sh/atul/3a88.png
    
    Reviewers: ashoat, palys-swm
    
    Reviewed By: ashoat
    
    Subscribers: palys-swm, Adrian, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D2082

--- landing/app-landing.react.js
+++ landing/app-landing.react.js
@@ -13,6 +13,20 @@ function AppLanding(): React.Node {
       <div className={css.app_copy}>
         <h1>Comm Messenger</h1>
         <p className={css.mono}>Web3 Messenger. E2E encrypted. Blah..</p>
+        <div className={css.tile_grid}>
+          <div className={css.tile_tl}>
+            <p>E2E Encryption</p>
+          </div>
+          <div className={css.tile_tr}>
+            <p>Self-hosted</p>
+          </div>
+          <div className={css.tile_bl}>
+            <p>Federated</p>
+          </div>
+          <div className={css.tile_br}>
+            <p>Sovereign Identity</p>
+          </div>
+        </div>
       </div>
     </div>
   );
