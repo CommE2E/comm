b70d224e33b89831b6968f2d5a104e6d93d2f7aa
Atul Madhugiri <atulm@berkeley.edu>
2021-09-28 13:46:38 -0400

    [landing] Create `app_landing_grid` grid
    Summary: CSS grid for `AppLanding` component
    
    Test Plan: Here's what it looks like: https://blob.sh/atul/6aad.png
    
    Reviewers: ashoat, palys-swm
    
    Reviewed By: ashoat
    
    Subscribers: palys-swm, Adrian, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D2081

--- landing/app-landing.react.js
+++ landing/app-landing.react.js
@@ -2,16 +2,18 @@
 
 import * as React from 'react';
 
-import StarBackground from './star-background.react';
+import css from './landing.css';
 
 function AppLanding(): React.Node {
   return (
-    <div>
-      <StarBackground />
-      <center>
-        <h1>App Landing Page</h1>
-        <img height={300} src="images/comm-screenshot.png" />
-      </center>
+    <div className={css.app_landing_grid}>
+      <div className={css.app_preview}>
+        <img height={600} src="images/comm-screenshot.png" />
+      </div>
+      <div className={css.app_copy}>
+        <h1>Comm Messenger</h1>
+        <p className={css.mono}>Web3 Messenger. E2E encrypted. Blah..</p>
+      </div>
     </div>
   );
 }
