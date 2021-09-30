bb2795175938395091190623e0d57c83763200cb
karol-bisztyga <karol.bisztyga@swmansion.com>
2021-09-30 13:35:09 +0200

    [native] Fix grpc for building
    Summary: I missed adding `SortIncludes: false` in D2012 which results in build errors. Sorry that I broke master but this fix works
    
    Test Plan: build
    
    Reviewers: ashoat
    
    Reviewed By: ashoat
    
    Subscribers: ashoat, palys-swm, Adrian, atul
    
    Differential Revision: https://phabricator.ashoat.com/D2101

--- native/cpp/CommonCpp/grpc/_generated/tunnelbroker.pb.h
+++ native/cpp/CommonCpp/grpc/_generated/tunnelbroker.pb.h
@@ -19,18 +19,18 @@
 #error regenerate this file with a newer version of protoc.
 #endif
 
+#include <google/protobuf/port_undef.inc>
+#include <google/protobuf/io/coded_stream.h>
 #include <google/protobuf/arena.h>
 #include <google/protobuf/arenastring.h>
-#include <google/protobuf/extension_set.h>  // IWYU pragma: export
-#include <google/protobuf/generated_enum_reflection.h>
-#include <google/protobuf/generated_message_reflection.h>
 #include <google/protobuf/generated_message_table_driven.h>
 #include <google/protobuf/generated_message_util.h>
-#include <google/protobuf/io/coded_stream.h>
-#include <google/protobuf/message.h>
 #include <google/protobuf/metadata_lite.h>
-#include <google/protobuf/port_undef.inc>
+#include <google/protobuf/generated_message_reflection.h>
+#include <google/protobuf/message.h>
 #include <google/protobuf/repeated_field.h>  // IWYU pragma: export
+#include <google/protobuf/extension_set.h>  // IWYU pragma: export
+#include <google/protobuf/generated_enum_reflection.h>
 #include <google/protobuf/unknown_field_set.h>
 // @@protoc_insertion_point(includes)
 #include <google/protobuf/port_def.inc>
