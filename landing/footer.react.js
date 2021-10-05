6637363064ac9ca4a2776034b8d9a08c4a2709dc
Atul Madhugiri <atulm@berkeley.edu>
2021-10-05 18:22:17 -0400

    [landing] Move sitemap/subscription-form styles to `footer.css`
    Summary: na, refactor
    
    Test Plan: Continues to look as expected
    
    Reviewers: ashoat, palys-swm
    
    Reviewed By: ashoat, palys-swm
    
    Subscribers: palys-swm, Adrian, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D2137

--- landing/footer.react.js
+++ landing/footer.react.js
@@ -6,7 +6,6 @@ import * as React from 'react';
 import { Link } from 'react-router-dom';
 
 import footerStyles from './footer.css';
-import commonStyles from './landing.css';
 import type { LandingPageName } from './landing.react';
 import SubscriptionForm from './subscription-form.react';
 
@@ -16,18 +15,18 @@ type FooterProps = {
 function Footer(props: FooterProps): React.Node {
   const { activePageName } = props;
   return (
-    <div className={commonStyles.footer_blur}>
+    <div className={footerStyles.footer_blur}>
       <div className={footerStyles.footer_grid}>
-        <div className={commonStyles.sitemap}>
-          <div className={commonStyles.footer_logo}>
+        <div className={footerStyles.sitemap}>
+          <div className={footerStyles.footer_logo}>
             <Link to="/">Comm</Link>
           </div>
 
           <div
             className={
               activePageName === 'keyservers'
-                ? commonStyles.active_tab
-                : commonStyles.inactive_tab
+                ? footerStyles.active_tab
+                : footerStyles.inactive_tab
             }
           >
             <Link to="/keyservers">Keyservers</Link>
@@ -36,8 +35,8 @@ function Footer(props: FooterProps): React.Node {
           <div
             className={
               activePageName === 'support'
-                ? commonStyles.active_tab
-                : commonStyles.inactive_tab
+                ? footerStyles.active_tab
+                : footerStyles.inactive_tab
             }
           >
             <Link to="/support">Support</Link>
@@ -46,8 +45,8 @@ function Footer(props: FooterProps): React.Node {
           <div
             className={
               activePageName === 'terms'
-                ? commonStyles.active_tab
-                : commonStyles.inactive_tab
+                ? footerStyles.active_tab
+                : footerStyles.inactive_tab
             }
           >
             <Link to="/terms">Terms of Use</Link>
@@ -56,14 +55,14 @@ function Footer(props: FooterProps): React.Node {
           <div
             className={
               activePageName === 'privacy'
-                ? commonStyles.active_tab
-                : commonStyles.inactive_tab
+                ? footerStyles.active_tab
+                : footerStyles.inactive_tab
             }
           >
             <Link to="/privacy">Privacy Policy</Link>
           </div>
 
-          <div className={commonStyles.inactive_tab}>
+          <div className={footerStyles.inactive_tab}>
             <a href="https://www.notion.so/How-Comm-works-d6217941db7c4237b9d08b427aef3234">
               How Comm works{' '}
               <FontAwesomeIcon
@@ -74,7 +73,7 @@ function Footer(props: FooterProps): React.Node {
             </a>
           </div>
         </div>
-        <div className={commonStyles.subscribe_updates}>
+        <div className={footerStyles.subscribe_updates}>
           <SubscriptionForm />
         </div>
       </div>
