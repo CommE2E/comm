9c027265418fe91185f97d46fa24e4ad5e2df72b
Atul Madhugiri <atulm@berkeley.edu>
2021-10-07 12:47:53 -0400

    [landing] Replace lorem ipsum with working copy
    Summary:
    Not the final copy, but better to work with than lorem ipsum
    
    (the tile CSS is gross, that'll get cleaned up in later diff)
    
    Test Plan: Looks as expected
    
    Reviewers: ashoat, palys-swm
    
    Reviewed By: ashoat
    
    Subscribers: palys-swm, Adrian, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D2161

--- landing/app-landing.react.js
+++ landing/app-landing.react.js
@@ -35,48 +35,53 @@ function AppLanding(): React.Node {
           (think &quot;Web3 Discord&quot;)
         </p>
         <div className={css.tile_grid}>
-          <div className={css.tile_tl}>
+          <div className={css.tile_one}>
             <FontAwesomeIcon size="2x" color="#485563" icon={faLock} />
-            <p className={css.tile_title}>E2E Encryption</p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-              porta, nisl eu pharetra malesuada, libero purus placerat mauris,
-              sed ultricies neque enim sit amet metus. Morbi ac augue eget nunc
-              vehicula interdum auctor ut ligula. Nulla sed risus in lorem
-              posuere euismod. Ut et luctus leo.
-            </p>
-          </div>
-          <div className={css.tile_tr}>
-            <FontAwesomeIcon size="2x" color="#485563" icon={faServer} />
-            <p className={css.tile_title}>Self-hosted</p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-              porta, nisl eu pharetra malesuada, libero purus placerat mauris,
-              sed ultricies neque enim sit amet metus. Morbi ac augue eget nunc
-              vehicula interdum auctor ut ligula. Nulla sed risus in lorem
-              posuere euismod. Ut et luctus leo.
-            </p>
-          </div>
-          <div className={css.tile_bl}>
-            <FontAwesomeIcon size="2x" color="#485563" icon={faUsers} />
             <p className={css.tile_title}>Federated</p>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-              porta, nisl eu pharetra malesuada, libero purus placerat mauris,
-              sed ultricies neque enim sit amet metus. Morbi ac augue eget nunc
-              vehicula interdum auctor ut ligula. Nulla sed risus in lorem
-              posuere euismod. Ut et luctus leo.
+              Comm is a protocol paired with an app. Each community hosts its
+              own backend, which we call a keyserver. Our keyserver software is
+              built to be forked.
             </p>
           </div>
-          <div className={css.tile_br}>
-            <FontAwesomeIcon size="2x" color="#485563" icon={faUserShield} />
-            <p className={css.tile_title}>Sovereign Identity</p>
+          <div className={css.tile_two}>
+            <FontAwesomeIcon size="2x" color="#485563" icon={faServer} />
+            <p className={css.tile_title}>Customizable</p>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-              porta, nisl eu pharetra malesuada, libero purus placerat mauris,
-              sed ultricies neque enim sit amet metus. Morbi ac augue eget nunc
-              vehicula interdum auctor ut ligula. Nulla sed risus in lorem
-              posuere euismod. Ut et luctus leo.
+              Write mini-apps and custom modules in React. Skin your community.
+              Customize your tabs and your home page.
+            </p>
+          </div>
+          <div className={css.tile_three}>
+            <FontAwesomeIcon size="2x" color="#485563" icon={faUsers} />
+            <p className={css.tile_title}>E2E-encrypted</p>
+            <p>
+              Comm started as a project to build a private, decentralized
+              alternative to Discord. Privacy is in our DNA.
+            </p>
+          </div>
+          <div className={css.tile_four}>
+            <FontAwesomeIcon size="2x" color="#485563" icon={faUserShield} />
+            <p className={css.tile_title}>Sovereign</p>
+            <p>
+              Log in with your ETH wallet. Use ENS as your username. On Comm,
+              your identity and data are yours to control.
+            </p>
+          </div>
+          <div className={css.tile_five}>
+            <FontAwesomeIcon size="2x" color="#485563" icon={faUserShield} />
+            <p className={css.tile_title}>Open Source</p>
+            <p>
+              All of our code is open source. Keyservers, iOS/Android app, our
+              cloud services… all of it. We believe in open platforms.
+            </p>
+          </div>
+          <div className={css.tile_six}>
+            <FontAwesomeIcon size="2x" color="#485563" icon={faUserShield} />
+            <p className={css.tile_title}>Less Noisy</p>
+            <p>
+              We let each user decide what they want to follow with detailed
+              notif controls and a powerful unified inbox.
             </p>
           </div>
         </div>
