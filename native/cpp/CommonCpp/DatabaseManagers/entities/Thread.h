e8187fb53b459bb764dee4a65d4bea9ca171f021
Jacek Nitychoruk <jacek.nitychoruk@swmansion.com>
2021-10-08 12:59:36 +0200

    [CommCoreModule] Add `Thread.h` with c++ thread type
    Summary: I introduced Thread type representing ThreadInfo in C++ layer.
    
    Test Plan: The change doesn't modify app behavior.
    
    Reviewers: palys-swm, atul, karol-bisztyga, ashoat
    
    Reviewed By: palys-swm, atul, karol-bisztyga, ashoat
    
    Subscribers: ashoat, palys-swm, Adrian, atul, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D2076

---
+++ native/cpp/CommonCpp/DatabaseManagers/entities/Thread.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <memory>
+#include <string>
+
+namespace comm {
+
+struct Thread {
+  std::string id;
+  int type;
+  std::unique_ptr<std::string> name;
+  std::unique_ptr<std::string> description;
+  std::string color;
+  int64_t creation_time;
+  std::unique_ptr<std::string> parent_thread_id;
+  std::unique_ptr<std::string> containing_thread_id;
+  std::unique_ptr<std::string> community;
+  std::string members;
+  std::string roles;
+  std::string current_user;
+  std::unique_ptr<std::string> source_message_id;
+  int replies_count;
+};
+
+} // namespace comm
