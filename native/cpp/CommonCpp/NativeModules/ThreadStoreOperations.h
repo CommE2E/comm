0a1e8d206577205bda37bdd4d9685bb635a60a0a
Jacek Nitychoruk <jacek.nitychoruk@swmansion.com>
2021-10-08 13:03:37 +0200

    [CommCoreModule] Add `ThreadStoreOperationBase.h` with c++ thread operations types
    Summary: I introduced c++ `ThreadStoreOperation` classes, that executes proper functions on database.
    
    Test Plan: The change doesn't affect app behavior yet. I tested the classes after changes introduced in next diffs and they seems to work correctly on virtual iOS device.
    
    Reviewers: palys-swm, atul, karol-bisztyga, ashoat
    
    Reviewed By: palys-swm, ashoat
    
    Subscribers: geekbrother, ashoat, palys-swm, Adrian, atul, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D2078

---
+++ native/cpp/CommonCpp/NativeModules/ThreadStoreOperations.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "../DatabaseManagers/entities/Media.h"
+#include "../DatabaseManagers/entities/Thread.h"
+#include "DatabaseManager.h"
+#include <vector>
+
+namespace comm {
+class ThreadStoreOperationBase {
+public:
+  virtual void execute() = 0;
+};
+
+class RemoveThreadsOperation : public ThreadStoreOperationBase {
+public:
+  RemoveThreadsOperation(std::vector<std::string> ids) : ids{ids} {
+  }
+
+  virtual void execute() override {
+    DatabaseManager::getQueryExecutor().removeThreads(this->ids);
+  }
+
+private:
+  std::vector<std::string> ids;
+};
+
+class ReplaceThreadOperation : public ThreadStoreOperationBase {
+public:
+  ReplaceThreadOperation(Thread &&thread) : thread{std::move(thread)} {
+  }
+
+  virtual void execute() override {
+    DatabaseManager::getQueryExecutor().replaceThread(this->thread);
+  }
+
+private:
+  Thread thread;
+};
+
+class RemoveAllThreadsOperation : public ThreadStoreOperationBase {
+public:
+  virtual void execute() override {
+    DatabaseManager::getQueryExecutor().removeAllThreads();
+  }
+};
+
+} // namespace comm
