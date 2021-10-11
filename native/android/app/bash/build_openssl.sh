6750f94e56d43678173cc06c29640339f041ec92
Max Kalashnikov <geekmaks@gmail.com>
2021-10-11 10:39:41 +0300

    [native] SQLCipher and OpenSSL integration for Android and iOS
    Summary:
    This diff related to the https://www.notion.so/commapp/Transparent-data-encryption-for-SQLite-cdc4ab73e3584050bb6c82db1197d4b0
    
    **Dependencies:**
    
    SQLCipher amalgamation sources file and Podspec file provided by the [[ https://www.npmjs.com/package/@commapp/sqlcipher-amalgamation | @commapp/sqlcipher-amalgamation ]] NPM package.
    
    **The building process on Android:**
    
    We need to have a prebuilt OpenSSL library which is needed to be linked to the SQLCipher.
    OpenSSL Prebuilding process invoked by the Gradle task and then run the bash script to pass multiple variables and exports to the OpenSSL make process.
    We build a library for ABIs provided by the 'android.buildTypes.(release/debug).ndk.abiFilters' for the bundleRelease or debug build.
    Then SQLCipher sources are built by the Gradle's Cmake (CMakeLists) from the NPM package sources content.
    
    OpenSSL library-specific:
    Gradle task created in `native/android/app/build.gradle` that downloads OpenSSL version from the `gradle.properties`.
    We are passing `-DOPENSSL_VERSION` from `gradle.properties` to `CMakeLists.txt`, because we need a version of the library for linking to SQLCipher and maybe something else in the future.
    
    **The building process on iOS:**
    
    SQLCipher added as a Pod to the `Podfile` from the NPM package pointing to the podspec file in it.
    We are using //OpenSSL-Universal// pod as an SSL library dependency.
    SQLCipher amalgamation sources building by the XCode building process itself from the podspec file parameters, we don't need to add it elsewhere.
    
    Test Plan:
    **Android:**
    
    Spin up a new clean dev environment from the GitHub repo and run `yarn` to install the dependency.
    Built an android app using `npx react-native run-android` from the `native` directory.
    
    Result:
    
    -Successful dependency installation,
    -Libraries successfully built,
    -App is built and run on the android emulator.
    
    **iOS:**
    
    Install `yarn` packages in the `native` directory.
    Install Cocoapods dependency running `pod install` in the `native/ios` directory (not necessary because the yarn installs it from the parent directory).
    Built an app using `npx react-native run-ios` from the `native` directory or from the XCode project file in `native/ios`.
    
    Result:
    
    -Successful dependency installation,
    -Libraries successfully built,
    -App is built and run on the ios emulator.
    
    Reviewers: ashoat, palys-swm, karol-bisztyga
    
    Reviewed By: ashoat
    
    Subscribers: Adrian, atul, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D1982

---
+++ native/android/app/bash/build_openssl.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+# OpenSSL library building automation script for
+# the Gradle build process integration.
+#
+# Libraries will be installed into:
+# $OPENSSL_SUBMODULE_PATH/build/$ANDROID_ARCH_ABI
+set -e
+
+# Arguments:
+readonly OPENSSL_SUBMODULE_PATH=$1
+readonly HOST_TAG=$2         # darwin-x86_64 / linux-x86_64
+readonly ANDROID_ARCH_ABI=$3 # arm64-v8a / armeabi-v7a / x86 / x86_64
+readonly MIN_SDK_VERSION=$4
+readonly THREADS=$6
+export ANDROID_NDK_HOME=$5
+
+case "$ANDROID_ARCH_ABI" in
+"arm64-v8a")
+  TARGET_ARCH="aarch64-linux-android"
+  BUILD_ARCH="android-arm64"
+  ;;
+"armeabi-v7a")
+  TARGET_ARCH="armv7a-linux-androideabi"
+  BUILD_ARCH="android-arm"
+  ;;
+"x86")
+  TARGET_ARCH="i686-linux-android"
+  BUILD_ARCH="android-x86"
+  ;;
+"x86_64")
+  TARGET_ARCH="x86_64-linux-android"
+  BUILD_ARCH="android-x86_64"
+  ;;
+*)
+  echo "Unsupported architecture: $ANDROID_ARCH_ABI"
+  exit 1
+  ;;
+esac
+
+CONFIGURE_AND_BUILD() {
+  # Local arguments:
+  readonly TARGET_ARCH=$1
+  readonly BUILD_ARCH=$2
+  readonly BUILD_PREFIX=$3
+
+  echo "Building OpenSSL library for ${BUILD_PREFIX} (${TARGET_ARCH})"
+  cd $OPENSSL_SUBMODULE_PATH
+
+  # Compilation flags:
+  export CFLAGS="-Os -ffunction-sections -fdata-sections -fno-unwind-tables -fno-asynchronous-unwind-tables"
+  export LDFLAGS="-Wl,-s -Wl,-Bsymbolic -Wl,--gc-sections"
+  export TOOLCHAIN=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/$HOST_TAG
+  export TARGET_HOST=${TARGET_ARCH}
+  PATH=$TOOLCHAIN/bin:$PATH
+
+  ./config no-asm -Wl,--enable-new-dtags,-rpath,'$(LIBRPATH)'
+  ./Configure ${BUILD_ARCH} no-shared \
+    -D__ANDROID_API__=$MIN_SDK_VERSION \
+    --prefix=$OPENSSL_SUBMODULE_PATH/build/${BUILD_PREFIX}
+  make -j"$THREADS"
+  make install_sw
+  make clean
+}
+
+CONFIGURE_AND_BUILD $TARGET_ARCH $BUILD_ARCH $ANDROID_ARCH_ABI
