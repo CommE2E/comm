0422ad22c2f31c256317cb0043f72f7867b350fa
Atul Madhugiri <atulm@berkeley.edu>
2021-10-13 16:18:55 -0400

    [landing] Absolutely position `StarBackground` and slow animation
    Summary: NA
    
    Test Plan: Here's how it looks: https://watch.videodelivery.net/f983e2bc2d327065faa984e547b763d0
    
    Reviewers: ashoat, palys-swm
    
    Reviewed By: ashoat
    
    Subscribers: palys-swm, Adrian, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D2204

--- landing/star-background.react.js
+++ landing/star-background.react.js
@@ -6,7 +6,7 @@ import css from './star-background.css';
 
 function StarBackground(): React.Node {
   return (
-    <div>
+    <div className={css.star_container}>
       <div className={css.stars_small} />
       <div className={css.stars_medium} />
       <div className={css.stars_large} />
