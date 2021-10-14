672fe9843eee90a6ad0485e190460b4a7a1857b4
karol-bisztyga <karol.bisztyga@swmansion.com>
2021-10-14 10:36:23 +0200

    [services] Fix make -j
    Summary: I noticed there are some errors using `-j` flag.
    
    Test Plan: `cd services && yarn run-grpc-server`
    
    Reviewers: palys-swm, ashoat
    
    Reviewed By: palys-swm, ashoat
    
    Subscribers: ashoat, palys-swm, Adrian, atul
    
    Differential Revision: https://phabricator.ashoat.com/D2189

--- services/tunnelbroker/contents/server/build.sh
+++ services/tunnelbroker/contents/server/build.sh
@@ -2,11 +2,19 @@
 
 set -e
 
-echo "building the server..."
+NPROC=0
+
+NPROC=$(nproc 2> /dev/null || echo 1)
+if [[ $NPROC -eq 1 ]]
+then
+  NPROC=$(sysctl -n hw.physicalcpu 2> /dev/null || echo 1)
+fi
+
+echo "building the server (nproc=$NPROC)..."
 
 pushd cmake/build
 cmake ../..
-make -j 2> /dev/null || (echo "failed to make with -j option, falling back to slower compilation" && make)
+make -j $NPROC
 
 popd
 
