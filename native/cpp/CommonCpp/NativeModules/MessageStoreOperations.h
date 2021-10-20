d95de95ff4e0b94021d94553405b3fd05e9e920a
Atul Madhugiri <atulm@berkeley.edu>
2021-10-20 14:19:58 -0400

    [CommCoreModule] Use `this->` instead of trailing underscore for member variables
    Summary: In `MessageStoreOperations.h`, I was using the convention of suffixing member variables with a trailing underscore. However, the team decided to use `this->` as a convention instead.
    
    Test Plan: Builds
    
    Reviewers: ashoat, palys-swm
    
    Reviewed By: ashoat
    
    Subscribers: palys-swm, Adrian, karol-bisztyga, benschac, boristopalov
    
    Differential Revision: https://phabricator.ashoat.com/D2259

--- native/cpp/CommonCpp/NativeModules/MessageStoreOperations.h
+++ native/cpp/CommonCpp/NativeModules/MessageStoreOperations.h
@@ -13,61 +13,60 @@ public:
 
 class RemoveMessagesOperation : public MessageStoreOperationBase {
 public:
-  RemoveMessagesOperation(std::vector<std::string> ids) : ids_{ids} {
+  RemoveMessagesOperation(std::vector<std::string> ids) : ids{ids} {
   }
 
   virtual void execute() override {
-    DatabaseManager::getQueryExecutor().removeMessages(ids_);
+    DatabaseManager::getQueryExecutor().removeMessages(this->ids);
   }
 
 private:
-  const std::vector<std::string> ids_;
+  const std::vector<std::string> ids;
 };
 
 class RemoveMessagesForThreadsOperation : public MessageStoreOperationBase {
 public:
-  RemoveMessagesForThreadsOperation(std::vector<std::string> ids) : ids_{ids} {
+  RemoveMessagesForThreadsOperation(std::vector<std::string> ids) : ids{ids} {
   }
 
   virtual void execute() override {
-    DatabaseManager::getQueryExecutor().removeMessagesForThreads(ids_);
+    DatabaseManager::getQueryExecutor().removeMessagesForThreads(this->ids);
   }
 
 private:
-  const std::vector<std::string> ids_;
+  const std::vector<std::string> ids;
 };
 
 class ReplaceMessageOperation : public MessageStoreOperationBase {
 public:
   ReplaceMessageOperation(Message msg, std::vector<Media> media_vector)
-      : msg_{std::move(msg)}, media_vector_{std::move(media_vector)} {
+      : msg{std::move(msg)}, media_vector{std::move(media_vector)} {
   }
 
   virtual void execute() override {
-    for (const Media &media : media_vector_) {
+    for (const Media &media : this->media_vector) {
       DatabaseManager::getQueryExecutor().replaceMedia(media);
     }
-    DatabaseManager::getQueryExecutor().replaceMessage(msg_);
+    DatabaseManager::getQueryExecutor().replaceMessage(this->msg);
   }
 
 private:
-  const Message msg_;
-  const std::vector<Media> media_vector_;
+  const Message msg;
+  const std::vector<Media> media_vector;
 };
 
 class RekeyMessageOperation : public MessageStoreOperationBase {
 public:
-  RekeyMessageOperation(std::string from, std::string to)
-      : from_{from}, to_{to} {
+  RekeyMessageOperation(std::string from, std::string to) : from{from}, to{to} {
   }
 
   virtual void execute() override {
-    DatabaseManager::getQueryExecutor().rekeyMessage(from_, to_);
+    DatabaseManager::getQueryExecutor().rekeyMessage(this->from, this->to);
   }
 
 private:
-  const std::string from_;
-  const std::string to_;
+  const std::string from;
+  const std::string to;
 };
 
 class RemoveAllMessagesOperation : public MessageStoreOperationBase {
