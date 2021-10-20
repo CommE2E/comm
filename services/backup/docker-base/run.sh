86b8588f1180cbd1e72538733d238c649bface03
karol-bisztyga <karol.bisztyga@swmansion.com>
2021-10-20 10:01:55 +0200

    [services] Add backup base image
    Summary:
    The whole stack: D2207, D2208, D2209, D2210, D2211
    
    Adding a new docker image built on the top of the base image for tunnelbroker(as we need grpc).
    
    This image's going to install AWS SDK and prepare to use AWS functionalities
    
    Test Plan: -
    
    Reviewers: palys-swm, ashoat
    
    Reviewed By: palys-swm, ashoat
    
    Subscribers: ashoat, palys-swm, Adrian, atul
    
    Tags: #aws_init
    
    Differential Revision: https://phabricator.ashoat.com/D2207

---
+++ services/backup/docker-base/run.sh
@@ -0,0 +1,27 @@
+#/bin/bash
+
+set -e
+
+# this script should be run from the comm's root directory
+
+IMAGE_NAME="commapp/backup-base"
+VERSION="1.0"
+IMAGES_IDS=$(docker images -f "reference=$IMAGE_NAME" -q)
+NIMAGES=$(docker images -f "reference=$IMAGE_NAME" -q | wc -l | sed 's/ //g')
+
+if [[ $NIMAGES -gt 1 ]]
+then
+  echo "There's more than one image '$IMAGE_NAME'. Due to possible large sizes of images creating a new one will be aborted now. Please remove images manually and run this script again."
+  echo "You can run:"
+  echo "docker rmi $(echo $IMAGES_IDS)"
+  exit 1;
+elif [[ $NIMAGES -eq 1 ]]
+then
+  echo "An image $IMAGE_NAME already exists. A new one will not be created, the current one will be used."
+  echo "If you encounter errors, please remove this image manually and re-run this script."
+  echo "To remove this image, you can run:"
+  echo "docker rmi $(echo $IMAGES_IDS)"
+else
+  echo "Creating a new image $IMAGE_NAME"
+  docker build -t $IMAGE_NAME:$VERSION -f services/backup/docker-base/Dockerfile .
+fi
