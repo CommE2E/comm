da1a9b7f3dea9036af58d636dcd8dd17d83cf218
Tomasz Palys <tomasz.palys@swmansion.com>
2021-11-02 18:11:20 +0100

    [CommCoreModule] Introduce GlobalNetworkSingleton
    Summary: Create a global object that can be used to interact with network module. This object is hiding all the threading related logic.
    
    Test Plan: Tested with the next diffs.
    
    Reviewers: karol-bisztyga, ashoat
    
    Reviewed By: ashoat
    
    Subscribers: def-au1t, Adrian, atul, benschac, boristopalov
    
    Tags: #background-notifications
    
    Differential Revision: https://phabricator.ashoat.com/D2313

---
+++ native/cpp/CommonCpp/NativeModules/InternalModules/GlobalNetworkSingleton.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "../../Tools/WorkerThread.h"
+#include "NetworkModule.h"
+#include <functional>
+#include <memory>
+
+namespace comm {
+class GlobalNetworkSingleton {
+  std::unique_ptr<WorkerThread> thread;
+  static NetworkModule &networkModule();
+
+public:
+  static GlobalNetworkSingleton instance;
+  void scheduleOrRun(std::function<void(NetworkModule &)> &&task);
+  void enableMultithreading();
+};
+} // namespace comm
