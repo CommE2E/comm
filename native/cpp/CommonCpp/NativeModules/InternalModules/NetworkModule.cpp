05a8a4f00921cdf3baacbcc605bdbed3861bb8e8
Tomasz Palys <tomasz.palys@swmansion.com>
2021-11-02 17:55:34 +0100

    [CommCoreModule] Introduce NetworkModule
    Summary:
    Create a new module that will handle all the networking logic. This module will be called from a global singleton (the next diff).
    This whole stack is based on a discussion from a couple of diffs, from which D2285 is the most significant.
    
    Test Plan: After this diff - no changes. Tested with the next diffs.
    
    Reviewers: karol-bisztyga, ashoat
    
    Reviewed By: karol-bisztyga, ashoat
    
    Subscribers: ashoat, Adrian, atul, benschac, boristopalov
    
    Tags: #background-notifications
    
    Differential Revision: https://phabricator.ashoat.com/D2312

---
+++ native/cpp/CommonCpp/NativeModules/InternalModules/NetworkModule.cpp
@@ -0,0 +1,25 @@
+#include "NetworkModule.h"
+#include "Logger.h"
+
+namespace comm {
+void NetworkModule::initializeNetworkModule(
+    const std::string &userId,
+    const std::string &deviceToken,
+    const std::string &hostname) {
+  std::string host = (hostname.size() == 0) ? "localhost" : hostname;
+  // initialize network module
+  // this is going to differ depending on a device
+  // 10.0.2.2 for android emulator
+  // 192.168.x.x for a physical device etc
+  const std::shared_ptr<grpc::ChannelCredentials> credentials =
+      (host.substr(0, 5) == "https")
+      ? grpc::SslCredentials(grpc::SslCredentialsOptions())
+      : grpc::InsecureChannelCredentials();
+  this->networkClient.reset(
+      new network::Client(host, "50051", credentials, userId, deviceToken));
+}
+
+void NetworkModule::sendPong() {
+  Logger::log("Sending PONG");
+}
+} // namespace comm
---
+++ native/cpp/CommonCpp/NativeModules/InternalModules/NetworkModule.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "../../grpc/Client.h"
+#include <memory>
+#include <string>
+
+namespace comm {
+class NetworkModule {
+  std::unique_ptr<network::Client> networkClient;
+
+public:
+  void initializeNetworkModule(
+      const std::string &userId,
+      const std::string &deviceToken,
+      const std::string &hostname = "");
+  void sendPong();
+};
+} // namespace comm
