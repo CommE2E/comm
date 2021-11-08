747bd8dc5f1d5acda8fff155832a9ec5f35ad88d
Benjamin Schachter <benschac@gmail.com>
2021-11-08 12:29:35 -0500

    [native] [3/n] make API calls from default notification screen UI
    Summary: Adds API calls to redux so an end user can click a default notification setting and update values in the settings table via the backend. This change does NOT reflect on the front end, yet.
    
    Test Plan: Open default notifications in Profile tab, click on all, background or none, check your local MySQL data base and you should see the notification setting reflected.
    
    Reviewers: atul, palys-swm, ashoat
    
    Reviewed By: ashoat
    
    Subscribers: ashoat, Adrian, karol-bisztyga, boristopalov
    
    Differential Revision: https://phabricator.ashoat.com/D2336

--- native/profile/default-notifications-preferences.react.js
+++ native/profile/default-notifications-preferences.react.js
@@ -1,7 +1,23 @@
 // @flow
 
 import * as React from 'react';
-import { View, Text, ScrollView, Platform } from 'react-native';
+import { View, Text, ScrollView, Platform, Alert } from 'react-native';
+
+import {
+  setUserSettings,
+  setUserSettingsActionTypes,
+} from 'lib/actions/user-actions';
+import { registerFetchKey } from 'lib/reducers/loading-reducer';
+import {
+  type UpdateUserSettingsRequest,
+  type NotificationTypes,
+  userSettingsTypes,
+} from 'lib/types/account-types';
+import {
+  type DispatchActionPromise,
+  useServerCall,
+  useDispatchActionPromise,
+} from 'lib/utils/action-utils';
 
 import Action from '../components/action-row.react';
 import SWMansionIcon from '../components/swmansion-icon.react';
@@ -41,10 +57,53 @@ type BaseProps = {
 
 type Props = {
   ...BaseProps,
-  styles: typeof unboundStyles,
+  +styles: typeof unboundStyles,
+  +dispatchActionPromise: DispatchActionPromise,
+  +changeNotificationSettings: (
+    notificationSettingsRequest: UpdateUserSettingsRequest,
+  ) => Promise<void>,
 };
 
 class DefaultNotificationsPreferences extends React.PureComponent<Props> {
+  async updatedDefaultNotifications(data: NotificationTypes) {
+    const { changeNotificationSettings } = this.props;
+
+    try {
+      await changeNotificationSettings({
+        name: userSettingsTypes.DEFAULT_NOTIFICATIONS,
+        data,
+      });
+    } catch (e) {
+      Alert.alert(
+        'Unknown error',
+        'Uhh... try again?',
+        [{ text: 'OK', onPress: () => {} }],
+        { cancelable: false },
+      );
+    }
+  }
+
+  selectNotificationSetting = (data: NotificationTypes) => {
+    const { dispatchActionPromise } = this.props;
+
+    dispatchActionPromise(
+      setUserSettingsActionTypes,
+      this.updatedDefaultNotifications(data),
+    );
+  };
+
+  selectAllNotifications = () => {
+    this.selectNotificationSetting('all');
+  };
+
+  selectBackgroundNotifications = () => {
+    this.selectNotificationSetting('background');
+  };
+
+  selectNoneNotifications = () => {
+    this.selectNotificationSetting('none');
+  };
+
   render() {
     const { styles } = this.props;
     return (
@@ -54,9 +113,18 @@ class DefaultNotificationsPreferences extends React.PureComponent<Props> {
       >
         <Text style={styles.header}>NOTIFICATIONS</Text>
         <View style={styles.section}>
-          <NotificationRow content="All" onPress={() => {}} />
-          <NotificationRow content="Background" onPress={() => {}} />
-          <NotificationRow content="None" onPress={() => {}} />
+          <NotificationRow
+            content="All"
+            onPress={this.selectAllNotifications}
+          />
+          <NotificationRow
+            content="Background"
+            onPress={this.selectBackgroundNotifications}
+          />
+          <NotificationRow
+            content="None"
+            onPress={this.selectNoneNotifications}
+          />
         </View>
       </ScrollView>
     );
@@ -90,10 +158,23 @@ const unboundStyles = {
   },
 };
 
+registerFetchKey(setUserSettingsActionTypes);
 const ConnectedDefaultNotificationPreferences: React.ComponentType<BaseProps> = React.memo<BaseProps>(
   function ConnectedDefaultNotificationPreferences(props: BaseProps) {
     const styles = useStyles(unboundStyles);
-    return <DefaultNotificationsPreferences {...props} {...{ styles }} />;
+    const dispatchActionPromise = useDispatchActionPromise();
+    const changeNotificationSettings = useServerCall(setUserSettings);
+
+    return (
+      <DefaultNotificationsPreferences
+        {...props}
+        {...{
+          styles,
+          dispatchActionPromise,
+          changeNotificationSettings,
+        }}
+      />
+    );
   },
 );
 
