d6cf5cf68c8b75f97beda1c976adf967e9978fcf
Benjamin Schachter <benschac@gmail.com>
2021-11-27 16:00:07 -0500

    [2/n] [landing] remove active highlight image scrolling functionality

--- landing/app-landing.react.js
+++ landing/app-landing.react.js
@@ -47,12 +47,8 @@ const screenshots = [
 
 function AppLanding(): React.Node {
   usePreLoadAssets(screenshots);
-  const [activeCardIdx, setActiveCardIdx] = React.useState(-1);
-
-  const imageIdx =
-    activeCardIdx === -1
-      ? 1
-      : Math.min(Math.max(0, activeCardIdx), screenshots.length - 1);
+  const [firstScreenShot] = screenshots;
+  const path = `${LandingAssetsS3URL}/${firstScreenShot.file}`;
 
   return (
     <>
@@ -60,18 +56,9 @@ function AppLanding(): React.Node {
       <div className={css.app_landing_grid}>
         <div className={css.app_preview}>
           <picture>
-            <source
-              srcSet={`${LandingAssetsS3URL}/${screenshots[imageIdx].file}.webp`}
-              type="image/webp"
-            />
-            <source
-              srcSet={`${LandingAssetsS3URL}/${screenshots[imageIdx].file}.png`}
-              type="image/png"
-            />
-            <img
-              src={`${LandingAssetsS3URL}/${screenshots[imageIdx].file}.png`}
-              alt={screenshots[imageIdx].alt}
-            />
+            <source srcSet={`${path}.webp`} type="image/webp" />
+            <source srcSet={`${path}.png`} type="image/png" />
+            <img src={`${path}.png`} alt={firstScreenShot.alt} />
           </picture>
         </div>
         <div className={css.app_copy}>
@@ -84,70 +71,47 @@ function AppLanding(): React.Node {
 
           <div className={css.tile_grid}>
             <InfoCard
-              idx={0}
-              active={activeCardIdx === 0}
               label="Federated"
               icon={faUsers}
               description="Comm is a protocol paired with an app. Each community hosts its
                 own backend, which we call a keyserver. Our keyserver software
                 is built to be forked."
               baseStyle={css.tile_one}
-              setActiveCardIdx={setActiveCardIdx}
             />
-
             <InfoCard
-              idx={1}
-              active={activeCardIdx === 1}
               label="Customizable"
               icon={faTools}
               description="Write mini-apps and custom modules in React. Skin your
                 community. Customize your tabs and your home page."
               baseStyle={css.tile_two}
-              setActiveCardIdx={setActiveCardIdx}
             />
-
             <InfoCard
-              idx={2}
-              active={activeCardIdx === 2}
               label="E2E-encrypted"
               icon={faLock}
               description="Comm started as a project to build a private, decentralized
                 alternative to Discord. Privacy is in our DNA."
               baseStyle={css.tile_three}
-              setActiveCardIdx={setActiveCardIdx}
             />
-
             <InfoCard
-              idx={3}
-              active={activeCardIdx === 3}
               label="Sovereign"
               icon={faUserShield}
               description="Log in with your ETH wallet. Use ENS as your username. On Comm,
                 your identity and data are yours to control."
               baseStyle={css.tile_four}
-              setActiveCardIdx={setActiveCardIdx}
             />
-
             <InfoCard
-              idx={4}
-              active={activeCardIdx === 4}
               label="Open Source"
               icon={faCodeBranch}
               description="All of our code is open source. Keyservers, iOS/Android app, our
                 cloud services… all of it. We believe in open platforms."
               baseStyle={css.tile_five}
-              setActiveCardIdx={setActiveCardIdx}
             />
-
             <InfoCard
-              idx={5}
-              active={activeCardIdx === 5}
               label="Less Noisy"
               icon={faBellSlash}
               description="We let each user decide what they want to follow with detailed
                 notif controls and a powerful unified inbox."
               baseStyle={css.tile_six}
-              setActiveCardIdx={setActiveCardIdx}
             />
           </div>
         </div>
--- landing/info-card.react.js
+++ landing/info-card.react.js
@@ -2,73 +2,25 @@
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import * as React from 'react';
-import { Waypoint } from 'react-waypoint';
 
 import css from './landing.css';
 
 type InfoCardProps = {
-  +idx: number,
-  +active: boolean,
   +label: string,
   +icon: mixed,
   +description: string,
   +baseStyle: string,
-  +setActiveCardIdx: number => void,
 };
 function InfoCard(props: InfoCardProps): React.Node {
-  const {
-    idx,
-    active,
-    label,
-    icon,
-    description,
-    baseStyle,
-    setActiveCardIdx,
-  } = props;
-
-  const onTopPositionChange = React.useCallback(
-    obj => {
-      if (
-        obj.previousPosition === 'above' &&
-        obj.currentPosition === 'inside'
-      ) {
-        setActiveCardIdx(idx);
-      } else if (
-        obj.previousPosition === 'inside' &&
-        obj.currentPosition === 'above'
-      ) {
-        setActiveCardIdx(idx + 1);
-      }
-    },
-    [idx, setActiveCardIdx],
-  );
-
-  const onBottomPositionChange = React.useCallback(
-    obj => {
-      if (
-        obj.previousPosition === 'below' &&
-        obj.currentPosition === 'inside'
-      ) {
-        setActiveCardIdx(idx);
-      } else if (
-        obj.previousPosition === 'inside' &&
-        obj.currentPosition === 'below'
-      ) {
-        setActiveCardIdx(idx - 1);
-      }
-    },
-    [idx, setActiveCardIdx],
-  );
+  const { label, icon, description, baseStyle } = props;
 
   return (
-    <div className={active ? `${baseStyle} ${css.active_card}` : baseStyle}>
-      <Waypoint onPositionChange={onTopPositionChange} />
+    <div className={baseStyle}>
       <div className={css.tile_title_row}>
         <FontAwesomeIcon size="2x" color="#ffffff" icon={icon} />
         <p className={css.tile_title}>{label}</p>
       </div>
       <p>{description}</p>
-      <Waypoint onPositionChange={onBottomPositionChange} />
     </div>
   );
 }
