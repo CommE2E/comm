a783066259dc45924f381527527b4ebb8e2ee61f
Benjamin Schachter <benschac@gmail.com>
2021-11-28 12:57:43 -0500

    [10/n] [landing] remove font-awesome

--- landing/app-landing.react.js
+++ landing/app-landing.react.js
@@ -1,13 +1,5 @@
 // @flow
 
-import {
-  faLock,
-  faUserShield,
-  faUsers,
-  faCodeBranch,
-  faTools,
-  faBellSlash,
-} from '@fortawesome/free-solid-svg-icons';
 import * as React from 'react';
 
 import CyclingHeader from './cycling-header.react';
@@ -47,7 +39,6 @@ function AppLanding(): React.Node {
           <div className={css.tile_grid}>
             <InfoCard
               label="Federated"
-              icon={faUsers}
               description="Comm is a protocol paired with an app. Each community hosts its
                 own backend, which we call a keyserver. Our keyserver software
                 is built to be forked."
@@ -55,35 +46,30 @@ function AppLanding(): React.Node {
             />
             <InfoCard
               label="Customizable"
-              icon={faTools}
               description="Write mini-apps and custom modules in React. Skin your
                 community. Customize your tabs and your home page."
               baseStyle={css.tile_two}
             />
             <InfoCard
               label="E2E-encrypted"
-              icon={faLock}
               description="Comm started as a project to build a private, decentralized
                 alternative to Discord. Privacy is in our DNA."
               baseStyle={css.tile_three}
             />
             <InfoCard
               label="Sovereign"
-              icon={faUserShield}
               description="Log in with your ETH wallet. Use ENS as your username. On Comm,
                 your identity and data are yours to control."
               baseStyle={css.tile_four}
             />
             <InfoCard
               label="Open Source"
-              icon={faCodeBranch}
               description="All of our code is open source. Keyservers, iOS/Android app, our
                 cloud services… all of it. We believe in open platforms."
               baseStyle={css.tile_five}
             />
             <InfoCard
               label="Less Noisy"
-              icon={faBellSlash}
               description="We let each user decide what they want to follow with detailed
                 notif controls and a powerful unified inbox."
               baseStyle={css.tile_six}
--- landing/info-card.react.js
+++ landing/info-card.react.js
@@ -1,23 +1,20 @@
 // @flow
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import * as React from 'react';
 
 import css from './landing.css';
 
 type InfoCardProps = {
   +label: string,
-  +icon: mixed,
   +description: string,
   +baseStyle: string,
 };
 function InfoCard(props: InfoCardProps): React.Node {
-  const { label, icon, description, baseStyle } = props;
+  const { label, description, baseStyle } = props;
 
   return (
     <div className={baseStyle}>
       <div className={css.tile_title_row}>
-        <FontAwesomeIcon size="2x" color="#ffffff" icon={icon} />
         <p className={css.tile_title}>{label}</p>
       </div>
       <p>{description}</p>
