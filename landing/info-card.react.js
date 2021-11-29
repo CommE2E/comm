b518f5dcf2207bfa656197828efb230d7888f48d
Benjamin Schachter <benschac@gmail.com>
2021-11-29 09:33:39 -0500

    [19/n] [landing] space column content correctly

--- landing/info-card.react.js
+++ landing/info-card.react.js
@@ -2,6 +2,7 @@
 
 import * as React from 'react';
 
+import infoBlock from './info-block.css';
 import css from './landing.css';
 import Picture from './picture.react';
 
@@ -30,11 +31,14 @@ function InfoBlock(props: InfoBlockProps): React.Node {
       <div className={imageClassName}>
         <Picture path={path} alt={alt} />
       </div>
-      <div className={infoClassName}>
-        <div className={css.tile_title_row}>
-          <h2 className={css.tile_title}>{label}</h2>
+      <div className={`${infoClassName}`}>
+        <div className={infoBlock.infoBlock}>
+          <div className={css.tile_title_row}>
+            <h2 className={css.tile_title}>{label}</h2>
+          </div>
+          <div className={infoBlock.contentSpacer} />
+          <p>{description}</p>
         </div>
-        <p>{description}</p>
       </div>
     </>
   );
