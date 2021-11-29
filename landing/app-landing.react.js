11e3cf124669bd1a31bd19c32bd2ca2a238055eb
Benjamin Schachter <benschac@gmail.com>
2021-11-28 20:51:31 -0500

    [18/n] [landing] create InfoBlock component

--- landing/app-landing.react.js
+++ landing/app-landing.react.js
@@ -4,10 +4,9 @@ import * as React from 'react';
 
 // import CyclingHeader from './cycling-header.react';
 import { assetMeta } from './asset-meta';
-import InfoCard from './info-card.react';
+import InfoBlock from './info-card.react';
 import './reset.css';
 import css from './landing.css';
-import Picture from './picture.react';
 import StarBackground from './star-background.react';
 import usePreLoadAssets, {
   LandingAssetsS3URL,
@@ -47,60 +46,55 @@ function AppLanding(): React.Node {
               (think &quot;Web3 Discord&quot;)
             </p>
           </div> */}
-        <div className={`${css.oneThird} ${css.federatedImage}`}>
-          <Picture path={federated.path} alt={federated.alt} />
-        </div>
-        <div className={`${css.twoThird} ${css.federatedInfo}`}>
-          <InfoCard
-            label={federated.label}
-            description={federated.description}
-          />
-        </div>
-        <div className={`${css.twoThirdInverted} ${css.customizableImage}`}>
-          <Picture path={customizable.path} alt={customizable.alt} />
-        </div>
-        <div className={`${css.oneThirdInverted} ${css.customizableInfo}`}>
-          <InfoCard
-            label={customizable.label}
-            description={customizable.description}
-          />
-        </div>
-        <div className={`${css.oneThird} ${css.encryptedImage}`}>
-          <Picture path={encrypted.path} alt={encrypted.alt} />
-        </div>
-        <div className={`${css.twoThird} ${css.encryptedInfo}`}>
-          <InfoCard
-            label={encrypted.label}
-            description={encrypted.description}
-          />
-        </div>
-        <div className={`${css.twoThirdInverted} ${css.sovereignImage}`}>
-          <Picture path={sovereign.path} alt={sovereign.alt} />
-        </div>
-        <div className={`${css.oneThirdInverted} ${css.sovereignInfo}`}>
-          <InfoCard
-            label={sovereign.label}
-            description={sovereign.description}
-          />
-        </div>
-        <div className={`${css.oneThird} ${css.openSourceImage}`}>
-          <Picture path={openSource.path} alt={openSource.alt} />
-        </div>
-        <div className={`${css.twoThird} ${css.openSourceInfo}`}>
-          <InfoCard
-            label={openSource.label}
-            description={openSource.description}
-          />
-        </div>
-        <div className={`${css.twoThirdInverted} ${css.lessNoisyImage}`}>
-          <Picture path={lessNoisy.path} alt={lessNoisy.alt} />
-        </div>
-        <div className={`${css.oneThirdInverted} ${css.lessNoisyInfo}`}>
-          <InfoCard
-            label={lessNoisy.label}
-            description={lessNoisy.description}
-          />
-        </div>
+
+        <InfoBlock
+          path={federated.path}
+          alt={federated.alt}
+          label={federated.label}
+          description={federated.description}
+          imageClassName={`${css.oneThird} ${css.federatedImage}`}
+          infoClassName={`${css.twoThird} ${css.federatedInfo}`}
+        />
+        <InfoBlock
+          path={customizable.path}
+          alt={customizable.alt}
+          label={customizable.label}
+          description={customizable.description}
+          imageClassName={`${css.twoThirdInverted} ${css.customizableImage}`}
+          infoClassName={`${css.oneThirdInverted} ${css.customizableInfo}`}
+        />
+        <InfoBlock
+          path={encrypted.path}
+          alt={encrypted.alt}
+          label={encrypted.label}
+          description={encrypted.description}
+          imageClassName={`${css.oneThird} ${css.encryptedImage}`}
+          infoClassName={`${css.twoThird} ${css.encryptedInfo}`}
+        />
+        <InfoBlock
+          path={sovereign.path}
+          alt={sovereign.alt}
+          label={sovereign.label}
+          description={sovereign.description}
+          imageClassName={`${css.oneThirdInverted} ${css.sovereignImage}`}
+          infoClassName={`${css.twoThirdInverted} ${css.sovereignInfo}`}
+        />
+        <InfoBlock
+          path={openSource.path}
+          alt={openSource.alt}
+          label={openSource.label}
+          description={openSource.description}
+          imageClassName={`${css.oneThird} ${css.openSourceImage}`}
+          infoClassName={`${css.twoThird} ${css.openSourceInfo}`}
+        />
+        <InfoBlock
+          path={lessNoisy.path}
+          alt={lessNoisy.alt}
+          label={lessNoisy.label}
+          description={lessNoisy.description}
+          imageClassName={`${css.oneThirdInverted} ${css.lessNoisyImage}`}
+          infoClassName={`${css.twoThirdInverted} ${css.lessNoisyInfo}`}
+        />
       </div>
     </>
   );
--- landing/info-card.react.js
+++ landing/info-card.react.js
@@ -3,22 +3,41 @@
 import * as React from 'react';
 
 import css from './landing.css';
+import Picture from './picture.react';
 
-type InfoCardProps = {
+type InfoBlockProps = {
   +label: string,
   +description: string,
+  +label: string,
+  +path: string,
+  +alt: string,
+  +imageClassName: string,
+  +infoClassName: string,
 };
-function InfoCard(props: InfoCardProps): React.Node {
-  const { label, description } = props;
+
+function InfoBlock(props: InfoBlockProps): React.Node {
+  const {
+    label,
+    description,
+    path,
+    alt,
+    imageClassName,
+    infoClassName,
+  } = props;
 
   return (
-    <div>
-      <div className={css.tile_title_row}>
-        <h2 className={css.tile_title}>{label}</h2>
+    <>
+      <div className={imageClassName}>
+        <Picture path={path} alt={alt} />
       </div>
-      <p>{description}</p>
-    </div>
+      <div className={infoClassName}>
+        <div className={css.tile_title_row}>
+          <h2 className={css.tile_title}>{label}</h2>
+        </div>
+        <p>{description}</p>
+      </div>
+    </>
   );
 }
 
-export default InfoCard;
+export default InfoBlock;
