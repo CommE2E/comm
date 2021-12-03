5bd2821d51e376b912bc13152e523bb8a1002e87
Benjamin Schachter <benschac@gmail.com>
2021-12-03 13:37:02 -0500

    [1/n] remove active_card styling functionality on scroll
    Summary: scroll page, active card should not light up anymore
    
    Test Plan: scroll page, active card should not light up anymore
    
    Reviewers: atul, ashoat
    
    Reviewed By: atul, ashoat
    
    Subscribers: ashoat, palys-swm, Adrian, karol-bisztyga, boristopalov
    
    Differential Revision: https://phabricator.ashoat.com/D2551

--- landing/app-landing.react.js
+++ landing/app-landing.react.js
@@ -45,8 +45,6 @@ const screenshots = [
 const LandingAssetsS3URL = 'https://dh9fld3hutpxf.cloudfront.net';
 
 function AppLanding(): React.Node {
-  const [activeCardIdx, setActiveCardIdx] = React.useState(-1);
-
   React.useEffect(() => {
     const testWEBP = 'UklGRiIAAABXRUJQVlA4IBYAAAAwAQCdASoBAAEADsD+JaQAA3AAAAAA';
     const testImg = new Image();
@@ -70,11 +68,6 @@ function AppLanding(): React.Node {
     testImg.src = `data:image/webp;base64,${testWEBP}`;
   }, []);
 
-  const imageIdx =
-    activeCardIdx === -1
-      ? 1
-      : Math.min(Math.max(0, activeCardIdx), screenshots.length - 1);
-
   return (
     <>
       <StarBackground />
@@ -82,16 +75,16 @@ function AppLanding(): React.Node {
         <div className={css.app_preview}>
           <picture>
             <source
-              srcSet={`${LandingAssetsS3URL}/${screenshots[imageIdx].file}.webp`}
+              srcSet={`${LandingAssetsS3URL}/${screenshots[0].file}.webp`}
               type="image/webp"
             />
             <source
-              srcSet={`${LandingAssetsS3URL}/${screenshots[imageIdx].file}.png`}
+              srcSet={`${LandingAssetsS3URL}/${screenshots[0].file}.png`}
               type="image/png"
             />
             <img
-              src={`${LandingAssetsS3URL}/${screenshots[imageIdx].file}.png`}
-              alt={screenshots[imageIdx].alt}
+              src={`${LandingAssetsS3URL}/${screenshots[0].file}.png`}
+              alt={screenshots[0].alt}
             />
           </picture>
         </div>
@@ -105,70 +98,52 @@ function AppLanding(): React.Node {
 
           <div className={css.tile_grid}>
             <InfoCard
-              idx={0}
-              active={activeCardIdx === 0}
               label="Federated"
               icon={faUsers}
               description="Comm is a protocol paired with an app. Each community hosts its
                 own backend, which we call a keyserver. Our keyserver software
                 is built to be forked."
               baseStyle={css.tile_one}
-              setActiveCardIdx={setActiveCardIdx}
             />
 
             <InfoCard
-              idx={1}
-              active={activeCardIdx === 1}
               label="Customizable"
               icon={faTools}
               description="Write mini-apps and custom modules in React. Skin your
                 community. Customize your tabs and your home page."
               baseStyle={css.tile_two}
-              setActiveCardIdx={setActiveCardIdx}
             />
 
             <InfoCard
-              idx={2}
-              active={activeCardIdx === 2}
               label="E2E-encrypted"
               icon={faLock}
               description="Comm started as a project to build a private, decentralized
                 alternative to Discord. Privacy is in our DNA."
               baseStyle={css.tile_three}
-              setActiveCardIdx={setActiveCardIdx}
             />
 
             <InfoCard
-              idx={3}
-              active={activeCardIdx === 3}
               label="Sovereign"
               icon={faUserShield}
               description="Log in with your ETH wallet. Use ENS as your username. On Comm,
                 your identity and data are yours to control."
               baseStyle={css.tile_four}
-              setActiveCardIdx={setActiveCardIdx}
             />
 
             <InfoCard
-              idx={4}
-              active={activeCardIdx === 4}
               label="Open Source"
               icon={faCodeBranch}
               description="All of our code is open source. Keyservers, iOS/Android app, our
                 cloud services… all of it. We believe in open platforms."
               baseStyle={css.tile_five}
-              setActiveCardIdx={setActiveCardIdx}
             />
 
             <InfoCard
-              idx={5}
-              active={activeCardIdx === 5}
               label="Less Noisy"
               icon={faBellSlash}
               description="We let each user decide what they want to follow with detailed
                 notif controls and a powerful unified inbox."
               baseStyle={css.tile_six}
-              setActiveCardIdx={setActiveCardIdx}
             />
           </div>
         </div>
