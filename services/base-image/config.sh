70a48e8c48a0d52b89dc6ab46dd9a63d1aeb2675
karol-bisztyga <karol.bisztyga@swmansion.com>
2021-12-03 10:40:31 +0100

    [3/n] [services] Use docker compose - mutual part
    Summary:
    In response to https://linear.app/comm/issue/ENG-156/investigate-docker-compose
    I split the solution into three parts because there were too many changes, this is the mutual part.
    
    Test Plan: You can run scripts from `services/package.json` and see if they work.
    
    Reviewers: palys-swm, def-au1t, geekbrother, ashoat
    
    Reviewed By: palys-swm, ashoat
    
    Subscribers: ashoat, palys-swm, Adrian, atul, benschac, boristopalov
    
    Tags: #services_docker_compose
    
    Differential Revision: https://phabricator.ashoat.com/D2417

--- services/base-image/config.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-
-set -e
-
-IMAGE_NAME="commapp/services-base"
-VERSION="1.0"
--- services/scripts/run_server_image.sh
+++ services/scripts/run_server_image.sh
@@ -1,65 +1,36 @@
-#/bin/bash
+#!/bin/bash
 
 set -e
 
-# this script should be run from the comm's root directory
-
 if [ "$#" -lt 1 ] || [ "$#" -gt 2 ]; then
   echo "Illegal number of parameters, expected:"
-  echo "- one argument with a path to a directory containing Dockerfile and a config file"
-  echo "- one optional argument `port`"
-  echo "- example: `./services/scripts/run_server_image.sh ./services/tunnelbroker/docker-server 12345`"
+  echo "- one argument with a name of the service, currently available services:"
+  ls | grep -vE 'base-image|docker.compose.yml|package.json|scripts'
+  echo "- one optional argument with port"
+  echo "- example: ./services/scripts/run_server_image.sh tunnelbroker 12345"
   exit 1;
 fi
 
-PORT="50051"
-if [ ! -z "$2" ]; then
-  PORT=$2
-fi
+. ./scripts/port_config.sh
 
-re='^[0-9]+$'
-if [[ ! "$PORT" =~ $re ]]; then
-  echo "given port value [$PORT] is not a number, aborting"
-  exit 1;
-fi
-
-GIVEN_PATH=$1
-CONFIG_PATH=$GIVEN_PATH/config.sh
-DOCKERFILE_PATH=$GIVEN_PATH/Dockerfile
-
-if [ ! -f $CONFIG_PATH ]; then
-  echo "Config file not found in $GIVEN_PATH, it has to be named \`config.sh\`"
-  exit 1;
-fi
-
-if [ ! -f $DOCKERFILE_PATH ]; then
-  echo "Dockerfile not found in $GIVEN_PATH"
-  exit 1;
-fi
-
-echo "reading configuration from $GIVEN_PATH"
-source $CONFIG_PATH
-
-CONTAINERS_IDS=$(docker ps -aq -f ancestor=$IMAGE_NAME:$VERSION)
-NCONTAINERS=$(docker ps -aq -f ancestor=$IMAGE_NAME:$VERSION | wc -l | sed 's/ //g')
-
-if [[ $NCONTAINERS -gt 1 ]]
-then
-  echo "There's more than one container of '$IMAGE_NAME' image. Due to possible large sizes of containers creating a new one will be aborted now. Please remove containers manually and run this script again."
-  echo "You can run:"
-  echo "docker stop $(echo $CONTAINERS_IDS) && docker rm $(echo $CONTAINERS_IDS)"
-  exit 0;
-elif [[ $NCONTAINERS -eq 1 ]]
-then
-  echo "A container of image $IMAGE_NAME already exists(container id: $CONTAINERS_IDS)."
-  echo "Overwriting an existing image"
-  docker stop $(echo $CONTAINERS_IDS) && docker rm $(echo $CONTAINERS_IDS)
-  docker rmi $IMAGE_NAME:$VERSION
-
-  docker build -t $IMAGE_NAME:$VERSION -f $DOCKERFILE_PATH .
-  docker run -it -p $PORT:50051 $EXTRA_ARGS $IMAGE_NAME:$VERSION
+SERVICE=$1
+PORT=0
+if [ "$1" == "tunnelbroker" ]; then
+  if [ ! -z "$2" ]; then
+    export COMM_SERVICES_PORT_TUNNELBROKER=$2
+  fi
+  PORT=$COMM_SERVICES_PORT_TUNNELBROKER
+elif [ "$1" == "backup" ]; then
+  if [ ! -z "$2" ]; then
+    export COMM_SERVICES_PORT_BACKUP=$2
+  fi
+  PORT=$COMM_SERVICES_PORT_BACKUP
 else
-  echo "Creating a new container for image $IMAGE_NAME"
-  docker build -t $IMAGE_NAME:$VERSION -f $DOCKERFILE_PATH .
-  docker run -it -p $PORT:50051 $EXTRA_ARGS $IMAGE_NAME:$VERSION
+  echo "No such service ${SERVICE}, aborting"
+  exit 1
 fi
+
+echo "${SERVICE} service will run at port ${PORT}"
+
+docker-compose build $SERVICE-server
+docker-compose up $SERVICE-server
