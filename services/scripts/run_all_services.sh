5108b7b1a24f34b961468d5eb815e6ff4eec1e1b
karol-bisztyga <karol.bisztyga@swmansion.com>
2021-12-03 10:40:55 +0100

    [4/n] [services] Run all services with one command
    Summary: In response to https://linear.app/comm/issue/ENG-206/run-all-services-with-one-command
    
    Test Plan:
    `cd services && yarn run-all-services`
    This should rebuild and run all services on different ports.
    
    Reviewers: palys-swm, geekbrother, def-au1t, ashoat
    
    Reviewed By: palys-swm, ashoat
    
    Subscribers: ashoat, palys-swm, Adrian, atul, benschac, boristopalov
    
    Tags: #services_docker_compose
    
    Differential Revision: https://phabricator.ashoat.com/D2503

---
+++ services/scripts/run_all_services.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+. ./scripts/port_config.sh
+
+echo "tunnelbroker service will run at port ${COMM_SERVICES_PORT_TUNNELBROKER}"
+echo "backup service will run at port ${COMM_SERVICES_PORT_BACKUP}"
+
+
+docker-compose build tunnelbroker-server
+docker-compose build backup-server
+
+docker-compose up
--- services/tunnelbroker/docker-server/contents/server/src/server.cpp
+++ services/tunnelbroker/docker-server/contents/server/src/server.cpp
@@ -22,7 +22,7 @@ void RunServer() {
   builder.RegisterService(&service);
   // Finally assemble the server.
   std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
-  std::cout << "Server listening on " << server_address << std::endl;
+  std::cout << "Server listening" << std::endl;
 
   // Wait for the server to shutdown. Note that some other thread must be
   // responsible for shutting down the server for this call to ever return.
