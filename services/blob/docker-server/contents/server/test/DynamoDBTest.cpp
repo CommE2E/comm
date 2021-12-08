37ea62b1879339f721da6849d6492ff00331f68a
karol-bisztyga <karol.bisztyga@swmansion.com>
2021-12-08 15:49:26 +0100

    add formatting for tests

--- services/blob/docker-server/contents/server/test/DynamoDBTest.cpp
+++ services/blob/docker-server/contents/server/test/DynamoDBTest.cpp
@@ -1,35 +1,30 @@
-#include<gtest/gtest.h>
+#include <gtest/gtest.h>
 
 #include "DatabaseManager.h"
 
 #include <iostream>
 
-#include <string>
-#include <random>
 #include <memory>
+#include <random>
+#include <string>
 
 using namespace comm::network;
 
-class DatabaseManagerTest : public testing::Test
-{
+class DatabaseManagerTest : public testing::Test {
 protected:
   std::unique_ptr<DatabaseManager> dbm;
 
-  virtual void SetUp()
-  {
+  virtual void SetUp() {
     Aws::InitAPI({});
     if (dbm == nullptr) {
-      dbm = std::make_unique<DatabaseManager>("blob-service-blob", "blob-service-reverse-index");
+      dbm = std::make_unique<DatabaseManager>("blob-service-blob",
+                                              "blob-service-reverse-index");
     }
   }
 
-  virtual void TearDown()
-  {
-    Aws::ShutdownAPI({});
-  }
+  virtual void TearDown() { Aws::ShutdownAPI({}); }
 
-  int randomNumber(const int from, const int to)
-  {
+  int randomNumber(const int from, const int to) {
     std::random_device rd;
     std::mt19937 mt(rd());
     std::uniform_int_distribution<int> dist(from, to);
@@ -37,14 +32,13 @@ protected:
     return dist(mt);
   }
 
-  std::string randomString(size_t size = 20)
-  {
-    std::string str("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
+  std::string randomString(size_t size = 20) {
+    std::string str(
+        "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
 
     std::string result;
 
-    for (size_t i = 0; i < size; ++i)
-    {
+    for (size_t i = 0; i < size; ++i) {
       result += str[randomNumber(0, str.size() - 1)];
     }
 
@@ -60,35 +54,38 @@ protected:
   }
 };
 
-TEST_F(DatabaseManagerTest, TestOperationsOnBlobItems)
-{
+TEST_F(DatabaseManagerTest, TestOperationsOnBlobItems) {
   BlobItem item = generateBlobItem();
 
   std::cout << "==> create db manager" << std::endl;
   std::cout << "==> put item" << std::endl;
   dbm->putBlobItem(item);
   std::cout << "==> find item" << std::endl;
-  std::shared_ptr<BlobItem> foundItem = std::dynamic_pointer_cast<BlobItem>(dbm->findBlobItem(item.hash));
+  std::shared_ptr<BlobItem> foundItem =
+      std::dynamic_pointer_cast<BlobItem>(dbm->findBlobItem(item.hash));
   std::cout << "==> checking hashes" << std::endl;
   EXPECT_EQ(item.hash.size(), foundItem->hash.size());
-  EXPECT_EQ(memcmp(item.hash.data(), foundItem->hash.data(), item.hash.size()), 0);
+  EXPECT_EQ(memcmp(item.hash.data(), foundItem->hash.data(), item.hash.size()),
+            0);
   std::cout << "==> remove item" << std::endl;
   dbm->removeBlobItem(foundItem->hash);
   std::cout << "==> done" << std::endl;
 }
 
-TEST_F(DatabaseManagerTest, TestOperationsOnReverseIndexItems)
-{
+TEST_F(DatabaseManagerTest, TestOperationsOnReverseIndexItems) {
   ReverseIndexItem item = generateReverseIndexItem();
 
   std::cout << "==> create db manager" << std::endl;
   std::cout << "==> put item" << std::endl;
   dbm->putReverseIndexItem(item);
   std::cout << "==> find item" << std::endl;
-  std::shared_ptr<ReverseIndexItem> foundItem = std::dynamic_pointer_cast<ReverseIndexItem>(dbm->findReverseIndexItem(item.hash));
+  std::shared_ptr<ReverseIndexItem> foundItem =
+      std::dynamic_pointer_cast<ReverseIndexItem>(
+          dbm->findReverseIndexItem(item.hash));
   std::cout << "==> checking hashes" << std::endl;
   EXPECT_EQ(item.hash.size(), foundItem->hash.size());
-  EXPECT_EQ(memcmp(item.hash.data(), foundItem->hash.data(), item.hash.size()), 0);
+  EXPECT_EQ(memcmp(item.hash.data(), foundItem->hash.data(), item.hash.size()),
+            0);
   std::cout << "==> remove item" << std::endl;
   dbm->removeReverseIndexItem(foundItem->hash);
   std::cout << "==> done" << std::endl;
