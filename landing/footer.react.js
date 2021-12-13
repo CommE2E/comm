2f84c6f515b2e4f26aaa100e392778f95d32e741
Benjamin Schachter <benschac@gmail.com>
2021-12-13 14:45:59 -0500

    [43/n] [landing] put footer on 12 column grid / re-implement
    Summary: style footer, it should match figma
    
    Test Plan: Test footer on desktop browsers and mobile browsers
    
    Reviewers: atul, ashoat
    
    Reviewed By: atul, ashoat
    
    Subscribers: ashoat, palys-swm, Adrian, karol-bisztyga, boristopalov
    
    Differential Revision: https://phabricator.ashoat.com/D2611

--- landing/footer.react.js
+++ landing/footer.react.js
@@ -5,6 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import * as React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import css from './footer.css';
 import SubscriptionForm from './subscription-form.react';
 
 const navLinkProps = {
@@ -16,29 +17,36 @@ const navLinkProps = {
 
 function Footer(): React.Node {
   return (
-    <footer>
-      <NavLink to="/">Comm</NavLink>
-      <NavLink to="/keyservers" exact {...navLinkProps}>
-        Keyservers
-      </NavLink>
-      <NavLink to="/support" exact {...navLinkProps}>
-        Support
-      </NavLink>
-      <NavLink to="/terms" exact {...navLinkProps}>
-        Terms of Use
-      </NavLink>
-      <NavLink to="/privacy" exact {...navLinkProps}>
-        Privacy Policy
-      </NavLink>
-      <a href="https://www.notion.so/How-Comm-works-d6217941db7c4237b9d08b427aef3234">
-        How Comm works{' '}
-        <FontAwesomeIcon
-          size="sm"
-          color="#ffffff"
-          icon={faExternalLinkSquareAlt}
-        />
-      </a>
-      <SubscriptionForm />
+    <footer className={css.wrapper}>
+      <div className={css.navigation}>
+        <NavLink className={css.logo} to="/">
+          Comm
+        </NavLink>
+        <NavLink to="/keyservers" exact {...navLinkProps}>
+          Keyservers
+        </NavLink>
+        <NavLink to="/support" exact {...navLinkProps}>
+          Support
+        </NavLink>
+        <NavLink to="/terms" exact {...navLinkProps}>
+          Terms of Use
+        </NavLink>
+        <NavLink to="/privacy" exact {...navLinkProps}>
+          Privacy Policy
+        </NavLink>
+        <a href="https://www.notion.so/How-Comm-works-d6217941db7c4237b9d08b427aef3234">
+          How Comm works{' '}
+          <FontAwesomeIcon
+            size="sm"
+            color="#ffffff"
+            icon={faExternalLinkSquareAlt}
+          />
+        </a>
+      </div>
+
+      <div className={css.submissionForm}>
+        <SubscriptionForm />
+      </div>
     </footer>
   );
 }
