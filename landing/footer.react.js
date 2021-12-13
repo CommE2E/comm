fb86cde764275c78543201ffd44bd62a7e6886f0
Benjamin Schachter <benschac@gmail.com>
2021-12-13 10:42:21 -0500

    [41/n] [landing] remove styles and html in footer
    Summary: Remove footer styles
    
    Test Plan: page should still render, footer styles should be broken
    
    Reviewers: atul, ashoat
    
    Reviewed By: atul, ashoat
    
    Subscribers: ashoat, palys-swm, Adrian, karol-bisztyga, boristopalov
    
    Differential Revision: https://phabricator.ashoat.com/D2609

--- landing/footer.react.js
+++ landing/footer.react.js
@@ -5,11 +5,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import * as React from 'react';
 import { NavLink } from 'react-router-dom';
 
-import footerStyles from './footer.css';
 import SubscriptionForm from './subscription-form.react';
 
 const navLinkProps = {
-  className: footerStyles.inactive_tab,
   activeStyle: {
     color: 'white',
     fontWeight: '500',
@@ -17,55 +15,30 @@ const navLinkProps = {
 };
 
 function Footer(): React.Node {
-  const footerGridStyle = `${footerStyles.footer_grid}`;
-
   return (
-    <div className={footerStyles.footer_blur}>
-      <div className={footerGridStyle}>
-        <div className={footerStyles.sitemap}>
-          <div className={footerStyles.footer_logo}>
-            <NavLink to="/">Comm</NavLink>
-          </div>
-
-          <div>
-            <NavLink to="/keyservers" exact {...navLinkProps}>
-              Keyservers
-            </NavLink>
-          </div>
-
-          <div>
-            <NavLink to="/support" exact {...navLinkProps}>
-              Support
-            </NavLink>
-          </div>
-
-          <div>
-            <NavLink to="/terms" exact {...navLinkProps}>
-              Terms of Use
-            </NavLink>
-          </div>
-
-          <div>
-            <NavLink to="/privacy" exact {...navLinkProps}>
-              Privacy Policy
-            </NavLink>
-          </div>
-
-          <div className={footerStyles.inactive_tab}>
-            <a href="https://www.notion.so/How-Comm-works-d6217941db7c4237b9d08b427aef3234">
-              How Comm works{' '}
-              <FontAwesomeIcon
-                size="sm"
-                color="#ffffff"
-                icon={faExternalLinkSquareAlt}
-              />
-            </a>
-          </div>
-        </div>
-        <div className={footerStyles.subscribe_updates}>
-          <SubscriptionForm />
-        </div>
-      </div>
+    <div>
+      <NavLink to="/">Comm</NavLink>
+      <NavLink to="/keyservers" exact {...navLinkProps}>
+        Keyservers
+      </NavLink>
+      <NavLink to="/support" exact {...navLinkProps}>
+        Support
+      </NavLink>
+      <NavLink to="/terms" exact {...navLinkProps}>
+        Terms of Use
+      </NavLink>
+      <NavLink to="/privacy" exact {...navLinkProps}>
+        Privacy Policy
+      </NavLink>
+      <a href="https://www.notion.so/How-Comm-works-d6217941db7c4237b9d08b427aef3234">
+        How Comm works{' '}
+        <FontAwesomeIcon
+          size="sm"
+          color="#ffffff"
+          icon={faExternalLinkSquareAlt}
+        />
+      </a>
+      <SubscriptionForm />
     </div>
   );
 }
