12a2e02f81b1037788ca26b84a1673809e7c5257
karol-bisztyga <karol.bisztyga@swmansion.com>
2021-12-14 16:24:51 +0100

    [3/n][services] Tests - tunnelbroker
    Summary: the backup part for changes from D2574
    
    Test Plan: See D2574
    
    Reviewers: palys-swm, geekbrother, ashoat
    
    Reviewed By: palys-swm, ashoat
    
    Subscribers: ashoat, palys-swm, Adrian, atul, benschac, boristopalov
    
    Differential Revision: https://phabricator.ashoat.com/D2576

---
+++ services/tunnelbroker/docker-server/contents/server/test/TunnelBrokerTest.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+
+class TunnelBrokerTest : public testing::Test
+{
+protected:
+  virtual void SetUp() {
+    //...
+  }
+
+  virtual void TearDown() {
+    //...
+  }
+};
+
+TEST_F(TunnelBrokerTest, passingTest) {
+  EXPECT_TRUE(true);
+}
+
+TEST_F(TunnelBrokerTest, failingTest) {
+  // EXPECT_TRUE(false);
+}
