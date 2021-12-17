82b1e33d8409fc8316ff2bbd6199f2b0c2f52bc4
Benjamin Schachter <benschac@gmail.com>
2021-12-17 08:29:12 -0500

    [2/2] [landing] style hero cta button per figma
    Summary: style hero cta button per figma {F6057}
    
    Test Plan: view the app on a mobile device and desktop
    
    Reviewers: atul, ashoat
    
    Reviewed By: atul, ashoat
    
    Subscribers: ashoat, palys-swm, Adrian, karol-bisztyga, boristopalov
    
    Differential Revision: https://phabricator.ashoat.com/D2637

--- landing/hero-content.react.js
+++ landing/hero-content.react.js
@@ -16,20 +16,22 @@ function HeroContent(props: HeroContentProps): React.Node {
 
   return (
     <section className={hero.infoStyle}>
-      <h1 className={css.cycling_header}>
-        {'Comm is crypto-native\nchat for '}
-        <TextLoop
-          interval={1750}
-          springConfig={{ stiffness: 180, damping: 16 }}
-        >
-          <span className={css.cycling_content}>DAOs</span>
-          <span className={css.cycling_content}>venture funds</span>
-          <span className={css.cycling_content}>open source</span>
-          <span className={css.cycling_content}>gaming guilds</span>
-          <span className={css.cycling_content}>social clubs</span>
-        </TextLoop>
-      </h1>
-      <p className={css.sub_heading}>(think &quot;Web3 Discord&quot;)</p>
+      <div className={css.contentWrapper}>
+        <h1 className={css.cycling_header}>
+          {'Comm is crypto-native\nchat for '}
+          <TextLoop
+            interval={1750}
+            springConfig={{ stiffness: 180, damping: 16 }}
+          >
+            <span className={css.cycling_content}>DAOs</span>
+            <span className={css.cycling_content}>venture funds</span>
+            <span className={css.cycling_content}>open source</span>
+            <span className={css.cycling_content}>gaming guilds</span>
+            <span className={css.cycling_content}>social clubs</span>
+          </TextLoop>
+        </h1>
+        <p className={css.sub_heading}>(think &quot;Web3 Discord&quot;)</p>
+      </div>
       <button className={css.request_access} onClick={onRequestAccess}>
         Request Access
       </button>
