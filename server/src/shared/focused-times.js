5766f1a15eefb1a318eeb7c09e179af3a60e2956
Tomasz Palys <tomasz.palys@swmansion.com>
2021-12-27 13:39:42 +0100

    [1/5][server] Change expired focus timeout to 4 hours
    Summary:
    We will no longer update focused time after every ping, so the value might be a lot older and still should be considered current.
    More context here: https://linear.app/comm/issue/ENG-308/check-server-logs-for-query-improvements
    
    Test Plan: Modify cron frequency and verify that rows older than the previous timeout are not deleted.
    
    Reviewers: atul, ashoat
    
    Reviewed By: atul, ashoat
    
    Subscribers: benschac, ashoat, Adrian, karol-bisztyga, boristopalov
    
    Differential Revision: https://phabricator.ashoat.com/D2736

--- server/src/shared/focused-times.js
+++ server/src/shared/focused-times.js
@@ -9,11 +9,13 @@ function earliestFocusedTimeConsideredCurrent(): number {
   return Date.now() - pingFrequency - 1500;
 }
 
+const fourHoursInMillis = 4 * 60 * 60 * 1000;
+
 // If the time column for a given entry in the focused table has a time earlier
 // than this, then that entry is considered expired, and consequently we will
 // remove it from the table in the nightly cronjob.
 function earliestFocusedTimeConsideredExpired(): number {
-  return Date.now() - pingFrequency * 2 - 1500;
+  return Date.now() - fourHoursInMillis;
 }
 
 export {
