8ff8f35d2f39aee96cbf8019d119504595154ad3
karol-bisztyga <karol.bisztyga@swmansion.com>
2021-12-28 13:36:01 +0100

    [9/n] [services] Blob - multi part uploader
    Summary:
    Full stack:
    D2666 D2667 D2668 D2707 D2708 D2709 D2669 D2670 D2671 D2672 D2673 D2674 D2675 D2676 D2677 D2678 D2679 D2680 D2681 D2682 D2683 D2684 D2685 D2686 D2734 D2735
    
    This introduces the blob service
    
    This code is copied from the backup service. Once the blob service is done and the backup service gets refactored, we should decide which parts of the code should be shared so we do not repeat functionalities in the codebase.
    
    Test Plan: Please see 2686
    
    Reviewers: geekbrother, palys-swm, ashoat
    
    Reviewed By: geekbrother, palys-swm, ashoat
    
    Subscribers: benschac, ashoat, palys-swm, Adrian, atul, boristopalov
    
    Differential Revision: https://phabricator.ashoat.com/D2671

---
+++ services/blob/docker-server/contents/server/src/MultiPartUploader.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <aws/core/Aws.h>
+#include <aws/s3/S3Client.h>
+#include <aws/s3/model/CompleteMultipartUploadRequest.h>
+
+#include <memory>
+#include <string>
+
+namespace comm {
+namespace network {
+
+class MultiPartUploader {
+  std::shared_ptr<Aws::S3::S3Client> client;
+  const std::string bucketName;
+  const std::string objectName;
+
+  Aws::S3::Model::CompleteMultipartUploadRequest completeMultipartUploadRequest;
+  Aws::S3::Model::CompletedMultipartUpload completedMultipartUpload;
+  std::string uploadId;
+
+  size_t partNumber = 1;
+
+public:
+  MultiPartUploader(
+      std::shared_ptr<Aws::S3::S3Client> client,
+      const std::string bucketName,
+      const std::string objectName);
+  void addPart(const std::string &part);
+  void finishUpload();
+};
+
+} // namespace network
+} // namespace comm
