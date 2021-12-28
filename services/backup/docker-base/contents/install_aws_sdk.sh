19b111e6e2ea4a04f39325fe4c2ddd346aea94f4
karol-bisztyga <karol.bisztyga@swmansion.com>
2021-12-28 13:36:29 +0100

    [22/n] [DRAFT] [services] Blob - add dynamodb to backup base
    Summary:
    Full stack:
    D2666 D2667 D2668 D2707 D2708 D2709 D2669 D2670 D2671 D2672 D2673 D2674 D2675 D2676 D2677 D2678 D2679 D2680 D2681 D2682 D2683 D2684 D2685 D2686 D2734 D2735
    
    This introduces the blob service
    
    Test Plan: Please see 2686
    
    Reviewers: palys-swm, geekbrother, ashoat
    
    Reviewed By: geekbrother
    
    Subscribers: benschac, ashoat, palys-swm, Adrian, atul, boristopalov
    
    Differential Revision: https://phabricator.ashoat.com/D2684

--- services/backup/docker-base/contents/install_aws_sdk.sh
+++ services/backup/docker-base/contents/install_aws_sdk.sh
@@ -13,7 +13,7 @@ pushd /usr/lib
 git clone --recurse-submodules https://github.com/aws/aws-sdk-cpp
 mkdir aws_sdk_build
 pushd aws_sdk_build
-cmake ../aws-sdk-cpp/ -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=/usr/local/aws_sdk -DBUILD_ONLY="core;s3"
+cmake ../aws-sdk-cpp/ -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=/usr/local/aws_sdk -DBUILD_ONLY="core;s3;dynamodb"
 make
 make install
 
