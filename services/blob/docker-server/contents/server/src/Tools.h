5d711e025aec7aa625806fd79567d3a4dbbbc632
karol-bisztyga <karol.bisztyga@swmansion.com>
2021-12-28 13:36:14 +0100

    [15/n] [DRAFT] [services] Blob - Tools for GET
    Summary:
    Full stack:
    D2666 D2667 D2668 D2707 D2708 D2709 D2669 D2670 D2671 D2672 D2673 D2674 D2675 D2676 D2677 D2678 D2679 D2680 D2681 D2682 D2683 D2684 D2685 D2686 D2734 D2735
    
    This introduces the blob service
    
    Test Plan: Please see 2686
    
    Reviewers: geekbrother, palys-swm, ashoat!
    
    Reviewed By: geekbrother
    
    Subscribers: benschac, ashoat, palys-swm, Adrian, atul, boristopalov
    
    Differential Revision: https://phabricator.ashoat.com/D2677

--- services/blob/docker-server/contents/server/src/Tools.h
+++ services/blob/docker-server/contents/server/src/Tools.h
@@ -33,6 +33,9 @@ public:
   database::S3Path
   generateS3Path(const std::string &bucketName, const std::string &blobHash);
   std::string computeHashForFile(const database::S3Path &s3Path);
+  database::S3Path findS3Path(const std::string &holder);
+  database::S3Path
+  findS3Path(const database::ReverseIndexItem &reverseIndexItem);
   long long getCurrentTimestamp();
 };
 
