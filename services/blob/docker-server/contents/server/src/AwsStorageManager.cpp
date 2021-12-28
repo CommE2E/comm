24e4c4149f184b8e86dfbc65d2460aa4d95140bc
karol-bisztyga <karol.bisztyga@swmansion.com>
2021-12-28 13:36:05 +0100

    [11/n] [services] Blob - storage manager
    Summary:
    Full stack:
    D2666 D2667 D2668 D2707 D2708 D2709 D2669 D2670 D2671 D2672 D2673 D2674 D2675 D2676 D2677 D2678 D2679 D2680 D2681 D2682 D2683 D2684 D2685 D2686 D2734 D2735
    
    This introduces the blob service
    
    This code is copied from the backup service. Once the blob service is done and the backup service gets refactored, we should decide which parts of the code should be shared so we do not repeat functionalities in the codebase.
    
    Test Plan: Please see 2686
    
    Reviewers: geekbrother, palys-swm, ashoat
    
    Reviewed By: geekbrother, palys-swm, ashoat
    
    Subscribers: benschac, ashoat, palys-swm, Adrian, atul, boristopalov
    
    Differential Revision: https://phabricator.ashoat.com/D2673

---
+++ services/blob/docker-server/contents/server/src/AwsStorageManager.cpp
@@ -0,0 +1,35 @@
+#include "AwsStorageManager.h"
+#include "AwsTools.h"
+#include "Tools.h"
+
+#include <aws/s3/model/Bucket.h>
+
+namespace comm {
+namespace network {
+
+AwsStorageManager &AwsStorageManager::getInstance() {
+  static AwsStorageManager instance;
+  return instance;
+}
+
+AwsS3Bucket AwsStorageManager::getBucket(const std::string &bucketName) const {
+  return AwsS3Bucket(bucketName);
+}
+
+std::vector<std::string> AwsStorageManager::listBuckets() const {
+  Aws::S3::Model::ListBucketsOutcome outcome =
+      AwsObjectsFactory::getS3Client()->ListBuckets();
+  std::vector<std::string> result;
+  if (!outcome.IsSuccess()) {
+    throw std::runtime_error(outcome.GetError().GetMessage());
+  }
+  Aws::Vector<Aws::S3::Model::Bucket> buckets =
+      outcome.GetResult().GetBuckets();
+  for (Aws::S3::Model::Bucket &bucket : buckets) {
+    result.push_back(bucket.GetName());
+  }
+  return result;
+}
+
+} // namespace network
+} // namespace comm
