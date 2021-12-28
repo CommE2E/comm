86e20dfe579c7d1bd44855d3f6582ffe1bef8532
karol-bisztyga <karol.bisztyga@swmansion.com>
2021-12-28 13:36:14 +0100

    [15/n] [services] Blob - Tools for GET
    Summary:
    Full stack:
    D2666 D2667 D2668 D2707 D2708 D2709 D2669 D2670 D2671 D2672 D2673 D2674 D2675 D2676 D2677 D2678 D2679 D2680 D2681 D2682 D2683 D2684 D2685 D2686 D2734 D2735
    
    This introduces the blob service
    
    Test Plan: Please see 2686
    
    Reviewers: geekbrother, palys-swm, ashoat
    
    Reviewed By: geekbrother, palys-swm, ashoat
    
    Subscribers: benschac, ashoat, palys-swm, Adrian, atul, boristopalov
    
    Differential Revision: https://phabricator.ashoat.com/D2677

--- services/blob/docker-server/contents/server/src/Tools.cpp
+++ services/blob/docker-server/contents/server/src/Tools.cpp
@@ -1,6 +1,8 @@
 #include "Tools.h"
 
+#include "AwsStorageManager.h"
 #include "DatabaseEntitiesTools.h"
+#include "DatabaseManager.h"
 
 #include <chrono>
 
@@ -15,6 +17,32 @@ database::S3Path Tools::generateS3Path(
 
 std::string Tools::computeHashForFile(const database::S3Path &s3Path) {
   return database::BlobItem::FIELD_FILE_HASH; // TODO
+}
+
+database::S3Path Tools::findS3Path(const std::string &holder) {
+  std::shared_ptr<database::ReverseIndexItem> reverseIndexItem =
+      database::DatabaseManager::getInstance().findReverseIndexItemByHolder(
+          holder);
+  if (reverseIndexItem == nullptr) {
+    std::string errorMessage = "provided holder: [";
+    errorMessage += holder + "] has not been found in the database";
+    throw std::runtime_error(errorMessage);
+  }
+  return Tools::getInstance().findS3Path(*reverseIndexItem);
+}
+
+database::S3Path
+Tools::findS3Path(const database::ReverseIndexItem &reverseIndexItem) {
+  std::shared_ptr<database::BlobItem> blobItem =
+      database::DatabaseManager::getInstance().findBlobItem(
+          reverseIndexItem.getBlobHash());
+  if (blobItem == nullptr) {
+    std::string errorMessage = "no blob found for blobHash: [";
+    errorMessage += reverseIndexItem.getBlobHash() + "]";
+    throw std::runtime_error(errorMessage);
+  }
+  database::S3Path result = blobItem->getS3Path();
+  return result;
 }
 
 long long Tools::getCurrentTimestamp() {
