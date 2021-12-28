6bc25e4d2627aea541527a0e0a8e565ec4c4b537
karol-bisztyga <karol.bisztyga@swmansion.com>
2021-12-28 13:36:07 +0100

    [12/n] [DRAFT] [services] Blob - db manager for PUT - inner functions
    Summary:
    Full stack:
    D2666 D2667 D2668 D2707 D2708 D2709 D2669 D2670 D2671 D2672 D2673 D2674 D2675 D2676 D2677 D2678 D2679 D2680 D2681 D2682 D2683 D2684 D2685 D2686 D2734 D2735
    
    This introduces the blob service
    
    Test Plan: Please see 2686
    
    Reviewers: palys-swm, geekbrother
    
    Subscribers: ashoat, palys-swm, Adrian, atul, boristopalov
    
    Differential Revision: https://phabricator.ashoat.com/D2674

---
+++ services/blob/docker-server/contents/server/src/DatabaseManager.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "AwsTools.h"
+#include "DatabaseEntitiesTools.h"
+
+#include <aws/core/Aws.h>
+#include <aws/dynamodb/model/AttributeDefinition.h>
+#include <aws/dynamodb/model/GetItemRequest.h>
+#include <aws/dynamodb/model/PutItemRequest.h>
+
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+namespace comm {
+namespace network {
+namespace database {
+
+// this class should be thread-safe in case any shared resources appear
+class DatabaseManager {
+
+  void innerPutItem(
+      std::shared_ptr<Item> item,
+      const Aws::DynamoDB::Model::PutItemRequest &request);
+
+  template <typename T>
+  std::shared_ptr<T>
+  innerFindItem(Aws::DynamoDB::Model::GetItemRequest &request);
+
+public:
+  static DatabaseManager &getInstance();
+};
+
+template <typename T>
+std::shared_ptr<T>
+DatabaseManager::innerFindItem(Aws::DynamoDB::Model::GetItemRequest &request) {
+  std::shared_ptr<T> item = createItemByType<T>();
+  request.SetTableName(item->getTableName());
+  const Aws::DynamoDB::Model::GetItemOutcome &outcome =
+      AwsObjectsFactory::getDynamoDBClient()->GetItem(request);
+  if (!outcome.IsSuccess()) {
+    throw std::runtime_error(outcome.GetError().GetMessage());
+  }
+  const AttributeValues &outcomeItem = outcome.GetResult().GetItem();
+  if (!outcomeItem.size()) {
+    return nullptr;
+  }
+  item->assignItemFromDatabase(outcomeItem);
+  return std::move(item);
+}
+
+} // namespace database
+} // namespace network
+} // namespace comm
