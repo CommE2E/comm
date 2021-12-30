6d7f4b51f0656793e4a6b350ab8f679e6a466a05
Max Kalashnikov <geekmaks@gmail.com>
2021-12-30 04:38:20 +0200

    [1/n][DRAFT][Services] Tunnelbroker - Aws DynamoDB client, factory.
    Summary:
    
    This is the draft, waiting for the full stack list...
    
    Test Plan:
    
    Draft, waiting for test.
    
    Reviewers:
    
    Subscribers:

---
+++ services/tunnelbroker/docker-server/contents/server/src/AwsTools.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <aws/core/Aws.h>
+#include <aws/dynamodb/DynamoDBClient.h>
+
+#include <memory>
+#include <string>
+
+namespace comm {
+namespace network {
+
+// TODO:
+// Maybe we shoud put the credentials to the global
+// AWS config file
+const std::string AWS_REGION = "us-east-2";
+const std::string DEVICES_TABLE_NAME = "tunnelbroker-service-devices";
+
+struct DynamoDBClientWrapper {
+  std::shared_ptr<Aws::DynamoDB::DynamoDBClient> client;
+  DynamoDBClientWrapper() {
+    Aws::Client::ClientConfiguration config;
+    config.region = AWS_REGION;
+    client = std::make_shared<Aws::DynamoDB::DynamoDBClient>(config);
+  }
+};
+
+class AwsDynamoFactory {
+public:
+  static std::shared_ptr<Aws::DynamoDB::DynamoDBClient> getDynamoDBClient() {
+    thread_local std::unique_ptr<DynamoDBClientWrapper> clientWrapper(
+        new DynamoDBClientWrapper());
+    return clientWrapper->client;
+  }
+};
+
+} // namespace network
+} // namespace comm
