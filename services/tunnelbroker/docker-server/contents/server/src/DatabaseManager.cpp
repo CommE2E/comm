bec4f36da0560a249c2d261661f80aa2b545ab06
Max Kalashnikov <geekmaks@gmail.com>
2022-01-04 03:00:27 +0200

    [5/n][DRAFT][Services] Tunnelbroker - Database manager.
    Test Plan: Draft, test plan updating...
    
    Subscribers: ashoat, palys-swm, Adrian, atul, karol-bisztyga, boristopalov
    
    Differential Revision: https://phabricator.ashoat.com/D2762

---
+++ services/tunnelbroker/docker-server/contents/server/src/DatabaseManager.cpp
@@ -0,0 +1,54 @@
+#include "DatabaseManager.h"
+#include "Tools.h"
+
+#include <aws/core/utils/Outcome.h>
+#include <aws/dynamodb/model/QueryRequest.h>
+#include <aws/dynamodb/model/ScanRequest.h>
+
+#include <iostream>
+
+namespace comm {
+namespace network {
+namespace database {
+
+DatabaseManager &DatabaseManager::getInstance() {
+  static DatabaseManager instance;
+  return instance;
+}
+
+void DatabaseManager::innerPutItem(
+    std::shared_ptr<Item> item,
+    const Aws::DynamoDB::Model::PutItemRequest &request) {
+  const Aws::DynamoDB::Model::PutItemOutcome outcome =
+      AwsObjectsFactory::getDynamoDBClient()->PutItem(request);
+  if (!outcome.IsSuccess()) {
+    throw std::runtime_error(outcome.GetError().GetMessage());
+  }
+}
+
+void DatabaseManager::putSessionItem(const DeviceSessionItem &item) {
+  Aws::DynamoDB::Model::PutItemRequest request;
+  request.SetTableName(BlobItem::tableName);
+  request.AddItem(
+      DeviceSessionItem::ATTR_NOTIFY_TOKEN,
+      Aws::DynamoDB::Model::AttributeValue(item.getNotifyToken()));
+  request.AddItem(
+      DeviceSessionItem::ATTR_TYPE_OS,
+      Aws::DynamoDB::Model::AttributeValue(item.getTypeOS()));
+  request.AddItem(
+      DeviceSessionItem::ATTR_CHECKPOINT_TIME,
+      Aws::DynamoDB::Model::AttributeValue(
+          std::to_string(item.getCheckpointTime());
+
+  this->innerPutItem(std::make_shared<DeviceSessionItem>(item), request);
+}
+
+std::shared_ptr<DeviceSessionItem>
+DatabaseManager::findSessionItem(const std::string &deviceID) {
+  // Todo:
+  // DynamoDB request with componoud key
+  return std::move(this->innerFindItem<DeviceSessionItem>(request));
+
+} // namespace database
+} // namespace database
+} // namespace network
---
+++ services/tunnelbroker/docker-server/contents/server/src/DatabaseManager.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "AwsTools.h"
+#include "DatabaseEntitiesTools.h"
+
+#include <aws/core/Aws.h>
+#include <aws/dynamodb/model/AttributeDefinition.h>
+#include <aws/dynamodb/model/GetItemRequest.h>
+#include <aws/dynamodb/model/PutItemRequest.h>
+
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+namespace comm {
+namespace network {
+namespace database {
+
+class DatabaseManager {
+
+  void innerPutItem(
+      std::shared_ptr<Item> item,
+      const Aws::DynamoDB::Model::PutItemRequest &request);
+
+  template <typename T>
+  std::shared_ptr<T>
+  innerFindItem(Aws::DynamoDB::Model::GetItemRequest &request);
+
+public:
+  static DatabaseManager &getInstance();
+  void putSessionItem(const DeviceSessionItem &item);
+  std::shared_ptr<DeviceSessionItem>
+  findSessionItem(const std::string &deviceID);
+};
+
+template <typename T>
+std::shared_ptr<T>
+DatabaseManager::innerFindItem(Aws::DynamoDB::Model::GetItemRequest &request) {
+  std::shared_ptr<T> item = createItemByType<T>();
+  request.SetTableName(item->getTableName());
+  const Aws::DynamoDB::Model::GetItemOutcome &outcome =
+      AwsObjectsFactory::getDynamoDBClient()->GetItem(request);
+  if (!outcome.IsSuccess()) {
+    throw std::runtime_error(outcome.GetError().GetMessage());
+  }
+  const AttributeValues &outcomeItem = outcome.GetResult().GetItem();
+  if (!outcomeItem.size()) {
+    return nullptr;
+  }
+  item->assignItemFromDatabase(outcomeItem);
+  return std::move(item);
+}
+
+} // namespace database
+} // namespace network
+} // namespace comm
