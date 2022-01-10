742fd2685a4137149a4816f1c2571143012c6a70
Max Kalashnikov <geekmaks@gmail.com>
2022-01-10 20:06:35 +0200

    [9/n][DRAFT][Services] Tunnelbroker - Constants.
    Summary:
    Full stack (Updating):
    D2751 D2752 D2759 D2760 D2762 D2769 D2807 D2808
    
    Constants introduction.
    
    Test Plan: Draft, test plan updating...
    
    Subscribers: ashoat, palys-swm, Adrian, atul, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D2809

---
+++ services/tunnelbroker/docker-server/contents/server/src/Constants.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <string>
+
+// Tunnelbroker server Identification
+const std::string TUNNELBROKER_ID = "tunnel1";
+
+// AWS
+const std::string AWS_REGION = "us-east-2";
+const std::string DEVICE_SESSIONS_TABLE_NAME = "tunnelbroker-service-devices";
+
+// Sessions
+const size_t SIGNATURE_REQUEST_LENGTH = 32;
+const size_t SESSION_ID_LENGTH = 16;
+
+// gRPC Server
+const std::string SERVER_LISTEN_ADDRESS = "0.0.0.0:50051";
+
+// gRPC Settings
+// 4MB limit
+const size_t GRPC_CHUNK_SIZE_LIMIT = 4 * 1024 * 1024;
+const size_t GRPC_METADATA_SIZE_PER_MESSAGE = 5;
+
+// AMQP (RabbitMQ)
+// number of the AMQP client threads
+const size_t AMQP_CLIENT_THREADS = 2;
+const std::string AMQP_URI = "amqp://guest:guest@0.0.0.0/";
+// message TTL 1 hour
+const size_t AMQP_MESSAGE_TTL = 3600 * 1000;
