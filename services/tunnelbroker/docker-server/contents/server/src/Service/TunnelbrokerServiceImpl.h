4aa05e9b940f70e07ab7d0bac7c33d4de3663ad4
Max Kalashnikov <geekmaks@gmail.com>
2022-01-10 17:02:23 +0200

    [7/n][DRAFT][Services] Tunnelbroker - Service implementation.
    Summary:
    Full stack (Updating):
    D2751 D2752 D2759 D2760 D2762 D2769
    
    Service requests implementation.
    
    Test Plan: Draft, test plan updating...
    
    Subscribers: ashoat, palys-swm, Adrian, atul, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D2807

---
+++ services/tunnelbroker/docker-server/contents/server/src/Service/TunnelbrokerServiceImpl.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <folly/concurrency/ConcurrentHashMap.h>
+#include <folly/stop_watch.h>
+#include <grpcpp/grpcpp.h>
+#include <chrono>
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include "../_generated/tunnelbroker.grpc.pb.h"
+#include "../_generated/tunnelbroker.pb.h"
+
+#include "AmqpManager.h"
+#include "AwsTools.h"
+#include "Constants.h"
+#include "DatabaseManager.h"
+// #include "DeliveryBroker.h"
+#include "Tools.h"
+
+namespace comm {
+namespace network {
+
+class TunnelBrokerServiceImpl final
+    : public tunnelbroker::TunnelbrokerService::Service {
+
+public:
+  TunnelBrokerServiceImpl();
+  virtual ~TunnelBrokerServiceImpl();
+
+  // Request new session signature
+  grpc::Status SessionSignature(
+      grpc::ServerContext *context,
+      const tunnelbroker::SessionSignatureRequest *request,
+      tunnelbroker::SessionSignatureResponse *reply) override;
+
+  // Create new session method
+  grpc::Status NewSession(
+      grpc::ServerContext *context,
+      const tunnelbroker::NewSessionRequest *request,
+      tunnelbroker::NewSessionResponse *reply) override;
+
+  // Send message to deviceID method
+  grpc::Status Send(
+      grpc::ServerContext *context,
+      const tunnelbroker::SendRequest *request,
+      google::protobuf::Empty *reply) override;
+
+  // Get messages for the deviceID from the queue as stream
+  grpc::Status
+  Get(grpc::ServerContext *context,
+      const tunnelbroker::GetRequest *request,
+      grpc::ServerWriter<tunnelbroker::GetResponse> *stream) override;
+};
+
+} // namespace network
+} // namespace comm
