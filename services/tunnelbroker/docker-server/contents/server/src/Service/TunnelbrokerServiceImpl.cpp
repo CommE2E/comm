989e3fdbb87f6d5fa8264c116eca21e85a1d33d7
Max Kalashnikov <geekmaks@gmail.com>
2022-01-10 17:02:23 +0200

    [7/n][DRAFT][Services] Tunnelbroker - Service implementation.
    Summary:
    Full stack (Updating):
    D2751 D2752 D2759 D2760 D2762 D2769
    
    Service requests implementation.
    
    Test Plan: Draft, test plan updating...
    
    Subscribers: ashoat, palys-swm, Adrian, atul, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D2807

---
+++ services/tunnelbroker/docker-server/contents/server/src/Service/TunnelbrokerServiceImpl.cpp
@@ -0,0 +1,117 @@
+#include "TunnelbrokerServiceImpl.h"
+
+namespace comm {
+namespace network {
+
+using namespace std::chrono_literals;
+
+TunnelBrokerServiceImpl::TunnelBrokerServiceImpl() {
+  Aws::InitAPI({});
+}
+
+TunnelBrokerServiceImpl::~TunnelBrokerServiceImpl() {
+  Aws::ShutdownAPI({});
+}
+
+grpc::Status TunnelBrokerServiceImpl::SessionSignature(
+    grpc::ServerContext *context,
+    const tunnelbroker::SessionSignatureRequest *request,
+    tunnelbroker::SessionSignatureResponse *reply) {
+
+  const std::string deviceID = request->deviceid();
+  // Generate new random string to sign request for the deviceID
+  const std::string toSign = generateRandomString(SIGNATURE_REQUEST_LENGTH);
+  // TODO: Save toSign to the global hashmap
+  // Linear task: ENG-436
+  // We need to save `toSign` to the global hashmap with the deviceID as a key
+  // to check the sign of the signature for the certain deviceID later on
+  // `NewSession` request.
+  reply->set_tosign(toSign);
+  return grpc::Status::OK;
+}
+
+grpc::Status TunnelBrokerServiceImpl::NewSession(
+    grpc::ServerContext *context,
+    const tunnelbroker::NewSessionRequest *request,
+    tunnelbroker::NewSessionResponse *reply) {
+
+  const std::string signature = request->signature();
+  const std::string pubkey = request->publickey();
+  // TODO: Check the key ownership
+  // Linear task: ENG-436
+  // We need to compare the signature as a signed `toSign` string by the
+  // publicKey provided for the certain deviceID. Get `toSign` from the
+  // global hashmap which saved from the `SessionSignature` request before
+  // `NewSession` request.
+
+  std::shared_ptr<database::DeviceSessionItem> deviceSessionItem;
+  // Generate new sessionId from random string
+  const std::string sessionId = generateRandomString(SESSION_ID_LENGTH);
+  try {
+    // Check if a session already exist and throw an error if not
+    if (database::DatabaseManager::getInstance().findSessionItem(sessionId) ==
+        nullptr) {
+      return grpc::Status(
+          grpc::StatusCode::ALREADY_EXISTS, "Session already exist.");
+    }
+    // Insert a new session item to the database
+    deviceSessionItem = std::make_shared<database::DeviceSessionItem>(
+        sessionId,
+        request->deviceid(),
+        request->publickey(),
+        request->notifytoken(),
+        tunnelbroker::NewSessionRequest_DeviceTypes_Name(request->devicetype()),
+        request->deviceappversion(),
+        request->deviceos());
+    database::DatabaseManager::getInstance().putSessionItem(*deviceSessionItem);
+  } catch (std::runtime_error &e) {
+    std::cout << "Error while processing NewSession request: " << e.what()
+              << std::endl;
+    return grpc::Status(grpc::StatusCode::INTERNAL, e.what());
+  }
+  // Send a reply with new sessionId
+  reply->set_sessionid(sessionId);
+  return grpc::Status::OK;
+}
+
+grpc::Status TunnelBrokerServiceImpl::Send(
+    grpc::ServerContext *context,
+    const tunnelbroker::SendRequest *request,
+    google::protobuf::Empty *reply) {
+
+  try {
+    // TODO:
+    // Send payload to deviceID implementation
+  } catch (std::runtime_error &e) {
+    std::cout << "Error while processing Send request: " << e.what()
+              << std::endl;
+    return grpc::Status(grpc::StatusCode::INTERNAL, e.what());
+  }
+  return grpc::Status::OK;
+}
+
+grpc::Status TunnelBrokerServiceImpl::Get(
+    grpc::ServerContext *context,
+    const tunnelbroker::GetRequest *request,
+    grpc::ServerWriter<tunnelbroker::GetResponse> *stream) {
+
+  try {
+    const std::string sessionID = request->sessionid();
+    // TODO: Find session item from database
+
+    // TODO: We need to check if there is any messages and push it
+    // to the gRPC stream.
+
+    // const std::string clientDeviceID;
+    // std::vector<DeliveryBrokerMessageStruct> messagesToDeliver =
+    //     DeliveryBrokerGet(clientDeviceID);
+  } catch (std::runtime_error &e) {
+    std::cout << "Error while processing Get request: " << e.what()
+              << std::endl;
+    return grpc::Status(grpc::StatusCode::INTERNAL, e.what());
+  }
+  return grpc::Status::OK;
+}
+
+} // namespace network
+} // namespace comm
