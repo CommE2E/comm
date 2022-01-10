ec3bc7aef89a1abace3e403818d83c1dd17b32fd
Max Kalashnikov <geekmaks@gmail.com>
2022-01-10 17:06:58 +0200

    [8/n][DRAFT][Services] Tunnelbroker - Tools.
    Summary:
    Full stack (Updating):
    D2751 D2752 D2759 D2760 D2762 D2769
    
    Tools implementation.
    
    Test Plan: Draft, test plan updating...
    
    Subscribers: ashoat, palys-swm, Adrian, atul, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D2808

---
+++ services/tunnelbroker/docker-server/contents/server/src/Tools.cpp
@@ -0,0 +1,18 @@
+#include "Tools.h"
+
+namespace comm {
+namespace network {
+std::string generateRandomString(std::size_t length) {
+  const std::string CHARACTERS =
+      "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  std::random_device random_device;
+  std::mt19937 generator(random_device());
+  std::uniform_int_distribution<> distribution(0, CHARACTERS.size() - 1);
+  std::string random_string;
+  for (std::size_t i = 0; i < length; ++i) {
+    random_string += CHARACTERS[distribution(generator)];
+  }
+  return random_string;
+}
+} // namespace network
+} // namespace comm
--- services/tunnelbroker/docker-server/contents/server/src/Tools.h
+++ services/tunnelbroker/docker-server/contents/server/src/Tools.h
@@ -1,31 +1,9 @@
 #pragma once
-
+#include <random>
 #include <string>
-
-#include <folly/MPMCQueue.h>
 
 namespace comm {
 namespace network {
-namespace ping {
-
-enum class ClientState {
-  ONLINE,
-  OFFLINE,
-};
-
-struct ClientData {
-  const std::string id;
-  const std::string deviceToken;
-
-  folly::MPMCQueue<bool> pingRequests = folly::MPMCQueue<bool>(10);
-  folly::MPMCQueue<bool> pingResponses = folly::MPMCQueue<bool>(10);
-  ClientState lastState = ClientState::ONLINE;
-
-  ClientData(const std::string id, const std::string deviceToken)
-      : id(id), deviceToken(deviceToken) {
-  }
-};
-
-} // namespace ping
+std::string generateRandomString(std::size_t length);
 } // namespace network
 } // namespace comm
