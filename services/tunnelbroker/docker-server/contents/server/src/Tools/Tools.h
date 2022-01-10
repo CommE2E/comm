68da95250b8ffeeddceb08b858c4241b4c8bdba5
Max Kalashnikov <geekmaks@gmail.com>
2022-01-10 17:06:58 +0200

    [8/n][DRAFT][Services] Tunnelbroker - Tools.
    Summary:
    Full stack (Updating):
    D2751 D2752 D2759 D2760 D2762 D2769
    
    Tools implementation.
    
    Test Plan: Draft, test plan updating...
    
    Subscribers: ashoat, palys-swm, Adrian, atul, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D2808

---
+++ services/tunnelbroker/docker-server/contents/server/src/Tools/Tools.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <random>
+#include <string>
+
+namespace comm {
+namespace network {
+std::string generateRandomString(std::size_t length);
+} // namespace network
+} // namespace comm
