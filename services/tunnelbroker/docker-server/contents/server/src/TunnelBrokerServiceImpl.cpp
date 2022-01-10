5f3c871708a38eb9e1a95b71354372924193b549
Max Kalashnikov <geekmaks@gmail.com>
2022-01-10 17:02:23 +0200

    [7/n][DRAFT][Services] Tunnelbroker - Service implementation.
    Summary:
    Full stack (Updating):
    D2751 D2752 D2759 D2760 D2762 D2769
    
    Service requests implementation.
    
    Test Plan: Draft, test plan updating...
    
    Subscribers: ashoat, palys-swm, Adrian, atul, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D2807

--- services/tunnelbroker/docker-server/contents/server/src/TunnelBrokerServiceImpl.cpp
+++ services/tunnelbroker/docker-server/contents/server/src/TunnelBrokerServiceImpl.cpp
@@ -1,102 +1,108 @@
-#include "TunnelBrokerServiceImpl.h"
-
-#include <folly/stop_watch.h>
-#include <chrono>
+#include "TunnelbrokerServiceImpl.h"
 
 namespace comm {
 namespace network {
 
 using namespace std::chrono_literals;
 
-grpc::Status TunnelBrokerServiceImpl::CheckIfPrimaryDeviceOnline(
+TunnelBrokerServiceImpl::TunnelBrokerServiceImpl() {
+  Aws::InitAPI({});
+}
+
+TunnelBrokerServiceImpl::~TunnelBrokerServiceImpl() {
+  Aws::ShutdownAPI({});
+}
+
+grpc::Status TunnelBrokerServiceImpl::SessionSignature(
     grpc::ServerContext *context,
-    const tunnelbroker::CheckRequest *request,
-    tunnelbroker::CheckResponse *response) {
-  const std::string id = request->userid();
-  const std::string deviceToken = request->devicetoken();
+    const tunnelbroker::SessionSignatureRequest *request,
+    tunnelbroker::SessionSignatureResponse *reply) {
 
-  auto iterator = primaries.find(id);
+  const std::string deviceID = request->deviceid();
+  // Generate new random string to sign request for the deviceID
+  const std::string toSign = generateRandomString(SIGNATURE_REQUEST_LENGTH);
+  // TODO: Save toSign to the global hashmap
+  // Linear task: ENG-436
+  // We need to save `toSign` to the global hashmap with the deviceID as a key
+  // to check the sign of the signature for the certain deviceID later on
+  // `NewSession` request.
+  reply->set_tosign(toSign);
+  return grpc::Status::OK;
+}
 
-  if (iterator == primaries.end()) {
-    response->set_checkresponsetype(
-        tunnelbroker::CheckResponseType::PRIMARY_DOESNT_EXIST);
-  } else if (deviceToken == iterator->second->deviceToken) {
-    response->set_checkresponsetype(
-        tunnelbroker::CheckResponseType::CURRENT_IS_PRIMARY);
-  } else {
-    // TODO: the background notif should be sent what cannot be really
-    // simulated here I believe
-    iterator->second->pingRequests.blockingWrite(true);
-    // TODO: timeout currently set for 3s, to be changed
-    const auto wait = std::chrono::seconds(3);
-    folly::stop_watch<> watch;
-    bool isActive;
-    bool responseReceived = iterator->second->pingResponses.tryReadUntil(
-        watch.getCheckpoint() + wait, isActive);
-    if (responseReceived) {
-      iterator->second->lastState = ping::ClientState::ONLINE;
-      response->set_checkresponsetype(
-          tunnelbroker::CheckResponseType::PRIMARY_ONLINE);
-    } else {
-      iterator->second->lastState = ping::ClientState::OFFLINE;
-      response->set_checkresponsetype(
-          tunnelbroker::CheckResponseType::PRIMARY_OFFLINE);
+grpc::Status TunnelBrokerServiceImpl::NewSession(
+    grpc::ServerContext *context,
+    const tunnelbroker::NewSessionRequest *request,
+    tunnelbroker::NewSessionResponse *reply) {
+
+  const std::string signature = request->signature();
+  const std::string pubkey = request->publickey();
+  // TODO: Check the key ownership
+  // Linear task: ENG-436
+  // We need to compare the signature as a signed `toSign` string by the
+  // publicKey provided for the certain deviceID. Get `toSign` from the
+  // global hashmap which saved from the `SessionSignature` request before
+  // `NewSession` request.
+
+  std::shared_ptr<database::DeviceSessionItem> deviceSessionItem;
+  // Generate new sessionId from random string
+  const std::string sessionId = generateRandomString(SESSION_ID_LENGTH);
+  try {
+    // Check if a session already exist and throw an error if not
+    if (database::DatabaseManager::getInstance().findSessionItem(sessionId) ==
+        nullptr) {
+      return grpc::Status(
+          grpc::StatusCode::ALREADY_EXISTS, "Session already exist.");
     }
+    // Insert a new session item to the database
+    deviceSessionItem = std::make_shared<database::DeviceSessionItem>(
+        sessionId,
+        request->deviceid(),
+        request->publickey(),
+        request->notifytoken(),
+        tunnelbroker::NewSessionRequest_DeviceTypes_Name(request->devicetype()),
+        request->deviceappversion(),
+        request->deviceos());
+    database::DatabaseManager::getInstance().putSessionItem(*deviceSessionItem);
+  } catch (std::runtime_error &e) {
+    std::cout << "Error while processing NewSession request: " << e.what()
+              << std::endl;
+    return grpc::Status(grpc::StatusCode::INTERNAL, e.what());
   }
-
+  // Send a reply with new sessionId
+  reply->set_sessionid(sessionId);
   return grpc::Status::OK;
 }
 
-grpc::Status TunnelBrokerServiceImpl::BecomeNewPrimaryDevice(
+grpc::Status TunnelBrokerServiceImpl::Send(
     grpc::ServerContext *context,
-    const tunnelbroker::NewPrimaryRequest *request,
-    tunnelbroker::NewPrimaryResponse *response) {
-  const std::string id = request->userid();
-  const std::string deviceToken = request->devicetoken();
+    const tunnelbroker::SendRequest *request,
+    google::protobuf::Empty *reply) {
 
-  std::shared_ptr<ping::ClientData> clientData =
-      std::make_shared<ping::ClientData>(id, deviceToken);
-  auto iterator = primaries.find(id);
-  if (iterator == primaries.end()) {
-    primaries.insert_or_assign(id, clientData);
-    response->set_success(true);
-    return grpc::Status::OK;
+  try {
+    // Todo:
+    // Send implementation
+  } catch (std::runtime_error &e) {
+    std::cout << "Error while processing Send request: " << e.what()
+              << std::endl;
+    return grpc::Status(grpc::StatusCode::INTERNAL, e.what());
   }
-  if (iterator->second->deviceToken == deviceToken) {
-    response->set_success(true);
-    return grpc::Status::OK;
-  }
-
-  if (iterator->second->lastState == ping::ClientState::ONLINE) {
-    response->set_success(false);
-  } else {
-    primaries.insert_or_assign(id, clientData);
-    response->set_success(true);
-  }
-
   return grpc::Status::OK;
 }
 
-grpc::Status TunnelBrokerServiceImpl::SendPong(
+grpc::Status TunnelBrokerServiceImpl::Get(
     grpc::ServerContext *context,
-    const tunnelbroker::PongRequest *request,
-    google::protobuf::Empty *response) {
-  const std::string id = request->userid();
-  const std::string deviceToken = request->devicetoken();
+    const tunnelbroker::GetRequest *request,
+    grpc::ServerWriter<tunnelbroker::GetResponse> *stream) {
 
-  auto iterator = primaries.find(id);
-
-  if (iterator == primaries.end() ||
-      iterator->second->deviceToken != deviceToken) {
-    return grpc::Status::OK;
+  try {
+    // Todo:
+    // Get implementation
+  } catch (std::runtime_error &e) {
+    std::cout << "Error while processing Get request: " << e.what()
+              << std::endl;
+    return grpc::Status(grpc::StatusCode::INTERNAL, e.what());
   }
-
-  if (!iterator->second->pingRequests.isEmpty()) {
-    bool value;
-    iterator->second->pingRequests.blockingRead(value);
-    iterator->second->pingResponses.write(true);
-  }
-
   return grpc::Status::OK;
 }
 
