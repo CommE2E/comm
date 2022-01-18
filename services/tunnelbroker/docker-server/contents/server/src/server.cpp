1c11d455d85dba47f7d48f3e62fe1de8e8d87322
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-01-18 13:14:41 +0100

    [13/n][Services] Tunnelbroker - gRPC Server.
    Summary:
    Full stack:
    D2809 D2875 D2752 D2904 D2759 D2760 D2808 D2762 D2876 D2769 D2877 D2807 D2814 D2870 D2888 D2915 D2976 D2977 D2978
    
    gRPC server listener.
    
    Test Plan: Bult uses the `docker-compose build tunnelbroker-server` and runs the server using the `docker-compose run tunnelbroker-server` command.
    
    Reviewers: karol-bisztyga, palys-swm, ashoat
    
    Reviewed By: karol-bisztyga, palys-swm, ashoat
    
    Subscribers: benschac, ashoat, palys-swm, Adrian, atul, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D2814

--- services/tunnelbroker/docker-server/contents/server/src/server.cpp
+++ services/tunnelbroker/docker-server/contents/server/src/server.cpp
@@ -1,29 +1,29 @@
-#include "TunnelBrokerServiceImpl.h"
+#include "AmqpManager.h"
+#include "Constants.h"
+#include "TunnelbrokerServiceImpl.h"
 
 #include <grpcpp/grpcpp.h>
 
 #include <iostream>
-#include <memory>
 #include <string>
+#include <thread>
 
 namespace comm {
 namespace network {
 
 void RunServer() {
-  std::string server_address = "0.0.0.0:50051";
   TunnelBrokerServiceImpl service;
-
   grpc::EnableDefaultHealthCheckService(true);
   grpc::ServerBuilder builder;
   // Listen on the given address without any authentication mechanism.
-  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  builder.AddListeningPort(
+      SERVER_LISTEN_ADDRESS, grpc::InsecureServerCredentials());
   // Register "service" as the instance through which we'll communicate with
   // clients. In this case it corresponds to an *synchronous* service.
   builder.RegisterService(&service);
-  // Finally assemble the server.
   std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
-  std::cout << "Server listening" << std::endl;
-
+  std::cout << "gRPC Server listening at :" << SERVER_LISTEN_ADDRESS
+            << std::endl;
   // Wait for the server to shutdown. Note that some other thread must be
   // responsible for shutting down the server for this call to ever return.
   server->Wait();
@@ -33,7 +33,9 @@ void RunServer() {
 } // namespace comm
 
 int main(int argc, char **argv) {
-  comm::network::RunServer();
-
+  std::thread amqpThread(comm::network::AMQPConnect);
+  std::thread grpcThread(comm::network::RunServer);
+  amqpThread.join();
+  grpcThread.join();
   return 0;
 }
