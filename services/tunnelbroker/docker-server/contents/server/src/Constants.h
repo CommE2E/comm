1c0d39e4d54298e82c71d23690fd2a9eda757863
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-01-18 13:12:07 +0100

    [1/n][Services] Tunnelbroker - Constants.
    Summary:
    Full stack:
    D2809 D2875 D2752 D2904 D2759 D2760 D2808 D2762 D2876 D2769 D2877 D2807 D2814 D2870 D2888 D2915 D2976 D2977 D2978
    
    Constants introduction.
    
    Test Plan: Bult stack uses the docker-compose build tunnelbroker-server and runs the server using the docker-compose run tunnelbroker-server command.
    
    Reviewers: karol-bisztyga, palys-swm, ashoat
    
    Reviewed By: karol-bisztyga, palys-swm, ashoat
    
    Subscribers: benschac, ashoat, palys-swm, Adrian, atul, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D2809

---
+++ services/tunnelbroker/docker-server/contents/server/src/Constants.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <string>
+
+namespace comm {
+namespace network {
+
+// Tunnelbroker server Identification
+const std::string TUNNELBROKER_ID = "tunnel1";
+
+// AWS
+const std::string DEVICE_SESSIONS_TABLE_NAME = "tunnelbroker-device-session";
+const std::string DEVICE_SESSIONS_VERIFICATION_MESSAGES_TABLE_NAME =
+    "tunnelbroker-verification-message";
+
+// Sessions
+const size_t SIGNATURE_REQUEST_LENGTH = 64;
+const size_t SESSION_ID_LENGTH = 64;
+const size_t SESSION_RECORD_TTL = 30 * 24 * 3600; // 30 days
+const size_t SESSION_SIGN_RECORD_TTL = 24 * 3600; // 24 hours
+
+// gRPC Server
+const std::string SERVER_LISTEN_ADDRESS = "0.0.0.0:50051";
+
+// AMQP (RabbitMQ)
+const std::string AMQP_URI = "amqp://guest:guest@0.0.0.0/vhost";
+const std::string AMQP_FANOUT_EXCHANGE_NAME = "allBrokers";
+// message TTL
+const size_t AMQP_MESSAGE_TTL = 300 * 1000; // 5 min
+// queue TTL in case of no consumers (tunnelbroker is down)
+const size_t AMQP_QUEUE_TTL = 24 * 3600 * 1000; // 24 hours
+// routing message headers name
+const std::string AMQP_HEADER_FROM_DEVICEID = "fromDeviceid";
+const std::string AMQP_HEADER_TO_DEVICEID = "toDeviceid";
+
+} // namespace network
+} // namespace comm
