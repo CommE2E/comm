e420e4f93e49cde71173d49ce6885f66b2f14082
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-01-21 10:25:22 +0100

    [15/n] [services] Blob - dev mode - storage manager test - changes
    Summary:
    Stack:
    
    D2815 D2816 D2861 D2817 D2862 D2818 D2819 D2820 D2821 D2822 D2823 D2824 D2863 D2825 [ D2864 ] D2826
    
    Dev mode for the blob service
    
    Test Plan: Please go to D2826
    
    Reviewers: palys-swm, geekbrother, ashoat
    
    Reviewed By: palys-swm, ashoat
    
    Subscribers: benschac, ashoat, palys-swm, Adrian, atul
    
    Differential Revision: https://phabricator.ashoat.com/D2864

--- services/blob/docker-server/contents/server/test/StorageManagerTest.cpp
+++ services/blob/docker-server/contents/server/test/StorageManagerTest.cpp
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 
-#include "AwsStorageManager.h"
+#include "AwsTools.h"
 #include "TestTools.h"
 
 #include <aws/core/Aws.h>
@@ -12,10 +12,9 @@
 
 using namespace comm::network;
 
-class StorageManagerTest : public testing::Test {
+class AWSToolsTest : public testing::Test {
 public:
 protected:
-  std::unique_ptr<AwsStorageManager> storageManager;
   const std::string bucketName = "commapp-test";
   const std::string data =
       "yiU3VaZlKfTteO10yrWmK1Q5BOvBQrdmj2aBlnoLuhxLfRZK1n8"
@@ -23,9 +22,6 @@ protected:
 
   virtual void SetUp() {
     Aws::InitAPI({});
-    if (storageManager == nullptr) {
-      storageManager = std::make_unique<AwsStorageManager>();
-    }
   }
 
   virtual void TearDown() {
@@ -33,41 +29,31 @@ protected:
   }
 };
 
-TEST_F(StorageManagerTest, ObjectOperationsTest) {
-  EXPECT_TRUE(storageManager->getBucket(bucketName).isAvailable());
-  std::string objectName = createObject(storageManager->getBucket(bucketName));
+TEST_F(AWSToolsTest, ObjectOperationsTest) {
+  EXPECT_TRUE(getBucket(bucketName).isAvailable());
+  std::string objectName = createObject(getBucket(bucketName));
 
-  storageManager->getBucket(bucketName).writeObject(objectName, data);
+  getBucket(bucketName).writeObject(objectName, data);
 
-  EXPECT_EQ(
-      storageManager->getBucket(bucketName).getObjectSize(objectName),
-      data.size());
-  EXPECT_TRUE(
-      storageManager->getBucket(bucketName).getObjectData(objectName) == data);
+  EXPECT_EQ(getBucket(bucketName).getObjectSize(objectName), data.size());
+  EXPECT_TRUE(getBucket(bucketName).getObjectData(objectName) == data);
   std::string chunkedData;
   const size_t chunkSize = data.size() / 10;
   std::function<void(const std::string &)> callback =
       [&chunkedData](const std::string &chunk) { chunkedData += chunk; };
-  storageManager->getBucket(bucketName)
-      .getObjectDataChunks(objectName, callback, chunkSize);
+  getBucket(bucketName).getObjectDataChunks(objectName, callback, chunkSize);
   EXPECT_TRUE(data == chunkedData);
 
-  storageManager->getBucket(bucketName)
-      .renameObject(objectName, objectName + "c");
+  getBucket(bucketName).renameObject(objectName, objectName + "c");
   EXPECT_THROW(
-      storageManager->getBucket(bucketName).getObjectData(objectName),
-      std::runtime_error);
-  EXPECT_TRUE(
-      storageManager->getBucket(bucketName).getObjectData(objectName + "c") ==
-      data);
-  storageManager->getBucket(bucketName)
-      .renameObject(objectName + "c", objectName);
+      getBucket(bucketName).getObjectData(objectName), std::runtime_error);
+  EXPECT_TRUE(getBucket(bucketName).getObjectData(objectName + "c") == data);
+  getBucket(bucketName).renameObject(objectName + "c", objectName);
 
-  storageManager->getBucket(bucketName).clearObject(objectName);
-  EXPECT_EQ(storageManager->getBucket(bucketName).getObjectSize(objectName), 0);
+  getBucket(bucketName).clearObject(objectName);
+  EXPECT_EQ(getBucket(bucketName).getObjectSize(objectName), 0);
 
-  storageManager->getBucket(bucketName).deleteObject(objectName);
+  getBucket(bucketName).removeObject(objectName);
   EXPECT_THROW(
-      storageManager->getBucket(bucketName).getObjectData(objectName),
-      std::runtime_error);
+      getBucket(bucketName).getObjectData(objectName), std::runtime_error);
 }
