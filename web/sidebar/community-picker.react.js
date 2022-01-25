bc3731d5bd8e38a1171c3b74be69b78306aed2a0
Benjamin Schachter <benschac@gmail.com>
2022-01-25 14:02:30 -0500

    [web] [1/n] add inbox icon
    Summary: add icon on sidebar. This is non-functional because we don't have community switcher
    
    Test Plan: make sure the page matches the figma
    
    Reviewers: atul
    
    Subscribers: ashoat, palys-swm, Adrian, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D2924

--- web/sidebar/community-picker.react.js
+++ web/sidebar/community-picker.react.js
@@ -2,10 +2,15 @@
 
 import * as React from 'react';
 
+import SWMansionIcon from '../SWMansionIcon.react';
 import css from './community-picker.css';
 
 function CommunityPicker(): React.Node {
-  return <div className={css.container}></div>;
+  return (
+    <div className={css.container}>
+      <SWMansionIcon icon="inbox" size={28} />
+    </div>
+  );
 }
 
 export default CommunityPicker;
