fb27646cd0ce21585de4dc8b14c4dbfc75867d45
Benjamin Schachter <benschac@gmail.com>
2022-01-25 16:22:34 -0500

    [web] [4/n] show user modal on settings gear click
    Summary: on click, user should be show modal <addgif>
    
    Test Plan: click modal
    
    Reviewers: atul
    
    Subscribers: ashoat, palys-swm, Adrian, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D2929

--- web/sidebar/sidebar.react.js
+++ web/sidebar/sidebar.react.js
@@ -5,11 +5,15 @@ import * as React from 'react';
 import AppSwitcher from './app-switcher.react';
 import CommunityPicker from './community-picker.react';
 import css from './sidebar.css';
+type Props = {
+  setModal: (modal: ?React.Node) => void,
+};
 
-function SideBar(): React.Node {
+function SideBar(props: Props): React.Node {
+  const { setModal } = props;
   return (
     <aside className={css.container}>
-      <CommunityPicker />
+      <CommunityPicker setModal={setModal} />
       <AppSwitcher />
     </aside>
   );
