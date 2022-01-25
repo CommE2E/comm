622a51cf717d652a758e0ad0981e997c18fa84f6
Benjamin Schachter <benschac@gmail.com>
2022-01-25 08:05:34 -0500

    [web] [5/n] [layout] add spaces component
    Summary: Add section for community and inbox selection
    
    Test Plan: should look like {F8539}
    
    Reviewers: atul, ashoat
    
    Reviewed By: atul, ashoat
    
    Subscribers: ashoat, palys-swm, Adrian, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D2869

---
+++ web/sidebar/community-picker.react.js
@@ -0,0 +1,11 @@
+// @flow
+
+import * as React from 'react';
+
+import css from './community-picker.css';
+
+function CommunityPicker(): React.Node {
+  return <div className={css.container}></div>;
+}
+
+export default CommunityPicker;
--- web/sidebar/sidebar.react.js
+++ web/sidebar/sidebar.react.js
@@ -3,11 +3,13 @@
 import * as React from 'react';
 
 import AppSwitcher from './app-switcher.react';
+import CommunityPicker from './community-picker.react';
 import css from './sidebar.css';
 
 function SideBar(): React.Node {
   return (
     <aside className={css.container}>
+      <CommunityPicker />
       <AppSwitcher />
     </aside>
   );
