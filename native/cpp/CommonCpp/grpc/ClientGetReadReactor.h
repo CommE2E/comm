68b2375f344cd1010d41d9b4ba09aad3134a646a
Atul Madhugiri <atulm@berkeley.edu>
2022-01-31 14:03:23 -0500

    [CommCoreModule] Introduce `ClientGetReadReactor.h`
    Summary:
    gRPC requires us to implement an `grpc::ClientReadReactor` to trigger callbacks on grpc "events" (`OnReadDone`, `Done`, etc) in a non-blocking way. The callbacks are called from threads that gRPC maintains as part of the library. The following proposal provides a good high-level overview: https://github.com/grpc/proposal/blob/master/L67-cpp-callback-api.md. Here's the specific example I followed closely: https://github.com/grpc/grpc/blob/master/examples/cpp/route_guide/route_guide_callback_client.cc.
    
    The critical method in this object is `OnReadDone`, which gets called by gRPC on every message successfully read from the `Get` stream. In later diffs we pass a lambda through `GRPCStreamHostObject` -> `NetworkModule` -> `Client` -> `ClientGetReadReactor` to be called by the gRPC threads. The lambda contains a `jsi::function` wrapped in `jsInvoker` to ensure that the `jsi::function` is only called from the main thread (a requirement of calling any function with `jsi::Runtime &runtime` as an argument).
    
    It's important to note that we're setting `std::function onReadDoneCallback` from the `network` thread, but it gets called from gRPC threads when a message comes through. This could lead to issues with thread safety, so we use a `std::lock_guard` and `std::mutex`. Here's the reference I used (and example I basically copied): https://blob.sh/atul/0e94.png
    
    We previously discussed using a `std::shared_mutex` to allow multiple concurrent readers, but a single writer. Unfortunately, `std::shared_mutex` is only available in C++ >17. There are alternatives in `boost` and whatnot, but I think the current solution is fine for now.
    
    We only set the `onmessage` callback once in the current code, and I think the cost of only allowing a single reader to access the callback at a time will be overshadowed by the network cost?
    
    {F11360}
    (http://norvig.com/21-days.html)
    
    Test Plan: Tested implicitly by later diffs.
    
    Reviewers: ashoat
    
    Reviewed By: ashoat
    
    Subscribers: palys-swm, Adrian, karol-bisztyga, benschac
    
    Differential Revision: https://phabricator.ashoat.com/D3038

---
+++ native/cpp/CommonCpp/grpc/ClientGetReadReactor.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <grpcpp/grpcpp.h>
+
+#include "_generated/tunnelbroker.grpc.pb.h"
+#include "_generated/tunnelbroker.pb.h"
+
+class ClientGetReadReactor
+    : public grpc::ClientReadReactor<tunnelbroker::GetResponse> {
+  grpc::ClientContext context;
+  tunnelbroker::GetRequest request;
+  tunnelbroker::GetResponse response;
+  std::mutex onReadDoneCallbackMutex;
+  std::string sessionID;
+  std::function<void(std::string)> onReadDoneCallback;
+
+public:
+  ClientGetReadReactor(
+      tunnelbroker::TunnelbrokerService::Stub *stub,
+      std::string sessionID)
+      : sessionID{sessionID}, request{} {
+    request.set_sessionid(sessionID);
+    stub->async()->Get(&(this->context), &(this->request), this);
+    StartRead(&(this->response));
+    StartCall();
+  }
+
+  void OnReadDone(bool ok) override {
+    if (!ok) {
+      return;
+    }
+    std::lock_guard<std::mutex> guard{this->onReadDoneCallbackMutex};
+    if (this->onReadDoneCallback) {
+      this->onReadDoneCallback(this->response.payload());
+    }
+    StartRead(&(this->response));
+  }
+
+  void
+  setOnReadDoneCallback(std::function<void(std::string)> onReadDoneCallback) {
+    std::lock_guard<std::mutex> guard{this->onReadDoneCallbackMutex};
+    this->onReadDoneCallback = onReadDoneCallback;
+  }
+
+  void tryCancel() {
+    this->context.TryCancel();
+  }
+};
