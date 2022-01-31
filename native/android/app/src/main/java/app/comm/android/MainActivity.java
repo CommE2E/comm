57e062441dd0968e84c8feaf5cb0474c57291a60
Ashoat Tevosyan <ashoat@comm.app>
2022-01-30 21:48:05 -0500

    [RN0.66] 7/n: Stop using ReactFragmentActivity
    Summary:
    We stopped using `ReactFragmentActivity` in D755, but then brought it back in D1065.
    
    The only thing that's not clear to me is whether we need to implement `onResume` and `onPause` again. I found that in [D755](https://phabricator.ashoat.com/D755?vs=2229&id=2245#inline-3725) @karol-bisztyga indicated they were only necessary for `onReactContextInitialized`, so I think it should be safe to remove them here.
    
    Test Plan: Built and ran the whole stack on iOS and Android across various architectures
    
    Reviewers: karol-bisztyga, palys-swm, atul
    
    Subscribers: Adrian, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D2986

--- native/android/app/src/main/java/app/comm/android/MainActivity.java
+++ native/android/app/src/main/java/app/comm/android/MainActivity.java
@@ -3,14 +3,14 @@ package app.comm.android;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.ReactFragmentActivity;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 import expo.modules.splashscreen.SplashScreenImageResizeMode;
 import expo.modules.splashscreen.singletons.SplashScreen;
 
-public class MainActivity extends ReactFragmentActivity {
+public class MainActivity extends ReactActivity {
 
   /**
    * Returns the name of the main component registered from JavaScript.
