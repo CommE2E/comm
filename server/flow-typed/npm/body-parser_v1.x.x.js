c006173deed1ac5d5ac1f1760126365680ef8210
Ashoat Tevosyan <ashoat@comm.app>
2022-01-30 23:44:57 -0500

    [RN0.66] 27/n: Move imports into module declarations for body-parser
    Summary: The new Flow errors when you import outside of a module declaration.
    
    Test Plan: Flow
    
    Reviewers: palys-swm, atul
    
    Reviewed By: palys-swm
    
    Subscribers: benschac
    
    Differential Revision: https://phabricator.ashoat.com/D3006

--- server/flow-typed/npm/body-parser_v1.x.x.js
+++ server/flow-typed/npm/body-parser_v1.x.x.js
@@ -1,46 +1,49 @@
 // flow-typed signature: bac0ee66e0653772d037dc47b51a5e1f
 // flow-typed version: da30fe6876/body-parser_v1.x.x/flow_>=v0.25.x
 
-import type { Middleware, $Request, $Response } from "express";
-
-declare type bodyParser$Options = {
-  inflate?: boolean,
-  limit?: number | string,
-  type?: string | string[] | ((req: $Request) => any),
-  verify?: (
-    req: $Request,
-    res: $Response,
-    buf: Buffer,
-    encoding: string
-  ) => void
-};
-
-declare type bodyParser$OptionsText = bodyParser$Options & {
-  reviver?: (key: string, value: any) => any,
-  strict?: boolean
-};
-
-declare type bodyParser$OptionsJson = bodyParser$Options & {
-  reviver?: (key: string, value: any) => any,
-  strict?: boolean
-};
-
-declare type bodyParser$OptionsUrlencoded = bodyParser$Options & {
-  extended?: boolean,
-  parameterLimit?: number
-};
-
 declare module "body-parser" {
-  declare type Options = bodyParser$Options;
-  declare type OptionsText = bodyParser$OptionsText;
-  declare type OptionsJson = bodyParser$OptionsJson;
-  declare type OptionsUrlencoded = bodyParser$OptionsUrlencoded;
+  import type { Middleware, $Request, $Response } from "express";
 
-  declare function json(options?: OptionsJson): Middleware;
+  declare type Options = {
+    inflate?: boolean,
+    limit?: number | string,
+    type?: string | string[] | ((req: $Request) => any),
+    verify?: (
+      req: $Request,
+      res: $Response,
+      buf: Buffer,
+      encoding: string
+    ) => void
+  };
 
-  declare function raw(options?: Options): Middleware;
+  declare type OptionsText = Options & {
+    reviver?: (key: string, value: any) => any,
+    strict?: boolean
+  };
 
-  declare function text(options?: OptionsText): Middleware;
+  declare type OptionsJson = Options & {
+    reviver?: (key: string, value: any) => any,
+    strict?: boolean
+  };
 
-  declare function urlencoded(options?: OptionsUrlencoded): Middleware;
+  declare type OptionsUrlencoded = Options & {
+    extended?: boolean,
+    parameterLimit?: number
+  };
+
+  declare function json<Request: $Request, Response: $Response>(
+    options?: OptionsJson,
+  ): Middleware<Request, Response>;
+
+  declare function raw<Request: $Request, Response: $Response>(
+    options?: Options,
+  ): Middleware<Request, Response>;
+
+  declare function text<Request: $Request, Response: $Response>(
+    options?: OptionsText,
+  ): Middleware<Request, Response>;
+
+  declare function urlencoded<Request: $Request, Response: $Response>(
+    options?: OptionsUrlencoded,
+  ): Middleware<Request, Response>;
 }
