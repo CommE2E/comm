9efa1ff71508c0343acca813442b47c447f81de5
Atul Madhugiri <atulm@berkeley.edu>
2022-02-03 15:11:54 -0500

    [CommCoreModule] Queue up `networkModule.get` on JS event loop to match `WebSocket` behavior
    Summary:
    1. Address feedback that we should move the call to `.get()` after the setting of the callbacks.
    2. Wrap the `GlobalNetworkSingleton` in `jsInvoker->invokeAsync(...` to ensure that the `onopen` callback is set from JS before we attempt to open the `GRPCStream`. This should mimic the behavior of JS `WebSocket`: https://stackoverflow.com/a/49211579
    
    Test Plan: Will be tested implicitly with subsequent diffs
    
    Reviewers: ashoat, def-au1t, palys-swm, karol-bisztyga
    
    Reviewed By: ashoat
    
    Subscribers: Adrian, benschac
    
    Differential Revision: https://phabricator.ashoat.com/D3097

--- native/cpp/CommonCpp/grpc/GRPCStreamHostObject.cpp
+++ native/cpp/CommonCpp/grpc/GRPCStreamHostObject.cpp
@@ -48,13 +48,6 @@ GRPCStreamHostObject::GRPCStreamHostObject(
           })},
       jsInvoker{jsInvoker} {
 
-  comm::GlobalNetworkSingleton::instance.scheduleOrRun(
-      [](comm::NetworkModule &networkModule) {
-        networkModule.initializeNetworkModule(
-            "userId-placeholder", "deviceToken-placeholder", "localhost");
-        networkModule.get("sessionID-placeholder");
-      });
-
   auto onReadDoneCallback = [this, &rt](std::string data) {
     this->jsInvoker->invokeAsync([this, &rt, data]() {
       if (this->onmessage.isNull()) {
@@ -109,6 +102,15 @@ GRPCStreamHostObject::GRPCStreamHostObject(
         networkModule.setOnCloseCallback(onCloseCallback);
         networkModule.assignSetReadyStateCallback(setReadyStateCallback);
       });
+
+  this->jsInvoker->invokeAsync([]() {
+    comm::GlobalNetworkSingleton::instance.scheduleOrRun(
+        [](comm::NetworkModule &networkModule) {
+          networkModule.initializeNetworkModule(
+              "userId-placeholder", "deviceToken-placeholder", "localhost");
+          networkModule.get("sessionID-placeholder");
+        });
+  });
 }
 
 std::vector<jsi::PropNameID>
