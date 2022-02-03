d6b18aaeba2cb8698d8d6ea5a879843cebeec868
Atul Madhugiri <atulm@berkeley.edu>
2022-02-03 12:58:45 -0500

    [CommCoreModule] Split `ClientGetReadReactor` `.h` and `.cpp`
    Summary: NA
    
    Test Plan: Copy/paste/still builds
    
    Reviewers: ashoat, palys-swm, def-au1t, varun, karol-bisztyga
    
    Reviewed By: ashoat
    
    Subscribers: Adrian, benschac
    
    Differential Revision: https://phabricator.ashoat.com/D3094

--- native/cpp/CommonCpp/grpc/ClientGetReadReactor.h
+++ native/cpp/CommonCpp/grpc/ClientGetReadReactor.h
@@ -21,42 +21,13 @@ class ClientGetReadReactor
 public:
   ClientGetReadReactor(
       tunnelbroker::TunnelbrokerService::Stub *stub,
-      std::string sessionID)
-      : sessionID{sessionID}, request{} {
-    request.set_sessionid(sessionID);
-    stub->async()->Get(&(this->context), &(this->request), this);
-    StartRead(&(this->response));
-    StartCall();
-  }
+      std::string sessionID);
 
-  void OnReadDone(bool ok) override {
-    if (!ok) {
-      return;
-    }
-    std::lock_guard<std::mutex> guard{this->onReadDoneCallbackMutex};
-    if (this->onReadDoneCallback) {
-      this->onReadDoneCallback(this->response.payload());
-    }
-    StartRead(&(this->response));
-  }
+  void OnReadDone(bool ok) override;
+  void close();
 
+  void setOnOpenCallback(std::function<void()> onOpenCallback);
   void
-  setOnReadDoneCallback(std::function<void(std::string)> onReadDoneCallback) {
-    std::lock_guard<std::mutex> guard{this->onReadDoneCallbackMutex};
-    this->onReadDoneCallback = onReadDoneCallback;
-  }
-
-  void setOnOpenCallback(std::function<void()> onOpenCallback) {
-    std::lock_guard<std::mutex> guard{this->onOpenCallbackMutex};
-    this->onOpenCallback = onOpenCallback;
-  }
-
-  void setOnCloseCallback(std::function<void()> onCloseCallback) {
-    std::lock_guard<std::mutex> guard{this->onCloseCallbackMutex};
-    this->onCloseCallback = onCloseCallback;
-  }
-
-  void close() {
-    this->context.TryCancel();
-  }
+  setOnReadDoneCallback(std::function<void(std::string)> onReadDoneCallback);
+  void setOnCloseCallback(std::function<void()> onCloseCallback);
 };
