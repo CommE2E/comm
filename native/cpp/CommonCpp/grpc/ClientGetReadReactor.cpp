4a1b6690ddad52055356b22a841036fe16b0907a
Atul Madhugiri <atulm@berkeley.edu>
2022-02-03 12:58:45 -0500

    [CommCoreModule] Split `ClientGetReadReactor` `.h` and `.cpp`
    Summary: NA
    
    Test Plan: Copy/paste/still builds
    
    Reviewers: ashoat, palys-swm, def-au1t, varun, karol-bisztyga
    
    Reviewed By: ashoat
    
    Subscribers: Adrian, benschac
    
    Differential Revision: https://phabricator.ashoat.com/D3094

---
+++ native/cpp/CommonCpp/grpc/ClientGetReadReactor.cpp
@@ -0,0 +1,44 @@
+#include "ClientGetReadReactor.h"
+
+ClientGetReadReactor::ClientGetReadReactor(
+    tunnelbroker::TunnelbrokerService::Stub *stub,
+    std::string sessionID)
+    : sessionID{sessionID}, request{} {
+  request.set_sessionid(sessionID);
+  stub->async()->Get(&(this->context), &(this->request), this);
+  StartRead(&(this->response));
+  StartCall();
+}
+
+void ClientGetReadReactor::OnReadDone(bool ok) {
+  if (!ok) {
+    return;
+  }
+  std::lock_guard<std::mutex> guard{this->onReadDoneCallbackMutex};
+  if (this->onReadDoneCallback) {
+    this->onReadDoneCallback(this->response.payload());
+  }
+  StartRead(&(this->response));
+}
+
+void ClientGetReadReactor::close() {
+  this->context.TryCancel();
+}
+
+void ClientGetReadReactor::setOnOpenCallback(
+    std::function<void()> onOpenCallback) {
+  std::lock_guard<std::mutex> guard{this->onOpenCallbackMutex};
+  this->onOpenCallback = onOpenCallback;
+}
+
+void ClientGetReadReactor::setOnReadDoneCallback(
+    std::function<void(std::string)> onReadDoneCallback) {
+  std::lock_guard<std::mutex> guard{this->onReadDoneCallbackMutex};
+  this->onReadDoneCallback = onReadDoneCallback;
+}
+
+void ClientGetReadReactor::setOnCloseCallback(
+    std::function<void()> onCloseCallback) {
+  std::lock_guard<std::mutex> guard{this->onCloseCallbackMutex};
+  this->onCloseCallback = onCloseCallback;
+}
