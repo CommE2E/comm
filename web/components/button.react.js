63c137107764589f1e337c80c59296acaf2cb616
Benjamin Schachter <benschac@gmail.com>
2022-02-09 15:19:16 -0500

    [web] button add, disabled and onClick
    Summary: add additional types to button
    
    Test Plan: make sure types are correct
    
    Reviewers: atul, ashoat
    
    Reviewed By: atul, ashoat
    
    Subscribers: ashoat, palys-swm, Adrian, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D3093

--- web/components/button.react.js
+++ web/components/button.react.js
@@ -6,17 +6,27 @@ import * as React from 'react';
 import css from './button.css';
 
 type Props = {
-  +onClick: () => mixed,
+  +onClick: (event: SyntheticEvent<HTMLButtonElement>) => void,
   +children: React.Node,
-  +variant?: 'round',
+  +variant?: 'primary' | 'round',
+  +type?: string,
+  +disabled?: boolean,
 };
 
 function Button(props: Props): React.Node {
-  const { onClick, children, variant } = props;
-  const btnCls = classnames(css.btn, { [css.round]: variant === 'round' });
+  const { onClick, children, variant, type, disabled = false } = props;
+  const btnCls = classnames(css.btn, {
+    [css.round]: variant === 'round',
+    [css.primary]: variant === 'primary',
+  });
 
   return (
-    <button className={btnCls} onClick={onClick}>
+    <button
+      type={type}
+      className={btnCls}
+      onClick={onClick}
+      disabled={disabled}
+    >
       {children}
     </button>
   );
