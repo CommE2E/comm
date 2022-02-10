1bf072f6166ce9bc18d4f4e9a2e327c37c3973f5
Benjamin Schachter <benschac@gmail.com>
2022-02-10 11:28:20 -0500

    [web] [remove] lastActivity from thread

--- web/chat/sidebar-item.react.js
+++ web/chat/sidebar-item.react.js
@@ -4,9 +4,7 @@ import classNames from 'classnames';
 import * as React from 'react';
 
 import type { SidebarInfo } from 'lib/types/thread-types';
-import { shortAbsoluteDate } from 'lib/utils/date-utils';
 
-import { useSelector } from '../redux/redux-utils';
 import { useOnClickThread } from '../selectors/nav-selectors';
 import SWMansionIcon from '../SWMansionIcon.react';
 import css from './chat-thread-list.css';
@@ -15,16 +13,12 @@ type Props = {
   +sidebarInfo: SidebarInfo,
 };
 function SidebarItem(props: Props): React.Node {
-  const { threadInfo, lastUpdatedTime } = props.sidebarInfo;
+  const { threadInfo } = props.sidebarInfo;
   const threadID = threadInfo.id;
-
   const onClick = useOnClickThread(threadID);
-
-  const timeZone = useSelector(state => state.timeZone);
-  const lastActivity = shortAbsoluteDate(lastUpdatedTime, timeZone);
-
   const { unread } = threadInfo.currentUser;
   const unreadCls = classNames(css.sidebarTitle, { [css.unread]: unread });
+
   return (
     <>
       <SWMansionIcon icon="right-angle-arrow" size={28} />
@@ -32,14 +26,6 @@ function SidebarItem(props: Props): React.Node {
       <a className={css.threadButtonSidebar} onClick={onClick}>
         <div className={css.threadRow}>
           <div className={unreadCls}>{threadInfo.uiName}</div>
-          <div
-            className={classNames([
-              css.sidebarLastActivity,
-              unread ? css.black : css.dark,
-            ])}
-          >
-            {lastActivity}
-          </div>
         </div>
       </a>
     </>
