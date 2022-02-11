b9a9d2e87882f0907199009ea13d7634704a4bd8
Benjamin Schachter <benschac@gmail.com>
2022-02-11 14:02:56 -0500

    [web] [darkmode] unread button
    Summary: theme unread button, no design in web. Generally just theme'ed with our color palette.
    
    Test Plan: screen shots
    
    Reviewers: atul, ashoat
    
    Subscribers: palys-swm, Adrian, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D3181

--- web/components/button.react.js
+++ web/components/button.react.js
@@ -8,7 +8,7 @@ import css from './button.css';
 type Props = {
   +onClick: (event: SyntheticEvent<HTMLButtonElement>) => void,
   +children: React.Node,
-  +variant?: 'primary' | 'secondary' | 'danger' | 'round',
+  +variant?: 'primary' | 'secondary' | 'danger' | 'round' | 'unread',
   +type?: string,
   +disabled?: boolean,
   +className?: string,
