53f9198c9144dc1a6845993387620363ea31fabf
Benjamin Schachter <benschac@gmail.com>
2022-02-14 15:28:08 -0500

    [web] [refactor] move modal state to context and a hook
    Summary: Add modal state to a hook and context.
    
    Test Plan: Nothing to test yet. Going to hook this context up into the app in a following diff
    
    Reviewers: atul
    
    Subscribers: ashoat, palys-swm, Adrian, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D3195

---
+++ web/modals/modal/modal-context.js
@@ -0,0 +1,40 @@
+// @flow
+
+import * as React from 'react';
+
+import useModal from './use-modal.react';
+
+type Props = {
+  +children: React.Node,
+};
+
+type ModalContextType = {
+  +handleModal: (component?: React.Node) => void,
+  +clearModal: () => void,
+  +modal: ?React.Node,
+};
+
+const ModalContext: React.Context<?ModalContextType> = React.createContext<?ModalContextType>(
+  { modal: null, clearModal: () => {}, handleModal: () => {} },
+);
+
+function ModalProvider(props: Props): React.Node {
+  const { children } = props;
+  const { handleModal, modal, clearModal } = useModal();
+
+  const modalContext = React.useMemo(
+    () => ({
+      handleModal,
+      clearModal,
+      modal,
+    }),
+    [handleModal, modal, clearModal],
+  );
+  return (
+    <ModalContext.Provider value={modalContext}>
+      {children}
+    </ModalContext.Provider>
+  );
+}
+
+export { ModalContext, ModalProvider };
