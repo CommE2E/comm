ccd8447d1f48a3711f1ba4b70de3d4edff055f68
marcinwasowicz <marcinkwasowicz@gmail.com>
2022-02-14 17:49:23 +0100

    Use reactApplicationContext and reflection mechanism to access Expo Secure Store from Java without disabling it from JavaScript
    Summary: Solution in previous differential to access Expo Secure Store from Java removed its accessibility from JavaScript. It also made MainApplication.java code much compilcated and full of hacks. Solution proposed in this diff allows us to touch the same Expo Secure Store from Java that is callable from JavaScript. Additionally as confirmed with expo team member it is more stable in terms of Expo's internal implementation since Expo team is planning to remove some classes that previous solution uses.
    
    Test Plan: Build an application and see it is wokring correctly. Additionally it was valuable to investigate logs since they contain information that secure store is correctly initialized and working (log regarding encryption key for local storage encryption)
    
    Reviewers: palys-swm, karol-bisztyga
    
    Subscribers: ashoat, Adrian, atul
    
    Differential Revision: https://phabricator.ashoat.com/D3193

--- native/android/app/src/main/java/app/comm/android/CommCoreJSIModulePackage.java
+++ native/android/app/src/main/java/app/comm/android/CommCoreJSIModulePackage.java
@@ -1,10 +1,16 @@
 package app.comm.android;
 
+import android.util.Log;
 import app.comm.android.fbjni.CommHybrid;
+import app.comm.android.fbjni.CommSecureStore;
 import com.facebook.react.bridge.JSIModuleSpec;
 import com.facebook.react.bridge.JavaScriptContextHolder;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.swmansion.reanimated.ReanimatedJSIModulePackage;
+import expo.modules.adapters.react.NativeModulesProxy;
+import expo.modules.core.ModuleRegistry;
+import expo.modules.securestore.SecureStoreModule;
+import java.lang.reflect.Field;
 import java.util.Collections;
 import java.util.List;
 
@@ -14,6 +20,25 @@ public class CommCoreJSIModulePackage extends ReanimatedJSIModulePackage {
   public List<JSIModuleSpec> getJSIModules(
       ReactApplicationContext reactApplicationContext,
       JavaScriptContextHolder jsContext) {
+    ModuleRegistry moduleRegistry;
+    try {
+      Field moduleRegistryField =
+          NativeModulesProxy.class.getDeclaredField("mModuleRegistry");
+      moduleRegistryField.setAccessible(true);
+      NativeModulesProxy proxy =
+          (NativeModulesProxy)reactApplicationContext.getCatalystInstance()
+              .getNativeModule("NativeUnimoduleProxy");
+      moduleRegistry = (ModuleRegistry)moduleRegistryField.get(proxy);
+    } catch (Exception e) {
+      throw new RuntimeException(
+          "Accessing expo modules registry resulted in an error: " +
+          Log.getStackTraceString(e) +
+          "This might be due to changes in expo's internal implementation.");
+    }
+    SecureStoreModule secureStoreModule =
+        (SecureStoreModule)moduleRegistry.getExportedModuleOfClass(
+            SecureStoreModule.class);
+    CommSecureStore.getInstance().initialize(secureStoreModule);
     CommHybrid.initHybrid(reactApplicationContext);
     super.getJSIModules(reactApplicationContext, jsContext);
     return Collections.emptyList();
