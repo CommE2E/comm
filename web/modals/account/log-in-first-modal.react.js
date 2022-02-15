ab85f198dbd8c2a18346e3394c1f8a010cd92784
Benjamin Schachter <benschac@gmail.com>
2022-02-15 11:14:03 -0500

    [web] [refactor] remove setModal from LoginModal

--- web/modals/account/log-in-first-modal.react.js
+++ web/modals/account/log-in-first-modal.react.js
@@ -1,44 +1,46 @@
 // @flow
 
+import invariant from 'invariant';
 import * as React from 'react';
 
 import css from '../../style.css';
 import Modal from '../modal.react';
+import { ModalContext } from '../modal/modal-context';
 import LogInModal from './log-in-modal.react';
-
 type Props = {
   +inOrderTo: string,
-  +setModal: (modal: ?React.Node) => void,
 };
-class LogInFirstModal extends React.PureComponent<Props> {
-  render(): React.Node {
-    return (
-      <Modal name="Log in" onClose={this.clearModal}>
-        <div className={css['modal-body']}>
-          <p>
-            {`In order to ${this.props.inOrderTo}, you'll first need to `}
-            <a
-              href="#"
-              className={css['show-login-modal']}
-              onClick={this.onClickLogIn}
-            >
-              log in
-            </a>
-            {'.'}
-          </p>
-        </div>
-      </Modal>
-    );
-  }
 
-  clearModal: () => void = () => {
-    this.props.setModal(null);
-  };
+function LogInFirstModal(props: Props): React.Node {
+  const { inOrderTo } = props;
+  const modalContext = React.useContext(ModalContext);
+  invariant(modalContext, 'modalContext');
 
-  onClickLogIn: (event: SyntheticEvent<HTMLAnchorElement>) => void = event => {
-    event.preventDefault();
-    this.props.setModal(<LogInModal setModal={this.props.setModal} />);
-  };
+  const onClickLogIn = React.useCallback(
+    (event: SyntheticEvent<HTMLAnchorElement>) => {
+      event.preventDefault();
+      this.props.setModal(<LogInModal />);
+    },
+    [],
+  );
+
+  return (
+    <Modal name="Log in" onClose={modalContext.clearModal}>
+      <div className={css['modal-body']}>
+        <p>
+          {`In order to ${inOrderTo}, you'll first need to `}
+          <a
+            href="#"
+            className={css['show-login-modal']}
+            onClick={onClickLogIn}
+          >
+            log in
+          </a>
+          {'.'}
+        </p>
+      </div>
+    </Modal>
+  );
 }
 
 export default LogInFirstModal;
