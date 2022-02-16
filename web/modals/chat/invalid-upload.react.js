1a5918ba64148d9bc5b6b49c52207021c0bc2ab5
Benjamin Schachter <benschac@gmail.com>
2022-02-16 10:33:03 -0500

    [web] [refactor] remove setModal prop drill from Invalid upload modal
    Summary: functionality should work as expected
    
    Test Plan: upload content and prompt modal
    
    Reviewers: atul
    
    Subscribers:

--- web/modals/chat/invalid-upload.react.js
+++ web/modals/chat/invalid-upload.react.js
@@ -1,27 +1,23 @@
 // @flow
 
+import invariant from 'invariant';
 import * as React from 'react';
 
+import { ModalContext } from '../../modals/modal-provider.react';
 import css from '../../style.css';
 import Modal from '../modal.react';
 
-type Props = {
-  +setModal: (modal: ?React.Node) => void,
-};
-class InvalidUploadModal extends React.PureComponent<Props> {
-  render(): React.Node {
-    return (
-      <Modal name="Invalid upload" onClose={this.clearModal}>
-        <div className={css['modal-body']}>
-          <p>We don&apos;t support that file type yet :(</p>
-        </div>
-      </Modal>
-    );
-  }
+function InvalidUploadModal(): React.Node {
+  const modalContext = React.useContext(ModalContext);
+  invariant(modalContext, 'ModalContext not found');
 
-  clearModal: () => void = () => {
-    this.props.setModal(null);
-  };
+  return (
+    <Modal name="Invalid upload" onClose={modalContext.clearModal}>
+      <div className={css['modal-body']}>
+        <p>We don&apos;t support that file type yet :(</p>
+      </div>
+    </Modal>
+  );
 }
 
 export default InvalidUploadModal;
