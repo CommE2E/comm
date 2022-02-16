ae1459a503560a24fd30d573b6c2edefa203a11f
Benjamin Schachter <benschac@gmail.com>
2022-02-16 10:23:05 -0500

    [web] [refactor] remove setModal from LeftLayoutAside, swap with context
    Summary: remove setModal and use context
    
    Test Plan: UserSettings modal works as expected
    
    Reviewers: atul
    
    Subscribers: ashoat, palys-swm, Adrian, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D3222

--- web/sidebar/community-picker.react.js
+++ web/sidebar/community-picker.react.js
@@ -1,20 +1,21 @@
 // @flow
 
+import invariant from 'invariant';
 import * as React from 'react';
 
 import Button from '../components/button.react';
 import UserSettingsModal from '../modals/account/user-settings-modal.react.js';
+import { ModalContext } from '../modals/modal-provider.react';
 import SWMansionIcon from '../SWMansionIcon.react';
 import css from './community-picker.css';
 
-type Props = { +setModal: (modal: ?React.Node) => void };
-
-function CommunityPicker(props: Props): React.Node {
-  const { setModal } = props;
+function CommunityPicker(): React.Node {
+  const modalContext = React.useContext(ModalContext);
+  invariant(modalContext, 'ModalContext not found');
 
   const setModalToUserSettings = React.useCallback(() => {
-    setModal(<UserSettingsModal />);
-  }, [setModal]);
+    modalContext.setModal(<UserSettingsModal />);
+  }, [modalContext]);
 
   return (
     <div className={css.container}>
