ef04f74b6f13e7d447320d0a89e0cfea0969dda7
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-02-18 13:47:34 +0100

    [services] Backup - authentication handlers
    Summary: todo
    
    Test Plan: todo
    
    Reviewers:
    
    Subscribers:

---
+++ services/backup/docker-server/contents/server/src/Authentication/PakeAuthenticationHandler.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "AuthenticationHandler.h"
+
+#include <string>
+
+namespace comm {
+namespace network {
+namespace crypto {
+
+// enum class AuthenticationState {
+//   NOT_STARTED = 1,
+//   IN_PROGRESS = 2,
+//   FINISHED_SUCCESS = 3,
+//   FINISHED_FAIL = 4,
+// };
+
+class PakeAuthenticationHandler : public AuthenticationHandler {
+  // AuthenticationState state = AuthenticationState::NOT_STARTED;
+public:
+  std::string processRequest(const std::string &data) override;
+};
+
+} // namespace crypto
+} // namespace network
+} // namespace comm
---
+++ services/backup/docker-server/contents/server/src/Authentication/WalletAuthenticationHandler.cpp
@@ -0,0 +1,14 @@
+#include "WalletAuthenticationHandler.h"
+
+namespace comm {
+namespace network {
+namespace crypto {
+
+std::string
+WalletAuthenticationHandler::processRequest(const std::string &data) {
+  return "";
+}
+
+} // namespace crypto
+} // namespace network
+} // namespace comm
