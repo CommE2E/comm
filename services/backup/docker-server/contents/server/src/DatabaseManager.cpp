3415a052e8f2ff1c8f00095f6e59e973cb84dc62
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-02-22 10:27:46 +0100

    [services] Backup - add Database Manager base
    Summary:
    Base/inner functions for the database manager. This diff may be beneficial for this task https://linear.app/comm/issue/ENG-324/avoid-repetitions-in-services-code because this is the mutual code that may be put in a separate place.
    
    Depends on D3081
    
    Test Plan:
    ```
    cd services
    yarn run-backup-service
    ```
    Still builds
    
    Reviewers: palys-swm, geekbrother, varun, ashoat, jimpo
    
    Reviewed By: palys-swm, ashoat
    
    Subscribers: benschac, ashoat, palys-swm, Adrian, atul
    
    Differential Revision: https://phabricator.ashoat.com/D3082

---
+++ services/backup/docker-server/contents/server/src/DatabaseManager.cpp
@@ -0,0 +1,47 @@
+#include "DatabaseManager.h"
+#include "Constants.h"
+
+#include <aws/core/utils/Outcome.h>
+#include <aws/dynamodb/model/QueryRequest.h>
+#include <aws/dynamodb/model/ScanRequest.h>
+#include <aws/dynamodb/model/DeleteItemRequest.h>
+
+#include <iostream>
+
+namespace comm {
+namespace network {
+namespace database {
+
+DatabaseManager &DatabaseManager::getInstance() {
+  static DatabaseManager instance;
+  return instance;
+}
+
+void DatabaseManager::innerPutItem(
+    std::shared_ptr<Item> item,
+    const Aws::DynamoDB::Model::PutItemRequest &request) {
+  const Aws::DynamoDB::Model::PutItemOutcome outcome =
+      getDynamoDBClient()->PutItem(request);
+  if (!outcome.IsSuccess()) {
+    throw std::runtime_error(outcome.GetError().GetMessage());
+  }
+}
+
+void DatabaseManager::innerRemoveItem(
+    const Item &item,
+    const std::string &key) {
+  Aws::DynamoDB::Model::DeleteItemRequest request;
+  request.SetTableName(item.getTableName());
+  request.AddKey(
+      item.getPrimaryKey(), Aws::DynamoDB::Model::AttributeValue(key));
+
+  const Aws::DynamoDB::Model::DeleteItemOutcome &outcome =
+      getDynamoDBClient()->DeleteItem(request);
+  if (!outcome.IsSuccess()) {
+    throw std::runtime_error(outcome.GetError().GetMessage());
+  }
+}
+
+} // namespace database
+} // namespace network
+} // namespace comm
---
+++ services/backup/docker-server/contents/server/src/DatabaseManager.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "AwsTools.h"
+#include "DatabaseEntitiesTools.h"
+
+#include <aws/core/Aws.h>
+#include <aws/dynamodb/model/AttributeDefinition.h>
+#include <aws/dynamodb/model/GetItemRequest.h>
+#include <aws/dynamodb/model/PutItemRequest.h>
+
+#include <memory>
+#include <string>
+
+#ifdef COMM_SERVICES_DEV_MODE
+#include "DatabaseSimulator.h"
+#endif
+
+namespace comm {
+namespace network {
+namespace database {
+
+// this class should be thread-safe in case any shared resources appear
+class DatabaseManager {
+#ifdef COMM_SERVICES_DEV_MODE
+  DatabaseSimulator dbSimulator;
+#endif
+
+  void innerPutItem(
+      std::shared_ptr<Item> item,
+      const Aws::DynamoDB::Model::PutItemRequest &request);
+
+  template <typename T>
+  std::shared_ptr<T>
+  innerFindItem(Aws::DynamoDB::Model::GetItemRequest &request);
+  void innerRemoveItem(const Item &item, const std::string &key);
+
+public:
+  static DatabaseManager &getInstance();
+};
+
+template <typename T>
+std::shared_ptr<T>
+DatabaseManager::innerFindItem(Aws::DynamoDB::Model::GetItemRequest &request) {
+  std::shared_ptr<T> item = createItemByType<T>();
+  request.SetTableName(item->getTableName());
+  const Aws::DynamoDB::Model::GetItemOutcome &outcome =
+      getDynamoDBClient()->GetItem(request);
+  if (!outcome.IsSuccess()) {
+    throw std::runtime_error(outcome.GetError().GetMessage());
+  }
+  const AttributeValues &outcomeItem = outcome.GetResult().GetItem();
+  if (!outcomeItem.size()) {
+    return nullptr;
+  }
+  item->assignItemFromDatabase(outcomeItem);
+  return item;
+}
+
+} // namespace database
+} // namespace network
+} // namespace comm
