8a2c688bfdea996fa17a5017d4541bcd6d6788a8
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-02-22 10:28:11 +0100

    [services] Backup - Fix primary key logic
    Summary:
    Based on my research about the dynamo DB: https://phabricator.ashoat.com/D2950#81654
    
    - Every DynamoDB table has a unique primary key
    - A primary key `[PKSK]` must be composed of a partition key `[PK]`, and can optionally have a sort key `[SK]`.
    - A GetItem request returns one and only one item using its unique primary key `[PKSK]`.
    - A Query does a fast lookup and must specify one and only one partition key `[PK]`. It can return multiple items.
    - A Scan evaluates every item in a table and may return a subset based on filter parameters. Scans are the correct choice in some circumstances but can be slow and costly if used incorrectly.
    
    Depends on D3176
    
    Test Plan: Backup service still builds
    
    Reviewers: palys-swm, geekbrother, varun, ashoat, jimpo
    
    Reviewed By: palys-swm, ashoat
    
    Subscribers: benschac, ashoat, palys-swm, Adrian, atul
    
    Differential Revision: https://phabricator.ashoat.com/D3177

--- services/backup/docker-server/contents/server/src/DatabaseEntities/Item.h
+++ services/backup/docker-server/contents/server/src/DatabaseEntities/Item.h
@@ -3,6 +3,7 @@
 #include <aws/core/Aws.h>
 #include <aws/dynamodb/DynamoDBClient.h>
 
+#include <memory>
 #include <string>
 
 namespace comm {
@@ -12,12 +13,34 @@ namespace database {
 typedef Aws::Map<Aws::String, Aws::DynamoDB::Model::AttributeValue>
     AttributeValues;
 
+struct PrimaryKeyBase {
+  PrimaryKeyBase(const std::string partitionKey)
+      : partitionKey(partitionKey), sortKey(nullptr) {
+  }
+  PrimaryKeyBase(const std::string partitionKey, const std::string sortKey)
+      : partitionKey(partitionKey),
+        sortKey(std::make_unique<std::string>(sortKey)) {
+  }
+
+  const std::string partitionKey;
+  std::unique_ptr<std::string> sortKey;
+};
+
+struct PrimaryKey : PrimaryKeyBase {
+  using PrimaryKeyBase::PrimaryKeyBase;
+};
+
+struct PrimaryKeyValue : PrimaryKeyBase {
+  using PrimaryKeyBase::PrimaryKeyBase;
+};
+
 class Item {
   virtual void validate() const = 0;
 
 public:
   virtual std::string getTableName() const = 0;
-  virtual std::string getPrimaryKey() const = 0;
+  virtual PrimaryKey getPrimaryKey() const = 0;
+  virtual PrimaryKeyValue getPrimaryKeyValue() const = 0;
   virtual void assignItemFromDatabase(const AttributeValues &itemFromDB) = 0;
 };
 
