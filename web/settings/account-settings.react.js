18dec803fe3afefa1ef764572da700acb1b101d2
Tomasz Palys <tomasz.palys@swmansion.com>
2022-02-25 16:39:15 +0100

    [web] Add logged in section
    Summary:
    Display a label and username
    https://www.figma.com/file/a1nkbWgbgjRlrOY9LVurTz/Comm-%2F-Desktop-app?node-id=1272%3A88750
    
    {F17278}
    {F17327}
    
    Depends on D3320
    
    Test Plan: Render the page instead of Calendar and check if it matches the design and if current username was displayed
    
    Reviewers: benschac, atul, def-au1t
    
    Reviewed By: benschac, atul
    
    Subscribers: ashoat, Adrian, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D3321

--- web/settings/account-settings.react.js
+++ web/settings/account-settings.react.js
@@ -2,12 +2,29 @@
 
 import * as React from 'react';
 
+import { useSelector } from '../redux/redux-utils';
 import css from './account-settings.css';
 
 function AccountSettings(): React.Node {
+  const currentUserInfo = useSelector(state => state.currentUserInfo);
+  if (!currentUserInfo || currentUserInfo.anonymous) {
+    return null;
+  }
+  const { username } = currentUserInfo;
+
   return (
     <div className={css.container}>
       <h4 className={css.header}>My Account</h4>
+      <div className={css.content}>
+        <ul>
+          <li>
+            <p className={css.logoutContainer}>
+              <span className={css.logoutLabel}>{'Logged in as '}</span>
+              <span className={css.username}>{username}</span>
+            </p>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
