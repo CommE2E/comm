7c1fe3eafb4c4c49993276eea4267a9b2bf0cf6f
Tomasz Palys <tomasz.palys@swmansion.com>
2022-02-25 16:29:12 +0100

    [web] Introduce empty account page
    Summary:
    An empty page with only a header. This page is not rendered yet, so there are no visible changes in the app. The rendering will be handled in much later diff.
    Designs can be found here: https://www.figma.com/file/a1nkbWgbgjRlrOY9LVurTz/Comm-%2F-Desktop-app?node-id=1272%3A88750
    
    {F17253}
    
    Test Plan: Display the page instead of e.g. Calendar and check if it is displayed correctly
    
    Reviewers: benschac, atul, def-au1t
    
    Reviewed By: benschac, atul
    
    Subscribers: ashoat, Adrian, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D3320

---
+++ web/settings/account-settings.react.js
@@ -0,0 +1,15 @@
+// @flow
+
+import * as React from 'react';
+
+import css from './account-settings.css';
+
+function AccountSettings(): React.Node {
+  return (
+    <div className={css.container}>
+      <h4 className={css.header}>My Account</h4>
+    </div>
+  );
+}
+
+export default AccountSettings;
