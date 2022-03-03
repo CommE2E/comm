70f06c8f49df3c4811803643777b9390d4067cd6
Tomasz Palys <tomasz.palys@swmansion.com>
2022-03-03 17:50:25 +0100

    [web] Reintroduce AppSwitcher
    Summary:
    This component renders a reusable component. The reusability will be achieved in next diffs by modifying `NavigationPanel` so that it receives navigation elements.
    Depends on D3338
    
    Test Plan: Check if AppSwitcher continues to work correctly
    
    Reviewers: benschac, atul, def-au1t
    
    Reviewed By: benschac, atul
    
    Subscribers: ashoat, Adrian, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D3339

---
+++ web/sidebar/app-switcher.react.js
@@ -0,0 +1,11 @@
+// @flow
+
+import * as React from 'react';
+
+import NavigationPanel from './navigation-panel.react';
+
+function AppSwitcher(): React.Node {
+  return <NavigationPanel />;
+}
+
+export default AppSwitcher;
