adc557bce79874b5da7e03eeea3f5488bb891ea2
Tomasz Palys <tomasz.palys@swmansion.com>
2022-03-03 18:18:08 +0100

    [web] Export navigation tab types
    Summary:
    These will be used in a couple of places
    Depends on D3339
    Depends on D3336
    
    Test Plan: Just check if opening chat and calendar still works
    
    Reviewers: benschac, atul, def-au1t
    
    Reviewed By: benschac, atul
    
    Subscribers: ashoat, Adrian, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D3340

--- web/types/nav-types.js
+++ web/types/nav-types.js
@@ -3,9 +3,11 @@
 import type { BaseNavInfo } from 'lib/types/nav-types';
 import type { ThreadInfo } from 'lib/types/thread-types';
 
+export type NavigationTab = 'calendar' | 'chat' | 'apps' | 'settings';
+
 export type NavInfo = {
   ...$Exact<BaseNavInfo>,
-  +tab: 'calendar' | 'chat' | 'apps' | 'settings',
+  +tab: NavigationTab,
   +activeChatThreadID: ?string,
   +pendingThread?: ThreadInfo,
   +settingsSection?: 'account',
