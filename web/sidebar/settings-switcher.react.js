72cb54a08d6338ba8d184edc485d31f0c68c8299
Tomasz Palys <tomasz.palys@swmansion.com>
2022-03-04 18:17:54 +0100

    [web] Navigate to the settings on click
    Summary:
    Navigate a user to account settings when an option is clicked.
    Depends on D3342
    
    Test Plan: Render the component instead of `AppSwitcher`. Open chat route - the `My Account` button shouldn't be highlighted. Click it - verify that the button is now highlighted and the the current route is `settings/account`.
    
    Reviewers: benschac, atul, def-au1t
    
    Reviewed By: benschac, atul
    
    Subscribers: ashoat, Adrian, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D3343

--- web/sidebar/settings-switcher.react.js
+++ web/sidebar/settings-switcher.react.js
@@ -1,17 +1,31 @@
 // @flow
 
 import * as React from 'react';
+import { useDispatch } from 'react-redux';
 
+import { updateNavInfoActionType } from '../types/nav-types';
 import NavigationPanel from './navigation-panel.react';
 
 function SettingsSwitcher(): React.Node {
+  const dispatch = useDispatch();
+  const onClickAccountSettings = React.useCallback(
+    (event: SyntheticEvent<HTMLAnchorElement>) => {
+      event.preventDefault();
+      dispatch({
+        type: updateNavInfoActionType,
+        payload: { tab: 'settings', settingsSection: 'account' },
+      });
+    },
+    [dispatch],
+  );
+
   const accountSettingsNavigationItem = React.useMemo(
     () => (
       <p>
-        <a>My Account</a>
+        <a onClick={onClickAccountSettings}>My Account</a>
       </p>
     ),
-    [],
+    [onClickAccountSettings],
   );
 
   return (
