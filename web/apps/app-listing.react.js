1db6f07a77f2a1e6c913aa935b4e9669987b4ee4
Jacek Nitychoruk <jacek.nitychoruk@swmansion.com>
2022-03-07 15:40:07 +0100

    [web] Introduce Apps page
    Summary:
    Introduce Apps panel similar to existing in native app.
    FontAwesome icons will soon be replaced by SWMIcons in follow-up diff.
    
    {F15735}
    
    Test Plan: Run web and confirm that switching calendar on and off works
    
    Reviewers: palys-swm, benschac, atul
    
    Reviewed By: palys-swm, benschac
    
    Subscribers: ashoat, Adrian, atul, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D3266

---
+++ web/apps/app-listing.react.js
@@ -0,0 +1,83 @@
+// @flow
+
+import { faCheckCircle } from '@fortawesome/free-regular-svg-icons';
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classnames from 'classnames';
+import * as React from 'react';
+import { useDispatch } from 'react-redux';
+
+import {
+  disableAppActionType,
+  enableAppActionType,
+} from 'lib/reducers/enabled-apps-reducer';
+import type { SupportedApps } from 'lib/types/enabled-apps';
+
+import SWMansionIcon from '../SWMansionIcon.react';
+import css from './apps.css';
+
+type Props = {
+  +id: SupportedApps | 'chat',
+  +readOnly: boolean,
+  +enabled: boolean,
+  +name: string,
+  +icon: 'message-square' | 'calendar',
+  +copy: string,
+};
+
+function AppListing(props: Props): React.Node {
+  const { id, readOnly, enabled, name, icon, copy } = props;
+  const dispatch = useDispatch();
+
+  const switchAppState = React.useCallback(
+    () =>
+      dispatch({
+        type: enabled ? disableAppActionType : enableAppActionType,
+        payload: id,
+      }),
+    [dispatch, enabled, id],
+  );
+
+  const actionButton = React.useMemo(() => {
+    const switchIcon = enabled ? faCheckCircle : faPlusCircle;
+    if (readOnly) {
+      const readOnlyIconClasses = classnames(
+        css.appListingIcon,
+        css.appListingIconState,
+        css.iconReadOnly,
+      );
+      return (
+        <div className={readOnlyIconClasses}>
+          <FontAwesomeIcon icon={switchIcon} />
+        </div>
+      );
+    }
+    const iconClasses = classnames(
+      css.appListingIcon,
+      css.appListingIconState,
+      {
+        [css.iconEnabled]: enabled,
+        [css.iconDisabled]: !enabled,
+      },
+    );
+    return (
+      <div className={iconClasses} onClick={switchAppState}>
+        <FontAwesomeIcon icon={enabled ? faCheckCircle : faPlusCircle} />
+      </div>
+    );
+  }, [enabled, readOnly, switchAppState]);
+  return (
+    <div className={css.appListingContainer}>
+      <div className={css.appListingIcon}>
+        <SWMansionIcon icon={icon} size={20} />
+      </div>
+      <div className={css.appListingTextContainer}>
+        <h5 className={css.appName}>{name}</h5>
+        <small className={css.appCopy}>{copy}</small>
+      </div>
+      {actionButton}
+    </div>
+  );
+}
+
+export default AppListing;
--- web/apps/apps-directory.react.js
+++ web/apps/apps-directory.react.js
@@ -1,9 +1,56 @@
 // @flow
 
 import * as React from 'react';
+import { useSelector } from 'react-redux';
+
+import AppListing from './app-listing.react';
+import css from './apps.css';
+
+const APP_DIRECTORY_DATA = [
+  {
+    id: 'chat',
+    defaultEnabled: true,
+    readOnly: true,
+    name: 'Chat',
+    icon: 'message-square',
+    copy: 'Keep in touch with your community',
+  },
+  {
+    id: 'calendar',
+    defaultEnabled: true,
+    readOnly: false,
+    name: 'Calendar',
+    icon: 'calendar',
+    copy: 'Shared calendar for your community',
+  },
+];
 
 function AppsDirectory(): React.Node {
-  return <div style={{ color: 'white' }}>Apps directory</div>;
+  const enabledApps = useSelector(state => state.enabledApps);
+
+  const appData = React.useMemo(
+    () =>
+      APP_DIRECTORY_DATA.map(app => {
+        const { defaultEnabled, ...data } = app;
+        return {
+          ...data,
+          enabled: enabledApps[app.id] ?? defaultEnabled,
+        };
+      }),
+    [enabledApps],
+  );
+
+  const appItems = React.useMemo(
+    () => appData.map(item => <AppListing key={item.id} {...item} />),
+    [appData],
+  );
+
+  return (
+    <div className={css.appsDirectoryContainer}>
+      <h4 className={css.appsHeader}>Choose Apps</h4>
+      <div className={css.appsDirectoryList}>{appItems}</div>
+    </div>
+  );
 }
 
 export default AppsDirectory;
