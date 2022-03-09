8d7b8c3e24574718b4fc377640f1c27aa9602e54
Atul Madhugiri <atulm@berkeley.edu>
2022-03-09 16:23:48 -0500

    [landing] Add "launch app" icon to `Header`
    Summary:
    Adding an icon so the web app (https://web.comm.app) can be launched from the landing page.
    
    Note: Won't land this diff until https://web.comm.app is live
    
    Test Plan:
    Here's what the header looks like after:
    
    {F19498}
    
    Reviewers: benschac, def-au1t, palys-swm, varun, ashoat
    
    Reviewed By: benschac, palys-swm, ashoat
    
    Subscribers: ashoat, Adrian, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D3390

--- landing/header.react.js
+++ landing/header.react.js
@@ -1,9 +1,12 @@
 // @flow
 
 import { faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import * as React from 'react';
 import { NavLink } from 'react-router-dom';
+
+import { isDev } from 'lib/utils/dev-utils.js';
 
 import Button from './button.react.js';
 import css from './header.css';
@@ -27,6 +30,14 @@ const navLinkProps = {
 function Header(props: HeaderProps): React.Node {
   const { onRequestAccess } = props;
 
+  let launchAppIcon;
+  if (isDev) {
+    launchAppIcon = (
+      <a className={css.social_icon} href="https://web.comm.app">
+        <FontAwesomeIcon icon={faExternalLinkAlt} {...iconProps} />
+      </a>
+    );
+  }
   return (
     <nav className={css.wrapper}>
       <NavLink to="/" className={css.logo}>
@@ -43,11 +54,12 @@ function Header(props: HeaderProps): React.Node {
       <div className={css.button_container}>
         <Button onClick={onRequestAccess}>Request Access</Button>
       </div>
-      <div className={css.social_icons}>
-        <a className={css.twitter_icon} href="https://twitter.com/commdotapp">
+      <div className={css.social_icons_container}>
+        {launchAppIcon}
+        <a className={css.social_icon} href="https://twitter.com/commdotapp">
           <FontAwesomeIcon icon={faTwitter} {...iconProps} />
         </a>
-        <a className={css.github_icon} href="https://github.com/CommE2E/comm">
+        <a className={css.social_icon} href="https://github.com/CommE2E/comm">
           <FontAwesomeIcon icon={faGithub} {...iconProps} />
         </a>
       </div>
