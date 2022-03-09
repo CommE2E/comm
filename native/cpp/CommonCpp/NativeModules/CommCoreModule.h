c06b6b718261d7629e0ddd6398f652e68390aba6
Atul Madhugiri <atulm@berkeley.edu>
2022-03-09 12:58:39 -0500

    [native] `codeVersion` -> 130

--- native/cpp/CommonCpp/NativeModules/CommCoreModule.h
+++ native/cpp/CommonCpp/NativeModules/CommCoreModule.h
@@ -13,7 +13,7 @@ namespace comm {
 namespace jsi = facebook::jsi;
 
 class CommCoreModule : public facebook::react::CommCoreModuleSchemaCxxSpecJSI {
-  const int codeVersion{129};
+  const int codeVersion{130};
 
   std::unique_ptr<WorkerThread> databaseThread;
   std::unique_ptr<WorkerThread> cryptoThread;
