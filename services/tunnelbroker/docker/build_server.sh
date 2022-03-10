9ee741642cd86838e67ed528cea07adff36badc5
Jim Posen <jim.posen@gmail.com>
2022-03-10 11:23:18 -0500

    tunnelbroker: Generate gRPC/protobuf code with CMake
    Summary:
    Solves https://linear.app/comm/issue/ENG-777/generate-grpcproto-code-as-cmake-target
    
    Also sets MAKEFLAGS for parallelism during build, which can be overridden with Docker build argument.
    
    Test Plan: Run the tunnelbroker service launch and test scripts
    
    Reviewers: ashoat, karol-bisztyga, geekbrother, varun
    
    Reviewed By: ashoat, karol-bisztyga
    
    Subscribers: palys-swm, Adrian, atul, benschac
    
    Differential Revision: https://phabricator.ashoat.com/D3236

--- services/tunnelbroker/docker/build_server.sh
+++ services/tunnelbroker/docker/build_server.sh
@@ -13,11 +13,11 @@ ln -s /usr/lib/glog
 ln -s /usr/lib/double-conversion
 popd # lib
 
-rm -rf _generated
-mkdir _generated
-
 rm -rf cmake/build
 mkdir -p cmake/build
 
-./generate.sh
-./build.sh
+echo "building the server (MAKEFLAGS=$MAKEFLAGS)..."
+pushd cmake/build
+cmake ../..
+make
+popd # cmake/build
