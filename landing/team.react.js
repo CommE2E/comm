c9038e694552c2f9f4cb6dbeede9ad180260c42e
Benjamin Schachter <benschac@gmail.com>
2022-03-10 16:31:15 -0500

    work with atul
    Summary: wip that wasn't in a diff
    
    Test Plan: ignore
    
    Reviewers: atul
    
    Subscribers:

--- landing/team.react.js
+++ landing/team.react.js
@@ -9,7 +9,7 @@ function Team(): React.Node {
   return (
     <>
       <div className={css.wrapper}>
-        <h2 className={css.header}>Team</h2>
+        {/* <h2 className={css.header}>Team</h2> */}
         <section className={css.teamWrapper}>
           <TeamProfile
             name="Ashoat Tevosyan"
@@ -18,12 +18,13 @@ function Team(): React.Node {
             twitterHandle="ashoat"
             imageURL="https://avatars.githubusercontent.com/u/863579?v=4"
           />
+
           <TeamProfile
             name="atul"
             role="Software Engineer"
-            githubHandle="atulsmadhugiri"
-            twitterHandle="atuli0"
-            imageURL="https://avatars.githubusercontent.com/u/32692685?v=4"
+            githubHandle="benschac"
+            twitterHandle="benschac"
+            imageURL="https://i.imgur.com/3XkVQ1H.png"
           />
           <TeamProfile
             name="Benjamin Schachter"
@@ -37,41 +38,55 @@ function Team(): React.Node {
             role="Software Engineer"
             githubHandle="vdhanan"
             twitterHandle="_va_run"
-            imageURL="https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F0f7471e7-e16b-45b1-bb7b-567d2ac5af44%2F1567382183343.jpeg?table=block&id=2f851c24-8cd6-412a-b8a9-0d989e3c2fbc&spaceId=3a3549de-adc7-4702-88ab-ca7eaf32792e&width=2000&userId=a7a3d7f5-a72b-45c3-8719-06e288a6c405&cache=v2"
+            imageURL="https://i.imgur.com/3XkVQ1H.png"
+          />
+          <TeamProfile
+            name="Max Kalashnikoff"
+            role="Software Engineer"
+            githubHandle="vdhanan"
+            twitterHandle="_va_run"
+            imageURL="https://i.imgur.com/3XkVQ1H.png"
           />
         </section>
       </div>
       <div className={css.wrapper}>
-        <h2 className={css.header}>Team at Software Mansion</h2>
+        {/* <h2 className={css.header}>Team at Software Mansion</h2> */}
         <section className={css.teamWrapper}>
           <TeamProfile
-            name="Ashoat Tevosyan"
-            role="Founder"
+            name="Tomasz Pałys"
+            role="software engineer"
             githubHandle="ashoat"
             twitterHandle="ashoat"
             imageURL="https://avatars.githubusercontent.com/u/863579?v=4"
           />
           <TeamProfile
-            name="atul"
+            name="Karol Bisztyga"
             role="Software Engineer"
             githubHandle="atulsmadhugiri"
             twitterHandle="atuli0"
             imageURL="https://avatars.githubusercontent.com/u/32692685?v=4"
           />
           <TeamProfile
-            name="Benjamin Schachter"
+            name="Jacek Nitychoruk"
+            role="Software Engineer"
+            githubHandle="atulsmadhugiri"
+            twitterHandle="atuli0"
+            imageURL="https://avatars.githubusercontent.com/u/32692685?v=4"
+          />
+          <TeamProfile
+            name="Marcin Wasowicz"
             role="Software Engineer"
             githubHandle="benschac"
             twitterHandle="benschac"
             imageURL="https://i.imgur.com/3XkVQ1H.png"
           />
-          <TeamProfile
+          {/* <TeamProfile
             name="Varun Dhananjaya"
             role="Software Engineer"
             githubHandle="vdhanan"
             twitterHandle="_va_run"
             imageURL="https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F0f7471e7-e16b-45b1-bb7b-567d2ac5af44%2F1567382183343.jpeg?table=block&id=2f851c24-8cd6-412a-b8a9-0d989e3c2fbc&spaceId=3a3549de-adc7-4702-88ab-ca7eaf32792e&width=2000&userId=a7a3d7f5-a72b-45c3-8719-06e288a6c405&cache=v2"
-          />
+          /> */}
           <TeamProfile
             name="Ashoat Tevosyan"
             role="Founder"
