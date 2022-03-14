95cf5185b84ce5ca9733a1b07ce3ac15f94f0f1a
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-03-14 14:21:53 +0100

    [services] Backup - authentication handlers
    Summary:
    This is a basic structure for the authentication handlers. Authentication is not yet fully figured out therefore this is more like a draft but I wanted to prepare a clear structure for how we may want to handle this.
    
    Depends on D3200
    
    Test Plan:
    ```
    cd services
    yarn run-backup-service
    ```
    
    Reviewers: palys-swm, geekbrother, varun, jimpo
    
    Subscribers: ashoat, palys-swm, Adrian, atul
    
    Differential Revision: https://phabricator.ashoat.com/D3245

---
+++ services/backup/docker-server/contents/server/src/Authentication/PakeAuthenticationHandler.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "AuthenticationHandlerBase.h"
+
+#include <string>
+
+namespace comm {
+namespace network {
+namespace auth {
+
+class PakeAuthenticationHandler : public AuthenticationHandlerBase {
+  const AuthenticationType authenticationType;
+
+public:
+  PakeAuthenticationHandler(AuthenticationType authenticationType);
+
+  backup::FullAuthenticationResponseData*
+  processRequest(const backup::FullAuthenticationRequestData &request) override;
+  AuthenticationType getAuthenticationType() const override;
+};
+
+} // namespace auth
+} // namespace network
+} // namespace comm
