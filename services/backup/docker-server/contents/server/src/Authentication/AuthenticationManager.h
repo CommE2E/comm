a1a601c3bc160c55917ffbe252101f7dc9b34d17
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-03-14 14:21:53 +0100

    [services] Backup - authentication handlers
    Summary:
    This is a basic structure for the authentication handlers. Authentication is not yet fully figured out therefore this is more like a draft but I wanted to prepare a clear structure for how we may want to handle this.
    
    Depends on D3200
    
    Test Plan:
    ```
    cd services
    yarn run-backup-service
    ```
    
    Reviewers: palys-swm, geekbrother, varun, jimpo
    
    Subscribers: ashoat, palys-swm, Adrian, atul
    
    Differential Revision: https://phabricator.ashoat.com/D3245

---
+++ services/backup/docker-server/contents/server/src/Authentication/AuthenticationManager.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "AuthenticationHandlerBase.h"
+
+#include "../_generated/backup.pb.h"
+
+#include <atomic>
+#include <memory>
+
+namespace comm {
+namespace network {
+namespace auth {
+
+enum class AuthenticationState {
+  IN_PROGRESS = 1,
+  SUCCESS = 2,
+  FAIL = 3,
+};
+
+class AuthenticationManager {
+  std::atomic<AuthenticationState> state = AuthenticationState::IN_PROGRESS;
+  std::unique_ptr<AuthenticationHandlerBase> authenticationHandler;
+
+  AuthenticationType getAuthenticationTypeForRequest(
+      const backup::FullAuthenticationRequestData &request) const;
+
+public:
+  AuthenticationState getState() const;
+  backup::FullAuthenticationResponseData
+  processRequest(const backup::FullAuthenticationRequestData &request);
+};
+
+} // namespace auth
+} // namespace network
+} // namespace comm
