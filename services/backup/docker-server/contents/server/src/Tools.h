c605a5a1c533d611ae9dacde6695126ae45f05c7
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-03-18 13:44:14 +0100

    [services] Backup - add generate random string to backup tools

--- services/backup/docker-server/contents/server/src/Tools.h
+++ services/backup/docker-server/contents/server/src/Tools.h
@@ -1,10 +1,12 @@
 #pragma once
 
 #include <cstdint>
+#include <string>
 
 namespace comm {
 namespace network {
 
+std::string generateRandomString(std::size_t length = 20);
 uint64_t getCurrentTimestamp();
 
 } // namespace network
