7028fb79cf4a4041f47f3f0b7a24ebf41a224820
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-03-21 09:58:03 +0100

    [services] Backup - Add random string to tools
    Summary:
    Depends on D3462
    
    I needed a random string generator for testing the backup service. Once we're done mocking things in this service, this can be removed.
    
    Test Plan:
    ```
    cd services
    yarn run-backup-service
    ```
    
    Reviewers: palys-swm, jimpo, geekbrother, varun
    
    Reviewed By: palys-swm
    
    Subscribers: ashoat, palys-swm, Adrian, atul, benschac
    
    Differential Revision: https://phabricator.ashoat.com/D3463

--- services/backup/docker-server/contents/server/src/Tools.cpp
+++ services/backup/docker-server/contents/server/src/Tools.cpp
@@ -1,9 +1,22 @@
 #include "Tools.h"
 
 #include <chrono>
+#include <random>
 
 namespace comm {
 namespace network {
+
+std::string generateRandomString(std::size_t length) {
+  const std::string CHARACTERS =
+      "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  thread_local std::random_device generator;
+  std::uniform_int_distribution<> distribution(0, CHARACTERS.size() - 1);
+  std::string random_string;
+  for (std::size_t i = 0; i < length; ++i) {
+    random_string += CHARACTERS[distribution(generator)];
+  }
+  return random_string;
+}
 
 uint64_t getCurrentTimestamp() {
   using namespace std::chrono;
