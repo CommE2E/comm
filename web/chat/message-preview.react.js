19913baac4b215c8487f9e7a64726dd6e1c92d60
Benjamin Schachter <benschac@gmail.com>
2022-03-23 15:31:57 -0400

    [web] [refactor] simplify username logic in message preview
    Summary: simplify rendering logic for username in message preview
    
    Test Plan: username logic is the same as production. No new functionality
    
    Reviewers: atul
    
    Subscribers: ashoat, palys-swm, Adrian, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D3506

--- web/chat/message-preview.react.js
+++ web/chat/message-preview.react.js
@@ -25,6 +25,7 @@ function MessagePreview(props: Props): React.Node {
   const { messageInfo: messageInfoProps, threadInfo } = props;
   const { unread } = threadInfo.currentUser;
 
+  let usernameText = null;
   const colorStyle = unread ? css.white : css.light;
 
   if (!messageInfoProps) {
@@ -44,30 +45,22 @@ function MessagePreview(props: Props): React.Node {
     threadInfo,
     getDefaultTextMessageRules().simpleMarkdownRules,
   );
-  if (messageInfo.type === messageTypes.TEXT) {
-    let usernameText = null;
-    if (
-      threadIsGroupChat(threadInfo) ||
-      threadInfo.name !== '' ||
-      messageInfo.creator.isViewer
-    ) {
-      const userString = stringForUser(messageInfo.creator);
-      const username = `${userString}: `;
-      usernameText = <span className={colorStyle}>{username}</span>;
-    }
-    return (
-      <div className={classNames(css.lastMessage, colorStyle)}>
-        {usernameText}
-        {messageTitle}
-      </div>
-    );
-  } else {
-    return (
-      <div className={classNames([css.lastMessage, colorStyle])}>
-        {messageTitle}
-      </div>
-    );
+
+  if (
+    (messageInfo.type === messageTypes.TEXT && threadIsGroupChat(threadInfo)) ||
+    threadInfo.name !== '' ||
+    messageInfo.creator.isViewer
+  ) {
+    const userString = stringForUser(messageInfo.creator);
+    usernameText = <span className={colorStyle}>{`${userString}: `}</span>;
   }
+
+  return (
+    <div className={classNames([css.lastMessage, colorStyle])}>
+      {usernameText}
+      {messageTitle}
+    </div>
+  );
 }
 
 export default MessagePreview;
