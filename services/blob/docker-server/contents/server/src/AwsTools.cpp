2a3e11eadf389286ed5e4bff2f178d130eba6a39
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-03-23 15:49:25 +0100

    [services] Dev Mode - Apply local cloud to the Blob service
    Summary: todo
    
    Test Plan: todo
    
    Reviewers:
    
    Subscribers:

--- services/blob/docker-server/contents/server/src/AwsTools.cpp
+++ services/blob/docker-server/contents/server/src/AwsTools.cpp
@@ -28,13 +28,24 @@ std::vector<std::string> listBuckets() {
 std::unique_ptr<Aws::DynamoDB::DynamoDBClient> getDynamoDBClient() {
   Aws::Client::ClientConfiguration config;
   config.region = AWS_REGION;
+#ifdef COMM_SERVICES_DEV_MODE
+  config.endpointOverride = Aws::String("localstack:4566");
+  config.scheme = Aws::Http::Scheme::HTTP;
+#endif
   return std::make_unique<Aws::DynamoDB::DynamoDBClient>(config);
 }
 
 std::unique_ptr<Aws::S3::S3Client> getS3Client() {
   Aws::Client::ClientConfiguration config;
   config.region = AWS_REGION;
+#ifdef COMM_SERVICES_DEV_MODE
+  config.endpointOverride = Aws::String("localstack:4566");
+  config.scheme = Aws::Http::Scheme::HTTP;
+  return std::make_unique<Aws::S3::S3Client>(
+      config, Aws::Client::AWSAuthV4Signer::PayloadSigningPolicy::Never, false);
+#else
   return std::make_unique<Aws::S3::S3Client>(config);
+#endif
 }
 
 } // namespace network
