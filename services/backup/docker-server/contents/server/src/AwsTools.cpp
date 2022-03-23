fbd3f90914ee8edda11829c22a9853a595c7319e
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-03-23 15:55:49 +0100

    [services] Dev Mode - Apply local cloud to the Backup service
    Summary: todo
    
    Test Plan: todo
    
    Reviewers:
    
    Subscribers:

--- services/backup/docker-server/contents/server/src/AwsTools.cpp
+++ services/backup/docker-server/contents/server/src/AwsTools.cpp
@@ -7,6 +7,10 @@ namespace network {
 std::unique_ptr<Aws::DynamoDB::DynamoDBClient> getDynamoDBClient() {
   Aws::Client::ClientConfiguration config;
   config.region = AWS_REGION;
+#ifdef COMM_SERVICES_DEV_MODE
+  config.endpointOverride = Aws::String("localstack:4566");
+  config.scheme = Aws::Http::Scheme::HTTP;
+#endif
   return std::make_unique<Aws::DynamoDB::DynamoDBClient>(config);
 }
 
