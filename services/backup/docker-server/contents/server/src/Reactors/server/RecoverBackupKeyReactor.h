e2cc537208c14b3b5f9a5e9738e290a459d213a2
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-03-24 09:37:59 +0100

    [services] Backup - Add server reactor implementations - recover backup key reactor

---
+++ services/backup/docker-server/contents/server/src/Reactors/server/RecoverBackupKeyReactor.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "ServerBidiReactorBase.h"
+
+#include "../_generated/backup.grpc.pb.h"
+#include "../_generated/backup.pb.h"
+
+#include <iostream>
+#include <memory>
+#include <string>
+
+namespace comm {
+namespace network {
+namespace reactor {
+
+class RecoverBackupKeyReactor : public ServerBidiReactorBase<
+                                    backup::RecoverBackupKeyRequest,
+                                    backup::RecoverBackupKeyResponse> {
+public:
+  std::unique_ptr<ServerBidiReactorStatus> handleRequest(
+      backup::RecoverBackupKeyRequest request,
+      backup::RecoverBackupKeyResponse *response);
+};
+
+std::unique_ptr<ServerBidiReactorStatus> RecoverBackupKeyReactor::handleRequest(
+    backup::RecoverBackupKeyRequest request,
+    backup::RecoverBackupKeyResponse *response) { // TODO handle request
+  return std::make_unique<ServerBidiReactorStatus>(
+      grpc::Status(grpc::StatusCode::UNIMPLEMENTED, "unimplemented"));
+}
+
+} // namespace reactor
+} // namespace network
+} // namespace comm
