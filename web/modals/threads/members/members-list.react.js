ff6060de2943c13dd217c2c3a6e872f11e47d98a
Jacek Nitychoruk <jacek.nitychoruk@swmansion.com>
2022-03-25 13:18:42 +0100

    [web] Introduce `ThreadMembersList` component
    Summary:
    Create component with list of thread members grouped by first letter of username. The component will be displayed in modal.
    The component disallows list scrolling if any menu from its children is open (that's why it keeps opened menus list).
    
    {F19099}
    
    Test Plan: The component can be tested after introducing members modal.
    
    Reviewers: palys-swm, benschac, atul
    
    Reviewed By: palys-swm
    
    Subscribers: ashoat, palys-swm, Adrian, atul, karol-bisztyga, benschac
    
    Differential Revision: https://phabricator.ashoat.com/D3383

---
+++ web/modals/threads/members/members-list.react.js
@@ -0,0 +1,76 @@
+// @flow
+
+import classNames from 'classnames';
+import _groupBy from 'lodash/fp/groupBy';
+import _toPairs from 'lodash/fp/toPairs';
+import * as React from 'react';
+
+import { stringForUser } from 'lib/shared/user-utils';
+import {
+  type ThreadInfo,
+  type RelativeMemberInfo,
+} from 'lib/types/thread-types';
+
+import ThreadMember from './member.react';
+import css from './members-modal.css';
+
+type Props = {
+  +threadInfo: ThreadInfo,
+  +threadMembers: $ReadOnlyArray<RelativeMemberInfo>,
+};
+
+function ThreadMembersList(props: Props): React.Node {
+  const { threadMembers, threadInfo } = props;
+  const [openMenu, setOpenMenu] = React.useState(null);
+  const hasMembers = threadMembers.length > 0;
+
+  const groupedByFirstLetterMembers = React.useMemo(
+    () => _groupBy(member => stringForUser(member)[0])(threadMembers),
+    [threadMembers],
+  );
+
+  const groupedMembersList = React.useMemo(
+    () =>
+      _toPairs(groupedByFirstLetterMembers)
+        .sort((a, b) => a[0].localeCompare(b[0]))
+        .map(([letter, users]) => {
+          const userList = users
+            .sort((a, b) => stringForUser(a).localeCompare(stringForUser(b)))
+            .map((user: RelativeMemberInfo) => (
+              <ThreadMember
+                key={user.id}
+                memberInfo={user}
+                threadInfo={threadInfo}
+                setOpenMenu={setOpenMenu}
+                isMenuOpen={openMenu === user.id}
+              />
+            ));
+          const letterHeader = (
+            <h5 className={css.memberletterHeader} key={letter}>
+              {letter.toUpperCase()}
+            </h5>
+          );
+          return (
+            <React.Fragment key={letter}>
+              {letterHeader}
+              {userList}
+            </React.Fragment>
+          );
+        }),
+    [groupedByFirstLetterMembers, openMenu, threadInfo],
+  );
+  let content = groupedMembersList;
+  if (!hasMembers) {
+    content = (
+      <div className={css.noUsers}>
+        No matching users were found in the thread!
+      </div>
+    );
+  }
+  const membersListClasses = classNames(css.membersList, {
+    [css.noScroll]: !!openMenu,
+  });
+  return <div className={membersListClasses}>{content}</div>;
+}
+
+export default ThreadMembersList;
