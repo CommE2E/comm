811a9b45b539c1cee61bb7056c95387946d5d0bd
Jacek Nitychoruk <jacek.nitychoruk@swmansion.com>
2022-03-25 13:18:37 +0100

    [native, lib] Move thread members actions callbacks into shared lib function
    Summary:
    Move logic for two functions:
    - removing user from thread
    - switch user role between user and admin
    into lib shares, so they could be used also in web app.
    
    Test Plan: Promoting user to an admin and removing the user from a thread should work exactly as before
    
    Reviewers: palys-swm, benschac, atul
    
    Reviewed By: palys-swm
    
    Subscribers: ashoat, palys-swm, Adrian, atul, karol-bisztyga, benschac
    
    Differential Revision: https://phabricator.ashoat.com/D3381

--- native/chat/settings/thread-settings-member-tooltip-modal.react.js
+++ native/chat/settings/thread-settings-member-tooltip-modal.react.js
@@ -1,16 +1,17 @@
 // @flow
 
-import invariant from 'invariant';
 import * as React from 'react';
 import { Alert } from 'react-native';
 
 import {
-  removeUsersFromThreadActionTypes,
   removeUsersFromThread,
-  changeThreadMemberRolesActionTypes,
   changeThreadMemberRoles,
 } from 'lib/actions/thread-actions';
-import { memberIsAdmin, roleIsAdminRole } from 'lib/shared/thread-utils';
+import {
+  memberIsAdmin,
+  removeMemberFromThread,
+  switchMemberAdminRoleInThread,
+} from 'lib/shared/thread-utils';
 import { stringForUser } from 'lib/shared/user-utils';
 import type { ThreadInfo, RelativeMemberInfo } from 'lib/types/thread-types';
 import type { DispatchFunctions, ActionFunc } from 'lib/utils/action-utils';
@@ -35,14 +36,13 @@ function onRemoveUser(
 ) {
   const { memberInfo, threadInfo } = route.params;
   const boundRemoveUsersFromThread = bindServerCall(removeUsersFromThread);
-  const onConfirmRemoveUser = () => {
-    const customKeyName = `${removeUsersFromThreadActionTypes.started}:${memberInfo.id}`;
-    dispatchFunctions.dispatchActionPromise(
-      removeUsersFromThreadActionTypes,
-      boundRemoveUsersFromThread(threadInfo.id, [memberInfo.id]),
-      { customKeyName },
+  const onConfirmRemoveUser = () =>
+    removeMemberFromThread(
+      threadInfo,
+      memberInfo,
+      dispatchFunctions.dispatchActionPromise,
+      boundRemoveUsersFromThread,
     );
-  };
 
   const userText = stringForUser(memberInfo);
   Alert.alert(
@@ -64,27 +64,14 @@ function onToggleAdmin(
   const { memberInfo, threadInfo } = route.params;
   const isCurrentlyAdmin = memberIsAdmin(memberInfo, threadInfo);
   const boundChangeThreadMemberRoles = bindServerCall(changeThreadMemberRoles);
-  const onConfirmMakeAdmin = () => {
-    let newRole = null;
-    for (const roleID in threadInfo.roles) {
-      const role = threadInfo.roles[roleID];
-      if (isCurrentlyAdmin && role.isDefault) {
-        newRole = role.id;
-        break;
-      } else if (!isCurrentlyAdmin && roleIsAdminRole(role)) {
-        newRole = role.id;
-        break;
-      }
-    }
-    invariant(newRole !== null, 'Could not find new role');
-
-    const customKeyName = `${changeThreadMemberRolesActionTypes.started}:${memberInfo.id}`;
-    dispatchFunctions.dispatchActionPromise(
-      changeThreadMemberRolesActionTypes,
-      boundChangeThreadMemberRoles(threadInfo.id, [memberInfo.id], newRole),
-      { customKeyName },
+  const onConfirmMakeAdmin = () =>
+    switchMemberAdminRoleInThread(
+      threadInfo,
+      memberInfo,
+      isCurrentlyAdmin,
+      dispatchFunctions.dispatchActionPromise,
+      boundChangeThreadMemberRoles,
     );
-  };
 
   const userText = stringForUser(memberInfo);
   const actionClause = isCurrentlyAdmin
