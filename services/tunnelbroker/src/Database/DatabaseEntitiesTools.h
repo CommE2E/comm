4b3a25af06b7e65ff04237d5b57bf9cd3f4498e2
Max Kalashnikov <geekmaks@gmail.com>
2022-03-28 14:29:24 +0300

    [services] Tunnelbroker - Avoid listing all entities in DatabaseEntities.
    Summary:
    Avoid listing all entities in `DatabaseEntitiesTools.h` and use a new approach like we already use in a Blob service.
    
    Related linear task: [[ https://linear.app/comm/issue/ENG-706/avoid-listing-all-entities-in-databaseentitiestoolsh-in-tunnelbroker | ENG-706 ]]
    
    Test Plan: Successfully built and run service using `yarn run-tunnelbroker-service`.
    
    Reviewers: karol-bisztyga, palys-swm, jimpo
    
    Reviewed By: karol-bisztyga, palys-swm
    
    Subscribers: ashoat, palys-swm, Adrian, atul, karol-bisztyga, benschac
    
    Differential Revision: https://phabricator.ashoat.com/D3524

--- services/tunnelbroker/src/Database/DatabaseEntitiesTools.h
+++ services/tunnelbroker/src/Database/DatabaseEntitiesTools.h
@@ -13,18 +13,9 @@ namespace network {
 namespace database {
 
 template <typename T> std::shared_ptr<T> createItemByType() {
-  throw std::runtime_error("invalid Item type");
+  static_assert(std::is_base_of<Item, T>::value, "T must inherit from Item");
+  return std::make_shared<T>();
 }
-
-template <>
-std::shared_ptr<DeviceSessionItem> createItemByType<DeviceSessionItem>();
-
-template <>
-std::shared_ptr<SessionSignItem> createItemByType<SessionSignItem>();
-
-template <> std::shared_ptr<PublicKeyItem> createItemByType<PublicKeyItem>();
-
-template <> std::shared_ptr<MessageItem> createItemByType<MessageItem>();
 
 } // namespace database
 } // namespace network
