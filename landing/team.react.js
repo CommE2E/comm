d4d03be502f76b5a396a6c49c591630f8c21b956
Benjamin Schachter <benschac@gmail.com>
2022-03-31 15:35:58 -0400

    [landing] add profile pictures from s3, remove placehodlr images
    Summary: add s3 images. should render teams head shots
    
    Test Plan: go to /team, headshot should show
    
    Reviewers: atul
    
    Subscribers:

--- landing/team.react.js
+++ landing/team.react.js
@@ -2,9 +2,9 @@
 
 import * as React from 'react';
 
+import { assetUrl } from './asset-meta-data.js';
 import TeamProfile from './team-profile.react.js';
 import css from './team.css';
-
 function Team(): React.Node {
   return (
     <>
@@ -16,36 +16,36 @@ function Team(): React.Node {
             role="Founder"
             githubHandle="ashoat"
             twitterHandle="ashoat"
-            imageURL="https://avatars.githubusercontent.com/u/863579?v=4"
+            imageURL={`${assetUrl}/ashoat.png`}
           />
 
           <TeamProfile
             name="atul"
             role="Software Engineer"
-            githubHandle="benschac"
-            twitterHandle="benschac"
-            imageURL="https://i.imgur.com/3XkVQ1H.png"
+            githubHandle="atulsmadhugiri"
+            twitterHandle="atulio"
+            imageURL={`${assetUrl}/atul.jpeg`}
           />
           <TeamProfile
             name="Benjamin Schachter"
             role="Software Engineer"
             githubHandle="benschac"
             twitterHandle="benschac"
-            imageURL="https://i.imgur.com/3XkVQ1H.png"
+            imageURL={`${assetUrl}/ben.png`}
           />
           <TeamProfile
             name="Varun Dhananjaya"
             role="Software Engineer"
             githubHandle="vdhanan"
             twitterHandle="_va_run"
-            imageURL="https://i.imgur.com/3XkVQ1H.png"
+            imageURL={`${assetUrl}/varun.jpeg`}
           />
           <TeamProfile
             name="Max Kalashnikoff"
             role="Software Engineer"
-            githubHandle="vdhanan"
-            twitterHandle="_va_run"
-            imageURL="https://i.imgur.com/3XkVQ1H.png"
+            githubHandle="geekbrother"
+            twitterHandle="GeekMaks"
+            imageURL={`${assetUrl}/max.jpeg`}
           />
         </section>
       </div>
@@ -55,66 +55,27 @@ function Team(): React.Node {
           <TeamProfile
             name="Tomasz Pałys"
             role="software engineer"
-            githubHandle="ashoat"
-            twitterHandle="ashoat"
-            imageURL="https://avatars.githubusercontent.com/u/863579?v=4"
+            githubHandle="palys-swm"
+            imageURL={`${assetUrl}/tomek.png`}
           />
           <TeamProfile
             name="Karol Bisztyga"
             role="Software Engineer"
-            githubHandle="atulsmadhugiri"
-            twitterHandle="atuli0"
-            imageURL="https://avatars.githubusercontent.com/u/32692685?v=4"
+            githubHandle="karol-bisztyga"
+            twitterHandle="KBisztyga"
+            imageURL={`${assetUrl}/karol.jpeg`}
           />
           <TeamProfile
             name="Jacek Nitychoruk"
             role="Software Engineer"
-            githubHandle="atulsmadhugiri"
-            twitterHandle="atuli0"
-            imageURL="https://avatars.githubusercontent.com/u/32692685?v=4"
+            githubHandle="def-au1t"
+            imageURL={`${assetUrl}/jacek.jpeg`}
           />
           <TeamProfile
             name="Marcin Wasowicz"
             role="Software Engineer"
-            githubHandle="benschac"
-            twitterHandle="benschac"
-            imageURL="https://i.imgur.com/3XkVQ1H.png"
-          />
-          {/* <TeamProfile
-            name="Varun Dhananjaya"
-            role="Software Engineer"
-            githubHandle="vdhanan"
-            twitterHandle="_va_run"
-            imageURL="https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F0f7471e7-e16b-45b1-bb7b-567d2ac5af44%2F1567382183343.jpeg?table=block&id=2f851c24-8cd6-412a-b8a9-0d989e3c2fbc&spaceId=3a3549de-adc7-4702-88ab-ca7eaf32792e&width=2000&userId=a7a3d7f5-a72b-45c3-8719-06e288a6c405&cache=v2"
-          /> */}
-          <TeamProfile
-            name="Ashoat Tevosyan"
-            role="Founder"
-            githubHandle="ashoat"
-            twitterHandle="ashoat"
-            imageURL="https://avatars.githubusercontent.com/u/863579?v=4"
-          />
-          <TeamProfile
-            name="atul"
-            role="Software Engineer"
-            githubHandle="atulsmadhugiri"
-            twitterHandle="atuli0"
-            imageURL="https://avatars.githubusercontent.com/u/32692685?v=4"
-          />
-
-          <TeamProfile
-            name="Ashoat Tevosyan"
-            role="Founder"
-            githubHandle="ashoat"
-            twitterHandle="ashoat"
-            imageURL="https://avatars.githubusercontent.com/u/863579?v=4"
-          />
-          <TeamProfile
-            name="atul"
-            role="Software Engineer"
-            githubHandle="atulsmadhugiri"
-            twitterHandle="atuli0"
-            imageURL="https://avatars.githubusercontent.com/u/32692685?v=4"
+            githubHandle="marcinwasowicz"
+            imageURL={`${assetUrl}/marcin.jpeg`}
           />
         </section>
       </div>
