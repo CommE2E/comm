886ec256ac88c18993fd7dd1abfc08ccbf885b84
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-03-31 16:19:54 +0200

    [services] Backup - Add client reactor base classes - read reactor
    Summary:
    Depends on D3465
    
    Adding client base classes for reactors - read reactor
    
    Test Plan:
    ```
    cd services
    yarn run-backup-service
    ```
    
    Reviewers: palys-swm, geekbrother, varun, jimpo
    
    Reviewed By: palys-swm
    
    Subscribers: ashoat, palys-swm, Adrian, atul, benschac
    
    Differential Revision: https://phabricator.ashoat.com/D3514

---
+++ services/backup/docker-server/contents/server/src/Reactors/client/base-reactors/ClientReadReactorBase.h
@@ -0,0 +1,78 @@
+#include <grpcpp/grpcpp.h>
+
+namespace comm {
+namespace network {
+namespace reactor {
+
+template <class Request, class Response>
+class ClientReadReactorBase : public grpc::ClientReadReactor<Response> {
+  Response response;
+  grpc::Status status;
+  bool done = false;
+  bool initialized = false;
+
+  void terminate(const grpc::Status &status);
+
+public:
+  Request request;
+  grpc::ClientContext context;
+
+  void start();
+  void OnReadDone(bool ok) override;
+  void OnDone(const grpc::Status &status) override;
+  bool isDone();
+
+  virtual std::unique_ptr<grpc::Status>
+  readResponse(const Response &response) = 0;
+  virtual void doneCallback() {
+  }
+};
+
+template <class Request, class Response>
+void ClientReadReactorBase<Request, Response>::terminate(
+    const grpc::Status &status) {
+  if (this->done) {
+    return;
+  }
+  this->status = status;
+  this->done = true;
+  this->doneCallback();
+}
+
+template <class Request, class Response>
+void ClientReadReactorBase<Request, Response>::start() {
+  this->StartRead(&this->response);
+  if (!this->initialized) {
+    this->StartCall();
+    this->initialized = true;
+  }
+}
+
+template <class Request, class Response>
+void ClientReadReactorBase<Request, Response>::OnReadDone(bool ok) {
+  if (!ok) {
+    this->terminate(grpc::Status(grpc::StatusCode::UNKNOWN, "read error"));
+    return;
+  }
+  std::unique_ptr<grpc::Status> status = this->readResponse(this->response);
+  if (status != nullptr) {
+    this->terminate(*status);
+    return;
+  }
+  this->StartRead(&this->response);
+}
+
+template <class Request, class Response>
+void ClientReadReactorBase<Request, Response>::OnDone(
+    const grpc::Status &status) {
+  this->terminate(status);
+}
+
+template <class Request, class Response>
+bool ClientReadReactorBase<Request, Response>::isDone() {
+  return this->done;
+}
+
+} // namespace reactor
+} // namespace network
+} // namespace comm
