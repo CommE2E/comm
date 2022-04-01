a54d824439a5f407e93dc17eb928ad0a2e3d5e56
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-04-01 08:35:14 +0200

    [services] Backup - Add server reactor implementations - send log reactor
    Summary:
    Depends on D3467
    
    Add implementation for the send log reactor
    
    Test Plan:
    ```
    cd services
    yarn run-backup-service
    ```
    
    Reviewers: palys-swm, varun, geekbrother, jimpo
    
    Reviewed By: palys-swm
    
    Subscribers: ashoat, palys-swm, Adrian, atul, benschac
    
    Differential Revision: https://phabricator.ashoat.com/D3516

---
+++ services/backup/docker-server/contents/server/src/Reactors/server/SendLogReactor.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "ServerReadReactorBase.h"
+
+#include "../_generated/backup.grpc.pb.h"
+#include "../_generated/backup.pb.h"
+
+#include <iostream>
+#include <memory>
+#include <string>
+
+namespace comm {
+namespace network {
+namespace reactor {
+
+class SendLogReactor : public ServerReadReactorBase<
+                           backup::SendLogRequest,
+                           google::protobuf::Empty> {
+
+public:
+  using ServerReadReactorBase<backup::SendLogRequest, google::protobuf::Empty>::
+      ServerReadReactorBase;
+
+  std::unique_ptr<grpc::Status>
+  readRequest(backup::SendLogRequest request) override;
+  void doneCallback() override;
+};
+
+std::unique_ptr<grpc::Status>
+SendLogReactor::readRequest(backup::SendLogRequest request) {
+  // TODO implement
+  std::cout << "handle request log chunk " << request.logdata().size()
+            << std::endl;
+  return nullptr;
+}
+
+void SendLogReactor::doneCallback() {
+  // TODO implement
+  std::cout << "receive logs done " << this->status.error_code() << "/"
+            << this->status.error_message() << std::endl;
+}
+
+} // namespace reactor
+} // namespace network
+} // namespace comm
