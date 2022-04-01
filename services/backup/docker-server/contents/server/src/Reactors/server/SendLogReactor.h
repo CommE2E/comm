9319b676597aea6e48db1ef559a12dbb029a6595
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-04-01 08:50:38 +0200

    [services] Backup - Apply reading user id in the reactors
    Summary:
    Depends on D3530
    
    In the previous diff, I added a user id for every request where it was missing. Now, we want to read it when creating a new backup and sending a log.
    
    Test Plan:
    ```
    cd services
    yarn run-backup-service
    ```
    
    Reviewers: palys-swm, geekbrother, varun, jimpo
    
    Subscribers: ashoat, palys-swm, Adrian, atul, benschac, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D3531

--- services/backup/docker-server/contents/server/src/Reactors/server/SendLogReactor.h
+++ services/backup/docker-server/contents/server/src/Reactors/server/SendLogReactor.h
@@ -16,6 +16,13 @@ namespace reactor {
 class SendLogReactor : public ServerReadReactorBase<
                            backup::SendLogRequest,
                            google::protobuf::Empty> {
+  enum class State {
+    USER_ID = 1,
+    LOG_CHUNK = 2,
+  };
+
+  State state = State::USER_ID;
+  std::string userID;
 
 public:
   using ServerReadReactorBase<backup::SendLogRequest, google::protobuf::Empty>::
@@ -29,9 +36,24 @@ public:
 std::unique_ptr<grpc::Status>
 SendLogReactor::readRequest(backup::SendLogRequest request) {
   // TODO implement
-  std::cout << "handle request log chunk " << request.logdata().size()
-            << std::endl;
-  return nullptr;
+  switch (this->state) {
+    case State::USER_ID: {
+      if (!request.has_userid()) {
+        throw std::runtime_error("user id expected but not received");
+      }
+      this->userID = request.userid();
+      this->state = State::LOG_CHUNK;
+      return nullptr;
+    };
+    case State::LOG_CHUNK: {
+      if (!request.has_logdata()) {
+        throw std::runtime_error("log data expected but not received");
+      }
+      std::string chunk = request.logdata();
+      std::cout << "log data received " << chunk << std::endl;
+    };
+  }
+  throw std::runtime_error("send log - invalid state");
 }
 
 void SendLogReactor::doneCallback() {
