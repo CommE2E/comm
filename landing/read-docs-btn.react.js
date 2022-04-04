18a5e194e034a9549a9e95b61c1fe2054a286ac0
Benjamin Schachter <benschac@gmail.com>
2022-04-04 10:24:43 -0400

    [web] [refactor] use assetUrl variable
    Summary:
    swap hard coded url for assetUrl variable like the rest of the project uses.
    
    {F29632}
    
    Test Plan: go to the keyserver page. arrow should render.
    
    Reviewers: atul, palys-swm, def-au1t, yayabosh
    
    Reviewed By: atul
    
    Subscribers: ashoat, palys-swm, Adrian, karol-bisztyga, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D3611

--- landing/read-docs-btn.react.js
+++ landing/read-docs-btn.react.js
@@ -2,6 +2,7 @@
 
 import * as React from 'react';
 
+import { assetUrl } from './asset-meta-data';
 import css from './read-docs-btn.css';
 
 function ReadDocsButton(): React.Node {
@@ -12,10 +13,7 @@ function ReadDocsButton(): React.Node {
     >
       <button className={css.button}>
         <span className={css.buttonText}>Read the documentation</span>
-        <img
-          src="https://dh9fld3hutpxf.cloudfront.net/corner_arrow.svg"
-          className={css.cornerIcon}
-        />
+        <img src={`${assetUrl}/corner_arrow.svg`} className={css.cornerIcon} />
       </button>
     </a>
   );
