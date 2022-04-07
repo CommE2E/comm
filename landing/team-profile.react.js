9e5f3f132f1ffd1670cbe11291d55796cf98002a
Benjamin Schachter <benschac@gmail.com>
2022-04-07 11:52:32 -0400

    [3/n] [landing] add basic teammate profile block with picture, title and social links
    Summary:
    Add a basic profile block for each team member with their picture, title, and social handles.
    
    Layout, team sections (ex. Team,  Team @ SWM), and general layout will be addressed in follow up diffs.
    
    If you patch this diff, you should see this. Again, layout will be addressed in follow up diffs.
    
    {F32550}
    
    Test Plan: render team page.
    
    Reviewers: atul, ashoat
    
    Reviewed By: atul, ashoat
    
    Subscribers: ashoat, palys-swm, Adrian, karol-bisztyga, boristopalov
    
    Differential Revision: https://phabricator.ashoat.com/D2750

---
+++ landing/team-profile.react.js
@@ -0,0 +1,63 @@
+// @flow
+
+import { faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import * as React from 'react';
+
+import css from './team-profile.css';
+
+type Props = {
+  +name: string,
+  +role: string,
+  +imageURL: string,
+  +githubHandle?: string,
+  +twitterHandle?: string,
+};
+
+const iconProps = {
+  size: 'm',
+};
+
+function TeamProfile(props: Props): React.Node {
+  const { name, role, imageURL, githubHandle, twitterHandle } = props;
+
+  let twitterLink;
+  if (twitterHandle) {
+    twitterLink = (
+      <a
+        href={`https://twitter.com/${twitterHandle}`}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FontAwesomeIcon icon={faTwitter} {...iconProps} />
+      </a>
+    );
+  }
+
+  let githubLink;
+  if (githubHandle) {
+    githubLink = (
+      <a
+        href={`https://github.com/${githubHandle}`}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FontAwesomeIcon icon={faGithub} {...iconProps} />
+      </a>
+    );
+  }
+
+  return (
+    <article className={css.profile}>
+      <img alt={`portrait of comm team member ${name}`} src={imageURL} />
+      <p>{name}</p>
+      <small>{role}</small>
+      <span>
+        {githubLink}
+        {twitterLink}
+      </span>
+    </article>
+  );
+}
+
+export default TeamProfile;
--- landing/team.react.js
+++ landing/team.react.js
@@ -2,10 +2,75 @@
 
 import * as React from 'react';
 
+import { assetsCacheURLPrefix } from './asset-meta-data';
+import TeamProfile from './team-profile.react.js';
+
 function Team(): React.Node {
   return (
     <div>
-      <h1>Team page</h1>
+      <h2>Team</h2>
+      <section>
+        <TeamProfile
+          name="Ashoat Tevosyan"
+          role="Founder"
+          githubHandle="ashoat"
+          twitterHandle="ashoat"
+          imageURL={`${assetsCacheURLPrefix}/ashoat.png`}
+        />
+        <TeamProfile
+          name="atul"
+          role="Software Engineer"
+          githubHandle="atulsmadhugiri"
+          twitterHandle="atuli0"
+          imageURL={`${assetsCacheURLPrefix}/atul.jpeg`}
+        />
+        <TeamProfile
+          name="Benjamin Schachter"
+          role="Software Engineer"
+          githubHandle="benschac"
+          twitterHandle="benschac"
+          imageURL={`${assetsCacheURLPrefix}/ben.png`}
+        />
+        <TeamProfile
+          name="Varun Dhananjaya"
+          role="Software Engineer"
+          githubHandle="vdhanan"
+          twitterHandle="_va_run"
+          imageURL={`${assetsCacheURLPrefix}/varun.jpeg`}
+        />
+        <TeamProfile
+          name="Max Kalashnikoff"
+          role="Software Engineer"
+          githubHandle="geekbrother"
+          twitterHandle="GeekMaks"
+          imageURL={`${assetsCacheURLPrefix}/max.jpeg`}
+        />
+        <TeamProfile
+          name="Tomasz Pałys"
+          role="Software Engineer"
+          githubHandle="palys-swm"
+          imageURL={`${assetsCacheURLPrefix}/tomek.png`}
+        />
+        <TeamProfile
+          name="Karol Bisztyga"
+          role="Software Engineer"
+          githubHandle="karol-bisztyga"
+          twitterHandle="KBisztyga"
+          imageURL={`${assetsCacheURLPrefix}/karol.jpeg`}
+        />
+        <TeamProfile
+          name="Jacek Nitychoruk"
+          role="Software Engineer"
+          githubHandle="def-au1t"
+          imageURL={`${assetsCacheURLPrefix}/jacek.jpeg`}
+        />
+        <TeamProfile
+          name="Marcin Wasowicz"
+          role="Software Engineer"
+          githubHandle="marcinwasowicz"
+          imageURL={`${assetsCacheURLPrefix}/marcin.jpeg`}
+        />
+      </section>
     </div>
   );
 }
