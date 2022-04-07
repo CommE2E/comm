291a16c110a58eb51f7ea0e02d5f182477067ed7
Tomasz Palys <tomasz.palys@swmansion.com>
2022-04-07 11:33:52 +0200

    [web] Add option to change search modal size
    Summary: It should be possible to use small and large modals.
    
    Test Plan: Check existing usages - they should look the same. Render a large modal and check if its size is increased.
    
    Reviewers: benschac, atul, def-au1t
    
    Reviewed By: benschac, atul
    
    Subscribers: ashoat, Adrian, karol-bisztyga, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D3644

--- web/modals/search-modal.react.js
+++ web/modals/search-modal.react.js
@@ -3,26 +3,27 @@
 import * as React from 'react';
 
 import Search from '../components/search.react';
-import Modal from './modal.react';
+import Modal, { type ModalSize } from './modal.react';
 import css from './search-modal.css';
 
 type Props = {
   +name: string,
   +searchPlaceholder: string,
+  +size?: ModalSize,
   +onClose: () => void,
   +children: (searchText: string) => React.Node,
 };
 
 function SearchModal(props: Props): React.Node {
   const [searchText, setSearchText] = React.useState('');
-  const { name, searchPlaceholder, onClose, children } = props;
+  const { name, searchPlaceholder, size, onClose, children } = props;
   const child = React.useMemo(() => children(searchText), [
     children,
     searchText,
   ]);
 
   return (
-    <Modal name={name} onClose={onClose}>
+    <Modal name={name} onClose={onClose} size={size}>
       <div className={css.container}>
         <Search
           onChangeText={setSearchText}
