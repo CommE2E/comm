044906b7ea600baa1ad83a410a603dda6e29125c
Tomasz Palys <tomasz.palys@swmansion.com>
2022-04-07 16:08:56 +0200

    [web] Display friend list buttons in the modal
    Summary:
    Display the buttons depending on relationship status.
    
    {F33332}
    
    Depends on D3676
    
    Test Plan: Render a couple of rows for users with different relationship statuses (friend, request sent, request received) and check if correct buttons are rendered.
    
    Reviewers: benschac, atul, def-au1t
    
    Reviewed By: benschac, atul
    
    Subscribers: ashoat, Adrian, karol-bisztyga, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D3677

--- web/settings/relationship/friend-list-row.react.js
+++ web/settings/relationship/friend-list-row.react.js
@@ -1,14 +1,48 @@
 // @flow
 
+import classnames from 'classnames';
 import * as React from 'react';
 
+import { userRelationshipStatus } from 'lib/types/relationship-types';
+
+import SWMansionIcon from '../../SWMansionIcon.react';
 import css from './friend-list-row.css';
 import type { UserRowProps } from './user-list.react';
 
 function FriendListRow(props: UserRowProps): React.Node {
   const { userInfo } = props;
+  let buttons = null;
+  if (userInfo.relationshipStatus === userRelationshipStatus.REQUEST_SENT) {
+    buttons = (
+      <button className={classnames([css.button, css.destructive])}>
+        Cancel request
+      </button>
+    );
+  } else if (
+    userInfo.relationshipStatus === userRelationshipStatus.REQUEST_RECEIVED
+  ) {
+    buttons = (
+      <>
+        <button className={css.button}>Accept</button>
+        <button className={classnames([css.button, css.destructive])}>
+          Reject
+        </button>
+      </>
+    );
+  } else if (userInfo.relationshipStatus === userRelationshipStatus.FRIEND) {
+    buttons = (
+      <button className={classnames([css.button, css.edit_button])}>
+        <SWMansionIcon icon="edit" size={22} />
+      </button>
+    );
+  }
 
-  return <div className={css.container}>{userInfo.username}</div>;
+  return (
+    <div className={css.container}>
+      {userInfo.username}
+      <div className={css.buttons}>{buttons}</div>
+    </div>
+  );
 }
 
 export default FriendListRow;
