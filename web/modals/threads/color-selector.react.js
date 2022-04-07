cfb5f87342d41c3c57f6e620a9726d00610ea5d5
Atul Madhugiri <atulm@berkeley.edu>
2022-04-07 18:39:27 -0400

    [web] Introduce `ColorSelector` component with basic styling
    Summary:
    Introduce `ColorSelector` component with basic layout/styling.
    
    Note: The actual functionality will be introduced in subsequent diffs.
    
    ---
    
    Depends on D3660
    
    Test Plan: Looks as expected:
    
    Reviewers: palys-swm, ashoat, def-au1t
    
    Subscribers: Adrian, karol-bisztyga, benschac, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D3662

---
+++ web/modals/threads/color-selector.react.js
@@ -0,0 +1,29 @@
+// @flow
+
+import * as React from 'react';
+
+import ColorSelectorButton from './color-selector-button.react';
+import css from './color-selector.css';
+
+function ColorSelector(): React.Node {
+  return (
+    <div className={css.container}>
+      <div className={css.row}>
+        <ColorSelectorButton color="#4B87AA" active={true} />
+        <ColorSelectorButton color="#5C9F5F" active={false} />
+        <ColorSelectorButton color="#B8753D" active={false} />
+        <ColorSelectorButton color="#AA4B4B" active={false} />
+        <ColorSelectorButton color="#6D49AB" active={false} />
+      </div>
+      <div className={css.row}>
+        <ColorSelectorButton color="#C85000" active={false} />
+        <ColorSelectorButton color="#008F83" active={false} />
+        <ColorSelectorButton color="#648CAA" active={false} />
+        <ColorSelectorButton color="#57697F" active={false} />
+        <ColorSelectorButton color="#575757" active={false} />
+      </div>
+    </div>
+  );
+}
+
+export default ColorSelector;
