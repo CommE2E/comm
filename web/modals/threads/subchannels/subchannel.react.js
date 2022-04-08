89dff72c45f77e74595ff5a126a26530121ca57f
Jacek Nitychoruk <jacek.nitychoruk@swmansion.com>
2022-04-08 16:03:14 +0200

    [web] Introduce `Subchannel` component
    Summary:
    Introduce component for displaying single subchannel with preview in thread subchannels modal
    
    {F28432}
    
    Figma:
    
    {F28433}
    https://www.figma.com/file/a1nkbWgbgjRlrOY9LVurTz/Comm-%2F-Desktop-app?node-id=633%3A58861
    
    Test Plan: The component is not used yet. The component can be tested when introducing subchannels modal.
    
    Reviewers: palys-swm, benschac, atul
    
    Reviewed By: palys-swm
    
    Subscribers: ashoat, Adrian, atul, karol-bisztyga, benschac, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D3599

---
+++ web/modals/threads/subchannels/subchannel.react.js
@@ -0,0 +1,75 @@
+// @flow
+
+import * as React from 'react';
+
+import { type ChatThreadItem } from 'lib/selectors/chat-selectors';
+import { getMessagePreview } from 'lib/shared/message-utils';
+import { shortAbsoluteDate } from 'lib/utils/date-utils';
+
+import { getDefaultTextMessageRules } from '../../../markdown/rules.react';
+import { useSelector } from '../../../redux/redux-utils';
+import { useOnClickThread } from '../../../selectors/nav-selectors';
+import SWMansionIcon from '../../../SWMansionIcon.react';
+import { useModalContext } from '../../modal-provider.react';
+import css from './subchannels-modal.css';
+
+type Props = {
+  +chatThreadItem: ChatThreadItem,
+};
+
+function Subchannel(props: Props): React.Node {
+  const { chatThreadItem } = props;
+  const {
+    threadInfo,
+    mostRecentMessageInfo,
+    lastUpdatedTimeIncludingSidebars,
+  } = chatThreadItem;
+
+  const timeZone = useSelector(state => state.timeZone);
+  const { clearModal } = useModalContext();
+
+  const navigateToThread = useOnClickThread(threadInfo);
+
+  const onClickThread = React.useCallback(
+    event => {
+      clearModal();
+      navigateToThread(event);
+    },
+    [clearModal, navigateToThread],
+  );
+
+  const lastActivity = React.useMemo(
+    () => shortAbsoluteDate(lastUpdatedTimeIncludingSidebars, timeZone),
+    [lastUpdatedTimeIncludingSidebars, timeZone],
+  );
+
+  const lastMessage = React.useMemo(() => {
+    if (!mostRecentMessageInfo) {
+      return <div className={css.noMessage}>No messages</div>;
+    }
+    const { message, username } = getMessagePreview(
+      mostRecentMessageInfo,
+      threadInfo,
+      getDefaultTextMessageRules().simpleMarkdownRules,
+    );
+    const previewText = username ? `${username}: ${message}` : message;
+    return (
+      <>
+        <div className={css.longTextEllipsis}>{previewText}</div>
+        <div className={css.lastActivity}>{lastActivity}</div>
+      </>
+    );
+  }, [lastActivity, mostRecentMessageInfo, threadInfo]);
+
+  return (
+    <div className={css.subchannelContainer} onClick={onClickThread}>
+      <SWMansionIcon icon="message-square" size={22} />
+      <div className={css.subchannelInfo}>
+        <div className={css.longTextEllipsis}>{threadInfo.name}</div>
+        <div className={css.lastMessage}>{lastMessage}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Subchannel;
