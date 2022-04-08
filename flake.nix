a3c5526316f512bbcadde28df7b7188eb6133753
Jonathan Ringer <jonringer117@gmail.com>
2022-04-07 23:00:32 -0700

    Improve wording of comments in overlay.nix
    Summary:
    
    Test Plan: Run `nix develop`
    
    Reviewers:
    
    Subscribers: ashoat, palys-swm, Adrian, atul, karol-bisztyga, benschac, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D3792

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Comm flake";
+
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs, utils, ... }:
+    let
+      # Overlays allow for extending a package set, in this case, we are extending nixpkgs
+      # with our devShell
+      localOverlay = import ./nix/overlay.nix;
+      overlays = [
+        localOverlay
+      ];
+
+      # Since we build for many systems (e.g. m1 mac, x86_64-linux), we create
+      # a helper function to help facilitate instantiation of the related package set
+      pkgsForSystem = system: import nixpkgs {
+        inherit overlays system;
+      };
+
+    # utils.lib.eachSystem helps create a result set of expected flake outputs
+    # of the form <output>.<system>
+    # https://github.com/numtide/flake-utils#usage for more examples
+    in utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ] (system: rec {
+      legacyPackages = pkgsForSystem system;
+      inherit (legacyPackages) devShell;
+    }) // {
+      # these outputs will lack the system suffix (e.g. devShell.aarch64-darwin), thus should
+      # be system agnostic such as overlays or utility functions.
+      overlays = { inherit localOverlay; };
+      overlay = localOverlay;
+    };
+}
---
+++ nix/overlay.nix
@@ -0,0 +1,15 @@
+# An overlay allows for a package set to be extended with new or modified packages
+
+# `final` refers to the package set with all overlays applied.
+# This allows for added or modified packages to be referenced with
+# all relevant changes
+final:
+
+# `prev` refers to the previous package set before this current overlay is applied.
+# This is cheaper for nix to evaluate, thus should be prefered over final when possible.
+prev:
+
+{
+  # add packages meant for just this repository
+  devShell = final.callPackage ./dev-shell.nix { };
+}
