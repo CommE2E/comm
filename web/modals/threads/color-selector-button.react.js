769453ce44f6c6ea24aab4c8cdf783747cd7ff23
Atul Madhugiri <atulm@berkeley.edu>
2022-04-10 22:11:17 -0400

    [web] Introduce `pendingColorSelection` state to `ColorSelector` component
    Summary:
    Introduce `pendingColorSelection` state to the `ColorSelector` component that keeps track of the user's color selection.
    
    `pendingColorSelection` is passed to the `ColorSelectorButton` component to style the button based on whether it is the actively selected color or not.
    
    `setPendingColorSelection` is also passed down to the `ColorSelectorButton` so any "new" color selections will update the `pendingColorSelection` state in `ColorSelector`.
    
    Test Plan:
    Works as expected:
    {F35981}
    
    Reviewers: palys-swm, def-au1t
    
    Reviewed By: palys-swm
    
    Subscribers: ashoat, palys-swm, Adrian, karol-bisztyga, benschac, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D3684

--- web/modals/threads/color-selector-button.react.js
+++ web/modals/threads/color-selector-button.react.js
@@ -3,15 +3,19 @@
 import classNames from 'classnames';
 import * as React from 'react';
 
+import { type SetState } from 'lib/types/hook-types.js';
+
 import css from './color-selector-button.css';
 
 type ColorSelectorButtonProps = {
   +color: string,
-  +active: boolean,
+  +pendingColorSelection: string,
+  +setPendingColorSelection: SetState<string>,
 };
 function ColorSelectorButton(props: ColorSelectorButtonProps): React.Node {
-  const { color, active } = props;
+  const { color, pendingColorSelection, setPendingColorSelection } = props;
 
+  const active = color === pendingColorSelection;
   const containerClassName = classNames(css.container, {
     [css.active]: active,
   });
@@ -23,8 +27,12 @@ function ColorSelectorButton(props: ColorSelectorButtonProps): React.Node {
     [color],
   );
 
+  const onColorSplotchClicked = React.useCallback(() => {
+    setPendingColorSelection(color);
+  }, [setPendingColorSelection, color]);
+
   return (
-    <div className={containerClassName}>
+    <div onClick={onColorSplotchClicked} className={containerClassName}>
       <div className={css.colorSplotch} style={colorSplotchStyle} />
     </div>
   );
--- web/modals/threads/color-selector.react.js
+++ web/modals/threads/color-selector.react.js
@@ -5,22 +5,70 @@ import * as React from 'react';
 import ColorSelectorButton from './color-selector-button.react';
 import css from './color-selector.css';
 
-function ColorSelector(): React.Node {
+type ColorSelectorProps = {
+  +currentThreadColor: string,
+};
+function ColorSelector(props: ColorSelectorProps): React.Node {
+  const { currentThreadColor } = props;
+  const [pendingColorSelection, setPendingColorSelection] = React.useState(
+    currentThreadColor,
+  );
+
   return (
     <div className={css.container}>
       <div className={css.row}>
-        <ColorSelectorButton color="#4B87AA" active={true} />
-        <ColorSelectorButton color="#5C9F5F" active={false} />
-        <ColorSelectorButton color="#B8753D" active={false} />
-        <ColorSelectorButton color="#AA4B4B" active={false} />
-        <ColorSelectorButton color="#6D49AB" active={false} />
+        <ColorSelectorButton
+          color="#4B87AA"
+          pendingColorSelection={pendingColorSelection}
+          setPendingColorSelection={setPendingColorSelection}
+        />
+        <ColorSelectorButton
+          color="#5C9F5F"
+          pendingColorSelection={pendingColorSelection}
+          setPendingColorSelection={setPendingColorSelection}
+        />
+        <ColorSelectorButton
+          color="#B8753D"
+          pendingColorSelection={pendingColorSelection}
+          setPendingColorSelection={setPendingColorSelection}
+        />
+        <ColorSelectorButton
+          color="#AA4B4B"
+          pendingColorSelection={pendingColorSelection}
+          setPendingColorSelection={setPendingColorSelection}
+        />
+        <ColorSelectorButton
+          color="#6D49AB"
+          pendingColorSelection={pendingColorSelection}
+          setPendingColorSelection={setPendingColorSelection}
+        />
       </div>
       <div className={css.row}>
-        <ColorSelectorButton color="#C85000" active={false} />
-        <ColorSelectorButton color="#008F83" active={false} />
-        <ColorSelectorButton color="#648CAA" active={false} />
-        <ColorSelectorButton color="#57697F" active={false} />
-        <ColorSelectorButton color="#575757" active={false} />
+        <ColorSelectorButton
+          color="#C85000"
+          pendingColorSelection={pendingColorSelection}
+          setPendingColorSelection={setPendingColorSelection}
+        />
+        <ColorSelectorButton
+          color="#008F83"
+          pendingColorSelection={pendingColorSelection}
+          setPendingColorSelection={setPendingColorSelection}
+        />
+        <ColorSelectorButton
+          color="#648CAA"
+          pendingColorSelection={pendingColorSelection}
+          setPendingColorSelection={setPendingColorSelection}
+        />
+        <ColorSelectorButton
+          color="#57697F"
+          pendingColorSelection={pendingColorSelection}
+          setPendingColorSelection={setPendingColorSelection}
+        />
+        <ColorSelectorButton
+          color="#575757"
+          pendingColorSelection={pendingColorSelection}
+          setPendingColorSelection={setPendingColorSelection}
+        />
       </div>
     </div>
   );
