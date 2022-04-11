f456a1f750ecaaeceb80869e4fb5c311883e5410
Max Kalashnikoff <geekmaks@gmail.com>
2022-04-11 16:08:36 +0530

    [services] Tunnelbroker - Database SessionSignatureItem tests
    Summary:
    Database (DynamoDB) item tests. For the `SessionSignatureItem` database entity we perform:
     - Check if the table is available,
     - Create and fulfill database item entity,
     - Put into the database table,
     - Find the record using the primary key,
     - Compare record fields of created item and item from database record,
     - Remove the testing record from the database.
    
    Related linear task: [[ https://linear.app/comm/issue/ENG-686 | ENG-686 ]]
    
    Test Plan: Run `yarn test-tunnelbroker-service` all tests are run and succeed.
    
    Reviewers: karol-bisztyga, palys-swm, varun
    
    Reviewed By: palys-swm
    
    Subscribers: ashoat, palys-swm, Adrian, atul, karol-bisztyga, benschac, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D3687

--- services/tunnelbroker/test/DatabaseManagerTest.cpp
+++ services/tunnelbroker/test/DatabaseManagerTest.cpp
@@ -173,3 +173,43 @@ TEST_F(DatabaseManagerTest, TestOperationsOnDeviceSessionItemGenerated) {
   database::DatabaseManager::getInstance().removeSessionItem(
       item.getSessionID());
 }
+
+TEST_F(DatabaseManagerTest, TestOperationsOnSessionSignItem) {
+  const database::SessionSignItem item(
+      "bB3OSLdKlY60KPBpw6VoGKX7Lmw3SA07FmNhnqnclvVeaxXueAQ0dpQSpiQTtlGn",
+      "mobile:EMQNoQ7b2ueEmQ4QsevRWlXxFCNt055y20T1PHdoYAQRt0S6TLzZWNM6XSvdWqxm";
+  EXPECT_EQ(
+      database::DatabaseManager::getInstance().isTableAvailable(
+          item.getTableName()),
+      true);
+  database::DatabaseManager::getInstance().putSessionSignItem(item);
+  std::shared_ptr<database::SessionSignItem> foundItem =
+      database::DatabaseManager::getInstance().findSessionSignItem(
+          item.getDeviceID());
+  EXPECT_NE(foundItem, nullptr);
+  EXPECT_EQ(item.getSign(), foundItem->getSign());
+  database::DatabaseManager::getInstance().removeSessionSignItem(
+      item.getDeviceID());
+}
+
+TEST_F(DatabaseManagerTest, TestOperationsOnSessionSignItemGenerated) {
+  const database::SessionSignItem item(
+      tools::generateRandomString(SIGNATURE_REQUEST_LENGTH),
+      "mobile:" + tools::generateRandomString(DEVICEID_CHAR_LENGTH));
+  EXPECT_EQ(
+      database::DatabaseManager::getInstance().isTableAvailable(
+          item.getTableName()),
+      true);
+  database::DatabaseManager::getInstance().putSessionSignItem(item);
+  std::shared_ptr<database::SessionSignItem> foundItem =
+      database::DatabaseManager::getInstance().findSessionSignItem(
+          item.getDeviceID());
+  EXPECT_NE(foundItem, nullptr) << "Item with the key of deviceID \""
+                                << item.getDeviceID() << "\" is not found";
+  EXPECT_EQ(item.getSign(), foundItem->getSign())
+      << "Generated signature value \"" << item.getSign()
+      << "\" is not equal of \"" + foundItem->getSign() +
+          "\" from the database value";
+  database::DatabaseManager::getInstance().removeSessionSignItem(
+      item.getDeviceID());
+}
