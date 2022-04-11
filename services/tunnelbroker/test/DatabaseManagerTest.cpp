8f2c63d89fcb127e800eb809c763c89e92a11c09
Max Kalashnikoff <geekmaks@gmail.com>
2022-04-11 16:13:47 +0530

    [services] Tunnelbroker - Database PublicKeyItem tests
    Summary:
    Database (DynamoDB) item tests. For the `PublicKeyItem` database entity we perform:
     - Check if the table is available,
     - Create and fulfill database item entity,
     - Put into the database table,
     - Find the record using the primary key,
     - Compare record fields of created item and item from database record,
     - Remove the testing record from the database.
    
    Related linear task: [[ https://linear.app/comm/issue/ENG-686 | ENG-686 ]]
    
    Test Plan: Run `yarn test-tunnelbroker-service` all tests are run and succeed.
    
    Reviewers: karol-bisztyga, palys-swm, varun
    
    Reviewed By: palys-swm
    
    Subscribers: ashoat, palys-swm, Adrian, atul, karol-bisztyga, benschac, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D3688

--- services/tunnelbroker/test/DatabaseManagerTest.cpp
+++ services/tunnelbroker/test/DatabaseManagerTest.cpp
@@ -213,3 +213,45 @@ TEST_F(DatabaseManagerTest, TestOperationsOnSessionSignItemGenerated) {
   database::DatabaseManager::getInstance().removeSessionSignItem(
       item.getDeviceID());
 }
+
+TEST_F(DatabaseManagerTest, PutAndFoundPublicKeyItemsStaticDataIsSame) {
+  const database::PublicKeyItem item(
+      "mobile:EMQNoQ7b2ueEmQ4QsevRWlXxFCNt055y20T1PHdoYAQRt0S6TLzZWNM6XSvdWqxm",
+      "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC9Q9wodsQdZNynbTnC35hA4mFW"
+      "mwZf9BhbI93aGAwPF9au0eYsawRz0jtYi4lSFXC9KleyQDg+6J+UW1kiWvE3ZRYG"
+      "ECqgx4zqajPTzVt7EAOGaIh/dPyQ6x2Ul1GlkkSYXUhhixEzExGp9g84eCyVkbCB"
+      "U3SK6SNKyR7anAXDVQIDAQAB");
+  EXPECT_EQ(
+      database::DatabaseManager::getInstance().isTableAvailable(
+          item.getTableName()),
+      true);
+  database::DatabaseManager::getInstance().putPublicKeyItem(item);
+  std::shared_ptr<database::PublicKeyItem> foundItem =
+      database::DatabaseManager::getInstance().findPublicKeyItem(
+          item.getDeviceID());
+  EXPECT_NE(foundItem, nullptr);
+  EXPECT_EQ(item.getPublicKey(), foundItem->getPublicKey());
+  database::DatabaseManager::getInstance().removePublicKeyItem(
+      item.getDeviceID());
+}
+
+TEST_F(DatabaseManagerTest, PutAndFoundPublicKeyItemsGeneratedDataIsSame) {
+  const database::PublicKeyItem item(
+      "mobile:" + tools::generateRandomString(DEVICEID_CHAR_LENGTH),
+      tools::generateRandomString(451));
+  EXPECT_EQ(
+      database::DatabaseManager::getInstance().isTableAvailable(
+          item.getTableName()),
+      true);
+  database::DatabaseManager::getInstance().putPublicKeyItem(item);
+  std::shared_ptr<database::PublicKeyItem> foundItem =
+      database::DatabaseManager::getInstance().findPublicKeyItem(
+          item.getDeviceID());
+  EXPECT_NE(foundItem, nullptr);
+  EXPECT_EQ(item.getPublicKey(), foundItem->getPublicKey())
+      << "Generated PublicKey \"" << item.getPublicKey()
+      << "\" differs from what is found in the database "
+      << foundItem->getPublicKey();
+  database::DatabaseManager::getInstance().removePublicKeyItem(
+      item.getDeviceID());
+}
