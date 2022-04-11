81657090550742b993c878e92e948d8c3571cb2c
Atul Madhugiri <atulm@berkeley.edu>
2022-04-11 18:24:33 -0400

    [web] First pass at styling `InvalidUploadModal`
    Summary:
    Started with styles borrowed from `thread-settings-modal.css` and made some tweaks so it looks decent.
    
    Before:
    {F37343}
    
    After:
    {F37344}
    
    Test Plan:
    1. Dragged SVG file into message list
    2. Confirmed that the modal looked as expected
    
    Reviewers: def-au1t, palys-swm
    
    Reviewed By: palys-swm
    
    Subscribers: ashoat, Adrian, karol-bisztyga, benschac, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D3707

--- web/modals/chat/invalid-upload.react.js
+++ web/modals/chat/invalid-upload.react.js
@@ -2,9 +2,10 @@
 
 import * as React from 'react';
 
+import Button from '../../components/button.react.js';
 import { useModalContext } from '../../modals/modal-provider.react';
-import css from '../../style.css';
 import Modal from '../modal.react';
+import css from './invalid-upload.css';
 
 type Props = {
   +clearModal: () => void,
@@ -13,8 +14,16 @@ class InvalidUploadModal extends React.PureComponent<Props> {
   render(): React.Node {
     return (
       <Modal name="Invalid upload" onClose={this.props.clearModal}>
-        <div className={css['modal-body']}>
+        <div className={css.modal_body}>
           <p>We don&apos;t support that file type yet :(</p>
+          <Button
+            onClick={this.props.clearModal}
+            type="submit"
+            variant="primary"
+            className={css.ok_button}
+          >
+            OK
+          </Button>
         </div>
       </Modal>
     );
