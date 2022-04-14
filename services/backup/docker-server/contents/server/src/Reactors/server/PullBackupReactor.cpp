cc7d781f23a30287eb7b9f7a34ab51aec31f5740
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-04-14 16:28:13 +0200

    [services] Backup - Move implementations to source files - pull backup reactor

---
+++ services/backup/docker-server/contents/server/src/Reactors/server/PullBackupReactor.cpp
@@ -0,0 +1,123 @@
+#include "PullBackupReactor.h"
+
+#include "DatabaseManager.h"
+
+namespace comm {
+namespace network {
+namespace reactor {
+
+PullBackupReactor::PullBackupReactor(const backup::PullBackupRequest *request)
+    : ServerWriteReactorBase<
+          backup::PullBackupRequest,
+          backup::PullBackupResponse>(request),
+      dataChunks(std::make_shared<folly::MPMCQueue<std::string>>(100)) {
+}
+
+void PullBackupReactor::initializeGetReactor(const std::string &holder) {
+  if (this->backupItem == nullptr) {
+    throw std::runtime_error(
+        "get reactor cannot be initialized when backup item is missing");
+  }
+  if (this->getReactor == nullptr) {
+    this->getReactor = std::make_shared<reactor::BlobGetClientReactor>(
+        holder, this->dataChunks);
+    this->getReactor->request.set_holder(holder);
+    this->blobClient.get(this->getReactor);
+  }
+}
+
+void PullBackupReactor::initialize() {
+  // we make sure that the blob client's state is flushed to the main memory
+  // as there may be multiple threads from the pool taking over here
+  const std::lock_guard<std::mutex> lock(this->reactorStateMutex);
+  if (this->request.userid().empty()) {
+    throw std::runtime_error("no user id provided");
+  }
+  if (this->request.backupid().empty()) {
+    throw std::runtime_error("no backup id provided");
+  }
+  this->backupItem = database::DatabaseManager::getInstance().findBackupItem(
+      this->request.userid(), this->request.backupid());
+  if (this->backupItem == nullptr) {
+    throw std::runtime_error(
+        "no backup found for provided parameters: user id [" +
+        this->request.userid() + "], backup id [" + this->request.backupid() +
+        "]");
+  }
+  this->logs = database::DatabaseManager::getInstance().findLogItemsForBackup(
+      this->request.backupid());
+}
+
+std::unique_ptr<grpc::Status>
+PullBackupReactor::writeResponse(backup::PullBackupResponse *response) {
+  // we make sure that the blob client's state is flushed to the main memory
+  // as there may be multiple threads from the pool taking over here
+  const std::lock_guard<std::mutex> lock(this->reactorStateMutex);
+  switch (this->state) {
+    case State::COMPACTION: {
+      this->initializeGetReactor(this->backupItem->getCompactionHolder());
+      std::string dataChunk;
+      this->dataChunks->blockingRead(dataChunk);
+      if (!dataChunk.empty()) {
+        response->set_compactionchunk(dataChunk);
+        return nullptr;
+      } else {
+        if (!this->dataChunks->isEmpty()) {
+          throw std::runtime_error(
+              "dangling data discovered after reading compaction");
+        }
+        this->getReactor = nullptr;
+        this->state = State::LOGS;
+        // WARNING: intentionally letting the flow enter case State::LOGS from
+        // here, because we want to start sending logs right away instead of
+        // going one more cycle
+      }
+    }
+    case State::LOGS: {
+      // TODO make sure logs are received in correct order regardless their size
+      if (this->logs.empty()) {
+        return std::make_unique<grpc::Status>(grpc::Status::OK);
+      }
+      if (this->currentLogIndex == this->logs.size()) {
+        return std::make_unique<grpc::Status>(grpc::Status::OK);
+      } else if (this->currentLogIndex > this->logs.size()) {
+        throw std::runtime_error("log index out of bound");
+      }
+      if (this->currentLog == nullptr) {
+        this->currentLog = this->logs.at(this->currentLogIndex);
+        if (this->currentLog->getPersistedInBlob()) {
+          this->initializeGetReactor(this->currentLog->getValue());
+        } else {
+          response->set_logchunk(this->currentLog->getValue());
+          ++this->currentLogIndex;
+          this->currentLog = nullptr;
+          return nullptr;
+        }
+      }
+      std::string dataChunk;
+      this->dataChunks->blockingRead(dataChunk);
+      if (dataChunk.empty()) {
+        ++this->currentLogIndex;
+        this->currentLog = nullptr;
+      } else {
+        response->set_logchunk(dataChunk);
+      }
+      return nullptr;
+    }
+  }
+  throw std::runtime_error("unhandled state");
+}
+
+void PullBackupReactor::terminateCallback() {
+  if (!this->getReactor->getStatus().ok()) {
+    throw std::runtime_error(this->getReactor->getStatus().error_message());
+  }
+}
+
+template class ServerWriteReactorBase<
+    backup::PullBackupRequest,
+    backup::PullBackupResponse>;
+
+} // namespace reactor
+} // namespace network
+} // namespace comm
