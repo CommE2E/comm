f5ffd17c335acb0c903b0fe12b746f45529dee02
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-04-19 12:18:38 +0200

    [services] Remove backup base Docker image

--- services/backup/docker/build_server.sh
+++ services/backup/docker/build_server.sh
@@ -5,8 +5,6 @@ set -e
 # folly hack - https://github.com/facebook/folly/pull/1231
 sed -i 's/#if __has_include(<demangle.h>)/#if __has_include(<Demangle.h>)/g' /usr/lib/folly/folly/detail/Demangle.h
 
-pushd /transferred/server
-
 rm -rf lib
 mkdir lib
 pushd lib
@@ -21,7 +19,5 @@ mkdir _generated
 rm -rf cmake/build
 mkdir -p cmake/build
 
-./generate.sh
-./build.sh
-
-popd # /transferred/server
+scripts/generate.sh
+scripts/build.sh
