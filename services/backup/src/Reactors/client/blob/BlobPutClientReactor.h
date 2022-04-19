e6654ab5c1b2b65f06678cb69baff5fe4c5b9c15
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-04-19 14:10:01 +0200

    [services] Backup - Move reactors' implementations to source files
    Summary: todo
    
    Test Plan: todo
    
    Subscribers: ashoat, palys-swm, Adrian, atul, benschac, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D3780

--- services/backup/src/Reactors/client/blob/BlobPutClientReactor.h
+++ services/backup/src/Reactors/client/blob/BlobPutClientReactor.h
@@ -1,16 +1,16 @@
 #pragma once
 
-#include "ClientBidiReactorBase.h"
 #include "Constants.h"
 
 #include "../_generated/blob.grpc.pb.h"
 #include "../_generated/blob.pb.h"
 
+#include "ClientBidiReactorBase.h"
+
 #include <folly/MPMCQueue.h>
 #include <grpcpp/grpcpp.h>
 
 #include <condition_variable>
-#include <iostream>
 #include <memory>
 #include <string>
 
@@ -50,62 +50,6 @@ public:
   grpc::Status getStatus() const;
   bool getDataExists() const;
 };
-
-BlobPutClientReactor::BlobPutClientReactor(
-    const std::string &holder,
-    const std::string &hash,
-    std::condition_variable *terminationNotifier)
-    : holder(holder),
-      hash(hash),
-      dataChunks(folly::MPMCQueue<std::string>(100)),
-      terminationNotifier(terminationNotifier) {
-}
-
-void BlobPutClientReactor::scheduleSendingDataChunk(
-    std::unique_ptr<std::string> dataChunk) {
-  if (!this->dataChunks.write(std::move(*dataChunk))) {
-    throw std::runtime_error(
-        "Error scheduling sending a data chunk to send to the blob service");
-  }
-}
-
-std::unique_ptr<grpc::Status> BlobPutClientReactor::prepareRequest(
-    blob::PutRequest &request,
-    std::shared_ptr<blob::PutResponse> previousResponse) {
-  if (this->state == State::SEND_HOLDER) {
-    this->request.set_holder(this->holder);
-    this->state = State::SEND_HASH;
-    return nullptr;
-  }
-  if (this->state == State::SEND_HASH) {
-    request.set_blobhash(this->hash);
-    this->state = State::SEND_CHUNKS;
-    return nullptr;
-  }
-  if (previousResponse->dataexists()) {
-    this->dataExists = true;
-    return std::make_unique<grpc::Status>(grpc::Status::OK);
-  }
-  std::string dataChunk;
-  this->dataChunks.blockingRead(dataChunk);
-  if (dataChunk.empty()) {
-    return std::make_unique<grpc::Status>(grpc::Status::OK);
-  }
-  request.set_datachunk(dataChunk);
-  return nullptr;
-}
-
-void BlobPutClientReactor::doneCallback() {
-  this->terminationNotifier->notify_one();
-}
-
-grpc::Status BlobPutClientReactor::getStatus() const {
-  return this->status;
-}
-
-bool BlobPutClientReactor::getDataExists() const {
-  return this->dataExists;
-}
 
 } // namespace reactor
 } // namespace network
