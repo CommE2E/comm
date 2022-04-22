c0b080eb8a52a27c482cac6d1a9ebdc20b8a138c
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-04-22 14:34:30 +0200

    [services] Blob - Pull more mutual code from base reactors - Apply in server reactors
    Summary:
    Depends on D3788
    
    https://linear.app/comm/issue/ENG-916/consider-pulling-more-mutual-code-from-the-async-reactors
    
    Applying inheriting the `BaseRactor` in the server reactors
    
    Test Plan:
    ```
    cd services
    yarn run-blob-service
    ```
    
    Reviewers: palys-swm, ashoat
    
    Subscribers: ashoat, palys-swm, Adrian, atul, benschac, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D3789

--- services/blob/src/Reactors/server/base-reactors/ServerReadReactorBase.h
+++ services/blob/src/Reactors/server/base-reactors/ServerReadReactorBase.h
@@ -1,73 +1,40 @@
 #pragma once
 
+#include "BaseReactor.h"
+
 #include <grpcpp/grpcpp.h>
 
+#include <atomic>
 #include <iostream>
 #include <memory>
 #include <string>
-#include <atomic>
 
 namespace comm {
 namespace network {
 namespace reactor {
 
 template <class Request, class Response>
-class ServerReadReactorBase : public grpc::ServerReadReactor<Request> {
+class ServerReadReactorBase : public grpc::ServerReadReactor<Request>,
+                              public BaseReactor {
   Request request;
-  std::atomic<bool> finished = false;
-
-  void terminate(grpc::Status status);
-
 protected:
   Response *response;
   grpc::Status status;
 
 public:
   ServerReadReactorBase(Response *response);
-
-  void OnDone() override;
   void OnReadDone(bool ok) override;
-
+  void terminate(const grpc::Status &status) override;
+  void OnDone() override;
   virtual std::unique_ptr<grpc::Status> readRequest(Request request) = 0;
-  virtual void initialize(){};
-  virtual void validate(){};
-  virtual void doneCallback(){};
-  virtual void terminateCallback(){};
 };
-
-template <class Request, class Response>
-void ServerReadReactorBase<Request, Response>::terminate(grpc::Status status) {
-  this->status = status;
-  try {
-    this->terminateCallback();
-    this->validate();
-  } catch (std::runtime_error &e) {
-    this->status = grpc::Status(grpc::StatusCode::INTERNAL, e.what());
-  }
-  if (!this->status.ok()) {
-    std::cout << "error: " << this->status.error_message() << std::endl;
-  }
-  if (this->finished) {
-    return;
-  }
-  this->Finish(this->status);
-  this->finished = true;
-}
 
 template <class Request, class Response>
 ServerReadReactorBase<Request, Response>::ServerReadReactorBase(
     Response *response)
     : response(response) {
-  this->initialize();
+  this->state = ReactorState::RUNNING;
   this->StartRead(&this->request);
-}
-
-template <class Request, class Response>
-void ServerReadReactorBase<Request, Response>::OnDone() {
-  this->doneCallback();
-  // This looks weird but apparently it is okay to do this. More information:
-  // https://phabricator.ashoat.com/D3246#87890
-  delete this;
 }
 
 template <class Request, class Response>
@@ -90,6 +57,35 @@ void ServerReadReactorBase<Request, Response>::OnReadDone(bool ok) {
     return;
   }
   this->StartRead(&this->request);
+}
+
+template <class Request, class Response>
+void ServerReadReactorBase<Request, Response>::terminate(
+    const grpc::Status &status) {
+  this->status = status;
+  try {
+    this->terminateCallback();
+    this->validate();
+  } catch (std::runtime_error &e) {
+    this->status = grpc::Status(grpc::StatusCode::INTERNAL, e.what());
+  }
+  if (!this->status.ok()) {
+    std::cout << "error: " << this->status.error_message() << std::endl;
+  }
+  if (this->state != ReactorState::RUNNING) {
+    return;
+  }
+  this->Finish(this->status);
+  this->state = ReactorState::TERMINATED;
+}
+
+template <class Request, class Response>
+void ServerReadReactorBase<Request, Response>::OnDone() {
+  this->state = ReactorState::DONE;
+  this->doneCallback();
+  // This looks weird but apparently it is okay to do this. More information:
+  // https://phabricator.ashoat.com/D3246#87890
+  delete this;
 }
 
 } // namespace reactor
