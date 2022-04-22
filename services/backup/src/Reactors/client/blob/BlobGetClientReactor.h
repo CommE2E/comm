5612688fda1b385c1abc65da2529f0680cb7cce8
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-04-22 14:59:04 +0200

    [services] Backup - Move reactors' implementations to source files
    Summary:
    Depends on D3779
    
    https://linear.app/comm/issue/ENG-917/try-to-move-reactors-implementations-to-cpp-files
    
    Moving reactors' implementations to the source files.
    
    Test Plan:
    ```
    cd services
    yarn run-backup-server
    ```
    
    Reviewers: palys-swm, jimpo, ashoat
    
    Reviewed By: palys-swm
    
    Subscribers: jimpo, ashoat, palys-swm, Adrian, atul, benschac, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D3780

--- services/backup/src/Reactors/client/blob/BlobGetClientReactor.h
+++ services/backup/src/Reactors/client/blob/BlobGetClientReactor.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#include "ClientReadReactorBase.h"
-
 #include "../_generated/blob.grpc.pb.h"
 #include "../_generated/blob.pb.h"
+
+#include "ClientReadReactorBase.h"
 
 #include <folly/MPMCQueue.h>
 #include <grpcpp/grpcpp.h>
@@ -30,28 +30,6 @@ public:
   void doneCallback() override;
   grpc::Status getStatus() const;
 };
-
-BlobGetClientReactor::BlobGetClientReactor(
-    const std::string &holder,
-    std::shared_ptr<folly::MPMCQueue<std::string>> dataChunks)
-    : holder(holder), dataChunks(dataChunks) {
-}
-
-std::unique_ptr<grpc::Status>
-BlobGetClientReactor::readResponse(blob::GetResponse &response) {
-  if (!this->dataChunks->write(std::move(*response.mutable_datachunk()))) {
-    throw std::runtime_error("error reading data from the blob service");
-  }
-  return nullptr;
-}
-
-void BlobGetClientReactor::doneCallback() {
-  this->dataChunks->write("");
-}
-
-grpc::Status BlobGetClientReactor::getStatus() const {
-  return this->status;
-}
 
 } // namespace reactor
 } // namespace network
