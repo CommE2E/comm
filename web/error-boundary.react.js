793f9b9df77fa2c944139e239b79c43f6b2f6c63
Benjamin Schachter <benschac@gmail.com>
2022-04-25 08:18:34 -0400

    [web] [feat/chore] [ENG-813] add error boundary to the app
    Summary:
    add error boundary to the web app, when the project crashes, end user gets a message.
    
    https://linear.app/comm/issue/ENG-813/add-error-boundaries
    
    {F43181}
    
    Test Plan: This is just a stripped down version of the `error-boundry.react.js` file in native
    
    Reviewers: atul, palys-swm, def-au1t, yayabosh, ashoat
    
    Reviewed By: ashoat
    
    Subscribers: ashoat, Adrian, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D3811

---
+++ web/error-boundary.react.js
@@ -0,0 +1,42 @@
+// @flow
+
+import * as React from 'react';
+
+import type { ErrorInfo, ErrorData } from 'lib/types/report-types';
+
+import css from './error-boundary.css';
+
+type Props = {
+  +children: React.Node,
+};
+
+type State = {
+  +errorData: $ReadOnlyArray<ErrorData>,
+  +showError: boolean,
+};
+
+class ErrorBoundary extends React.PureComponent<Props, State> {
+  state: State = {
+    errorData: [],
+    showError: false,
+  };
+
+  componentDidCatch(error: Error, info: ErrorInfo) {
+    this.setState(prevState => ({
+      errorData: [...prevState.errorData, { error, info }],
+    }));
+  }
+
+  render(): React.Node {
+    if (this.state.errorData.length > 0) {
+      return (
+        <div className={css.container}>
+          <h3>Something has gone wrong, please reload the page</h3>
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
--- web/hot.js
+++ web/hot.js
@@ -5,15 +5,18 @@ import { hot } from 'react-hot-loader/root';
 import { Router, Route } from 'react-router';
 
 import App from './app.react';
+import ErrorBoundary from './error-boundary.react';
 import history from './router-history';
 import Socket from './socket.react';
 
 const RootComponent = () => (
   <React.Fragment>
-    <Router history={history.getHistoryObject()}>
-      <Route path="*" component={App} />
-    </Router>
-    <Socket />
+    <ErrorBoundary>
+      <Router history={history.getHistoryObject()}>
+        <Route path="*" component={App} />
+      </Router>
+      <Socket />
+    </ErrorBoundary>
   </React.Fragment>
 );
 
