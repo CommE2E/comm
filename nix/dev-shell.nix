526318477a082a96f56bf9c05a1e97969ed09cbc
Jonathan Ringer <jonringer117@gmail.com>
2022-04-28 21:20:25 -0700

    Update nix dev shell
    Summary:
    A lot has happened since the first initial yarn dev diff
    This is to update the dependencies as they have evolved since
    then
    
    Depends on D3865
    
    Test Plan:
    Run `nix develop` to create shell.
    Run `yarn cleaninstall` to build all yarn projects
    
    Reviewers: ashoat, varun
    
    Reviewed By: ashoat, varun
    
    Subscribers: ashoat, palys-swm, Adrian, atul, karol-bisztyga, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D3874

--- nix/dev-shell.nix
+++ nix/dev-shell.nix
@@ -1,10 +1,21 @@
 { mkShell
 , stdenv
 , lib
+, amqp-cpp
 , arcanist
+, cargo
+, cmake
+, cryptopp
 , darwin
+, grpc
+, libuv
 , nodejs-16_x
-, protobuf3_15
+, pkg-config
+, protobuf_3_15_cmake
+, python2
+, python3
+, watchman
+, rustfmt
 , yarn
 }:
 
@@ -12,17 +23,39 @@ mkShell {
 
   # programs which are meant to be executed should go here
   nativeBuildInputs = [
+    # generic development
     arcanist
+
+    # node development
     nodejs-16_x
-    protobuf3_15
     yarn
+    watchman # react native
+    python2
+    python3
+
+    # native dependencies
+    # C/CXX toolchains are already brought in with mkShell
+    # Identity Service
+    cargo # includes rustc
+    rustfmt
+
+    # Tunnelbroker + CMake
+    amqp-cpp
+    cryptopp
+    cmake
+    libuv
+    pkg-config
+    protobuf_3_15_cmake
+    grpc
+
   ];
 
   # include any libraries buildInputs
   buildInputs = [
-    protobuf3_15 # exposes both a library and a command, thus should appear in both inputs
+    protobuf_3_15_cmake # exposes both a library and a command, thus should appear in both inputs
   ] ++ lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
     CoreFoundation
+    CoreServices
     Security
   ]);
 
