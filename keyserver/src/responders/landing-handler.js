36d9c7660b67ce5511bcd91edbe59d20fb2d19db
Abosh Upadhyaya <aboshu@gmail.com>
2022-04-30 23:41:26 -0700

    [landing] Add thumbnail and description for landing page when shared
    Summary:
    Add image thumbnail and website description (from Twitter bio) for the landing page so it appears in Twitter Cards/Facebook messages/posts when shared.
    
    Related Linear issues with full context: [[ https://linear.app/comm/issue/ENG-1066/add-image-thumbnailwebsite-description-for-landing-page-when-shared | ENG-1066 ]].
    
    Test Plan:
    N/A, as of now. There is no staging environment with a public URL so not sure how to test these changes yet. This is tracked by [[ https://linear.app/comm/issue/ENG-889/investigate-deploy-previews-for-web-and-landing | ENG-889 ]] (FB called this "Sandcastle").
    
    Once there is, though:
    - The [[ https://cards-dev.twitter.com/validator | Twitter Card Validator ]] tests how URLs look on Twitter Cards when Tweeted.
    - Facebook has its own [[ https://developers.facebook.com/tools/debug/ | Markup Tester ]] to see how URLs look on Facebook posts when shared. I'm not sure if this works for Messenger as well, but that can be tested by sending a message to someone.
    
    Reviewers: atul, ashoat
    
    Reviewed By: atul, ashoat
    
    Subscribers: benschac, ashoat, palys-swm, Adrian, karol-bisztyga
    
    Differential Revision: https://phabricator.ashoat.com/D3886

--- keyserver/src/responders/landing-handler.js
+++ keyserver/src/responders/landing-handler.js
@@ -112,6 +112,13 @@ async function landingResponder(req: $Request, res: $Response) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width" />
+        <meta property="og:url" content="https://comm.app/" />
+        <meta property="og:title" content="Comm" />
+        <meta property="og:description" content="Crypto-native messaging based on federated keyservers" />
+        <meta property="og:image" content="https://comm.app/landing-favicon-32x32.png" />
+        <meta property="og:image:alt" content="Comm logo" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@CommDotApp" />
         <title>Comm</title>
         <base href="${basePath}" />
         ${fontsInclude}
