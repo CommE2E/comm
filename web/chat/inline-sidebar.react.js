bec3f23ed027c3c63d1497a7f469380830dd882c
Benjamin Schachter <benschac@gmail.com>
2022-05-02 17:34:21 -0400

    [web] [chore] [ENG-530] remove old styles from inline sidebar component
    Summary: remove all old styles and icons since the new design is pretty different from what we have. This diff shouldn't be landed on it's own. More just clean up for updating the component
    
    Test Plan: N/A additionially, there's literally nothing to see here. The text is black by default so you're not going to see anything on the screen.
    
    Reviewers: atul, ashoat
    
    Subscribers: palys-swm, Adrian, karol-bisztyga, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D3893

--- web/chat/inline-sidebar.react.js
+++ web/chat/inline-sidebar.react.js
@@ -1,17 +1,11 @@
 // @flow
 
-import classNames from 'classnames';
 import * as React from 'react';
-import {
-  CornerDownRight as CornerDownRightIcon,
-  CornerDownLeft as CornerDownLeftIcon,
-} from 'react-feather';
 
 import useInlineSidebarText from 'lib/hooks/inline-sidebar-text.react';
 import type { ThreadInfo } from 'lib/types/thread-types';
 
 import { useOnClickThread } from '../selectors/nav-selectors';
-import css from './inline-sidebar.css';
 
 type Props = {
   +threadInfo: ThreadInfo,
@@ -23,40 +17,14 @@ function InlineSidebar(props: Props): React.Node {
 
   const onClick = useOnClickThread(threadInfo);
 
-  let viewerIcon, nonViewerIcon, alignStyle;
-  if (props.positioning === 'right') {
-    viewerIcon = (
-      <CornerDownLeftIcon className={css.inlineSidebarIcon} size={18} />
-    );
-    alignStyle = css.viewerMessageBoxContainer;
-  } else if (props.positioning === 'left') {
-    nonViewerIcon = (
-      <CornerDownRightIcon className={css.inlineSidebarIcon} size={18} />
-    );
-    alignStyle = css.nonViewerMessageBoxContainer;
-  } else {
-    nonViewerIcon = (
-      <CornerDownRightIcon className={css.inlineSidebarIcon} size={18} />
-    );
-    alignStyle = css.centerContainer;
-  }
-
-  const unreadStyle = threadInfo.currentUser.unread ? css.unread : null;
-
   return (
-    <div className={classNames([css.inlineSidebarContent, alignStyle])}>
-      <div onClick={onClick} className={css.inlineSidebar}>
-        {nonViewerIcon}
-        <div className={classNames([css.inlineSidebarName, unreadStyle])}>
-          {sendersText}
-          {repliesText}
-        </div>
-        {viewerIcon}
+    <div onClick={onClick}>
+      <div>
+        {sendersText}
+        {repliesText}
       </div>
     </div>
   );
 }
 
-const inlineSidebarHeight = 20;
-
-export { InlineSidebar, inlineSidebarHeight };
+export default InlineSidebar;
