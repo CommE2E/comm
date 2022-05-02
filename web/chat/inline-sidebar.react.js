5760e0f7f8c6b2d30339678b23cba41fad30b5e9
Benjamin Schachter <benschac@gmail.com>
2022-05-02 17:54:20 -0400

    [web] [chore] [ENG-530] style inline sidebar
    Summary: core styling to match figma. The inside bar is not positioned yet, that will be addressed in the next diff.
    
    Test Plan: Inspect the inline chat bar with figma. Make sure the design matches the code.
    
    Reviewers: atul, ashoat, def-au1t, palys-swm
    
    Subscribers: Adrian, karol-bisztyga, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D3895

--- web/chat/inline-sidebar.react.js
+++ web/chat/inline-sidebar.react.js
@@ -1,25 +1,29 @@
 // @flow
-
+import classNames from 'classnames';
 import * as React from 'react';
 
 import useInlineSidebarText from 'lib/hooks/inline-sidebar-text.react';
 import type { ThreadInfo } from 'lib/types/thread-types';
 
 import { useOnClickThread } from '../selectors/nav-selectors';
+import SWMansionIcon from '../SWMansionIcon.react';
+import css from './inline-sidebar.css';
 
 type Props = {
   +threadInfo: ThreadInfo,
   +position: 'left' | 'center' | 'right',
 };
 function InlineSidebar(props: Props): React.Node {
-  const { threadInfo } = props;
+  const { threadInfo, position } = props;
   const { repliesText } = useInlineSidebarText(threadInfo);
-
   const onClick = useOnClickThread(threadInfo);
+  const containerCls = classNames(css.container, [css[position]]);
+  const replyCls = classNames({ [css.unread]: threadInfo.currentUser.unread });
 
   return (
-    <div onClick={onClick}>
-      <div>{repliesText}</div>
+    <div className={containerCls} onClick={onClick}>
+      <SWMansionIcon icon="reply-chat-bubble" size={12} />
+      <div className={replyCls}>{repliesText}</div>
     </div>
   );
 }
