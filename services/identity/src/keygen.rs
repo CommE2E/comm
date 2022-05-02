adcab69a1a6f1ca4dc22be17015d968a48eb4a03
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2022-05-02 11:41:41 -0400

    [Identity] Script to generate and persist a server keypair for PAKE
    Summary:
    Depends on D3747
    
    The Identity service will need to persist a keypair to use for PAKE registration and login. We use the ciphersuite implementation from our common library to generate a random keypair, then write the secret key (public key can be derived from this) to secrets/secret_key.
    
    Test Plan:
    Ran the keygen executable a couple times to make sure the directory is created if it doesn't exist and the file contents are overwritten each time
    
    varun@varuns-MBP identity % ./target/debug/keygen
    Creating secrets directory "/Users/varun/Code/comm/services/identity/secrets"
    Writing secret key to "/Users/varun/Code/comm/services/identity/secrets/secret_key"
    varun@varuns-MBP identity % ./target/debug/keygen
    Writing secret key to "/Users/varun/Code/comm/services/identity/secrets/secret_key"
    
    Reviewers: jimpo, karol-bisztyga, palys-swm, atul, ashoat
    
    Reviewed By: jimpo, ashoat
    
    Subscribers: ashoat, Adrian, benschac, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D3799

---
+++ services/identity/src/keygen.rs
@@ -0,0 +1,26 @@
+use crate::opaque::Cipher;
+use opaque_ke::{ciphersuite::CipherSuite, rand::rngs::OsRng};
+use std::{env, fs, io};
+
+const SECRETS_FILE_NAME: &str = "secret_key";
+const SECRETS_FILE_EXTENSION: &str = "txt";
+
+pub fn generate_and_persist_keypair(dir: &str) -> Result<(), io::Error> {
+  let mut rng = OsRng;
+  let server_kp = Cipher::generate_random_keypair(&mut rng);
+  let mut path = env::current_dir()?;
+  path.push(dir);
+  if !path.exists() {
+    println!("Creating secrets directory {:?}", path);
+    fs::create_dir(&path)?;
+  }
+  path.push(SECRETS_FILE_NAME);
+  path.set_extension(SECRETS_FILE_EXTENSION);
+  if path.exists() {
+    println!("{:?} already exists", path);
+    return Ok(());
+  }
+  println!("Writing secret key to {:?}", path);
+  fs::write(&path, server_kp.private().to_arr())?;
+  Ok(())
+}
--- services/identity/src/main.rs
+++ services/identity/src/main.rs
@@ -1,19 +1,53 @@
+use clap::{Parser, Subcommand};
 use tonic::transport::Server;
 
+mod keygen;
+mod opaque;
 mod service;
+
+use keygen::generate_and_persist_keypair;
 use service::{IdentityServiceServer, MyIdentityService};
 
 const IDENTITY_SERVICE_SOCKET_ADDR: &str = "[::]:50051";
+const DEFAULT_SECRETS_DIRECTORY: &str = "secrets";
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+#[clap(propagate_version = true)]
+struct Cli {
+  #[clap(subcommand)]
+  command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+  /// Runs the server
+  Server,
+  /// Generates and persists a keypair to use for PAKE registration and login
+  Keygen {
+    #[clap(short, long)]
+    #[clap(default_value_t = String::from(DEFAULT_SECRETS_DIRECTORY))]
+    dir: String,
+  },
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-  let addr = IDENTITY_SERVICE_SOCKET_ADDR.parse()?;
-  let identity_service = MyIdentityService::default();
+  let cli = Cli::parse();
+  match &cli.command {
+    Commands::Keygen { dir } => {
+      generate_and_persist_keypair(dir)?;
+    }
+    Commands::Server => {
+      let addr = IDENTITY_SERVICE_SOCKET_ADDR.parse()?;
+      let identity_service = MyIdentityService::default();
 
-  Server::builder()
-    .add_service(IdentityServiceServer::new(identity_service))
-    .serve(addr)
-    .await?;
+      Server::builder()
+        .add_service(IdentityServiceServer::new(identity_service))
+        .serve(addr)
+        .await?;
+    }
+  }
 
   Ok(())
 }
