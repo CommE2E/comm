1f8d82731f395f05b3c030ec5a66867468e3f1ed
Benjamin Schachter <benschac@gmail.com>
2022-05-02 17:54:20 -0400

    [web] [chore] [ENG-530] style inline sidebar
    Summary: core styling to match figma. The inside bar is not positioned yet, that will be addressed in the next diff.
    
    Test Plan: Inspect the inline chat bar with figma. Make sure the design matches the code.
    
    Reviewers: @atul, @ashoat, @def-au1t, @palys-swm
    
    Subscribers:

--- web/chat/inline-sidebar.react.js
+++ web/chat/inline-sidebar.react.js
@@ -6,6 +6,8 @@ import useInlineSidebarText from 'lib/hooks/inline-sidebar-text.react';
 import type { ThreadInfo } from 'lib/types/thread-types';
 
 import { useOnClickThread } from '../selectors/nav-selectors';
+import SWMansionIcon from '../SWMansionIcon.react';
+import css from './inline-sidebar.css';
 
 type Props = {
   +threadInfo: ThreadInfo,
@@ -14,11 +16,11 @@ type Props = {
 function InlineSidebar(props: Props): React.Node {
   const { threadInfo } = props;
   const { repliesText } = useInlineSidebarText(threadInfo);
-
   const onClick = useOnClickThread(threadInfo);
 
   return (
-    <div onClick={onClick}>
+    <div className={css.container} onClick={onClick}>
+      <SWMansionIcon icon="reply-chat-bubble" size={12} />
       <div>{repliesText}</div>
     </div>
   );
