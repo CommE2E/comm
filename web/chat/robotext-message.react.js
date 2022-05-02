1a116dacc827efa4d6bcd683af67f6cbd7109404
Benjamin Schachter <benschac@gmail.com>
2022-05-02 17:34:21 -0400

    [web] [chore] [ENG-530] remove old styles from inline sidebar component
    Summary: remove all old styles and icons since the new design is pretty different from what we have. This diff shouldn't be landed on it's own. More just clean up for updating the component
    
    Test Plan: N/A additionially, there's literally nothing to see here. The text is black by default so you're not going to see anything on the screen.
    
    Reviewers: atul, ashoat
    
    Subscribers: palys-swm, Adrian, karol-bisztyga, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D3893

--- web/chat/robotext-message.react.js
+++ web/chat/robotext-message.react.js
@@ -15,7 +15,7 @@ import Markdown from '../markdown/markdown.react';
 import { linkRules } from '../markdown/rules.react';
 import { useSelector } from '../redux/redux-utils';
 import { updateNavInfoActionType } from '../types/nav-types';
-import { InlineSidebar } from './inline-sidebar.react';
+import InlineSidebar from './inline-sidebar.react';
 import MessageActionButtons from './message-action-buttons';
 import type {
   MessagePositionInfo,
