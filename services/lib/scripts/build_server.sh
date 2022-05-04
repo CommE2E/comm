c76cc263d656ac557e5307081f98db0ff5035a8a
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-05-04 12:55:07 +0200

    [services] Lib - Add scripts to lib
    Summary: todo
    
    Test Plan: todo
    
    Subscribers: ashoat, palys-swm, Adrian, atul, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D3906

---
+++ services/lib/scripts/build_server.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+# folly hack - https://github.com/facebook/folly/pull/1231
+sed -i 's/#if __has_include(<demangle.h>)/#if __has_include(<Demangle.h>)/g' /usr/lib/folly/folly/detail/Demangle.h
+
+rm -rf lib
+mkdir lib
+pushd lib
+ln -s /usr/lib/folly
+ln -s /usr/lib/glog
+ln -s /usr/lib/double-conversion
+popd # lib
+
+rm -rf _generated
+mkdir _generated
+
+rm -rf cmake/build
+mkdir -p cmake/build
+
+scripts/generate.sh
+scripts/build.sh
