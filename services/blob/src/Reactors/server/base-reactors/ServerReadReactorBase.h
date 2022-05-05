a6a831ba792039d91d70112d367121c35ebe6ab2
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-05-05 10:43:36 +0200

    [services] Blob - Pull more mutual code from base reactors - Apply in server reactors
    Summary:
    Depends on D3788
    
    https://linear.app/comm/issue/ENG-916/consider-pulling-more-mutual-code-from-the-async-reactors
    
    Applying inheriting the `BaseRactor` in the server reactors
    
    Test Plan:
    ```
    cd services
    yarn run-blob-service
    ```
    
    Reviewers: palys-swm, ashoat
    
    Reviewed By: ashoat
    
    Subscribers: ashoat, palys-swm, Adrian, atul, benschac, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D3789

--- services/blob/src/Reactors/server/base-reactors/ServerReadReactorBase.h
+++ services/blob/src/Reactors/server/base-reactors/ServerReadReactorBase.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "BaseReactor.h"
+
 #include <grpcpp/grpcpp.h>
 
 #include <atomic>
@@ -12,62 +14,35 @@ namespace network {
 namespace reactor {
 
 template <class Request, class Response>
-class ServerReadReactorBase : public grpc::ServerReadReactor<Request> {
+class ServerReadReactorBase : public grpc::ServerReadReactor<Request>,
+                              public BaseReactor {
+  std::shared_ptr<ReactorUtility> utility;
   Request request;
-  std::atomic<bool> finished = false;
-
-  void terminate(grpc::Status status);
 
 protected:
   Response *response;
-  grpc::Status status;
 
 public:
   ServerReadReactorBase(Response *response);
 
-  void OnDone() override;
+  void validate() override{};
+  void doneCallback() override{};
+  void terminateCallback() override{};
+
   void OnReadDone(bool ok) override;
+  void terminate(const grpc::Status &status) override;
+  void OnDone() override;
+  std::shared_ptr<ReactorUtility> getUtility() override;
 
   virtual std::unique_ptr<grpc::Status> readRequest(Request request) = 0;
-  virtual void initialize(){};
-  virtual void validate(){};
-  virtual void doneCallback(){};
-  virtual void terminateCallback(){};
 };
-
-template <class Request, class Response>
-void ServerReadReactorBase<Request, Response>::terminate(grpc::Status status) {
-  this->status = status;
-  try {
-    this->terminateCallback();
-    this->validate();
-  } catch (std::runtime_error &e) {
-    this->status = grpc::Status(grpc::StatusCode::INTERNAL, e.what());
-  }
-  if (!this->status.ok()) {
-    std::cout << "error: " << this->status.error_message() << std::endl;
-  }
-  if (this->finished) {
-    return;
-  }
-  this->Finish(this->status);
-  this->finished = true;
-}
 
 template <class Request, class Response>
 ServerReadReactorBase<Request, Response>::ServerReadReactorBase(
     Response *response)
     : response(response) {
-  this->initialize();
+  this->utility->state = ReactorState::RUNNING;
   this->StartRead(&this->request);
-}
-
-template <class Request, class Response>
-void ServerReadReactorBase<Request, Response>::OnDone() {
-  this->doneCallback();
-  // This looks weird but apparently it is okay to do this. More information:
-  // https://phabricator.ashoat.com/D3246#87890
-  delete this;
 }
 
 template <class Request, class Response>
@@ -90,6 +65,43 @@ void ServerReadReactorBase<Request, Response>::OnReadDone(bool ok) {
     return;
   }
   this->StartRead(&this->request);
+}
+
+template <class Request, class Response>
+void ServerReadReactorBase<Request, Response>::terminate(
+    const grpc::Status &status) {
+  this->utility->setStatus(status);
+  try {
+    this->terminateCallback();
+    this->validate();
+  } catch (std::runtime_error &e) {
+    this->utility->setStatus(
+        grpc::Status(grpc::StatusCode::INTERNAL, e.what()));
+  }
+  if (!this->utility->getStatus().ok()) {
+    std::cout << "error: " << this->utility->getStatus().error_message()
+              << std::endl;
+  }
+  if (this->utility->state != ReactorState::RUNNING) {
+    return;
+  }
+  this->Finish(this->utility->getStatus());
+  this->utility->state = ReactorState::TERMINATED;
+}
+
+template <class Request, class Response>
+void ServerReadReactorBase<Request, Response>::OnDone() {
+  this->utility->state = ReactorState::DONE;
+  this->doneCallback();
+  // This looks weird but apparently it is okay to do this. More information:
+  // https://phabricator.ashoat.com/D3246#87890
+  delete this;
+}
+
+template <class Request, class Response>
+std::shared_ptr<ReactorUtility>
+ServerReadReactorBase<Request, Response>::getUtility() {
+  return this->utility;
 }
 
 } // namespace reactor
