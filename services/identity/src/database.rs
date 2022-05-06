15980a381f92114ac7359c01d8b3783000b9a779
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2022-05-06 16:24:25 -0400

    [Identity] Add new Error variant
    Summary:
    The Rusoto Error variant is too specific to handle both get and put errors. I'm therefore breaking it out into two variants, RusotoGet and RusotoPut. This will help us handle errors related to the `put_item` Rusoto method.
    
    Depends on D3960
    
    Test Plan: cargo build
    
    Reviewers: jimpo, karol-bisztyga, palys-swm
    
    Reviewed By: jimpo, karol-bisztyga
    
    Subscribers: ashoat, Adrian, yayabosh, atul
    
    Differential Revision: https://phabricator.ashoat.com/D3961

--- services/identity/src/database.rs
+++ services/identity/src/database.rs
@@ -75,7 +75,7 @@ impl DatabaseClient {
           "DynamoDB client failed to get registration data for user {}: {}",
           user_id, e
         );
-        Err(Error::Rusoto(e))
+        Err(Error::RusotoGet(e))
       }
     }
   }
@@ -154,7 +154,7 @@ impl DatabaseClient {
           "DynamoDB client failed to get token for user {} on device {}: {}",
           user_id, device_id, e
         );
-        Err(Error::Rusoto(e))
+        Err(Error::RusotoGet(e))
       }
     }
   }
@@ -165,7 +165,9 @@ impl DatabaseClient {
 )]
 pub enum Error {
   #[display(...)]
-  Rusoto(RusotoError<GetItemError>),
+  RusotoGet(RusotoError<GetItemError>),
+  #[display(...)]
+  RusotoPut(RusotoError<PutItemError>),
   #[display(...)]
   Pake(ProtocolError),
   #[display(...)]
