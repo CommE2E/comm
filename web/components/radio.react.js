83bd059bf34c5d279b4b2cd88389643dd8aa9d3a
Jacek Nitychoruk <jacek.nitychoruk@swmansion.com>
2022-05-06 16:14:42 +0200

    [web] Introduce custom `Radio` component
    Summary: Introduce custom "Radio" component matching the one from our design system.
    
    Test Plan: component is not used yet, tested when introducing notification settings.
    
    Subscribers: ashoat, palys-swm, Adrian, atul, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D3946

---
+++ web/components/radio.react.js
@@ -0,0 +1,15 @@
+// @flow
+import * as React from 'react';
+
+import css from './radio.css';
+
+type Props = {
+  +checked: boolean,
+};
+
+function Radio(props: Props): React.Node {
+  const { checked } = props;
+  return <input className={css.radio} type="radio" checked={checked} />;
+}
+
+export default Radio;
