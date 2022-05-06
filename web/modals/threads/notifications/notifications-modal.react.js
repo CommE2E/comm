8cc916478c074645e14f7044f7c67ed388f26a49
Jacek Nitychoruk <jacek.nitychoruk@swmansion.com>
2022-05-06 16:39:26 +0200

    [web] Introduce thread notification settings options in modal

--- web/modals/threads/notifications/notifications-modal.react.js
+++ web/modals/threads/notifications/notifications-modal.react.js
@@ -6,8 +6,10 @@ import { threadInfoSelector } from 'lib/selectors/thread-selectors';
 
 import Button from '../../../components/button.react';
 import { useSelector } from '../../../redux/redux-utils';
+import SWMansionIcon from '../../../SWMansionIcon.react';
 import Modal from '../../modal.react';
 import css from './notifications-modal.css';
+import NotificationsOption from './notifications-option.react';
 
 type Props = {
   +threadID: string,
@@ -33,17 +35,92 @@ function NotificationsModal(props: Props): React.Node {
 
   const [
     notificationSettings,
-    // eslint-disable-next-line no-unused-vars
     setNotificationSettings,
   ] = React.useState<NotificationSettings>(threadOriginalSetting);
 
-  const onClickSave = React.useCallback(() => {}, []);
-  const notificationOptions = [];
+  const notificationIconStyle = React.useMemo(() => ({ width: 'auto' }), []);
 
+  const focusedItem = React.useMemo(() => {
+    const description = [
+      ['Banner notifs', true],
+      ['Badge count', true],
+      ['Lives in Focused tab', true],
+    ];
+    const icon = (
+      <SWMansionIcon
+        icon="all-notifs"
+        size={86}
+        style={notificationIconStyle}
+      />
+    );
+    return (
+      <NotificationsOption
+        active={notificationSettings === 'focused'}
+        title="Focused (enabled)"
+        description={description}
+        icon={icon}
+        onSelect={() => setNotificationSettings('focused')}
+      ></NotificationsOption>
+    );
+  }, [notificationIconStyle, notificationSettings]);
+
+  const focusedBadgeOnlyItem = React.useMemo(() => {
+    const description = [
+      ['Banner notifs', false],
+      ['Badge count', true],
+      ['Lives in Focused tab', true],
+    ];
+    const icon = (
+      <SWMansionIcon
+        icon="badge-notifs"
+        size={86}
+        style={notificationIconStyle}
+      />
+    );
+    return (
+      <NotificationsOption
+        active={notificationSettings === 'badge-only'}
+        title="Focused (badge only)"
+        description={description}
+        icon={icon}
+        onSelect={() => setNotificationSettings('badge-only')}
+      ></NotificationsOption>
+    );
+  }, [notificationIconStyle, notificationSettings]);
+
+  const backgroundItem = React.useMemo(() => {
+    const description = [
+      ['Banner notifs', false],
+      ['Badge count', false],
+      ['Lives in Backgound tab', true],
+    ];
+    const icon = (
+      <SWMansionIcon
+        icon="muted-notifs"
+        size={86}
+        style={notificationIconStyle}
+      />
+    );
+    return (
+      <NotificationsOption
+        active={notificationSettings === 'background'}
+        title="Background"
+        description={description}
+        icon={icon}
+        onSelect={() => setNotificationSettings('background')}
+      ></NotificationsOption>
+    );
+  }, [notificationIconStyle, notificationSettings]);
+
+  const onClickSave = React.useCallback(() => {}, []);
   return (
     <Modal name="Channel notifications" size="fit-content" onClose={onClose}>
       <div className={css.container}>
-        <div className={css.optionsContainer}>{notificationOptions}</div>
+        <div className={css.optionsContainer}>
+          {focusedItem}
+          {focusedBadgeOnlyItem}
+          {backgroundItem}
+        </div>
         <Button
           type="primary"
           onClick={onClickSave}
