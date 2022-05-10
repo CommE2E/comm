a9c364854643c70d2c8061e61781c507cb82a892
marcinwasowicz <marcinkwasowicz@gmail.com>
2022-05-10 13:44:58 +0200

    Make App Groups SQLite path a default one. Implement migration from previous path
    Summary: This diff changes implementation of getSQliteFilePath method to return new sqlite path defined by App Groups. Previous version of the method is also kept under different name. It also implements migrationfrom old to new path. Migration uses std::rename instead of NSFileManager API since, std::rename documentation explicitly states that file content shall not be altered in the case of failure. NSFileManager probably works similar but I could not find confirmation in its documentation.
    
    Test Plan: Unistall Comm, build app with new code and install it again. Record new path defined by App Groups. Unistall comm, build with previous version of getSQliteFilePath. Check that the path looks like previous versions. Finally build the new code again, install it, check that database is again under app groups path and the app works correctly which means migration was succesfull.
    
    Reviewers: palys-swm, karol-bisztyga, ashoat
    
    Reviewed By: palys-swm, karol-bisztyga, ashoat
    
    Subscribers: ashoat, Adrian, atul, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D3993

--- native/ios/Comm/Tools.mm
+++ native/ios/Comm/Tools.mm
@@ -5,6 +5,16 @@
 @implementation Tools
 
 + (NSString *)getSQLiteFilePath {
+  NSURL *groupUrl = [NSFileManager.defaultManager
+      containerURLForSecurityApplicationGroupIdentifier:@"group.app.comm"];
+  if (groupUrl == nil) {
+    throw std::runtime_error(
+        "Failed to resolve database path - could not find groupUrl");
+  }
+  return [groupUrl URLByAppendingPathComponent:@"comm.sqlite"].path;
+}
+
++ (NSString *)getAppSpecificSQLiteFilePath {
   NSError *err = nil;
   NSURL *documentsUrl =
       [NSFileManager.defaultManager URLForDirectory:NSDocumentDirectory
