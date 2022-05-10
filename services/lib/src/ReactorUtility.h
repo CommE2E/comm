ca522f38216148f1fa1f35305d1d9f397b37a2b1
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-05-10 15:37:20 +0200

    [services] Lib - Move reactor utility
    Summary:
    Depends on D3910
    
    Moving reactor utility from both blob/backup to the mutual "lib" space.
    
    Test Plan:
    No real changes are applied, just moving stuff around, so services should still build and that's the test plan
    ```
    cd services
    
    yarn run-blob-service
    yarn run-backup-service
    ```
    
    Reviewers: palys-swm, ashoat
    
    Reviewed By: ashoat
    
    Subscribers: ashoat, palys-swm, Adrian, atul, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D3911

---
+++ services/lib/src/ReactorUtility.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <grpcpp/grpcpp.h>
+
+#include <atomic>
+#include <mutex>
+
+namespace comm {
+namespace network {
+namespace reactor {
+
+enum class ReactorState {
+  NONE = 0,
+  RUNNING = 1,
+  TERMINATED = 2,
+  DONE = 3,
+};
+
+class ReactorStatusHolder {
+private:
+  grpc::Status status = grpc::Status::OK;
+  std::mutex statusAccessMutex;
+
+public:
+  std::atomic<ReactorState> state = ReactorState::NONE;
+
+  grpc::Status getStatus() {
+    const std::unique_lock<std::mutex> lock(this->statusAccessMutex);
+    return this->status;
+  }
+  void setStatus(const grpc::Status &status) {
+    const std::unique_lock<std::mutex> lock(this->statusAccessMutex);
+    this->status = status;
+  }
+};
+
+} // namespace reactor
+} // namespace network
+} // namespace comm
