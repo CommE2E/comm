a4db08d2e02653122a9a47fa5de15e4dea99b0cd
marcinwasowicz <marcinkwasowicz@gmail.com>
2022-05-10 11:26:58 +0200

    Implement interface for MessageOperationsUtilities class. Define interface for translation from raw message string into SQLite database entities
    Summary: Defines in C++ functions that are implemented in message-ops-utils.js
    
    Test Plan: No testing plan. Future diffs will introduce code that was used on iOS and Andrdoid to parse raw message strings and store them in database.
    
    Reviewers: palys-swm, karol-bisztyga
    
    Subscribers: ashoat, Adrian, atul, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D3941

---
+++ native/cpp/CommonCpp/NativeModules/MessageOperationsUtilities/MessageOperationsUtilities.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "../../DatabaseManagers/DatabaseManager.h"
+#include "../../DatabaseManagers/entities/Media.h"
+#include "../../DatabaseManagers/entities/Message.h"
+
+#include <folly/dynamic.h>
+#include <string>
+#include <vector>
+
+namespace comm {
+typedef std::pair<Message, std::vector<Media>> ClientDBMessageInfo;
+class MessageOperationsUtilities {
+  static ClientDBMessageInfo translateRawMessageInfoToClientDBMessageInfo(
+      const folly::dynamic &rawMessageInfo);
+  static Media translateMediaToClientDBMediaInfo(
+      const folly::dynamic &rawMediaInfo,
+      const std::string &container,
+      const std::string &thread);
+
+public:
+  static std::vector<ClientDBMessageInfo>
+  translateStringToClientDBMessageInfos(std::string &rawMessageInfosString);
+  static void storeMessageInfos(std::string &rawMessageInfosString);
+};
+} // namespace comm
