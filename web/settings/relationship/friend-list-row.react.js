f8ae5734bbedcb5692bbd24be4bab9c9622a3d93
Tomasz Palys <tomasz.palys@swmansion.com>
2022-05-11 17:35:11 +0200

    [web] Memoize friend list row buttons
    Summary:
    Shouldn't matter that much, but still it is probably a good idea
    
    Depends on D4007
    
    Test Plan: The same as D4007. Also check if cancel request and reject still work.
    
    Reviewers: benschac, atul, def-au1t
    
    Reviewed By: benschac, atul
    
    Subscribers: ashoat, Adrian, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D4008

--- web/settings/relationship/friend-list-row.react.js
+++ web/settings/relationship/friend-list-row.react.js
@@ -16,42 +16,50 @@ function FriendListRow(props: UserRowProps): React.Node {
   const { userInfo } = props;
 
   const { friendUser, unfriendUser } = useRelationshipCallbacks(userInfo.id);
-  let buttons = null;
-  if (userInfo.relationshipStatus === userRelationshipStatus.REQUEST_SENT) {
-    buttons = (
-      <button
-        className={classnames([css.button, css.destructive])}
-        onClick={unfriendUser}
-      >
-        Cancel request
-      </button>
-    );
-  } else if (
-    userInfo.relationshipStatus === userRelationshipStatus.REQUEST_RECEIVED
-  ) {
-    buttons = (
-      <>
-        <button className={css.button} onClick={friendUser}>
-          Accept
-        </button>
+  const buttons = React.useMemo(() => {
+    if (userInfo.relationshipStatus === userRelationshipStatus.REQUEST_SENT) {
+      return (
         <button
           className={classnames([css.button, css.destructive])}
           onClick={unfriendUser}
         >
-          Reject
+          Cancel request
         </button>
-      </>
-    );
-  } else if (userInfo.relationshipStatus === userRelationshipStatus.FRIEND) {
-    const editIcon = <SWMansionIcon icon="edit" size={22} />;
-    buttons = (
-      <div className={css.edit_menu}>
-        <Menu icon={editIcon} variant="member-actions">
-          <MenuItem text="Unfriend" icon="user-cross" onClick={unfriendUser} />
-        </Menu>
-      </div>
-    );
-  }
+      );
+    }
+    if (
+      userInfo.relationshipStatus === userRelationshipStatus.REQUEST_RECEIVED
+    ) {
+      return (
+        <>
+          <button className={css.button} onClick={friendUser}>
+            Accept
+          </button>
+          <button
+            className={classnames([css.button, css.destructive])}
+            onClick={unfriendUser}
+          >
+            Reject
+          </button>
+        </>
+      );
+    }
+    if (userInfo.relationshipStatus === userRelationshipStatus.FRIEND) {
+      const editIcon = <SWMansionIcon icon="edit" size={22} />;
+      return (
+        <div className={css.edit_menu}>
+          <Menu icon={editIcon} variant="member-actions">
+            <MenuItem
+              text="Unfriend"
+              icon="user-cross"
+              onClick={unfriendUser}
+            />
+          </Menu>
+        </div>
+      );
+    }
+    return undefined;
+  }, [friendUser, unfriendUser, userInfo.relationshipStatus]);
 
   return (
     <div className={css.container}>
