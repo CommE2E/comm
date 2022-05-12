aafd1dbd63f08a629296f6b4ed28e4dc1b45aea9
Tomasz Palys <tomasz.palys@swmansion.com>
2022-05-12 20:24:37 +0200

    [web] Introduce block list row
    Summary:
    Create a component that is displayed as a row on block list. The design can be found here https://www.figma.com/file/a1nkbWgbgjRlrOY9LVurTz/Comm-%2F-Desktop-app?node-id=1313%3A98810.
    
    {F55677}
    
    Depends on D4036
    
    Test Plan: Check if all blocked users are displayed. Unblock a user and check if the row is no longer present on the list.
    
    Reviewers: benschac, atul, def-au1t
    
    Reviewed By: atul
    
    Subscribers: ashoat, Adrian, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D4037

---
+++ web/settings/relationship/block-list-row.react.js
@@ -0,0 +1,36 @@
+// @flow
+
+import * as React from 'react';
+
+import { useRelationshipCallbacks } from 'lib/hooks/relationship-prompt';
+
+import MenuItem from '../../components/menu-item.react';
+import Menu from '../../components/menu.react';
+import SWMansionIcon from '../../SWMansionIcon.react';
+import css from './user-list-row.css';
+import type { UserRowProps } from './user-list.react';
+
+function BlockListRow(props: UserRowProps): React.Node {
+  const { userInfo, onMenuVisibilityChange } = props;
+  const { unblockUser } = useRelationshipCallbacks(userInfo.id);
+  const editIcon = <SWMansionIcon icon="edit" size={22} />;
+
+  return (
+    <div className={css.container}>
+      <div className={css.usernameContainer}>{userInfo.username}</div>
+      <div className={css.buttons}>
+        <div className={css.edit_menu}>
+          <Menu
+            onChange={onMenuVisibilityChange}
+            icon={editIcon}
+            variant="member-actions"
+          >
+            <MenuItem text="Unblock" icon="user-plus" onClick={unblockUser} />
+          </Menu>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default BlockListRow;
