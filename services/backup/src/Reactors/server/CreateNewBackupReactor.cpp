3be1d7e77a42b3448775d687053950bdf8d6a616
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-05-12 14:58:58 +0200

    [services] Backup - use generateHolder for new backup

--- services/backup/src/Reactors/server/CreateNewBackupReactor.cpp
+++ services/backup/src/Reactors/server/CreateNewBackupReactor.cpp
@@ -63,7 +63,7 @@ std::unique_ptr<ServerBidiReactorStatus> CreateNewBackupReactor::handleRequest(
             this->userID + "] already exists, creation aborted");
       }
       response->set_backupid(this->backupID);
-      this->holder = this->backupID;
+      this->holder = generateHolder(this->dataHash, this->backupID);
       this->putReactor = std::make_shared<reactor::BlobPutClientReactor>(
           this->holder, this->dataHash, &this->blobPutDoneCV);
       this->blobClient.put(this->putReactor);
