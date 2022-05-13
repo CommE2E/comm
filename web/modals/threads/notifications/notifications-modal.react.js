3503a1e4c9ea411b307d4023accfd1947759833b
Jacek Nitychoruk <jacek.nitychoruk@swmansion.com>
2022-05-13 10:50:31 +0200

    [web] Introduce thread notification settings options in modal
    Summary:
    Introduce notification settings options in modal, that can be selected by the user.
    
    {F51650}
    
    Test Plan: The modal can be tested after introducing notification item in thread actions menu.
    
    Reviewers: palys-swm, atul, benschac
    
    Reviewed By: atul
    
    Subscribers: ashoat, Adrian, atul, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D3953

--- web/modals/threads/notifications/notifications-modal.react.js
+++ web/modals/threads/notifications/notifications-modal.react.js
@@ -6,8 +6,10 @@ import { threadInfoSelector } from 'lib/selectors/thread-selectors';
 
 import Button from '../../../components/button.react';
 import { useSelector } from '../../../redux/redux-utils';
+import SWMansionIcon from '../../../SWMansionIcon.react';
 import Modal from '../../modal.react';
 import css from './notifications-modal.css';
+import NotificationsOption from './notifications-option.react';
 
 type NotificationSettings = 'focused' | 'badge-only' | 'background';
 
@@ -33,17 +35,95 @@ function NotificationsModal(props: Props): React.Node {
 
   const [
     notificationSettings,
-    // eslint-disable-next-line no-unused-vars
     setNotificationSettings,
   ] = React.useState<NotificationSettings>(initialThreadSetting);
 
-  const onClickSave = React.useCallback(() => {}, []);
-  const notificationOptions = [];
+  const notificationIconStyle = React.useMemo(() => ({ width: 'auto' }), []);
 
+  const isFocusedSelected = notificationSettings === 'focused';
+  const focusedItem = React.useMemo(() => {
+    const description = [
+      ['Banner notifs', true],
+      ['Badge count', true],
+      ['Lives in Focused tab', true],
+    ];
+    const icon = (
+      <SWMansionIcon
+        icon="all-notifs"
+        size={86}
+        style={notificationIconStyle}
+      />
+    );
+    return (
+      <NotificationsOption
+        selected={isFocusedSelected}
+        title="Focused (enabled)"
+        description={description}
+        icon={icon}
+        onSelect={() => setNotificationSettings('focused')}
+      />
+    );
+  }, [isFocusedSelected, notificationIconStyle]);
+
+  const isFocusedBadgeOnlySelected = notificationSettings === 'badge-only';
+  const focusedBadgeOnlyItem = React.useMemo(() => {
+    const description = [
+      ['Banner notifs', false],
+      ['Badge count', true],
+      ['Lives in Focused tab', true],
+    ];
+    const icon = (
+      <SWMansionIcon
+        icon="badge-notifs"
+        size={86}
+        style={notificationIconStyle}
+      />
+    );
+    return (
+      <NotificationsOption
+        selected={isFocusedBadgeOnlySelected}
+        title="Focused (badge only)"
+        description={description}
+        icon={icon}
+        onSelect={() => setNotificationSettings('badge-only')}
+      />
+    );
+  }, [isFocusedBadgeOnlySelected, notificationIconStyle]);
+
+  const isBackgroundSelected = notificationSettings === 'background';
+  const backgroundItem = React.useMemo(() => {
+    const description = [
+      ['Banner notifs', false],
+      ['Badge count', false],
+      ['Lives in Backgound tab', true],
+    ];
+    const icon = (
+      <SWMansionIcon
+        icon="muted-notifs"
+        size={86}
+        style={notificationIconStyle}
+      />
+    );
+    return (
+      <NotificationsOption
+        selected={isBackgroundSelected}
+        title="Background"
+        description={description}
+        icon={icon}
+        onSelect={() => setNotificationSettings('background')}
+      />
+    );
+  }, [isBackgroundSelected, notificationIconStyle]);
+
+  const onClickSave = React.useCallback(() => {}, []);
   return (
     <Modal name="Channel notifications" size="fit-content" onClose={onClose}>
       <div className={css.container}>
-        <div className={css.optionsContainer}>{notificationOptions}</div>
+        <div className={css.optionsContainer}>
+          {focusedItem}
+          {focusedBadgeOnlyItem}
+          {backgroundItem}
+        </div>
         <Button
           type="primary"
           onClick={onClickSave}
