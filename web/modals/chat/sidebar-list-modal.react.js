6aaf1c5da508c6b5dee86914fee5077b625bacc2
Benjamin Schachter <benschac@gmail.com>
2022-05-16 17:17:06 -0400

    [lib, web] [feat] [ENG-536] hook up sidebar data to search modal
    Summary: Add search sidebar functionality to search modal <addgif>
    
    Test Plan: Go to sidebar in web/ and native. Test functionality. Both should work as they did before. Web UI should be updated
    
    Reviewers: @atul, @palys-swm, @def-au1t
    
    Subscribers:

--- web/modals/chat/sidebar-list-modal.react.js
+++ web/modals/chat/sidebar-list-modal.react.js
@@ -1,7 +1,5 @@
 // @flow
 
-import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames';
 import * as React from 'react';
 
@@ -10,78 +8,62 @@ import type { ThreadInfo } from 'lib/types/thread-types';
 
 import chatThreadListCSS from '../../chat/chat-thread-list.css';
 import SidebarItem from '../../chat/sidebar-item.react';
-import globalCSS from '../../style.css';
-import { MagnifyingGlass } from '../../vectors.react';
-import Input from '../input.react';
 import { useModalContext } from '../modal-provider.react';
-import Modal from '../modal.react';
+import SearchModal from '../search-modal.react';
 
 type Props = {
   +threadInfo: ThreadInfo,
 };
 
-function SidebarListModal(props: Props): React.Node {
-  const { threadInfo } = props;
-  const [searchText, setSearchText] = React.useState('');
-  const { listData, onChangeSearchText, clearQuery } = useSearchSidebars(
-    threadInfo,
-    searchText,
-    setSearchText,
-  );
+type SidebarModalContentProps = {
+  +threadInfo: ThreadInfo,
+  +onClose: () => void,
+  +searchText: string,
+};
+
+function SidebarModalContent(props: SidebarModalContentProps): React.Node {
+  const { threadInfo, searchText } = props;
   const { popModal } = useModalContext();
+  const { listData } = useSearchSidebars(threadInfo, searchText);
 
-  const sidebars = React.useMemo(
-    () =>
-      listData.map(item => (
-        <div
-          className={classNames(
-            chatThreadListCSS.thread,
-            chatThreadListCSS.sidebar,
-          )}
-          key={item.threadInfo.id}
-          onClick={popModal}
-        >
-          <SidebarItem sidebarInfo={item} />
-        </div>
-      )),
-    [popModal, listData],
+  return listData.map(item => (
+    <div
+      className={classNames(
+        chatThreadListCSS.thread,
+        chatThreadListCSS.sidebar,
+      )}
+      key={item.threadInfo.id}
+      onClick={popModal}
+    >
+      <SidebarItem sidebarInfo={item} />
+    </div>
+  ));
+}
+
+function SidebarListModal(props: Props): React.Node {
+  const { popModal } = useModalContext();
+  const { threadInfo } = props;
+
+  const searchSidebarModalContent = React.useCallback(
+    (searchSidebarText: string) => (
+      <SidebarModalContent
+        onClose={popModal}
+        threadInfo={threadInfo}
+        searchText={searchSidebarText}
+      />
+    ),
+    [popModal, threadInfo],
   );
-
-  let clearQueryButton = null;
-  if (searchText) {
-    clearQueryButton = (
-      <a href="#" onClick={clearQuery}>
-        <FontAwesomeIcon
-          icon={faTimesCircle}
-          className={chatThreadListCSS.clearQuery}
-        />
-      </a>
-    );
-  }
 
   return (
-    <Modal name="Sidebars" onClose={popModal}>
-      <div
-        className={classNames(
-          globalCSS['modal-body'],
-          globalCSS['resized-modal-body'],
-        )}
-      >
-        <div>
-          <div className={chatThreadListCSS.search}>
-            <MagnifyingGlass className={chatThreadListCSS.searchVector} />
-            <Input
-              type="text"
-              placeholder="Search sidebars"
-              value={searchText}
-              onChange={onChangeSearchText}
-            />
-            {clearQueryButton}
-          </div>
-        </div>
-        <ul className={chatThreadListCSS.list}>{sidebars}</ul>
-      </div>
-    </Modal>
+    <SearchModal
+      name="Sidebars"
+      onClose={popModal}
+      size="fit-content"
+      searchPlaceholder="Search sidebars"
+    >
+      {searchSidebarModalContent}
+    </SearchModal>
   );
 }
 
