e6da88fd75130aa48f9319cce5dfa15ae18523d1
Tomasz Palys <tomasz.palys@swmansion.com>
2022-05-20 18:32:59 +0200

    [web] Implement danger zone UI
    Summary:
    Add all the components to the page. The design https://www.figma.com/file/a1nkbWgbgjRlrOY9LVurTz/Comm-%2F-Desktop-app?node-id=1272%3A87631 doesn't specify the copy below the button, but I think it might be a good idea to have a warning there - the same we currently have in delete tab of account settings.
    
    {F59981}
    
    Depends on D4094
    
    Test Plan: Open http://localhost/comm/settings/danger-zone/ and check if it is displayed correctly.
    
    Reviewers: def-au1t, atul, benschac, ashoat
    
    Reviewed By: atul, ashoat
    
    Subscribers: ashoat, Adrian, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D4096

--- web/settings/danger-zone.react.js
+++ web/settings/danger-zone.react.js
@@ -2,12 +2,21 @@
 
 import * as React from 'react';
 
+import Button from '../components/button.react.js';
 import css from './danger-zone.css';
 
 function DangerZone(): React.Node {
   return (
     <div className={css.container}>
       <h4 className={css.header}>Danger Zone</h4>
+      <h5 className={css.subheading}>Delete Account</h5>
+      <Button onClick={() => {}} variant="danger" className={css.button}>
+        Delete Account
+      </Button>
+      <p className={css.explanation}>
+        Your account will be permanently deleted. There is no way to reverse
+        this.
+      </p>
     </div>
   );
 }
