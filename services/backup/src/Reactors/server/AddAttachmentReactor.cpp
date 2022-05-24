34393b79b76d6f168ee2ed182fe047391feac8d1
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-05-24 11:15:09 +0200

    [services] Backup - Add Attachment Reactor - read request
    Summary: todo
    
    Test Plan: todo
    
    Subscribers: ashoat, palys-swm, Adrian, atul, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D4112

--- services/backup/src/Reactors/server/AddAttachmentReactor.cpp
+++ services/backup/src/Reactors/server/AddAttachmentReactor.cpp
@@ -16,12 +16,32 @@ AddAttachmentReactor::readRequest(backup::AddAttachmentRequest request) {
   // we make sure that the blob client's state is flushed to the main memory
   // as there may be multiple threads from the pool taking over here
   const std::lock_guard<std::mutex> lock(this->reactorStateMutex);
+
+  if (!this->parametersPassed) {
+    if (!request.has_attachmentparameters()) {
+      throw std::runtime_error(
+          "attachment parameters expected but not received");
+    }
+    this->userID = request.attachmentparameters().userid();
+    this->backupID = request.attachmentparameters().backupid();
+    if (!request.attachmentparameters().logid().empty()) {
+      this->logID = request.attachmentparameters().logid();
+      this->parentType = ParentType::LOG;
+    } else {
+      this->parentType = ParentType::BACKUP;
+    }
+    this->parametersPassed = true;
+    return nullptr;
+  }
+  if (!request.has_holder()) {
+    return std::make_unique<grpc::Status>(grpc::Status::OK);
+  }
+  this->holders.push_back(request.holder());
   return nullptr;
 }
 
 void AddAttachmentReactor::terminateCallback() {
 }
-
 } // namespace reactor
 } // namespace network
 } // namespace comm
