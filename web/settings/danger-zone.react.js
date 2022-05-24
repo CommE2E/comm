f8fd1e734f5b7cbf3b78ad138055e1e4c9c6769f
Tomasz Palys <tomasz.palys@swmansion.com>
2022-05-24 15:55:50 +0200

    [web] Open delete account modal after `Delete Account` button is clicked.
    Summary:
    Show a modal when user clicks the button.
    
    Depends on D4119
    
    Test Plan: Click the button and check if the modal was displayed. Close it by clicking outside and by clicking `x` button.
    
    Reviewers: def-au1t, atul, benschac
    
    Reviewed By: atul
    
    Subscribers: ashoat, Adrian, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D4120

--- web/settings/danger-zone.react.js
+++ web/settings/danger-zone.react.js
@@ -3,14 +3,26 @@
 import * as React from 'react';
 
 import Button from '../components/button.react.js';
+import { useModalContext } from '../modals/modal-provider.react.js';
+import AccountDeleteModal from './account-delete-modal.react';
 import css from './danger-zone.css';
 
 function DangerZone(): React.Node {
+  const { pushModal } = useModalContext();
+  const onDeleteAccountClick = React.useCallback(
+    () => pushModal(<AccountDeleteModal />),
+    [pushModal],
+  );
+
   return (
     <div className={css.container}>
       <h4 className={css.header}>Danger Zone</h4>
       <h5 className={css.subheading}>Delete Account</h5>
-      <Button onClick={() => {}} variant="danger" className={css.button}>
+      <Button
+        onClick={onDeleteAccountClick}
+        variant="danger"
+        className={css.button}
+      >
         Delete Account
       </Button>
       <p className={css.explanation}>
