9afdcc4f1ab570c5b6f9d29e9ebe92b46e47af6b
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-05-24 11:15:38 +0200

    [services] Backup - Add Attachment Reactor - terminate callback

--- services/backup/src/Reactors/server/AddAttachmentReactor.cpp
+++ services/backup/src/Reactors/server/AddAttachmentReactor.cpp
@@ -41,7 +41,32 @@ AddAttachmentReactor::readRequest(backup::AddAttachmentRequest request) {
 }
 
 void AddAttachmentReactor::terminateCallback() {
+  const std::lock_guard<std::mutex> lock(this->reactorStateMutex);
+
+  if (this->parentType == ParentType::BACKUP) {
+    std::shared_ptr<database::BackupItem> backupItem =
+        database::DatabaseManager::getInstance().findBackupItem(
+            this->userID, this->backupID);
+
+    for (const std::string &holder : this->holders) {
+      backupItem->appendAttachmentHolder(holder);
+    }
+    database::DatabaseManager::getInstance().putBackupItem(*backupItem);
+    return;
+  }
+  if (this->parentType == ParentType::LOG) {
+    std::shared_ptr<database::LogItem> logItem =
+        database::DatabaseManager::getInstance().findLogItem(
+            this->backupID, this->logID);
+    for (const std::string &holder : this->holders) {
+      logItem->appendAttachmentHolder(holder);
+    }
+    database::DatabaseManager::getInstance().putLogItem(*logItem);
+    return;
+  }
+  throw std::runtime_error("unhandled parent type");
 }
+
 } // namespace reactor
 } // namespace network
 } // namespace comm
