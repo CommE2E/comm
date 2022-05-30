34ecda1b7f42e3a5fd5eb97bb64f39aaed4f1462
Ashoat Tevosyan <ashoat@comm.app>
2022-05-30 16:34:15 -0400

    [keyserver] Extract migration config to separate file
    Summary:
    In the following diff, I'm going to check the latest migration index from `setup-db.js` so we can start with a default migration version.
    
    Without this diff, that change would result in a require cycle.
    
    Depends on D4151
    
    Test Plan: This is a simple refactor. I tested it in combination with stack (see test plan in D4151), and also used Flow
    
    Reviewers: palys-swm, atul
    
    Reviewed By: palys-swm, atul
    
    Subscribers: Adrian, atul, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D4152

--- keyserver/src/database/migrations.js
+++ keyserver/src/database/migrations.js
@@ -1,92 +1,12 @@
 // @flow
 
-import fs from 'fs';
 import type { QueryResults } from 'mysql';
 
 import { getMessageForException } from 'lib/utils/errors';
 
 import { dbQuery, SQL } from './database';
+import { migrations } from './migration-config';
 import { setupDB } from './setup-db';
-
-async function makeSureBaseRoutePathExists(filePath: string): Promise<void> {
-  let readFile, json;
-  try {
-    readFile = await fs.promises.open(filePath, 'r');
-    const contents = await readFile.readFile('utf8');
-    json = JSON.parse(contents);
-  } catch {
-    return;
-  } finally {
-    if (readFile) {
-      await readFile.close();
-    }
-  }
-  if (json.baseRoutePath) {
-    return;
-  }
-  let baseRoutePath;
-  if (json.baseDomain === 'http://localhost') {
-    baseRoutePath = json.basePath;
-  } else if (filePath.endsWith('commapp_url.json')) {
-    baseRoutePath = '/commweb/';
-  } else {
-    baseRoutePath = '/';
-  }
-  const newJSON = { ...json, baseRoutePath };
-  console.warn(`updating ${filePath} to ${JSON.stringify(newJSON)}`);
-  const writeFile = await fs.promises.open(filePath, 'w');
-  await writeFile.writeFile(JSON.stringify(newJSON, null, '  '), 'utf8');
-  await writeFile.close();
-}
-
-async function fixBaseRoutePathForLocalhost(filePath: string): Promise<void> {
-  let readFile, json;
-  try {
-    readFile = await fs.promises.open(filePath, 'r');
-    const contents = await readFile.readFile('utf8');
-    json = JSON.parse(contents);
-  } catch {
-    return;
-  } finally {
-    if (readFile) {
-      await readFile.close();
-    }
-  }
-  if (json.baseDomain !== 'http://localhost') {
-    return;
-  }
-  const baseRoutePath = '/';
-  json = { ...json, baseRoutePath };
-  console.warn(`updating ${filePath} to ${JSON.stringify(json)}`);
-  const writeFile = await fs.promises.open(filePath, 'w');
-  await writeFile.writeFile(JSON.stringify(json, null, '  '), 'utf8');
-  await writeFile.close();
-}
-
-const migrations: $ReadOnlyMap<number, () => Promise<void>> = new Map([
-  [
-    0,
-    async () => {
-      await makeSureBaseRoutePathExists('facts/commapp_url.json');
-      await makeSureBaseRoutePathExists('facts/squadcal_url.json');
-    },
-  ],
-  [
-    1,
-    async () => {
-      try {
-        await fs.promises.unlink('facts/url.json');
-      } catch {}
-    },
-  ],
-  [
-    2,
-    async () => {
-      await fixBaseRoutePathForLocalhost('facts/commapp_url.json');
-      await fixBaseRoutePathForLocalhost('facts/squadcal_url.json');
-    },
-  ],
-]);
 
 async function migrate(): Promise<boolean> {
   let dbVersion = null;
