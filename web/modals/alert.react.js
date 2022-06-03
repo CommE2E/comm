b91900098640d694f08be32f7777161cb156cdfc
Atul Madhugiri <atulm@berkeley.edu>
2022-06-03 02:06:04 -0400

    [web] Introduce minimally viable `Alert` component
    Summary:
    At this point pretty much copied from `InvalidUploadModal` with `title` and `children` passed in as props. In the next diff will "consume" the `Alert` component in `InvalidUploadModal`
    
    ---
    
    Depends on D4206
    
    Test Plan: Will be tested implicitly by the next diff in the stack when we "consume" the newly introduced `Alert` component from `InvalidUploadModal`
    
    Reviewers: def-au1t, palys-swm
    
    Reviewed By: palys-swm
    
    Subscribers: ashoat, Adrian, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D4207

---
+++ web/modals/alert.react.js
@@ -0,0 +1,35 @@
+// @flow
+
+import * as React from 'react';
+
+import Button from '../components/button.react';
+import css from './alert.css';
+import { useModalContext } from './modal-provider.react';
+import Modal from './modal.react';
+
+type AlertProps = {
+  +title: string,
+  +children: string,
+};
+function Alert(props: AlertProps): React.Node {
+  const { title, children } = props;
+  const { popModal } = useModalContext();
+
+  return (
+    <Modal name={title} onClose={popModal}>
+      <div className={css.modal_body}>
+        <p>{children}</p>
+        <Button
+          onClick={popModal}
+          type="submit"
+          variant="primary"
+          className={css.ok_button}
+        >
+          OK
+        </Button>
+      </div>
+    </Modal>
+  );
+}
+
+export default Alert;
