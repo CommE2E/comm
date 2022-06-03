a2f2b4f1156282b8fb53cf8b7a09c23398f10610
Atul Madhugiri <atulm@berkeley.edu>
2022-06-03 02:11:21 -0400

    [web] Use `Alert` component in `InvalidUploadModal`
    Summary:
    "Consume" the `Alert` component that was introduced in the previous diff.
    
    ---
    
    Depends on D4207
    
    Test Plan: Dragged a random non-supported file into the chat area and observed that the `InvalidUploadModal` looked as expected:
    
    Reviewers: def-au1t, palys-swm
    
    Reviewed By: palys-swm
    
    Subscribers: ashoat, Adrian, yayabosh
    
    Differential Revision: https://phabricator.ashoat.com/D4208

--- web/modals/chat/invalid-upload.react.js
+++ web/modals/chat/invalid-upload.react.js
@@ -2,27 +2,13 @@
 
 import * as React from 'react';
 
-import Button from '../../components/button.react.js';
-import { useModalContext } from '../../modals/modal-provider.react';
-import Modal from '../modal.react';
-import css from './invalid-upload.css';
+import Alert from '../alert.react.js';
 
 function InvalidUploadModal(): React.Node {
-  const { popModal } = useModalContext();
   return (
-    <Modal name="Invalid upload" onClose={popModal}>
-      <div className={css.modal_body}>
-        <p>We don&apos;t support that file type yet :(</p>
-        <Button
-          onClick={popModal}
-          type="submit"
-          variant="primary"
-          className={css.ok_button}
-        >
-          OK
-        </Button>
-      </div>
-    </Modal>
+    <Alert title="Invalid upload">
+      We don&apos;t support that file type yet :(
+    </Alert>
   );
 }
 
