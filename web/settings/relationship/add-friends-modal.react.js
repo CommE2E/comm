4e6518437d65144c912674364765961f6094efcb
Tomasz Palys <tomasz.palys@swmansion.com>
2022-06-13 15:11:03 +0200

    [web] Introduce add friends modal
    Summary:
    This modal allows adding new friends. Only users that do not have a relationship with viewer or are blocked are displayed as potential friends.
    
    {F80244}
    
    Depends on D4311
    
    Test Plan: Render the modal and check if sending new friend requests work correctly.
    
    Reviewers: def-au1t, atul
    
    Reviewed By: atul
    
    Subscribers: ashoat, Adrian, yayabosh
    
    Differential Revision: https://phab.comm.dev/D4312

---
+++ web/settings/relationship/add-friends-modal.react.js
@@ -0,0 +1,38 @@
+// @flow
+
+import * as React from 'react';
+
+import {
+  relationshipActions,
+  userRelationshipStatus,
+} from 'lib/types/relationship-types.js';
+
+import AddUsersListModal from './add-users-list-modal.react';
+
+const excludedStatuses = new Set([
+  userRelationshipStatus.FRIEND,
+  userRelationshipStatus.BLOCKED_VIEWER,
+  userRelationshipStatus.BOTH_BLOCKED,
+  userRelationshipStatus.REQUEST_SENT,
+  userRelationshipStatus.REQUEST_RECEIVED,
+]);
+
+type Props = {
+  +onClose: () => void,
+};
+
+function AddFriendsModal(props: Props): React.Node {
+  const { onClose } = props;
+
+  return (
+    <AddUsersListModal
+      closeModal={onClose}
+      name="Add Friends"
+      excludedStatuses={excludedStatuses}
+      confirmButtonContent="Send Friend Requests"
+      relationshipAction={relationshipActions.FRIEND}
+    />
+  );
+}
+
+export default AddFriendsModal;
