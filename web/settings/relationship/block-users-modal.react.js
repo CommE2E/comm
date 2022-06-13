cbbc1a7d19815144fa21d93fd1b82db23559b1c5
Tomasz Palys <tomasz.palys@swmansion.com>
2022-06-13 19:16:51 +0200

    [web] Introduce block users modal
    Summary:
    This modal allows blocking users. Only users that are not blocked by the viewer can be blocked.
    
    {F82770}
    
    Depends on D4312
    
    Test Plan: Render the modal and check if blocking users works correctly.
    
    Reviewers: def-au1t, atul
    
    Reviewed By: atul
    
    Subscribers: ashoat, Adrian, yayabosh
    
    Differential Revision: https://phab.comm.dev/D4313

---
+++ web/settings/relationship/block-users-modal.react.js
@@ -0,0 +1,45 @@
+// @flow
+
+import { faUserShield } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import * as React from 'react';
+
+import {
+  relationshipActions,
+  userRelationshipStatus,
+} from 'lib/types/relationship-types.js';
+
+import AddUsersListModal from './add-users-list-modal.react.js';
+
+const excludedStatuses = new Set([
+  userRelationshipStatus.BOTH_BLOCKED,
+  userRelationshipStatus.BLOCKED_BY_VIEWER,
+]);
+
+type Props = {
+  +onClose: () => void,
+};
+
+function BlockUsersModal(props: Props): React.Node {
+  const { onClose } = props;
+
+  const buttonContent = (
+    <div>
+      <FontAwesomeIcon icon={faUserShield} />
+      {' Block Users'}
+    </div>
+  );
+
+  return (
+    <AddUsersListModal
+      closeModal={onClose}
+      name="Block Users"
+      excludedStatuses={excludedStatuses}
+      confirmButtonContent={buttonContent}
+      confirmButtonVariant="danger"
+      relationshipAction={relationshipActions.BLOCK}
+    />
+  );
+}
+
+export default BlockUsersModal;
