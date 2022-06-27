5c6ae6d154c327cfa93f9cd601ca7912f505650d
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-06-27 12:09:42 +0200

    [services] Tests - Add tools
    Summary:
    Depends on D4164
    
    Adding tools with one function: `generate_nbytes`. It returns `n` bytes (that are the same).
    
    Test Plan: this doesn't do anything on its own. Tests can be built.
    
    Reviewers: palys-swm, geekbrother, varun
    
    Reviewed By: palys-swm, varun
    
    Subscribers: ashoat, palys-swm, Adrian, atul, yayabosh
    
    Differential Revision: https://phab.comm.dev/D4225

---
+++ services/commtest/tests/lib/tools.rs
@@ -0,0 +1,19 @@
+use bytesize::ByteSize;
+
+#[allow(dead_code)]
+pub fn generate_nbytes(number_of_bytes: usize, predefined_byte_value: Option<u8>) -> Vec<u8> {
+  let byte_value = predefined_byte_value.unwrap_or(b'A');
+  return vec![byte_value; number_of_bytes];
+}
+
+#[derive(
+  Debug, derive_more::Display, derive_more::From, derive_more::Error,
+)]
+pub enum Error {
+  #[display(...)]
+  Proto(std::io::Error),
+  #[display(...)]
+  Tonic(tonic::transport::Error),
+  #[display(...)]
+  TonicStatus(tonic::Status),
+}
