c8c20687c78e99f4f46b3d9657f2024390bbf446
Abosh Upadhyaya <aboshu@gmail.com>
2022-06-29 16:36:00 -0400

    [services] Use `[[ ]]` instead of `[ ]` in `run_server_image.sh`
    Had a git issue, this addresses https://phab.comm.dev/D4398?vs=on&id=13979#124507.

--- services/scripts/run_server_image.sh
+++ services/scripts/run_server_image.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [ "$#" -lt 1 ] || [ "$#" -gt 2 ]; then
+if [[ "$#" -lt 1 ]] || [[ "$#" -gt 2 ]]; then
   echo "Illegal number of arguments, expected 2:"
   echo "- one argument with a name of the service, currently available services:"
   ./scripts/list_services.sh
@@ -12,16 +12,16 @@ if [ "$#" -lt 1 ] || [ "$#" -gt 2 ]; then
 fi
 
 SERVICE=$1
-if [ "$SERVICE" == "tunnelbroker" ]; then
-  if [ -n "$2" ]; then
+if [[ "$SERVICE" == "tunnelbroker" ]]; then
+  if [[ -n "$2" ]]; then
     export COMM_SERVICES_PORT_TUNNELBROKER=$2
   fi
-elif [ "$SERVICE" == "backup" ]; then
-  if [ -n "$2" ]; then
+elif [[ "$SERVICE" == "backup" ]]; then
+  if [[ -n "$2" ]]; then
     export COMM_SERVICES_PORT_BACKUP=$2
   fi
-elif [ "$SERVICE" == "blob" ]; then
-  if [ -n "$2" ]; then
+elif [[ "$SERVICE" == "blob" ]]; then
+  if [[ -n "$2" ]]; then
     export COMM_SERVICES_PORT_BLOB=$2
   fi
 else
