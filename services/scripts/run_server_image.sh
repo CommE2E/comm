69b13a14348dbdf7d68129a168ad366fb204bf2e
Abosh Upadhyaya <aboshu@gmail.com>
2022-06-29 11:08:00 -0400

    [services] Clean up `run_server_image.sh`
    Summary: Cleaned up `run_server_image.sh` with style fixes and some double quoting to safely prevent globbing and word splitting. More details in inline comments.
    
    Test Plan: [[ https://www.shellcheck.net/ | ShellCheck ]], close reading.
    
    Reviewers: ashoat, karol-bisztyga, atul
    
    Reviewed By: ashoat
    
    Subscribers: palys-swm, Adrian
    
    Differential Revision: https://phab.comm.dev/D4398

--- services/scripts/run_server_image.sh
+++ services/scripts/run_server_image.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [ "$#" -lt 1 ] || [ "$#" -gt 2 ]; then
-  echo "Illegal number of parameters, expected:"
+  echo "Illegal number of arguments, expected 2:"
   echo "- one argument with a name of the service, currently available services:"
   ./scripts/list_services.sh
   echo "- one optional argument with port"
@@ -13,15 +13,15 @@ fi
 
 SERVICE=$1
 if [ "$SERVICE" == "tunnelbroker" ]; then
-  if [ ! -z "$2" ]; then
+  if [ -n "$2" ]; then
     export COMM_SERVICES_PORT_TUNNELBROKER=$2
   fi
 elif [ "$SERVICE" == "backup" ]; then
-  if [ ! -z "$2" ]; then
+  if [ -n "$2" ]; then
     export COMM_SERVICES_PORT_BACKUP=$2
   fi
 elif [ "$SERVICE" == "blob" ]; then
-  if [ ! -z "$2" ]; then
+  if [ -n "$2" ]; then
     export COMM_SERVICES_PORT_BLOB=$2
   fi
 else
@@ -29,5 +29,5 @@ else
   exit 1
 fi
 
-docker-compose build $SERVICE-server
-docker-compose up $SERVICE-server
+docker-compose build "$SERVICE"-server
+docker-compose up "$SERVICE"-server
