23ee43573ca07792eafb30eb3bf36738e4e700d7
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-06-30 15:34:23 +0200

    [services] Tests - Blob - Add Get
    Summary:
    Depends on D4274
    
    Adding `Get` method to blob's tests.
    
    Test Plan: For now, this method's not invoked. The Rust project should build normally.
    
    Reviewers: palys-swm, varun
    
    Reviewed By: palys-swm, varun
    
    Subscribers: ashoat, palys-swm, Adrian, atul, yayabosh
    
    Differential Revision: https://phab.comm.dev/D4275

---
+++ services/commtest/tests/blob/get.rs
@@ -0,0 +1,29 @@
+#[path = "./blob_utils.rs"]
+mod blob_utils;
+#[path = "../lib/tools.rs"]
+mod tools;
+
+use tonic::Request;
+
+use crate::blob_utils::{proto::GetRequest, BlobData, BlobServiceClient};
+use crate::tools::Error;
+
+pub async fn run(
+  client: &mut BlobServiceClient<tonic::transport::Channel>,
+  blob_data: &BlobData,
+) -> Result<Vec<usize>, Error> {
+  let cloned_holder = blob_data.holder.clone();
+  println!("get {}", cloned_holder);
+
+  let response = client
+    .get(Request::new(GetRequest {
+      holder: cloned_holder,
+    }))
+    .await?;
+  let mut inbound = response.into_inner();
+  let mut sizes: Vec<usize> = Vec::new();
+  while let Some(response) = inbound.message().await? {
+    sizes.push(response.data_chunk.len());
+  }
+  Ok(sizes)
+}
