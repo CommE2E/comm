1397f1ae0adc58b4e29f342a41f9ce93a9494e64
Abosh Upadhyaya <aboshu@gmail.com>
2022-06-30 15:49:18 -0400

    [services] Clean up `build_base_image.sh`
    Summary: Relevant Linear issue [[ https://linear.app/comm/issue/ENG-157/make-brackets-consistent-in-bash-scripts-in-services | here ]]. Replaced `[ ]` with `[[ ]]` and added some double quotes to prevent globbing and word splitting.
    
    Test Plan: [[ https://www.shellcheck.net/ | ShellCheck ]], close reading.
    
    Reviewers: ashoat, atul
    
    Reviewed By: ashoat
    
    Subscribers: palys-swm, Adrian, karol-bisztyga
    
    Differential Revision: https://phab.comm.dev/D4420

--- services/scripts/build_base_image.sh
+++ services/scripts/build_base_image.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-if [ "$#" -gt 1 ]; then
+if [[ "$#" -gt 1 ]]; then
   echo "usage: $0 [TAG]"
   exit 1
 fi
 
 tag=${1:-"1.1"}
-docker build -t commapp/services-base:${tag} base-image
+docker build -t commapp/services-base:"${tag}" base-image
