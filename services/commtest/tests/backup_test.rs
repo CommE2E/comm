d0b8ddc64eb413eb881c0f007ffefce1adf82410
karol-bisztyga <karol.bisztyga@swmansion.com>
2022-06-30 15:34:02 +0200

    [services] Tests - Add dummy tests
    Summary:
    Depends on D4249
    
    Added the rust files for tests where they're supposed to be placed. For now, hardcoded throwing `unimplemented` but it's supposed to be replaced with tests' logic.
    
    Test Plan: none for now
    
    Reviewers: palys-swm, varun
    
    Reviewed By: palys-swm, varun
    
    Subscribers: ashoat, palys-swm, Adrian, atul, yayabosh
    
    Differential Revision: https://phab.comm.dev/D4250

---
+++ services/commtest/tests/backup_test.rs
@@ -0,0 +1,7 @@
+#[path = "./lib/tools.rs"]
+mod tools;
+
+#[tokio::test]
+async fn backup_test() {
+  assert!(false, "not implemented");
+}
---
+++ services/commtest/tests/tunnelbroker_test.rs
@@ -0,0 +1,7 @@
+#[path = "./lib/tools.rs"]
+mod tools;
+
+#[tokio::test]
+async fn tunnelbroker_test() {
+  assert!(false, "not implemented");
+}
