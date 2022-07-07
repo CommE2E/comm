2c7dbf3314ece2e149789e970b6dd2b2dbe6b41a
Abosh Upadhyaya <aboshu@gmail.com>
2022-07-07 13:35:02 -0400

    [web] Reintroduce `MessageTimestampTooltip` in `ChatMessageList`
    Summary:
    This diff is the first part in a stack of commits that will work to fix the timestamp bug outlined in this [[ https://linear.app/comm/issue/ENG-1266/timestamp-appears-below-chatinputbar-on-web | Linear issue ]]. Also see @ashoat's [[ https://phab.comm.dev/D3567#125491 | comment ]] on D3567 for more context.
    
    This diff removes the timestamp from `MessageTooltip` and instead reintroduces `MessageTimestampTooltip` in `ChatMessageList`, thus reverting the visual aspect of this change to what the timestamp tooltip used to look like before `MessageActionButtons` was introduced.
    
    The reason this was done is:
    1. Start the new stack of diffs to fix the timestamp bug
    2. By moving the timestamp out of `MessageTooltip`, the component is no longer inside the `messageContainer` in `ChatMessageList` which has a `overflow-y: auto` property. This `overflow-y` property was causing the timestamp to be cut off within the `ChatMessageList` container since it did not allow any elements to extend beyond its perimeter.
    3. This reverts the code to a familiar place that we have seen before. This diff should be fairly easy to review -- although there are added files, these are mostly just copied over from the old version of the tooltip/timestamp and the front-end changes are identical.
    
    Test Plan:
    Tested on Chrome/Safari and looks as expected.
    {F95071}
    
    Reviewers: tomek, jacek, atul
    
    Reviewed By: tomek, atul
    
    Subscribers: atul, adrian, ashoat
    
    Differential Revision: https://phab.comm.dev/D4481

---
+++ web/chat/message-timestamp-tooltip.react.js
@@ -0,0 +1,148 @@
+// @flow
+
+import invariant from 'invariant';
+import * as React from 'react';
+
+import { isComposableMessageType } from 'lib/types/message-types';
+import { longAbsoluteDate } from 'lib/utils/date-utils';
+
+import css from './message-timestamp-tooltip.css';
+import type { OnMessagePositionWithContainerInfo } from './position-types';
+import {
+  type TooltipPosition,
+  tooltipPositions,
+  sizeOfTooltipArrow,
+} from './tooltip-utils';
+import {
+  TooltipMenu,
+  type TooltipStyle,
+  TooltipTextItem,
+} from './tooltip.react';
+
+const availablePositionsForComposedViewerMessage = [
+  tooltipPositions.BOTTOM_RIGHT,
+];
+const availablePositionsForNonComposedOrNonViewerMessage = [
+  tooltipPositions.LEFT,
+];
+
+type Props = {
+  +messagePositionInfo: OnMessagePositionWithContainerInfo,
+  +timeZone: ?string,
+};
+function MessageTimestampTooltip(props: Props): React.Node {
+  const { messagePositionInfo, timeZone } = props;
+  const { time, creator, type } = messagePositionInfo.item.messageInfo;
+  console.log(messagePositionInfo);
+
+  const text = React.useMemo(() => longAbsoluteDate(time, timeZone), [
+    time,
+    timeZone,
+  ]);
+  const availableTooltipPositions = React.useMemo(() => {
+    const { isViewer } = creator;
+    const isComposed = isComposableMessageType(type);
+    return isComposed && isViewer
+      ? availablePositionsForComposedViewerMessage
+      : availablePositionsForNonComposedOrNonViewerMessage;
+  }, [creator, type]);
+
+  const { messagePosition, containerPosition } = messagePositionInfo;
+  const pointingToInfo = React.useMemo(() => {
+    return {
+      containerPosition,
+      itemPosition: messagePosition,
+    };
+  }, [messagePosition, containerPosition]);
+
+  const getTooltipStyle = React.useCallback(
+    (tooltipPosition: TooltipPosition) =>
+      getTimestampTooltipStyle(messagePositionInfo, tooltipPosition),
+    [messagePositionInfo],
+  );
+  return (
+    <TooltipMenu
+      availableTooltipPositions={availableTooltipPositions}
+      targetPositionInfo={pointingToInfo}
+      layoutPosition="absolute"
+      getStyle={getTooltipStyle}
+    >
+      <TooltipTextItem text={text} />
+    </TooltipMenu>
+  );
+}
+
+function getTimestampTooltipStyle(
+  messagePositionInfo: OnMessagePositionWithContainerInfo,
+  tooltipPosition: TooltipPosition,
+): TooltipStyle {
+  const { messagePosition, containerPosition } = messagePositionInfo;
+  const { height: containerHeight, width: containerWidth } = containerPosition;
+
+  let style, className;
+  if (tooltipPosition === tooltipPositions.LEFT) {
+    const centerOfMessage = messagePosition.top + messagePosition.height / 2;
+    const tooltipPointing = Math.max(
+      Math.min(centerOfMessage, containerHeight),
+      0,
+    );
+    style = {
+      right: containerWidth - messagePosition.left + sizeOfTooltipArrow,
+      top: tooltipPointing,
+    };
+    className = css.messageLeftTooltip;
+  } else if (tooltipPosition === tooltipPositions.RIGHT) {
+    const centerOfMessage = messagePosition.top + messagePosition.height / 2;
+    const tooltipPointing = Math.max(
+      Math.min(centerOfMessage, containerHeight),
+      0,
+    );
+    style = {
+      left: messagePosition.right + sizeOfTooltipArrow,
+      top: tooltipPointing,
+    };
+    className = css.messageRightTooltip;
+  } else if (tooltipPosition === tooltipPositions.TOP_LEFT) {
+    const tooltipPointing = Math.min(
+      containerHeight - messagePosition.top,
+      containerHeight,
+    );
+    style = {
+      left: messagePosition.left,
+      bottom: tooltipPointing + sizeOfTooltipArrow,
+    };
+    className = css.messageTopLeftTooltip;
+  } else if (tooltipPosition === tooltipPositions.TOP_RIGHT) {
+    const tooltipPointing = Math.min(
+      containerHeight - messagePosition.top,
+      containerHeight,
+    );
+    style = {
+      right: containerWidth - messagePosition.right,
+      bottom: tooltipPointing + sizeOfTooltipArrow,
+    };
+    className = css.messageTopRightTooltip;
+  } else if (tooltipPosition === tooltipPositions.BOTTOM_LEFT) {
+    const tooltipPointing = Math.min(messagePosition.bottom, containerHeight);
+    style = {
+      left: messagePosition.left,
+      top: tooltipPointing + sizeOfTooltipArrow,
+    };
+    className = css.messageBottomLeftTooltip;
+  } else if (tooltipPosition === tooltipPositions.BOTTOM_RIGHT) {
+    const tooltipPointing = Math.min(messagePosition.bottom, containerHeight);
+    style = {
+      right: containerWidth - messagePosition.right,
+      top: tooltipPointing + sizeOfTooltipArrow,
+    };
+    className = css.messageBottomRightTooltip;
+    console.log(`container height:${containerHeight}`);
+  }
+  invariant(
+    className && style,
+    `${tooltipPosition} is not valid for timestamp tooltip`,
+  );
+  return { className, style };
+}
+
+export default MessageTimestampTooltip;
