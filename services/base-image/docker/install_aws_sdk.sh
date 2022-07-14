2ca80e0b3df718d7682e5322bf8ef7aca4897dcb
Jonathan Ringer <jonringer117@gmail.com>
2022-07-14 09:22:37 -0700

    [Services] Install double and folly in base-image
    Summary:
    These are required by tunnelbroker and related
    services. By building the `install` targets,
    we can use `find_package()` in the cmake
    logic similar to how cmake would find dependencies
    with nix
    
    Test Plan: N/A later diff will include
    Test plan including the changes from this
    diff
    
    Reviewers: max, varun, karol
    
    Subscribers: ashoat, tomek, adrian, atul, abosh
    
    Differential Revision: https://phab.comm.dev/D4630

--- services/base-image/docker/install_aws_sdk.sh
+++ services/base-image/docker/install_aws_sdk.sh
@@ -4,12 +4,18 @@ set -e
 
 cd /tmp
 
-git clone --recurse-submodules -b 1.9.176 --single-branch https://github.com/aws/aws-sdk-cpp
+git clone https://github.com/aws/aws-sdk-cpp \
+  --recurse-submodules \
+  -b 1.9.176 \
+  --single-branch
+
 mkdir aws-sdk-cpp/build
 pushd aws-sdk-cpp/build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=/usr/local/aws_sdk -DBUILD_ONLY="core;s3;dynamodb"
-make
-make install
+cmake .. \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_PREFIX_PATH=/usr/local/aws_sdk \
+  -DBUILD_ONLY="core;s3;dynamodb"
+make install -l$(nproc) -j
 
 popd # aws-sdk-cpp/build
 rm -rf aws-sdk-cpp
--- services/base-image/docker/install_glog.sh
+++ services/base-image/docker/install_glog.sh
@@ -4,4 +4,12 @@ set -e
 
 pushd /usr/lib
 git clone https://github.com/google/glog.git --branch v0.4.0 --single-branch
+
+pushd glog
+cmake . -DBUILD_TESTING=OFF
+make install -j$(nproc) -l$(nproc)
+popd # glog
+
+rm -rf glog
+
 popd # /usr/lib
