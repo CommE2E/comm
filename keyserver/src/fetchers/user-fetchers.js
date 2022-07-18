CommE2E/comm

commit ac199c492c8259f1d6c20eefbdac183857ceb9f0

[keyserver] Cast NULLs in UNION SELECT
Summary:
See [comment on Linear](https://linear.app/comm/issue/ENG-111#comment-3b07dac7). This fixes a bug where we were seeing `string`s where we previously saw `number`s.

Depends on D4528

Test Plan:
1. I made sure the syntax was valid on both MySQL 5.7 and MariaDB 10.8 based on docs
2. I tested with MariaDB 10.8
3. I made sure the issue only occured when merging in `NULL` in cases where you aren't expecting a string by testing queries
3. I `git grep`d through the codebase to make sure this was the only place we were doing that

Reviewers: jonringer-comm, atul, palys-swm

Reviewed By: atul

Subscribers: Adrian, yayabosh

Differential Revision: https://phab.comm.dev/D4530

keyserver/src/fetchers/user-fetchers.js
@@ -85,9 +85,10 @@ async function fetchKnownUserInfos(
     `);
   }
   query.append(SQL`
-    UNION SELECT id AS user1, NULL AS user2, NULL AS undirected_status,
-      NULL AS user1_directed_status, NULL AS user2_directed_status,
-      username
+    UNION SELECT id AS user1, NULL AS user2, username,
+      CAST(NULL AS UNSIGNED) AS undirected_status,
+      CAST(NULL AS UNSIGNED) AS user1_directed_status,
+      CAST(NULL AS UNSIGNED) AS user2_directed_status
     FROM users
     WHERE id = ${viewer.userID}
   `);

commit 69b3a66efe711d5d637df336e3f6137bd2e844e3

[keyserver] Fix fetchKnownUserInfos
Summary:
More context in [Linear issue](https://linear.app/comm/issue/ENG-1409/relationship-prompt-appears-even-if-users-are-already-friends). This diff fixes an issue introduced in D4530.

In that diff I reordered the `UNION SELECT` list without reordering the `SELECT` list. Oops!

Test Plan: This time I definitely tested both MySQL 5.7 and MariaDB 10.8 and made sure that ENG-1409 is resolved on both

Reviewers: def-au1t, atul, palys-swm

Reviewed By: atul

Subscribers: Adrian, yayabosh

Differential Revision: https://phab.comm.dev/D4558

keyserver/src/fetchers/user-fetchers.js
@@ -63,9 +63,8 @@ async function fetchKnownUserInfos(
   }
 
   const query = SQL`
-    SELECT ru.user1, ru.user2, ru.status AS undirected_status,
-      rd1.status AS user1_directed_status, rd2.status AS user2_directed_status,
-      u.username
+    SELECT ru.user1, ru.user2, u.username, ru.status AS undirected_status,
+      rd1.status AS user1_directed_status, rd2.status AS user2_directed_status
     FROM relationships_undirected ru
     LEFT JOIN relationships_directed rd1
       ON rd1.user1 = ru.user1 AND rd1.user2 = ru.user2