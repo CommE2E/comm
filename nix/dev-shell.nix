0195c08e0fcd93078b9a37952325a8aff456dc66
Jonathan Ringer <jonringer117@gmail.com>
2022-07-21 12:24:45 -0700

    [NixOS] Allow for usage of Android NDK binaries
    Summary:
    Android NDK distributes pre-compiled binaries which link against
    libstdc++.so.6 and zlib. NixOS can't run them without some hacks, and
    LD_LIBRARY_PATH is the quickest fix.
    
    Test Plan: On NixOS, `nix develop` then build android
    
    Reviewers: varun
    
    Subscribers: ashoat, tomek, adrian, atul, abosh
    
    Differential Revision: https://phab.comm.dev/D4602

--- nix/dev-shell.nix
+++ nix/dev-shell.nix
@@ -38,6 +38,7 @@
 , watchman
 , rustfmt
 , yarn
+, zlib
 }:
 
 mkShell {
@@ -121,6 +122,11 @@ mkShell {
 
     # Provide decent bash prompt
     source "${better-prompt}/bin/better-prompt"
+
+    if [[ -f /etc/NIXOS ]]; then
+      # allow for impurely downloaded Android NDK tools to be used on NixOS
+      export LD_LIBRARY_PATH=${lib.makeLibraryPath [ stdenv.cc.cc.lib zlib ]}
+    fi
 
     echo "Welcome to Comm dev environment! :)"
   '';
