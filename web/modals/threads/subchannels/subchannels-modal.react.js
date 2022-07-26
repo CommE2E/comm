740dc18c65588ff935082731cc62c3485351e9e7
Jacek Nitychoruk <jacek.nitychoruk@swmansion.com>
2022-07-26 17:29:04 +0200

    [web] Introduce `useFilteredChildThreads` hook
    Summary:
    Introduce hook containing logic responsible for selecting `ChatThreadItem`s containing child threads with some filter, so it could be used in both Subchannels and Sidebars modals on web.
    The hook also adds watching to threads and fetches most recent message from server if necessary, so all threads data are always updated.
    The logic is almost completely moved from `SubchannelsModal` and only some default parameters were added.
    
    Test Plan: The subchannels modal should behave in the same way as before change.
    
    Reviewers: tomek, atul
    
    Reviewed By: tomek
    
    Subscribers: ashoat, adrian, abosh
    
    Differential Revision: https://phab.comm.dev/D4606

--- web/modals/threads/subchannels/subchannels-modal.react.js
+++ web/modals/threads/subchannels/subchannels-modal.react.js
@@ -2,21 +2,9 @@
 
 import * as React from 'react';
 
-import {
-  fetchSingleMostRecentMessagesFromThreads,
-  fetchSingleMostRecentMessagesFromThreadsActionTypes,
-} from 'lib/actions/message-actions';
-import { useFilteredChatListData } from 'lib/selectors/chat-selectors';
-import { threadSearchIndex } from 'lib/selectors/nav-selectors';
-import { childThreadInfos } from 'lib/selectors/thread-selectors';
-import { threadIsChannel, threadInChatList } from 'lib/shared/thread-utils';
-import threadWatcher from 'lib/shared/thread-watcher';
-import {
-  useServerCall,
-  useDispatchActionPromise,
-} from 'lib/utils/action-utils';
+import { useFilteredChildThreads } from 'lib/hooks/child-threads';
+import { threadIsChannel } from 'lib/shared/thread-utils';
 
-import { useSelector } from '../../../redux/redux-utils';
 import SearchModal from '../../search-modal.react';
 import Subchannel from './subchannel.react';
 import css from './subchannels-modal.css';
@@ -28,102 +16,25 @@ type ContentProps = {
 
 function SubchannelsModalContent(props: ContentProps): React.Node {
   const { searchText, threadID } = props;
-  const childThreads = useSelector(state => childThreadInfos(state)[threadID]);
-  const subchannelIDs = React.useMemo(() => {
-    if (!childThreads) {
-      return new Set();
-    }
-    return new Set(
-      childThreads.filter(threadIsChannel).map(threadInfo => threadInfo.id),
-    );
-  }, [childThreads]);
-
-  const filterSubchannels = React.useCallback(
-    thread => subchannelIDs.has(thread?.id),
-    [subchannelIDs],
-  );
-  const allSubchannelsList = useFilteredChatListData(filterSubchannels);
-
-  const searchIndex = useSelector(threadSearchIndex);
-
-  const searchResultIDs = React.useMemo(
-    () => searchIndex.getSearchResults(searchText),
-    [searchIndex, searchText],
-  );
-
-  const searchTextExists = !!searchText.length;
-
-  const subchannelIDsNotInChatList = React.useMemo(
-    () =>
-      new Set(
-        allSubchannelsList
-          .filter(item => !threadInChatList(item.threadInfo))
-          .map(item => item.threadInfo.id),
-      ),
-    [allSubchannelsList],
-  );
-
-  React.useEffect(() => {
-    if (!subchannelIDsNotInChatList.size) {
-      return;
-    }
-    subchannelIDsNotInChatList.forEach(tID => threadWatcher.watchID(tID));
-
-    return () =>
-      subchannelIDsNotInChatList.forEach(tID => threadWatcher.removeID(tID));
-  }, [subchannelIDsNotInChatList]);
-
-  const filteredSubchannelsChatList = React.useMemo(() => {
-    if (!searchTextExists) {
-      return allSubchannelsList;
-    }
-    return allSubchannelsList.filter(item =>
-      searchResultIDs.includes(item.threadInfo.id),
-    );
-  }, [allSubchannelsList, searchResultIDs, searchTextExists]);
-
-  const threadIDsWithNoMessages = React.useMemo(
-    () =>
-      new Set(
-        filteredSubchannelsChatList
-          .filter(item => !item.mostRecentMessageInfo)
-          .map(item => item.threadInfo.id),
-      ),
-    [filteredSubchannelsChatList],
-  );
-  const dispatchActionPromise = useDispatchActionPromise();
-  const fetchSingleMostRecentMessages = useServerCall(
-    fetchSingleMostRecentMessagesFromThreads,
-  );
-
-  React.useEffect(() => {
-    if (!threadIDsWithNoMessages.size) {
-      return;
-    }
-    dispatchActionPromise(
-      fetchSingleMostRecentMessagesFromThreadsActionTypes,
-      fetchSingleMostRecentMessages(Array.from(threadIDsWithNoMessages)),
-    );
-  }, [
-    threadIDsWithNoMessages,
-    fetchSingleMostRecentMessages,
-    dispatchActionPromise,
-  ]);
+  const subchannelList = useFilteredChildThreads(threadID, {
+    predicate: threadIsChannel,
+    searchText,
+  });
   const subchannels = React.useMemo(() => {
-    if (!filteredSubchannelsChatList.length) {
+    if (!subchannelList.length) {
       return (
         <div className={css.noSubchannels}>
           No matching subchannels were found in the channel!
         </div>
       );
     }
-    return filteredSubchannelsChatList.map(childThreadItem => (
+    return subchannelList.map(childThreadItem => (
       <Subchannel
         chatThreadItem={childThreadItem}
         key={childThreadItem.threadInfo.id}
       />
     ));
-  }, [filteredSubchannelsChatList]);
+  }, [subchannelList]);
 
   return <div className={css.subchannelsListContainer}>{subchannels}</div>;
 }
