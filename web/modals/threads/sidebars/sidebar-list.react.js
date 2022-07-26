0c00898702611218a09852f76161aa6950d9c487
Jacek Nitychoruk <jacek.nitychoruk@swmansion.com>
2022-07-26 17:29:04 +0200

    [web] Introduce `SidebarList` component
    Summary:
    Introduce component displaying list of sidebars in Sidebars modal.
    {F113329}
    {F115291}
    
    Test Plan: The component will be used in Sidebars Modal after next diff.
    
    Reviewers: tomek, atul, ashoat
    
    Reviewed By: tomek, ashoat
    
    Subscribers: ashoat, adrian, abosh
    
    Differential Revision: https://phab.comm.dev/D4609

---
+++ web/modals/threads/sidebars/sidebar-list.react.js
@@ -0,0 +1,39 @@
+// @flow
+
+import * as React from 'react';
+
+import type { ChatThreadItem } from 'lib/selectors/chat-selectors';
+
+import Sidebar from './sidebar.react';
+import css from './sidebars-modal.css';
+
+type Props = {
+  +sidebars: $ReadOnlyArray<ChatThreadItem>,
+};
+
+function SidebarList(props: Props): React.Node {
+  const { sidebars } = props;
+
+  const sidebarItems = React.useMemo(
+    () =>
+      sidebars.map((sidebarChatItem, idx, sidebarArray) => (
+        <Sidebar
+          sidebar={sidebarChatItem}
+          key={sidebarChatItem.threadInfo.id}
+          isLastItem={idx === sidebarArray.length - 1}
+        />
+      )),
+    [sidebars],
+  );
+
+  if (sidebars.length === 0) {
+    return (
+      <div className={css.noSidebars}>
+        No matching threads were found in the chat.
+      </div>
+    );
+  }
+  return <div className={css.sidebarList}>{sidebarItems}</div>;
+}
+
+export default SidebarList;
