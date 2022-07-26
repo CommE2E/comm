496e35e418ab10130c930acdcdf559707d31abb6
Jacek Nitychoruk <jacek.nitychoruk@swmansion.com>
2022-07-26 17:29:04 +0200

    [web] Introduce `SidebarsModal`
    Summary:
    Introducing new `SidebarsModal` component with two tabs - with all sidebars and those displayed in chat list (that we are member of)
    
    {F113331}
    
    Test Plan: The modal is not used yet, next diff replaces an old one with this component in menu and after clicking "see more sidebars".
    
    Reviewers: tomek, atul, ashoat
    
    Reviewed By: tomek, ashoat
    
    Subscribers: ashoat, adrian, abosh
    
    Differential Revision: https://phab.comm.dev/D4610

---
+++ web/modals/threads/sidebars/sidebars-modal.react.js
@@ -0,0 +1,80 @@
+// @flow
+
+import * as React from 'react';
+
+import { useFilteredChildThreads } from 'lib/hooks/child-threads';
+import { threadInChatList, threadIsSidebar } from 'lib/shared/thread-utils';
+
+import Tabs from '../../../components/tabs.react';
+import SearchModal from '../../search-modal.react';
+import SidebarList from './sidebar-list.react';
+import css from './sidebars-modal.css';
+
+type SidebarTab = 'All Threads' | 'My Threads';
+
+type ContentProps = {
+  +searchText: string,
+  +threadID: string,
+  +defaultTab: SidebarTab,
+};
+
+function SidebarsModalContent(props: ContentProps): React.Node {
+  const { searchText, threadID, defaultTab } = props;
+  const [tab, setTab] = React.useState<SidebarTab>(defaultTab);
+
+  const sidebarList = useFilteredChildThreads(threadID, {
+    predicate: threadIsSidebar,
+    searchText,
+  });
+
+  const sidebarsChatListVisibleInChat = sidebarList.filter(chatItem =>
+    threadInChatList(chatItem.threadInfo),
+  );
+
+  return (
+    <div className={css.sidebarListContainer}>
+      <Tabs.Container activeTab={tab} setTab={setTab}>
+        <Tabs.Item id="All Threads" header="All Threads">
+          <SidebarList sidebars={sidebarList} />
+        </Tabs.Item>
+        <Tabs.Item id="My Threads" header="My Threads">
+          <SidebarList sidebars={sidebarsChatListVisibleInChat} />
+        </Tabs.Item>
+      </Tabs.Container>
+    </div>
+  );
+}
+
+type Props = {
+  +threadID: string,
+  +onClose: () => void,
+  +defaultTab?: SidebarTab,
+};
+
+function SidebarsModal(props: Props): React.Node {
+  const { threadID, onClose, defaultTab = 'All Threads' } = props;
+
+  const sidebarsContent = React.useCallback(
+    (searchText: string) => (
+      <SidebarsModalContent
+        searchText={searchText}
+        defaultTab={defaultTab}
+        threadID={threadID}
+      />
+    ),
+    [defaultTab, threadID],
+  );
+
+  return (
+    <SearchModal
+      name="Threads"
+      searchPlaceholder="Search"
+      onClose={onClose}
+      size="fit-content"
+    >
+      {sidebarsContent}
+    </SearchModal>
+  );
+}
+
+export default SidebarsModal;
