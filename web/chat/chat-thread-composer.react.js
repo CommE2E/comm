13cfca12039527b8b37a095b1eb3235777e61d01
Jacek Nitychoruk <jacek.nitychoruk@swmansion.com>
2022-07-26 10:10:21 +0200

    [web] Introduce `ChatThreadComposer` component for selecting users to new thread
    Summary:
    Introducing main component used in thread creation view, that allows selecting users to new thread and displays list of found users:
    {F94139}
    The component doesn't contain callbacks implementation yet. The diff focuses mostly on UI. The callbacks will be added in following diffs.
    
    Test Plan: The component is not used in UI yet. I tested it after introducing chat creation view in next diffs.
    
    Reviewers: tomek, atul, abosh, ashoat
    
    Reviewed By: tomek, ashoat
    
    Subscribers: ashoat, adrian, abosh
    
    Differential Revision: https://phab.comm.dev/D4461

---
+++ web/chat/chat-thread-composer.react.js
@@ -0,0 +1,124 @@
+// @flow
+import classNames from 'classnames';
+import * as React from 'react';
+
+import { userSearchIndexForPotentialMembers } from 'lib/selectors/user-selectors';
+import { getPotentialMemberItems } from 'lib/shared/search-utils';
+import type { AccountUserInfo, UserListItem } from 'lib/types/user-types';
+
+import Label from '../components/label.react';
+import Search from '../components/search.react';
+import type { InputState } from '../input/input-state';
+import { useSelector } from '../redux/redux-utils';
+import SWMansionIcon from '../SWMansionIcon.react';
+import css from './chat-thread-composer.css';
+
+type Props = {
+  +userInfoInputArray: $ReadOnlyArray<AccountUserInfo>,
+  +otherUserInfos: { [id: string]: AccountUserInfo },
+  +threadID: string,
+  +inputState: InputState,
+};
+
+function ChatThreadComposer(props: Props): React.Node {
+  const { userInfoInputArray, otherUserInfos } = props;
+
+  const [usernameInputText, setUsernameInputText] = React.useState('');
+
+  const userSearchIndex = useSelector(userSearchIndexForPotentialMembers);
+
+  const userInfoInputIDs = React.useMemo(
+    () => userInfoInputArray.map(userInfo => userInfo.id),
+    [userInfoInputArray],
+  );
+
+  const userListItems = React.useMemo(
+    () =>
+      getPotentialMemberItems(
+        usernameInputText,
+        otherUserInfos,
+        userSearchIndex,
+        userInfoInputIDs,
+      ),
+    [usernameInputText, otherUserInfos, userSearchIndex, userInfoInputIDs],
+  );
+
+  // eslint-disable-next-line no-unused-vars
+  const onSelectUserFromSearch = React.useCallback((id: string) => {}, []);
+  // eslint-disable-next-line no-unused-vars
+  const onRemoveUserFromSelected = React.useCallback((id: string) => {}, []);
+
+  const userSearchResultList = React.useMemo(() => {
+    if (
+      !userListItems.length ||
+      (!usernameInputText && userInfoInputArray.length)
+    ) {
+      return null;
+    }
+
+    return (
+      <ul className={css.searchResultsContainer}>
+        {userListItems.map((userSearchResult: UserListItem) => (
+          <li
+            className={css.searchResultsItem}
+            key={userSearchResult.id}
+            onClick={() => onSelectUserFromSearch(userSearchResult.id)}
+          >
+            <div className={css.userName}>{userSearchResult.username}</div>
+            <div className={css.userInfo}>{userSearchResult.alertTitle}</div>
+          </li>
+        ))}
+      </ul>
+    );
+  }, [
+    onSelectUserFromSearch,
+    userInfoInputArray.length,
+    userListItems,
+    usernameInputText,
+  ]);
+
+  const hideSearch = React.useCallback(() => {}, []);
+
+  const tagsList = React.useMemo(() => {
+    if (!userInfoInputArray?.length) {
+      return null;
+    }
+    const labels = userInfoInputArray.map(user => {
+      return (
+        <Label key={user.id} onClose={() => onRemoveUserFromSelected(user.id)}>
+          {user.username}
+        </Label>
+      );
+    });
+    return <div className={css.userSelectedTags}>{labels}</div>;
+  }, [userInfoInputArray, onRemoveUserFromSelected]);
+
+  const threadSearchContainerStyles = React.useMemo(
+    () =>
+      classNames(css.threadSearchContainer, {
+        [css.fullHeight]: !userInfoInputArray.length,
+      }),
+    [userInfoInputArray.length],
+  );
+
+  return (
+    <div className={threadSearchContainerStyles}>
+      <div className={css.searchRow}>
+        <div className={css.searchField}>
+          <Search
+            onChangeText={setUsernameInputText}
+            searchText={usernameInputText}
+            placeholder="Select users for chat"
+          />
+        </div>
+        <div className={css.closeSearch} onClick={hideSearch}>
+          <SWMansionIcon size={25} icon="cross" />
+        </div>
+      </div>
+      {tagsList}
+      {userSearchResultList}
+    </div>
+  );
+}
+
+export default ChatThreadComposer;
