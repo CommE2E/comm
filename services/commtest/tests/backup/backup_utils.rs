72db45ea8145b840b7644fde91a9acd89f34ed5b
Karol Bisztyga <karol.bisztyga@swmansion.com>
2022-07-26 10:46:05 +0200

    [services] Tests - Backup - Move compare backups to backup utils
    Summary:
    Depends on D4634
    
    Excluding the functionality of comparing the backups from the backup integration test into the backup utils so it can also be used in the backup performance tests.
    
    Test Plan:
    ```
    cd services
    yarn run-performance-tests backup 1
    ```
    
    Reviewers: tomek, varun
    
    Reviewed By: tomek
    
    Subscribers: ashoat, tomek, adrian, atul, abosh
    
    Differential Revision: https://phab.comm.dev/D4635

--- services/commtest/tests/backup/backup_utils.rs
+++ services/commtest/tests/backup/backup_utils.rs
@@ -36,3 +36,46 @@ pub struct BackupData {
   pub backup_item: Item,
   pub log_items: Vec<Item>,
 }
+
+#[allow(dead_code)]
+pub fn compare_backups(backup_data: &BackupData, result: &BackupData) {
+  // check backup size
+  let expected: usize = backup_data.backup_item.chunks_sizes.iter().sum();
+  let from_result: usize = result.backup_item.chunks_sizes.iter().sum();
+  assert_eq!(
+    from_result, expected,
+    "backup sizes do not match, expected {}, got {}",
+    expected, from_result
+  );
+
+  // check backup attachments
+  let expected: usize = backup_data.backup_item.attachments_holders.len();
+  let from_result: usize = result.backup_item.attachments_holders.len();
+  assert_eq!(
+    from_result, expected,
+    "backup: number of attachments holders do not match, expected {}, got {}",
+    expected, from_result
+  );
+
+  // check number of logs
+  let expected: usize = backup_data.log_items.len();
+  let from_result: usize = result.log_items.len();
+  assert_eq!(
+    expected, from_result,
+    "number of logs do not match, expected {}, got {}",
+    expected, from_result
+  );
+
+  // check log sizes
+  for i in 0..backup_data.log_items.len() {
+    let expected: usize = backup_data.log_items[i].chunks_sizes.iter().sum();
+    let from_result: usize = result.log_items[i].chunks_sizes.iter().sum();
+    assert_eq!(
+      from_result, expected,
+      "log number {} sizes do not match, expected {}, got {}",
+      i, expected, from_result
+    );
+  }
+
+  // todo: check logs attachment holders
+}
--- services/commtest/tests/backup_integration_test.rs
+++ services/commtest/tests/backup_integration_test.rs
@@ -83,45 +83,9 @@ async fn backup_integration_test() -> Result<(), Error> {
     add_attachments::run(&mut client, &backup_data, Some(log_index)).await?;
   }
 
-  let result = pull_backup::run(&mut client, &backup_data).await?;
+  let result: BackupData = pull_backup::run(&mut client, &backup_data).await?;
 
-  // check backup size
-  let expected: usize = backup_data.backup_item.chunks_sizes.iter().sum();
-  let from_result: usize = result.backup_item.chunks_sizes.iter().sum();
-  assert_eq!(
-    from_result, expected,
-    "backup sizes do not match, expected {}, got {}",
-    expected, from_result
-  );
-
-  // check backup attachments
-  let expected: usize = backup_data.backup_item.attachments_holders.len();
-  let from_result: usize = result.backup_item.attachments_holders.len();
-  assert_eq!(
-    from_result, expected,
-    "backup: number of attachments holders do not match, expected {}, got {}",
-    expected, from_result
-  );
-
-  // check number of logs
-  let expected: usize = backup_data.log_items.len();
-  let from_result: usize = result.log_items.len();
-  assert_eq!(
-    expected, from_result,
-    "number of logs do not match, expected {}, got {}",
-    expected, from_result
-  );
-
-  // check log sizes
-  for i in 0..backup_data.log_items.len() {
-    let expected: usize = backup_data.log_items[i].chunks_sizes.iter().sum();
-    let from_result: usize = result.log_items[i].chunks_sizes.iter().sum();
-    assert_eq!(
-      from_result, expected,
-      "log number {} sizes do not match, expected {}, got {}",
-      i, expected, from_result
-    );
-  }
+  backup_utils::compare_backups(&backup_data, &result);
 
   // push so many attachments that the log item's data will have to be moved
   // from the db to the s3
