871d00d59ab1698545f7caa78108bf3f00ee57a3
Jonathan Ringer <jonringer117@gmail.com>
2022-07-27 18:40:45 -0700

    [Nix] Re-package arcanist as just the php scripts
    Summary:
    Nixpkgs `arcanist` pacakge includes "phage" shell which
    is a fairly large c++ and php code base. However,
    we only really care about using the `arc` command, so we should reduce
    the scope of the `arcanist` package to be what we actually use.
    
    This avoids situations such as https://phab.comm.dev/D4483#131136
    where the build for phage (and it's requirements), aren't thread safe. They
    use bison and flex, and the generated files aren't delcared in a way in which
    make will be aware that generation of those files need to be ordered before
    usage of them.
    
    Test Plan:
    ```
    # this can be used from any branch
    nix develop github:commE2E/comm?ref=update-nix-flakes
    # use any arc command, 8.1 would fail on `arc diff`
    ```
    
    Reviewers: varun, ashoat
    
    Reviewed By: ashoat
    
    Subscribers: tomek, adrian, atul, abosh
    
    Differential Revision: https://phab.comm.dev/D4662

---
+++ nix/arcanist.nix
@@ -0,0 +1,64 @@
+{ stdenv
+, lib
+, cacert
+, fetchFromGitHub
+, installShellFiles
+, makeWrapper
+, php80
+, python3
+, which
+}:
+
+stdenv.mkDerivation {
+  pname = "arcanist";
+  version = "20220517";
+
+  src = fetchFromGitHub {
+    owner = "phacility";
+    repo = "arcanist";
+    rev = "85c953ebe4a6fef332158fd757d97c5a58682d3a";
+    sha256 = "0x847fw74mzrbhzpgc4iqgvs6dsf4svwfa707dsbxi78fn2lxbl7";
+  };
+
+  # These need to be in PATH during the build
+  nativeBuildInputs = [ php80 python3 installShellFiles makeWrapper ];
+
+  # Since we are exporting a script, we do not need to do an actual "build"
+  doBuild = false;
+
+  installPhase = ''
+    # Copy arcanist contents
+    mkdir -p $out/libexec
+    cp -R . $out/libexec/arcanist
+
+    # provide a recent up-to-date certificate bundle for ssl
+    ln -sf ${cacert}/etc/ssl/certs/ca-bundle.crt \
+      $out/libexec/arcanist/resources/ssl/default.pem
+
+    # convert `#!/usr/bin/env php` into calling nixpkgs php interpreter
+    patchShebangs $out/libexec/arcanist/bin/arc
+
+    # Create a bin/arc which points to the real script, but provides
+    # assumptions such as the PATH including python3 and which
+    makeWrapper $out/libexec/arcanist/bin/arc $out/bin/arc \
+      --prefix PATH : ${lib.makeBinPath [ which python3 ]}
+
+    # Add shell completion for bash
+    $out/bin/arc shell-complete --generate --
+    installShellCompletion --cmd arc --bash \
+      $out/libexec/arcanist/support/shell/rules/bash-rules.sh
+  '';
+
+  # Ensure `arc` is able to run without failing
+  doInstallCheck = true;
+  installCheckPhase = ''
+    $out/bin/arc help diff -- > /dev/null
+  '';
+
+  meta = {
+    description = "Command line interface to Phabricator";
+    homepage = "http://phabricator.org";
+    license = lib.licenses.asl20;
+    platforms = lib.platforms.unix;
+  };
+}
