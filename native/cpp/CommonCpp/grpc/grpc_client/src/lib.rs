00e7acfc26064449d7d7d1e23e4ff2a8693965d1
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2022-08-01 17:30:13 -0400

    [native] create bridge module and runtime
    Summary: lazily create a static tokio runtime object that can be passed around for client calls. create the ffi module where library APIs will go.
    
    Test Plan: cargo build
    
    Reviewers: tomek, karol
    
    Reviewed By: tomek
    
    Subscribers: ashoat, adrian, atul, abosh
    
    Differential Revision: https://phab.comm.dev/D4707

--- native/cpp/CommonCpp/grpc/grpc_client/src/lib.rs
+++ native/cpp/CommonCpp/grpc/grpc_client/src/lib.rs
@@ -1,3 +1,17 @@
+use lazy_static::lazy_static;
+use std::sync::Arc;
+use tokio::runtime::{Builder, Runtime};
 pub mod identity {
   tonic::include_proto!("identity");
+}
+
+lazy_static! {
+  pub static ref RUNTIME: Arc<Runtime> = Arc::new(
+    Builder::new_multi_thread()
+      .worker_threads(1)
+      .max_blocking_threads(1)
+      .enable_all()
+      .build()
+      .unwrap()
+  );
 }
