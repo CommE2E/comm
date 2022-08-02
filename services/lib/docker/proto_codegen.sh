6a9572b069307e57a947eed5d15052124970424d
Abosh Upadhyaya <aboshu@gmail.com>
2022-08-02 13:05:35 -0400

    [services] Clean up `proto_codegen.sh` using ShellCheck
    Summary: Related Linear issue [[ https://linear.app/comm/issue/ENG-1295/clean-up-shell-scripts-in-codebase | here ]]. This is part of a set of diffs that will allow ShellCheck to be added to the CI. See inline comments for specific details of the ShellCheck error/warning output.
    
    Test Plan:
    [[ https://www.shellcheck.net/ | ShellCheck ]]
    
    Additionally, this script is run by `build_service.sh`, which is run during the Backup, Blob, and Tunnelbroker builds so if those workflows continue to pass CI we can reasonably assume that the script continues to work as expected.
    
    Reviewers: atul, jon, tomek, ashoat
    
    Reviewed By: jon
    
    Subscribers: adrian, karol
    
    Differential Revision: https://phab.comm.dev/D4721

--- services/lib/docker/proto_codegen.sh
+++ services/lib/docker/proto_codegen.sh
@@ -4,8 +4,8 @@ set -e
 
 echo "generating files from protos..."
 
-for PROTO_FILE in $(ls ./protos); do
-  protoc -I=./protos --cpp_out=_generated --grpc_out=_generated --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` ./protos/$PROTO_FILE
+for PROTO_FILE in ./protos/*; do
+  protoc -I=./protos --cpp_out=_generated --grpc_out=_generated --plugin=protoc-gen-grpc="$(which grpc_cpp_plugin)" ./"$PROTO_FILE"
 done
 
 echo "success - code generated from protos"
