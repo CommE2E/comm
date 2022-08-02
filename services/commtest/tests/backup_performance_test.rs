b3cac6b748e06f5d7c6a0df9e445e9c75a019d53
Karol Bisztyga <karol.bisztyga@swmansion.com>
2022-08-02 12:37:48 +0200

    update

--- services/commtest/tests/backup_performance_test.rs
+++ services/commtest/tests/backup_performance_test.rs
@@ -45,7 +45,7 @@ async fn backup_performance_test() -> Result<(), Error> {
         vec![ByteSize::mib(1).as_u64() as usize; 3 + (i % 5)],
         (0..(i % 5)).map(|x| format!("holder{}", x)).collect(),
       ),
-      log_items: (0..(i % 4))
+      log_items: (0..((i % 4) + 2))
         .map(|x| {
           Item::new(
             String::new(),
