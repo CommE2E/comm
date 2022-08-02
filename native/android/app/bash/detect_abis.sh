9a3c816fc6e36c441bebc3e8f9b3fcad5750e54c
Abosh Upadhyaya <aboshu@gmail.com>
2022-08-02 12:38:25 -0400

    [native] Clean up `detect_abis.sh` using ShellCheck
    Summary: Related Linear issue [[ https://linear.app/comm/issue/ENG-1295/clean-up-shell-scripts-in-codebase | here ]]. This is part of a set of diffs that will allow ShellCheck to be added to the CI. See inline comments for specific details of the ShellCheck error/warning output.
    
    Test Plan:
    [[ https://www.shellcheck.net/ | ShellCheck ]]
    
    Additionally, this script is run during the Android build so if that workflow continues to pass CI we can reasonably assume that the script continues to work as expected.
    
    Reviewers: atul, jon, tomek, ashoat, karol
    
    Reviewed By: atul, jon, karol
    
    Subscribers: ashoat, adrian
    
    Differential Revision: https://phab.comm.dev/D4717

--- native/android/app/bash/detect_abis.sh
+++ native/android/app/bash/detect_abis.sh
@@ -8,11 +8,19 @@ ABIS=()
 for ID in ${IDS}
 do
   ABI=$(adb -s "$ID" shell getprop ro.product.cpu.abi)
-  # check if we already have this ABI
-  if [[ " ${ABIS[*]} " =~ " ${ABI} " ]]; then
-    continue
+  
+  found=false
+  for ABI_ in "${ABIS[@]}"
+  do
+    # check if we already have this ABI
+    if [[ "${ABI} " == "${ABI_}" ]]; then
+      found=true
+      break
+    fi
+  done
+  if [[ ! $found ]]; then
+    ABIS+=("${ABI} ")
   fi
-	ABIS+="${ABI} "
 done
 
 echo "$ABIS"
