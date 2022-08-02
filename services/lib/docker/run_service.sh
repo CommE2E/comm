78e3a9f5e2fe582c4b48c6866302b831c3061d7f
Abosh Upadhyaya <aboshu@gmail.com>
2022-08-02 15:46:17 -0400

    [services] Clean up `run_service.sh` using ShellCheck
    Summary: Related Linear issue [[ https://linear.app/comm/issue/ENG-1295/clean-up-shell-scripts-in-codebase | here ]]. This is part of a set of diffs that will allow ShellCheck to be added to the CI. See inline comments for specific details of the ShellCheck error/warning output.
    
    Test Plan: [[ https://www.shellcheck.net/ | ShellCheck ]]
    
    Reviewers: atul, jon, tomek
    
    Reviewed By: atul, jon, tomek
    
    Subscribers: max, ashoat, adrian, karol
    
    Differential Revision: https://phab.comm.dev/D4723

--- services/lib/docker/run_service.sh
+++ services/lib/docker/run_service.sh
@@ -4,12 +4,12 @@ set -e
 
 EXE_PATH="./cmake/build/bin"
 
-EXE=`ls $EXE_PATH`
-EXES=`ls $EXE_PATH | wc -l`
+EXE=$(find $EXE_PATH -mindepth 1 -maxdepth 1 -not -path '*/.*')
+EXES=$(wc -l <<< "$EXE")
 
 if [[ $EXES -ne 1 ]]; then
   echo "there should be exactly one executable of a service, $EXES found";
   exit 1;
 fi
 
-$EXE_PATH/$EXE
+"$EXE"
