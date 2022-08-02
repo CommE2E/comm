b02560464e4914e51b3bd336e23e6a523cc08540
Abosh Upadhyaya <aboshu@gmail.com>
2022-08-02 12:17:46 -0400

    [keyserver] Clean up `source-nvm.sh` using ShellCheck
    Summary: Related Linear issue [[ https://linear.app/comm/issue/ENG-1295/clean-up-shell-scripts-in-codebase | here ]]. This is part of a set of diffs that will allow ShellCheck to be added to the CI. See inline comments for specific details of the ShellCheck error/warning output.
    
    Test Plan:
    [[ https://www.shellcheck.net/ | ShellCheck ]]
    
    Additionally, this script is run during the keyserver build so if that workflow continues to pass CI we can reasonably assume that the script continues to work as expected.
    
    Reviewers: atul, jon, tomek, ashoat
    
    Reviewed By: atul, ashoat
    
    Subscribers: adrian
    
    Differential Revision: https://phab.comm.dev/D4715

--- keyserver/bash/source-nvm.sh
+++ keyserver/bash/source-nvm.sh
@@ -9,9 +9,12 @@ unset PREFIX
 [ -n "$IN_NIX_SHELL" ] && return 0
 
 # Intel Mac
+# shellcheck source=/dev/null
 [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"
 # ARM-based Mac
+# shellcheck source=/dev/null
 [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && . "/opt/homebrew/opt/nvm/nvm.sh"
 # Ubuntu
+# shellcheck source=/dev/null
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 nvm install --no-progress
