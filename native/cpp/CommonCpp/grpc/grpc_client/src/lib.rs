0735ad75abbd515a1cb34bd11f167f337826f7ba
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2022-08-03 13:42:38 -0400

    [native] create Client struct and add method for get_user_id
    Summary: The two new functions are small enough that I've included them in the same diff. We construct a Client that holds an identity_client and then use the identity_client to call the GetUserID rpc
    
    Test Plan: cargo build (will test more thoroughly once library is integrated into application)
    
    Reviewers: tomek, karol, max
    
    Reviewed By: tomek
    
    Subscribers: ashoat, adrian, atul, abosh
    
    Differential Revision: https://phab.comm.dev/D4733

--- native/cpp/CommonCpp/grpc/grpc_client/src/lib.rs
+++ native/cpp/CommonCpp/grpc/grpc_client/src/lib.rs
@@ -1,9 +1,18 @@
 use lazy_static::lazy_static;
 use std::sync::Arc;
 use tokio::runtime::{Builder, Runtime};
+use tonic::{transport::Channel, Response, Status};
+
+use identity::{
+  get_user_id_request::AuthType,
+  identity_service_client::IdentityServiceClient, GetUserIdRequest,
+  GetUserIdResponse,
+};
 pub mod identity {
   tonic::include_proto!("identity");
 }
+
+const IDENTITY_SERVICE_SOCKET_ADDR: &str = "[::1]:50051";
 
 lazy_static! {
   pub static ref RUNTIME: Arc<Runtime> = Arc::new(
@@ -14,4 +23,34 @@ lazy_static! {
       .build()
       .unwrap()
   );
+}
+
+pub struct Client {
+  identity_client: IdentityServiceClient<Channel>,
+}
+
+impl Client {
+  async fn new() -> Self {
+    Self {
+      identity_client: IdentityServiceClient::connect(
+        IDENTITY_SERVICE_SOCKET_ADDR,
+      )
+      .await
+      .unwrap(),
+    }
+  }
+
+  async fn get_user_id(
+    &mut self,
+    auth_type: AuthType,
+    user_info: String,
+  ) -> Result<Response<GetUserIdResponse>, Status> {
+    self
+      .identity_client
+      .get_user_id(GetUserIdRequest {
+        auth_type: auth_type.into(),
+        user_info,
+      })
+      .await
+  }
 }
