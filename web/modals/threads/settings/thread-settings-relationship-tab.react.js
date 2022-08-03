0bd92b5ec2a1c6fcabbfd5803911f725431eed08
Jacek Nitychoruk <jacek.nitychoruk@swmansion.com>
2022-08-03 13:22:41 +0200

    [web] Introduce `ThreadSettingsRelationshipTab` component
    Summary:
    Introduce container for list of relationship buttons. It will be used as tab in thread settings modal on web
    
    {F120835}
    
    Test Plan: The component is not used yet. Test if tab displays correctly after introducing this tab in thread settings modal
    
    Reviewers: tomek, atul
    
    Reviewed By: tomek
    
    Subscribers: ashoat, adrian, abosh
    
    Differential Revision: https://phab.comm.dev/D4680

---
+++ web/modals/threads/settings/thread-settings-relationship-tab.react.js
@@ -0,0 +1,35 @@
+// @flow
+
+import * as React from 'react';
+
+import { type SetState } from 'lib/types/hook-types';
+import { type RelationshipButton } from 'lib/types/relationship-types';
+import type { UserInfo } from 'lib/types/user-types';
+
+import ThreadSettingsRelationshipButton from './thread-settings-relationship-button.react';
+import css from './thread-settings-relationship-tab.css';
+
+type Props = {
+  +setErrorMessage: SetState<string>,
+  +relationshipButtons: $ReadOnlyArray<RelationshipButton>,
+  +otherUserInfo: UserInfo,
+};
+
+function ThreadSettingsRelationshipTab(props: Props): React.Node {
+  const { relationshipButtons, otherUserInfo, setErrorMessage } = props;
+  const buttons = React.useMemo(
+    () =>
+      relationshipButtons.map(action => (
+        <ThreadSettingsRelationshipButton
+          key={action}
+          relationshipButton={action}
+          otherUserInfo={otherUserInfo}
+          setErrorMessage={setErrorMessage}
+        />
+      )),
+    [otherUserInfo, relationshipButtons, setErrorMessage],
+  );
+  return <div className={css.container}>{buttons}</div>;
+}
+
+export default ThreadSettingsRelationshipTab;
