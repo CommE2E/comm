f51da22f67e1b736961ba58861c23adb7ec21974
marcinwasowicz <marcinkwasowicz@gmail.com>
2022-08-03 13:59:19 +0200

    Implement react component that clears database anbd secure store when user logs out or deletes account
    Summary: This differential implements react component that attempts to clear SQLite and secure store when user logs out or deletes account. Component attempts deletion when 'currentUserInfo' of redux-state changes to 'anonymous' which indicates that log out succeded or account was succesfully deleted
    
    Test Plan: Build the app and repeat log in and log out action several times switching users. Additionally some accounts might also be deleted. Application should work without any difference no matter whether it is kept foregrounded, backgrounded or closed by the user between subsequent log-in and log-out actions. Additionally database size should be monitored in XCode/Android studio after each log out and log in to ensure it is actually deleted and recreated empty.
    
    Reviewers: tomek, atul
    
    Subscribers: ashoat, adrian, abosh
    
    Differential Revision: https://phab.comm.dev/D4730

---
+++ native/data/sensitive-data-cleaner.react.js
@@ -0,0 +1,36 @@
+// @flow
+
+import * as React from 'react';
+import ExitApp from 'react-native-exit-app';
+
+import { useSelector } from '../redux/redux-utils';
+
+function SensitiveDataCleaner(): null {
+  const currentUserInfo = useSelector(state => state.currentUserInfo);
+  React.useEffect(() => {
+    try {
+      const databaseCurrentUserInfoID = global.CommCoreModule.getCurrentUserID();
+      const userLoggedOut =
+        !currentUserInfo || currentUserInfo.anonymous ? true : false;
+      if (
+        (userLoggedOut && databaseCurrentUserInfoID) ||
+        (databaseCurrentUserInfoID &&
+          currentUserInfo?.id !== databaseCurrentUserInfoID)
+      ) {
+        global.CommCoreModule.clearSensitiveData();
+      } else if (!userLoggedOut && !databaseCurrentUserInfoID) {
+        global.CommCoreModule.setCurrentUserID(currentUserInfo?.id);
+      }
+    } catch (e) {
+      if (__DEV__) {
+        throw e;
+      } else {
+        console.log(e);
+        ExitApp.exitApp();
+      }
+    }
+  }, [currentUserInfo]);
+  return null;
+}
+
+export { SensitiveDataCleaner };
