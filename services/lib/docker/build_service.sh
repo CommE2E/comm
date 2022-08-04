1c7091c1836d672aee04c339bbf37ddd508ba410
Jonathan Ringer <jonringer117@gmail.com>
2022-08-03 23:26:16 -0700

    [Services] Remove build_services compatibility logic
    Summary:
    This largely undoes the compatibility logic in D4734 to make
    gates pass.
    
    Since blob, backup, and tunnelbroker have been updated, there's no reason to
    use the legacy "special paths" docker-specific workflow
    
    Test Plan: Blob, Backup, and Tunnelbroker gates pass
    
    Reviewers: abosh, atul, max, karol
    
    Subscribers: ashoat, tomek, adrian
    
    Differential Revision: https://phab.comm.dev/D4735

--- services/lib/docker/build_service.sh
+++ services/lib/docker/build_service.sh
@@ -6,27 +6,8 @@ set -e
 SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd -P)
 
 # folly hack - https://github.com/facebook/folly/pull/1231
-if [[ -f /usr/lib/folly/folly/detail/Demangle.h ]]; then
-  # Legacy path-centric workflow
-  sed -i 's/#if __has_include(<demangle.h>)/#if __has_include(<Demangle.h>)/g' \
-    /usr/lib/folly/folly/detail/Demangle.h
-  rm -rf lib
-  mkdir lib
-  pushd lib
-  ln -s /usr/lib/folly .
-  ln -s /usr/lib/glog .
-  ln -s /usr/lib/double-conversion .
-  popd # lib
-
-  rm -rf _generated
-  mkdir _generated
-
-  "${SCRIPT_DIR}"/proto_codegen.sh
-else
-  # New installation centric workflow
-  sed -i 's/#if __has_include(<demangle.h>)/#if __has_include(<Demangle.h>)/g' \
-    /usr/local/include/folly/detail/Demangle.h
-fi
+sed -i 's/#if __has_include(<demangle.h>)/#if __has_include(<Demangle.h>)/g' \
+  /usr/local/include/folly/detail/Demangle.h
 
 rm -rf cmake/build
 mkdir -p cmake/build
