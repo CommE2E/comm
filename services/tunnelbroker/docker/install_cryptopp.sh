975bbc29f97e771286653deb87f662fa3385e3a4
Abosh Upadhyaya <aboshu@gmail.com>
2022-08-04 12:57:50 -0400

    [services] Clean up `install_cryptopp.sh` using ShellCheck
    Summary: Related Linear issue [[ https://linear.app/comm/issue/ENG-1295/clean-up-shell-scripts-in-codebase | here ]]. This is part of a set of diffs that will allow ShellCheck to be added to the CI. See inline comments for specific details of the ShellCheck error/warning output.
    
    Test Plan:
    [[ https://www.shellcheck.net/ | ShellCheck ]]
    
    This script is run in the `tunnelbroker` Docker build, so if that CI continues to build with no errors we can reasonably assume this script continues to work as expected.
    
    Also, I ran `git grep 'CXXFLAGS'` and found no occurrences of it in the codebase.
    
    Reviewers: atul, jon, tomek, karol
    
    Reviewed By: atul, jon
    
    Subscribers: ashoat, adrian, karol
    
    Differential Revision: https://phab.comm.dev/D4738

--- services/tunnelbroker/docker/install_cryptopp.sh
+++ services/tunnelbroker/docker/install_cryptopp.sh
@@ -6,8 +6,7 @@ cd /tmp
 git clone --recurse-submodules -b CRYPTOPP_8_6_0 --single-branch https://github.com/weidai11/cryptopp
 pushd cryptopp
 mkdir build
-CXXFLAGS="-DNDEBUG -g2 -O3 -std=c++11"
-make
+make CXXFLAGS="-DNDEBUG -g2 -O3 -std=c++11"
 make libcryptopp.pc
 make install
 
