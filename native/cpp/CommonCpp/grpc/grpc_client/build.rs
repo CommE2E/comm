3aecb610154dad25f18989e1906d00d47d30e315
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2022-08-11 23:41:15 -0400

    [native] set up CXX bridge
    Summary:
    create the ffi module that will be used to generate the bridge between C++ and Rust code
    
    Depends on D4815
    
    Test Plan: `cargo build`
    
    Reviewers: tomek, karol
    
    Reviewed By: tomek, karol
    
    Subscribers: jon, ashoat, adrian, atul, abosh
    
    Differential Revision: https://phab.comm.dev/D4820

--- native/cpp/CommonCpp/grpc/grpc_client/build.rs
+++ native/cpp/CommonCpp/grpc/grpc_client/build.rs
@@ -1,4 +1,6 @@
 fn main() {
   tonic_build::compile_protos("../protos/identity.proto")
     .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
+  let _cxx_build = cxx_build::bridge("src/lib.rs");
+  println!("cargo:rerun-if-changed=src/hello.c");
 }
