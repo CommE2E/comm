8b9d7d60eb4b7c3eb3fcf16c499475b7a4c3073d
Jacek Nitychoruk <jacek.nitychoruk@swmansion.com>
2022-08-12 11:20:10 +0200

    [native] Redesign `InlineSidebar` for reactions feature
    Summary:
    Introduce redesigned `InlineSidebar` component for native. It is now able to keep both sidebar and reactions.
    Now it receives reactions as an array of strings - so it would be easy to test visually by providing an array of emojis. Probably, in the future we will keep reactions in some more complex data structure, so the types should be adjusted then.
    
    Test Plan:
    Tested the component look on both platforms in both color themes. Also provided smoji and tested displaying reactions and/or sidebar.
    The visual issues when opening message tooltip will be fixed in next diff.
    
    Reviewers: tomek, atul, abosh
    
    Subscribers: ashoat, adrian
    
    Differential Revision: https://phab.comm.dev/D4824

--- native/chat/inline-sidebar.react.js
+++ native/chat/inline-sidebar.react.js
@@ -2,69 +2,91 @@
 
 import * as React from 'react';
 import { Text, View } from 'react-native';
-import Icon from 'react-native-vector-icons/Feather';
 
 import useInlineSidebarText from 'lib/hooks/inline-sidebar-text.react';
 import type { ThreadInfo } from 'lib/types/thread-types';
 
-import Button from '../components/button.react';
+import GestureTouchableOpacity from '../components/gesture-touchable-opacity.react';
+import SWMansionIcon from '../components/swmansion-icon.react';
 import { useStyles } from '../themes/colors';
 import { inlineSidebarHeight } from './chat-constants';
 import { useNavigateToThread } from './message-list-types';
 
 type Props = {
-  +threadInfo: ThreadInfo,
-  +positioning: 'left' | 'center' | 'right',
+  +threadInfo: ?ThreadInfo,
+  +reactions?: $ReadOnlyArray<string>,
+  +disabled?: boolean,
 };
 function InlineSidebar(props: Props): React.Node {
-  const { threadInfo } = props;
-  const { sendersText, repliesText } = useInlineSidebarText(threadInfo);
+  const { disabled = false, reactions, threadInfo } = props;
+  const { repliesText } = useInlineSidebarText(threadInfo);
 
   const navigateToThread = useNavigateToThread();
   const onPress = React.useCallback(() => {
-    navigateToThread({ threadInfo });
-  }, [navigateToThread, threadInfo]);
+    if (threadInfo && !disabled) {
+      navigateToThread({ threadInfo });
+    }
+  }, [disabled, navigateToThread, threadInfo]);
 
   const styles = useStyles(unboundStyles);
-  let viewerIcon, nonViewerIcon, alignStyle;
-  if (props.positioning === 'right') {
-    viewerIcon = <Icon name="corner-down-left" size={18} style={styles.icon} />;
-    alignStyle = styles.rightAlign;
-  } else if (props.positioning === 'left') {
-    nonViewerIcon = (
-      <Icon name="corner-down-right" size={18} style={styles.icon} />
-    );
-    alignStyle = styles.leftAlign;
-  } else {
-    nonViewerIcon = (
-      <Icon name="corner-down-right" size={18} style={styles.icon} />
-    );
-    alignStyle = styles.centerAlign;
-  }
 
-  const unreadStyle = threadInfo.currentUser.unread ? styles.unread : null;
+  const reactionList = React.useMemo(() => {
+    if (!reactions || reactions.length === 0) {
+      return null;
+    }
+    const reactionItems = reactions.map((reaction, i) => {
+      return (
+        <Text key={i} style={styles.reaction}>
+          {reaction}
+        </Text>
+      );
+    });
+    return <View style={styles.reactionsContainer}>{reactionItems}</View>;
+  }, [reactions, styles.reaction, styles.reactionsContainer]);
 
-  return (
-    <View style={[styles.content, alignStyle]}>
-      <Button style={styles.sidebar} onPress={onPress}>
-        {nonViewerIcon}
-        <Text style={[styles.name, unreadStyle]}>
-          {sendersText}
+  const unreadStyle = threadInfo?.currentUser.unread ? styles.unread : null;
+  const marginRight = reactionList ? styles.repliesMarginRight : null;
+  const sidebarInfo = React.useMemo(() => {
+    if (!threadInfo) {
+      return null;
+    }
+    return (
+      <>
+        <SWMansionIcon style={styles.icon} size={14} name="sidebar-filled" />
+        <Text style={[styles.repliesText, unreadStyle, marginRight]}>
           {repliesText}
         </Text>
-        {viewerIcon}
-      </Button>
+      </>
+    );
+  }, [
+    marginRight,
+    repliesText,
+    styles.icon,
+    styles.repliesText,
+    threadInfo,
+    unreadStyle,
+  ]);
+  return (
+    <View style={styles.container}>
+      <GestureTouchableOpacity
+        onPress={onPress}
+        activeOpacity={0.7}
+        style={styles.sidebar}
+      >
+        {sidebarInfo}
+        {reactionList}
+      </GestureTouchableOpacity>
     </View>
   );
 }
 
 const unboundStyles = {
-  content: {
+  container: {
     flexDirection: 'row',
-    marginRight: 30,
-    marginLeft: 10,
-    flex: 1,
     height: inlineSidebarHeight,
+    display: 'flex',
+    backgroundColor: 'listBackground',
+    borderRadius: 16,
   },
   unread: {
     color: 'listForegroundLabel',
@@ -74,25 +96,32 @@ const unboundStyles = {
     flexDirection: 'row',
     display: 'flex',
     alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'inlineSidebarBackground',
+    padding: 8,
+    borderRadius: 16,
+    height: inlineSidebarHeight,
   },
   icon: {
-    color: 'listForegroundTertiaryLabel',
+    color: 'inlineSidebarLabel',
+    marginRight: 4,
   },
-  name: {
-    paddingTop: 1,
-    color: 'listForegroundTertiaryLabel',
-    fontSize: 16,
-    paddingLeft: 4,
-    paddingRight: 2,
+  repliesText: {
+    color: 'inlineSidebarLabel',
+    fontSize: 14,
+    lineHeight: 22,
   },
-  leftAlign: {
-    justifyContent: 'flex-start',
+  repliesMarginRight: {
+    marginRight: 12,
   },
-  rightAlign: {
-    justifyContent: 'flex-end',
+  reaction: {
+    marginLeft: 4,
+    color: 'inlineSidebarLabel',
   },
-  centerAlign: {
-    justifyContent: 'center',
+  reactionsContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    marginLeft: -4,
   },
 };
 
