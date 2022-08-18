e364a99f5260c085c065265fe4fb15659ac2a251
Karol Bisztyga <karol.bisztyga@swmansion.com>
2022-08-18 13:23:30 +0200

    [services] Backup - Rust lib - Add is initialized
    Summary: todo
    
    Test Plan: todo
    
    Reviewers:
    
    Subscribers:

--- services/backup/rust_lib/src/lib.rs
+++ services/backup/rust_lib/src/lib.rs
@@ -1,10 +1,52 @@
 #[cxx::bridge]
 mod ffi {
   extern "Rust" {
-    fn test_function() -> i32;
+    fn rust_is_initialized_cxx() -> bool;
+    fn rust_initialize_cxx() -> ();
+    unsafe fn rust_process_cxx(_: *const c_char) -> ();
+    fn rust_terminate_cxx() -> ();
   }
 }
 
-pub fn test_function() -> i32 {
-  0
+use tokio::runtime::Runtime;
+use tokio::sync::mpsc;
+use tokio::task::JoinHandle;
+use lazy_static::lazy_static;
+use std::sync::{Arc, Mutex};
+extern crate libc;
+use libc::c_char;
+
+pub struct Client {
+  tx: Option<mpsc::Sender<String>>,
+  handle: Option<JoinHandle<()>>,
+}
+
+lazy_static! {
+  pub static ref CLIENT: Arc<Mutex<Client>> = Arc::new(Mutex::new(Client {
+    tx: None,
+    handle: None
+  }));
+  pub static ref RUNTIME: Runtime = Runtime::new().unwrap();
+}
+
+pub fn rust_is_initialized_cxx() -> bool {
+  if CLIENT.lock().expect("access client").tx.is_none() {
+    return false;
+  }
+  if CLIENT.lock().expect("access client").handle.is_none() {
+    return false;
+  }
+  return true;
+}
+
+pub fn rust_initialize_cxx() -> () {
+  unimplemented!();
+}
+
+pub fn rust_process_cxx(_: *const c_char) -> () {
+  unimplemented!();
+}
+
+pub fn rust_terminate_cxx() -> () {
+  unimplemented!();
 }
