9070f9b72d5dbf39de5fe9b92e31b95ddbc1d0dd
Karol Bisztyga <karol.bisztyga@swmansion.com>
2022-08-19 14:48:37 +0200

    repro

--- services/backup/rust_lib/build.rs
+++ services/backup/rust_lib/build.rs
@@ -1,4 +1,32 @@
-fn main() {
-  let _build = cxx_build::bridge("src/lib.rs");
-  println!("cargo:rerun-if-changed=src/lib.rs");
+use std::env::current_dir;
+use std::fs;
+use std::io::{Error, ErrorKind};
+
+fn list_dir(path: &str) -> Result<(), Box<dyn std::error::Error>> {
+  println!("--- scanning path [{}]", path);
+  let proto_files = fs::read_dir(path)?;
+  for path in proto_files {
+    let path_str = path?
+      .path()
+      .file_name()
+      .ok_or(Error::new(
+        ErrorKind::Other,
+        "failed to obtain the file name",
+      ))?
+      .to_string_lossy()
+      .into_owned();
+    println!(" - {}", path_str);
+  }
+  Ok(())
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+  // let _build = cxx_build::bridge("src/lib.rs");
+  // println!("cargo:rerun-if-changed=src/lib.rs");
+  list_dir(".")?;
+  list_dir("..")?;
+  list_dir("../grpc")?;
+  list_dir("../grpc/protos")?; // file exists ../grpc/protos/blob.proto
+  tonic_build::compile_protos("../grpc/protos/blob.proto")?;
+  panic!();
 }
