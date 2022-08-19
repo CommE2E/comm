7b3572d5c29526ed21f4110d53b682b74264b299
Karol Bisztyga <karol.bisztyga@swmansion.com>
2022-08-19 15:59:58 +0200

    [services] Backup - Connect to Blob - Compile protos
    Summary: todo
    
    Test Plan: todo
    
    Subscribers: ashoat, tomek, atul, abosh
    
    Differential Revision: https://phab.comm.dev/D4889

--- services/backup/blob_client/build.rs
+++ services/backup/blob_client/build.rs
@@ -1,4 +1,24 @@
-fn main() {
+use regex::Regex;
+use std::env::current_dir;
+use std::path::MAIN_SEPARATOR;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
   let _build = cxx_build::bridge("src/lib.rs");
   println!("cargo:rerun-if-changed=src/lib.rs");
+
+  let mut proto_path = "../../../native/cpp/CommonCpp/grpc/protos";
+  let is_in_docker_regex = Regex::new(r"^/transferred.*").unwrap();
+  if is_in_docker_regex.is_match(
+    &current_dir()
+      .unwrap()
+      .into_os_string()
+      .into_string()
+      .unwrap(),
+  ) {
+    proto_path = "../grpc/protos";
+  }
+
+  tonic_build::compile_protos(format!("{}/blob.proto", proto_path))?;
+
+  Ok(())
 }
