2f2a6092241979d206ccf5e7ffca729c3ff64ea5
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2022-08-24 17:12:32 -0400

    [native] add c++11 flag to build
    Summary: this ensures that the compiler knows which edition of C++ to use.
    
    Test Plan: `cargo build`
    
    Reviewers: tomek!, karol, atul
    
    Subscribers: ashoat, abosh
    
    Differential Revision: https://phab.comm.dev/D4950

--- native/cpp/CommonCpp/grpc/grpc_client/build.rs
+++ native/cpp/CommonCpp/grpc/grpc_client/build.rs
@@ -1,6 +1,7 @@
 fn main() {
   tonic_build::compile_protos("../protos/identity.proto")
     .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
-  let _cxx_build = cxx_build::bridge("src/lib.rs");
+  let _cxx_build =
+    cxx_build::bridge("src/lib.rs").flag_if_supported("-std=c++11");
   println!("cargo:rerun-if-changed=src/hello.c");
 }
