aaf7ffa5fafe72c4d7eafa9d35d65a2f1e838d8e
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2022-08-25 18:44:31 -0400

    [native] add Rust gRPC client to XCode project
    Summary:
    This change adds the static library for the gRPC client to XCode. Now the functions can be referenced from other C++ files in the project. I had to add a new dependency, cargo lipo, to build a universal static library that works on both x86 and arm since the simulator doesn't use arm for some reason...
    
    Depends on D4951
    
    Test Plan: built the app for physical iOS devices and for the simulator
    
    Reviewers: tomek, atul, marcin, jon
    
    Reviewed By: tomek, atul, marcin, jon
    
    Subscribers: inka, jon, ashoat, abosh
    
    Differential Revision: https://phab.comm.dev/D4953

--- native/cpp/CommonCpp/grpc/grpc_client/build.rs
+++ native/cpp/CommonCpp/grpc/grpc_client/build.rs
@@ -1,5 +1,5 @@
 fn main() {
-  tonic_build::compile_protos("../protos/identity.proto")
+  tonic_build::compile_protos("../../../../../shared/protos/identity.proto")
     .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
   let _cxx_build =
     cxx_build::bridge("src/lib.rs").flag_if_supported("-std=c++11");
