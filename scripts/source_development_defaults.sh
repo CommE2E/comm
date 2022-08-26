f6a0d01ca6a08ccce03e40d7b2c8996ae9e4176f
Jonathan Ringer <jonringer117@gmail.com>
2022-08-26 15:25:56 -0700

    [Nix] Set PATH for android utilities
    Summary:
    Avoid user having to set this manually.
    
    https://github.com/CommE2E/comm/blob/master/docs/dev_environment.md#enable-android-cli-commands
    
    Test Plan:
    ```
    nix develop
    echo $PATH | grep emulator && echo "Yep, emulator is there"
    ```
    
    Reviewers: abosh, varun
    
    Reviewed By: abosh
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D4965

--- scripts/source_development_defaults.sh
+++ scripts/source_development_defaults.sh
@@ -10,3 +10,6 @@ if [[ "$OSTYPE" == 'darwin'* ]]; then
   export MYSQL_UNIX_PORT="$MARIADB_DIR"/mysql.sock
   export ANDROID_SDK_ROOT=${ANDROID_SDK_ROOT:-$HOME/Library/Android/sdk}
 fi
+
+export PATH=$PATH:$ANDROID_SDK_ROOT/emulator:$ANDROID_SDK_ROOT/tools
+export PATH=$PATH:$ANDROID_SDK_ROOT/tools/bin:$ANDROID_SDK_ROOT/platform-tools
