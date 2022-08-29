519d106c33138263cdf3312c37764db84f8d1909
Karol Bisztyga <karol.bisztyga@swmansion.com>
2022-08-29 15:57:07 +0200

    [services] Backup - Connect to Blob - Compile protos
    Summary:
    Depends on D4888
    
    I added the code that compiles proto files. We need to compile the blob proto file in order to write a client that will communicate with the service.
    
    Test Plan: backup still builds
    
    Reviewers: varun, max, tomek, ashoat, jon
    
    Reviewed By: varun, tomek, ashoat, jon
    
    Subscribers: jon, ashoat, tomek, atul, abosh
    
    Differential Revision: https://phab.comm.dev/D4889

--- services/backup/blob_client/build.rs
+++ services/backup/blob_client/build.rs
@@ -1,4 +1,24 @@
-fn main() {
+use regex::Regex;
+use std::env::current_dir;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
   let _build = cxx_build::bridge("src/lib.rs").flag_if_supported("-std=c++17");
   println!("cargo:rerun-if-changed=src/lib.rs");
+
+  let is_in_docker_regex = Regex::new(r"^/transferred.*").unwrap();
+  let proto_path = if is_in_docker_regex.is_match(
+    &current_dir()
+      .unwrap()
+      .into_os_string()
+      .into_string()
+      .unwrap(),
+  ) {
+    "../grpc/protos"
+  } else {
+    "../../../native/cpp/CommonCpp/grpc/protos"
+  };
+
+  tonic_build::compile_protos(format!("{}/blob.proto", proto_path))?;
+
+  Ok(())
 }
