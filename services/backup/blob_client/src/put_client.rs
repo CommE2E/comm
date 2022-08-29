492226420223dab879e9177a4d58b231037cd79e
Karol Bisztyga <karol.bisztyga@swmansion.com>
2022-08-29 13:17:11 +0200

    [services] Backup - Connect to Blob - Add a helper comment to put_client_write_cxx

--- services/backup/blob_client/src/put_client.rs
+++ services/backup/blob_client/src/put_client.rs
@@ -231,6 +231,12 @@ pub fn put_client_blocking_read_cxx() -> Result<String, String> {
   response.ok_or("response not received properly".to_string())
 }
 
+/**
+ * field index:
+ * 0 - holder (utf8 string)
+ * 1 - blob hash (utf8 string)
+ * 2 - data chunk (bytes)
+ */
 pub fn put_client_write_cxx(
   field_index: usize,
   data: *const c_char,
