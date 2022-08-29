dc0e687fa0e8e3ed9a6e237e1f3c0e6ca4225904
Karol Bisztyga <karol.bisztyga@swmansion.com>
2022-08-29 15:57:05 +0200

    [services] Backup - Connect to Blob - split code to diferent files
    Summary:
    Depends on D4885
    
    With this diff, I restructured previously added code so it'll be easier to understand what comes next.
    
    Test Plan: backup still builds
    
    Reviewers: varun, tomek, max, jon
    
    Reviewed By: tomek
    
    Subscribers: ashoat, tomek, atul, abosh
    
    Differential Revision: https://phab.comm.dev/D4888

---
+++ services/backup/blob_client/src/lib.rs
@@ -0,0 +1,18 @@
+mod constants;
+mod get_client;
+mod put_client;
+
+use put_client::{
+  put_client_blocking_read_cxx, put_client_initialize_cxx,
+  put_client_terminate_cxx, put_client_write_cxx,
+};
+
+#[cxx::bridge]
+mod ffi {
+  extern "Rust" {
+    fn put_client_initialize_cxx() -> ();
+    fn put_client_blocking_read_cxx() -> ();
+    unsafe fn put_client_write_cxx(data: *const c_char) -> ();
+    fn put_client_terminate_cxx() -> ();
+  }
+}
---
+++ services/backup/blob_client/src/put_client.rs
@@ -0,0 +1,17 @@
+use libc::c_char;
+
+pub fn put_client_initialize_cxx() -> () {
+  unimplemented!();
+}
+
+pub fn put_client_blocking_read_cxx() -> () {
+  unimplemented!();
+}
+
+pub fn put_client_write_cxx(data: *const c_char) -> () {
+  unimplemented!();
+}
+
+pub fn put_client_terminate_cxx() -> () {
+  unimplemented!();
+}
