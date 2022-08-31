b41c56d2498d01828bcc426e9148af49725470a8
Derek <dereknelsony0@gmail.com>
2022-08-31 13:52:39 -0400

    [landing] SIWE component with RainbowKit that connects & signs a message
    Summary:
    take two of D4510, which was reverted because of breaking css changes. re-pasting the summary here
    
    The goal: to sign in with ethereum! to do that we need a signed message from the wallet to pass to the identity service (which is currently native code, not in the browser).
    
    This diff adds a top level routing page that includes RainbowKit's Connect Wallet button, as well as a function that will pass the signed message back from the webview to native where the identity service code lives.
    
    More context for why we're making these changes in landing, why there are so many package changes, and what other options have been considered can be found in linear, issue 1212.
    
    Test Plan: run /landing and open /commlanding/siwe on a simulator webview to get a message over the bridge
    
    Reviewers: abosh, atul, ashoat, varun
    
    Subscribers: abosh, atul, adrian, tomek, ashoat, varun, jacek
    
    Differential Revision: https://phab.comm.dev/D4997

---
+++ landing/siwe.react.js
@@ -0,0 +1,96 @@
+// @flow
+
+import {
+  ConnectButton,
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import * as React from 'react';
+import { SiweMessage } from 'siwe';
+import '@rainbow-me/rainbowkit/dist/index.css';
+import {
+  useAccount,
+  useSigner,
+  chain,
+  configureChains,
+  createClient,
+  WagmiConfig,
+} from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
+
+import css from './siwe.css';
+
+const { chains, provider } = configureChains(
+  [chain.mainnet],
+  [publicProvider()],
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'comm',
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
+function createSiweMessage(address, statement) {
+  const domain = window.location.host;
+  const origin = window.location.origin;
+  const message = new SiweMessage({
+    domain,
+    address,
+    statement,
+    uri: origin,
+    version: '1',
+    chainId: '1',
+  });
+  return message.prepareMessage();
+}
+
+async function signInWithEthereum(address, signer) {
+  const message = createSiweMessage(address, 'Sign in to Comm with Ethereum');
+  const signedMessage = await signer.signMessage(message);
+  const messageToPost = JSON.stringify({ address, signer });
+  window.ReactNativeWebView?.postMessage?.(messageToPost);
+  return signedMessage;
+}
+
+function SIWE(): React.Node {
+  const { data } = useAccount();
+  const { address } = data || {};
+  const { data: signer } = useSigner();
+  const onClick = React.useCallback(() => signInWithEthereum(address, signer), [
+    address,
+    signer,
+  ]);
+  const SignInButton = !signer ? null : (
+    <div className={css.button} onClick={onClick}>
+      <img src="images/ethereum_icon.svg" style={ethIconStyle} />
+      sign in
+    </div>
+  );
+  return (
+    <div className={css.wrapper}>
+      <h1 className={css.h1}>SIWE</h1>
+      <ConnectButton />
+      <div className={css.spacer} />
+      {SignInButton}
+    </div>
+  );
+}
+function SIWEWrapper(): React.Node {
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <SIWE />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
+
+const ethIconStyle = { height: 25, paddingRight: 10 };
+
+export default SIWEWrapper;
