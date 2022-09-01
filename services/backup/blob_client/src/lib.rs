77d843164f0be77cb16c9f39ab300b225da95371
Karol Bisztyga <karol.bisztyga@swmansion.com>
2022-09-01 12:55:52 +0200

    [services] Backup - Blob Get Client - Add base structure
    Summary:
    Depends on D5012
    
    Adding base structure to the blob get client.
    
    Test Plan: backup builds
    
    Reviewers: jon, tomek, varun, max
    
    Reviewed By: tomek
    
    Subscribers: ashoat, tomek, atul, abosh
    
    Differential Revision: https://phab.comm.dev/D5013

--- services/backup/blob_client/src/lib.rs
+++ services/backup/blob_client/src/lib.rs
@@ -7,6 +7,10 @@ use put_client::{
   put_client_terminate_cxx, put_client_write_cxx,
 };
 
+use get_client::{
+  get_client_blocking_read_cxx, get_client_initialize_cxx,
+  get_client_terminate_cxx,
+};
 #[cxx::bridge]
 mod ffi {
   extern "Rust" {
@@ -17,5 +21,10 @@ mod ffi {
     ) -> Result<()>;
     fn put_client_blocking_read_cxx() -> Result<String>;
     fn put_client_terminate_cxx() -> Result<()>;
+    unsafe fn get_client_initialize_cxx(
+      holder_char: *const c_char,
+    ) -> Result<()>;
+    fn get_client_blocking_read_cxx() -> Result<Vec<u8>>;
+    fn get_client_terminate_cxx() -> Result<()>;
   }
 }
