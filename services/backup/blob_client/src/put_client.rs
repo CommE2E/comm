cb606d240c43fc73dbd783ab8f52e0d396c56e62
Karol Bisztyga <karol.bisztyga@swmansion.com>
2022-09-01 12:53:34 +0200

    [services] Backup - Blob Put Client - Add blocking read
    Summary: a
    
    Test Plan: a
    
    Reviewers: jon
    
    Subscribers: ashoat, tomek, atul, abosh
    
    Differential Revision: https://phab.comm.dev/D5010

--- services/backup/blob_client/src/put_client.rs
+++ services/backup/blob_client/src/put_client.rs
@@ -191,7 +191,28 @@ pub fn put_client_initialize_cxx() -> Result<(), String> {
 }
 
 pub fn put_client_blocking_read_cxx() -> Result<String, String> {
-  unimplemented!();
+  let mut response: Option<String> = None;
+  check_error()?;
+  RUNTIME.block_on(async {
+    if let Ok(mut maybe_client) = CLIENT.lock() {
+      if let Some(mut client) = (*maybe_client).take() {
+        if let Some(data) = client.rx.recv().await {
+          response = Some(data);
+        } else {
+          report_error(
+            "couldn't receive data via client's receiver".to_string(),
+          );
+        }
+        *maybe_client = Some(client);
+      } else {
+        report_error("no client detected".to_string());
+      }
+    } else {
+      report_error("couldn't access client".to_string());
+    }
+  });
+  check_error()?;
+  response.ok_or("response not received properly".to_string())
 }
 
 /**
