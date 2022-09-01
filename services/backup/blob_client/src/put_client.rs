990714b714a36a8c4276f2ff38cfe3770a738fd0
Karol Bisztyga <karol.bisztyga@swmansion.com>
2022-09-01 12:48:33 +0200

    [services] Backup - Blob Put Client - Update put API
    Summary: todo
    
    Test Plan: todo
    
    Reviewers: jon
    
    Subscribers: ashoat, tomek, atul, abosh
    
    Differential Revision: https://phab.comm.dev/D5004

--- services/backup/blob_client/src/put_client.rs
+++ services/backup/blob_client/src/put_client.rs
@@ -1,17 +1,26 @@
 use libc::c_char;
 
-pub fn put_client_initialize_cxx() -> () {
+pub fn put_client_initialize_cxx() -> Result<(), String> {
   unimplemented!();
 }
 
-pub fn put_client_blocking_read_cxx() -> () {
+pub fn put_client_blocking_read_cxx() -> Result<String, String> {
   unimplemented!();
 }
 
-pub fn put_client_write_cxx(data: *const c_char) -> () {
+/**
+ * field index:
+ * 0 - holder (utf8 string)
+ * 1 - blob hash (utf8 string)
+ * 2 - data chunk (bytes)
+ */
+pub fn put_client_write_cxx(
+  field_index: usize,
+  data: *const c_char,
+) -> Result<(), String> {
   unimplemented!();
 }
 
-pub fn put_client_terminate_cxx() -> () {
+pub fn put_client_terminate_cxx() -> Result<(), String> {
   unimplemented!();
 }
