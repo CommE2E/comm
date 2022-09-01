5c027e3b701d837f27b84b4490acaee6d0a738fa
Karol Bisztyga <karol.bisztyga@swmansion.com>
2022-09-01 12:44:42 +0200

    [services] Backup - Add tools for blob client
    Summary: todo
    
    Test Plan: todo
    
    Reviewers: jon
    
    Subscribers: ashoat, tomek, atul, abosh
    
    Differential Revision: https://phab.comm.dev/D5003

--- services/backup/src/Tools.h
+++ services/backup/src/Tools.h
@@ -6,6 +6,12 @@ namespace comm {
 namespace network {
 namespace tools {
 
+enum class BlobPutField {
+  HOLDER = 0,
+  HASH = 1,
+  DATA_CHUNK = 2,
+};
+
 std::string generateRandomString(std::size_t length = 20);
 
 std::string generateHolder(
@@ -14,6 +20,10 @@ std::string generateHolder(
     const std::string &resourceID = "");
 
 std::string validateAttachmentHolders(const std::string &holders);
+
+int charPtrToInt(const char *str);
+
+size_t getBlobPutField(BlobPutField field);
 
 } // namespace tools
 } // namespace network
