67ad52998e9cbfd26d3a08982c54880543a6d502
Karol Bisztyga <karol.bisztyga@swmansion.com>
2022-09-01 12:55:52 +0200

    [services] Backup - Blob Get Client - Add base structure
    Summary: a
    
    Test Plan: a
    
    Reviewers: jon
    
    Subscribers: ashoat, tomek, atul, abosh
    
    Differential Revision: https://phab.comm.dev/D5013

--- services/backup/blob_client/src/get_client.rs
+++ services/backup/blob_client/src/get_client.rs
@@ -1,1 +1,73 @@
+mod proto {
+  tonic::include_proto!("blob");
+}
 
+use proto::blob_service_client::BlobServiceClient;
+use proto::GetRequest;
+
+use crate::constants::{BLOB_ADDRESS, MPSC_CHANNEL_BUFFER_CAPACITY};
+use lazy_static::lazy_static;
+use libc;
+use libc::c_char;
+use std::ffi::CStr;
+use std::sync::{Arc, Mutex};
+use tokio::runtime::Runtime;
+use tokio::sync::mpsc;
+use tokio::task::JoinHandle;
+use tracing::error;
+
+struct ReadClient {
+  rx: mpsc::Receiver<Vec<u8>>,
+  rx_handle: JoinHandle<()>,
+}
+
+lazy_static! {
+  static ref CLIENT: Arc<Mutex<Option<ReadClient>>> =
+    Arc::new(Mutex::new(None));
+  static ref RUNTIME: Runtime = Runtime::new().unwrap();
+  static ref ERROR_MESSAGES: Arc<Mutex<Vec<String>>> =
+    Arc::new(Mutex::new(Vec::new()));
+}
+
+fn is_initialized() -> bool {
+  if let Ok(client) = CLIENT.lock() {
+    if client.is_some() {
+      return true;
+    }
+  } else {
+    report_error("couldn't access client".to_string());
+  }
+  false
+}
+
+fn report_error(message: String) {
+  println!("[RUST] [get] Error: {}", message);
+  if let Ok(mut error_messages) = ERROR_MESSAGES.lock() {
+    error_messages.push(message);
+  }
+  error!("could not access error messages");
+}
+
+fn check_error() -> Result<(), String> {
+  if let Ok(errors) = ERROR_MESSAGES.lock() {
+    return match errors.is_empty() {
+      true => Ok(()),
+      false => Err(errors.join("\n")),
+    };
+  }
+  Err("could not access error messages".to_string())
+}
+
+pub fn get_client_initialize_cxx(
+  holder_char: *const c_char,
+) -> Result<(), String> {
+  unimplemented!();
+}
+
+pub fn get_client_blocking_read_cxx() -> Result<Vec<u8>, String> {
+  unimplemented!();
+}
+
+pub fn get_client_terminate_cxx() -> Result<(), String> {
+  unimplemented!();
+}
