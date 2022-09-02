faea3ebb5e19a08382dc52bd62f904866a979ef4
marcinwasowicz <marcinkwasowicz@gmail.com>
2022-09-02 16:10:22 +0200

    Define tokio runtime Differential Revision: https://phab.comm.dev/D5053

--- native/cpp/CommonCpp/grpc/blob_client/rust/src/lib.rs
+++ native/cpp/CommonCpp/grpc/blob_client/rust/src/lib.rs
@@ -1,3 +1,18 @@
+use lazy_static::lazy_static;
+use std::sync::Arc;
+use tokio::runtime::{Builder, Runtime};
+
 pub mod blob {
   tonic::include_proto!("blob");
+}
+
+lazy_static! {
+  pub static ref RUNTIME: Arc<Runtime> = Arc::new(
+    Builder::new_multi_thread()
+      .worker_threads(1)
+      .max_blocking_threads(1)
+      .enable_all()
+      .build()
+      .unwrap()
+  );
 }
