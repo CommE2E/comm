3d91bd2b41b3d697d2c87a50290c40934f0771e4
Jacek Nitychoruk <jacek.nitychoruk@swmansion.com>
2022-09-02 12:51:25 +0200

    [web] Redesign `InlineSidebar` with reactions support
    Summary:
    Introduce redesigned InlineSidebar component.
    Currently it receives reactios as an array of strings to simplify testing with emojis, but it the future it will problably need to receive some complex rection data type.
    
    Test Plan: Tested text, media and robotext messages in all cases with/without sidebar or/and reactions.
    
    Reviewers: tomek, atul, abosh
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D4924

--- web/chat/inline-sidebar.react.js
+++ web/chat/inline-sidebar.react.js
@@ -2,61 +2,74 @@
 
 import classNames from 'classnames';
 import * as React from 'react';
-import {
-  CornerDownRight as CornerDownRightIcon,
-  CornerDownLeft as CornerDownLeftIcon,
-} from 'react-feather';
 
 import useInlineSidebarText from 'lib/hooks/inline-sidebar-text.react';
 import type { ThreadInfo } from 'lib/types/thread-types';
 
+import CommIcon from '../CommIcon.react';
 import { useOnClickThread } from '../selectors/nav-selectors';
 import css from './inline-sidebar.css';
 
 type Props = {
-  +threadInfo: ThreadInfo,
+  +threadInfo: ?ThreadInfo,
+  +reactions?: $ReadOnlyArray<string>,
   +positioning: 'left' | 'center' | 'right',
 };
 function InlineSidebar(props: Props): React.Node {
-  const { threadInfo } = props;
-  const { sendersText, repliesText } = useInlineSidebarText(threadInfo);
+  const { threadInfo, positioning, reactions } = props;
+  const inlineSidebarText = useInlineSidebarText(threadInfo);
+
+  const containerClasses = classNames([
+    css.inlineSidebarContainer,
+    {
+      [css.leftContainer]: positioning === 'left',
+      [css.centerContainer]: positioning === 'center',
+      [css.rightContainer]: positioning === 'right',
+    },
+  ]);
+
+  const reactionsList = React.useMemo(() => {
+    if (!reactions || reactions.length === 0) {
+      return null;
+    }
+    const reactionsItems = reactions.map(reaction => {
+      return (
+        <div key={reaction} className={css.reactions}>
+          {reaction}
+        </div>
+      );
+    });
+    return <div className={css.reactionsContainer}>{reactionsItems}</div>;
+  }, [reactions]);
 
   const onClick = useOnClickThread(threadInfo);
 
-  let viewerIcon, nonViewerIcon, alignStyle;
-  if (props.positioning === 'right') {
-    viewerIcon = (
-      <CornerDownLeftIcon className={css.inlineSidebarIcon} size={18} />
-    );
-    alignStyle = css.viewerMessageBoxContainer;
-  } else if (props.positioning === 'left') {
-    nonViewerIcon = (
-      <CornerDownRightIcon className={css.inlineSidebarIcon} size={18} />
-    );
-    alignStyle = css.nonViewerMessageBoxContainer;
-  } else {
-    nonViewerIcon = (
-      <CornerDownRightIcon className={css.inlineSidebarIcon} size={18} />
-    );
-    alignStyle = css.centerContainer;
-  }
+  const threadInfoExists = !!threadInfo;
 
-  const unreadStyle = threadInfo.currentUser.unread ? css.unread : null;
+  const sidebarItem = React.useMemo(() => {
+    if (!threadInfoExists || !inlineSidebarText) {
+      return null;
+    }
+
+    return (
+      <div className={css.replies}>
+        <CommIcon size={14} icon="sidebar-filled" />
+        {inlineSidebarText.repliesText}
+      </div>
+    );
+  }, [threadInfoExists, inlineSidebarText]);
 
   return (
-    <div className={classNames([css.inlineSidebarContent, alignStyle])}>
-      <div onClick={onClick} className={css.inlineSidebar}>
-        {nonViewerIcon}
-        <div className={classNames([css.inlineSidebarName, unreadStyle])}>
-          {sendersText}
-          {repliesText}
-        </div>
-        {viewerIcon}
+    <div className={containerClasses}>
+      <div
+        className={css.inlineSidebarContent}
+        onClick={threadInfoExists ? onClick : null}
+      >
+        {sidebarItem}
+        {reactionsList}
       </div>
     </div>
   );
 }
 
-const inlineSidebarHeight = 20;
-
-export { InlineSidebar, inlineSidebarHeight };
+export default InlineSidebar;
