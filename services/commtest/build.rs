0fe62495322b77d4bf2dd7e3195c2d475b641c3d
Jonathan Ringer <jonringer117@gmail.com>
2022-09-09 11:57:06 -0700

    [Services] Update commtest protobuf location
    Summary:
    Follow-up to D5046. These paths weren't referenced in any CI gate,
    so they were overlooked.
    
    Depends on D5101
    
    Test Plan:
    ```
    nix develop  # optional, if cargo + yarn is already installed
    cd services/commtest
    cargo clean && cargo build
    ```
    
    Reviewers: varun, atul, abosh
    
    Reviewed By: atul
    
    Subscribers: ashoat, tomek, karol
    
    Differential Revision: https://phab.comm.dev/D5102

--- services/commtest/build.rs
+++ services/commtest/build.rs
@@ -2,7 +2,7 @@ use std::fs;
 use std::io::{Error, ErrorKind};
 
 fn main() -> Result<(), Error> {
-  const PROTO_FILES_PATH: &str = "../../native/cpp/CommonCpp/grpc/protos/";
+  const PROTO_FILES_PATH: &str = "../../shared/protos";
   let proto_files = fs::read_dir(PROTO_FILES_PATH)?;
   for path in proto_files {
     let path_str = path?
@@ -14,8 +14,13 @@ fn main() -> Result<(), Error> {
       ))?
       .to_string_lossy()
       .into_owned();
+    println!("{}", path_str.as_str());
+    // skip CMakeLists.txt and _generated directory
+    if path_str.contains("CMakeLists.txt") || path_str.eq("_generated") {
+      continue;
+    }
     assert!(path_str.contains(".proto"));
-    tonic_build::compile_protos(format!("{}{}", PROTO_FILES_PATH, path_str))?;
+    tonic_build::compile_protos(format!("{}/{}", PROTO_FILES_PATH, path_str))?;
   }
   Ok(())
 }
