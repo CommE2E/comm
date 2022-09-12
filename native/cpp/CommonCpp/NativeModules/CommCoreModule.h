e46d3e9b0f80a0f82ac5a1b2d6cc71217ec76b59
marcinwasowicz <marcinkwasowicz@gmail.com>
2022-09-12 18:11:09 +0200

    [native] codeVersion -> 144

--- native/cpp/CommonCpp/NativeModules/CommCoreModule.h
+++ native/cpp/CommonCpp/NativeModules/CommCoreModule.h
@@ -13,7 +13,7 @@ namespace comm {
 namespace jsi = facebook::jsi;
 
 class CommCoreModule : public facebook::react::CommCoreModuleSchemaCxxSpecJSI {
-  const int codeVersion{143};
+  const int codeVersion{144};
 
   std::shared_ptr<WorkerThread> databaseThread;
   std::unique_ptr<WorkerThread> cryptoThread;
