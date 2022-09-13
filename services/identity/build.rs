9d0d1c31e451837b522364eed6ed798f29e77f43
Jonathan Ringer <jonringer117@gmail.com>
2022-09-13 10:12:16 -0700

    [Identity] Reference new proto location
    Summary:
    Update identity reference to new protobuf location.
    Also update dockerfile to have a similar structure to the repo to avoid
    issues around symlinking or other hacks to make the file present.
    
    Test Plan:
    ```
    # Outside docker
    nix develop # or install rust and protobuf 3.15 + grpc
    cd services/identity
    cargo test
    
    # Docker
    docker build . -f services/identity/Dockerfile
    ```
    
    Reviewers: varun, atul, abosh, tomek
    
    Reviewed By: atul
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D5125

--- services/identity/build.rs
+++ services/identity/build.rs
@@ -1,4 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-  tonic_build::compile_protos("protos/identity.proto")?;
+  tonic_build::compile_protos("../../shared/protos/identity.proto")?;
   Ok(())
 }
