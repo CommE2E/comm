2449e426c4771ebe694712f57b924540f144149e
Ginsu Eddy <ginsu.eddy@gmail.com>
2022-09-15 11:57:44 +0900

    [web] implemented darker thread color tint to blockquote container
    Test Plan:
    Please view the screenshots to see the before and after of the changes I made
    
    Before:
    {F169707}
    
    After:
    Add friend and block case:
    {F169708}
    
    Withdraw friend request case:
    {F169709}
    
    Accept and reject friend request case:
    {F169710}
    
    unfriend case:
    {F169711}
    
    unblock case:
    {F169712}
    
    Reviewers: atul, abosh
    
    Reviewed By: atul
    
    Subscribers: ashoat, tomek, atul, abosh
    
    Differential Revision: https://phab.comm.dev/D5140

--- web/markdown/rules.react.js
+++ web/markdown/rules.react.js
@@ -3,7 +3,9 @@
 import _memoize from 'lodash/memoize';
 import * as React from 'react';
 import * as SimpleMarkdown from 'simple-markdown';
+import tinycolor from 'tinycolor2';
 
+import { threadInfoSelector } from 'lib/selectors/thread-selectors';
 import { relativeMemberInfoSelectorForMembersOfThread } from 'lib/selectors/user-selectors';
 import * as SharedMarkdown from 'lib/shared/markdown';
 import type { RelativeMemberInfo } from 'lib/types/thread-types';
@@ -65,76 +67,115 @@ const linkRules: boolean => MarkdownRules = _memoize(useDarkStyle => {
   };
 });
 
-const markdownRules: boolean => MarkdownRules = _memoize(useDarkStyle => {
-  const linkMarkdownRules = linkRules(useDarkStyle);
+const markdownRules: (
+  useDarkStyle: boolean,
+  threadColor: ?string,
+) => MarkdownRules = _memoize(
+  (useDarkStyle, threadColor) => {
+    const linkMarkdownRules = linkRules(useDarkStyle);
 
-  const simpleMarkdownRules = {
-    ...linkMarkdownRules.simpleMarkdownRules,
-    autolink: SimpleMarkdown.defaultRules.autolink,
-    link: {
-      ...linkMarkdownRules.simpleMarkdownRules.link,
-      match: SimpleMarkdown.defaultRules.link.match,
-    },
-    blockQuote: {
-      ...SimpleMarkdown.defaultRules.blockQuote,
-      // match end of blockQuote by either \n\n or end of string
-      match: SharedMarkdown.matchBlockQuote(SharedMarkdown.blockQuoteRegex),
-      parse: SharedMarkdown.parseBlockQuote,
-    },
-    inlineCode: SimpleMarkdown.defaultRules.inlineCode,
-    em: SimpleMarkdown.defaultRules.em,
-    strong: SimpleMarkdown.defaultRules.strong,
-    del: SimpleMarkdown.defaultRules.del,
-    u: SimpleMarkdown.defaultRules.u,
-    heading: {
-      ...SimpleMarkdown.defaultRules.heading,
-      match: SimpleMarkdown.blockRegex(SharedMarkdown.headingRegex),
-    },
-    mailto: SimpleMarkdown.defaultRules.mailto,
-    codeBlock: {
-      ...SimpleMarkdown.defaultRules.codeBlock,
-      match: SimpleMarkdown.blockRegex(SharedMarkdown.codeBlockRegex),
-      parse: (capture: SharedMarkdown.Capture) => ({
-        content: capture[0].replace(/^ {4}/gm, ''),
-      }),
-    },
-    fence: {
-      ...SimpleMarkdown.defaultRules.fence,
-      match: SimpleMarkdown.blockRegex(SharedMarkdown.fenceRegex),
-      parse: (capture: SharedMarkdown.Capture) => ({
-        type: 'codeBlock',
-        content: capture[2],
-      }),
-    },
-    json: {
-      order: SimpleMarkdown.defaultRules.paragraph.order - 1,
-      match: (source: string, state: SharedMarkdown.State) => {
-        if (state.inline) {
-          return null;
-        }
-        return SharedMarkdown.jsonMatch(source);
+    const simpleMarkdownRules = {
+      ...linkMarkdownRules.simpleMarkdownRules,
+      autolink: SimpleMarkdown.defaultRules.autolink,
+      link: {
+        ...linkMarkdownRules.simpleMarkdownRules.link,
+        match: SimpleMarkdown.defaultRules.link.match,
       },
-      parse: (capture: SharedMarkdown.Capture) => {
-        const jsonCapture: SharedMarkdown.JSONCapture = (capture: any);
-        return {
+      blockQuote: {
+        ...SimpleMarkdown.defaultRules.blockQuote,
+        // match end of blockQuote by either \n\n or end of string
+        match: SharedMarkdown.matchBlockQuote(SharedMarkdown.blockQuoteRegex),
+        parse: SharedMarkdown.parseBlockQuote,
+        // eslint-disable-next-line react/display-name
+        react: (
+          node: SharedMarkdown.SingleASTNode,
+          output: SharedMarkdown.Output<SharedMarkdown.ReactElement>,
+          state: SharedMarkdown.State,
+        ) => {
+          let defaultTextMessageBackgroundColor;
+          if (typeof window !== 'undefined') {
+            const style = window.getComputedStyle(document.body);
+            defaultTextMessageBackgroundColor = style.getPropertyValue(
+              '--text-message-default-background',
+            );
+          }
+          const backgroundColor = threadColor
+            ? tinycolor(threadColor).darken(20).toString()
+            : tinycolor(defaultTextMessageBackgroundColor)
+                .lighten(40)
+                .toString();
+          const borderLeftColor = threadColor
+            ? tinycolor(threadColor).darken(30).toString()
+            : tinycolor(defaultTextMessageBackgroundColor)
+                .lighten(20)
+                .toString();
+
+          return (
+            <blockquote
+              key={state.key}
+              style={{ backgroundColor, borderLeftColor }}
+            >
+              {output(node.content, state)}
+            </blockquote>
+          );
+        },
+      },
+      inlineCode: SimpleMarkdown.defaultRules.inlineCode,
+      em: SimpleMarkdown.defaultRules.em,
+      strong: SimpleMarkdown.defaultRules.strong,
+      del: SimpleMarkdown.defaultRules.del,
+      u: SimpleMarkdown.defaultRules.u,
+      heading: {
+        ...SimpleMarkdown.defaultRules.heading,
+        match: SimpleMarkdown.blockRegex(SharedMarkdown.headingRegex),
+      },
+      mailto: SimpleMarkdown.defaultRules.mailto,
+      codeBlock: {
+        ...SimpleMarkdown.defaultRules.codeBlock,
+        match: SimpleMarkdown.blockRegex(SharedMarkdown.codeBlockRegex),
+        parse: (capture: SharedMarkdown.Capture) => ({
+          content: capture[0].replace(/^ {4}/gm, ''),
+        }),
+      },
+      fence: {
+        ...SimpleMarkdown.defaultRules.fence,
+        match: SimpleMarkdown.blockRegex(SharedMarkdown.fenceRegex),
+        parse: (capture: SharedMarkdown.Capture) => ({
           type: 'codeBlock',
-          content: SharedMarkdown.jsonPrint(jsonCapture),
-        };
+          content: capture[2],
+        }),
       },
-    },
-    list: {
-      ...SimpleMarkdown.defaultRules.list,
-      match: SharedMarkdown.matchList,
-      parse: SharedMarkdown.parseList,
-    },
-    escape: SimpleMarkdown.defaultRules.escape,
-  };
-  return {
-    ...linkMarkdownRules,
-    simpleMarkdownRules,
-    useDarkStyle,
-  };
-});
+      json: {
+        order: SimpleMarkdown.defaultRules.paragraph.order - 1,
+        match: (source: string, state: SharedMarkdown.State) => {
+          if (state.inline) {
+            return null;
+          }
+          return SharedMarkdown.jsonMatch(source);
+        },
+        parse: (capture: SharedMarkdown.Capture) => {
+          const jsonCapture: SharedMarkdown.JSONCapture = (capture: any);
+          return {
+            type: 'codeBlock',
+            content: SharedMarkdown.jsonPrint(jsonCapture),
+          };
+        },
+      },
+      list: {
+        ...SimpleMarkdown.defaultRules.list,
+        match: SharedMarkdown.matchList,
+        parse: SharedMarkdown.parseList,
+      },
+      escape: SimpleMarkdown.defaultRules.escape,
+    };
+    return {
+      ...linkMarkdownRules,
+      simpleMarkdownRules,
+      useDarkStyle,
+    };
+  },
+  (...args) => JSON.stringify(args),
+);
 
 function useTextMessageRulesFunc(
   threadID: ?string,
@@ -142,21 +183,31 @@ function useTextMessageRulesFunc(
   const threadMembers = useSelector(
     relativeMemberInfoSelectorForMembersOfThread(threadID),
   );
+
+  const threadColor = useSelector(state => {
+    if (!threadID) {
+      return undefined;
+    }
+    const threadInfo = threadInfoSelector(state)[threadID];
+    return threadInfo ? threadInfo.color : null;
+  });
+
   return React.useMemo(() => {
     if (!threadMembers) {
       return undefined;
     }
     return _memoize<[boolean], MarkdownRules>((useDarkStyle: boolean) =>
-      textMessageRules(threadMembers, useDarkStyle),
+      textMessageRules(threadMembers, useDarkStyle, threadColor),
     );
-  }, [threadMembers]);
+  }, [threadMembers, threadColor]);
 }
 
 function textMessageRules(
   members: $ReadOnlyArray<RelativeMemberInfo>,
   useDarkStyle: boolean,
+  threadColor: ?string,
 ): MarkdownRules {
-  const baseRules = markdownRules(useDarkStyle);
+  const baseRules = markdownRules(useDarkStyle, threadColor);
   return {
     ...baseRules,
     simpleMarkdownRules: {
@@ -182,7 +233,7 @@ let defaultTextMessageRules = null;
 
 function getDefaultTextMessageRules(): MarkdownRules {
   if (!defaultTextMessageRules) {
-    defaultTextMessageRules = textMessageRules([], false);
+    defaultTextMessageRules = textMessageRules([], false, null);
   }
   return defaultTextMessageRules;
 }
