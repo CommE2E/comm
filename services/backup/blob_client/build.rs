6cd442767371e5082a38584dbefd7eb73305de6a
Jonathan Ringer <jonringer117@gmail.com>
2022-09-15 16:12:09 -0700

    [Backup] Remove docker context differences
    Summary:
    Make the docker context look like the repo structure for backup,
    then update all assumptions.
    
    Re-ordered rust installation to avoid changes to services directories causing rust to re-install
    
    https://linear.app/comm/issue/ENG-1809
    
    Test Plan:
    ```
    # In repo
    nix develop
    cd services/backup
    rm -rf build && mkdir build && cd build && cmake .. && make -j
    
    # In docker
    docker build . -f services/backup/Dockerfile
    ```
    
    backup CI gate should also suffice for docker workflow
    
    Reviewers: atul, varun, abosh, karol, max
    
    Reviewed By: max
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D5156

--- services/backup/blob_client/build.rs
+++ services/backup/blob_client/build.rs
@@ -1,24 +1,10 @@
-use regex::Regex;
-use std::env::current_dir;
+const PROTO_DIR: &'static str = "../../../shared/protos";
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<(), std::io::Error> {
   let _build = cxx_build::bridge("src/lib.rs").flag_if_supported("-std=c++17");
   println!("cargo:rerun-if-changed=src/lib.rs");
 
-  let is_in_docker_regex = Regex::new(r"^/transferred.*").unwrap();
-  let proto_path = if is_in_docker_regex.is_match(
-    &current_dir()
-      .unwrap()
-      .into_os_string()
-      .into_string()
-      .unwrap(),
-  ) {
-    "../grpc"
-  } else {
-    "../../../shared/protos"
-  };
-
-  tonic_build::compile_protos(format!("{}/blob.proto", proto_path))?;
+  tonic_build::compile_protos(format!("{}/blob.proto", PROTO_DIR))?;
 
   Ok(())
 }
