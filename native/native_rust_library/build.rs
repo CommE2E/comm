74cf174852c71240708224acc784f2bfcf38207a
marcinwasowicz <marcinkwasowicz@gmail.com>
2022-09-19 12:10:38 +0200

    Expose module with generated tonic blob client code Differential Revision: https://phab.comm.dev/D5052

--- native/native_rust_library/build.rs
+++ native/native_rust_library/build.rs
@@ -1,6 +1,8 @@
 fn main() {
   tonic_build::compile_protos("../../shared/protos/identity.proto")
-    .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
+    .unwrap_or_else(|e| panic!("Failed to compile identity protos {:?}", e));
+  tonic_build::compile_protos("../../shared/protos/blob.proto")
+    .unwrap_or_else(|e| panic!("Failed to compile blob protos {:?}", e));
   let _cxx_build =
     cxx_build::bridge("src/lib.rs").flag_if_supported("-std=c++14");
   println!("cargo:rerun-if-changed=src/hello.c");
