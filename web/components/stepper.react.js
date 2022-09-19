c9c012b417b620d386aa64b284558797c13433b5
Jakub Kędra <jakub.kedra@swmansion.com>
2022-09-19 13:05:45 +0200

    [web] Introduce `Stepper` - common multiple step component
    Summary:
    Depends on D5185
    Context: [[ https://linear.app/comm/issue/ENG-1768/create-common-multi-step-component | here ]]
    
    Introduce common `Stepper` component, for creating multiple step forms and (in the future) multiple page modals.
    
    The component receives steps, information about active step and steps setter from parent - it doesn't keep the state with current active step itself.
    
    For better code cleanliness, to set activeStep, we use children `key` property instead of using array indexes.
    {F175163}
    
    Test Plan: This component will be used in next diff (firstly in `ComposeSubchannelModal`). Currently, no changes in the app.
    
    Reviewers: tomek, atul
    
    Reviewed By: tomek
    
    Subscribers: ashoat, tomek, atul, abosh
    
    Differential Revision: https://phab.comm.dev/D5186

---
+++ web/components/stepper.react.js
@@ -0,0 +1,108 @@
+// @flow
+
+import classnames from 'classnames';
+import * as React from 'react';
+
+import LoadingIndicator from '../loading-indicator.react';
+import Button from './button.react';
+import css from './stepper.css';
+
+export type ButtonProps = {
+  +content: React.Node,
+  +disabled?: boolean,
+  +loading?: boolean,
+  +onClick: () => void,
+};
+
+type ButtonType = 'prev' | 'next';
+
+type ActionButtonProps = {
+  +buttonProps: ButtonProps,
+  +type: ButtonType,
+};
+
+function ActionButton(props: ActionButtonProps) {
+  const { buttonProps, type } = props;
+  const { content, loading, disabled, onClick } = buttonProps;
+
+  const buttonContent = loading ? (
+    <>
+      <div className={css.hide}>{content}</div>
+      <LoadingIndicator status="loading" />
+    </>
+  ) : (
+    content
+  );
+
+  return (
+    <Button
+      className={css.button}
+      variant={type === 'prev' ? 'secondary' : 'primary'}
+      disabled={disabled || loading}
+      onClick={onClick}
+    >
+      <div className={css.buttonContainer}>{buttonContent}</div>
+    </Button>
+  );
+}
+
+type ItemProps = {
+  +content: React.Node,
+  +name: string,
+  +errorMessage?: string,
+  +prevProps?: ButtonProps,
+  +nextProps?: ButtonProps,
+};
+
+function StepperItem(props: ItemProps): React.Node {
+  const { content, errorMessage, prevProps, nextProps } = props;
+
+  const prevButton = React.useMemo(
+    () =>
+      prevProps ? <ActionButton buttonProps={prevProps} type="prev" /> : null,
+    [prevProps],
+  );
+
+  const nextButton = React.useMemo(
+    () =>
+      nextProps ? <ActionButton buttonProps={nextProps} type="next" /> : null,
+    [nextProps],
+  );
+
+  return (
+    <>
+      <div className={css.stepperItem}>{content}</div>
+      <div className={css.errorMessage}>{errorMessage}</div>
+      <div className={css.stepperFooter}>
+        {prevButton}
+        {nextButton}
+      </div>
+    </>
+  );
+}
+
+type ContainerProps = {
+  +activeStep: string,
+  +className?: string,
+  +children: React.ChildrenArray<React.Element<typeof StepperItem>>,
+};
+
+function StepperContainer(props: ContainerProps): React.Node {
+  const { children, activeStep, className = '' } = props;
+
+  const index = new Map(
+    React.Children.toArray(children).map(child => [child.props.name, child]),
+  );
+
+  const activeComponent = index.get(activeStep);
+  const styles = classnames(css.stepperContainer, className);
+
+  return <div className={styles}>{activeComponent}</div>;
+}
+
+const Stepper = {
+  Container: StepperContainer,
+  Item: StepperItem,
+};
+
+export default Stepper;
