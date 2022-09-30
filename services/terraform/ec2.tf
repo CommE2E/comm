eda0be828c59eacdcb9bbb46ef46e3355f576063
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2022-09-30 15:58:43 +0100

    [services] Provision EC2 Instances for remaining services with Terraform
    Summary:
    Essentially the same as the previous diff in the stack, but for the other three services.
    
    Depends on D5269
    
    Test Plan: `terraform validate && terraform plan`
    
    Reviewers: O2!
    
    Subscribers:

--- services/terraform/ec2.tf
+++ services/terraform/ec2.tf
@@ -5,3 +5,27 @@ resource "aws_instance" "identity_service" {
     Name = "identity_service"
   }
 }
+
+resource "aws_instance" "blob_service" {
+  ami           = "ami-0f924dc71d44d23e2"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "blob_service"
+  }
+}
+
+resource "aws_instance" "backup_service" {
+  ami           = "ami-0f924dc71d44d23e2"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "backup_service"
+  }
+}
+
+resource "aws_instance" "tunnelbroker_service" {
+  ami           = "ami-0f924dc71d44d23e2"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "tunnelbroker_service"
+  }
+}
