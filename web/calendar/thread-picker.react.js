d6fb44cfa3465de6b0d8c4e5d2f0d2c731449c9d
Ginsu Eddy <ginsu.eddy@gmail.com>
2022-10-03 15:16:14 -0400

    [web] refactored ThreadPicker from class based to functional
    Summary:
    refactored the `ThreadPicker` component to be a functional component now. The purpose of this was to optimize the searching and filtering of threads through the `useMemo` hook. Again there is no UI for this diff due to `Search` breaking the app since the focusing/blurring of the picker component prevents input in `Search`
    
    ---
    
    [[ https://linear.app/comm/issue/ENG-1914/refactor-threadpicker-from-a-class-based-component-to-a-functional | ENG-1914 ]]
    
    Depends on D5239
    
    Test Plan:
    To test the search feature, please modify the return function:
    
    ```
      return (
        <div
          className={css.container}
          tabIndex="0"
          ref={pickerDivRef}
          // onBlur={closePicker}
          onKeyDown={onPickerKeyDown}
          // onMouseDown={onMouseDown}
        >
          <Search
            searchText={searchText}
            onChangeText={onChangeSearchText}
            placeholder="Search chats"
          />
          {options}
        </div>
      );
    ```
    
    You will also need to import the `Search` component from `../components/search.react` at the top of the file
    
    Once you have the `Search` component being returned you can add a new entry in the calendar and test that the search feature is working as expected
    
    Reviewers: atul, rohan, abosh, tomek
    
    Reviewed By: atul
    
    Subscribers: ashoat, tomek, atul, abosh
    
    Differential Revision: https://phab.comm.dev/D5289

--- web/calendar/thread-picker.react.js
+++ web/calendar/thread-picker.react.js
@@ -6,7 +6,6 @@ import { createSelector } from 'reselect';
 
 import { threadSearchIndex } from 'lib/selectors/nav-selectors';
 import { onScreenEntryEditableThreadInfos } from 'lib/selectors/thread-selectors';
-import SearchIndex from 'lib/shared/search-index';
 import type { ThreadInfo } from 'lib/types/thread-types';
 
 import { useSelector } from '../redux/redux-utils';
@@ -35,126 +34,111 @@ function ThreadPickerOption(props: OptionProps) {
   );
 }
 
-type BaseProps = {
+type Props = {
   +createNewEntry: (threadID: string) => void,
   +closePicker: () => void,
 };
-type Props = {
-  ...BaseProps,
-  +onScreenThreadInfos: $ReadOnlyArray<ThreadInfo>,
-  +searchIndex: SearchIndex,
-};
-type State = {
-  +searchText: string,
-  +searchResults: Set<string>,
-};
-type PropsAndState = { ...Props, ...State };
 
-class ThreadPicker extends React.PureComponent<Props, State> {
-  pickerDiv: ?HTMLDivElement;
+function ThreadPicker(props: Props): React.Node {
+  const { closePicker, createNewEntry } = props;
 
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      searchText: '',
-      searchResults: new Set(),
-    };
-    invariant(
-      props.onScreenThreadInfos.length > 0,
-      "ThreadPicker can't be open when onScreenThreadInfos is empty",
-    );
-  }
+  const onScreenThreadInfos = useSelector(onScreenEntryEditableThreadInfos);
+  const searchIndex = useSelector(state => threadSearchIndex(state));
 
-  componentDidMount() {
-    invariant(this.pickerDiv, 'pickerDiv ref unset');
-    this.pickerDiv.focus();
-  }
+  invariant(
+    onScreenThreadInfos.length > 0,
+    "ThreadPicker can't be open when onScreenThreadInfos is empty",
+  );
 
-  listDataSelector = createSelector(
-    (propsAndState: PropsAndState) => propsAndState.onScreenThreadInfos,
-    (propsAndState: PropsAndState) => propsAndState.searchText,
-    (propsAndState: PropsAndState) => propsAndState.searchResults,
+  const pickerDivRef = React.useRef<?HTMLDivElement>(null);
+
+  React.useLayoutEffect(() => {
+    invariant(pickerDivRef, 'pickerDivRef must be set');
+    const { current } = pickerDivRef;
+    current?.focus();
+  }, []);
+
+  const [searchText, setSearchText] = React.useState<string>('');
+  const [searchResults, setSearchResults] = React.useState<Set<string>>(
+    new Set(),
+  );
+
+  const onPickerKeyDown = React.useCallback(
+    (event: SyntheticKeyboardEvent<HTMLDivElement>) => {
+      if (event.keyCode === 27) {
+        // esc
+        closePicker();
+      }
+    },
+    [closePicker],
+  );
+
+  const onMouseDown = React.useCallback(
+    (event: SyntheticEvent<HTMLDivElement>) => {
+      const target = htmlTargetFromEvent(event);
+      invariant(pickerDivRef, 'pickerDivRef must be set');
+      if (pickerDivRef.current?.contains(target)) {
+        // This prevents onBlur from firing
+        event.preventDefault();
+      }
+    },
+    [],
+  );
+
+  // eslint-disable-next-line no-unused-vars
+  const onChangeSearchText = React.useCallback(
+    (text: string) => {
+      const results = searchIndex.getSearchResults(text);
+      setSearchText(text);
+      setSearchResults(new Set(results));
+    },
+    [searchIndex],
+  );
+
+  const listDataSelector = createSelector(
+    state => state.onScreenThreadInfos,
+    state => state.searchText,
+    state => state.searchResults,
     (
       threadInfos: $ReadOnlyArray<ThreadInfo>,
       text: string,
-      searchResults: Set<string>,
+      results: Set<string>,
     ) =>
       text
-        ? threadInfos.filter(threadInfo => searchResults.has(threadInfo.id))
+        ? threadInfos.filter(threadInfo => results.has(threadInfo.id))
         : [...threadInfos],
   );
 
-  get getListData() {
-    return this.listDataSelector({ ...this.props, ...this.state });
-  }
+  const threads = useSelector(() =>
+    listDataSelector({
+      onScreenThreadInfos,
+      searchText,
+      searchResults,
+    }),
+  );
 
-  render() {
-    const length = this.props.onScreenThreadInfos.length;
-    invariant(
-      length > 0,
-      "ThreadPicker can't be open when onScreenThreadInfos is empty",
-    );
-
-    const options = this.getListData.map(threadInfo => (
+  const options = React.useMemo(() => {
+    return threads.map(threadInfo => (
       <ThreadPickerOption
         threadInfo={threadInfo}
-        createNewEntry={this.props.createNewEntry}
+        createNewEntry={createNewEntry}
         key={threadInfo.id}
       />
     ));
+  }, [threads, createNewEntry]);
 
-    return (
-      <div
-        className={css.container}
-        tabIndex="0"
-        onBlur={this.props.closePicker}
-        onKeyDown={this.onPickerKeyDown}
-        onMouseDown={this.onMouseDown}
-        ref={this.pickerDivRef}
-      >
-        {options}
-      </div>
-    );
-  }
-
-  pickerDivRef = (pickerDiv: ?HTMLDivElement) => {
-    this.pickerDiv = pickerDiv;
-  };
-
-  onPickerKeyDown = (event: SyntheticKeyboardEvent<HTMLDivElement>) => {
-    if (event.keyCode === 27) {
-      // Esc
-      this.props.closePicker();
-    }
-  };
-
-  onMouseDown = (event: SyntheticEvent<HTMLDivElement>) => {
-    const target = htmlTargetFromEvent(event);
-    invariant(this.pickerDiv, 'pickerDiv ref not set');
-    if (this.pickerDiv.contains(target)) {
-      // This prevents onBlur from firing
-      event.preventDefault();
-    }
-  };
-
-  onChangeSearchText = (searchText: string) => {
-    const results = this.props.searchIndex.getSearchResults(searchText);
-    this.setState({ searchText, searchResults: new Set(results) });
-  };
+  return (
+    <div
+      className={css.container}
+      tabIndex="0"
+      ref={pickerDivRef}
+      onBlur={closePicker}
+      onKeyDown={onPickerKeyDown}
+      onMouseDown={onMouseDown}
+    >
+      {options}
+    </div>
+  );
 }
 
-const ConnectedThreadPicker: React.ComponentType<BaseProps> = React.memo<BaseProps>(
-  function ConnectedThreadPicker(props) {
-    const onScreenThreadInfos = useSelector(onScreenEntryEditableThreadInfos);
-    const index = useSelector(state => threadSearchIndex(state));
-    return (
-      <ThreadPicker
-        {...props}
-        onScreenThreadInfos={onScreenThreadInfos}
-        searchIndex={index}
-      />
-    );
-  },
-);
-
-export default ConnectedThreadPicker;
+export default ThreadPicker;
