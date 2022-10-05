c97b93fa6a1f2e17273a71d51599337cc2641f2d
Inka Sokołowska <inka.sokolowska@swmansion.com>
2022-10-05 10:53:57 +0200

    Add generating device ID rust code to our general native rust library
    Summary:
    The code from D4754 had to be moved to our general rust library. As suggested in comments on D4754, the enum has been renamed to DeviceType.
    
    Test Plan:
    In native/cpp/CommoonCpp/NativeModules/CommCoreModule.cpp add `#include "../../../native_rust_library/lib.rs.h"` and in the same file in function `getAllMessages` add `printf("%s\n",
    generate_device_id(DeviceType::KEYSERVER));` and see that after launching ios from XCode a string subjet to "^(ks|mobile|web):[a-zA-Z0-9]{64}$" is logged to the XCode console. I tested this launching
    a phisical device as for some reason I cannot launch the simulator from XCode.
    
    Reviewers:
    marcin, varun,
    
    Subscribers:

--- native/native_rust_library/src/lib.rs
+++ native/native_rust_library/src/lib.rs
@@ -4,12 +4,14 @@ use tokio::runtime::{Builder, Runtime};
 use tonic::{transport::Channel, Status};
 use tracing::instrument;
 
+mod crypto_tools;
 mod identity_client;
 mod opaque;
 mod identity {
   tonic::include_proto!("identity");
 }
 
+pub use crypto_tools::generate_device_id;
 use identity::identity_service_client::IdentityServiceClient;
 use identity_client::{
   get_user_id, login_user_pake, login_user_wallet, register_user,
@@ -31,6 +33,13 @@ lazy_static! {
 
 #[cxx::bridge]
 mod ffi {
+
+  enum DeviceType {
+    KEYSERVER,
+    WEB,
+    MOBILE,
+  }
+
   extern "Rust" {
     type Client;
     fn initialize_client() -> Box<Client>;
@@ -68,6 +77,7 @@ mod ffi {
       siwe_signature: Vec<u8>,
       user_public_key: String,
     ) -> Result<String>;
+    fn generate_device_id(device_type: DeviceType) -> Result<String>;
   }
 }
 
