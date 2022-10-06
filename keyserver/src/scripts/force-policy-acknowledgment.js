941e2fefcbefeaf8fdf6673da91f4491b0918e8c
xsanm <kkurowski.kk@gmail.com>
2022-10-06 13:20:57 +0200

    [keyserver] add script to force users to acknowledge updated policy
    Summary: TODO
    
    Test Plan: TODO
    
    Reviewers: tomek
    
    Subscribers: ashoat, atul, abosh
    
    Differential Revision: https://phab.comm.dev/D5310

---
+++ keyserver/src/scripts/force-policy-acknowledgment.js
@@ -0,0 +1,19 @@
+// @flow
+
+import { policyTypes } from 'lib/facts/policies.js';
+
+import { dbQuery, SQL } from '../database/database.js';
+import { main } from './utils.js';
+
+// time when policy was officially published for users
+const policyUpdateTime = new Date('2022-10-30T00:00:00').getTime();
+
+async function forcePolicyAcknowledgment() {
+  await dbQuery(SQL`
+    UPDATE policy_acknowledgments
+      SET confirmed = 0
+      WHERE date <= ${policyUpdateTime} AND policy = ${policyTypes.tosAndPrivacyPolicy};
+  `);
+}
+
+main([forcePolicyAcknowledgment]);
