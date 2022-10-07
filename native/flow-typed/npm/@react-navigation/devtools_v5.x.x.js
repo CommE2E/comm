fe6624bec8cd5ab5ea963ac1f1d3a0b2aa59836a
Ashoat Tevosyan <ashoat@comm.app>
2022-10-07 13:39:11 -0400

    [native] [7/34] ReactNav6: add Group component
    Summary:
    Context on change [here](https://reactnavigation.org/docs/upgrading-from-5.x/#new-group-component).
    
    Depends on D5399
    
    Test Plan: Flow + ran the app on my phone and played around with it
    
    Reviewers: atul, tomek, bartek
    
    Reviewed By: atul
    
    Subscribers: abosh
    
    Differential Revision: https://phab.comm.dev/D5400

--- native/flow-typed/npm/@react-navigation/devtools_v5.x.x.js
+++ native/flow-typed/npm/@react-navigation/devtools_v5.x.x.js
@@ -1078,6 +1078,11 @@ declare module '@react-navigation/devtools' {
       ...$Exact<ExtraNavigatorProps>,
       ...ScreenOptionsProp<ScreenOptions, NavProp>,
     |}>,
+    +Group: React$ComponentType<{|
+      ...ScreenOptionsProp<ScreenOptions, NavProp>,
+      +children: React$Node,
+      +navigationKey?: string,
+    |}>,
   |};
 
   declare export type CreateNavigatorFactory = <
