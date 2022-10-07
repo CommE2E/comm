c58c2c7ace998f6be306dba860b8841fb5f54785
Ginsu Eddy <ginsu.eddy@gmail.com>
2022-10-07 14:00:53 -0400

    [web] introduced thread-picker-modal.react
    Summary:
    introduced `thread-picker-modal.react` component. This diff just introduces the component, but it is not used anywhere at the moment
    
    Next steps:
    
      # Implement `ThreadPickerModal` component
      # Clean up/get rid of old `ThreadPicker` component
    
    ---
    
    Linear Issue: [[ https://linear.app/comm/issue/ENG-1848/implement-modal-design | ENG-1848 ]]
    
    Design: [[ https://linear.app/comm/issue/ENG-1846/design-modal-ui-on-figma | ENG-1778 ]]
    
    Test Plan:
    Please see the video to see how the adding calendar event interaction now works with this new component.
    
    {F193659}
    
    To test interaction on local machine:
    
    Replace `this.setState({ pickerOpen: true });` on line 213 of `web/calendar/day.react.js`
    ```
          this.props.pushModal(
            <ThreadPickerModal
              name="Chats"
              onClose={this.props.popModal}
              createNewEntry={this.createNewEntry}
            />,
          );
    ```
    
    import ThreadPickerModal with `import ThreadPickerModal from '../modals/threads/thread-picker-modal.react';`
    
    And add the `popModal` function by adding this code in the `ConnectedDay` component and `Day` component props:
    
    {F193665}
    
    {F193666}
    
    Reviewers: atul, abosh, rohan
    
    Reviewed By: atul
    
    Subscribers: ashoat, tomek, atul, abosh
    
    Differential Revision: https://phab.comm.dev/D5317

---
+++ web/modals/threads/thread-picker-modal.react.js
@@ -0,0 +1,134 @@
+// @flow
+
+import invariant from 'invariant';
+import * as React from 'react';
+import { createSelector } from 'reselect';
+
+import { threadSearchIndex } from 'lib/selectors/nav-selectors';
+import { onScreenEntryEditableThreadInfos } from 'lib/selectors/thread-selectors';
+import type { ThreadInfo } from 'lib/types/thread-types';
+
+import Search from '../../components/search.react';
+import { useSelector } from '../../redux/redux-utils';
+import Modal, { type ModalOverridableProps } from '../modal.react';
+import css from './thread-picker-modal.css';
+
+type OptionProps = {
+  +threadInfo: ThreadInfo,
+  +createNewEntry: (threadID: string) => void,
+  +onCloseModal: () => void,
+};
+
+function ThreadPickerOption(props: OptionProps) {
+  const { threadInfo, createNewEntry, onCloseModal } = props;
+
+  const onClickThreadOption = React.useCallback(() => {
+    createNewEntry(threadInfo.id);
+    onCloseModal();
+  }, [threadInfo.id, createNewEntry, onCloseModal]);
+
+  const splotchColorStyle = React.useMemo(
+    () => ({
+      backgroundColor: `#${threadInfo.color}`,
+    }),
+    [threadInfo.color],
+  );
+
+  return (
+    <div
+      key={threadInfo.id}
+      className={css.threadPickerOptionContainer}
+      onClick={onClickThreadOption}
+    >
+      <div style={splotchColorStyle} className={css.threadSplotch} />
+      <div className={css.threadNameText}>{threadInfo.uiName}</div>
+    </div>
+  );
+}
+
+type Props = {
+  ...ModalOverridableProps,
+  +createNewEntry: (threadID: string) => void,
+};
+
+function ThreadPickerModal(props: Props): React.Node {
+  const { createNewEntry, ...modalProps } = props;
+
+  const onScreenThreadInfos = useSelector(onScreenEntryEditableThreadInfos);
+  const searchIndex = useSelector(state => threadSearchIndex(state));
+
+  invariant(
+    onScreenThreadInfos.length > 0,
+    "ThreadPicker can't be open when onScreenThreadInfos is empty",
+  );
+
+  const [searchText, setSearchText] = React.useState<string>('');
+  const [searchResults, setSearchResults] = React.useState<Set<string>>(
+    new Set(),
+  );
+
+  const onChangeSearchText = React.useCallback(
+    (text: string) => {
+      const results = searchIndex.getSearchResults(text);
+      setSearchText(text);
+      setSearchResults(new Set(results));
+    },
+    [searchIndex],
+  );
+
+  const listDataSelector = createSelector(
+    state => state.onScreenThreadInfos,
+    state => state.searchText,
+    state => state.searchResults,
+    (
+      threadInfos: $ReadOnlyArray<ThreadInfo>,
+      text: string,
+      results: Set<string>,
+    ) =>
+      text
+        ? threadInfos.filter(threadInfo => results.has(threadInfo.id))
+        : [...threadInfos],
+  );
+
+  const threads = useSelector(() =>
+    listDataSelector({
+      onScreenThreadInfos,
+      searchText,
+      searchResults,
+    }),
+  );
+
+  const threadPickerContent = React.useMemo(() => {
+    const options = threads.map(threadInfo => (
+      <ThreadPickerOption
+        threadInfo={threadInfo}
+        createNewEntry={createNewEntry}
+        key={threadInfo.id}
+        onCloseModal={modalProps.onClose}
+      />
+    ));
+
+    if (options.length === 0 && searchText.length > 0) {
+      return (
+        <div className={css.noResultsText}>No results for {searchText}</div>
+      );
+    } else {
+      return options;
+    }
+  }, [threads, createNewEntry, modalProps.onClose, searchText]);
+
+  return (
+    <Modal {...modalProps} size="large">
+      <div className={css.container}>
+        <Search
+          onChangeText={onChangeSearchText}
+          searchText={searchText}
+          placeholder="Search chats"
+        />
+        <div className={css.contentContainer}>{threadPickerContent}</div>
+      </div>
+    </Modal>
+  );
+}
+
+export default ThreadPickerModal;
