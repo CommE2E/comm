9b58bd86c2135eb283c05300eed5b4e070ab94d5
Jonathan Ringer <jonringer117@gmail.com>
2022-10-10 07:52:01 -0700

    [Services] Force c++17 usage with cxx builds
    Summary:
    Before we can use the system C compiler, we have to force c++17 usage
    for cxx builds. `cxx`'s `flag_if_supported` does not get correctly passed
    to the C compiler invocation. This is also needed because clang defaults to
    C++03 unless otherwise specified.
    
    Nix's clang compiler wasn't affected by this, as it would default to GCC12
    behavior of using a more recent version.
    
    Test Plan: Tunnelbroker nix gate passes
    
    Reviewers: atul, abosh, varun, max
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D5338

--- services/tunnelbroker/rust-lib/build.rs
+++ services/tunnelbroker/rust-lib/build.rs
@@ -1,4 +1,4 @@
 fn main() {
-  let _build = cxx_build::bridge("src/lib.rs").flag_if_supported("-std=c++17");
+  let _build = cxx_build::bridge("src/lib.rs").flag("-std=c++17");
   println!("cargo:rerun-if-changed=src/lib.rs");
 }
