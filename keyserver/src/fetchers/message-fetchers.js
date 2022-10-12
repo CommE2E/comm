8875f4fd7f97e0399bb0809d1775f77fdeb1c41f
Ashoat Tevosyan <ashoat@comm.app>
2022-10-12 00:20:16 -0400

    [keyserver] Go back to using window function for getMessageInfos
    Summary:
    The window function is a much more modern and civilized approach to this query.
    
    I first introduced this in D4675, but then reverted it in D4796 because it caused [ENG-957](https://linear.app/comm/issue/ENG-957/mobile-startreached-issue).
    
    However, once I found that the window function approach solved [ENG-2009](https://linear.app/comm/issue/ENG-2009/messages-missing-in-ashoatmarks-chat-history), I spent a second thinking about how to change the window function to avoid the issue from ENG-957. The easy solution is just to move the `LEFT JOIN uploads` line to the outer query.
    
    In addition, I noticed the window function also needed an `ORDER BY`, as it was exhibiting the same out-of-order weirdness that caused ENG-2009. Although it wasn't affecting the partition function, our JS code for parsing the results expects that they appear in a specific order.
    
    Test Plan:
    I tried to test this thoroughly given previous experiences.
    
    - I manually composed a query that reproduces the current issue (ENG-2009)
    - I edited it for the window function approach and saw that the issue was resolved
    - I manually composed a query that reproduces the old issue (ENG-957)
    - I edited it for the window function approach and saw that the old issue did not reappear
    - I patched my production keyserver live after midnight ET to confirm that the missing content now appears
    - I also live-tested to make sure the old issue was resolved by checking a chat that recently had a message with three images
    
    Reviewers: tomek, atul
    
    Reviewed By: tomek
    
    Subscribers: abosh
    
    Differential Revision: https://phab.comm.dev/D5349

--- keyserver/src/fetchers/message-fetchers.js
+++ keyserver/src/fetchers/message-fetchers.js
@@ -279,46 +279,29 @@ async function fetchMessageInfos(
 
   const viewerID = viewer.id;
   const query = SQL`
-    SELECT * FROM (
-      SELECT x.id, x.content, x.time, x.type, x.user AS creatorID,
-        x.creation, x.subthread_permissions, x.uploadID, x.uploadType,
-        x.uploadSecret, x.uploadExtra,
-        @num := if(
-          @thread = x.thread,
-          if(@message = x.id, @num, @num + 1),
-          1
-        ) AS number,
-        @message := x.id AS messageID,
-        @thread := x.thread AS threadID
-      FROM (SELECT @num := 0, @thread := '', @message := '') init
-      JOIN (
-        SELECT m.id, m.thread, m.user, m.content, m.time, m.type,
-          m.creation, stm.permissions AS subthread_permissions,
-          up.id AS uploadID, up.type AS uploadType, up.secret AS uploadSecret,
-          up.extra AS uploadExtra
-        FROM messages m
-        LEFT JOIN uploads up ON up.container = m.id
-        LEFT JOIN memberships mm
-          ON mm.thread = m.thread AND mm.user = ${viewerID}
-        LEFT JOIN memberships stm ON m.type = ${messageTypes.CREATE_SUB_THREAD}
-          AND stm.thread = m.content AND stm.user = ${viewerID}
-        WHERE JSON_EXTRACT(mm.permissions, ${visibleExtractString}) IS TRUE AND
+    WITH thread_window AS (
+      SELECT m.id, m.thread AS threadID, m.user AS creatorID, m.content,
+        m.time, m.type, m.creation, stm.permissions AS subthread_permissions,
+        ROW_NUMBER() OVER (
+          PARTITION BY threadID ORDER BY m.time DESC, m.id DESC
+        ) n
+      FROM messages m
+      LEFT JOIN memberships mm
+        ON mm.thread = m.thread AND mm.user = ${viewerID}
+      LEFT JOIN memberships stm ON m.type = ${messageTypes.CREATE_SUB_THREAD}
+        AND stm.thread = m.content AND stm.user = ${viewerID}
+      WHERE JSON_EXTRACT(mm.permissions, ${visibleExtractString}) IS TRUE AND
   `;
   query.append(selectionClause);
-  // - We specify a ridiculously high LIMIT here to force MariaDB to maintain
-  //   the ORDER BY. Otherwise the query optimizer will assume that the order
-  //   doesn't matter because the outer query doesn't have an ORDER BY.
-  // - Setting an ORDER BY on the outer query won't work because our quirky
-  //   counting logic in the SELECT would be executed before the ORDER, on the
-  //   unsorted results from the inner query.
-  // - More details available here:
-  //   https://mariadb.com/kb/en/why-is-order-by-in-a-from-subquery-ignored/
   query.append(SQL`
-        ORDER BY m.thread, m.time DESC, m.id DESC
-        LIMIT 18446744073709551615
-      ) x
-    ) y
-    WHERE y.number <= ${numberPerThread}
+    )
+    SELECT tw.*,
+      up.id AS uploadID, up.type AS uploadType, up.secret AS uploadSecret,
+      up.extra AS uploadExtra
+    FROM thread_window tw
+    LEFT JOIN uploads up ON up.container = tw.id
+    WHERE tw.n <= ${numberPerThread}
+    ORDER BY tw.threadID, tw.time DESC, tw.id DESC
   `);
 
   const [result] = await dbQuery(query);
