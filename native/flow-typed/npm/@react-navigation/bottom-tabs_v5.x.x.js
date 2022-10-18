4f73988bb3629ee5e70fc7f86877caefaf46951c
Ashoat Tevosyan <ashoat@comm.app>
2022-10-18 09:00:56 -0400

    [native] [15/34] ReactNav6: add navigator ID
    Summary:
    See info about navigator IDs in the [ReactNav API reference](https://reactnavigation.org/docs/navigation-prop/#getparent)
    
    Depends on D5407
    
    Test Plan: Flow + ran the app on my phone and played around with it
    
    Reviewers: atul, tomek, bartek
    
    Reviewed By: atul
    
    Subscribers: abosh
    
    Differential Revision: https://phab.comm.dev/D5408

--- native/flow-typed/npm/@react-navigation/bottom-tabs_v5.x.x.js
+++ native/flow-typed/npm/@react-navigation/bottom-tabs_v5.x.x.js
@@ -870,7 +870,8 @@ declare module '@react-navigation/bottom-tabs' {
     +goBack: () => void,
     +isFocused: () => boolean,
     +canGoBack: () => boolean,
-    +getParent: <Parent: NavigationProp<ParamListBase>>() => ?Parent,
+    +getId: () => string | void,
+    +getParent: <Parent: NavigationProp<ParamListBase>>(id?: string) => ?Parent,
     +getState: () => NavigationState,
     +addListener: <EventName: $Keys<
       {| ...EventMap, ...EventMapCore<State> |},
@@ -1058,6 +1059,7 @@ declare module '@react-navigation/bottom-tabs' {
   |};
   declare export type ExtraNavigatorPropsBase = {
     ...$Exact<DefaultRouterOptions>,
+    +id?: string,
     +children?: React$Node,
     ...
   };
@@ -1149,6 +1151,7 @@ declare module '@react-navigation/bottom-tabs' {
       +children?: React$Node,
     |},
   ) => {|
+    +id?: string,
     +state: State,
     +descriptors: {| +[key: string]: Descriptor<NavHelpers, ScreenOptions> |},
     +navigation: NavHelpers,
