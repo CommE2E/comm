dfb194a3df9303a7f645f31f04ca1410ecbe323c
Ashoat Tevosyan <ashoat@comm.app>
2022-10-19 16:35:53 -0400

    [native] [32/34] ReactNav6: Update MaterialTopTabsNavigator
    Summary:
    Context on changes [in this section](https://reactnavigation.org/docs/upgrading-from-5.x/#material-top-tab-navigator).
    
    Depends on D5424
    
    Test Plan: Flow + ran the app on my phone and played around with it
    
    Reviewers: atul, tomek, bartek
    
    Reviewed By: atul
    
    Subscribers: abosh
    
    Differential Revision: https://phab.comm.dev/D5425

--- native/flow-typed/npm/@react-navigation/bottom-tabs_v5.x.x.js
+++ native/flow-typed/npm/@react-navigation/bottom-tabs_v5.x.x.js
@@ -1808,6 +1808,26 @@ declare module '@react-navigation/bottom-tabs' {
     +tabBarIcon: ({| +focused: boolean, +color: string |}) => React$Node,
     +tabBarAccessibilityLabel: string,
     +tabBarTestID: string,
+    +tabBarActiveTintColor: string,
+    +tabBarInactiveTintColor: string,
+    +tabBarPressColor: string,
+    +tabBarPressOpacity: number,
+    +tabBarShowLabel: boolean,
+    +tabBarShowIcon: boolean,
+    +tabBarAllowFontScaling: boolean,
+    +tabBarBounces: boolean,
+    +tabBarScrollEnabled: boolean,
+    +tabBarIconStyle: ViewStyleProp,
+    +tabBarLabelStyle: TextStyleProp,
+    +tabBarItemStyle: ViewStyleProp,
+    +tabBarIndicatorStyle: ViewStyleProp,
+    +tabBarIndicatorContainerStyle: ViewStyleProp,
+    +tabBarContentContainerStyle: ViewStyleProp,
+    +tabBarStyle: ViewStyleProp,
+    +tabBarBadge: () => React$Node,
+    +tabBarIndicator: MaterialTopTabBarIndicatorProps => React$Node,
+    +lazy: boolean,
+    +lazyPlaceholder: ({| +route: Route<> |}) => React$Node,
   |}>;
 
   /**
@@ -1891,34 +1911,11 @@ declare module '@react-navigation/bottom-tabs' {
   |};
 
   declare export type MaterialTopTabBarIndicatorProps = {|
-    +navigationState: TabNavigationState,
+    +state: TabNavigationState,
     +width: string,
     +style?: ViewStyleProp,
     +getTabWidth: number => number,
   |};
-
-  declare export type MaterialTopTabBarOptions = $Partial<{|
-    +scrollEnabled: boolean,
-    +bounces: boolean,
-    +pressColor: string,
-    +pressOpacity: number,
-    +getAccessible: ({| +route: Route<> |}) => boolean,
-    +renderBadge: ({| +route: Route<> |}) => React$Node,
-    +renderIndicator: MaterialTopTabBarIndicatorProps => React$Node,
-    +tabStyle: ViewStyleProp,
-    +indicatorStyle: ViewStyleProp,
-    +indicatorContainerStyle: ViewStyleProp,
-    +labelStyle: TextStyleProp,
-    +contentContainerStyle: ViewStyleProp,
-    +style: ViewStyleProp,
-    +activeTintColor: string,
-    +inactiveTintColor: string,
-    +iconStyle: ViewStyleProp,
-    +labelStyle: TextStyleProp,
-    +showLabel: boolean,
-    +showIcon: boolean,
-    +allowFontScaling: boolean,
-  |}>;
 
   declare export type MaterialTopTabDescriptor = Descriptor<
     MaterialBottomTabNavigationHelpers<>,
@@ -1932,7 +1929,6 @@ declare module '@react-navigation/bottom-tabs' {
   |};
 
   declare export type MaterialTopTabBarProps = {|
-    ...MaterialTopTabBarOptions,
     ...MaterialTopTabNavigationBuilderResult,
     +layout: {| +width: number, +height: number |},
     +position: any, // Reanimated.Node<number>
@@ -1944,16 +1940,13 @@ declare module '@react-navigation/bottom-tabs' {
     +position?: any, // Reanimated.Value<number>
     +tabBarPosition?: 'top' | 'bottom',
     +initialLayout?: $Partial<{| +width: number, +height: number |}>,
-    +lazy?: boolean,
     +lazyPreloadDistance?: number,
     +removeClippedSubviews?: boolean,
     +sceneContainerStyle?: ViewStyleProp,
     +style?: ViewStyleProp,
     +gestureHandlerProps?: PanGestureHandlerProps,
     +pager?: MaterialTopTabPagerProps => React$Node,
-    +lazyPlaceholder?: ({| +route: Route<> |}) => React$Node,
     +tabBar?: MaterialTopTabBarProps => React$Node,
-    +tabBarOptions?: MaterialTopTabBarOptions,
   |};
 
   declare export type ExtraMaterialTopTabNavigatorProps = {|
