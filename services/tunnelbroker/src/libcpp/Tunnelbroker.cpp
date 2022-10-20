e8d2e66a98bc55f71a82b062d0b565e57376c015
Max Kalashnikoff <geekmaks@gmail.com>
2022-10-20 15:23:53 +0200

    [services] Tunnelbroker - Initialization function exposed to Rust
    Summary:
    This diff is a **part of the flat diffs stack** to transform the current Tunnelbroker project into a Rust app by calling the current C++ codebase from Rust. The related transition task is [[ https://linear.app/comm/issue/ENG-1072 | ENG-1072 ]].
    
    This diff introduces creating a `Tunnelbroker.h` header file which will be exposed and run in Rust main application entry point at `main.rs` (D5432) which contains an initialization function that calls the initialization functions from the former [[ https://github.com/CommE2E/comm/blob/3a908c7a58964246f29e8061742333cf22958e12/services/tunnelbroker/src/server.cpp#L37-L40 | server.cpp ]].
    
    Related Linear task: [[ https://linear.app/comm/issue/ENG-2092/moving-initialization-procedure-call-from-c-to-rust-for-tunnelbroker | ENG-2092 ]]
    
    **CI Notice:**
    The CI will fail on this and further diffs until the D5461 where the changes to CI build commands are made.
    This stack will be** landed all in one** to prevent CI from failing on diffs out of this stack.
    
    Test Plan:
    **1. CI Testing**:
    These diff changes are included as a part of the stack in the later D5461 diff, where the changes to CI build commands are made. The CI succeded on the D5461 which reflects that this changes are passing the build.
    
    **2. Manually testing:**
    These diff changes can be tested in the middle of the stack at D5436. To test it patch to D5436 using `arc patch D5436`, run `nix develop`. Go to tunnelbroker service directory `services/tunnelbroker` and run the Cargo building process `cargo build` or `cargo run`.
    The Rust application will be successfully built.
    
    Reviewers: jon, varun, marcin, tomek
    
    Reviewed By: jon, tomek
    
    Subscribers: ashoat, tomek, atul, abosh
    
    Differential Revision: https://phab.comm.dev/D5429

---
+++ services/tunnelbroker/src/libcpp/Tunnelbroker.cpp
@@ -0,0 +1,37 @@
+#include "Tunnelbroker.h"
+#include "AmqpManager.h"
+#include "AwsTools.h"
+#include "ConfigManager.h"
+#include "DatabaseManager.h"
+#include "GlobalTools.h"
+#include "Tools.h"
+
+#include "rust/cxx.h"
+
+void initialize() {
+  comm::network::tools::InitLogging("tunnelbroker");
+  comm::network::config::ConfigManager::getInstance().load();
+  Aws::InitAPI({});
+  // List of AWS DynamoDB tables to check if they are created and can be
+  // accessed before any AWS API methods
+  const std::list<std::string> tablesList = {
+      comm::network::config::ConfigManager::getInstance().getParameter(
+          comm::network::config::ConfigManager::OPTION_DYNAMODB_SESSIONS_TABLE),
+      comm::network::config::ConfigManager::getInstance().getParameter(
+          comm::network::config::ConfigManager::
+              OPTION_DYNAMODB_SESSIONS_VERIFICATION_TABLE),
+      comm::network::config::ConfigManager::getInstance().getParameter(
+          comm::network::config::ConfigManager::
+              OPTION_DYNAMODB_SESSIONS_PUBLIC_KEY_TABLE),
+      comm::network::config::ConfigManager::getInstance().getParameter(
+          comm::network::config::ConfigManager::
+              OPTION_DYNAMODB_MESSAGES_TABLE)};
+  for (const std::string &table : tablesList) {
+    if (!comm::network::database::DatabaseManager::getInstance()
+             .isTableAvailable(table)) {
+      throw std::runtime_error(
+          "Error: AWS DynamoDB table '" + table + "' is not available");
+    }
+  };
+  comm::network::AmqpManager::getInstance().init();
+}
---
+++ services/tunnelbroker/src/libcpp/Tunnelbroker.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "rust/cxx.h"
+
+void initialize();
