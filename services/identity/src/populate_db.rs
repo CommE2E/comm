1cd082b191496e5b09d40e06e0bc7b7fea88bdff
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2022-10-27 17:49:01 -0400

    [services][identity] function to get users from mysql database
    Summary: returns a vector of Users from the users table in MySQL
    
    Test Plan: called the function locally and confirmed that the returned contents matched what I saw in TablePlus
    
    This function will be tested more in future diffs
    
    Reviewers: tomek!, bartek
    
    Subscribers:

--- services/identity/src/populate_db.rs
+++ services/identity/src/populate_db.rs
@@ -1,5 +1,32 @@
+use mysql_async::prelude::{Query, WithParams};
+use tracing::info;
+
+#[derive(Debug)]
 struct User {
   id: i64,
   username: String,
   creation_time: i64,
+}
+
+pub async fn get_users(
+  user: &str,
+  password: &str,
+  domain: &str,
+  port: &str,
+  database: &str,
+) -> Result<Vec<User>, mysql_async::Error> {
+  let database_url = format!(
+    "mysql://{}:{}@{}:{}/{}",
+    user, password, domain, port, database
+  );
+  let pool = mysql_async::Pool::new(database_url.as_str());
+  let mut conn = pool.get_conn().await?;
+  "SELECT id, username, creation_time FROM users"
+    .with(())
+    .map(&mut conn, |(id, username, creation_time)| User {
+      id,
+      username,
+      creation_time,
+    })
+    .await
 }
