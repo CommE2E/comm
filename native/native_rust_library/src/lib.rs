0253ef7b1fd821fdd5f7829c83f3bfd81ed38046
Inka Sokołowska <inka.sokolowska@swmansion.com>
2022-10-31 10:07:13 +0100

    Revert "[native] Generate device ID using Rust"
    Summary: This reverts commit ac8f8e05eee51d33e05773272918f7b6146234a5.
    
    Test Plan:
    The test is pushing to phabricator and seeing if CI fails
    
    Reviewers:
    ashoat, tomek, varun, atul
    Subscribers:

--- native/native_rust_library/src/lib.rs
+++ native/native_rust_library/src/lib.rs
@@ -4,14 +4,12 @@ use tokio::runtime::{Builder, Runtime};
 use tonic::{transport::Channel, Status};
 use tracing::instrument;
 
-mod crypto_tools;
 mod identity_client;
 mod opaque;
 mod identity {
   tonic::include_proto!("identity");
 }
 
-use crypto_tools::generate_device_id;
 use identity::identity_service_client::IdentityServiceClient;
 use identity_client::{
   get_user_id, login_user_pake, login_user_wallet, register_user,
@@ -33,15 +31,7 @@ lazy_static! {
 
 #[cxx::bridge]
 mod ffi {
-
-  enum DeviceType {
-    KEYSERVER,
-    WEB,
-    MOBILE,
-  }
-
   extern "Rust" {
-    // Identity Service Client
     type Client;
     fn initialize_client() -> Box<Client>;
     fn get_user_id_blocking(
@@ -78,8 +68,6 @@ mod ffi {
       siwe_signature: Vec<u8>,
       user_public_key: String,
     ) -> Result<String>;
-    // Crypto Tools
-    fn generate_device_id(device_type: DeviceType) -> Result<String>;
   }
 }
 
