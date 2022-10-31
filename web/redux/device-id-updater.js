4f06b17c28f7c151e66924530faa27031ac8f680
Inka Sokołowska <inka.sokolowska@swmansion.com>
2022-10-31 09:55:57 +0100

    Revert "Rename deviceIDTypes to deviceTypes, add comments"
    Summary: This reverts commit 1482f5e7c1640629f2503bbc1061bf95bc419b73.
    
    Test Plan:
    The test is pushing to phabricator and seeing if CI fails
    
    Reviewers:
    ashoat, tomek, varun, atul
    Subscribers:

--- web/redux/device-id-updater.js
+++ web/redux/device-id-updater.js
@@ -2,7 +2,7 @@
 import * as React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { generateDeviceID, deviceTypes } from '../utils/device-id';
+import { generateDeviceID, deviceIDTypes } from '../utils/device-id';
 import { setDeviceIDActionType } from './action-types';
 
 function DeviceIDUpdater(): null {
@@ -13,7 +13,7 @@ function DeviceIDUpdater(): null {
 
   React.useEffect(() => {
     if (hadDeviceIDRef.current !== false && !hasDeviceID) {
-      const newDeviceID = generateDeviceID(deviceTypes.WEB);
+      const newDeviceID = generateDeviceID(deviceIDTypes.WEB);
       dispatch({
         type: setDeviceIDActionType,
         payload: newDeviceID,
--- web/utils/device-id.js
+++ web/utils/device-id.js
@@ -4,38 +4,32 @@ import invariant from 'invariant';
 
 import { generateRandomString } from './text-utils';
 
-const deviceTypes = Object.freeze({
+const deviceIDTypes = Object.freeze({
   KEYSERVER: 0,
   WEB: 1,
   MOBILE: 2,
 });
-type DeviceType = $Values<typeof deviceTypes>;
+type DeviceIDType = $Values<typeof deviceIDTypes>;
 
 const alphanumeric =
   '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 // deviceIDCharLength has to be kept in sync with DEVICEID_CHAR_LENGTH
 // which is defined in services/tunnelbroker/src/Constants.h
-// and with DEVICE_ID_CHAR_LENGTH
-// defined in native/native_rust_library/src/crypto_tools.rs
 const deviceIDCharLength = 64;
 // deviceIDFormatRegex has to be kept in sync with DEVICEID_FORMAT_REGEX
 // which is defined in services/tunnelbroker/src/Constants.h
-// and with DEVICE_ID_FORMAT_REGEX
-// defined in native/native_rust_library/src/crypto_tools.rs
 const deviceIDFormatRegex: RegExp = new RegExp(
   `^(ks|mobile|web):[a-zA-Z0-9]{${deviceIDCharLength.toString()}}$`,
 );
 
-// generateDeviceID has to be kept in sync with generate_device_id
-// which is defined in native/native_rust_library/src/crypto_tools.rs
-function generateDeviceID(type: DeviceType): string {
+function generateDeviceID(type: DeviceIDType): string {
   const suffix = generateRandomString(deviceIDCharLength, alphanumeric);
 
-  if (type === deviceTypes.KEYSERVER) {
+  if (type === deviceIDTypes.KEYSERVER) {
     return `ks:${suffix}`;
-  } else if (type === deviceTypes.WEB) {
+  } else if (type === deviceIDTypes.WEB) {
     return `web:${suffix}`;
-  } else if (type === deviceTypes.MOBILE) {
+  } else if (type === deviceIDTypes.MOBILE) {
     return `mobile:${suffix}`;
   }
   invariant(false, `Unhandled device type ${type}`);
@@ -44,6 +38,6 @@ function generateDeviceID(type: DeviceType): string {
 export {
   generateDeviceID,
   deviceIDCharLength,
-  deviceTypes,
+  deviceIDTypes,
   deviceIDFormatRegex,
 };
