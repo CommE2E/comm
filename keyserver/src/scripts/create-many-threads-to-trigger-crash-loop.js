50d12886e7e757fdb876a84ddcda9b98382bf460
Inka Sokołowska <inka.sokolowska@swmansion.com>
2022-11-04 15:08:41 +0100

    [keyserver] Add a script for triggering socket crash loop
    Summary:
    Linear issue: https://linear.app/comm/issue/ENG-2075/reproduce-socket-crash-loop-in-production-with-artificial-test-data
    Calling this script in keyserver/ causes 1000 threads to be created under GENESIS with ashoat user and user whos id is specified as spammedUserID.
    Having my physical iOS device use 3G this caused a socket crash loop.
    Mind that creating 1000 threads takes about 15min.
    
    Test Plan:
    Set iOS physical device network profile to 3G and run the script in keyserver/
    This was tested on 22ac9645d0eb5f83a23a8c1daae0d31ebed5a4a1 D5513
    
    Reviewers: tomek, atul, marcin
    
    Reviewed By: tomek
    
    Subscribers: ashoat, abosh
    
    Differential Revision: https://phab.comm.dev/D5530

---
+++ keyserver/src/scripts/create-many-threads-to-trigger-crash-loop.js
@@ -0,0 +1,42 @@
+// @flow
+import ashoat from 'lib/facts/ashoat';
+
+import { createThread } from '../creators/thread-creator';
+import { createScriptViewer } from '../session/scripts';
+import { main } from './utils';
+
+const testUserID = '';
+const numOfThreads = 1000;
+
+async function createThreads(
+  n: number,
+  spammedUserID: string,
+  spammingUserID: string,
+): Promise<$ReadOnlyArray<string>> {
+  const threads = [];
+  const viewer = createScriptViewer(spammingUserID);
+  const initialMemberIDs = [spammedUserID];
+  const threadRequest = { type: 3, initialMemberIDs, parentThreadID: '1' };
+
+  for (let i = 0; i < n; i++) {
+    const threadResponse = await createThread(viewer, threadRequest);
+    if (threadResponse.newThreadID) {
+      const threadID: string = threadResponse.newThreadID;
+      threads.push(threadID);
+    }
+  }
+  return threads;
+}
+
+// This script is used to trigger socket crash loop
+// Linear issue: https://linear.app/comm/issue/ENG-2075/reproduce-socket-crash-loop-in-production-with-artificial-test-data
+// Usage: set testUserID to the user you wish to trigger the crash loop for,
+// set iOS physical device networking profile to 3G and run the script,
+// open comm on physical device once the script has finnished
+// the app should be in a crash loop
+
+async function createManyThreadsToTriggerCrashLoop() {
+  await createThreads(numOfThreads, testUserID, ashoat.id);
+}
+
+main([createManyThreadsToTriggerCrashLoop]);
