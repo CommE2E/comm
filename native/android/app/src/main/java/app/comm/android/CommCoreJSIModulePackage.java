f262eeb5a96aa35b663dafc16009098d3b241f72
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2022-11-11 08:53:06 +0100

    [native] [28/40] RN 0.70: Fix Expo 47 SecureStore crash on Android
    Summary:
    I (@ashoat) was seeing a crash-on-start in the Android app after the Expo update: [ENG-2211](https://linear.app/comm/issue/ENG-2211/secure-store-error-after-expo-secure-store-update). In the Linear issue, you can see that @bartek took on the investigation and created this commit that resolved the issue. Here's the initial commit text from him:
    
    > The `expo-secure-store` on Android now requires also providing Expo-specific `ModuleRegistry`. This is only needed, because the module now requires two interfaces: `UIManager` and `ActivityProvider` and it tries to access them using the module registry.  Both of them are implemented by `UIManagerModuleWrapper` class which is simply a wrapper for some RN/Android functionalities.
    >
    > My workaround is as follows:
    > - Create mock expo ModuleRegistry object
    > - Add a single module to the registry: `UIManagerModuleWrapper`
    > - Provide the registry to `SecureStoreModule.onCreate()` to make it happy
    
    Depends on D5922
    
    Test Plan:
    1. Confirm crash-on-start with error text from linked Linear task is gone
    2. Tested along with whole stack: [test plan](https://www.notion.so/commapp/Test-plan-for-React-Native-0-70-upgrade-718fa7e0fe4348099b4c169eb121da16)
    
    Reviewers: tomek, bartek
    
    Subscribers: atul, bartek
    
    Differential Revision: https://phab.comm.dev/D5923

--- native/android/app/src/main/java/app/comm/android/CommCoreJSIModulePackage.java
+++ native/android/app/src/main/java/app/comm/android/CommCoreJSIModulePackage.java
@@ -1,16 +1,12 @@
 package app.comm.android;
 
-import android.util.Log;
 import app.comm.android.fbjni.CommHybrid;
 import app.comm.android.fbjni.CommSecureStore;
 import com.facebook.react.bridge.JSIModuleSpec;
 import com.facebook.react.bridge.JavaScriptContextHolder;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.swmansion.reanimated.ReanimatedJSIModulePackage;
-import expo.modules.adapters.react.NativeModulesProxy;
-import expo.modules.core.ModuleRegistry;
 import expo.modules.securestore.SecureStoreModule;
-import java.lang.reflect.Field;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Supplier;
@@ -22,7 +18,7 @@ public class CommCoreJSIModulePackage extends ReanimatedJSIModulePackage {
       ReactApplicationContext reactApplicationContext,
       JavaScriptContextHolder jsContext) {
     Supplier<SecureStoreModule> secureStoreModuleSupplier =
-        () -> new SecureStoreModule(reactApplicationContext);
+        ExpoUtils.createExpoSecureStoreSupplier(reactApplicationContext);
     CommSecureStore.getInstance().initialize(secureStoreModuleSupplier);
     CommHybrid.initHybrid(reactApplicationContext);
     super.getJSIModules(reactApplicationContext, jsContext);
