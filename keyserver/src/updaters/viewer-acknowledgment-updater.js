328b5c5d0af4da14b4e654e4ebe38b166a7a8933
xsanm <kkurowski.kk@gmail.com>
2022-11-16 15:28:34 +0100

    [keyserver] add logic to update acknowledgment table
    Summary:
    The function should be called after detecting login from a proper source.
    
    Functionality:
        -      a. if there is no record with the given user and policy it means that this is a new one - so It adds a new row
        -      b. if there is a record with a given user and policy but confirmed is set to `0` it means that policy was updated, so it updated `confirmed` and `date`
        -      c. in any other case do nothing
    
    Test Plan:
    Check if described in Summary logic works for:
    1. new policy
    2. updated policy
    4. policy accepted by user previously
    
    Reviewers: atul, marcin, ashoat, tomek
    
    Reviewed By: marcin, ashoat, tomek
    
    Subscribers: ashoat, atul, abosh
    
    Differential Revision: https://phab.comm.dev/D5311

---
+++ keyserver/src/updaters/viewer-acknowledgment-updater.js
@@ -0,0 +1,23 @@
+// @flow
+
+import type { PolicyType } from 'lib/facts/policies.js';
+
+import { dbQuery, SQL } from '../database/database.js';
+import type { Viewer } from '../session/viewer.js';
+
+async function viewerAcknowledgmentUpdater(viewer: Viewer, policy: PolicyType) {
+  if (!viewer.loggedIn) {
+    return;
+  }
+
+  const time = Date.now();
+  await dbQuery(SQL`
+    INSERT INTO policy_acknowledgments (user, policy, date, confirmed)
+    VALUES (${viewer.data.id}, ${policy}, ${time}, 1)
+    ON DUPLICATE KEY UPDATE
+     date = IF (confirmed = 1, date, ${time}),
+     confirmed = 1
+  `);
+}
+
+export { viewerAcknowledgmentUpdater };
