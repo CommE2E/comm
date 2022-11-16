d22cf359cf998f8600b51ddeb5ffec814a91833a
Max Kalashnikoff <geekmaks@gmail.com>
2022-11-16 12:12:24 +0100

    [services] Tunnelbroker - Notifications config initialization
    Summary:
    In this diff, we initialize the static configuration as a struct for the internal notifications library to avoid passing repeated parameters on every notification-sending call. We are using a lazy static initialization and parameters from the Tunnelbroker's configuration file.
    This diff is related to the abandoned D5329. The static configuration is used in the D5642 diff updates.
    
    Linear task: [[ https://linear.app/comm/issue/ENG-1737/change-to-use-builder-approach-and-thread-local-in-rust-notification | ENG-1737 ]]
    
    Test Plan:
    1. Service is successfully built.
    2. The diff D5641 where the function is used the service is successfully built.
    
    Reviewers: jon, tomek, varun
    
    Reviewed By: jon, tomek
    
    Subscribers: ashoat, tomek, atul, abosh
    
    Differential Revision: https://phab.comm.dev/D5641

--- services/tunnelbroker/src/notifications/mod.rs
+++ services/tunnelbroker/src/notifications/mod.rs
@@ -1,2 +1,23 @@
 pub mod apns;
+pub mod config;
 pub mod fcm;
+use super::cxx_bridge::ffi::{getConfigParameter, isSandbox};
+use lazy_static::lazy_static;
+
+lazy_static! {
+  static ref CONFIG: config::Config = config::Config {
+    fcm_api_key: getConfigParameter("notifications.fcm_server_key")
+      .expect("Error getting `notifications.fcm_server_key` config parameter"),
+    apns_certificate_path: getConfigParameter("notifications.apns_cert_path")
+      .expect("Error getting `notifications.apns_cert_path` config parameter"),
+    apns_certificate_password: getConfigParameter(
+      "notifications.apns_cert_password"
+    )
+    .expect(
+      "Error getting `notifications.apns_cert_password` config parameter"
+    ),
+    apns_topic: getConfigParameter("notifications.apns_topic")
+      .expect("Error getting `apns_topic` config parameter"),
+    is_sandbox: isSandbox().expect("Error determining of sandboxing"),
+  };
+}
