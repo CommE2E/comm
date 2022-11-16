14e7077b61e54b8e0d06ef170a241fd2b49c8072
xsanm <kkurowski.kk@gmail.com>
2022-11-16 15:26:03 +0100

    [keyserver] add migration to create `policy_acknowledgments` table
    Summary:
    Add the table to track user acknowledgments.
    
    Table structure was described in [ENG-1868](https://linear.app/comm/issue/ENG-1868/create-new-table-to-track-whether-policies-have-been-acknowledged).
    
    Table using only primary key with two columns - (user, policy) and user is in first place on purpose.
    A lot of queries will be performed on this table based on the user id. `InnoDB` by default will use [BTREE](https://mariadb.com/kb/en/storage-engine-index-types/) as indexing algorithm which works fine for this approach (keep rows sorted based on user ID and should be decent).
    I also did a test with added additional index only for `user` column a perform some queries on table with ~900 rows and ~400 users, measured times were almost equal on both approaches so I think there is no need for another index (might waster some resources) and primary key with pair (user, policy) should be sufficient.
    
    Test Plan:
    1. Restart the keyserver
    2. Check in `metadata` table if the database version was incremented.
    3. Check if `policy_acknowledgments` was created with proper structure, screenshot with table:
    {F192514}
    
    Reviewers: atul, marcin, ashoat, tomek
    
    Reviewed By: atul, marcin, ashoat, tomek
    
    Subscribers: tomek, ashoat, atul, abosh
    
    Differential Revision: https://phab.comm.dev/D5307

--- keyserver/src/database/migration-config.js
+++ keyserver/src/database/migration-config.js
@@ -89,6 +89,20 @@ const migrations: $ReadOnlyMap<number, () => Promise<void>> = new Map([
       );
     },
   ],
+  [
+    10,
+    async () => {
+      await dbQuery(SQL`
+        CREATE TABLE IF NOT EXISTS policy_acknowledgments (
+          user bigint(20) NOT NULL,
+          policy varchar(255) NOT NULL,
+          date bigint(20) NOT NULL,
+          confirmed tinyint(1) UNSIGNED NOT NULL DEFAULT 0,
+          PRIMARY KEY (user, policy)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+      `);
+    },
+  ],
 ]);
 const newDatabaseVersion: number = Math.max(...migrations.keys());
 
