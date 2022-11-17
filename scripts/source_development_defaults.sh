7413dab98cda56c42f58f450f7d20f3f5ae67bef
Jonathan Ringer <jonringer117@gmail.com>
2022-11-17 11:50:45 -0800

    [Nix] Default value of ANDROID_HOME
    Summary:
    Prefer to use ANDROID_HOME as it's the new canonical way to describe
    the installation path of the Android SDK. Also alias the value to
    ANDROID_SDK_ROOT for compatibility with certain tools such as `sdkmanager`.
    
    https://linear.app/comm/issue/ENG-2274
    
    Test Plan:
    ```
    nix develop
    
    echo $ANDROID_HOME
    echo $ANDROID_SDK_ROOT # should match, and point to sdk installation
    ```
    
    Reviewers: atul, ashoat, varun
    
    Reviewed By: ashoat
    
    Subscribers: tomek, abosh
    
    Differential Revision: https://phab.comm.dev/D5656

--- scripts/source_development_defaults.sh
+++ scripts/source_development_defaults.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
 if [[ "$OSTYPE" == 'linux'* ]]; then
-  export MYSQL_UNIX_PORT=${XDG_RUNTIME_DIR:-/run/user/$UID}/mysql.sock
-  export ANDROID_SDK_ROOT=${ANDROID_SDK_ROOT:-$HOME/Android/Sdk}
+  export MYSQL_UNIX_PORT="${XDG_RUNTIME_DIR:-/run/user/$UID}/mysql.sock"
+  export ANDROID_HOME="${ANDROID_HOME:-$HOME/Android/Sdk}"
 fi
 
 if [[ "$OSTYPE" == 'darwin'* ]]; then
-  MARIADB_DIR=${XDG_DATA_HOME:-$HOME/.local/share}/MariaDB
+  MARIADB_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/MariaDB"
   export MYSQL_UNIX_PORT="$MARIADB_DIR"/mysql.sock
-  export ANDROID_SDK_ROOT=${ANDROID_SDK_ROOT:-$HOME/Library/Android/sdk}
+  export ANDROID_HOME="${ANDROID_HOME:-$HOME/Library/Android/sdk}"
 fi
 
-export PATH="$PATH":"$ANDROID_SDK_ROOT"/emulator:$ANDROID_SDK_ROOT/tools
-export PATH="$PATH":"$ANDROID_SDK_ROOT"/tools/bin:$ANDROID_SDK_ROOT/platform-tools
+export PATH="$PATH":"$ANDROID_HOME"/emulator:"$ANDROID_HOME"/tools
+export PATH="$PATH":"$ANDROID_HOME"/tools/bin:"$ANDROID_HOME"/platform-tools
+
+# ANDROID_SDK_ROOT is deprecated, but it's still used by some tooling
+# such as sdkmanager. ANDROID_HOME is the new prefered env var.
+export ANDROID_SDK_ROOT="${ANDROID_SDK_ROOT:-$ANDROID_HOME}"
 
 export PATH="$PATH":./node_modules/.bin
