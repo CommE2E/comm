7790e0898b88dc33d6f112a63143ac25f38033de
Bartomiej Klocek <bartlomiej.klocek@swmansion.com>
2022-11-25 16:09:03 -0800

    [services][blob] Introduce database client
    Summary:
    Introduced DynamoDB database client - the same way as it is implemented in Identity Service.
    
    The interface of the C++ implementation [[ https://github.com/CommE2E/comm/blob/master/services/blob/src/DatabaseManager.h | is here ]].
    
    Depends on D5680
    
    Test Plan: Only ensured that build does not fail.
    
    Reviewers: varun, jon, tomek, marcin
    
    Reviewed By: varun, tomek
    
    Subscribers: ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D5681

---
+++ services/blob/src/database.rs
@@ -0,0 +1,74 @@
+use anyhow::Result;
+use std::sync::Arc;
+
+use crate::s3::S3Path;
+
+#[derive(Clone, Debug)]
+pub struct BlobItem {
+  pub blob_hash: String,
+  pub s3_path: S3Path,
+  pub created: i64,
+}
+
+#[derive(Clone, Debug)]
+pub struct ReverseIndexItem {
+  pub holder: String,
+  pub blob_hash: String,
+}
+
+#[derive(Clone)]
+pub struct DatabaseClient {
+  client: Arc<aws_sdk_dynamodb::Client>,
+}
+
+impl DatabaseClient {
+  pub fn new(aws_config: &aws_types::SdkConfig) -> Self {
+    DatabaseClient {
+      client: Arc::new(aws_sdk_dynamodb::Client::new(aws_config)),
+    }
+  }
+
+  // Blob item
+
+  pub async fn put_blob_item(&self, blob_item: BlobItem) -> Result<()> {
+    unimplemented!();
+  }
+
+  pub async fn find_blob_item(
+    &self,
+    blob_hash: &str,
+  ) -> Result<Option<BlobItem>> {
+    unimplemented!();
+  }
+
+  pub async fn remove_blob_item(&self, blob_hash: &str) -> Result<()> {
+    unimplemented!();
+  }
+
+  // Reverse index item
+
+  pub async fn put_reverse_index_item(
+    &self,
+    reverse_index_item: ReverseIndexItem,
+  ) -> Result<()> {
+    unimplemented!();
+  }
+
+  pub async fn find_reverse_index_by_holder(
+    &self,
+    holder: &str,
+  ) -> Result<Option<ReverseIndexItem>> {
+    unimplemented!();
+  }
+
+  pub async fn find_reverse_index_by_hash(
+    &self,
+    blob_hash: &str,
+  ) -> Result<Vec<ReverseIndexItem>> {
+    unimplemented!();
+  }
+
+  pub async fn remove_reverse_index_item(&self, holder: &str) -> Result<()> {
+    unimplemented!();
+  }
+}
