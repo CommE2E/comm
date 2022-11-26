b7272eb60aec8b2a4a628952ea8c6de3a52ad231
Bartomiej Klocek <bartlomiej.klocek@swmansion.com>
2022-11-25 16:08:52 -0800

    [services][blob] Add AWS SDK config
    Summary:
    Added a function to retrieve SDK config, required for accessing
    the S3 and DynamoDB SDKs. It is unused yet, but will be needed
    later.
    
    Conditionally, if `COMM_SERVICES_SANDBOX` environment variable is set,
    it uses localstack, which reflects the
    [current C++ implementation](https://github.com/CommE2E/comm/blob/2007dbe05b7d41d1603b74ccbef515e6942b1d21/services/lib/src/DynamoDBTools.cpp).
    
    Depends on D5677
    
    Test Plan: This function is tested a few diffs later, when S3 / DynamoDB are used.
    
    Reviewers: varun, jon, tomek, marcin, ashoat
    
    Reviewed By: varun, tomek, ashoat
    
    Subscribers: ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D5678

--- services/blob/src/constants.rs
+++ services/blob/src/constants.rs
@@ -1,1 +1,7 @@
 pub const GRPC_SERVER_DEFAULT_PORT: u64 = 50051;
+
+pub const AWS_REGION: &str = "us-east-2";
+
+pub const LOCALSTACK_URL: &str = "http://localhost:4566";
+
+pub const SANDBOX_ENV_VAR: &str = "COMM_SERVICES_SANDBOX";
