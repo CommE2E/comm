b142b7f0580adba2763a10234a4125d93c907db4
Bartomiej Klocek <bartlomiej.klocek@swmansion.com>
2022-11-25 16:08:48 -0800

    [services][blob] Scaffold gRPC server
    Summary:
    Boilerplate for proto interface. The server listens on `50051` port,
    this is the default in [current C++ implementation for all services](https://github.com/CommE2E/comm/blob/2007dbe05b7d41d1603b74ccbef515e6942b1d21/services/lib/src/GlobalConstants.h#L28). In subsequent diffs
    I'll add possibility to configure this.
    
    Depends on D5676
    
    Test Plan:
    Run the service. Client should be able to send requests, server should send
    "unimplemented" error messages to client.
    
    Reviewers: varun, jon, tomek, marcin
    
    Reviewed By: varun, tomek
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D5677

--- services/blob/src/service.rs
+++ services/blob/src/service.rs
@@ -1,3 +1,41 @@
+use anyhow::Result;
+use blob::blob_service_server::BlobService;
+use std::pin::Pin;
+use tokio_stream::Stream;
+use tonic::{Request, Response, Status};
+
 pub mod blob {
   tonic::include_proto!("blob");
+}
+
+#[derive(Default)]
+pub struct MyBlobService {}
+
+// gRPC implementation
+#[tonic::async_trait]
+impl BlobService for MyBlobService {
+  type PutStream =
+    Pin<Box<dyn Stream<Item = Result<blob::PutResponse, Status>> + Send>>;
+  async fn put(
+    &self,
+    _request: Request<tonic::Streaming<blob::PutRequest>>,
+  ) -> Result<Response<Self::PutStream>, Status> {
+    Err(Status::unimplemented("Not implemented yet"))
+  }
+
+  type GetStream =
+    Pin<Box<dyn Stream<Item = Result<blob::GetResponse, Status>> + Send>>;
+  async fn get(
+    &self,
+    _request: Request<blob::GetRequest>,
+  ) -> Result<Response<Self::GetStream>, Status> {
+    Err(Status::unimplemented("Not implemented yet"))
+  }
+
+  async fn remove(
+    &self,
+    _request: Request<blob::RemoveRequest>,
+  ) -> Result<Response<()>, Status> {
+    Err(Status::unimplemented("Not implemented yet"))
+  }
 }
