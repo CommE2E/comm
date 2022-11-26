869b273a9bfdbbbff61fd9658f134283c87b45b4
Bartomiej Klocek <bartlomiej.klocek@swmansion.com>
2022-11-25 16:09:23 -0800

    [services][blob] Add helpers and constants for Get RPC
    Summary:
    - Added helper convenience functions to get S3 path from database
    - Copied constants from [C++ service](https://github.com/CommE2E/comm/blob/de3b477b669f67ac685f6787606f73557f9d9ac2/services/lib/src/GlobalConstants.h#L8-L21) with comments
    
    Depends on D5694
    
    Test Plan: These functions are tested in next diff when they are used.
    
    Reviewers: varun, jon, tomek, marcin
    
    Reviewed By: tomek
    
    Subscribers: ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D5700

--- services/blob/src/constants.rs
+++ services/blob/src/constants.rs
@@ -3,6 +3,26 @@
 pub const GRPC_SERVER_DEFAULT_PORT: u64 = 50051;
 pub const AWS_REGION: &str = "us-east-2";
 pub const LOCALSTACK_URL: &str = "http://localhost:4566";
+pub const MPSC_CHANNEL_BUFFER_CAPACITY: usize = 1;
+
+/// 4MB limit
+///
+/// WARNING: use keeping in mind that grpc adds its own headers to messages
+/// https://github.com/grpc/grpc/blob/master/doc/PROTOCOL-HTTP2.md
+/// so the message that actually is being sent over the network looks like this
+/// ```
+/// [Compressed-Flag] [Message-Length] [Message]
+///    [Compressed-Flag]   1 byte  - added by grpc
+///    [Message-Length]    4 bytes - added by grpc
+///    [Message]           N bytes - actual data
+/// ```
+/// so for every message we get 5 additional bytes of data
+/// as [mentioned here](https://github.com/grpc/grpc/issues/15734#issuecomment-396962671),
+/// gRPC stream may contain more than one message
+pub const GRPC_CHUNK_SIZE_LIMIT: u64 = 4 * 1024 * 1024;
+
+/// See [`GRPC_CHUNK_SIZE_LIMIT`] description for details
+pub const GRPC_METADATA_SIZE_PER_MESSAGE: u64 = 5;
 
 // DynamoDB constants
 
