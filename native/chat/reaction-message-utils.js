1822281a8e27e5061fc2019307d217d4c0193469
Ginsu Eddy <ginsu.eddy@gmail.com>
2022-11-30 15:11:04 -0500

    [native] introduce onPressReact to reaction message utils
    Summary:
    introduce `onPressReact` function to new file called `reaction-message-utils`. Some things to note about this method, for now, I am hardcoding the like emoji since at this current state we only support liking a message. I am thinking that this will change in subsequent diffs when I introduce "unliking" and different reactions. Also, since we are not doing retry behavior for reactions (at least for now) I figured the best way to handle an error for the user would be to show an alert saying that the reaction couldn't be sent. This is something we also do in [[ https://github.com/CommE2E/comm/blob/191e6dfea10fe633f2546d13f6abde7637487483/native/chat/message-report-utils.js | message report utils ]]
    
    ---
    
    Depends on D5770 and D5769
    
    Linear Task:
    [[ https://linear.app/comm/issue/ENG-2246/liking-a-message | ENG-2246 ]]
    
    Test Plan:
    This will be tested further in subsequent diffs. Ran this locally on native and nothing crashed. Also helped me get to the point where I could push a reaction message into the DB (see screenshot below)
    
    {F267584}
    
    Reviewers: atul, rohan, tomek
    
    Reviewed By: atul, tomek
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D5780

---
+++ native/chat/reaction-message-utils.js
@@ -0,0 +1,81 @@
+// @flow
+
+import invariant from 'invariant';
+import Alert from 'react-native/Libraries/Alert/Alert';
+
+import {
+  sendReactionMessage,
+  sendReactionMessageActionTypes,
+} from 'lib/actions/message-actions';
+import type { BindServerCall, DispatchFunctions } from 'lib/utils/action-utils';
+
+import type { TooltipRoute } from '../navigation/tooltip.react';
+
+function onPressReact(
+  route:
+    | TooltipRoute<'TextMessageTooltipModal'>
+    | TooltipRoute<'MultimediaMessageTooltipModal'>
+    | TooltipRoute<'RobotextMessageTooltipModal'>,
+  dispatchFunctions: DispatchFunctions,
+  bindServerCall: BindServerCall,
+) {
+  const messageID = route.params.item.messageInfo.id;
+  invariant(messageID, 'messageID should be set');
+
+  const threadID = route.params.item.threadInfo.id;
+  invariant(threadID, 'threadID should be set');
+
+  sendReaction(
+    messageID,
+    threadID,
+    '👍',
+    'add_reaction',
+    dispatchFunctions,
+    bindServerCall,
+  );
+}
+
+function sendReaction(
+  messageID: string,
+  threadID: string,
+  reaction: string,
+  action: 'add_reaction' | 'remove_reaction',
+  dispatchFunctions: DispatchFunctions,
+  bindServerCall: BindServerCall,
+) {
+  const callSendReactionMessage = bindServerCall(sendReactionMessage);
+
+  const reactionMessagePromise = (async () => {
+    try {
+      const result = await callSendReactionMessage({
+        threadID,
+        targetMessageID: messageID,
+        reaction,
+        action,
+      });
+      return {
+        serverID: result.id,
+        threadID,
+        time: result.newMessageInfo.time,
+        newMessageInfos: [result.newMessageInfo],
+      };
+    } catch (e) {
+      Alert.alert(
+        'Couldn’t send the reaction',
+        'Please try again later',
+        [{ text: 'OK' }],
+        {
+          cancelable: true,
+        },
+      );
+      throw e;
+    }
+  })();
+
+  dispatchFunctions.dispatchActionPromise(
+    sendReactionMessageActionTypes,
+    reactionMessagePromise,
+  );
+}
+
+export { onPressReact };
