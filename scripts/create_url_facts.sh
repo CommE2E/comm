92ec9560a7d1526619d8134a2af1f20d741f3767
Jonathan Ringer <jonringer117@gmail.com>
2022-11-30 16:28:18 -0800

    [Nix] Provide default for squalcal_url.json
    Summary:
    Add squalcal_url.json default with other facts.
    
    squalcal_url.json is being used a fallback router for "/" in
    this regard.
    
    May be redundant with D5846, but aligns with intent.
    
    https://linear.app/comm/issue/ENG-2260
    
    Depends on D5846
    
    Test Plan:
    ```
    rm keyserver/facts/*.json
    
    nix develop
    
    # Assert files were written, and content matches scripts/templates
    ls keyserver/facts
    cat keyserver/facts/*.json
    ```
    
    Reviewers: atul, varun
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D5847

--- scripts/create_url_facts.sh
+++ scripts/create_url_facts.sh
@@ -7,6 +7,7 @@ ROOT_DIR="${SCRIPT_DIR}/.."
 KEYSERVER_FACTS_DIR="${ROOT_DIR}/keyserver/facts"
 COMMAPP_URL_PATH="${KEYSERVER_FACTS_DIR}/commapp_url.json"
 LANDING_URL_PATH="${KEYSERVER_FACTS_DIR}/landing_url.json"
+SQUADCAL_URL_PATH="${KEYSERVER_FACTS_DIR}/squadcal_url.json"
 
 if [[ ! -d "$KEYSERVER_FACTS_DIR" ]]; then
   mkdir -p "$KEYSERVER_FACTS_DIR"
@@ -18,4 +19,8 @@ fi
 
 if [[ ! -f "$LANDING_URL_PATH" ]]; then
   cp "$SCRIPT_DIR/templates/landing_url.json" "$LANDING_URL_PATH"
+fi
+
+if [[ ! -f "$SQUADCAL_URL_PATH" ]]; then
+  cp "$SCRIPT_DIR/templates/squadcal_url.json" "$SQUADCAL_URL_PATH"
 fi
