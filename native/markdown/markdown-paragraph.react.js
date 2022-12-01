272876219926cf64ecfb1445b681049e26169c03
Rohan Khanderia <rohan.khanderia@gmail.com>
2022-12-01 17:05:17 -0500

    [native] Prevent text from being highlighted on press when if is no pressable
    Summary:
    We want to only define an onPress handler in `MarkdownParagraph` if there is a `pressable` in the message.
    
    Context: https://linear.app/comm/issue/ENG-2389/prevent-text-from-being-highlighted-on-press-when-if-is-no-pressable
    
    Test Plan:
    The before is attached in the Linear post, and I will attach the after video below.
    
    {F268801}
    
    Reviewers: atul, ginsu, ashoat
    
    Reviewed By: ashoat
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D5794

--- native/markdown/markdown-paragraph.react.js
+++ native/markdown/markdown-paragraph.react.js
@@ -4,6 +4,7 @@ import * as React from 'react';
 import { Text } from 'react-native';
 
 import { MessagePressResponderContext } from '../chat/message-press-responder-context';
+import { TextMessageMarkdownContext } from '../chat/text-message-markdown-context';
 import type { TextStyle } from '../types/styles';
 
 type Props = {
@@ -11,10 +12,20 @@ type Props = {
   +children: React.Node,
 };
 function MarkdownParagraph(props: Props): React.Node {
+  const textMessageMarkdownContext = React.useContext(
+    TextMessageMarkdownContext,
+  );
+
   const messagePressResponderContext = React.useContext(
     MessagePressResponderContext,
   );
-  const onPressMessage = messagePressResponderContext?.onPressMessage;
+
+  // We only want to define the onPress handler if the message
+  // has any kind of pressable in it
+  const onPressMessage = textMessageMarkdownContext?.markdownHasPressable
+    ? messagePressResponderContext?.onPressMessage
+    : null;
+
   return (
     <Text
       style={props.style}
