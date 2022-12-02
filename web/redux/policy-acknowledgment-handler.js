fb61360b4537ad4c768e32bfb579e2b6500f8869
xsanm <kkurowski.kk@gmail.com>
2022-12-02 17:07:05 +0100

    [web] policy acknowledgment handler
    Summary: Code responsible for showing/hiding modal with policy acceptance. Currently, only with Terms of Use & Privacy because it's the only police we have right now.
    
    Test Plan:
    1. Log in to web app
    2. Change `confirmed` field for logged user in policy table
    3. Modal should appear (not immediately but after any request will be sent to keyserver)
    4. `I accept` should close modal and update value on keyserver db
    6. User should not be able to close modal using any method
    
    Reviewers: tomek, atul
    
    Subscribers:
    
    Differential Revision: https://phab.comm.dev/D5830

---
+++ web/redux/policy-acknowledgment-handler.js
@@ -0,0 +1,41 @@
+// @flow
+
+import * as React from 'react';
+
+import { useModalContext } from 'lib/components/modal-provider.react.js';
+import { policyTypes } from 'lib/facts/policies.js';
+
+import TermsAndPrivacyModal from '../modals/terms-and-privacy-modal.react.js';
+import { useSelector } from './redux-utils.js';
+
+function PolicyAcknowledgmentHandler(): null {
+  const termsAndPrivacyState = useSelector(
+    state => state.userPolicies?.[policyTypes.tosAndPrivacyPolicy],
+  );
+  const [policyModalKey, setPolicyModalKey] = React.useState('');
+  const { pushModal, popModal, modals } = useModalContext();
+
+  const policyModalVisible = React.useMemo(
+    () => !!modals.find(modalEntry => modalEntry[1] === policyModalKey),
+    [modals, policyModalKey],
+  );
+
+  React.useEffect(() => {
+    if (!termsAndPrivacyState) {
+      return;
+    }
+
+    const { isAcknowledged } = termsAndPrivacyState;
+    if (!isAcknowledged && !policyModalVisible) {
+      const modalKey = pushModal(<TermsAndPrivacyModal />);
+      setPolicyModalKey(modalKey);
+    }
+    if (isAcknowledged && policyModalVisible) {
+      popModal();
+    }
+  }, [policyModalVisible, popModal, pushModal, termsAndPrivacyState]);
+
+  return null;
+}
+
+export default PolicyAcknowledgmentHandler;
