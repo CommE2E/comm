f5a6e302ef79cae2fb4ead202a57d1c676511dd1
Max Kalashnikoff <geekmaks@gmail.com>
2022-12-05 11:30:28 +0100

    [native] Add Tunnelbroker protobuf file compilation to the native client
    Summary: TBD
    
    Test Plan: TBD
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D5807

--- native/native_rust_library/build.rs
+++ native/native_rust_library/build.rs
@@ -1,5 +1,13 @@
 fn main() {
-  tonic_build::compile_protos("../../shared/protos/identity.proto")
+  tonic_build::configure()
+    .build_server(false)
+    .compile(
+      &[
+        "../../shared/protos/identity.proto",
+        "../../shared/protos/tunnelbroker.proto",
+      ],
+      &["../../shared/protos"],
+    )
     .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
   let _cxx_build =
     cxx_build::bridge("src/lib.rs").flag_if_supported("-std=c++14");
