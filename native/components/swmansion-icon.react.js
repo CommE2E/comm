e2509e83ceeabacf54f2f68bf1377994eb860f5b
Ashoat Tevosyan <ashoat@comm.app>
2022-12-05 14:58:47 -0700

    [native] [19/40] RN 0.70: Type @expo/vector-icons
    Summary:
    I checked out the latest `react-native-vector-icons` NPM package and pulled the glyph types out of there. Mostly handled the rest myself.
    
    Depends on D5912
    
    Test Plan: Flow
    
    Reviewers: tomek
    
    Subscribers: atul
    
    Differential Revision: https://phab.comm.dev/D5913

--- native/components/swmansion-icon.react.js
+++ native/components/swmansion-icon.react.js
@@ -4,7 +4,7 @@ import { createIconSetFromIcoMoon, type Icon } from '@expo/vector-icons';
 
 import icoMoonConfig from 'lib/shared/swmansion-icon-config.json';
 
-const SWMansionIcon: Class<Icon<string>> = createIconSetFromIcoMoon(
+const SWMansionIcon: Icon<string> = createIconSetFromIcoMoon(
   icoMoonConfig,
   'SWMansionIcons',
   'SWMansionIcons.ttf',
