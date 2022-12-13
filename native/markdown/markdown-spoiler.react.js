e66703f549b1591dba3e30133ae82453b792f8dd
Rohan Khanderia <rohan.khanderia@gmail.com>
2022-12-13 14:53:12 -0500

    [native] Break spoiler text into individual components for rendering
    Summary: Currently, if we wrap spoilers in a `GestureTouchableOpacity`, long spoilers will force a line break both before and after, causing some really bad UI. The purpose of this diff is to handle one of
    the potential solutions to this problem, where we tokenize the spoiler text by splitting each word into it's own `Text` component encompassed by a `GestureTouchableOpacity`. This becomes a little complicated
    with nested markdown within spoilers, so this is explained by the in-line comments. *This isn't in a state to be landed, but it helps demonstrate the exploration process.*
    
    All context is found in the Linear task: https://linear.app/comm/issue/ENG-2446/[exploration]-break-spoiler-text-into-individual-text-elements
    
    Test Plan: All of the testing videos / screenshots for both iOS and Android can be found in the Linear issue.
    
    Reviewers: ashoat, atul, michal, ginsu
    
    Subscribers:

--- native/markdown/markdown-spoiler.react.js
+++ native/markdown/markdown-spoiler.react.js
@@ -7,6 +7,7 @@ import { Text } from 'react-native';
 import type { ReactElement } from 'lib/shared/markdown';
 
 import { TextMessageMarkdownContext } from '../chat/text-message-markdown-context';
+import GestureTouchableOpacity from '../components/gesture-touchable-opacity.react';
 import { useStyles } from '../themes/colors';
 import { MarkdownContext } from './markdown-context';
 
@@ -69,15 +70,67 @@ function MarkdownSpoiler(props: MarkdownSpoilerProps): React.Node {
     parsedSpoilerIdentifier,
   ]);
 
-  const memoizedSpoiler = React.useMemo(() => {
-    return (
-      <Text onPress={onSpoilerClick} style={styleBasedOnSpoilerState}>
-        {text}
-      </Text>
-    );
-  }, [onSpoilerClick, styleBasedOnSpoilerState, text]);
+  // The idea is to break down the spoiler text into individual words,
+  // and then encompass each word within a Text component, and
+  // each text component within a GestureTouchableOpacity.
+  // This is a way we can resolve the issue of long spoilers
+  // breaking the layout of the message, as the spoiler would force
+  // itself onto a new line if it exceeded the width of a text
+  // message.
 
-  return memoizedSpoiler;
+  // The conditional for {index !== arrayOfSpoilerText.length - 1 ? ' ' : ''}
+  // is a way to not add a space after the last word in the spoiler text.
+
+  // Sample text with nested markdown:
+  // Hey this is a spoiler **and this is bold text**
+  // This is converted to:
+  // [
+  //    "Hey this is a spoiler ",
+  //    <Text style={{"fontWeight": "bold"}}>and this is bold text</Text>
+  // ]
+
+  // We split this ReactElement into an array of individual ReactElements
+  // and then map over each individual ReactElement and return it
+  // within a GestureTouchableOpacity.
+
+  const componentsArray: ReactElement[] = React.useMemo(() => {
+    return React.Children.map(text, (child, index) => {
+      // If the element is a string, we split it into an array of words
+      // and wrap each word in a Text within a GestureTouchableOpacity
+      if (typeof child === 'string') {
+        const words = child.split(' ');
+        return words.map((word, wordIndex) => {
+          return (
+            <GestureTouchableOpacity
+              key={`${index}-${wordIndex}`}
+              onPress={onSpoilerClick}
+            >
+              <Text
+                key={`${index}-${wordIndex}`}
+                style={styleBasedOnSpoilerState}
+              >
+                {word}
+                {wordIndex !== words.length - 1 ? ' ' : ''}
+              </Text>
+            </GestureTouchableOpacity>
+          );
+        });
+      }
+      // If it's a nested ReactElement,
+      // we return it within a GestureTouchableOpacity.
+      // We preserve the structure of the nested ReactElement
+      // since we don't want to break the markdown.
+      return (
+        <GestureTouchableOpacity key={index} onPress={onSpoilerClick}>
+          <Text key={index} style={styleBasedOnSpoilerState}>
+            {child}
+          </Text>
+        </GestureTouchableOpacity>
+      );
+    });
+  }, [text, onSpoilerClick, styleBasedOnSpoilerState]);
+
+  return componentsArray;
 }
 
 const unboundStyles = {
