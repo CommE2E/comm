44f702fc8f8e79585c15f4d8c1d827c17b7f31b2
Rohan Khanderia <rohan.khanderia@gmail.com>
2022-12-14 20:54:05 -0500

    [web] Update thread promotion modal on web
    Summary:
    This diff updates the thread promotion modal on web, following the Design task.
    
    1. Updated modal_body padding to `24px 32px 32px 32px`
    2. Make the button the usual purple color
    3. Remove the line break between the text
    
    Design Task: https://linear.app/comm/issue/DES-14/thread-promote-button-modal-looks-scary
    Engineering Task: https://linear.app/comm/issue/ENG-2410/update-thread-promotion-modal-on-web
    
    Test Plan:
    See screenshots below
    
    Before:
    {F285631}
    
    After:
    {F285630}
    
    Reviewers: atul, ginsu, ashoat
    
    Reviewed By: ashoat
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D5874

--- web/modals/chat/sidebar-promote-modal.react.js
+++ web/modals/chat/sidebar-promote-modal.react.js
@@ -4,7 +4,7 @@ import * as React from 'react';
 
 import type { ThreadInfo } from 'lib/types/thread-types';
 
-import Button, { buttonThemes } from '../../components/button.react';
+import Button from '../../components/button.react';
 import Modal from '../modal.react';
 import css from './sidebar-promote-modal.css';
 
@@ -31,18 +31,15 @@ function SidebarPromoteModal(props: Props): React.Node {
       onClose={onClose}
     >
       <div className={css.modal_body}>
-        <p>{`Are you sure you want to promote "${uiName}"?`}</p>
-        <p>Promoting a thread to a channel cannot be undone.</p>
+        <p>
+          {`Are you sure you want to promote "${uiName}"? 
+          Promoting a thread to a channel cannot be undone.`}
+        </p>
         <div className={css.buttonContainer}>
           <Button onClick={onClose} type="submit" variant="outline">
             Cancel
           </Button>
-          <Button
-            onClick={handleConfirm}
-            type="submit"
-            variant="filled"
-            buttonColor={buttonThemes.danger}
-          >
+          <Button onClick={handleConfirm} type="submit" variant="filled">
             Promote to channel
           </Button>
         </div>
