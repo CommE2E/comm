d64f7d4d179cd1f5ff80ed7cb1e8aa6b3da4a040
Max Kalashnikoff <geekmaks@gmail.com>
2022-12-19 15:46:17 +0100

    [services/commtest] Tunnelbroker - Adding `deviceID` validation check function
    Summary:
    This diff introduces a function that makes a call to the Tunnelbroker (get signature API) with the `deviceID` in the wrong format.
    In this case, we are making sure that the Tunnelbroker returns an error on the wrong format `deviceID` provided.
    This function is called during the test in D5931.
    
    Linear task: [[ https://linear.app/comm/issue/ENG-1657 | ENG-1657 ]]
    
    Test Plan: The `commtest` app is successfully built and the tests are successfully passed in the following D5931 where the function is used.
    
    Reviewers: jon, bartek
    
    Reviewed By: jon
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D5929

--- services/commtest/src/tunnelbroker/tunnelbroker_utils.rs
+++ services/commtest/src/tunnelbroker/tunnelbroker_utils.rs
@@ -1,9 +1,11 @@
+use anyhow::bail;
 use std::env;
 use tonic::transport::Channel;
 use tonic::transport::Error;
 pub mod proto {
   tonic::include_proto!("tunnelbroker");
 }
+use super::new_session::get_string_to_sign;
 pub use proto::tunnelbroker_service_client::TunnelbrokerServiceClient;
 
 pub async fn tonic_client_builder(
@@ -13,4 +15,15 @@ pub async fn tonic_client_builder(
   let host = env::var("COMM_SERVICES_HOST_TUNNELBROKER")
     .unwrap_or(String::from("localhost"));
   TunnelbrokerServiceClient::connect(format!("http://{}:{}", host, port)).await
+}
+
+pub async fn session_signature_device_id_format_validation(
+  client: &mut TunnelbrokerServiceClient<tonic::transport::Channel>,
+) -> anyhow::Result<()> {
+  let wrong_device_id =
+    "some:OOOQ7b2ueEmQ4QsevRWlXxFCNt055y20T1PHdoYAQRt0S6TLzZWNM6XSvdWqxm";
+  if let Ok(_) = get_string_to_sign(client, wrong_device_id).await {
+    bail!("Got success result on wrong deviceID format")
+  }
+  Ok(())
 }
