89b1b3578fcf12d362bd8a55af8b0230f6d20cc6
Max Kalashnikoff <geekmaks@gmail.com>
2022-12-19 15:42:32 +0100

    [services/commtest] Tunnelbroker - Adding the tonic client builder function
    Summary:
    This diff introduces a function to build a Tonic client for further API calls in integrational tests. The pointer to the client will be passed to the helper functions (in D5931) instead of making the "global" client instance.
    
    We are using `COMM_SERVICES_PORT_TUNNELBROKER` environment variable to provide a port to connect to the Tunnelbroker.
    The `COMM_SERVICES_HOST_TUNNELBROKER` environment variable provides a host to connect to the Tunnelbroker.
    
    Linear task: [[ https://linear.app/comm/issue/ENG-1657 | ENG-1657 ]]
    
    Test Plan: The `commtest` app is successfully built and the tests are successfully passed in the following D5931 using this function results.
    
    Reviewers: jon, bartek
    
    Reviewed By: jon, bartek
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D5928

--- services/commtest/src/tunnelbroker/tunnelbroker_utils.rs
+++ services/commtest/src/tunnelbroker/tunnelbroker_utils.rs
@@ -1,4 +1,16 @@
+use std::env;
+use tonic::transport::Channel;
+use tonic::transport::Error;
 pub mod proto {
   tonic::include_proto!("tunnelbroker");
 }
 pub use proto::tunnelbroker_service_client::TunnelbrokerServiceClient;
+
+pub async fn tonic_client_builder(
+) -> Result<TunnelbrokerServiceClient<Channel>, Error> {
+  let port = env::var("COMM_SERVICES_PORT_TUNNELBROKER")
+    .unwrap_or(String::from("50051"));
+  let host = env::var("COMM_SERVICES_HOST_TUNNELBROKER")
+    .unwrap_or(String::from("localhost"));
+  TunnelbrokerServiceClient::connect(format!("http://{}:{}", host, port)).await
+}
