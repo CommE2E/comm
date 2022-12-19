defc668d683c122e2df45d328e19f093a14fcd17
Max Kalashnikoff <geekmaks@gmail.com>
2022-12-19 15:50:33 +0100

    [services/commtest] Tunnelbroker - Adding the API integration tests call function
    Summary:
    This diff introduces an integrational tests call for the Tunnelbroker API:
    1. We are checking for the wrong formatted `deviceID`: the server should return an error.
    2. Generating a keypair for making a signature.
    3. Getting a string to be signed from the Tunnelbroker server for the `deviceID`.
    4. Checking for the server error in case the provided signature is wrong.
    5. Create a new session (getting `sessionID`) by calling the Tunnelbroker new session API call.
    
    Linear task: [[ https://linear.app/comm/issue/ENG-1657 | ENG-1657 ]]
    
    Test Plan:
    1. The `commtest` app is successfully built.
    2. To run tests:
      - Start the Tunnelbroker server using the `nix develop --accept-flake-config -c bash -c "cargo run"` command in the `services/tunnelbroker` directory.
      - Open another terminal and run the following command from the `services/commtest` directory:
    `nix develop --accept-flake-config -c bash -c "cargo t --test tunnelbroker_integration_test -- --nocapture"`
    
    The expected result is that the `commapp` will be running the tests for the Tunnelbroker API.
    
    Reviewers: jon, bartek
    
    Reviewed By: jon, bartek
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D5931

--- services/commtest/tests/tunnelbroker_integration_test.rs
+++ services/commtest/tests/tunnelbroker_integration_test.rs
@@ -1,4 +1,68 @@
+use commtest::tunnelbroker::{
+  new_session::create_new_session,
+  new_session::get_string_to_sign,
+  new_session::sign_string_with_private_key,
+  tunnelbroker_utils::{
+    proto::new_session_request::DeviceTypes,
+    session_signature_device_id_format_validation, tonic_client_builder,
+  },
+};
+use openssl::pkey::PKey;
+use openssl::rsa::Rsa;
+
 #[tokio::test]
-async fn tunnelbroker_integration_test() {
-  assert!(false, "not implemented");
+async fn tunnelbroker_integration_test() -> Result<(), anyhow::Error> {
+  const DEVICE_ID: &str =
+    "mobile:OOOTESTb2ueEmQ4QsevRWlXxFCNt055y20T1PHdoYAQRt0S6TLzZWNM6XSvdW000";
+
+  // Should fail on the wrong device ID format provided
+  let mut client = tonic_client_builder().await?;
+  assert!(
+    session_signature_device_id_format_validation(&mut client)
+      .await
+      .is_ok(),
+    "DeviceID format validation failed on getting session signature request"
+  );
+
+  // Generate a keypair
+  let keypair = Rsa::generate(1024)?;
+  let keypair = PKey::from_rsa(keypair)?;
+  let public_key_pem = String::from_utf8(keypair.public_key_to_pem()?)?;
+
+  // Get and sign the string to be signed
+  let string_to_be_signed = get_string_to_sign(&mut client, DEVICE_ID).await?;
+  let signature_base64 =
+    sign_string_with_private_key(&keypair, &string_to_be_signed)?;
+
+  // Should fail on the wrong signature provided
+  assert!(
+    create_new_session(
+      &mut client,
+      DEVICE_ID,
+      &public_key_pem,
+      "wrong_signature",
+      "fake_notify_token",
+      DeviceTypes::Mobile,
+      "v.x.x.x",
+      "iOS x.x.x",
+    )
+    .await
+    .is_err(),
+    "New session returns success when using wrong signature"
+  );
+
+  // Create a new session with the correct signature
+  let _session_id = create_new_session(
+    &mut client,
+    DEVICE_ID,
+    &public_key_pem,
+    &signature_base64,
+    "fake_notify_token",
+    DeviceTypes::Mobile,
+    "v.x.x.x",
+    "iOS x.x.x",
+  )
+  .await?;
+
+  Ok(())
 }
