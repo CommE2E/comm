ab97236145f71d93b0ab204de93bcae13fd532bf
xsanm <kkurowski.kk@gmail.com>
2022-12-21 11:58:37 +0100

    [native] add function to initialize `DatabaseQueryExecutor`
    Summary:
    Implementing approach described in [ENG-1974](https://linear.app/comm/issue/ENG-1974/analysis-of-errors-in-sqlitequeryexecutor).
    This function will be called separately in iOS and Android so this time separating function from it's call to make it easier for reviewers.
    
    Test Plan:
    1. Make it possible to throw and exception on `migrate()` function on demand.
    2. Call this function.
    3. Build the app for both platforms
    4. Check if:
    - `getQueryExecutor()` works properly -> db status = `WORKABLE`
    - `getQueryExecutor()` throws exception and there is nothing in store -> db status = 'FIRST_FAILURE' and app is terminated
    - `getQueryExecutor()` throws exception and there `WORKABLE` -> db status = 'FIRST_FAILURE' and app is terminated
    - `getQueryExecutor()` throws exception and there `FIRST_FAILURE` -> db status = 'SECOND_FAILURE' and app proceed
    - `getQueryExecutor()` throws exception and there `SECOND_FAILURE` -> db status = 'SECOND_FAILURE' and app proceed
    
    Reviewers: marcin
    
    Differential Revision: https://phab.comm.dev/D5991

--- native/cpp/CommonCpp/DatabaseManagers/DatabaseManager.cpp
+++ native/cpp/CommonCpp/DatabaseManagers/DatabaseManager.cpp
@@ -1,13 +1,56 @@
 #include "DatabaseManager.h"
+#include "../Tools/CommSecureStore.h"
+#include "../Tools/TerminateApp.h"
+#include "Logger.h"
 #include "SQLiteQueryExecutor.h"
 
 namespace comm {
+
+typedef const std::string DatabaseManagerStatus;
+DatabaseManagerStatus DB_MANAGER_WORKABLE = "WORKABLE";
+DatabaseManagerStatus DB_MANAGER_FIRST_FAILURE = "FIRST_FAILURE";
+DatabaseManagerStatus DB_MANAGER_SECOND_FAILURE = "SECOND_FAILURE";
+
+const std::string DATABASE_MANAGER_STATUS_KEY = "DATABASE_MANAGER_STATUS";
 
 const DatabaseQueryExecutor &DatabaseManager::getQueryExecutor() {
   //  TODO: conditionally create desired type of db manager
   //  maybe basing on some preprocessor flag
   thread_local SQLiteQueryExecutor instance;
   return instance;
+}
+
+void handleFirstFailure(comm::CommSecureStore commSecureStore) {
+  commSecureStore.set(DATABASE_MANAGER_STATUS_KEY, DB_MANAGER_FIRST_FAILURE);
+  Logger::log("Database manager initialization issue, terminating app");
+  TerminateApp::terminate();
+}
+
+void DatabaseManager::initializeQueryExecutor() {
+  comm::CommSecureStore commSecureStore{};
+  try {
+    DatabaseManager::getQueryExecutor();
+    commSecureStore.set(DATABASE_MANAGER_STATUS_KEY, DB_MANAGER_WORKABLE);
+    Logger::log("Database manager initialized");
+  } catch (...) {
+    folly::Optional<std::string> databaseManagerStatus =
+        commSecureStore.get(DATABASE_MANAGER_STATUS_KEY);
+    if (!databaseManagerStatus.hasValue()) {
+      handleFirstFailure(commSecureStore);
+    }
+    std::string databaseManagerStatusValue = databaseManagerStatus.value();
+    if (databaseManagerStatusValue == DB_MANAGER_WORKABLE) {
+      handleFirstFailure(commSecureStore);
+    }
+    if (databaseManagerStatusValue == DB_MANAGER_FIRST_FAILURE) {
+      commSecureStore.set(
+          DATABASE_MANAGER_STATUS_KEY, DB_MANAGER_SECOND_FAILURE);
+      Logger::log(
+          "Database manager initialization issue, app proceeding, but "
+          "database needs to be deleted");
+      return;
+    }
+  }
 }
 
 } // namespace comm
