407f2a2c7d022e4f8dda1287407afdcf35e79609
Max Kalashnikoff <geekmaks@gmail.com>
2022-12-23 18:29:26 +0100

    [native/rust_library] Removing of testing `hello.c` and increase C++ standard to 17 in `build.rs`
    Summary:
    This diff introduces the following changes to the Cargo `build.rs`:
    - Removing the testing building of the `hello.c` [[ https://phab.comm.dev/D5807?id=19130#inline-40604 | based on the comment ]];
    - Increasing the C++ standard to 17, [[ https://phab.comm.dev/D5899?id=19462#inline-39787 | because RN now supports it ]].
    
    Linear task: [[ https://linear.app/comm/issue/ENG-2536/refactoring-and-minor-fixes-of-the-native-rust-library | ENG-2536 ]]
    
    Test Plan: Native CI gates are passed.
    
    Reviewers: varun, jon
    
    Reviewed By: varun, jon
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D6014

--- native/native_rust_library/build.rs
+++ native/native_rust_library/build.rs
@@ -10,6 +10,9 @@ fn main() {
     )
     .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
   let _cxx_build =
-    cxx_build::bridge("src/lib.rs").flag_if_supported("-std=c++14");
-  println!("cargo:rerun-if-changed=src/hello.c");
+    cxx_build::bridge("src/lib.rs").flag_if_supported("-std=c++17");
+
+  println!("cargo:rerun-if-changed=src/lib.rs");
+  println!("cargo:rerun-if-changed=../../shared/protos/identity.proto");
+  println!("cargo:rerun-if-changed=../../shared/protos/tunnelbroker.proto");
 }
