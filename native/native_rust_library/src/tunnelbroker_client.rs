1f8e559f412bcfccdf66ff5ef3c2683d4b5f9193
Max Kalashnikoff <geekmaks@gmail.com>
2022-12-27 12:30:23 +0100

    [native/rust_library] Tunnelbroker client - Adding of the `get_new_session_id` function
    Summary:
    
    TBD
    
    Test Plan:
    
    TBD
    
    Reviewers:
    
    Subscribers:

--- native/native_rust_library/src/tunnelbroker_client.rs
+++ native/native_rust_library/src/tunnelbroker_client.rs
@@ -1,6 +1,9 @@
 use crate::TunnelbrokerClient;
 
-use crate::tunnelbroker::SessionSignatureRequest;
+use crate::tunnelbroker::NewSessionRequest;
+use crate::tunnelbroker::{
+  new_session_request::DeviceTypes, SessionSignatureRequest,
+};
 
 pub async fn get_nonce_to_sign(
   mut client: Box<TunnelbrokerClient>,
@@ -13,5 +16,33 @@ pub async fn get_nonce_to_sign(
       .await?
       .into_inner()
       .to_sign,
+  )
+}
+
+pub async fn get_new_session_id(
+  mut client: Box<TunnelbrokerClient>,
+  device_id: String,
+  public_key: String,
+  signature: String,
+  device_type: DeviceTypes,
+  device_app_version: String,
+  device_os: String,
+  notify_token: Option<String>,
+) -> Result<String, tonic::Status> {
+  Ok(
+    client
+      .tunnelbroker_client
+      .new_session(NewSessionRequest {
+        device_id,
+        public_key,
+        signature,
+        notify_token,
+        device_type: device_type as i32,
+        device_app_version,
+        device_os,
+      })
+      .await?
+      .into_inner()
+      .session_id,
   )
 }
