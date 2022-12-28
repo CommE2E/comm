440d71f06ec7242f9cac9996a66a515fc462d5ea
marcinwasowicz <marcinkwasowicz@gmail.com>
2022-12-28 11:53:33 +0100

    CommIOSNotifications module functionality definition
    Summary: This differential puts foundation for work on react-native-notifications replacement on iOS. It introduces necessary file group to XCode and introduces header file that defines notifications related API used across native code on iOS.
    
    Test Plan: No test plan for this differential.
    
    Reviewers: tomek, atul
    
    Reviewed By: tomek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D6064

---
+++ native/ios/Comm/CommIOSNotifications/CommIOSNotifications.h
@@ -0,0 +1,23 @@
+@import UIKit;
+
+#import <PushKit/PushKit.h>
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
+
+@interface CommIOSNotifications : RCTEventEmitter <RCTBridgeModule>
+@property(nonatomic, strong) NSMutableDictionary *remoteNotificationCallbacks;
+@property(nonatomic) BOOL hasListeners;
+
+typedef void (^RCTRemoteNotificationCallback)(UIBackgroundFetchResult result);
+
++ (void)didRegisterForRemoteNotificationsWithDeviceToken:(id)deviceToken;
++ (void)didFailToRegisterForRemoteNotificationsWithError:(NSError *)error;
++ (void)didReceiveRemoteNotification:(NSDictionary *)notification
+              fetchCompletionHandler:
+                  (void (^)(UIBackgroundFetchResult))completionHandler;
++ (void)clearNotificationFromNotificationsCenter:(NSString *)notificationId
+                               completionHandler:
+                                   (void (^)(UIBackgroundFetchResult))
+                                       completionHandler;
+
+@end
