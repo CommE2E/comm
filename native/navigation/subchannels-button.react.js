50fd4f6c76910415121130077b7ff3287aacd848
Inka Sokołowska <inka.sokolowska@swmansion.com>
2022-12-29 18:09:05 +0100

    [native] Add subchannels button
    Summary:
    Linear issue: https://linear.app/comm/issue/ENG-2284/add-links-to-subchannels-in-level-2-chats
    Add a button that will be used in the community drawer to navigate to the subchannels modal
    
    {F301620}
    
    Test Plan: Added it to every community item and saw that pressing it navigates to the subchannels modal
    
    Reviewers: ginsu, bartek, tomek
    
    Reviewed By: tomek
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D6001

---
+++ native/navigation/subchannels-button.react.js
@@ -0,0 +1,63 @@
+// @flow
+
+import Icon from '@expo/vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
+import * as React from 'react';
+import { TouchableOpacity, Text, View } from 'react-native';
+
+import type { ThreadInfo } from 'lib/types/thread-types';
+
+import { useStyles } from '../themes/colors';
+import { SubchannelsListModalRouteName } from './route-names';
+
+type Props = {
+  +threadInfo: ThreadInfo,
+};
+
+function SubchnnelsButton(props: Props): React.Node {
+  const styles = useStyles(unboundStyles);
+
+  const { threadInfo } = props;
+  const { navigate } = useNavigation();
+
+  const onPress = React.useCallback(
+    () =>
+      navigate<'SubchannelsListModal'>({
+        name: SubchannelsListModalRouteName,
+        params: { threadInfo },
+      }),
+    [navigate, threadInfo],
+  );
+
+  return (
+    <TouchableOpacity style={styles.view} onPress={onPress}>
+      <View style={styles.iconWrapper}>
+        <Icon name="corner-down-right" style={styles.label} size={16} />
+      </View>
+      <Text style={styles.label}>Subchannels</Text>
+    </TouchableOpacity>
+  );
+}
+
+const unboundStyles = {
+  view: {
+    flexDirection: 'row',
+  },
+  label: {
+    color: 'drawerExpandButton',
+    fontWeight: '500',
+    fontSize: 12,
+    lineHeight: 18,
+  },
+  iconWrapper: {
+    height: 16,
+    width: 16,
+    alignItems: 'center',
+  },
+  icon: {
+    color: 'drawerExpandButton',
+    marginRight: 2,
+  },
+};
+
+export default SubchnnelsButton;
