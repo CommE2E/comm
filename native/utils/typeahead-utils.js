403eed130c16f0cb274966e19fb78233f5f0dfa5
pweglik <przemek.weglik@onet.pl>
2023-01-03 13:19:09 +0100

    [2/n] Native Typeahead - Adding utility file and native regex
    Summary:
    Native regex is a little bit different than web one as there are no named captruing groups yet on native.
    See: https://linear.app/comm/issue/ENG-2456/rewrite-typeahead-regexes-when-named-capturing-groups-are-released-for#comment-4bb6cf1e
    
    Test Plan: Checked if regex works in online editor.
    
    Reviewers: inka, ginsu, kamil, michal
    
    Subscribers: ashoat, tomek, atul, michal, kamil, ginsu, inka
    
    Differential Revision: https://phab.comm.dev/D6298

---
+++ native/utils/typeahead-utils.js
@@ -0,0 +1,11 @@
+// @flow
+
+import { oldValidUsernameRegexString } from 'lib/shared/account-utils';
+
+// Native regex is a little bit different than web one as
+// there are no named captruing groups yet on native.
+const nativeTypeaheadRegex: RegExp = new RegExp(
+  `((^(.|\n)*\\s+)|^)@(${oldValidUsernameRegexString})?$`,
+);
+
+export { nativeTypeaheadRegex };
