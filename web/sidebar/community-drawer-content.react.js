a14e69d78f2862dff7d63c232192ca738b407b7c
Inka Sokołowska <inka.sokolowska@swmansion.com>
2023-01-04 15:29:19 +0100

    [web] Add community drawer
    Summary:
    Linear issue: https://linear.app/comm/issue/ENG-2631/drawer-sidebar-inside-of-community-picker
    Adding the community drawer.
    Adding the community drawer on native: D5723 and D5732
    
    Test Plan:
    added community drawer to AppsDirectory, checked that it behaves as expected.
    
    Reviewers:
    przemek, michal, ginsu
    
    Subscribers:

---
+++ web/sidebar/community-drawer-content.react.js
@@ -0,0 +1,57 @@
+// @flow
+
+import * as React from 'react';
+import { useSelector } from 'react-redux';
+
+import {
+  childThreadInfos,
+  communityThreadSelector,
+} from 'lib/selectors/thread-selectors';
+import {
+  createRecursiveDrawerItemsData,
+  appendSuffix,
+} from 'lib/utils/drawer-utils.react';
+
+import css from './community-drawer-content.css';
+import CommunityDrawerItemCommunity from './community-drawer-item-community.react';
+
+const maxDepth = 2;
+const labelStyles = ['titleLevel0', 'titleLevel1', 'titleLevel2'];
+
+function CommunityDrawerContent(): React.Node {
+  const childThreadInfosMap = useSelector(childThreadInfos);
+  const communities = useSelector(communityThreadSelector);
+  const communitiesSuffixed = React.useMemo(() => appendSuffix(communities), [
+    communities,
+  ]);
+
+  const drawerItemsData = createRecursiveDrawerItemsData(
+    childThreadInfosMap,
+    communitiesSuffixed,
+    labelStyles,
+    maxDepth,
+  );
+
+  const [openCommunity, setOpenCommunity] = React.useState(
+    communitiesSuffixed.length === 1 ? communitiesSuffixed[0].id : null,
+  );
+
+  const setOpenCommunnityOrClose = React.useCallback((index: string) => {
+    setOpenCommunity(open => (open === index ? null : index));
+  }, []);
+
+  const communitiesComponents = drawerItemsData.map(item => {
+    return (
+      <CommunityDrawerItemCommunity
+        itemData={item}
+        key={item.threadInfo.id}
+        toggleExpanded={setOpenCommunnityOrClose}
+        expanded={item.threadInfo.id === openCommunity}
+      />
+    );
+  });
+
+  return <div className={css.container}>{communitiesComponents}</div>;
+}
+
+export default CommunityDrawerContent;
