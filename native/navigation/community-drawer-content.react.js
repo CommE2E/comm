6ebf83f5505b32bccade621e904484eeba9a3b52
Inka Sokołowska <inka.sokolowska@swmansion.com>
2023-01-04 12:10:14 +0100

    [native, lib] Move community drawer related code that can be reused on web from native to lib
    Summary: Some code used for community drawer on native can be reused for community drawer on web, so I moved it to lib.
    
    Test Plan: run iOS simulator, checked that communinty drawer works as expected.
    
    Reviewers: ginsu, przemek, michal
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D6156

--- native/navigation/community-drawer-content.react.js
+++ native/navigation/community-drawer-content.react.js
@@ -9,12 +9,13 @@ import {
   childThreadInfos,
   communityThreadSelector,
 } from 'lib/selectors/thread-selectors';
-import { threadIsChannel } from 'lib/shared/thread-utils';
-import { type ThreadInfo, communitySubthreads } from 'lib/types/thread-types';
+import {
+  createRecursiveDrawerItemsData,
+  appendSuffix,
+} from 'lib/utils/drawer-utils.react';
 
 import { useNavigateToThread } from '../chat/message-list-types';
 import { useStyles } from '../themes/colors';
-import type { TextStyle } from '../types/styles';
 import CommunityDrawerItemCommunity from './community-drawer-item-cummunity.react';
 
 const maxDepth = 2;
@@ -43,18 +44,12 @@ function CommunityDrawerContent(): React.Node {
 
   const renderItem = React.useCallback(
     ({ item }) => {
-      const itemData = {
-        threadInfo: item.threadInfo,
-        itemChildren: item.itemChildren,
-        labelStyle: item.labelStyle,
-        hasSubchannelsButton: item.subchannelsButton,
-      };
       return (
         <CommunityDrawerItemCommunity
-          key={item.key}
-          itemData={itemData}
+          key={item.threadInfo.id}
+          itemData={item}
           toggleExpanded={setOpenCommunnityOrClose}
-          expanded={itemData.threadInfo.id === openCommunity}
+          expanded={item.threadInfo.id === openCommunity}
           navigateToThread={navigateToThread}
         />
       );
@@ -78,6 +73,7 @@ function CommunityDrawerContent(): React.Node {
         childThreadInfosMap,
         communitiesSuffixed,
         labelStyles,
+        maxDepth,
       ),
     [childThreadInfosMap, communitiesSuffixed, labelStyles],
   );
@@ -87,71 +83,6 @@ function CommunityDrawerContent(): React.Node {
       <FlatList data={drawerItemsData} renderItem={renderItem} />
     </SafeAreaView>
   );
-}
-
-function createRecursiveDrawerItemsData(
-  childThreadInfosMap: { +[id: string]: $ReadOnlyArray<ThreadInfo> },
-  communities: $ReadOnlyArray<ThreadInfo>,
-  labelStyles: $ReadOnlyArray<TextStyle>,
-) {
-  const result = communities.map(community => ({
-    key: community.id,
-    threadInfo: community,
-    itemChildren: [],
-    labelStyle: labelStyles[0],
-    subchannelsButton: false,
-  }));
-  let queue = result.map(item => [item, 0]);
-
-  for (let i = 0; i < queue.length; i++) {
-    const [item, lvl] = queue[i];
-    const itemChildThreadInfos = childThreadInfosMap[item.threadInfo.id] ?? [];
-
-    if (lvl < maxDepth) {
-      item.itemChildren = itemChildThreadInfos
-        .filter(childItem => communitySubthreads.includes(childItem.type))
-        .map(childItem => ({
-          threadInfo: childItem,
-          itemChildren: [],
-          labelStyle: labelStyles[Math.min(lvl + 1, labelStyles.length - 1)],
-          hasSubchannelsButton:
-            lvl + 1 === maxDepth &&
-            threadHasSubchannels(childItem, childThreadInfosMap),
-        }));
-      queue = queue.concat(
-        item.itemChildren.map(childItem => [childItem, lvl + 1]),
-      );
-    }
-  }
-  return result;
-}
-
-function threadHasSubchannels(
-  threadInfo: ThreadInfo,
-  childThreadInfosMap: { +[id: string]: $ReadOnlyArray<ThreadInfo> },
-) {
-  if (!childThreadInfosMap[threadInfo.id]?.length) {
-    return false;
-  }
-  return childThreadInfosMap[threadInfo.id].some(thread =>
-    threadIsChannel(thread),
-  );
-}
-
-function appendSuffix(chats: $ReadOnlyArray<ThreadInfo>): ThreadInfo[] {
-  const result = [];
-  const names = new Map<string, number>();
-
-  for (const chat of chats) {
-    let name = chat.uiName;
-    const numberOfOccurrences = names.get(name);
-    names.set(name, (numberOfOccurrences ?? 0) + 1);
-    if (numberOfOccurrences) {
-      name = `${name} (${numberOfOccurrences.toString()})`;
-    }
-    result.push({ ...chat, uiName: name });
-  }
-  return result;
 }
 
 const unboundStyles = {
