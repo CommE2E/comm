d44aa9b86949c1083c08c96104071e729ca6d578
Jonathan Ringer <jonringer117@gmail.com>
2023-01-06 15:18:18 -0800

    [Services] Lint list_services.sh
    Summary: To make the later fix easier to review, decided to lint this file.
    
    Test Plan: N/A, only \ and new lines were added
    
    Reviewers: varun, bartek, max
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D6191

--- services/scripts/list_services.sh
+++ services/scripts/list_services.sh
@@ -2,5 +2,15 @@
 
 set -e
 
-find . -maxdepth 1 ! -name "base-image" ! -name "docker-compose.yml" ! -name "package.json" ! -name "scripts" ! -name "node_modules" ! -name "commtest" ! -name "lib" ! -name "terraform" ! -name ".*" -execdir echo {} ';'
+find . -maxdepth 1 \
+  ! -name "base-image" \
+  ! -name "docker-compose.yml" \
+  ! -name "package.json" \
+  ! -name "scripts" \
+  ! -name "node_modules" \
+  ! -name "commtest" \
+  ! -name "lib" \
+  ! -name "terraform" \
+  ! -name ".*" \
+  -execdir echo {} ';'
 
