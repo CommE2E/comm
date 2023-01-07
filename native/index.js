1fefaeea8bc14806434cd7d6978bd8b3828b3824
Ashoat Tevosyan <ashoat@comm.app>
2023-01-06 20:46:16 -0500

    [native] Fix FlatList scroll perf issues on Android 13
    Summary: See context on [this Linear issue](https://linear.app/comm/issue/ENG-793#comment-5bf0f56b) and [this React Native GitHub issue](https://github.com/facebook/react-native/issues/30034). The original fix comes from [here](https://github.com/facebook/react-native/issues/30034#issuecomment-806396274), but in React Native 0.70 that style prop gets deprecated, so [this additional hack](https://github.com/facebook/react-native/issues/30034#issuecomment-1277360480) is necessary.
    
    Test Plan:
    Compiled Android release build on physical Android 13 device
    
    | before | action |
    | {F321185} | {F321205} |
    
    Reviewers: tomek, bartek, atul
    
    Reviewed By: tomek, bartek
    
    Differential Revision: https://phab.comm.dev/D6193

--- native/index.js
+++ native/index.js
@@ -1,6 +1,16 @@
 // @flow
 
+/* eslint-disable import/order */
+
 import 'react-native-gesture-handler';
+
+// react-native has an issue with inverted lists on Android, and it got worse
+// with Android 13. To avoid it we patch a react-native style, but that style
+// got deprecated in React Native 0.70. For now the deprecation is limited to a
+// JS runtime check, which we disable here.
+import ViewReactNativeStyleAttributes from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
+ViewReactNativeStyleAttributes.scaleY = true;
+
 import './reactotron';
 import './config';
 
