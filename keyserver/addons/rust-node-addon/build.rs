23eb8e45b9592b0d31a14ec4578c613a1cac564b
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2023-01-12 09:40:08 -0500

    [keyserver] expose new client method to keyserver
    expose identity service RPC to keyserver that will be called during registration workflow.

--- keyserver/addons/rust-node-addon/build.rs
+++ keyserver/addons/rust-node-addon/build.rs
@@ -2,4 +2,6 @@ extern crate napi_build;
 
 fn main() {
   napi_build::setup();
+  tonic_build::compile_protos("../../../shared/protos/identity.proto")
+    .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
 }
