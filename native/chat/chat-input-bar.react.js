ffb5f1d2790d31a68caef05671739899ccc53776
pweglik <przemek.weglik@onet.pl>
2023-01-13 15:24:42 +0100

    [6/n] Native Typeahead - Refactor focusAndUpdateText and add focusAndUpdateTextAndSelection
    Summary:
    Preparing for native typeahead.
    Refactored utility funtion used for focusing text input and introduced new function that will be used in typeahead. Common part was extrated to `focusAndHandleButtons`.
    
    Test Plan:
    Checked if app builds.
    Tested if focusing on reply still works correctly.
    `focusAndUpdateTextAndSelection` tested in future diffs when it's actually used.
    
    Reviewers: inka, ginsu, kamil, michal
    
    Subscribers: ashoat, tomek, atul, inka, ginsu, kamil, michal
    
    Differential Revision: https://phab.comm.dev/D6368

--- native/chat/chat-input-bar.react.js
+++ native/chat/chat-input-bar.react.js
@@ -64,6 +64,7 @@ import {
   useServerCall,
   useDispatchActionPromise,
 } from 'lib/utils/action-utils';
+import sleep from 'lib/utils/sleep';
 
 import Button from '../components/button.react';
 import ClearableTextInput from '../components/clearable-text-input.react';
@@ -722,20 +723,54 @@ class ChatInputBar extends React.Component<Props, State> {
     });
   }, 400);
 
+  focusAndUpdateTextAndSelection = (text: string, selection: Selection) => {
+    // On iOS, the update of text below will trigger a selection change which
+    // will overwrite our update of selection. To address this, we schedule a
+    // second update of selection after waiting for the animation frame to
+    // flush. We also make sure that the overwriting selection change is not
+    // saved to our selection state by ignoring selection updates until our
+    // second update.
+    this.setState(
+      {
+        text,
+        textEdited: true,
+        selection,
+        controlSelection: true,
+      },
+      async () => {
+        if (Platform.OS !== 'ios') {
+          return;
+        }
+        await sleep(100);
+        this.setState({
+          selection,
+          controlSelection: true,
+        });
+      },
+    );
+    this.saveDraft(text);
+
+    this.focusAndUpdateButtonsVisibility();
+  };
+
   focusAndUpdateText = (text: string) => {
+    const currentText = this.state.text;
+    if (!currentText.startsWith(text)) {
+      const prependedText = text.concat(currentText);
+      this.updateText(prependedText);
+    }
+
+    this.focusAndUpdateButtonsVisibility();
+  };
+
+  focusAndUpdateButtonsVisibility = () => {
     const { textInput } = this;
     if (!textInput) {
       return;
     }
 
-    const currentText = this.state.text;
-    if (!currentText.startsWith(text)) {
-      const prependedText = text.concat(currentText);
-      this.updateText(prependedText);
-      this.immediatelyShowSendButton();
-      this.immediatelyHideButtons();
-    }
-
+    this.immediatelyShowSendButton();
+    this.immediatelyHideButtons();
     textInput.focus();
   };
 
