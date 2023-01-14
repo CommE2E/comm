cc689095047a496d99845236259afef670190078
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2023-01-14 17:19:40 +0100

    [native] Default to 'natDevHostname' from expo config
    Summary:
    Updated the `dev-hostname.js` to try gathering the `natDevHostname` from the Expo config first.
    If this fails, then it falls back to the existing `facts/network.json` flow.
    
    If none is present, the warning will be displayed
    
    Depends on D6267
    
    Test Plan:
    1. Deleted `facts/network.json`
    2. Opened the app **on physical device**
    3. No related warning is displayed
    4. Redux devtools are working
    
    Reviewers: ashoat, atul
    
    Reviewed By: ashoat
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D6268

--- native/utils/dev-hostname.js
+++ native/utils/dev-hostname.js
@@ -1,31 +1,45 @@
 // @flow
+import Constants from 'expo-constants';
 
-let natDevHostname: string;
 let warnNatDevHostnameUndefined = true;
 const defaultNatDevHostname = '192.168.1.1';
 
-try {
-  // this is your machine's hostname in the local network
-  // usually it looks like this: 192.168.1.x
-  // to find it you may want to use `ifconfig | grep '192.168'`
-  // command in the terminal
-  // example of native/facts/network.json:
-  // { "natDevHostname": "192.168.1.x" }
-  // $FlowExpectedError: That's a conditional require so the file may not exist
-  const hostname: string = require('../facts/network').natDevHostname;
-  natDevHostname = hostname;
+function readHostnameFromExpoConfig(): ?string {
+  const { natDevHostname: detectedHostname } = Constants.expoConfig.extra || {};
+  if (!detectedHostname || typeof detectedHostname !== 'string') {
+    return null;
+  }
   warnNatDevHostnameUndefined = false;
-} catch (e) {
-  natDevHostname = defaultNatDevHostname;
+  return detectedHostname;
 }
+
+function readHostnameFromNetworkJson(): string {
+  try {
+    // this is your machine's hostname in the local network
+    // usually it looks like this: 192.168.1.x
+    // to find it you may want to use `ifconfig | grep '192.168'`
+    // command in the terminal
+    // example of native/facts/network.json:
+    // { "natDevHostname": "192.168.1.x" }
+    // $FlowExpectedError: It's a conditional require so the file may not exist
+    const hostname: string = require('../facts/network').natDevHostname;
+    warnNatDevHostnameUndefined = false;
+    return hostname;
+  } catch (e) {
+    return defaultNatDevHostname;
+  }
+}
+
+const natDevHostname: string =
+  readHostnameFromExpoConfig() ?? readHostnameFromNetworkJson();
 
 function checkForMissingNatDevHostname() {
   if (!warnNatDevHostnameUndefined) {
     return;
   }
   console.warn(
-    'Failed to read natDevHostname. ' +
-      'Please specify it in native/facts/network.json',
+    'Failed to autodetect natDevHostname. ' +
+      'Please specify it manually in native/facts/network.json',
   );
   warnNatDevHostnameUndefined = false;
 }
