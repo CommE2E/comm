081dad457e865eb847d011be4650da5f918c6474
Max Kalashnikoff <geekmaks@gmail.com>
2023-01-17 18:39:58 +0400

    [keyserver] Adding `tunnelbroker-client` to the keyserver napi-rs dependencies

--- keyserver/addons/rust-node-addon/install_external_deps.sh
+++ keyserver/addons/rust-node-addon/install_external_deps.sh
@@ -2,15 +2,6 @@
 
 set -e
 
-echo "Buildkite var set: $BUILDKITE"
-
-# We can skip this script if it's not part of a CI workflow
-if ! { [ -n "$BUILDKITE" ] || [ -n "$CI" ]; }
-then
-  echo "Not in a CI workflow, exiting"
-  exit
-fi
-
 # Install protobuf if it's not already installed
 if [[ ! "$(command -v protoc)" ]]
 then
