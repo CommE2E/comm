198b5282a0276406843d06a276560542cf1b95c9
Max Kalashnikoff <geekmaks@gmail.com>
2023-01-17 18:29:16 +0400

    [tunnelbroker-client] Adding Tunnelbroker tonic client initialization function
    Summary:
    This diff introduces an `initialize_client` function that initializes the Tonic client instance and returns a client channel for it. The client instance from this function will be passed as an argument into the publish and consume functions in the follow-up diffs.
    
    Linear task: [[ https://linear.app/comm/issue/ENG-2728/implement-tunnelbroker-shared-client-library | ENG-2728 ]]
    
    Test Plan: Running of `run nix develop --accept-flake-config -c bash -c "cargo test"` will successfully build the library.
    
    Reviewers: jon, varun
    
    Reviewed By: jon, varun
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D6281

--- shared/tunnelbroker-client/src/lib.rs
+++ shared/tunnelbroker-client/src/lib.rs
@@ -1,1 +1,26 @@
+use lazy_static::lazy_static;
+use std::sync::Arc;
+use tokio::runtime::{Builder, Runtime};
+use tonic::transport::Channel;
 
+pub mod tunnelbroker_pb {
+  tonic::include_proto!("tunnelbroker");
+}
+use tunnelbroker_pb::tunnelbroker_service_client::TunnelbrokerServiceClient;
+
+lazy_static! {
+  pub static ref RUNTIME: Arc<Runtime> = Arc::new(
+    Builder::new_multi_thread()
+      .worker_threads(1)
+      .max_blocking_threads(1)
+      .enable_all()
+      .build()
+      .unwrap()
+  );
+}
+
+pub fn initialize_client(addr: String) -> TunnelbrokerServiceClient<Channel> {
+  RUNTIME
+    .block_on(TunnelbrokerServiceClient::connect(addr))
+    .expect("Failed to connect to the Tunnelbroker Service")
+}
