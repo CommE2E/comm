01b14b91d7b42b4cbdd3a26cb5f694f6f1d0dfc9
Max Kalashnikoff <geekmaks@gmail.com>
2023-01-17 18:29:16 +0400

    [tunnelbroker-client] Adding Tunnelbroker tonic client initialization function
    Summary: TBD
    
    Test Plan: TBD
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D6281

--- shared/tunnelbroker-client/src/lib.rs
+++ shared/tunnelbroker-client/src/lib.rs
@@ -1,1 +1,28 @@
+use lazy_static::lazy_static;
+use std::sync::Arc;
+use tokio::runtime::{Builder, Runtime};
+use tonic::transport::Channel;
+use tunnelbroker::tunnelbroker_service_client::TunnelbrokerServiceClient;
 
+mod tunnelbroker {
+  tonic::include_proto!("tunnelbroker");
+}
+
+lazy_static! {
+  pub static ref RUNTIME: Arc<Runtime> = Arc::new(
+    Builder::new_multi_thread()
+      .worker_threads(1)
+      .max_blocking_threads(1)
+      .enable_all()
+      .build()
+      .unwrap()
+  );
+}
+
+pub fn initialize_client<T>(
+  addr: String,
+) -> TunnelbrokerServiceClient<Channel> {
+  RUNTIME
+    .block_on(TunnelbrokerServiceClient::connect(addr))
+    .expect("Failed to create Tokio runtime for the Tunnelbroker client")
+}
