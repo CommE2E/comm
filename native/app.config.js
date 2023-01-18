e476fb95a0c7307c2e4933ed26267eb3fe85c65d
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2023-01-18 09:52:03 +0100

    [native] Make 'natDevHostname' configurable from env
    Summary:
    Resolves concerns raised in [[ https://linear.app/comm/issue/ENG-2734/add-possibility-to-overwrite-autodetected-natdevhostname | ENG-2734 ]] and [[ https://linear.app/comm/issue/ENG-2707/replace-nativefactsnetworkjson-with-code-that-detects-lan-ip-address | ENG-2707 ]].
    
    This diff introduces two environment variables, whose are read by Expo start command (`yarn dev`):
    - `COMM_DEV` - This one must be set, otherwise `natDevHostname` will be `null`. Its purpose is not to embed developer machine IP in production builds.
    - `COMM_NAT_DEV_HOSTNAME` - This one can be used to overwrite the autodetected IP address - I consider this as a replacement for `network.json` in special cases we want to have full control over the hostname.
    
    Depends on D6267
    
    Test Plan:
    The `expo config` command below evaluates the `app.config.js` (and some other Expo defaults), it's used internally when running Metro dev server.
    
    ```
    lang=sh
    cd native
    
    # should be empty
    yarn expo config | grep natDevHostname
    
    # should detect local IP
    COMM_DEV=1 yarn expo config | grep natDevHostname
    
    # should resolve to '1.2.3.4'
    COMM_DEV=1 COMM_NAT_DEV_HOSTNAME=1.2.3.4 yarn expo config | grep natDevHostname
    ```
    
    Reviewers: ashoat, jon, atul
    
    Reviewed By: ashoat
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D6293

--- native/app.config.js
+++ native/app.config.js
@@ -2,9 +2,25 @@
 
 import ip from 'internal-ip';
 
+// Finds this machine's hostname in the local network. This is useful for
+// debugging on a real device. The `COMM_DEV` environment variable must be set
+// or this function will return `null`. The `COMM_NAT_DEV_HOSTNAME` environment
+// variable can be used to override the autodetected hostname.
+function getDevHostname() {
+  const { COMM_DEV: isDev, COMM_NAT_DEV_HOSTNAME } = process.env;
+
+  if (!isDev) {
+    return null;
+  }
+  if (COMM_NAT_DEV_HOSTNAME) {
+    return COMM_NAT_DEV_HOSTNAME;
+  }
+  return ip.v4.sync();
+}
+
 export default {
   extra: {
     // developer machine's hostname in the local network
-    natDevHostname: ip.v4.sync(),
+    natDevHostname: getDevHostname(),
   },
 };
