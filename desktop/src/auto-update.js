59cd7a9966208fd583f38451402c1dc7c5e7d30b
Michal Gniadek <michal.gniadek@swmansion.com>
2023-01-19 22:40:04 +0100

    [desktop] Add auto updating
    Summary:
    Uses the `autoUpdater` module and check for updates every 5 minutes. Exposes to the web app:
    - current version
    - listener for a new version
    - method to trigger the update.
    
    Depends on D6321
    
    Test Plan: Tested with the later diffs that introduce UI in the web app. Created a few version of the app and checked if the download and automatic update is triggered correctly after uploading a new one (on both macos and windows).
    
    Reviewers: inka, przemek, tomek
    
    Reviewed By: tomek
    
    Subscribers: ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D6322

---
+++ desktop/src/auto-update.js
@@ -0,0 +1,27 @@
+// @flow
+
+import { app, ipcMain, autoUpdater } from 'electron/main';
+
+const getUpdateUrl = version =>
+  `https://electron-update.commtechnologies.org/update/${process.platform}/${version}`;
+
+export function initAutoUpdate(): void {
+  autoUpdater.setFeedURL({ url: getUpdateUrl(app.getVersion()) });
+
+  // Check for new updates every 10 minutes
+  const updateIntervalMs = 10 * 60_000;
+  const scheduleCheckForUpdates = () => {
+    setTimeout(() => autoUpdater.checkForUpdates(), updateIntervalMs);
+  };
+
+  scheduleCheckForUpdates();
+
+  autoUpdater.on('update-not-available', scheduleCheckForUpdates);
+
+  autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+    autoUpdater.setFeedURL({ url: getUpdateUrl(releaseName) });
+    scheduleCheckForUpdates();
+  });
+
+  ipcMain.on('update-to-new-version', () => autoUpdater.quitAndInstall());
+}
