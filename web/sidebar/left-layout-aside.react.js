7b7009e8aab0129785715645410dfb79c1033428
Inka Sokołowska <inka.sokolowska@swmansion.com>
2023-01-23 17:50:01 +0100

    [web] Remove navigation panel from LeftLayoutAside
    Summary:
    issue: https://linear.app/comm/issue/ENG-2739/move-navigationpanel-from-the-sidebar-to-a-top-bar
    The `AppSwitcher` will be in a top bar, and `SettingsSwitcher` has to be moved to main content, because of how we use grid.
    So For now I'm romoving them from the LeftLayoutAside.
    
    Test Plan: Run web app, check that in inbox and settinngs tabs the switchers are not present
    
    Reviewers: przemek, michal, bartek
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D6344

--- web/sidebar/left-layout-aside.react.js
+++ web/sidebar/left-layout-aside.react.js
@@ -2,24 +2,13 @@
 
 import * as React from 'react';
 
-import { useSelector } from '../redux/redux-utils';
-import AppSwitcher from '../topbar/app-switcher.react';
 import CommunityPicker from './community-picker.react';
 import css from './left-layout-aside.css';
-import SettingsSwitcher from './settings-switcher.react';
 
 function LeftLayoutAside(): React.Node {
-  const navInfo = useSelector(state => state.navInfo);
-  const navigationPanel = React.useMemo(() => {
-    if (navInfo.tab === 'settings') {
-      return <SettingsSwitcher />;
-    }
-    return <AppSwitcher />;
-  }, [navInfo.tab]);
   return (
     <aside className={css.container}>
       <CommunityPicker />
-      {navigationPanel}
     </aside>
   );
 }
