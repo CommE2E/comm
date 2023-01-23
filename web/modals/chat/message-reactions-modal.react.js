2742cf6e1b87d2bb46b613bd879f0d9d7275a988
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-01-23 05:12:42 -0500

    [web/lib] put logic for creating message reaction list in reaction utils
    Summary: This logic will be used in both message reaction modals in `web` and `native` so we should make a new function in `lib` to abstract it
    
    Test Plan: `flow` and the message reaction modal works just as it did before
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D6348

--- web/modals/chat/message-reactions-modal.react.js
+++ web/modals/chat/message-reactions-modal.react.js
@@ -1,20 +1,12 @@
 // @flow
 
-import _sortBy from 'lodash/fp/sortBy';
 import * as React from 'react';
 
 import type { MessageReactionInfo } from 'lib/selectors/chat-selectors';
-import { stringForUserExplicit } from 'lib/shared/user-utils';
+import { createMessageReactionsList } from 'lib/shared/reaction-utils';
 
 import Modal from '../modal.react';
 import css from './message-reactions-modal.css';
-
-type MessageReactionListInfo = {
-  +id: string,
-  +isViewer: boolean,
-  +reaction: string,
-  +username: string,
-};
 
 type Props = {
   +onClose: () => void,
@@ -25,33 +17,9 @@ function MessageReactionsModal(props: Props): React.Node {
   const { onClose, reactions } = props;
 
   const reactionsList = React.useMemo(() => {
-    const result = [];
+    const messageReactionsList = createMessageReactionsList(reactions);
 
-    for (const [reaction, reactionInfo] of reactions) {
-      reactionInfo.users.forEach(user => {
-        result.push({
-          ...user,
-          username: stringForUserExplicit(user),
-          reaction,
-        });
-      });
-    }
-
-    const sortedResult = _sortBy(
-      ([
-        (reactionInfo: MessageReactionListInfo) => {
-          const numOfReactions = reactions.get(reactionInfo.reaction)?.users
-            .length;
-
-          return numOfReactions ? -numOfReactions : 0;
-        },
-        'username',
-      ]: $ReadOnlyArray<
-        ((reactionInfo: MessageReactionListInfo) => mixed) | string,
-      >),
-    )(result);
-
-    return sortedResult.map(messageReactionUser => (
+    return messageReactionsList.map(messageReactionUser => (
       <div key={messageReactionUser.id} className={css.userRowContainer}>
         <div>{messageReactionUser.username}</div>
         <div>{messageReactionUser.reaction}</div>
