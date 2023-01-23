f5377b330b4b5b4b269a6678ada40f3481c75627
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-01-23 05:12:42 -0500

    [web/lib] put logic for creating message reaction list in reaction utils
    Summary: This logic will be used in both message reaction modals in `web` and `native` so we should make a new function in `lib` to abstract it
    
    Test Plan: `flow` and the message reaction modal works just as it did before
    
    Reviewers:
    
    Subscribers:

--- web/modals/chat/message-reactions-modal.react.js
+++ web/modals/chat/message-reactions-modal.react.js
@@ -3,7 +3,7 @@
 import * as React from 'react';
 
 import type { MessageReactionInfo } from 'lib/selectors/chat-selectors';
-import { stringForUserExplicit } from 'lib/shared/user-utils';
+import { createMessageReactionsList } from 'lib/shared/reaction-utils';
 
 import Modal from '../modal.react';
 import css from './message-reactions-modal.css';
@@ -17,42 +17,9 @@ function MessageReactionsModal(props: Props): React.Node {
   const { onClose, reactions } = props;
 
   const reactionsList = React.useMemo(() => {
-    const result = [];
+    const messageReactionsList = createMessageReactionsList(reactions);
 
-    for (const [reaction, reactionInfo] of reactions) {
-      reactionInfo.users.forEach(user => {
-        result.push({
-          ...user,
-          username: stringForUserExplicit(user),
-          reaction,
-        });
-      });
-    }
-
-    result.sort((a, b) => {
-      const numOfReactionsA = reactions.get(a.reaction)?.users.length;
-      const numOfReactionsB = reactions.get(b.reaction)?.users.length;
-
-      if (!numOfReactionsA || !numOfReactionsB) {
-        return 0;
-      }
-
-      if (numOfReactionsA < numOfReactionsB) {
-        return 1;
-      } else if (numOfReactionsA > numOfReactionsB) {
-        return -1;
-      }
-
-      if (a.username < b.username) {
-        return -1;
-      } else if (a.username > b.username) {
-        return 1;
-      }
-
-      return 0;
-    });
-
-    return result.map(messageReactionUser => (
+    return messageReactionsList.map(messageReactionUser => (
       <div key={messageReactionUser.id} className={css.userRowContainer}>
         <div>{messageReactionUser.username}</div>
         <div>{messageReactionUser.reaction}</div>
