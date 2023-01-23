056417ef2a2329a3531fdb8dcc7b8493b1feb6dc
Inka Sokołowska <inka.sokolowska@swmansion.com>
2023-01-23 16:22:29 +0100

    [web] Create a dummy topbar component
    Summary:
    issue: https://linear.app/comm/issue/ENG-2739/move-navigationpanel-from-the-sidebar-to-a-top-bar
    Adding a `Topbar` component that will contain the `AppSwitcher` and the navigation info bar. It will be displayed in the `Inbox` tab.
    For now it displays "dummy", because adding it's contents requires a lot of changes to the code. So in the interest of keeping the diff small it has a dummy content for now.
    
    Test Plan: Displayed it in the `App` component.
    
    Reviewers: przemek, michal, bartek
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D6341

---
+++ web/topbar/topbar.react.js
@@ -0,0 +1,22 @@
+// @flow
+
+import * as React from 'react';
+
+import type { NavInfo } from '../types/nav-types';
+import css from './topbar.css';
+
+type Props = {
+  navInfo: NavInfo,
+};
+function Topbar(props: Props): React.Node {
+  const { navInfo } = props;
+  if (navInfo.tab === 'settings') {
+    return null;
+  }
+
+  return <div className={css.container}>dummy</div>;
+}
+
+const MemoizedTopbar: React.ComponentType<Props> = React.memo<Props>(Topbar);
+
+export default MemoizedTopbar;
