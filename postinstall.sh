09a9a2b587c18188f39dff7fa10875448c85ba3c
Michal Gniadek <michal.gniadek@swmansion.com>
2023-01-25 16:28:00 +0100

    [services][electron update server] Add patch-package to root postinstall
    Summary: Currently `patch-package` is only run from the `native` workspace but it would make sense to run it at the end after all workspace members are correctly installed.
    
    Test Plan: Run `yarn cleaninstall` and see that `patch-package` is run at the end
    
    Reviewers: atul, tomek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D6391

--- postinstall.sh
+++ postinstall.sh
@@ -7,14 +7,12 @@ if [[ "$OSTYPE" == "msys" ]]; then
   exit 0
 fi
 
-cd ../
 echo '{"name": "olm", "version": "3.2.4"}' > ./node_modules/olm/package.json
 yarn patch-package
 yarn flow-mono create-symlinks native
 
-cd native
-yarn jetify
+yarn workspace native jetify
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  (cd ios && PATH=/usr/bin:"$PATH" pod install)
+  (cd native/ios && PATH=/usr/bin:"$PATH" pod install)
 fi
