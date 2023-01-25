a7374be8b830c89e1dad63426c88520d5fb55803
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2023-01-25 09:59:56 +0100

    [backup-service] Add blob client init function
    Summary:
    Part of https://linear.app/comm/issue/ENG-2688/backup-service-reuse-single-blob-client-instance
    
    Created a function that initializes a blob client instance. The connection is lazy-initialized so it won't be established until first use.
    
    Depends on D6383
    
    Test Plan: Backup service compiles.
    
    Reviewers: varun, jon, michal, tomek
    
    Reviewed By: tomek
    
    Subscribers: ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D6384

--- services/backup/src/blob/mod.rs
+++ services/backup/src/blob/mod.rs
@@ -1,6 +1,7 @@
 mod proto {
   tonic::include_proto!("blob");
 }
+use proto::blob_service_client::BlobServiceClient;
 pub use proto::put_request::Data as PutRequestData;
 pub use proto::{PutRequest, PutResponse};
 
@@ -8,3 +9,14 @@ mod downloader;
 mod uploader;
 pub use downloader::*;
 pub use uploader::*;
+
+pub type BlobClient = BlobServiceClient<tonic::transport::Channel>;
+
+/// Creates a new Blob service client instance. It does not attempt to connect
+/// to the service until first use.
+pub fn init_blob_client() -> BlobClient {
+  let service_url = &crate::CONFIG.blob_service_url;
+  let channel =
+    tonic::transport::Channel::from_static(service_url).connect_lazy();
+  BlobServiceClient::new(channel)
+}
