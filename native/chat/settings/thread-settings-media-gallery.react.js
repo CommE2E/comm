5274b78187b7ce96b640b432ebc536fb2b1d2897
Rohan Khanderia <rohan.khanderia@gmail.com>
2023-01-30 11:37:43 -0500

    [native] Create a component for the media gallery on native
    Summary:
    Here, we create the native component for the thread media
    gallery. Currently, `mediaInfos` is defaulted to an empty array, but it
    will be replaced to hold the media data in subsequent diffs.
    `galleryItemGap` refers to the gap between media. `galleryItemWidth` is
    determined using `useWindowDimensions` so we can hopefully ensure that each item
    takes 1/3 of the width of the gallery.
    
    Encompassing the media in <TouchableOpacity> will be changed in later
    diffs, alongside just rendering an <Image> component.
    
    Depends on D6465
    
    Test Plan:
    This will be tested in the next diff, and a screenshot of what
    the media gallery will look like with sample, hard coded images will be
    shown.
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D6466

---
+++ native/chat/settings/thread-settings-media-gallery.react.js
@@ -0,0 +1,76 @@
+// @flow
+
+import * as React from 'react';
+import {
+  View,
+  TouchableOpacity,
+  Image,
+  FlatList,
+  useWindowDimensions,
+} from 'react-native';
+
+import type { Media } from 'lib/types/media-types';
+
+import { useStyles } from '../../themes/colors';
+
+function ThreadSettingsMediaGallery(): React.Node {
+  const styles = useStyles(unboundStyles);
+  const galleryItemGap = 8;
+  const { width } = useWindowDimensions();
+
+  const galleryItemWidth = React.useMemo(() => {
+    return (width - galleryItemGap * 3) / 3;
+  }, [galleryItemGap, width]);
+  const mediaInfos = [];
+
+  const memoizedStyles = React.useMemo(() => {
+    return {
+      mediaContainer: {
+        marginTop: galleryItemGap,
+        marginLeft: galleryItemGap,
+        width: galleryItemWidth,
+        ...styles.mediaContainer,
+      },
+      media: {
+        width: galleryItemWidth,
+        ...styles.media,
+      },
+    };
+  }, [galleryItemGap, galleryItemWidth, styles.media, styles.mediaContainer]);
+
+  const renderItem = React.useCallback(
+    ({ item }) => {
+      return <GalleryItem item={item} memoizedStyles={memoizedStyles} />;
+    },
+    [memoizedStyles],
+  );
+
+  return <FlatList data={mediaInfos} numColumns={3} renderItem={renderItem} />;
+}
+
+const GalleryItem: React.ComponentType<{
+  item: Media,
+  memoizedStyles: Object,
+}> = React.memo(({ item, memoizedStyles }) => {
+  return (
+    <View key={item.id} style={memoizedStyles.mediaContainer}>
+      <TouchableOpacity>
+        <Image source={item.uri} style={memoizedStyles.media} />
+      </TouchableOpacity>
+    </View>
+  );
+});
+GalleryItem.displayName = 'GalleryItem';
+
+const unboundStyles = {
+  mediaContainer: {
+    height: 180,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  media: {
+    height: 180,
+  },
+};
+
+export default ThreadSettingsMediaGallery;
