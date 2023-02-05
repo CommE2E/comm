0e1154b52e4a5cb9fa03b98fb90d2908b7a07f4d
pweglik <przemek.weglik@onet.pl>
2023-02-05 11:28:31 -0500

    [8/n] Native Typeahead - Create tooltip component in chat input bar
    Summary:
    Creating typeahead in ChatInputBar. There is one iOS bug/bad UX described and showed in a thread here: https://linear.app/comm/issue/ENG-2367/typeahead-in-tooltip-for-mentions-for-native
    
    I'm putting up diffs regardless so they can be reviewed, but I can't find a way to fix it.
    Adding @ashoat as blocking as the diff contains currently unsolved bug.
    
    Test Plan:
    Checked if app builds.
    Video android:
    {F344237}
    Video iOS (showing bug with cursor not moving past name)
    {F344238}
    
    Reviewers: tomek, inka, ginsu, kamil, michal, ashoat
    
    Reviewed By: tomek, ashoat
    
    Subscribers: ashoat, atul, michal, kamil, ginsu, inka
    
    Differential Revision: https://phab.comm.dev/D6390

--- native/chat/chat-input-bar.react.js
+++ native/chat/chat-input-bar.react.js
@@ -43,7 +43,11 @@ import {
   draftKeyFromThreadID,
   colorIsDark,
 } from 'lib/shared/thread-utils';
-import type { Selection } from 'lib/shared/typeahead-utils';
+import {
+  getTypeaheadUserSuggestions,
+  getTypeaheadRegexMatches,
+  type Selection,
+} from 'lib/shared/typeahead-utils';
 import type { CalendarQuery } from 'lib/types/entry-types';
 import type { LoadingStatus } from 'lib/types/loading-types';
 import type { PhotoPaste } from 'lib/types/media-types';
@@ -88,8 +92,10 @@ import { type Colors, useStyles, useColors } from '../themes/colors';
 import type { LayoutEvent, SelectionChangeEvent } from '../types/react-native';
 import { type AnimatedViewStyle, AnimatedView } from '../types/styles';
 import { runTiming } from '../utils/animation-utils';
+import { nativeTypeaheadRegex } from '../utils/typeahead-utils';
 import { ChatContext } from './chat-context';
 import type { ChatNavigationProp } from './chat.react';
+import TypeaheadTooltip from './typeahead-tooltip.react';
 
 /* eslint-disable import/no-named-as-default-member */
 const {
@@ -464,6 +470,39 @@ class ChatInputBar extends React.PureComponent<Props, State> {
       );
     }
 
+    const typeaheadRegexMatches = getTypeaheadRegexMatches(
+      this.state.text,
+      this.state.selection,
+      nativeTypeaheadRegex,
+    );
+
+    let typeaheadTooltip = null;
+
+    if (typeaheadRegexMatches) {
+      const typeaheadMatchedStrings = {
+        textBeforeAtSymbol: typeaheadRegexMatches[1] ?? '',
+        usernamePrefix: typeaheadRegexMatches[4] ?? '',
+      };
+
+      const suggestedUsers = getTypeaheadUserSuggestions(
+        this.props.userSearchIndex,
+        this.props.threadMembers,
+        this.props.viewerID,
+        typeaheadMatchedStrings.usernamePrefix,
+      );
+
+      if (suggestedUsers.length > 0) {
+        typeaheadTooltip = (
+          <TypeaheadTooltip
+            text={this.state.text}
+            matchedStrings={typeaheadMatchedStrings}
+            suggestedUsers={suggestedUsers}
+            focusAndUpdateTextAndSelection={this.focusAndUpdateTextAndSelection}
+          />
+        );
+      }
+    }
+
     let content;
     const defaultMembersAreVoiced = checkIfDefaultMembersAreVoiced(
       this.props.threadInfo,
@@ -509,6 +548,7 @@ class ChatInputBar extends React.PureComponent<Props, State> {
         style={this.props.styles.container}
         onLayout={this.props.onInputBarLayout}
       >
+        {typeaheadTooltip}
         {joinButton}
         {content}
         {keyboardInputHost}
