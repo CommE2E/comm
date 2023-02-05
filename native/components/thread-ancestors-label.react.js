af8e1ef3d74eeb41617135e76d0270fe4e77ea65
Ashoat Tevosyan <ashoat@comm.app>
2023-02-04 20:30:20 -0500

    [native] Fetch ENS names in ThreadAncestorsLabel
    Summary: Depends on D6599
    
    Test Plan: | {F363566} |
    
    Reviewers: atul
    
    Reviewed By: atul
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D6600

--- native/components/thread-ancestors-label.react.js
+++ native/components/thread-ancestors-label.react.js
@@ -6,6 +6,7 @@ import { Text, View } from 'react-native';
 
 import { useAncestorThreads } from 'lib/shared/ancestor-threads';
 import { type ThreadInfo } from 'lib/types/thread-types';
+import { useResolvedThreadInfos } from 'lib/utils/entity-helpers';
 
 import { useColors, useStyles } from '../themes/colors';
 
@@ -18,6 +19,7 @@ function ThreadAncestorsLabel(props: Props): React.Node {
   const styles = useStyles(unboundStyles);
   const colors = useColors();
   const ancestorThreads = useAncestorThreads(threadInfo);
+  const resolvedAncestorThreads = useResolvedThreadInfos(ancestorThreads);
 
   const chevronIcon = React.useMemo(
     () => (
@@ -32,7 +34,7 @@ function ThreadAncestorsLabel(props: Props): React.Node {
 
   const ancestorPath = React.useMemo(() => {
     const path = [];
-    for (const thread of ancestorThreads) {
+    for (const thread of resolvedAncestorThreads) {
       path.push(<Text key={thread.id}>{thread.uiName}</Text>);
       path.push(
         <View key={`>${thread.id}`} style={styles.chevron}>
@@ -42,7 +44,7 @@ function ThreadAncestorsLabel(props: Props): React.Node {
     }
     path.pop();
     return path;
-  }, [ancestorThreads, chevronIcon, styles.chevron]);
+  }, [resolvedAncestorThreads, chevronIcon, styles.chevron]);
 
   const ancestorPathStyle = React.useMemo(() => {
     return unread ? [styles.pathText, styles.unread] : styles.pathText;
