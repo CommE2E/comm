4e2acb6f9cc567e794d8959daf71cfc5bab57519
Atul Madhugiri <atulm@berkeley.edu>
2023-02-06 12:48:53 -0500

    [web] Show Rainbowkit emoji in `ConnectedWalletInfo`
    Summary:
    We want `ConnectedWalletInfo` to replace `ConnectButton`. In this diff we get the Rainbowkit emoji using the `emojiAvatarForAddress` function and display it in `ConnectedWalletInfo`. There's also some additional layout stuff to ensure things look decent.
    
    NOTE: Displaying ENS Avatars (if they exist) will be handled in subsequent diff
    
    Test Plan:
    1. Looks as expected:
    
    {F365337}
    
    2. Tried with various wallets and made sure the emoji/color matched that of the `ConnectButton`.
    
    Reviewers: ashoat, tomek
    
    Reviewed By: ashoat
    
    Differential Revision: https://phab.comm.dev/D6618

--- web/account/connected-wallet-info.react.js
+++ web/account/connected-wallet-info.react.js
@@ -1,19 +1,44 @@
 // @flow
 
+import { emojiAvatarForAddress } from '@rainbow-me/rainbowkit';
 import * as React from 'react';
 import { useAccount } from 'wagmi';
 
 import { useENSName } from 'lib/hooks/ens-cache.js';
 
+import SWMansionIcon from '../SWMansionIcon.react.js';
 import css from './connected-wallet-info.css';
+
+type RainbowKitEmojiAvatar = {
+  +color: string,
+  +emoji: string,
+};
 
 function ConnectedWalletInfo(): React.Node {
   const { address } = useAccount();
   const potentiallyENSName = useENSName(address);
 
+  const emojiAvatar: RainbowKitEmojiAvatar = React.useMemo(
+    () => emojiAvatarForAddress(address),
+    [address],
+  );
+
+  const emojiAvatarStyle = React.useMemo(
+    () => ({ backgroundColor: emojiAvatar.color }),
+    [emojiAvatar.color],
+  );
+
   return (
     <div className={css.container}>
-      <p>{potentiallyENSName}</p>
+      <div className={css.avatar} style={emojiAvatarStyle}>
+        <p>{emojiAvatar.emoji}</p>
+      </div>
+      <div className={css.address}>
+        <p>{potentiallyENSName}</p>
+      </div>
+      <div className={css.chevronDown}>
+        <SWMansionIcon icon="chevron-down" size={18} />
+      </div>
     </div>
   );
 }
