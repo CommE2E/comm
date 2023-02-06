9f49efe7dd16e4ee30ffd22c93ff0cc267f5fb59
Inka Sokołowska <inka.sokolowska@swmansion.com>
2023-02-06 11:02:46 +0100

    [web] Add community drawer
    Summary:
    Linear issue: https://linear.app/comm/issue/ENG-2631/drawer-sidebar-inside-of-community-picker
    Adding the community drawer.
    Adding the community drawer on native: D5723 and D5732
    
    Test Plan: added community drawer to AppsDirectory, checked that it behaves as expected.
    
    Reviewers: przemek, michal, ginsu
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D6162

---
+++ web/sidebar/community-drawer.react.js
@@ -0,0 +1,63 @@
+// @flow
+
+import * as React from 'react';
+import { useSelector } from 'react-redux';
+
+import {
+  childThreadInfos,
+  communityThreadSelector,
+} from 'lib/selectors/thread-selectors';
+import {
+  createRecursiveDrawerItemsData,
+  appendSuffix,
+} from 'lib/utils/drawer-utils.react';
+import { useResolvedThreadInfos } from 'lib/utils/entity-helpers';
+
+import { ThreadListProvider } from '../chat/thread-list-provider';
+import CommunityDrawerItemCommunity from './community-drawer-item-community.react';
+import css from './community-drawer.css';
+
+const maxDepth = 2;
+const labelStyles = ['titleLevel0', 'titleLevel1', 'titleLevel2'];
+
+function CommunityDrawer(): React.Node {
+  const childThreadInfosMap = useSelector(childThreadInfos);
+  const communities = useSelector(communityThreadSelector);
+  const resolvedCommunities = useResolvedThreadInfos(communities);
+  const communitiesSuffixed = React.useMemo(
+    () => appendSuffix(resolvedCommunities),
+    [resolvedCommunities],
+  );
+
+  const drawerItemsData = createRecursiveDrawerItemsData(
+    childThreadInfosMap,
+    communitiesSuffixed,
+    labelStyles,
+    maxDepth,
+  );
+
+  const [openCommunity, setOpenCommunity] = React.useState(
+    communitiesSuffixed.length === 1 ? communitiesSuffixed[0].id : null,
+  );
+
+  const setOpenCommunityOrClose = React.useCallback((communityID: string) => {
+    setOpenCommunity(open => (open === communityID ? null : communityID));
+  }, []);
+
+  const communitiesComponents = drawerItemsData.map(item => (
+    <CommunityDrawerItemCommunity
+      itemData={item}
+      key={item.threadInfo.id}
+      toggleExpanded={setOpenCommunityOrClose}
+      expanded={item.threadInfo.id === openCommunity}
+    />
+  ));
+
+  return (
+    <ThreadListProvider>
+      <div className={css.container}>{communitiesComponents}</div>
+    </ThreadListProvider>
+  );
+}
+
+export default CommunityDrawer;
