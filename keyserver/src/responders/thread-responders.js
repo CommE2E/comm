82ebbc058c9aa538ebe8454f57fc949d8f842a79
Rohan Khanderia <rohan.khanderia@gmail.com>
2023-02-06 16:55:32 -0500

    [keyserver/lib] Set up the endpoints and responder for retrieving media
    Summary:
    This diff handles setting up the endpoints and responder for actually fetching the media for a given thread. The changes made are:
    
    1. Set up the endpoint (`keyserver/src/endpoints.js`) and the type (`lib/types/endpoints.js`)
    2. Use `callServerEndpoint` in `lib/actions/thread-actions.js` to hit the newly created endpoint, and route to the appropriate responder.
    3. Handle the validation of the request and call `fetchMediaForThread` from the responder in `keyserver/src/responders/thread-responders.js`
    
    Linear: https://linear.app/comm/issue/ENG-2874/set-up-the-endpoints-and-responder-for-retrieving-media
    
    Depends on D6485
    
    Test Plan: Ran `flow` for all of the new type introductions, and this will be tested in the next diff where we actually use `useServerCall` to retrieve the media.
    
    Reviewers: atul, ginsu, kamil, tomek, ashoat
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D6531

--- keyserver/src/responders/thread-responders.js
+++ keyserver/src/responders/thread-responders.js
@@ -15,6 +15,8 @@ import {
   type NewThreadResponse,
   type ServerThreadJoinRequest,
   type ThreadJoinResult,
+  type ThreadFetchMediaResult,
+  type ThreadFetchMediaRequest,
   threadTypes,
 } from 'lib/types/thread-types';
 import { values } from 'lib/utils/objects';
@@ -27,6 +29,7 @@ import {
 
 import { createThread } from '../creators/thread-creator';
 import { deleteThread } from '../deleters/thread-deleters';
+import { fetchMediaForThread } from '../fetchers/upload-fetchers';
 import type { Viewer } from '../session/viewer';
 import {
   updateRole,
@@ -176,6 +179,20 @@ async function threadJoinResponder(
   return await joinThread(viewer, request);
 }
 
+const threadFetchMediaRequestInputValidator = tShape({
+  threadID: t.String,
+  limit: t.Number,
+  offset: t.Number,
+});
+async function threadFetchMediaResponder(
+  viewer: Viewer,
+  input: any,
+): Promise<ThreadFetchMediaResult> {
+  const request: ThreadFetchMediaRequest = input;
+  await validateInput(viewer, threadFetchMediaRequestInputValidator, request);
+  return await fetchMediaForThread(request);
+}
+
 export {
   threadDeletionResponder,
   roleUpdateResponder,
@@ -184,5 +201,6 @@ export {
   threadUpdateResponder,
   threadCreationResponder,
   threadJoinResponder,
+  threadFetchMediaResponder,
   newThreadRequestInputValidator,
 };
