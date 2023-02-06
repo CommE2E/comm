34da64b51f1036c55214c5a90bcf85851e4e2525
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-02-06 15:16:49 -0500

    [web] implement emoji keyboard position into message tooltip
    Summary: implement emoji keyboard position into the message tooltip
    
    Test Plan: Please watch the demo video to see the changes I made
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D6634

--- web/chat/message-tooltip.react.js
+++ web/chat/message-tooltip.react.js
@@ -15,15 +15,23 @@ import {
   tooltipStyle,
 } from './chat-constants.js';
 import css from './message-tooltip.css';
-import { useSendReaction } from './reaction-message-utils.js';
+import {
+  useSendReaction,
+  getEmojiKeyboardPosition,
+} from './reaction-message-utils.js';
 import { useTooltipContext } from './tooltip-provider.js';
 import { useSelector } from '../redux/redux-utils.js';
-import { type MessageTooltipAction } from '../utils/tooltip-utils.js';
+import type {
+  MessageTooltipAction,
+  TooltipSize,
+  TooltipPositionStyle,
+} from '../utils/tooltip-utils.js';
 
 type MessageTooltipProps = {
   +actions: $ReadOnlyArray<MessageTooltipAction>,
   +messageTimestamp: string,
-  +alignment?: 'left' | 'center' | 'right',
+  +tooltipPositionStyle: TooltipPositionStyle,
+  +tooltipSize: TooltipSize,
   +item: ChatMessageInfoItem,
   +threadInfo: ThreadInfo,
 };
@@ -31,15 +39,25 @@ function MessageTooltip(props: MessageTooltipProps): React.Node {
   const {
     actions,
     messageTimestamp,
-    alignment = 'left',
+    tooltipPositionStyle,
+    tooltipSize,
     item,
     threadInfo,
   } = props;
   const { messageInfo, reactions } = item;
 
+  const { alignment = 'left' } = tooltipPositionStyle;
+
   const [activeTooltipLabel, setActiveTooltipLabel] = React.useState<?string>();
 
   const { renderEmojiKeyboard } = useTooltipContext();
+
+  const [emojiKeyboardNode, setEmojiKeyboardNode] = React.useState(null);
+  const emojiKeyboardRef = React.useCallback(node => {
+    if (node !== null) {
+      setEmojiKeyboardNode(node);
+    }
+  }, []);
 
   const messageActionButtonsContainerClassName = classNames(
     css.messageActionContainer,
@@ -113,6 +131,22 @@ function MessageTooltip(props: MessageTooltipProps): React.Node {
     );
   }, [messageTimestamp, messageTooltipLabelStyle]);
 
+  const emojiKeyboardPosition = React.useMemo(() => {
+    return getEmojiKeyboardPosition(
+      emojiKeyboardNode,
+      tooltipPositionStyle,
+      tooltipSize,
+    );
+  }, [emojiKeyboardNode, tooltipPositionStyle, tooltipSize]);
+
+  const emojiKeyboardPositionStyle = React.useMemo(
+    () => ({
+      bottom: emojiKeyboardPosition.bottom,
+      left: emojiKeyboardPosition.left,
+    }),
+    [emojiKeyboardPosition.bottom, emojiKeyboardPosition.left],
+  );
+
   const nextLocalID = useSelector(state => state.nextLocalID);
   const localID = `${localIDPrefix}${nextLocalID}`;
 
@@ -133,18 +167,26 @@ function MessageTooltip(props: MessageTooltipProps): React.Node {
     if (!renderEmojiKeyboard) {
       return null;
     }
-    return <Picker data={data} onEmojiSelect={onEmojiSelect} />;
-  }, [onEmojiSelect, renderEmojiKeyboard]);
+
+    return (
+      <div
+        ref={emojiKeyboardRef}
+        style={emojiKeyboardPositionStyle}
+        className={css.emojiKeyboard}
+      >
+        <Picker data={data} onEmojiSelect={onEmojiSelect} />
+      </div>
+    );
+  }, [
+    emojiKeyboardPositionStyle,
+    emojiKeyboardRef,
+    onEmojiSelect,
+    renderEmojiKeyboard,
+  ]);
 
   const messageTooltipContainerStyle = React.useMemo(() => tooltipStyle, []);
 
   const containerClassName = classNames({
-    [css.container]: true,
-    [css.containerLeftAlign]: alignment === 'left',
-    [css.containerCenterAlign]: alignment === 'center',
-  });
-
-  const messageTooltipContainerClassNames = classNames({
     [css.messageTooltipContainer]: true,
     [css.leftTooltipAlign]: alignment === 'left',
     [css.centerTooltipAlign]: alignment === 'center',
@@ -152,17 +194,14 @@ function MessageTooltip(props: MessageTooltipProps): React.Node {
   });
 
   return (
-    <div className={containerClassName}>
+    <>
       {emojiKeyboard}
-      <div
-        className={messageTooltipContainerClassNames}
-        style={messageTooltipContainerStyle}
-      >
+      <div className={containerClassName} style={messageTooltipContainerStyle}>
         <div style={messageTooltipTopLabelStyle}>{tooltipLabel}</div>
         {tooltipButtons}
         {tooltipTimestamp}
       </div>
-    </div>
+    </>
   );
 }
 
