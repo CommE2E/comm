26716c2d20660e78ceae895aa464ca343fd37dae
Rohan Khanderia <rohan.khanderia@gmail.com>
2023-02-06 12:55:55 -0500

    [native] Display both photos and videos in the media gallery on native
    Summary: Now that the responders and endpoints are set up, we need to populate the `FlatList` with real
    data from the database. This means we need to get the data and use it as the `data` prop in the
    `FlatList`.
    
    Previously, the component memoized an empty array `mediaInfos` to prevent the `FlatList` from re-rendering
    each time `ThreadSettingsMediaGallery` re-rendered, since otherwise the array would have a new reference.
    Now, with `useState`, the reference to `mediaInfos` should persist across re-renders and `mediaInfos`
    shouldn't change until we fetch the media. This will be important for when we incorporate a full screen
    media gallery and fetch more media as the user scrolls to populate `mediaInfos`.
    
    Depends on D6587
    
    Test Plan: Confirmed on both iOS and Android that the media loads in the thread settings. The videos are
    linked below.
    
    Reviewers: atul, tomek, ginsu
    
    Subscribers:

--- native/chat/settings/thread-settings-media-gallery.react.js
+++ native/chat/settings/thread-settings-media-gallery.react.js
@@ -4,20 +4,41 @@ import * as React from 'react';
 import {
   View,
   TouchableOpacity,
-  Image,
   FlatList,
   useWindowDimensions,
 } from 'react-native';
+import FastImage from 'react-native-fast-image';
+import Video from 'react-native-video';
+
+import { fetchThreadMedia } from 'lib/actions/thread-actions';
+import { useServerCall } from 'lib/utils/action-utils';
 
 import { useStyles } from '../../themes/colors';
 
-function ThreadSettingsMediaGallery(): React.Node {
+type ThreadSettingsMediaGalleryProps = {
+  +threadID: string,
+  +limit: number,
+};
+
+function ThreadSettingsMediaGallery(
+  props: ThreadSettingsMediaGalleryProps,
+): React.Node {
   const styles = useStyles(unboundStyles);
   const galleryItemGap = 8;
   const { width } = useWindowDimensions();
 
   const galleryItemWidth = (width - galleryItemGap * 3) / 3;
-  const mediaInfos = React.useMemo(() => [], []);
+  const { threadID, limit } = props;
+  const [mediaInfos, setMediaInfos] = React.useState([]);
+  const callFetchThreadMedia = useServerCall(fetchThreadMedia);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const result = await callFetchThreadMedia({ threadID, limit, offset: 0 });
+      setMediaInfos(result.media);
+    };
+    fetchData();
+  }, [callFetchThreadMedia, threadID, limit]);
 
   const memoizedStyles = React.useMemo(() => {
     return {
@@ -39,7 +60,21 @@ function ThreadSettingsMediaGallery(): React.Node {
       return (
         <View key={item.id} style={memoizedStyles.mediaContainer}>
           <TouchableOpacity>
-            <Image source={item.source} style={memoizedStyles.media} />
+            {item.type === 'photo' ? (
+              <FastImage
+                source={{ uri: item.uri }}
+                style={memoizedStyles.media}
+              />
+            ) : (
+              <Video
+                source={{ uri: item.uri }}
+                style={memoizedStyles.media}
+                resizeMode="cover"
+                repeat={true}
+                muted={true}
+                paused={false}
+              />
+            )}
           </TouchableOpacity>
         </View>
       );
