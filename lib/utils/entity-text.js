CommE2E/comm

commit bab60a49f5ee1f1d238ff6904db058b8552b2f97

[lib] Update ThreadInfo.uiName to return an array of UserEntities
Summary:
This diff enables all of the diffs below it in the stack to actually work. I had to wait until the last diff to turn everything on because it otherwise would break Flow types.

Depends on D6614

Test Plan: Tested in combination with all of the diffs earlier in the stack

Reviewers: atul

Reviewed By: atul

Subscribers: tomek

Differential Revision: https://phab.comm.dev/D6615

lib/utils/entity-text.js
@@ -15,7 +15,7 @@ import {
 import { basePluralize } from '../utils/text-utils';
 import type { GetENSNames } from './ens-helpers';
 
-type UserEntity = {
+export type UserEntity = {
   +type: 'user',
   +id: string,
   +username?: ?string,

lib/shared/thread-utils.js
@@ -73,13 +73,15 @@ import { useDispatchActionPromise, useServerCall } from '../utils/action-utils';
 import type { DispatchActionPromise } from '../utils/action-utils';
 import type { GetENSNames } from '../utils/ens-helpers';
 import {
+  ET,
   entityTextToRawString,
   getEntityTextAsString,
+  type UserEntity,
 } from '../utils/entity-text';
 import { values } from '../utils/objects';
 import { useSelector } from '../utils/redux-utils';
 import { firstLine } from '../utils/string-utils';
-import { pluralize, trimText } from '../utils/text-utils';
+import { trimText } from '../utils/text-utils';
 import { type ParserRules } from './markdown';
 import { getMessageTitle } from './message-utils';
 import { relationshipBlockedInEitherDirection } from './relationship-utils';
@@ -772,28 +774,16 @@ function filterThreadEditDetailedPermissions(
   return newPermissions;
 }
 
-function robotextName(
-  threadInfo: RawThreadInfo | ThreadInfo,
-  viewerID: ?string,
-  userInfos: UserInfos,
-): string {
-  const threadUsernames = threadOtherMembers(threadInfo.members, viewerID)
-    .map(member => userInfos[member.id] && userInfos[member.id].username)
-    .filter(Boolean);
-  if (threadUsernames.length === 0) {
-    return 'just you';
-  }
-  return pluralize(threadUsernames);
-}
-
 function threadUIName(
-  threadInfo: RawThreadInfo | ThreadInfo,
-  viewerID: ?string,
-  userInfos: UserInfos,
-): string {
-  return firstLine(
-    threadInfo.name || robotextName(threadInfo, viewerID, userInfos),
+  threadInfo: ThreadInfo,
+): string | $ReadOnlyArray<UserEntity> {
+  if (threadInfo.name) {
+    return firstLine(threadInfo.name);
+  }
+  const threadMembers = threadInfo.members.filter(
+    memberInfo => memberInfo.role,
   );
+  return threadMembers.map(member => ET.user({ userInfo: member }));
 }
 
 function threadInfoFromRawThreadInfo(
@@ -805,7 +795,7 @@ function threadInfoFromRawThreadInfo(
     id: rawThreadInfo.id,
     type: rawThreadInfo.type,
     name: rawThreadInfo.name,
-    uiName: threadUIName(rawThreadInfo, viewerID, userInfos),
+    uiName: '',
     description: rawThreadInfo.description,
     color: rawThreadInfo.color,
     creationTime: rawThreadInfo.creationTime,
@@ -816,6 +806,10 @@ function threadInfoFromRawThreadInfo(
     roles: rawThreadInfo.roles,
     currentUser: getCurrentUser(rawThreadInfo, viewerID, userInfos),
     repliesCount: rawThreadInfo.repliesCount,
+  };
+  threadInfo = {
+    ...threadInfo,
+    uiName: threadUIName(threadInfo),
   };
   const { sourceMessageID } = rawThreadInfo;
   if (sourceMessageID) {
@@ -1494,7 +1488,6 @@ export {
   threadFrozenDueToViewerBlock,
   rawThreadInfoFromServerThreadInfo,
   filterThreadEditDetailedPermissions,
-  robotextName,
   threadUIName,
   threadInfoFromRawThreadInfo,
   rawThreadInfoFromThreadInfo,

lib/types/thread-types.js
@@ -2,6 +2,7 @@
 
 import invariant from 'invariant';
 
+import type { UserEntity } from '../utils/entity-text';
 import type { Shape } from './core';
 import type { CalendarQuery, RawEntryInfo } from './entry-types';
 import type {
@@ -207,7 +208,7 @@ export type ThreadInfo = {
   +id: string,
   +type: ThreadType,
   +name: ?string,
-  +uiName: string,
+  +uiName: string | $ReadOnlyArray<UserEntity>,
   +description: ?string,
   +color: string, // hex, without "#" or "0x"
   +creationTime: number, // millisecond timestamp

web/modals/threads/settings/thread-settings-modal.react.js
@@ -52,10 +52,6 @@ const ConnectedThreadSettingsModal: React.ComponentType<BaseProps> = React.memo<
         deleteThreadLoadingStatusSelector(state) === 'loading' ||
         changeThreadSettingsLoadingStatusSelector(state) === 'loading',
     );
-    const viewerID = useSelector(
-      state => state.currentUserInfo && state.currentUserInfo.id,
-    );
-    const userInfos = useSelector(state => state.userStore.userInfos);
     const threadInfo: ?ThreadInfo = useSelector(
       state => threadInfoSelector(state)[props.threadID],
     );
@@ -76,19 +72,22 @@ const ConnectedThreadSettingsModal: React.ComponentType<BaseProps> = React.memo<
       const withNoName = { ...threadInfo, name: undefined };
       return {
         ...withNoName,
-        uiName: threadUIName(withNoName, viewerID, userInfos),
+        uiName: threadUIName(withNoName),
       };
-    }, [threadInfo, viewerID, userInfos]);
+    }, [threadInfo]);
     const resolvedThreadInfo = useResolvedThreadInfo(threadInfoWithNoName);
     const namePlaceholder = resolvedThreadInfo.uiName;
 
+    const viewerID = useSelector(
+      state => state.currentUserInfo && state.currentUserInfo.id,
+    );
+    const userInfos = useSelector(state => state.userStore.userInfos);
     const otherMemberID = React.useMemo(() => {
       if (!threadInfo) {
         return null;
       }
       return getSingleOtherUser(threadInfo, viewerID);
     }, [threadInfo, viewerID]);
-
     const otherUserInfo = otherMemberID ? userInfos[otherMemberID] : null;
 
     const availableRelationshipActions = React.useMemo(() => {

commit 181fa7607ab661e426001a466ae113b6442851f4

[lib] Show "just you" for thread title if there are no members besides you
Summary:
This resolves [ENG-2933](https://linear.app/comm/issue/ENG-2933/calendar-filters-displaying-incorrectly-on-master), which was broken in D6615 when I removed the `'just you'` special-case there.

Since filtering out the user (and pluralization) are now handled in `getNameForThreadEntity`, we need to check the length of the list there instead of in `threadUIName` now.

Test Plan: Make sure there are no empty strings in the web calendar filters, and that 'just you' appears where the empty strings previously did

Reviewers: inka, tomek, atul

Reviewed By: tomek

Differential Revision: https://phab.comm.dev/D6628

lib/utils/entity-text.js
@@ -228,6 +228,9 @@ function getNameForThreadEntity(
     if (!params?.ignoreViewer) {
       userEntities = userEntities.filter(innerEntity => !innerEntity.isViewer);
     }
+    if (userEntities.length === 0) {
+      return 'just you';
+    }
     const pluralized = pluralizeEntityText(
       userEntities.map(innerEntity => [innerEntity]),
     );