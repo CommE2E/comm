6acdedd79de8e853c92ed3e280eea8f382dae7a1
Tomasz Palys <tomasz.palys@swmansion.com>
2023-02-08 13:11:41 +0100

    [services][feature-flags] Set up the logger
    Summary: Use tracing as a logging library - the same as is used in other services.
    Added a couple of dependencies with the same versions as used in `backup` service.
    
    Depends on D6663
    
    Test Plan: Run the service and check if log message is displayed.
    Introduce two new logs with debug and warn severity and check if only warn is being displayed.
    Set `RUST_LOG` env variable to debug and later to warn and check if the logging filtering behaves correctly.
    
    Reviewers: michal, bartek, jon, varun

--- services/feature-flags/src/main.rs
+++ services/feature-flags/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-  println!("Hello, world!");
+use anyhow::Result;
+use tracing::{info, Level};
+use tracing_subscriber::EnvFilter;
+
+pub mod constants;
+
+fn configure_logging() -> Result<()> {
+  let filter = EnvFilter::builder()
+    .with_default_directive(Level::INFO.into())
+    .with_env_var(constants::LOG_LEVEL_ENV_VAR)
+    .from_env_lossy();
+
+  let subscriber = tracing_subscriber::fmt().with_env_filter(filter).finish();
+  tracing::subscriber::set_global_default(subscriber)?;
+  Ok(())
+}
+
+fn main() -> Result<()> {
+  configure_logging()?;
+  info!("Starting the service");
+  Ok(())
 }
