6f04010a54a40a12c432fc23b00a74caa65493fb
Inka Sokołowska <inka.sokolowska@swmansion.com>
2023-02-08 08:50:05 +0100

    [web] Add a selector fetching thread IDs according to COMMUNITIES calendar filter
    Summary:
    issue: https://linear.app/comm/issue/ENG-2794/make-it-possible-to-display-only-chats-from-a-given-community-in-the
    Since D6458 the calendar filters reducer reacts to `updateCalendarCommunityFilter` and `clearCalendarCommunityFilter` actions. We want to be able to exctract the thread IDs based on the filters
    these actions set.
    We also need to update `onScreenThreadInfos` to
    
    Test Plan:
    Dispatch `updateCalendarCommunityFilter` and `clearCalendarCommunityFilter` actions, log the value returned by `filteredCommunityThreadIDsSelector`. Check that it is correct. Check that it is only
    possible to add an event for a chat in the selected community (in the web app) - this is a result of updating `onScreenThreadInfos` that is used by `ThreadPickerModal`
    
    Reviewers: kamil, tomek
    
    Subscribers: ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D6461

--- web/selectors/thread-selectors.js
+++ web/selectors/thread-selectors.js
@@ -3,17 +3,21 @@
 import invariant from 'invariant';
 import * as React from 'react';
 import { useDispatch } from 'react-redux';
+import { createSelector } from 'reselect';
 
 import { ENSCacheContext } from 'lib/components/ens-cache-provider.react';
+import { threadInfoSelector } from 'lib/selectors/thread-selectors';
 import { createPendingSidebar } from 'lib/shared/thread-utils';
 import type {
   ComposableMessageInfo,
   RobotextMessageInfo,
 } from 'lib/types/message-types';
-import type { ThreadInfo } from 'lib/types/thread-types';
+import type { ThreadInfo, RawThreadInfo } from 'lib/types/thread-types';
+import { values } from 'lib/utils/objects';
 
 import { getDefaultTextMessageRules } from '../markdown/rules.react';
 import { updateNavInfoActionType } from '../redux/action-types';
+import type { AppState } from '../redux/redux-setup';
 import { useSelector } from '../redux/redux-utils';
 
 function useOnClickThread(
@@ -107,9 +111,37 @@ function useOnClickNewThread(): (event: SyntheticEvent<HTMLElement>) => void {
   );
 }
 
+function filteredCommunityThreadIDs(
+  communityID: string,
+  threadInfosObj: { +[id: string]: ThreadInfo | RawThreadInfo },
+): $ReadOnlySet<string> {
+  const threadInfos = values(threadInfosObj);
+  const threadIDs = threadInfos
+    .filter(
+      thread => thread.community === communityID || thread.id === communityID,
+    )
+    .map(item => item.id);
+  return new Set(threadIDs);
+}
+
+const filteredCommunityThreadIDsSelector: (
+  state: AppState,
+) => ?$ReadOnlySet<string> = createSelector(
+  (state: AppState) => state.communityIDFilter,
+  threadInfoSelector,
+  (communityIDFilter: ?string, threadInfos: { +[id: string]: ThreadInfo }) => {
+    if (!communityIDFilter) {
+      return null;
+    }
+    return filteredCommunityThreadIDs(communityIDFilter, threadInfos);
+  },
+);
+
 export {
   useOnClickThread,
   useThreadIsActive,
   useOnClickPendingSidebar,
   useOnClickNewThread,
+  filteredCommunityThreadIDsSelector,
+  filteredCommunityThreadIDs,
 };
