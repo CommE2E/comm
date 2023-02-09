8bb14f8a38f75f60f8dca5bcb62c67b6e6234a7e
Tomasz Palys <tomasz.palys@swmansion.com>
2023-02-09 17:35:58 +0100

    [services][feature-flags] Introduce AWS config
    Summary: Add new configuration for AWS and localstack. Versions of added libraries are the same as in `backup` service.
    Depends on D6666
    
    Test Plan: Write additional code to connect to dynamodb instance and check if it is possible to list all the tables both from remote and localstack instance.
    
    Reviewers: michal, bartek, jon, varun

--- services/feature-flags/src/config.rs
+++ services/feature-flags/src/config.rs
@@ -1,7 +1,10 @@
+use aws_sdk_dynamodb::{Endpoint, Region};
 use clap::{builder::FalseyValueParser, Parser};
+use http::Uri;
 use once_cell::sync::Lazy;
+use tracing::info;
 
-use crate::constants::DEFAULT_LOCALSTACK_URL;
+use crate::constants::{AWS_REGION, DEFAULT_LOCALSTACK_URL};
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -19,4 +22,21 @@ pub static CONFIG: Lazy<AppConfig> = Lazy::new(|| AppConfig::parse());
 
 pub fn parse_cmdline_args() {
   Lazy::force(&CONFIG);
+}
+
+pub async fn load_aws_config() -> aws_types::SdkConfig {
+  let mut config_builder =
+    aws_config::from_env().region(Region::new(AWS_REGION));
+
+  if CONFIG.is_sandbox {
+    info!(
+      "Running in sandbox environment. Localstack URL: {}",
+      &CONFIG.localstack_url
+    );
+    config_builder = config_builder.endpoint_resolver(Endpoint::immutable(
+      Uri::from_static(&CONFIG.localstack_url),
+    ));
+  }
+
+  config_builder.load().await
 }
