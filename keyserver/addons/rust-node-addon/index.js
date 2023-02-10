0417bcad2e121554a656f3899fda1968eb86cf5a
Max Kalashnikoff <geekmaks@gmail.com>
2023-02-10 19:02:11 +0200

    [keyserver] Adding of Tunnelbroker client type and getter from napi

--- keyserver/addons/rust-node-addon/index.js
+++ keyserver/addons/rust-node-addon/index.js
@@ -34,8 +34,42 @@ async function getIdentityRustAPI(): Promise<IdentityAPI> {
     throw new Error('Failed to load native binding');
   }
 
-  const { registerUser } = nativeBinding.default;
+  const { registerUser } = nativeBinding.registerUser;
   return { registerUser };
 }
 
-export { getIdentityRustAPI };
+interface TunnelbrokerClientClass {
+  new(
+    onReceiveCallback: (err: Error | null, value: string) => any,
+  ): TunnelbrokerClientClass;
+  publish(toDeviceId: string, payload: string): Promise<void>;
+}
+
+async function getTunnelbrokerRustClient(): Promise<TunnelbrokerClientClass> {
+  let nativeBinding = null;
+  if (platform === 'darwin' && arch === 'x64') {
+    // $FlowFixMe
+    nativeBinding = await import('./naupi/rust-node-addon.darwin-x64.node');
+  } else if (platform === 'darwin' && arch === 'arm64') {
+    // $FlowFixMe
+    nativeBinding = await import('./napi/rust-node-addon.darwin-arm64.node');
+  } else if (platform === 'linux' && arch === 'x64') {
+    // $FlowFixMe
+    nativeBinding = await import('./napi/rust-node-addon.linux-x64-gnu.node');
+  } else if (platform === 'linux' && arch === 'arm64') {
+    // $FlowFixMe
+    nativeBinding = await import('./napi/rust-node-addon.linux-arm64-gnu.node');
+  } else {
+    throw new Error(`Unsupported OS: ${platform}, architecture: ${arch}`);
+  }
+
+  if (!nativeBinding) {
+    throw new Error('Failed to load native binding');
+  }
+
+  const { TunnelbrokerClient } = nativeBinding.TunnelbrokerClient;
+  // $FlowFixMe
+  return { TunnelbrokerClient };
+}
+
+export { getIdentityRustAPI, getTunnelbrokerRustClient };
