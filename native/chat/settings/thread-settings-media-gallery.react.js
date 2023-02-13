9e1681ed15f33b00fe7df75fcb2015197dc36958
Rohan Khanderia <rohan.khanderia@gmail.com>
2023-02-13 11:32:50 -0500

    [native] Navigate to the image / video modal when a user clicks on a media item
    Summary: We want to allow users to click on a media item in the gallery and navigate to either the `VideoPlaybackModal` or the `ImageModal`. The `key` parameter for the navigation is similar to the
    `getMediaKey` method (https://github.com/CommE2E/comm/blob/dc2f711faf30930a7c9f7f906a2a2b9f28d38bb0/native/chat/multimedia-message-utils.js#L129-L134). Here, for the thread media gallery, the key
    `multimedia|${threadID}|${mediaInfo.index}` is unique per media so when the key is pushed to the StackNavigator, there will be no existing duplicate key. We calculate the corners by checking the index of the
    media item relative to the gallery and the length of `mediaInfos`.
    
    https://linear.app/comm/issue/ENG-2990/navigate-to-the-image-video-modal-when-a-user-clicks-on-a-media-item
    
    Test Plan: Checked that the navigation works on both iOS and Android.
    
    Reviewers: atul, ginsu, tomek
    
    Subscribers:

--- native/chat/settings/thread-settings-media-gallery.react.js
+++ native/chat/settings/thread-settings-media-gallery.react.js
@@ -1,5 +1,6 @@
 // @flow
 
+import { useNavigation, useRoute } from '@react-navigation/native';
 import * as React from 'react';
 import { View, TouchableOpacity, useWindowDimensions } from 'react-native';
 import FastImage from 'react-native-fast-image';
@@ -7,9 +8,18 @@ import { FlatList } from 'react-native-gesture-handler';
 import Video from 'react-native-video';
 
 import { fetchThreadMedia } from 'lib/actions/thread-actions.js';
+import type { Corners, MediaInfo } from 'lib/types/media-types.js';
 import { useServerCall } from 'lib/utils/action-utils.js';
 
+import {
+  ImageModalRouteName,
+  VideoPlaybackModalRouteName,
+} from '../../navigation/route-names.js';
 import { useStyles } from '../../themes/colors.js';
+import type {
+  LayoutCoordinates,
+  VerticalBounds,
+} from '../../types/layout-types.js';
 
 const galleryItemGap = 8;
 const numColumns = 3;
@@ -17,6 +27,7 @@ const numColumns = 3;
 type ThreadSettingsMediaGalleryProps = {
   +threadID: string,
   +limit: number,
+  +verticalBounds: ?VerticalBounds,
 };
 
 function ThreadSettingsMediaGallery(
@@ -24,6 +35,8 @@ function ThreadSettingsMediaGallery(
 ): React.Node {
   const styles = useStyles(unboundStyles);
   const { width } = useWindowDimensions();
+  const navigation = useNavigation();
+  const route = useRoute();
 
   // Explanation of galleryItemWidth:
   // The FlatList has a horizontal padding of 16px on each side,
@@ -35,7 +48,7 @@ function ThreadSettingsMediaGallery(
   const galleryItemWidth =
     (width - 32 - (numColumns - 1) * galleryItemGap) / numColumns;
 
-  const { threadID, limit } = props;
+  const { threadID, limit, verticalBounds } = props;
   const [mediaInfos, setMediaInfos] = React.useState([]);
   const callFetchThreadMedia = useServerCall(fetchThreadMedia);
 
@@ -67,6 +80,55 @@ function ThreadSettingsMediaGallery(
     };
   }, [galleryItemWidth, styles.media, styles.mediaContainer]);
 
+  const navigateToMedia = React.useCallback(
+    (nativeEvent, item, index) => {
+      const { pageX, pageY } = nativeEvent;
+
+      const initialCoordinates: LayoutCoordinates = {
+        x: pageX,
+        y: pageY,
+        width: galleryItemWidth,
+        height: 180,
+      };
+
+      const corners: Corners = {
+        topLeft: index === 0,
+        topRight: index === numColumns - 1,
+        bottomLeft: index === mediaInfos.length - numColumns,
+        bottomRight: index === mediaInfos.length - 1,
+      };
+
+      const mediaInfo: MediaInfo = {
+        ...item,
+        index,
+        corners,
+      };
+
+      navigation.navigate<'VideoPlaybackModal' | 'ImageModal'>({
+        name:
+          mediaInfo.type === 'video'
+            ? VideoPlaybackModalRouteName
+            : ImageModalRouteName,
+        key: `multimedia|${threadID}|${mediaInfo.index}`,
+        params: {
+          presentedFrom: route.key,
+          mediaInfo,
+          item,
+          initialCoordinates,
+          verticalBounds,
+        },
+      });
+    },
+    [
+      galleryItemWidth,
+      mediaInfos.length,
+      navigation,
+      route.key,
+      threadID,
+      verticalBounds,
+    ],
+  );
+
   const renderItem = React.useCallback(
     ({ item, index }) => {
       const containerStyle =
@@ -77,7 +139,9 @@ function ThreadSettingsMediaGallery(
       if (item.type === 'photo') {
         return (
           <View key={item.id} style={containerStyle}>
-            <TouchableOpacity>
+            <TouchableOpacity
+              onPress={e => navigateToMedia(e.nativeEvent, item, index)}
+            >
               <FastImage
                 source={{ uri: item.uri }}
                 style={memoizedStyles.media}
@@ -88,7 +152,9 @@ function ThreadSettingsMediaGallery(
       }
       return (
         <View key={item.id} style={containerStyle}>
-          <TouchableOpacity>
+          <TouchableOpacity
+            onPress={e => navigateToMedia(e.nativeEvent, item, index)}
+          >
             <Video
               source={{ uri: item.uri }}
               style={memoizedStyles.media}
@@ -105,6 +171,7 @@ function ThreadSettingsMediaGallery(
       memoizedStyles.media,
       memoizedStyles.mediaContainer,
       memoizedStyles.mediaContainerWithMargin,
+      navigateToMedia,
     ],
   );
 
