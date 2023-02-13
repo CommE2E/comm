4b70814a8bd4e950f069b236243ada1404bb5783
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2023-02-13 17:01:23 -0500

    [services][identity] refactor streaming RPCs to remove loops, use new client method to add users to DDB
    Summary:
    - due to how interconnected this code is, it was hard to split it out into more diffs
    - using the update_item API to add new users to the users table in DynamoDB was a mistake. we should be using put_item (hence why we now call the add_user_to_users_table fn, which uses the put_item API)
    - since we know exactly how many messages we'll receive in the stream, we can avoid looping through messages on the server side
    
    Test Plan: Called register_user and login_user from main.rs and confirmed that both workflows still work
    
    Reviewers: jon, bartek, tomek
    
    Reviewed By: jon
    
    Subscribers: ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D6723

---
+++ services/identity/src/service/login.rs
@@ -0,0 +1,97 @@
+use super::*;
+pub struct LoginState {
+  user_id: String,
+  device_id: String,
+  user_public_key: String,
+  pake_state: ServerLogin<Cipher>,
+}
+pub async fn handle_login_request(
+  message: Option<Result<LoginRequest, Status>>,
+  tx: mpsc::Sender<Result<LoginResponse, Status>>,
+  client: DatabaseClient,
+  config: &Config,
+) -> Result<Option<LoginState>, Status> {
+  match message {
+    Some(Ok(LoginRequest {
+      data: Some(WalletLoginRequest(req)),
+    })) => {
+      let wallet_login_result =
+        wallet_login_helper(client, req, &mut OsRng).await;
+      if let Err(e) = tx.send(wallet_login_result).await {
+        error!("Response was dropped: {}", e);
+        Err(Status::aborted("failure"))
+      } else {
+        Ok(None)
+      }
+    }
+    Some(Ok(LoginRequest {
+      data:
+        Some(PakeLoginRequest(PakeLoginRequestStruct {
+          data:
+            Some(PakeCredentialRequestAndUserId(
+              pake_credential_request_and_user_id,
+            )),
+        })),
+    })) => {
+      let response_and_state = pake_login_start(
+        config,
+        client,
+        &pake_credential_request_and_user_id.user_id,
+        &pake_credential_request_and_user_id.pake_credential_request,
+      )
+      .await?;
+      let login_response = LoginResponse {
+        data: Some(PakeLoginResponse(response_and_state.response)),
+      };
+      if let Err(e) = tx.send(Ok(login_response)).await {
+        error!("Response was dropped: {}", e);
+        return Err(Status::aborted("failure"));
+      }
+
+      Ok(Some(LoginState {
+        user_id: pake_credential_request_and_user_id.user_id,
+        device_id: pake_credential_request_and_user_id.device_id,
+        user_public_key: pake_credential_request_and_user_id.user_public_key,
+        pake_state: response_and_state.pake_state,
+      }))
+    }
+    Some(_) | None => Err(Status::aborted("failure")),
+  }
+}
+
+pub async fn handle_credential_finalization(
+  message: Option<Result<LoginRequest, Status>>,
+  tx: mpsc::Sender<Result<LoginResponse, Status>>,
+  client: DatabaseClient,
+  login_state: LoginState,
+) -> Result<(), Status> {
+  match message {
+    Some(Ok(LoginRequest {
+      data:
+        Some(PakeLoginRequest(PakeLoginRequestStruct {
+          data: Some(PakeCredentialFinalization(pake_credential_finalization)),
+        })),
+    })) => {
+      let login_finish_result = pake_login_finish(
+        &login_state.user_id,
+        &login_state.device_id,
+        &login_state.user_public_key,
+        client,
+        login_state.pake_state,
+        &pake_credential_finalization,
+        &mut OsRng,
+        PakeWorkflow::Login,
+      )
+      .await
+      .map(|pake_login_response| LoginResponse {
+        data: Some(PakeLoginResponse(pake_login_response)),
+      });
+      if let Err(e) = tx.send(login_finish_result).await {
+        error!("Response was dropped: {}", e);
+        return Err(Status::aborted("failure"));
+      }
+      Ok(())
+    }
+    Some(_) | None => Err(Status::aborted("failure")),
+  }
+}
---
+++ services/identity/src/service/registration.rs
@@ -0,0 +1,153 @@
+use super::*;
+pub struct RegistrationState {
+  user_id: String,
+  device_id: String,
+  username: String,
+  user_public_key: String,
+  pub pake_state: Option<ServerRegistration<Cipher>>,
+}
+
+pub async fn handle_registration_request(
+  message: Option<Result<RegistrationRequest, Status>>,
+  client: DatabaseClient,
+  tx: mpsc::Sender<Result<RegistrationResponse, Status>>,
+  config: &Config,
+) -> Result<RegistrationState, Status> {
+  match message {
+    Some(Ok(RegistrationRequest {
+      data:
+        Some(PakeRegistrationRequestAndUserId(
+          pake_registration_request_and_user_id,
+        )),
+    })) => {
+      let get_item_output = client
+        .get_item_from_users_table(
+          &pake_registration_request_and_user_id.user_id,
+        )
+        .await;
+      match get_item_output {
+        Ok(GetItemOutput { item: Some(_), .. }) => {
+          error!("User already exists");
+          if let Err(e) = tx
+            .send(Err(Status::already_exists("User already exists")))
+            .await
+          {
+            error!("Response was dropped: {}", e);
+          }
+        }
+        Err(e) => return Err(handle_db_error(e)),
+        _ => {}
+      };
+      let response_and_state = pake_registration_start(
+        config,
+        &mut OsRng,
+        &pake_registration_request_and_user_id.pake_registration_request,
+      )
+      .await?;
+      if let Err(e) = tx.send(Ok(response_and_state.response)).await {
+        error!("Response was dropped: {}", e);
+      }
+
+      Ok(RegistrationState {
+        user_id: pake_registration_request_and_user_id.user_id,
+        device_id: pake_registration_request_and_user_id.device_id,
+        username: pake_registration_request_and_user_id.username,
+        user_public_key: pake_registration_request_and_user_id.user_public_key,
+        pake_state: Some(response_and_state.pake_state),
+      })
+    }
+    None | Some(_) => Err(Status::aborted("failure")),
+  }
+}
+
+pub async fn handle_registration_upload_and_credential_request(
+  message: Option<Result<RegistrationRequest, Status>>,
+  tx: mpsc::Sender<Result<RegistrationResponse, Status>>,
+  client: DatabaseClient,
+  registration_state: &RegistrationState,
+  pake_state: ServerRegistration<Cipher>,
+  config: &Config,
+) -> Result<ServerLogin<Cipher>, Status> {
+  match message {
+    Some(Ok(RegistrationRequest {
+      data:
+        Some(PakeRegistrationUploadAndCredentialRequest(
+          pake_registration_upload_and_credential_request,
+        )),
+    })) => {
+      let response_and_state = match pake_registration_finish(
+        &registration_state.user_id,
+        &registration_state.device_id,
+        client.clone(),
+        &pake_registration_upload_and_credential_request
+          .pake_registration_upload,
+        Some(pake_state),
+        &registration_state.username,
+        &registration_state.user_public_key,
+      )
+      .await
+      {
+        Ok(_) => {
+          pake_login_start(
+            config,
+            client,
+            &registration_state.user_id,
+            &pake_registration_upload_and_credential_request
+              .pake_credential_request,
+          )
+          .await?
+        }
+
+        Err(e) => {
+          return Err(e);
+        }
+      };
+      let registration_response = RegistrationResponse {
+        data: Some(PakeRegistrationLoginResponse(response_and_state.response)),
+      };
+      if let Err(e) = tx.send(Ok(registration_response)).await {
+        error!("Response was dropped: {}", e);
+        Err(Status::aborted("failure"))
+      } else {
+        Ok(response_and_state.pake_state)
+      }
+    }
+    None | Some(_) => Err(Status::aborted("failure")),
+  }
+}
+
+pub async fn handle_credential_finalization(
+  message: Option<Result<RegistrationRequest, Status>>,
+  tx: mpsc::Sender<Result<RegistrationResponse, Status>>,
+  client: DatabaseClient,
+  registration_state: &RegistrationState,
+  server_login: ServerLogin<Cipher>,
+) -> Result<(), Status> {
+  match message {
+    Some(Ok(RegistrationRequest {
+      data:
+        Some(PakeRegistrationCredentialFinalization(pake_credential_finalization)),
+    })) => {
+      let login_finish_result = pake_login_finish(
+        &registration_state.user_id,
+        &registration_state.device_id,
+        &registration_state.user_public_key,
+        client,
+        server_login,
+        &pake_credential_finalization,
+        &mut OsRng,
+        PakeWorkflow::Registration,
+      )
+      .await
+      .map(|pake_login_response| RegistrationResponse {
+        data: Some(PakeRegistrationLoginResponse(pake_login_response)),
+      });
+      if let Err(e) = tx.send(login_finish_result).await {
+        error!("Response was dropped: {}", e);
+        return Err(Status::aborted("failure"));
+      }
+      Ok(())
+    }
+    Some(_) | None => Err(Status::aborted("failure")),
+  }
+}
