f7c3512361e4807c98a5aaf4435f4e444e4b4fa4
Inka Sokołowska <inka.sokolowska@swmansion.com>
2023-02-14 09:16:39 +0100

    [web] Add expand buttons
    Summary: Add buttons to be used in community drawer to expand items.
    
    Test Plan: Displayed buttons in AppSwitcher, checked that they display as expected and onClick works
    
    Reviewers: przemek, michal, ginsu
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D6158

---
+++ web/sidebar/expand-buttons.react.js
@@ -0,0 +1,33 @@
+// @flow
+
+import { faCaretDown, faCaretRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames';
+import * as React from 'react';
+
+import css from './expand-buttons.css';
+import Button from '../components/button.react.js';
+
+type Props = {
+  +onClick?: () => mixed,
+  +expanded?: boolean,
+  +disabled?: boolean,
+};
+
+function ExpandButton(props: Props): React.Node {
+  const { onClick, expanded = false, disabled } = props;
+  const icon = expanded ? faCaretDown : faCaretRight;
+
+  const iconClass = classNames({
+    [css.disabledButtonIcon]: disabled,
+    [css.buttonIcon]: !disabled,
+  });
+
+  return (
+    <Button onClick={onClick} className={css.button} disabled={disabled}>
+      <FontAwesomeIcon icon={icon} className={iconClass} />
+    </Button>
+  );
+}
+
+export { ExpandButton };
