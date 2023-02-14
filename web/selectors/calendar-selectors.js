6238c432bc99eb4506309997965a77d232e894b3
Inka Sokołowska <inka.sokolowska@swmansion.com>
2023-02-14 09:16:43 +0100

    [web] Add a selector fetching thread IDs according to COMMUNITIES calendar filter
    Summary:
    issue: https://linear.app/comm/issue/ENG-2794/make-it-possible-to-display-only-chats-from-a-given-community-in-the
    Since D6645 the calendar filters reducer reacts to `updateCalendarCommunityFilter` and `clearCalendarCommunityFilter` actions. We want to be able to extract the thread IDs based on the filters these actions have set.
    
    Test Plan:
    Dispatch `updateCalendarCommunityFilter` and `clearCalendarCommunityFilter` actions, log the value returned by `filteredCommunityThreadIDsSelector`. Check that it is correct. Check that it is only
    possible to add an event for a chat in the selected community (in the web app) - this is a result of updating `onScreenThreadInfos` that is used by `ThreadPickerModal`
    
    Reviewers: kamil, tomek
    
    Reviewed By: kamil, tomek
    
    Subscribers: ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D6461

--- web/selectors/calendar-selectors.js
+++ web/selectors/calendar-selectors.js
@@ -1,12 +1,19 @@
 // @flow
 
+import { createSelector } from 'reselect';
+
 import {
   useFilterThreadInfos as baseUseFilterThreadInfos,
   useFilterThreadSearchIndex as baseUseFilterThreadSearchIndex,
 } from 'lib/selectors/calendar-selectors.js';
+import { threadInfoSelector } from 'lib/selectors/thread-selectors.js';
 import type SearchIndex from 'lib/shared/search-index.js';
+import { threadInFilterList } from 'lib/shared/thread-utils.js';
 import type { FilterThreadInfo } from 'lib/types/filter-types.js';
+import type { ThreadInfo, RawThreadInfo } from 'lib/types/thread-types.js';
+import { values } from 'lib/utils/objects.js';
 
+import type { AppState } from '../redux/redux-setup.js';
 import { useSelector } from '../redux/redux-utils.js';
 
 function useFilterThreadInfos(): $ReadOnlyArray<FilterThreadInfo> {
@@ -19,4 +26,37 @@ function useFilterThreadSearchIndex(): SearchIndex {
   return baseUseFilterThreadSearchIndex(calendarActive);
 }
 
-export { useFilterThreadInfos, useFilterThreadSearchIndex };
+function filterThreadIDsBelongingToCommunity(
+  communityID: string,
+  threadInfosObj: { +[id: string]: ThreadInfo | RawThreadInfo },
+): $ReadOnlySet<string> {
+  const threadInfos = values(threadInfosObj);
+  const threadIDs = threadInfos
+    .filter(
+      thread =>
+        (thread.community === communityID || thread.id === communityID) &&
+        threadInFilterList(thread),
+    )
+    .map(item => item.id);
+  return new Set(threadIDs);
+}
+
+const filterThreadIDsBelongingToCommunitySelector: (
+  state: AppState,
+) => ?$ReadOnlySet<string> = createSelector(
+  (state: AppState) => state.communityIDFilter,
+  threadInfoSelector,
+  (communityIDFilter: ?string, threadInfos: { +[id: string]: ThreadInfo }) => {
+    if (!communityIDFilter) {
+      return null;
+    }
+    return filterThreadIDsBelongingToCommunity(communityIDFilter, threadInfos);
+  },
+);
+
+export {
+  useFilterThreadInfos,
+  useFilterThreadSearchIndex,
+  filterThreadIDsBelongingToCommunitySelector,
+  filterThreadIDsBelongingToCommunity,
+};
