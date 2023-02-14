73c9b4d5dfdbb75c038c645a672ec7b712233d4f
Inka Sokołowska <inka.sokolowska@swmansion.com>
2023-02-14 09:16:39 +0100

    [web] Add drawer item implementations
    Summary:
    Linear issue: https://linear.app/comm/issue/ENG-2631/drawer-sidebar-inside-of-community-picker
    Adding drawer item implementations on native: D5721
    There are two types of drawer items - [community] and [a thread in a community]. They behave almost the same, but there is a difference when their expand buttons are toggled: only one community can
    be open, so opening a community sometimes has to trigger closing another. On the other hand multiple level 1 and level 2 threads can be open at once.
    The information about which community is open is thus held by a parent component, and a community is passed an expanded flag and a setExpanded function allowing it to change its parents state.
    This is why I created CommunityDrawerItemCommunity and CommunityDrawerItemChat components.
    
    Test Plan:
    Added community drawer item to ChatThreadListItem, checked that pressinng it navigates to the corresponding thread, and pressinng its subchannels button navigates to the corresponding subchannels
    modal.
    
    Reviewers: przemek, michal, ginsu
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D6160

---
+++ web/sidebar/community-drawer-item-community.react.js
@@ -0,0 +1,25 @@
+// @flow
+
+import classnames from 'classnames';
+import * as React from 'react';
+
+import css from './community-drawer-item.css';
+import type { DrawerItemProps } from './community-drawer-item.react.js';
+import CommunityDrawerItem from './community-drawer-item.react.js';
+
+function CommunityDrawerItemCommunity(props: DrawerItemProps): React.Node {
+  const classes = classnames({
+    [css.communityBase]: true,
+    [css.communityExpanded]: props.expanded,
+  });
+  return (
+    <div className={classes}>
+      <CommunityDrawerItem {...props} />
+    </div>
+  );
+}
+
+const MemoizedCommunityDrawerItemCommunity: React.ComponentType<DrawerItemProps> = React.memo(
+  CommunityDrawerItemCommunity,
+);
+export default MemoizedCommunityDrawerItemCommunity;
