5c087ebe0ed84811eb386a8f0fc2d45fa16eb8bd
Inka Sokołowska <inka.sokolowska@swmansion.com>
2023-02-14 09:16:41 +0100

    [web] Use app switcher in the topbar
    Summary:
    issue: https://linear.app/comm/issue/ENG-2739/move-navigationpanel-from-the-sidebar-to-a-top-bar
    Use `AppSwitcher` in `Topbar`. Now, that the `AppSwitcher` is inside of the `Topbar`, it can be made horizontal, and it can use the styles for a horizontal switcher. The styles for icons in the `left-layout-aside.css` are no longer needed,
    because the other switcher doesn't display any icons.
    
    Test Plan: use `Topbar` in `App` component, check that it displayes correctly.
    
    Reviewers: przemek, michal, bartek
    
    Reviewed By: bartek
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D6345

--- web/topbar/topbar.react.js
+++ web/topbar/topbar.react.js
@@ -2,10 +2,15 @@
 
 import * as React from 'react';
 
+import AppSwitcher from './app-switcher.react.js';
 import css from './topbar.css';
 
 function Topbar(): React.Node {
-  return <div className={css.container}>dummy</div>;
+  return (
+    <div className={css.container}>
+      <AppSwitcher />
+    </div>
+  );
 }
 
 const MemoizedTopbar: React.ComponentType<{}> = React.memo<{}>(Topbar);
