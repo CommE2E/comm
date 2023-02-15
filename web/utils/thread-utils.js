4ab37cb3344848919e290b30a12ef3539f83fe3b
Inka Sokołowska <inka.sokolowska@swmansion.com>
2023-02-15 14:17:36 +0100

    [web] Move logic for fetching ThreadInfo for possibly pending chat out of ChatMessageListContainer component
    Summary:
    Issue: https://linear.app/comm/issue/ENG-2740/add-navigation-state-info-to-the-top-bar
    The new designs call for the navigation state info to be moved from message list header, to main content's header. The component that is used for displaying the nav state info ('ThreadTopBar') needs the ThreadInfo of the open chat, or
    a special ThreadInfo object when a chat is being created. The excrated logic is used to obtain that exact ThreadInfo. This will allow me to use it in the `Topbar` component, where I'll need to display the nav state info.
    
    Test Plan: Run web app, check that the `ThreadTopBar` component displys correctly in the message list header
    
    Reviewers: michal, kamil, kuba
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D6741

---
+++ web/utils/thread-utils.js
@@ -0,0 +1,95 @@
+// @flow
+
+import invariant from 'invariant';
+import * as React from 'react';
+
+import { threadInfoSelector } from 'lib/selectors/thread-selectors.js';
+import { userInfoSelectorForPotentialMembers } from 'lib/selectors/user-selectors.js';
+import {
+  createPendingThread,
+  useExistingThreadInfoFinder,
+} from 'lib/shared/thread-utils.js';
+import type { ThreadInfo } from 'lib/types/thread-types.js';
+import { threadTypes } from 'lib/types/thread-types.js';
+import type { AccountUserInfo } from 'lib/types/user-types.js';
+
+import { useSelector } from '../redux/redux-utils.js';
+
+type Props = {
+  +activeChatThreadID: string,
+};
+
+function useThreadInfoForPossiblyPendingThread(props: Props): ?ThreadInfo {
+  const { activeChatThreadID } = props;
+
+  const isChatCreation =
+    useSelector(state => state.navInfo.chatMode) === 'create';
+  const selectedUserIDs = useSelector(state => state.navInfo.selectedUserList);
+  const otherUserInfos = useSelector(userInfoSelectorForPotentialMembers);
+  const userInfoInputArray: $ReadOnlyArray<AccountUserInfo> = React.useMemo(
+    () => selectedUserIDs?.map(id => otherUserInfos[id]).filter(Boolean) ?? [],
+    [otherUserInfos, selectedUserIDs],
+  );
+  const viewerID = useSelector(state => state.currentUserInfo?.id);
+  invariant(viewerID, 'should be set');
+  const newThreadID = 'pending/new_thread';
+
+  const pendingNewThread = React.useMemo(
+    () => ({
+      ...createPendingThread({
+        viewerID,
+        threadType: threadTypes.PRIVATE,
+        name: 'New thread',
+      }),
+      id: newThreadID,
+    }),
+    [viewerID],
+  );
+  const pendingPrivateThread = React.useRef(
+    createPendingThread({
+      viewerID,
+      threadType: threadTypes.PRIVATE,
+    }),
+  );
+  const existingThreadInfoFinderForCreatingThread = useExistingThreadInfoFinder(
+    pendingPrivateThread.current,
+  );
+
+  const baseThreadInfo = useSelector(state => {
+    if (!activeChatThreadID) {
+      return null;
+    }
+    return (
+      threadInfoSelector(state)[activeChatThreadID] ??
+      state.navInfo.pendingThread
+    );
+  });
+  const existingThreadInfoFinder = useExistingThreadInfoFinder(baseThreadInfo);
+  const threadInfo = React.useMemo(() => {
+    if (isChatCreation) {
+      if (userInfoInputArray.length === 0) {
+        return pendingNewThread;
+      }
+
+      return existingThreadInfoFinderForCreatingThread({
+        searching: true,
+        userInfoInputArray,
+      });
+    }
+
+    return existingThreadInfoFinder({
+      searching: false,
+      userInfoInputArray: [],
+    });
+  }, [
+    existingThreadInfoFinder,
+    existingThreadInfoFinderForCreatingThread,
+    isChatCreation,
+    userInfoInputArray,
+    pendingNewThread,
+  ]);
+
+  return threadInfo;
+}
+
+export { useThreadInfoForPossiblyPendingThread };
