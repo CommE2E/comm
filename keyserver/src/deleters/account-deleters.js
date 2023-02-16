bb1eee0985f8a11004fc52b59c4ce16297a1ed4a
Jonathan Ringer <jonringer117@gmail.com>
2023-02-15 20:52:06 -0800

    [Keyserver/rust-addon] Call identity deleteUser on account deletion
    Summary:
    Integrate identity service's delete_user functionality
    with Keyserver.
    
    Part of https://linear.app/comm/issue/ENG-2764
    Part of https://linear.app/comm/issue/ENG-2748
    
    Depends on D6789
    
    Test Plan:
    ```
    nix develop
    
    comm-dev services start # start localstack
    
    # Configure "aws" resources on localstack
    (cd services/terraform && ./run.sh)
    
    # start identity service and run mobile workflow
    (cd services/identity && cargo run -- server &)
    (cd keyserver && yarn dev &)
    (cd native && yarn dev &)
    ```
    
    (User exists in MySQL but not dynamodb use case)
    Run either the iOS or Android client:
    - Create an account
    - Delete the account
    - Assert that the keyserver is still running, and identity service emits logs attempting to delete the user
    
    (User exist in both MySQL and dynamodb)
    Run either the iOS or Android client:
    - Create an account
    - Use TablePlus or other tool to find userID of your user
    - Seed a user:
      - `aws dynamodb put-item --table-name identity-users --item '{"userID": {"S": "<USERID>"}}'`
    - Verify user exists:
      - `aws dynamodb get-item --table-name identity-users --key '{ "userID": {"S": "<USERID>"} }'`
    - Delete account in App
    - Verify user has been deleted:
      - `aws dynamodb get-item --table-name identity-users --key '{ "userID": {"S": "<USERID>"} }'`
    
    Reviewers: varun, ashoat
    
    Subscribers: tomek, atul
    
    Differential Revision: https://phab.comm.dev/D6790

--- keyserver/src/deleters/account-deleters.js
+++ keyserver/src/deleters/account-deleters.js
@@ -1,5 +1,6 @@
 // @flow
 
+import { getRustAPI } from 'rust-node-addon';
 import bcrypt from 'twin-bcrypt';
 
 import type {
@@ -48,6 +49,7 @@ async function deleteAccount(
 
   const deletedUserID = viewer.userID;
   await rescindPushNotifs(SQL`n.user = ${deletedUserID}`, SQL`NULL`);
+  const rustAPIPromise = getRustAPI();
   const knownUserInfos = await fetchKnownUserInfos(viewer);
   const usersToUpdate = values(knownUserInfos).filter(
     userID => userID !== deletedUserID,
@@ -95,7 +97,9 @@ async function deleteAccount(
       deviceToken: viewer.deviceToken,
     });
   }
-  const { anonymousViewerData } = await promiseAll(promises);
+  promises.rustAPI = rustAPIPromise;
+  const { anonymousViewerData, rustAPI } = await promiseAll(promises);
+  handleAsyncPromise(rustAPI.deleteUser(deletedUserID));
   if (anonymousViewerData) {
     viewer.setNewCookie(anonymousViewerData);
   }
