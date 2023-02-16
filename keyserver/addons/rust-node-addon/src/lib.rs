efbfd2061524783e9287bf748f7b289efabe8856
Jonathan Ringer <jonringer117@gmail.com>
2023-02-15 20:47:47 -0800

    [Keyserver] Move shared identity client resources to lib.rs

--- keyserver/addons/rust-node-addon/src/lib.rs
+++ keyserver/addons/rust-node-addon/src/lib.rs
@@ -1,4 +1,17 @@
+pub mod delete_user;
 pub mod identity_client;
+pub mod identity {
+  tonic::include_proto!("identity");
+}
 
 #[macro_use]
 extern crate napi_derive;
+
+use lazy_static::lazy_static;
+use std::env::var;
+
+lazy_static! {
+  pub static ref IDENTITY_SERVICE_SOCKET_ADDR: String =
+    var("COMM_IDENTITY_SERVICE_SOCKET_ADDR")
+      .unwrap_or("https://[::1]:50051".to_string());
+}
