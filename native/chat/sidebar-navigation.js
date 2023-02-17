603c11f2a8a9b00da0f6ceab5878363167c5416a
Ashoat Tevosyan <ashoat@comm.app>
2023-02-16 19:12:30 -0500

    [lib][native][web] Include current user as member when calling createPendingThread from baseCreatePendingSidebar
    Summary:
    This diff should be a no-op, as `createPendingThread` currently separates out the viewer.
    
    In the next diff, we'll update `createPendingThread` to require the viewer to be passed in as a member, so that we can always have the viewer's username available to us.
    
    Depends on D6757
    
    Test Plan: In combination with diffs I haven't submitted yet, I made sure that @-mentions of the viewer get bolded in a pending sidebar
    
    Reviewers: atul
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D6758

--- native/chat/sidebar-navigation.js
+++ native/chat/sidebar-navigation.js
@@ -4,33 +4,34 @@ import invariant from 'invariant';
 import * as React from 'react';
 
 import { ENSCacheContext } from 'lib/components/ens-cache-provider.react.js';
+import { useLoggedInUserInfo } from 'lib/hooks/account-hooks.js';
 import {
   createPendingSidebar,
   createUnresolvedPendingSidebar,
 } from 'lib/shared/thread-utils.js';
 import type { ThreadInfo } from 'lib/types/thread-types.js';
+import type { LoggedInUserInfo } from 'lib/types/user-types.js';
 import type { GetENSNames } from 'lib/utils/ens-helpers.js';
 
 import { ChatContext } from './chat-context.js';
 import { useNavigateToThread } from './message-list-types.js';
 import { getDefaultTextMessageRules } from '../markdown/rules.react.js';
-import { useSelector } from '../redux/redux-utils.js';
 import type { ChatMessageInfoItemWithHeight } from '../types/chat-types.js';
 
 type GetUnresolvedSidebarThreadInfoInput = {
   +sourceMessage: ChatMessageInfoItemWithHeight,
-  +viewerID?: ?string,
+  +loggedInUserInfo: ?LoggedInUserInfo,
 };
 function getUnresolvedSidebarThreadInfo(
   input: GetUnresolvedSidebarThreadInfoInput,
 ): ?ThreadInfo {
-  const { sourceMessage, viewerID } = input;
+  const { sourceMessage, loggedInUserInfo } = input;
   const threadCreatedFromMessage = sourceMessage.threadCreatedFromMessage;
   if (threadCreatedFromMessage) {
     return threadCreatedFromMessage;
   }
 
-  if (!viewerID) {
+  if (!loggedInUserInfo) {
     return null;
   }
 
@@ -38,7 +39,7 @@ function getUnresolvedSidebarThreadInfo(
   return createUnresolvedPendingSidebar({
     sourceMessageInfo: messageInfo,
     parentThreadInfo: threadInfo,
-    viewerID,
+    loggedInUserInfo,
     markdownRules: getDefaultTextMessageRules().simpleMarkdownRules,
   });
 }
@@ -50,13 +51,13 @@ type GetSidebarThreadInfoInput = {
 async function getSidebarThreadInfo(
   input: GetSidebarThreadInfoInput,
 ): Promise<?ThreadInfo> {
-  const { sourceMessage, viewerID, getENSNames } = input;
+  const { sourceMessage, loggedInUserInfo, getENSNames } = input;
   const threadCreatedFromMessage = sourceMessage.threadCreatedFromMessage;
   if (threadCreatedFromMessage) {
     return threadCreatedFromMessage;
   }
 
-  if (!viewerID) {
+  if (!loggedInUserInfo) {
     return null;
   }
 
@@ -64,7 +65,7 @@ async function getSidebarThreadInfo(
   return await createPendingSidebar({
     sourceMessageInfo: messageInfo,
     parentThreadInfo: threadInfo,
-    viewerID,
+    loggedInUserInfo,
     markdownRules: getDefaultTextMessageRules().simpleMarkdownRules,
     getENSNames,
   });
@@ -73,21 +74,19 @@ async function getSidebarThreadInfo(
 function useNavigateToSidebar(
   item: ChatMessageInfoItemWithHeight,
 ): () => mixed {
-  const viewerID = useSelector(
-    state => state.currentUserInfo && state.currentUserInfo.id,
-  );
+  const loggedInUserInfo = useLoggedInUserInfo();
   const navigateToThread = useNavigateToThread();
   const cacheContext = React.useContext(ENSCacheContext);
   const { getENSNames } = cacheContext;
   return React.useCallback(async () => {
     const threadInfo = await getSidebarThreadInfo({
       sourceMessage: item,
-      viewerID,
+      loggedInUserInfo,
       getENSNames,
     });
     invariant(threadInfo, 'threadInfo should be set');
     navigateToThread({ threadInfo });
-  }, [navigateToThread, item, viewerID, getENSNames]);
+  }, [navigateToThread, item, loggedInUserInfo, getENSNames]);
 }
 
 function useAnimatedNavigateToSidebar(
