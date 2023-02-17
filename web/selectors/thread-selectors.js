9fa53b2820dc39b42e977a007aff79b13d90eaa9
Inka Sokołowska <inka.sokolowska@swmansion.com>
2023-02-17 10:09:39 +0100

    [web] Add a selector for getting the item currently selected in the drawer
    Summary:
    issue: https://linear.app/comm/issue/ENG-2740/add-navigation-state-info-to-the-top-bar
    Adding a selector for getting the item currently selected in the drawer, taking into account that in different tabs this value is stored differently
    
    Test Plan: Log to the console the value returned when being in different tabs, checked that it is correct.
    
    Reviewers: tomek, michal, kamil
    
    Subscribers: ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D6746

--- web/selectors/thread-selectors.js
+++ web/selectors/thread-selectors.js
@@ -107,9 +107,22 @@ function useOnClickNewThread(): (event: SyntheticEvent<HTMLElement>) => void {
   );
 }
 
+function useDrawerSelectedThreadID(): ?string {
+  const activeChatThreadID = useSelector(
+    state => state.navInfo.activeChatThreadID,
+  );
+  const pickedCommunityID = useSelector(
+    state => state.calendarPickedCommunityID,
+  );
+  const inCalendar = useSelector(state => state.navInfo.tab === 'calendar');
+
+  return inCalendar ? pickedCommunityID : activeChatThreadID;
+}
+
 export {
   useOnClickThread,
   useThreadIsActive,
   useOnClickPendingSidebar,
   useOnClickNewThread,
+  useDrawerSelectedThreadID,
 };
