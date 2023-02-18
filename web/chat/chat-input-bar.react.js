3316fc0e8bb48b0f0400bca83f67f914eb2f495c
Ashoat Tevosyan <ashoat@comm.app>
2023-02-18 13:52:08 -0500

    [web][native] Rename threadMembers in mentions typeahead code to mentionsCandidates
    Summary:
    In a later diff I'm going to update the mentions typeahead to sometimes show results that aren't members of the thread, so I want to rename this variable to avoid misleading the reader.
    
    Depends on D6806
    
    Test Plan:
    I tested this stack with the following steps on both `web` and `native`:
    
    1. Make sure users get auto-added to a sidebar when @-mentioned
    2. Test mentions typeahead and make sure it includes all members of parent
    3. If a user is @-mentioned in a text message that triggers the creation of a sidebar, then make sure they are included in the sidebar creation message
    4. If a user is @-mentioned in a text message that triggers the creation of a sidebar, then make sure their name is bolded from the very start of when the message is visible on-screen
    
    Reviewers: atul
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D6807

--- web/chat/chat-input-bar.react.js
+++ web/chat/chat-input-bar.react.js
@@ -549,7 +549,7 @@ const ConnectedChatInputBar: React.ComponentType<BaseProps> =
     const dispatchActionPromise = useDispatchActionPromise();
     const callJoinThread = useServerCall(joinThread);
     const userSearchIndex = useSelector(userStoreSearchIndex);
-    const threadMembers = useSelector(
+    const mentionsCandidates = useSelector(
       relativeMemberInfoSelectorForMembersOfThread(props.threadInfo.id),
     );
 
@@ -582,11 +582,11 @@ const ConnectedChatInputBar: React.ComponentType<BaseProps> =
       if (props.inputState.typeaheadState.keepUpdatingThreadMembers) {
         const setter = props.inputState.setTypeaheadState;
         setter({
-          frozenThreadMembers: threadMembers,
+          frozenMentionsCandidates: mentionsCandidates,
         });
       }
     }, [
-      threadMembers,
+      mentionsCandidates,
       props.inputState.setTypeaheadState,
       props.inputState.typeaheadState.keepUpdatingThreadMembers,
     ]);
@@ -598,13 +598,13 @@ const ConnectedChatInputBar: React.ComponentType<BaseProps> =
         }
         return getTypeaheadUserSuggestions(
           userSearchIndex,
-          props.inputState.typeaheadState.frozenThreadMembers,
+          props.inputState.typeaheadState.frozenMentionsCandidates,
           viewerID,
           typeaheadMatchedStrings.usernamePrefix,
         );
       }, [
         userSearchIndex,
-        props.inputState.typeaheadState.frozenThreadMembers,
+        props.inputState.typeaheadState.frozenMentionsCandidates,
         viewerID,
         typeaheadMatchedStrings,
       ]);
