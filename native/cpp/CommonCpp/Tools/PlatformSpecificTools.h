83b4dcdb66d6e26b13b81059d09ea55e33375e2a
marcinwasowicz <marcinkwasowicz@gmail.com>
2023-02-20 12:17:55 +0100

    Extend PlatformSpecificTools CommonCpp api with an API to retrieve notifications olm account storage path
    Summary:
    This differential extends  PlatformSpecificTools CommonCpp api with an API to retrieve notifications olm account storage path. It also implements it on iOS and in C++ on Android. Relevant native Java method is implemented in parent differential to this one and is called via JNI
    in Android C++ implementation.
    
    Test Plan: Call `PlatformSpecificTools::getNotificationsCryptoAccountPath()` in NotificationService.mm and in AppDelegate.mm. This tests iOS. Call it somewhere in SQLiteQueryEXecutor or CommCoreModule. This tests on Android (and iOS as well).
    
    Reviewers: bartek, tomek, kamil
    
    Subscribers: ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D6773

--- native/cpp/CommonCpp/Tools/PlatformSpecificTools.h
+++ native/cpp/CommonCpp/Tools/PlatformSpecificTools.h
@@ -8,6 +8,7 @@ class PlatformSpecificTools {
 public:
   static void generateSecureRandomBytes(crypto::OlmBuffer &buffer, size_t size);
   static std::string getDeviceOS();
+  static std::string getNotificationsCryptoAccountPath();
 };
 
 } // namespace comm
