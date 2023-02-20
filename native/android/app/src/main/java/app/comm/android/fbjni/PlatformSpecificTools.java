f20bbd43d2c83fb155d4ce3189d18d1d1c1dd74d
marcinwasowicz <marcinkwasowicz@gmail.com>
2023-02-20 12:08:37 +0100

    Implement native method on Android to get notifications olm account storage path
    Summary:
    This differential extends PlatformSpecificTools.java with method to retrieve notifications olm account path. This functionality requires main application context to be accessible in PlatformSpecificTools. Normally we would pass it in constructor or as method argument, but this
    class is designed to be static method only class callable from C++. Therefore we need a "static" way of accessing main application context here. This approach is described here: https://stackoverflow.com/questions/2002288/static-way-to-get-context-in-android. Firther diffs will extend
    C++ header to include this method.
    
    Test Plan: Call this method from CommNotificationsHandler on each message and log path to the console.
    
    Reviewers: bartek
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D6772

--- native/android/app/src/main/java/app/comm/android/fbjni/PlatformSpecificTools.java
+++ native/android/app/src/main/java/app/comm/android/fbjni/PlatformSpecificTools.java
@@ -1,10 +1,11 @@
 package app.comm.android.fbjni;
 
+import android.content.Context;
 import android.util.Log;
+import app.comm.android.MainApplication;
 import java.nio.charset.StandardCharsets;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
-
 public class PlatformSpecificTools {
   static SecureRandom secureRandom = new SecureRandom();
 
@@ -12,5 +13,17 @@ public class PlatformSpecificTools {
     byte[] buffer = new byte[size];
     secureRandom.nextBytes(buffer);
     return buffer;
+  }
+
+  public static String getNotificationsCryptoAccountPath() {
+    Context mainApplicationContext =
+        MainApplication.getMainApplicationContext();
+    if (mainApplicationContext == null) {
+      throw new RuntimeException(
+          "Failed to resolve notifications crypto account path - main application context not initialized.");
+    }
+    return mainApplicationContext
+        .getFileStreamPath("comm_notifications_crypto_account")
+        .getPath();
   }
 }
