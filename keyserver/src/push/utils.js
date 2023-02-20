b7b294b0ea429f0e8677471c6b0cdde8b7c575c8
Michal Gniadek <michal.gniadek@swmansion.com>
2023-02-20 16:18:59 +0100

    [keyserver] Send web push notification
    Summary:
    This diff actually sends a web push notification using the `web-push` library. The library is initialized during configuration loading (added in one of the previous diffs). Then we can just send a notification with the `device_token` (which contains e.g. an endpoint and keys but it's all handled by `web-push`). Otherwise the logic should be similar to iOS and Android notifications.
    
    Depends on D6815, D6813, D6717
    
    Test Plan:
    Tested with next diff initializes notif sending on the web app side:
    - test that notifs are send with correct values
    - check that the results are saved inside of the `notifications` table
    - check that errors are saved inside of the `notifications` table
    - check that `device_token` is removed after unsuccessful push notif
    
    Reviewers: tomek, atul, kamil, marcin
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D6817

--- keyserver/src/push/utils.js
+++ keyserver/src/push/utils.js
@@ -4,7 +4,9 @@ import apn from '@parse/node-apn';
 import type { ResponseFailure } from '@parse/node-apn';
 import type { FirebaseApp, FirebaseError } from 'firebase-admin';
 import invariant from 'invariant';
+import webpush from 'web-push';
 
+import type { WebNotification } from 'lib/types/notif-types.js';
 import { threadSubscriptions } from 'lib/types/subscription-types.js';
 import { threadPermissions } from 'lib/types/thread-types.js';
 
@@ -13,6 +15,7 @@ import {
   getFCMPushProfileForCodeVersion,
   getAPNProvider,
   getFCMProvider,
+  ensureWebPushInitialized,
 } from './providers.js';
 import { dbQuery, SQL } from '../database/database.js';
 
@@ -25,6 +28,7 @@ const apnTokenInvalidationErrorCode = 410;
 const apnBadRequestErrorCode = 400;
 const apnBadTokenErrorString = 'BadDeviceToken';
 const apnMaxNotificationPayloadByteSize = 4096;
+const webInvalidTokenErrorCodes = [404, 410];
 
 type APNPushResult =
   | { +success: true }
@@ -198,9 +202,66 @@ async function getUnreadCounts(
   return usersToUnreadCounts;
 }
 
+export type WebPushError = {
+  +statusCode: number,
+  +headers: { +[string]: string },
+  +body: string,
+};
+type WebPushResult = {
+  +success?: true,
+  +errors?: $ReadOnlyArray<WebPushError>,
+  +invalidTokens?: $ReadOnlyArray<string>,
+};
+async function webPush({
+  notification,
+  deviceTokens,
+}: {
+  +notification: WebNotification,
+  +deviceTokens: $ReadOnlyArray<string>,
+}): Promise<WebPushResult> {
+  await ensureWebPushInitialized();
+  const notificationString = JSON.stringify(notification);
+
+  const pushResults = await Promise.all(
+    deviceTokens.map(async deviceTokenString => {
+      const deviceToken: PushSubscriptionJSON = JSON.parse(deviceTokenString);
+      try {
+        await webpush.sendNotification(deviceToken, notificationString);
+      } catch (error) {
+        return { error };
+      }
+      return {};
+    }),
+  );
+
+  const errors = [];
+  const invalidTokens = [];
+  for (let i = 0; i < pushResults.length; i++) {
+    const pushResult = pushResults[i];
+    if (pushResult.error) {
+      errors.push(pushResult.error);
+      if (webInvalidTokenErrorCodes.includes(pushResult.error.statusCode)) {
+        invalidTokens.push(deviceTokens[i]);
+      }
+    }
+  }
+
+  const result = {};
+  if (errors.length > 0) {
+    result.errors = errors;
+  } else {
+    result.success = true;
+  }
+  if (invalidTokens.length > 0) {
+    result.invalidTokens = invalidTokens;
+  }
+  return { ...result };
+}
+
 export {
   apnPush,
   fcmPush,
+  webPush,
   getUnreadCounts,
   apnMaxNotificationPayloadByteSize,
   fcmMaxNotificationPayloadByteSize,
