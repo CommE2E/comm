3d9128434f0bab5227fc728138265bae32071679
Ashoat Tevosyan <ashoat@comm.app>
2023-02-20 12:53:20 -0500

    [keyserver] Fix rust-node-addon after deprecation of --experimental-specifier-resolution=node
    Summary:
    Context in [ENG-3059](https://linear.app/comm/issue/ENG-3059/keyserver-fails-to-load-napi-compiled-module). `rust-node-addon` was broken by D6695, but `--experimental-specifier-resolution=node` is going away so we need another solution.
    
    I got this solution from [this GitHub comment](https://github.com/nodejs/node/issues/40541#issuecomment-951096740).
    
    Depends on D6783
    
    Test Plan: Confirm I could import `rust-node-addon` and call methods on it from @jon's `jonringer/identity-deleteuserrpc` branch
    
    Reviewers: jon, varun
    
    Subscribers: tomek, atul, jon
    
    Differential Revision: https://phab.comm.dev/D6784

--- keyserver/addons/rust-node-addon/index.js
+++ keyserver/addons/rust-node-addon/index.js
@@ -1,6 +1,13 @@
 // @flow
 
+import invariant from 'invariant';
+import { createRequire } from 'module';
+
 const { platform, arch } = process;
+
+const importMetaURL = import.meta.url;
+invariant(importMetaURL, 'import.meta.url should be set');
+const require = createRequire(importMetaURL);
 
 type RustAPI = {
   +registerUser: (
@@ -16,16 +23,16 @@ async function getRustAPI(): Promise<RustAPI> {
   let nativeBinding = null;
   if (platform === 'darwin' && arch === 'x64') {
     // $FlowFixMe
-    nativeBinding = await import('./napi/rust-node-addon.darwin-x64.node');
+    nativeBinding = require('./napi/rust-node-addon.darwin-x64.node');
   } else if (platform === 'darwin' && arch === 'arm64') {
     // $FlowFixMe
-    nativeBinding = await import('./napi/rust-node-addon.darwin-arm64.node');
+    nativeBinding = require('./napi/rust-node-addon.darwin-arm64.node');
   } else if (platform === 'linux' && arch === 'x64') {
     // $FlowFixMe
-    nativeBinding = await import('./napi/rust-node-addon.linux-x64-gnu.node');
+    nativeBinding = require('./napi/rust-node-addon.linux-x64-gnu.node');
   } else if (platform === 'linux' && arch === 'arm64') {
     // $FlowFixMe
-    nativeBinding = await import('./napi/rust-node-addon.linux-arm64-gnu.node');
+    nativeBinding = require('./napi/rust-node-addon.linux-arm64-gnu.node');
   } else {
     throw new Error(`Unsupported OS: ${platform}, architecture: ${arch}`);
   }
@@ -34,7 +41,7 @@ async function getRustAPI(): Promise<RustAPI> {
     throw new Error('Failed to load native binding');
   }
 
-  const { registerUser } = nativeBinding.default;
+  const { registerUser } = nativeBinding;
   return { registerUser };
 }
 
