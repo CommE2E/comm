d132e6020d4cb40e85426b8abf197d5f94438bf7
Inka Sokołowska <inka.sokolowska@swmansion.com>
2023-02-20 15:36:02 +0100

    [web] Fix tabs in modals displaying incorrectly
    Summary:
    issue: https://linear.app/comm/issue/ENG-3049/fix-tabs-header-container
    In D6734 I missed the fact that `Tabs` was also used in some modals. This made them display incorrectly - not like in our designs. This diff fixes that by applying different styles based on a
    flag passed as a prop. I dodn't create a separate component, because they would differ in only two lines. Instead, I change the css they use, like we do in `NavigationPanel`.
    
    Test Plan:
    Run we app, checked that tabs in modals display as they used to, and tabs in `Chat` display according to the new designs
    
    Reviewers:
    tomek, michal, kamil
    Subscribers:

--- web/components/tabs-header.js
+++ web/components/tabs-header.js
@@ -4,17 +4,22 @@ import classnames from 'classnames';
 import * as React from 'react';
 
 import Button from './button.react.js';
-import css from './tabs.css';
+import cssPill from './tabs-pill.css';
+import cssUnderline from './tabs-underline.css';
 
 type Props<T: string> = {
   +children: React.Node,
   +isActive: boolean,
   +setTab: T => mixed,
   +id: T,
+  +isPillStyle?: boolean,
 };
 
 function TabsHeader<T: string>(props: Props<T>): React.Node {
-  const { children, isActive, setTab, id } = props;
+  const { children, isActive, setTab, id, isPillStyle = false } = props;
+  const css = React.useMemo(() => (isPillStyle ? cssPill : cssUnderline), [
+    isPillStyle,
+  ]);
   const headerClasses = classnames(css.tabHeader, {
     [css.backgroundTabHeader]: !isActive,
   });
