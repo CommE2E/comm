234fe6c4cd2b77d24f88a1b8d9714de33daf42fe
Michal Gniadek <michal.gniadek@swmansion.com>
2023-02-21 16:13:10 +0100

    [web] Add hook for creating push subscription
    Summary: Add a hook that will return a function that creates a new push subscription. It will be used in the later diffs.
    
    Test Plan: Tested with the later diffs: check that the subscription is correctly created and saved in the keyserver db and web redux
    
    Reviewers: atul, tomek, inka
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D6819

--- web/push-notif/push-notifs-handler.js
+++ web/push-notif/push-notifs-handler.js
@@ -2,7 +2,45 @@
 
 import * as React from 'react';
 
+import {
+  setDeviceToken,
+  setDeviceTokenActionTypes,
+} from 'lib/actions/device-actions.js';
+import {
+  useDispatchActionPromise,
+  useServerCall,
+} from 'lib/utils/action-utils.js';
+
 import electron from '../electron.js';
+import { useSelector } from '../redux/redux-utils.js';
+
+function useCreatePushSubscription(): () => Promise<void> {
+  const publicKey = useSelector(state => state.pushApiPublicKey);
+
+  const dispatchActionPromise = useDispatchActionPromise();
+  const callSetDeviceToken = useServerCall(setDeviceToken);
+
+  return React.useCallback(async () => {
+    if (!publicKey) {
+      return;
+    }
+
+    const workerRegistration = await navigator.serviceWorker?.ready;
+    if (!workerRegistration) {
+      return;
+    }
+
+    const subscription = await workerRegistration.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: publicKey,
+    });
+
+    dispatchActionPromise(
+      setDeviceTokenActionTypes,
+      callSetDeviceToken(JSON.stringify(subscription)),
+    );
+  }, [callSetDeviceToken, dispatchActionPromise, publicKey]);
+}
 
 function PushNotificationsHandler(): React.Node {
   React.useEffect(() => {
@@ -18,4 +56,4 @@ function PushNotificationsHandler(): React.Node {
   return null;
 }
 
-export { PushNotificationsHandler };
+export { PushNotificationsHandler, useCreatePushSubscription };
