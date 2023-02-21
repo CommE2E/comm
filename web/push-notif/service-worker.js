bf13566bc9e9d520dd42757a1dc3c719943d4c7a
Michal Gniadek <michal.gniadek@swmansion.com>
2023-02-21 17:19:40 +0100

    [web] Handle notifications in service worker
    Summary:
    This diff modifies the service worker to actually handle the push event and display a notification, additionaly it modifies the other events:
    - 'install' -> adds a `skipWaiting` so it will immediately take over the previous service worker
    - 'activate' -> `clients.claim()` so it will claim tabs that aren't controlled by a service worker (while this tab wouldn't have a event listener and wouldn't be able to navigate to a chat when user click a notification, we will still be able to focus on it)
    
    `event.waitUntil` is needed because otherwise the browser could put the service worker to sleep before the promises are resolved.
    
    Test Plan:
    Rest if the notification shows up after getting a push event
    
    Reviewers: tomek, atul, kamil
    
    Subscribers:

--- web/push-notif/service-worker.js
+++ web/push-notif/service-worker.js
@@ -1,9 +1,46 @@
 // @flow
 
+declare class PushMessageData {
+  json(): Object;
+}
+declare class PushEvent extends ExtendableEvent {
+  +data: PushMessageData;
+}
+
+declare var clients: Clients;
+declare function skipWaiting(): Promise<void>;
+
 self.addEventListener('install', () => {
-  console.log('service worker installed');
+  skipWaiting();
 });
 
-self.addEventListener('activate', () => {
-  console.log('service worker activated');
+self.addEventListener('activate', (event: ExtendableEvent) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('push', (event: PushEvent) => {
+  const data = event.data.json();
+
+  event.waitUntil(
+    (async () => {
+      let body = data.body;
+      if (data.prefix) {
+        body = `${data.prefix} ${body}`;
+      }
+      await self.registration.showNotification(data.title, {
+        body,
+        badge: 'https://web.comm.app/favicon.ico',
+        icon: 'https://web.comm.app/favicon.ico',
+        tag: data.id,
+        data: {
+          unreadCount: data.unreadCount,
+          threadID: data.threadID,
+        },
+      });
+    })(),
+  );
+});
+
+self.addEventListener('notificationclick', (event: NotificationEvent) => {
+  event.notification.close();
 });
