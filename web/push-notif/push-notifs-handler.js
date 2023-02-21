4c995969fd0924b0ec9f0f5df29c7699b7ee63bb
Michal Gniadek <michal.gniadek@swmansion.com>
2023-02-21 16:11:04 +0100

    [web] Register service worker for notifs
    Summary:
    We need to register a service worker to get push notifs from the browser. This diff registers the service worker from the endpoint exposed by the keyserver in one of the previous diffs on page load.
    
    Test Plan:
    - check if service worker is correctly registered (you can check that in the browser's dev tools)
    - check that the service worker is correctly updated after changing the service worker file
    
    Reviewers: tomek, atul, kamil
    
    Subscribers:

---
+++ web/push-notif/push-notifs-handler.js
@@ -0,0 +1,22 @@
+// @flow
+
+import React from 'React';
+
+import electron from '../electron.js';
+
+function PushNotificationsHandler(): React.Node {
+  React.useEffect(() => {
+    (async () => {
+      if (!navigator.serviceWorker || electron) {
+        return;
+      }
+
+      await navigator.serviceWorker.register('/worker/notif', { scope: '/' });
+    })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return null;
+}
+
+export { PushNotificationsHandler };
