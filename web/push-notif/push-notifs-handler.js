fc22197cdbca2293003d14a1d2cc661979bd2f48
Michal Gniadek <michal.gniadek@swmansion.com>
2023-02-21 17:21:35 +0100

    [web] Allow users to enable push notifs
    Summary: Depending on notification permission we either want to show a modal that asks for permission or (if we already have the permission) create a notification subscription immediately. We want to do this both for existing users and when someone new logs in.
    
    Test Plan: Test if subscription is created/ the modal shows up when someone logs in or open the page when logged in
    
    Reviewers: tomek, kamil, inka
    
    Subscribers: ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D6833

--- web/push-notif/push-notifs-handler.js
+++ web/push-notif/push-notifs-handler.js
@@ -6,12 +6,15 @@ import {
   setDeviceToken,
   setDeviceTokenActionTypes,
 } from 'lib/actions/device-actions.js';
+import { useModalContext } from 'lib/components/modal-provider.react.js';
+import { isLoggedIn } from 'lib/selectors/user-selectors.js';
 import {
   useDispatchActionPromise,
   useServerCall,
 } from 'lib/utils/action-utils.js';
 
 import electron from '../electron.js';
+import { PushNotifModal } from '../modals/push-notif-modal.react.js';
 import { useSelector } from '../redux/redux-utils.js';
 
 function useCreatePushSubscription(): () => Promise<void> {
@@ -43,6 +46,11 @@ function useCreatePushSubscription(): () => Promise<void> {
 }
 
 function PushNotificationsHandler(): React.Node {
+  const createPushSubscription = useCreatePushSubscription();
+
+  const modalContext = useModalContext();
+  const loggedIn = useSelector(isLoggedIn);
+
   React.useEffect(() => {
     (async () => {
       if (!navigator.serviceWorker || electron) {
@@ -50,8 +58,34 @@ function PushNotificationsHandler(): React.Node {
       }
 
       await navigator.serviceWorker.register('/worker/notif', { scope: '/' });
+
+      if (Notification.permission === 'granted') {
+        // Make sure the subscription is current if we have the permissions
+        await createPushSubscription();
+      } else if (Notification.permission === 'default' && loggedIn) {
+        // Ask existing users that are already logged in for permission
+        modalContext.pushModal(<PushNotifModal />);
+      }
     })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  // Ask for permission on login
+  const prevLoggedIn = React.useRef(loggedIn);
+  React.useEffect(() => {
+    if (!navigator.serviceWorker || electron) {
+      return;
+    }
+
+    if (!prevLoggedIn.current && loggedIn) {
+      if (Notification.permission === 'granted') {
+        createPushSubscription();
+      } else if (Notification.permission === 'default') {
+        modalContext.pushModal(<PushNotifModal />);
+      }
+    }
+    prevLoggedIn.current = loggedIn;
+  }, [createPushSubscription, loggedIn, modalContext, prevLoggedIn]);
 
   return null;
 }
