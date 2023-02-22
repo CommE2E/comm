06f0c5e07c23195326eb37c805483d18d9f4077a
Tomasz Palys <tomasz.palys@swmansion.com>
2023-02-22 11:47:42 +0100

    [services][feature-flags] Determine enabled features set
    Summary:
    Determine a set of enabled features based on the provided params. The idea is described in https://linear.app/comm/issue/ENG-2614/specify-requirements-of-feature-flags-service but the most important part is that when determining a config we take it from the newest version not younger that the version we check.
    
    Depends on D6857
    
    Test Plan: Called the function and checked if it returns correct value depending on the params. It is though a good idea to have some unit tests for it.
    
    Reviewers: michal, bartek, jon, varun
    
    Subscribers: ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D6858

--- services/feature-flags/src/main.rs
+++ services/feature-flags/src/main.rs
@@ -5,6 +5,7 @@ use tracing_subscriber::EnvFilter;
 pub mod config;
 pub mod constants;
 pub mod database;
+pub mod service;
 
 fn configure_logging() -> Result<()> {
   let filter = EnvFilter::builder()
