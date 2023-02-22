86eec7d92bbb00fb9606a0466fa11126e05004e1
Inka Sokołowska <inka.sokolowska@swmansion.com>
2023-02-22 18:25:08 +0100

    [web] Change the Apps tab to be a + button
    Summary: Issue: https://linear.app/comm/issue/ENG-2792/change-the-apps-tab-to-be-a
    
    Test Plan: Run web app, checked that the button displays correctly, and pressing it navigates to Apps screen
    
    Reviewers: michal, kuba, kamil
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D6733

--- web/navigation-panels/topbar.react.js
+++ web/navigation-panels/topbar.react.js
@@ -1,14 +1,45 @@
 // @flow
 
 import * as React from 'react';
+import { useDispatch } from 'react-redux';
+
+import SWMansionIcon from 'lib/components/SWMansionIcon.react.js';
 
 import AppSwitcher from './app-switcher.react.js';
 import css from './topbar.css';
+import { updateNavInfoActionType } from '../redux/action-types.js';
 
 function Topbar(): React.Node {
+  const dispatch = useDispatch();
+
+  const onClickApps = React.useCallback(
+    (event: SyntheticEvent<HTMLAnchorElement>) => {
+      event.preventDefault();
+      dispatch({
+        type: updateNavInfoActionType,
+        payload: {
+          tab: 'apps',
+        },
+      });
+    },
+    [dispatch],
+  );
+
+  const appNavigationItem = React.useMemo(
+    () => (
+      <a className={css.plusButton} onClick={onClickApps}>
+        <SWMansionIcon icon="plus-small" size={24} />
+      </a>
+    ),
+    [onClickApps],
+  );
+
   return (
     <div className={css.container}>
-      <AppSwitcher />
+      <div className={css.tabs}>
+        <AppSwitcher />
+      </div>
+      {appNavigationItem}
     </div>
   );
 }
