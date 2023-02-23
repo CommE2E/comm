f95747ecac53c7736a588cfc31467403c745054d
Max Kalashnikoff <geekmaks@gmail.com>
2023-02-23 14:39:18 +0200

    [services] Tunnelbroker - Disabling messages persistence on messages send
    Summary:
    This diff introduces changes to the Tunnelbroker gRPC messages stream implementation according to the [[ https://linear.app/comm/issue/ENG-2641/disabling-messages-persistence-in-the-database-in-tunnelbroker | ENG-2641 ]] task, where we need to disable messages persistence in the DynamoDB database using the config file parameter from D6622.
    In this diff, we are disabling saving messages into the database when calling send messages, and a config flag from D6622 is presented.
    
    Linear task: [[ https://linear.app/comm/issue/ENG-2641/disabling-messages-persistence-in-the-database-in-tunnelbroker | ENG-2641 ]]
    
    Test Plan:
    1. CI gates are passed.
    2. Manual testing:
     The client connects to the gRPC messages stream and sends the messages to the certain `deviceID`. The expected result is messages are not stored in the messages table DynamoDB database when the config flag `messages.skip_persistence` is presented.
    
    Reviewers: jon, bartek
    
    Reviewed By: jon, bartek
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D6624

--- services/tunnelbroker/src/libcpp/Tunnelbroker.cpp
+++ services/tunnelbroker/src/libcpp/Tunnelbroker.cpp
@@ -262,8 +262,12 @@ rust::Vec<rust::String> sendMessages(const rust::Vec<MessageItem> &messages) {
         }});
     messagesIDs.push_back(rust::String{messageID});
   };
-  comm::network::database::DatabaseManager::getInstance()
-      .putMessageItemsByBatch(vectorOfMessages);
+  if (!comm::network::config::ConfigManager::getInstance().isParameterSet(
+          comm::network::config::ConfigManager::
+              OPTION_MESSAGES_SKIP_PERSISTENCE)) {
+    comm::network::database::DatabaseManager::getInstance()
+        .putMessageItemsByBatch(vectorOfMessages);
+  };
   for (auto message : vectorOfMessages) {
     comm::network::AmqpManager::getInstance().send(&message);
   }
