06732ac6a7f23533d770dd40b5a7e7911c1d0e56
Atul Madhugiri <atulm@berkeley.edu>
2023-02-27 18:19:28 -0500

    [keyserver] Introduce `getOLMUtility(...)` and initialize OLM on `keyserver` start
    Summary: Initialize OLM by calling `await olm.init()` on `keyserver` start and introduce `getOLMUtility` to potentially access cached `OLMUtility`... or instantiate a new one.
    
    Test Plan:
    - Logged in a bunch of times and ensured things worked as expected.
    - Set breakpoint on cache hit:
    {F404093}
    
    Reviewers: ashoat, tomek
    
    Reviewed By: ashoat
    
    Differential Revision: https://phab.comm.dev/D6901

--- keyserver/src/responders/user-responders.js
+++ keyserver/src/responders/user-responders.js
@@ -1,6 +1,5 @@
 // @flow
 
-import olm from '@matrix-org/olm';
 import invariant from 'invariant';
 import { ErrorTypes, SiweMessage } from 'siwe';
 import t from 'tcomb';
@@ -94,6 +93,8 @@ import {
 } from '../updaters/account-updaters.js';
 import { userSubscriptionUpdater } from '../updaters/user-subscription-updaters.js';
 import { viewerAcknowledgmentUpdater } from '../updaters/viewer-acknowledgment-updater.js';
+import { getOLMUtility } from '../utils/olm-utils.js';
+import type { OLMUtility } from '../utils/olm-utils.js';
 import { validateInput } from '../utils/validation-utils.js';
 
 const subscriptionUpdateRequestInputValidator = tShape({
@@ -342,8 +343,7 @@ async function logInResponder(
       signedIdentityKeysBlob.payload,
     );
 
-    await olm.init();
-    const olmUtil = new olm.Utility();
+    const olmUtil: OLMUtility = getOLMUtility();
     try {
       olmUtil.ed25519_verify(
         identityKeys.primaryIdentityPublicKeys.ed25519,
