a388ba1a1aa57293f2bb5b50358d1cc830a017b8
xsanm <kkurowski.kk@gmail.com>
2023-02-28 16:20:33 +0100

    [web-db] add indexedDB config
    Summary:
    Adding config according to [docs](https://localforage.github.io/localForage/#settings-api-config).
    
    Depends on D6986, D6989, D6988
    
    Test Plan:
    Run this code:
    ```
            try {
              const value = await localforage.setItem('test', {
                prop: 'test prop',
              });
              console.log(value);
            } catch (err) {
              console.log(err);
            }
    ```
    and check in devtools (web and desktop) if database was properly created
    
    Reviewers: michal, tomek
    
    Subscribers: ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D6990

--- web/database/worker/web-db-worker.js
+++ web/database/worker/web-db-worker.js
@@ -1,6 +1,17 @@
 // @flow
 
+import localforage from 'localforage';
+
 import { type SharedWorkerMessageEvent } from '../../types/worker-types.js';
+
+const localforageConfig: PartialConfig = {
+  driver: localforage.INDEXEDDB,
+  name: 'comm',
+  storeName: 'commStorage',
+  description: 'Comm encrypted database storage',
+  version: '1.0',
+};
+localforage.config(localforageConfig);
 
 function connectHandler(event: SharedWorkerMessageEvent) {
   const port: MessagePort = event.ports[0];
