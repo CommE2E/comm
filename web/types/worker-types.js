661a2b6b9ccdb2a644365b39d92e4c7029c4cea5
xsanm <kkurowski.kk@gmail.com>
2023-03-02 14:26:43 +0100

    [web-db] implement basic app-worker connection types
    Summary:
    Adding basic types and concept of sharing information between worker and app. Current type (PING/PONG) will be used probably only for development, will add more concrete ones in next diffs.
    
    Depends on D6990
    
    Test Plan:
    Run this code:
    ```
    const webDbWorker = new SharedWorker('/worker/webDatabase');
    webDbWorker.port.onmessage = function (e) {
      console.log('message from worker: ', e.data);
    };
    const message: WorkerRequestMessage = {
      type: workerRequestMessageTypes.PING,
      text: 'hi from app',
    };
    webDbWorker.port.postMessage(message);
    ```
    and check results in browser, desktop and shared worker consoles.
    
    Check error flow.
    
    Reviewers: michal, tomek
    
    Reviewed By: tomek
    
    Subscribers: ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D6991

--- web/types/worker-types.js
+++ web/types/worker-types.js
@@ -1,5 +1,35 @@
 // @flow
 
+// The types of messages sent from app to worker
+export const workerRequestMessageTypes = Object.freeze({
+  PING: 0,
+});
+
+export type PingWorkerRequestMessage = {
+  +type: 0,
+  +text: string,
+};
+
+export type WorkerRequestMessage = PingWorkerRequestMessage;
+
+// The types of messages sent from worker to app
+export const workerResponseMessageTypes = Object.freeze({
+  PONG: 0,
+});
+
+export type ErrorWorkerResponseMessage = {
+  +error: Error,
+};
+export type PongWorkerResponseMessage = {
+  +type: 0,
+  +text: string,
+};
+
+export type WorkerResponseMessage =
+  | PongWorkerResponseMessage
+  | ErrorWorkerResponseMessage;
+
+// SharedWorker types
 export type SharedWorkerMessageEvent = MessageEvent & {
   +ports: $ReadOnlyArray<MessagePort>,
   ...
