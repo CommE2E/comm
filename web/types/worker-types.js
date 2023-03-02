3de89fd327d0fc055544e64c665f6b4ee957b985
xsanm <kkurowski.kk@gmail.com>
2023-03-02 15:57:47 +0100

    [web-db] implement proxy for promise-based connection with worker
    Summary:
    Abstraction layer over native event-based communication.
    
    Depends on D6991
    
    Test Plan:
    Run this code:
    ```
    const rawWebDbWorker = new SharedWorker('/worker/webDatabase');
    const webDBWorker = new WorkerConnectionProxy(
      rawWebDbWorker.port,
      error => {
        console.error(error);
      },
    );
    const workerResult = await webDBWorker.scheduleOnWorker({
      type: workerRequestMessageTypes.PING,
      text: 'PING',
    });
    console.log('result: ', workerResult);
    
    const combinedResult = await Promise.all([
      webDBWorker.scheduleOnWorker({
        type: workerRequestMessageTypes.PING,
        text: 'PING',
      }),
      webDBWorker.scheduleOnWorker({
        type: workerRequestMessageTypes.PING,
        text: 'PING',
      }),
      webDBWorker.scheduleOnWorker({
        type: workerRequestMessageTypes.PING,
        text: 'PING',
      }),
      webDBWorker.scheduleOnWorker({
        type: workerRequestMessageTypes.PING,
        text: 'PING',
      }),
    ]);
    console.log(combinedResult);
    ```
    and check results in browser, desktop, and shared worker consoles.
    Also test throwing errors.
    
    Reviewers: michal, tomek
    
    Reviewed By: michal
    
    Subscribers: ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D6992

--- web/types/worker-types.js
+++ web/types/worker-types.js
@@ -12,22 +12,28 @@ export type PingWorkerRequestMessage = {
 
 export type WorkerRequestMessage = PingWorkerRequestMessage;
 
+export type WorkerRequestProxyMessage = {
+  +id: number,
+  +message: WorkerRequestMessage,
+};
+
 // The types of messages sent from worker to app
 export const workerResponseMessageTypes = Object.freeze({
   PONG: 0,
 });
 
-export type ErrorWorkerResponseMessage = {
-  +error: Error,
-};
 export type PongWorkerResponseMessage = {
   +type: 0,
   +text: string,
 };
 
-export type WorkerResponseMessage =
-  | PongWorkerResponseMessage
-  | ErrorWorkerResponseMessage;
+export type WorkerResponseMessage = PongWorkerResponseMessage;
+
+export type WorkerResponseProxyMessage = {
+  +id?: number,
+  +message?: WorkerResponseMessage,
+  +error?: Error,
+};
 
 // SharedWorker types
 export type SharedWorkerMessageEvent = MessageEvent & {
