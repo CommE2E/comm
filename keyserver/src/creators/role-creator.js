7666e7f54163e9b569dec81b40936065498835ed
Rohan Khanderia <rohan.khanderia@gmail.com>
2023-03-02 12:55:03 -0500

    [keyserver] Introduce a new thread permission for pinning / unpinning messages
    Summary: We need to introduce s new thread permission that limits who is able to manage the pins for a thread. At the moment, this is only granted to Admins, so there's a couple of things that need to be done
    here:
    1. Update the baseAdminPriviledges so new chats with admins will have the correct, updated permissions
    2. Run a migration to cover existing threads
    3. Include the necessary migration to create the new `pinned_messages` table from the previous diff.
    
    The general overview is to update the `roles` table for Admins, and then call `updateRolesAndPermissionsForAllThreads()` which should handle updating the permissions for each of the threads given the new
    roles.
    
    The migration was discussed in the [[ https://linear.app/comm/issue/ENG-3181/introduce-a-new-thread-permission-for-pinning-unpinning-messages | Linear ]] task.
    
    Depends on D6924
    
    Test Plan:
    1. Confirm that the migration is successful
    
    2. Confirm that the `roles` table is updated for Admins (expected result is that the query only finds Admins to have the new permissions)
    
    3. Confirm that the `memberships` table for each of the threads is updated (expected result is that the query finds the permissions to be NOT NULL in each thread for user 256, the admin user locally)
    
    Reviewers: atul, ginsu, tomek
    
    Subscribers:

--- keyserver/src/creators/role-creator.js
+++ keyserver/src/creators/role-creator.js
@@ -145,6 +145,7 @@ function getRolePermissionBlobsForCommunity(
     DESCENDANT + threadPermissions.EDIT_PERMISSIONS;
   const descendantRemoveMembers = DESCENDANT + threadPermissions.REMOVE_MEMBERS;
   const descendantChangeRole = DESCENDANT + threadPermissions.CHANGE_ROLE;
+  const descendantManagePins = DESCENDANT + threadPermissions.MANAGE_PINS;
 
   const baseAdminPermissions = {
     [threadPermissions.KNOW_OF]: true,
@@ -160,6 +161,7 @@ function getRolePermissionBlobsForCommunity(
     [threadPermissions.DELETE_THREAD]: true,
     [threadPermissions.REMOVE_MEMBERS]: true,
     [threadPermissions.CHANGE_ROLE]: true,
+    [threadPermissions.MANAGE_PINS]: true,
     [descendantKnowOf]: true,
     [descendantVisible]: true,
     [topLevelDescendantJoinThread]: true,
@@ -176,6 +178,7 @@ function getRolePermissionBlobsForCommunity(
     [descendantEditPermissions]: true,
     [descendantRemoveMembers]: true,
     [descendantChangeRole]: true,
+    [descendantManagePins]: true,
   };
 
   let adminPermissions;
