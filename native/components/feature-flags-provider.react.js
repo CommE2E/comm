50b37e393ce7fde8af49b3136187a3d6da5b7d23
Tomasz Palys <tomasz.palys@swmansion.com>
2023-03-02 15:42:47 +0100

    [native] Fetch and parse feature flags response
    Summary:
    Fetch a config, parse, and use in a state.
    
    Depends on D6948
    
    Test Plan: The service isn't yet deployed using https, so the networking stuff wasn't tested. I've hardcoded a response and verified that setting the state works ok. After https is in place, I'm going to update this test plan.
    
    Reviewers: kamil, bartek, inka
    
    Subscribers: ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D6949

--- native/components/feature-flags-provider.react.js
+++ native/components/feature-flags-provider.react.js
@@ -1,6 +1,12 @@
 // @flow
 
 import * as React from 'react';
+import { Platform } from 'react-native';
+
+import { fetchFeatureFlags } from 'lib/utils/feature-flags-utils.js';
+import { useIsCurrentUserStaff } from 'native/utils/staff-utils.js';
+
+import { codeVersion } from '../redux/persist.js';
 
 type FeatureFlagsConfiguration = {
   +[feature: string]: boolean,
@@ -24,8 +30,33 @@ type Props = {
 };
 function FeatureFlagsProvider(props: Props): React.Node {
   const { children } = props;
+  const isStaff = useIsCurrentUserStaff();
+
+  const [featuresConfig, setFeaturesConfig] = React.useState(defaultContext);
+  React.useEffect(() => {
+    (async () => {
+      try {
+        const config = await fetchFeatureFlags(
+          Platform.OS,
+          isStaff,
+          codeVersion,
+        );
+        const configuration = {};
+        for (const feature of config.enabledFeatures) {
+          configuration[feature] = true;
+        }
+        setFeaturesConfig({
+          configuration,
+          loaded: true,
+        });
+      } catch (e) {
+        console.error('Feature flag retrieval failed:', e);
+      }
+    })();
+  }, [isStaff]);
+
   return (
-    <FeatureFlagsContext.Provider value={defaultContext}>
+    <FeatureFlagsContext.Provider value={featuresConfig}>
       {children}
     </FeatureFlagsContext.Provider>
   );
