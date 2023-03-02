0ec92823f213fe9cfa918506fbee8d83444c9d36
xsanm <kkurowski.kk@gmail.com>
2023-03-02 14:26:43 +0100

    [web-db] implement basic app-worker connection types
    Summary: Adding basic types and concept of sharing information between worker and app. Current type (PING/PONG) will be used probably only for development, will add more concrete ones in next diffs.
    
    Depends on D6990
    
    Test Plan: Run this code:
    ```
    const webDbWorker = new SharedWorker('/worker/webDatabase');
    webDbWorker.port.onmessage = function (e) {
      console.log('message from worker: ', e.data);
    };
    const message: WorkerRequestMessage = {
      type: workerRequestMessageTypes.PING,
      text: 'hi from app',
    };
    webDbWorker.port.postMessage(message);
    ```
    and check results in browser, desktop and shared worker consoles.
    
    Reviewers: michal, tomek

--- web/database/worker/web-db-worker.js
+++ web/database/worker/web-db-worker.js
@@ -2,7 +2,13 @@
 
 import localforage from 'localforage';
 
-import { type SharedWorkerMessageEvent } from '../../types/worker-types.js';
+import {
+  type SharedWorkerMessageEvent,
+  type WorkerRequestMessage,
+  type WorkerResponseMessage,
+  workerRequestMessageTypes,
+  workerResponseMessageTypes,
+} from '../../types/worker-types.js';
 
 const localforageConfig: PartialConfig = {
   driver: localforage.INDEXEDDB,
@@ -13,14 +19,34 @@ const localforageConfig: PartialConfig = {
 };
 localforage.config(localforageConfig);
 
+function processAppRequest(
+  message: WorkerRequestMessage,
+): ?WorkerResponseMessage {
+  if (message.type === workerRequestMessageTypes.PING) {
+    return {
+      type: workerResponseMessageTypes.PONG,
+      text: 'PONG',
+    };
+  }
+
+  throw new Error('Request type not supported');
+}
+
 function connectHandler(event: SharedWorkerMessageEvent) {
   const port: MessagePort = event.ports[0];
   console.log('Web database worker alive!');
 
   port.onmessage = async function (messageEvent: MessageEvent) {
-    console.log('message from main thread: ', messageEvent.data);
+    const message: WorkerRequestMessage = (messageEvent.data: any);
 
-    port.postMessage({ message: 'response from worker' });
+    try {
+      const result = processAppRequest(message);
+      port.postMessage(result);
+    } catch (e) {
+      port.postMessage({
+        error: e,
+      });
+    }
   };
 }
 
