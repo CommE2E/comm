881ba7db4ba68c0f0618e752346343dd141a0f50
Jonathan Ringer <jonringer117@gmail.com>
2023-03-06 11:56:11 -0800

    [comm-opaque] Create opaque facade for usage in Comm
    Summary:
    This exposes our usage of opaque in a way that
    many of the details are obscured from the calling code.
    
    This should eventually allow for significant reduction
    in code re-usage in current code base, as well as
    open the door for exporting the facade as a wasm api.
    
    https://linear.app/comm/issue/ENG-3277
    
    Related: https://linear.app/comm/issue/ENG-3265
    
    Depends on D7020
    
    Test Plan:
    Full end-to-end testing would need to be done in another diff.
    
    However, there is a unit test which goes through a register + login workflow.
    ```
    cd shared/comm-opaque
    cargo test
    ```
    
    Reviewers: varun, bartek
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7022

--- shared/comm-opaque/src/lib.rs
+++ shared/comm-opaque/src/lib.rs
@@ -1,2 +1,44 @@
+mod client;
+mod config;
+mod constants;
+mod grpc;
 mod opaque;
+mod server;
+
 pub use crate::opaque::Cipher;
+
+#[test]
+pub fn test_register_and_login() {
+  let pass = "test";
+
+  // Register user
+  let (client_reg_res, client_message) =
+    client::register_start(pass.as_bytes()).unwrap();
+
+  let (server_reg_res, server_response) =
+    server::register_start(&client_message).unwrap();
+
+  let client_upload =
+    client::register_finish(client_reg_res, &server_response).unwrap();
+
+  let password_file =
+    server::register_finish(server_reg_res, &client_upload).unwrap();
+  // These bytes are the used to validate future login sessions, normally it
+  // would saved to a database or other data store
+  let password_registration_bytes = password_file.serialize();
+
+  // Login user
+  let (client_login_res, client_request) =
+    client::login_start(pass.as_bytes()).unwrap();
+
+  let registration =
+    server::deserialize_registration(&password_registration_bytes).unwrap();
+  let (server_login_res, server_response) =
+    server::login_start(registration, &client_request).unwrap();
+
+  let client_finish_res =
+    client::login_finish(client_login_res, &server_response).unwrap();
+  let client_upload = client_finish_res.message.serialize().unwrap();
+
+  server::login_finish(server_login_res, &client_upload).unwrap();
+}
