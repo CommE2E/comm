76e1c3cad89305f92d1f68768a2b6678066ed938
Jonathan Ringer <jonringer117@gmail.com>
2023-03-06 11:56:11 -0800

    [comm-opaque] Create opaque facade for usage in Comm
    Summary:
    This exposes our usage of opaque in a way that
    many of the details are obscured from the calling code.
    
    This should eventually allow for significant reduction
    in code re-usage in current code base, as well as
    open the door for exporting the facade as a wasm api.
    
    https://linear.app/comm/issue/ENG-3277
    
    Related: https://linear.app/comm/issue/ENG-3265
    
    Depends on D7020
    
    Test Plan:
    Full end-to-end testing would need to be done in another diff.
    
    However, there is a unit test which goes through a register + login workflow.
    ```
    cd shared/comm-opaque
    cargo test
    ```
    
    Reviewers: varun, bartek
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7022

---
+++ shared/comm-opaque2/src/server/register.rs
@@ -0,0 +1,40 @@
+use opaque_ke::{errors::ProtocolError, ServerRegistration};
+use opaque_ke::{RegistrationRequest, RegistrationUpload, ServerSetup};
+
+use crate::Cipher;
+
+pub struct Registration {}
+
+impl Registration {
+  pub fn new() -> Registration {
+    Registration {}
+  }
+
+  pub fn start(
+    &self,
+    server_setup: &ServerSetup<Cipher>,
+    payload: &[u8],
+    credential_identifier: &[u8],
+  ) -> Result<Vec<u8>, ProtocolError> {
+    let upload = RegistrationRequest::deserialize(payload)?;
+    let result = ServerRegistration::<Cipher>::start(
+      server_setup,
+      upload,
+      credential_identifier,
+    )?;
+    Ok(result.message.serialize().to_vec())
+  }
+
+  pub fn finish(
+    &mut self,
+    response_payload: &[u8],
+  ) -> Result<Vec<u8>, ProtocolError> {
+    let upload_payload =
+      RegistrationUpload::<Cipher>::deserialize(response_payload)?;
+    Ok(
+      ServerRegistration::finish(upload_payload)
+        .serialize()
+        .to_vec(),
+    )
+  }
+}
