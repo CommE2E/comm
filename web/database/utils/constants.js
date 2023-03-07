54f368eee16c61291bc073caa59f4cc918e174f3
xsanm <kkurowski.kk@gmail.com>
2023-03-07 18:23:41 +0100

    [web-db] initialize SQLite database
    Summary:
    Creating `sql.js` object which is using SQLite wasm file.
    
    Depends on D6992
    
    Test Plan:
    Run this code:
    ```
    const rawWebDbWorker = new SharedWorker('/worker/database');
    const webDBWorker = new WorkerConnectionProxy(
      rawWebDbWorker.port,
      error => {
        console.error(error);
      },
    );
    
    await webDBWorker.scheduleOnWorker({
      type: workerRequestMessageTypes.INIT,
    });
    ```
    And check if db version was properly logged on web and desktop
    
    Reviewers: michal, tomek
    
    Subscribers: ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D6993

---
+++ web/database/utils/constants.js
@@ -0,0 +1,3 @@
+// @flow
+
+export const SQLITE_CONTENT = 'sqliteFileContent';
--- web/database/worker/db-worker.js
+++ web/database/worker/db-worker.js
@@ -1,6 +1,9 @@
 // @flow
 
 import localforage from 'localforage';
+import initSqlJs from 'sql.js';
+
+import { isDev } from 'lib/utils/dev-utils.js';
 
 import {
   type SharedWorkerMessageEvent,
@@ -10,6 +13,7 @@ import {
   workerResponseMessageTypes,
   type WorkerRequestProxyMessage,
 } from '../../types/worker-types.js';
+import { SQLITE_CONTENT } from '../utils/constants.js';
 
 const localforageConfig: PartialConfig = {
   driver: localforage.INDEXEDDB,
@@ -20,14 +24,44 @@ const localforageConfig: PartialConfig = {
 };
 localforage.config(localforageConfig);
 
-function processAppRequest(
+let sqliteDb = null;
+
+async function initDatabase() {
+  const content = await localforage.getItem(SQLITE_CONTENT);
+
+  const wasmFilePath = isDev
+    ? 'http://localhost:3000/comm/compiled'
+    : 'https://web.comm.app/compiled';
+
+  const SQL = await initSqlJs({
+    locateFile: file => `${wasmFilePath}/${file}`,
+  });
+
+  if (content) {
+    sqliteDb = new SQL.Database(new Uint8Array(content));
+  } else {
+    sqliteDb = new SQL.Database();
+  }
+
+  const versionData = sqliteDb.exec('PRAGMA user_version;');
+  if (versionData.length && versionData[0].values.length) {
+    console.info(`Db version: ${versionData[0].values[0]}`);
+  } else {
+    throw new Error('Error while retrieving database version');
+  }
+}
+
+async function processAppRequest(
   message: WorkerRequestMessage,
-): ?WorkerResponseMessage {
+): Promise<?WorkerResponseMessage> {
   if (message.type === workerRequestMessageTypes.PING) {
     return {
       type: workerResponseMessageTypes.PONG,
       text: 'PONG',
     };
+  } else if (message.type === workerRequestMessageTypes.INIT) {
+    await initDatabase();
+    return;
   }
 
   throw new Error('Request type not supported');
@@ -51,7 +85,7 @@ function connectHandler(event: SharedWorkerMessageEvent) {
     }
 
     try {
-      const result = processAppRequest(message);
+      const result = await processAppRequest(message);
       port.postMessage({
         id,
         message: result,
