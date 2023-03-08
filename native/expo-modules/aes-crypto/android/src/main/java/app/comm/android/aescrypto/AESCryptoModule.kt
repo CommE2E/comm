b54e9585b702cc4552b2795298d4ef6f478348d1
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2023-03-08 10:32:44 +0100

    [native][AES] Implement generateKey function
    Summary:
    This diff implements a function, that generates AES-256 key using platforms' native APIs.
    
    iOS docs: https://developer.apple.com/documentation/cryptokit/aes/gcm/nonce
    Android docs: https://developer.android.com/reference/kotlin/javax/crypto/KeyGenerator
    
    Depends on D7002
    
    Test Plan:
    Ensured the function returns random numbers on both platforms:
    ```
    lang=js
    
    import { generateKey } from './utils/aes-crypto-module.js';
    
    console.log(generateKey());
    // should display sth like: [82, 226, ..., 215, 19, 13] // 32 values
    ```
    
    Reviewers: marcin, atul
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D7004

--- native/expo-modules/aes-crypto/android/src/main/java/app/comm/android/aescrypto/AESCryptoModule.kt
+++ native/expo-modules/aes-crypto/android/src/main/java/app/comm/android/aescrypto/AESCryptoModule.kt
@@ -1,14 +1,49 @@
 package app.comm.android.aescrypto
 
+import expo.modules.core.errors.CodedException
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
+import expo.modules.kotlin.typedarray.Uint8Array
+import java.security.SecureRandom
+import javax.crypto.KeyGenerator
+
+private const val ALGORITHM_AES = "AES"
+private const val KEY_SIZE = 32 // bytes
 
 class AESCryptoModule : Module() {
+  private val secureRandom by lazy { SecureRandom() }
+
   override fun definition() = ModuleDefinition {
     Name("AESCrypto")
 
-    Function("hello") {
-      return@Function "Hello world! 👋"
-    }
+    Function("generateKey", this@AESCryptoModule::generateKey)
   }
+
+  // region Function implementations
+
+  /**
+   * Generates AES-256 key and stores it in [destination] array
+   *
+   * @param destination JS typed array, must be [KEY_SIZE] bytes long
+   */
+  private fun generateKey(destination: Uint8Array) {
+    if (destination.byteLength != KEY_SIZE) {
+      throw InvalidKeyLengthException()
+    }
+
+    val keygen = KeyGenerator.getInstance(ALGORITHM_AES).apply {
+      init(KEY_SIZE * 8, secureRandom)
+    }
+    val keyBytes = keygen.generateKey().encoded
+    destination.write(keyBytes, position = 0, size = keyBytes.size)
+  }
+
+  // endregion
 }
+
+// region Exception definitions
+
+private class InvalidKeyLengthException :
+  CodedException("The AES key has invalid length")
+
+// endregion
