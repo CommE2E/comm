431ac30b0fa934cf364aa2a686d90392a7a59d6c
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2023-03-08 10:32:44 +0100

    [native][AES] Implement generateKey function
    Summary:
    
    This diff implements a function, that generates AES-256 key using platforms' native APIs.
    
    iOS docs: https://developer.apple.com/documentation/cryptokit/aes/gcm/nonce
    Android docs: https://developer.android.com/reference/kotlin/javax/crypto/KeyGenerator
    
    Depends on D7002
    
    Test Plan:
    
    Ensured the function returns random numbers on both platforms:
    ```
    lang=js
    
    import { generateKey } from './utils/aes-crypto-module.js';
    
    console.log(generateKey());
    // should display sth like: [82, 226, ..., 215, 19, 13] // 32 values
    ```
    
    Reviewers: marcin, atul
    
    Subscribers:

--- native/expo-modules/aes-crypto/ios/AESCryptoModule.swift
+++ native/expo-modules/aes-crypto/ios/AESCryptoModule.swift
@@ -1,11 +1,40 @@
 import ExpoModulesCore
+import CryptoKit
 
 public class AESCryptoModule: Module {
   public func definition() -> ModuleDefinition {
     Name("AESCrypto")
 
-    Function("hello") { () -> String in
-      return "Hello world! 👋"
-    }
+    Function("generateKey", generateKey)
+  }
+}
+
+// MARK: - Function implementations
+
+private func generateKey(destination: Uint8Array) throws {
+  let key = SymmetricKey(size: .bits256)
+  guard destination.byteLength * 8 == key.bitCount else {
+    throw InvalidKeyLengthException()
+  }
+  
+  key.withUnsafeBytes { bytes in
+    let _ = bytes.copyBytes(to: destination.rawBufferPtr())
+  }
+}
+
+// MARK: - Utilities
+
+extension TypedArray {
+  func rawBufferPtr() -> UnsafeMutableRawBufferPointer {
+    UnsafeMutableRawBufferPointer(start: self.rawPointer,
+                                  count: self.byteLength)
+  }
+}
+
+// MARK: - Exception definitions
+
+private class InvalidKeyLengthException: Exception {
+  override var reason: String {
+    "The AES key has invalid length"
   }
 }
