5789ca85981d54435b0d390dc4fde19a37fe1483
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2023-03-13 12:42:35 +0100

    [native][AES] Implement encrypt function on iOS
    Summary:
    This diff implements the `encrypt()` function on iOS.
    
    Its desired behavior is described here: https://linear.app/comm/issue/ENG-388#comment-76a38890
    
    Docs: https://developer.apple.com/documentation/cryptokit/aes/gcm/seal(_:using:nonce:)
    Inspired by: https://dev.to/craftzdog/how-to-encrypt-decrypt-with-aes-gcm-using-cryptokit-in-swift-24h1
    
    Depends on D7004
    
    Test Plan:
    ```
    lang = js
    import * as AES from './utils/aes-crypto-module.js';
    
    const key = AES.generateKey();
    const plaintextBytes = new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
    const ciphertext = AES.encrypt(key, plaintextBytes);
    console.log('Ciphertext', ciphertext);
    // ensured that ciphertext is 12+10+16=38 bytes long (ciphertext.length==38)
    ```
    
    Reviewers: marcin, atul
    
    Reviewed By: atul
    
    Subscribers: anunay, ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D7005

--- native/utils/aes-crypto-module.js
+++ native/utils/aes-crypto-module.js
@@ -1,15 +1,30 @@
 // @flow
 
 import { requireNativeModule } from 'expo-modules-core';
+import invariant from 'invariant';
 
 const KEY_SIZE = 32; // bytes
+const IV_LENGTH = 12; // bytes, IV - unique Initialization Vector (nonce)
+const TAG_LENGTH = 16; // bytes - GCM auth tag
 
 const AESCryptoModule: {
   +generateKey: (destination: Uint8Array) => void,
+  +encrypt: (
+    key: Uint8Array,
+    data: Uint8Array,
+    destination: Uint8Array,
+  ) => void,
 } = requireNativeModule('AESCrypto');
 
 export function generateKey(): Uint8Array {
   const keyBuffer = new Uint8Array(KEY_SIZE);
   AESCryptoModule.generateKey(keyBuffer);
   return keyBuffer;
+}
+
+export function encrypt(key: Uint8Array, data: Uint8Array): Uint8Array {
+  invariant(AESCryptoModule.encrypt, 'AESCrypto.encrypt is not implemented');
+  const sealedDataBuffer = new Uint8Array(data.length + IV_LENGTH + TAG_LENGTH);
+  AESCryptoModule.encrypt(key, data, sealedDataBuffer);
+  return sealedDataBuffer;
 }
