2da126ff4acf7a68c4cf27c0073b4757db8e4b79
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2023-03-13 12:42:35 +0100

    [native][AES] Implement encrypt function on iOS
    Summary:
    This diff implements the `encrypt()` function on iOS.
    
    Its desired behavior is described here: https://linear.app/comm/issue/ENG-388#comment-76a38890
    
    Docs: https://developer.apple.com/documentation/cryptokit/aes/gcm/seal(_:using:nonce:)
    Inspired by: https://dev.to/craftzdog/how-to-encrypt-decrypt-with-aes-gcm-using-cryptokit-in-swift-24h1
    
    Depends on D7004
    
    Test Plan:
    ```
    lang = js
    import * as AES from './utils/aes-crypto-module.js';
    
    const key = AES.generateKey();
    const plaintextBytes = new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
    const ciphertext = AES.encrypt(key, plaintextBytes);
    console.log('Ciphertext', ciphertext);
    // ensured that ciphertext is 12+10+16=38 bytes long (ciphertext.length==38)
    ```
    
    Reviewers: marcin, atul
    
    Subscribers: anunay, ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D7005

--- native/expo-modules/aes-crypto/ios/AESCryptoModule.swift
+++ native/expo-modules/aes-crypto/ios/AESCryptoModule.swift
@@ -2,12 +2,15 @@ import ExpoModulesCore
 import CryptoKit
 
 private let KEY_SIZE = 32 // bytes
+private let IV_LENGTH = 12 // bytes, IV - unique Initialization Vector (nonce)
+private let TAG_LENGTH = 16 // bytes - GCM auth tag
 
 public class AESCryptoModule: Module {
   public func definition() -> ModuleDefinition {
     Name("AESCrypto")
 
     Function("generateKey", generateKey)
+    Function("encrypt", encrypt)
   }
 }
 
@@ -23,9 +26,38 @@ private func generateKey(destination: Uint8Array) throws {
   }
 }
 
+private func encrypt(rawKey: Uint8Array,
+                     plaintext: Uint8Array,
+                     destination: Uint8Array) throws {
+  guard destination.byteLength == plaintext.byteLength + IV_LENGTH + TAG_LENGTH
+  else {
+    throw InvalidDataLengthException()
+  }
+  
+  let key = SymmetricKey(data: rawKey.data())
+  let iv = AES.GCM.Nonce()
+  let encryptionResult = try AES.GCM.seal(plaintext.data(),
+                                          using: key,
+                                          nonce: iv)
+
+  // 'combined' returns concatenated: iv || ciphertext || tag
+  guard let sealedData = encryptionResult.combined else {
+    // this happens only if Nonce/IV != 12 bytes long
+    throw EncryptionFailedException("Incorrect AES configuration")
+  }
+  guard sealedData.count == destination.byteLength else {
+    throw EncryptionFailedException("Encrypted data has unexpected length")
+  }
+  sealedData.copyBytes(to: destination.rawBufferPtr())
+}
+
 // MARK: - Utilities
 
 extension TypedArray {
+  func data() -> Data {
+    Data(bytes: self.rawPointer, count: self.byteLength)
+  }
+  
   func rawBufferPtr() -> UnsafeMutableRawBufferPointer {
     UnsafeMutableRawBufferPointer(start: self.rawPointer,
                                   count: self.byteLength)
@@ -37,5 +69,17 @@ extension TypedArray {
 private class InvalidKeyLengthException: Exception {
   override var reason: String {
     "The AES key has invalid length"
+  }
+}
+
+private class InvalidDataLengthException: Exception {
+  override var reason: String {
+    "Source or destination array has invalid length"
+  }
+}
+
+private class EncryptionFailedException: GenericException<String> {
+  override var reason: String {
+    "Failed to encrypt data: \(param)"
   }
 }
