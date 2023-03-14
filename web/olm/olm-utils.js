ac1d2d67035637bc9fc1064ef40d203a31d483ea
Ashoat Tevosyan <ashoat@comm.app>
2023-03-14 16:52:19 -0400

    [keyserver][web] Use unique filename for each prod olm.wasm asset
    Summary: More context in [ENG-3322](https://linear.app/comm/issue/ENG-3322/fix-stale-cache-error-for-olmwasm). Basically we started seeing an issue after updating Olm where the old version was still in cache. This is "by design" – keyserver serves all prod assets as immutable to improve performance. For this to work, we need to make sure each prod asset has a unique filename.
    
    Test Plan: Test both dev and prod, open up Chrome Dev Tools network tab (while web is logged out) and confirm `olm.wasm` was loaded correctly
    
    Reviewers: atul, michal, ginsu
    
    Reviewed By: atul
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D7068

---
+++ web/olm/olm-utils.js
@@ -0,0 +1,16 @@
+// @flow
+
+import olm from '@matrix-org/olm';
+
+declare var olmFilename: string;
+
+async function initOlm(): Promise<void> {
+  if (!olmFilename) {
+    return await olm.init();
+  }
+  const locateFile = (wasmFilename: string, httpAssetsHost: string) =>
+    httpAssetsHost + olmFilename;
+  return await olm.init({ locateFile });
+}
+
+export { initOlm };
