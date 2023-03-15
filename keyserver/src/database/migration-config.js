bc34e9bd4cbf7c69fd47c0fb535344e2f8495614
Ashoat Tevosyan <ashoat@comm.app>
2023-03-14 22:50:53 -0400

    [lib][keyserver] Introduce new threadPermissions types for reactions
    Summary:
    I want to allow people to react in announcement channels even though they can't send messages, so I'm introducing a new `threadPermissions` type.
    
    This diff introduces the type, adds it to all roles except GENESIS members, and adds a new migration to populate it.
    
    Test Plan: I tested this in combination with the next diff by:
    
    1. Running the migration by restarting the `keyserver` after introducing it and making sure it runs successfully
    2. Testing reacting as a member (non-admin) to a message in a `COMMUNITY_ANNOUNCEMENT_ROOT` community root. I don't have permission to send messages, but I can react on both native and web. I didn't need to log out / back in on native, which indicates the the `updatesTypes.UPDATE_THREAD` updates were propagated correctly by the migration
    
    Reviewers: ginsu, kuba, rohan
    
    Subscribers:

--- keyserver/src/database/migration-config.js
+++ keyserver/src/database/migration-config.js
@@ -204,6 +204,7 @@ const migrations: $ReadOnlyMap<number, () => Promise<void>> = new Map([
     },
   ],
   [18, updateRolesAndPermissionsForAllThreads],
+  [19, updateRolesAndPermissionsForAllThreads],
 ]);
 const newDatabaseVersion: number = Math.max(...migrations.keys());
 
