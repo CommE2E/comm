29707c550f8a172ef4e1b8ad55ecfc0f0200edda
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-03-17 16:45:33 -0400

    [native] render thread avatar in message list header title
    Summary: Rendered avatar in message list header title
    
    Test Plan: Do on phab
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7134

--- native/chat/message-list-header-title.react.js
+++ native/chat/message-list-header-title.react.js
@@ -8,14 +8,18 @@ import {
 import * as React from 'react';
 import { View, Platform } from 'react-native';
 
+import { useGetAvatarForThread } from 'lib/shared/avatar-utils.js';
+import type { ClientAvatar } from 'lib/types/avatar-types.js';
 import type { ThreadInfo } from 'lib/types/thread-types.js';
 import { useResolvedThreadInfo } from 'lib/utils/entity-helpers.js';
 import { firstLine } from 'lib/utils/string-utils.js';
 
 import type { ChatNavigationProp } from './chat.react.js';
+import Avatar from '../components/avatar.react.js';
 import Button from '../components/button.react.js';
 import { ThreadSettingsRouteName } from '../navigation/route-names.js';
 import { useStyles } from '../themes/colors.js';
+import { useShouldRenderAvatars } from '../utils/avatar-utils.js';
 
 type BaseProps = {
   +threadInfo: ThreadInfo,
@@ -28,6 +32,8 @@ type Props = {
   ...BaseProps,
   +styles: typeof unboundStyles,
   +title: string,
+  +avatarInfo: ClientAvatar,
+  +shouldRenderAvatars: boolean,
 };
 class MessageListHeaderTitle extends React.PureComponent<Props> {
   render() {
@@ -38,16 +44,31 @@ class MessageListHeaderTitle extends React.PureComponent<Props> {
       areSettingsEnabled,
       styles,
       title,
+      avatarInfo,
+      shouldRenderAvatars,
       ...rest
     } = this.props;
 
-    let icon, fakeIcon;
+    let icon;
     if (Platform.OS === 'ios' && areSettingsEnabled) {
       icon = (
         <Icon name="chevron-forward" size={20} style={styles.forwardIcon} />
       );
+    }
+
+    let fakeIcon;
+    if (Platform.OS === 'ios' && areSettingsEnabled && !shouldRenderAvatars) {
       fakeIcon = (
         <Icon name="chevron-forward" size={20} style={styles.fakeIcon} />
+      );
+    }
+
+    let avatar;
+    if (!isSearchEmpty && shouldRenderAvatars) {
+      avatar = (
+        <View style={styles.avatarContainer}>
+          <Avatar avatarInfo={avatarInfo} size="small" />
+        </View>
       );
     }
 
@@ -60,6 +81,7 @@ class MessageListHeaderTitle extends React.PureComponent<Props> {
       >
         <View style={styles.container}>
           {fakeIcon}
+          {avatar}
           <HeaderTitle {...rest}>{firstLine(title)}</HeaderTitle>
           {icon}
         </View>
@@ -78,6 +100,9 @@ class MessageListHeaderTitle extends React.PureComponent<Props> {
 }
 
 const unboundStyles = {
+  avatarContainer: {
+    marginRight: 8,
+  },
   button: {
     flex: 1,
   },
@@ -109,11 +134,24 @@ const ConnectedMessageListHeaderTitle: React.ComponentType<BaseProps> =
   ) {
     const styles = useStyles(unboundStyles);
 
-    const { uiName } = useResolvedThreadInfo(props.threadInfo);
-    const { isSearchEmpty } = props;
-    const title = isSearchEmpty ? 'New Message' : uiName;
+    const shouldRenderAvatars = useShouldRenderAvatars();
 
-    return <MessageListHeaderTitle {...props} styles={styles} title={title} />;
+    const resolvedThreadInfo = useResolvedThreadInfo(props.threadInfo);
+    const avatarInfo = useGetAvatarForThread(resolvedThreadInfo);
+
+    const { isSearchEmpty } = props;
+
+    const title = isSearchEmpty ? 'New Message' : resolvedThreadInfo.uiName;
+
+    return (
+      <MessageListHeaderTitle
+        {...props}
+        styles={styles}
+        title={title}
+        avatarInfo={avatarInfo}
+        shouldRenderAvatars={shouldRenderAvatars}
+      />
+    );
   });
 
 export default ConnectedMessageListHeaderTitle;
