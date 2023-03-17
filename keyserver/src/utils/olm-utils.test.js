f5dafe54a5ac986643d342a8c0b4d2444dad0a70
Ashoat Tevosyan <ashoat@comm.app>
2023-03-16 21:51:23 -0400

    Unit tests for Olm's new X3DH methods
    Summary:
    This adds a quick test test to confirm Anunay's new methods work on `web` and `keyserver` that are introduced in the [fork](D7089).
    
    Note that more extensive testing was done by updating the upstream tests in the fork, which include a [Bob/Alice scenario in JavaScript](https://github.com/CommE2E/olm/commit/5b7ef049acfa1aebfbef528c5971ff8a0564bbde).
    
    Depends on D7091
    
    Test Plan: Run the tests
    
    Reviewers: anunay, jon, varun, bartek
    
    Subscribers: bartek, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7090

--- keyserver/src/utils/olm-utils.test.js
+++ keyserver/src/utils/olm-utils.test.js
@@ -10,4 +10,11 @@ describe('olm.Account', () => {
     const utility = getOlmUtility();
     expect(utility).not.toBe(undefined);
   });
+  it('should be able to generate and return prekey', async () => {
+    await olm.init();
+    const account = new olm.Account();
+    account.create();
+    account.generate_prekey();
+    expect(account.prekey()).not.toBe(undefined);
+  });
 });
