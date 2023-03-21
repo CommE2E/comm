e31bf737ede26cb06e6eeae5526d6c04dc6649f7
xsanm <kkurowski.kk@gmail.com>
2023-03-21 11:50:34 +0100

    [web-db] add function for parsing SQLite query
    Summary:
    Function will help work with query results.
    For this table:
    ```
    idx|text
    1|test1
    2|test2
    ```
    
    `sql.js` result is:
    ```
        [
          {
            columns: ['idx', 'text'],
            values: [
              [1, 'test1'],
              [2, 'test2'],
            ],
          },
        ];
    ``
    
    This function convert this into:
    ```
       [
          { idx: 1, text: 'test1' },
          { idx: 2, text: null },
       ]
    ```
    
    Test Plan: Run tests
    
    Reviewers: michal, tomek
    
    Subscribers: ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D7118

---
+++ web/database/utils/db-utils.js
@@ -0,0 +1,27 @@
+// @flow
+
+import type { QueryExecResult } from 'sql.js';
+
+function parseSQLiteQueryResult<T>(result: QueryExecResult): T[] {
+  const { columns, values } = result;
+  return values.map(rowResult => {
+    const row: any = Object.fromEntries(
+      columns.map((key, index) => [key, rowResult[index]]),
+    );
+    return row;
+  });
+}
+
+// NOTE: sql.js has behavior that when there are multiple statements in query
+// e.g. "statement1; statement2; statement3;"
+// and statement2 will not return anything, the result will be:
+// [result1, result3], not [result1, undefined, result3]
+function parseSQLiteResult<T>(
+  rawResult: $ReadOnlyArray<QueryExecResult>,
+): T[][] {
+  return rawResult.map((queryResult: QueryExecResult) =>
+    parseSQLiteQueryResult<T>(queryResult),
+  );
+}
+
+export { parseSQLiteResult };
