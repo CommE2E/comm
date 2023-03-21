f786cbd624456508527a159ce8c065e26fe6046e
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2023-03-20 23:21:12 -0400

    [native] RustPromiseManager
    Summary: In this diff, I've created a new singleton called RustPromiseManager. This singleton is responsible for keeping track of unresolved promises that depend on the execution of async Rust code, and for eventually rejecting or resolving the promises.
    
    Test Plan: See the following diff
    
    Reviewers: tomek, ashoat
    
    Subscribers: atul
    
    Differential Revision: https://phab.comm.dev/D7113

---
+++ native/cpp/CommonCpp/NativeModules/InternalModules/RustPromiseManager.cpp
@@ -0,0 +1,62 @@
+#include "RustPromiseManager.h"
+
+namespace comm {
+RustPromiseManager RustPromiseManager::instance;
+
+RustPromiseManager::RustPromiseManager(){};
+
+uint32_t RustPromiseManager::addPromise(
+    std::shared_ptr<Promise> promise,
+    std::shared_ptr<facebook::react::CallInvoker> jsInvoker,
+    facebook::jsi::Runtime &rt) {
+  uint32_t id = getNextID();
+  PromiseInfo info = {promise, jsInvoker, rt};
+  // Acquire a lock for writing
+  std::unique_lock<std::shared_mutex> lock(mutex);
+  promises.insert({id, info});
+  return id;
+}
+
+void RustPromiseManager::removePromise(uint32_t id) {
+  // Acquire a lock for writing
+  std::unique_lock<std::shared_mutex> lock(mutex);
+  promises.erase(id);
+}
+
+void RustPromiseManager::resolvePromise(uint32_t id, folly::dynamic ret) {
+  // Acquire a shared lock for reading
+  std::shared_lock<std::shared_mutex> lock(mutex);
+  auto it = promises.find(id);
+  if (it != promises.end()) {
+    // Release the shared lock
+    lock.unlock();
+    auto promiseInfo = it->second;
+    if (promiseInfo.jsInvoker) {
+      promiseInfo.jsInvoker->invokeAsync([promiseInfo, ret]() {
+        promiseInfo.promise->resolve(valueFromDynamic(promiseInfo.rt, ret));
+      });
+    } else {
+      promiseInfo.promise->resolve(valueFromDynamic(promiseInfo.rt, ret));
+    }
+    removePromise(id);
+  }
+}
+
+void RustPromiseManager::rejectPromise(uint32_t id, const std::string &error) {
+  // Acquire a shared lock for reading
+  std::shared_lock<std::shared_mutex> lock(mutex);
+  auto it = promises.find(id);
+  if (it != promises.end()) {
+    // Release the shared lock
+    lock.unlock();
+    if (it->second.jsInvoker) {
+      it->second.jsInvoker->invokeAsync(
+          [promise = it->second.promise, error]() { promise->reject(error); });
+    } else {
+      it->second.promise->reject(error);
+    }
+    removePromise(id);
+  }
+}
+
+} // namespace comm
---
+++ native/cpp/CommonCpp/NativeModules/InternalModules/RustPromiseManager.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <ReactCommon/TurboModuleUtils.h>
+#include <folly/dynamic.h>
+#include <jsi/JSIDynamic.h>
+
+#include <atomic>
+#include <shared_mutex>
+
+namespace comm {
+
+using namespace facebook::react;
+
+class RustPromiseManager {
+  std::atomic<uint32_t> id{0};
+
+  RustPromiseManager();
+
+  uint32_t getNextID() {
+    return this->id++;
+  };
+
+public:
+  static RustPromiseManager instance;
+  uint32_t addPromise(
+      std::shared_ptr<Promise> promise,
+      std::shared_ptr<facebook::react::CallInvoker> jsInvoker,
+      facebook::jsi::Runtime &rt);
+  void removePromise(uint32_t id);
+  void resolvePromise(uint32_t id, folly::dynamic ret);
+  void rejectPromise(uint32_t id, const std::string &error);
+
+  struct PromiseInfo {
+    std::shared_ptr<Promise> promise;
+    std::shared_ptr<facebook::react::CallInvoker> jsInvoker;
+    facebook::jsi::Runtime &rt;
+  };
+  std::unordered_map<uint32_t, PromiseInfo> promises;
+  std::shared_mutex mutex;
+};
+} // namespace comm
