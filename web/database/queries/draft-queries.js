0293595052bbfea9c204a3e90e11ca4727289472
xsanm <kkurowski.kk@gmail.com>
2023-03-21 14:23:14 +0100

    [web-db] implement Draft Store Operations
    Summary: SQL implementation of [draft store operations](https://github.com/CommE2E/comm/blob/fa13b21baf73045d560fb8377a24c435087476a8/lib/types/draft-types.js#L26).
    
    Depends on D7121
    
    Test Plan: Tests
    
    Reviewers: michal, tomek, marcin

---
+++ web/database/queries/draft-queries.js
@@ -0,0 +1,34 @@
+// @flow
+
+function removeAllDrafts(db: SqliteDatabase) {
+  db.exec(`DELETE from drafts`);
+}
+
+function updateDraft(db: SqliteDatabase, key: string, text: string) {
+  const query = `
+    INSERT OR REPLACE INTO drafts(key, text)
+    VALUES ($key, $text)
+  `;
+  const params = {
+    $key: key,
+    $text: text,
+  };
+
+  db.exec(query, params);
+}
+
+function moveDraft(db: SqliteDatabase, oldKey: string, newKey: string) {
+  const query = `
+    UPDATE OR REPLACE drafts
+    SET key = $newKey, text = (SELECT text FROM drafts WHERE key = $oldKey)
+    WHERE key = $oldKey
+  `;
+  const params = {
+    $newKey: newKey,
+    $oldKey: oldKey,
+  };
+
+  db.exec(query, params);
+}
+
+export { removeAllDrafts, updateDraft, moveDraft };
