6040d0931ad48c0afdddd2d9a5ad008214069057
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-03-21 15:42:30 -0400

    [native] render thread avatars for parent/child threads in thread settings
    Summary: rendered thread avatars for both parent and child threads in thread settings
    
    Test Plan: do on phab
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7137

--- native/chat/settings/thread-settings-parent.react.js
+++ native/chat/settings/thread-settings-parent.react.js
@@ -1,37 +1,64 @@
 // @flow
 
-import invariant from 'invariant';
 import * as React from 'react';
 import { Text, View } from 'react-native';
 
+import { useGetAvatarForThread } from 'lib/shared/avatar-utils.js';
 import { type ThreadInfo } from 'lib/types/thread-types.js';
 
+import Avatar from '../../components/avatar.react.js';
 import Button from '../../components/button.react.js';
 import ThreadPill from '../../components/thread-pill.react.js';
 import { useStyles } from '../../themes/colors.js';
+import { useShouldRenderAvatars } from '../../utils/avatar-utils.js';
 import { useNavigateToThread } from '../message-list-types.js';
 
-type Props = {
-  +threadInfo: ThreadInfo,
-  +parentThreadInfo: ?ThreadInfo,
+type ParentButtonProps = {
+  +parentThreadInfo: ThreadInfo,
 };
-function ThreadSettingsParent(props: Props): React.Node {
-  const { threadInfo, parentThreadInfo } = props;
+function ParentButton(props: ParentButtonProps): React.Node {
   const styles = useStyles(unboundStyles);
 
   const navigateToThread = useNavigateToThread();
+
   const onPressParentThread = React.useCallback(() => {
-    invariant(parentThreadInfo, 'should be set');
-    navigateToThread({ threadInfo: parentThreadInfo });
-  }, [parentThreadInfo, navigateToThread]);
+    navigateToThread({ threadInfo: props.parentThreadInfo });
+  }, [props.parentThreadInfo, navigateToThread]);
+
+  const avatarInfo = useGetAvatarForThread(props.parentThreadInfo);
+  const shouldRenderAvatars = useShouldRenderAvatars();
+
+  const avatar = React.useMemo(() => {
+    if (!shouldRenderAvatars) {
+      return null;
+    }
+
+    return (
+      <View style={styles.avatarContainer}>
+        <Avatar size="small" avatarInfo={avatarInfo} />
+      </View>
+    );
+  }, [avatarInfo, shouldRenderAvatars, styles.avatarContainer]);
+
+  return (
+    <Button onPress={onPressParentThread} style={styles.parentContainer}>
+      {avatar}
+      <ThreadPill threadInfo={props.parentThreadInfo} />
+    </Button>
+  );
+}
+
+type ThreadSettingsParentProps = {
+  +threadInfo: ThreadInfo,
+  +parentThreadInfo: ?ThreadInfo,
+};
+function ThreadSettingsParent(props: ThreadSettingsParentProps): React.Node {
+  const { threadInfo, parentThreadInfo } = props;
+  const styles = useStyles(unboundStyles);
 
   let parent;
   if (parentThreadInfo) {
-    parent = (
-      <Button onPress={onPressParentThread}>
-        <ThreadPill threadInfo={parentThreadInfo} />
-      </Button>
-    );
+    parent = <ParentButton parentThreadInfo={parentThreadInfo} />;
   } else if (threadInfo.parentThreadID) {
     parent = (
       <Text
@@ -63,6 +90,9 @@ function ThreadSettingsParent(props: Props): React.Node {
 }
 
 const unboundStyles = {
+  avatarContainer: {
+    marginRight: 8,
+  },
   currentValue: {
     flex: 1,
   },
@@ -82,6 +112,9 @@ const unboundStyles = {
     fontStyle: 'italic',
     paddingLeft: 2,
   },
+  parentContainer: {
+    flexDirection: 'row',
+  },
   row: {
     backgroundColor: 'panelForeground',
     flexDirection: 'row',
@@ -91,7 +124,7 @@ const unboundStyles = {
   },
 };
 
-const ConnectedThreadSettingsParent: React.ComponentType<Props> =
-  React.memo<Props>(ThreadSettingsParent);
+const ConnectedThreadSettingsParent: React.ComponentType<ThreadSettingsParentProps> =
+  React.memo<ThreadSettingsParentProps>(ThreadSettingsParent);
 
 export default ConnectedThreadSettingsParent;
