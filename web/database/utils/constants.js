4ec416f4717d8c1928ad205d8bb61ea7f8e5856f
xsanm <kkurowski.kk@gmail.com>
2023-03-25 18:03:55 +0100

    [web-db] add database module provider
    Summary:
    Making database connection accessible from entire web app
    
    Depends on D7188
    
    Test Plan:
    Check how it behaves in multiple places in web app, try sending some worker messages, e.g.:
    ```
        databaseModule
          .schedule({
            type: workerRequestMessageTypes.GET_CLIENT_STORE,
          })
          .then(e => {
            console.log(e.store.drafts);
          })
          .catch(e => {
            console.error(e);
          });
    ```
    
    Reviewers: michal, tomek
    
    Reviewed By: tomek
    
    Subscribers: ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D7189

--- web/database/utils/constants.js
+++ web/database/utils/constants.js
@@ -7,6 +7,9 @@ export const CURRENT_USER_ID_KEY = 'current_user_id';
 
 export const DB_PERSIST_THROTTLE_WAIT_MS = 300;
 
+export const DATABASE_WORKER_PATH = '/worker/database';
+export const SQLJS_FILE_PATH = '/compiled/webworkers';
+
 export const DB_SUPPORTED_OS: $ReadOnlyArray<string> = [
   'Windows 10',
   'Linux',
