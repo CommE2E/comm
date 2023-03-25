e31b4b32f2fc23ae39cdd04e2f8175182c62b7f6
xsanm <kkurowski.kk@gmail.com>
2023-03-25 18:03:55 +0100

    [web-db] add database module provider
    Summary: Making database connection accessible from entire web app
    
    Depends on D7188
    
    Test Plan:
    Check how it behaves in multiple places in web app, try sending some worker messages, e.g.:
    ```
        databaseModule
          .schedule({
            type: workerRequestMessageTypes.GET_CLIENT_STORE,
          })
          .then(e => {
            console.log(e.store.drafts);
          })
          .catch(e => {
            console.error(e);
          });
    ```
    
    Reviewers: michal, tomek

--- web/database/utils/constants.js
+++ web/database/utils/constants.js
@@ -4,3 +4,6 @@ export const SQLITE_CONTENT = 'sqliteFileContent';
 export const SQLITE_ENCRYPTION_KEY = 'encryptionKey';
 
 export const CURRENT_USER_ID_KEY = 'current_user_id';
+
+export const DATABASE_WORKER_PATH = '/worker/database';
+export const SQLJS_FILE_PATH = '/compiled/webworkers';
