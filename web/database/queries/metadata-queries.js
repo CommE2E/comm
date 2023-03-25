005ff6a137c5c676d4999fc9b1937776443adee7
xsanm <kkurowski.kk@gmail.com>
2023-03-25 14:12:19 +0100

    [web-db] add metadata table operations
    Summary:
    Native version: [link](https://github.com/CommE2E/comm/blob/8e3ede03a4e41a82c66bd1ab3047311c077a8e1c/native/cpp/CommonCpp/DatabaseManagers/SQLiteQueryExecutor.cpp#L363);
     On the web (for now) we will need this to set `current_user_id` - to somehow connect database with user, to decide when we should clear sensitive data.
    
     NOTE: D7121 is not landed yet, and even so, my local environment is the only place where this code was executed so adding this without migration.
    
    Depends on D7154
    
    Test Plan: run tests D7181
    
    Reviewers: michal, tomek, marcin, ashoat
    
    Reviewed By: ashoat
    
    Subscribers: ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D7180

---
+++ web/database/queries/metadata-queries.js
@@ -0,0 +1,56 @@
+// @flow
+
+import { SqliteDatabase } from 'sql.js';
+
+import { parseMultiStatementSQLiteResult } from '../utils/db-utils.js';
+
+type Metadata = {
+  name: string,
+  data: string,
+};
+
+function setMetadata(db: SqliteDatabase, entryName: string, data: string) {
+  const query = `
+    INSERT OR REPLACE INTO metadata (name, data)
+    VALUES ($entryName, $data)
+  `;
+  const params = {
+    $entryName: entryName,
+    $data: data,
+  };
+
+  db.exec(query, params);
+}
+
+function getMetadata(db: SqliteDatabase, entryName: string): ?string {
+  const query = `
+    SELECT * 
+    FROM metadata
+    WHERE name = $entryName
+  `;
+  const params = {
+    $entryName: entryName,
+  };
+
+  const rawResult = db.exec(query, params);
+  const result = parseMultiStatementSQLiteResult<Metadata>(rawResult);
+  if (result.length === 0 || result[0].length === 0) {
+    return undefined;
+  }
+  const [entry] = result[0];
+  return entry.data;
+}
+
+function clearMetadata(db: SqliteDatabase, entryName: string) {
+  const query = `
+    DELETE FROM metadata
+    WHERE name = $entryName
+  `;
+  const params = {
+    $entryName: entryName,
+  };
+
+  db.exec(query, params);
+}
+
+export { setMetadata, getMetadata, clearMetadata };
