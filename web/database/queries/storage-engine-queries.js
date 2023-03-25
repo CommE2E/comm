2c8bec465409059a1876157e0ae2d58413db0bde
xsanm <kkurowski.kk@gmail.com>
2023-03-25 16:58:53 +0100

    [web-db] add table and operations for redux-persist engine
    Summary:
    Supporting operations for custom [storage-engine](https://github.com/rt2zz/redux-persist#storage-engines)
    
    NOTE: There is a code repetition between this and metadata because both are basically operation on a key-value table. Creating task to unify this as a follow-up.
    
    Depends on D7182
    
    Test Plan: Run tests
    
    Reviewers: michal, tomek, marcin
    
    Subscribers: ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D7183

---
+++ web/database/queries/storage-engine-queries.js
@@ -0,0 +1,59 @@
+// @flow
+
+import { SqliteDatabase } from 'sql.js';
+
+import { parseMultiStatementSQLiteResult } from '../utils/db-utils.js';
+
+type Entry = {
+  key: string,
+  item: string,
+};
+
+function setPersistStorageItem(db: SqliteDatabase, key: string, item: string) {
+  const query = `
+    INSERT OR REPLACE INTO persist_storage (key, item)
+    VALUES ($key, $item)
+  `;
+  const params = {
+    $key: key,
+    $item: item,
+  };
+
+  db.exec(query, params);
+}
+
+function getPersistStorageItem(db: SqliteDatabase, key: string): string {
+  const query = `
+    SELECT * FROM persist_storage
+    WHERE key = $key
+  `;
+  const params = {
+    $key: key,
+  };
+
+  const rawResult = db.exec(query, params);
+  const result = parseMultiStatementSQLiteResult<Entry>(rawResult);
+  if (result.length === 0 || result[0].length === 0) {
+    return '';
+  }
+  const [entry] = result[0];
+  return entry.item;
+}
+
+function removePersistStorageItem(db: SqliteDatabase, key: string) {
+  const query = `
+    DELETE FROM persist_storage
+    WHERE key = $key
+  `;
+  const params = {
+    $key: key,
+  };
+
+  db.exec(query, params);
+}
+
+export {
+  setPersistStorageItem,
+  getPersistStorageItem,
+  removePersistStorageItem,
+};
