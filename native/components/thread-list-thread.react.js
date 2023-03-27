086416ce36d8f7a05d866f279c10341a0349e70c
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-03-27 18:02:45 -0400

    [native] render the rest of the thread avatars
    Summary:
    rendered the rest of thread avatars which included the thread list thread and the chat thread list item
    
    ---
    
    Depends on D7137
    
    Test Plan:
    Please check out the screenshots to see the changes that I made:
    
    Thread list:
    
    staff user:
    {F439935}
    
    non staff user:
    {F439937}
    
    Chat thread list:
    
    staff user:
    {F439936}
    
    non staff user:
    {F439938}
    
    Reviewers: ashoat, atul
    
    Reviewed By: ashoat
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7138

--- native/components/thread-list-thread.react.js
+++ native/components/thread-list-thread.react.js
@@ -2,14 +2,18 @@
 
 import * as React from 'react';
 
+import { useGetAvatarForThread } from 'lib/shared/avatar-utils.js';
+import type { ClientAvatar } from 'lib/types/avatar-types.js';
 import type { ThreadInfo, ResolvedThreadInfo } from 'lib/types/thread-types.js';
 import { useResolvedThreadInfo } from 'lib/utils/entity-helpers.js';
 
+import Avatar from './avatar.react.js';
 import Button from './button.react.js';
 import ColorSplotch from './color-splotch.react.js';
 import { SingleLine } from './single-line.react.js';
 import { type Colors, useStyles, useColors } from '../themes/colors.js';
 import type { ViewStyle, TextStyle } from '../types/styles.js';
+import { useShouldRenderAvatars } from '../utils/avatar-utils.js';
 
 type SharedProps = {
   +onSelect: (threadID: string) => void,
@@ -23,12 +27,22 @@ type BaseProps = {
 type Props = {
   ...SharedProps,
   +threadInfo: ResolvedThreadInfo,
+  +avatarInfo: ClientAvatar,
+  +shouldRenderAvatars: boolean,
   +colors: Colors,
   +styles: typeof unboundStyles,
 };
 class ThreadListThread extends React.PureComponent<Props> {
   render() {
     const { modalIosHighlightUnderlay: underlayColor } = this.props.colors;
+
+    let avatar;
+    if (this.props.shouldRenderAvatars) {
+      avatar = <Avatar size="small" avatarInfo={this.props.avatarInfo} />;
+    } else {
+      avatar = <ColorSplotch color={this.props.threadInfo.color} />;
+    }
+
     return (
       <Button
         onPress={this.onSelect}
@@ -37,7 +51,7 @@ class ThreadListThread extends React.PureComponent<Props> {
         iosActiveOpacity={0.85}
         style={[this.props.styles.button, this.props.style]}
       >
-        <ColorSplotch color={this.props.threadInfo.color} />
+        {avatar}
         <SingleLine style={[this.props.styles.text, this.props.textStyle]}>
           {this.props.threadInfo.uiName}
         </SingleLine>
@@ -71,11 +85,15 @@ const ConnectedThreadListThread: React.ComponentType<BaseProps> =
     const styles = useStyles(unboundStyles);
     const colors = useColors();
     const resolvedThreadInfo = useResolvedThreadInfo(threadInfo);
+    const avatarInfo = useGetAvatarForThread(threadInfo);
+    const shouldRenderAvatars = useShouldRenderAvatars();
 
     return (
       <ThreadListThread
         {...rest}
         threadInfo={resolvedThreadInfo}
+        avatarInfo={avatarInfo}
+        shouldRenderAvatars={shouldRenderAvatars}
         styles={styles}
         colors={colors}
       />
