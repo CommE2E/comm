c38e70c6d6ee911d832aa380fb99ddda0cac08e5
xsanm <kkurowski.kk@gmail.com>
2023-03-27 14:57:29 +0200

    [web-db] start persisting SQLite content
    Summary:
    Saving database file in indexed db
    
    Depends on D7185
    
    Test Plan:
    1. Check in devtools if indexedDB key exists
    2. Reopen app and save something into db - database should be read from the indexed db, not freshly created
    
    {F446908}
    
    Reviewers: michal, tomek
    
    Reviewed By: tomek
    
    Subscribers: ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D7186

--- web/database/utils/constants.js
+++ web/database/utils/constants.js
@@ -4,3 +4,5 @@ export const SQLITE_CONTENT = 'sqliteFileContent';
 export const SQLITE_ENCRYPTION_KEY = 'encryptionKey';
 
 export const CURRENT_USER_ID_KEY = 'current_user_id';
+
+export const DB_PERSIST_THROTTLE_WAIT_MS = 300;
