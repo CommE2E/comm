9b43e4e8d54ff5e9864ec61c9db8c8d0dc947a67
Jonathan Ringer <jonathan.ringer@comm.app>
2023-03-27 08:55:32 -0700

    [Native] Update native_rust_library ffi to reflect client gRPC
    Summary: Update ffi to avoid merge conflicts between varun and I.
    
    https://linear.app/comm/issue/ENG-3385
    
    Test Plan:
    ```
    cd native/native_rust_library
    cargo build
    ```
    Android build gate
    
    Reviewers: ashoat, varun, bartek
    
    Subscribers:

--- native/native_rust_library/src/identity_client.rs
+++ native/native_rust_library/src/identity_client.rs
@@ -4,29 +4,49 @@ use crate::IdentityClient;
 
 pub async fn register_user(
   mut _client: Box<IdentityClient>,
-  _user_id: String,
-  _signing_public_key: String,
   _username: String,
   _password: String,
+  _key_payload: String,
+  _key_payload_signature: String,
+  _identity_prekey: String,
+  _identity_prekey_signature: String,
+  _notif_prekey: String,
+  _notif_prekey_signature: String,
+  _identity_onetime_keys: Vec<String>,
+  _notif_onetime_keys: Vec<String>,
 ) -> Result<String, Status> {
   unimplemented!();
 }
 
+// User could be logging in from new device, need to resend device information
 pub async fn login_user_pake(
   mut _client: Box<IdentityClient>,
-  _user_id: String,
-  _signing_public_key: String,
+  _username: String,
   _password: String,
+  _key_payload: String,
+  _key_payload_signature: String,
+  _identity_prekey: String,
+  _identity_prekey_signature: String,
+  _notif_prekey: String,
+  _notif_prekey_signature: String,
+  _identity_onetime_keys: Vec<String>,
+  _notif_onetime_keys: Vec<String>,
 ) -> Result<String, Status> {
   unimplemented!();
 }
 
 pub async fn login_user_wallet(
   mut _client: Box<IdentityClient>,
-  _user_id: String,
-  _signing_public_key: String,
   _siwe_message: String,
   _siwe_signature: String,
+  _key_payload: String,
+  _key_payload_signature: String,
+  _identity_prekey: String,
+  _identity_prekey_signature: String,
+  _notif_prekey: String,
+  _notif_prekey_signature: String,
+  _identity_onetime_keys: Vec<String>,
+  _notif_onetime_keys: Vec<String>,
 ) -> Result<String, Status> {
   unimplemented!();
 }
