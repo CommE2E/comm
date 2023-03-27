d0d4fad4e98af43f59287b376a05dab79f73ce00
xsanm <kkurowski.kk@gmail.com>
2023-03-27 12:42:32 +0200

    [web-db] implement clearing sensitive data
    Summary:
    Initial version of hook responsible for handling sensitive data. It'll have more functions but pushing initial version to start iterating on this.
    
    This component will be rendered here: [root.js](https://github.com/CommE2E/comm/blob/112846eaebdd8624bc8b54b392ea739ca2406789/web/root.js#L71)
    
    Test Plan: Render component, log in/log out and check if it works
    
    Reviewers: michal, tomek, marcin
    
    Subscribers: ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D7191

---
+++ web/database/sqlite-data-handler.js
@@ -0,0 +1,60 @@
+// @flow
+
+import * as React from 'react';
+
+import { databaseModule } from './database-module-provider.js';
+import { useSelector } from '../redux/redux-utils.js';
+import { workerRequestMessageTypes } from '../types/worker-types.js';
+
+function SQLiteDataHandler(): React.Node {
+  const rehydrateConcluded = useSelector(
+    state => !!(state._persist && state._persist.rehydrated),
+  );
+  const currentLoggedInUserID = useSelector(state =>
+    state.currentUserInfo?.anonymous ? undefined : state.currentUserInfo?.id,
+  );
+
+  const handleSensitiveData = React.useCallback(async () => {
+    try {
+      const currentUserData = await databaseModule.schedule({
+        type: workerRequestMessageTypes.GET_CURRENT_USER_ID,
+      });
+
+      if (
+        currentUserData?.userID &&
+        currentUserData.userID !== currentLoggedInUserID
+      ) {
+        await databaseModule.clearSensitiveData();
+      }
+      if (currentLoggedInUserID) {
+        await databaseModule.schedule({
+          type: workerRequestMessageTypes.SET_CURRENT_USER_ID,
+          userID: currentLoggedInUserID,
+        });
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }, [currentLoggedInUserID]);
+
+  React.useEffect(() => {
+    (async () => {
+      if (currentLoggedInUserID) {
+        await databaseModule.userLoggedIn(currentLoggedInUserID);
+      }
+      if (!rehydrateConcluded) {
+        return;
+      }
+
+      const isWorkable = await databaseModule.isDatabaseWorkable();
+      if (!isWorkable) {
+        return;
+      }
+      await handleSensitiveData();
+    })();
+  }, [currentLoggedInUserID, handleSensitiveData, rehydrateConcluded]);
+
+  return null;
+}
+
+export { SQLiteDataHandler };
