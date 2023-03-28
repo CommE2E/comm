27021602a9dc46bb7f0e871406f1876022e30baf
Inka Sokołowska <inka.sokolowska@swmansion.com>
2023-03-28 09:43:43 +0200

    [keyserver] Process new messages for search
    Summary:
    issues: https://linear.app/comm/issue/ENG-3314/implement-a-function-for-processing-messages-for-search-and-storing,
    https://linear.app/comm/issue/ENG-3315/implement-adding-processed-messages-to-db-when-new-messages-are
    `tokenizeAndStem` splits the text into words, removes stopwords, and stemms the remaining words. It returns an array. The second paramether is a boolean tellinng whether to keep the stopwords. source
    code:https://github.com/NaturalNode/natural/blob/master/lib/natural/stemmers/stemmer.js
    
    Test Plan:
    Tested that when a new message is created, a proper field appears in the `search` table. Tested that when a message is edited (by passing an artificial edit message to processMessagesForSearch
    function) a proper field is edited in the `search` table.
    
    Reviewers: kamil, bartek, kuba
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7077

---
+++ keyserver/src/database/search-utils.js
@@ -0,0 +1,48 @@
+// @flow
+
+import natural from 'natural';
+
+import type { RawMessageInfo } from 'lib/types/message-types';
+import { messageTypes } from 'lib/types/message-types.js';
+
+import { dbQuery, SQL } from '../database/database.js';
+
+async function processMessagesForSearch(
+  messages: $ReadOnlyArray<RawMessageInfo>,
+): Promise<void> {
+  const processedMessages = [];
+
+  for (const msg of messages) {
+    if (
+      msg.type !== messageTypes.TEXT &&
+      msg.type !== messageTypes.EDIT_MESSAGE
+    ) {
+      continue;
+    }
+
+    const processed_msg = natural.PorterStemmer.tokenizeAndStem(
+      msg.text,
+      false,
+    ).join(' ');
+
+    if (msg.type === messageTypes.TEXT) {
+      processedMessages.push([msg.id, msg.id, processed_msg]);
+    } else {
+      processedMessages.push([msg.targetMessageID, msg.id, processed_msg]);
+    }
+  }
+
+  if (processedMessages.length === 0) {
+    return;
+  }
+
+  await dbQuery(SQL`
+    INSERT INTO message_search (original_message_id, message_id, processed_content)
+    VALUES ${processedMessages}
+    ON DUPLICATE KEY UPDATE 
+      message_id = VALUE(message_id), 
+      processed_content = VALUE(processed_content);
+  `);
+}
+
+export { processMessagesForSearch };
