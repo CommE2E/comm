039243f1d610501a04cc5071d8c90e331eea2b7b
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2023-03-29 10:23:39 +0200

    [native] Extract LoadableImage component
    Summary: Addressing feedback from https://phab.comm.dev/D7225 - extracted the common part from `<RemoteImage>` to a new component: `<LoadableImage>`
    
    Test Plan: Media images are still loaded and displayed correctly.
    
    Reviewers: ashoat, atul, ginsu
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D7232

---
+++ native/media/loadable-image.react.js
@@ -0,0 +1,68 @@
+// @flow
+
+import { Image } from 'expo-image';
+import * as React from 'react';
+import { View, StyleSheet, ActivityIndicator } from 'react-native';
+import type { ImageSource } from 'react-native/Libraries/Image/ImageSource';
+
+import type { ImageStyle } from '../types/styles.js';
+
+type Props = {
+  +source: ?ImageSource,
+  +onLoad: () => void,
+  +spinnerColor: string,
+  +style: ImageStyle,
+  +invisibleLoad: boolean,
+};
+function LoadableImage(props: Props): React.Node {
+  const { source, onLoad: onLoadProp } = props;
+
+  const [loaded, setLoaded] = React.useState(false);
+
+  const onLoad = React.useCallback(() => {
+    setLoaded(true);
+    onLoadProp && onLoadProp();
+  }, [onLoadProp]);
+
+  const invisibleStyle = React.useMemo(
+    () => [props.style, styles.invisible],
+    [props.style],
+  );
+
+  if (source && loaded) {
+    return <Image source={source} onLoad={onLoad} style={props.style} />;
+  }
+
+  if (props.invisibleLoad) {
+    return <Image source={source} onLoad={onLoad} style={invisibleStyle} />;
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.spinnerContainer}>
+        <ActivityIndicator color={props.spinnerColor} size="large" />
+      </View>
+      <Image source={source} onLoad={onLoad} style={props.style} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  invisible: {
+    opacity: 0,
+  },
+  spinnerContainer: {
+    alignItems: 'center',
+    bottom: 0,
+    justifyContent: 'center',
+    left: 0,
+    position: 'absolute',
+    right: 0,
+    top: 0,
+  },
+});
+
+export default LoadableImage;
