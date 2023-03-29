a369a8279b485ed1204800634eb7c3fbcfe2066d
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2023-03-29 10:23:39 +0200

    [native] Extract LoadableImage component
    Summary: Addressing feedback from https://phab.comm.dev/D7225 - extracted the common part from `<RemoteImage>` to a new component: `<LoadableImage>`
    
    Test Plan: Media images are still loaded and displayed correctly.
    
    Reviewers: ashoat, atul, ginsu
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D7232

--- native/media/remote-image.react.js
+++ native/media/remote-image.react.js
@@ -1,11 +1,10 @@
 // @flow
 
-import { Image } from 'expo-image';
 import * as React from 'react';
-import { View, StyleSheet, ActivityIndicator } from 'react-native';
 
 import { type ConnectionStatus } from 'lib/types/socket-types.js';
 
+import LoadableImage from './loadable-image.react.js';
 import { useSelector } from '../redux/redux-utils.js';
 import type { ImageStyle } from '../types/styles.js';
 
@@ -22,17 +21,16 @@ type Props = {
 };
 type State = {
   +attempt: number,
-  +loaded: boolean,
 };
 class RemoteImage extends React.PureComponent<Props, State> {
+  loaded: boolean = false;
   state: State = {
     attempt: 0,
-    loaded: false,
   };
 
-  componentDidUpdate(prevProps: Props, prevState: State) {
+  componentDidUpdate(prevProps: Props) {
     if (
-      !this.state.loaded &&
+      !this.loaded &&
       this.props.connectionStatus === 'connected' &&
       prevProps.connectionStatus !== 'connected'
     ) {
@@ -40,72 +38,29 @@ class RemoteImage extends React.PureComponent<Props, State> {
         attempt: otherPrevState.attempt + 1,
       }));
     }
-    if (this.state.loaded && !prevState.loaded) {
-      this.props.onLoad && this.props.onLoad(this.props.uri);
-    }
   }
 
   render() {
-    const source = { uri: this.props.uri };
-    if (this.state.loaded) {
-      return (
-        <Image
-          source={source}
-          onLoad={this.onLoad}
-          style={this.props.style}
-          key={this.state.attempt}
-        />
-      );
-    }
-
-    if (this.props.invisibleLoad) {
-      return (
-        <Image
-          source={source}
-          onLoad={this.onLoad}
-          style={[this.props.style, styles.invisible]}
-          key={this.state.attempt}
-        />
-      );
-    }
+    const { style, spinnerColor, invisibleLoad, uri } = this.props;
+    const source = { uri };
 
     return (
-      <View style={styles.container}>
-        <View style={styles.spinnerContainer}>
-          <ActivityIndicator color={this.props.spinnerColor} size="large" />
-        </View>
-        <Image
-          source={source}
-          onLoad={this.onLoad}
-          style={this.props.style}
-          key={this.state.attempt}
-        />
-      </View>
+      <LoadableImage
+        source={source}
+        onLoad={this.onLoad}
+        spinnerColor={spinnerColor}
+        style={style}
+        invisibleLoad={invisibleLoad}
+        key={this.state.attempt}
+      />
     );
   }
 
   onLoad = () => {
-    this.setState({ loaded: true });
+    this.loaded = true;
+    this.props.onLoad && this.props.onLoad(this.props.uri);
   };
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  invisible: {
-    opacity: 0,
-  },
-  spinnerContainer: {
-    alignItems: 'center',
-    bottom: 0,
-    justifyContent: 'center',
-    left: 0,
-    position: 'absolute',
-    right: 0,
-    top: 0,
-  },
-});
 
 const ConnectedRemoteImage: React.ComponentType<BaseProps> =
   React.memo<BaseProps>(function ConnectedRemoteImage(props: BaseProps) {
