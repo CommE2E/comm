ec7d2704c11abbc8264bfe84d80859533835a416
Inka Sokołowska <inka.sokolowska@swmansion.com>
2023-03-29 18:24:30 +0200

    [keyserver] Add type workaround for Intl.Segmenter
    Summary:
    As discussed in D7077 we want to add type definition for Intl, because the type provided by flow doesn't include Segmenter field we need.
    Here are the typescript types: https://github.com/microsoft/TypeScript/blob/main/src/lib/es2022.intl.d.ts
    
    Test Plan: run `flow` in keyserver/. Checked that it is possible to use `segment` method of the returned object
    
    Reviewers: ashoat, tomek, kamil
    
    Subscribers: atul
    
    Differential Revision: https://phab.comm.dev/D7238

---
+++ keyserver/src/utils/segmenter.js
@@ -0,0 +1,52 @@
+// @flow
+
+type SegmenterOptions = {
+  localeMatcher?: 'best fit' | 'lookup',
+  granularity?: 'grapheme' | 'word' | 'sentence',
+};
+
+type ResolvedSegmenterOptions = {
+  locale: string,
+  granularity: 'grapheme' | 'word' | 'sentence',
+};
+
+declare class Segmenter {
+  constructor(
+    locales?: Intl$Locale | Intl$Locale[],
+    options?: SegmenterOptions,
+  ): Segmenter;
+
+  resolvedOptions(): ResolvedSegmenterOptions;
+
+  segment(input: string): Segments;
+}
+
+type SegmentData = {
+  segment: string,
+  index: number,
+  input: string,
+  isWordLike?: boolean,
+};
+
+declare class Segments {
+  containing(codeUnitIndex?: number): SegmentData;
+  @@iterator: () => Iterator<SegmentData>;
+}
+
+type OurIntlType = {
+  +Collator: Class<Intl$Collator>,
+  +DateTimeFormat: Class<Intl$DateTimeFormat>,
+  +NumberFormat: Class<Intl$NumberFormat>,
+  +PluralRules: ?Class<Intl$PluralRules>,
+  +getCanonicalLocales?: (locales?: Intl$Locales) => Intl$Locale[],
+  +Segmenter: Class<Segmenter>,
+};
+
+const OurIntl: OurIntlType = (Intl: any);
+const segmenter = new OurIntl.Segmenter('eng', { granularity: 'word' });
+
+function getSegmenter(): any {
+  return segmenter;
+}
+
+export { getSegmenter };
