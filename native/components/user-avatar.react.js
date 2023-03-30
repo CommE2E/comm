c65e46d322e02035c6a70fcf91bf9b466518d1a0
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-03-30 16:47:01 -0400

    [native/lib] introduce useAvatarForUserID hook in avatar-utils
    Summary: This logic will need to be used in both `UserAvatars` and `ThreadAvatar` so I factored out the logic to easily reuse in both cases
    
    Test Plan: Same test plan as D7246 and everything rendered correctly
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7260

--- native/components/user-avatar.react.js
+++ native/components/user-avatar.react.js
@@ -2,11 +2,9 @@
 
 import * as React from 'react';
 
-import { useENSAvatar } from 'lib/hooks/ens-cache.js';
-import { userIdentifiedByETHAddress } from 'lib/shared/account-utils.js';
 import {
-  defaultAnonymousUserEmojiAvatar,
   getAvatarForUser,
+  useENSResolvedAvatar,
 } from 'lib/shared/avatar-utils.js';
 
 import Avatar from './avatar.react.js';
@@ -25,34 +23,9 @@ function UserAvatar(props: Props): React.Node {
   );
   const avatarInfo = getAvatarForUser(userInfo);
 
-  const ethAddress = React.useMemo(() => {
-    let address = null;
-    if (userInfo && avatarInfo.type === 'ens') {
-      const { username } = userInfo;
-      address =
-        username && userIdentifiedByETHAddress(userInfo) ? username : null;
-    }
-    return address;
-  }, [avatarInfo.type, userInfo]);
+  const resolvedUserAvatar = useENSResolvedAvatar(avatarInfo, userInfo);
 
-  const ensAvatarURI = useENSAvatar(ethAddress);
-
-  const userAvatarInfo = React.useMemo(() => {
-    if (avatarInfo.type !== 'ens') {
-      return avatarInfo;
-    }
-
-    if (ensAvatarURI) {
-      return {
-        type: 'image',
-        uri: ensAvatarURI,
-      };
-    }
-
-    return defaultAnonymousUserEmojiAvatar;
-  }, [ensAvatarURI, avatarInfo]);
-
-  return <Avatar size={size} avatarInfo={userAvatarInfo} />;
+  return <Avatar size={size} avatarInfo={resolvedUserAvatar} />;
 }
 
 export default UserAvatar;
