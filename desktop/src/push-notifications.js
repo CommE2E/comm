6c7a320eafacac75e56f0cb5e6145bd659701410
Michal Gniadek <michal.gniadek@swmansion.com>
2023-03-31 13:56:52 +0200

    [desktop] Handle windows notifications
    Summary:
    This diff adds handling of the Windows (WNS) notifications to the desktop client. It uses the native windows node module, built in the previous diffs.
    
    Depends on D7286, D7253
    
    Test Plan: Run the app, connect to the keyserver and check if it correctly gets the notifications. Check if it redirects to the app after clicking on the notification.
    
    Reviewers: kamil, tomek
    
    Subscribers: ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D7287

--- desktop/src/push-notifications.js
+++ desktop/src/push-notifications.js
@@ -1,18 +1,83 @@
 // @flow
 
 // eslint-disable-next-line import/extensions
-import { pushNotifications, Notification } from 'electron/main';
+import { app, pushNotifications, Notification } from 'electron/main';
+import EventEmitter from 'events';
+import { resolve } from 'path';
+
+import { isNormalStartup } from './handle-squirrel-event.js';
+
+let windowsPushNotificationManager;
+const windowsPushNotifEventEmitter = new EventEmitter();
+if (process.platform === 'win32' && app.isPackaged && isNormalStartup()) {
+  (async () => {
+    try {
+      const { PushNotificationManager } = await import('@commapp/windowspush');
+
+      if (!PushNotificationManager.isSupported()) {
+        return;
+      }
+
+      windowsPushNotificationManager = PushNotificationManager.default;
+
+      const handleEvent = (manager, event) => {
+        const byteArray = [];
+        for (let i = 0; i < event.payload.length; i++) {
+          byteArray.push(event.payload[i]);
+        }
+        const payload = Buffer.from(byteArray).toString('utf-8');
+        windowsPushNotifEventEmitter.emit(
+          'received-wns-notification',
+          JSON.parse(payload),
+        );
+      };
+
+      // Windows requires that this must be called before the call to `register`
+      windowsPushNotificationManager.addListener('PushReceived', handleEvent);
+
+      windowsPushNotificationManager.register();
+
+      app.on('quit', () => {
+        windowsPushNotificationManager.removeListener(
+          'PushReceived',
+          handleEvent,
+        );
+        windowsPushNotificationManager.unregisterAll();
+      });
+    } catch (err) {
+      console.error(
+        `Error while loading windows push notifications ${err.message}`,
+      );
+    }
+  })();
+}
 
 async function registerForNotifications(): Promise<?string> {
-  if (process.platform !== 'darwin') {
-    return null;
-  }
+  if (process.platform === 'darwin') {
+    try {
+      const token = await pushNotifications.registerForAPNSNotifications();
+      return token;
+    } catch (err) {
+      console.error(err);
+    }
+  } else if (process.platform === 'win32' && windowsPushNotificationManager) {
+    try {
+      const token = await new Promise((resolvePromise, reject) => {
+        windowsPushNotificationManager.createChannelAsync(
+          'XXX',
+          (error, result) => {
+            if (error) {
+              reject(error);
+            }
+            resolvePromise(result.channel.uri);
+          },
+        );
+      });
 
-  try {
-    const token = await pushNotifications.registerForAPNSNotifications();
-    return token;
-  } catch (err) {
-    console.error(err);
+      return token;
+    } catch (err) {
+      console.error(err);
+    }
   }
 
   return null;
@@ -30,21 +95,25 @@ function showNewNotification(
     return;
   }
   const { title, body, threadID } = payload;
+  const windowsIconPath = resolve(__dirname, '../icons/icon.ico');
   const notif = new Notification({
     title,
     body,
+    icon: process.platform === 'win32' ? windowsIconPath : undefined,
   });
   notif.on('click', () => handleClick(threadID));
   notif.show();
 }
 
 function listenForNotifications(handleClick: (threadID: string) => void) {
-  if (process.platform !== 'darwin') {
-    return;
+  if (process.platform === 'darwin') {
+    pushNotifications.on('received-apns-notification', (event, userInfo) => {
+      showNewNotification(userInfo, handleClick);
+    });
+  } else if (process.platform === 'win32') {
+    windowsPushNotifEventEmitter.on('received-wns-notification', payload => {
+      showNewNotification(payload, handleClick);
+    });
   }
-  pushNotifications.on('received-apns-notification', (event, userInfo) => {
-    showNewNotification(userInfo, handleClick);
-  });
 }
-
 export { listenForNotifications, registerForNotifications };
