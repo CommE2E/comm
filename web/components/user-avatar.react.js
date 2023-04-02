8c1caa2ffa80b59fdb4d54213d72d41543e26019
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-04-01 20:20:07 -0400

    [web] introduce UserAvatar component
    Summary: After some discussion with @Ashoat and @atul about how to fetch the ens uri for ens avatars we decided to have two separate components to render avatars: `UserAvatar` and `ThreadAvatar` this diff handles `UserAvatar`
    
    Test Plan: I was able to render all three avatar types: Emoji, Image, and ENS
    
    Subscribers: tomek, ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D7273

---
+++ web/components/user-avatar.react.js
@@ -0,0 +1,31 @@
+// @flow
+
+import * as React from 'react';
+
+import {
+  getAvatarForUser,
+  useENSResolvedAvatar,
+} from 'lib/shared/avatar-utils.js';
+
+import Avatar from './avatar.react.js';
+import { useSelector } from '../redux/redux-utils.js';
+
+type Props = {
+  +userID: ?string,
+  +size: 'micro' | 'small' | 'large' | 'profile',
+};
+
+function UserAvatar(props: Props): React.Node {
+  const { userID, size } = props;
+
+  const userInfo = useSelector(state =>
+    userID ? state.userStore.userInfos[userID] : null,
+  );
+  const avatarInfo = getAvatarForUser(userInfo);
+
+  const resolvedUserAvatar = useENSResolvedAvatar(avatarInfo, userInfo);
+
+  return <Avatar size={size} avatarInfo={resolvedUserAvatar} />;
+}
+
+export default UserAvatar;
