6ff50c66c3ff71f2fe70c63a413f65b8f53a91a0
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-04-01 20:20:51 -0400

    [web] introduce ThreadAvatar component
    Summary: After some discussion with @Ashoat and @atul about how to fetch the ens avatars we decided to have two separate components to render avatars: `UserAvatar` and `ThreadAvatar`. This diff handles `ThreadAvatar`.
    
    Test Plan: I was able to render all three avatar types: Emoji, Image, and ENS:
    
    Subscribers: tomek, ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D7274

---
+++ web/components/thread-avatar.react.js
@@ -0,0 +1,51 @@
+// @flow
+
+import * as React from 'react';
+
+import {
+  useAvatarForThread,
+  useENSResolvedAvatar,
+} from 'lib/shared/avatar-utils.js';
+import { getSingleOtherUser } from 'lib/shared/thread-utils.js';
+import {
+  threadTypes,
+  type RawThreadInfo,
+  type ThreadInfo,
+} from 'lib/types/thread-types.js';
+
+import Avatar from './avatar.react.js';
+import { useSelector } from '../redux/redux-utils.js';
+
+type Props = {
+  +threadInfo: RawThreadInfo | ThreadInfo,
+  +size: 'micro' | 'small' | 'large' | 'profile',
+};
+
+function ThreadAvatar(props: Props): React.Node {
+  const { threadInfo, size } = props;
+
+  const avatarInfo = useAvatarForThread(threadInfo);
+
+  const viewerID = useSelector(
+    state => state.currentUserInfo && state.currentUserInfo.id,
+  );
+
+  let displayUserIDForThread;
+  if (threadInfo.type === threadTypes.PRIVATE) {
+    displayUserIDForThread = viewerID;
+  } else if (threadInfo.type === threadTypes.PERSONAL) {
+    displayUserIDForThread = getSingleOtherUser(threadInfo, viewerID);
+  }
+
+  const displayUser = useSelector(state =>
+    displayUserIDForThread
+      ? state.userStore.userInfos[displayUserIDForThread]
+      : null,
+  );
+
+  const resolvedThreadAvatar = useENSResolvedAvatar(avatarInfo, displayUser);
+
+  return <Avatar size={size} avatarInfo={resolvedThreadAvatar} />;
+}
+
+export default ThreadAvatar;
