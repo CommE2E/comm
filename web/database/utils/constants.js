cca53b523bba7424312ae978bab1b759e6187a5f
xsanm <kkurowski.kk@gmail.com>
2023-04-03 09:54:47 +0200

    [web-db] support SQLite for staff and non-mobile browsers
    Summary:
    Adding flag in website responder to get this information at the very begging of web app lifecycle.
    
    I did some research and there is no one good method for detecting it is mobile browser. I choose solution which I think is most likely to work, but it'll be good to revisit this in the future [ENG-3407](https://linear.app/comm/issue/ENG-3407/revisit-method-for-detecting-mobile-browser).
    
    Test Plan:
    1. Test for web app and electron
    2. Test in browser on mobile
    
    Reviewers: michal, tomek
    
    Subscribers: ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D7188

--- web/database/utils/constants.js
+++ web/database/utils/constants.js
@@ -6,3 +6,18 @@ export const SQLITE_ENCRYPTION_KEY = 'encryptionKey';
 export const CURRENT_USER_ID_KEY = 'current_user_id';
 
 export const DB_PERSIST_THROTTLE_WAIT_MS = 300;
+
+export const DB_SUPPORTED_OS: $ReadOnlyArray<string> = [
+  'Windows 10',
+  'Linux',
+  'Mac OS',
+];
+
+export const DB_SUPPORTED_BROWSERS: $ReadOnlyArray<string> = [
+  'edge',
+  'edge-chromium',
+  'chrome',
+  'safari',
+  'firefox',
+  'opera',
+];
