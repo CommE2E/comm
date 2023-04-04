6593564179792d6463450534134315758dbb95a7
Jonathan Ringer <jonathan.ringer@comm.app>
2023-04-04 10:15:32 -0700

    [Identity] Support keygen and server setup for opaque 2.0
    Summary:
    Opaque 2.0 requires different setup for it to run.
    This adds support generating the secrets and adding the
    ServerSetup object to the configuration.
    
    Test Plan:
    ```
    cd services/identity
    cargo run -- keygen
    cargo run -- keygen # should skip writting files
    cargo run -- server
    ```
    
    Reviewers: varun, bartek
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7299

--- services/identity/src/keygen.rs
+++ services/identity/src/keygen.rs
@@ -1,9 +1,13 @@
-use crate::constants::{SECRETS_FILE_EXTENSION, SECRETS_FILE_NAME};
+use crate::constants::{
+  SECRETS_DIRECTORY, SECRETS_FILE_EXTENSION, SECRETS_FILE_NAME,
+  SECRETS_SETUP_FILE,
+};
 use comm_opaque::Cipher;
 use opaque_ke::{ciphersuite::CipherSuite, rand::rngs::OsRng};
-use std::{env, fs, io};
+use std::{env, fs, io, path};
 
 pub fn generate_and_persist_keypair(dir: &str) -> Result<(), io::Error> {
+  // Opaque_ke 1.2.0 setup
   let mut rng = OsRng;
   let server_kp = Cipher::generate_random_keypair(&mut rng);
   let mut path = env::current_dir()?;
@@ -14,11 +18,28 @@ pub fn generate_and_persist_keypair(dir: &str) -> Result<(), io::Error> {
   }
   path.push(SECRETS_FILE_NAME);
   path.set_extension(SECRETS_FILE_EXTENSION);
-  if path.exists() {
-    println!("{:?} already exists", path);
-    return Ok(());
+  if !path.exists() {
+    println!("Writing secret key to {:?}", path);
+    fs::write(&path, server_kp.private().to_arr())?;
+  } else {
+    println!("{:?} already exists, skipping", path);
   }
-  println!("Writing secret key to {:?}", path);
-  fs::write(&path, server_kp.private().to_arr())?;
+
+  // Opaque 2.0 setup
+  let server_setup = comm_opaque2::server::generate_server_setup();
+  let mut path = path::PathBuf::new();
+  path.push(SECRETS_DIRECTORY);
+  if !path.exists() {
+    println!("Creating secrets directory {:?}", path);
+    fs::create_dir(&path)?;
+  }
+  path.push(SECRETS_SETUP_FILE);
+  if path.exists() {
+    eprintln!("{:?} already exists, skipping", path);
+  } else {
+    println!("Writing setup file to {:?}", path);
+    fs::write(&path, server_setup.serialize())?;
+  }
+
   Ok(())
 }
