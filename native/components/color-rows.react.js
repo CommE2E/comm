4bc9000419910a0e683fbdae0e1cd5db9afdc962
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-04-04 13:46:06 -0400

    [native] introduce ColorRows component
    Summary: For the emoji avatar creation flow we want the same color rows logic that are found in `ColorSelector`. I factored out this logic into its own component so that we can better resuse it in both places
    
    Test Plan: There were no regressions, and `ColorSelector` looks and behaves as it did previously
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7302

---
+++ native/components/color-rows.react.js
@@ -0,0 +1,57 @@
+// @flow
+
+import * as React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+import { selectedThreadColors } from 'lib/shared/color-utils.js';
+import type { SetState } from 'lib/types/hook-types.js';
+
+import ColorSelectorButton from './color-selector-button.react.js';
+
+type Props = {
+  +pendingColor: string,
+  +setPendingColor: SetState<string>,
+};
+
+function ColorRows(props: Props): React.Node {
+  const { pendingColor, setPendingColor } = props;
+
+  const colorSelectorButtons = React.useMemo(
+    () =>
+      selectedThreadColors.map(color => (
+        <ColorSelectorButton
+          key={color}
+          color={color}
+          pendingColor={pendingColor}
+          setPendingColor={setPendingColor}
+        />
+      )),
+    [pendingColor, setPendingColor],
+  );
+
+  const firstRow = React.useMemo(
+    () => colorSelectorButtons.slice(0, colorSelectorButtons.length / 2),
+    [colorSelectorButtons],
+  );
+
+  const secondRow = React.useMemo(
+    () => colorSelectorButtons.slice(colorSelectorButtons.length / 2),
+    [colorSelectorButtons],
+  );
+
+  return (
+    <>
+      <View style={styles.colorRowContainer}>{firstRow}</View>
+      <View style={styles.colorRowContainer}>{secondRow}</View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  colorRowContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
+});
+
+export default ColorRows;
