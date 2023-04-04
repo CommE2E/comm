4f43142f912cda57cb8b3409bcd7d7e8e0565116
Jonathan Ringer <jonathan.ringer@comm.app>
2023-04-04 10:15:32 -0700

    [Identity] Support keygen and server setup for opaque 2.0
    Summary: Opaque 2.0 requires different setup for it to run.
    This adds support generating the secrets and adding the
    ServerSetup object to the configuration.
    
    Test Plan:
    ```
    cd services/identity
    cargo run -- keygen
    cargo run -- keygen # should skip writting files
    cargo run -- server
    ```
    
    Reviewers: varun, bartek
    
    Subscribers:

--- services/identity/src/config.rs
+++ services/identity/src/config.rs
@@ -1,11 +1,11 @@
 use curve25519_dalek::ristretto::RistrettoPoint;
 use once_cell::sync::Lazy;
 use opaque_ke::{errors::PakeError, keypair::KeyPair};
-use std::{env, fmt, fs, io, path::Path};
+use std::{env, fmt, fs, io, path, path::Path};
 
 use crate::constants::{
   AUTH_TOKEN, LOCALSTACK_ENDPOINT, SECRETS_DIRECTORY, SECRETS_FILE_EXTENSION,
-  SECRETS_FILE_NAME,
+  SECRETS_FILE_NAME, SECRETS_SETUP_FILE,
 };
 
 pub static CONFIG: Lazy<Config> =
@@ -17,10 +17,13 @@ pub(super) fn load_config() {
 
 #[derive(Clone)]
 pub struct Config {
+  // Opaque 1.2 server secrets
   pub server_keypair: KeyPair<RistrettoPoint>,
   // this is temporary, while the only authorized caller is ashoat's keyserver
   pub keyserver_auth_token: String,
   pub localstack_endpoint: Option<String>,
+  // Opaque 2.0 server secrets
+  pub server_setup: comm_opaque2::ServerSetup<comm_opaque2::Cipher>,
 }
 
 impl Config {
@@ -34,10 +37,16 @@ impl Config {
       env::var(AUTH_TOKEN).unwrap_or_else(|_| String::from("test"));
     let localstack_endpoint = env::var(LOCALSTACK_ENDPOINT).ok();
 
+    let mut path = path::PathBuf::new();
+    path.push(SECRETS_DIRECTORY);
+    path.push(SECRETS_SETUP_FILE);
+    let server_setup = get_server_setup_from_file(&path)?;
+
     Ok(Self {
       server_keypair,
       keyserver_auth_token,
       localstack_endpoint,
+      server_setup,
     })
   }
 }
@@ -52,12 +61,12 @@ impl fmt::Debug for Config {
   }
 }
 
-#[derive(
-  Debug, derive_more::Display, derive_more::From, derive_more::Error,
-)]
+#[derive(Debug, derive_more::Display, derive_more::From)]
 pub enum Error {
   #[display(...)]
   Pake(PakeError),
+  #[display(...)]
+  Opaque(comm_opaque2::ProtocolError),
   #[display(...)]
   IO(io::Error),
   #[display(...)]
@@ -70,4 +79,11 @@ fn get_keypair_from_file<P: AsRef<Path>>(
   let bytes = fs::read(path)?;
   KeyPair::from_private_key_slice(&bytes)
     .map_err(|e| Error::Pake(PakeError::CryptoError(e)))
+}
+
+fn get_server_setup_from_file<P: AsRef<Path>>(
+  path: &P,
+) -> Result<comm_opaque2::ServerSetup<comm_opaque2::Cipher>, Error> {
+  let bytes = fs::read(path)?;
+  comm_opaque2::ServerSetup::deserialize(&bytes).map_err(|e| Error::Opaque(e))
 }
--- services/identity/src/constants.rs
+++ services/identity/src/constants.rs
@@ -3,6 +3,7 @@
 pub const SECRETS_DIRECTORY: &str = "secrets";
 pub const SECRETS_FILE_NAME: &str = "secret_key";
 pub const SECRETS_FILE_EXTENSION: &str = "txt";
+pub const SECRETS_SETUP_FILE: &str = "server_setup.txt";
 
 // DynamoDB
 
