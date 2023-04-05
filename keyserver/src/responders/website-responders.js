f9140a8371bf0f7c37b12cb7e8ad074d95298aaf
Tomasz Palys <tomasz.palys@swmansion.com>
2023-04-05 15:07:10 +0200

    [keyserver] Create a page with invite link
    Summary:
    When a user doesn't have the app installed on iOS, clicking the invite link will open this page. User should use a link to App Store to install the app and click a link again to open the invitation.
    The design was based on https://linear.app/comm/issue/DES-49#comment-71838f8d but the values were guessed as only images were shared. They will be updated in next iterations.
    Community details section is just a placeholder as it requires some backend work before we can match a link with a community.
    The font will be updated in the next diff.
    
    {F463770}
    
    Depends on D7311
    
    Test Plan: Open the page on simulator and on a physical iPhone and check if links work correctly.
    
    Reviewers: bartek, kamil
    
    Subscribers: ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D7312

--- keyserver/src/responders/website-responders.js
+++ keyserver/src/responders/website-responders.js
@@ -453,18 +453,101 @@ function inviteResponder(req: $Request, res: $Response) {
   const { secret } = req.params;
   const userAgent = req.get('User-Agent');
   const detectionResult = detectBrowser(userAgent);
-  let redirectUrl = stores.appStoreUrl;
   if (detectionResult.os === 'Android OS') {
     const isSecretValid = inviteSecretRegex.test(secret);
     const referrer = isSecretValid
       ? `&referrer=${encodeURIComponent(`utm_source=invite/${secret}`)}`
       : '';
-    redirectUrl = `${stores.googlePlayUrl}${referrer}`;
+    const redirectUrl = `${stores.googlePlayUrl}${referrer}`;
+    res.writeHead(301, {
+      Location: redirectUrl,
+    });
+    res.end();
+  } else {
+    res.end(html`
+      <html lang="en">
+        <head>
+          <meta charset="utf-8" />
+          <title>Comm</title>
+          <style>
+            * {
+              -webkit-font-smoothing: antialiased;
+              -moz-osx-font-smoothing: grayscale;
+              box-sizing: border-box;
+              padding: 0;
+              margin: 0;
+            }
+
+            body {
+              background-color: #0a0a0a;
+              color: #ffffff;
+              display: flex;
+              flex-direction: column;
+              justify-content: space-between;
+              align-items: center;
+              height: 100%;
+              padding: 24px;
+              font-size: 1.6rem;
+              line-height: 1.5;
+            }
+
+            section {
+              display: flex;
+              flex-direction: column;
+              justify-content: space-between;
+              align-items: center;
+              width: 100%;
+              gap: 16px;
+            }
+
+            h1 {
+              font-size: 3.2rem;
+              line-height: 1.3;
+              margin-top: 32px;
+            }
+
+            .community {
+              background-color: #1f1f1f;
+              padding: 32px;
+              border-radius: 12px;
+            }
+
+            a {
+              all: unset;
+              box-sizing: border-box;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              width: 100%;
+              padding: 24px;
+              border-radius: 4px;
+              border: 1px solid transparent;
+              background-color: #7e57c2;
+            }
+
+            a.secondary {
+              background-color: #0a0a0a;
+              border-color: #808080;
+            }
+          </style>
+        </head>
+        <body>
+          <section>
+            <h1>Comm</h1>
+            <p>
+              To join this community, download the Comm app and reopen this
+              invite link
+            </p>
+          </section>
+          <section class="community">Community Details</section>
+          <section>
+            <a href="${stores.appStoreUrl}">Download Comm</a>
+            <a class="secondary" href="/invite/${secret}">Invite Link</a>
+          </section>
+        </body>
+      </html>
+    `);
   }
-  res.writeHead(301, {
-    Location: redirectUrl,
-  });
-  res.end();
 }
 
 export { websiteResponder, inviteResponder };
