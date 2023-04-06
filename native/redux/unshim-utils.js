27959733759b0b32ff93064bd538dcf668db647c
Ashoat Tevosyan <ashoat@comm.app>
2023-04-06 17:08:22 -0400

    [native] Extract unshimClientDB into function
    Summary: To reduce copy-paste I'm extracting this into a function.
    
    Test Plan: Flow
    
    Reviewers: atul, kamil, bartek, rohan
    
    Reviewed By: atul
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D7338

---
+++ native/redux/unshim-utils.js
@@ -0,0 +1,63 @@
+// @flow
+
+import { unshimFunc } from 'lib/shared/unshim-utils.js';
+import type {
+  MessageType,
+  ClientDBMessageStoreOperation,
+  ClientDBMessageInfo,
+} from 'lib/types/message-types.js';
+import {
+  translateClientDBMessageInfoToRawMessageInfo,
+  translateRawMessageInfoToClientDBMessageInfo,
+} from 'lib/utils/message-ops-utils.js';
+
+import type { AppState } from './state-types.js';
+import { commCoreModule } from '../native-modules.js';
+
+function unshimClientDB(
+  state: AppState,
+  unshimTypes: $ReadOnlyArray<MessageType>,
+): AppState {
+  // 1. Get messages from SQLite `messages` table.
+  const clientDBMessageInfos = commCoreModule.getAllMessagesSync();
+
+  // 2. Translate `ClientDBMessageInfo`s to `RawMessageInfo`s.
+  const rawMessageInfos = clientDBMessageInfos.map(
+    translateClientDBMessageInfoToRawMessageInfo,
+  );
+
+  // 3. "Unshim" translated `RawMessageInfo`s.
+  const unshimmedRawMessageInfos = rawMessageInfos.map(messageInfo =>
+    unshimFunc(messageInfo, new Set(unshimTypes)),
+  );
+
+  // 4. Translate unshimmed `RawMessageInfo`s back to `ClientDBMessageInfo`s.
+  const unshimmedClientDBMessageInfos = unshimmedRawMessageInfos.map(
+    translateRawMessageInfoToClientDBMessageInfo,
+  );
+
+  // 5. Construct `ClientDBMessageStoreOperation`s to clear SQLite `messages`
+  //    table and repopulate with unshimmed `ClientDBMessageInfo`s.
+  const operations: $ReadOnlyArray<ClientDBMessageStoreOperation> = [
+    {
+      type: 'remove_all',
+    },
+    ...unshimmedClientDBMessageInfos.map((message: ClientDBMessageInfo) => ({
+      type: 'replace',
+      payload: message,
+    })),
+  ];
+
+  // 6. Try processing `ClientDBMessageStoreOperation`s and log out if
+  //    `processMessageStoreOperationsSync(...)` throws an exception.
+  try {
+    commCoreModule.processMessageStoreOperationsSync(operations);
+  } catch (exception) {
+    console.log(exception);
+    return { ...state, cookie: null };
+  }
+
+  return state;
+}
+
+export { unshimClientDB };
