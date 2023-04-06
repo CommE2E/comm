750e68fa633a8cf24efcff54dbea411210301701
Michal Gniadek <michal.gniadek@swmansion.com>
2023-04-06 15:12:58 +0200

    [desktop] Type windows notifs module
    Summary: Added types to the native notifs module based on the typescript types generated by NodeRT and on the Windows API documentation.
    
    Test Plan: Run flow in desktop
    
    Reviewers: kamil, tomek
    
    Reviewed By: tomek
    
    Subscribers: ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D7326

--- desktop/flow-typed/@commapp/windowspush_vx.x.x.js
+++ desktop/flow-typed/@commapp/windowspush_vx.x.x.js
@@ -1,5 +1,100 @@
 // @flow
 
 declare module '@commapp/windowspush' {
-  declare module.exports: any;
+  declare export class PushNotificationCreateChannelStatus {
+    +status: PushNotificationChannelStatus;
+    +extendedError: number;
+    +retryCount: number;
+  }
+
+  declare export class PushNotificationsContract {}
+
+  declare export type PushNotificationChannelStatus = {
+    +inProgress: 0,
+    +inProgressRetry: 1,
+    +completedSuccess: 2,
+    +completedFailure: 3,
+  };
+
+  declare export class PushNotificationChannel {
+    +expirationTime: Date;
+    +uri: string;
+
+    close(): void;
+  }
+
+  declare export class PushNotificationCreateChannelResult {
+    +channel: PushNotificationChannel;
+    +extendedError: number;
+    +status: $Values<PushNotificationChannelStatus>;
+  }
+
+  declare export class PushNotificationManager {
+    static +default: PushNotificationManager;
+
+    static isSupported(): boolean;
+
+    createChannelAsync(
+      remoteId: string,
+      callback: (
+        error: Error,
+        result: PushNotificationCreateChannelResult,
+      ) => void,
+    ): void;
+
+    register(): void;
+
+    unregister(): void;
+
+    unregisterAll(): void;
+
+    addListener(
+      type: 'PushReceived',
+      listener: (
+        manager: PushNotificationManager,
+        args: PushNotificationReceivedEventArgs,
+      ) => void,
+    ): void;
+    removeListener(
+      type: 'PushReceived',
+      listener: (
+        manager: PushNotificationManager,
+        args: PushNotificationReceivedEventArgs,
+      ) => void,
+    ): void;
+    on(
+      type: 'PushReceived',
+      listener: (
+        manager: PushNotificationManager,
+        args: PushNotificationReceivedEventArgs,
+      ) => void,
+    ): void;
+    off(
+      type: 'PushReceived',
+      listener: (
+        manager: PushNotificationManager,
+        args: PushNotificationReceivedEventArgs,
+      ) => void,
+    ): void;
+  }
+
+  declare export class PushNotificationReceivedEventArgs {
+    +payload: {
+      +[number]: number,
+      +length: number,
+    };
+
+    getDeferral(): Object;
+
+    addListener(
+      type: 'Canceled',
+      listener: (sender: any, reason: any) => void,
+    ): void;
+    removeListener(
+      type: 'Canceled',
+      listener: (sender: any, reason: any) => void,
+    ): void;
+    on(type: 'Canceled', listener: (sender: any, reason: any) => void): void;
+    off(type: 'Canceled', listener: (sender: any, reason: any) => void): void;
+  }
 }
