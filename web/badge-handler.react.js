acbb3e34671c63775ddd79713b3d194ce50f4664
Michal Gniadek <michal.gniadek@swmansion.com>
2023-04-06 16:13:51 +0200

    [web] Only update badge when connected
    Summary:
    Part of [ENG-3350](https://linear.app/comm/issue/ENG-3350/).
    Changed the tab title/ icon badge logic on web to update only when websocket is connected (otherwise the `unreadCound` could be outdated). Additionaly we update the badge immediately when connecting to the websocket. The logic is identical to the one on native (`native/push/push-handler.react.js:226-232`);
    
    Depends on D7327
    
    Test Plan:
    Open the web app in the browser and in the desktop app (without notifs). Check if sending a message updated the tab title (browser)/ badge (desktop), and reading it removes them.
    
    Reviewers: kamil, inka
    
    Subscribers:

--- web/badge-handler.react.js
+++ web/badge-handler.react.js
@@ -9,12 +9,25 @@ import electron from './electron.js';
 import getTitle from './title/getTitle.js';
 
 function useBadgeHandler() {
+  const connection = useSelector(state => state.connection);
+  const prevConnection = React.useRef(connection);
+
   const boundUnreadCount = useSelector(unreadCount);
+  const prevUnreadCount = React.useRef(boundUnreadCount);
 
   React.useEffect(() => {
-    document.title = getTitle(boundUnreadCount);
-    electron?.setBadge(boundUnreadCount === 0 ? null : boundUnreadCount);
-  }, [boundUnreadCount]);
+    if (
+      connection.status === 'connected' &&
+      (prevConnection.current.status !== 'connected' ||
+        boundUnreadCount !== prevUnreadCount.current)
+    ) {
+      document.title = getTitle(boundUnreadCount);
+      electron?.setBadge(boundUnreadCount === 0 ? null : boundUnreadCount);
+    }
+
+    prevConnection.current = connection;
+    prevUnreadCount.current = boundUnreadCount;
+  }, [boundUnreadCount, connection]);
 }
 
 export default useBadgeHandler;
