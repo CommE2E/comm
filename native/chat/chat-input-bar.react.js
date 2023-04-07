641ba7af7a1ae91c3656af5bfdccbcab96f6d81a
kosmydel <kukubaczek@gmail.com>
2023-04-07 14:18:53 +0200

    [native] Preserve the draft for ChatInputBar after leaving edit mode
    Summary: We want to preserve user input after exiting edit mode. If we are in edit mode we don't override the draft. After exiting it we want to restore the previous content. We need a callback, to always have the latest state.
    
    Test Plan: Run the app on iOS. Typed some messages in the input bar. Entered edit mode. Changed the content of the message in the input bar. Pressed cross to exit edit mode. The draft was restored correctly.
    
    Reviewers: michal, inka, ginsu
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7347

--- native/chat/chat-input-bar.react.js
+++ native/chat/chat-input-bar.react.js
@@ -690,6 +690,9 @@ class ChatInputBar extends React.PureComponent<Props, State> {
 
   updateText = (text: string) => {
     this.setState({ text, textEdited: true });
+    if (this.isEditMode()) {
+      return;
+    }
     this.saveDraft(text);
   };
 
@@ -788,7 +791,15 @@ class ChatInputBar extends React.PureComponent<Props, State> {
   };
 
   onPressExitEditMode = () => {
-    this.props.inputState?.setEditedMessage(null);
+    this.exitEditMode();
+  };
+
+  exitEditMode = () => {
+    this.props.inputState?.setEditedMessage(null, () => {
+      this.updateText(this.props.draft);
+      this.focusAndUpdateButtonsVisibility();
+      this.updateSendButton(this.props.draft);
+    });
   };
 
   onPressJoin = () => {
