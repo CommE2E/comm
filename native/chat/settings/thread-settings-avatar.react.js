f28a3b11d0c7893ea7fd7bb447e27537fa365c47
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-04-10 15:17:18 -0400

    [native] add edit avatar badge to thread avatar
    Summary: add edit avatar badge to thread avatar. Most of this code was ported over from `profile-screen.react.js`; however, with thread avatars we need to make sure that this badge renders conditonally, since there are some threads where the user should not be allowed to edit thread avatar
    
    Test Plan: Please see the screenshots to see the changes I made
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7378

--- native/chat/settings/thread-settings-avatar.react.js
+++ native/chat/settings/thread-settings-avatar.react.js
@@ -1,22 +1,62 @@
 // @flow
 
 import * as React from 'react';
-import { View } from 'react-native';
+import { View, TouchableWithoutFeedback } from 'react-native';
 
 import { type ResolvedThreadInfo } from 'lib/types/thread-types.js';
 
+import SWMansionIcon from '../../components/swmansion-icon.react.js';
 import ThreadAvatar from '../../components/thread-avatar.react.js';
-import { useStyles } from '../../themes/colors.js';
+import { useColors, useStyles } from '../../themes/colors.js';
 
 type Props = {
   +threadInfo: ResolvedThreadInfo,
+  +canChangeSettings: boolean,
 };
 function ThreadSettingsAvatar(props: Props): React.Node {
+  const { threadInfo, canChangeSettings } = props;
+
+  const colors = useColors();
   const styles = useStyles(unboundStyles);
+
+  const onPressEditAvatar = React.useCallback(() => {
+    // TODO:
+    // Display action sheet with all the different avatar creation options
+  }, []);
+
+  const editBadge = React.useMemo(() => {
+    if (!canChangeSettings) {
+      return null;
+    }
+
+    return (
+      <View style={styles.editAvatarIconContainer}>
+        <SWMansionIcon
+          name="edit-2"
+          size={16}
+          style={styles.editAvatarIcon}
+          color={colors.floatingButtonLabel}
+        />
+      </View>
+    );
+  }, [
+    canChangeSettings,
+    colors.floatingButtonLabel,
+    styles.editAvatarIcon,
+    styles.editAvatarIconContainer,
+  ]);
 
   return (
     <View style={styles.container}>
-      <ThreadAvatar size="profile" threadInfo={props.threadInfo} />
+      <TouchableWithoutFeedback
+        onPress={onPressEditAvatar}
+        disabled={!canChangeSettings}
+      >
+        <View>
+          <ThreadAvatar size="profile" threadInfo={threadInfo} />
+          {editBadge}
+        </View>
+      </TouchableWithoutFeedback>
     </View>
   );
 }
@@ -27,6 +67,21 @@ const unboundStyles = {
     backgroundColor: 'panelForeground',
     flex: 1,
     paddingVertical: 16,
+  },
+  editAvatarIconContainer: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    borderWidth: 2,
+    borderColor: 'panelForeground',
+    borderRadius: 18,
+    width: 36,
+    height: 36,
+    backgroundColor: 'purpleButton',
+    justifyContent: 'center',
+  },
+  editAvatarIcon: {
+    textAlign: 'center',
   },
 };
 
