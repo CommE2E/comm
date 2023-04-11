497ad22e1484dae0749fa7dbfc564901dff0378b
Jonathan Ringer <jonringer117@gmail.com>
2023-04-11 14:44:57 -0700

    [Tunnelbroker] Create websocket server
    Summary:
    Keyservers and in the future, clients, will need to
    open a websocket to connect with tunnelbroker. This adds
    the initial websocket server and enables logging in rust code.
    
    Depends on D7391
    
    Test Plan:
    ```
    cd services/tunnelbroker
    cargo run # should emit log saying that it's bound to port
    ```
    
    Reviewers: varun, bartek, max
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7392

--- services/tunnelbroker/src/main.rs
+++ services/tunnelbroker/src/main.rs
@@ -2,10 +2,16 @@ pub mod constants;
 pub mod cxx_bridge;
 pub mod notifications;
 pub mod server;
-use anyhow::Result;
+mod websockets;
+use std::io;
+use tracing;
 
 #[tokio::main]
-async fn main() -> Result<()> {
+async fn main() -> Result<(), io::Error> {
+  let subscriber = tracing_subscriber::FmtSubscriber::new();
+  tracing::subscriber::set_global_default(subscriber)
+    .expect("Unable to configure tracing");
+
   cxx_bridge::ffi::initialize();
-  server::run_grpc_server().await
+  websockets::create_server().await
 }
---
+++ services/tunnelbroker/src/websockets/mod.rs
@@ -0,0 +1,22 @@
+use std::{env, io::Error};
+
+use tokio::net::{TcpListener, TcpStream};
+use tracing::info;
+
+pub async fn create_server() -> Result<(), Error> {
+  let addr = env::var("TUNNELBROKER_WEBSOCKET_ADDR")
+    .unwrap_or_else(|_| "127.0.0.1:51001".to_string());
+
+  let listener = TcpListener::bind(&addr).await.expect("Failed to bind");
+  info!("Listening on: {}", addr);
+
+  while let Ok((stream, _)) = listener.accept().await {
+    tokio::spawn(accept_connection(stream));
+  }
+
+  Ok(())
+}
+
+async fn accept_connection(_stream: TcpStream) {
+  unimplemented!()
+}
