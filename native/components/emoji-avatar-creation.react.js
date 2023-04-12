89edb8dcd31cf7b189b3cd045cf9500b9c6fa2d1
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-04-12 19:04:54 -0400

    [native] introduce useSaveUserAvatar hook
    Summary: To better prepare the `EmojiAvatarCreation` component to handle both user and thread avatars I want to factor out this User avatar specfic function into a hook in the avatar util file so that the component won't be as cluttered when we extend to thread avatars
    
    Test Plan: `flow` and I am still able to save my avatar with the expected behavoir
    
    Reviewers:
    
    Subscribers:

--- native/components/emoji-avatar-creation.react.js
+++ native/components/emoji-avatar-creation.react.js
@@ -6,28 +6,20 @@ import {
   Text,
   TouchableWithoutFeedback,
   ActivityIndicator,
-  Alert,
 } from 'react-native';
 import EmojiPicker from 'rn-emoji-keyboard';
 
-import {
-  updateUserAvatar,
-  updateUserAvatarActionTypes,
-} from 'lib/actions/user-actions.js';
+import { updateUserAvatarActionTypes } from 'lib/actions/user-actions.js';
 import { createLoadingStatusSelector } from 'lib/selectors/loading-selectors.js';
 import { savedEmojiAvatarSelectorForCurrentUser } from 'lib/selectors/user-selectors.js';
 import type { ClientEmojiAvatar } from 'lib/types/avatar-types.js';
-import {
-  useDispatchActionPromise,
-  useServerCall,
-} from 'lib/utils/action-utils.js';
 
 import Avatar from '../components/avatar.react.js';
 import Button from '../components/button.react.js';
 import ColorRows from '../components/color-rows.react.js';
-import { displayActionResultModal } from '../navigation/action-result-modal.js';
 import { useSelector } from '../redux/redux-utils.js';
 import { useStyles } from '../themes/colors.js';
+import { useSaveUserAvatar } from '../utils/avatar-utils.js';
 
 const loadingStatusSelector = createLoadingStatusSelector(
   updateUserAvatarActionTypes,
@@ -49,8 +41,7 @@ function EmojiAvatarCreation(): React.Node {
 
   const styles = useStyles(unboundStyles);
 
-  const dispatchActionPromise = useDispatchActionPromise();
-  const callUpdateUserAvatar = useServerCall(updateUserAvatar);
+  const saveUserAvatar = useSaveUserAvatar();
 
   const saveAvatarCallLoading = useSelector(
     state => loadingStatusSelector(state) === 'loading',
@@ -67,27 +58,8 @@ function EmojiAvatarCreation(): React.Node {
       color: pendingColor,
     };
 
-    const saveAvatarPromise = (async () => {
-      try {
-        const response = await callUpdateUserAvatar(newEmojiAvatarRequest);
-        displayActionResultModal('Avatar updated!');
-
-        return response;
-      } catch (e) {
-        Alert.alert(
-          'Couldn’t save avatar',
-          'Please try again later',
-          [{ text: 'OK' }],
-          {
-            cancelable: true,
-          },
-        );
-        throw e;
-      }
-    })();
-
-    dispatchActionPromise(updateUserAvatarActionTypes, saveAvatarPromise);
-  }, [callUpdateUserAvatar, dispatchActionPromise, pendingColor, pendingEmoji]);
+    saveUserAvatar(newEmojiAvatarRequest);
+  }, [pendingColor, pendingEmoji, saveUserAvatar]);
 
   const onPressReset = React.useCallback(() => {
     const resetEmojiAvatar = savedEmojiAvatarFunc();
--- native/utils/avatar-utils.js
+++ native/utils/avatar-utils.js
@@ -1,14 +1,60 @@
 // @flow
 
 import * as React from 'react';
+import { Alert } from 'react-native';
+
+import {
+  updateUserAvatar,
+  updateUserAvatarActionTypes,
+} from 'lib/actions/user-actions.js';
+import type { ClientEmojiAvatar } from 'lib/types/avatar-types.js';
+import {
+  useServerCall,
+  useDispatchActionPromise,
+} from 'lib/utils/action-utils.js';
 
 import { FeatureFlagsContext } from '../components/feature-flags-provider.react.js';
+import { displayActionResultModal } from '../navigation/action-result-modal.js';
 
 function useShouldRenderAvatars(): boolean {
   const { configuration: featureFlagConfig } =
     React.useContext(FeatureFlagsContext);
 
-  return !!featureFlagConfig['AVATARS_DISPLAY'];
+  return !featureFlagConfig['AVATARS_DISPLAY'];
 }
 
-export { useShouldRenderAvatars };
+function useSaveUserAvatar(): (
+  newEmojiAvatarRequest: ClientEmojiAvatar,
+) => mixed {
+  const callUpdateUserAvatar = useServerCall(updateUserAvatar);
+  const dispatchActionPromise = useDispatchActionPromise();
+
+  return React.useCallback(
+    newEmojiAvatarRequest => {
+      console.log(newEmojiAvatarRequest);
+      const saveAvatarPromise = (async () => {
+        try {
+          const response = await callUpdateUserAvatar(newEmojiAvatarRequest);
+          displayActionResultModal('Avatar updated!');
+
+          return response;
+        } catch (e) {
+          Alert.alert(
+            'Couldn’t save avatar',
+            'Please try again later',
+            [{ text: 'OK' }],
+            {
+              cancelable: true,
+            },
+          );
+          throw e;
+        }
+      })();
+
+      dispatchActionPromise(updateUserAvatarActionTypes, saveAvatarPromise);
+    },
+    [callUpdateUserAvatar, dispatchActionPromise],
+  );
+}
+
+export { useShouldRenderAvatars, useSaveUserAvatar };
