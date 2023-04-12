34d11786c9348fb4b331a8daaeb6e9068759c39e
Jonathan Ringer <jonringer117@gmail.com>
2023-04-12 13:27:58 -0700

    [Keyserver/rust-node-addon] Remove tunnelbroker grpc client
    Summary:
    We are transitioning from grpc based communication to websockets for
    bidi communications.
    
    Remove existing grpc code for keyserver to talk to tunnelbroker.
    
    Part of https://linear.app/comm/issue/ENG-3660
    
    Test Plan: Did `yarn cleaninstall`, then did a iOS register new user.
    
    Reviewers: ashoat, max, bartek
    
    Reviewed By: ashoat
    
    Subscribers: tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7405

--- keyserver/addons/rust-node-addon/src/lib.rs
+++ keyserver/addons/rust-node-addon/src/lib.rs
@@ -1,5 +1,4 @@
 pub mod identity_client;
-pub mod tunnelbroker_client;
 
 #[macro_use]
 extern crate napi_derive;
--- shared/tunnelbroker-client/build.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-  tonic_build::compile_protos("../protos/tunnelbroker.proto")
-    .unwrap_or_else(|e| panic!("Failed to compile proto {:?}", e));
-  println!("cargo:rerun-if-changed=../protos/tunnelbroker.proto");
-}
