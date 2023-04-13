cb71a00c0c0b54493bbe84cf5fd6b55105e5a9cd
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-04-13 18:14:13 -0400

    [native] introduce EditAvatar component
    Summary: `EditAvatar` is a component that handles all the common logic between editing a user and thread avatar. Following diffs will handle the introduction of the logic for things like the action sheet, image picker, camera modal, loading, etc. in this component
    
    Test Plan: Please watch the demo video to see the changes I made
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7438

--- native/chat/settings/thread-settings-avatar.react.js
+++ native/chat/settings/thread-settings-avatar.react.js
@@ -1,13 +1,15 @@
 // @flow
 
+import { useNavigation } from '@react-navigation/native';
 import * as React from 'react';
-import { View, TouchableWithoutFeedback } from 'react-native';
+import { View } from 'react-native';
 
 import { type ResolvedThreadInfo } from 'lib/types/thread-types.js';
 
-import SWMansionIcon from '../../components/swmansion-icon.react.js';
+import EditAvatar from '../../components/edit-avatar.react.js';
 import ThreadAvatar from '../../components/thread-avatar.react.js';
-import { useColors, useStyles } from '../../themes/colors.js';
+import { EmojiAvatarCreationRouteName } from '../../navigation/route-names.js';
+import { useStyles } from '../../themes/colors.js';
 
 type Props = {
   +threadInfo: ResolvedThreadInfo,
@@ -16,47 +18,28 @@ type Props = {
 function ThreadSettingsAvatar(props: Props): React.Node {
   const { threadInfo, canChangeSettings } = props;
 
-  const colors = useColors();
+  const { navigate } = useNavigation();
+
   const styles = useStyles(unboundStyles);
 
-  const onPressEditAvatar = React.useCallback(() => {
-    // TODO:
-    // Display action sheet with all the different avatar creation options
-  }, []);
-
-  const editBadge = React.useMemo(() => {
-    if (!canChangeSettings) {
-      return null;
-    }
-
-    return (
-      <View style={styles.editAvatarIconContainer}>
-        <SWMansionIcon
-          name="edit-2"
-          size={16}
-          style={styles.editAvatarIcon}
-          color={colors.floatingButtonLabel}
-        />
-      </View>
-    );
-  }, [
-    canChangeSettings,
-    colors.floatingButtonLabel,
-    styles.editAvatarIcon,
-    styles.editAvatarIconContainer,
-  ]);
+  const onPressEmojiAvatarFlow = React.useCallback(() => {
+    navigate<'EmojiAvatarCreation'>({
+      name: EmojiAvatarCreationRouteName,
+      params: {
+        threadID: threadInfo.id,
+        containingThreadID: threadInfo.containingThreadID,
+      },
+    });
+  }, [navigate, threadInfo.containingThreadID, threadInfo.id]);
 
   return (
     <View style={styles.container}>
-      <TouchableWithoutFeedback
-        onPress={onPressEditAvatar}
+      <EditAvatar
+        onPressEmojiAvatarFlow={onPressEmojiAvatarFlow}
         disabled={!canChangeSettings}
       >
-        <View>
-          <ThreadAvatar size="profile" threadInfo={threadInfo} />
-          {editBadge}
-        </View>
-      </TouchableWithoutFeedback>
+        <ThreadAvatar size="profile" threadInfo={threadInfo} />
+      </EditAvatar>
     </View>
   );
 }
@@ -67,21 +50,6 @@ const unboundStyles = {
     backgroundColor: 'panelForeground',
     flex: 1,
     paddingVertical: 16,
-  },
-  editAvatarIconContainer: {
-    position: 'absolute',
-    bottom: 0,
-    right: 0,
-    borderWidth: 2,
-    borderColor: 'panelForeground',
-    borderRadius: 18,
-    width: 36,
-    height: 36,
-    backgroundColor: 'purpleButton',
-    justifyContent: 'center',
-  },
-  editAvatarIcon: {
-    textAlign: 'center',
   },
 };
 
