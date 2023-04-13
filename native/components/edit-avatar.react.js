4f5d90c5b8cd2c579920b5ae7c1939e367bee66f
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-04-13 18:14:13 -0400

    [native] introduce EditAvatar component
    Summary: `EditAvatar` is a component that handles all the common logic between editing a user and thread avatar. Following diffs will handle the introduction of the logic for things like the action sheet, image picker, camera modal, loading, etc. in this component
    
    Test Plan: Please watch the demo video to see the changes I made
    
    Reviewers:
    
    Subscribers:

---
+++ native/components/edit-avatar.react.js
@@ -0,0 +1,73 @@
+// @flow
+
+import * as React from 'react';
+import { View, TouchableWithoutFeedback } from 'react-native';
+
+import SWMansionIcon from '../components/swmansion-icon.react.js';
+import { useColors, useStyles } from '../themes/colors.js';
+
+type Props = {
+  +children: React.Node, // also could maybe make this children?
+  +onPressEmojiAvatarFlow: () => mixed,
+  +disabled?: boolean,
+};
+function EditAvatar(props: Props): React.Node {
+  const { onPressEmojiAvatarFlow, children, disabled } = props;
+
+  const colors = useColors();
+  const styles = useStyles(unboundStyles);
+
+  const editBadge = React.useMemo(() => {
+    if (disabled) {
+      return null;
+    }
+
+    return (
+      <View style={styles.editAvatarIconContainer}>
+        <SWMansionIcon
+          name="edit-2"
+          size={16}
+          style={styles.editAvatarIcon}
+          color={colors.floatingButtonLabel}
+        />
+      </View>
+    );
+  }, [
+    colors.floatingButtonLabel,
+    disabled,
+    styles.editAvatarIcon,
+    styles.editAvatarIconContainer,
+  ]);
+
+  return (
+    <TouchableWithoutFeedback
+      onPress={onPressEmojiAvatarFlow}
+      disabled={disabled}
+    >
+      <View>
+        {children}
+        {editBadge}
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const unboundStyles = {
+  editAvatarIconContainer: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    borderWidth: 2,
+    borderColor: 'panelForeground',
+    borderRadius: 18,
+    width: 36,
+    height: 36,
+    backgroundColor: 'purpleButton',
+    justifyContent: 'center',
+  },
+  editAvatarIcon: {
+    textAlign: 'center',
+  },
+};
+
+export default EditAvatar;
