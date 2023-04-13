53afae3de8d42e260e9d32d3dd64f530ecb43189
Jonathan Ringer <jonringer117@gmail.com>
2023-04-13 11:50:25 -0700

    [Tunnelbroker] Use comm services protobuf
    Summary:
    Use the services+tunnelbroker protobuf, and create a grpc
    server for incoming connections.
    
    part of https://linear.app/comm/issue/ENG-3681
    
    Test Plan:
    ```
    cd services/tunnelbroker
    cargo run # Should see logs about server running
    ```
    
    Reviewers: varun, bartek, max
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7431

--- services/tunnelbroker/build.rs
+++ services/tunnelbroker/build.rs
@@ -1,1 +1,10 @@
-fn main() {}
+fn main() {
+  tonic_build::configure()
+    .build_server(true)
+    .build_client(false)
+    .compile(
+      &["../../shared/protos/tunnelbroker.proto"],
+      &["../../shared/protos/"],
+    )
+    .expect("Failed to compile protobuf file");
+}
---
+++ services/tunnelbroker/src/comm_server/mod.rs
@@ -0,0 +1,37 @@
+mod proto {
+  tonic::include_proto!("identity.tunnelbroker");
+}
+
+use proto::comm_tunnelbroker_service_server::{
+  CommTunnelbrokerService, CommTunnelbrokerServiceServer,
+};
+use tonic::transport::Server;
+
+use crate::constants;
+
+#[derive(Debug, Default)]
+struct CommService {}
+
+#[tonic::async_trait]
+impl CommTunnelbrokerService for CommService {
+  async fn send_message_to_device(
+    &self,
+    _request: tonic::Request<proto::MessageToDevice>,
+  ) -> Result<tonic::Response<proto::Empty>, tonic::Status> {
+    unimplemented!()
+  }
+}
+
+pub async fn run_identity_server() -> Result<(), tonic::transport::Error> {
+  let addr = format!("[::1]:{}", constants::GRPC_SERVER_PORT)
+    .parse()
+    .expect("Unable to parse identity address");
+
+  tracing::info!("Websocket server listening on {}", &addr);
+  Server::builder()
+    .http2_keepalive_interval(Some(constants::GRPC_KEEP_ALIVE_PING_INTERVAL))
+    .http2_keepalive_timeout(Some(constants::GRPC_KEEP_ALIVE_PING_TIMEOUT))
+    .add_service(CommTunnelbrokerServiceServer::new(CommService::default()))
+    .serve(addr)
+    .await
+}
