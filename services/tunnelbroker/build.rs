933ce5fa8abc6931d76a18ccf900691f9800ba58
Jonathan Ringer <jonringer117@gmail.com>
2023-04-13 11:50:25 -0700

    [Tunnelbroker] Use comm services protobuf
    Summary:
    Use the services+tunnelbroker protobuf, and create a grpc
    server for incoming connections.
    
    part of https://linear.app/comm/issue/ENG-3681
    
    Test Plan:
    ```
    cd services/tunnelbroker
    cargo run # Should see logs about server running
    ```
    
    Reviewers: varun, bartek, max
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7431

--- services/tunnelbroker/build.rs
+++ services/tunnelbroker/build.rs
@@ -1,1 +1,10 @@
-fn main() {}
+fn main() {
+  tonic_build::configure()
+    .build_server(true)
+    .build_client(false)
+    .compile(
+      &["../../shared/protos/identity_tunnelbroker.proto"],
+      &["../../shared/protos/"],
+    )
+    .expect("Failed to compile protobuf file");
+}
