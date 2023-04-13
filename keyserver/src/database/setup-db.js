73dd0ebae86f1204f9e81cd16062edc27cee5e71
marcinwasowicz <marcinkwasowicz@gmail.com>
2023-04-13 17:21:50 +0200

    Add table to store olm notifications sessions
    Summary: This differential adds new table to MariaDB database on the keyserver to store olm sessions for encrypted notifications.
    
    Test Plan: to be updated in GUI
    
    Reviewers: kamil, atul, inka, tomek, bartek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D7463

--- keyserver/src/database/setup-db.js
+++ keyserver/src/database/setup-db.js
@@ -249,6 +249,12 @@ async function createTables() {
         processed_content mediumtext COLLATE utf8mb4_bin
       ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
+      CREATE TABLE keyserver_hosted_olm_sessions (
+        cookie_id bigint(20) NOT NULL,
+        is_primary tinyint(1) NOT NULL,
+        pickled_olm_session text CHARACTER SET ascii COLLATE ascii_bin DEFAULT NULL
+      ) ENGINE=InnoDB DEFAULT CHARSET=ascii COLLATE=ascii_bin;
+
       ALTER TABLE cookies
         ADD PRIMARY KEY (id),
         ADD UNIQUE KEY device_token (device_token(512)),
@@ -368,6 +374,9 @@ async function createTables() {
       ALTER TABLE message_search
         ADD PRIMARY KEY (original_message_id),
         ADD FULLTEXT INDEX processed_content (processed_content);
+
+      ALTER TABLE keyserver_hosted_olm_sessions
+        ADD PRIMARY KEY (cookie_id, is_primary);
     `,
     { multipleStatements: true },
   );
