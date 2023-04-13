42addc2abaa863df746f2aa7dff2c205a6dc6e6e
xsanm <kkurowski.kk@gmail.com>
2023-04-13 16:57:12 +0200

    [native] extract processing store operations to separate function
    Summary:
    In future there will be a need to call this from more places - to be precise [here](https://github.com/CommE2E/comm/blob/36ec83e43d51dbd32766b28d1523c08ecfaf754a/native/redux/redux-setup.js#L274).
    This action can not be moved lower in file to use the same code as previously as it's only-native and should be finalized before base reducer.
    
    Test Plan: Check if operations are processed properly
    
    Reviewers: tomek, marcin
    
    Reviewed By: tomek
    
    Subscribers: ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D7421

--- native/redux/redux-utils.js
+++ native/redux/redux-utils.js
@@ -2,7 +2,13 @@
 
 import { useSelector as reactReduxUseSelector } from 'react-redux';
 
+import type { StoreOperations } from 'lib/types/store-ops-types.js';
+import { convertMessageStoreOperationsToClientDBOperations } from 'lib/utils/message-ops-utils.js';
+import { convertThreadStoreOperationsToClientDBOperations } from 'lib/utils/thread-ops-utils.js';
+
 import type { AppState } from './state-types.js';
+import { commCoreModule } from '../native-modules.js';
+import { isTaskCancelledError } from '../utils/error-handling.js';
 
 function useSelector<SS>(
   selector: (state: AppState) => SS,
@@ -11,4 +17,51 @@ function useSelector<SS>(
   return reactReduxUseSelector(selector, equalityFn);
 }
 
-export { useSelector };
+async function processDBStoreOperations(
+  storeOperations: StoreOperations,
+): Promise<void> {
+  const {
+    draftStoreOperations,
+    threadStoreOperations,
+    messageStoreOperations,
+  } = storeOperations;
+
+  const convertedThreadStoreOperations =
+    convertThreadStoreOperationsToClientDBOperations(threadStoreOperations);
+  const convertedMessageStoreOperations =
+    convertMessageStoreOperationsToClientDBOperations(messageStoreOperations);
+
+  try {
+    const promises = [];
+    if (convertedThreadStoreOperations.length > 0) {
+      promises.push(
+        commCoreModule.processThreadStoreOperations(
+          convertedThreadStoreOperations,
+        ),
+      );
+    }
+    if (convertedMessageStoreOperations.length > 0) {
+      promises.push(
+        commCoreModule.processMessageStoreOperations(
+          convertedMessageStoreOperations,
+        ),
+      );
+    }
+    if (draftStoreOperations.length > 0) {
+      promises.push(
+        commCoreModule.processDraftStoreOperations(draftStoreOperations),
+      );
+    }
+    await Promise.all(promises);
+  } catch (e) {
+    if (isTaskCancelledError(e)) {
+      return;
+    }
+    // this code will make an entry in SecureStore and cause re-creating
+    // database when user will open app again
+    commCoreModule.reportDBOperationsFailure();
+    commCoreModule.terminate();
+  }
+}
+
+export { useSelector, processDBStoreOperations };
