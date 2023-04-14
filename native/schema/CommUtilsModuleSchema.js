d2bdc31faaf0e5bb87c5ee739383d50583e6cb76
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2023-04-14 13:52:59 +0200

    [native] Add CommUtilsModule spec
    Summary: As a part of [[ https://linear.app/comm/issue/ENG-3408/fix-file-encryption-performance-on-native | ENG-3408 ]] I decided to introduce a separate TurboModule for C++ utilities to avoid bloating CommCoreModule.
    
    Test Plan: This diff is tested together with the child one that runs jsi-codegen.
    
    Reviewers: ashoat, atul, marcin, kamil
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D7449

---
+++ native/schema/CommUtilsModuleSchema.js
@@ -0,0 +1,26 @@
+// @flow
+
+'use strict';
+
+import { TurboModuleRegistry } from 'react-native';
+import type { TurboModule } from 'react-native/Libraries/TurboModule/RCTExport.js';
+
+// codegen doesn't understand ArrayBuffers, so we need to map them to Objects
+type JSIArrayBuffer = Object;
+interface Spec extends TurboModule {
+  // filesystem utils
+  +writeBufferToFile: (path: string, data: JSIArrayBuffer) => Promise<void>;
+  +readBufferFromFile: (path: string) => Promise<JSIArrayBuffer>;
+  +base64EncodeBuffer: (data: JSIArrayBuffer) => string;
+}
+
+// for public interface, we use the correct types
+export interface UtilsModuleSpec {
+  +writeBufferToFile: (path: string, data: ArrayBuffer) => Promise<void>;
+  +readBufferFromFile: (path: string) => Promise<ArrayBuffer>;
+  +base64EncodeBuffer: (data: ArrayBuffer) => string;
+}
+
+export default (TurboModuleRegistry.getEnforcing<Spec>(
+  'CommUtilsTurboModule',
+): Spec);
