3639c3eee8c052455b0fad5106fa5d6c3c81a221
kosmydel <kukubaczek@gmail.com>
2023-04-14 15:47:54 +0200

    [native] Handle entering edit mode when already in edit mode
    Summary: We want to prevent a user from losing edited changes when entering edit mode for another message. Added displaying confirmation alert. For now, it doesn't check if there are some changes, which can be added later.
    
    Test Plan: Turned on the app on iOS. Entered edit mode. Checked if the edit button doesn't show anymore for the currently edited message. Tried to edit another message. The confirmation alert appeared.
    
    Reviewers: michal, inka
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7520

--- native/utils/edit-messages-utils.js
+++ native/utils/edit-messages-utils.js
@@ -1,6 +1,7 @@
 // @flow
 
 import * as React from 'react';
+import Alert from 'react-native/Libraries/Alert/Alert.js';
 
 import { FeatureFlagsContext } from '../components/feature-flags-provider.react.js';
 
@@ -11,4 +12,20 @@ function useShouldRenderEditButton(): boolean {
   return !!featureFlagConfig['EDIT_BUTTON_DISPLAY'];
 }
 
-export { useShouldRenderEditButton };
+function exitEditAlert(onDiscard: () => void): void {
+  Alert.alert('Are you sure?', 'Your edits will be discarded.', [
+    {
+      text: 'Continue editing',
+      style: 'cancel',
+    },
+    {
+      text: 'Discard edit',
+      style: 'destructive',
+      onPress: () => {
+        onDiscard();
+      },
+    },
+  ]);
+}
+
+export { useShouldRenderEditButton, exitEditAlert };
