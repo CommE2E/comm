ab9a72f659a5f82c7e9a3d9c27f4e0c1de26739e
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-04-16 13:31:03 -0400

    [native] add safe area insets for android action sheet
    Summary: Another thing I encountered when testing the action sheet for the edit avatar flow on android is that if the nav buttons are visible then it partly covers the most bottom option on the action sheet. To fix this I used `SafeAreaContext` to set the `paddingBottom` of the action sheet which fixes the issue.
    
    Test Plan: Please check out the screenshots to see the changes I made
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7457

--- native/tooltip/tooltip-context.react.js
+++ native/tooltip/tooltip-context.react.js
@@ -3,6 +3,7 @@
 import { useActionSheet } from '@expo/react-native-action-sheet';
 import * as React from 'react';
 import { Platform, StyleSheet } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import type { TooltipItemBaseProps } from './tooltip-item.react.js';
 
@@ -74,6 +75,8 @@ function TooltipContextProvider(props: ProviderProps): React.Node {
     optionsRef.current = optionsRef.current.filter(option => option.id !== id);
   }, []);
 
+  const insets = useSafeAreaInsets();
+
   const { cancel, hideTooltip } = props;
   const { showActionSheetWithOptions } = useActionSheet();
   const showActionSheet = React.useCallback(() => {
@@ -125,7 +128,7 @@ function TooltipContextProvider(props: ProviderProps): React.Node {
     };
 
     const containerStyle = {
-      paddingBottom: 24,
+      paddingBottom: insets.bottom,
     };
     showActionSheetWithOptions(
       {
@@ -140,9 +143,10 @@ function TooltipContextProvider(props: ProviderProps): React.Node {
   }, [
     hideTooltip,
     maxOptionsToDisplay,
+    insets.bottom,
+    showActionSheetWithOptions,
     visibleEntryIDsSet,
     cancel,
-    showActionSheetWithOptions,
   ]);
 
   const shouldShowMore = React.useCallback(() => {
