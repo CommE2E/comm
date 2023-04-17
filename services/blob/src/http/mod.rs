be1f504fa0c61e9eceaed9d90b02f7fcba53dabd
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2023-04-17 14:31:42 +0200

    [services][blob] Introduce HTTP context utils
    Summary:
    
    This diff introduces a `AppContext` structure that is passed to each HTTP request handler. It contains references
    to database and S3 instances along with some utilities that will be used in the handlers.
    
    Depends on D7444
    
    Test Plan:
    
    This is not yet used, it will be tested with the next diff.
    
    Reviewers: marcin, tomek, jon, michal
    
    Subscribers:

--- services/blob/src/http/mod.rs
+++ services/blob/src/http/mod.rs
@@ -6,12 +6,13 @@ use actix_web::{web, App, HttpServer};
 use anyhow::Result;
 use tracing::info;
 
+mod context;
+use context::AppContext;
+
 async fn hello_handler() -> impl actix_web::Responder {
   "Hello, world!"
 }
 
-// disable unused warning for now
-#[allow(unused)]
 pub async fn run_http_server(
   db_client: DatabaseClient,
   s3_client: S3Client,
@@ -21,8 +22,14 @@ pub async fn run_http_server(
     CONFIG.listen_port
   );
   HttpServer::new(move || {
+    // context that is passed to every handler
+    let ctx = AppContext {
+      db: db_client.to_owned(),
+      s3: s3_client.to_owned(),
+    };
     App::new()
       .wrap(tracing_actix_web::TracingLogger::default())
+      .app_data(web::Data::new(ctx))
       .service(web::resource("/hello").route(web::get().to(hello_handler)))
   })
   .bind(("0.0.0.0", CONFIG.listen_port))?
