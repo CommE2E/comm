1ecd401b99bc2cbc5b5845cb3b08bbe9a7f20edb
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-04-17 11:34:10 -0400

    [native] have actionsheet pop up on press when editing avatar
    Summary:
    When the user now presses on a user or thread avatar in the profile or thread settings, an action sheet popups displaying various options for editing an avatar. For this diff the only option is the emoji avatar edit flow; however, diffs further down this stack will introduce other edit options
    
    Test Plan:
    Please watch the demo videos:
    
    iOS:
    {F489877}
    
    Android:
    {F489878}
    
    Reviewers: ashoat, atul
    
    Reviewed By: ashoat
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7459

--- native/components/edit-avatar.react.js
+++ native/components/edit-avatar.react.js
@@ -1,8 +1,11 @@
 // @flow
 
+import { useActionSheet } from '@expo/react-native-action-sheet';
 import * as React from 'react';
-import { View, TouchableOpacity } from 'react-native';
+import { View, TouchableOpacity, Platform } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
+import CommIcon from '../components/comm-icon.react.js';
 import SWMansionIcon from '../components/swmansion-icon.react.js';
 import { useColors, useStyles } from '../themes/colors.js';
 
@@ -14,8 +17,76 @@ type Props = {
 function EditAvatar(props: Props): React.Node {
   const { onPressEmojiAvatarFlow, children, disabled } = props;
 
+  const { showActionSheetWithOptions } = useActionSheet();
+
   const colors = useColors();
   const styles = useStyles(unboundStyles);
+
+  const editAvatarOptions = React.useMemo(() => {
+    const options = [
+      {
+        id: 'emoji',
+        text: 'Use Emoji',
+        onPress: onPressEmojiAvatarFlow,
+        icon: (
+          <CommIcon
+            name="emote-smile-filled"
+            size={18}
+            style={styles.bottomSheetIcon}
+          />
+        ),
+      },
+    ];
+
+    if (Platform.OS === 'ios') {
+      options.push({
+        id: 'cancel',
+        text: 'Cancel',
+        isCancel: true,
+      });
+    }
+    return options;
+  }, [onPressEmojiAvatarFlow, styles.bottomSheetIcon]);
+
+  const insets = useSafeAreaInsets();
+
+  const onPressEditAvatar = React.useCallback(() => {
+    const texts = editAvatarOptions.map(option => option.text);
+
+    const cancelButtonIndex = editAvatarOptions.findIndex(
+      option => option.isCancel,
+    );
+
+    const containerStyle = {
+      paddingBottom: insets.bottom,
+    };
+
+    const icons = editAvatarOptions.map(option => option.icon);
+
+    const onPressAction = (selectedIndex: ?number) => {
+      if (
+        selectedIndex === null ||
+        selectedIndex === undefined ||
+        selectedIndex < 0
+      ) {
+        return;
+      }
+      const option = editAvatarOptions[selectedIndex];
+      if (option.onPress) {
+        option.onPress();
+      }
+    };
+
+    showActionSheetWithOptions(
+      {
+        options: texts,
+        cancelButtonIndex,
+        containerStyle,
+        icons,
+      },
+      onPressAction,
+    );
+  }, [editAvatarOptions, insets.bottom, showActionSheetWithOptions]);
 
   const editBadge = React.useMemo(() => {
     if (disabled) {
@@ -40,7 +111,7 @@ function EditAvatar(props: Props): React.Node {
   ]);
 
   return (
-    <TouchableOpacity onPress={onPressEmojiAvatarFlow} disabled={disabled}>
+    <TouchableOpacity onPress={onPressEditAvatar} disabled={disabled}>
       {children}
       {editBadge}
     </TouchableOpacity>
@@ -62,6 +133,9 @@ const unboundStyles = {
   },
   editAvatarIcon: {
     textAlign: 'center',
+  },
+  bottomSheetIcon: {
+    color: '#000000',
   },
 };
 
