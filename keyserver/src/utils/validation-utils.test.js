2b39f8639e779de5597f562627bb5d5b3836b9a0
Michal Gniadek <michal.gniadek@swmansion.com>
2023-04-17 15:13:19 +0200

    [keyserver] Introduce convertObject function
    Summary:
    Introdices `convertObject` function that implements the visitor pattern and uses it to implement `sanitizeInput`. It will used in the later diffs for implementing the id conversion function.
    
    `sanitizeInput` should still redact passwords where it previously redacted them but it will now redact them in more places, e.g. lists, non-string optionals. Previously added tests succeed, and I have also added tests fot the new cases.
    
    Test Plan: Run `yarn jest`
    
    Reviewers: kamil, inka, tomek
    
    Subscribers: ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D7488

--- keyserver/src/utils/validation-utils.test.js
+++ keyserver/src/utils/validation-utils.test.js
@@ -24,4 +24,51 @@ describe('sanitization', () => {
     const redacted = { password: redactedString };
     expect(sanitizeInput(validator, object)).toStrictEqual(redacted);
   });
+
+  it('should redact a string in optional object', () => {
+    const validator = tShape({ obj: t.maybe(tShape({ password: tPassword })) });
+    const object = { obj: { password: 'password' } };
+    const redacted = { obj: { password: redactedString } };
+    expect(sanitizeInput(validator, object)).toStrictEqual(redacted);
+  });
+
+  it('should redact a string array', () => {
+    const validator = tShape({ passwords: t.list(tPassword) });
+    const object = { passwords: ['password', 'password'] };
+    const redacted = { passwords: [redactedString, redactedString] };
+    expect(sanitizeInput(validator, object)).toStrictEqual(redacted);
+  });
+
+  it('should redact a string inside a dict', () => {
+    const validator = tShape({ passwords: t.dict(t.String, tPassword) });
+    const object = { passwords: { a: 'password', b: 'password' } };
+    const redacted = { passwords: { a: redactedString, b: redactedString } };
+    expect(sanitizeInput(validator, object)).toStrictEqual(redacted);
+  });
+
+  it('should redact password dict key', () => {
+    const validator = tShape({ passwords: t.dict(tPassword, t.Bool) });
+    const object = { passwords: { password1: true, password2: false } };
+    const redacted = { passwords: {} };
+    redacted.passwords[redactedString] = false;
+    expect(sanitizeInput(validator, object)).toStrictEqual(redacted);
+  });
+
+  it('should redact a string inside a union', () => {
+    const validator = tShape({
+      password: t.union([tPassword, t.String, t.Bool]),
+    });
+    const object = { password: 'password' };
+    const redacted = { password: redactedString };
+    expect(sanitizeInput(validator, object)).toStrictEqual(redacted);
+  });
+
+  it('should redact a string inside an object array', () => {
+    const validator = tShape({
+      passwords: t.list(tShape({ password: tPassword })),
+    });
+    const object = { passwords: [{ password: 'password' }] };
+    const redacted = { passwords: [{ password: redactedString }] };
+    expect(sanitizeInput(validator, object)).toStrictEqual(redacted);
+  });
 });
