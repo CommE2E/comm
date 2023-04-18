5e846b54f7f91035acbab9fb4bde1b2fbcf864aa
Ashoat Tevosyan <ashoat@comm.app>
2023-04-18 16:04:15 -0400

    [native] Introduce RegistrationContainer
    Summary: Factoring out the container and header height logic so it can be reused in other screens. More context [in the preceding diff](https://phab.comm.dev/D7500#inline-49122).
    
    Depends on D7500
    
    Test Plan: Everything still works the same as before. Also Flow
    
    Reviewers: ginsu, atul
    
    Subscribers:

---
+++ native/account/registration/registration-container.react.js
@@ -0,0 +1,33 @@
+// @flow
+
+import { useHeaderHeight } from '@react-navigation/elements';
+import * as React from 'react';
+import { View } from 'react-native';
+
+import { useStyles } from '../../themes/colors.js';
+
+type Props = {
+  +children: React.Node,
+};
+function RegistrationContainer(props: Props): React.Node {
+  const styles = useStyles(unboundStyles);
+
+  const headerHeight = useHeaderHeight();
+  const backgroundStyle = React.useMemo(
+    () => ({
+      ...styles.background,
+      marginTop: headerHeight,
+    }),
+    [headerHeight, styles.background],
+  );
+
+  return <View style={backgroundStyle}>{props.children}</View>;
+}
+
+const unboundStyles = {
+  background: {
+    backgroundColor: 'panelBackground',
+  },
+};
+
+export default RegistrationContainer;
