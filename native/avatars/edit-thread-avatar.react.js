78de2854ff2ea64e72dbd05a4ae3bfa73026e1e9
Atul Madhugiri <atulm@berkeley.edu>
2023-04-18 18:27:55 -0400

    [native] Move complexity of `actionSheetConfig` to `useShowAvatarActionSheet`
    Summary:
    We can simplify the `actionSheetConfig` constructed in `Edit*Avatar` by moving some of what's common to `useShowAvatarActionSheet`. This cuts down on reptition a decent amount.
    
    All of this refactoring brought the size of `Edit*Avatar` from  234 lines to 40:
    
    This should make the subsequent changes easier to manage.
    
    ---
    
    Depends on D7506
    
    Test Plan: Everything looks and works as it did previously + ESLint + Flow
    
    Reviewers: ashoat, ginsu
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D7507

--- native/avatars/edit-thread-avatar.react.js
+++ native/avatars/edit-thread-avatar.react.js
@@ -1,15 +1,13 @@
 // @flow
 
 import * as React from 'react';
-import { TouchableOpacity, Platform } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 
 import {
   useSelectAndUploadFromGallery,
   useShowAvatarActionSheet,
 } from './avatar-hooks.js';
 import EditAvatarBadge from './edit-avatar-badge.react.js';
-import SWMansionIcon from '../components/swmansion-icon.react.js';
-import { useStyles } from '../themes/colors.js';
 
 type Props = {
   +children: React.Node,
@@ -19,71 +17,24 @@ type Props = {
 function EditThreadAvatar(props: Props): React.Node {
   const { onPressEmojiAvatarFlow, children, disabled } = props;
 
-  const styles = useStyles(unboundStyles);
-
   const selectAndUploadFromGallery = useSelectAndUploadFromGallery();
 
-  const editAvatarOptions = React.useMemo(() => {
-    const options = [
-      {
-        id: 'emoji',
-        text: 'Use Emoji',
-        onPress: onPressEmojiAvatarFlow,
-        icon: (
-          <SWMansionIcon
-            name="emote-smile"
-            size={22}
-            style={styles.bottomSheetIcon}
-          />
-        ),
-      },
-      {
-        id: 'image',
-        text: 'Select image',
-        onPress: selectAndUploadFromGallery,
-        icon: (
-          <SWMansionIcon
-            name="image-1"
-            size={22}
-            style={styles.bottomSheetIcon}
-          />
-        ),
-      },
-    ];
+  const actionSheetConfig = React.useMemo(
+    () => [
+      { id: 'emoji', onPress: onPressEmojiAvatarFlow },
+      { id: 'image', onPress: selectAndUploadFromGallery },
+    ],
+    [onPressEmojiAvatarFlow, selectAndUploadFromGallery],
+  );
 
-    if (Platform.OS === 'ios') {
-      options.push({
-        id: 'cancel',
-        text: 'Cancel',
-        isCancel: true,
-      });
-    }
-    return options;
-  }, [
-    onPressEmojiAvatarFlow,
-    selectAndUploadFromGallery,
-    styles.bottomSheetIcon,
-  ]);
-
-  const showAvatarActionSheet = useShowAvatarActionSheet(editAvatarOptions);
-
-  let editBadge;
-  if (!disabled) {
-    editBadge = <EditAvatarBadge />;
-  }
+  const showAvatarActionSheet = useShowAvatarActionSheet(actionSheetConfig);
 
   return (
     <TouchableOpacity onPress={showAvatarActionSheet} disabled={disabled}>
       {children}
-      {editBadge}
+      {!disabled ? <EditAvatarBadge /> : null}
     </TouchableOpacity>
   );
 }
-
-const unboundStyles = {
-  bottomSheetIcon: {
-    color: '#000000',
-  },
-};
 
 export default EditThreadAvatar;
