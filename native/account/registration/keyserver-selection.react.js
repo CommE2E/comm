22ef9dfc868bed869f6b6536e7dc8d593153c412
Ashoat Tevosyan <ashoat@comm.app>
2023-04-18 16:04:15 -0400

    [native] Introduce RegistrationContainer
    Summary:
    Factoring out the container and header height logic so it can be reused in other screens. More context [in the preceding diff](https://phab.comm.dev/D7500#inline-49122).
    
    Depends on D7500
    
    Test Plan: Everything still works the same as before. Also Flow
    
    Reviewers: ginsu, atul
    
    Reviewed By: atul
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D7501

--- native/account/registration/keyserver-selection.react.js
+++ native/account/registration/keyserver-selection.react.js
@@ -1,9 +1,9 @@
 // @flow
 
-import { useHeaderHeight } from '@react-navigation/elements';
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
 
+import RegistrationContainer from './registration-container.react.js';
 import type { RegistrationNavigationProp } from './registration-navigator.react.js';
 import type { NavigationRoute } from '../../navigation/route-names.js';
 import { useStyles } from '../../themes/colors.js';
@@ -15,27 +15,14 @@ type Props = {
 // eslint-disable-next-line no-unused-vars
 function KeyserverSelection(props: Props): React.Node {
   const styles = useStyles(unboundStyles);
-
-  const headerHeight = useHeaderHeight();
-  const backgroundStyle = React.useMemo(
-    () => ({
-      ...styles.background,
-      marginTop: headerHeight,
-    }),
-    [headerHeight, styles.background],
-  );
-
   return (
-    <View style={backgroundStyle}>
+    <RegistrationContainer>
       <Text style={styles.testText}>Test Hello Test</Text>
-    </View>
+    </RegistrationContainer>
   );
 }
 
 const unboundStyles = {
-  background: {
-    backgroundColor: 'panelBackground',
-  },
   testText: {
     fontSize: 24,
     color: 'white',
