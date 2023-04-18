be310bab12f884de42a14acb4a457b7a2c196a99
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-04-18 15:51:29 -0400

    [native] naively split EmojiAvatarCreation into EmojiUserAvatarCreation and EmojiThreadAvatarCreation
    Summary: Copied the code in `EmojiAvatarCreation` and pasted it into both `EmojiUserAvatarCration` and `EmojiThreadAvatarCreation`. Also updated the navigators and imports to use these two new components.
    
    Test Plan: Saving emoji avatar still works, no flow errors
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7503

--- native/profile/emoji-user-avatar-creation.react.js
+++ native/profile/emoji-user-avatar-creation.react.js
@@ -16,9 +16,11 @@ import { savedEmojiAvatarSelectorForThread } from 'lib/selectors/thread-selector
 import { savedEmojiAvatarSelectorForCurrentUser } from 'lib/selectors/user-selectors.js';
 import type { ClientEmojiAvatar } from 'lib/types/avatar-types.js';
 
-import Avatar from './avatar.react.js';
+import Avatar from '../avatars/avatar.react.js';
 import Button from '../components/button.react.js';
 import ColorRows from '../components/color-rows.react.js';
+import type { NavigationRoute } from '../navigation/route-names.js';
+import type { ProfileNavigationProp } from '../profile/profile.react.js';
 import { useSelector } from '../redux/redux-utils.js';
 import { useStyles } from '../themes/colors.js';
 import {
@@ -35,18 +37,18 @@ const threadAvatarLoadingStatusSelector = createLoadingStatusSelector(
   `${changeThreadSettingsActionTypes.started}:avatar`,
 );
 
-export type EmojiAvatarCreationParams = {
+export type EmojiUserAvatarCreationParams = {
   +threadID?: string,
   +containingThreadID?: ?string,
 };
 
 type Props = {
-  +threadID?: string,
-  +containingThreadID?: ?string,
+  +navigation: ProfileNavigationProp<'EmojiUserAvatarCreation'>,
+  +route: NavigationRoute<'EmojiUserAvatarCreation'>,
 };
 
-function EmojiAvatarCreation(props: Props): React.Node {
-  const { threadID, containingThreadID } = props;
+function EmojiUserAvatarCreation(props: Props): React.Node {
+  const { threadID, containingThreadID } = props.route.params;
 
   const selector = threadID
     ? savedEmojiAvatarSelectorForThread(threadID, containingThreadID)
@@ -251,4 +253,4 @@ const unboundStyles = {
   },
 };
 
-export default EmojiAvatarCreation;
+export default EmojiUserAvatarCreation;
