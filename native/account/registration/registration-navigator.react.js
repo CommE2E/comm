73b4fb133763b54ae5d3b51527e8cb12587d9a0a
Ashoat Tevosyan <ashoat@comm.app>
2023-04-18 14:44:15 -0400

    [native] Add empty RegistrationModal
    Summary:
    This diff adds the plumbing for `RegistrationModal` and makes the new registration button open it.
    
    Depends on D7495
    
    Test Plan:
    When the `RegistrationModal` is opened on iOS, the screen becomes unpressable, which is expected
    
    Also Flow
    
    Reviewers: ginsu, atul
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D7496

---
+++ native/account/registration/registration-navigator.react.js
@@ -0,0 +1,55 @@
+// @flow
+
+import {
+  createStackNavigator,
+  type StackNavigationProp,
+  type StackNavigationHelpers,
+} from '@react-navigation/stack';
+import * as React from 'react';
+import { View } from 'react-native';
+
+import KeyserverSelection from './keyserver-selection.react.js';
+import type { RootNavigationProp } from '../../navigation/root-navigator.react.js';
+import {
+  KeyserverSelectionRouteName,
+  type ScreenParamList,
+  type RegistrationParamList,
+} from '../../navigation/route-names.js';
+import { useStyles } from '../../themes/colors.js';
+
+export type RegistrationNavigationProp<
+  RouteName: $Keys<RegistrationParamList> = $Keys<RegistrationParamList>,
+> = StackNavigationProp<ScreenParamList, RouteName>;
+
+const Registration = createStackNavigator<
+  ScreenParamList,
+  RegistrationParamList,
+  StackNavigationHelpers<ScreenParamList>,
+>();
+type Props = {
+  +navigation: RootNavigationProp<'Registration'>,
+  ...
+};
+// eslint-disable-next-line no-unused-vars
+function RegistrationNavigator(props: Props): React.Node {
+  const styles = useStyles(unboundStyles);
+  return (
+    <View style={styles.view}>
+      <Registration.Navigator>
+        <Registration.Screen
+          name={KeyserverSelectionRouteName}
+          component={KeyserverSelection}
+        />
+      </Registration.Navigator>
+    </View>
+  );
+}
+
+const unboundStyles = {
+  view: {
+    flex: 1,
+    backgroundColor: 'panelBackground',
+  },
+};
+
+export default RegistrationNavigator;
