2eec01a57e12d165784e712cbe2645ca101fb276
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2023-04-18 11:55:26 +0200

    [native][utils-module] Implement file r/w operations
    Summary:
    Part of [[ https://linear.app/comm/issue/ENG-3408/fix-file-encryption-performance-on-native | ENG-3408 ]].
    
    This diff implements asynchronous functions to read/write file from/to ArrayBuffer.
    
    Depends on D7452
    
    Test Plan:
    In the native JS code (e.g. root.react.js):
    
    ```
    lang=js
    const myArray = new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
    const dir = temporaryDirectoryPath;
    console.log('temp dir:', dir);
    await commUtilsModule.writeBufferToFile(dir + 'temp.dat', myArray.buffer);
    commUtilsModule.readBufferFromFile(dir + 'temp.dat').then(data => {
      console.log('read:', new Uint8Array(data));
    });
    ```
    The read value should be the same as initial array. Also checked existence of the file
    For iOS:
    - `open [printed temp dir path]` - check if file exists
    - `xxd [printed temp dir path]/temp.dat` - checked if hex is equal to `01 02 ... 0a` (myArray)
    
    For Android:
    - `adb root && adb pull [temp dir]/temp.dat` - then `xxd` that file
    - `adb shell su ; ls [temp dir]`
    
    Reviewers: marcin, kamil, inka
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7485

--- native/cpp/CommonCpp/NativeModules/CommUtilsModule.cpp
+++ native/cpp/CommonCpp/NativeModules/CommUtilsModule.cpp
@@ -1,6 +1,7 @@
 #include "CommUtilsModule.h"
 
 #include <ReactCommon/TurboModuleUtils.h>
+#include <fstream>
 
 namespace comm {
 
@@ -15,12 +16,85 @@ jsi::Value CommUtilsModule::writeBufferToFile(
     jsi::Runtime &rt,
     jsi::String path,
     jsi::Object data) {
-  return jsi::Value::undefined();
+  auto arrayBuffer = data.getArrayBuffer(rt);
+  auto size = arrayBuffer.size(rt);
+  auto dataPtr = arrayBuffer.data(rt);
+  auto filePath = path.utf8(rt);
+  return createPromiseAsJSIValue(
+      rt, [=](jsi::Runtime &innerRt, std::shared_ptr<Promise> promise) {
+        taskType job = [=, &innerRt]() {
+          std::string error;
+          try {
+            std::ofstream file;
+            file.exceptions(std::fstream::failbit | std::fstream::badbit);
+            file.open(filePath, std::ios::binary);
+            file.write((char *)dataPtr, size);
+            file.close();
+          } catch (const std::exception &e) {
+            error = "Failed to write file " + filePath + ": " + e.what();
+          }
+
+          this->jsInvoker_->invokeAsync([error, promise]() {
+            if (error.size()) {
+              promise->reject(error);
+            } else {
+              promise->resolve(jsi::Value::undefined());
+            }
+          });
+        };
+        this->utilsThread->scheduleTask(job);
+      });
 }
 
 jsi::Value
 CommUtilsModule::readBufferFromFile(jsi::Runtime &rt, jsi::String path) {
-  return jsi::Value::undefined();
+  auto filePath = path.utf8(rt);
+  return createPromiseAsJSIValue(
+      rt, [=](jsi::Runtime &innerRt, std::shared_ptr<Promise> promise) {
+        taskType job = [=, &innerRt]() {
+          std::string error;
+          std::streampos file_size;
+          std::shared_ptr<uint8_t> data(nullptr);
+          try {
+            // Open a file
+            std::ifstream file;
+            file.exceptions(std::fstream::failbit | std::fstream::badbit);
+            file.open(filePath, std::ios::binary);
+
+            // Get file size
+            std::streampos current_pos = file.tellg();
+            file.seekg(0, std::ios::end);
+            file_size = file.tellg();
+            file.seekg(current_pos);
+
+            // Read file content
+            data = std::shared_ptr<uint8_t>(new uint8_t[file_size]);
+            file.read((char *)data.get(), file_size);
+            file.close();
+          } catch (const std::exception &e) {
+            error = "Failed to read file " + filePath + ": " + e.what();
+          }
+
+          this->jsInvoker_->invokeAsync([=, &innerRt]() {
+            if (error.size()) {
+              promise->reject(error);
+              return;
+            }
+            auto arrayBuffer =
+                innerRt.global()
+                    .getPropertyAsFunction(innerRt, "ArrayBuffer")
+                    // ArrayBuffer constructor takes one parameter: byte length
+                    .callAsConstructor(
+                        innerRt, {static_cast<double>(file_size)})
+                    .asObject(innerRt)
+                    .getArrayBuffer(innerRt);
+            auto bufferPtr = arrayBuffer.data(innerRt);
+            memcpy((void *)bufferPtr, data.get(), file_size);
+            promise->resolve(std::move(arrayBuffer));
+          });
+        };
+        this->utilsThread->scheduleTask(job);
+      });
 }
 
 jsi::String
