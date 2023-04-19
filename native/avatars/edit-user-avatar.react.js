c7b6506084da5e5b789c61c659a20831ad297fd8
Atul Madhugiri <atulm@berkeley.edu>
2023-04-19 14:43:54 -0400

    [native] Show `ActivityIndicator` in `EditUserAvatar` during update
    Summary:
    We consume `isGalleryAvatarUpdateLoading` and `isRemoveAvatarUpdateLoading` introduced in previous diffs to display `ActivityIndicator` (ie spinner) when an update is in progress.
    
    Note: Yeah there are obvious things that can be factored out. I want to get more of the error handling stuff done first and then I'll go through and consolidate things.
    
    ---
    
    Depends on D7528
    
    Test Plan: Works as expected:
    
    Reviewers: ashoat, ginsu
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D7529

--- native/avatars/edit-user-avatar.react.js
+++ native/avatars/edit-user-avatar.react.js
@@ -1,7 +1,7 @@
 // @flow
 
 import * as React from 'react';
-import { TouchableOpacity } from 'react-native';
+import { ActivityIndicator, TouchableOpacity, View } from 'react-native';
 
 import {
   useRemoveUserAvatar,
@@ -10,6 +10,7 @@ import {
 } from './avatar-hooks.js';
 import EditAvatarBadge from './edit-avatar-badge.react.js';
 import UserAvatar from './user-avatar.react.js';
+import { useStyles } from '../themes/colors.js';
 
 type Props = {
   +userID: ?string,
@@ -17,12 +18,16 @@ type Props = {
   +disabled?: boolean,
 };
 function EditUserAvatar(props: Props): React.Node {
+  const styles = useStyles(unboundStyles);
   const { userID, onPressEmojiAvatarFlow, disabled } = props;
 
-  const [selectFromGalleryAndUpdateUserAvatar] =
+  const [selectFromGalleryAndUpdateUserAvatar, isGalleryAvatarUpdateLoading] =
     useSelectFromGalleryAndUpdateUserAvatar();
 
-  const [removeUserAvatar] = useRemoveUserAvatar();
+  const [removeUserAvatar, isRemoveAvatarUpdateLoading] = useRemoveUserAvatar();
+
+  const isAvatarUpdateInProgress =
+    isGalleryAvatarUpdateLoading || isRemoveAvatarUpdateLoading;
 
   const actionSheetConfig = React.useMemo(
     () => [
@@ -39,12 +44,34 @@ function EditUserAvatar(props: Props): React.Node {
 
   const showAvatarActionSheet = useShowAvatarActionSheet(actionSheetConfig);
 
+  let spinner;
+  if (isAvatarUpdateInProgress) {
+    spinner = (
+      <View style={styles.spinnerContainer}>
+        <ActivityIndicator color="white" size="large" />
+      </View>
+    );
+  }
+
   return (
     <TouchableOpacity onPress={showAvatarActionSheet} disabled={disabled}>
       <UserAvatar userID={userID} size="profile" />
+      {spinner}
       {!disabled ? <EditAvatarBadge /> : null}
     </TouchableOpacity>
   );
 }
+
+const unboundStyles = {
+  spinnerContainer: {
+    position: 'absolute',
+    alignItems: 'center',
+    justifyContent: 'center',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+};
 
 export default EditUserAvatar;
