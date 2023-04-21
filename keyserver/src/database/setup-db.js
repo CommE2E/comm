4fb935b59ff2fe2e96b6739131b700a0de25957d
marcinwasowicz <marcinkwasowicz@gmail.com>
2023-04-21 13:19:13 +0200

    Add table for keyserver olm accounts
    Summary: This differential adds new table to MariaDB database on the keyserver to store both primary and notifications olm accounts.
    
    Test Plan:
    To test `migrations-config.js` restart your keyserver terminal. Ensure `DB version: 32` log line is visible.
    To test `setup-db.js` create new database and update `secrets` folder so that it points to new database instance. Start your keyserver.
    
    In both cases using a database connection tool of choice (I prefer to use `mariadb` terminal command), ensure `keyserver_olm_accounts` table is present and its structure matches SQL statement used to create it.
    
    Reviewers: tomek, bartek, atul
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D7560

--- keyserver/src/database/setup-db.js
+++ keyserver/src/database/setup-db.js
@@ -266,6 +266,12 @@ async function createTables() {
         pickled_olm_session text CHARACTER SET ascii COLLATE latin1_bin NOT NULL
       ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_bin;
 
+      CREATE TABLE olm_accounts (
+        is_content tinyint(1) NOT NULL,
+        pickling_key text CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
+        pickled_olm_account text CHARACTER SET latin1 COLLATE latin1_bin NOT NULL
+      ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_bin;
+
       ALTER TABLE cookies
         ADD PRIMARY KEY (id),
         ADD UNIQUE KEY device_token (device_token(512)),
@@ -393,6 +399,9 @@ async function createTables() {
 
       ALTER TABLE olm_sessions
         ADD PRIMARY KEY (cookie_id, is_content);
+       
+      ALTER TABLE olm_accounts
+        ADD PRIMARY KEY (is_content);
     `,
     { multipleStatements: true },
   );
