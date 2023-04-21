f2aadb0846f169230151a7aaa000d27333e83d37
marcinwasowicz <marcinkwasowicz@gmail.com>
2023-04-21 17:10:33 +0200

    Implement prekey lifecycle on the keyserver
    Summary: This differential implements prekey lifecycle on the keyserver that is compliant with what was established during crypto sync on 20th April 2023.
    
    Test Plan: Create fresh olm accounts in the database (by changin db version for example and running migrations). Then examine the contents of pickled accounts strings. Change cron to run every minute or every second. Continue examining pickled accounts strings in the database. Make sure they
    change once and only once. It means that prekey absence is correctly detected on first cron run, but sbusequents runs do not change anything since it hasn't been enough time since the last run.
    
    Reviewers: tomek, atul, bartek, kamil, inka
    
    Subscribers:

--- keyserver/src/cron/cron.js
+++ keyserver/src/cron/cron.js
@@ -23,6 +23,9 @@ import { deleteStaleSIWENonceEntries } from '../deleters/siwe-nonce-deleters.js'
 import { deleteInaccessibleThreads } from '../deleters/thread-deleters.js';
 import { deleteExpiredUpdates } from '../deleters/update-deleters.js';
 import { deleteUnassignedUploads } from '../deleters/upload-deleters.js';
+import { fetchKeyserverOlmAccount } from '../fetchers/olm-account-fetcher.js';
+import { updateOlmAccount } from '../updaters/olm-account-updater.js';
+import { validateAccountPrekey } from '../utils/olm-utils.js';
 
 if (cluster.isMaster) {
   schedule.scheduleJob(
@@ -87,6 +90,40 @@ if (cluster.isMaster) {
           'encountered error while trying to create daily updates thread',
           e,
         );
+      }
+    },
+  );
+
+  schedule.scheduleJob(
+    '0 0 * * *', // every day at midnight in the keyserver's timezone
+    async () => {
+      try {
+        const [
+          { picklingKey: primaryPicklingKey, account: primaryAccount },
+          {
+            picklingKey: notificationsPicklingKey,
+            account: notificationsAccount,
+          },
+        ] = await Promise.all([
+          fetchKeyserverOlmAccount(true),
+          fetchKeyserverOlmAccount(false),
+        ]);
+
+        await Promise.all([
+          validateAccountPrekey(primaryAccount),
+          validateAccountPrekey(notificationsAccount),
+        ]);
+
+        await Promise.all([
+          updateOlmAccount(primaryAccount, primaryPicklingKey, true),
+          updateOlmAccount(
+            notificationsAccount,
+            notificationsPicklingKey,
+            false,
+          ),
+        ]);
+      } catch (e) {
+        console.warn('encountered error while trying to validate prekeys', e);
       }
     },
   );
