68d0c3e4e24c45604f41e543ea63d1b5e9c8695d
marcinwasowicz <marcinkwasowicz@gmail.com>
2023-04-21 17:04:30 +0200

    Implement fetcher and updater for olm accounts on the keyserver
    Summary:
    This differential implements a fetcher and an updater for olm accounts on the keyserver. All changes to olm account are done in-memory on olm.Account instance. Therefore the updater takes just olm.Account instance since all updates are already in this object and it just need to be
    serialized and stored in database.
    
    Test Plan:
    Find a place in the keyserver codebase to execute the following sequence:
    1. Use fetcher to get olm account.
    2. Change its structure by calling for example `generate_prekey()`
    3. Store it back by calling updater.
    In each step use database inspection tool of choice, to examine that pickled account string in the database has changed.
    
    Reviewers: tomek, atul, bartek, inka, kamil
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D7568

---
+++ keyserver/src/updaters/olm-account-updater.js
@@ -0,0 +1,112 @@
+// @flow
+
+import type { Account as OlmAccount } from '@commapp/olm';
+
+import { ServerError } from 'lib/utils/errors.js';
+import sleep from 'lib/utils/sleep.js';
+
+import { SQL, dbQuery } from '../database/database.js';
+import { unpickleOlmAccount } from '../utils/olm-utils.js';
+
+const maxOlmAccountUpdateRetriesCount = 5;
+const olmAccountUpdateRetryDelay = 2000;
+
+async function fetchCallUpdateOlmAccount<T>(
+  olmAccountType: 'content' | 'notifications',
+  callback: (account: OlmAccount) => Promise<T>,
+): Promise<T> {
+  const isContent = olmAccountType === 'content';
+  let retriesLeft = maxOlmAccountUpdateRetriesCount;
+
+  while (retriesLeft > 0) {
+    const [olmAccountResult] = await dbQuery(
+      SQL`
+        SELECT version, pickling_key, pickled_olm_account
+        FROM olm_accounts 
+        WHERE is_content = ${isContent}
+      `,
+    );
+
+    if (olmAccountResult.length === 0) {
+      throw new ServerError('missing_olm_account');
+    }
+
+    const [
+      {
+        version,
+        pickling_key: picklingKey,
+        pickled_olm_account: pickledAccount,
+      },
+    ] = olmAccountResult;
+
+    const account = await unpickleOlmAccount({
+      picklingKey,
+      pickledAccount,
+    });
+    const result = await callback(account);
+    const updatedAccount = account.pickle(picklingKey);
+
+    const [transactionResult] = await dbQuery(
+      SQL`
+        START TRANSACTION;
+
+        SELECT version INTO @currentVersion
+        FROM olm_accounts 
+        WHERE is_content = ${isContent} 
+        FOR UPDATE;
+
+        UPDATE olm_accounts
+        SET 
+          pickled_olm_account = ${updatedAccount}, 
+          version = ${version} + 1
+        WHERE version = ${version} AND is_content = ${isContent};
+        
+        COMMIT;
+
+        SELECT @currentVersion AS versionOnUpdateAttempt;
+      `,
+      { multipleStatements: true },
+    );
+    const selectResult = transactionResult.pop();
+    const [{ versionOnUpdateAttempt }] = selectResult;
+
+    if (version === versionOnUpdateAttempt) {
+      return result;
+    }
+
+    retriesLeft = retriesLeft - 1;
+    await sleep(olmAccountUpdateRetryDelay);
+  }
+
+  throw new ServerError('max_olm_account_update_retry_exceeded');
+}
+
+async function fetchOlmAccount(
+  olmAccountType: 'content' | 'notifications',
+): Promise<{
+  account: OlmAccount,
+  picklingKey: string,
+}> {
+  const isContent = olmAccountType === 'content';
+  const [olmAccountResult] = await dbQuery(
+    SQL`
+      SELECT pickling_key, pickled_olm_account 
+      FROM olm_accounts 
+      WHERE is_content = ${isContent}
+    `,
+  );
+  if (olmAccountResult.length === 0) {
+    throw new ServerError('missing_olm_account');
+  }
+  const picklingKey = olmAccountResult[0].pickling_key;
+  const pickledAccount = olmAccountResult[0].pickled_olm_account;
+
+  const account = await unpickleOlmAccount({
+    picklingKey,
+    pickledAccount,
+  });
+
+  return { account, picklingKey };
+}
+
+export { fetchCallUpdateOlmAccount, fetchOlmAccount };
