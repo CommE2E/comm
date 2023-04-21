4d817a3a0e8300af7066f67a7047fee68ec7b32d
marcinwasowicz <marcinkwasowicz@gmail.com>
2023-04-21 17:04:30 +0200

    Implement fetcher and updater for olm accounts on the keyserver
    Summary:
    This differential implements a fetcher and an updater for olm accounts on the keyserver. All changes to olm account are done in-memory on olm.Account instance. Therefore the updater takes just olm.Account instance since all updates are already in this object and it just need to be
    serialized and stored in database.
    
    Test Plan:
    Find a place in the keyserver codebase to execute the following sequence:
    1. Use fetcher to get olm account.
    2. Change its structure by calling for example `generate_prekey()`
    3. Store it back by calling updater.
    In each step use database inspection tool of choice, to examine that pickled account string in the database has changed.
    
    Reviewers: tomek, atul, bartek, inka, kamil
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D7568

---
+++ keyserver/src/updaters/olm-account-updater.js
@@ -0,0 +1,61 @@
+// @flow
+
+import type { Account as OlmAccount } from '@commapp/olm';
+
+import { ServerError } from 'lib/utils/errors.js';
+
+import { SQL, getMultipleStatementsConnection } from '../database/database.js';
+import { unpicklePickledOlmAccount } from '../utils/olm-utils.js';
+
+async function fetchCallUpdateOlmAccount(
+  olmAccountType: 'primary' | 'notifications',
+  callback: (account: OlmAccount) => Promise<void>,
+): Promise<void> {
+  const isPrimary = olmAccountType === 'primary';
+  const connection = await getMultipleStatementsConnection();
+  await connection.query(
+    SQL`
+      START TRANSACTION
+    `,
+  );
+  const [olmAccountResult] = await connection.query(
+    SQL`
+      SELECT pickling_key, pickled_olm_account 
+      FROM keyserver_olm_accounts 
+      WHERE is_primary = ${isPrimary};
+    `,
+  );
+
+  if (olmAccountResult.length === 0) {
+    await connection.query(
+      SQL`
+        ROLLBACK;
+      `,
+    );
+    throw new ServerError('missing_olm_account');
+  }
+
+  const picklingKey = olmAccountResult[0].pickling_key;
+  const pickledAccount = olmAccountResult[0].pickled_olm_account;
+  const account = await unpicklePickledOlmAccount({
+    picklingKey,
+    pickledAccount,
+  });
+  await callback(account);
+  const updatedPickledAccount = account.pickle(picklingKey);
+
+  await connection.query(
+    SQL`
+      UPDATE keyserver_olm_accounts 
+      SET pickled_olm_account = ${updatedPickledAccount} 
+      WHERE is_primary = ${isPrimary};
+    `,
+  );
+  await connection.query(
+    SQL`
+      COMMIT;
+    `,
+  );
+}
+
+export { fetchCallUpdateOlmAccount };
