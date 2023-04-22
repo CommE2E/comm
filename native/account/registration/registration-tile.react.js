55eeb404f375b6b490b21c0579a1754eef9f21df
Ashoat Tevosyan <ashoat@comm.app>
2023-04-22 15:27:30 -0400

    [native] Introduce RegistrationTile and fill in KeyserverSelection
    Summary:
    This diff styles `KeyserverSelection` to match [what's in Figma](https://www.figma.com/file/a1nkbWgbgjRlrOY9LVurTz/Comm-%2F-Desktop-app?node-id=11725%3A252655&t=nS41Gv0y9g6arpro-1), but without any selection state or "Next" button.
    
    I also introduce two new components: `KeyserverTile` and `KeyserverTileHeader`. They serve to factor out the tile JSX, and in later diffs will also hold the selection state.
    
    Depends on D7575
    
    Test Plan:
    | before | after |
    | {F502096} | {F502099} |
    
    Reviewers: ginsu, atul
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D7576

---
+++ native/account/registration/registration-tile.react.js
@@ -0,0 +1,71 @@
+// @flow
+
+import * as React from 'react';
+import { View } from 'react-native';
+
+import { useStyles } from '../../themes/colors.js';
+
+type TileProps = {
+  +children: React.Node,
+};
+function RegistrationTile(props: TileProps): React.Node {
+  const { children } = props;
+
+  const [body, header] = React.useMemo(() => {
+    let registrationBody = children;
+    let registrationHeader;
+    if (Array.isArray(children)) {
+      registrationBody = [];
+      for (const child of children) {
+        if (child.type.name === RegistrationTileHeader.name) {
+          registrationHeader = React.cloneElement(child);
+        } else {
+          registrationBody.push(child);
+        }
+      }
+    }
+    return [registrationBody, registrationHeader];
+  }, [children]);
+
+  const styles = useStyles(unboundStyles);
+  return (
+    <View style={styles.tile}>
+      <View style={styles.tileTitle}>
+        {header}
+        <View style={styles.tileRadio} />
+      </View>
+      {body}
+    </View>
+  );
+}
+
+type HeaderProps = {
+  +children: React.Node,
+};
+function RegistrationTileHeader(props: HeaderProps): React.Node {
+  return props.children;
+}
+
+const unboundStyles = {
+  tile: {
+    backgroundColor: 'panelForeground',
+    paddingHorizontal: 16,
+    paddingVertical: 20,
+    borderRadius: 8,
+    marginTop: 24,
+  },
+  tileTitle: {
+    paddingBottom: 16,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  tileRadio: {
+    borderColor: 'panelSecondaryForegroundBorder',
+    borderWidth: 1,
+    height: 24,
+    width: 24,
+    borderRadius: 12,
+  },
+};
+
+export { RegistrationTile, RegistrationTileHeader };
