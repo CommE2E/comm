44bd260ae904a852da610bcd1ad8b1fdc74a738f
Jonathan Ringer <jonringer117@gmail.com>
2023-04-23 14:25:55 -0700

    [Identity] Rename key request RPCs to reflect Olm terms
    Summary:
    Use inbound and outbound instead of receiver and sender to differientiate
    which key information is needed.
    
    Use direction of the message is a bit more intuitive, as the opposite role
    needs to request the other's information.
    
    Part of https://linear.app/comm/issue/ENG-3740
    
    Test Plan:
    Identity service builds with new naming
    ```
    cd services/identity
    cargo build
    ```
    
    Reviewers: ashoat, varun
    
    Subscribers: tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7580

--- services/identity/src/client_service.rs
+++ services/identity/src/client_service.rs
@@ -6,14 +6,14 @@ use std::str::FromStr;
 
 use crate::{
   client_service::client_proto::{
-    DeleteUserRequest, Empty, GenerateNonceResponse, KeyserverKeysRequest,
-    KeyserverKeysResponse, OpaqueLoginFinishRequest, OpaqueLoginFinishResponse,
+    DeleteUserRequest, Empty, GenerateNonceResponse, InboundKeysForUserRequest,
+    InboundKeysForUserResponse, KeyserverKeysRequest, KeyserverKeysResponse,
+    OpaqueLoginFinishRequest, OpaqueLoginFinishResponse,
     OpaqueLoginStartRequest, OpaqueLoginStartResponse,
-    ReceiverKeysForUserRequest, ReceiverKeysForUserResponse,
+    OutboundKeysForUserRequest, OutboundKeysForUserResponse,
     RefreshUserPreKeysRequest, RegistrationFinishRequest,
     RegistrationFinishResponse, RegistrationStartRequest,
-    RegistrationStartResponse, SenderKeysForUserRequest,
-    SenderKeysForUserResponse, UpdateUserPasswordFinishRequest,
+    RegistrationStartResponse, UpdateUserPasswordFinishRequest,
     UpdateUserPasswordFinishResponse, UpdateUserPasswordStartRequest,
     UpdateUserPasswordStartResponse, UploadOneTimeKeysRequest,
     VerifyUserAccessTokenRequest, VerifyUserAccessTokenResponse,
@@ -264,17 +264,17 @@ impl IdentityClientService for ClientService {
     }
   }
 
-  async fn get_receiver_keys_for_user(
+  async fn get_outbound_keys_for_user(
     &self,
-    _request: tonic::Request<ReceiverKeysForUserRequest>,
-  ) -> Result<tonic::Response<ReceiverKeysForUserResponse>, tonic::Status> {
+    _request: tonic::Request<OutboundKeysForUserRequest>,
+  ) -> Result<tonic::Response<OutboundKeysForUserResponse>, tonic::Status> {
     unimplemented!();
   }
 
-  async fn get_sender_keys_for_user(
+  async fn get_inbound_keys_for_user(
     &self,
-    _request: tonic::Request<SenderKeysForUserRequest>,
-  ) -> Result<tonic::Response<SenderKeysForUserResponse>, tonic::Status> {
+    _request: tonic::Request<InboundKeysForUserRequest>,
+  ) -> Result<tonic::Response<InboundKeysForUserResponse>, tonic::Status> {
     unimplemented!();
   }
 
