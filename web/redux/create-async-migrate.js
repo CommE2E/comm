f55b50a89caec8008499a5f6f041c5a469dd1177
xsanm <kkurowski.kk@gmail.com>
2023-04-24 15:34:56 +0200

    [web-db] implement asynchronous `redux-persist` migrations
    Summary:
    This is [createMigrate](https://github.com/rt2zz/redux-persist/blob/master/src/createMigrate.ts) code allowing to async migrations.
    
    This could potentially be patched, but this code will have more responsibilities in near future, so it's better to maintain this that way.
    
    Depends on D7289
    
    Test Plan:
    1. Run tests
    2. Add one more async migration and check if it works
    
    Tests could not be added directly, this function will read something from local storage - that being said this test will fail.
    
    Reviewers: tomek, marcin, michal
    
    Subscribers: ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D7658

---
+++ web/redux/create-async-migrate.js
@@ -0,0 +1,74 @@
+// @flow
+
+import { DEFAULT_VERSION } from 'redux-persist/es/constants.js';
+import type { PersistState } from 'redux-persist/es/types.js';
+
+type MigrationManifest = {
+  [number | string]: (PersistedState) => Promise<PersistedState>,
+};
+type PersistedState = {
+  _persist: PersistState,
+  ...
+} | void;
+
+function creatAsyncMigrate(
+  migrations: MigrationManifest,
+  config: { debug: boolean },
+): (state: PersistedState, currentVersion: number) => Promise<PersistedState> {
+  const debug = !!config?.debug;
+  return async function (
+    state: PersistedState,
+    currentVersion: number,
+  ): Promise<PersistedState> {
+    if (!state) {
+      if (process.env.NODE_ENV !== 'production' && debug) {
+        console.log('redux-persist: no inbound state, skipping migration');
+      }
+      return undefined;
+    }
+
+    const inboundVersion: number =
+      state._persist && state._persist.version !== undefined
+        ? state._persist.version
+        : DEFAULT_VERSION;
+
+    if (inboundVersion === currentVersion) {
+      if (process.env.NODE_ENV !== 'production' && debug) {
+        console.log('redux-persist: versions match, noop migration');
+      }
+      return state;
+    }
+    if (inboundVersion > currentVersion) {
+      if (process.env.NODE_ENV !== 'production') {
+        console.error('redux-persist: downgrading version is not supported');
+      }
+      return state;
+    }
+
+    const migrationKeys = Object.keys(migrations)
+      .map(ver => parseInt(ver))
+      .filter(key => currentVersion >= key && key > inboundVersion)
+      .sort((a, b) => a - b);
+
+    if (process.env.NODE_ENV !== 'production' && debug) {
+      console.log('redux-persist: migrationKeys', migrationKeys);
+    }
+
+    let migratedState: PersistedState = state;
+    for (const versionKey of migrationKeys) {
+      if (process.env.NODE_ENV !== 'production' && debug) {
+        console.log(
+          'redux-persist: running migration for versionKey',
+          versionKey,
+        );
+      }
+      if (versionKey) {
+        migratedState = await migrations[versionKey](migratedState);
+      }
+    }
+
+    return migratedState;
+  };
+}
+
+export { creatAsyncMigrate };
