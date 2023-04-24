98448b32b77675b64ba000d672519a414ddd1cb7
marcinwasowicz <marcinkwasowicz@gmail.com>
2023-04-24 15:39:43 +0200

    Implement endpoint on the keyserver to get one time keys and prekey to initialize olmsession
    Summary: This differential implements an endpoint on the keyserver to get data necessary to initialize olm session with the client.
    
    Test Plan:
    Call `commCoreModule.initializeCryptoAccount()` (it is called in log-in-panel via `nativeLogInExtraInfoSelector`). This ensures notifications session is initialized. Then call this endpoint to get necessary data to initialize sesion for notifications. Pass obtained data to
    `commCoreModule.initializeNotificationsSession()`.
    
    Reviewers: tomek, atul, inka, kamil, bartek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D7586

--- keyserver/src/responders/keys-responders.js
+++ keyserver/src/responders/keys-responders.js
@@ -1,13 +1,21 @@
 // @flow
 
+import type { Account as OlmAccount } from '@commapp/olm';
 import t from 'tcomb';
 
-import type { GetSessionPublicKeysArgs } from 'lib/types/request-types.js';
+import type {
+  GetOlmNotifsSessionInitializationDataResponse,
+  GetOlmNotifsSessionInitializationDataRequest,
+  GetSessionPublicKeysArgs,
+} from 'lib/types/request-types.js';
 import type { SessionPublicKeys } from 'lib/types/session-types.js';
+import { ServerError } from 'lib/utils/errors.js';
 import { tShape } from 'lib/utils/validation-utils.js';
 
 import { fetchSessionPublicKeys } from '../fetchers/key-fetchers.js';
 import type { Viewer } from '../session/viewer.js';
+import { fetchCallUpdateOlmAccount } from '../updaters/olm-account-updater.js';
+import { validateAccountPrekey } from '../utils/olm-utils.js';
 import { validateInput } from '../utils/validation-utils.js';
 
 const getSessionPublicKeysInputValidator = tShape({
@@ -26,4 +34,74 @@ async function getSessionPublicKeysResponder(
   return await fetchSessionPublicKeys(request.session);
 }
 
-export { getSessionPublicKeysResponder };
+const getOlmNotifsSessionInitializationDataRequestInputValidator = tShape({
+  oneTimeKeysCount: t.Number,
+});
+
+async function getOlmNotifsSessionInitializationDataResponder(
+  viewer: Viewer,
+  input: any,
+): Promise<GetOlmNotifsSessionInitializationDataResponse> {
+  await validateInput(
+    viewer,
+    getOlmNotifsSessionInitializationDataRequestInputValidator,
+    input,
+  );
+  const request: GetOlmNotifsSessionInitializationDataRequest = input;
+
+  let prekey, notificationsIdentityKeysString, oneTimeKeys;
+  const notificationsAccountCallback = async (account: OlmAccount) => {
+    notificationsIdentityKeysString = account.identity_keys();
+
+    await validateAccountPrekey(account);
+    prekey = account.prekey();
+    // Until transfer of prekeys to the identity service is not implemented
+    // notifs prekey will be marked as published each time it is accessed
+    // to establish olm notifs session to mitigate the risk of notifs
+    // prekey being in use long enough to cause security concerns
+    account.mark_prekey_as_published();
+
+    account.generate_one_time_keys(request.oneTimeKeysCount);
+    oneTimeKeys = account.one_time_keys();
+    account.mark_keys_as_published();
+  };
+
+  await fetchCallUpdateOlmAccount(
+    'notifications',
+    notificationsAccountCallback,
+  );
+  if (!prekey || !oneTimeKeys) {
+    throw new ServerError('olm_account_query_failure');
+  }
+
+  let identityKeysBlob, identityKeysBlobPayload, signedIdentityKeysBlob;
+  const primaryAccountCallback = async (account: OlmAccount) => {
+    identityKeysBlob = {
+      primaryIdentityPublicKeys: JSON.parse(account.identity_keys()),
+      notificationIdentityPublicKeys: JSON.parse(
+        notificationsIdentityKeysString,
+      ),
+    };
+    identityKeysBlobPayload = JSON.stringify(identityKeysBlob);
+    signedIdentityKeysBlob = {
+      payload: identityKeysBlobPayload,
+      signature: account.sign(identityKeysBlobPayload),
+    };
+  };
+
+  await fetchCallUpdateOlmAccount('primary', primaryAccountCallback);
+  if (!signedIdentityKeysBlob) {
+    throw new ServerError('olm_account_query_failure');
+  }
+
+  return {
+    signedIdentityKeysBlob,
+    prekey,
+    oneTimeKeys,
+  };
+}
+
+export {
+  getSessionPublicKeysResponder,
+  getOlmNotifsSessionInitializationDataResponder,
+};
