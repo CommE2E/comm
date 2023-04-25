9f9aaa1410f0333184734b57846090e6765268d2
Michal Gniadek <michal.gniadek@swmansion.com>
2023-04-25 15:51:43 +0200

    [keyserver] Introduce responder validators
    Summary: Introduce validators for entry, keys, siwe, message report, relationship and search responders and tests, most of which are taken from real requests that I've logged. I decided to not split it in multiple diffs because it's mostly just 1-2 validators per responder type and each change is localized to the responder.
    
    Test Plan: `yarn jest`
    
    Reviewers: kamil, tomek
    
    Subscribers: ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D7685

--- keyserver/src/responders/keys-responders.js
+++ keyserver/src/responders/keys-responders.js
@@ -1,10 +1,13 @@
 // @flow
 
-import t from 'tcomb';
+import t, { type TUnion } from 'tcomb';
 
 import type { GetSessionPublicKeysArgs } from 'lib/types/request-types.js';
-import type { SessionPublicKeys } from 'lib/types/session-types.js';
-import { tShape } from 'lib/utils/validation-utils.js';
+import {
+  type SessionPublicKeys,
+  sessionPublicKeysValidator,
+} from 'lib/types/session-types.js';
+import { tShape, tNull } from 'lib/utils/validation-utils.js';
 
 import { fetchSessionPublicKeys } from '../fetchers/key-fetchers.js';
 import type { Viewer } from '../session/viewer.js';
@@ -14,10 +17,14 @@ const getSessionPublicKeysInputValidator = tShape({
   session: t.String,
 });
 
+type GetSessionPublicKeysResponse = SessionPublicKeys | null;
+export const getSessionPublicKeysResponseValidator: TUnion<GetSessionPublicKeysResponse> =
+  t.union([sessionPublicKeysValidator, tNull]);
+
 async function getSessionPublicKeysResponder(
   viewer: Viewer,
   input: any,
-): Promise<SessionPublicKeys | null> {
+): Promise<GetSessionPublicKeysResponse> {
   if (!viewer.loggedIn) {
     return null;
   }
