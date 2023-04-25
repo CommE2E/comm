0f35ba1d74f131c05ef2c2ef2fa776e260f40654
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-04-25 15:50:19 -0400

    [native] introduce user avatar camera modal
    Summary: Introduce user avatar camera modal
    
    Test Plan:
    Difficult to provide a demo video, but I was able to use my camera to take a photo and the photo i snapped was set as my avatar after hitting "send" in the camera modal. Also tested going back, and when I hit the "X" button it just goes back to the profile screen. I also double checked that the loading state was in sync with the edit user avatar
    provider
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7624

--- native/navigation/route-names.js
+++ native/navigation/route-names.js
@@ -78,6 +78,7 @@ export const ThreadPickerModalRouteName = 'ThreadPickerModal';
 export const ThreadSettingsMemberTooltipModalRouteName =
   'ThreadSettingsMemberTooltipModal';
 export const ThreadSettingsRouteName = 'ThreadSettings';
+export const UserAvatarCameraModalRouteName = 'UserAvatarCameraModal';
 export const VideoPlaybackModalRouteName = 'VideoPlaybackModal';
 export const TermsAndPrivacyRouteName = 'TermsAndPrivacyModal';
 export const RegistrationRouteName = 'Registration';
@@ -117,6 +118,7 @@ export type OverlayParamList = {
   +ImageModal: ImageModalParams,
   +ActionResultModal: ActionResultModalParams,
   +ChatCameraModal: ChatCameraModalParams,
+  +UserAvatarCameraModal: void,
   +VideoPlaybackModal: VideoPlaybackModalParams,
   ...TooltipModalParamList,
 };
