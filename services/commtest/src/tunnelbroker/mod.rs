009feee62c4b9cef4f9a180e402222680977d4dd
Jonathan Ringer <jonringer117@gmail.com>
2023-04-25 10:53:07 -0700

    [Tunnelbroker] Add integration tests
    Summary:
    For some features, it's easier to just have a unittest demonstrate
    the expected behavior. This sets up that framework.
    
    Depends on D7659
    
    Test Plan:
    ```
    (cd services/tunnelbroker && RUST_LOG=debug cargo run &)
    cd services/commtest && cargo test --test tunnelbroker_integration_test
    ```
    
    Reviewers: varun, bartek
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7661

--- services/commtest/src/tunnelbroker/mod.rs
+++ services/commtest/src/tunnelbroker/mod.rs
@@ -1,2 +1,1 @@
-pub mod new_session;
-pub mod tunnelbroker_utils;
+
--- services/tunnelbroker/src/constants.rs
+++ services/tunnelbroker/src/constants.rs
@@ -4,3 +4,6 @@ pub const GRPC_TX_QUEUE_SIZE: usize = 32;
 pub const GRPC_SERVER_PORT: u64 = 50051;
 pub const GRPC_KEEP_ALIVE_PING_INTERVAL: Duration = Duration::from_secs(3);
 pub const GRPC_KEEP_ALIVE_PING_TIMEOUT: Duration = Duration::from_secs(10);
+
+pub const LOG_LEVEL_ENV_VAR: &str =
+  tracing_subscriber::filter::EnvFilter::DEFAULT_ENV;
