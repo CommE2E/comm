cf9fc33e19ed20adea88ed638807ba51c2995def
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-04-25 15:07:49 -0400

    [native] move selectFromGalleryAndUpdateUserAvatar from avatar utils to edit user avatar provider
    Summary: Introduce the edit user avatar provider. Another provider for the thread avatars will be introduced in a subsequent diff.
    
    Test Plan: I tested all the avatar options we had (ENS, Emoji, and Photo gallery) and in all cases the behvaoir was to be expected with setting the avatar, loading while the avatar was saved
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7621

--- native/avatars/edit-user-avatar-provider.react.js
+++ native/avatars/edit-user-avatar-provider.react.js
@@ -9,19 +9,23 @@ import {
 } from 'lib/actions/user-actions.js';
 import { createLoadingStatusSelector } from 'lib/selectors/loading-selectors.js';
 import type {
+  ImageAvatarDBContent,
   ENSAvatarDBContent,
   UpdateUserAvatarRemoveRequest,
 } from 'lib/types/avatar-types.js';
 import type { LoadingStatus } from 'lib/types/loading-types.js';
+import type { MediaLibrarySelection } from 'lib/types/media-types.js';
 import {
   useDispatchActionPromise,
   useServerCall,
 } from 'lib/utils/action-utils.js';
 
+import { selectFromGallery, useUploadSelectedMedia } from './avatar-hooks.js';
 import { useSelector } from '../redux/redux-utils.js';
 
 export type EditUserAvatarContextType = {
   +userAvatarSaveInProgress: boolean,
+  +selectFromGalleryAndUpdateUserAvatar: () => Promise<void>,
   +setENSUserAvatar: () => void,
   +removeUserAvatar: () => void,
 };
@@ -42,14 +46,52 @@ function EditUserAvatarProvider(props: Props): React.Node {
   const dispatchActionPromise = useDispatchActionPromise();
   const updateUserAvatarCall = useServerCall(updateUserAvatar);
 
+  const [userAvatarMediaUploadInProgress, setUserAvatarMediaUploadInProgress] =
+    React.useState(false);
+
   const updateUserAvatarLoadingStatus: LoadingStatus = useSelector(
     updateUserAvatarLoadingStatusSelector,
   );
 
   const userAvatarSaveInProgress = React.useMemo(
-    () => updateUserAvatarLoadingStatus === 'loading',
-    [updateUserAvatarLoadingStatus],
+    () =>
+      userAvatarMediaUploadInProgress ||
+      updateUserAvatarLoadingStatus === 'loading',
+    [userAvatarMediaUploadInProgress, updateUserAvatarLoadingStatus],
   );
+
+  const uploadUserAvatarSelectedMedia = useUploadSelectedMedia(
+    setUserAvatarMediaUploadInProgress,
+  );
+  const selectFromGalleryAndUpdateUserAvatar = React.useCallback(async () => {
+    const selection: ?MediaLibrarySelection = await selectFromGallery();
+    const uploadedMediaID = await uploadUserAvatarSelectedMedia(selection);
+
+    if (!uploadedMediaID) {
+      return;
+    }
+
+    const imageAvatarUpdateRequest: ImageAvatarDBContent = {
+      type: 'image',
+      uploadID: uploadedMediaID,
+    };
+
+    dispatchActionPromise(
+      updateUserAvatarActionTypes,
+      (async () => {
+        setUserAvatarMediaUploadInProgress(false);
+        try {
+          return await updateUserAvatarCall(imageAvatarUpdateRequest);
+        } catch {
+          Alert.alert('Avatar update failed', 'Unable to update avatar.');
+        }
+      })(),
+    );
+  }, [
+    dispatchActionPromise,
+    updateUserAvatarCall,
+    uploadUserAvatarSelectedMedia,
+  ]);
 
   const setENSUserAvatar = React.useCallback(() => {
     const ensAvatarRequest: ENSAvatarDBContent = {
@@ -88,10 +130,16 @@ function EditUserAvatarProvider(props: Props): React.Node {
   const context = React.useMemo(
     () => ({
       userAvatarSaveInProgress,
+      selectFromGalleryAndUpdateUserAvatar,
       setENSUserAvatar,
       removeUserAvatar,
     }),
-    [removeUserAvatar, setENSUserAvatar, userAvatarSaveInProgress],
+    [
+      removeUserAvatar,
+      selectFromGalleryAndUpdateUserAvatar,
+      setENSUserAvatar,
+      userAvatarSaveInProgress,
+    ],
   );
 
   return (
