9bb950c2e505f541bc393ba023251698ce50b433
Jonathan Ringer <jonringer117@gmail.com>
2023-04-25 10:53:07 -0700

    [Tunnelbroker] Add integration tests
    Summary: For some features, it's easier to just have a unittest demonstrate
    the expected behavior. This sets up that framework.
    
    Depends on D7659
    
    Test Plan:
    ```
    (cd services/tunnelbroker && RUST_LOG=debug cargo run &)
    cd services/commtest && cargo test --test tunnelbroker_integration_test
    ```
    
    Reviewers: varun, bartek
    
    Subscribers:

--- shared/tunnelbroker_messages/src/messages/mod.rs
+++ shared/tunnelbroker_messages/src/messages/mod.rs
@@ -8,6 +8,7 @@ pub use session::*;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
+#[serde(untagged)]
 pub enum Messages {
   RefreshKeysRequest(RefreshKeyRequest),
   SessionRequest(SessionRequest),
