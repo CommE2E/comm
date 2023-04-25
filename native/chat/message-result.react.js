4d2b8d7399f13803a3fd08067d63a0d1fcf6a27b
RohanK6 <rohan.khanderia@gmail.com>
2023-04-25 09:05:18 -0400

    [native] Render the message inside of the pin modal
    Summary:
    Following the previous diff, the target message that the user is trying to pin should render inside of the toggle pin modal.
    
    Depends on D7607
    
    Test Plan: {F519860}
    
    Reviewers: atul, ginsu, tomek, ashoat
    
    Reviewed By: atul, ashoat
    
    Subscribers: inka, ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7608

--- native/chat/message-result.react.js
+++ native/chat/message-result.react.js
@@ -1,20 +1,68 @@
 // @flow
 
 import * as React from 'react';
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 
 import { type ThreadInfo } from 'lib/types/thread-types.js';
+import { longAbsoluteDate } from 'lib/utils/date-utils.js';
 
+import { MessageListContextProvider } from './message-list-types.js';
+import { Message } from './message.react.js';
+import type { AppNavigationProp } from '../navigation/app-navigator.react';
+import type { NavigationRoute } from '../navigation/route-names';
+import { useStyles } from '../themes/colors.js';
 import type { ChatMessageInfoItemWithHeight } from '../types/chat-types.js';
 
 type MessageResultProps = {
   +item: ChatMessageInfoItemWithHeight,
   +threadInfo: ThreadInfo,
+  +navigation: AppNavigationProp<'TogglePinModal'>,
+  +route: NavigationRoute<'TogglePinModal'>,
 };
 
-/* eslint-disable no-unused-vars */
 function MessageResult(props: MessageResultProps): React.Node {
-  return <View />;
+  const styles = useStyles(unboundStyles);
+
+  const onToggleFocus = React.useCallback(() => {}, []);
+
+  return (
+    <ScrollView style={styles.container}>
+      <MessageListContextProvider threadInfo={props.threadInfo}>
+        <View style={styles.viewContainer}>
+          <Message
+            item={props.item}
+            focused={false}
+            navigation={props.navigation}
+            route={props.route}
+            toggleFocus={onToggleFocus}
+            verticalBounds={null}
+          />
+          <Text style={styles.messageDate}>
+            {longAbsoluteDate(props.item.messageInfo.time)}
+          </Text>
+        </View>
+      </MessageListContextProvider>
+    </ScrollView>
+  );
 }
+
+const unboundStyles = {
+  container: {
+    marginTop: 5,
+    backgroundColor: 'panelForeground',
+    overflow: 'scroll',
+    maxHeight: 400,
+  },
+  viewContainer: {
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  messageDate: {
+    color: 'messageLabel',
+    fontSize: 12,
+    marginLeft: 55,
+  },
+};
 
 export default MessageResult;
