fd0203f1c83a226e3da627a2358f57de74baa7af
xsanm <kkurowski.kk@gmail.com>
2023-04-26 15:55:34 +0200

    [web-db] choose storage engine depending on whether database is supported or not
    Summary:
    Conditionally use default persist storage API or database depending on whether database is supported or not.
    
    Depends on D7666
    
    Test Plan: Switch to `commReduxStorageEngine` and test if it works for not supported database
    
    Reviewers: tomek, michal
    
    Reviewed By: tomek
    
    Subscribers: ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D7667

--- web/redux/comm-redux-storage-engine.js
+++ web/redux/comm-redux-storage-engine.js
@@ -1,10 +1,17 @@
 // @flow
 
+import storage from 'redux-persist/es/storage/index.js';
+
 import { databaseModule } from '../database/database-module-provider.js';
 import { workerRequestMessageTypes } from '../types/worker-types.js';
 
 const commReduxStorageEngine = {
   getItem: async (key: string): Promise<string> => {
+    const isSupported = await databaseModule.isDatabaseSupported();
+    if (!isSupported) {
+      return await storage.getItem(key);
+    }
+
     const result = await databaseModule.schedule({
       type: workerRequestMessageTypes.GET_PERSIST_STORAGE_ITEM,
       key,
@@ -15,6 +22,12 @@ const commReduxStorageEngine = {
     return result.item;
   },
   setItem: async (key: string, item: string): Promise<void> => {
+    const isSupported = await databaseModule.isDatabaseSupported();
+    if (!isSupported) {
+      await storage.setItem(key, item);
+      return;
+    }
+
     await databaseModule.schedule({
       type: workerRequestMessageTypes.SET_PERSIST_STORAGE_ITEM,
       key,
@@ -22,6 +35,12 @@ const commReduxStorageEngine = {
     });
   },
   removeItem: async (key: string): Promise<void> => {
+    const isSupported = await databaseModule.isDatabaseSupported();
+    if (!isSupported) {
+      await storage.removeItem(key);
+      return;
+    }
+
     await databaseModule.schedule({
       type: workerRequestMessageTypes.REMOVE_PERSIST_STORAGE_ITEM,
       key,
