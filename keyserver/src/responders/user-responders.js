92cf4723a43df4088b8364d1f327b4b5ff79c639
marcinwasowicz <marcinkwasowicz@gmail.com>
2023-04-27 15:25:41 +0200

    Establish olm sessions for notifs on both sides during log-in
    Summary: This differential implements establishment of olm sessions for e2e notifs on both sides during log in.
    
    Test Plan: Log in to the keyserver and examine content on `olm_sessions` table. New entry should appear.
    
    Reviewers: tomek, atul, bartek, jon
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D7654

--- keyserver/src/responders/user-responders.js
+++ keyserver/src/responders/user-responders.js
@@ -99,6 +99,7 @@ import {
   createAccount,
   processSIWEAccountCreation,
 } from '../creators/account-creator.js';
+import { createOlmSession } from '../creators/olm-session-creator.js';
 import { dbQuery, SQL } from '../database/database.js';
 import { deleteAccount } from '../deleters/account-deleters.js';
 import { deleteCookie } from '../deleters/cookie-deleters.js';
@@ -297,6 +298,7 @@ type ProcessSuccessfulLoginParams = {
   +calendarQuery: ?CalendarQuery,
   +socialProof?: ?SIWESocialProof,
   +signedIdentityKeysBlob?: ?SignedIdentityKeysBlob,
+  +initialNotificationsEncryptedMessage?: string,
 };
 
 async function processSuccessfulLogin(
@@ -309,6 +311,7 @@ async function processSuccessfulLogin(
     calendarQuery,
     socialProof,
     signedIdentityKeysBlob,
+    initialNotificationsEncryptedMessage,
   } = params;
 
   const request: LogInRequest = input;
@@ -351,6 +354,19 @@ async function processSuccessfulLogin(
   if (calendarQuery) {
     await setNewSession(viewer, calendarQuery, newServerTime);
   }
+  const olmSessionPromise = (async () => {
+    if (
+      userViewerData.cookieID &&
+      initialNotificationsEncryptedMessage &&
+      signedIdentityKeysBlob
+    ) {
+      await createOlmSession(
+        initialNotificationsEncryptedMessage,
+        'notifications',
+        userViewerData.cookieID,
+      );
+    }
+  })();
 
   const threadCursors = {};
   for (const watchedThreadID of request.watchedIDs) {
@@ -370,6 +386,7 @@ async function processSuccessfulLogin(
     calendarQuery ? fetchEntryInfos(viewer, [calendarQuery]) : undefined,
     fetchKnownUserInfos(viewer),
     fetchLoggedInUserInfo(viewer),
+    olmSessionPromise,
   ]);
 
   const rawEntryInfos = entriesResult ? entriesResult.rawEntryInfos : null;
@@ -406,6 +423,7 @@ const logInRequestInputValidator = tShape({
   // old clients, but we no longer do anything with it.
   primaryIdentityPublicKey: t.maybe(tRegex(primaryIdentityPublicKeyRegex)),
   signedIdentityKeysBlob: t.maybe(signedIdentityKeysBlobValidator),
+  initialNotificationsEncryptedMessage: t.maybe(t.String),
 });
 
 export const logInResponseValidator: TInterface<LogInResponse> =
@@ -434,7 +452,8 @@ async function logInResponder(
   const request: LogInRequest = input;
 
   let identityKeys: ?IdentityKeysBlob;
-  const { signedIdentityKeysBlob } = request;
+  const { signedIdentityKeysBlob, initialNotificationsEncryptedMessage } =
+    request;
   if (signedIdentityKeysBlob) {
     identityKeys = JSON.parse(signedIdentityKeysBlob.payload);
 
@@ -498,6 +517,7 @@ async function logInResponder(
     userID: id,
     calendarQuery,
     signedIdentityKeysBlob,
+    initialNotificationsEncryptedMessage,
   });
 }
 
