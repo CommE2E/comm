644f42eaa785557d06ff0aaba3e5861aebeaecbc
Jonathan Ringer <jonringer117@gmail.com>
2023-04-27 08:51:51 -0700

    [Keyserver] Open websocket connection with tunnelbroker
    Summary:
    Attempt to connect to tunnelbroker if available.
    This is meant to be a "hello world" for establishing
    a tunnelbroker connection.
    
    https://linear.app/comm/issue/ENG-3766
    
    Test Plan:
    ```
    (cd services/tunnelbroker && cargo run &)
    # should see message about db migration, then tunnelbroker message
    (cd keyserver && yarn dev)
    ```
    
    Reviewers: ashoat
    
    Subscribers: tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7691

---
+++ keyserver/src/socket/tunnelbroker.js
@@ -0,0 +1,33 @@
+// @flow
+import WebSocket from 'ws';
+
+function createTunnelbrokerWebsocket() {
+  try {
+    const tunnelbrokerSocket = new WebSocket('ws://localhost:51001');
+    tunnelbrokerSocket.on('open', () => {
+      // TODO: Replace keyserver details with actual details
+      const message = {
+        type: 'sessionRequest',
+        accessToken: 'foobar',
+        deviceId: 'foo',
+        deviceType: 'keyserver',
+      };
+      console.log(
+        'Sending message to tunnelbroker: ' + JSON.stringify(message),
+      );
+      tunnelbrokerSocket.send(JSON.stringify(message));
+    });
+
+    tunnelbrokerSocket.on('close', () => {
+      console.log('Connection to tunnelbroker closed');
+    });
+
+    tunnelbrokerSocket.on('error', (error: Error) => {
+      console.error('Tunnelbroker socket error: ' + error.message);
+    });
+  } catch (err) {
+    console.log('Failed to open connection with tunnelbroker');
+  }
+}
+
+export { createTunnelbrokerWebsocket };
