2b514e57219c677def00abca9234fcefbedd4ad5
Jonathan Ringer <jonringer117@gmail.com>
2023-04-27 08:51:51 -0700

    [Keyserver] Open websocket connection with tunnelbroker
    Summary:
    Attempt to connect to tunnelbroker if available.
    This is meant to be a "hello world" for establishing
    a tunnelbroker connection.
    
    https://linear.app/comm/issue/ENG-3766
    
    Test Plan:
    ```
    (cd services/tunnelbroker && cargo run &)
    # should see message about db migration, then tunnelbroker message
    (cd keyserver && yarn dev)
    ```
    
    Reviewers: ashoat
    
    Subscribers: tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7691

---
+++ keyserver/src/socket/tunnelbroker.js
@@ -0,0 +1,60 @@
+// @flow
+
+import WebSocket from 'ws';
+
+import { type TBKeyserverConnectionInitializationMessage } from 'lib/types/tunnelbroker-messages.js';
+import sleep from 'lib/utils/sleep.js';
+
+import { fetchOlmAccount } from '../updaters/olm-account-updater.js';
+import type { IdentityInfo } from '../user/identity.js';
+
+async function createAndMaintainTunnelbrokerWebsocket(
+  identityInfo: IdentityInfo,
+) {
+  const accountInfo = await fetchOlmAccount('content');
+  const deviceId = JSON.parse(accountInfo.account.identity_keys()).curve25519;
+
+  await openTunnelbrokerConnection(
+    deviceId,
+    identityInfo.userId,
+    identityInfo.accessToken,
+  );
+}
+
+function openTunnelbrokerConnection(
+  deviceId: string,
+  userId: string,
+  accessToken: string,
+) {
+  try {
+    const tunnelbrokerSocket = new WebSocket('ws://localhost:51001');
+
+    tunnelbrokerSocket.on('open', () => {
+      const message: TBKeyserverConnectionInitializationMessage = {
+        type: 'sessionRequest',
+        accessToken,
+        deviceId,
+        deviceType: 'keyserver',
+        userId,
+      };
+
+      tunnelbrokerSocket.send(JSON.stringify(message));
+      console.info('Connection to tunnelbroker established');
+    });
+
+    tunnelbrokerSocket.on('close', async () => {
+      console.warn('Connection to tunnelbroker closed');
+      await sleep(1000);
+      console.info('Attempting to re-establish tunnelbroker connection');
+      openTunnelbrokerConnection(deviceId, userId, accessToken);
+    });
+
+    tunnelbrokerSocket.on('error', (error: Error) => {
+      console.error('Tunnelbroker socket error: ' + error.message);
+    });
+  } catch (err) {
+    console.log('Failed to open connection with tunnelbroker');
+  }
+}
+
+export { createAndMaintainTunnelbrokerWebsocket };
