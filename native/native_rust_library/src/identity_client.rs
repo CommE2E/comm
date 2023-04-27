e6e4aa118bcb777e7a9d722f90f8e51d1dfe89d6
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2023-04-27 16:12:36 -0400

    [native] register user on native (rust changes)
    Summary: add register_user function to native rust library
    
    Test Plan: tested in subsequent diff
    
    Reviewers: jon, bartek
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7694

--- native/native_rust_library/src/identity_client.rs
+++ native/native_rust_library/src/identity_client.rs
@@ -2,22 +2,6 @@ use tonic::Status;
 
 use crate::IdentityClient;
 
-pub async fn register_user(
-  mut _client: Box<IdentityClient>,
-  _username: String,
-  _password: String,
-  _key_payload: String,
-  _key_payload_signature: String,
-  _identity_prekey: String,
-  _identity_prekey_signature: String,
-  _notif_prekey: String,
-  _notif_prekey_signature: String,
-  _identity_onetime_keys: Vec<String>,
-  _notif_onetime_keys: Vec<String>,
-) -> Result<String, Status> {
-  unimplemented!();
-}
-
 // User could be logging in from new device, need to resend device information
 pub async fn login_user_pake(
   mut _client: Box<IdentityClient>,
