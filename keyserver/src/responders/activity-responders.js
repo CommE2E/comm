da8663cf8eebd2f067921dcb885a801fd9718775
Michal Gniadek <michal.gniadek@swmansion.com>
2023-04-28 12:48:57 +0200

    [keyserver] Mark ids in input validators
    Summary:
    We will also need to convert ids in the input, so this diff marks the ids for conversion inside existing input validators. `tID` should behave the same as `t.String` so there should be no runtime difference.
    
    Depends on D7712
    
    Test Plan: Test if the web and mobile app still work.
    
    Reviewers: kamil, tomek
    
    Subscribers: ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D7713

--- keyserver/src/responders/activity-responders.js
+++ keyserver/src/responders/activity-responders.js
@@ -12,7 +12,7 @@ import {
   setThreadUnreadStatusResult,
   updateActivityResultValidator,
 } from 'lib/types/activity-types.js';
-import { tShape } from 'lib/utils/validation-utils.js';
+import { tShape, tID } from 'lib/utils/validation-utils.js';
 
 import type { Viewer } from '../session/viewer.js';
 import {
@@ -24,8 +24,8 @@ import { validateInput, validateOutput } from '../utils/validation-utils.js';
 const activityUpdatesInputValidator: TList<Array<ActivityUpdate>> = t.list(
   tShape({
     focus: t.Bool,
-    threadID: t.String,
-    latestMessage: t.maybe(t.String),
+    threadID: tID,
+    latestMessage: t.maybe(tID),
   }),
 );
 
@@ -43,10 +43,10 @@ async function updateActivityResponder(
   return validateOutput(viewer, updateActivityResultValidator, result);
 }
 
-const setThreadUnreadStatusValidator = tShape({
-  threadID: t.String,
+const setThreadUnreadStatusValidator = tShape<SetThreadUnreadStatusRequest>({
+  threadID: tID,
   unread: t.Bool,
-  latestMessage: t.maybe(t.String),
+  latestMessage: t.maybe(tID),
 });
 async function threadSetUnreadStatusResponder(
   viewer: Viewer,
