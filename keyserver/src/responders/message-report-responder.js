5c48e173ee1e77c9c2618388b3e9a231f45a2d22
Michal Gniadek <michal.gniadek@swmansion.com>
2023-04-28 12:48:57 +0200

    [keyserver] Mark ids in input validators
    Summary:
    We will also need to convert ids in the input, so this diff marks the ids for conversion inside existing input validators. `tID` should behave the same as `t.String` so there should be no runtime difference.
    
    Depends on D7712
    
    Test Plan:
    Went through all existing input validators and sub-validators and changed `t.String` to `tID` in places where ids should be updated.
    Test if the web and mobile app still work.
    
    Reviewers: kamil, tomek
    
    Reviewed By: tomek
    
    Subscribers: ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D7713

--- keyserver/src/responders/message-report-responder.js
+++ keyserver/src/responders/message-report-responder.js
@@ -1,20 +1,20 @@
 // @flow
 
-import t, { type TInterface } from 'tcomb';
+import type { TInterface } from 'tcomb';
 
 import {
   type MessageReportCreationRequest,
   type MessageReportCreationResult,
 } from 'lib/types/message-report-types.js';
 import { rawMessageInfoValidator } from 'lib/types/message-types.js';
-import { tShape } from 'lib/utils/validation-utils.js';
+import { tShape, tID } from 'lib/utils/validation-utils.js';
 
 import createMessageReport from '../creators/message-report-creator.js';
 import type { Viewer } from '../session/viewer.js';
 import { validateInput, validateOutput } from '../utils/validation-utils.js';
 
 const messageReportCreationRequestInputValidator = tShape({
-  messageID: t.String,
+  messageID: tID,
 });
 
 export const messageReportCreationResultValidator: TInterface<MessageReportCreationResult> =
