130b93bd07807114136497ad6f79cc3654fad031
RohanK6 <rohan.khanderia@gmail.com>
2023-04-28 20:55:57 -0400

    [native] Display the pinned messages for the thread in the new screen
    Summary:
    This is the screen that should display the pinned messages / the search results.
    The styling will either come in a revision to this diff or in the next diff, but I'm
    putting this up for now to unblock message searching.
    
    Note: This diff is a rough implementation of this screen, it's not had a chance to be
    refined yet.
    
    Depends on D7672
    
    Test Plan:
    Check to see the pinned messages render in the screen, despite the styling
    being incorrect for them.
    
    Reviewers: atul, ginsu, tomek
    
    Subscribers: ashoat, inka
    
    Differential Revision: https://phab.comm.dev/D7673

--- native/chat/message-results-screen.react.js
+++ native/chat/message-results-screen.react.js
@@ -1,11 +1,22 @@
 // @flow
 
+import invariant from 'invariant';
 import * as React from 'react';
+import { View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 
+import { fetchPinnedMessages } from 'lib/actions/message-actions.js';
+import { messageListData } from 'lib/selectors/chat-selectors.js';
+import { createMessageInfo } from 'lib/shared/message-utils.js';
 import type { ThreadInfo } from 'lib/types/thread-types.js';
+import { useServerCall } from 'lib/utils/action-utils.js';
 
+import { useHeightMeasurer } from './chat-context.js';
 import type { ChatNavigationProp } from './chat.react';
+import MessageResult from './message-result.react.js';
 import type { NavigationRoute } from '../navigation/route-names';
+import { useSelector } from '../redux/redux-utils.js';
+import type { ChatMessageItemWithHeight } from '../types/chat-types.js';
 
 export type MessageResultsScreenParams = {
   +threadInfo: ThreadInfo,
@@ -16,9 +27,153 @@ type MessageResultsScreenProps = {
   +route: NavigationRoute<'MessageResultsScreen'>,
 };
 
-// eslint-disable-next-line no-unused-vars
 function MessageResultsScreen(props: MessageResultsScreenProps): React.Node {
-  return null;
+  const { navigation, route } = props;
+  const { threadInfo } = route.params;
+  const { id: threadID } = threadInfo;
+  const [rawMessageResults, setRawMessageResults] = React.useState([]);
+
+  const measureMessages = useHeightMeasurer();
+  const [measuredMessages, setMeasuredMessages] = React.useState([]);
+
+  const callFetchPinnedMessages = useServerCall(fetchPinnedMessages);
+  const userInfos = useSelector(state => state.userStore.userInfos);
+
+  React.useEffect(() => {
+    (async () => {
+      const result = await callFetchPinnedMessages({ threadID });
+      setRawMessageResults(result.pinnedMessages);
+    })();
+  }, [callFetchPinnedMessages, threadID]);
+
+  const translatedMessageResults = React.useMemo(() => {
+    const threadInfos = { [threadID]: threadInfo };
+
+    return rawMessageResults
+      .map(messageInfo =>
+        createMessageInfo(messageInfo, null, userInfos, threadInfos),
+      )
+      .filter(Boolean);
+  }, [rawMessageResults, userInfos, threadID, threadInfo]);
+
+  const chatMessageInfos = useSelector(
+    messageListData(threadInfo.id, translatedMessageResults),
+  );
+
+  const sortedUniqueChatMessageInfoItems = React.useMemo(() => {
+    if (!chatMessageInfos) {
+      return [];
+    }
+
+    const chatMessageInfoItems = chatMessageInfos.filter(
+      item => item.itemType === 'message' && item.isPinned,
+    );
+
+    // By the nature of using messageListData and passing in
+    // the desired translatedMessageResults as additional
+    // messages, we will have duplicate ChatMessageInfoItems.
+    const uniqueChatMessageInfoItemsMap = new Map();
+    chatMessageInfoItems.forEach(
+      item =>
+        item.messageInfo &&
+        item.messageInfo.id &&
+        uniqueChatMessageInfoItemsMap.set(item.messageInfo.id, item),
+    );
+
+    // Push the items in the order they appear in the rawMessageResults
+    // since the messages fetched from the server are already sorted
+    // in the order of pin_time (newest first).
+    const sortedChatMessageInfoItems = [];
+    for (let i = 0; i < rawMessageResults.length; i++) {
+      sortedChatMessageInfoItems.push(
+        uniqueChatMessageInfoItemsMap.get(rawMessageResults[i].id),
+      );
+    }
+
+    return sortedChatMessageInfoItems.filter(Boolean);
+  }, [chatMessageInfos, rawMessageResults]);
+
+  const measureCallback = React.useCallback(
+    (listDataWithHeights: $ReadOnlyArray<ChatMessageItemWithHeight>) => {
+      setMeasuredMessages(listDataWithHeights);
+    },
+    [],
+  );
+
+  React.useEffect(() => {
+    measureMessages(
+      sortedUniqueChatMessageInfoItems,
+      threadInfo,
+      measureCallback,
+    );
+  }, [
+    measureCallback,
+    measureMessages,
+    sortedUniqueChatMessageInfoItems,
+    threadInfo,
+  ]);
+
+  const modifiedItems = React.useMemo(
+    () =>
+      measuredMessages.map(item => {
+        invariant(item.itemType !== 'loader', 'should not be loader');
+        invariant(
+          item.messageShapeType !== 'robotext',
+          'should not be robotext',
+        );
+
+        if (item.messageShapeType === 'multimedia') {
+          return {
+            ...item,
+            startsConversation: false,
+            startsCluster: true,
+            endsCluster: true,
+            messageInfo: {
+              ...item.messageInfo,
+              creator: {
+                ...item.messageInfo.creator,
+                isViewer: false,
+              },
+            },
+          };
+        }
+
+        return {
+          ...item,
+          startsConversation: false,
+          startsCluster: true,
+          endsCluster: true,
+          messageInfo: {
+            ...item.messageInfo,
+            creator: {
+              ...item.messageInfo.creator,
+              isViewer: false,
+            },
+          },
+        };
+      }),
+    [measuredMessages],
+  );
+
+  const messageResultsToDisplay = React.useMemo(
+    () =>
+      modifiedItems.map(item => (
+        <MessageResult
+          key={item.messageInfo.id}
+          item={item}
+          threadInfo={threadInfo}
+          navigation={navigation}
+          route={route}
+        />
+      )),
+    [modifiedItems, threadInfo, navigation, route],
+  );
+
+  return (
+    <View>
+      <ScrollView>{messageResultsToDisplay}</ScrollView>
+    </View>
+  );
 }
 
 export default MessageResultsScreen;
