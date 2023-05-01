CommE2E/comm

commit 2f974fabcf7d8739925f5400f772570e184e746c

[native] render avatars in chat screen
Summary: Rendered avatars in the chat screen. Avatars only render for users who are not the viewer and at the end of a meassage cluster. When the message gets swiped the avatar moves with the message

Test Plan: Please watch the demo videos to see all the edge cases I tested

Subscribers: ashoat, tomek, atul

Differential Revision: https://phab.comm.dev/D7067

native/chat/composed-message.react.js
@@ -6,6 +6,7 @@ import * as React from 'react';
 import { StyleSheet, View } from 'react-native';
 import Animated from 'react-native-reanimated';
 
+import { getAvatarForUser } from 'lib/shared/avatar-utils.js';
 import { createMessageReply } from 'lib/shared/message-utils.js';
 import { assertComposableMessageType } from 'lib/types/message-types.js';
 
@@ -23,6 +24,7 @@ import { MessageHeader } from './message-header.react.js';
 import { useNavigateToSidebar } from './sidebar-navigation.js';
 import SwipeableMessage from './swipeable-message.react.js';
 import { useContentAndHeaderOpacity, useDeliveryIconOpacity } from './utils.js';
+import Avatar from '../components/avatar.react.js';
 import { type InputState, InputStateContext } from '../input/input-state.js';
 import { type Colors, useColors } from '../themes/colors.js';
 import type { ChatMessageInfoItemWithHeight } from '../types/chat-types.js';
@@ -121,6 +123,19 @@ class ComposedMessage extends React.PureComponent<Props> {
       swipeOptions === 'sidebar' || swipeOptions === 'both'
         ? navigateToSidebar
         : undefined;
+
+    let avatar;
+    if (!isViewer && item.endsCluster) {
+      const avatarInfo = getAvatarForUser(item.messageInfo.creator);
+      avatar = (
+        <View style={styles.avatarContainer}>
+          <Avatar size="small" avatarInfo={avatarInfo} />
+        </View>
+      );
+    } else if (!isViewer) {
+      avatar = <View style={styles.avatarOffset} />;
+    }
+
     const messageBox = (
       <View style={styles.messageBox}>
         <SwipeableMessage
@@ -130,9 +145,12 @@ class ComposedMessage extends React.PureComponent<Props> {
           messageBoxStyle={messageBoxStyle}
           threadColor={item.threadInfo.color}
         >
-          <AnimatedView style={{ opacity: contentAndHeaderOpacity }}>
-            {children}
-          </AnimatedView>
+          <View style={styles.swipeableContainer}>
+            {avatar}
+            <AnimatedView style={{ opacity: contentAndHeaderOpacity }}>
+              {children}
+            </AnimatedView>
+          </View>
         </SwipeableMessage>
       </View>
     );
@@ -187,6 +205,12 @@ const styles = StyleSheet.create({
     marginLeft: composedMessageStyle.marginLeft,
     marginRight: composedMessageStyle.marginRight,
   },
+  avatarContainer: {
+    marginRight: 8,
+  },
+  avatarOffset: {
+    width: 32,
+  },
   content: {
     alignItems: 'center',
     flexDirection: 'row-reverse',
@@ -208,6 +232,7 @@ const styles = StyleSheet.create({
   },
   leftInlineEngagement: {
     justifyContent: 'flex-start',
+    marginLeft: 32,
     position: 'relative',
     top: inlineEngagementLeftStyle.topOffset,
   },
@@ -222,6 +247,10 @@ const styles = StyleSheet.create({
     position: 'relative',
     right: inlineEngagementRightStyle.marginRight,
     top: inlineEngagementRightStyle.topOffset,
+  },
+  swipeableContainer: {
+    alignItems: 'flex-end',
+    flexDirection: 'row',
   },
 });
 

native/chat/message-header.react.js
@@ -69,7 +69,7 @@ const unboundStyles = {
     color: 'listBackgroundSecondaryLabel',
     fontSize: 14,
     height: authorNameHeight,
-    marginLeft: 12,
+    marginLeft: 44,
     marginRight: 7,
     paddingHorizontal: 12,
     paddingVertical: 4,

native/chat/multimedia-message-tooltip-button.react.js
@@ -1,9 +1,11 @@
 // @flow
 
 import * as React from 'react';
+import { StyleSheet, View } from 'react-native';
 import Animated from 'react-native-reanimated';
 import EmojiPicker from 'rn-emoji-keyboard';
 
+import { getAvatarForUser } from 'lib/shared/avatar-utils.js';
 import { localIDPrefix } from 'lib/shared/message-utils.js';
 import { useCanCreateReactionFromMessage } from 'lib/shared/reaction-utils.js';
 
@@ -14,6 +16,7 @@ import { useSendReaction } from './reaction-message-utils.js';
 import ReactionSelectionPopover from './reaction-selection-popover.react.js';
 import SidebarInputBarHeightMeasurer from './sidebar-input-bar-height-measurer.react.js';
 import { useAnimatedMessageTooltipButton } from './utils.js';
+import Avatar from '../components/avatar.react.js';
 import type { AppNavigationProp } from '../navigation/app-navigator.react.js';
 import { useSelector } from '../redux/redux-utils.js';
 import { useTooltipActions } from '../tooltip/tooltip-hooks.js';
@@ -151,6 +154,22 @@ function MultimediaMessageTooltipButton(props: Props): React.Node {
     [sendReaction, dismissTooltip],
   );
 
+  const avatarInfo = React.useMemo(
+    () => getAvatarForUser(item.messageInfo.creator),
+    [item.messageInfo.creator],
+  );
+
+  const avatar = React.useMemo(() => {
+    if (item.messageInfo.creator.isViewer) {
+      return null;
+    }
+    return (
+      <View style={styles.avatarContainer}>
+        <Avatar size="small" avatarInfo={avatarInfo} />
+      </View>
+    );
+  }, [avatarInfo, item.messageInfo.creator.isViewer]);
+
   return (
     <>
       <Animated.View style={messageContainerStyle}>
@@ -161,6 +180,7 @@ function MultimediaMessageTooltipButton(props: Props): React.Node {
         <Animated.View style={headerStyle}>
           <MessageHeader item={item} focused={true} display="modal" />
         </Animated.View>
+        {avatar}
         {reactionSelectionPopover}
         {innerMultimediaMessage}
         {inlineEngagement}
@@ -173,5 +193,13 @@ function MultimediaMessageTooltipButton(props: Props): React.Node {
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  avatarContainer: {
+    bottom: 0,
+    left: -32,
+    position: 'absolute',
+  },
+});
 
 export default MultimediaMessageTooltipButton;

native/chat/text-message-tooltip-button.react.js
@@ -1,9 +1,11 @@
 // @flow
 
 import * as React from 'react';
+import { StyleSheet, View } from 'react-native';
 import Animated from 'react-native-reanimated';
 import EmojiPicker from 'rn-emoji-keyboard';
 
+import { getAvatarForUser } from 'lib/shared/avatar-utils.js';
 import { localIDPrefix } from 'lib/shared/message-utils.js';
 import { useCanCreateReactionFromMessage } from 'lib/shared/reaction-utils.js';
 
@@ -16,6 +18,7 @@ import { useSendReaction } from './reaction-message-utils.js';
 import ReactionSelectionPopover from './reaction-selection-popover.react.js';
 import SidebarInputBarHeightMeasurer from './sidebar-input-bar-height-measurer.react.js';
 import { useAnimatedMessageTooltipButton } from './utils.js';
+import Avatar from '../components/avatar.react.js';
 import type { AppNavigationProp } from '../navigation/app-navigator.react.js';
 import { useSelector } from '../redux/redux-utils.js';
 import { useTooltipActions } from '../tooltip/tooltip-hooks.js';
@@ -148,6 +151,22 @@ function TextMessageTooltipButton(props: Props): React.Node {
     [sendReaction, dismissTooltip],
   );
 
+  const avatarInfo = React.useMemo(
+    () => getAvatarForUser(item.messageInfo.creator),
+    [item.messageInfo.creator],
+  );
+
+  const avatar = React.useMemo(() => {
+    if (item.messageInfo.creator.isViewer) {
+      return null;
+    }
+    return (
+      <View style={styles.avatarContainer}>
+        <Avatar size="small" avatarInfo={avatarInfo} />
+      </View>
+    );
+  }, [avatarInfo, item.messageInfo.creator.isViewer]);
+
   return (
     <MessageListContextProvider threadInfo={threadInfo}>
       <SidebarInputBarHeightMeasurer
@@ -158,6 +177,7 @@ function TextMessageTooltipButton(props: Props): React.Node {
         <Animated.View style={headerStyle}>
           <MessageHeader item={item} focused={true} display="modal" />
         </Animated.View>
+        {avatar}
         {reactionSelectionPopover}
         <MessagePressResponderContext.Provider
           value={messagePressResponderContext}
@@ -179,5 +199,13 @@ function TextMessageTooltipButton(props: Props): React.Node {
     </MessageListContextProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  avatarContainer: {
+    bottom: 0,
+    left: -32,
+    position: 'absolute',
+  },
+});
 
 export default TextMessageTooltipButton;

commit fbb54ca792ed1503c7b9692aa4e5b63b20e62558

[native] Clean up ComposedMessage
Summary:
Two minor fixes here for issues introduced in D7067:

1. Rename `messageBoxStyleContainerStyle` to `messageBoxContainerStyle` (typo)
2. Avoid inline styles in favor of using combined style sheet (see `leftChatContainer` and `rightChatContainer`)

Test Plan: Flow

Reviewers: ginsu, atul

Subscribers:

native/chat/composed-message.react.js
@@ -99,11 +99,11 @@ class ComposedMessage extends React.PureComponent<Props> {
       { maxWidth: composedMessageMaxWidth },
     ];
 
-    const messageBoxStyleContainerStyle = [styles.messageBoxContainer];
+    const messageBoxContainerStyle = [styles.messageBoxContainer];
     const positioningStyle = isViewer
-      ? { alignItems: 'flex-end' }
-      : { alignItems: 'flex-start' };
-    messageBoxStyleContainerStyle.push(positioningStyle);
+      ? styles.rightChatContainer
+      : styles.leftChatContainer;
+    messageBoxContainerStyle.push(positioningStyle);
 
     let deliveryIcon = null;
     let failedSendInfo = null;
@@ -152,7 +152,7 @@ class ComposedMessage extends React.PureComponent<Props> {
     }
 
     const messageBox = (
-      <View style={messageBoxStyleContainerStyle}>
+      <View style={messageBoxContainerStyle}>
         <SwipeableMessage
           triggerReply={triggerReply}
           triggerSidebar={triggerSidebar}
@@ -243,12 +243,18 @@ const styles = StyleSheet.create({
   leftChatBubble: {
     justifyContent: 'flex-end',
   },
+  leftChatContainer: {
+    alignItems: 'flex-start',
+  },
   messageBoxContainer: {
     flex: 1,
     marginRight: 5,
   },
   rightChatBubble: {
     justifyContent: 'flex-start',
+  },
+  rightChatContainer: {
+    alignItems: 'flex-end',
   },
   swipeableContainer: {
     alignItems: 'flex-end',