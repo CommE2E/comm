2eaf09a70a6a0013b2cc306576bf5f0b60b4ce64
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-05-01 15:03:36 -0400

    [landing] update header strucutre to use flexbox over grid
    Summary: To update the `Header` component to match the redesigns, I broke it up into three diffs. This is the first diff. This diff removes the grid layout in favor for a flexbox layout. I chose to do this
    
    Test Plan: The header looks and behaves as expected when resizing the window to different widths
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7835

--- landing/header.react.js
+++ landing/header.react.js
@@ -1,7 +1,7 @@
 // @flow
 
 import { faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
+import { faExternalLinkAlt, faBars } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import * as React from 'react';
 import { NavLink } from 'react-router-dom';
@@ -22,34 +22,39 @@ const navLinkProps = {
 
 function Header(): React.Node {
   return (
-    <nav className={css.wrapper}>
-      <NavLink to="/" className={css.logo}>
-        <h1>Comm</h1>
-      </NavLink>
-      <div className={css.page_nav}>
-        <NavLink to="/" exact {...navLinkProps}>
-          App
+    <nav className={css.headerContainer}>
+      <div className={css.headerNavContentContainer}>
+        <NavLink to="/" className={css.logo}>
+          <h1>Comm</h1>
         </NavLink>
-        <NavLink to="/keyservers" exact {...navLinkProps}>
-          Keyserver
-        </NavLink>
-      </div>
-      <div className={css.social_icons_container}>
-        <a href="https://web.comm.app">
-          <div className={css.webapp_icon}>
-            <FontAwesomeIcon icon={faExternalLinkAlt} {...iconProps} />
+        <div className={css.pageNav}>
+          <NavLink to="/" exact {...navLinkProps}>
+            App
+          </NavLink>
+          <NavLink to="/keyservers" exact {...navLinkProps}>
+            Keyserver
+          </NavLink>
+        </div>
+        <div className={css.socialIconsContainer}>
+          <a href="https://web.comm.app">
+            <div className={css.webappIcon}>
+              <FontAwesomeIcon icon={faExternalLinkAlt} {...iconProps} />
+            </div>
+          </a>
+          <a href="https://twitter.com/commdotapp">
+            <div className={css.twitterIcon}>
+              <FontAwesomeIcon icon={faTwitter} {...iconProps} />
+            </div>
+          </a>
+          <a href="https://github.com/CommE2E/comm">
+            <div className={css.githubIcon}>
+              <FontAwesomeIcon icon={faGithub} {...iconProps} />
+            </div>
+          </a>
+          <div className={css.menuIcon}>
+            <FontAwesomeIcon icon={faBars} {...iconProps} />
           </div>
-        </a>
-        <a href="https://twitter.com/commdotapp">
-          <div className={css.twitter_icon}>
-            <FontAwesomeIcon icon={faTwitter} {...iconProps} />
-          </div>
-        </a>
-        <a href="https://github.com/CommE2E/comm">
-          <div className={css.github_icon}>
-            <FontAwesomeIcon icon={faGithub} {...iconProps} />
-          </div>
-        </a>
+        </div>
       </div>
     </nav>
   );
