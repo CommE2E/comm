98fb6d1ddf61f6dd55f49087dd917833b68aba73
marcinwasowicz <marcinkwasowicz@gmail.com>
2023-05-02 16:59:52 +0200

    Implement utilities and database query wrapper to create new olm session on eht keyserver side Differential Revision: https://phab.comm.dev/D7631

---
+++ keyserver/src/creators/olm-session-creator.js
@@ -0,0 +1,47 @@
+// @flow
+import type { Account as OlmAccount } from '@commapp/olm';
+
+import { ServerError } from 'lib/utils/errors.js';
+
+import { dbQuery, SQL } from '../database/database.js';
+import { fetchCallUpdateOlmAccount } from '../updaters/olm-account-updater.js';
+import { createPickledOlmSession } from '../utils/olm-utils.js';
+
+async function createOlmSession(
+  initialEncryptedMessage: string,
+  olmSessionType: 'content' | 'notifications',
+  cookieID: string,
+): Promise<void> {
+  const callback = async (account: OlmAccount, picklingKey: string) => {
+    return await createPickledOlmSession(
+      account,
+      picklingKey,
+      initialEncryptedMessage,
+    );
+  };
+
+  let pickledOlmSession;
+  try {
+    pickledOlmSession = await fetchCallUpdateOlmAccount(
+      olmSessionType,
+      callback,
+    );
+  } catch (e) {
+    throw new ServerError('olm_session_creation_failure');
+  }
+
+  const isContent = olmSessionType === 'content';
+  // We match the native client behavior here where olm session is overwritten
+  // in case it is initialized twice for the same pair of identities
+  await dbQuery(
+    SQL`
+      INSERT INTO olm_sessions (cookie_id, is_content, 
+        version, pickled_olm_session)
+      VALUES (${cookieID}, ${isContent}, 0, ${pickledOlmSession})
+      ON DUPLICATE KEY UPDATE
+        pickled_olm_session = ${pickledOlmSession}
+    `,
+  );
+}
+
+export { createOlmSession };
