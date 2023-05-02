8cb3f8f1e7221ae6937724115c515b10b58ef409
RohanK6 <rohan.khanderia@gmail.com>
2023-05-02 15:30:38 -0400

    [native] Create the toggle pin modal
    Summary:
    When pressing the icon in the tooltip, a modal should pop up on the screen allowing the user to pin / unpin a message. This diff handles the modal creation, but to make it easier to review, the
    actual rendering of the message in the modal will be in the next diff following this one.
    
    I placed the modal in `app-navigator.react.js` since the `TextMessage` component requires the `OverlayContext` to be set, so when rendering the `Message > TextMessage` inside of this modal in the next diff,
    we won't have the `invariant` requiring this context be thrown.
    
    Note: The approach here on `native` follows closely the approach for `web` here: https://phab.comm.dev/D7309
    
    Depends on D7606
    
    Test Plan: Confirm that the modal appears (not rendering the message yet) for messages and multimedia, both that should be pinned and unpinned.
    
    Reviewers: atul, ginsu, tomek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D7607

---
+++ native/utils/toggle-pin-utils.js
@@ -0,0 +1,59 @@
+// @flow
+
+import { StackActions, useNavigation } from '@react-navigation/native';
+import * as React from 'react';
+
+import type { OverlayContextType } from '../navigation/overlay-context.js';
+import {
+  type NavigationRoute,
+  PinnableMessageTooltipRouteNames,
+  TogglePinModalRouteName,
+} from '../navigation/route-names.js';
+
+function useNavigateToPinModal(
+  overlayContext: ?OverlayContextType,
+  route:
+    | NavigationRoute<'TextMessageTooltipModal'>
+    | NavigationRoute<'MultimediaMessageTooltipModal'>,
+): () => mixed {
+  const navigation = useNavigation();
+
+  const { params } = route;
+  const { item } = params;
+  const { threadInfo } = item;
+
+  return React.useCallback(() => {
+    // Since the most recent overlay is the tooltip modal, prior to opening the
+    // toggle pin modal, we want to dismiss it so the overlay is not visible
+    // once the toggle pin modal is closed. This is also necessary with the
+    // TextMessageTooltipModal, since otherwise the toggle pin modal fails to
+    // render the message since we 'hide' the original message and
+    // show another message on top when the tooltip is active, and this
+    // state carries through into the modal.
+    const mostRecentOverlay = overlayContext?.visibleOverlays?.slice(-1)[0];
+    const routeName = mostRecentOverlay?.routeName;
+    const routeKey = mostRecentOverlay?.routeKey;
+
+    // If there is not a valid routeKey or the most recenet routeName is
+    // not included in PinnableMessageTooltipRouteNames, we want to
+    // just navigate to the toggle pin modal as normal.
+    if (!routeKey || !PinnableMessageTooltipRouteNames.includes(routeName)) {
+      navigation.navigate(TogglePinModalRouteName, {
+        threadInfo,
+        item,
+      });
+      return;
+    }
+
+    // Otherwise, we want to replace the tooltip overlay with the pin modal.
+    navigation.dispatch({
+      ...StackActions.replace(TogglePinModalRouteName, {
+        threadInfo,
+        item,
+      }),
+      source: routeKey,
+    });
+  }, [navigation, overlayContext, threadInfo, item]);
+}
+
+export { useNavigateToPinModal };
