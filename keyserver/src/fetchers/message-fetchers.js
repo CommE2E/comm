2409cc34c2e5d30a4dbf7dffb23717df6bb963b8
Inka Sokołowska <inka.sokolowska@swmansion.com>
2023-05-03 10:14:58 +0200

    [keyserver, lib] Add message search endpoint
    Summary:
    issue: https://linear.app/comm/issue/ENG-3170/create-a-keyserver-endpoint-for-the-message-search-within-specific
    This endpoint allows searching messages. For now we can only search in one thread. It will have to be changed a bit in the future to be able to search in many threads, but  we don't yet know how we will want that to work.
    
    Test Plan: Tested with the next diff
    
    Reviewers: kamil, bartek, kuba
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7651

--- keyserver/src/fetchers/message-fetchers.js
+++ keyserver/src/fetchers/message-fetchers.js
@@ -25,6 +25,7 @@ import {
   type FetchPinnedMessagesRequest,
   type FetchPinnedMessagesResult,
   isMessageSidebarSourceReactionOrEdit,
+  type SearchMessagesResponse,
 } from 'lib/types/message-types.js';
 import { defaultNumberPerThread } from 'lib/types/message-types.js';
 import { threadPermissions } from 'lib/types/thread-types.js';
@@ -908,17 +909,17 @@ async function rawMessageInfoForRowsAndRelatedMessages(
   return [...rawMessageInfos, ...rawRelatedMessageInfos];
 }
 
-const searchMessagesPageSize: number = defaultNumberPerThread + 1;
+const searchMessagesPageSize = defaultNumberPerThread + 1;
 
 async function searchMessagesInSingleChat(
   inputQuery: string,
   threadID: string,
   viewer?: Viewer,
   cursor?: string,
-): Promise<$ReadOnlyArray<RawMessageInfo>> {
+): Promise<SearchMessagesResponse> {
   if (inputQuery === '') {
     console.warn('received empty search query');
-    return [];
+    return { messages: [], endReached: true };
   }
   const pattern = processQueryForSearch(inputQuery);
 
@@ -948,18 +949,25 @@ async function searchMessagesInSingleChat(
 
   const [results] = await dbQuery(query);
   if (results.length === 0) {
-    return [];
+    return { messages: [], endReached: true };
   }
 
+  const endReached = results.length < searchMessagesPageSize;
+
+  const resultsPage = endReached ? results : results.slice(0, -1);
+
   const rawMessageInfos = await rawMessageInfoForRowsAndRelatedMessages(
-    results,
+    resultsPage,
     viewer,
   );
 
-  return shimUnsupportedRawMessageInfos(
-    rawMessageInfos,
-    viewer?.platformDetails,
-  );
+  return {
+    messages: shimUnsupportedRawMessageInfos(
+      rawMessageInfos,
+      viewer?.platformDetails,
+    ),
+    endReached: endReached,
+  };
 }
 
 export {
@@ -976,5 +984,4 @@ export {
   fetchRelatedMessages,
   rawMessageInfoForRowsAndRelatedMessages,
   searchMessagesInSingleChat,
-  searchMessagesPageSize,
 };
