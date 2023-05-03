56dfbfbbc6a447b7ec4d281d307b2e3e8c3743bf
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2023-05-03 11:25:16 +0200

    [native] Add C++ function to decode base64
    Summary:
    The thumbhash bytes will be stored as base64 string so we need a function to decode it.
    
    This code, similarly to the encode function, is based on [[ https://matgomes.com/base64-encode-decode-cpp/ | this article ]], with slight modifications.
    
    Test Plan: This will be tested together with the next diff.
    
    Reviewers: kamil, inka, marcin
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7758

--- native/cpp/CommonCpp/Tools/Base64.h
+++ native/cpp/CommonCpp/Tools/Base64.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <string_view>
 #include <vector>
 
 namespace comm {
@@ -8,6 +9,7 @@ namespace comm {
 class Base64 {
 public:
   static std::string encode(const std::vector<uint8_t> &data);
+  static std::vector<uint8_t> decode(const std::string_view base64String);
 };
 
 } // namespace comm
