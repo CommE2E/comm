b76141925bbf9f688a7f49a6d4c46d8ae4be4c83
Jonathan Ringer <jonathan.ringer@comm.app>
2023-05-04 10:42:21 -0700

    [Tunnelbroker] Define undelivered-messages table in rust
    Summary:
    Describe the table in rust.
    Part of: https://linear.app/comm/issue/ENG-3822
    
    Test Plan: N/A, this is able design
    
    Reviewers: varun, bartek, ashoat
    
    Reviewed By: varun, ashoat
    
    Subscribers: tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7720

--- services/tunnelbroker/src/constants.rs
+++ services/tunnelbroker/src/constants.rs
@@ -7,3 +7,22 @@ pub const GRPC_KEEP_ALIVE_PING_TIMEOUT: Duration = Duration::from_secs(10);
 
 pub const LOG_LEVEL_ENV_VAR: &str =
   tracing_subscriber::filter::EnvFilter::DEFAULT_ENV;
+
+pub mod dynamodb {
+  // This table holds messages which could not be immediately delivered to
+  // a device.
+  //
+  // - (primary key) = (deviceID: Partition Key, createdAt: Sort Key)
+  // - deviceID: The public key of a device's olm identity key
+  // - payload: Message to be delivered. See shared/tunnelbroker_messages.
+  // - createdAt: UNIX timestamp of when the item was inserted.
+  //     Timestamp is needed to order the messages correctly to the device.
+  pub mod undelivered_messages {
+    pub const TABLE_NAME: &str = "tunnelbroker-undelivered-messages";
+    pub const PARTITION_KEY: &str = "deviceID";
+    pub const DEVICE_ID: &str = "deviceID";
+    pub const PAYLOAD: &str = "payload";
+    pub const CREATED_AT: &str = "createdAt";
+    pub const SORT_KEY: &str = "createdAt";
+  }
+}
