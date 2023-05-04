b2230a871aeda38baa6a0b5703b554192981bc43
Jonathan Ringer <jonathan.ringer@comm.app>
2023-05-04 08:22:16 -0700

    [Tunnelbroker] Add aws-sdk support, and cli configuration
    Summary:
    Add ability to target localstack endpoint, and
    add initial support for aws-sdk.
    
    Part of:
    - https://linear.app/comm/issue/ENG-3822
    - https://linear.app/comm/issue/ENG-3671
    
    Test Plan:
    ```
    cd services/tunnelbroker
    # --help should emit usage now
    cargo run -- --help
    ```
    
    Reviewers: bartek, varun
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7715

---
+++ services/tunnelbroker/src/config.rs
@@ -0,0 +1,51 @@
+use crate::constants;
+use anyhow::{ensure, Result};
+use clap::Parser;
+use once_cell::sync::Lazy;
+use tracing::info;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+pub struct AppConfig {
+  /// gRPC server listening port
+  #[arg(long, default_value_t = constants::GRPC_SERVER_PORT)]
+  pub grpc_port: u16,
+  /// HTTP server listening port
+  #[arg(long, default_value_t = 51001)]
+  pub http_port: u16,
+  /// AWS Localstack service URL
+  #[arg(env = "LOCALSTACK_ENDPOINT")]
+  #[arg(long)]
+  pub localstack_endpoint: Option<String>,
+}
+
+/// Stores configuration parsed from command-line arguments
+/// and environment variables
+pub static CONFIG: Lazy<AppConfig> = Lazy::new(AppConfig::parse);
+
+/// Processes the command-line arguments and environment variables.
+/// Should be called at the beginning of the `main()` function.
+pub(super) fn parse_cmdline_args() -> Result<()> {
+  // force evaluation of the lazy initialized config
+  let cfg = Lazy::force(&CONFIG);
+
+  // Perform some additional validation for CLI args
+  ensure!(
+    cfg.grpc_port != cfg.http_port,
+    "gRPC and HTTP ports cannot be the same: {}",
+    cfg.grpc_port
+  );
+  Ok(())
+}
+
+/// Provides region/credentials configuration for AWS SDKs
+pub async fn load_aws_config() -> aws_config::SdkConfig {
+  let mut config_builder = aws_config::from_env();
+
+  if let Some(endpoint) = &CONFIG.localstack_endpoint {
+    info!("Using localstack URL: {}", endpoint);
+    config_builder = config_builder.endpoint_url(endpoint);
+  }
+
+  config_builder.load().await
+}
--- services/tunnelbroker/src/main.rs
+++ services/tunnelbroker/src/main.rs
@@ -1,9 +1,11 @@
+pub mod config;
 pub mod constants;
 pub mod grpc;
 pub mod websockets;
+use anyhow::{anyhow, Result};
+use config::CONFIG;
 use dashmap::DashMap;
 use once_cell::sync::Lazy;
-use std::io::{self, Error, ErrorKind};
 use tokio::sync::mpsc::UnboundedSender;
 use tracing::{self, Level};
 use tracing_subscriber::EnvFilter;
@@ -12,7 +14,7 @@ pub static ACTIVE_CONNECTIONS: Lazy<DashMap<String, UnboundedSender<String>>> =
   Lazy::new(DashMap::new);
 
 #[tokio::main]
-async fn main() -> Result<(), io::Error> {
+async fn main() -> Result<()> {
   let filter = EnvFilter::builder()
     .with_default_directive(Level::INFO.into())
     .with_env_var(constants::LOG_LEVEL_ENV_VAR)
@@ -22,6 +24,9 @@ async fn main() -> Result<(), io::Error> {
   tracing::subscriber::set_global_default(subscriber)
     .expect("Unable to configure tracing");
 
+  config::parse_cmdline_args()?;
+  let _aws_config = config::load_aws_config().await;
+
   let grpc_server = grpc::run_server();
   let websocket_server = websockets::run_server();
 
@@ -30,7 +35,7 @@ async fn main() -> Result<(), io::Error> {
     Ok(_) = websocket_server => { Ok(()) },
     else => {
       tracing::error!("A grpc or websocket server crashed.");
-      Err(Error::new(ErrorKind::Other, "A grpc or websocket server crashed."))
+      Err(anyhow!("A grpc or websocket server crashed."))
     }
   }
 }
