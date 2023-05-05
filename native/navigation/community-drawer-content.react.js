d163b608179c4b58c3dcbd46df34c353aab2b341
Tomasz Palys <tomasz.palys@swmansion.com>
2023-05-05 14:40:20 +0200

    [native] Display a button and navigate to invite links if they are present
    Summary:
    When a community has a primary link, display three dots button next to it. The links are updated on every drawer open. Clicking this link shows a button that can be used to open a modal with the links.
    
    Depends on D7730
    
    Test Plan: Create a primary link for one community. Check if the button is displayed next to it. Click the button and open invite links modal - it should contain the link.
    
    Reviewers: bartek, kamil, inka
    
    Subscribers: ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D7733

--- native/navigation/community-drawer-content.react.js
+++ native/navigation/community-drawer-content.react.js
@@ -1,14 +1,26 @@
 // @flow
 
+import { useDrawerStatus } from '@react-navigation/drawer';
+import { useNavigation } from '@react-navigation/native';
 import * as React from 'react';
 import { FlatList, Platform } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useSelector } from 'react-redux';
 
 import {
+  fetchPrimaryInviteLinkActionTypes,
+  fetchPrimaryInviteLinks,
+} from 'lib/actions/link-actions.js';
+import {
   childThreadInfos,
   communityThreadSelector,
 } from 'lib/selectors/thread-selectors.js';
+import type { InviteLink } from 'lib/types/link-types';
+import type { ThreadInfo } from 'lib/types/thread-types';
+import {
+  useDispatchActionPromise,
+  useServerCall,
+} from 'lib/utils/action-utils.js';
 import {
   createRecursiveDrawerItemsData,
   appendSuffix,
@@ -16,6 +28,10 @@ import {
 import { useResolvedThreadInfos } from 'lib/utils/entity-helpers.js';
 
 import CommunityDrawerItemCommunity from './community-drawer-item-community.react.js';
+import {
+  InviteLinkNavigatorRouteName,
+  ViewInviteLinksRouteName,
+} from './route-names.js';
 import { useNavigateToThread } from '../chat/message-list-types.js';
 import { useStyles } from '../themes/colors.js';
 
@@ -34,6 +50,30 @@ function CommunityDrawerContent(): React.Node {
   );
   const styles = useStyles(unboundStyles);
 
+  const [inviteLinks, setInviteLinks] = React.useState(null);
+  const callFetchPrimaryLinks = useServerCall(fetchPrimaryInviteLinks);
+  const dispatchActionPromise = useDispatchActionPromise();
+  const drawerStatus = useDrawerStatus();
+  React.useEffect(() => {
+    (async () => {
+      if (drawerStatus !== 'open') {
+        return;
+      }
+      const createFetchPrimaryLinksPromise = async () => {
+        const response = await callFetchPrimaryLinks();
+        const links = {};
+        for (const link of response.links) {
+          links[link.communityID] = link;
+        }
+        setInviteLinks(links);
+      };
+      await dispatchActionPromise(
+        fetchPrimaryInviteLinkActionTypes,
+        createFetchPrimaryLinksPromise(),
+      );
+    })();
+  }, [callFetchPrimaryLinks, dispatchActionPromise, drawerStatus]);
+
   const [openCommunity, setOpenCommunity] = React.useState(
     communitiesSuffixed.length === 1 ? communitiesSuffixed[0].id : null,
   );
@@ -45,6 +85,20 @@ function CommunityDrawerContent(): React.Node {
     setOpenCommunity(open => (open === index ? null : index));
   }, []);
 
+  const { navigate } = useNavigation();
+  const navigateToInviteLinksView = React.useCallback(
+    (community: ThreadInfo, inviteLink: InviteLink) => {
+      navigate<'InviteLinkNavigator'>(InviteLinkNavigatorRouteName, {
+        screen: ViewInviteLinksRouteName,
+        params: {
+          community,
+          inviteLink,
+        },
+      });
+    },
+    [navigate],
+  );
+
   const renderItem = React.useCallback(
     ({ item }) => (
       <CommunityDrawerItemCommunity
@@ -53,9 +107,15 @@ function CommunityDrawerContent(): React.Node {
         toggleExpanded={setOpenCommunityOrClose}
         expanded={item.threadInfo.id === openCommunity}
         navigateToThread={navigateToThread}
+        navigateToInviteLinksView={navigateToInviteLinksView}
       />
     ),
-    [navigateToThread, openCommunity, setOpenCommunityOrClose],
+    [
+      navigateToInviteLinksView,
+      navigateToThread,
+      openCommunity,
+      setOpenCommunityOrClose,
+    ],
   );
 
   const labelStylesObj = useStyles(labelUnboundStyles);
@@ -75,8 +135,9 @@ function CommunityDrawerContent(): React.Node {
         communitiesSuffixed,
         labelStyles,
         maxDepth,
+        inviteLinks,
       ),
-    [childThreadInfosMap, communitiesSuffixed, labelStyles],
+    [childThreadInfosMap, communitiesSuffixed, inviteLinks, labelStyles],
   );
 
   return (
