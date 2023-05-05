7b9d280ef4f77573d153628077eca29862f1d224
Ashoat Tevosyan <ashoat@comm.app>
2023-05-05 11:58:56 -0400

    [native] Add Ethereum logo to ConnectEthereum screen
    Summary: See [designs](https://www.figma.com/file/a1nkbWgbgjRlrOY9LVurTz/Comm-%2F-Desktop-app?node-id=11725%3A252688&t=jhvps5aBDXMy4HNn-1) for context. I'll handle vertically centering it a later diff.
    
    Depends on D7729
    
    Test Plan: [screenshot]
    
    Reviewers: atul, ginsu
    
    Subscribers:

---
+++ native/vectors/ethereum-logo-dark.react.js
@@ -0,0 +1,50 @@
+// @flow
+
+import * as React from 'react';
+import Svg, { G, Path, Defs, ClipPath, Rect } from 'react-native-svg';
+
+function EthereumLogoDark(): React.Node {
+  return (
+    <Svg
+      width="114"
+      height="186"
+      viewBox="0 0 114 186"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <G clip-path="url(#clip0_11725_257565)">
+        <Path
+          d="M56.9824 0L55.7378 4.23741V127.197L56.9824 128.442L113.966 94.7039L56.9824 0Z"
+          fill="#4F4F4F"
+        />
+        <Path
+          d="M56.9831 0L0 94.7039L56.9831 128.442V68.7611V0Z"
+          fill="#A3A3A3"
+        />
+        <Path
+          d="M56.9826 139.248L56.2812 140.105V183.906L56.9826 185.958L114 105.527L56.9826 139.248Z"
+          fill="#595958"
+        />
+        <Path
+          d="M56.9831 185.958V139.248L0 105.527L56.9831 185.958Z"
+          fill="#A3A3A3"
+        />
+        <Path
+          d="M56.9829 128.442L113.965 94.7045L56.9829 68.7617V128.442Z"
+          fill="#303030"
+        />
+        <Path
+          d="M0.000488281 94.7045L56.9827 128.442V68.7617L0.000488281 94.7045Z"
+          fill="#545454"
+        />
+      </G>
+      <Defs>
+        <ClipPath id="clip0_11725_257565">
+          <Rect width="114" height="186" fill="white" />
+        </ClipPath>
+      </Defs>
+    </Svg>
+  );
+}
+
+export default EthereumLogoDark;
