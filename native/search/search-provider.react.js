3033a3cd4203ce525d4557db07325b95df19aae4
Inka Sokołowska <inka.sokolowska@swmansion.com>
2023-05-08 11:12:58 +0200

    [native] Add message search context
    Summary:
    issue: https://linear.app/comm/issue/ENG-3163/create-the-search-screen-for-message-searching
    Since the query has to be passed between the search screen and the header component, that is passed in screen options, I created a context to hold the query
    
    Test Plan:
    tested with next diffs
    
    Reviewers:
    kuba, kamil, bartek
    Subscribers:

---
+++ native/search/search-provider.react.js
@@ -0,0 +1,41 @@
+// @flow
+import * as React from 'react';
+
+import type { SetState } from 'lib/types/hook-types.js';
+
+export type MessageSearchContextType = {
+  +query: string,
+  +setQuery: SetState<string>,
+  +clearQuery: () => void,
+};
+
+const MessageSearchContext: React.Context<?MessageSearchContextType> =
+  React.createContext<?MessageSearchContextType>();
+
+type Props = {
+  +children: React.Node,
+};
+function MessageSearchProvider(props: Props): React.Node {
+  const { children } = props;
+
+  const [query, setQuery] = React.useState<string>('');
+
+  const clearQuery = React.useCallback(() => setQuery(''), []);
+
+  const context = React.useMemo(
+    () => ({
+      query,
+      setQuery,
+      clearQuery,
+    }),
+    [query, clearQuery],
+  );
+
+  return (
+    <MessageSearchContext.Provider value={context}>
+      {children}
+    </MessageSearchContext.Provider>
+  );
+}
+
+export { MessageSearchContext, MessageSearchProvider };
