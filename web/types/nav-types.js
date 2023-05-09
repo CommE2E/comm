33b84e9df2f41a3fbd7961e3f46fcda5f1ede021
Michal Gniadek <michal.gniadek@swmansion.com>
2023-05-09 11:31:15 +0200

    [web] Validate website responder
    Summary:
    Validate website responder by adding an intial redux state validator. It's not a full (Web)`AppState` validator because we don't need it for this purpose.
    
    Depends on D7785
    
    Test Plan: Load the web app and check if there were any errors. Repeat with a few other accounts.
    
    Reviewers: kamil, tomek
    
    Subscribers: ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D7786

--- web/types/nav-types.js
+++ web/types/nav-types.js
@@ -1,13 +1,25 @@
 // @flow
+import t from 'tcomb';
+import type { TInterface } from 'tcomb';
 
-import type { BaseNavInfo } from 'lib/types/nav-types.js';
-import type { ThreadInfo } from 'lib/types/thread-types.js';
+import { type BaseNavInfo } from 'lib/types/nav-types.js';
+import {
+  type ThreadInfo,
+  threadInfoValidator,
+} from 'lib/types/thread-types.js';
+import { tID, tShape } from 'lib/utils/validation-utils.js';
 
 export type NavigationTab = 'calendar' | 'chat' | 'settings';
+const navigationTabValidator = t.enums.of(['calendar', 'chat', 'settings']);
 
 export type NavigationSettingsSection = 'account' | 'danger-zone';
+const navigationSettingsSectionValidator = t.enums.of([
+  'account',
+  'danger-zone',
+]);
 
 export type NavigationChatMode = 'view' | 'create';
+const navigationChatModeValidator = t.enums.of(['view', 'create']);
 
 export type NavInfo = {
   ...$Exact<BaseNavInfo>,
@@ -18,3 +30,14 @@ export type NavInfo = {
   +selectedUserList?: $ReadOnlyArray<string>,
   +chatMode?: NavigationChatMode,
 };
+
+export const navInfoValidator: TInterface<NavInfo> = tShape<$Exact<NavInfo>>({
+  startDate: t.String,
+  endDate: t.String,
+  tab: navigationTabValidator,
+  activeChatThreadID: t.maybe(tID),
+  pendingThread: t.maybe(threadInfoValidator),
+  settingsSection: t.maybe(navigationSettingsSectionValidator),
+  selectedUserList: t.maybe(t.list(t.String)),
+  chatMode: t.maybe(navigationChatModeValidator),
+});
