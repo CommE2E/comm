0b1d64472d94c7c004d3387e5df92995b4da2967
Tomasz Palys <tomasz.palys@swmansion.com>
2023-05-09 16:37:30 +0200

    [keyserver] Create an endpoint that can be used to create or update a public link
    Summary:
    If a community doesn't have a public invite link - it will be created. If it has one, it will be updated.
    
    Depends on D7762
    
    Test Plan: Create a new community, create a link for it using manage public link screen (later in the stack) - a link should be created. Update it, using the same screen - it should be updated.
    
    Reviewers: bartek, kamil, inka
    
    Subscribers: ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D7878

---
+++ keyserver/src/creators/invite-link-creator.js
@@ -0,0 +1,121 @@
+// @flow
+
+import type {
+  CreateOrUpdatePublicLinkRequest,
+  InviteLink,
+} from 'lib/types/link-types.js';
+import { threadPermissions } from 'lib/types/thread-permission-types.js';
+import { ServerError } from 'lib/utils/errors.js';
+
+import createIDs from './id-creator.js';
+import { dbQuery, SQL } from '../database/database.js';
+import { fetchPrimaryInviteLinks } from '../fetchers/link-fetchers.js';
+import { fetchServerThreadInfos } from '../fetchers/thread-fetchers.js';
+import { checkThreadPermission } from '../fetchers/thread-permission-fetchers.js';
+import { Viewer } from '../session/viewer.js';
+
+const secretRegex = /^[a-zA-Z0-9]+$/;
+
+async function createOrUpdatePublicLink(
+  viewer: Viewer,
+  request: CreateOrUpdatePublicLinkRequest,
+): Promise<InviteLink> {
+  if (!secretRegex.test(request.name)) {
+    throw new ServerError('invalid_parameters');
+  }
+
+  const permissionPromise = checkThreadPermission(
+    viewer,
+    request.communityID,
+    threadPermissions.MANAGE_INVITE_LINKS,
+  );
+  const existingPrimaryLinksPromise = fetchPrimaryInviteLinks(viewer);
+  const fetchThreadInfoPromise = fetchServerThreadInfos(
+    SQL`t.id = ${request.communityID}`,
+  );
+  const [hasPermission, existingPrimaryLinks, { threadInfos }] =
+    await Promise.all([
+      permissionPromise,
+      existingPrimaryLinksPromise,
+      fetchThreadInfoPromise,
+    ]);
+  if (!hasPermission) {
+    throw new ServerError('invalid_credentials');
+  }
+  const threadInfo = threadInfos[request.communityID];
+  if (!threadInfo) {
+    throw new ServerError('invalid_parameters');
+  }
+  const defaultRoleID = Object.keys(threadInfo.roles).find(
+    roleID => threadInfo.roles[roleID].isDefault,
+  );
+  if (!defaultRoleID) {
+    throw new ServerError('invalid_parameters');
+  }
+
+  const existingPrimaryLink = existingPrimaryLinks.find(
+    link => link.communityID === request.communityID && link.primary,
+  );
+  if (existingPrimaryLink) {
+    const query = SQL`
+      UPDATE invite_links
+      SET name = ${request.name}
+      WHERE \`primary\` = 1 AND community = ${request.communityID}
+    `;
+    try {
+      await dbQuery(query);
+    } catch {
+      throw new ServerError('invalid_parameters');
+    }
+    return {
+      name: request.name,
+      primary: true,
+      role: defaultRoleID,
+      communityID: request.communityID,
+      expirationTime: 0,
+      limitOfUses: null,
+      numberOfUses: 0,
+    };
+  }
+
+  const [id] = await createIDs('invite_links', 1);
+
+  const row = [id, request.name, true, request.communityID, defaultRoleID];
+
+  const createLinkQuery = SQL`
+    INSERT INTO invite_links(id, name, \`primary\`, community, role)
+    SELECT ${row}
+    WHERE NOT EXISTS (
+      SELECT i.id
+      FROM invite_links i
+      WHERE i.\`primary\` = 1 AND i.community = ${request.communityID}
+    )
+  `;
+  let result = null;
+  try {
+    result = (await dbQuery(createLinkQuery))[0];
+  } catch {
+    throw new ServerError('invalid_parameters');
+  }
+
+  if (result.affectedRows === 0) {
+    const deleteIDs = SQL`
+        DELETE FROM ids
+        WHERE id = ${id}
+      `;
+    await dbQuery(deleteIDs);
+    throw new ServerError('invalid_parameters');
+  }
+
+  return {
+    name: request.name,
+    primary: true,
+    role: defaultRoleID,
+    communityID: request.communityID,
+    expirationTime: 0,
+    limitOfUses: null,
+    numberOfUses: 0,
+  };
+}
+
+export { createOrUpdatePublicLink };
