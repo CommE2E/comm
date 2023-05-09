6ffe1fb799d2dbb3c8459b61e0113dc6c814891e
Inka Sokołowska <inka.sokolowska@swmansion.com>
2023-05-09 12:15:29 +0200

    [native] Add a header componnent with a search bar
    Summary:
    issue: https://linear.app/comm/issue/ENG-3405/create-a-header-with-the-search-input-field
    This will be used in the message search screen. Some design changes were discussed by me and Ted, and noted on Linear.
    
    Test Plan: Tested with next diffs
    
    Reviewers: kuba, kamil, bartek
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7740

---
+++ native/search/search-bar.react.js
@@ -0,0 +1,110 @@
+//@flow
+
+import * as React from 'react';
+import { TextInput, TouchableOpacity, View } from 'react-native';
+
+import SWMansionIcon from '../components/swmansion-icon.react.js';
+import { useStyles } from '../themes/colors.js';
+
+type Props = {
+  onPressSend: (text: string) => void,
+  onClear: () => void,
+};
+
+function SearchBox(props: Props): React.Node {
+  const { onPressSend, onClear } = props;
+
+  const [text, setText] = React.useState('');
+
+  const onEndEditing = React.useCallback(
+    () => onPressSend(text),
+    [onPressSend, text],
+  );
+
+  const onClearSearchFiled = React.useCallback(() => {
+    onClear();
+    setText('');
+  }, [onClear]);
+
+  const styles = useStyles(unboundStyles);
+
+  const clearButton = React.useMemo(() => {
+    if (text.length !== 0) {
+      return (
+        <TouchableOpacity
+          onPress={onClearSearchFiled}
+          style={styles.cancelButton}
+        >
+          <SWMansionIcon
+            name="cross"
+            size={11}
+            style={styles.cancelButtonIcon}
+          />
+        </TouchableOpacity>
+      );
+    }
+    return null;
+  }, [
+    onClearSearchFiled,
+    styles.cancelButton,
+    styles.cancelButtonIcon,
+    text.length,
+  ]);
+
+  return (
+    <View style={styles.container}>
+      <SWMansionIcon name="search" size={24} style={styles.button} />
+
+      <View style={styles.wrapper}>
+        <TextInput
+          value={text}
+          onChangeText={setText}
+          placeholder="Search"
+          style={styles.searchBoxTextStyle}
+          placeholderTextColor="#808080"
+          onEndEditing={onEndEditing}
+          returnKeyType="search"
+        />
+      </View>
+      {clearButton}
+    </View>
+  );
+}
+
+const unboundStyles = {
+  container: {
+    backgroundColor: 'searchMessagesInputFieldBackground',
+    borderRadius: 8,
+    paddingHorizontal: 11,
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  searchBoxTextStyle: {
+    color: 'listForegroundLabel',
+    fontSize: 16,
+    fontWeight: '400',
+    placeholderTextColor: 'searchMessagesSecondaryForeground',
+    marginHorizontal: 8,
+    marginVertical: 6,
+  },
+  button: {
+    color: 'searchMessagesSecondaryForeground',
+  },
+  cancelButton: {
+    backgroundColor: 'modalButton',
+    borderRadius: 56,
+    width: 18,
+    height: 18,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  cancelButtonIcon: {
+    color: 'modalButtonLabel',
+  },
+  wrapper: {
+    flex: 1,
+  },
+};
+
+export default SearchBox;
