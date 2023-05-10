f95e4878ca00046b0eb8225d53c16fd8a1e8ba06
Jonathan Ringer <jonathan.ringer@comm.app>
2023-05-09 23:09:05 -0700

    [Tunnelbroker] Initial message persistence
    Summary:
    Allow for messages to be enqueued to dynamoDB when the
    device is not available. Also, deliver messages once a connection with
    said device becomes available.
    
    https://linear.app/comm/issue/ENG-3822
    
    Test Plan:
    ```
    nix develop
    
    # init localstack and run terraform
    comm-dev services start
    (cd services/terraform && ./run.sh)
    
    (cd services/tunnelbroker && cargo run &)
    (cd services/commtest && cargo test --test tunnelbroker_integration_test)
    ```
    
    Reviewers: bartek, varun
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7799

--- services/tunnelbroker/src/websockets/session.rs
+++ services/tunnelbroker/src/websockets/session.rs
@@ -1,26 +1,58 @@
 use tracing::debug;
 use tunnelbroker_messages::Messages;
 
-use crate::ACTIVE_CONNECTIONS;
+use crate::{
+  constants::dynamodb::undelivered_messages::CREATED_AT,
+  database::DatabaseClient, ACTIVE_CONNECTIONS,
+};
 
 pub struct WebsocketSession {
   tx: tokio::sync::mpsc::UnboundedSender<std::string::String>,
+  db_client: DatabaseClient,
 }
 
 impl WebsocketSession {
   pub fn new(
     tx: tokio::sync::mpsc::UnboundedSender<std::string::String>,
+    db_client: DatabaseClient,
   ) -> WebsocketSession {
-    WebsocketSession { tx }
+    WebsocketSession { tx, db_client }
   }
 
-  pub fn handle_message_from_device(
+  pub async fn handle_message_from_device(
     &self,
     message: &str,
   ) -> Result<(), serde_json::Error> {
     match serde_json::from_str::<Messages>(message)? {
       Messages::SessionRequest(session_info) => {
-        ACTIVE_CONNECTIONS.insert(session_info.device_id, self.tx.clone());
+        // TODO: Authenticate device using auth token
+        // Check for persisted messages
+        let messages = self
+          .db_client
+          .retrieve_messages(&session_info.device_id)
+          .await
+          .expect("Failed to retreive messages");
+
+        ACTIVE_CONNECTIONS
+          .insert(session_info.device_id.clone(), self.tx.clone());
+
+        for message in messages {
+          let payload =
+            message.get("payload").unwrap().as_s().unwrap().to_string();
+          self
+            .tx
+            .send(payload)
+            .expect("Failed to send message to client");
+          let created_at =
+            message.get(CREATED_AT).unwrap().as_n().unwrap().to_string();
+          self
+            .db_client
+            .delete_message(&session_info.device_id, &created_at)
+            .await
+            .expect("Failed to delete messages");
+        }
+
+        debug!("Flushed messages for device: {}", &session_info.device_id);
       }
       _ => {
         debug!("Received invalid request");
