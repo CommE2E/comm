80172738eae3e8bfea8cd5f817b1c6cf5ba54908
Inka Sokołowska <inka.sokolowska@swmansion.com>
2023-05-10 10:04:12 +0200

    [natvie] Add Statement component
    Summary:
    issue: https://linear.app/comm/issue/ENG-3842/add-the-end-of-results-and-no-results-messages
    On the search screen there are different messages that appear at the end of the results (like "End of results") or instead of them (like "No results, ..."). They are all styled the same, so I created a component for them
    
    Test Plan: Displayed the coponent with some message. Checked that it looks as it should.
    
    Reviewers: kuba, kamil, bartek
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7772

---
+++ native/search/statement.react.js
@@ -0,0 +1,31 @@
+// @flow
+
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+import { useStyles } from '../themes/colors.js';
+
+function SearchFooter(props: { text: string }): React.Node {
+  const styles = useStyles(unboundStyles);
+
+  return (
+    <View style={styles.messageWrapper}>
+      <Text style={styles.message}>{props.text}</Text>
+    </View>
+  );
+}
+
+const unboundStyles = {
+  message: {
+    color: 'searchMessagesSecondaryForeground',
+    size: 14,
+    fontWeight: '400',
+    textAlign: 'center',
+  },
+  messageWrapper: {
+    paddingVertical: 24,
+    display: 'flex',
+  },
+};
+
+export default SearchFooter;
