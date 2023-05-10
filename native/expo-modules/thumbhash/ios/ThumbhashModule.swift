991a876865aaa90e62dd97c55849fd883e7426e1
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2023-05-10 14:31:28 +0200

    [native] Implement thumbhash generation on iOS
    Summary:
    Implemented the `generateThumbHash()` function on iOS.
    Used code from the thumbhash repository: https://github.com/evanw/thumbhash
    
    Depends on D7778
    
    Test Plan: One easy way is to hook this to expo-image-picker result (e.g. avatar picker) and provide picked uri to the function. It should usually return a 43 char long string (sometimes longer if there is alpha channel).
    
    Reviewers: atul, marcin, ashoat
    
    Reviewed By: atul, ashoat
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D7779

--- native/expo-modules/thumbhash/ios/ThumbhashModule.swift
+++ native/expo-modules/thumbhash/ios/ThumbhashModule.swift
@@ -4,8 +4,25 @@ public class ThumbhashModule: Module {
   public func definition() -> ModuleDefinition {
     Name("Thumbhash")
 
-    AsyncFunction("generateThumbHash") { () -> String in
-      "unimplemented"
-    }
+    AsyncFunction("generateThumbHash", generateThumbHash)
+  }
+}
+
+// MARK: - Function implementations
+
+private func generateThumbHash(photoURI: URL) throws -> String {
+  let imageData = try Data(contentsOf: photoURI)
+  guard let image = UIImage(data: imageData) else {
+    throw LoadingFailureException(photoURI.absoluteString)
+  }
+
+  return thumbHash(fromImage: image).base64EncodedString()
+}
+
+// MARK: - Exception definitions
+
+private class LoadingFailureException: GenericException<String> {
+  override var reason: String {
+    "Failed to load image URI: \(param)"
   }
 }
