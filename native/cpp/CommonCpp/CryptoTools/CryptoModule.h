b821a692f3ec9cfd106fb3c274386466234b7bdd
marcinwasowicz <marcinkwasowicz@gmail.com>
2023-05-11 16:14:02 +0200

    Implement methods in NotificationsCryptoModule to decrypt data
    Summary: This differential implements a method to decrypt data in NotificationsCryptoModule. Additionally some CryptoModule code is
    simplified. I don't see the reason the ckeck for matching inbound sessions every time we receive a message. Additionally we would
    have to store keyserver identity keys which would make the code more complicated.
    
    Test Plan: After succesfull log-in execute in the keyserver code to encrypt some mock data. Then hardcode this data in
    NotificationsService and call NotificationsCryptoModule::decrypt() method on it. Build the app again and send a notif to trigger
    decryption code execution. Examine logs from the NSE.
    
    Reviewers: tomek, atul, bartek
    
    Subscribers:

--- native/cpp/CommonCpp/CryptoTools/CryptoModule.h
+++ native/cpp/CommonCpp/CryptoTools/CryptoModule.h
@@ -73,10 +73,8 @@ public:
 
   EncryptedData
   encrypt(const std::string &targetUserId, const std::string &content);
-  std::string decrypt(
-      const std::string &targetUserId,
-      EncryptedData encryptedData,
-      const OlmBuffer &theirIdentityKey);
+  std::string
+  decrypt(const std::string &targetUserId, EncryptedData encryptedData);
 
   std::string signMessage(const std::string &message);
   static void verifySignature(
