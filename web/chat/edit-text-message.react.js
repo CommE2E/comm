2257ec1f96ba4c595a0e06e27d78671a25a51d7a
kosmydel <kukubaczek@gmail.com>
2023-05-11 13:37:16 +0200

    [web] Added EditTextMessage component
    Summary: Added editing GUI
    
    Test Plan:
    Tested in later diffs, if the editing GUI looks correct.
    
    Screenshot:
    {F539984}
    
    Reviewers: michal, inka
    
    Reviewed By: michal
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7823

---
+++ web/chat/edit-text-message.react.js
@@ -0,0 +1,102 @@
+// @flow
+
+import classNames from 'classnames';
+import * as React from 'react';
+import { XCircle as XCircleIcon } from 'react-feather';
+
+import type { ChatMessageInfoItem } from 'lib/selectors/chat-selectors.js';
+import { type ThreadInfo } from 'lib/types/thread-types.js';
+
+import cssInputBar from './chat-input-bar.css';
+import ChatInputTextArea from './chat-input-text-area.react.js';
+import ComposedMessage from './composed-message.react.js';
+import { useEditModalContext } from './edit-message-provider.js';
+import css from './edit-text-message.css';
+import type { ButtonColor } from '../components/button.react.js';
+import Button from '../components/button.react.js';
+
+type Props = {
+  +item: ChatMessageInfoItem,
+  +threadInfo: ThreadInfo,
+  +background: boolean,
+};
+
+const cancelButtonColor: ButtonColor = {
+  backgroundColor: 'transparent',
+};
+
+function EditTextMessage(props: Props): React.Node {
+  const { background, threadInfo } = props;
+  const { editState, clearEditModal } = useEditModalContext();
+
+  const editedMessageDraft = editState?.editedMessageDraft ?? '';
+  const threadColor = threadInfo.color;
+  const saveButtonColor: ButtonColor = React.useMemo(
+    () => ({
+      backgroundColor: `#${threadColor}`,
+    }),
+    [threadColor],
+  );
+  let editFailed;
+  if (editState?.isError) {
+    editFailed = (
+      <div className={css.failedText}>
+        <XCircleIcon className={css.icon} />
+        <div className={css.errorColor}>Edit failed.</div>
+        <div className={css.whiteColor}>Please try again.</div>
+      </div>
+    );
+  }
+
+  const containerStyle = classNames(css.editMessage, {
+    [css.backgroundEditMessage]: background,
+  });
+
+  return (
+    <div className={containerStyle}>
+      <div className={cssInputBar.inputBarTextInput}>
+        <ChatInputTextArea
+          focus={!background}
+          currentText={editedMessageDraft}
+        />
+      </div>
+      <div className={css.bottomRow}>
+        {editFailed}
+        <div className={css.buttons}>
+          <Button
+            className={[css.saveButton, css.smallButton]}
+            variant="filled"
+            buttonColor={saveButtonColor}
+          >
+            Save (enter)
+          </Button>
+          <Button
+            className={css.smallButton}
+            variant="filled"
+            buttonColor={cancelButtonColor}
+            onClick={clearEditModal}
+          >
+            Cancel (esc)
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const ComposedEditTextMessage: React.ComponentType<Props> = React.memo<Props>(
+  function ComposedEditTextMessage(props) {
+    const { background, ...restProps } = props;
+    return (
+      <ComposedMessage
+        {...restProps}
+        sendFailed={false}
+        shouldDisplayPinIndicator={false}
+      >
+        <EditTextMessage {...props} />
+      </ComposedMessage>
+    );
+  },
+);
+
+export { EditTextMessage, ComposedEditTextMessage };
