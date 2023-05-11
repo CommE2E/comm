1534b734f687be00c4fd424b91a66ab099c9326e
kosmydel <kukubaczek@gmail.com>
2023-05-11 13:06:47 +0200

    [web] Add new edit message provider
    Summary: Created a new context for message editing. Used ModalOverlay template.
    
    Test Plan: In later diffs checked if the context works correctly.
    
    Reviewers: michal, inka
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7794

---
+++ web/chat/edit-message-provider.js
@@ -0,0 +1,81 @@
+// @flow
+
+import invariant from 'invariant';
+import * as React from 'react';
+
+import ModalOverlay from 'lib/components/modal-overlay.react.js';
+import type { ChatMessageInfoItem } from 'lib/selectors/chat-selectors.js';
+import type { ThreadInfo } from 'lib/types/thread-types';
+
+export type EditState = {
+  +messageInfo: ChatMessageInfoItem,
+  +threadInfo: ThreadInfo,
+  +editedMessageDraft: ?string,
+  +isError: boolean,
+};
+
+type EditModalContextType = {
+  +renderEditModal: (params: EditState) => void,
+  +clearEditModal: () => void,
+  +editState: ?EditState,
+};
+
+const EditModalContext: React.Context<EditModalContextType> =
+  React.createContext<EditModalContextType>({
+    renderEditModal: () => {},
+    clearEditModal: () => {},
+    editState: null,
+  });
+
+type Props = {
+  +children: React.Node,
+};
+function EditModalProvider(props: Props): React.Node {
+  const { children } = props;
+
+  const [editState, setEditState] = React.useState<?EditState>(null);
+
+  const clearEditModal = React.useCallback(() => {
+    setEditState(null);
+  }, []);
+
+  const renderEditModal = React.useCallback((newEditState: EditState): void => {
+    setEditState(newEditState);
+  }, []);
+
+  const modal = React.useMemo(() => {
+    // TODO: Add modal
+    return null;
+  }, []);
+
+  const value = React.useMemo(
+    () => ({
+      renderEditModal,
+      clearEditModal: clearEditModal,
+      editState,
+    }),
+    [renderEditModal, clearEditModal, editState],
+  );
+
+  const modalOverlay = React.useMemo(() => {
+    if (!modal) {
+      return null;
+    }
+    return <ModalOverlay onClose={clearEditModal}>{modal}</ModalOverlay>;
+  }, [clearEditModal, modal]);
+
+  return (
+    <EditModalContext.Provider value={value}>
+      {children}
+      {modalOverlay}
+    </EditModalContext.Provider>
+  );
+}
+
+function useEditModalContext(): EditModalContextType {
+  const context = React.useContext(EditModalContext);
+  invariant(context, 'EditModalContext not found');
+  return context;
+}
+
+export { EditModalProvider, useEditModalContext };
