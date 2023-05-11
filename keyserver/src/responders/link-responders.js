946c6ec39a12780bca1180f029b29899894b4d0b
Michal Gniadek <michal.gniadek@swmansion.com>
2023-05-11 12:15:14 +0200

    [keyserver] Validate link responders
    Summary:
    Add validators and validate link responders, add tests.
    
    Depends on D7806
    
    Test Plan: `yarn jest`
    
    Reviewers: kamil, tomek
    
    Subscribers: ashoat, atul
    
    Differential Revision: https://phab.comm.dev/D7807

--- keyserver/src/responders/link-responders.js
+++ keyserver/src/responders/link-responders.js
@@ -1,32 +1,51 @@
 // @flow
 
-import t from 'tcomb';
+import t, { type TUnion, type TInterface } from 'tcomb';
 
 import type {
   InviteLinkVerificationRequest,
   InviteLinkVerificationResponse,
 } from 'lib/types/link-types.js';
-import { tShape } from 'lib/utils/validation-utils.js';
+import { tShape, tID } from 'lib/utils/validation-utils.js';
 
 import { verifyInviteLink } from '../fetchers/link-fetchers.js';
 import { Viewer } from '../session/viewer.js';
-import { validateInput } from '../utils/validation-utils.js';
+import { validateInput, validateOutput } from '../utils/validation-utils.js';
 
-const inviteLinkVerificationRequestInputValidator = tShape({
-  secret: t.String,
-});
+const inviteLinkVerificationRequestInputValidator: TInterface<InviteLinkVerificationRequest> =
+  tShape({
+    secret: t.String,
+  });
+
+export const inviteLinkVerificationResponseValidator: TUnion<InviteLinkVerificationResponse> =
+  t.union([
+    tShape({
+      status: t.enums.of(['valid', 'already_joined']),
+      community: tShape({
+        name: t.String,
+        id: tID,
+      }),
+    }),
+    tShape({
+      status: t.enums.of(['invalid', 'expired']),
+    }),
+  ]);
 
 async function inviteLinkVerificationResponder(
   viewer: Viewer,
   input: any,
 ): Promise<InviteLinkVerificationResponse> {
-  const request: InviteLinkVerificationRequest = input;
-  await validateInput(
+  const request = await validateInput(
     viewer,
     inviteLinkVerificationRequestInputValidator,
-    request,
+    input,
   );
-  return await verifyInviteLink(viewer, request);
+  const response = await verifyInviteLink(viewer, request);
+  return validateOutput(
+    viewer.platformDetails,
+    inviteLinkVerificationResponseValidator,
+    response,
+  );
 }
 
 export { inviteLinkVerificationResponder };
