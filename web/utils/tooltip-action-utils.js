05f538c68328299ebe67776f8c854e72272bf970
kosmydel <kukubaczek@gmail.com>
2023-05-11 13:53:58 +0200

    [web] Enter edit state after pressing edit button
    Summary: Set edited message after pressing edit button
    
    Test Plan: Run the app. Checked in the devtools if the state is changed after pressing the edit button.
    
    Reviewers: michal, inka
    
    Subscribers:

--- web/utils/tooltip-action-utils.js
+++ web/utils/tooltip-action-utils.js
@@ -28,6 +28,7 @@ import {
   type TooltipSize,
   type TooltipPosition,
 } from './tooltip-utils.js';
+import { useEditModalContext } from '../chat/edit-message-provider.js';
 import MessageTooltip from '../chat/message-tooltip.react.js';
 import type { PositionInfo } from '../chat/position-types.js';
 import { useTooltipContext } from '../chat/tooltip-provider.js';
@@ -231,25 +232,33 @@ function useMessageEditAction(
   threadInfo: ThreadInfo,
 ): ?MessageTooltipAction {
   const { messageInfo } = item;
-
   const canEditMessage = useCanEditMessage(threadInfo, messageInfo);
+  const { renderEditModal } = useEditModalContext();
 
   return React.useMemo(() => {
     if (!canEditMessage) {
       return null;
     }
+
     const buttonContent = (
       <SWMansionIcon icon="edit-1" size={18} disableFill={false} />
     );
+
     const onClickEdit = () => {
-      // TODO: Enter edit mode
+      renderEditModal({
+        messageInfo: item,
+        threadInfo,
+        isError: false,
+        editedMessageDraft: messageInfo.text,
+      });
     };
+
     return {
       actionButtonContent: buttonContent,
       onClick: onClickEdit,
       label: 'Edit',
     };
-  }, [canEditMessage]);
+  }, [canEditMessage, item, messageInfo.text, renderEditModal, threadInfo]);
 }
 
 function useMessageTooltipActions(
