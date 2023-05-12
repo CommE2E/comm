9372f4589dfc4671977efe3205efd8c8265ef3cb
Jonathan Ringer <jonathan.ringer@comm.app>
2023-05-12 15:08:11 -0700

    [Tunnelbroker] Remove usages of unwrap and expect
    Summary:
    Unwrap and expect were convenient for the happy cases. However,
    they are unsatisfactory for actual edge cases as they cause panic which
    puts tunnelbroker into a semi-broken state.
    
    Depends on D7800
    
    Test Plan:
    ```
    nix develop
    
    # init localstack and run terraform
    comm-dev services start
    (cd services/terraform && ./run.sh)
    
    (cd services/tunnelbroker && cargo run &)
    (cd services/commtest && cargo test --test tunnelbroker_integration_test)
    ```
    
    Reviewers: bartek, varun
    
    Reviewed By: bartek, varun
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7801

---
+++ services/tunnelbroker/src/database/message.rs
@@ -0,0 +1,50 @@
+use std::collections::HashMap;
+
+use aws_sdk_dynamodb::types::AttributeValue;
+
+use crate::constants::dynamodb::undelivered_messages::{
+  CREATED_AT, DEVICE_ID, PAYLOAD,
+};
+
+#[derive(Debug)]
+pub struct DeviceMessage {
+  pub device_id: String,
+  pub created_at: String,
+  pub payload: String,
+}
+
+#[derive(Debug, derive_more::Display)]
+pub enum MessageErrors {
+  SerializationError,
+}
+
+impl DeviceMessage {
+  pub fn from_hashmap(
+    hashmap: HashMap<String, AttributeValue>,
+  ) -> Result<DeviceMessage, MessageErrors> {
+    let device_id: String = hashmap
+      .get(DEVICE_ID)
+      .ok_or(MessageErrors::SerializationError)?
+      .as_s()
+      .map_err(|_| MessageErrors::SerializationError)?
+      .to_string();
+    let created_at: String = hashmap
+      .get(CREATED_AT)
+      .ok_or(MessageErrors::SerializationError)?
+      .as_n()
+      .map_err(|_| MessageErrors::SerializationError)?
+      .to_string();
+    let payload: String = hashmap
+      .get(PAYLOAD)
+      .ok_or(MessageErrors::SerializationError)?
+      .as_s()
+      .map_err(|_| MessageErrors::SerializationError)?
+      .to_string();
+
+    Ok(DeviceMessage {
+      device_id,
+      created_at,
+      payload,
+    })
+  }
+}
