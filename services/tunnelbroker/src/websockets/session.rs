e9209308664f01a5809201773aaf9e7e9aaa6aab
Jonathan Ringer <jonathan.ringer@comm.app>
2023-05-12 14:23:17 -0700

    [Tunnelbroker] Refactor connection lifetimes into session object
    Summary:
    For better encapsulation around connection events
    (e.g. device being authenticated and identified), moved
    more of the connection logic into the session object.
    
    Also, refine how connections are handled so they are properly
    deregistered once a client closes a connection.
    
    Depends on D7799
    
    Test Plan:
    ```
    nix develop
    
    # init localstack and run terraform
    comm-dev services start
    (cd services/terraform && ./run.sh)
    
    (cd services/tunnelbroker && cargo run &)
    (cd services/commtest && cargo test --test tunnelbroker_integration_test)
    # Previously, running the suite twice would fail
    (cd services/commtest && cargo test --test tunnelbroker_integration_test)
    ```
    
    Reviewers: bartek, varun
    
    Reviewed By: bartek, varun
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7800

--- services/tunnelbroker/src/websockets/session.rs
+++ services/tunnelbroker/src/websockets/session.rs
@@ -1,58 +1,124 @@
-use tracing::debug;
-use tunnelbroker_messages::Messages;
+use derive_more;
+use futures_util::stream::SplitSink;
+use futures_util::SinkExt;
+use tokio::{net::TcpStream, sync::mpsc::UnboundedSender};
+use tokio_tungstenite::{tungstenite::Message, WebSocketStream};
+use tracing::{debug, error};
+use tunnelbroker_messages::{session::DeviceTypes, Messages};
 
 use crate::{
   constants::dynamodb::undelivered_messages::CREATED_AT,
   database::DatabaseClient, ACTIVE_CONNECTIONS,
 };
 
-pub struct WebsocketSession {
-  tx: tokio::sync::mpsc::UnboundedSender<std::string::String>,
-  db_client: DatabaseClient,
+pub struct DeviceInfo {
+  pub device_id: String,
+  pub notify_token: Option<String>,
+  pub device_type: DeviceTypes,
+  pub device_app_version: Option<String>,
+  pub device_os: Option<String>,
 }
 
+pub struct WebsocketSession {
+  tx: SplitSink<WebSocketStream<TcpStream>, Message>,
+  db_client: DatabaseClient,
+  device_info: Option<DeviceInfo>,
+}
+
+#[derive(Debug, derive_more::Display, derive_more::From)]
+pub enum SessionError {
+  InvalidMessage,
+  SerializationError(serde_json::Error),
+}
+
+fn consume_error<T>(result: Result<T, SessionError>) {
+  if let Err(e) = result {
+    error!("{}", e)
+  }
+}
 impl WebsocketSession {
   pub fn new(
-    tx: tokio::sync::mpsc::UnboundedSender<std::string::String>,
+    tx: SplitSink<WebSocketStream<TcpStream>, Message>,
     db_client: DatabaseClient,
   ) -> WebsocketSession {
-    WebsocketSession { tx, db_client }
+    WebsocketSession {
+      tx,
+      db_client,
+      device_info: None,
+    }
+  }
+
+  pub async fn handle_websocket_frame_from_device(
+    &mut self,
+    frame: Message,
+    tx: UnboundedSender<String>,
+  ) {
+    debug!("Received message from device: {}", frame);
+    let result = match frame {
+      Message::Text(payload) => {
+        self.handle_message_from_device(&payload, tx).await
+      }
+      Message::Close(_) => {
+        self.close().await;
+        Ok(())
+      }
+      _ => Err(SessionError::InvalidMessage),
+    };
+    consume_error(result);
   }
 
   pub async fn handle_message_from_device(
-    &self,
+    &mut self,
     message: &str,
-  ) -> Result<(), serde_json::Error> {
-    match serde_json::from_str::<Messages>(message)? {
-      Messages::SessionRequest(session_info) => {
+    tx: UnboundedSender<String>,
+  ) -> Result<(), SessionError> {
+    let serialized_message = serde_json::from_str::<Messages>(message)?;
+
+    match serialized_message {
+      Messages::SessionRequest(mut session_info) => {
         // TODO: Authenticate device using auth token
+
+        // Check if session request was already sent
+        if self.device_info.is_some() {
+          return Err(SessionError::InvalidMessage);
+        }
+
+        let device_info = DeviceInfo {
+          device_id: session_info.device_id.clone(),
+          notify_token: session_info.notify_token.take(),
+          device_type: session_info.device_type,
+          device_app_version: session_info.device_app_version.take(),
+          device_os: session_info.device_os.take(),
+        };
+
         // Check for persisted messages
         let messages = self
           .db_client
-          .retrieve_messages(&session_info.device_id)
+          .retrieve_messages(&device_info.device_id)
           .await
-          .expect("Failed to retreive messages");
+          .unwrap_or_else(|e| {
+            error!("Error while retrieving messages: {}", e);
+            Vec::new()
+          });
 
-        ACTIVE_CONNECTIONS
-          .insert(session_info.device_id.clone(), self.tx.clone());
+        ACTIVE_CONNECTIONS.insert(device_info.device_id.clone(), tx.clone());
 
         for message in messages {
           let payload =
             message.get("payload").unwrap().as_s().unwrap().to_string();
-          self
-            .tx
-            .send(payload)
-            .expect("Failed to send message to client");
           let created_at =
             message.get(CREATED_AT).unwrap().as_n().unwrap().to_string();
+          self.send_message_to_device(payload).await;
           self
             .db_client
-            .delete_message(&session_info.device_id, &created_at)
+            .delete_message(&device_info.device_id, &created_at)
             .await
             .expect("Failed to delete messages");
         }
 
         debug!("Flushed messages for device: {}", &session_info.device_id);
+
+        self.device_info = Some(device_info);
       }
       _ => {
         debug!("Received invalid request");
@@ -60,5 +126,22 @@ impl WebsocketSession {
     }
 
     Ok(())
+  }
+
+  pub async fn send_message_to_device(&mut self, incoming_payload: String) {
+    if let Err(e) = self.tx.send(Message::Text(incoming_payload)).await {
+      error!("Failed to send message to device: {}", e);
+    }
+  }
+
+  // Release websocket and remove from active connections
+  pub async fn close(&mut self) {
+    if let Some(device_info) = &self.device_info {
+      ACTIVE_CONNECTIONS.remove(&device_info.device_id);
+    }
+
+    if let Err(e) = self.tx.close().await {
+      debug!("Failed to close session: {}", e);
+    }
   }
 }
