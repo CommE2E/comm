2368d38c7c446470d0d3b58f00b3a40c9cd7cd0b
marcinwasowicz <marcinkwasowicz@gmail.com>
2023-05-15 17:14:53 +0200

    Implement function to encrypt relevant parts of iOS notification
    Summary: This differential implements a function that encrypts parts of iOS notification payload that are important to user's privacy
    
    Test Plan:
    1. Pull cookie of currently logged in user from MariaDB.
    2. Call this function in the keyserver code where  iOS notification is constructed with hardcoded cookie id.
    3. Send notification to the physical iOS device. Log content of send notification and ensure relevant fields are encrypted.
    
    Reviewers: tomek, atul, bartek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D7813

---
+++ keyserver/src/push/crypto.js
@@ -0,0 +1,106 @@
+// @flow
+
+import apn from '@parse/node-apn';
+import invariant from 'invariant';
+
+import { encryptAndUpdateOlmSession } from '../updaters/olm-session-updater.js';
+
+async function encryptIOSNotification(
+  cookieID: string,
+  notification: apn.Notification,
+): Promise<apn.Notification> {
+  invariant(
+    !notification.collapseId,
+    'Collapsible notifications encryption currently not implemented',
+  );
+
+  const encryptedNotification = new apn.Notification();
+
+  encryptedNotification.id = notification.id;
+  encryptedNotification.payload.id = notification.id;
+  encryptedNotification.topic = notification.topic;
+  encryptedNotification.sound = notification.aps.sound;
+  encryptedNotification.pushType = 'alert';
+
+  invariant(
+    notification.aps['mutable-content'],
+    'Notification decryption impossible without mutableContent set to true',
+  );
+  encryptedNotification.mutableContent = true;
+
+  const { id, ...payloadSansId } = notification.payload;
+  let notificationFieldsToEncrypt = {
+    badge: notification.aps.badge.toString(),
+    ...payloadSansId,
+  };
+
+  if (notification.body) {
+    notificationFieldsToEncrypt = {
+      merged: notification.body,
+      ...notificationFieldsToEncrypt,
+    };
+  }
+
+  let encryptedFields;
+  try {
+    encryptedFields = await encryptAndUpdateOlmSession(
+      cookieID,
+      'notifications',
+      notificationFieldsToEncrypt,
+    );
+  } catch (e) {
+    console.log('Notification encryption failed: ' + e);
+
+    encryptedNotification.body = notification.aps.alert;
+
+    invariant(
+      typeof notification.aps.badge === 'number',
+      'Unencrypted notification must have badge as a number',
+    );
+    encryptedNotification.badge = notification.aps.badge;
+
+    encryptedNotification.threadId = notification.aps['thread-id'];
+    encryptedNotification.payload = {
+      ...encryptedNotification.payload,
+      ...notification.payload,
+      encrypted: 0,
+    };
+    return encryptedNotification;
+  }
+
+  const { merged, threadID, badge, ...restPayload } = encryptedFields;
+  // node-apn library does not allow to store
+  // strings in 'badge' property. It will be
+  // restored in NSE on the device.
+  encryptedNotification.badge = undefined;
+  encryptedNotification.payload.badge = badge.body;
+
+  if (threadID) {
+    encryptedNotification.threadId = threadID.body;
+    encryptedNotification.payload.threadID = threadID.body;
+  }
+
+  if (merged) {
+    encryptedNotification.body = merged.body;
+  }
+
+  for (const payloadField in restPayload) {
+    encryptedNotification.payload[payloadField] =
+      restPayload[payloadField].body;
+  }
+
+  encryptedNotification.payload.encrypted = 1;
+  return encryptedNotification;
+}
+
+function prepareEncryptedIOSNotifications(
+  cookieIDs: $ReadOnlyArray<string>,
+  notification: apn.Notification,
+): Promise<Array<apn.Notification>> {
+  const notificationPromises = cookieIDs.map(cookieID =>
+    encryptIOSNotification(cookieID, notification),
+  );
+  return Promise.all(notificationPromises);
+}
+
+export { prepareEncryptedIOSNotifications };
