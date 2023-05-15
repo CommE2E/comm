7d08215632c8367bee6dd24c9197d7dd7d65e732
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-05-15 19:43:13 +0200

    [landing] introduce new header elements to header component
    Summary: This diff is the second part to updating the `Header` component to match the redesigns. The foucs of this diff is to introduce and layout the new elments needed for the header redesign
    
    Test Plan: Please watch the demo video to see how the new `Header` component looks and behaves
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7839

--- landing/header.react.js
+++ landing/header.react.js
@@ -3,56 +3,79 @@
 import { faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faExternalLinkAlt, faBars } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames';
 import * as React from 'react';
 import { NavLink } from 'react-router-dom';
 
 import css from './header.css';
-
-const iconProps = {
-  size: 'sm',
-  color: '#ebedee',
-};
-
-const navLinkProps = {
-  className: css.tab,
-  activeStyle: {
-    color: 'white',
-  },
-};
+import typography from './typography.css';
 
 function Header(): React.Node {
+  const logoTextClassName = classNames([typography.heading2, css.logoText]);
+  const badgeClassName = classNames([typography.paragraph2, css.betaBadge]);
+  const navLinkClassName = classNames([typography.subheading2, css.tab]);
+
   return (
     <nav className={css.headerContainer}>
       <div className={css.headerNavContentContainer}>
-        <NavLink to="/" className={css.logo}>
-          <h1>Comm</h1>
-        </NavLink>
-        <div className={css.pageNav}>
-          <NavLink to="/" exact {...navLinkProps}>
-            App
+        <div className={css.logo}>
+          <NavLink to="/">
+            <h1 className={logoTextClassName}>Comm</h1>
           </NavLink>
-          <NavLink to="/keyservers" exact {...navLinkProps}>
+          <div className={badgeClassName}>Beta</div>
+        </div>
+        <div className={css.pageNav}>
+          <NavLink
+            to="/keyservers"
+            exact
+            className={navLinkClassName}
+            activeClassName={css.activeTab}
+          >
             Keyserver
+          </NavLink>
+          <NavLink
+            to="/team"
+            exact
+            className={navLinkClassName}
+            activeClassName={css.activeTab}
+          >
+            Team
+          </NavLink>
+          <NavLink
+            to="/investors"
+            exact
+            className={navLinkClassName}
+            activeClassName={css.activeTab}
+          >
+            Investors
           </NavLink>
         </div>
         <div className={css.socialIconsContainer}>
           <a href="https://web.comm.app">
             <div className={css.webappIcon}>
-              <FontAwesomeIcon icon={faExternalLinkAlt} {...iconProps} />
+              <FontAwesomeIcon
+                icon={faExternalLinkAlt}
+                className={css.icon}
+                size="sm"
+              />
             </div>
           </a>
           <a href="https://twitter.com/commdotapp">
             <div className={css.twitterIcon}>
-              <FontAwesomeIcon icon={faTwitter} {...iconProps} />
+              <FontAwesomeIcon
+                icon={faTwitter}
+                className={css.icon}
+                size="sm"
+              />
             </div>
           </a>
           <a href="https://github.com/CommE2E/comm">
             <div className={css.githubIcon}>
-              <FontAwesomeIcon icon={faGithub} {...iconProps} />
+              <FontAwesomeIcon icon={faGithub} className={css.icon} size="sm" />
             </div>
           </a>
           <div className={css.menuIcon}>
-            <FontAwesomeIcon icon={faBars} {...iconProps} />
+            <FontAwesomeIcon icon={faBars} className={css.icon} size="sm" />
           </div>
         </div>
       </div>
