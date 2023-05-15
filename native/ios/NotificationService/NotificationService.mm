cf57a509ad8adc982965795b96e30e08e9d1f95e
marcinwasowicz <marcinkwasowicz@gmail.com>
2023-05-15 17:18:47 +0200

    Implement method in NSE to decrypt notification
    Summary: This differential impements methods in NSE to decrypt encrypted notification.
    
    Test Plan:
    1. Repeat steps from parent differential test plan
    2. Ensure this time the encrypted notification is the one that is sent, not the original.
    3. Ensure decrypted notification is displayed on the device.
    
    Reviewers: tomek, atul, bartek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D7814

--- native/ios/NotificationService/NotificationService.mm
+++ native/ios/NotificationService/NotificationService.mm
@@ -1,8 +1,13 @@
 #import "NotificationService.h"
 #import "Logger.h"
+#import "NotificationsCryptoModule.h"
 #import "TemporaryMessageStorage.h"
 
 NSString *const backgroundNotificationTypeKey = @"backgroundNotifType";
+NSString *const messageInfosKey = @"messageInfos";
+NSString *const encryptedPayloadKey = @"encryptedPayload";
+NSString *const encryptionFailureKey = @"encryptionFailure";
+const std::string callingProcessName = "NSE";
 // The context for this constant can be found here:
 // https://linear.app/comm/issue/ENG-3074#comment-bd2f5e28
 int64_t const notificationRemovalDelay = (int64_t)(0.1 * NSEC_PER_SEC);
@@ -25,6 +30,24 @@ CFStringRef newMessageInfosDarwinNotification =
                            contentHandler {
   self.contentHandler = contentHandler;
   self.bestAttemptContent = [request.content mutableCopy];
+
+  if ([self shouldBeDecrypted:self.bestAttemptContent.userInfo]) {
+    @try {
+      [self decryptBestAttemptContent];
+    } @catch (NSException *e) {
+      comm::Logger::log(
+          "NSE: Received exception: " + std::string([e.name UTF8String]) +
+          " with reason: " + std::string([e.reason UTF8String]) +
+          " during notification decryption");
+      self.contentHandler([[UNNotificationContent alloc] init]);
+      return;
+    }
+  } else if ([self shouldAlertUnencryptedNotification:self.bestAttemptContent
+                                                          .userInfo]) {
+    // In future this will be replaced by notification content
+    // modification for DEV environment and staff members
+    comm::Logger::log("NSE: Received erroneously unencrypted notitication.");
+  }
 
   [self persistMessagePayload:self.bestAttemptContent.userInfo];
   // Message payload persistence is a higher priority task, so it has
@@ -52,6 +75,16 @@ CFStringRef newMessageInfosDarwinNotification =
     // It is an extremely unlikely to happen.
     comm::Logger::log("NSE: Exceeded time limit to rescind a notification.");
     self.contentHandler([[UNNotificationContent alloc] init]);
+    return;
+  }
+  if ([self shouldBeDecrypted:self.bestAttemptContent.userInfo] &&
+      !self.bestAttemptContent.userInfo[@"succesfullyDecrypted"]) {
+    // If we get to this place it means we were unable to
+    // decrypt encrypted notification content in time
+    // given to NSE to process notification.
+    comm::Logger::log("NSE: Exceeded time limit to decrypt a notification.");
+    self.contentHandler([[UNNotificationContent alloc] init]);
+    return;
   }
   self.contentHandler(self.bestAttemptContent);
 }
@@ -85,10 +118,10 @@ CFStringRef newMessageInfosDarwinNotification =
 }
 
 - (void)persistMessagePayload:(NSDictionary *)payload {
-  if (payload[@"messageInfos"]) {
+  if (payload[messageInfosKey]) {
     TemporaryMessageStorage *temporaryStorage =
         [[TemporaryMessageStorage alloc] init];
-    [temporaryStorage writeMessage:payload[@"messageInfos"]];
+    [temporaryStorage writeMessage:payload[messageInfosKey]];
     return;
   }
 
@@ -129,6 +162,87 @@ CFStringRef newMessageInfosDarwinNotification =
       (__bridge const void *)(self),
       nil,
       TRUE);
+}
+
+- (BOOL)shouldBeDecrypted:(NSDictionary *)payload {
+  return payload[encryptedPayloadKey];
+}
+
+- (BOOL)shouldAlertUnencryptedNotification:(NSDictionary *)payload {
+  return payload[encryptionFailureKey] &&
+      [payload[encryptionFailureKey] isEqualToNumber:@(1)];
+}
+
+- (NSString *)singleDecrypt:(NSString *)data {
+  std::string encryptedData = std::string([data UTF8String]);
+  return [NSString
+      stringWithUTF8String:
+          (comm::NotificationsCryptoModule::decrypt(
+               encryptedData,
+               comm::NotificationsCryptoModule::olmEncryptedTypeMessage,
+               callingProcessName))
+              .c_str()];
+}
+
+- (void)decryptBestAttemptContent {
+  NSString *decryptedSerializedPayload = [self
+      singleDecrypt:self.bestAttemptContent.userInfo[encryptedPayloadKey]];
+  NSDictionary *decryptedPayload = [NSJSONSerialization
+      JSONObjectWithData:[decryptedSerializedPayload
+                             dataUsingEncoding:NSUTF8StringEncoding]
+                 options:0
+                   error:nil];
+
+  NSMutableDictionary *mutableUserInfo =
+      [self.bestAttemptContent.userInfo mutableCopy];
+
+  NSMutableDictionary *mutableAps = nil;
+  if (mutableUserInfo[@"aps"]) {
+    mutableAps = [mutableUserInfo[@"aps"] mutableCopy];
+  }
+
+  NSString *body = decryptedPayload[@"merged"];
+  if (body) {
+    self.bestAttemptContent.body = body;
+    if (mutableAps && mutableAps[@"alert"]) {
+      mutableAps[@"alert"] = body;
+    }
+  }
+
+  NSString *threadID = decryptedPayload[@"threadID"];
+  if (threadID) {
+    self.bestAttemptContent.threadIdentifier = threadID;
+    mutableUserInfo[@"threadID"] = threadID;
+    if (mutableAps) {
+      mutableAps[@"thread-id"] = threadID;
+    }
+  }
+
+  NSString *badgeStr = decryptedPayload[@"badge"];
+  if (badgeStr) {
+    NSNumber *badge = @([badgeStr intValue]);
+    self.bestAttemptContent.badge = badge;
+    if (mutableAps) {
+      mutableAps[@"badge"] = badge;
+    }
+  }
+
+  // The rest have been already decrypted and handled.
+  static NSArray<NSString *> *handledKeys =
+      @[ @"merged", @"badge", @"threadID" ];
+
+  for (NSString *payloadKey in decryptedPayload) {
+    if ([handledKeys containsObject:payloadKey]) {
+      continue;
+    }
+    mutableUserInfo[payloadKey] = decryptedPayload[payloadKey];
+  }
+
+  if (mutableAps) {
+    mutableUserInfo[@"aps"] = mutableAps;
+  }
+  [mutableUserInfo removeObjectForKey:encryptedPayloadKey];
+  self.bestAttemptContent.userInfo = mutableUserInfo;
 }
 
 @end
