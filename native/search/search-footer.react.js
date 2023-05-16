99e342020e38882dda12110442978a9b8e2bf1c5
Inka Sokołowska <inka.sokolowska@swmansion.com>
2023-05-16 10:31:46 +0200

    [natvie] Add SearchFooter component
    Summary:
    issue: https://linear.app/comm/issue/ENG-3842/add-the-end-of-results-and-no-results-messages
    On the search screen there are different messages that appear at the end of the results (like "End of results") or instead of them (like "No results, ..."). They are all styled the same, so I created a component for them
    
    Test Plan: Displayed the coponent with some message. Checked that it looks as it should.
    
    Reviewers: kuba, kamil, bartek
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7772

---
+++ native/search/search-footer.react.js
@@ -0,0 +1,32 @@
+// @flow
+
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+import { useStyles } from '../themes/colors.js';
+
+function SearchFooter(props: { +text: string }): React.Node {
+  const styles = useStyles(unboundStyles);
+
+  return (
+    <View style={styles.messageWrapper}>
+      <Text style={styles.message}>{props.text}</Text>
+    </View>
+  );
+}
+
+const unboundStyles = {
+  message: {
+    color: 'panelInputSecondaryForeground',
+    size: 14,
+    fontWeight: '400',
+    textAlign: 'center',
+  },
+  messageWrapper: {
+    paddingVertical: 24,
+    paddingHorizontal: 60,
+    display: 'flex',
+  },
+};
+
+export default SearchFooter;
