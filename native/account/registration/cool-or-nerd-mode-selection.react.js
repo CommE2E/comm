f1983bafc7ebbe2f66fce6a3c6eb7464afbf9586
Ashoat Tevosyan <ashoat@comm.app>
2023-05-16 15:20:12 -0400

    [native] Introduce RegistrationContainer to handle SafeAreaView
    Summary:
    Before this diff, we wrapped all registration screens with a `SafeAreaView` in `RegistrationNavigator`.
    
    However, this meant that the `SIWEPanel` background had a sharp cutoff at the `SafeAreaView` bounds.
    
    In order to allow the `SIWEPanel` to appear over the `SafeAreaView`, this diff moves responsibility for rendering `SafeAreaView` to the individual screens. This is achieved with a new `RegistrationContainer` component.
    
    Depends on D7844
    
    Test Plan:
    | before | after |
    | {F541608} | {F541609} |
    
    Reviewers: ginsu, atul
    
    Reviewed By: atul
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D7845

--- native/account/registration/cool-or-nerd-mode-selection.react.js
+++ native/account/registration/cool-or-nerd-mode-selection.react.js
@@ -2,10 +2,11 @@
 
 import invariant from 'invariant';
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text } from 'react-native';
 
 import RegistrationButtonContainer from './registration-button-container.react.js';
 import RegistrationButton from './registration-button.react.js';
+import RegistrationContainer from './registration-container.react.js';
 import RegistrationContentContainer from './registration-content-container.react.js';
 import type { RegistrationNavigationProp } from './registration-navigator.react.js';
 import {
@@ -48,7 +49,7 @@ function CoolOrNerdModeSelection(props: Props): React.Node {
   const buttonState = currentSelection ? 'enabled' : 'disabled';
   const styles = useStyles(unboundStyles);
   return (
-    <View style={styles.container}>
+    <RegistrationContainer>
       <RegistrationContentContainer>
         <Text style={styles.header}>To begin, choose your fighter</Text>
         <Text style={styles.body}>
@@ -92,16 +93,11 @@ function CoolOrNerdModeSelection(props: Props): React.Node {
           variant={buttonState}
         />
       </RegistrationButtonContainer>
-    </View>
+    </RegistrationContainer>
   );
 }
 
 const unboundStyles = {
-  container: {
-    backgroundColor: 'panelBackground',
-    justifyContent: 'space-between',
-    flex: 1,
-  },
   header: {
     fontSize: 24,
     color: 'panelForegroundLabel',
