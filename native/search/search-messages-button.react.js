9ffdf1f072f05ab00fb9143dbc02a579cb802a93
Inka Sokołowska <inka.sokolowska@swmansion.com>
2023-05-16 10:31:46 +0200

    [native] Add search button to chat header
    Summary: issue: https://linear.app/comm/issue/ENG-3404/add-magnifying-glass-button-to-message-list-header
    
    Test Plan: Opened a chat, pressed on the button, checked that is navigates to the search screen
    
    Reviewers: kuba, kamil, bartek
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7777

---
+++ native/search/search-messages-button.react.js
@@ -0,0 +1,44 @@
+// @flow
+
+import * as React from 'react';
+
+import { type ThreadInfo } from 'lib/types/thread-types.js';
+
+import type { ChatNavigationProp } from '../chat/chat.react.js';
+import Button from '../components/button.react.js';
+import SWMansionIcon from '../components/swmansion-icon.react.js';
+import { MessageSearchRouteName } from '../navigation/route-names.js';
+import { useStyles } from '../themes/colors.js';
+
+type Props = {
+  +threadInfo: ThreadInfo,
+  +navigate: $PropertyType<ChatNavigationProp<'MessageList'>, 'navigate'>,
+};
+
+function SearchMessagesButton(props: Props): React.Node {
+  const { threadInfo, navigate } = props;
+  const styles = useStyles(unboundStyles);
+
+  const onPress = React.useCallback(() => {
+    navigate<'MessageSearch'>({
+      name: MessageSearchRouteName,
+      params: { threadInfo },
+      key: `${MessageSearchRouteName}${threadInfo.id}`,
+    });
+  }, [navigate, threadInfo]);
+
+  return (
+    <Button onPress={onPress} androidBorderlessRipple={true}>
+      <SWMansionIcon name="search" size={24} style={styles.button} />
+    </Button>
+  );
+}
+
+const unboundStyles = {
+  button: {
+    color: 'panelForegroundLabel',
+    paddingHorizontal: 10,
+  },
+};
+
+export default SearchMessagesButton;
