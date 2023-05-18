9b9e0d77d65abf088d1a5bb33a2db01f5950f53c
Ashoat Tevosyan <ashoat@comm.app>
2023-05-18 15:33:55 -0400

    [keyserver][lib] Plumbing for new exact user search endpoint
    Summary:
    We want to be able to query whether a username exists in order to decide whether to show [this screen](https://www.figma.com/file/a1nkbWgbgjRlrOY9LVurTz/Comm-%2F-Desktop-app?type=design&node-id=11725%3A252911&t=C4R9LC2iNwVlCxJJ-1).
    
    In the case where the ETH wallet already has an account, we'll let them log in directly and (potentially?) skip the avatar-setting step.
    
    This diff:
    
    1. Introduces new types for the input and output of the new endpoint
    2. Updates the endpoints types and adds plumbing for the endpoint on the `keyserver` side
    3. Introduces a new action and associated types in `lib`
    4. Introduces a callsite in `ConnectEthereum` for the new endpoint
    
    Depends on D7864
    
    Test Plan: I added a `console.log` of the result on `native`, and confirmed an already-existing ETH account properly returned the existing `userInfo`
    
    Reviewers: atul, ginsu
    
    Subscribers:

--- native/account/registration/connect-ethereum.react.js
+++ native/account/registration/connect-ethereum.react.js
@@ -3,6 +3,16 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
 
+import {
+  exactSearchUser,
+  exactSearchUserActionTypes,
+} from 'lib/actions/user-actions.js';
+import type { SIWEResult } from 'lib/types/siwe-types.js';
+import {
+  useServerCall,
+  useDispatchActionPromise,
+} from 'lib/utils/action-utils.js';
+
 import RegistrationButtonContainer from './registration-button-container.react.js';
 import RegistrationButton from './registration-button.react.js';
 import RegistrationContainer from './registration-container.react.js';
@@ -28,8 +38,8 @@ type Props = {
   +route: NavigationRoute<'ConnectEthereum'>,
 };
 function ConnectEthereum(props: Props): React.Node {
-  const isNerdMode =
-    props.route.params.userSelections.coolOrNerdMode === 'nerd';
+  const { userSelections } = props.route.params;
+  const isNerdMode = userSelections.coolOrNerdMode === 'nerd';
   const styles = useStyles(unboundStyles);
 
   let body;
@@ -94,8 +104,27 @@ function ConnectEthereum(props: Props): React.Node {
     [panelState],
   );
 
-  const onSkip = React.useCallback(() => {}, []);
-  const onSuccessfulWalletSignature = React.useCallback(() => {}, []);
+  const exactSearchUserCall = useServerCall(exactSearchUser);
+  const dispatchActionPromise = useDispatchActionPromise();
+
+  const onResult = React.useCallback(
+    async (result: SIWEResult) => {
+      const searchPromise = exactSearchUserCall(result.address);
+      dispatchActionPromise(exactSearchUserActionTypes, searchPromise);
+      const { userInfo } = await searchPromise;
+
+      if (userInfo) {
+        // show duplicate account screen
+      } else {
+        // show avatar selection screen
+      }
+    },
+    [exactSearchUserCall, dispatchActionPromise],
+  );
+
+  const onSkip = React.useCallback(() => {
+    // show username selection screen
+  }, []);
 
   let siwePanel;
   if (panelState !== 'closed') {
@@ -104,7 +133,7 @@ function ConnectEthereum(props: Props): React.Node {
         onClosing={onPanelClosing}
         onClosed={onPanelClosed}
         closing={panelState === 'closing'}
-        onSuccessfulWalletSignature={onSuccessfulWalletSignature}
+        onSuccessfulWalletSignature={onResult}
         setLoading={siwePanelSetLoading}
       />
     );
