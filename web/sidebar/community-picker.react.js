0e6e442d466b7e7f0841e1521f88c3a84e30103b
Atul Madhugiri <atulm@berkeley.edu>
2023-05-18 12:33:14 -0400

    [DO NOT LAND] Enable community creation button.

--- web/sidebar/community-picker.react.js
+++ web/sidebar/community-picker.react.js
@@ -69,7 +69,7 @@ function CommunityPicker(): React.Node {
     modalContext.pushModal(<CommunityCreationModal />);
   }, [modalContext]);
 
-  const isCommunityCreationButtonEnabled = false;
+  const isCommunityCreationButtonEnabled = true;
   let communityCreationButton;
   if (isCommunityCreationButtonEnabled) {
     communityCreationButton = (
