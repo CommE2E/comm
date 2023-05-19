caad7ccaf790b65083ffff817b4e7e6177e1aaf8
Tomasz Palys <tomasz.palys@swmansion.com>
2023-05-19 12:20:19 +0200

    [native] Add a link to manage public link screen
    Summary: The link is visible only to admin users.
    
    Depends on D7884
    
    Test Plan: Check if a link is visible only to admin users and if clicking it navigates the user to manage links screen.
    
    Reviewers: bartek, kamil, inka

--- native/invite-links/view-invite-links-screen.react.js
+++ native/invite-links/view-invite-links-screen.react.js
@@ -6,13 +6,16 @@ import { Text, View } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
 import { primaryInviteLinksSelector } from 'lib/selectors/invite-links-selectors.js';
+import { threadHasPermission } from 'lib/shared/thread-utils.js';
 import type { InviteLink } from 'lib/types/link-types.js';
+import { threadPermissions } from 'lib/types/thread-permission-types.js';
 import type { ThreadInfo } from 'lib/types/thread-types.js';
 
 import SWMansionIcon from '../components/swmansion-icon.react.js';
 import { displayActionResultModal } from '../navigation/action-result-modal.js';
 import type { RootNavigationProp } from '../navigation/root-navigator.react.js';
 import type { NavigationRoute } from '../navigation/route-names.js';
+import { ManagePublicLinkRouteName } from '../navigation/route-names.js';
 import { useSelector } from '../redux/redux-utils.js';
 import { useStyles, useColors } from '../themes/colors.js';
 
@@ -41,8 +44,41 @@ function ViewInviteLinksScreen(props: Props): React.Node {
     setTimeout(confirmCopy);
   }, [linkUrl]);
 
+  const { navigate } = props.navigation;
+  const onEditButtonClick = React.useCallback(() => {
+    navigate<'ManagePublicLink'>({
+      name: ManagePublicLinkRouteName,
+      params: {
+        community,
+      },
+    });
+  }, [community, navigate]);
   let publicLinkSection = null;
-  if (inviteLink) {
+  if (
+    inviteLink ||
+    threadHasPermission(community, threadPermissions.MANAGE_INVITE_LINKS)
+  ) {
+    let description;
+    if (threadHasPermission(community, threadPermissions.MANAGE_INVITE_LINKS)) {
+      description = (
+        <>
+          <Text style={styles.details}>
+            Public links allow unlimited uses and never expire.
+          </Text>
+          <TouchableOpacity onPress={onEditButtonClick}>
+            <Text style={[styles.details, styles.editLinkButton]}>
+              Edit public link
+            </Text>
+          </TouchableOpacity>
+        </>
+      );
+    } else {
+      description = (
+        <Text style={styles.details}>
+          Use this public link to invite your friends into the community!
+        </Text>
+      );
+    }
     publicLinkSection = (
       <>
         <Text style={styles.sectionTitle}>PUBLIC LINK</Text>
@@ -58,9 +94,7 @@ function ViewInviteLinksScreen(props: Props): React.Node {
               <Text style={styles.copy}>Copy</Text>
             </View>
           </TouchableOpacity>
-          <Text style={styles.details}>
-            Use this public link to invite your friends into the community!
-          </Text>
+          {description}
         </View>
       </>
     );
@@ -92,6 +126,7 @@ const unboundStyles = {
   link: {
     paddingHorizontal: 16,
     paddingVertical: 9,
+    marginBottom: 16,
     backgroundColor: 'inviteLinkButtonBackground',
     borderRadius: 20,
     flexDirection: 'row',
@@ -119,7 +154,9 @@ const unboundStyles = {
     fontWeight: '400',
     lineHeight: 18,
     color: 'modalForegroundLabel',
-    paddingTop: 16,
+  },
+  editLinkButton: {
+    color: 'purpleLink',
   },
 };
 
