c8c6f3b07f065b6ae6b93a1fddbe7afb48d1be24
Ashoat Tevosyan <ashoat@comm.app>
2023-05-22 15:20:56 -0400

    [native] Implement UsernameSelection logic
    Summary: See designs [here](https://www.figma.com/file/a1nkbWgbgjRlrOY9LVurTz/Comm-%2F-Desktop-app?type=design&node-id=11725%3A253976&t=0NCPGkwVAXQJXx10-1). Some additional comments inline.
    
    Test Plan: [video]
    
    Reviewers: atul, ginsu
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D7925

--- native/account/registration/username-selection.react.js
+++ native/account/registration/username-selection.react.js
@@ -1,16 +1,33 @@
 // @flow
 
 import * as React from 'react';
-import { Text } from 'react-native';
+import { View, Text } from 'react-native';
+
+import {
+  exactSearchUser,
+  exactSearchUserActionTypes,
+} from 'lib/actions/user-actions.js';
+import { createLoadingStatusSelector } from 'lib/selectors/loading-selectors.js';
+import { validUsernameRegex } from 'lib/shared/account-utils.js';
+import {
+  useServerCall,
+  useDispatchActionPromise,
+} from 'lib/utils/action-utils.js';
 
 import RegistrationButtonContainer from './registration-button-container.react.js';
 import RegistrationButton from './registration-button.react.js';
 import RegistrationContainer from './registration-container.react.js';
 import RegistrationContentContainer from './registration-content-container.react.js';
 import type { RegistrationNavigationProp } from './registration-navigator.react.js';
+import RegistrationTextInput from './registration-text-input.react.js';
 import type { CoolOrNerdMode } from './registration-types.js';
 import type { NavigationRoute } from '../../navigation/route-names.js';
+import { useSelector } from '../../redux/redux-utils.js';
 import { useStyles } from '../../themes/colors.js';
+
+const exactSearchUserLoadingStatusSelector = createLoadingStatusSelector(
+  exactSearchUserActionTypes,
+);
 
 export type UsernameSelectionParams = {
   +userSelections: {
@@ -25,19 +42,112 @@ type Props = {
 };
 // eslint-disable-next-line no-unused-vars
 function UsernameSelection(props: Props): React.Node {
-  const onProceed = React.useCallback(() => {}, []);
+  const [username, setUsername] = React.useState('');
+  const validUsername = username.search(validUsernameRegex) > -1;
 
+  const [errorText, setErrorText] = React.useState<React.Node>(null);
   const styles = useStyles(unboundStyles);
+  const checkUsernameValidity = React.useCallback(() => {
+    if (validUsername) {
+      return true;
+    }
+    setErrorText(
+      <>
+        <Text style={styles.errorText}>Usernames must:</Text>
+        <View style={styles.listItem}>
+          <Text style={[styles.errorText, styles.listItemNumber]}>{'1. '}</Text>
+          <Text style={[styles.errorText, styles.listItemContent]}>
+            Be at least one character long.
+          </Text>
+        </View>
+        <View style={styles.listItem}>
+          <Text style={[styles.errorText, styles.listItemNumber]}>{'2. '}</Text>
+          <Text style={[styles.errorText, styles.listItemContent]}>
+            Start with either a letter or a number.
+          </Text>
+        </View>
+        <View style={styles.listItem}>
+          <Text style={[styles.errorText, styles.listItemNumber]}>{'3. '}</Text>
+          <Text style={[styles.errorText, styles.listItemContent]}>
+            Contain only letters, numbers, or the characters “-” and “_”.
+          </Text>
+        </View>
+      </>,
+    );
+    return false;
+  }, [
+    validUsername,
+    styles.errorText,
+    styles.listItem,
+    styles.listItemNumber,
+    styles.listItemContent,
+  ]);
+
+  const exactSearchUserCall = useServerCall(exactSearchUser);
+  const dispatchActionPromise = useDispatchActionPromise();
+  const onProceed = React.useCallback(async () => {
+    if (!checkUsernameValidity()) {
+      return;
+    }
+
+    const searchPromise = exactSearchUserCall(username);
+    dispatchActionPromise(exactSearchUserActionTypes, searchPromise);
+    const { userInfo } = await searchPromise;
+
+    if (userInfo) {
+      setErrorText(
+        <Text style={styles.errorText}>
+          Username taken. Please try another one
+        </Text>,
+      );
+      return;
+    }
+
+    setErrorText('');
+  }, [
+    checkUsernameValidity,
+    username,
+    exactSearchUserCall,
+    dispatchActionPromise,
+    styles.errorText,
+  ]);
+
+  const exactSearchUserCallLoading = useSelector(
+    state => exactSearchUserLoadingStatusSelector(state) === 'loading',
+  );
+  let buttonVariant = 'disabled';
+  if (exactSearchUserCallLoading) {
+    buttonVariant = 'loading';
+  } else if (validUsername) {
+    buttonVariant = 'enabled';
+  }
+
   return (
     <RegistrationContainer>
       <RegistrationContentContainer>
         <Text style={styles.header}>Pick a username</Text>
+        <RegistrationTextInput
+          value={username}
+          onChangeText={setUsername}
+          placeholder="Username"
+          autoFocus={true}
+          autoCorrect={false}
+          autoCapitalize="none"
+          keyboardType="ascii-capable"
+          textContentType="username"
+          autoComplete="username-new"
+          returnKeyType="next"
+          onSubmitEditing={onProceed}
+          editable={!exactSearchUserCallLoading}
+          onBlur={checkUsernameValidity}
+        />
+        <View style={styles.error}>{errorText}</View>
       </RegistrationContentContainer>
       <RegistrationButtonContainer>
         <RegistrationButton
           onPress={onProceed}
           label="Next"
-          variant="disabled"
+          variant={buttonVariant}
         />
       </RegistrationButtonContainer>
     </RegistrationContainer>
@@ -49,6 +159,24 @@ const unboundStyles = {
     fontSize: 24,
     color: 'panelForegroundLabel',
     paddingBottom: 16,
+  },
+  error: {
+    marginTop: 16,
+  },
+  errorText: {
+    fontFamily: 'Arial',
+    fontSize: 15,
+    lineHeight: 20,
+    color: 'redText',
+  },
+  listItem: {
+    flexDirection: 'row',
+  },
+  listItemNumber: {
+    fontWeight: 'bold',
+  },
+  listItemContent: {
+    flexShrink: 1,
   },
 };
 
