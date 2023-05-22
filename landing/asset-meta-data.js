0a23afad61a752cbc6821d13597d779243bb7c7b
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-05-22 11:14:51 +0300

    [landing] redesign hero section layout
    Summary: This diff redesigns the hero layout to match the new designs shown in the figma document.
    
    Test Plan: Please watch the demo video and see the screenshots below
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8075

--- landing/asset-meta-data.js
+++ landing/asset-meta-data.js
@@ -1,24 +1,14 @@
 // @flow
 
-import css from './landing.css';
-
 export type ImageAsset = {
   +alt: string,
   +url: string,
-  +title: string,
-  +description: string,
-  +imageStyle: string,
-  +infoStyle: string,
 };
 
 export const assetsCacheURLPrefix = 'https://dh9fld3hutpxf.cloudfront.net';
 export const imageAssetMetaData = [
   {
-    alt: 'a mobile phone screen highlighting chat and DAO voting',
-    url: `${assetsCacheURLPrefix}/Header`,
-    imageStyle: css.heroImage,
-    infoStyle: css.heroInfo,
-    title: 'Header',
-    description: '',
+    alt: 'hero image showcasing Comm',
+    url: `${assetsCacheURLPrefix}/hero`,
   },
 ];
