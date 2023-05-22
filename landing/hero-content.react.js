21efc567b7d18ef7a543a51b8130952b96fef4c8
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-05-22 11:14:51 +0300

    [landing] redesign hero section layout
    Summary: This diff redesigns the hero layout to match the new designs shown in the figma document.
    
    Test Plan: Please watch the demo video and see the screenshots below
    
    Reviewers:
    
    Subscribers:

--- landing/hero-content.react.js
+++ landing/hero-content.react.js
@@ -3,13 +3,16 @@
 import classNames from 'classnames';
 import * as React from 'react';
 
-import { imageAssetMetaData } from './asset-meta-data.js';
 import css from './hero-content.css';
 import SubscriptionForm from './subscription-form.react.js';
 import typography from './typography.css';
 
-function HeroContent(): React.Node {
-  const [hero] = imageAssetMetaData;
+type Props = {
+  +infoStyle: string,
+};
+
+function HeroContent(props: Props): React.Node {
+  const { infoStyle } = props;
 
   const headerClassName = classNames([typography.display3, css.header]);
   const subheaderClassName = classNames([
@@ -18,17 +21,15 @@ function HeroContent(): React.Node {
   ]);
 
   return (
-    <section className={hero.infoStyle}>
-      <div className={css.contentWrapper}>
-        <h1 className={headerClassName}>
-          Comm is an encrypted messaging app for <span>communities</span>
-        </h1>
-        <p className={subheaderClassName}>
-          We extend Signal&rsquo;s model of E2E encryption with personal servers
-          to enable richer functionality.
-        </p>
-        <SubscriptionForm />
-      </div>
+    <section className={infoStyle}>
+      <h1 className={headerClassName}>
+        Comm is an encrypted messaging app for <span>communities</span>
+      </h1>
+      <p className={subheaderClassName}>
+        We extend Signal&rsquo;s model of E2E encryption with personal servers
+        to enable richer functionality.
+      </p>
+      <SubscriptionForm />
     </section>
   );
 }
