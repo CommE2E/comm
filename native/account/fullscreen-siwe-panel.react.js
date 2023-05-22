043323cea22006dec571cd0b5c6091513b684a6b
Ashoat Tevosyan <ashoat@comm.app>
2023-05-22 16:16:05 -0400

    [native] Extract onClose-disabling behavior from SIWEPanel to FullscreenSIWEPanel
    Summary:
    In the original `SIWEPanel`, we [implemented logic](https://phab.comm.dev/D6068) to make sure that after a successful SIWE, we didn't trigger `goBackToPrompt` after the closing of the bottom sheet.
    
    However, this logic is currently messing with the new implementation, where it's preventing a second SIWE authorization since the first one was never cleared since the `onClose` wasn't called.
    
    This diff moves the behavior that blocks `goBackToPrompt` to `FullscreenSIWEPanel`, so that both use cases work correctly.
    
    Depends on D7927
    
    Test Plan:
    1. I tested the old workflow and made sure that `goBackToPrompt` wasn't triggered after a successful SIWE
    2. See videos below for the impact on the new workflow:
    
    [videos]
    
    Reviewers: atul, ginsu
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D7928

--- native/account/fullscreen-siwe-panel.react.js
+++ native/account/fullscreen-siwe-panel.react.js
@@ -3,6 +3,8 @@
 import * as React from 'react';
 import { Alert, ActivityIndicator, View } from 'react-native';
 
+import type { SIWEResult } from 'lib/types/siwe-types.js';
+
 import { useSIWEServerCall } from './siwe-hooks.js';
 import SIWEPanel from './siwe-panel.react.js';
 
@@ -22,19 +24,34 @@ function FullscreenSIWEPanel(props: Props): React.Node {
     [],
   );
 
-  const { onClose } = props;
+  const onCloseProp = props.onClose;
   const siweServerCallParams = React.useMemo(() => {
     const onServerCallFailure = () => {
       Alert.alert(
         'Unknown error',
         'Uhh... try again?',
-        [{ text: 'OK', onPress: onClose }],
+        [{ text: 'OK', onPress: onCloseProp }],
         { cancelable: false },
       );
     };
     return { onFailure: onServerCallFailure };
-  }, [onClose]);
+  }, [onCloseProp]);
   const siweServerCall = useSIWEServerCall(siweServerCallParams);
+
+  const successRef = React.useRef(false);
+  const onSuccess = React.useCallback(
+    (result: SIWEResult) => {
+      successRef.current = true;
+      return siweServerCall(result);
+    },
+    [siweServerCall],
+  );
+
+  const onClose = React.useCallback(() => {
+    if (!successRef.current) {
+      onCloseProp();
+    }
+  }, [onCloseProp]);
 
   const { closing } = props;
   return (
@@ -44,7 +61,7 @@ function FullscreenSIWEPanel(props: Props): React.Node {
         closing={closing}
         onClosed={onClose}
         onClosing={onClose}
-        onSuccessfulWalletSignature={siweServerCall}
+        onSuccessfulWalletSignature={onSuccess}
         setLoading={setLoading}
       />
     </>
