983f258fb7d967733a8767c6e4b9632f18c177bb
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2023-05-23 15:41:55 +0200

    [web] Add loadable video component
    Summary:
    Extracted the video logic from D7902 as this will also be needed for modals.
    This component is intended to display thumbnail while video is loading. And while thumbnail is decrypted/loaded, the thumbhash is displayed.  The component also supports encrypted thumbnails.
    
    Test Plan: Tested together with D7902
    
    Reviewers: ashoat, atul, ginsu, inka
    
    Reviewed By: ashoat
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D7946

---
+++ web/media/loadable-video.react.js
@@ -0,0 +1,86 @@
+// @flow
+
+import invariant from 'invariant';
+import * as React from 'react';
+
+import type { Shape } from 'lib/types/core.js';
+
+import { decryptMedia } from './encryption-utils.js';
+import { preloadImage } from './media-utils.js';
+
+type ThumbnailSource =
+  | {
+      +thumbnailURI: string,
+    }
+  | {
+      +thumbnailHolder: string,
+      +thumbnailEncryptionKey: string,
+    };
+type Props = {
+  +uri: ?string,
+  +thumbnailSource: ThumbnailSource,
+  +thumbHashDataURL?: ?string,
+  +elementStyle?: ?Shape<CSSStyleDeclaration>,
+};
+
+function LoadableVideo(props: Props, videoRef: React.Ref<'video'>): React.Node {
+  const { uri, thumbHashDataURL, thumbnailSource, elementStyle } = props;
+  const { thumbnailURI, thumbnailHolder, thumbnailEncryptionKey } =
+    thumbnailSource;
+
+  const [thumbnailImage, setThumbnailImage] = React.useState(null);
+
+  React.useEffect(() => {
+    let isMounted = true,
+      uriToDispose;
+    setThumbnailImage(null);
+
+    (async () => {
+      if (thumbnailURI) {
+        await preloadImage(thumbnailURI);
+        if (isMounted) {
+          setThumbnailImage(thumbnailURI);
+        }
+        return;
+      }
+
+      invariant(
+        thumbnailHolder && thumbnailEncryptionKey,
+        'invalid encrypted thumbnail source',
+      );
+      const { result } = await decryptMedia(
+        thumbnailHolder,
+        thumbnailEncryptionKey,
+      );
+      if (isMounted && result.success) {
+        setThumbnailImage(result.uri);
+        uriToDispose = result.uri;
+      }
+    })();
+
+    return () => {
+      isMounted = false;
+      if (uriToDispose) {
+        URL.revokeObjectURL(uriToDispose);
+      }
+    };
+  }, [thumbnailURI, thumbnailHolder, thumbnailEncryptionKey]);
+
+  let videoSource;
+  if (uri) {
+    videoSource = <source src={uri} />;
+  }
+  const poster = thumbnailImage ?? thumbHashDataURL;
+  return (
+    <video controls poster={poster} style={elementStyle} ref={videoRef}>
+      {videoSource}
+    </video>
+  );
+}
+
+const MemoizedLoadableVideo: React.AbstractComponent<Props, HTMLVideoElement> =
+  React.memo<Props, HTMLVideoElement>(
+    React.forwardRef<Props, HTMLVideoElement>(LoadableVideo),
+  );
+
+export default MemoizedLoadableVideo;
