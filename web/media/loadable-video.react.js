2788f6601c38e4db4fffa16a22c294af88372e04
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2023-05-23 15:41:55 +0200

    [web] Add loadable video component
    Summary:
    Extracted the video logic from D7902 as this will also be needed for modals.
    
    Depends on D7901
    
    Test Plan: Tested together with D7902
    
    Reviewers: ashoat, atul, ginsu, inka
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D7946

---
+++ web/media/loadable-video.react.js
@@ -0,0 +1,42 @@
+// @flow
+
+import * as React from 'react';
+
+import type { Shape } from 'lib/types/core.js';
+
+import { preloadImage } from './media-utils.js';
+
+type Props = {
+  +uri: ?string,
+  +thumbnailURI: ?string,
+  +thumbHashDataURL?: ?string,
+  +elementStyle?: ?Shape<CSSStyleDeclaration>,
+};
+
+function LoadableVideo(props: Props): React.Node {
+  const { uri, thumbHashDataURL, thumbnailURI, elementStyle } = props;
+
+  const [isVideoLoaded, setVideoLoaded] = React.useState(false);
+  const handleVideoLoad = React.useCallback(() => setVideoLoaded(true), []);
+  React.useEffect(() => {
+    // video thumbnail is used as a poster image when the video is loaded
+    // preload it so the browser can immediately load it from cache
+    if (thumbnailURI) {
+      preloadImage(thumbnailURI);
+    }
+  }, [thumbnailURI]);
+
+  const poster = isVideoLoaded ? thumbnailURI : thumbHashDataURL;
+  return (
+    <video
+      controls
+      poster={poster}
+      style={elementStyle}
+      onLoadedData={handleVideoLoad}
+    >
+      <source src={uri} />
+    </video>
+  );
+}
+
+export default LoadableVideo;
