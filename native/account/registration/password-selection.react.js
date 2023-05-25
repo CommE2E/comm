8cf3121212dbcaac67ec81ba39b2757d969c80b1
Ashoat Tevosyan <ashoat@comm.app>
2023-05-25 15:25:36 -0400

    [native] Implement PasswordSelection logic
    Summary: See designs [here](https://www.figma.com/file/a1nkbWgbgjRlrOY9LVurTz/Comm-%2F-Desktop-app?type=design&node-id=11725%3A254022&t=W51OpMbZ5pa07t00-1).
    
    Depends on D7990
    
    Test Plan: [video]
    
    Reviewers: atul, ginsu

--- native/account/registration/password-selection.react.js
+++ native/account/registration/password-selection.react.js
@@ -1,13 +1,14 @@
 // @flow
 
 import * as React from 'react';
-import { Text } from 'react-native';
+import { View, Text } from 'react-native';
 
 import RegistrationButtonContainer from './registration-button-container.react.js';
 import RegistrationButton from './registration-button.react.js';
 import RegistrationContainer from './registration-container.react.js';
 import RegistrationContentContainer from './registration-content-container.react.js';
 import type { RegistrationNavigationProp } from './registration-navigator.react.js';
+import RegistrationTextInput from './registration-text-input.react.js';
 import type { CoolOrNerdMode } from './registration-types.js';
 import type { NavigationRoute } from '../../navigation/route-names.js';
 import { useStyles } from '../../themes/colors.js';
@@ -20,25 +21,95 @@ export type PasswordSelectionParams = {
   },
 };
 
+type PasswordError = 'passwords_dont_match' | 'empty_password';
+
 type Props = {
   +navigation: RegistrationNavigationProp<'PasswordSelection'>,
   +route: NavigationRoute<'PasswordSelection'>,
 };
 // eslint-disable-next-line no-unused-vars
 function PasswordSelection(props: Props): React.Node {
-  const onProceed = React.useCallback(() => {}, []);
+  const [password, setPassword] = React.useState('');
+  const [confirmPassword, setConfirmPassword] = React.useState('');
+  const passwordsMatch = password === confirmPassword;
+  const passwordIsEmpty = password === '';
+
+  const [passwordError, setPasswordError] = React.useState<?PasswordError>();
+  const potentiallyClearErrors = React.useCallback(() => {
+    if (!passwordsMatch || passwordIsEmpty) {
+      return false;
+    }
+    setPasswordError(null);
+    return true;
+  }, [passwordsMatch, passwordIsEmpty]);
+  const checkPasswordValidity = React.useCallback(() => {
+    if (!passwordsMatch) {
+      setPasswordError('passwords_dont_match');
+      return false;
+    } else if (passwordIsEmpty) {
+      setPasswordError('empty_password');
+      return false;
+    }
+    return potentiallyClearErrors();
+  }, [passwordsMatch, passwordIsEmpty, potentiallyClearErrors]);
+
+  const onProceed = React.useCallback(() => {
+    if (!checkPasswordValidity()) {
+      return;
+    }
+  }, [checkPasswordValidity]);
 
   const styles = useStyles(unboundStyles);
+  let errorText;
+  if (passwordError === 'passwords_dont_match') {
+    errorText = (
+      <Text style={styles.errorText}>Passwords don&rsquo;t match</Text>
+    );
+  } else if (passwordError === 'empty_password') {
+    errorText = <Text style={styles.errorText}>Password cannot be empty</Text>;
+  }
+
+  const confirmPasswordInputRef = React.useRef();
+  const focusConfirmPasswordInput = React.useCallback(() => {
+    confirmPasswordInputRef.current?.focus();
+  }, []);
+
   return (
     <RegistrationContainer>
       <RegistrationContentContainer>
         <Text style={styles.header}>Pick a password</Text>
+        <RegistrationTextInput
+          value={password}
+          onChangeText={setPassword}
+          placeholder="Password"
+          autoFocus={true}
+          secureTextEntry={true}
+          textContentType="password"
+          autoComplete="password-new"
+          returnKeyType="next"
+          onSubmitEditing={focusConfirmPasswordInput}
+          onBlur={potentiallyClearErrors}
+        />
+        <RegistrationTextInput
+          value={confirmPassword}
+          onChangeText={setConfirmPassword}
+          placeholder="Confirm password"
+          secureTextEntry={true}
+          textContentType="password"
+          autoComplete="password-new"
+          returnKeyType="go"
+          onSubmitEditing={onProceed}
+          onBlur={checkPasswordValidity}
+          style={styles.confirmPassword}
+          ref={confirmPasswordInputRef}
+        />
+        <View style={styles.error}>{errorText}</View>
       </RegistrationContentContainer>
       <RegistrationButtonContainer>
         <RegistrationButton
           onPress={onProceed}
           label="Next"
-          variant="disabled"
+          variant={passwordsMatch ? 'enabled' : 'disabled'}
         />
       </RegistrationButtonContainer>
     </RegistrationContainer>
@@ -50,6 +121,18 @@ const unboundStyles = {
     fontSize: 24,
     color: 'panelForegroundLabel',
     paddingBottom: 16,
+  },
+  error: {
+    marginTop: 16,
+  },
+  errorText: {
+    fontFamily: 'Arial',
+    fontSize: 15,
+    lineHeight: 20,
+    color: 'redText',
+  },
+  confirmPassword: {
+    marginTop: 16,
   },
 };
 
