17d011c9fe8d89af9cd655169b59893bd474e84f
kosmydel <kukubaczek@gmail.com>
2023-05-25 11:04:28 +0200

    [web] Disabled edit button if edited message is empty
    Summary: We want to match editing behavior with the native. If the user types an empty message, the edit button will be disabled, and the user will be prevented from saving the changes.
    
    Test Plan:
    - Entered edit mode of a message.
    - Cleared the whole message.
    - Checked if the edit button is disabled.
    - Typed some spaces.
    - Checked if the edit button is still disabled.
    - Typed some text.
    - Checked if the edit button is enabled.
    
    Reviewers: michal, inka
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D7971

--- web/chat/edit-text-message.react.js
+++ web/chat/edit-text-message.react.js
@@ -45,6 +45,11 @@ function EditTextMessage(props: Props): React.Node {
     [threadColor],
   );
 
+  const isMessageEmpty = React.useMemo(
+    () => trimMessage(editedMessageDraft) === '',
+    [editedMessageDraft],
+  );
+
   const isMessageEdited = React.useMemo(() => {
     const { messageInfo } = item;
     if (!messageInfo || !messageInfo.text || !editState) {
@@ -59,6 +64,9 @@ function EditTextMessage(props: Props): React.Node {
 
   const checkAndEdit = async () => {
     const { id: messageInfoID } = item.messageInfo;
+    if (isMessageEmpty) {
+      return;
+    }
     if (!isMessageEdited) {
       clearEditModal();
       return;
@@ -149,6 +157,7 @@ function EditTextMessage(props: Props): React.Node {
             variant="filled"
             buttonColor={saveButtonColor}
             onClick={checkAndEdit}
+            disabled={isMessageEmpty}
           >
             Save (enter)
           </Button>
