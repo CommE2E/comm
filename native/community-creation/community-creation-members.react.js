973eb2486f282486d5c3171fea84ae59d37adee3
Atul Madhugiri <atulm@berkeley.edu>
2023-05-25 15:14:32 -0400

    [native] Add `CommunityCreationKeyserverLabel` to `CommunityCreationMembers`
    Summary:
    Pretty straightforward, just adding keyserver label to "Add members" screen as in the designs.
    
    ---
    
    Depends on D7988
    
    Test Plan:
    Looks as expected:

--- native/community-creation/community-creation-members.react.js
+++ native/community-creation/community-creation-members.react.js
@@ -2,8 +2,12 @@
 
 import * as React from 'react';
 
+import CommunityCreationKeyserverLabel from './community-creation-keyserver-label.react.js';
 import type { CommunityCreationNavigationProp } from './community-creation-navigator.react.js';
+import RegistrationContainer from '../account/registration/registration-container.react.js';
+import RegistrationContentContainer from '../account/registration/registration-content-container.react.js';
 import type { NavigationRoute } from '../navigation/route-names.js';
+import { useStyles } from '../themes/colors.js';
 
 type Props = {
   +navigation: CommunityCreationNavigationProp<'CommunityCreationMembers'>,
@@ -12,7 +16,21 @@ type Props = {
 
 // eslint-disable-next-line no-unused-vars
 function CommunityCreationMembers(props: Props): React.Node {
-  return null;
+  const styles = useStyles(unboundStyles);
+
+  return (
+    <RegistrationContainer>
+      <RegistrationContentContainer style={styles.containerPaddingOverride}>
+        <CommunityCreationKeyserverLabel />
+      </RegistrationContentContainer>
+    </RegistrationContainer>
+  );
 }
+
+const unboundStyles = {
+  containerPaddingOverride: {
+    padding: 0,
+  },
+};
 
 export default CommunityCreationMembers;
