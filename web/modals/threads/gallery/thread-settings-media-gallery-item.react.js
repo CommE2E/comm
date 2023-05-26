f599a6ea375d62b4fc2d94cb98e92c8e2939e735
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2023-05-26 09:34:28 +0200

    [web] Add MediaGalleryItem component
    Summary:
    Proper displaying of encrypted and non-encrypted thread gallery media required more code so I extracted it into a separate component.
    Basically, most of the work is already done by the `<EncryptedMultimedia>` component so I only needed to add thumbhash and background for non-encrypted images.
    
    Depends on D7993
    
    Test Plan: Tested together with next diff where this is used.
    
    Reviewers: inka, ginsu, atul, kuba
    
    Reviewed By: atul
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D7994

---
+++ web/modals/threads/gallery/thread-settings-media-gallery-item.react.js
@@ -0,0 +1,71 @@
+// @flow
+
+import invariant from 'invariant';
+import * as React from 'react';
+
+import { fetchableMediaURI } from 'lib/media/media-utils.js';
+
+import EncryptedMultimedia from '../../../media/encrypted-multimedia.react.js';
+import { usePlaceholder } from '../../../media/media-utils.js';
+
+type MediaSource =
+  | {
+      +kind: 'plain',
+      +uri: string,
+      +thumbHash: ?string,
+    }
+  | {
+      +kind: 'encrypted',
+      +holder: string,
+      +encryptionKey: string,
+      +thumbHash: ?string,
+    };
+type Props = {
+  +imageSource: MediaSource,
+  +imageCSSClass: string,
+  +imageContainerCSSClass: string,
+  +onClick?: () => void,
+};
+function MediaGalleryItem(props: Props) {
+  const { imageSource, imageCSSClass, imageContainerCSSClass } = props;
+
+  const { thumbHash, encryptionKey } = imageSource;
+  const placeholderImage = usePlaceholder(thumbHash, encryptionKey);
+  const imageStyle = React.useMemo(
+    () => ({
+      background: placeholderImage
+        ? `center / cover url(${placeholderImage})`
+        : undefined,
+    }),
+    [placeholderImage],
+  );
+
+  let image;
+  if (imageSource.kind === 'plain') {
+    const uri = fetchableMediaURI(imageSource.uri);
+    image = <img src={uri} className={imageCSSClass} style={imageStyle} />;
+  } else if (imageSource.kind === 'encrypted') {
+    const { holder } = imageSource;
+    invariant(encryptionKey, 'encryptionKey undefined for encrypted image');
+    image = (
+      <EncryptedMultimedia
+        type="encrypted_photo"
+        holder={holder}
+        encryptionKey={encryptionKey}
+        placeholderSrc={placeholderImage}
+        multimediaClassName={imageCSSClass}
+      />
+    );
+  }
+
+  return (
+    <div className={imageContainerCSSClass} onClick={props.onClick}>
+      {image}
+    </div>
+  );
+}
+
+const MemoizedItem: React.ComponentType<Props> =
+  React.memo<Props>(MediaGalleryItem);
+
+export default MemoizedItem;
