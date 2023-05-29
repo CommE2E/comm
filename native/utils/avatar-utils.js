5b726625fce40ab2e8e255c250d64e16ead33ff3
Ashoat Tevosyan <ashoat@comm.app>
2023-05-29 18:06:34 -0400

    [native] Replace generic useSaveThreadAvatar with new generic setThreadAvatar
    Summary:
    Unifying codepaths here, similar to what D8018 did for user avatars.
    
    In a later diff I'll need to make sure that `activeThreadSelector` works with the `EmojiThreadAvatarCreation` screen.
    
    Depends on D8020
    
    Test Plan: Test setting emoji avatar for thread from iOS simulator
    
    Reviewers: ginsu, atul
    
    Subscribers:

--- native/utils/avatar-utils.js
+++ native/utils/avatar-utils.js
@@ -1,63 +1,7 @@
 // @flow
 
-import * as React from 'react';
-import { Alert } from 'react-native';
-
-import {
-  changeThreadSettings,
-  changeThreadSettingsActionTypes,
-} from 'lib/actions/thread-actions.js';
-import type { UpdateUserAvatarRequest } from 'lib/types/avatar-types.js';
-import {
-  useServerCall,
-  useDispatchActionPromise,
-} from 'lib/utils/action-utils.js';
-
-import { displayActionResultModal } from '../navigation/action-result-modal.js';
-
 function useShouldRenderAvatars(): boolean {
   return true;
 }
 
-function useSaveThreadAvatar(): (
-  newAvatarRequest: UpdateUserAvatarRequest,
-  threadID: string,
-) => mixed {
-  const callChangeThreadSettings = useServerCall(changeThreadSettings);
-  const dispatchActionPromise = useDispatchActionPromise();
-
-  return React.useCallback(
-    (newAvatarRequest, threadID) => {
-      const saveAvatarPromise = (async () => {
-        try {
-          const response = await callChangeThreadSettings({
-            threadID,
-            changes: { avatar: newAvatarRequest },
-          });
-          displayActionResultModal('Avatar updated!');
-
-          return response;
-        } catch (e) {
-          Alert.alert(
-            'Couldn’t save avatar',
-            'Please try again later',
-            [{ text: 'OK' }],
-            {
-              cancelable: true,
-            },
-          );
-          throw e;
-        }
-      })();
-
-      dispatchActionPromise(
-        changeThreadSettingsActionTypes,
-        saveAvatarPromise,
-        { customKeyName: `${changeThreadSettingsActionTypes.started}:avatar` },
-      );
-    },
-    [callChangeThreadSettings, dispatchActionPromise],
-  );
-}
-
-export { useShouldRenderAvatars, useSaveThreadAvatar };
+export { useShouldRenderAvatars };
