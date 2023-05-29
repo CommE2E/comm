d1aec78f634bce104c87c2c60600e13235b0c38f
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-05-29 16:57:29 -0400

    [landing] introduce competitor feature base component
    Summary: The competitor comparison cards and competitor comparison modal will have a similar structure so I factored out the base component so that both places can use this code.
    
    Test Plan: The competitor feature cards look and behave the same, and I will do more testing of the competitor comparison modal when I introduce it
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D8024

--- landing/competitor-feature-card.react.js
+++ landing/competitor-feature-card.react.js
@@ -1,14 +1,11 @@
 // @flow
 
-import { faWrench } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames';
 import * as React from 'react';
 
-import CommLogo from './assets/comm-logo.react.js';
 import type { Competitors } from './competitor-data.js';
 import css from './competitor-feature-card.css';
-import CompetitorLogo from './competitor-logo.react.js';
+import CompetitorFeature from './competitor-feature.react.js';
 import typography from './typography.css';
 
 type Props = {
@@ -30,45 +27,20 @@ function CompetitorFeatureCard(props: Props): React.Node {
     onClick,
   } = props;
 
-  const headingClassName = classNames([typography.heading3, css.headingText]);
-  const comingSoonClassName = classNames([
-    typography.paragraph3,
-    css.comingSoonText,
-  ]);
-  const descriptionClassName = classNames([
-    typography.paragraph1,
-    css.descriptionText,
-  ]);
   const readMoreClassName = classNames([
     typography.paragraph2,
     css.readMoreText,
   ]);
 
-  let comingSoonBadge;
-  if (comingSoon) {
-    comingSoonBadge = (
-      <div className={css.comingSoonBadge}>
-        <FontAwesomeIcon
-          size="sm"
-          className={css.comingSoonIcon}
-          icon={faWrench}
-        />
-        <span className={comingSoonClassName}>Coming Soon</span>
-      </div>
-    );
-  }
-
   return (
     <a className={css.container} onClick={onClick}>
-      <div className={css.headingContainer}>
-        <p className={headingClassName}>{title}</p>
-        {comingSoonBadge}
-      </div>
-      <CompetitorLogo name={competitorID} size={30} />
-      <p className={descriptionClassName}>{competitorDescription}</p>
-      <hr />
-      <CommLogo size={30} />
-      <p className={descriptionClassName}>{commDescription}</p>
+      <CompetitorFeature
+        competitorID={competitorID}
+        title={title}
+        comingSoon={comingSoon}
+        competitorDescription={competitorDescription}
+        commDescription={commDescription}
+      />
       <p className={readMoreClassName}>Read more</p>
     </a>
   );
