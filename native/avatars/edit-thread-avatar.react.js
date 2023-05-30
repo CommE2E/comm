CommE2E/comm

commit e043e60b6b46d6f3068e1d7d7027172b1438c2be

[native] Replace generic useSaveThreadAvatar with new generic setThreadAvatar
Summary:
Unifying codepaths here, similar to what D8018 did for user avatars.

In a later diff I'll need to make sure that `activeThreadSelector` works with the `EmojiThreadAvatarCreation` screen.

Depends on D8020

Test Plan: Test setting emoji avatar for thread from iOS simulator

Reviewers: ginsu, atul

Subscribers: tomek

Differential Revision: https://phab.comm.dev/D8021

native/chat/settings/emoji-thread-avatar-creation.react.js
@@ -1,21 +1,15 @@
 // @flow
 
+import invariant from 'invariant';
 import * as React from 'react';
 
-import { changeThreadSettingsActionTypes } from 'lib/actions/thread-actions.js';
-import { createLoadingStatusSelector } from 'lib/selectors/loading-selectors.js';
 import { savedEmojiAvatarSelectorForThread } from 'lib/selectors/thread-selectors.js';
 
+import { EditThreadAvatarContext } from '../../avatars/edit-thread-avatar-provider.react.js';
 import EmojiAvatarCreation from '../../avatars/emoji-avatar-creation.react.js';
 import type { ChatNavigationProp } from '../../chat/chat.react.js';
+import { displayActionResultModal } from '../../navigation/action-result-modal.js';
 import type { NavigationRoute } from '../../navigation/route-names.js';
-import { useSelector } from '../../redux/redux-utils.js';
-import { useSaveThreadAvatar } from '../../utils/avatar-utils.js';
-
-const threadAvatarLoadingStatusSelector = createLoadingStatusSelector(
-  changeThreadSettingsActionTypes,
-  `${changeThreadSettingsActionTypes.started}:avatar`,
-);
 
 export type EmojiThreadAvatarCreationParams = {
   +threadID: string,
@@ -35,20 +29,24 @@ function EmojiThreadAvatarCreation(props: Props): React.Node {
     containingThreadID,
   );
 
-  const saveThreadAvatar = useSaveThreadAvatar();
-  const saveThreadAvatarCallLoading = useSelector(
-    state => threadAvatarLoadingStatusSelector(state) === 'loading',
-  );
+  const editThreadAvatarContext = React.useContext(EditThreadAvatarContext);
+  invariant(editThreadAvatarContext, 'editThreadAvatarContext should be set');
 
-  const saveThreadAvatarCallback = React.useCallback(
-    newAvatarRequest => saveThreadAvatar(newAvatarRequest, threadID),
-    [saveThreadAvatar, threadID],
+  const { setThreadAvatar, threadAvatarSaveInProgress } =
+    editThreadAvatarContext;
+  const setAvatar = React.useCallback(
+    async avatarRequest => {
+      const result = await setThreadAvatar(threadID, avatarRequest);
+      displayActionResultModal('Avatar updated!');
+      return result;
+    },
+    [setThreadAvatar, threadID],
   );
 
   return (
     <EmojiAvatarCreation
-      saveAvatarCall={saveThreadAvatarCallback}
-      saveAvatarCallLoading={saveThreadAvatarCallLoading}
+      saveAvatarCall={setAvatar}
+      saveAvatarCallLoading={threadAvatarSaveInProgress}
       savedEmojiAvatarSelector={selector}
     />
   );

native/utils/avatar-utils.js
@@ -1,63 +1,7 @@
 // @flow
 
-import * as React from 'react';
-import { Alert } from 'react-native';
-
-import {
-  changeThreadSettings,
-  changeThreadSettingsActionTypes,
-} from 'lib/actions/thread-actions.js';
-import type { UpdateUserAvatarRequest } from 'lib/types/avatar-types.js';
-import {
-  useServerCall,
-  useDispatchActionPromise,
-} from 'lib/utils/action-utils.js';
-
-import { displayActionResultModal } from '../navigation/action-result-modal.js';
-
 function useShouldRenderAvatars(): boolean {
   return true;
 }
 
-function useSaveThreadAvatar(): (
-  newAvatarRequest: UpdateUserAvatarRequest,
-  threadID: string,
-) => mixed {
-  const callChangeThreadSettings = useServerCall(changeThreadSettings);
-  const dispatchActionPromise = useDispatchActionPromise();
-
-  return React.useCallback(
-    (newAvatarRequest, threadID) => {
-      const saveAvatarPromise = (async () => {
-        try {
-          const response = await callChangeThreadSettings({
-            threadID,
-            changes: { avatar: newAvatarRequest },
-          });
-          displayActionResultModal('Avatar updated!');
-
-          return response;
-        } catch (e) {
-          Alert.alert(
-            'Couldn’t save avatar',
-            'Please try again later',
-            [{ text: 'OK' }],
-            {
-              cancelable: true,
-            },
-          );
-          throw e;
-        }
-      })();
-
-      dispatchActionPromise(
-        changeThreadSettingsActionTypes,
-        saveAvatarPromise,
-        { customKeyName: `${changeThreadSettingsActionTypes.started}:avatar` },
-      );
-    },
-    [callChangeThreadSettings, dispatchActionPromise],
-  );
-}
-
-export { useShouldRenderAvatars, useSaveThreadAvatar };
+export { useShouldRenderAvatars };

commit 437425a63ca202d5b7646b348e450e29e8322dac

[native] Make EmojiThreadAvatarCreation work with activeThreadSelector
Summary: In D8021, we changing how our loading status worked. Before that the custom key name was not thread specific, but afterwards it is.

In the new system, for `EditThreadAvatarProvider` to know which `threadID` to look up, it calls `activeThreadSelector` on the nav state. For this to work, we need two things to be true:

1. The React Navigation route needs to be in `routeNames` in `route-names.js`
2. The React Navigation route params should have a field called `threadInfo`

This diff makes both changes so that the loading detection works

Depends on D8022

Test Plan: I added an artificial delay on the keyserver side and confirmed that the loading spinner appears currently now (which was a regression introduced in D8021)

Reviewers: ginsu, atul

Subscribers:

native/avatars/edit-thread-avatar.react.js
@@ -39,11 +39,10 @@ function EditThreadAvatar(props: Props): React.Node {
     navigate<'EmojiThreadAvatarCreation'>({
       name: EmojiThreadAvatarCreationRouteName,
       params: {
-        threadID: threadInfo.id,
-        containingThreadID: threadInfo.containingThreadID,
+        threadInfo,
       },
     });
-  }, [navigate, threadInfo.containingThreadID, threadInfo.id]);
+  }, [navigate, threadInfo]);
 
   const selectFromGallery = React.useCallback(
     () => selectFromGalleryAndUpdateThreadAvatar(threadInfo.id),

native/chat/settings/emoji-thread-avatar-creation.react.js
@@ -4,6 +4,7 @@ import invariant from 'invariant';
 import * as React from 'react';
 
 import { savedEmojiAvatarSelectorForThread } from 'lib/selectors/thread-selectors.js';
+import type { RawThreadInfo, ThreadInfo } from 'lib/types/thread-types.js';
 
 import { EditThreadAvatarContext } from '../../avatars/edit-thread-avatar-provider.react.js';
 import EmojiAvatarCreation from '../../avatars/emoji-avatar-creation.react.js';
@@ -12,8 +13,7 @@ import { displayActionResultModal } from '../../navigation/action-result-modal.j
 import type { NavigationRoute } from '../../navigation/route-names.js';
 
 export type EmojiThreadAvatarCreationParams = {
-  +threadID: string,
-  +containingThreadID?: ?string,
+  +threadInfo: RawThreadInfo | ThreadInfo,
 };
 
 type Props = {
@@ -22,7 +22,7 @@ type Props = {
 };
 
 function EmojiThreadAvatarCreation(props: Props): React.Node {
-  const { threadID, containingThreadID } = props.route.params;
+  const { id: threadID, containingThreadID } = props.route.params.threadInfo;
 
   const selector = savedEmojiAvatarSelectorForThread(
     threadID,

native/navigation/route-names.js
@@ -265,4 +265,5 @@ export const threadRoutes = [
   FullScreenThreadMediaGalleryRouteName,
   MessageResultsScreenRouteName,
   MessageSearchRouteName,
+  EmojiThreadAvatarCreationRouteName,
 ];