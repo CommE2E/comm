76dfd7c4938ccb3c123ccd861d1e996efeff99ca
Michal Gniadek <michal.gniadek@swmansion.com>
2023-05-31 11:23:59 +0200

    [web] Use react-window
    Summary:
    Part of [ENG-3976](https://linear.app/comm/issue/ENG-3976)
    This diff uses `react-window` package to render only visible threads. `react-virtualized-auto-sizer` is the recommended utility to automatically set height of the `VariableSizeList`.
    
    There's one issue that I'm aware of: if you scroll down, the search bar will stop rendering and lose focus (but it will keep it's contents). On 1-1 with @ashoat we decided that's good enough for now. I will create an issue explaining a possible solution with docking the search bar before landing this diff.
    
    Adding ashoat as reviewer because of new dependencies.
    
    Test Plan: Check if thread list looks the same, and scrolls correctly. Try searching chat and check if everything works correctly. Check if an empty background tab renders correctly.
    
    Reviewers: inka, kuba, kamil, ashoat
    
    Subscribers: tomek, ashoat
    
    Differential Revision: https://phab.comm.dev/D8234

--- web/chat/chat-thread-list.react.js
+++ web/chat/chat-thread-list.react.js
@@ -1,18 +1,59 @@
 // @flow
 
 import invariant from 'invariant';
+import _sum from 'lodash/fp/sum.js';
 import * as React from 'react';
+import AutoSizer from 'react-virtualized-auto-sizer';
+import { VariableSizeList } from 'react-window';
 
+import type { ChatThreadItem } from 'lib/selectors/chat-selectors.js';
 import { emptyItemText } from 'lib/shared/thread-utils.js';
 
 import ChatThreadListItem from './chat-thread-list-item.react.js';
+import ChatThreadListSearch from './chat-thread-list-search.react.js';
 import css from './chat-thread-list.css';
 import { ThreadListContext } from './thread-list-provider.js';
 import BackgroundIllustration from '../assets/background-illustration.react.js';
 import Button from '../components/button.react.js';
-import Search from '../components/search.react.js';
 import { useSelector } from '../redux/redux-utils.js';
 import { useOnClickNewThread } from '../selectors/thread-selectors.js';
+
+type Item = ChatThreadItem | { +type: 'search' } | { +type: 'empty' };
+
+const sizes = {
+  search: 68,
+  empty: 249,
+  thread: 81,
+  sidebars: { sidebar: 32, seeMore: 22, spacer: 6 },
+};
+
+const getChatThreadItemSize = (item: ChatThreadItem) => {
+  const sidebarHeight = _sum(item.sidebars.map(s => sizes.sidebars[s.type]));
+  return sizes.thread + sidebarHeight;
+};
+
+const itemKey = (index: number, data: $ReadOnlyArray<Item>) => {
+  if (data[index].type === 'search') {
+    return 'search';
+  } else if (data[index].type === 'empty') {
+    return 'empty';
+  } else {
+    return data[index].threadInfo.id;
+  }
+};
+
+const renderItem = ({ index, data, style }) => {
+  let item;
+  if (data[index].type === 'search') {
+    item = <ChatThreadListSearch />;
+  } else if (data[index].type === 'empty') {
+    item = <EmptyItem />;
+  } else {
+    item = <ChatThreadListItem item={data[index]} />;
+  }
+
+  return <div style={style}>{item}</div>;
+};
 
 function ChatThreadList(): React.Node {
   const threadListContext = React.useContext(ThreadListContext);
@@ -20,8 +61,7 @@ function ChatThreadList(): React.Node {
     threadListContext,
     'threadListContext should be set in ChatThreadList',
   );
-  const { activeTab, threadList, setSearchText, searchText } =
-    threadListContext;
+  const { activeTab, threadList } = threadListContext;
 
   const onClickNewThread = useOnClickNewThread();
 
@@ -33,31 +73,63 @@ function ChatThreadList(): React.Node {
 
   const communityID = useSelector(state => state.communityPickerStore.chat);
 
-  const threadComponents: React.Node[] = React.useMemo(() => {
-    const threads = threadList
-      .filter(
+  const threadListContainerRef = React.useRef();
+
+  const threads = React.useMemo(
+    () =>
+      threadList.filter(
         item =>
           !communityID ||
           item.threadInfo.community === communityID ||
           item.threadInfo.id === communityID,
-      )
-      .map(item => <ChatThreadListItem item={item} key={item.threadInfo.id} />);
-    if (threads.length === 0 && isBackground) {
-      threads.push(<EmptyItem key="emptyItem" />);
+      ),
+    [communityID, threadList],
+  );
+
+  React.useEffect(() => {
+    if (threadListContainerRef.current) {
+      threadListContainerRef.current.resetAfterIndex(0, false);
     }
-    return threads;
-  }, [threadList, isBackground, communityID]);
+  }, [threads]);
+
+  const threadListContainer = React.useMemo(() => {
+    const items: Item[] = [{ type: 'search' }, ...threads];
+
+    if (isBackground && threads.length === 0) {
+      items.push({ type: 'empty' });
+    }
+
+    const itemSize = index => {
+      if (items[index].type === 'search') {
+        return sizes.search;
+      } else if (items[index].type === 'empty') {
+        return sizes.empty;
+      }
+
+      return getChatThreadItemSize(items[index]);
+    };
+
+    return (
+      <AutoSizer disableWidth>
+        {({ height }) => (
+          <VariableSizeList
+            itemData={items}
+            itemCount={items.length}
+            itemSize={itemSize}
+            itemKey={itemKey}
+            height={height}
+            ref={threadListContainerRef}
+          >
+            {renderItem}
+          </VariableSizeList>
+        )}
+      </AutoSizer>
+    );
+  }, [isBackground, threads]);
 
   return (
     <>
-      <div className={css.threadListContainer}>
-        <Search
-          onChangeText={setSearchText}
-          searchText={searchText}
-          placeholder="Search chats"
-        />
-        <div>{threadComponents}</div>
-      </div>
+      <div className={css.threadListContainer}>{threadListContainer}</div>
       <div className={css.createNewThread}>
         <Button
           variant="filled"
