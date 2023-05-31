636872563598c0ee9c58120bb6606fac2b0f6f78
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-05-31 11:34:33 -0400

    [landing] add feature cards to competitor comparison
    Summary: This diff is the second part to the introduction of the competitor comparison. This part introduces the feature cards and all the functionality around it
    
    Test Plan: Please watch the demo videos/see the screenshots to see how the competitor comparison looks/behaves with the cards
    
    Subscribers: ashoat, tomek, atul
    
    Differential Revision: https://phab.comm.dev/D8038

--- landing/competitor-comparison.react.js
+++ landing/competitor-comparison.react.js
@@ -3,8 +3,17 @@
 import classNames from 'classnames';
 import * as React from 'react';
 
+import { useModalContext } from 'lib/components/modal-provider.react.js';
+
 import css from './competitor-comparison.css';
+import {
+  competitorData,
+  type Competitor,
+  type FeatureComparison,
+} from './competitor-data.js';
+import CompetitorFeatureCard from './competitor-feature-card.react.js';
 import CompetitorLogo from './competitor-logo.react.js';
+import FeatureModal from './feature-modal.react.js';
 import typography from './typography.css';
 
 const competitors = [
@@ -17,8 +26,17 @@ const competitors = [
 ];
 
 function CompetitorComparison(): React.Node {
+  const { pushModal } = useModalContext();
+
   const [selectedCompetitorID, setSelectedCompetitorID] =
     React.useState<string>('signal');
+
+  const onFeatureCardClick = React.useCallback(
+    (competitor: Competitor, feature: FeatureComparison) => {
+      pushModal(<FeatureModal competitor={competitor} feature={feature} />);
+    },
+    [pushModal],
+  );
 
   const competitorSelector = React.useMemo(
     () =>
@@ -46,12 +64,29 @@ function CompetitorComparison(): React.Node {
     [selectedCompetitorID],
   );
 
+  const featureCards = React.useMemo(() => {
+    const selectedCompetitor = competitorData[selectedCompetitorID];
+
+    return selectedCompetitor.featureComparison.map(feature => (
+      <CompetitorFeatureCard
+        key={`${selectedCompetitor.id}_${feature.title}`}
+        competitorID={selectedCompetitor.id}
+        title={feature.title}
+        comingSoon={feature.comingSoon}
+        competitorDescription={feature.competitorDescriptionShort}
+        commDescription={feature.commDescriptionShort}
+        onClick={() => onFeatureCardClick(selectedCompetitor, feature)}
+      />
+    ));
+  }, [onFeatureCardClick, selectedCompetitorID]);
+
   const headerClassName = classNames([typography.heading1, css.headerText]);
 
   return (
     <section className={css.competitorComparisonSection}>
       <h1 className={headerClassName}>See how Comm is different</h1>
       <div className={css.competitorsContainer}>{competitorSelector}</div>
+      <div className={css.featureCardsContainer}>{featureCards}</div>
     </section>
   );
 }
