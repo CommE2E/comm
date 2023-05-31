afad8943bccb6fdd8676a62c7a942e8fb1782341
Ashoat Tevosyan <ashoat@comm.app>
2023-05-31 13:10:19 -0400

    [native] Construct synthetic UserInfo to pass to EditUserAvatar
    Summary:
    `EditUserAvatar` renders an avatar via `UserAvatar`, which currently only works when the data (`UserInfo` or `CurrentUserInfo`) is in Redux.
    
    For the registration flow, we want to render a "pending" avatar that hasn't been saved into Redux yet. To that end, this diff introduces a way to pass `UserInfo` to `UserAvatar` and `EditUserAvatar`.
    
    Depends on D8045
    
    Test Plan:
    This was tested in combination with the rest of the stack. I tested:
    
    1. ENS flow, both when user has an ENS avatar and when they don't
    2. Setting an emoji avatar
    3. Setting an image avatar via camera
    4. Setting an image avatar via media library
    5. Tested avatar update flow after being logged in to make sure I didn't regress anything
    
    Reviewers: ginsu, atul
    
    Subscribers:

--- native/avatars/user-avatar.react.js
+++ native/avatars/user-avatar.react.js
@@ -6,21 +6,21 @@ import {
   getAvatarForUser,
   useENSResolvedAvatar,
 } from 'lib/shared/avatar-utils.js';
+import type { GenericUserInfoWithAvatar } from 'lib/types/avatar-types.js';
 
 import Avatar from './avatar.react.js';
 import { useSelector } from '../redux/redux-utils.js';
 
-type Props = {
-  +userID: ?string,
-  +size: 'micro' | 'small' | 'large' | 'profile',
-};
-
+type Size = 'micro' | 'small' | 'large' | 'profile';
+type Props =
+  | { +userID: ?string, +size: Size }
+  | { +userInfo: ?GenericUserInfoWithAvatar, +size: Size };
 function UserAvatar(props: Props): React.Node {
-  const { userID, size } = props;
+  const { userID, userInfo: userInfoProp, size } = props;
 
   const currentUserInfo = useSelector(state => state.currentUserInfo);
   const userInfo = useSelector(state =>
-    userID ? state.userStore.userInfos[userID] : null,
+    userID ? state.userStore.userInfos[userID] : userInfoProp,
   );
 
   const avatarUserInfo =
