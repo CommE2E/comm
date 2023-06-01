174f5b8a9a25a051c451e9813d4a3dc582aa6e62
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-06-01 18:21:57 -0400

    [landing] move hero section code into own component
    Summary: Simple move diff. All other sections in `AppLanding` will be put in their own component so I figured it would be good to have one for the hero as well.
    
    Test Plan: Double checked that the hero section still looked and behaved as it did previously
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8077

--- landing/app-landing.react.js
+++ landing/app-landing.react.js
@@ -3,9 +3,7 @@
 import * as React from 'react';
 
 import { imageAssetMetaData } from './asset-meta-data.js';
-import HeroContent from './hero-content.react.js';
-import css from './landing.css';
-import Picture from './Picture.react.js';
+import Hero from './hero.react.js';
 import usePreloadImageAssets from './use-pre-load-image-assets.react.js';
 
 function AppLanding(): React.Node {
@@ -14,15 +12,7 @@ function AppLanding(): React.Node {
 
   return (
     <main>
-      <section className={css.heroSection}>
-        <div className={css.heroContainer}>
-          <div className={css.heroImage}>
-            <Picture url={hero.url} alt={hero.alt} />
-            <div className={css.glow} />
-          </div>
-          <HeroContent />
-        </div>
-      </section>
+      <Hero {...hero} />
     </main>
   );
 }
