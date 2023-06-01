66f4b2afeb0c6aa45c8f1f5a3bc68aff74146a1f
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-06-01 19:10:01 -0400

    [landing] redesign footer component
    Summary: This is the second part to implmenenting the redesign for the footer component. This diff adds the new elments that are shown in the figma design (screenshot below) and arranges them to match the design
    
    Test Plan: Please watch the demo video/see the screenshots below
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8083

--- landing/footer.react.js
+++ landing/footer.react.js
@@ -1,11 +1,14 @@
 // @flow
 
-import { faExternalLinkSquareAlt } from '@fortawesome/free-solid-svg-icons';
+import { faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames';
 import * as React from 'react';
 import { NavLink } from 'react-router-dom';
 
 import css from './footer.css';
+import typography from './typography.css';
 
 const navLinkProps = {
   activeStyle: {
@@ -15,47 +18,116 @@ const navLinkProps = {
 };
 
 function Footer(): React.Node {
+  const logoClassName = classNames([typography.heading2, css.logo]);
+  const navLinkClassName = classNames([typography.paragraph1, css.links]);
+  const legalTextClassName = classNames([
+    typography.paragraph2,
+    css.copyrightText,
+  ]);
+  const legalLinksClassName = classNames([typography.paragraph2, css.links]);
+
   return (
     <footer className={css.wrapper}>
       <div className={css.contentWrapper}>
         <div className={css.navigation}>
-          <NavLink className={css.logo} to="/">
-            Comm
-          </NavLink>
-          <NavLink to="/keyservers" exact {...navLinkProps}>
-            Keyservers
-          </NavLink>
-          <NavLink to="/support" exact {...navLinkProps}>
-            Support
-          </NavLink>
-          <NavLink to="/team" exact {...navLinkProps}>
-            Team
-          </NavLink>
-          <NavLink to="/investors" exact {...navLinkProps}>
-            Investors
-          </NavLink>
-          <NavLink to="/terms" exact {...navLinkProps}>
-            Terms of Use
-          </NavLink>
-          <NavLink to="/privacy" exact {...navLinkProps}>
-            Privacy Policy
-          </NavLink>
-          <a href="https://www.notion.so/How-Comm-works-d6217941db7c4237b9d08b427aef3234">
-            Learn how Comm works
-            <FontAwesomeIcon
-              size="sm"
-              color="#ffffff"
-              icon={faExternalLinkSquareAlt}
-            />
-          </a>
-          <a href="https://commapp.notion.site/Learn-more-about-Comm-1efb9b646d504dddae30a20b4f33200e">
-            About Comm
-            <FontAwesomeIcon
-              size="sm"
-              color="#ffffff"
-              icon={faExternalLinkSquareAlt}
-            />
-          </a>
+          <div className={css.linksContainer}>
+            <NavLink className={logoClassName} to="/">
+              Comm
+            </NavLink>
+            <NavLink
+              to="/keyservers"
+              exact
+              {...navLinkProps}
+              className={navLinkClassName}
+            >
+              Keyservers
+            </NavLink>
+            <NavLink
+              to="/support"
+              exact
+              {...navLinkProps}
+              className={navLinkClassName}
+            >
+              Support
+            </NavLink>
+            <NavLink
+              to="/team"
+              exact
+              {...navLinkProps}
+              className={navLinkClassName}
+            >
+              Team
+            </NavLink>
+            <NavLink
+              to="/investors"
+              exact
+              {...navLinkProps}
+              className={navLinkClassName}
+            >
+              Investors
+            </NavLink>
+            <a
+              href="https://www.notion.so/How-Comm-works-d6217941db7c4237b9d08b427aef3234"
+              className={navLinkClassName}
+            >
+              Learn how Comm works
+              <FontAwesomeIcon
+                size="sm"
+                className={css.navLinkIcons}
+                icon={faExternalLinkAlt}
+              />
+            </a>
+            <a
+              href="https://commapp.notion.site/Learn-more-about-Comm-1efb9b646d504dddae30a20b4f33200e"
+              className={navLinkClassName}
+            >
+              About Comm
+              <FontAwesomeIcon
+                size="sm"
+                className={css.navLinkIcons}
+                icon={faExternalLinkAlt}
+              />
+            </a>
+          </div>
+          <div className={css.socialIconsContainer}>
+            <a href="https://twitter.com/commdotapp">
+              <FontAwesomeIcon
+                size="lg"
+                className={css.socialIcons}
+                icon={faTwitter}
+              />
+            </a>
+            <a href="https://github.com/CommE2E/comm">
+              <FontAwesomeIcon
+                size="lg"
+                className={css.socialIcons}
+                icon={faGithub}
+              />
+            </a>
+          </div>
+        </div>
+        <div className={css.legalContainer}>
+          <p className={legalTextClassName}>
+            &copy; 2023 Comm. All Rights Reserved
+          </p>
+          <div className={css.legalLinks}>
+            <NavLink
+              to="/terms"
+              exact
+              {...navLinkProps}
+              className={legalLinksClassName}
+            >
+              Terms of Use
+            </NavLink>
+            <NavLink
+              to="/privacy"
+              exact
+              {...navLinkProps}
+              className={legalLinksClassName}
+            >
+              Privacy Policy
+            </NavLink>
+          </div>
         </div>
       </div>
     </footer>
