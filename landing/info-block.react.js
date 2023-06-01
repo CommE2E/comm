236b4a1e459e752e452c76eeac3c1057d680525d
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-06-01 18:58:56 -0400

    [landing] repurpose InfoBlock component
    Summary: This diff repurposes the `InfoBlock` component to work for this landing page redesign. A big change with this `InfoBlock` component is that we no longer need an image, and all the content in the `InfoBlock` is centered. Here is a screenshot of the designs for extra context
    
    Test Plan: Please see the demo videos/screenshots below to see how the new `InfoBlock` component
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8079

--- landing/info-block.react.js
+++ landing/info-block.react.js
@@ -1,31 +1,71 @@
 // @flow
 
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames';
 import * as React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import css from './info-block.css';
-import Picture from './Picture.react.js';
+import typography from './typography.css';
 
 type InfoBlockProps = {
-  +title: string,
-  +description: string,
-  +url: string,
-  +alt: string,
-  +imageStyle: string,
-  +infoStyle: string,
+  +containerClassName: string,
+  +colorHeader: boolean,
+  +headerTextContent: string,
+  +paragraphTextContent: string,
+  +navLinkDestination: string,
+  +linkTextContent: string,
 };
 function InfoBlock(props: InfoBlockProps): React.Node {
-  const { title, description, url, alt, imageStyle, infoStyle } = props;
+  const {
+    containerClassName,
+    headerTextContent,
+    colorHeader,
+    paragraphTextContent,
+    navLinkDestination,
+    linkTextContent,
+  } = props;
+
+  const headerClassName = classNames([typography.heading1, css.header]);
+  const paragraphClassName = classNames([
+    typography.subheading2,
+    css.paragraph,
+  ]);
+
+  const linkClassName = classNames([typography.paragraph1, css.link]);
+
+  let headerText;
+  if (colorHeader) {
+    const lastSpaceIndex = headerTextContent.lastIndexOf(' ');
+    const remainingSentence = headerTextContent.substring(0, lastSpaceIndex);
+    const lastWord = headerTextContent.substring(lastSpaceIndex + 1);
+
+    headerText = (
+      <>
+        {remainingSentence}
+        <span> {lastWord}</span>
+      </>
+    );
+  } else {
+    headerText = headerTextContent;
+  }
 
   return (
-    <>
-      <div className={imageStyle}>
-        <Picture alt={alt} url={url} />
+    <section className={containerClassName}>
+      <div className={css.textContainer}>
+        <h1 className={headerClassName}>{headerText}</h1>
+        <p className={paragraphClassName}>{paragraphTextContent}</p>
+        <NavLink to={navLinkDestination} exact className={css.linkContainer}>
+          <p className={linkClassName}>{linkTextContent}</p>
+          <FontAwesomeIcon
+            size="sm"
+            className={css.icon}
+            icon={faExternalLinkAlt}
+          />
+        </NavLink>
       </div>
-      <div className={`${css.info_block} ${infoStyle}`}>
-        <p className={css.title}>{title}</p>
-        <p className={css.description}>{description}</p>
-      </div>
-    </>
+    </section>
   );
 }
 
