f0bbf0d674522ba1caa607056393c72066e7405c
Ashoat Tevosyan <ashoat@comm.app>
2023-06-01 18:58:26 -0400

    [native] Move some types from avatar-selection.react.js to registration-types.js
    Summary: Going to use these types in a new place, so figured it would be better to export them from the shared types file
    
    Depends on D8087
    
    Test Plan: Flow
    
    Reviewers: atul, ginsu
    
    Subscribers:

--- native/account/registration/registration-types.js
+++ native/account/registration/registration-types.js
@@ -1,3 +1,38 @@
 // @flow
 
+import type {
+  UpdateUserAvatarRequest,
+  ClientAvatar,
+} from 'lib/types/avatar-types.js';
+import type { NativeMediaSelection } from 'lib/types/media-types.js';
+import type { SIWEResult } from 'lib/types/siwe-types.js';
+
 export type CoolOrNerdMode = 'cool' | 'nerd';
+
+export type EthereumAccountSelection = {
+  +accountType: 'ethereum',
+  ...SIWEResult,
+  +avatarURI: ?string,
+};
+
+export type UsernameAccountSelection = {
+  +accountType: 'username',
+  +username: string,
+  +password: string,
+};
+
+export type AccountSelection =
+  | EthereumAccountSelection
+  | UsernameAccountSelection;
+
+export type AvatarData =
+  | {
+      +needsUpload: true,
+      +mediaSelection: NativeMediaSelection,
+      +clientAvatar: ClientAvatar,
+    }
+  | {
+      +needsUpload: false,
+      +updateUserAvatarRequest: UpdateUserAvatarRequest,
+      +clientAvatar: ClientAvatar,
+    };
