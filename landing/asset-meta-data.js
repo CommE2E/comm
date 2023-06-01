bd28efb6606330cab2a153afd4b0778ff5af92d2
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-06-01 18:52:33 -0400

    DRAFT: [landing] introduce new assetMetaData
    Summary: In prepartion of repurposing the InfoBlocks to match the new designs, I reintroduced and updated the type for the `assetMetaData` array.
    
    Test Plan: N/A will be further tested in a subsequent diff where I inject the new `InfoBlock` component with this data
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8078

--- landing/asset-meta-data.js
+++ landing/asset-meta-data.js
@@ -1,8 +1,19 @@
 // @flow
 
+import css from './landing.css';
+
 export type ImageAsset = {
   +alt: string,
   +url: string,
+};
+
+export type Asset = {
+  +containerClassName: string,
+  +colorHeader: boolean,
+  +headerTextContent: string,
+  +paragraphTextContent: string,
+  +navLinkDestination: string,
+  +linkTextContent: string,
 };
 
 export const assetsCacheURLPrefix = 'https://dh9fld3hutpxf.cloudfront.net';
@@ -10,5 +21,26 @@ export const imageAssetMetaData = [
   {
     alt: 'hero image showcasing Comm',
     url: `${assetsCacheURLPrefix}/hero`,
+  },
+];
+
+export const assetMetaData = [
+  {
+    containerClassName: css.keyserverInfo,
+    colorHeader: true,
+    headerTextContent: 'Own your data and communities with keyservers',
+    paragraphTextContent:
+      'Porem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molesti, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus.',
+    navLinkDestination: '/keyservers',
+    linkTextContent: 'Learn more about keyservers',
+  },
+  {
+    containerClassName: css.teamInfo,
+    colorHeader: false,
+    headerTextContent: 'Our story and vision',
+    paragraphTextContent:
+      'Porem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molesti, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus.',
+    navLinkDestination: '/team',
+    linkTextContent: 'Learn more about our team',
   },
 ];
