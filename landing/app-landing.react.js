96e67f405291a4eca185ad82884c21772187ae60
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-06-01 01:55:53 -0400

    [landing] remove deprecated info blocks
    Summary:
    Removing old info blocks since we will be replacing them with new info blocks in subsequent diffs.
    
    Depends on D8039
    
    Test Plan:
    Please see the demo videos to see that the old info blocks are no longer rendered:
    
    {F567810}
    
    Reviewers: atul, kamil
    
    Reviewed By: atul
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8054

--- landing/app-landing.react.js
+++ landing/app-landing.react.js
@@ -4,22 +4,13 @@ import * as React from 'react';
 
 import { assetMetaData } from './asset-meta-data.js';
 import HeroContent from './hero-content.react.js';
-import InfoBlock from './info-block.react.js';
 import css from './landing.css';
 import Picture from './Picture.react.js';
 import usePreloadAssets from './use-pre-load-assets.react.js';
 
 function AppLanding(): React.Node {
   usePreloadAssets(assetMetaData);
-  const [
-    hero,
-    federated,
-    customizable,
-    encrypted,
-    sovereign,
-    openSource,
-    lessNoisy,
-  ] = assetMetaData;
+  const [hero] = assetMetaData;
 
   return (
     <main className={css.wrapper}>
@@ -27,12 +18,6 @@ function AppLanding(): React.Node {
         <Picture url={hero.url} alt={hero.alt} />
       </div>
       <HeroContent />
-      <InfoBlock {...federated} />
-      <InfoBlock {...customizable} />
-      <InfoBlock {...encrypted} />
-      <InfoBlock {...sovereign} />
-      <InfoBlock {...openSource} />
-      <InfoBlock {...lessNoisy} />
     </main>
   );
 }
