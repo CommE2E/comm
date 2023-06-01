c83c2b9c26f2c4627a6ba021716e18f384ad1088
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-06-01 19:02:32 -0400

    [landing] add all section components to AppLanding
    Summary: Now that we have built all the components for each section. This diff just simply adds them to the `AppLanding` component.
    
    Test Plan: Please see the demo video below
    
    Reviewers:
    
    Subscribers:

--- landing/app-landing.react.js
+++ landing/app-landing.react.js
@@ -2,17 +2,25 @@
 
 import * as React from 'react';
 
-import { imageAssetMetaData } from './asset-meta-data.js';
+import { imageAssetMetaData, assetMetaData } from './asset-meta-data.js';
+import CompetitorComparison from './competitor-comparison.react.js';
 import Hero from './hero.react.js';
+import InfoBlock from './info-block.react.js';
+import RequestAccess from './request-access.react.js';
 import usePreloadImageAssets from './use-pre-load-image-assets.react.js';
 
 function AppLanding(): React.Node {
   usePreloadImageAssets(imageAssetMetaData);
   const [hero] = imageAssetMetaData;
+  const [keyserver, team] = assetMetaData;
 
   return (
     <main>
       <Hero {...hero} />
+      <CompetitorComparison />
+      <InfoBlock {...keyserver} />
+      <InfoBlock {...team} />
+      <RequestAccess />
     </main>
   );
 }
