b372a4e879082669266e28dd525657a7568b8a06
Atul Madhugiri <atulm@berkeley.edu>
2023-06-02 12:03:42 -0400

    [native] Introduce `CommunityCreationContentContainer` component
    Summary:
    This component was heavily inspired by `RegistrationContentContainer`.
    
    Replaced the `ScrollView` in `RegistrationContentContainer` with a `View` to avoid issue with virtualized list being nested within `ScrollView`:
    
    {F568067}
    
    Kept all the other styling and whatnot for consistency.
    
    As with `RegistrationContentContainer`, this component can probably be made more general, "added to the design system," and moved to `/components` in the future. Will discuss how to approach this w/ @ted when he's back.
    
    ---
    
    Depends on D8056
    
    Test Plan: Things look as they did before and the RedBox no longer appears when the screen renders.
    
    Reviewers: ashoat, ginsu
    
    Reviewed By: ashoat
    
    Subscribers: tomek, ted
    
    Differential Revision: https://phab.comm.dev/D8057

---
+++ native/community-creation/community-creation-content-container.react.js
@@ -0,0 +1,28 @@
+// @flow
+
+import { useHeaderHeight } from '@react-navigation/elements';
+import * as React from 'react';
+import { View } from 'react-native';
+
+import KeyboardAvoidingView from '../components/keyboard-avoiding-view.react.js';
+
+type ViewProps = React.ElementConfig<typeof View>;
+type Props = ViewProps;
+
+function CommunityCreationContentContainer(props: Props): React.Node {
+  const { children, style, ...rest } = props;
+
+  const headerHeight = useHeaderHeight();
+  const backgroundStyle = React.useMemo(
+    () => ({ marginTop: headerHeight, flex: 1 }),
+    [headerHeight],
+  );
+
+  return (
+    <KeyboardAvoidingView behavior="padding" style={backgroundStyle} {...rest}>
+      {children}
+    </KeyboardAvoidingView>
+  );
+}
+
+export default CommunityCreationContentContainer;
