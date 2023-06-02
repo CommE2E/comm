d239b7f651c5b485f82a93691581f8f203962bbb
Rohan Khanderia <rohan.khanderia@gmail.com>
2023-06-02 16:22:56 -0400

    [web] Create a model that allows admins to change a member's role (dropdown menu, actionable buttons)
    Summary:
    This is the second part of the modal creation, utilizing the earlier `Dropdown` component and creating the two 'back' and 'save' buttons. Functionality for these buttons will be added up the stack to
    make this diff easier to review.
    
    https://linear.app/comm/issue/ENG-4038/create-a-modal-that-allows-admins-to-change-a-members-role
    
    Depends on D8137
    
    Test Plan: Please see a screenshot of what the complete modal looks like now
    
    Reviewers: atul, ginsu
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8138

--- web/modals/threads/members/change-member-role-modal.react.js
+++ web/modals/threads/members/change-member-role-modal.react.js
@@ -2,9 +2,13 @@
 
 import * as React from 'react';
 
+import { useModalContext } from 'lib/components/modal-provider.react.js';
 import type { RelativeMemberInfo, ThreadInfo } from 'lib/types/thread-types';
+import { values } from 'lib/utils/objects.js';
 
 import css from './change-member-role-modal.css';
+import Button, { buttonThemes } from '../../../components/button.react.js';
+import Dropdown from '../../../components/dropdown.react.js';
 import UserAvatar from '../../../components/user-avatar.react.js';
 import Modal from '../../modal.react.js';
 
@@ -14,11 +18,31 @@ type ChangeMemberRoleModalProps = {
 };
 
 function ChangeMemberRoleModal(props: ChangeMemberRoleModalProps): React.Node {
-  const { memberInfo } = props;
+  const { memberInfo, threadInfo } = props;
+  const { popModal } = useModalContext();
   const modalName = 'Change Role';
 
+  const roleOptions = React.useMemo(
+    () =>
+      values(threadInfo.roles).map(role => ({
+        id: role.id,
+        name: role.name,
+      })),
+    [threadInfo.roles],
+  );
+
+  const initialSelectedRole = React.useMemo(() => {
+    if (!memberInfo.role) {
+      return null;
+    }
+    return memberInfo.role;
+  }, [memberInfo.role]);
+
+  const [selectedRole, setSelectedRole] =
+    React.useState<?string>(initialSelectedRole);
+
   return (
-    <Modal name={modalName} onClose={() => {}} size="large">
+    <Modal name={modalName} onClose={popModal} size="large">
       <div className={css.roleModalDescription}>
         Members can only be assigned to one role at a time. Changing a
         member&rsquo;s role will replace their previously assigned role.
@@ -28,6 +52,25 @@ function ChangeMemberRoleModal(props: ChangeMemberRoleModalProps): React.Node {
           <UserAvatar userID={memberInfo.id} size="large" />
         </div>
         <div className={css.roleModalMemberName}>{memberInfo.username}</div>
+      </div>
+      <div className={css.roleModalRoleSelector}>
+        <Dropdown
+          options={roleOptions}
+          activeSelection={selectedRole}
+          setActiveSelection={setSelectedRole}
+        />
+      </div>
+      <div className={css.roleModalActionButtons}>
+        <Button variant="outline" className={css.roleModalBackButton}>
+          Back
+        </Button>
+        <Button
+          variant="filled"
+          className={css.roleModalSaveButton}
+          buttonColor={buttonThemes.primary}
+        >
+          Save
+        </Button>
       </div>
     </Modal>
   );
