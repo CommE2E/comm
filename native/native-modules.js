2ae9682a129ad592ae523888ef55884f3bc0abe6
marcinwasowicz <marcinkwasowicz@gmail.com>
2023-06-02 12:05:29 +0200

    Export CommConstants HostObject to JavaScript
    Summary: This differential exports CommValidationModule to JavaScript.
    
    Test Plan:
    I decided to call `commConstants.NATIVE_MESSAGE_SPECS` in `sqlite-data-handler.js` for testing purposes. So the test plan is:
    1. Build the app
    2. Apply this diff: https://gist.github.com/marcinwasowicz/da35ee527660a6a020daf88ddae61d24
    3. Open the app. Ensure the warning message like this is visible:
    ```
    Object {
      "message": "C++ MessageSpec implementation missing for message types: ADD_MEMBERS, REMOVE_MEMBERS, LEAVE_THREAD, JOIN_THREAD, TOGGLE_PIN",
    }
    ```
    
    Reviewers: bartek, atul
    
    Reviewed By: bartek
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8071

--- native/native-modules.js
+++ native/native-modules.js
@@ -5,3 +5,6 @@ import type { UtilsModuleSpec } from './schema/CommUtilsModuleSchema.js';
 
 export const commCoreModule: CoreModuleSpec = global.CommCoreModule;
 export const commUtilsModule: UtilsModuleSpec = global.CommUtilsModule;
+export const commConstants: {
+  +NATIVE_MESSAGE_TYPES: $ReadOnlyArray<number>,
+} = global.CommConstants;
