7ad68e8773d2b75db021220600e5f87bb10ff557
Ashoat Tevosyan <ashoat@comm.app>
2023-06-02 12:09:05 -0400

    [native] Introduce code to call keyserver for registration in new flow
    Summary:
    This is the code that will actually register a user ([ENG-4047](https://linear.app/comm/issue/ENG-4047/code-to-create-an-account-for-new-registration-flow)). Some notes:
    
    1. I initially considered passing the avatar directly to the registration endpoint, but realized that gets complicated since we need a logged-in user in order to do a media upload. Instead here I first register the user, and in a later diff I'll handle setting the user's avatar.
    2. We don't yet have a screen for accepting terms ([ENG-3951](https://linear.app/comm/issue/ENG-3951/add-terms-of-use-privacy-policy-acceptance-to-new-registration-flow)). For now I'm calling the registration endpoint from `AvatarSelection`, but after introducing a screen for accepting terms I'll move the registration call.
    
    Depends on D8148
    
    Test Plan:
    The whole stack was tested as follows:
    
    1. Try creating an account with the default avatar
    2. Try creating an account with an emoji avatar
    3. Try creating an account with an image avatar (that needs to be uploaded)
    
    I made sure of the following:
    
    1. Loading spinner appeared on the button, not on the avatar, and was present throughout
    2. After the `RegistrationNavigator` is dismissed, the avatar that appears for the user's `PRIVATE` chat in the `ChatThreadList` is correct
    3. Subsequent delete-account-and-then-register-again worked correctly
    4. Various error cases are triggered correctly
    
    Reviewers: atul, ginsu
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D8149

---
+++ native/account/registration/registration-server-call.js
@@ -0,0 +1,126 @@
+// @flow
+
+import * as React from 'react';
+import { Alert, Platform } from 'react-native';
+import { useDispatch } from 'react-redux';
+
+import { setDataLoadedActionType } from 'lib/actions/client-db-store-actions.js';
+import { registerActionTypes, register } from 'lib/actions/user-actions.js';
+import type { LogInStartingPayload } from 'lib/types/account-types.js';
+import {
+  useServerCall,
+  useDispatchActionPromise,
+} from 'lib/utils/action-utils.js';
+
+import type {
+  RegistrationServerCallInput,
+  UsernameAccountSelection,
+  EthereumAccountSelection,
+} from './registration-types.js';
+import { NavContext } from '../../navigation/navigation-context.js';
+import { useSelector } from '../../redux/redux-utils.js';
+import { nativeLogInExtraInfoSelector } from '../../selectors/account-selectors.js';
+import { setNativeCredentials } from '../native-credentials.js';
+import { useSIWEServerCall } from '../siwe-hooks.js';
+
+function useRegistrationServerCall(): RegistrationServerCallInput => Promise<void> {
+  const navContext = React.useContext(NavContext);
+  const logInExtraInfo = useSelector(state =>
+    nativeLogInExtraInfoSelector({
+      redux: state,
+      navContext,
+    }),
+  );
+
+  const dispatchActionPromise = useDispatchActionPromise();
+  const callRegister = useServerCall(register);
+
+  const registerUsernameAccount = React.useCallback(
+    async (accountSelection: UsernameAccountSelection) => {
+      const extraInfo = await logInExtraInfo();
+      const registerPromise = (async () => {
+        try {
+          const result = await callRegister({
+            ...extraInfo,
+            username: accountSelection.username,
+            password: accountSelection.password,
+          });
+          await setNativeCredentials({
+            username: result.currentUserInfo.username,
+            password: accountSelection.password,
+          });
+          return result;
+        } catch (e) {
+          if (e.message === 'username_reserved') {
+            Alert.alert(
+              'Username reserved',
+              'This username is currently reserved. Please contact support@' +
+                'comm.app if you would like to claim this account.',
+            );
+          } else if (e.message === 'username_taken') {
+            Alert.alert(
+              'Username taken',
+              'An account with that username already exists',
+            );
+          } else if (e.message === 'client_version_unsupported') {
+            const app = Platform.select({
+              ios: 'App Store',
+              android: 'Play Store',
+            });
+            Alert.alert(
+              'App out of date',
+              'Your app version is pretty old, and the server doesn’t know how ' +
+                `to speak to it anymore. Please use the ${app} app to update!`,
+            );
+          } else {
+            Alert.alert('Unknown error', 'Uhh... try again?');
+          }
+          throw e;
+        }
+      })();
+      dispatchActionPromise(
+        registerActionTypes,
+        registerPromise,
+        undefined,
+        ({ calendarQuery: extraInfo.calendarQuery }: LogInStartingPayload),
+      );
+      await registerPromise;
+    },
+    [logInExtraInfo, callRegister, dispatchActionPromise],
+  );
+
+  const siweServerCallParams = React.useMemo(() => {
+    const onServerCallFailure = () => {
+      Alert.alert('Unknown error', 'Uhh... try again?');
+    };
+    return { onFailure: onServerCallFailure };
+  }, []);
+  const siweServerCall = useSIWEServerCall(siweServerCallParams);
+
+  const registerEthereumAccount = React.useCallback(
+    (accountSelection: EthereumAccountSelection) => {
+      return siweServerCall(accountSelection);
+    },
+    [siweServerCall],
+  );
+
+  const dispatch = useDispatch();
+  return React.useCallback(
+    async (input: RegistrationServerCallInput) => {
+      if (input.accountSelection.accountType === 'username') {
+        await registerUsernameAccount(input.accountSelection);
+      } else {
+        await registerEthereumAccount(input.accountSelection);
+      }
+      dispatch({
+        type: setDataLoadedActionType,
+        payload: {
+          dataLoaded: true,
+        },
+      });
+    },
+    [registerUsernameAccount, registerEthereumAccount, dispatch],
+  );
+}
+
+export { useRegistrationServerCall };
