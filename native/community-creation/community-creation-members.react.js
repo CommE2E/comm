9a3b372c57b2132d6e42ef36f30677f27400f56a
Atul Madhugiri <atulm@berkeley.edu>
2023-06-02 11:53:57 -0400

    [native] Barebones user selection in `CommunityCreationMembers`
    Summary:
    Extremely barebones user selection screen in `CommunityCreationMembers` using `TagInput` and `UserList`.
    
    There's a warning about nested scroll views, the styling isn't correct, and we aren't differentiating between `COMMUNITY_ROOT` and `COMMUNITY_ANNOUNCEMENT_ROOT`. Going to handle each of those in followup diffs to keep changes easier to review.
    
    Test Plan:
    Works as expected:
    
    {F568038}
    
    Reviewers: ashoat, ginsu
    
    Reviewed By: ashoat
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D8056

--- native/community-creation/community-creation-members.react.js
+++ native/community-creation/community-creation-members.react.js
@@ -2,12 +2,31 @@
 
 import * as React from 'react';
 
+import {
+  userInfoSelectorForPotentialMembers,
+  userSearchIndexForPotentialMembers,
+} from 'lib/selectors/user-selectors.js';
+import { getPotentialMemberItems } from 'lib/shared/search-utils.js';
+import { threadTypes } from 'lib/types/thread-types-enum.js';
+import type { AccountUserInfo } from 'lib/types/user-types.js';
+
 import CommunityCreationKeyserverLabel from './community-creation-keyserver-label.react.js';
 import type { CommunityCreationNavigationProp } from './community-creation-navigator.react.js';
 import RegistrationContainer from '../account/registration/registration-container.react.js';
 import RegistrationContentContainer from '../account/registration/registration-content-container.react.js';
+import { createTagInput } from '../components/tag-input.react.js';
+import UserList from '../components/user-list.react.js';
 import type { NavigationRoute } from '../navigation/route-names.js';
+import { useSelector } from '../redux/redux-utils.js';
 import { useStyles } from '../themes/colors.js';
+
+const TagInput = createTagInput<AccountUserInfo>();
+const tagInputProps = {
+  placeholder: 'username',
+  autoFocus: true,
+  returnKeyType: 'go',
+};
+const tagDataLabelExtractor = (userInfo: AccountUserInfo) => userInfo.username;
 
 type Props = {
   +navigation: CommunityCreationNavigationProp<'CommunityCreationMembers'>,
@@ -18,10 +37,63 @@ type Props = {
 function CommunityCreationMembers(props: Props): React.Node {
   const styles = useStyles(unboundStyles);
 
+  const otherUserInfos = useSelector(userInfoSelectorForPotentialMembers);
+  const userSearchIndex = useSelector(userSearchIndexForPotentialMembers);
+
+  const [usernameInputText, setUsernameInputText] = React.useState<string>('');
+  const [selectedUsers, setSelectedUsers] = React.useState<
+    $ReadOnlyArray<AccountUserInfo>,
+  >([]);
+
+  const selectedUserIDs = React.useMemo(
+    () => selectedUsers.map(userInfo => userInfo.id),
+    [selectedUsers],
+  );
+
+  const userSearchResults = React.useMemo(
+    () =>
+      getPotentialMemberItems(
+        usernameInputText,
+        otherUserInfos,
+        userSearchIndex,
+        selectedUserIDs,
+        null,
+        null,
+        threadTypes.COMMUNITY_ROOT,
+      ),
+    [otherUserInfos, selectedUserIDs, userSearchIndex, usernameInputText],
+  );
+
+  const onSelectUser = React.useCallback(
+    userID => {
+      if (selectedUserIDs.some(existingUserID => userID === existingUserID)) {
+        return;
+      }
+      setSelectedUsers(oldUserInfoInputArray => [
+        ...oldUserInfoInputArray,
+        otherUserInfos[userID],
+      ]);
+      setUsernameInputText('');
+    },
+    [otherUserInfos, selectedUserIDs],
+  );
+
+  const tagInputRef = React.useRef();
+
   return (
     <RegistrationContainer>
       <RegistrationContentContainer style={styles.containerPaddingOverride}>
         <CommunityCreationKeyserverLabel />
+        <TagInput
+          value={selectedUsers}
+          onChange={setSelectedUsers}
+          text={usernameInputText}
+          onChangeText={setUsernameInputText}
+          labelExtractor={tagDataLabelExtractor}
+          inputProps={tagInputProps}
+          ref={tagInputRef}
+        />
+        <UserList userInfos={userSearchResults} onSelect={onSelectUser} />
       </RegistrationContentContainer>
     </RegistrationContainer>
   );
