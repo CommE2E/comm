9347caa7e2ecd70a2298a8bce5f8f1606a96de46
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-06-05 09:28:37 -0400

    [landing] introduce keyserver faq component
    Summary: This diff is the first part to introduce the keyserver faq component. The focus of this diff is to get the general structure/behavior of the keyserver faq.
    
    Test Plan: Please see the demo video/screenshots below
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8093

---
+++ landing/keyserver-faq.react.js
@@ -0,0 +1,69 @@
+// @flow
+
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames';
+import * as React from 'react';
+
+import { faqData } from './keyserver-faq-data.js';
+import css from './keyserver-faq.css';
+import typography from './typography.css';
+
+function KeyserverFAQ(): React.Node {
+  const questionClassName = classNames([
+    typography.subheading2,
+    css.questionText,
+  ]);
+  const answerClassName = classNames([typography.paragraph1, css.answerText]);
+
+  const [activeFAQIndex, setActiveFAQIndex] = React.useState(null);
+
+  const onClickFAQItem = React.useCallback(
+    (index: number) => {
+      if (index === activeFAQIndex) {
+        setActiveFAQIndex(null);
+      } else {
+        setActiveFAQIndex(index);
+      }
+    },
+    [activeFAQIndex],
+  );
+
+  const keyserverFAQ = React.useMemo(() => {
+    return faqData.map((faq, index) => {
+      const answerContainerClassName = classNames({
+        [css.answerContainer]: true,
+        [css.activeAnswerContainer]: activeFAQIndex === index,
+      });
+
+      return (
+        <div
+          key={index}
+          className={css.faqItemContainer}
+          onClick={() => onClickFAQItem(index)}
+        >
+          <div className={css.questionContainer}>
+            <p className={questionClassName}>{faq.question}</p>
+            <FontAwesomeIcon
+              size="sm"
+              className={css.icon}
+              icon={faChevronDown}
+            />
+          </div>
+          <div className={answerContainerClassName}>
+            <p className={answerClassName}>{faq.answer}</p>
+          </div>
+        </div>
+      );
+    });
+  }, [activeFAQIndex, answerClassName, onClickFAQItem, questionClassName]);
+
+  return (
+    <section className={css.faqSection}>
+      <h1 className={typography.heading1}>FAQ</h1>
+      <div className={css.faqContainer}>{keyserverFAQ}</div>
+    </section>
+  );
+}
+
+export default KeyserverFAQ;
