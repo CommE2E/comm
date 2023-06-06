2464cc168dde722421a6fb82d08a8909a488885a
Rohan Khanderia <rohan.khanderia@gmail.com>
2023-06-06 16:19:34 -0400

    [web] Implement a custom dropdown menu component
    Summary:
    The discussion was held on Linear (https://linear.app/comm/issue/ENG-4038/create-a-modal-that-allows-admins-to-change-a-members-role), but we needed a dropdown menu to match the Figma designs for
    displaying potential roles that can be assigned to a member in the modal that will come down the stack. This diff creates a `Dropdown` component that is general enough to be reused for different use cases
    down the line, if needed.
    
    Depends on D8135
    
    Test Plan: The actual dropdown menu will be verified in a later diff, but please see below a sample rendering of the component with fake data
    
    Reviewers: atul, ginsu
    
    Subscribers: ashoat, tomek, ted
    
    Differential Revision: https://phab.comm.dev/D8136

---
+++ web/components/dropdown.react.js
@@ -0,0 +1,106 @@
+// @flow
+
+import classNames from 'classnames';
+import invariant from 'invariant';
+import * as React from 'react';
+
+import SWMansionIcon from 'lib/components/SWMansionIcon.react.js';
+
+import css from './dropdown.css';
+
+type DropdownOption = {
+  +id: string,
+  +name: string,
+};
+
+type DropdownProps = {
+  +options: $ReadOnlyArray<DropdownOption>,
+  +activeSelection: string,
+  +setActiveSelection: string => mixed,
+  +disabled?: boolean,
+};
+
+function Dropdown(props: DropdownProps): React.Node {
+  const { options, activeSelection, setActiveSelection, disabled } = props;
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const dropdownMenuClassNames = classNames({
+    [css.dropdownMenu]: true,
+    [css.dropdownDisabled]: !!disabled,
+  });
+
+  const dropdownTextClassNames = classNames({
+    [css.dropdownDisplayText]: true,
+    [css.dropdownDisabled]: !!disabled,
+  });
+
+  const toggleMenu = React.useCallback(() => {
+    if (disabled) {
+      return;
+    }
+
+    setIsOpen(!isOpen);
+  }, [disabled, isOpen]);
+
+  const handleSelection = React.useCallback(
+    selection => {
+      setActiveSelection(selection.id);
+      setIsOpen(false);
+    },
+    [setActiveSelection],
+  );
+
+  const activeDisplayedOption = React.useMemo(() => {
+    const activeOption = options.find(option => option.id === activeSelection);
+    invariant(activeOption, 'Active option must be in options list');
+    return activeOption.name;
+  }, [activeSelection, options]);
+
+  const dropdownList = React.useMemo(() => {
+    if (!isOpen) {
+      return null;
+    }
+
+    const dropdownOptions = options.map(option => {
+      const checkIcon =
+        option.id === activeSelection ? (
+          <SWMansionIcon icon="check" size={18} />
+        ) : null;
+
+      return (
+        <li
+          className={css.dropdownListItem}
+          key={option.id}
+          onClick={() => handleSelection(option)}
+        >
+          <button className={css.dropdownListItemButton}>
+            <p className={css.dropdownListDisplayText}>{option.name}</p>
+          </button>
+          <div className={css.dropdownListCheckIcon}>{checkIcon}</div>
+        </li>
+      );
+    });
+
+    return <ul className={css.dropdownList}>{dropdownOptions}</ul>;
+  }, [activeSelection, handleSelection, isOpen, options]);
+
+  return (
+    <>
+      <div className={css.dropdownContainer} onClick={toggleMenu}>
+        <div className={dropdownMenuClassNames}>
+          <p className={dropdownTextClassNames}>{activeDisplayedOption}</p>
+          <div className={css.dropdownIcon}>
+            <SWMansionIcon
+              icon={isOpen ? 'chevron-up' : 'chevron-down'}
+              color={disabled ? 'gray' : 'white'}
+              size={18}
+            />
+          </div>
+        </div>
+      </div>
+      {dropdownList}
+    </>
+  );
+}
+
+export default Dropdown;
