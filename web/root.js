d68e4d8acdc68525f84253efa420c15bde37ff86
xsanm <kkurowski.kk@gmail.com>
2023-06-06 15:40:09 +0200

    [web] initialize `localforage` on main thread
    Summary:
    On Safari we need to persist key from main thread. ([context](https://linear.app/comm/issue/ENG-3872#comment-05084359))
    
    Depends on D8116
    
    Test Plan:
    1. Try to add data to indexedDB from main thread
    2. Check in database on worker still works
    
    Reviewers: michal, tomek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D8117

--- web/root.js
+++ web/root.js
@@ -1,5 +1,6 @@
 // @flow
 
+import localforage from 'localforage';
 import * as React from 'react';
 import { Provider } from 'react-redux';
 import { Router, Route } from 'react-router';
@@ -13,12 +14,15 @@ import { reduxLoggerMiddleware } from 'lib/utils/action-logger.js';
 
 import App from './app.react.js';
 import { SQLiteDataHandler } from './database/sqlite-data-handler.js';
+import { localforageConfig } from './database/utils/constants.js';
 import ErrorBoundary from './error-boundary.react.js';
 import Loading from './loading.react.js';
 import { persistConfig } from './redux/persist.js';
 import { type AppState, type Action, reducer } from './redux/redux-setup.js';
 import history from './router-history.js';
 import Socket from './socket.react.js';
+
+localforage.config(localforageConfig);
 
 declare var preloadedState: AppState;
 
