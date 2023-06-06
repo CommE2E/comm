0c6c9bcbac28230d30a3c24d0489679854db0ce8
Rohan Khanderia <rohan.khanderia@gmail.com>
2023-06-06 16:29:27 -0400

    [web] Confirm a user wants to exit if changes are made in the modal
    Summary:
    Based on the designs, if a member's role is changed (not saved, but just different to their current role) in the dropdown menu, we need to add a confirmation exit modal to warn them that they will
    lose their changes by closing the role change modal.
    
    https://linear.app/comm/issue/ENG-4039/confirm-a-user-wants-to-exit-if-changes-are-made-in-the-modal
    
    Depends on D8138
    
    Test Plan: Please see the video below that covers the different use cases of no changes, changes, making a change but then changing the role back to the member's current role, etc.
    
    Reviewers: atul, ginsu
    
    Subscribers: ashoat, tomek, ted
    
    Differential Revision: https://phab.comm.dev/D8139

--- web/modals/threads/members/change-member-role-modal.react.js
+++ web/modals/threads/members/change-member-role-modal.react.js
@@ -11,6 +11,7 @@ import Button, { buttonThemes } from '../../../components/button.react.js';
 import Dropdown from '../../../components/dropdown.react.js';
 import UserAvatar from '../../../components/user-avatar.react.js';
 import Modal from '../../modal.react.js';
+import UnsavedChangesModal from '../../unsaved-changes-modal.react.js';
 
 type ChangeMemberRoleModalProps = {
   +memberInfo: RelativeMemberInfo,
@@ -19,7 +20,7 @@ type ChangeMemberRoleModalProps = {
 
 function ChangeMemberRoleModal(props: ChangeMemberRoleModalProps): React.Node {
   const { memberInfo, threadInfo } = props;
-  const { popModal } = useModalContext();
+  const { pushModal, popModal } = useModalContext();
   const modalName = 'Change Role';
 
   const roleOptions = React.useMemo(
@@ -41,6 +42,15 @@ function ChangeMemberRoleModal(props: ChangeMemberRoleModalProps): React.Node {
   const [selectedRole, setSelectedRole] =
     React.useState<?string>(initialSelectedRole);
 
+  const onBackClick = React.useCallback(() => {
+    if (selectedRole === initialSelectedRole) {
+      popModal();
+      return;
+    }
+
+    pushModal(<UnsavedChangesModal />);
+  }, [initialSelectedRole, popModal, pushModal, selectedRole]);
+
   return (
     <Modal name={modalName} onClose={popModal} size="large">
       <div className={css.roleModalDescription}>
@@ -61,7 +71,11 @@ function ChangeMemberRoleModal(props: ChangeMemberRoleModalProps): React.Node {
         />
       </div>
       <div className={css.roleModalActionButtons}>
-        <Button variant="outline" className={css.roleModalBackButton}>
+        <Button
+          variant="outline"
+          className={css.roleModalBackButton}
+          onClick={onBackClick}
+        >
           Back
         </Button>
         <Button
