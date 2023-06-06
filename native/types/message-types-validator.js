b7065e9f0580df23e974263b72e229c489bb74b6
marcinwasowicz <marcinkwasowicz@gmail.com>
2023-06-06 12:08:12 +0200

    Implement message typoes validator and import it in root.react.js for side effect
    Summary:
    This differential itroduces a file with custom code that checks if all message specs that might generate notifications are implemented in C++ as well. In the case of missing C++ message spec an dalert is displayed. Additionally this
    feature required us to alter MessageSpec type a little bit so that `generatesNotifs` field is now optional.
    
    Test Plan: Build the app and launch it. Ensure alert is not displayed. Go to `message-types-validator.js` file and comment the check for `generatesNotifs` in message spec. Now an alert with 5 message types should be displayed.
    
    Reviewers: tomek, atul, bartek
    
    Reviewed By: tomek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D8103

---
+++ native/types/message-types-validator.js
@@ -0,0 +1,29 @@
+// @flow
+import { Alert } from 'react-native';
+
+import { messageSpecs } from 'lib/shared/messages/message-specs.js';
+import { messageTypes } from 'lib/types/message-types-enum.js';
+
+import { commConstants } from '../native-modules.js';
+
+if (__DEV__) {
+  const messageTypesCpp = new Set(commConstants.NATIVE_MESSAGE_TYPES);
+  const missingMessageTypesCpp = [];
+  for (const messageName in messageTypes) {
+    const messageType = messageTypes[messageName];
+    if (
+      messageSpecs[messageType]?.generatesNotifs &&
+      !messageTypesCpp.has(messageType)
+    ) {
+      missingMessageTypesCpp.push(messageName);
+    }
+  }
+
+  if (missingMessageTypesCpp.length !== 0) {
+    Alert.alert(
+      'C++ MessageSpecs missing',
+      'C++ MessageSpec implementation missing for message types: ' +
+        missingMessageTypesCpp.join(', '),
+    );
+  }
+}
