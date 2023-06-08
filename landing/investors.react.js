501c8229902b39632877634bc0b8923853b94afa
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-06-08 14:09:53 -0400

    [landing] reskin investors page
    Summary: This diff updates the UI (typography/colors) for the investors page. I worked closely with @ted on this, and made sure I got his stamp of approval
    
    Test Plan: Please see the demo vidoes/screenshots below
    
    Subscribers: ashoat, tomek, ted
    
    Differential Revision: https://phab.comm.dev/D8171

--- landing/investors.react.js
+++ landing/investors.react.js
@@ -1,5 +1,6 @@
 // @flow
 
+import classNames from 'classnames';
 import * as React from 'react';
 
 import { useModalContext } from 'lib/components/modal-provider.react.js';
@@ -8,6 +9,7 @@ import { shuffledInvestorsData } from './investor-data.js';
 import InvestorProfileModal from './investor-profile-modal.react.js';
 import InvestorProfile from './investor-profile.react.js';
 import css from './investors.css';
+import typography from './typography.css';
 
 function Investors(): React.Node {
   const { pushModal } = useModalContext();
@@ -35,13 +37,16 @@ function Investors(): React.Node {
     ));
   }, [onClickInvestorProfileCard]);
 
+  const headerClassName = classNames([typography.heading1, css.header]);
+  const subheaderClassName = classNames([typography.subheading2, css.subtitle]);
+
   return (
     <div className={css.wrapper}>
-      <h2 className={css.header}>Investors</h2>
+      <h2 className={headerClassName}>Investors</h2>
 
       <section>
         <div className={css.headingContainer}>
-          <p className={css.subtitle}>
+          <p className={subheaderClassName}>
             Comm is proud to count over 80 individuals &amp; organizations from
             our community as investors.
           </p>
