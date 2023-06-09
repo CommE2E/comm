72a73ae34a193f8d4361c612758bdfeb48a0ef23
Rohan Khanderia <rohan.khanderia@gmail.com>
2023-06-09 11:14:43 -0400

    [native] Handle unsaved changes when 'cancel' is pressed
    Summary:
    As a second part to the header left button, we want to ensure that a user is prompted with an alert warning them that they are discarding changes if they've selected a different role but not saved.
    
    Depends on D8158
    
    Test Plan: Please see the video below
    
    Reviewers: atul, ginsu, tomek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D8159

--- native/roles/change-roles-header-left-button.react.js
+++ native/roles/change-roles-header-left-button.react.js
@@ -1,14 +1,44 @@
 // @flow
 
 import { HeaderBackButton as BaseHeaderBackButton } from '@react-navigation/elements';
+import invariant from 'invariant';
 import * as React from 'react';
-import { Text } from 'react-native';
+import { Alert, Text } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
+import type { NavigationRoute } from '../navigation/route-names';
 import { useColors } from '../themes/colors.js';
 
-type Props = React.ElementConfig<typeof BaseHeaderBackButton>;
-function ChangeRolesHeaderLeftButton(props: Props): React.Node {
+type ChangeRolesHeaderLeftButtonProps = {
+  +route: NavigationRoute<'ChangeRolesScreen'>,
+  ...React.ElementConfig<typeof BaseHeaderBackButton>,
+};
+
+function ChangeRolesHeaderLeftButton(
+  props: ChangeRolesHeaderLeftButtonProps,
+): React.Node {
+  const { memberInfo, role: selectedRole } = props.route.params;
+  const { role: memberRole } = memberInfo;
+
+  const onCancel = React.useCallback(() => {
+    const { onPress } = props;
+    invariant(onPress, 'onPress must be defined');
+
+    if (selectedRole === memberRole) {
+      onPress();
+      return;
+    }
+
+    Alert.alert(
+      'Discard changes?',
+      'You have unsaved changes which will be discarded if you navigate away',
+      [
+        { text: 'Leave', onPress },
+        { text: 'Stay', style: 'cancel' },
+      ],
+    );
+  }, [memberRole, props, selectedRole]);
+
   const { panelForegroundSecondaryLabel } = useColors();
   const labelStyle = React.useMemo(
     () => ({
@@ -19,7 +49,7 @@ function ChangeRolesHeaderLeftButton(props: Props): React.Node {
   );
 
   return (
-    <TouchableOpacity onPress={props.onPress}>
+    <TouchableOpacity onPress={onCancel}>
       <Text style={labelStyle}>Cancel</Text>
     </TouchableOpacity>
   );
