a9fc417b35afab21c54e8a1fe582b0cd9e7b5d89
Jonathan Ringer <jonathan.ringer@comm.app>
2023-06-11 18:12:28 -0700

    [Tunnelbroker] Use rabbitmq for message delivery
    Summary:
    Replace hashmap of concurrent connections with
    rabbitmq.
    
    Depends on D8177
    
    Part of:
    - https://linear.app/comm/issue/ENG-3717
    - https://linear.app/comm/issue/ENG-3684
    
    Test Plan:
    Run the tunnelbroker integration suite. First start docker, then:
    ```
    nix develop
    comm-dev services start
    
    (cd services/tunnelbroker && RUST_LOG=debug cargo run)
    (cd services/commtest && cargo test --test tunnelbroker_integration_test)
    ```
    
    Reviewers: varun, bartek
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8178

--- services/tunnelbroker/src/websockets/mod.rs
+++ services/tunnelbroker/src/websockets/mod.rs
@@ -1,15 +1,23 @@
 mod session;
 
 use crate::database::DatabaseClient;
+use crate::websockets::session::SessionError;
 use crate::CONFIG;
+use futures_util::stream::SplitSink;
 use futures_util::StreamExt;
 use std::net::SocketAddr;
 use std::{env, io::Error};
 use tokio::net::{TcpListener, TcpStream};
-use tokio::sync::mpsc;
-use tracing::{debug, info};
+use tokio_tungstenite::tungstenite::Message;
+use tokio_tungstenite::WebSocketStream;
+use tracing::{debug, error, info};
 
-pub async fn run_server(db_client: DatabaseClient) -> Result<(), Error> {
+use self::session::WebsocketSession;
+
+pub async fn run_server(
+  db_client: DatabaseClient,
+  amqp_connection: &lapin::Connection,
+) -> Result<(), Error> {
   let addr = env::var("COMM_TUNNELBROKER_WEBSOCKET_ADDR")
     .unwrap_or_else(|_| format!("127.0.0.1:{}", &CONFIG.http_port));
 
@@ -17,7 +25,11 @@ pub async fn run_server(db_client: DatabaseClient) -> Result<(), Error> {
   info!("Listening on: {}", addr);
 
   while let Ok((stream, addr)) = listener.accept().await {
-    tokio::spawn(accept_connection(stream, addr, db_client.clone()));
+    let channel = amqp_connection
+      .create_channel()
+      .await
+      .expect("Unable to create amqp channel");
+    tokio::spawn(accept_connection(stream, addr, db_client.clone(), channel));
   }
 
   Ok(())
@@ -28,6 +40,7 @@ async fn accept_connection(
   raw_stream: TcpStream,
   addr: SocketAddr,
   db_client: DatabaseClient,
+  amqp_channel: lapin::Channel,
 ) {
   debug!("Incoming connection from: {}", addr);
 
@@ -43,20 +56,39 @@ async fn accept_connection(
   };
 
   let (outgoing, mut incoming) = ws_stream.split();
-  // Create channel for messages to be passed to this connection
-  let (tx, mut rx) = mpsc::unbounded_channel::<String>();
 
-  let mut session = session::WebsocketSession::new(outgoing, db_client.clone());
+  // We don't know the identity of the device until it sends the session
+  // request over the websocket connection
+  let mut session = if let Some(Ok(first_msg)) = incoming.next().await {
+    match initiate_session(outgoing, first_msg, db_client, amqp_channel).await {
+      Ok(session) => session,
+      Err(_) => {
+        error!("Failed to create session with device");
+        return;
+      }
+    }
+  } else {
+    error!("Failed to create session with device");
+    return;
+  };
 
   // Poll for messages either being sent to the device (rx)
   // or messages being received from the device (incoming)
   loop {
     debug!("Polling for messages from: {}", addr);
     tokio::select! {
-      Some(message) = rx.recv() => { session.send_message_to_device(message).await; },
+      Some(Ok(delivery)) = session.next_amqp_message() => {
+        if let Ok(message) = std::str::from_utf8(&delivery.data) {
+          session.send_message_to_device(message.to_string()).await;
+        } else {
+          error!("Invalid payload");
+        }
+      },
       device_message = incoming.next() => {
         match device_message {
-          Some(Ok(msg)) => session.handle_websocket_frame_from_device(msg, tx.clone()).await,
+          Some(Ok(msg)) => {
+            session::consume_error(session.handle_websocket_frame_from_device(msg).await);
+          }
           _ => {
             debug!("Connection to {} closed remotely.", addr);
             break;
@@ -72,4 +104,27 @@ async fn accept_connection(
 
   info!("Unregistering connection to: {}", addr);
   session.close().await
+}
+
+async fn initiate_session(
+  outgoing: SplitSink<WebSocketStream<TcpStream>, Message>,
+  frame: Message,
+  db_client: DatabaseClient,
+  amqp_channel: lapin::Channel,
+) -> Result<WebsocketSession, session::SessionError> {
+  let mut session = session::WebsocketSession::from_frame(
+    outgoing,
+    db_client.clone(),
+    frame,
+    &amqp_channel,
+  )
+  .await
+  .map_err(|_| {
+    error!("Device failed to send valid connection request.");
+    SessionError::InvalidMessage
+  })?;
+
+  session::consume_error(session.deliver_persisted_messages().await);
+
+  Ok(session)
 }
