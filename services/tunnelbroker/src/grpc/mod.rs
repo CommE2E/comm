bd61eade5c8f6c0e484138674835c64a2953e0de
Jonathan Ringer <jonathan.ringer@comm.app>
2023-06-11 18:12:28 -0700

    [Tunnelbroker] Use rabbitmq for message delivery
    Summary: Replace hashmap of concurrent connections with
    rabbitmq.
    
    Depends on D8177
    
    Part of:
    - https://linear.app/comm/issue/ENG-3717
    - https://linear.app/comm/issue/ENG-3684
    
    Test Plan:
    Run the tunnelbroker integration suite. First start docker, then:
    ```
    nix develop
    comm-dev services start
    
    (cd services/tunnelbroker && RUST_LOG=debug cargo run)
    (cd services/commtest && cargo test --test tunnelbroker_integration_test)
    ```
    
    Reviewers: varun, bartek
    
    Subscribers:

--- services/tunnelbroker/src/grpc/mod.rs
+++ services/tunnelbroker/src/grpc/mod.rs
@@ -2,19 +2,28 @@ mod proto {
   tonic::include_proto!("tunnelbroker");
 }
 
+use lapin::{options::BasicPublishOptions, BasicProperties};
 use proto::tunnelbroker_service_server::{
   TunnelbrokerService, TunnelbrokerServiceServer,
 };
 use proto::Empty;
 use tonic::transport::Server;
-use tracing::{debug, error};
+use tracing::debug;
 
 use crate::database::{handle_ddb_error, DatabaseClient};
-use crate::{constants, ACTIVE_CONNECTIONS, CONFIG};
+use crate::{constants, CONFIG};
 
 struct TunnelbrokerGRPC {
   client: DatabaseClient,
+  amqp_channel: lapin::Channel,
 }
+
+// By setting mandatory to true, we don't wait for a confirmation for an eventual
+// delivery, instead we get an immediate undelivered error
+const PUBLISH_OPTIONS: BasicPublishOptions = BasicPublishOptions {
+  immediate: false,
+  mandatory: true,
+};
 
 #[tonic::async_trait]
 impl TunnelbrokerService for TunnelbrokerGRPC {
@@ -25,11 +34,19 @@ impl TunnelbrokerService for TunnelbrokerGRPC {
     let message = request.into_inner();
 
     debug!("Received message for {}", &message.device_id);
-    if let Some(tx) = ACTIVE_CONNECTIONS.get(&message.device_id) {
-      if let Err(_) = tx.send(message.payload) {
-        error!("Unable to send message to device: {}", &message.device_id);
-        ACTIVE_CONNECTIONS.remove(&message.device_id);
-      }
+    if let Ok(confirmation) = self
+      .amqp_channel
+      .basic_publish(
+        "",
+        &message.device_id,
+        PUBLISH_OPTIONS,
+        &message.payload.as_bytes(),
+        BasicProperties::default(),
+      )
+      .await
+    {
+      debug!("Forwarded message: {:?}", &message);
+      confirmation.await.expect("Didn't get acknowledgement");
     } else {
       self
         .client
@@ -45,16 +62,25 @@ impl TunnelbrokerService for TunnelbrokerGRPC {
 
 pub async fn run_server(
   client: DatabaseClient,
+  ampq_connection: &lapin::Connection,
 ) -> Result<(), tonic::transport::Error> {
   let addr = format!("[::1]:{}", CONFIG.grpc_port)
     .parse()
     .expect("Unable to parse gRPC address");
 
+  let amqp_channel = ampq_connection
+    .create_channel()
+    .await
+    .expect("Unable to create amqp channel");
+
   tracing::info!("Websocket server listening on {}", &addr);
   Server::builder()
     .http2_keepalive_interval(Some(constants::GRPC_KEEP_ALIVE_PING_INTERVAL))
     .http2_keepalive_timeout(Some(constants::GRPC_KEEP_ALIVE_PING_TIMEOUT))
-    .add_service(TunnelbrokerServiceServer::new(TunnelbrokerGRPC { client }))
+    .add_service(TunnelbrokerServiceServer::new(TunnelbrokerGRPC {
+      client,
+      amqp_channel,
+    }))
     .serve(addr)
     .await
 }
--- services/tunnelbroker/src/websockets/mod.rs
+++ services/tunnelbroker/src/websockets/mod.rs
@@ -3,13 +3,17 @@ mod session;
 use crate::database::DatabaseClient;
 use crate::CONFIG;
 use futures_util::StreamExt;
+use lapin::options::{BasicConsumeOptions, QueueDeclareOptions};
+use lapin::types::FieldTable;
 use std::net::SocketAddr;
 use std::{env, io::Error};
 use tokio::net::{TcpListener, TcpStream};
-use tokio::sync::mpsc;
-use tracing::{debug, info};
+use tracing::{debug, error, info};
 
-pub async fn run_server(db_client: DatabaseClient) -> Result<(), Error> {
+pub async fn run_server(
+  db_client: DatabaseClient,
+  amqp_connection: &lapin::Connection,
+) -> Result<(), Error> {
   let addr = env::var("COMM_TUNNELBROKER_WEBSOCKET_ADDR")
     .unwrap_or_else(|_| format!("127.0.0.1:{}", &CONFIG.http_port));
 
@@ -17,7 +21,11 @@ pub async fn run_server(db_client: DatabaseClient) -> Result<(), Error> {
   info!("Listening on: {}", addr);
 
   while let Ok((stream, addr)) = listener.accept().await {
-    tokio::spawn(accept_connection(stream, addr, db_client.clone()));
+    let channel = amqp_connection
+      .create_channel()
+      .await
+      .expect("Unable to create amqp channel");
+    tokio::spawn(accept_connection(stream, addr, db_client.clone(), channel));
   }
 
   Ok(())
@@ -28,6 +36,7 @@ async fn accept_connection(
   raw_stream: TcpStream,
   addr: SocketAddr,
   db_client: DatabaseClient,
+  amqp_channel: lapin::Channel,
 ) {
   debug!("Incoming connection from: {}", addr);
 
@@ -43,20 +52,66 @@ async fn accept_connection(
   };
 
   let (outgoing, mut incoming) = ws_stream.split();
-  // Create channel for messages to be passed to this connection
-  let (tx, mut rx) = mpsc::unbounded_channel::<String>();
 
-  let mut session = session::WebsocketSession::new(outgoing, db_client.clone());
+  // We don't know the identity of the device until it sends the session
+  // request over the websocket connection
+  let mut session = if let Some(Ok(first_msg)) = incoming.next().await {
+    if let Ok(mut session) = session::WebsocketSession::from_frame(
+      outgoing,
+      db_client.clone(),
+      first_msg,
+    ) {
+      // TODO: Authenticate device
+      session::consume_error(session.deliver_persisted_messages().await);
+      session
+    } else {
+      error!("Device failed to send valid connection request.");
+      return;
+    }
+  } else {
+    error!("Device closed connection before sending first message");
+    return;
+  };
+
+  let _amqp_queue = amqp_channel
+    .queue_declare(
+      &session.device_info.device_id,
+      QueueDeclareOptions::default(),
+      FieldTable::default(),
+    )
+    .await
+    .expect(&format!(
+      "Failed to create amqp queue for device: {}",
+      &session.device_info.device_id
+    ));
+
+  let mut amqp_consumer = amqp_channel
+    .basic_consume(
+      &session.device_info.device_id,
+      "tunnelbroker",
+      BasicConsumeOptions::default(),
+      FieldTable::default(),
+    )
+    .await
+    .expect("Failed to create amqp consumer.");
 
   // Poll for messages either being sent to the device (rx)
   // or messages being received from the device (incoming)
   loop {
     debug!("Polling for messages from: {}", addr);
     tokio::select! {
-      Some(message) = rx.recv() => { session.send_message_to_device(message).await; },
+      Some(Ok(delivery)) = amqp_consumer.next() => {
+        if let Ok(message) = std::str::from_utf8(&delivery.data) {
+          session.send_message_to_device(message.to_string()).await;
+        } else {
+          error!("Invalid payload");
+        }
+      },
       device_message = incoming.next() => {
         match device_message {
-          Some(Ok(msg)) => session.handle_websocket_frame_from_device(msg, tx.clone()).await,
+          Some(Ok(msg)) => {
+            session::consume_error(session.handle_websocket_frame_from_device(msg).await);
+          }
           _ => {
             debug!("Connection to {} closed remotely.", addr);
             break;
