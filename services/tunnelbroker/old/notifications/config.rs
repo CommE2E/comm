50e3b6214a8f5c4eacfe5d50428911ce719b2f92
Jonathan Ringer <jonathan.ringer@comm.app>
2023-06-14 08:01:58 -0700

    [Tunnelbroker] Remove C++ codebase
    Summary: Thought that the old codebase would be more useful
    when developing the "new" tunnelbroker. However, protocol
    and assumption differences between the two made this
    legacy codebase less valuable.
    
    Test Plan: N/A
    
    Reviewers: ashoat, varun, bartek
    
    Subscribers:

--- services/tunnelbroker/old/notifications/config.rs
+++
@@ -1,8 +0,0 @@
-#[derive(Default, Clone)]
-pub struct Config {
-  pub fcm_api_key: String,
-  pub apns_certificate_path: String,
-  pub apns_certificate_password: String,
-  pub apns_topic: String,
-  pub is_sandbox: bool,
-}
