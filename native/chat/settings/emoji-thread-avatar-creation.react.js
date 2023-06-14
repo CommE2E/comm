de1accf26d269b5fd76c9ca3db872d799c161a62
Atul Madhugiri <atulm@berkeley.edu>
2023-06-14 16:48:19 -0400

    [lib] Rename `EditThreadAvatarProvider` to `BaseEditThreadAvatarProvider` and move to `lib`
    Summary:
    This component is now "platform-agnostic" and can be moved to `lib`. On `native` it is wrapped by `NativeEditThreadAvatarProvider` which passes in platform-specific props.
    
    ---
    
    Depends on D8220
    
    Test Plan:
    Renamed and moved file and let IDE auto-fix imports.
    
    Everything continues to work as expected.
    
    Reviewers:
    ashoat, ginsu

--- native/chat/settings/emoji-thread-avatar-creation.react.js
+++ native/chat/settings/emoji-thread-avatar-creation.react.js
@@ -3,10 +3,10 @@
 import invariant from 'invariant';
 import * as React from 'react';
 
+import { EditThreadAvatarContext } from 'lib/components/base-edit-thread-avatar-provider.react.js';
 import { savedEmojiAvatarSelectorForThread } from 'lib/selectors/thread-selectors.js';
 import type { RawThreadInfo, ThreadInfo } from 'lib/types/thread-types.js';
 
-import { EditThreadAvatarContext } from '../../avatars/edit-thread-avatar-provider.react.js';
 import EmojiAvatarCreation from '../../avatars/emoji-avatar-creation.react.js';
 import type { ChatNavigationProp } from '../../chat/chat.react.js';
 import { displayActionResultModal } from '../../navigation/action-result-modal.js';
