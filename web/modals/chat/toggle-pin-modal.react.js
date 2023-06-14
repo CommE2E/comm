52d4e6c1be5930ce70d9c6e489d7c52d0d3a1a04
Inka Sokołowska <inka.sokolowska@swmansion.com>
2023-06-14 15:13:31 +0200

    [web] Change how margins / paddings are used in message result
    Summary:
    I need to change how margins / paddings are used in message result, because I will be using this component with our `SearchModal` component that has some default margin. So I move the responsibility of setting the margins / paddings to the outer container.
    
    Additionally the margin in `TogglePinModal` gets fixed: it seems to me from the [[ https://www.figma.com/file/a1nkbWgbgjRlrOY9LVurTz/Comm-%2F-Desktop-app?type=design&node-id=10106-285040&t=VkRZGqxBRynDLvRt-4 | designs ]] that there should be a space between the message and the top text. @rohan please tell me if I'm misunderstanding
    
    Test Plan:
    Before:
    
    {F586881}
    
    After:
    
    {F586883}
    
    Reviewers: rohan
    
    Reviewed By: rohan
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8207

--- web/modals/chat/toggle-pin-modal.react.js
+++ web/modals/chat/toggle-pin-modal.react.js
@@ -102,11 +102,13 @@ function TogglePinModal(props: TogglePinModalProps): React.Node {
   return (
     <Modal name={modalInfo.name} onClose={popModal} size="large">
       <div className={css.confirmationText}>{modalInfo.confirmationText}</div>
-      <MessageResult
-        item={modifiedItem}
-        threadInfo={threadInfo}
-        scrollable={true}
-      />
+      <div className={css.messageContainer}>
+        <MessageResult
+          item={modifiedItem}
+          threadInfo={threadInfo}
+          scrollable={true}
+        />
+      </div>
       <div className={css.buttonContainer}>
         <Button
           variant="filled"
