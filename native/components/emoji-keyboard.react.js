99a91a6090cf2309ac7d43547c0775eb363088b0
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-06-15 16:27:13 -0400

    [native] persist recently used emojis
    Summary: This diff persists the recently selected emojis from the emoji keyboard into async storage. To create this diff I read through the docs and found these examples right here:
    
    Test Plan: Please watch the demo video
    
    Reviewers:
    
    Subscribers:

--- native/components/emoji-keyboard.react.js
+++ native/components/emoji-keyboard.react.js
@@ -1,7 +1,10 @@
 // @flow
 
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as React from 'react';
-import EmojiPicker from 'rn-emoji-keyboard';
+import EmojiPicker, { useRecentPicksPersistence } from 'rn-emoji-keyboard';
+
+const STORAGE_KEY = 'EMOJI_KEYBOARD_RECENT';
 
 const categoryOrder = [
   'recently_used',
@@ -34,6 +37,26 @@ type Props = {
 
 function EmojiKeyboard(props: Props): React.Node {
   const { onEmojiSelected, emojiKeyboardOpen, onEmojiKeyboardClose } = props;
+
+  const initializationCallback = React.useCallback(async () => {
+    const recentlyUsedEmojis = await AsyncStorage.getItem(STORAGE_KEY);
+    return JSON.parse(recentlyUsedEmojis ?? '[]');
+  }, []);
+
+  const onStateChangeCallback = React.useCallback(
+    async nextRecentlyUsedEmojis => {
+      await AsyncStorage.setItem(
+        STORAGE_KEY,
+        JSON.stringify(nextRecentlyUsedEmojis),
+      );
+    },
+    [],
+  );
+
+  useRecentPicksPersistence({
+    initialization: initializationCallback,
+    onStateChange: onStateChangeCallback,
+  });
 
   return (
     <EmojiPicker
