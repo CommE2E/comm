ea26d8627b4fc74b8e0118861eaa0b6f313d00d7
Jonathan Ringer <jonringer117@gmail.com>
2023-06-15 16:15:41 -0700

    [Tunnelbroker] Add docker image
    Summary: Package tunnelbroker into a docker image
    
    https://linear.app/comm/issue/ENG-4103
    
    Test Plan:
    ```
    ./services/tunnelbroker/make_docker_image.sh
    ```
    
    Reviewers: varun, bartek, ashoat
    
    Subscribers:

---
+++ services/tunnelbroker/make_docker_image.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# This file exists to make a smaller docker context, so that building it is
+# significantly faster and requires less system resources
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" || exit 1; pwd -P)"
+BUILD_DIR="${SCRIPT_DIR}/target/oci_image"
+
+rm -rf "$BUILD_DIR"
+mkdir -p "$BUILD_DIR"/{scripts,shared,services/tunnelbroker}
+
+cp "$SCRIPT_DIR/../../scripts/install_protobuf.sh" "$BUILD_DIR"/scripts
+cp -r "${SCRIPT_DIR}/../../shared/protos" "$BUILD_DIR"/shared/protos
+cp -r "${SCRIPT_DIR}/../../shared/tunnelbroker_messages" \
+  "$BUILD_DIR"/shared/tunnelbroker_messages
+cp -r "${SCRIPT_DIR}"/{Cargo.toml,Cargo.lock,build.rs,src} \
+  "$BUILD_DIR"/services/tunnelbroker/
+
+docker build -f "${SCRIPT_DIR}/Dockerfile" "$BUILD_DIR"
