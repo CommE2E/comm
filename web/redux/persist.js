be76e1f997dbae916f3d81140cee0d37d762bc36
Michal Gniadek <michal.gniadek@swmansion.com>
2023-06-20 12:56:35 +0200

    [web] Move createAsyncMigrate to lib
    Summary:
    https://linear.app/comm/issue/ENG-3997/support-async-migrations-on-native
    We want to support async migrations on native, as we do on the web. This diffs moves the `createAsyncMigrate` function to `lib`, and extracts `web`-specific code.
    
    Test Plan:
    (tested on firefox)
    1. Remove firefox from `DB_SUPPORTED_BROWSERS`, change stateVersion on web from 2 to 1, clear all data for localhost in firefox
    2. Open the web app, check if indexedDB is empty, create a dummy draft
    3. Add firefox to supported browsers, bump stateVersion to 2
    4. Open the app, check if the draft is still there, check if indexedDB has data
    5. Refresh the app check if the draft is still there
    
    Reviewers: kamil!, tomek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D8352

--- web/redux/persist.js
+++ web/redux/persist.js
@@ -1,11 +1,16 @@
 // @flow
 
+import { getStoredState, purgeStoredState } from 'redux-persist';
+import storage from 'redux-persist/es/storage/index.js';
 import type { PersistConfig } from 'redux-persist/src/types.js';
 
+import {
+  createAsyncMigrate,
+  type StorageMigrationFunction,
+} from 'lib/shared/create-async-migrate.js';
 import { isDev } from 'lib/utils/dev-utils.js';
 
 import commReduxStorageEngine from './comm-redux-storage-engine.js';
-import { createAsyncMigrate } from './create-async-migrate.js';
 import type { AppState } from './redux-setup.js';
 import { databaseModule } from '../database/database-module-provider.js';
 import { isSQLiteSupported } from '../database/utils/db-utils.js';
@@ -67,13 +72,38 @@ const persistWhitelist = [
   'lastCommunicatedPlatformDetails',
 ];
 
+const rootKey = 'root';
+
+const migrateStorageToSQLite: StorageMigrationFunction = async debug => {
+  const isSupported = await databaseModule.isDatabaseSupported();
+  if (!isSupported) {
+    return undefined;
+  }
+
+  const oldStorage = await getStoredState({ storage, key: rootKey });
+  if (!oldStorage) {
+    return undefined;
+  }
+
+  purgeStoredState({ storage, key: rootKey });
+  if (debug) {
+    console.log('redux-persist: migrating state to SQLite storage');
+  }
+
+  return oldStorage;
+};
+
 const persistConfig: PersistConfig = {
-  key: 'root',
+  key: rootKey,
   storage: commReduxStorageEngine,
   whitelist: isDatabaseSupported
     ? persistWhitelist
     : [...persistWhitelist, 'draftStore'],
-  migrate: (createAsyncMigrate(migrations, { debug: isDev }): any),
+  migrate: (createAsyncMigrate(
+    migrations,
+    { debug: isDev },
+    migrateStorageToSQLite,
+  ): any),
   version: 2,
 };
 
