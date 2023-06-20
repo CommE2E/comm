245a91a4b43bcae59630d612e1a461a82fd5e4c0
Atul Madhugiri <atulm@berkeley.edu>
2023-06-20 12:47:22 -0400

    [web] Introduce placeholder `WebEditUserAvatarProvider`
    Summary:
    Very very naive implementation of `WebEditUserAvatarProvider` w/ platform-specific props set to dummy values for the timebeing.
    
    This unblocks (non-image) avatar work (remove avatar, emoji avatar, etc) and lets us defer the image avatar stuff for later. Will obviously have to come back and implement these when it comes time to work on image avatar work, just want to start w/ the simplest flows first.
    
    Test Plan:
    Made sure I could "get values out of the context" from `EditUserAvatar` w/ some logging:
    
    {F594246}
    
    Reviewers: ashoat, ginsu
    
    Reviewed By: ashoat
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D8249

---
+++ web/avatars/web-edit-user-avatar-provider.react.js
@@ -0,0 +1,32 @@
+// @flow
+
+import * as React from 'react';
+
+import { BaseEditUserAvatarProvider } from 'lib/components/base-edit-user-avatar-provider.react.js';
+
+const displayAvatarUpdateFailureAlert = () =>
+  alert('Couldn&rsquo;t save avatar. Please try again later.');
+
+// TODO: Implement `selectFromGallery(...)` for `web`.
+const selectFromGallery = async () => null;
+
+// TODO: Implement `useUploadSelectedMedia(...)` for `web`.
+const useUploadSelectedMedia = () => async () => null;
+
+type Props = {
+  +children: React.Node,
+};
+function WebEditUserAvatarProvider(props: Props): React.Node {
+  const { children } = props;
+  return (
+    <BaseEditUserAvatarProvider
+      displayFailureAlert={displayAvatarUpdateFailureAlert}
+      selectFromGallery={selectFromGallery}
+      useUploadSelectedMedia={useUploadSelectedMedia}
+    >
+      {children}
+    </BaseEditUserAvatarProvider>
+  );
+}
+
+export default WebEditUserAvatarProvider;
