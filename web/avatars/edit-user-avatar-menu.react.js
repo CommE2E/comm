96307d0d1bcdc025ead66dd0559235b55c9f8e91
Atul Madhugiri <atulm@berkeley.edu>
2023-06-20 15:50:56 -0400

    [web] Introduce placeholder `EmojiAvatarSelectionModal`
    Summary:
    Introduce placeholder `EmojiAvatarSelectionModal` component. Modal opens when "Select emoji" `MenuItem` in `EditUserAvatarMenu` is clicked.
    
    Layout/styling/functionality will be introduced incrementally in subsequent diffs.
    
    ---
    
    Depends on D8264
    
    Test Plan: Opens and looks about as expected:
    
    Reviewers: ashoat, ginsu
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D8265

--- web/avatars/edit-user-avatar-menu.react.js
+++ web/avatars/edit-user-avatar-menu.react.js
@@ -4,9 +4,11 @@ import invariant from 'invariant';
 import * as React from 'react';
 
 import { EditUserAvatarContext } from 'lib/components/base-edit-user-avatar-provider.react.js';
+import { useModalContext } from 'lib/components/modal-provider.react.js';
 import SWMansionIcon from 'lib/components/SWMansionIcon.react.js';
 
 import css from './edit-user-avatar-menu.css';
+import EmojiAvatarSelectionModal from './emoji-avatar-selection-modal.react.js';
 import MenuItem from '../components/menu-item.react.js';
 import Menu from '../components/menu.react.js';
 
@@ -27,9 +29,23 @@ function EditUserAvatarMenu(): React.Node {
     [setUserAvatar],
   );
 
+  const { pushModal } = useModalContext();
+
+  const openEmojiSelectionModal = React.useCallback(
+    () => pushModal(<EmojiAvatarSelectionModal />),
+    [pushModal],
+  );
+
   const emojiMenuItem = React.useMemo(
-    () => <MenuItem key="emoji" text="Select emoji" icon="emote-smile" />,
-    [],
+    () => (
+      <MenuItem
+        key="emoji"
+        text="Select emoji"
+        icon="emote-smile"
+        onClick={openEmojiSelectionModal}
+      />
+    ),
+    [openEmojiSelectionModal],
   );
   const imageMenuItem = React.useMemo(
     () => <MenuItem key="image" text="Select image" icon="image-1" />,
