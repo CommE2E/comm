cff404849492d0a3c54458b75104a083f309d034
Atul Madhugiri <atulm@berkeley.edu>
2023-06-20 16:30:34 -0400

    [web] Add `Avatar` preview to `EmojiAvatarSelectionModal`
    Summary:
    Inspired loosely by `CommunityCreationModal`.
    
    ---
    
    Depends on D8265
    
    Test Plan: Looks as expected:
    
    Reviewers: ashoat, ginsu
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D8266

--- web/avatars/emoji-avatar-selection-modal.react.js
+++ web/avatars/emoji-avatar-selection-modal.react.js
@@ -3,17 +3,61 @@
 import * as React from 'react';
 
 import { useModalContext } from 'lib/components/modal-provider.react.js';
+import {
+  defaultAnonymousUserEmojiAvatar,
+  getAvatarForUser,
+  getDefaultAvatar,
+} from 'lib/shared/avatar-utils.js';
+import type {
+  ClientAvatar,
+  ClientEmojiAvatar,
+} from 'lib/types/avatar-types.js';
 
+import Avatar from './avatar.react.js';
+import css from './emoji-avatar-selection-modal.css';
 import Modal from '../modals/modal.react.js';
+import { useSelector } from '../redux/redux-utils.js';
 
 function EmojiAvatarSelectionModal(): React.Node {
   const { popModal } = useModalContext();
+
+  const currentUserInfo = useSelector(state => state.currentUserInfo);
+  const currentUserAvatar: ClientAvatar = getAvatarForUser(currentUserInfo);
+  const defaultUserAvatar: ClientEmojiAvatar = currentUserInfo?.username
+    ? getDefaultAvatar(currentUserInfo.username)
+    : defaultAnonymousUserEmojiAvatar;
+
+  // eslint-disable-next-line no-unused-vars
+  const [pendingAvatarEmoji, setPendingAvatarEmoji] = React.useState(
+    currentUserAvatar.type === 'emoji'
+      ? currentUserAvatar.emoji
+      : defaultUserAvatar.emoji,
+  );
+
+  // eslint-disable-next-line no-unused-vars
+  const [pendingAvatarColor, setPendingAvatarColor] = React.useState(
+    currentUserAvatar.type === 'emoji'
+      ? currentUserAvatar.color
+      : defaultUserAvatar.color,
+  );
+
+  const pendingEmojiAvatar: ClientEmojiAvatar = React.useMemo(
+    () => ({
+      type: 'emoji',
+      emoji: pendingAvatarEmoji,
+      color: pendingAvatarColor,
+    }),
+    [pendingAvatarColor, pendingAvatarEmoji],
+  );
+
   return (
-    <Modal
-      name="Emoji avatar selection"
-      size="large"
-      onClose={popModal}
-    ></Modal>
+    <Modal name="Emoji avatar selection" size="large" onClose={popModal}>
+      <div className={css.modalBody}>
+        <div className={css.avatarContainer}>
+          <Avatar avatarInfo={pendingEmojiAvatar} size="profile" />
+        </div>
+      </div>
+    </Modal>
   );
 }
 
