fdcc193d610b6771a670b2cdfb9b5a812c2d69e8
Atul Madhugiri <atulm@berkeley.edu>
2023-06-20 13:43:39 -0400

    [web] Display `loadingSpinner` over `EditUserAvatar` when change in progress
    Summary:
    Show a loading spinner over `EditUserAvatar` when a save is in progress. Pretty straightforward. In next diff I'll quickly "disable" the edit button as well.
    
    ---
    
    Depends on D8260
    
    Test Plan: Looks as expected:
    
    Reviewers: ashoat, ginsu
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D8261

--- web/avatars/edit-user-avatar.react.js
+++ web/avatars/edit-user-avatar.react.js
@@ -1,20 +1,34 @@
 // @flow
 
+import invariant from 'invariant';
 import * as React from 'react';
+
+import { EditUserAvatarContext } from 'lib/components/base-edit-user-avatar-provider.react.js';
 
 import EditUserAvatarMenu from './edit-user-avatar-menu.react.js';
 import css from './edit-user-avatar.css';
 import UserAvatar from './user-avatar.react.js';
+import LoadingIndicator from '../loading-indicator.react.js';
+
+const loadingSpinner = <LoadingIndicator status="loading" size="large" />;
 
 type Props = {
   +userID: ?string,
   +disabled?: boolean,
 };
 function EditUserAvatar(props: Props): React.Node {
+  const editUserAvatarContext = React.useContext(EditUserAvatarContext);
+  invariant(editUserAvatarContext, 'editUserAvatarContext should be set');
+
+  const { userAvatarSaveInProgress } = editUserAvatarContext;
+
   const { userID } = props;
 
   return (
     <div className={css.editUserAvatarContainer}>
+      <div className={css.editAvatarLoadingSpinner}>
+        {userAvatarSaveInProgress ? loadingSpinner : undefined}
+      </div>
       <UserAvatar userID={userID} size="profile" />
       <div className={css.editAvatarBadge}>
         <EditUserAvatarMenu />
