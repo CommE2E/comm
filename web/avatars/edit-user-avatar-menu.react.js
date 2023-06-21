964adba37cf9d9d75121859b39d7857c32ba5114
Atul Madhugiri <atulm@berkeley.edu>
2023-06-21 14:23:05 -0400

    [web] Add `removeUserAvatar` to `EditUserAvatarMenu`
    Summary: Sets "Reset to default" menu item `onClick` to `removeUserAvatar` which removes the user's avatar.
    
    Test Plan:
    Works as expected. User had image avatar, now they have default emoji avatar:
    
    {F597512}
    
    Reviewers: ashoat, ginsu, rohan, kamil, inka, michal
    
    Reviewed By: rohan
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D8260

--- web/avatars/edit-user-avatar-menu.react.js
+++ web/avatars/edit-user-avatar-menu.react.js
@@ -1,7 +1,9 @@
 // @flow
 
+import invariant from 'invariant';
 import * as React from 'react';
 
+import { EditUserAvatarContext } from 'lib/components/base-edit-user-avatar-provider.react.js';
 import SWMansionIcon from 'lib/components/SWMansionIcon.react.js';
 
 import MenuItem from '../components/menu-item.react.js';
@@ -10,6 +12,16 @@ import Menu from '../components/menu.react.js';
 const editIcon = <SWMansionIcon icon="edit-2" size={20} />;
 
 function EditUserAvatarMenu(): React.Node {
+  const editUserAvatarContext = React.useContext(EditUserAvatarContext);
+  invariant(editUserAvatarContext, 'editUserAvatarContext should be set');
+
+  const { setUserAvatar } = editUserAvatarContext;
+
+  const removeUserAvatar = React.useCallback(
+    () => setUserAvatar({ type: 'remove' }),
+    [setUserAvatar],
+  );
+
   const emojiMenuItem = React.useMemo(
     () => <MenuItem key="emoji" text="Select emoji" icon="emote-smile" />,
     [],
@@ -19,8 +31,15 @@ function EditUserAvatarMenu(): React.Node {
     [],
   );
   const removeMenuItem = React.useMemo(
-    () => <MenuItem key="remove" text="Reset to default" icon="trash-2" />,
-    [],
+    () => (
+      <MenuItem
+        key="remove"
+        text="Reset to default"
+        icon="trash-2"
+        onClick={removeUserAvatar}
+      />
+    ),
+    [removeUserAvatar],
   );
 
   const menuItems = React.useMemo(
