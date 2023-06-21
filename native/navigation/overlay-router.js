ea47bf2b464adf06bc64d589f55bc277f71baab7
kosmydel <kukubaczek@gmail.com>
2023-06-21 11:54:48 +0200

    [native] Blocking navigation in edit mode in OverlayRouter
    Summary: When we are in edit mode and user made changes, we prevent him in this diff from opening modals i.g.:
    - opening images,
    - opening tooltips.
    
    Test Plan:
    - Enter edit mode,
    - Open modal without making changes.
    - Enter edit mode and make changes to the message,
    - Open modal - should confirm with alert.
    - Check if draft is restored.
    
    Reviewers: michal, inka, ashoat, tomek

--- native/navigation/overlay-router.js
+++ native/navigation/overlay-router.js
@@ -15,6 +15,7 @@ import {
   clearOverlayModalsActionType,
   setRouteParamsActionType,
 } from './action-types.js';
+import { getChatTabSubrouteState, getRemoveEditMode } from './nav-selectors.js';
 import { removeScreensFromStack } from './navigation-utils.js';
 
 type ClearOverlayModalsAction = {
@@ -56,6 +57,8 @@ function OverlayRouter(
       action: OverlayRouterNavigationAction,
       options: RouterConfigOptions,
     ) => {
+      const subrouteState = getChatTabSubrouteState(lastState);
+      const removeEditMode = getRemoveEditMode(subrouteState);
       if (action.type === clearOverlayModalsActionType) {
         const { keys } = action.payload;
         if (!lastState) {
@@ -81,6 +84,9 @@ function OverlayRouter(
           ...lastState,
           routes: newRoutes,
         };
+      } else if (removeEditMode) {
+        removeEditMode(action);
+        return lastState;
       } else {
         return baseGetStateForAction(lastState, action, options);
       }
