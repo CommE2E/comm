9ab26a0fd2a047789e1b86d9984e0613106d7c9b
kosmydel <kukubaczek@gmail.com>
2023-06-21 11:59:32 +0200

    [native] Unblock scrolling when continuing editing
    Summary: After the user decides to continue editing if tried to open the modal, he wasn't able to scroll anymore. This diff addresses that, by unlocking scrolling after closing the alert.
    
    Test Plan:
    - Enter edit mode and make changes to the message,
    - Try to open the tooltip modal
    - Press continue editing
    - Check if the user still can scroll the messages list
    
    Reviewers: michal, inka, ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8271

--- native/chat/chat-input-bar.react.js
+++ native/chat/chat-input-bar.react.js
@@ -111,6 +111,8 @@ import {
   activeThreadSelector,
 } from '../navigation/nav-selectors.js';
 import { NavContext } from '../navigation/navigation-context.js';
+import { OverlayContext } from '../navigation/overlay-context.js';
+import type { OverlayContextType } from '../navigation/overlay-context.js';
 import {
   type NavigationRoute,
   ChatCameraModalRouteName,
@@ -171,6 +173,7 @@ type Props = {
   ) => Promise<SendEditMessageResponse>,
   +navigation: ?ChatNavigationProp<'MessageList'>,
   +isFocused?: boolean,
+  +overlayContext: ?OverlayContextType,
 };
 type State = {
   +text: string,
@@ -893,7 +896,9 @@ class ChatInputBar extends React.PureComponent<Props, State> {
       unblockAndDispatch();
       return;
     }
-    exitEditAlert(unblockAndDispatch);
+    exitEditAlert(unblockAndDispatch, () => {
+      this.props.overlayContext?.setScrollBlockingModalStatus('closed');
+    });
   };
 
   blockNavigation = () => {
@@ -1222,6 +1227,7 @@ function ConnectedChatInputBarBase(props: ConnectedChatInputBarBaseProps) {
     getDefaultTextMessageRules().simpleMarkdownRules,
   );
   const editMessage = useEditMessage();
+  const overlayContext = React.useContext(OverlayContext);
 
   return (
     <ChatInputBar
@@ -1248,6 +1254,7 @@ function ConnectedChatInputBarBase(props: ConnectedChatInputBarBaseProps) {
       editedMessageInfo={editedMessageInfo}
       editMessage={editMessage}
       navigation={props.navigation}
+      overlayContext={overlayContext}
     />
   );
 }
