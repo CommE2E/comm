85f6e86b6ef97931ef13ce760d26f6dfae482207
Inka Sokołowska <inka.sokolowska@swmansion.com>
2023-06-22 15:56:48 +0200

    [web] Add to MessageSearchContext results related information
    Summary:
    issue: https://linear.app/comm/issue/ENG-4163/add-results-and-position-to-message-search-context
    We want to remember the results the user has fetched until they change/clear the query. We want to be able to close and reopen the modal, and see those results.
    We need to remember `endReached` so that when the user scrolls to the bottom, and they had already fetched all messages, they don't query the server again.
    `query` was changed to `getQuery` because:
    If the user searches for something, closes the modal and reopens it, we want to show them the results they have previously fetched. We only want the client to call the server, if the user presses the "Search" button, or scrolls to the bottom.
    When the "Search" button is pressed, we first have to set the query via `setQuery` function provided by this context. Only once the query is set, we want to call the server. In class react we could pass a callback to `setState`, but we cannot
    do it in functional react. So to be able to set the query and call the server with its correct value, we will keep the query in a ref. This allows us to change the query without having to wait for the next rerender. So for the calling code to
    always have the current query, we need to give it a getter.
    This solution was disscussed with @tomek.
    
    Test Plan: Checked that the context state changes as expected when supplied functions are called
    
    Reviewers: kamil, kuba, michal
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8293

--- web/search/message-search-state-provider.react.js
+++ web/search/message-search-state-provider.react.js
@@ -3,10 +3,22 @@
 import invariant from 'invariant';
 import * as React from 'react';
 
+import { useSearchMessages } from 'lib/shared/search-utils.js';
+import { messageTypes } from 'lib/types/message-types-enum.js';
+import type { RawMessageInfo } from 'lib/types/message-types.js';
+
 type MessageSearchState = {
   +getQuery: (threadID: string) => string,
   +setQuery: (query: string, threadID: string) => void,
   +clearQuery: (threadID: string) => void,
+  +getSearchResults: (threadID: string) => $ReadOnlyArray<RawMessageInfo>,
+  +appendSearchResult: (
+    $ReadOnlyArray<RawMessageInfo>,
+    threadID: string,
+  ) => void,
+  +getEndReached: (threadID: string) => boolean,
+  +setEndReached: (threadID: string) => void,
+  +searchMessages: (threadID: string) => void,
 };
 
 const MessageSearchContext: React.Context<?MessageSearchState> =
@@ -17,28 +29,121 @@ type Props = {
 };
 
 function MessageSearchStateProvider(props: Props): React.Node {
-  const [queries, setQueries] = React.useState<{
+  const queries = React.useRef<{
     [threadID: string]: string,
   }>({});
 
-  const setQuery = React.useCallback(
-    (query: string, threadID: string) =>
-      setQueries(prevQueries => ({ ...prevQueries, [threadID]: query })),
+  const [results, setResults] = React.useState<{
+    [threadID: string]: $ReadOnlyArray<RawMessageInfo>,
+  }>({});
+
+  const endsReached = React.useRef(new Set());
+
+  const lastIDs = React.useRef<{
+    [threadID: string]: string,
+  }>({});
+
+  const setEndReached = React.useCallback((threadID: string) => {
+    endsReached.current.add(threadID);
+  }, []);
+
+  const removeEndReached = React.useCallback(
+    (threadID: string) => endsReached.current.delete(threadID),
     [],
   );
 
-  const clearQuery = React.useCallback(
-    (threadID: string) =>
-      setQueries(prevQueries => {
-        const { [threadID]: deleted, ...newState } = prevQueries;
-        return newState;
-      }),
+  const getEndReached = React.useCallback(
+    (threadID: string) => endsReached.current.has(threadID),
+    [endsReached],
+  );
+
+  const appendResult = React.useCallback(
+    (result: $ReadOnlyArray<RawMessageInfo>, threadID: string) => {
+      const lastMessageID = oldestMessageID(result);
+      if (lastMessageID) {
+        lastIDs.current[threadID] = lastMessageID;
+      }
+      setResults(prevResults => {
+        const prevThreadResults = prevResults[threadID] ?? [];
+        const newThreadResults = [...prevThreadResults, ...result];
+        return { ...prevResults, [threadID]: newThreadResults };
+      });
+    },
     [],
+  );
+
+  const clearResults = React.useCallback(
+    (threadID: string) => {
+      loading.current = false;
+      delete lastIDs.current[threadID];
+      removeEndReached(threadID);
+      setResults(prevResults => {
+        const { [threadID]: deleted, ...newState } = prevResults;
+        return newState;
+      });
+    },
+    [removeEndReached],
+  );
+
+  const getResults = React.useCallback(
+    (threadID: string) => results[threadID] ?? [],
+    [results],
   );
 
   const getQuery = React.useCallback(
-    (threadID: string) => queries[threadID] ?? '',
-    [queries],
+    (threadID: string) => queries.current[threadID] ?? '',
+    [],
+  );
+
+  const setQuery = React.useCallback(
+    (query: string, threadID: string) => {
+      clearResults(threadID);
+      queries.current = { ...queries.current, [threadID]: query };
+    },
+    [clearResults],
+  );
+
+  const clearQuery = React.useCallback(
+    (threadID: string) => {
+      clearResults(threadID);
+      delete queries.current[threadID];
+    },
+    [clearResults],
+  );
+
+  const searchMessagesCall = useSearchMessages();
+
+  const loading = React.useRef(false);
+
+  const appendResults = React.useCallback(
+    (
+      newMessages: $ReadOnlyArray<RawMessageInfo>,
+      end: boolean,
+      threadID: string,
+    ) => {
+      appendResult(newMessages, threadID);
+      if (end) {
+        setEndReached(threadID);
+      }
+      loading.current = false;
+    },
+    [appendResult, setEndReached],
+  );
+
+  const searchMessages = React.useCallback(
+    (threadID: string) => {
+      if (loading.current || endsReached.current.has(threadID)) {
+        return;
+      }
+      loading.current = true;
+      searchMessagesCall(
+        queries.current[threadID],
+        threadID,
+        appendResults,
+        lastIDs.current[threadID],
+      );
+    },
+    [appendResults, endsReached, searchMessagesCall],
   );
 
   const state = React.useMemo(
@@ -46,8 +151,22 @@ function MessageSearchStateProvider(props: Props): React.Node {
       getQuery,
       setQuery,
       clearQuery,
+      getSearchResults: getResults,
+      appendSearchResult: appendResult,
+      getEndReached,
+      setEndReached,
+      searchMessages,
     }),
-    [getQuery, setQuery, clearQuery],
+    [
+      getQuery,
+      setQuery,
+      clearQuery,
+      getResults,
+      appendResult,
+      getEndReached,
+      setEndReached,
+      searchMessages,
+    ],
   );
 
   return (
@@ -55,6 +174,18 @@ function MessageSearchStateProvider(props: Props): React.Node {
       {props.children}
     </MessageSearchContext.Provider>
   );
+}
+
+function oldestMessageID(data: $ReadOnlyArray<RawMessageInfo>) {
+  if (!data) {
+    return undefined;
+  }
+  for (let i = data.length - 1; i >= 0; i--) {
+    if (data[i].type === messageTypes.TEXT) {
+      return data[i].id;
+    }
+  }
+  return undefined;
 }
 
 function useMessageSearchContext(): MessageSearchState {
