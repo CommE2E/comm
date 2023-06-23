9be393b234f2a69746bd72f9f8334dc909a8d34f
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-06-23 13:11:08 -0700

    [lib/native] introduce getViewerAlreadySelectedMessageReactions
    Summary:
    This diff is the second part to highlight the already selected emojis to indicate on the emoji keyboard what emojis the user has already selected. This diff handles the case when `selectMultipleEmojis` is equal to `true`.
    
    To handle this case we need to iterate through the `ReactionInfo` map and return an array of emojis that the user has already selected. I introduced a separate helper function called `getViewerAlreadySelectedMessageReactions` since `MultimediaMessageTooltipButton`, `TextMessageTooltipButton` and `RobotextMessageTooltipButton` will all need to do this similar functionality.
    
    Here is a link to an example to the docs that helped me with this implementation: https://thewidlarzgroup.github.io/rn-emoji-keyboard/docs/documentation/Examples/selected_emojis
    
    Linear Task: https://linear.app/comm/issue/ENG-3794/mobile-emoji-keyboard-highlight-emojis-that-a-user-has-already
    
    Depends on D8284
    
    Test Plan:
    Please watch the demo video below:
    
    {F601071}
    
    Reviewers: atul, kamil
    
    Reviewed By: kamil
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8288

--- native/chat/text-message-tooltip-button.react.js
+++ native/chat/text-message-tooltip-button.react.js
@@ -4,7 +4,10 @@ import * as React from 'react';
 import Animated from 'react-native-reanimated';
 
 import { localIDPrefix } from 'lib/shared/message-utils.js';
-import { useCanCreateReactionFromMessage } from 'lib/shared/reaction-utils.js';
+import {
+  getViewerAlreadySelectedMessageReactions,
+  useCanCreateReactionFromMessage,
+} from 'lib/shared/reaction-utils.js';
 
 import { TooltipInlineEngagement } from './inline-engagement.react.js';
 import { InnerTextMessage } from './inner-text-message.react.js';
@@ -149,7 +152,10 @@ function TextMessageTooltipButton(props: Props): React.Node {
     [sendReaction, dismissTooltip],
   );
 
-  const alreadySelectedEmojis = React.useMemo(() => [], []);
+  const alreadySelectedEmojis = React.useMemo(
+    () => getViewerAlreadySelectedMessageReactions(item.reactions),
+    [item.reactions],
+  );
 
   return (
     <MessageListContextProvider threadInfo={threadInfo}>
