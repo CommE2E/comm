ec1778864b4f632b91f23e1d83cf489f34e322fc
Tomasz Palys <tomasz.palys@swmansion.com>
2023-06-27 14:28:12 +0200

    [web] Create manage links modal
    Summary:
    A modal where links can be managed
    https://www.figma.com/file/a1nkbWgbgjRlrOY9LVurTz/Comm-%2F-Desktop-app?node-id=11400%3A288445&mode=dev
    https://www.figma.com/file/a1nkbWgbgjRlrOY9LVurTz/Comm-%2F-Desktop-app?node-id=11341%3A282899&mode=dev
    
    Depends on D8412
    
    Test Plan: Click manage links button and check if the modal is correct, both when a public link is present and when a community doesn't have a link.
    
    Reviewers: bartek, kamil, inka
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D8413

---
+++ web/invite-links/manage-invite-links-modal.react.js
@@ -0,0 +1,79 @@
+// @flow
+
+import * as React from 'react';
+
+import { useModalContext } from 'lib/components/modal-provider.react.js';
+import { primaryInviteLinksSelector } from 'lib/selectors/invite-links-selectors.js';
+import { threadInfoSelector } from 'lib/selectors/thread-selectors.js';
+import type { InviteLink } from 'lib/types/link-types.js';
+import { useResolvedThreadInfo } from 'lib/utils/entity-helpers.js';
+
+import CopyInviteLinkButton from './copy-invite-link-button.react.js';
+import css from './manage-invite-links-modal.css';
+import Button from '../components/button.react.js';
+import Modal from '../modals/modal.react.js';
+import { useSelector } from '../redux/redux-utils.js';
+
+type Props = {
+  +communityID: string,
+};
+
+const buttonColor = {
+  color: 'var(--purple-link)',
+};
+
+function ManageInviteLinksModal(props: Props): React.Node {
+  const { communityID } = props;
+  const inviteLink: ?InviteLink = useSelector(primaryInviteLinksSelector)[
+    communityID
+  ];
+  const community = useSelector(
+    state => threadInfoSelector(state)[communityID],
+  );
+  const resolvedThreadInfo = useResolvedThreadInfo(community);
+  const { popModal } = useModalContext();
+
+  let enableLinkButton = null;
+  let publicLinkSection = null;
+  if (inviteLink) {
+    publicLinkSection = (
+      <div>
+        <CopyInviteLinkButton inviteLink={inviteLink} />
+        <div className={css.description}>
+          {'Public links allow unlimited uses and never expire. '}
+          <Button
+            variant="text"
+            buttonColor={buttonColor}
+            className={css.inlineButton}
+          >
+            Edit public link
+          </Button>
+        </div>
+      </div>
+    );
+  } else {
+    enableLinkButton = (
+      <Button variant="text" buttonColor={buttonColor}>
+        Enable
+      </Button>
+    );
+  }
+
+  return (
+    <Modal
+      name={`Invite people to ${resolvedThreadInfo.uiName}`}
+      onClose={popModal}
+      size="large"
+    >
+      <div className={css.container}>
+        <div className={css.sectionHeaderRow}>
+          <div className={css.sectionHeaderText}>Public link</div>
+          {enableLinkButton}
+        </div>
+        {publicLinkSection}
+      </div>
+    </Modal>
+  );
+}
+
+export default ManageInviteLinksModal;
