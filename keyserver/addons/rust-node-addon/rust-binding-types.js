ea1ac74c0ee1b12ce6c8512735352927e36b71e8
Jonathan Ringer <jonathan.ringer@comm.app>
2023-06-27 06:46:23 -0700

    [Keyserver] Remove legacy tunnelbroker types
    Summary:
    Small cleanup from when tunnelbroker was going to be a
    gRPC based service for clients.
    
    Test Plan: N/A
    
    Reviewers: ashoat, varun, bartek
    
    Reviewed By: bartek
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D8365

--- keyserver/addons/rust-node-addon/rust-binding-types.js
+++ keyserver/addons/rust-node-addon/rust-binding-types.js
@@ -2,19 +2,6 @@
 
 import type { SignedIdentityKeysBlob } from 'lib/types/crypto-types.js';
 
-type tunnelbrokerOnReceiveCallback = (
-  err: Error | null,
-  payload: string,
-) => mixed;
-
-declare class TunnelbrokerClientClass {
-  constructor(
-    deviceId: string,
-    onReceiveCallback: tunnelbrokerOnReceiveCallback,
-  ): TunnelbrokerClientClass;
-  publish(toDeviceId: string, payload: string): Promise<void>;
-}
-
 type RustNativeBindingAPI = {
   +registerUser: (
     username: string,
@@ -26,7 +13,6 @@ type RustNativeBindingAPI = {
     message: string,
     signature: string,
   ) => Promise<void>,
-  +TunnelbrokerClient: Class<TunnelbrokerClientClass>,
 };
 
 export type { RustNativeBindingAPI };
