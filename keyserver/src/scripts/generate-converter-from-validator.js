30aacca8a4a01a61410f6291780a96e24775f65c
Michal Gniadek <michal.gniadek@swmansion.com>
2023-06-27 17:56:35 +0200

    [lib] Generate converters from validators
    Summary:
    https://linear.app/comm/issue/ENG-3995/create-functions-for-migrating-the-client-stores
    
    We need to migrate client db stores, and redux persist. We can't use validators themselves because they may be changed in the future. This diffs adds a script that takes a validator and generated a function (as a string containing js code) that converts the type described by this validator. This way the migration code will remain static even if the user updates the app. The generated function are kept in `lib/_generated/migration-utils.js`. The only manual change (other than pasting the generating functions) in this file is adding the required imports.
    
    Example of the generated code for the linkStore:
    ```
    export function convertInviteLinksStoreToNewIDSchema(
      input: InviteLinksStore,
    ): InviteLinksStore {
      return {
        ...input,
        links: Object.fromEntries(
          entries(input.links).map(([key, value]) => [
            '256|' + key,
            {
              ...value,
              primaryLink:
                value.primaryLink !== null && value.primaryLink !== undefined
                  ? {
                      ...value.primaryLink,
                      role: '256|' + value.primaryLink.role,
                      communityID: '256|' + value.primaryLink.communityID,
                    }
                  : value.primaryLink,
            },
          ]),
        ),
      };
    }
    ```
    
    Test Plan: Tested with the rest of the diffs, by running schema id migrations on clients using the generated functions.
    
    Reviewers: kamil, tomek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D8355

---
+++ keyserver/src/scripts/generate-converter-from-validator.js
@@ -0,0 +1,234 @@
+// @flow
+
+import t, { type TInterface, type TType } from 'tcomb';
+
+import {
+  nativeMediaSelectionValidator,
+  mediaValidator,
+} from 'lib/types/media-types.js';
+import { threadPermissionInfoValidator } from 'lib/types/thread-permission-types.js';
+import { rawThreadInfoValidator } from 'lib/types/thread-types.js';
+import { keyserverPrefixID, tID } from 'lib/utils/validation-utils.js';
+
+import { main } from './utils.js';
+
+function getDiscriminatorFieldForUnionValidator(validator: TType<mixed>) {
+  if (validator === threadPermissionInfoValidator) {
+    return 'value';
+  }
+  if (validator === nativeMediaSelectionValidator) {
+    return 'step';
+  }
+  return 'type';
+}
+
+function flattenInnerUnionValidators<T>(
+  innerValidators: $ReadOnlyArray<TType<T>>,
+): TInterface<{ +[string]: mixed }>[] {
+  let result = [];
+  for (const innerValidator of innerValidators) {
+    if (innerValidator.meta.kind === 'interface') {
+      // In flow, union refinement only works if every variant has a key
+      // that is a literal. In this case we don't get a refinement of
+      // `innerValidator` because we are checking value of the inner
+      // `meta` object.
+      const recastValidator: TInterface<{ +[string]: mixed }> =
+        (innerValidator: any);
+      result.push(recastValidator);
+    } else if (innerValidator.meta.kind === 'union') {
+      result = [
+        ...result,
+        ...flattenInnerUnionValidators(innerValidator.meta.types),
+      ];
+    } else if ([t.String, t.Number, t.Boolean].includes(innerValidator)) {
+      // We don't need to handle literal types because they can't be
+      // converted
+    } else {
+      throw new Error(
+        `Validator not supported in union: ${innerValidator.displayName}`,
+      );
+    }
+  }
+  return result;
+}
+
+// MediaValidator is special cased because of flow issues
+function getConverterForMediaValidator(inputName: string) {
+  return `(${inputName}.type === 'photo'
+  ? { ...${inputName}, id: '256|' + ${inputName}.id }
+  : ${inputName}.type === 'video'
+  ? {
+      ...${inputName},
+      id: '256|' + ${inputName}.id,
+      thumbnailID: '256|' + ${inputName}.thumbnailID,
+    }
+  : ${inputName}.type === 'encrypted_photo'
+  ? ({ ...${inputName}, id: '256|' + ${inputName}.id }: any)
+  : ${inputName}.type === 'encrypted_video'
+  ? ({
+      ...${inputName},
+      id: '256|' + ${inputName}.id,
+      thumbnailID: '256|' + ${inputName}.thumbnailID,
+    }: any)
+  : ${inputName})`;
+}
+
+// `null` is returned if there is no conversion needed in T or any
+// of it's inner types
+function generateConverterFromValidator<T, C>(
+  validator: TType<T>,
+  inputName: string,
+  validatorToBeConverted: TType<C>,
+  conversionExpressionString: (inputName: string) => string,
+): ?string {
+  if (validator === validatorToBeConverted) {
+    return `(${conversionExpressionString(inputName)})`;
+  } else if (validator === mediaValidator) {
+    return getConverterForMediaValidator(inputName);
+  }
+
+  if (validator.meta.kind === 'maybe') {
+    const inner = generateConverterFromValidator(
+      validator.meta.type,
+      inputName,
+      validatorToBeConverted,
+      conversionExpressionString,
+    );
+    if (!inner) {
+      return null;
+    }
+    return `((${inputName} !== null && ${inputName} !== undefined) ? (${inner}) : (${inputName}))`;
+  }
+  if (validator.meta.kind === 'subtype') {
+    return generateConverterFromValidator(
+      validator.meta.type,
+      inputName,
+      validatorToBeConverted,
+      conversionExpressionString,
+    );
+  }
+  if (validator.meta.kind === 'interface') {
+    // In flow, union refinement only works if every variant has a key
+    // that is a literal. In this case we don't get a refinement of `validator`
+    // because we are checking value of the inner `meta` object.
+    const recastValidator: TInterface<{ +[string]: mixed }> = (validator: any);
+    const fieldConverters = [];
+    for (const key in recastValidator.meta.props) {
+      const inner = generateConverterFromValidator(
+        recastValidator.meta.props[key],
+        `${inputName}.${key}`,
+        validatorToBeConverted,
+        conversionExpressionString,
+      );
+      if (inner) {
+        fieldConverters.push(`${key}:${inner}`);
+      }
+    }
+    if (fieldConverters.length === 0) {
+      return null;
+    }
+
+    return `({...${inputName}, ${fieldConverters.join(',')}})`;
+  }
+  if (validator.meta.kind === 'union') {
+    const innerValidators = flattenInnerUnionValidators(validator.meta.types);
+
+    const variantConverters = [];
+    for (const innerValidator of innerValidators) {
+      const discriminatorField =
+        getDiscriminatorFieldForUnionValidator(validator);
+      const discriminatorValidator =
+        innerValidator.meta.props[discriminatorField];
+
+      if (!discriminatorValidator) {
+        throw new Error(
+          'Union should have a discriminator ' + validator.displayName,
+        );
+      }
+
+      const discriminatorValue = discriminatorValidator.meta.name;
+
+      const inner = generateConverterFromValidator(
+        innerValidator,
+        inputName,
+        validatorToBeConverted,
+        conversionExpressionString,
+      );
+
+      if (inner) {
+        variantConverters.push(
+          `(${inputName}.${discriminatorField} === ${discriminatorValue}) ? (${inner})`,
+        );
+      }
+    }
+    if (variantConverters.length === 0) {
+      return null;
+    }
+    variantConverters.push(`(${inputName})`);
+    return `(${variantConverters.join(':')})`;
+  }
+  if (validator.meta.kind === 'list') {
+    const inner = generateConverterFromValidator(
+      validator.meta.type,
+      'elem',
+      validatorToBeConverted,
+      conversionExpressionString,
+    );
+    if (!inner) {
+      return inputName;
+    }
+    return `(${inputName}.map(elem => ${inner}))`;
+  }
+  if (validator.meta.kind === 'dict') {
+    const domainValidator = validator.meta.domain;
+    const codomainValidator = validator.meta.codomain;
+
+    let domainConverter = null;
+    if (domainValidator === validatorToBeConverted) {
+      domainConverter = conversionExpressionString('key');
+    }
+
+    let codomainConverter = generateConverterFromValidator(
+      codomainValidator,
+      'value',
+      validatorToBeConverted,
+      conversionExpressionString,
+    );
+
+    if (!domainConverter && !codomainConverter) {
+      return null;
+    }
+
+    domainConverter = domainConverter ?? 'key';
+    codomainConverter = codomainConverter ?? 'value';
+
+    return `(Object.fromEntries(
+          entries(${inputName}).map(
+            ([key, value]) => [${domainConverter}, ${codomainConverter}]
+          )
+        ))`;
+  }
+
+  return null;
+}
+
+// Input arguments:
+const validator = rawThreadInfoValidator;
+const typeName = 'RawThreadInfo';
+const validatorToBeConverted = tID;
+const conversionExpressionString = inputName =>
+  `'${keyserverPrefixID}|' + ${inputName}`;
+main([
+  async () => {
+    console.log(
+      `export function convert${typeName}ToNewIDSchema(input: ${typeName}): ${typeName} { return`,
+      generateConverterFromValidator(
+        validator,
+        'input',
+        validatorToBeConverted,
+        conversionExpressionString,
+      ) ?? 'input',
+      ';}',
+    );
+  },
+]);
