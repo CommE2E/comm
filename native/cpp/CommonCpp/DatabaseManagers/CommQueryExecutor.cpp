d0cf03dd4343096d59d24c70b2221069ec341882
xsanm <kkurowski.kk@gmail.com>
2023-06-29 17:20:15 +0200

    [web] implement test c++ class
    Summary:
    This class will be removed, adding this here to iterate over compiling code to WASM and share a general idea.
     I will start adding proper implementation after some changes to the native code.
    
    Depends on D8375
    
    Test Plan: N/A
    
    Reviewers: jon, tomek, michal
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D8389

---
+++ native/cpp/CommonCpp/DatabaseManagers/CommQueryExecutor.cpp
@@ -0,0 +1,75 @@
+#include "CommQueryExecutor.h"
+#include "sqlite_orm.h"
+
+#include <sqlite3.h>
+#include <cstdio>
+#include <iostream>
+#include <string>
+
+#ifdef EMSCRIPTEN
+#include <emscripten/bind.h>
+#endif
+
+namespace comm {
+
+using namespace sqlite_orm;
+
+std::string DB_PATH = "test.sqlite";
+struct Test {
+  std::string id;
+  std::string data;
+};
+
+std::string CommQueryExecutor::testDBOperation() {
+  sqlite3 *db;
+  sqlite3_open(DB_PATH.c_str(), &db);
+
+  std::string createTableQuery =
+      "CREATE TABLE IF NOT EXISTS test ("
+      "	 id TEXT UNIQUE PRIMARY KEY NOT NULL,"
+      "	 data TEXT NOT NULL"
+      ");";
+
+  char *error = nullptr;
+  sqlite3_exec(db, createTableQuery.c_str(), nullptr, nullptr, &error);
+
+  if (error != nullptr) {
+    std::ostringstream stringStream;
+    stringStream << "Error creating test table: " << error;
+    sqlite3_free(error);
+    return stringStream.str();
+  }
+
+  sqlite3_close(db);
+
+  auto storage = make_storage(
+      DB_PATH.c_str(),
+      make_table(
+          "test",
+          make_column("id", &Test::id, primary_key()),
+          make_column("data", &Test::data)));
+
+  Test test1{"a", "data"};
+  Test test2{"b", "data"};
+  storage.replace(test1);
+  storage.replace(test2);
+
+  auto tests = storage.get_all<Test>();
+  if (tests.size() == 2) {
+    std::remove(DB_PATH.c_str());
+    return "sqlite3 and sqlite_orm works on web";
+  } else {
+    return "there were some problems...";
+  }
+}
+
+} // namespace comm
+
+#ifdef EMSCRIPTEN
+EMSCRIPTEN_BINDINGS(CommQueryExecutor) {
+  emscripten::class_<comm::CommQueryExecutor>("CommQueryExecutor")
+      .constructor<>()
+      .class_function(
+          "testDBOperation", &comm::CommQueryExecutor::testDBOperation);
+}
+#endif
