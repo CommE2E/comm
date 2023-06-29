03587cc413aa212636439a713fee93727f36d3aa
Tomasz Palys <tomasz.palys@swmansion.com>
2023-06-29 16:23:09 +0200

    [web] Split modal into multiple components
    Summary:
    Manage links modal is complicated so it makes sense to split it into multiple components. Following diffs will introduce new components that are conditionally displayed.
    
    Depends on D8413
    
    Test Plan: This is only a refactoring. Tested if the modal still works.
    
    Reviewers: kamil, inka, bartek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D8414

---
+++ web/invite-links/manage/existing-link-content.react.js
@@ -0,0 +1,42 @@
+// @flow
+
+import * as React from 'react';
+
+import type { InviteLink } from 'lib/types/link-types.js';
+
+import css from './manage-invite-links-modal.css';
+import Button from '../../components/button.react.js';
+import CopyInviteLinkButton from '../copy-invite-link-button.react.js';
+
+const buttonColor = {
+  color: 'var(--purple-link)',
+};
+
+type Props = {
+  +inviteLink: InviteLink,
+};
+function ExistingLinkContent(props: Props): React.Node {
+  const { inviteLink } = props;
+  return (
+    <>
+      <div className={css.sectionHeaderRow}>
+        <div className={css.sectionHeaderText}>Public link</div>
+      </div>
+      <div>
+        <CopyInviteLinkButton inviteLink={inviteLink} />
+        <div className={css.description}>
+          {'Public links allow unlimited uses and never expire. '}
+          <Button
+            variant="text"
+            buttonColor={buttonColor}
+            className={css.inlineButton}
+          >
+            Edit public link
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ExistingLinkContent;
