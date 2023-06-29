2785749a1534ca4d8f939870e7b84830fbb1a837
Jonathan Ringer <jonathan.ringer@comm.app>
2023-06-29 09:06:03 -0700

    [Keyserver] Apply DB Migrations
    Summary:
    Ensure that the user_credentials are available, and allow
    for the metadata table to hold a 512 character long access token.
    
    Depends on D8399
    
    Test Plan:
    ```
    yarn dev
    ```
    
    Reviewers: ashoat
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D8400

---
+++ keyserver/src/user/checks.js
@@ -0,0 +1,31 @@
+// @flow
+
+import { getCommConfig } from 'lib/utils/comm-config.js';
+type UserCredentials = { +username: string, +password: string };
+
+async function ensureUserCredentials() {
+  const userCredentials = await getCommConfig<UserCredentials>({
+    folder: 'secrets',
+    name: 'user_credentials',
+  });
+
+  if (!userCredentials) {
+    console.warn(
+      'User credentials for the keyserver owner must be specified. They can be ' +
+        'specified either by setting the ' +
+        '`COMM_JSONCONFIG_secrets_user_credentials` environmental variable, or by ' +
+        'setting a file at keyserver/secrets/user_credentials.json. The contents ' +
+        'should be a JSON blob that looks like this:\n' +
+        '{\n' +
+        '  "username": <user>,\n' +
+        '  "password": <pass>\n' +
+        '}\n',
+    );
+
+    // Since we don't want to apply the migration until there are credentials;
+    // throw the error and force keyserver to be configured next restart
+    throw new Error('missing_keyserver_owner_credentials');
+  }
+}
+
+export { ensureUserCredentials };
