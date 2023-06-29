2613a7c67de321565065ba6d077c3ef27efb89f0
Tomasz Palys <tomasz.palys@swmansion.com>
2023-06-29 17:20:18 +0200

    [web] Introduce edit link modal
    Summary:
    Introduce a stage of manage link modal where a link can be modified / created.
    
    https://www.figma.com/file/a1nkbWgbgjRlrOY9LVurTz/Comm-%2F-Desktop-app?node-id=11341%3A281468&mode=dev
    
    Depends on D8414
    
    Test Plan: Check if edit link and enable link buttons point to edit link modal. Check if the modal is displayed correctly.
    
    Reviewers: kamil, inka, bartek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D8415

---
+++ web/invite-links/manage/edit-link-modal.react.js
@@ -0,0 +1,65 @@
+// @flow
+
+import classnames from 'classnames';
+import * as React from 'react';
+
+import { useModalContext } from 'lib/components/modal-provider.react.js';
+import { inviteLinkUrl } from 'lib/facts/links.js';
+import type { InviteLink } from 'lib/types/link-types.js';
+
+import css from './manage-invite-links-modal.css';
+import Button from '../../components/button.react.js';
+import Input from '../../modals/input.react.js';
+import Modal from '../../modals/modal.react.js';
+
+type Props = {
+  +inviteLink: ?InviteLink,
+  +enterViewMode: () => mixed,
+};
+
+function EditLinkModal(props: Props): React.Node {
+  const { inviteLink, enterViewMode } = props;
+  const [name, setName] = React.useState(
+    inviteLink?.name ?? Math.random().toString(36).slice(-9),
+  );
+  const { popModal } = useModalContext();
+
+  const onChangeName = React.useCallback(
+    (event: SyntheticEvent<HTMLInputElement>) => {
+      setName(event.currentTarget.value);
+    },
+    [],
+  );
+
+  return (
+    <Modal name="Public link" onClose={popModal} size="large">
+      <div className={classnames(css.container, css.editLinkContainer)}>
+        <div className={css.editLinkDescription}>
+          Invite links make it easy for your friends to join your community.
+          Anybody who knows your community’s invite link will be able to join
+          it.
+          <br />
+          <br />
+          Note that if you change your public link’s URL, other communities will
+          be able to claim the old URL.
+        </div>
+        <hr className={css.separator} />
+        <div className={css.linkSection}>
+          Invite URL
+          <div className={css.linkRow}>
+            {inviteLinkUrl('')}
+            <Input type="text" value={name} onChange={onChangeName} />
+          </div>
+        </div>
+        <div className={css.buttonRow}>
+          <Button variant="outline" onClick={enterViewMode}>
+            Back
+          </Button>
+          <Button variant="filled">Save & enable public link</Button>
+        </div>
+      </div>
+    </Modal>
+  );
+}
+
+export default EditLinkModal;
