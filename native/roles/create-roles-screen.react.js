0d5c2d49e7fc99cb63d2d71942aaf4e8ae7b31c5
Rohan Khanderia <rohan.khanderia@gmail.com>
2023-06-29 16:56:38 -0400

    [native] Populate the create role screen
    Summary:
    This diff adds the necessary content to the create roles screen, i.e. a role name input and a panel of selectable permissions for the given role. The `onSave` functionality will be added in a later
    diff, as well as the removal of the `eslint-disable-next-line no-unused-vars`.
    
    Depends on D8379
    
    [[ https://linear.app/comm/issue/ENG-4172/populate-the-create-role-screen  | ENG-4172 ]]
    
    Test Plan:
    Navigated to the screen and typed in different role names, selected several permissions, tried clearing all permissions at once, and tried deselected some permissions individually.
    
    {F613867}
    
    Reviewers: atul, ginsu, ashoat
    
    Reviewed By: ashoat
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8391

--- native/roles/create-roles-screen.react.js
+++ native/roles/create-roles-screen.react.js
@@ -1,11 +1,22 @@
 // @flow
 
 import * as React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 
+import {
+  type UserSurfacedPermissionOption,
+  type UserSurfacedPermission,
+} from 'lib/types/thread-permission-types.js';
 import type { ThreadInfo } from 'lib/types/thread-types.js';
+import { useFilterPermissionOptionsByThreadType } from 'lib/utils/role-utils.js';
 
 import type { RolesNavigationProp } from './roles-navigator.react.js';
+import EnumSettingsOption from '../components/enum-settings-option.react.js';
+import SWMansionIcon from '../components/swmansion-icon.react.js';
+import TextInput from '../components/text-input.react.js';
 import type { NavigationRoute } from '../navigation/route-names.js';
+import { useStyles } from '../themes/colors.js';
 
 export type CreateRolesScreenParams = {
   +threadInfo: ThreadInfo,
@@ -17,9 +28,168 @@ type CreateRolesScreenProps = {
   +route: NavigationRoute<'CreateRolesScreen'>,
 };
 
-// eslint-disable-next-line no-unused-vars
 function CreateRolesScreen(props: CreateRolesScreenProps): React.Node {
-  return <></>;
+  // eslint-disable-next-line no-unused-vars
+  const { threadInfo, action } = props.route.params;
+
+  const [customRoleName, setCustomRoleName] =
+    React.useState<string>('New role');
+  const [selectedPermissions, setSelectedPermissions] = React.useState<
+    $ReadOnlySet<UserSurfacedPermission>,
+  >(new Set());
+
+  const styles = useStyles(unboundStyles);
+
+  const onClearPermissions = React.useCallback(() => {
+    setSelectedPermissions(new Set());
+  }, []);
+
+  const isSelectedPermissionsEmpty = selectedPermissions.size === 0;
+  const clearPermissionsText = React.useMemo(() => {
+    const textStyle = isSelectedPermissionsEmpty
+      ? styles.clearPermissionsTextDisabled
+      : styles.clearPermissionsText;
+
+    return (
+      <TouchableOpacity
+        onPress={onClearPermissions}
+        disabled={isSelectedPermissionsEmpty}
+      >
+        <Text style={textStyle}>Clear permissions</Text>
+      </TouchableOpacity>
+    );
+  }, [
+    isSelectedPermissionsEmpty,
+    onClearPermissions,
+    styles.clearPermissionsText,
+    styles.clearPermissionsTextDisabled,
+  ]);
+
+  const isUserSurfacedPermissionSelected = React.useCallback(
+    (option: UserSurfacedPermissionOption) =>
+      selectedPermissions.has(option.userSurfacedPermission),
+    [selectedPermissions],
+  );
+
+  const onEnumValuePress = React.useCallback(
+    (option: UserSurfacedPermissionOption) =>
+      setSelectedPermissions(currentPermissions => {
+        if (currentPermissions.has(option.userSurfacedPermission)) {
+          const newPermissions = new Set(currentPermissions);
+          newPermissions.delete(option.userSurfacedPermission);
+          return newPermissions;
+        } else {
+          return new Set([
+            ...currentPermissions,
+            option.userSurfacedPermission,
+          ]);
+        }
+      }),
+    [],
+  );
+
+  const filteredUserSurfacedPermissionOptions =
+    useFilterPermissionOptionsByThreadType(threadInfo.type);
+
+  const permissionsList = React.useMemo(
+    () =>
+      [...filteredUserSurfacedPermissionOptions].map(permission => (
+        <EnumSettingsOption
+          key={permission.title}
+          name={permission.title}
+          description={permission.description}
+          enumValue={isUserSurfacedPermissionSelected(permission)}
+          onEnumValuePress={() => onEnumValuePress(permission)}
+        />
+      )),
+    [
+      isUserSurfacedPermissionSelected,
+      filteredUserSurfacedPermissionOptions,
+      onEnumValuePress,
+    ],
+  );
+
+  const onChangeRoleNameInput = React.useCallback((roleName: string) => {
+    setCustomRoleName(roleName);
+  }, []);
+
+  return (
+    <View>
+      <View style={styles.roleNameContainer}>
+        <Text style={styles.roleNameText}>ROLE NAME</Text>
+        <View style={styles.roleInput}>
+          <TextInput
+            style={styles.roleInputComponent}
+            value={customRoleName}
+            onChangeText={onChangeRoleNameInput}
+            editable={true}
+          />
+          <SWMansionIcon name="edit-1" size={20} style={styles.pencilIcon} />
+        </View>
+      </View>
+      <View style={styles.permissionsContainer}>
+        <View style={styles.permissionsHeader}>
+          <Text style={styles.permissionsText}>PERMISSIONS</Text>
+          {clearPermissionsText}
+        </View>
+        <ScrollView style={styles.permissionsListContainer}>
+          {permissionsList}
+        </ScrollView>
+      </View>
+    </View>
+  );
 }
+
+const unboundStyles = {
+  roleNameContainer: {
+    marginTop: 30,
+  },
+  roleNameText: {
+    color: 'panelBackgroundLabel',
+    fontSize: 12,
+    marginBottom: 5,
+    marginLeft: 10,
+  },
+  roleInput: {
+    backgroundColor: 'panelForeground',
+    padding: 12,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  roleInputComponent: {
+    color: 'panelForegroundLabel',
+    fontSize: 16,
+  },
+  pencilIcon: {
+    color: 'panelInputSecondaryForeground',
+  },
+  permissionsContainer: {
+    marginTop: 20,
+    paddingBottom: 220,
+  },
+  permissionsHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  permissionsText: {
+    color: 'panelBackgroundLabel',
+    fontSize: 12,
+    marginLeft: 10,
+  },
+  clearPermissionsText: {
+    color: 'purpleLink',
+    fontSize: 12,
+    marginRight: 15,
+  },
+  clearPermissionsTextDisabled: {
+    color: 'disabledButton',
+    fontSize: 12,
+    marginRight: 15,
+  },
+  permissionsListContainer: {
+    backgroundColor: 'panelForeground',
+    marginTop: 10,
+  },
+};
 
 export default CreateRolesScreen;
