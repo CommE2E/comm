1c1c441933e71ba21566fb4442fe728d24170526
Tomasz Palys <tomasz.palys@swmansion.com>
2023-06-30 14:39:43 +0200

    [web] Call update or create invite link endpoint
    Summary:
    Update or create a new link when a button is clicked.
    
    https://www.figma.com/file/a1nkbWgbgjRlrOY9LVurTz/Comm-%2F-Desktop-app?node-id=11400%3A288456&mode=dev
    
    {F621669}
    
    Depends on D8416
    
    Test Plan: Check if creating new links works. Then, check if it is possible to update it.
    
    Reviewers: kamil, inka, bartek
    
    Reviewed By: inka
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D8417

--- web/invite-links/manage/edit-link-modal.react.js
+++ web/invite-links/manage/edit-link-modal.react.js
@@ -5,7 +5,9 @@ import * as React from 'react';
 
 import { useModalContext } from 'lib/components/modal-provider.react.js';
 import { inviteLinkUrl } from 'lib/facts/links.js';
+import { useInviteLinksActions } from 'lib/hooks/invite-links.js';
 import type { InviteLink } from 'lib/types/link-types.js';
+import type { ThreadInfo } from 'lib/types/thread-types.js';
 
 import css from './manage-invite-links-modal.css';
 import Button from '../../components/button.react.js';
@@ -15,21 +17,26 @@ import Modal from '../../modals/modal.react.js';
 type Props = {
   +inviteLink: ?InviteLink,
   +enterViewMode: () => mixed,
+  +community: ThreadInfo,
 };
 
 function EditLinkModal(props: Props): React.Node {
-  const { inviteLink, enterViewMode } = props;
-  const [name, setName] = React.useState(
-    inviteLink?.name ?? Math.random().toString(36).slice(-9),
-  );
+  const { inviteLink, enterViewMode, community } = props;
   const { popModal } = useModalContext();
 
+  const { error, isLoading, name, setName, createOrUpdateInviteLink } =
+    useInviteLinksActions(community.id, inviteLink);
   const onChangeName = React.useCallback(
     (event: SyntheticEvent<HTMLInputElement>) => {
       setName(event.currentTarget.value);
     },
-    [],
+    [setName],
   );
+
+  let errorComponent = null;
+  if (error) {
+    errorComponent = <div className={css.errorContainer}>{error}</div>;
+  }
 
   return (
     <Modal name="Public link" onClose={popModal} size="large">
@@ -52,12 +59,23 @@ function EditLinkModal(props: Props): React.Node {
             {inviteLinkUrl('')}
             <Input type="text" value={name} onChange={onChangeName} />
           </div>
-        </div>
-        <div className={css.buttonRow}>
-          <Button variant="outline" onClick={enterViewMode}>
-            Back
-          </Button>
-          <Button variant="filled">Save & enable public link</Button>
+          {errorComponent}
+          <div
+            className={classnames(css.buttonRow, {
+              [css.buttonRowWithError]: !!error,
+            })}
+          >
+            <Button variant="outline" onClick={enterViewMode}>
+              Back
+            </Button>
+            <Button
+              variant="filled"
+              onClick={createOrUpdateInviteLink}
+              disabled={isLoading}
+            >
+              Save & enable public link
+            </Button>
+          </div>
         </div>
       </div>
     </Modal>
