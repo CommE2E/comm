c3a6f98ee7f65ccd30645c37e8ad1651ab03a4eb
Michal Gniadek <michal.gniadek@swmansion.com>
2023-06-30 13:08:42 +0200

    [keyserver] Validate siweNonceResponder
    Summary:
    https://linear.app/comm/issue/ENG-4085/siwenonceresponsevalidator-doesnt-appear-to-be-used
    There was a validator for the response but it wasn't used. It doesn't contain any ids to be converted, but we should be consistent.
    
    Test Plan:
    Call the endpoint, check if it returns the value correctly
    
    Reviewers: atul
    
    Subscribers:

--- keyserver/src/responders/siwe-nonce-responders.js
+++ keyserver/src/responders/siwe-nonce-responders.js
@@ -7,14 +7,18 @@ import type { SIWENonceResponse } from 'lib/types/siwe-types.js';
 import { tShape } from 'lib/utils/validation-utils.js';
 
 import { createSIWENonceEntry } from '../creators/siwe-nonce-creator.js';
+import type { Viewer } from '../session/viewer.js';
+import { validateOutput } from '../utils/validation-utils.js';
 
 export const siweNonceResponseValidator: TInterface<SIWENonceResponse> =
   tShape<SIWENonceResponse>({ nonce: t.String });
 
-async function siweNonceResponder(): Promise<SIWENonceResponse> {
+async function siweNonceResponder(viewer: Viewer): Promise<SIWENonceResponse> {
   const generatedNonce = generateNonce();
   await createSIWENonceEntry(generatedNonce);
-  return { nonce: generatedNonce };
+  return validateOutput(viewer.platformDetails, siweNonceResponseValidator, {
+    nonce: generatedNonce,
+  });
 }
 
 export { siweNonceResponder };
