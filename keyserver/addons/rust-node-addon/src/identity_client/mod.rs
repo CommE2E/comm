cd422bcc68c044f5cb789626e047f7eb739bdd5f
Jonathan Ringer <jonathan.ringer@comm.app>
2023-06-30 14:28:18 -0700

    [Keyserver/rust] Enable logging of rust addon
    Summary:
    To improve debugging the addon, allow for it to log to console.
    Previously the logging statements would never be outputted.
    
    Depends on D8402
    
    Test Plan:
    ```
    cd keyserver
    RUST_LOG=debug yarn dev
    # assert debugging output from aws_sdk
    ```
    
    Reviewers: varun, bartek
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8403

--- keyserver/addons/rust-node-addon/src/identity_client/mod.rs
+++ keyserver/addons/rust-node-addon/src/identity_client/mod.rs
@@ -17,10 +17,20 @@ use napi::bindgen_prelude::*;
 use serde::{Deserialize, Serialize};
 use std::env::var;
 use tonic::{transport::Channel, Request};
-use tracing::instrument;
+use tracing::{self, info, instrument, warn, Level};
+use tracing_subscriber::EnvFilter;
 
 lazy_static! {
   static ref IDENTITY_SERVICE_CONFIG: IdentityServiceConfig = {
+    let filter = EnvFilter::builder()
+      .with_default_directive(Level::INFO.into())
+      .with_env_var(EnvFilter::DEFAULT_ENV)
+      .from_env_lossy();
+
+    let subscriber = tracing_subscriber::fmt().with_env_filter(filter).finish();
+    tracing::subscriber::set_global_default(subscriber)
+      .expect("Unable to configure tracing");
+
     let config_json_string =
       var("COMM_JSONCONFIG_secrets_identity_service_config");
     match config_json_string {
@@ -39,6 +49,7 @@ struct IdentityServiceConfig {
 
 impl Default for IdentityServiceConfig {
   fn default() -> Self {
+    info!("Using default identity configuration");
     Self {
       identity_socket_addr: "https://[::1]:50054".to_string(),
       identity_auth_token: "test".to_string(),
@@ -47,6 +58,7 @@ impl Default for IdentityServiceConfig {
 }
 
 async fn get_identity_service_channel() -> Result<Channel> {
+  info!("Connecting to identity service");
   Channel::from_static(&IDENTITY_SERVICE_CONFIG.identity_socket_addr)
     .connect()
     .await
@@ -68,4 +80,9 @@ pub struct SignedIdentityKeysBlob {
 pub struct UserLoginInfo {
   pub user_id: String,
   pub access_token: String,
+}
+
+pub fn handle_grpc_error(error: tonic::Status) -> napi::Error {
+  warn!("Received error: {}", error.message());
+  Error::new(Status::GenericFailure, error.message())
 }
