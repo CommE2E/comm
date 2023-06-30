4dfb96244ce598d35c8b8ecef5150e7cf312c576
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2023-06-30 15:07:17 -0400

    [keyserver] Add conditional handling for import.meta in tests and update Babel configuration
    Summary:
    The file `index.js` was modified to conditionally handle the `require` function, based on whether the code is running in a testing environment or not. This is because Jest, our testing framework, does not fully support ES modules or `import.meta` as of our current setup. Therefore, for the testing environment, we fall back to the traditional Node.js `require` function to load our modules.
    
    In the non-testing environment, we rely on `import.meta.url` and dynamic imports using the `createRequire` function. This approach is more flexible and aligns with the future-proof ES module standard.
    
    In addition, I've updated the `babel.config.cjs` file to include the `babel-plugin-transform-import-meta` plugin. This plugin replaces the `import.meta` object with `{ url: __filename }` only in the Jest testing environment. The Babel configuration allows Jest to understand import.meta.url in our code by transpiling it to something Jest knows (__filename).
    
    Test Plan: No Jest errors in the following diff, the rust-node-addon APIs work as expected
    
    Reviewers: ashoat!
    
    Subscribers:

--- keyserver/addons/rust-node-addon/index.js
+++ keyserver/addons/rust-node-addon/index.js
@@ -7,24 +7,40 @@ import type { RustNativeBindingAPI } from './rust-binding-types.js';
 
 const { platform, arch } = process;
 
-const importMetaURL = import.meta.url;
-invariant(importMetaURL, 'import.meta.url should be set');
-const require = createRequire(importMetaURL);
+let nativeBindingRequire;
+
+if (process.env.NODE_ENV === 'test') {
+  // In Jest, ES modules and import.meta are not fully supported, so we fall
+  // back to the traditional Node.js require function to load our modules.
+  nativeBindingRequire = require;
+} else {
+  const importMetaURL = import.meta.url;
+  invariant(importMetaURL, 'import.meta.url should be set');
+  nativeBindingRequire = createRequire(importMetaURL);
+}
 
 async function getRustAPI(): Promise<RustNativeBindingAPI> {
   let nativeBinding = null;
   if (platform === 'darwin' && arch === 'x64') {
     // $FlowFixMe
-    nativeBinding = require('./napi/rust-node-addon.darwin-x64.node');
+    nativeBinding = nativeBindingRequire(
+      './napi/rust-node-addon.darwin-x64.node',
+    );
   } else if (platform === 'darwin' && arch === 'arm64') {
     // $FlowFixMe
-    nativeBinding = require('./napi/rust-node-addon.darwin-arm64.node');
+    nativeBinding = nativeBindingRequire(
+      './napi/rust-node-addon.darwin-arm64.node',
+    );
   } else if (platform === 'linux' && arch === 'x64') {
     // $FlowFixMe
-    nativeBinding = require('./napi/rust-node-addon.linux-x64-gnu.node');
+    nativeBinding = nativeBindingRequire(
+      './napi/rust-node-addon.linux-x64-gnu.node',
+    );
   } else if (platform === 'linux' && arch === 'arm64') {
     // $FlowFixMe
-    nativeBinding = require('./napi/rust-node-addon.linux-arm64-gnu.node');
+    nativeBinding = nativeBindingRequire(
+      './napi/rust-node-addon.linux-arm64-gnu.node',
+    );
   } else {
     throw new Error(`Unsupported OS: ${platform}, architecture: ${arch}`);
   }
