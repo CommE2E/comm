4f8c96c261b9b9eca30e9f18210938ecb447e3d7
Rohan Khanderia <rohan.khanderia@gmail.com>
2023-07-03 11:32:01 -0400

    [native] Use CreateRolesScreenHeaderRightButton to save a new role
    Summary:
    This is the client-side implementation of allowing a user to save their custom role for their community. Since `CreateRolesHeaderRightButton` needs access to both the `roleName` and the
    `rolePermissions`, it made sense to include those in the navigation route params, and I can pull them directly from there. That's why the `React.useEffect` hook updates the params.
    
    This is also useful because once I work on the `edit_role` flow, I can default the `roleName` and `rolePermissions` to the existing name/permissions to 'pre-fill' the info for the client.
    
    Depends on D8420
    
    [[ https://linear.app/comm/issue/ENG-4174/use-createrolesscreenheaderrightbutton-to-save-a-new-role | ENG-4174 ]].
    
    Test Plan:
    Create a new role and click on the 'Create' button. Check my local DB and verify that the role has been created with the selected permissions (for the demo video, you should be able to see all of
    the guarenteed permissions from the previous diff, and just the `manage_pins` and `descendant_manage_pins` from the configurable permissions).
    
    Reviewers: atul, ginsu
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8421

--- native/roles/create-roles-screen.react.js
+++ native/roles/create-roles-screen.react.js
@@ -11,6 +11,7 @@ import {
 import type { ThreadInfo } from 'lib/types/thread-types.js';
 import { getKeysByValue } from 'lib/utils/objects.js';
 
+import CreateRolesHeaderRightButton from './create-roles-header-right-button.react.js';
 import type { RolesNavigationProp } from './roles-navigator.react.js';
 import EnumSettingsOption from '../components/enum-settings-option.react.js';
 import SWMansionIcon from '../components/swmansion-icon.react.js';
@@ -21,6 +22,8 @@ import { useStyles } from '../themes/colors.js';
 export type CreateRolesScreenParams = {
   +threadInfo: ThreadInfo,
   +action: 'create_role' | 'edit_role',
+  +roleName: string,
+  +rolePermissions: string[],
 };
 
 type CreateRolesScreenProps = {
@@ -29,12 +32,18 @@ type CreateRolesScreenProps = {
 };
 
 function CreateRolesScreen(props: CreateRolesScreenProps): React.Node {
-  // eslint-disable-next-line no-unused-vars
-  const { threadInfo, action } = props.route.params;
+  const {
+    threadInfo,
+    action,
+    roleName: defaultRoleName,
+    rolePermissions: defaultRolePermissions,
+  } = props.route.params;
 
   const [customRoleName, setCustomRoleName] =
-    React.useState<string>('New Role');
-  const [selectedPermissions, setSelectedPermissions] = React.useState([]);
+    React.useState<string>(defaultRoleName);
+  const [selectedPermissions, setSelectedPermissions] = React.useState(
+    defaultRolePermissions,
+  );
 
   const styles = useStyles(unboundStyles);
 
@@ -110,6 +119,21 @@ function CreateRolesScreen(props: CreateRolesScreenProps): React.Node {
     [isUserFacingPermissionSelected],
   );
 
+  React.useEffect(() => {
+    props.navigation.setParams({
+      threadInfo,
+      action,
+      roleName: customRoleName,
+      rolePermissions: selectedPermissions,
+    });
+  }, [
+    props.navigation,
+    threadInfo,
+    action,
+    customRoleName,
+    selectedPermissions,
+  ]);
+
   const permissionsList = React.useMemo(
     () =>
       [...configurableCommunityPermissionsOptions].map(permission => (
@@ -127,6 +151,13 @@ function CreateRolesScreen(props: CreateRolesScreenProps): React.Node {
   const onChangeRoleNameInput = React.useCallback((roleName: string) => {
     setCustomRoleName(roleName);
   }, []);
+
+  React.useEffect(() => {
+    props.navigation.setOptions({
+      // eslint-disable-next-line react/display-name
+      headerRight: () => <CreateRolesHeaderRightButton route={props.route} />,
+    });
+  }, [props.navigation, props.route]);
 
   return (
     <View>
