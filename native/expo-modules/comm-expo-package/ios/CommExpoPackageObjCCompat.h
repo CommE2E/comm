8d9869b38d9ab4065d65e73028be9d4c210699c9
marcinwasowicz <marcinkwasowicz@gmail.com>
2023-07-04 16:54:06 +0200

    Refactor AESCryptoModule and CommExpoPackage so that AES stack is available in NSE
    Summary:
    This differential implements Objective C compatibility class for AESCryptoModule that internally calls the same functionality as AESCryptoModule expo module. It also introduces necessary refactors and build config files changes so that we can
    use Compatibility class from any pure Objective C place like NSE.
    
    Test Plan: to be edited in GUI
    
    Reviewers: bartek
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8424

---
+++ native/expo-modules/comm-expo-package/ios/CommExpoPackageObjCCompat.h
@@ -0,0 +1,15 @@
+/*
+ XCode generated Swift compatibility header is available only
+ in the project that implements its header (it is private).
+ Therefore we create public header that includes private Swift
+ compatibility header so that other projects can use generated
+ Objective C interfaces.
+
+ Macro `__has_include` is used since this header is generated
+ during library compilation so it is available only after the
+ library is compiled. This solution is inspired by:
+ https://github.com/expo/expo/blob/895fafd4aa32e64fa5b77e9e88d6d4f8c3638f9c/packages/expo-modules-core/ios/Swift.h#L10-L16
+*/
+#if __has_include("CommExpoPackage-Swift.h")
+#import "CommExpoPackage-Swift.h"
+#endif
