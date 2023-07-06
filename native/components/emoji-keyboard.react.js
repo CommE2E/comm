88f1b76047df56f43df03d84ebffab108b05e248
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-07-06 11:24:12 -0400

    [native] fix emoji keyboard theme colors
    Summary: Small follow up diff to address comments in D8310
    
    Test Plan: Please see the screenshots below to confirm that the color themes for both dark and light mode okay. Also had @ted take a look irl
    
    Subscribers: ashoat, tomek, ted
    
    Differential Revision: https://phab.comm.dev/D8433

--- native/components/emoji-keyboard.react.js
+++ native/components/emoji-keyboard.react.js
@@ -92,35 +92,35 @@ function EmojiKeyboard(props: Props): React.Node {
 
   const theme = React.useMemo(
     () => ({
-      knob: colors.emojiKeyboardKnob,
+      knob: colors.modalKnob,
       container: colors.modalForeground,
       header: colors.modalBackgroundLabel,
       category: {
-        icon: colors.emojiKeyboardCategoryIcon,
-        iconActive: colors.panelForegroundLabel,
-        container: colors.panelInputBackground,
-        containerActive: colors.emojiKeyboardContainerActive,
+        icon: colors.modalButtonLabel,
+        iconActive: colors.modalButtonLabel,
+        container: colors.modalInputBackground,
+        containerActive: colors.modalInputForeground,
       },
       search: {
-        text: colors.panelForegroundLabel,
-        placeholder: colors.panelInputSecondaryForeground,
-        icon: colors.panelInputSecondaryForeground,
-        background: colors.panelInputBackground,
+        text: colors.modalForegroundLabel,
+        placeholder: colors.modalForegroundSecondaryLabel,
+        icon: colors.modalForegroundSecondaryLabel,
+        background: colors.modalInputBackground,
       },
       emoji: {
-        selected: colors.panelSecondaryForegroundBorder,
+        selected: colors.modalButton,
       },
     }),
     [
-      colors.emojiKeyboardCategoryIcon,
-      colors.emojiKeyboardKnob,
-      colors.emojiKeyboardContainerActive,
-      colors.modalBackgroundLabel,
+      colors.modalKnob,
       colors.modalForeground,
-      colors.panelForegroundLabel,
-      colors.panelInputBackground,
-      colors.panelInputSecondaryForeground,
-      colors.panelSecondaryForegroundBorder,
+      colors.modalBackgroundLabel,
+      colors.modalButtonLabel,
+      colors.modalInputBackground,
+      colors.modalInputForeground,
+      colors.modalForegroundLabel,
+      colors.modalForegroundSecondaryLabel,
+      colors.modalButton,
     ],
   );
 
