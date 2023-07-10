ec066abbbf8af6fad510521610bad1c218e9ac60
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-07-10 14:30:57 -0400

    [lib/native/web] convert useInlineEngagementText to getInlineEngagmentSidebarText
    Summary: Small cleanup diff to convert this hook into a general function. This "hook" was not using any react features so this really should just be a function
    
    Test Plan: `flow` and confirmed that native and web apps work fine
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8609

--- native/chat/inline-engagement.react.js
+++ native/chat/inline-engagement.react.js
@@ -9,8 +9,8 @@ import Animated, {
   interpolateNode,
 } from 'react-native-reanimated';
 
-import useInlineEngagementText from 'lib/hooks/inline-engagement-text.react.js';
 import type { ReactionInfo } from 'lib/selectors/chat-selectors.js';
+import { getInlineEngagementSidebarText } from 'lib/shared/inline-engagement-utils.js';
 import { localIDPrefix } from 'lib/shared/message-utils.js';
 import { useViewerAlreadySelectedMessageReactions } from 'lib/shared/reaction-utils.js';
 import type { MessageInfo } from 'lib/types/message-types.js';
@@ -92,7 +92,7 @@ function InlineEngagement(props: Props): React.Node {
     }
   }, [disabled, navigateToThread, sidebarThreadInfo]);
 
-  const repliesText = useInlineEngagementText(sidebarThreadInfo);
+  const repliesText = getInlineEngagementSidebarText(sidebarThreadInfo);
 
   const sidebarStyle = React.useMemo(() => {
     const stylesResult = [styles.sidebar];
