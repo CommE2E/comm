0429f8f7c7ce535c88190599ee7e755cf3ffa0df
Rohan Khanderia <rohan.khanderia@gmail.com>
2023-07-10 16:43:29 -0400

    [native] Show a menu button next to each role except admins
    Summary:
    To prepare for editing and deleting roles, we want to display a menu button next to each role except for admins. It's safe to check by name here since the Admins role cannot be renamed, but it's
    worth noting that a more sound check would be what is described in [[ https://linear.app/comm/issue/ENG-4155/add-a-admin-role-column-for-threads | ENG-4155 ]]. For now though, I don't think it makes sense to
    prioritize this and will be easy enough to handle down the line.
    
    Part of [[ https://linear.app/comm/issue/ENG-4367/show-a-menu-button-next-to-each-role-except-members-and-admins | ENG-4367 ]].
    
    Depends on D8502
    
    Test Plan: Confirmed that the menu button displays for Members and custom roles, but not Admins
    
    Reviewers: atul, ginsu
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8522

--- native/roles/role-panel-entry.react.js
+++ native/roles/role-panel-entry.react.js
@@ -1,9 +1,10 @@
 // @flow
 
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 import CommIcon from '../components/comm-icon.react.js';
+import SWMansionIcon from '../components/swmansion-icon.react.js';
 import { useStyles } from '../themes/colors.js';
 
 type RolePanelEntryProps = {
@@ -15,13 +16,31 @@ function RolePanelEntry(props: RolePanelEntryProps): React.Node {
   const { roleName, memberCount } = props;
   const styles = useStyles(unboundStyles);
 
+  const menuButton = React.useMemo(() => {
+    if (roleName === 'Admins') {
+      return <View style={styles.rolePanelEmptyMenuButton} />;
+    }
+    return (
+      <TouchableOpacity>
+        <SWMansionIcon
+          name="menu-horizontal"
+          size={22}
+          style={styles.rolePanelMenuButton}
+        />
+      </TouchableOpacity>
+    );
+  }, [roleName, styles.rolePanelEmptyMenuButton, styles.rolePanelMenuButton]);
+
   return (
     <View style={styles.rolePanelEntry}>
       <Text style={styles.rolePanelNameEntry}>{roleName}</Text>
-      <Text style={styles.rolePanelCountEntry}>
-        {memberCount}
-        <CommIcon name="user-filled" size={14} />
-      </Text>
+      <View style={styles.rolePanelCountEntryContainer}>
+        <Text style={styles.rolePanelCountEntry}>
+          {memberCount}
+          <CommIcon name="user-filled" size={14} />
+        </Text>
+      </View>
+      {menuButton}
     </View>
   );
 }
@@ -34,16 +53,27 @@ const unboundStyles = {
     padding: 8,
   },
   rolePanelNameEntry: {
+    flex: 1,
     color: 'panelForegroundLabel',
     fontWeight: '600',
     fontSize: 14,
+  },
+  rolePanelCountEntryContainer: {
+    marginRight: 40,
+    alignItmes: 'flex-end',
   },
   rolePanelCountEntry: {
     color: 'panelForegroundLabel',
     fontWeight: '600',
     fontSize: 14,
-    marginRight: 72,
+    marginRight: 22,
     padding: 8,
+  },
+  rolePanelEmptyMenuButton: {
+    marginRight: 22,
+  },
+  rolePanelMenuButton: {
+    color: 'panelForegroundLabel',
   },
 };
 
