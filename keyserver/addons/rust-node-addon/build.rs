0af8781446c2a5c46fdf9e06d1a4e5ec2aa0d162
Jonathan Ringer <jonathan.ringer@comm.app>
2023-07-10 07:28:06 -0700

    [Keyserver/rust] Add publish_prekeys implementation
    Summary:
    Implement the rust bindings for publishing prekeys to
    identity service.
    
    Part of https://linear.app/comm/issue/ENG-3944
    
    Depends on D8464
    
    Test Plan: Tested in a later diff
    
    Reviewers: bartek, varun
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8473

--- keyserver/addons/rust-node-addon/build.rs
+++ keyserver/addons/rust-node-addon/build.rs
@@ -5,7 +5,10 @@ fn main() {
   tonic_build::configure()
     .build_server(false)
     .compile(
-      &["../../../shared/protos/identity_client.proto"],
+      &[
+        "../../../shared/protos/identity_client.proto",
+        "../../../shared/protos/identity_authenticated.proto",
+      ],
       &["../../../shared/protos"],
     )
     .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
