a3d8841d6a6d9026d26122ea3c9938a2991b8d61
Jonathan Ringer <jonathan.ringer@comm.app>
2023-07-11 09:52:12 -0700

    [Keyserver] Publish prekeys to identity service for cron job
    Summary:
    Have cron job for prekeys lifecycle publish
    the keys to identity service.
    
    Part of https://linear.app/comm/issue/ENG-3944
    
    Depends on D8475
    
    Test Plan:
    Run identity service + localstack
    - Start docker
    - Start localstack
    - Run terraform
    - Run identity service
    
    ```
    cd keyserver
    RUST_LOG=debug yarn dev
    ```
    
    Reviewers: kamil, michal
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8476

--- keyserver/src/utils/olm-utils.js
+++ keyserver/src/utils/olm-utils.js
@@ -6,9 +6,13 @@ import type {
   Utility as OlmUtility,
   Session as OlmSession,
 } from '@commapp/olm';
+import { getRustAPI } from 'rust-node-addon';
 import uuid from 'uuid';
 
 import { olmEncryptedMessageTypes } from 'lib/types/crypto-types.js';
+import { values } from 'lib/utils/objects.js';
+
+import { fetchIdentityInfo } from '../user/identity.js';
 
 type PickledOlmAccount = {
   +picklingKey: string,
@@ -77,24 +81,101 @@ function getOlmUtility(): OlmUtility {
   return cachedOLMUtility;
 }
 
-function validateAccountPrekey(account: OlmAccount) {
+function shouldRotatePrekey(account: OlmAccount): boolean {
+  const currentDate = new Date();
+  const lastPrekeyPublishDate = new Date(account.last_prekey_publish_time());
+  const prekeyPublished = !account.unpublished_prekey();
+
+  const shouldRotate =
+    prekeyPublished &&
+    currentDate - lastPrekeyPublishDate > maxPublishedPrekeyAge;
+
+  return shouldRotate;
+}
+
+function shouldForgetPrekey(account: OlmAccount): boolean {
+  if (account.unpublished_prekey()) {
+    return false;
+  }
+
   const currentDate = new Date();
   const lastPrekeyPublishDate = new Date(account.last_prekey_publish_time());
 
-  const prekeyPublished = !account.unpublished_prekey();
-  if (
-    prekeyPublished &&
-    currentDate - lastPrekeyPublishDate > maxPublishedPrekeyAge
-  ) {
+  return currentDate - lastPrekeyPublishDate >= maxOldPrekeyAge;
+}
+
+async function revalidateAccountPrekeys(
+  contentAccount: OlmAccount,
+  notifAccount: OlmAccount,
+): Promise<void> {
+  // Since keys are rotated synchronously, only check validity of one
+  if (shouldRotatePrekey(contentAccount)) {
+    await publishNewPrekeys(contentAccount, notifAccount);
+  }
+  if (shouldForgetPrekey(contentAccount)) {
+    contentAccount.forget_old_prekey();
+    notifAccount.forget_old_prekey();
+  }
+}
+
+async function publishNewPrekeys(
+  contentAccount: OlmAccount,
+  notifAccount: OlmAccount,
+): Promise<void> {
+  const rustAPIPromise = getRustAPI();
+  const fetchIdentityInfoPromise = fetchIdentityInfo();
+
+  contentAccount.generate_prekey();
+  notifAccount.generate_prekey();
+  const deviceId = JSON.parse(contentAccount.identity_keys()).curve25519;
+
+  const contentPrekeyMap = JSON.parse(contentAccount.prekey()).curve25519;
+  const [contentPrekey] = values(contentPrekeyMap);
+  const contentPrekeySignature = contentAccount.prekey_signature();
+
+  const notifPrekeyMap = JSON.parse(notifAccount.prekey()).curve25519;
+  const [notifPrekey] = values(notifPrekeyMap);
+  const notifPrekeySignature = notifAccount.prekey_signature();
+
+  if (!contentPrekeySignature || !notifPrekeySignature) {
+    console.warn('Warning: Unable to create valid signature for a prekey');
+    return;
+  }
+
+  const [rustAPI, identityInfo] = await Promise.all([
+    rustAPIPromise,
+    fetchIdentityInfoPromise,
+  ]);
+
+  if (!identityInfo) {
+    console.warn(
+      'Warning: Attempted to refresh prekeys before registering with identity service',
+    );
+    return;
+  }
+
+  await rustAPI.publish_prekeys(
+    identityInfo.userId,
+    deviceId,
+    identityInfo.accessToken,
+    contentPrekey,
+    contentPrekeySignature,
+    notifPrekey,
+    notifPrekeySignature,
+  );
+
+  contentAccount.mark_prekey_as_published();
+  notifAccount.mark_prekey_as_published();
+}
+
+function validateAccountPrekey(account: OlmAccount) {
+  if (shouldRotatePrekey(account)) {
     // If there is no prekey or the current prekey is older than month
     // we need to generate new one.
     account.generate_prekey();
   }
 
-  if (
-    prekeyPublished &&
-    currentDate - lastPrekeyPublishDate >= maxOldPrekeyAge
-  ) {
+  if (shouldForgetPrekey(account)) {
     account.forget_old_prekey();
   }
 }
@@ -106,4 +187,6 @@ export {
   unpickleOlmAccount,
   unpickleOlmSession,
   validateAccountPrekey,
+  revalidateAccountPrekeys,
+  publishNewPrekeys,
 };
