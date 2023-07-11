302b813cf5a77dfed50b5e709ac99b09deead4fa
Jonathan Ringer <jonathan.ringer@comm.app>
2023-07-11 09:51:15 -0700

    [Keyserver] Refactor identity utilities
    Summary:
    These are useful when interacting with identity service,
    pull them out of login.js to make them more widely available.
    
    Part of https://linear.app/comm/issue/ENG-3944
    
    Depends on D8474
    
    Test Plan: Launch keyserver with login workflow (missing identity info)
    
    Reviewers: kamil, michal
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8475

---
+++ keyserver/src/user/identity.js
@@ -0,0 +1,37 @@
+// @flow
+
+import type { QueryResults } from 'mysql';
+
+import { SQL, dbQuery } from '../database/database.js';
+
+const userIDMetadataKey = 'user_id';
+const accessTokenMetadataKey = 'access_token';
+
+// This information is minted when registering with identity service
+export type IdentityInfo = { +userId: string, +accessToken: string };
+
+async function fetchIdentityInfo(): Promise<?IdentityInfo> {
+  const versionQuery = SQL`
+    SELECT data
+    FROM metadata
+    WHERE name IN (${userIDMetadataKey}, ${accessTokenMetadataKey});
+  `;
+
+  const [[userId, accessToken]] = await dbQuery(versionQuery);
+  if (!userId || !accessToken) {
+    return null;
+  }
+  return { userId, accessToken };
+}
+
+async function saveIdentityInfo(userInfo: IdentityInfo): Promise<QueryResults> {
+  const updateQuery = SQL`
+    REPLACE INTO metadata (name, data)
+    VALUES (${userIDMetadataKey}, ${userInfo.userId}),
+      (${accessTokenMetadataKey}, ${userInfo.accessToken})
+  `;
+
+  return dbQuery(updateQuery);
+}
+
+export { fetchIdentityInfo, saveIdentityInfo };
