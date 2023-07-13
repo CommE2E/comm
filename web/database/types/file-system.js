7f9b8591715ad94d1fa0a145cf01acf0e9746a73
xsanm <kkurowski.kk@gmail.com>
2023-07-13 14:34:04 +0200

    [web] Add types for WASM File System API
    Summary: Based on:
    - [emscripten docs](https://emscripten.org/docs/api_reference/Filesystem-API.html)
    - [TS definition](https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/emscripten/index.d.ts)
    - inspection of printed FS retrieved from module
    
    Test Plan: `flow`
    
    Reviewers: michal, tomek

---
+++ web/database/types/file-system.js
@@ -0,0 +1,116 @@
+// @flow
+
+type Emscripten$FileSystemType =
+  | 'MEMFS'
+  | 'NODEFS'
+  | 'NODERAWFS'
+  | 'IDBFS'
+  | 'WORKERFS'
+  | 'PROXYFS';
+
+interface FS$Lookup {
+  +path: string;
+  +node: FS$FSNode;
+}
+
+interface FS$FSStream {}
+
+interface FS$FSNode {}
+
+interface FS$stats {
+  +[key: string]: mixed;
+}
+
+export interface FS {
+  // paths
+  lookupPath(
+    path: string,
+    opts: { +parent: boolean, +follow: boolean },
+  ): FS$Lookup;
+  getPath(node: FS$FSNode): string;
+
+  // nodes
+  isFile(mode: number): boolean;
+  isDir(mode: number): boolean;
+  isLink(mode: number): boolean;
+  isChrdev(mode: number): boolean;
+  isBlkdev(mode: number): boolean;
+  isFIFO(mode: number): boolean;
+  isSocket(mode: number): boolean;
+
+  // devices
+  major(dev: number): number;
+  minor(dev: number): number;
+  makedev(ma: number, mi: number): number;
+  registerDevice(dev: number, ops: any): void;
+
+  // core
+  syncfs(populate: boolean, callback: (e: any) => void): void;
+  syncfs(callback: (e: any) => void, populate?: boolean): void;
+  mount(type: Emscripten$FileSystemType, opts: any, mountpoint: string): void;
+  unmount(mountpoint: string): void;
+
+  mkdir(path: string, mode?: number): any;
+  mkdev(path: string, mode?: number, dev?: number): void;
+  symlink(oldpath: string, newpath: string): void;
+  rename(old_path: string, new_path: string): void;
+  rmdir(path: string): void;
+  readdir(path: string): $ReadOnlyArray<string>;
+  unlink(path: string): void;
+  readlink(path: string): string;
+  stat(path: string, dontFollow?: boolean): FS$stats;
+  lstat(path: string): FS$stats;
+  chmod(path: string, mode: number, dontFollow?: boolean): void;
+  lchmod(path: string, mode: number): void;
+  fchmod(fd: number, mode: number): void;
+  chown(path: string, uid: number, gid: number, dontFollow?: boolean): void;
+  lchown(path: string, uid: number, gid: number): void;
+  fchown(fd: number, uid: number, gid: number): void;
+  truncate(path: string, len: number): void;
+  ftruncate(fd: number, len: number): void;
+  utime(path: string, atime: number, mtime: number): void;
+  open(
+    path: string,
+    flags: string,
+    mode?: number,
+    fd_start?: number,
+    fd_end?: number,
+  ): FS$FSStream;
+  close(stream: FS$FSStream): void;
+  llseek(stream: FS$FSStream, offset: number, whence: number): number;
+  read(
+    stream: FS$FSStream,
+    buffer: Uint8Array,
+    offset: number,
+    length: number,
+    position?: number,
+  ): number;
+  write(
+    stream: FS$FSStream,
+    buffer: Uint8Array,
+    offset: number,
+    length: number,
+    position?: number,
+    canOwn?: boolean,
+  ): number;
+  allocate(stream: FS$FSStream, offset: number, length: number): void;
+  readFile(
+    path: string,
+    opts: { +encoding: 'binary', +flags?: string },
+  ): Uint8Array;
+  readFile(path: string, opts: { +encoding: 'utf8', +flags?: string }): string;
+  writeFile(
+    path: string,
+    data: string | Uint8Array,
+    opts?: { +flags?: string },
+  ): void;
+
+  // module-level FS code
+  cwd(): string;
+  chdir(path: string): void;
+  init(
+    input: null | (() => number | null),
+    output: null | ((c: number) => any),
+    error: null | ((c: number) => any),
+  ): void;
+}
