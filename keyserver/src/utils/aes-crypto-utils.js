7f90863290f1e9f7e5ee1597236e4fdad229492e
marcinwasowicz <marcinkwasowicz@gmail.com>
2023-07-17 15:40:50 +0200

    Enable webcrypto usage in keyserver codebase and unify aes crypto stack across web and the keyserver
    Summary: This differential brings WebCrypt API typing to keyserver and enables AES crypto stack implemented for web to be used on the keyserver as well
    
    Test Plan: Use aes-crypto-utils.js to encrypt and decrypt simple string on the keyserver. Ensure that encryption and decryption is succesfull and flow does not complain.
    
    Reviewers: bartek, kamil
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8521

--- keyserver/src/utils/aes-crypto-utils.js
+++ keyserver/src/utils/aes-crypto-utils.js
@@ -1,27 +1,33 @@
 // @flow
 
+import crypto from 'crypto';
+
 import {
   generateKeyCommon,
   encryptCommon,
   decryptCommon,
 } from 'lib/media/aes-crypto-utils-common.js';
 
+// crypto.webcrypto was introduced in Node 15.10.0.
+// It is not defined in Flow so we need a cast
+const commonCrypto: Crypto = (crypto: any).webcrypto;
+
 async function generateKey(): Promise<Uint8Array> {
-  return await generateKeyCommon(crypto);
+  return await generateKeyCommon(commonCrypto);
 }
 
 async function encrypt(
   keyBytes: Uint8Array,
   plaintext: Uint8Array,
 ): Promise<Uint8Array> {
-  return await encryptCommon(crypto, keyBytes, plaintext);
+  return await encryptCommon(commonCrypto, keyBytes, plaintext);
 }
 
 async function decrypt(
   keyBytes: Uint8Array,
   sealedData: Uint8Array,
 ): Promise<Uint8Array> {
-  return await decryptCommon(crypto, keyBytes, sealedData);
+  return await decryptCommon(commonCrypto, keyBytes, sealedData);
 }
 
 export { generateKey, encrypt, decrypt };
