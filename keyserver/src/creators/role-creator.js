64996590402e4669e28b8c39c0eccc2f41166675
Rohan Khanderia <rohan.khanderia@gmail.com>
2023-07-17 12:42:46 -0400

    [keyserver/lib] Add keyserver support for creating roles
    Summary:
    This diff adds the `keyserver` support for creating new roles with a set of permissions. `configurableCommunityPermissions` was introduced in the previous diff, and those are the user-selectable
    permissions that are provided here, and we introduce `guaranteedCommunityPermissions` here and merge the two arrays to derive a set of permissions for the new role.
    
    The reason I named things `modifyRoles` / `RoleModification`, as opposted to `createRole` / `RoleCreation` is because I plan to re-use this code as much as possible for the `edit_roles` flow. Because of this,
    I intended to name these functions / types as something that could be used both across creating roles and editing roles.
    
    Only adding @ashoat as a reviewer here since it 1) directly addresses his feedback about introducing the `guaranteedCommunityPermissions` alongside its callsite, and 2) it involves community permissions and
    I'm not sure who has the most context on them.
    
    Test Plan:
    Between this diff and the last diff, I added a rough call to create a custom role when the client-side 'Create role' button is clicked, and confirmed that it saved in the DB. The next diff will add
    the logic to actually save a new role from the client.
    
    Reviewers: atul, ginsu, ashoat
    
    Subscribers: tomek, ashoat
    
    Differential Revision: https://phab.comm.dev/D8420

--- keyserver/src/creators/role-creator.js
+++ keyserver/src/creators/role-creator.js
@@ -1,11 +1,25 @@
 // @flow
 
 import { getRolePermissionBlobs } from 'lib/permissions/thread-permissions.js';
+import {
+  universalCommunityPermissions,
+  userSurfacedPermissionsSet,
+  configurableCommunityPermissions,
+  threadPermissions,
+} from 'lib/types/thread-permission-types.js';
 import type { ThreadType } from 'lib/types/thread-types-enum.js';
-import type { RoleInfo } from 'lib/types/thread-types.js';
+import { threadTypes } from 'lib/types/thread-types-enum.js';
+import type {
+  RoleInfo,
+  RoleModificationRequest,
+} from 'lib/types/thread-types.js';
+import { ServerError } from 'lib/utils/errors.js';
 
 import createIDs from './id-creator.js';
 import { dbQuery, SQL } from '../database/database.js';
+import { fetchThreadInfos } from '../fetchers/thread-fetchers.js';
+import { checkThreadPermission } from '../fetchers/thread-permission-fetchers.js';
+import type { Viewer } from '../session/viewer.js';
 
 type InitialRoles = {
   +default: RoleInfo,
@@ -59,4 +73,71 @@ async function createInitialRolesForNewThread(
   };
 }
 
-export { createInitialRolesForNewThread };
+async function modifyRole(
+  viewer: Viewer,
+  request: RoleModificationRequest,
+): Promise<void> {
+  const hasPermission = await checkThreadPermission(
+    viewer,
+    request.community,
+    threadPermissions.CHANGE_ROLE,
+  );
+  if (!hasPermission) {
+    throw new ServerError('invalid_credentials');
+  }
+
+  const { community, name, permissions, action } = request;
+
+  for (const permission of permissions) {
+    if (!userSurfacedPermissionsSet.has(permission)) {
+      throw new ServerError('invalid_parameters');
+    }
+  }
+
+  const [id] = await createIDs('roles', 1);
+  const time = Date.now();
+
+  const configuredPermissions = permissions
+    .map(permission => [...configurableCommunityPermissions[permission]])
+    .flat();
+
+  const rolePermissions = [
+    ...universalCommunityPermissions,
+    ...configuredPermissions,
+  ];
+
+  // For communities of the type `COMMUNITY_ANNOUNCEMENT_ROOT`, the ability for
+  // the role to be voiced needs to be configured (i.e. the parameters should
+  // include the user-facing permission VOICED_IN_ANNOUNCEMENT_CHANNELS). This
+  // means we do not give 'voiced' permissions by default to all new roles. As
+  // a result, if the thread type is `COMMUNITY_ROOT`, we want to ensure that
+  // the role has the voiced permission.
+  const { threadInfos } = await fetchThreadInfos(viewer, {
+    threadID: community,
+  });
+  const threadInfo = threadInfos[community];
+
+  if (threadInfo.type === threadTypes.COMMUNITY_ROOT) {
+    rolePermissions.push(threadPermissions.VOICED);
+  }
+
+  const permissionsBlob = JSON.stringify(
+    Object.fromEntries(rolePermissions.map(permission => [permission, true])),
+  );
+
+  const row = [id, community, name, permissionsBlob, time];
+
+  let query = SQL``;
+  if (action === 'create_role') {
+    query = SQL`
+      INSERT INTO roles (id, thread, name, permissions, creation_time)
+      VALUES (${row})
+    `;
+  } else if (action === 'edit_role') {
+    throw new ServerError("unimplemented: can't edit roles yet");
+  }
+
+  await dbQuery(query);
+}
+
+export { createInitialRolesForNewThread, modifyRole };
