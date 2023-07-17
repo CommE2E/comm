54667df1db7adeafa1236ea406fdb1f922ac912d
marcinwasowicz <marcinkwasowicz@gmail.com>
2023-07-17 15:25:33 +0200

    Modify NIX and keyserver configuration files to allow node version >= 18
    Summary:
    This differential applies temporary solution to allow usage of node version >= 18 since it is necessary for current project regarding blob service and big notifications.
    The solution is not perfect and the correct one will be implemented in future: https://linear.app/comm/issue/ENG-4373/revert-to-using-nvm-to-lock-into-specific-node-version
    
    Test Plan:
    1. run `yarn cleaninstall`
    2. run `nix develop`
    3. run `node --version` and expect the output to match 18.12.1
    
    Reviewers: ashoat, jon, O1 Nix!
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D8520

--- nix/dev-shell.nix
+++ nix/dev-shell.nix
@@ -27,7 +27,7 @@
 , localstack
 , mariadb
 , mariadb-up
-, nodejs-16_x-openssl_1_1
+, nodejs
 , olm
 , openjdk11
 , openssl
@@ -63,7 +63,7 @@ mkShell {
 
     # node development
     mariadb
-    nodejs-16_x-openssl_1_1
+    nodejs
     yarn
     python3
     redis
