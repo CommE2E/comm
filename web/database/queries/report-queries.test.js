d3c857366f5ff3609bbf80c7bbc0089fb697efac
xsanm <kkurowski.kk@gmail.com>
2023-07-18 11:14:30 +0200

    [web] rework report tests to use CommQueryExecutor
    Summary:
    Updates test to new module
    
    Depends on D8553
    
    Test Plan: Run tests
    
    Reviewers: michal, tomek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D8554

--- web/database/queries/report-queries.test.js
+++ web/database/queries/report-queries.test.js
@@ -1,81 +1,50 @@
 // @flow
 
-import initSqlJs from 'sql.js';
-
-import {
-  createReportTable,
-  getAllReports,
-  removeAllReports,
-  removeReports,
-  updateReport,
-} from './report-queries.js';
-import { parseMultiStatementSQLiteResult } from '../utils/db-utils.js';
+import { getDatabaseModule } from '../db-module.js';
 
 describe('Report Store queries', () => {
-  let db;
+  let queryExecutor;
 
   beforeAll(async () => {
-    const SQL = await initSqlJs();
-    db = new SQL.Database();
-    createReportTable(db);
+    const module = getDatabaseModule();
+    queryExecutor = new module.SQLiteQueryExecutor('test.sqlite');
   });
 
   beforeEach(() => {
-    db.exec(`
-      INSERT INTO reports VALUES ("1", "{report_content_1");
-      INSERT INTO reports VALUES ("2", "{report_content_2}");
-    `);
+    queryExecutor.replaceReport({ id: '1', report: '{report_content_1}' });
+    queryExecutor.replaceReport({ id: '2', report: '{report_content_2}' });
   });
 
   afterEach(() => {
-    db.exec(`DELETE FROM reports`);
-  });
-
-  it('should create table', () => {
-    const res = db.exec(`PRAGMA table_info(reports);`);
-    const [parsedRes] = parseMultiStatementSQLiteResult(res);
-
-    expect(parsedRes).toHaveLength(2);
-    expect(parsedRes).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({
-          name: 'id',
-          type: 'TEXT',
-        }),
-        expect.objectContaining({
-          name: 'report',
-          type: 'TEXT',
-        }),
-      ]),
-    );
+    queryExecutor.removeAllReports();
   });
 
   it('should return all reports', () => {
-    const reports = getAllReports(db);
+    const reports = queryExecutor.getAllReports();
     expect(reports).toHaveLength(2);
   });
 
   it('should remove all reports', () => {
-    removeAllReports(db);
-    const reports = getAllReports(db);
+    queryExecutor.removeAllReports();
+    const reports = queryExecutor.getAllReports();
     expect(reports).toHaveLength(0);
   });
 
   it('should insert not existing report', () => {
     const id = '3';
     const report = '{some_content}';
-    updateReport(db, id, report);
+    queryExecutor.replaceReport({ id, report });
 
-    const reports = getAllReports(db);
+    const reports = queryExecutor.getAllReports();
     expect(reports).toHaveLength(3);
     expect(reports).toContainEqual({ id, report });
   });
 
   it('should reports with given id', () => {
     const id = '2';
-    removeReports(db, [id]);
+    queryExecutor.removeReports([id]);
 
-    const reports = getAllReports(db);
+    const reports = queryExecutor.getAllReports();
     expect(reports).toHaveLength(1);
     expect(reports).not.toContain(
       expect.objectContaining({
