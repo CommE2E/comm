5e5322afe3f86580420ea6c01d0a4286a821b57d
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-07-18 01:20:25 -0400

    [native] seperate out each reaction into their own pill
    Summary: The next step to redesigning the `InlineEngagement` component is to split out each reaction into its own pill. In this diff we introduced a regression where we got rid of the spacing betweent he sidebar pill and the first reaction pill; however, we will address this regression in the very next diff
    
    Test Plan: Please see the screenshots below
    
    Reviewers:
    
    Subscribers:

--- native/chat/chat-constants.js
+++ native/chat/chat-constants.js
@@ -9,18 +9,16 @@ export const inlineEngagementStyle = {
   height: 38,
   marginTop: 5,
   marginBottom: 3,
-};
-export const inlineEngagementLeftStyle = {
   topOffset: -10,
 };
 
 export const inlineEngagementCenterStyle = {
   topOffset: -5,
+  marginRight: 22,
 };
 
 export const inlineEngagementRightStyle = {
-  marginRight: 22,
-  topOffset: -10,
+  marginLeft: 22,
 };
 
 export const inlineEngagementLabelStyle = {
