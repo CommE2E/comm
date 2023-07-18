9b7497236d30a31a2375db8b3de1ad1ee4eca028
Patryk Klatka <patryk.klatka@gmail.com>
2023-07-18 15:56:50 +0200

    [web] Warn the user when deleting a chat that has contained chats
    Summary: TODO
    
    Test Plan: TODO
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8530

---
+++ web/modals/threads/settings/thread-settings-delete-confirmation-modal.react.js
@@ -0,0 +1,52 @@
+// @flow
+
+import * as React from 'react';
+
+import { useModalContext } from 'lib/components/modal-provider.react.js';
+
+import css from './thread-settings-delete-confirmation-modal.css';
+import Button from '../../../components/button.react.js';
+import Modal from '../../modal.react.js';
+
+type BaseProps = {
+  +isCommunityRoot: boolean,
+  +onConfirmation: () => void,
+};
+
+function ThreadDeleteConfirmationModal({
+  isCommunityRoot,
+  onConfirmation,
+}: BaseProps): React.Node {
+  const { popModal } = useModalContext();
+  const deletionTargetText = React.useMemo(
+    () => (isCommunityRoot ? 'Subchannels and threads' : 'Threads'),
+    [isCommunityRoot],
+  );
+
+  return (
+    <Modal
+      size="large"
+      name="Warning"
+      icon="warning-circle"
+      withCloseButton={false}
+      onClose={popModal}
+    >
+      <div className={css.container}>
+        <p className={css.text}>
+          {deletionTargetText} within this chat will also be permanently
+          deleted. Are you sure you want to continue?
+        </p>
+        <div className={css.buttonContainer}>
+          <Button variant="outline" onClick={popModal}>
+            No
+          </Button>
+          <Button variant="filled" onClick={onConfirmation} type="submit">
+            Yes
+          </Button>
+        </div>
+      </div>
+    </Modal>
+  );
+}
+
+export default ThreadDeleteConfirmationModal;
