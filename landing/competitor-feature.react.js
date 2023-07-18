c1f32ad2111bcdbc77513b0d4a71a40c4761d6db
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-07-18 15:58:29 -0400

    [landing] introduce multi paragraph functionality in CompetitorFeature
    Summary: The new copy for the long matrix feature comparison descriptions has multiple paragraphs. This diff introduces new logic in CompetitorFeature so that it can handle spacing multiple paragraphs the "right way" (not using spaces/newlines, but instead using seperate elements and spacing them through margin/padding)
    
    Test Plan: Please see the screenshots below
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8537

--- landing/competitor-feature.react.js
+++ landing/competitor-feature.react.js
@@ -15,8 +15,8 @@ type Props = {
   +competitorID: Competitors,
   +title: string,
   +comingSoon: boolean,
-  +competitorDescription: string,
-  +commDescription: string,
+  +competitorDescription: string | $ReadOnlyArray<string>,
+  +commDescription: string | $ReadOnlyArray<string>,
 };
 
 function CompetitorFeature(props: Props): React.Node {
@@ -37,6 +37,10 @@ function CompetitorFeature(props: Props): React.Node {
     typography.paragraph1,
     css.descriptionText,
   ]);
+  const descriptionMultiClassName = classNames([
+    typography.paragraph1,
+    css.descriptionTextMutli,
+  ]);
 
   let comingSoonBadge;
   if (comingSoon) {
@@ -52,6 +56,42 @@ function CompetitorFeature(props: Props): React.Node {
     );
   }
 
+  const competitorInfo = React.useMemo(() => {
+    if (typeof competitorDescription === 'string') {
+      return <p className={descriptionClassName}>{competitorDescription}</p>;
+    } else {
+      const paragraphs = competitorDescription.map((paragraph, index) => {
+        const className =
+          index > 0 ? descriptionMultiClassName : descriptionClassName;
+
+        return (
+          <p className={className} key={paragraph}>
+            {paragraph}
+          </p>
+        );
+      });
+      return <>{paragraphs}</>;
+    }
+  }, [competitorDescription, descriptionClassName, descriptionMultiClassName]);
+
+  const commInfo = React.useMemo(() => {
+    if (typeof commDescription === 'string') {
+      return <p className={descriptionClassName}>{commDescription}</p>;
+    } else {
+      const paragraphs = commDescription.map((paragraph, index) => {
+        const className =
+          index > 0 ? descriptionMultiClassName : descriptionClassName;
+
+        return (
+          <p className={className} key={paragraph}>
+            {paragraph}
+          </p>
+        );
+      });
+      return <>{paragraphs}</>;
+    }
+  }, [commDescription, descriptionClassName, descriptionMultiClassName]);
+
   return (
     <div className={css.container}>
       <div className={css.headingContainer}>
@@ -59,10 +99,10 @@ function CompetitorFeature(props: Props): React.Node {
         {comingSoonBadge}
       </div>
       <CompetitorLogo name={competitorID} size={30} />
-      <p className={descriptionClassName}>{competitorDescription}</p>
+      {competitorInfo}
       <hr />
       <CommLogo size={30} />
-      <p className={descriptionClassName}>{commDescription}</p>
+      {commInfo}
     </div>
   );
 }
