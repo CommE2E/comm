bc1d622f75dbd05cebde2dcbec680818e9a555c0
xsanm <kkurowski.kk@gmail.com>
2023-07-18 16:57:53 +0200

    [web] remove old raw SQL queries
    Summary:
    Now we are using `.wasm` database module with logic from native
    
    Depends on D8560
    
    Test Plan: Flow
    
    Reviewers: michal, tomek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D8561

--- web/database/utils/db-utils.js
+++ web/database/utils/db-utils.js
@@ -1,7 +1,6 @@
 // @flow
 
 import { detect as detectBrowser } from 'detect-browser';
-import type { QueryExecResult } from 'sql.js';
 
 import { isStaff } from 'lib/shared/staff-utils.js';
 import { isDev } from 'lib/utils/dev-utils.js';
@@ -10,28 +9,6 @@ import { DB_SUPPORTED_BROWSERS, DB_SUPPORTED_OS } from './constants.js';
 import type { EmscriptenModule } from '../types/module.js';
 
 const browser = detectBrowser();
-
-function parseSQLiteQueryResult<T>(result: QueryExecResult): T[] {
-  const { columns, values } = result;
-  return values.map(rowResult => {
-    const row: any = Object.fromEntries(
-      columns.map((key, index) => [key, rowResult[index]]),
-    );
-    return row;
-  });
-}
-
-// NOTE: sql.js has behavior that when there are multiple statements in query
-// e.g. "statement1; statement2; statement3;"
-// and statement2 will not return anything, the result will be:
-// [result1, result3], not [result1, undefined, result3]
-function parseMultiStatementSQLiteResult<T: Object>(
-  rawResult: $ReadOnlyArray<QueryExecResult>,
-): T[][] {
-  return rawResult.map((queryResult: QueryExecResult) =>
-    parseSQLiteQueryResult<T>(queryResult),
-  );
-}
 
 function importDatabaseContent(
   content: Uint8Array,
@@ -70,7 +47,6 @@ const isDesktopSafari: boolean =
   browser && browser.name === 'safari' && browser.os === 'Mac OS';
 
 export {
-  parseMultiStatementSQLiteResult,
   isSQLiteSupported,
   isDesktopSafari,
   importDatabaseContent,
