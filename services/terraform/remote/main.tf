f4da8e4539388180f9786591fdf52590912e0956
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2023-07-19 17:49:24 +0200

    [services][terraform] Add default ManagedBy tag to all resources
    Summary:
    
    Set up a default tag `ManagedBy = Terraform` to all resources created by Terraform. This will allow us to identify resources created by Terraform and distinguish them from resources created manually.
    
    Depends on D8570
    
    Test Plan:
    
    `terraform plan` shows that the tag will be added to dynamodb tables and s3 buckets.
    
    Reviewers: jon, varun, michal
    
    Subscribers:

--- services/terraform/remote/main.tf
+++ services/terraform/remote/main.tf
@@ -4,10 +4,18 @@ variable "aws_profile" {
 
 provider "aws" {
   region = "us-east-2"
- 
-  shared_config_files = ["${pathexpand("~/.aws/config")}"]
+
+  shared_config_files      = ["${pathexpand("~/.aws/config")}"]
   shared_credentials_files = ["${pathexpand("~/.aws/credentials")}"]
-  profile = var.aws_profile
+  profile                  = var.aws_profile
+
+  # automatically add these tags to all resources
+  default_tags {
+    tags = {
+      # Helps to distinguish which resources are managed by Terraform
+      ManagedBy = "Terraform"
+    }
+  }
 }
 
 # Shared resources between local dev environment and remote AWS
