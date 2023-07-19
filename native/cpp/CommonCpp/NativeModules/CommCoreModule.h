afc4fcf27cd805a838fbc42bf5c560e9ed5373c3
Ashoat Tevosyan <ashoat@comm.app>
2023-07-19 14:44:22 -0700

    [native] codeVersion -> 234
    NOTE: this is not the real mobile-v1.0.234

--- native/cpp/CommonCpp/NativeModules/CommCoreModule.h
+++ native/cpp/CommonCpp/NativeModules/CommCoreModule.h
@@ -15,7 +15,7 @@ namespace comm {
 namespace jsi = facebook::jsi;
 
 class CommCoreModule : public facebook::react::CommCoreModuleSchemaCxxSpecJSI {
-  const int codeVersion{232};
+  const int codeVersion{234};
   std::unique_ptr<WorkerThread> cryptoThread;
 
   CommSecureStore secureStore;
