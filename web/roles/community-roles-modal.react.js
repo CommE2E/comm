fc317ae2c6e24a1c5edc2e95406204945da0e1b8
Rohan Khanderia <rohan.khanderia@gmail.com>
2023-07-20 14:21:33 -0400

    [web] Create a community roles modal to display all roles and member counts
    Summary:
    For a community when the "Roles" option is clicked in the community actions menu, we want to display a modal that lists each of the member counts and role names for that given community. The logic is
    similar to what was done on `native` in D8359.
    
    Something to note about the `reduxThreadInfo` line of code is that it's used so after creating a role, then returning back to this modal, we want to pull in the latest `ThreadInfo` from the redux store so the
    modal can show the newly created role in the list of roles. Without this (as was a similar case in `native`), the new role will only show once the modal is closed and re-opened, since the props have not
    changed.
    
    Additional note: any common / shared logic between `web` and `native` are tracked to be extracted to a shared utils file in [[
    https://linear.app/comm/issue/ENG-4443/[follow-up]-extract-shared-logic-between-web-and-native-to-utils | ENG-4443 ]] to not delay this goal.
    
    [[ https://linear.app/comm/issue/ENG-4426/create-a-community-roles-modal-to-display-all-roles-and-member-counts | ENG-4426 ]]
    
    Depends on D8592
    
    Test Plan: Checked that the community roles modal showed the correct role names and member counts for a given community
    
    Reviewers: atul, ginsu
    
    Subscribers: ashoat, tomek, ted
    
    Differential Revision: https://phab.comm.dev/D8593

---
+++ web/roles/community-roles-modal.react.js
@@ -0,0 +1,91 @@
+// @flow
+
+import * as React from 'react';
+
+import { useModalContext } from 'lib/components/modal-provider.react.js';
+import { threadInfoSelector } from 'lib/selectors/thread-selectors.js';
+import { getRoleMemberCountsForCommunity } from 'lib/shared/thread-utils.js';
+import type { ThreadInfo } from 'lib/types/thread-types.js';
+
+import css from './community-roles-modal.css';
+import RolePanelEntry from './role-panel-entry.react.js';
+import Button, { buttonThemes } from '../components/button.react.js';
+import Modal from '../modals/modal.react.js';
+import { useSelector } from '../redux/redux-utils.js';
+
+type CommunityRolesModalProps = {
+  +threadInfo: ThreadInfo,
+};
+
+function CommunityRolesModal(props: CommunityRolesModalProps): React.Node {
+  const { popModal } = useModalContext();
+  const { threadInfo } = props;
+
+  const [updatedThreadInfo, setUpdatedThreadInfo] =
+    React.useState<ThreadInfo>(threadInfo);
+
+  const threadID = threadInfo.id;
+  const reduxThreadInfo: ?ThreadInfo = useSelector(
+    state => threadInfoSelector(state)[threadID],
+  );
+
+  React.useEffect(() => {
+    if (reduxThreadInfo) {
+      setUpdatedThreadInfo(reduxThreadInfo);
+    }
+  }, [reduxThreadInfo]);
+
+  const roleNamesToMembers = React.useMemo(
+    () => getRoleMemberCountsForCommunity(updatedThreadInfo),
+    [updatedThreadInfo],
+  );
+
+  const rolePanelList = React.useMemo(() => {
+    const rolePanelEntries = [];
+
+    Object.keys(roleNamesToMembers).forEach(roleName => {
+      rolePanelEntries.push(
+        <RolePanelEntry
+          key={roleName}
+          roleName={roleName}
+          memberCount={roleNamesToMembers[roleName]}
+        />,
+      );
+    });
+
+    return rolePanelEntries;
+  }, [roleNamesToMembers]);
+
+  const onClickCreateRole = React.useCallback(() => {}, []);
+
+  return (
+    <Modal name="Roles" onClose={popModal} size="large">
+      <div className={css.modalDescription}>
+        Roles help you group community members together and assign them certain
+        permissions. When people join the community, they are automatically
+        assigned the Members role.
+      </div>
+      <div className={css.modalDescription}>
+        Communities must always have the Admins and Members role.
+      </div>
+      <div className={css.rolePanelTitleContainer}>
+        <div className={css.rolePanelTitle}>Roles</div>
+        <div className={css.rolePanelTitle}>Members</div>
+      </div>
+      <hr className={css.separator} />
+      <div className={css.rolePanelList}>{rolePanelList}</div>
+      <div className={css.createRoleButtonContainer}>
+        <Button
+          variant="filled"
+          className={css.createRoleButton}
+          buttonColor={buttonThemes.standard}
+          onClick={onClickCreateRole}
+        >
+          Create Role
+        </Button>
+      </div>
+    </Modal>
+  );
+}
+
+export default CommunityRolesModal;
