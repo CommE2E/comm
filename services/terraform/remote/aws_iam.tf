deb2e9c3a1f3be76d6746b9f41470c16ccd0b0a8
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2023-07-20 14:25:22 +0200

    [services][terraform] Set up electron-update ECS service
    Summary:
    This diff introduces Terraform configuration that is needed to bootstrap the electron-update service.
    It is deployed as a ECS Fargate service - this diff more or less the deployment configuration for services.
    
    A few notes:
    - The task definition, ecs-service and target-group were created using Terraform
    - Existing load balancer was imported into Terraform state and the ECS target group was linked to it
    - A test listener was added to be able to reach the ECS service directly. I'll delete it once we redirect 100% traffic to the ECS service.
    
    Depends on D8583
    
    Test Plan: The service is already deployed to production and handles most of real network traffic.
    
    Reviewers: michal, jon, varun
    
    Reviewed By: michal
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8585

--- services/terraform/remote/aws_iam.tf
+++ services/terraform/remote/aws_iam.tf
@@ -24,3 +24,28 @@ resource "aws_iam_role" "ecs_instance_role" {
   ]
 }
 
+# ECS Task execution role
+# Docs: https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task_execution_IAM_role.html
+resource "aws_iam_role" "ecs_task_execution" {
+  name = "ecsTaskExecutionRole"
+  assume_role_policy = jsonencode({
+    Version = "2008-10-17"
+    Statement = [
+      {
+        Sid    = ""
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy",
+    # Let ECS write logs to CloudWatch
+    "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess",
+  ]
+}
+
