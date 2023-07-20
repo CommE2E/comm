d1aa3073e884afed21932e0f60463c58d3969ba7
Patryk Klatka <patryk.klatka@gmail.com>
2023-07-20 15:02:04 +0200

    [native] Use currently selected theme on alerts
    Summary: TODO
    
    Test Plan: TODO
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8586

---
+++ native/utils/alert.js
@@ -0,0 +1,48 @@
+// @flow
+
+import { Alert } from 'react-native';
+
+import { store } from '../redux/redux-setup.js';
+import { type GlobalTheme, defaultGlobalThemeInfo } from '../types/themes.js';
+
+type AlertWrapper = {
+  alert: typeof Alert.alert,
+  prompt: typeof Alert.prompt,
+};
+
+const getCurrentTheme = (): GlobalTheme =>
+  store.getState().globalThemeInfo.activeTheme ||
+  defaultGlobalThemeInfo.activeTheme;
+
+const alertWrapper: AlertWrapper = {
+  alert(title, message, buttons, options) {
+    Alert.alert(title, message, buttons, {
+      userInterfaceStyle: getCurrentTheme(),
+      ...options,
+    });
+  },
+  prompt(
+    title,
+    message,
+    callbackOrButtons,
+    type,
+    defaultValue,
+    keyboardType,
+    options,
+  ) {
+    Alert.prompt(
+      title,
+      message,
+      callbackOrButtons,
+      type,
+      defaultValue,
+      keyboardType,
+      {
+        userInterfaceStyle: getCurrentTheme(),
+        ...options,
+      },
+    );
+  },
+};
+
+export default alertWrapper;
