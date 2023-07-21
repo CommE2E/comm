b009eed69ce9fe0b735df262a74b6f2c4388c4b2
Rohan Khanderia <rohan.khanderia@gmail.com>
2023-07-21 10:57:12 -0400

    [keyserver/lib] Enable the option to delete custom roles
    Summary:
    This diff handles the flow for deleting custom roles. There are two cases to consider here when a role is deleted: (1) if there are no members assigned to the role, just delete it. (2) If there are members assigned to the role, then we need to display in the Alert message that they will be assigned to the default role (I did not hardcode the role name 'Members' since we're allowing it to be renamed, so instead I pull the default role name from the threadInfo).
    
    A lot of the code in this diff is the scaffolding for the deletion API, so I decided that covering it all in one diff would be more logical and easier to review.
    
    [[ https://linear.app/comm/issue/ENG-4389/enable-the-option-to-delete-custom-roles | ENG-4389 ]]
    
    Depends on D8564
    
    Test Plan:
    Confirmed that role deletion works successfully for both cases (1) and (2).
    
    {F645347}
    
    Reviewers: atul, ginsu, ashoat!
    
    Reviewed By: ginsu
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8574

--- keyserver/src/deleters/role-deleters.js
+++ keyserver/src/deleters/role-deleters.js
@@ -1,6 +1,23 @@
 // @flow
 
+import { threadPermissions } from 'lib/types/thread-permission-types.js';
+import type {
+  RoleDeletionRequest,
+  RoleDeletionResult,
+} from 'lib/types/thread-types.js';
+import { updateTypes } from 'lib/types/update-types-enum.js';
+import { ServerError } from 'lib/utils/errors.js';
+
+import { createUpdates } from '../creators/update-creator.js';
 import { dbQuery, SQL } from '../database/database.js';
+import {
+  fetchServerThreadInfos,
+  rawThreadInfosFromServerThreadInfos,
+  fetchThreadInfos,
+} from '../fetchers/thread-fetchers.js';
+import { checkThreadPermission } from '../fetchers/thread-permission-fetchers.js';
+import type { Viewer } from '../session/viewer.js';
+import { updateRole } from '../updaters/thread-updaters.js';
 
 async function deleteOrphanedRoles(): Promise<void> {
   await dbQuery(SQL`
@@ -12,4 +29,109 @@ async function deleteOrphanedRoles(): Promise<void> {
   `);
 }
 
-export { deleteOrphanedRoles };
+async function deleteRole(
+  viewer: Viewer,
+  request: RoleDeletionRequest,
+): Promise<RoleDeletionResult> {
+  const hasPermission = await checkThreadPermission(
+    viewer,
+    request.community,
+    threadPermissions.CHANGE_ROLE,
+  );
+  if (!hasPermission) {
+    throw new ServerError('invalid_credentials');
+  }
+
+  const { community, roleID } = request;
+
+  const defaultRoleQuery = SQL`
+    SELECT default_role
+    FROM threads
+    WHERE id = ${community}
+  `;
+
+  const membersWithRoleQuery = SQL`
+    SELECT user
+    FROM memberships
+    WHERE thread = ${community}
+      AND role = ${roleID}
+  `;
+
+  const [[defaultRoleResult], [membersWithRoleResult], { threadInfos }] =
+    await Promise.all([
+      dbQuery(defaultRoleQuery),
+      dbQuery(membersWithRoleQuery),
+      fetchThreadInfos(viewer, {
+        threadID: community,
+      }),
+    ]);
+  const threadInfo = threadInfos[community];
+
+  if (!threadInfo) {
+    throw new ServerError('invalid_parameters');
+  }
+
+  const defaultRoleID = defaultRoleResult[0].default_role.toString();
+  const membersWithRole = membersWithRoleResult.map(result => result.user);
+  const adminRoleID = Object.keys(threadInfo.roles).find(
+    role => threadInfo.roles[role].name === 'Admins',
+  );
+
+  if (roleID === defaultRoleID || roleID === adminRoleID) {
+    throw new ServerError('invalid_parameters');
+  }
+
+  if (membersWithRole.length > 0) {
+    await updateRole(viewer, {
+      threadID: community,
+      memberIDs: membersWithRole,
+      role: defaultRoleID,
+    });
+  }
+
+  const deleteFromRolesQuery = SQL`
+    DELETE FROM roles
+    WHERE id = ${roleID}
+      AND thread = ${community}
+  `;
+
+  await dbQuery(deleteFromRolesQuery);
+
+  const fetchServerThreadInfosResult = await fetchServerThreadInfos({
+    threadID: community,
+  });
+  const { threadInfos: serverThreadInfos } = fetchServerThreadInfosResult;
+  const serverThreadInfo = serverThreadInfos[community];
+
+  const time = Date.now();
+
+  const updateDatas = [];
+  for (const memberInfo of serverThreadInfo.members) {
+    updateDatas.push({
+      type: updateTypes.UPDATE_THREAD,
+      userID: memberInfo.id,
+      time,
+      threadID: community,
+    });
+  }
+
+  const { viewerUpdates } = await createUpdates(updateDatas, {
+    viewer,
+    updatesForCurrentSession: 'return',
+  });
+
+  const { threadInfos: rawThreadInfos } = rawThreadInfosFromServerThreadInfos(
+    viewer,
+    fetchServerThreadInfosResult,
+  );
+  const rawThreadInfo = rawThreadInfos[community];
+
+  return {
+    threadInfo: rawThreadInfo,
+    updatesResult: {
+      newUpdates: viewerUpdates,
+    },
+  };
+}
+
+export { deleteOrphanedRoles, deleteRole };
