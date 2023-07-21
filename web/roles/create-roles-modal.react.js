91833b7afeece3ff7b8efc3354375bd2662c0351
Rohan Khanderia <rohan.khanderia@gmail.com>
2023-07-21 11:45:53 -0400

    [web] Introduce client-side error handling to prevent duplicate role names
    Summary:
    This diff adds client-side error handling for preventing a user from trying to create a role with a name that already exists in the community. This is already handled on the `keyserver` and on the
    database with a unique key, but ideally we prevent the client from reaching a bad state in the first place. I confirmed with ted that the designs for this are fine
    
    Depends on D8596
    
    Test Plan: Tried to create a role with an identical name to an existing role and saw the message, then tried to create a role with a unique name to confirm that still works.
    
    Reviewers: atul, ginsu
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8597

--- web/roles/create-roles-modal.react.js
+++ web/roles/create-roles-modal.react.js
@@ -14,6 +14,7 @@ import {
   useServerCall,
   useDispatchActionPromise,
 } from 'lib/utils/action-utils.js';
+import { values } from 'lib/utils/objects.js';
 
 import css from './create-roles-modal.css';
 import {
@@ -47,8 +48,11 @@ function CreateRolesModal(props: CreateRolesModalProps): React.Node {
   const [pendingRolePermissions, setPendingRolePermissions] =
     React.useState<$ReadOnlyArray<UserSurfacedPermission>>(rolePermissions);
 
+  const [shouldShowError, setShouldShowError] = React.useState<boolean>(false);
+
   const onChangeRoleName = React.useCallback(
     (event: SyntheticEvent<HTMLInputElement>) => {
+      setShouldShowError(false);
       setPendingRoleName(event.currentTarget.value);
     },
     [],
@@ -131,8 +135,17 @@ function CreateRolesModal(props: CreateRolesModalProps): React.Node {
     ],
   );
 
+  const errorMessageClassNames = classNames({
+    [css.errorMessage]: true,
+    [css.errorMessageVisible]: shouldShowError,
+  });
+
   const onClickCreateRole = React.useCallback(() => {
-    // TODO: Error handling in a later diff
+    const threadRoleNames = values(threadInfo.roles).map(role => role.name);
+    if (threadRoleNames.includes(pendingRoleName) && action === 'create_role') {
+      setShouldShowError(true);
+      return;
+    }
 
     dispatchActionPromise(
       modifyCommunityRoleActionTypes,
@@ -167,6 +180,9 @@ function CreateRolesModal(props: CreateRolesModalProps): React.Node {
             value={pendingRoleName}
             onChange={onChangeRoleName}
           />
+        </div>
+        <div className={errorMessageClassNames}>
+          There is already a role with this name in the community
         </div>
       </form>
       <hr className={css.separator} />
