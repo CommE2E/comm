90567f87602c9ef2103e6fc7c7af74b3776b6904
xsanm <kkurowski.kk@gmail.com>
2023-07-24 11:43:14 +0200

    [CommCoreModule] introduce BaseDataStore template class
    Summary:
    **Problems I want to solve:**
    1. Code duplication - [this part of code](https://github.com/CommE2E/comm/blob/7a6f37fbccf9a419ccdb45fc09d0152cf0306574/native/cpp/CommonCpp/NativeModules/CommCoreModule.cpp#L753-L819) is duplicated at least 5 times (and will grow).
    
    2. Lack of separation - a lot of not related code like parsing is here, this can be grouped logically.
    
    **How it works:**
    1. This template class provides two generic functions which will be reused (processStoreOperations, processStoreOperationsSync) - this solves problem 1.
    2. Has two pure virtual methods which need to be overridden (parseDBDataStore, createOperations), those methods are specialized for each data type (Messages, Threads, etc.) - this solves problem 2.
    
    **Why `.h` file?**
    When a template is used in C++, the compiler needs access to the entire template definition (not just a declaration) because it needs to generate the code for the specific instantiation at the compile time. ([source](https://isocpp.org/wiki/faq/templates#templates-defn-vs-decl)).
    
    **Why not static methods when the class does not have a state?**
    I wanted to ensure that each child class (which will inherit from `BaseDataStore`) will override methods for parsing and creating ops. This can not be achieved easily with static methods - but I am open to modifying if someone has valid suggestions.
    
    For more details check the next diff in the stack.
    
    Depends on D8601
    
    Test Plan: Build iOS and Android (rest is tested in detailed implementation in next diffs).
    
    Reviewers: michal, jon, atul, tomek, marcin
    
    Reviewed By: michal, jon, atul, tomek
    
    Subscribers: marcin, ashoat
    
    Differential Revision: https://phab.comm.dev/D8603

---
+++ native/cpp/CommonCpp/NativeModules/PersistentStorageUtilities/DataStores/BaseDataStore.h
@@ -0,0 +1,103 @@
+#pragma once
+
+#include "DatabaseManager.h"
+#include "GlobalDBSingleton.h"
+#include "NativeModuleUtils.h"
+#include "WorkerThread.h"
+
+#include <ReactCommon/TurboModuleUtils.h>
+#include <jsi/jsi.h>
+#include <vector>
+
+namespace comm {
+
+namespace jsi = facebook::jsi;
+
+template <typename Operation, typename Entity> class BaseDataStore {
+private:
+  std::shared_ptr<facebook::react::CallInvoker> jsInvoker;
+
+public:
+  BaseDataStore(std::shared_ptr<facebook::react::CallInvoker> _jsInvoker)
+      : jsInvoker(_jsInvoker) {
+  }
+
+  virtual ~BaseDataStore(){};
+
+  virtual std::vector<std::unique_ptr<Operation>>
+  createOperations(jsi::Runtime &rt, const jsi::Array &operations) const = 0;
+
+  virtual jsi::Array parseDBDataStore(
+      jsi::Runtime &rt,
+      std::shared_ptr<std::vector<Entity>> dataVectorPtr) const = 0;
+
+  jsi::Value processStoreOperations(jsi::Runtime &rt, jsi::Array &&operations) {
+    std::string createOperationsError;
+    std::shared_ptr<std::vector<std::unique_ptr<Operation>>> storeOpsPtr;
+
+    try {
+      auto storeOps = createOperations(rt, operations);
+      storeOpsPtr = std::make_shared<std::vector<std::unique_ptr<Operation>>>(
+          std::move(storeOps));
+    } catch (std::runtime_error &e) {
+      createOperationsError = e.what();
+    }
+
+    return facebook::react::createPromiseAsJSIValue(
+        rt,
+        [=](jsi::Runtime &innerRt,
+            std::shared_ptr<facebook::react::Promise> promise) {
+          taskType job = [=]() {
+            std::string error = createOperationsError;
+
+            if (!error.size()) {
+              try {
+                DatabaseManager::getQueryExecutor().beginTransaction();
+                for (const auto &operation : *storeOpsPtr) {
+                  operation->execute();
+                }
+                DatabaseManager::getQueryExecutor().commitTransaction();
+              } catch (std::system_error &e) {
+                error = e.what();
+                DatabaseManager::getQueryExecutor().rollbackTransaction();
+              }
+            }
+
+            this->jsInvoker->invokeAsync([=]() {
+              if (error.size()) {
+                promise->reject(error);
+              } else {
+                promise->resolve(jsi::Value::undefined());
+              }
+            });
+          };
+          GlobalDBSingleton::instance.scheduleOrRunCancellable(
+              job, promise, this->jsInvoker);
+        });
+  }
+
+  void processStoreOperationsSync(jsi::Runtime &rt, jsi::Array &&operations) {
+    std::vector<std::unique_ptr<Operation>> storeOps;
+
+    try {
+      storeOps = createOperations(rt, operations);
+    } catch (const std::exception &e) {
+      throw jsi::JSError(rt, e.what());
+    }
+
+    NativeModuleUtils::runSyncOrThrowJSError<void>(rt, [&storeOps]() {
+      try {
+        DatabaseManager::getQueryExecutor().beginTransaction();
+        for (const auto &operation : storeOps) {
+          operation->execute();
+        }
+        DatabaseManager::getQueryExecutor().commitTransaction();
+      } catch (const std::exception &e) {
+        DatabaseManager::getQueryExecutor().rollbackTransaction();
+        throw e;
+      }
+    });
+  }
+};
+
+} // namespace comm
