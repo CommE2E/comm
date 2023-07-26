ff4ecfe390fe788d3fa43c62748c38f576394f7c
Rohan Khanderia <rohan.khanderia@gmail.com>
2023-07-26 14:57:19 -0400

    [web] Push the create role modal with prepopulated fields when Edit Role is clicked
    Summary:
    This diff essentially handles the edit role workflow on web, where the permission enum options will be pre-selected if previously selected. The logic is similar to what was done in D8524 on `native`
    
    [[ https://linear.app/comm/issue/ENG-4430/push-the-create-role-modal-with-prepopulated-fields-when-edit-role-is | ENG-4430 ]]
    
    Depends on D8623
    
    Test Plan: Verified that the modal is opened with the correct pre-populated fields after creating a new role then trying to edit it
    
    Reviewers: atul, ginsu
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8624

--- web/roles/create-roles-modal.react.js
+++ web/roles/create-roles-modal.react.js
@@ -65,6 +65,8 @@ function CreateRolesModal(props: CreateRolesModalProps): React.Node {
   const [roleCreationFailed, setRoleCreationFailed] =
     React.useState<?RoleCreationErrorVariant>();
 
+  const createButtonText = action === 'create_role' ? 'Create' : 'Save';
+
   const onChangeRoleName = React.useCallback(
     (event: SyntheticEvent<HTMLInputElement>) => {
       setRoleCreationFailed(null);
@@ -218,8 +220,8 @@ function CreateRolesModal(props: CreateRolesModalProps): React.Node {
       );
     }
 
-    return 'Save';
-  }, [createRolesLoadingStatus]);
+    return createButtonText;
+  }, [createRolesLoadingStatus, createButtonText]);
 
   return (
     <Modal name="Create Role" onClose={onCloseModal} size="large">
