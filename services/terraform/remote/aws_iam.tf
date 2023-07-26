4ccf652afd59b184ed77ac63a17fe9f9eb030314
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2023-07-26 10:49:50 +0200

    [services][terraform] Set up IAM for feature-flags service
    Summary:
    Sets up IAM for feature-flags service. Also this is example of how to set up IAM to give minimum required permissions to a service.
    
    Created a role that:
    - Can be assumed by EC2 instances and ECS tasks - basically they are allowed to use it
    - Allows read operations on feature-flags DDB table
    
    Depends on D8583
    
    Test Plan: Tested together with next diff - they're live on AWS now.
    
    Reviewers: tomek, michal, jon, varun
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D8635

--- services/terraform/remote/aws_iam.tf
+++ services/terraform/remote/aws_iam.tf
@@ -7,3 +7,56 @@ data "aws_iam_role" "ecs_task_execution" {
 data "aws_iam_role" "services_ddb_full_access" {
   name = "dynamodb-s3-full-access"
 }
+
+# Assume Role Policy Document for EC2 and ECS
+# This policy allows ECS and EC2 use roles that it is assigned to
+data "aws_iam_policy_document" "assume_role_ecs_ec2" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "sts:AssumeRole",
+    ]
+    principals {
+      type = "Service"
+      identifiers = [
+        "ec2.amazonaws.com",
+        "ecs-tasks.amazonaws.com"
+      ]
+    }
+  }
+}
+
+# Feature Flags IAM
+data "aws_dynamodb_table" "feature_flags" {
+  name = "feature-flags"
+}
+data "aws_iam_policy_document" "read_feature_flags" {
+  statement {
+    sid    = "FeatureFlagsDDBReadAccess"
+    effect = "Allow"
+    actions = [
+      "dynamodb:BatchGetItem",
+      "dynamodb:GetItem",
+      "dynamodb:Query",
+      "dynamodb:Scan",
+    ]
+    resources = [
+      data.aws_dynamodb_table.feature_flags.arn,
+    ]
+  }
+}
+resource "aws_iam_policy" "read_feature_flags" {
+  name        = "feature-flags-ddb-read-access"
+  policy      = data.aws_iam_policy_document.read_feature_flags.json
+  description = "Allows full read access to feature-flags DynamoDB table"
+}
+
+resource "aws_iam_role" "feature_flags_service" {
+  name               = "feature-flags-service-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_ecs_ec2.json
+
+  managed_policy_arns = [
+    aws_iam_policy.read_feature_flags.arn
+  ]
+}
+
