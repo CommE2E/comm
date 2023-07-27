0b1634102493b2c23e1eafa2e399568bfac854f8
Rohan Khanderia <rohan.khanderia@gmail.com>
2023-07-27 04:59:46 -0400

    [native] Introduce client-side logic for deleting roles on native
    Summary:
    Split out from D8574, this diff introduces the client-side logic and behavior for deleting roles on `native`.
    
    Depends on D8639
    
    Test Plan: Same test plan / results video as was originally in D8574
    
    Reviewers: atul, ginsu
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8640

---
+++ native/roles/role-utils.react.js
@@ -0,0 +1,64 @@
+// @flow
+
+import * as React from 'react';
+import { Alert } from 'react-native';
+
+import {
+  deleteCommunityRole,
+  deleteCommunityRoleActionTypes,
+} from 'lib/actions/thread-actions.js';
+import type { ThreadInfo } from 'lib/types/thread-types.js';
+import {
+  useDispatchActionPromise,
+  useServerCall,
+} from 'lib/utils/action-utils.js';
+import { constructRoleDeletionMessagePrompt } from 'lib/utils/role-utils.js';
+
+function useDisplayDeleteRoleAlert(
+  threadInfo: ThreadInfo,
+  existingRoleID: string,
+  defaultRoleID: string,
+  memberCount: number,
+): () => void {
+  const defaultRoleName = threadInfo.roles[defaultRoleID].name;
+  const callDeleteCommunityRole = useServerCall(deleteCommunityRole);
+  const dispatchActionPromise = useDispatchActionPromise();
+
+  const onDeleteRole = React.useCallback(() => {
+    dispatchActionPromise(
+      deleteCommunityRoleActionTypes,
+      callDeleteCommunityRole({
+        community: threadInfo.id,
+        roleID: existingRoleID,
+      }),
+    );
+  }, [
+    callDeleteCommunityRole,
+    dispatchActionPromise,
+    existingRoleID,
+    threadInfo.id,
+  ]);
+
+  const message = constructRoleDeletionMessagePrompt(
+    defaultRoleName,
+    memberCount,
+  );
+
+  return React.useCallback(
+    () =>
+      Alert.alert('Delete role', message, [
+        {
+          text: 'Yes, delete role',
+          style: 'destructive',
+          onPress: onDeleteRole,
+        },
+        {
+          text: 'Cancel',
+          style: 'cancel',
+        },
+      ]),
+    [message, onDeleteRole],
+  );
+}
+
+export { useDisplayDeleteRoleAlert };
