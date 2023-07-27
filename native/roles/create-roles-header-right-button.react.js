0e3968a960c75830c7c597f2bdf5b0b0f5237163
Rohan Khanderia <rohan.khanderia@gmail.com>
2023-07-27 04:53:25 -0400

    [keyserver/lib/native] Create updates for new role creation
    Summary:
    We want to generate updates for the thread when a new role is created and each member of that thread should recieve the update. The next diff will handle the client-side update of pulling the updated
    threadInfo from redux (a solution discussed in my 1:1 with Ashoat).
    
    Depends on D8421
    
    Test Plan:
    Verified that the `threadInfo` was successfully updated in redux after the updates were generated, and checked the thread store operations in the reducer and confirmed that the thread that needed
    to be replaced was the one that just had a new role created for it.
    
    Reviewers: atul, ginsu
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8422

--- native/roles/create-roles-header-right-button.react.js
+++ native/roles/create-roles-header-right-button.react.js
@@ -4,8 +4,14 @@ import { useNavigation } from '@react-navigation/native';
 import * as React from 'react';
 import { TouchableOpacity, Text } from 'react-native';
 
-import { modifyCommunityRole } from 'lib/actions/thread-actions.js';
-import { useServerCall } from 'lib/utils/action-utils.js';
+import {
+  modifyCommunityRole,
+  modifyCommunityRoleActionTypes,
+} from 'lib/actions/thread-actions.js';
+import {
+  useServerCall,
+  useDispatchActionPromise,
+} from 'lib/utils/action-utils.js';
 
 import type { NavigationRoute } from '../navigation/route-names';
 import { useStyles } from '../themes/colors.js';
@@ -20,18 +26,23 @@ function CreateRolesHeaderRightButton(props: Props): React.Node {
   const styles = useStyles(unboundStyles);
 
   const callModifyCommunityRole = useServerCall(modifyCommunityRole);
+  const dispatchActionPromise = useDispatchActionPromise();
 
   const onPressCreate = React.useCallback(() => {
-    callModifyCommunityRole({
-      community: threadInfo.id,
-      action,
-      name: roleName,
-      permissions: [...rolePermissions],
-    });
+    dispatchActionPromise(
+      modifyCommunityRoleActionTypes,
+      callModifyCommunityRole({
+        community: threadInfo.id,
+        action,
+        name: roleName,
+        permissions: [...rolePermissions],
+      }),
+    );
 
     navigation.goBack();
   }, [
     callModifyCommunityRole,
+    dispatchActionPromise,
     threadInfo,
     action,
     roleName,
