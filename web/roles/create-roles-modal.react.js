b16f4666b93bfda2a73f7dc124b6a52a845b53e4
Rohan Khanderia <rohan.khanderia@gmail.com>
2023-07-27 05:19:36 -0400

    [web] Add save functionality to the create role modal
    Summary:
    This adds functionality to the Create button in the create-role modal, where clicking it will save the role and close the modal.
    
    [[ https://linear.app/comm/issue/ENG-4434/add-save-functionality-to-the-create-role-modal | ENG-4434 ]]
    
    Depends on D8594
    
    Test Plan: Confirmed that a new role is created when the button is clicked
    
    Reviewers: atul, ginsu
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8595

--- web/roles/create-roles-modal.react.js
+++ web/roles/create-roles-modal.react.js
@@ -3,30 +3,55 @@
 import classNames from 'classnames';
 import * as React from 'react';
 
+import {
+  modifyCommunityRole,
+  modifyCommunityRoleActionTypes,
+} from 'lib/actions/thread-actions.js';
 import { useModalContext } from 'lib/components/modal-provider.react.js';
+import { createLoadingStatusSelector } from 'lib/selectors/loading-selectors.js';
+import type { LoadingStatus } from 'lib/types/loading-types.js';
 import type {
   UserSurfacedPermission,
   UserSurfacedPermissionOption,
 } from 'lib/types/thread-permission-types.js';
 import type { ThreadInfo } from 'lib/types/thread-types.js';
+import {
+  useServerCall,
+  useDispatchActionPromise,
+} from 'lib/utils/action-utils.js';
 import { useFilterPermissionOptionsByThreadType } from 'lib/utils/role-utils.js';
 
 import css from './create-roles-modal.css';
 import Button, { buttonThemes } from '../components/button.react.js';
 import EnumSettingsOption from '../components/enum-settings-option.react.js';
+import LoadingIndicator from '../loading-indicator.react.js';
 import Input from '../modals/input.react.js';
 import Modal from '../modals/modal.react.js';
+import { useSelector } from '../redux/redux-utils.js';
+
+const createRolesLoadingStatusSelector = createLoadingStatusSelector(
+  modifyCommunityRoleActionTypes,
+);
 
 type CreateRolesModalProps = {
   +threadInfo: ThreadInfo,
   +action: 'create_role' | 'edit_role',
+  +existingRoleID?: string,
   +roleName: string,
   +rolePermissions: $ReadOnlySet<UserSurfacedPermission>,
 };
 
 function CreateRolesModal(props: CreateRolesModalProps): React.Node {
   const { popModal } = useModalContext();
-  const { threadInfo, roleName, rolePermissions } = props;
+  const { threadInfo, action, existingRoleID, roleName, rolePermissions } =
+    props;
+
+  const callModifyCommunityRole = useServerCall(modifyCommunityRole);
+  const dispatchActionPromise = useDispatchActionPromise();
+
+  const createRolesLoadingStatus: LoadingStatus = useSelector(
+    createRolesLoadingStatusSelector,
+  );
 
   const [pendingRoleName, setPendingRoleName] =
     React.useState<string>(roleName);
@@ -101,6 +126,44 @@ function CreateRolesModal(props: CreateRolesModalProps): React.Node {
     ],
   );
 
+  const onClickCreateRole = React.useCallback(() => {
+    // TODO: Error handling in a later diff
+
+    dispatchActionPromise(
+      modifyCommunityRoleActionTypes,
+      (async () => {
+        const response = await callModifyCommunityRole({
+          community: threadInfo.id,
+          existingRoleID,
+          action,
+          name: pendingRoleName,
+          permissions: [...pendingRolePermissions],
+        });
+        popModal();
+        return response;
+      })(),
+    );
+  }, [
+    callModifyCommunityRole,
+    dispatchActionPromise,
+    threadInfo,
+    action,
+    existingRoleID,
+    pendingRoleName,
+    pendingRolePermissions,
+    popModal,
+  ]);
+
+  const saveButtonContent = React.useMemo(() => {
+    if (createRolesLoadingStatus === 'loading') {
+      return (
+        <LoadingIndicator status={createRolesLoadingStatus} size="medium" />
+      );
+    }
+
+    return 'Save';
+  }, [createRolesLoadingStatus]);
+
   return (
     <Modal name="Create Role" onClose={onCloseModal} size="large">
       <form method="POST" className={css.formContainer}>
@@ -137,9 +200,9 @@ function CreateRolesModal(props: CreateRolesModalProps): React.Node {
           variant="filled"
           className={css.createRoleButton}
           buttonColor={buttonThemes.standard}
-          onClick={null}
+          onClick={onClickCreateRole}
         >
-          Create
+          {saveButtonContent}
         </Button>
       </div>
     </Modal>
