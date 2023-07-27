aa6a8cad90e63c2cf12f24f827e59e56812790e4
Rohan Khanderia <rohan.khanderia@gmail.com>
2023-07-27 03:56:47 -0400

    [web] Add an option to edit and delete select roles
    Summary:
    This diff introduces the dropdown select options to edit a role and delete a role. The implementation of editing roles will come in the next diff, and the implementation of deleting roles will come
    in the following one. Here, all roles besides `Admins` can be edited (we don't have a better way to check for Admins than by name for now, once [[
    https://linear.app/comm/issue/ENG-4155/add-a-admin-role-column-for-threads ENG-4155 ]] is taken on, that will be the better way). Similarly, all roles besides `Members` and `Admins` can be deleted.
    
    Part of [[ https://linear.app/comm/issue/ENG-4429/add-an-option-to-edit-roles-for-all-roles-besides-admins | ENG-4429 ]] and [[
    https://linear.app/comm/issue/ENG-4431/show-an-option-to-delete-roles-besides-admins-and-members | ENG-4431 ]].
    
    Depends on D8597
    
    Test Plan: Confirmed that the options appear for each role as expected
    
    Reviewers: atul, ginsu
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8623

--- web/roles/role-panel-entry.react.js
+++ web/roles/role-panel-entry.react.js
@@ -2,22 +2,29 @@
 
 import * as React from 'react';
 
+import type { ThreadInfo } from 'lib/types/thread-types.js';
+
+import RoleActionsMenu from './role-actions-menu.react.js';
 import css from './role-panel-entry.css';
 import CommIcon from '../CommIcon.react.js';
 
 type RolePanelEntryProps = {
+  +threadInfo: ThreadInfo,
   +roleName: string,
   +memberCount: number,
 };
 
 function RolePanelEntry(props: RolePanelEntryProps): React.Node {
-  const { roleName, memberCount } = props;
+  const { threadInfo, roleName, memberCount } = props;
   return (
     <div className={css.rolePanelEntry}>
       <div className={css.rolePanelNameEntry}>{roleName}</div>
       <div className={css.rolePanelCountEntryContainer}>
-        <div className={css.rolePanelCountEntry}>{memberCount}</div>
-        <CommIcon icon="user-filled" size={18} />
+        <div className={css.rolePanelCountAndIcon}>
+          <div className={css.rolePanelCountEntry}>{memberCount}</div>
+          <CommIcon icon="user-filled" size={18} />
+        </div>
+        <RoleActionsMenu threadInfo={threadInfo} roleName={roleName} />
       </div>
     </div>
   );
