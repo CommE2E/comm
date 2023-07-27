637d5273be33c1482d3bc41fc5e35576d8618ea5
Jonathan Ringer <jonathan.ringer@comm.app>
2023-07-27 10:59:14 -0700

    [Identity] Add DDB table for onetime-keys
    Summary:
    Add new one-time-keys table.
    
    https://linear.app/comm/issue/ENG-4535
    
    Depends on D8642
    
    Test Plan:
    ```
    cd services/terraform/dev
    ./run.sh
    ```
    The `identity-one-time-keys` table should be created.
    
    The constants in `service/identity/` are tested later
    
    Reviewers: bartek, varun
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8675

---
+++ services/identity/src/ddb_utils.rs
@@ -0,0 +1,58 @@
+use aws_sdk_dynamodb::model::{AttributeValue, PutRequest, WriteRequest};
+use std::collections::HashMap;
+use std::iter::IntoIterator;
+
+#[derive(Copy, Clone, Debug)]
+pub enum OlmAccountType {
+  Content,
+  Notification,
+}
+
+// Prefix the one time keys with the olm account variant. This allows for a single
+// DDB table to contain both notification and content keys for a device.
+fn create_one_time_key_partition_key(
+  device_id: &str,
+  account_type: OlmAccountType,
+) -> String {
+  match account_type {
+    OlmAccountType::Content => format!("content_{device_id}"),
+    OlmAccountType::Notification => format!("notification_{device_id}"),
+  }
+}
+
+fn create_one_time_key_put_request(
+  device_id: &str,
+  one_time_key: String,
+  account_type: OlmAccountType,
+) -> WriteRequest {
+  use crate::constants::one_time_keys_table::*;
+
+  let partition_key =
+    create_one_time_key_partition_key(device_id, account_type);
+  let builder = PutRequest::builder();
+  let attrs = HashMap::from([
+    (PARTITION_KEY.to_string(), AttributeValue::S(partition_key)),
+    (SORT_KEY.to_string(), AttributeValue::S(one_time_key)),
+  ]);
+
+  let put_request = builder.set_item(Some(attrs)).build();
+
+  WriteRequest::builder().put_request(put_request).build()
+}
+
+pub fn into_one_time_put_requests<T>(
+  device_id: &str,
+  one_time_keys: T,
+  account_type: OlmAccountType,
+) -> Vec<WriteRequest>
+where
+  T: IntoIterator,
+  <T as IntoIterator>::Item: ToString,
+{
+  one_time_keys
+    .into_iter()
+    .map(|otk| {
+      create_one_time_key_put_request(device_id, otk.to_string(), account_type)
+    })
+    .collect()
+}
