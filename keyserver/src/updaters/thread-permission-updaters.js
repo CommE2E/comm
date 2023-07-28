CommE2E/comm

commit c4f1cf4a2253a14ddd633238b6dd60ad289b3587

[keyserver][lib] Simplify remaining thread responders following deprecation of old codeVersions
Summary:
Doing these together because they share the same types and validators.

This is the last of four diffs that simplify the code, types, and validators for endpoints mutating threads. These can be simplified following the deprecation of older `codeVersion`s in the first diff of this stack.

I finally update the code in `thread-permission-updaters.js` that I mentioned [here](https://linear.app/comm/issue/ENG-108#comment-8b4a79c8), which is sort of the whole point of this stack. I'm hoping this change will improve performance of most of these endpoints.

Depends on D8508

Test Plan: I tested this whole stack together by playing around with the web app and making sure thread mutation endpoints seemed to work correctly

Reviewers: michal, kamil

Subscribers: tomek

Differential Revision: https://phab.comm.dev/D8509

keyserver/src/updaters/thread-permission-updaters.js
@@ -36,7 +36,6 @@ import { dbQuery, SQL } from '../database/database.js';
 import {
   fetchServerThreadInfos,
   rawThreadInfosFromServerThreadInfos,
-  type FetchThreadInfosResult,
 } from '../fetchers/thread-fetchers.js';
 import { rescindPushNotifs } from '../push/rescind.js';
 import { createScriptViewer } from '../session/scripts.js';
@@ -975,7 +974,6 @@ const emptyCommitMembershipChangesetConfig = Object.freeze({});
 // only needs to be specified if a JOIN_THREAD update will be generated for the
 // viewer, in which case it's necessary for knowing the set of entries to fetch.
 type ChangesetCommitResult = {
-  ...FetchThreadInfosResult,
   ...CreateUpdatesResult,
 };
 async function commitMembershipChangeset(
@@ -1054,10 +1052,9 @@ async function commitMembershipChangeset(
     rescindPushNotifsForMemberDeletion(toRescindPushNotifs),
   ]);
 
-  // We fetch all threads here because old clients still expect the full list of
-  // threads on most thread operations. Once verifyClientSupported gates on
-  // codeVersion 62, we can add a WHERE clause on changedThreadIDs here
-  const serverThreadInfoFetchResult = await fetchServerThreadInfos();
+  const serverThreadInfoFetchResult = await fetchServerThreadInfos(
+    SQL`t.id IN (${[...changedThreadIDs]})`,
+  );
   const { threadInfos: serverThreadInfos } = serverThreadInfoFetchResult;
 
   const time = Date.now();
@@ -1117,7 +1114,6 @@ async function commitMembershipChangeset(
   });
 
   return {
-    ...threadInfoFetchResult,
     userInfos,
     viewerUpdates,
   };

keyserver/src/responders/thread-responders.js
@@ -25,7 +25,6 @@ import {
   type ThreadFetchMediaRequest,
   type ToggleMessagePinRequest,
   type ToggleMessagePinResult,
-  rawThreadInfoValidator,
 } from 'lib/types/thread-types.js';
 import { serverUpdateInfoValidator } from 'lib/types/update-types.js';
 import { userInfosValidator } from 'lib/types/user-types.js';
@@ -102,8 +101,6 @@ export const roleChangeRequestInputValidator: TInterface<RoleChangeRequest> =
 export const changeThreadSettingsResultValidator: TInterface<ChangeThreadSettingsResult> =
   tShape<ChangeThreadSettingsResult>({
     newMessageInfos: t.list(rawMessageInfoValidator),
-    threadInfo: t.maybe(rawThreadInfoValidator),
-    threadInfos: t.maybe(t.dict(tID, rawThreadInfoValidator)),
     updatesResult: tShape({
       newUpdates: t.list(serverUpdateInfoValidator),
     }),

keyserver/src/updaters/thread-updaters.js
@@ -10,7 +10,6 @@ import {
   getThreadTypeParentRequirement,
   validChatNameRegex,
 } from 'lib/shared/thread-utils.js';
-import { hasMinCodeVersion } from 'lib/shared/version-utils.js';
 import type { Shape } from 'lib/types/core.js';
 import { messageTypes } from 'lib/types/message-types-enum.js';
 import { threadPermissions } from 'lib/types/thread-permission-types.js';
@@ -138,10 +137,7 @@ async function updateRole(
   }
 
   const changeset = await changeRole(request.threadID, memberIDs, request.role);
-  const { threadInfos, viewerUpdates } = await commitMembershipChangeset(
-    viewer,
-    changeset,
-  );
+  const { viewerUpdates } = await commitMembershipChangeset(viewer, changeset);
 
   const messageData = {
     type: messageTypes.CHANGE_ROLE,
@@ -153,18 +149,7 @@ async function updateRole(
   };
   const newMessageInfos = await createMessages(viewer, [messageData]);
 
-  if (hasMinCodeVersion(viewer.platformDetails, { native: 62 })) {
-    return { updatesResult: { newUpdates: viewerUpdates }, newMessageInfos };
-  }
-
-  return {
-    threadInfo: threadInfos[request.threadID],
-    threadInfos,
-    updatesResult: {
-      newUpdates: viewerUpdates,
-    },
-    newMessageInfos,
-  };
+  return { updatesResult: { newUpdates: viewerUpdates }, newMessageInfos };
 }
 
 async function removeMembers(
@@ -224,10 +209,7 @@ async function removeMembers(
   }
 
   const changeset = await changeRole(request.threadID, actualMemberIDs, 0);
-  const { threadInfos, viewerUpdates } = await commitMembershipChangeset(
-    viewer,
-    changeset,
-  );
+  const { viewerUpdates } = await commitMembershipChangeset(viewer, changeset);
 
   const newMessageInfos = await (async () => {
     if (actualMemberIDs.length === 0) {
@@ -243,18 +225,7 @@ async function removeMembers(
     return await createMessages(viewer, [messageData]);
   })();
 
-  if (hasMinCodeVersion(viewer.platformDetails, { native: 62 })) {
-    return { updatesResult: { newUpdates: viewerUpdates }, newMessageInfos };
-  }
-
-  return {
-    threadInfo: threadInfos[request.threadID],
-    threadInfos,
-    updatesResult: {
-      newUpdates: viewerUpdates,
-    },
-    newMessageInfos,
-  };
+  return { updatesResult: { newUpdates: viewerUpdates }, newMessageInfos };
 }
 
 async function leaveThread(
@@ -752,18 +723,14 @@ async function updateThread(
   }
 
   const changeset = { membershipRows, relationshipChangeset };
-  const { threadInfos, viewerUpdates } = await commitMembershipChangeset(
-    viewer,
-    changeset,
-    {
-      // This forces an update for this thread,
-      // regardless of whether any membership rows are changed
-      changedThreadIDs:
-        Object.keys(sqlUpdate).length > 0
-          ? new Set([request.threadID])
-          : new Set(),
-    },
-  );
+  const { viewerUpdates } = await commitMembershipChangeset(viewer, changeset, {
+    // This forces an update for this thread,
+    // regardless of whether any membership rows are changed
+    changedThreadIDs:
+      Object.keys(sqlUpdate).length > 0
+        ? new Set([request.threadID])
+        : new Set(),
+  });
 
   let newMessageInfos = [];
   if (!silenceMessages) {
@@ -792,18 +759,7 @@ async function updateThread(
     newMessageInfos = await createMessages(viewer, messageDatas);
   }
 
-  if (hasMinCodeVersion(viewer.platformDetails, { native: 62 })) {
-    return { updatesResult: { newUpdates: viewerUpdates }, newMessageInfos };
-  }
-
-  return {
-    threadInfo: threadInfos[request.threadID],
-    threadInfos,
-    updatesResult: {
-      newUpdates: viewerUpdates,
-    },
-    newMessageInfos,
-  };
+  return { updatesResult: { newUpdates: viewerUpdates }, newMessageInfos };
 }
 
 async function joinThread(

lib/types/thread-types.js
@@ -282,8 +282,6 @@ export type RoleChangeRequest = {
 };
 
 export type ChangeThreadSettingsResult = {
-  +threadInfo?: RawThreadInfo,
-  +threadInfos?: { +[id: string]: RawThreadInfo },
   +updatesResult: {
     +newUpdates: $ReadOnlyArray<ServerUpdateInfo>,
   },

commit 70bf6367087c23bfa5fbecec2ec1b87042a7253f

[keyserver] Make sure we don't call fetchServerThreadInfos with empty threadIDs
Summary:
This diff fixes a regression introduced in D8509. We should never specify an empty `threadIDs` parameter to the `FetchThreadInfosFilter` we pass to `fetchServerThreadInfos`.

This diff addresses an urgent regression – see [ENG-4529](https://linear.app/comm/issue/ENG-4529/sql-error-breaking-message-sends) for details.

Test Plan: I confirmed that created a message in a thread with an @-tag of somebody who is already in the thread does not cause the error described in the linked task

Reviewers: atul, tomek, michal, bartek, kamil, inka, ginsu, varun

Subscribers:

keyserver/src/updaters/thread-permission-updaters.js
@@ -1052,9 +1052,14 @@ async function commitMembershipChangeset(
     rescindPushNotifsForMemberDeletion(toRescindPushNotifs),
   ]);
 
-  const serverThreadInfoFetchResult = await fetchServerThreadInfos({
-    threadIDs: changedThreadIDs,
-  });
+  const serverThreadInfoFetchResult = await (async () => {
+    if (changedThreadIDs.size === 0) {
+      return { threadInfos: {} };
+    }
+    return await fetchServerThreadInfos({
+      threadIDs: changedThreadIDs,
+    });
+  })();
   const { threadInfos: serverThreadInfos } = serverThreadInfoFetchResult;
 
   const time = Date.now();
@@ -1074,6 +1079,7 @@ async function commitMembershipChangeset(
       });
     }
   }
+
   for (const row of membershipRowMap.values()) {
     const { userID, threadID } = row;
     if (row.operation === 'delete' || row.role === '-1') {