61ceaa086b6542a79bc6cd7abe18dbdf9ce70a20
xsanm <kkurowski.kk@gmail.com>
2023-07-28 16:15:14 +0200

    [native] install CommRustModule
    Summary:
    [ENG-4232](https://linear.app/comm/issue/ENG-4232/create-separate-module-for-rust-native-function)
    
    Creating a separate module for Rust API. This is a part of `CommCoreModule` refactor.
    
    Code to install new module for both iOS and Android.
    
    Depends on D8657
    
    Test Plan:
    Build Android nad iOS
    On iOS added test `generateNonce` to make sure Rust methods are callable. Inspired by test plan from D7215.
    
    Reviewers: bartek, michal, tomek, varun
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D8658

--- native/native-modules.js
+++ native/native-modules.js
@@ -1,10 +1,12 @@
 // @flow
 
 import type { Spec as CoreModuleSpec } from './schema/CommCoreModuleSchema.js';
+import type { Spec as RustModuleSpec } from './schema/CommRustModuleSchema.js';
 import type { UtilsModuleSpec } from './schema/CommUtilsModuleSchema.js';
 
 export const commCoreModule: CoreModuleSpec = global.CommCoreModule;
 export const commUtilsModule: UtilsModuleSpec = global.CommUtilsModule;
+export const commRustModule: RustModuleSpec = global.CommRustModule;
 export const commConstants: {
   +NATIVE_MESSAGE_TYPES: $ReadOnlyArray<number>,
 } = global.CommConstants;
