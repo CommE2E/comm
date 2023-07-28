3aee560e5890c8df9564f3692ec11fd0d5bcee9a
Tomasz Palys <tomasz.palys@swmansion.com>
2023-07-28 17:50:55 +0200

    [native] Cache feature flags config
    Summary:
    Save fetched feature flags config in AsyncStorage. When the app starts, read the value from the storage and override it with a value from service when it is fetched.
    
    This diff is an implementation of point 2 from https://linear.app/comm/issue/ENG-3252/improve-feature-flags-reliability
    
    Test Plan: Ran the app and checked if after fresh start no value is read from the store and config is set after successful fetch. Then checked if restarting the app causes AsyncStorage read and saving to state which is then replaced by the config from service.
    
    Reviewers: kamil, bartek
    
    Reviewed By: bartek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D8652

--- native/utils/crash-utils.js
+++ native/utils/crash-utils.js
@@ -4,6 +4,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import sleep from 'lib/utils/sleep.js';
 
+import { featureFlagsStorageKey } from '../components/feature-flags-provider.react.js';
 import { commCoreModule } from '../native-modules.js';
 import { navStateAsyncStorageKey } from '../navigation/persistance.js';
 import { getPersistor } from '../redux/persist.js';
@@ -13,6 +14,7 @@ async function wipeAndExit() {
     getPersistor().purge(),
     __DEV__ ? AsyncStorage.removeItem(navStateAsyncStorageKey) : null,
     AsyncStorage.removeItem('ANDROID_REFERRER'),
+    AsyncStorage.removeItem(featureFlagsStorageKey),
   ]);
   await sleep(50);
   commCoreModule.terminate();
