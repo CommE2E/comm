77c0ef7d8200c730b5c8a91a0511a666eb270463
xsanm <kkurowski.kk@gmail.com>
2023-07-28 15:40:02 +0200

    [native] add CommRustModule spec
    Summary:
    [ENG-4232](https://linear.app/comm/issue/ENG-4232/create-separate-module-for-rust-native-function)
    
    Creating a separate module for Rust API. This is a part of `CommCoreModule` refactor.
    
    Introducing TurboModule Spec.
    
    Test Plan: Tested later in the stack.
    
    Reviewers: bartek, michal, tomek, varun
    
    Reviewed By: bartek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D8655

---
+++ native/schema/CommRustModuleSchema.js
@@ -0,0 +1,51 @@
+// @flow
+
+'use strict';
+
+import { TurboModuleRegistry } from 'react-native';
+import type { TurboModule } from 'react-native/Libraries/TurboModule/RCTExport.js';
+
+export interface Spec extends TurboModule {
+  +generateNonce: () => Promise<string>;
+  +registerUser: (
+    username: string,
+    password: string,
+    keyPayload: string,
+    keyPayloadSignature: string,
+    contentPrekey: string,
+    contentPrekeySignature: string,
+    notifPrekey: string,
+    notifPrekeySignature: string,
+    contentOneTimeKeys: $ReadOnlyArray<string>,
+    notifOneTimeKeys: $ReadOnlyArray<string>,
+  ) => Promise<string>;
+  +loginPasswordUser: (
+    username: string,
+    password: string,
+    keyPayload: string,
+    keyPayloadSignature: string,
+    contentPrekey: string,
+    contentPrekeySignature: string,
+    notifPrekey: string,
+    notifPrekeySignature: string,
+    contentOneTimeKeys: $ReadOnlyArray<string>,
+    notifOneTimeKeys: $ReadOnlyArray<string>,
+  ) => Promise<string>;
+  +loginWalletUser: (
+    siweMessage: string,
+    siweSignature: string,
+    keyPayload: string,
+    keyPayloadSignature: string,
+    contentPrekey: string,
+    contentPrekeySignature: string,
+    notifPrekey: string,
+    notifPrekeySignature: string,
+    contentOneTimeKeys: $ReadOnlyArray<string>,
+    notifOneTimeKeys: $ReadOnlyArray<string>,
+    socialProof: string,
+  ) => Promise<string>;
+}
+
+export default (TurboModuleRegistry.getEnforcing<Spec>(
+  'CommRustTurboModule',
+): Spec);
