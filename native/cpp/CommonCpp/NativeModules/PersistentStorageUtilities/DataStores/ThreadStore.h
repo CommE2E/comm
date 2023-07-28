ef04728fafd89212ec90029cb7f96008600e9546
xsanm <kkurowski.kk@gmail.com>
2023-07-28 20:04:27 +0200

    [CommCoreModule] implement C++ ThreadStore
    Summary:
    Grouping logic in one class to make it cleaner and reuse some code.
    
    Depends on: D8604
    
    Test Plan: Build iOS and Android and play with threads to make sure functionality still works.
    
    Reviewers: michal, jon, atul, tomek
    
    Reviewed By: tomek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D8659

---
+++ native/cpp/CommonCpp/NativeModules/PersistentStorageUtilities/DataStores/ThreadStore.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "../../../DatabaseManagers/entities/Thread.h"
+#include "BaseDataStore.h"
+#include "ThreadStoreOperations.h"
+
+#include <jsi/jsi.h>
+
+namespace comm {
+
+class ThreadStore : public BaseDataStore<ThreadStoreOperationBase, Thread> {
+private:
+  static OperationType REMOVE_OPERATION;
+  static OperationType REMOVE_ALL_OPERATION;
+  static OperationType REPLACE_OPERATION;
+
+public:
+  ThreadStore(std::shared_ptr<facebook::react::CallInvoker> jsInvoker);
+
+  std::vector<std::unique_ptr<ThreadStoreOperationBase>> createOperations(
+      jsi::Runtime &rt,
+      const jsi::Array &operations) const override;
+
+  jsi::Array parseDBDataStore(
+      jsi::Runtime &rt,
+      std::shared_ptr<std::vector<Thread>> dataVectorPtr) const override;
+};
+
+} // namespace comm
