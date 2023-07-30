3ddf28a609ce8d84fc0a5cca2d27b482a49573eb
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2023-07-30 08:00:02 +0200

    [terraform] Introduce SOPS secrets
    Summary:
    This diff:
    - Introduces `.sops.yaml` configuration file for SOPS files in the repo.
    - Introduces `services/terraform/remote/secrets.json` file that contains some secrets.
      Now it contains a few examples:
      - Prod/staging account IDs
      - Keyserver public key, used by Identity Service
    - Makes these secrets accessible by Terraform
    
    Depends on D8666
    
    Test Plan:
    > Note that the test plan requires access to the "Terraform/Infra" AWS account. Contact me if you need access.
    
    - CLI decryption with plaintext! output to stdout:
      ```
      cd services/terraform/remote
      sops -d secrets.json
      ```
    - Secrets should be accessible from Terraform:
      1. Add example output:
      ```
      lang=terraform
      output "my_secret" {
        # It must be wrapped in nonsensitive() or TF will output only redacted placeholder
        value = nonsensitive(local.secrets["accountIDs.staging"])
      }
      ```
    
      2. Run Terraform:
      ```
      cd services/terraform/remote
      terraform apply
      ```
      Should output:
      ```
      Apply complete! Resources: 0 added, 0 changed, 0 destroyed.
      Outputs:
      my_secret = "123456789012"
      ```
    
    Reviewers: ashoat, jon, varun
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D8667

--- services/terraform/remote/main.tf
+++ services/terraform/remote/main.tf
@@ -1,3 +1,13 @@
+provider "sops" {}
+
+data "sops_file" "secrets_json" {
+  source_file = "secrets.json"
+}
+
+locals {
+  secrets = jsondecode(data.sops_file.secrets_json.raw)
+}
+
 provider "aws" {
   region = "us-east-2"
 
