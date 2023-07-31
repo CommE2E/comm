f7912baa5acf8d6049729df53e8111fd37fa5119
Tomasz Palys <tomasz.palys@swmansion.com>
2023-07-31 16:08:51 +0200

    [native] Retry feature flags call
    Summary:
    Retry feature flags call. If number of tries is exceeded, rethrow an error.
    
    Depends on D8652
    
    Test Plan: Add console logs and simulate failed requests - the requests should be retried up to `numberOfTries` times with delays.
    
    Reviewers: kamil, bartek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D8679

--- native/components/feature-flags-provider.react.js
+++ native/components/feature-flags-provider.react.js
@@ -5,6 +5,7 @@ import * as React from 'react';
 import { Platform } from 'react-native';
 
 import { fetchFeatureFlags } from 'lib/utils/feature-flags-utils.js';
+import sleep from 'lib/utils/sleep.js';
 import { useIsCurrentUserStaff } from 'native/utils/staff-utils.js';
 
 import { codeVersion } from '../redux/persist.js';
@@ -64,11 +65,12 @@ function FeatureFlagsProvider(props: Props): React.Node {
   React.useEffect(() => {
     (async () => {
       try {
-        const config = await fetchFeatureFlags(
-          Platform.OS,
-          isStaff,
-          codeVersion,
+        const config = await tryMultipleTimes(
+          () => fetchFeatureFlags(Platform.OS, isStaff, codeVersion),
+          3,
+          5000,
         );
+
         const configuration = {};
         for (const feature of config.enabledFeatures) {
           configuration[feature] = true;
@@ -92,6 +94,26 @@ function FeatureFlagsProvider(props: Props): React.Node {
       {children}
     </FeatureFlagsContext.Provider>
   );
+}
+
+async function tryMultipleTimes<T>(
+  f: () => Promise<T>,
+  numberOfTries: number,
+  delay: number,
+): Promise<T> {
+  let lastError;
+  while (numberOfTries > 0) {
+    try {
+      return await f();
+    } catch (e) {
+      --numberOfTries;
+      lastError = e;
+      if (numberOfTries > 0) {
+        await sleep(delay);
+      }
+    }
+  }
+  throw lastError;
 }
 
 export { FeatureFlagsContext, FeatureFlagsProvider, featureFlagsStorageKey };
