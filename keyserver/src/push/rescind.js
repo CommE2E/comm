f1d4cec6b4778b7990ea3826795f511795f4b3eb
marcinwasowicz <marcinkwasowicz@gmail.com>
2023-07-31 16:21:43 +0200

    Significant refactor of notification encryption code Differential Revision: https://phab.comm.dev/D8697

--- keyserver/src/push/rescind.js
+++ keyserver/src/push/rescind.js
@@ -10,7 +10,7 @@ import { promiseAll } from 'lib/utils/promises.js';
 
 import {
   prepareEncryptedAndroidNotificationRescinds,
-  prepareEncryptedIOSNotifications,
+  prepareEncryptedIOSNotificationRescind,
 } from './crypto.js';
 import { getAPNsNotificationTopic } from './providers.js';
 import type {
@@ -223,9 +223,15 @@ async function conditionallyEncryptNotification<T>(
   codeVersion: ?number,
   devices: $ReadOnlyArray<NotificationTargetDevice>,
   encryptCallback: (
-    cookieIDs: $ReadOnlyArray<string>,
+    devices: $ReadOnlyArray<NotificationTargetDevice>,
     notification: T,
-  ) => Promise<$ReadOnlyArray<T>>,
+  ) => Promise<
+    $ReadOnlyArray<{
+      +notification: T,
+      +cookieID: string,
+      +deviceToken: string,
+    }>,
+  >,
 ): Promise<$ReadOnlyArray<{ +deviceToken: string, +notification: T }>> {
   const shouldBeEncrypted = codeVersion && codeVersion > NEXT_CODE_VERSION;
   if (!shouldBeEncrypted) {
@@ -234,16 +240,11 @@ async function conditionallyEncryptNotification<T>(
       deviceToken,
     }));
   }
-  const notificationPromises = devices.map(({ cookieID, deviceToken }) =>
-    (async () => {
-      const [encryptedNotif] = await encryptCallback([cookieID], notification);
-      return {
-        notification: encryptedNotif,
-        deviceToken,
-      };
-    })(),
-  );
-  return await Promise.all(notificationPromises);
+  const notifications = await encryptCallback(devices, notification);
+  return notifications.map(({ deviceToken, notification: notif }) => ({
+    deviceToken,
+    notification: notif,
+  }));
 }
 
 async function prepareIOSNotification(
@@ -286,7 +287,7 @@ async function prepareIOSNotification(
     notification,
     codeVersion,
     devices,
-    prepareEncryptedIOSNotifications,
+    prepareEncryptedIOSNotificationRescind,
   );
 }
 
