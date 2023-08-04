582a932cb99457cb4438c6a0edf819923b27f93e
marcinwasowicz <marcinkwasowicz@gmail.com>
2023-08-04 13:28:12 -0400

    Possible solution for broken iOS notifications
    Summary:
    IMPORTANT: This differential is for test release purposes.
    
    The context for this change is described here: https://linear.app/comm/issue/ENG-4453#comment-4d1770b3. We introduce dummy body to the encrypted notification to make sure we comply to apple docs. Additionally we make necessary changes to
    NotificationService extension to prevent from displaying notification that contains ENCRYPTED text.
    
    Test Plan: Build release iOS app. Ensure that notifications, rescinds and bade-only notifications work correctly
    
    Reviewers: tomek, atul, bartek, ashoat
    
    Reviewed By: ashoat
    
    Differential Revision: https://phab.comm.dev/D8638

--- keyserver/src/push/rescind.js
+++ keyserver/src/push/rescind.js
@@ -224,6 +224,7 @@ async function conditionallyEncryptNotification<T>(
   encryptCallback: (
     cookieIDs: $ReadOnlyArray<string>,
     notification: T,
+    codeVersion?: ?number,
   ) => Promise<$ReadOnlyArray<T>>,
 ): Promise<$ReadOnlyArray<{ +deviceToken: string, +notification: T }>> {
   const shouldBeEncrypted = codeVersion && codeVersion >= 233;
@@ -235,7 +236,11 @@ async function conditionallyEncryptNotification<T>(
   }
   const notificationPromises = devices.map(({ cookieID, deviceToken }) =>
     (async () => {
-      const [encryptedNotif] = await encryptCallback([cookieID], notification);
+      const [encryptedNotif] = await encryptCallback(
+        [cookieID],
+        notification,
+        codeVersion,
+      );
       return {
         notification: encryptedNotif,
         deviceToken,
