39ef3b37e4dcf79f91bcd09dc394add780f85cbc
Jonathan Ringer <jonathan.ringer@comm.app>
2023-08-07 06:52:28 -0700

    [Keyserver] Refactor getOneTimeKeyValues
    Summary:
    This is useful for more than the current login workflow,
    refactor to separate file
    
    In support of https://linear.app/comm/issue/ENG-4602
    
    Depends on D8750
    
    Test Plan:
    ```
    # Start localstack and rabbitmq
    comm-dev services start
    
    (cd services/identity && cargo run -- server)
    (cd services/tunnelbroker && cargo run)
    
    # If keyserver has already logged in, reset user_id entry in metadata table
    # This will force keyserver to re-login
    
    cd keyserver
    yarn dev
    
    # Assert the user is populated in MariaDB
    ```
    
    Reviewers: marcin, kamil
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8751

--- keyserver/src/utils/olm-utils.js
+++ keyserver/src/utils/olm-utils.js
@@ -9,7 +9,10 @@ import type {
 import { getRustAPI } from 'rust-node-addon';
 import uuid from 'uuid';
 
-import { olmEncryptedMessageTypes } from 'lib/types/crypto-types.js';
+import {
+  olmEncryptedMessageTypes,
+  type OLMOneTimeKeys,
+} from 'lib/types/crypto-types.js';
 import { values } from 'lib/utils/objects.js';
 
 import { fetchIdentityInfo } from '../user/identity.js';
@@ -79,6 +82,12 @@ function getOlmUtility(): OlmUtility {
   }
   cachedOLMUtility = new olm.Utility();
   return cachedOLMUtility;
+}
+
+function getOneTimeKeyValues(keyBlob: string): $ReadOnlyArray<string> {
+  const content: OLMOneTimeKeys = JSON.parse(keyBlob);
+  const keys: $ReadOnlyArray<string> = values(content.curve25519);
+  return keys;
 }
 
 function shouldRotatePrekey(account: OlmAccount): boolean {
@@ -185,6 +194,7 @@ export {
   createPickledOlmAccount,
   createPickledOlmSession,
   getOlmUtility,
+  getOneTimeKeyValues,
   unpickleOlmAccount,
   unpickleOlmSession,
   validateAccountPrekey,
