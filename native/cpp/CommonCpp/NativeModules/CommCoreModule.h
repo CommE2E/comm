8880e17d11b3591be1117a31c022565baedcdfdc
Ashoat Tevosyan <ashoat@comm.app>
2023-08-08 14:37:16 -0400

    [native] codeVersion -> 245

--- native/cpp/CommonCpp/NativeModules/CommCoreModule.h
+++ native/cpp/CommonCpp/NativeModules/CommCoreModule.h
@@ -18,7 +18,7 @@ namespace comm {
 namespace jsi = facebook::jsi;
 
 class CommCoreModule : public facebook::react::CommCoreModuleSchemaCxxSpecJSI {
-  const int codeVersion{244};
+  const int codeVersion{245};
   std::unique_ptr<WorkerThread> cryptoThread;
 
   CommSecureStore secureStore;
