bd1993a66eb916332618f88ef15894487c42afae
Rohan Khanderia <rohan.khanderia@gmail.com>
2023-08-08 14:24:20 -0400

    [native] Add a 'linked devices' section on the app for primary devices
    Summary:
    Per the designs, we want to add a 'Linked devices' section in the profile screen. It will remain empty while the device list is in progress, but it will be the intermediate screen in accessing the barcode scanner so it's necessary to add for now. I've rendered it only if it's in development mode though so it won't be released to the public.
    
    Designs:
    {F679739}
    
    One thing to note is that `LinkedDevices` needs to accept props, otherwise we get this error in `profile.react.js`:
    
    ```
    Cannot create `Profile.Screen` element because property `navigation` is missing in  function type [1] but exists in  props [2] in property `component`.Flow(prop-missing)
    ```
    
    [[ https://linear.app/comm/issue/ENG-4476/add-a-linked-devices-section-on-the-app-for-primary-devices | ENG-4476 ]]
    
    Depends on D8763
    
    Test Plan:
    Confirmed that the Linked devices section appears for me in dev mode
    
    {F679741}
    
    Reviewers: atul, ginsu, tomek, varun
    
    Reviewed By: ginsu
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D8764

---
+++ native/profile/linked-devices.react.js
@@ -0,0 +1,9 @@
+// @flow
+import * as React from 'react';
+
+// eslint-disable-next-line no-unused-vars
+function LinkedDevices(props: { ... }): React.Node {
+  return null;
+}
+
+export default LinkedDevices;
