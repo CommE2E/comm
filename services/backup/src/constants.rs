6a4c82d326ec38fb3f4a54ce6eb702efda775465
Michal Gniadek <michal.gniadek@swmansion.com>
2023-08-11 17:32:27 +0200

    [backup] Remove gRPC from backup
    Summary:
    1. Remove gRPC interface of backup service. More info here: [ENG-4501](https://linear.app/comm/issue/ENG-4501/update-backup-endpoints), but generally web app will also need to send large files in the future
    2. Remove blob gRPC client. Start of [ENG-3687](https://linear.app/comm/issue/ENG-3687/use-http-blob-client)
    3. Updated `Cargo.toml` (and run the default formatter)
    4. I didn't touch tests, they will be updated later
    
    I decided to do it first instead of first creating backup interface and only then removing the gRPC one because:
    - as far as I know the current backup service isn't used anywhere
    - I will be able to reuse some database code but it will need to be changed and I don't want to have to change the gRPC code to match, if it's going to be removed anyway
    
    Test Plan:
    1. `cargo build`
    2. Built the docker image
    
    Reviewers: bartek, patryk, jon
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8796

--- services/backup/src/constants.rs
+++ services/backup/src/constants.rs
@@ -31,8 +31,6 @@ pub const GRPC_CHUNK_SIZE_LIMIT: usize = 4 * 1024 * 1024;
 pub const GRPC_METADATA_SIZE_PER_MESSAGE: usize = 5;
 
 // Configuration defaults
-
-pub const DEFAULT_GRPC_SERVER_PORT: u64 = 50051;
 pub const DEFAULT_LOCALSTACK_URL: &str = "http://localhost:4566";
 pub const DEFAULT_BLOB_SERVICE_URL: &str = "http://localhost:50053";
 
