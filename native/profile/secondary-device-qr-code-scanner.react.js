9e01f356044014ebbe1a3f4b8f2b3ff09c0876b9
Rohan Khanderia <rohan.khanderia@gmail.com>
2023-08-14 11:28:04 -0400

    [native] Set up the BarCodeScanner to scan QR codes
    Summary:
    This diff introduces the usage of the Expo `BarCodeScanner` when
    the header right 'Add' is clicked on the linked devices screen.
    
    The [[ https://docs.expo.dev/versions/latest/sdk/bar-code-scanner/ | Expo
    docs ]] were closely followed in setting this up.
    
    For now, the `onConnect` will not do anything besides alert the user that
    the qr code has been scanned with the data encoded, but down the line this
    will be changed to read the secondary devices' keys encoded in the qr
    code.
    
    Test Plan:
    Confirmed that the scanner opened if I granted camera
    permissions, and also that it blocked launching the scanner if I denied
    permissions.
    
    Reviewers: atul, ginsu, tomek, varun
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D8805

---
+++ native/profile/secondary-device-qr-code-scanner.react.js
@@ -0,0 +1,110 @@
+// @flow
+
+import { useNavigation } from '@react-navigation/native';
+import { BarCodeScanner, type BarCodeEvent } from 'expo-barcode-scanner';
+import * as React from 'react';
+import { View } from 'react-native';
+
+import { useStyles } from '../themes/colors.js';
+import Alert from '../utils/alert.js';
+
+const barCodeTypes = [BarCodeScanner.Constants.BarCodeType.qr];
+
+// eslint-disable-next-line no-unused-vars
+function SecondaryDeviceQRCodeScanner(props: { ... }): React.Node {
+  const [hasPermission, setHasPermission] = React.useState(null);
+  const [scanned, setScanned] = React.useState(false);
+
+  const styles = useStyles(unboundStyles);
+  const navigation = useNavigation();
+
+  React.useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+
+      if (status !== 'granted') {
+        Alert.alert(
+          'No access to camera',
+          'Please allow Comm to access your camera in order to scan the QR code.',
+          [{ text: 'OK' }],
+        );
+
+        navigation.goBack();
+      }
+    })();
+  }, [navigation]);
+
+  const onConnect = React.useCallback((barCodeEvent: BarCodeEvent) => {
+    const { type, data } = barCodeEvent;
+    Alert.alert(
+      'Scan successful',
+      `Bar code with type ${type} and data ${data} has been scanned!`,
+      [{ text: 'OK' }],
+    );
+  }, []);
+
+  const onCancelScan = React.useCallback(() => setScanned(false), []);
+
+  const handleBarCodeScanned = React.useCallback(
+    (barCodeEvent: BarCodeEvent) => {
+      setScanned(true);
+      Alert.alert(
+        'Connect with this device?',
+        'Are you sure you want to allow this device to log in to your account?',
+        [
+          {
+            text: 'Cancel',
+            style: 'cancel',
+            onPress: onCancelScan,
+          },
+          {
+            text: 'Connect',
+            onPress: () => onConnect(barCodeEvent),
+          },
+        ],
+        { cancelable: false },
+      );
+    },
+    [onCancelScan, onConnect],
+  );
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+
+  // Note: According to the BarCodeScanner Expo docs, we should adhere to two
+  // guidances when using the BarCodeScanner:
+  // 1. We should specify the potential barCodeTypes we want to scan for to
+  //    minimize battery usage.
+  // 2. We should set the onBarCodeScanned callback to undefined if it scanned
+  //    in order to 'pause' the scanner from continuing to scan while we
+  //    process the data from the scan.
+  // See: https://docs.expo.io/versions/latest/sdk/bar-code-scanner
+  return (
+    <View style={styles.container}>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        barCodeTypes={barCodeTypes}
+        style={styles.scanner}
+      />
+    </View>
+  );
+}
+
+const unboundStyles = {
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  scanner: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+};
+
+export default SecondaryDeviceQRCodeScanner;
