4b2c4645a16d0122685de1a452f14d3906fe7583
Rohan Khanderia <rohan.khanderia@gmail.com>
2023-08-14 11:48:42 -0400

    [native] Prevent emulators from attempting to open the barcode scanner
    Summary: Launching the barcode scanner on a simulator crashes the entire
    app (this is because it is not a [[
    https://docs.expo.dev/versions/latest/sdk/bar-code-scanner/#platform-compatibility
    | compatible platform ]].
    
    We should prevent this from being accessed in the simulator.
    
    Test Plan: Confirmed that the scanner no longer is launched when on a
    simulator.
    
    Reviewers: atul, ginsu, tomek, varun
    
    Subscribers:

--- native/profile/linked-devices-header-right-button.react.js
+++ native/profile/linked-devices-header-right-button.react.js
@@ -6,15 +6,25 @@ import { Text, TouchableOpacity } from 'react-native';
 
 import { SecondaryDeviceQRCodeScannerRouteName } from '../navigation/route-names.js';
 import { useStyles } from '../themes/colors.js';
+import Alert from '../utils/alert.js';
+import { devIsEmulator } from '../utils/url-utils.js';
 
 function LinkedDevicesHeaderRightButton(): React.Node {
   const styles = useStyles(unboundStyles);
   const navigation = useNavigation();
 
-  const navigateToQRCodeScanner = React.useCallback(
-    () => navigation.navigate(SecondaryDeviceQRCodeScannerRouteName),
-    [navigation],
-  );
+  const navigateToQRCodeScanner = React.useCallback(() => {
+    if (devIsEmulator) {
+      Alert.alert(
+        'Unsupported device',
+        "You can't access the QR code scanner on a simulator.",
+        [{ text: 'OK' }],
+        { cancelable: false },
+      );
+      return;
+    }
+    navigation.navigate(SecondaryDeviceQRCodeScannerRouteName);
+  }, [navigation]);
 
   return (
     <TouchableOpacity onPress={navigateToQRCodeScanner}>
