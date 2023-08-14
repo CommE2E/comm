d89982af1fcebaf31da2cbdb4bd9e77957fb39f1
Jonathan Ringer <jonathan.ringer@comm.app>
2023-08-14 06:51:34 -0700

    [Shared] Create shared grpc_client library
    Summary:
    Every service and native device will need to talk to either identity
    service or tunnelbroker. This begins the trend with making identity service
    and related protos available as a shared library.
    
    https://linear.app/comm/issue/ENG-4640
    
    Test Plan:
    ```
    cargo build
    ```
    This diff will be integrated in a later diff
    
    Reviewers: bartek, varun, michal
    
    Reviewed By: varun
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8804

---
+++ shared/grpc_clients/src/identity/unauthenticated.rs
@@ -0,0 +1,11 @@
+use tonic::transport::Channel;
+
+use super::protos::client::identity_client_service_client::IdentityClientServiceClient;
+use crate::error::Error;
+
+pub async fn get_unauthenticated_client(
+  url: &str,
+) -> Result<IdentityClientServiceClient<Channel>, Error> {
+  let channel = super::get_identity_service_channel(url).await?;
+  Ok(IdentityClientServiceClient::new(channel))
+}
