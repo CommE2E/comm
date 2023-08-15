4fb1013083891ea4719253ca82d28da1a9169c24
Rohan Khanderia <rohan.khanderia@gmail.com>
2023-08-15 13:20:48 -0400

    [web] Create a new 'secondary device' screen that will generate the QR code
    Summary:
    This is the page that will display the QR code, alongside the instructions on where to find the scanner on the `native` app. These are the designs shown on [[
    https://www.figma.com/file/a1nkbWgbgjRlrOY9LVurTz/Comm-%2F-Desktop-app?type=design&node-id=1274-90205&mode=design&t=nW4COkAGp0dOVe9x-0 | Figma ]].
    
    For now, the data encoded in the QR code is just a link, but I will work on supporting 'deep-linking' from scanning with an external camera in [[
    https://linear.app/comm/issue/ENG-4648/support-external-cameras-scanning-the-qr-code | ENG-4648 ]]. So ultimately that will be changed.
    
    Test Plan: Please see the video below
    
    Reviewers: atul, ginsu, tomek, varun
    
    Subscribers: ashoat, ted
    
    Differential Revision: https://phab.comm.dev/D8821

--- web/account/qr-code-login.react.js
+++ web/account/qr-code-login.react.js
@@ -1,9 +1,39 @@
 // @flow
 
+import { QRCodeSVG } from 'qrcode.react';
 import * as React from 'react';
 
+import css from './qr-code-login.css';
+
 function QrCodeLogin(): React.Node {
-  return <div />;
+  return (
+    <div className={css.qrContainer}>
+      <h1 className={css.title}>Log in to Comm</h1>
+      <h4 className={css.scanInstructions}>
+        Open the Comm app on your phone and scan the QR code below
+      </h4>
+      <QRCodeSVG
+        value="https://comm.app/"
+        size={300}
+        marginSize={4}
+        level="L"
+      />
+      <div className={css.instructionsContainer}>
+        <div className={css.instructionsTitle}>
+          <b>How to find the scanner:</b>
+        </div>
+        <div className={css.instructionsStep}>
+          Go to <b>Profile</b>
+        </div>
+        <div className={css.instructionsStep}>
+          Select <b>Linked devices</b>
+        </div>
+        <div className={css.instructionsStep}>
+          Click <b>Add</b> on the top right
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default QrCodeLogin;
