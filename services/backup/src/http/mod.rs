bbf1feaf671587f66f733c2985858c751d8bb74a
Michal Gniadek <michal.gniadek@swmansion.com>
2023-08-16 12:30:22 +0200

    [backup] Use blob client
    Summary:
    [ENG-3687](https://linear.app/comm/issue/ENG-3687)
    Use new http blob client
    
    Depends on D8882
    
    Test Plan:
    Add logging in main and check if url was set correctly when running
    `cargo run`
    `cargo run -- --blob-service-url localhost:123`
    Check if it fails when running
    `cargo run -- --blob-service-url localhost:123asd`
    
    Reviewers: bartek, kamil, patryk, jon
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8883

--- services/backup/src/http/mod.rs
+++ services/backup/src/http/mod.rs
@@ -1,16 +1,21 @@
 use actix_web::{web, App, HttpServer};
 use anyhow::Result;
+use comm_services_lib::blob::client::BlobServiceClient;
 use tracing::info;
 
 use crate::{database::DatabaseClient, CONFIG};
 
-pub async fn run_http_server(db_client: DatabaseClient) -> Result<()> {
+pub async fn run_http_server(
+  db_client: DatabaseClient,
+  blob_client: BlobServiceClient,
+) -> Result<()> {
   info!(
     "Starting HTTP server listening at port {}",
     CONFIG.http_port
   );
 
   let db = web::Data::new(db_client);
+  let blob = web::Data::new(blob_client);
 
   HttpServer::new(move || {
     App::new()
@@ -19,6 +24,7 @@ pub async fn run_http_server(db_client: DatabaseClient) -> Result<()> {
         CONFIG.localstack_endpoint.is_some(),
       ))
       .app_data(db.clone())
+      .app_data(blob.clone())
       .service(
         web::resource("/hello").route(web::get().to(|| async { "world" })),
       )
