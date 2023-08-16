593bdab41a30224c10ea47614855b139302c4a1b
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-08-16 02:26:49 -0400

    [web] update calculateMaxTextWidth to consider different font sizes
    Summary: Our reaction tooltip has texts with two different font sizes (see image below for context) to accomadate for this I upadted `calculateMaxTextWidth` to consider differnet font sizes so that we can use this function when we introduce the reaction toolitp
    
    Test Plan: `flow` and confirmed that the result of `calculateMaxTextWidth` was the same before and after the changes
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8831

--- web/utils/text-utils.js
+++ web/utils/text-utils.js
@@ -6,8 +6,12 @@ let canvas;
 
 function calculateMaxTextWidth(
   texts: $ReadOnlyArray<string>,
-  font: string,
+  fontSize: number,
 ): number {
+  const font =
+    `${fontSize}px "Inter", -apple-system, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", ` +
+    '"Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", ui-sans-serif';
+
   if (!canvas) {
     canvas = document.createElement('canvas');
   }
