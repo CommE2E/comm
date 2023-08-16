f45d933ef293327edeccb99e502672cb00dadad4
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2023-08-15 23:57:24 -0400

    [identity] add ttl to nonces tables
    Summary:
    Added an expiration time as an RFC 3339 date-and-time string and as a UNIX timestamp to DynamoDB. I added the RFC 3339 version to preserve precision, since UNIX timestamps only go to the second. Open to removing it, though.
    
    The UNIX timestamp lets us set a TTL on the DDB table with Terraform (see next diff). DynamoDB will then remove expired nonces, saving us from having to implement a cron job ourselves.
    
    Depends on D8825
    
    Test Plan: verified that the expirationTime and expirationTimeUnix attributes appeared in my local DDB table.
    
    Reviewers: jon, bartek
    
    Reviewed By: bartek
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8830

--- services/identity/src/nonce.rs
+++ services/identity/src/nonce.rs
@@ -1,15 +1,20 @@
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, Duration, Utc};
 use rand::{
   distributions::{Alphanumeric, DistString},
   CryptoRng, Rng,
 };
 
 use crate::constants::NONCE_LENGTH;
+use crate::constants::NONCE_TTL_DURATION;
 
 pub fn generate_nonce_data(rng: &mut (impl Rng + CryptoRng)) -> NonceData {
+  let nonce = Alphanumeric.sample_string(rng, NONCE_LENGTH);
+  let created = Utc::now();
+  let expiration_time = created + Duration::minutes(NONCE_TTL_DURATION);
   NonceData {
-    nonce: Alphanumeric.sample_string(rng, NONCE_LENGTH),
-    created: Utc::now(),
+    nonce,
+    created,
+    expiration_time,
   }
 }
 
@@ -17,4 +22,5 @@ pub fn generate_nonce_data(rng: &mut (impl Rng + CryptoRng)) -> NonceData {
 pub struct NonceData {
   pub nonce: String,
   pub created: DateTime<Utc>,
+  pub expiration_time: DateTime<Utc>,
 }
