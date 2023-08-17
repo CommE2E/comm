c52de9a0e0236b3e3966a347b5ae81405aee5b05
Patryk Klatka <patryk.klatka@gmail.com>
2023-08-17 11:05:05 +0200

    [native] Introduce markdown component for chat mentions
    Summary: TODO
    
    Test Plan: TODO
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8845

---
+++ native/markdown/markdown-chat-mention.react.js
@@ -0,0 +1,39 @@
+// @flow
+
+import * as React from 'react';
+import { Text } from 'react-native';
+
+import type { ResolvedThreadInfo } from 'lib/types/thread-types.js';
+
+import {
+  useMarkdownOnPressUtils,
+  useHandleChatMentionClick,
+} from './markdown-utils.js';
+
+type TextProps = React.ElementConfig<typeof Text>;
+type Props = {
+  +threadInfo: ResolvedThreadInfo,
+  +hasAccessToChat: boolean,
+  +children: React.Node,
+  ...TextProps,
+};
+function MarkdownChatMention(props: Props): React.Node {
+  const { threadInfo, hasAccessToChat, style, ...rest } = props;
+  const { markdownContext, messageKey, shouldBePressable, onLongPressHandler } =
+    useMarkdownOnPressUtils(hasAccessToChat);
+  const onPressHandler = useHandleChatMentionClick(
+    threadInfo,
+    markdownContext,
+    messageKey,
+  );
+  return (
+    <Text
+      onPress={shouldBePressable ? onPressHandler : null}
+      onLongPress={shouldBePressable ? onLongPressHandler : null}
+      style={hasAccessToChat ? style : null}
+      {...rest}
+    />
+  );
+}
+
+export default MarkdownChatMention;
