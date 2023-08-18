3d51263313ba5486c064a4abdb34cc102ac67d2e
Patryk Klatka <patryk.klatka@gmail.com>
2023-08-18 23:13:20 +0200

    [native] Introduce markdown component for chat mentions
    Summary:
    Solution for [ENG-4560](https://linear.app/comm/issue/ENG-4560/bold-mentions-when-user-has-access-to-given-chat) and [ENG-4561](https://linear.app/comm/issue/ENG-4561/redirect-to-given-chat-when-mention-is-clicked).
    
    This diff creates new component for chat mentions which handles bolding mentions when user has access to given chat and onPress events.
    
    Test Plan: Tested later in the stack
    
    Reviewers: tomek, inka, rohan
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8845

--- native/chat/message-list-types.js
+++ native/chat/message-list-types.js
@@ -1,16 +1,21 @@
 // @flow
 
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useNavigationState } from '@react-navigation/native';
 import invariant from 'invariant';
 import * as React from 'react';
 
+import { useThreadChatMentionCandidates } from 'lib/shared/thread-utils.js';
 import type { ThreadInfo } from 'lib/types/thread-types.js';
 import { type UserInfo } from 'lib/types/user-types.js';
 
+import { ChatContext } from './chat-context.js';
 import type { ChatRouterNavigationAction } from './chat-router.js';
 import type { MarkdownRules } from '../markdown/rules.react.js';
 import { useTextMessageRulesFunc } from '../markdown/rules.react.js';
-import { MessageListRouteName } from '../navigation/route-names.js';
+import {
+  MessageListRouteName,
+  TextMessageTooltipModalRouteName,
+} from '../navigation/route-names.js';
 
 export type MessageListParams = {
   +threadInfo: ThreadInfo,
@@ -31,7 +36,11 @@ const MessageListContext: React.Context<?MessageListContextType> =
   React.createContext<?MessageListContextType>();
 
 function useMessageListContext(threadInfo: ThreadInfo) {
-  const getTextMessageMarkdownRules = useTextMessageRulesFunc(threadInfo);
+  const chatMentionCandidates = useThreadChatMentionCandidates(threadInfo);
+  const getTextMessageMarkdownRules = useTextMessageRulesFunc(
+    threadInfo,
+    chatMentionCandidates,
+  );
   return React.useMemo(
     () => ({
       getTextMessageMarkdownRules,
@@ -84,9 +93,40 @@ function useTextMessageMarkdownRules(useDarkStyle: boolean): MarkdownRules {
   return messageListContext.getTextMessageMarkdownRules(useDarkStyle);
 }
 
+function useNavigateToThreadWithFadeAnimation(
+  threadInfo: ThreadInfo,
+  messageKey: ?string,
+): () => mixed {
+  const chatContext = React.useContext(ChatContext);
+  invariant(chatContext, 'ChatContext should be set');
+  const setSidebarSourceID = chatContext?.setCurrentTransitionSidebarSourceID;
+  const setSidebarAnimationType = chatContext?.setSidebarAnimationType;
+  const navigateToThread = useNavigateToThread();
+  const navigationStack = useNavigationState(state => state.routes);
+
+  return React.useCallback(() => {
+    if (
+      navigationStack[navigationStack.length - 1].name ===
+      TextMessageTooltipModalRouteName
+    ) {
+      setSidebarSourceID && setSidebarSourceID(messageKey);
+      setSidebarAnimationType && setSidebarAnimationType('fade_source_message');
+    }
+    navigateToThread({ threadInfo });
+  }, [
+    messageKey,
+    navigateToThread,
+    navigationStack,
+    setSidebarAnimationType,
+    setSidebarSourceID,
+    threadInfo,
+  ]);
+}
+
 export {
   MessageListContextProvider,
   createNavigateToThreadAction,
   useNavigateToThread,
   useTextMessageMarkdownRules,
+  useNavigateToThreadWithFadeAnimation,
 };
