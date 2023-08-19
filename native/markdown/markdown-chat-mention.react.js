cdca9566796f6c82552328c99fb1f943699a5794
Patryk Klatka <patryk.klatka@gmail.com>
2023-08-19 14:56:41 +0200

    [native] Introduce markdown component for chat mentions
    Summary:
    Solution for [ENG-4560](https://linear.app/comm/issue/ENG-4560/bold-mentions-when-user-has-access-to-given-chat) and [ENG-4561](https://linear.app/comm/issue/ENG-4561/redirect-to-given-chat-when-mention-is-clicked).
    
    This diff creates new component for chat mentions which handles bolding mentions when user has access to given chat and onPress events.
    
    Test Plan: Tested later in the stack
    
    Reviewers: tomek, inka, rohan
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8845

---
+++ native/markdown/markdown-chat-mention.react.js
@@ -0,0 +1,31 @@
+// @flow
+
+import * as React from 'react';
+import { Text } from 'react-native';
+
+import type { ResolvedThreadInfo } from 'lib/types/thread-types.js';
+
+import { useMarkdownOnPressUtils } from './markdown-utils.js';
+
+type TextProps = React.ElementConfig<typeof Text>;
+type Props = {
+  +threadInfo: ResolvedThreadInfo,
+  +hasAccessToChat: boolean,
+  +children: React.Node,
+  ...TextProps,
+};
+function MarkdownChatMention(props: Props): React.Node {
+  const { threadInfo, hasAccessToChat, style, ...rest } = props;
+  const { shouldBePressable, onLongPressHandler } = useMarkdownOnPressUtils(
+    !hasAccessToChat,
+  );
+  return (
+    <Text
+      onLongPress={shouldBePressable ? onLongPressHandler : null}
+      style={hasAccessToChat ? style : null}
+      {...rest}
+    />
+  );
+}
+
+export default MarkdownChatMention;
