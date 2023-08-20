0a36a6c2d8ffd8943abe5d71489782954450f27a
Patryk Klatka <patryk.klatka@gmail.com>
2023-08-20 23:30:28 +0200

    [native] Make chat mentions clickable
    Summary:
    Solution for [ENG-4561](https://linear.app/comm/issue/ENG-4561/redirect-to-given-chat-when-mention-is-clicked).
    
    This diff introduces new hook which redirects to chat when mention is clicked and uses it in `markdown-utils.js` and finally in `markdown-chat-mention.react.js`
    
    Depends on D8845.
    
    Test Plan:
    Before testing, please apply the entire native stack.
    
    1. Test if mentions inside a spoiler are properly rendered and are clickable only when spoiler is opened:
    {F697993}
    2. Test if mentions are clickable and if animation is changed when tooltip is opened:
    {F697994}
    
    Reviewers: tomek, inka, rohan
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8873

--- native/markdown/markdown-chat-mention.react.js
+++ native/markdown/markdown-chat-mention.react.js
@@ -6,6 +6,7 @@ import { Text } from 'react-native';
 import type { ResolvedThreadInfo } from 'lib/types/thread-types.js';
 
 import { useMarkdownOnPressUtils } from './markdown-utils.js';
+import { useNavigateToThreadWithFadeAnimation } from '../chat/message-list-types.js';
 
 type TextProps = React.ElementConfig<typeof Text>;
 type Props = {
@@ -16,11 +17,15 @@ type Props = {
 };
 function MarkdownChatMention(props: Props): React.Node {
   const { threadInfo, hasAccessToChat, ...rest } = props;
-  const { shouldBePressable, onLongPressHandler } = useMarkdownOnPressUtils(
-    !hasAccessToChat,
+  const { messageKey, shouldBePressable, onLongPressHandler } =
+    useMarkdownOnPressUtils(!hasAccessToChat);
+  const onPressHandler = useNavigateToThreadWithFadeAnimation(
+    threadInfo,
+    messageKey,
   );
   return (
     <Text
+      onPress={shouldBePressable ? onPressHandler : null}
       onLongPress={shouldBePressable ? onLongPressHandler : null}
       {...rest}
     />
