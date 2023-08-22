0f4bea858dfb2b414a1cccbc52158a29b2e573df
Rohan Khanderia <rohan.khanderia@gmail.com>
2023-08-22 11:52:37 -0400

    [native] Add a new screen to display a QR code as part of a new native login flow
    Summary:
    This is the `native` counterpart of D8821. I created a new navigator because this sign in via QR code flow will inevitably have multiple 'steps' in it (like choosing a device name once the QR code is
    scanned), so I decided to have it done now rather than later.
    
    One important note: The designs are not finalized, but I've spoken with @ted and I'll do another pass on the designs in a later diff once they're ready (this is tracked in [[
    https://linear.app/comm/issue/ENG-4701/follow-up-on-qr-code-page-screen-designs | ENG-4701 ]].
    
    Addresses [[ https://linear.app/comm/issue/ENG-4515/add-a-new-screen-to-display-a-qr-code-as-part-of-a-new-native-login | ENG-4515 ]]
    
    Depends on D8870
    
    Test Plan: Click on the button and expect the new screen to show
    
    Reviewers: atul, ginsu, tomek, varun
    
    Subscribers: ashoat, ted
    
    Differential Revision: https://phab.comm.dev/D8912

---
+++ native/qr-code/qr-code-screen.react.js
@@ -0,0 +1,86 @@
+// @flow
+
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import QRCode from 'react-native-qrcode-svg';
+
+import type { QRCodeSignInNavigationProp } from './qr-code-sign-in-navigator.react.js';
+import type { NavigationRoute } from '../navigation/route-names.js';
+import { useStyles } from '../themes/colors.js';
+
+type QRCodeScreenProps = {
+  +navigation: QRCodeSignInNavigationProp<'QRCodeScreen'>,
+  +route: NavigationRoute<'QRCodeScreen'>,
+};
+
+// eslint-disable-next-line no-unused-vars
+function QRCodeScreen(props: QRCodeScreenProps): React.Node {
+  const styles = useStyles(unboundStyles);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>Log in to Comm</Text>
+      <Text style={styles.headingSubtext}>
+        Open the Comm app on your phone and scan the QR code below
+      </Text>
+      <QRCode value="https://www.google.com" size={200} />
+      <View style={styles.instructionsBox}>
+        <Text style={styles.instructionsTitle}>How to find the scanner:</Text>
+        <Text style={styles.instructionsStep}>
+          <Text>Go to </Text>
+          <Text style={styles.instructionsBold}>Profile</Text>
+        </Text>
+        <Text style={styles.instructionsStep}>
+          <Text>Select </Text>
+          <Text style={styles.instructionsBold}>Linked devices </Text>
+        </Text>
+        <Text style={styles.instructionsStep}>
+          <Text>Click </Text>
+          <Text style={styles.instructionsBold}>Add </Text>
+          <Text>on the top right</Text>
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+const unboundStyles = {
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    marginTop: 125,
+  },
+  heading: {
+    fontSize: 24,
+    color: 'panelForegroundLabel',
+    paddingBottom: 12,
+  },
+  headingSubtext: {
+    fontSize: 12,
+    color: 'panelForegroundLabel',
+    paddingBottom: 30,
+  },
+  instructionsBox: {
+    alignItems: 'center',
+    width: 300,
+    marginTop: 40,
+    padding: 15,
+    borderColor: 'panelForegroundLabel',
+    borderWidth: 2,
+    borderRadius: 8,
+  },
+  instructionsTitle: {
+    fontSize: 12,
+    color: 'panelForegroundLabel',
+    paddingBottom: 15,
+  },
+  instructionsStep: {
+    fontSize: 12,
+    padding: 1,
+    color: 'panelForegroundLabel',
+  },
+  instructionsBold: {
+    fontWeight: 'bold',
+  },
+};
+
+export default QRCodeScreen;
