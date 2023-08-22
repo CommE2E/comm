4bb7278b8602a505a6782d617d0936627a5d91a6
Atul Madhugiri <atulm@berkeley.edu>
2023-08-22 13:43:04 -0400

    [web] Introduce `ThreadEmojiAvatarSelectionModal`
    Summary:
    Enables setting of emoji avatars on `web`. As of this diff all varieties of thread avatar can be set from `web`. We just need to make a few layout tweaks in `ThreadSettingsModalGeneralTab` in order to get this ready to ship.
    
    ---
    
    Depends on D8855
    
    Test Plan: Works as expected: {F694299}
    
    Reviewers: ashoat, ginsu
    
    Reviewed By: ashoat
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D8856

---
+++ web/avatars/thread-emoji-avatar-selection-modal.react.js
@@ -0,0 +1,54 @@
+// @flow
+
+import invariant from 'invariant';
+import * as React from 'react';
+
+import { EditThreadAvatarContext } from 'lib/components/base-edit-thread-avatar-provider.react.js';
+import {
+  getDefaultAvatar,
+  useAvatarForThread,
+} from 'lib/shared/avatar-utils.js';
+import type {
+  ClientAvatar,
+  ClientEmojiAvatar,
+} from 'lib/types/avatar-types.js';
+import type { RawThreadInfo, ThreadInfo } from 'lib/types/thread-types.js';
+
+import EmojiAvatarSelectionModal from './emoji-avatar-selection-modal.react.js';
+
+type Props = {
+  +threadInfo: ThreadInfo | RawThreadInfo,
+};
+
+function ThreadEmojiAvatarSelectionModal(props: Props): React.Node {
+  const { threadInfo } = props;
+  const editThreadAvatarContext = React.useContext(EditThreadAvatarContext);
+  invariant(editThreadAvatarContext, 'editThreadAvatarContext should be set');
+
+  const { baseSetThreadAvatar, threadAvatarSaveInProgress } =
+    editThreadAvatarContext;
+
+  const currentThreadAvatar: ClientAvatar = useAvatarForThread(threadInfo);
+  const defaultThreadAvatar: ClientEmojiAvatar = getDefaultAvatar(
+    threadInfo.id,
+    threadInfo.color,
+  );
+
+  const setEmojiAvatar = React.useCallback(
+    async (pendingEmojiAvatar: ClientEmojiAvatar): Promise<void> => {
+      await baseSetThreadAvatar(threadInfo.id, pendingEmojiAvatar);
+    },
+    [baseSetThreadAvatar, threadInfo.id],
+  );
+
+  return (
+    <EmojiAvatarSelectionModal
+      currentAvatar={currentThreadAvatar}
+      defaultAvatar={defaultThreadAvatar}
+      setEmojiAvatar={setEmojiAvatar}
+      avatarSaveInProgress={threadAvatarSaveInProgress}
+    />
+  );
+}
+
+export default ThreadEmojiAvatarSelectionModal;
