125fc482a672cc5b23d1da8878690b80bcf215d7
Tomasz Palys <tomasz.palys@swmansion.com>
2023-08-22 12:50:11 +0200

    [keyserver] Merge `fetchAll` and `fetchByID` functions
    Summary:
    Instead of having two functions: ane which fetches all the entries and one which filters by id, we can have a single one which handles both cases. Additional benefit is that for `current-user` spec, we don't need to introduce en exception in spec's interface (we don't need to fetch current user by id).
    
    https://linear.app/comm/issue/ENG-4631/migrate-checkstate-from-session-utils-to-a-spec
    
    Depends on D8927
    
    Test Plan: Modify keyserver db and check if the state get fixed.
    
    Reviewers: kamil, bartek, inka
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D8941

--- keyserver/src/shared/state-sync/users-state-sync-spec.js
+++ keyserver/src/shared/state-sync/users-state-sync-spec.js
@@ -1,5 +1,6 @@
 // @flow
 
+import type { CalendarQuery } from 'lib/types/entry-types.js';
 import type { UserInfos } from 'lib/types/user-types.js';
 
 import type { StateSyncSpec } from './state-sync-spec.js';
@@ -7,7 +8,15 @@ import { fetchKnownUserInfos } from '../../fetchers/user-fetchers.js';
 import type { Viewer } from '../../session/viewer.js';
 
 export const usersStateSyncSpec: StateSyncSpec<UserInfos> = Object.freeze({
-  fetchAll(viewer: Viewer) {
+  fetch(
+    viewer: Viewer,
+    query: $ReadOnlyArray<CalendarQuery>,
+    ids?: $ReadOnlySet<string>,
+  ) {
+    if (ids) {
+      return fetchKnownUserInfos(viewer, [...ids]);
+    }
+
     return fetchKnownUserInfos(viewer);
   },
 });
