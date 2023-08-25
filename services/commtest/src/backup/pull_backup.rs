62436ae30596ba1701a5b79549617c0091a1740c
Michal Gniadek <michal.gniadek@swmansion.com>
2023-08-25 14:33:00 +0200

    [backup] Add integration and performance tests
    Summary:
    [ENG-4511](https://linear.app/comm/issue/ENG-4511)
    Add tests for backup. They are not deterministic yet (they require a clean cloud), I'm going to update them as a followup if I have the time.
    
    Depends on D8965
    
    Test Plan: Run both integration and performance tests
    
    Reviewers: bartek, patryk, kamil, jon
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8966

--- services/commtest/src/backup/pull_backup.rs
+++ services/commtest/src/backup/pull_backup.rs
@@ -1,130 +1,80 @@
-use crate::backup::backup_utils::{
-  proto::pull_backup_response::Data, proto::pull_backup_response::Data::*,
-  proto::pull_backup_response::Id, proto::pull_backup_response::Id::*,
-  proto::PullBackupRequest, BackupData, BackupServiceClient, Item,
-};
-use crate::constants::ATTACHMENT_DELIMITER;
-use crate::tools::Error;
-use std::io::{Error as IOError, ErrorKind};
-use tonic::Request;
+use std::fmt::Display;
 
-#[derive(PartialEq, Debug)]
-enum State {
-  Compaction,
-  Log,
+use crate::tools::Error;
+use comm_services_lib::auth::UserIdentity;
+
+#[derive(Debug, Clone)]
+pub enum BackupDescriptor {
+  BackupID {
+    backup_id: String,
+    user_identity: UserIdentity,
+  },
+  Latest {
+    username: String,
+  },
 }
 
-pub async fn run(
-  client: &mut BackupServiceClient<tonic::transport::Channel>,
-  backup_data: &BackupData,
-) -> Result<BackupData, Error> {
-  println!("pull backup");
-  let cloned_user_id = backup_data.user_id.clone();
-  let cloned_backup_id = backup_data.backup_item.id.clone();
-
-  let mut result = BackupData {
-    user_id: String::new(),
-    device_id: String::new(),
-    backup_item: Item::new(String::new(), Vec::new(), Vec::new()),
-    log_items: Vec::new(),
-  };
-
-  let response = client
-    .pull_backup(Request::new(PullBackupRequest {
-      user_id: cloned_user_id,
-      backup_id: cloned_backup_id,
-    }))
-    .await?;
-  let mut inbound = response.into_inner();
-  let mut state: State = State::Compaction;
-  let mut current_id: String = String::new();
-  while let Some(response) = inbound.message().await? {
-    let response_data: Option<Data> = response.data;
-    let id: Option<Id> = response.id;
-    let mut backup_id: Option<String> = None;
-    let mut log_id: Option<String> = None;
-    match id {
-      Some(BackupId(id)) => backup_id = Some(id),
-      Some(LogId(id)) => log_id = Some(id),
-      None => {}
-    };
-    match response_data {
-      Some(CompactionChunk(chunk)) => {
-        assert_eq!(
-          state,
-          State::Compaction,
-          "invalid state, expected compaction, got {:?}",
-          state
-        );
-        current_id = backup_id.ok_or(IOError::new(
-          ErrorKind::Other,
-          "backup id expected but not received",
-        ))?;
-        println!(
-          "compaction (id {}), pulling chunk (size: {})",
-          current_id,
-          chunk.len()
-        );
-        result.backup_item.chunks_sizes.push(chunk.len())
-      }
-      Some(LogChunk(chunk)) => {
-        if state == State::Compaction {
-          state = State::Log;
-        }
-        assert_eq!(
-          state,
-          State::Log,
-          "invalid state, expected log, got {:?}",
-          state
-        );
-        let log_id = log_id.ok_or(IOError::new(
-          ErrorKind::Other,
-          "log id expected but not received",
-        ))?;
-        if log_id != current_id {
-          result.log_items.push(Item::new(
-            log_id.clone(),
-            Vec::new(),
-            Vec::new(),
-          ));
-          current_id = log_id;
-        }
-        let log_items_size = result.log_items.len() - 1;
-        result.log_items[log_items_size]
-          .chunks_sizes
-          .push(chunk.len());
-
-        println!("log (id {}) chunk size {}", current_id, chunk.len());
-      }
-      None => {}
-    }
-    if let Some(holders) = response.attachment_holders {
-      let holders_split: Vec<&str> =
-        holders.split(ATTACHMENT_DELIMITER).collect();
-      if state == State::Compaction {
-        for holder in holders_split {
-          if holder.len() == 0 {
-            continue;
-          }
-          println!("attachments for the backup: {}", holder);
-          result
-            .backup_item
-            .attachments_holders
-            .push(holder.to_string());
-        }
-      } else if state == State::Log {
-        println!("attachments for the log {:?}: {}", current_id, holders);
-        for holder in holders_split {
-          if holder.len() == 0 {
-            continue;
-          }
-          let log_items_size = result.log_items.len() - 1;
-          result.log_items[log_items_size]
-            .attachments_holders
-            .push(holder.to_string())
-        }
+impl Display for BackupDescriptor {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    match self {
+      BackupDescriptor::BackupID {
+        backup_id,
+        user_identity,
+      } => write!(
+        f,
+        "backup '{backup_id}' for user '{}'",
+        user_identity.user_id
+      ),
+      BackupDescriptor::Latest { username } => {
+        write!(f, "latest backup for user '{username}'")
       }
     }
   }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum RequestedData {
+  BackupID,
+  UserKeys,
+  UserData,
+}
+
+pub async fn run(
+  url: reqwest::Url,
+  backup_descriptor: BackupDescriptor,
+  requested_data: RequestedData,
+) -> Result<Vec<u8>, Error> {
+  println!("Pulling data: {requested_data:?}, from {backup_descriptor}");
+
+  let client = reqwest::Client::new();
+  let url = url.join("backups/")?;
+  let url = match &backup_descriptor {
+    BackupDescriptor::BackupID { backup_id, .. } => {
+      url.join(&format!("{backup_id}/"))?
+    }
+    BackupDescriptor::Latest { username } => {
+      url.join(&format!("latest/{username}/"))?
+    }
+  };
+  let url = match &requested_data {
+    RequestedData::BackupID => url.join("backup_id")?,
+    RequestedData::UserKeys => url.join("user_keys")?,
+    RequestedData::UserData => url.join("user_data")?,
+  };
+
+  let mut request = client.get(url);
+
+  if let BackupDescriptor::BackupID { user_identity, .. } = backup_descriptor {
+    request = request.bearer_auth(user_identity.as_authorization_token()?)
+  }
+
+  let response = request.send().await?;
+
+  if !response.status().is_success() {
+    return Err(Error::HttpStatus(response.status()));
+  }
+
+  let result = response.bytes().await?.to_vec();
+
   Ok(result)
 }
