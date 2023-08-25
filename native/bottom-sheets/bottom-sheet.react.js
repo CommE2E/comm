a647baab06920df2ddddd2ba277a168efcd5701d
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-08-25 14:12:19 -0400

    [native] introduce barebones bottom sheet component
    Summary: This components introduces the barebones bottom sheet component. I am very much working off the idea that this is far from the finished bottom sheet component, and some stuff here might not look super polished. I thought for review this would be easier to follow the progression of this component as I add the bits and pieces to it.
    
    Test Plan: Please see the demo video to see how the bottom sheet looks/behaves so far
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8955

---
+++ native/bottom-sheets/bottom-sheet.react.js
@@ -0,0 +1,57 @@
+// @flow
+
+import { BottomSheetModal } from '@gorhom/bottom-sheet';
+import * as React from 'react';
+
+import { useStyles } from '../themes/colors.js';
+
+type Props = {
+  +children: React.Node,
+};
+
+function ForwardedBottomSheet(
+  props: Props,
+  ref: React.Ref<typeof BottomSheetModal>,
+): React.Node {
+  const { children } = props;
+
+  const styles = useStyles(unboundStyles);
+
+  const snapPoints = React.useMemo(() => ['25%', '50%'], []);
+
+  return (
+    <BottomSheetModal
+      ref={ref}
+      backgroundStyle={styles.background}
+      snapPoints={snapPoints}
+      handleIndicatorStyle={styles.handleIndicator}
+    >
+      {children}
+    </BottomSheetModal>
+  );
+}
+
+const unboundStyles = {
+  background: {
+    backgroundColor: 'modalForeground',
+  },
+  handleIndicator: {
+    backgroundColor: 'modalKnob',
+    width: 64,
+  },
+};
+
+const BottomSheet: React.AbstractComponent<
+  Props,
+  React.ElementRef<typeof BottomSheetModal>,
+> = React.forwardRef<Props, React.ElementRef<typeof BottomSheetModal>>(
+  ForwardedBottomSheet,
+);
+BottomSheet.displayName = 'BottomSheet';
+
+const MemoizedBottomSheet: typeof BottomSheet = React.memo<
+  Props,
+  React.ElementRef<typeof BottomSheetModal>,
+>(BottomSheet);
+
+export default MemoizedBottomSheet;
