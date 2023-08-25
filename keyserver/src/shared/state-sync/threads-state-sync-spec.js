a31857662e6d9638b70b6091577e13da63b3e596
Tomasz Palys <tomasz.palys@swmansion.com>
2023-08-25 12:42:46 +0200

    [keyserver] Simplify fetching threads
    Summary: All the other fetchers return a collection directly without intermediate object - we can do the same for threads so that the code becomes simpler and more simplifications could be made further in the stack.
    
    https://linear.app/comm/issue/ENG-4654/unify-interfaces-in-session-utils
    
    Depends on D8941
    
    Test Plan: Modify threads in the keyserver db and check if the state is correctly fixed.
    
    Reviewers: kamil, bartek, inka

--- keyserver/src/shared/state-sync/threads-state-sync-spec.js
+++ keyserver/src/shared/state-sync/threads-state-sync-spec.js
@@ -5,22 +5,25 @@ import type { CalendarQuery } from 'lib/types/entry-types.js';
 import type { StateSyncSpec } from './state-sync-spec.js';
 import {
   fetchThreadInfos,
-  type FetchThreadInfosResult,
+  type RawThreadInfos,
 } from '../../fetchers/thread-fetchers.js';
 import type { Viewer } from '../../session/viewer.js';
 
-export const threadsStateSyncSpec: StateSyncSpec<FetchThreadInfosResult> =
+export const threadsStateSyncSpec: StateSyncSpec<RawThreadInfos> =
   Object.freeze({
-    fetch(
+    async fetch(
       viewer: Viewer,
       query: $ReadOnlyArray<CalendarQuery>,
       ids?: $ReadOnlySet<string>,
     ) {
+      let result;
       if (ids) {
-        return fetchThreadInfos(viewer, {
+        result = await fetchThreadInfos(viewer, {
           threadIDs: ids,
         });
+      } else {
+        result = await fetchThreadInfos(viewer);
       }
-      return fetchThreadInfos(viewer);
+      return result.threadInfos;
     },
   });
