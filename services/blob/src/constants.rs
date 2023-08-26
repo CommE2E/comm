91e9dcd3d77bbdfaff41cfd446e6340dada98172
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2023-08-26 08:41:22 +0200

    [blob-service] Remove gRPC service, change default port
    Summary:
    In D8796 we removed last blob gRPC client. This diff removes gRPC server as well.
    
    First part of [[ https://linear.app/comm/issue/ENG-4297/blob-service-delete-grpc-interface | ENG-4297 ]].
    - Deletes all gRPC related code from Blob service.
    - Changes default HTTP port to 50053.
    
    Test Plan: HTTP service still works.
    
    Reviewers: michal!, varun, patryk, jon
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8959

--- services/blob/src/constants.rs
+++ services/blob/src/constants.rs
@@ -1,27 +1,7 @@
 // Assorted constants
 
-pub const DEFAULT_GRPC_PORT: u16 = 50051;
-pub const DEFAULT_HTTP_PORT: u16 = 51001;
+pub const DEFAULT_HTTP_PORT: u16 = 50053;
 pub const MPSC_CHANNEL_BUFFER_CAPACITY: usize = 1;
-
-/// 4MB limit
-///
-/// WARNING: use keeping in mind that grpc adds its own headers to messages
-/// https://github.com/grpc/grpc/blob/master/doc/PROTOCOL-HTTP2.md
-/// so the message that actually is being sent over the network looks like this
-/// ```
-/// [Compressed-Flag] [Message-Length] [Message]
-///    [Compressed-Flag]   1 byte  - added by grpc
-///    [Message-Length]    4 bytes - added by grpc
-///    [Message]           N bytes - actual data
-/// ```
-/// so for every message we get 5 additional bytes of data
-/// as [mentioned here](https://github.com/grpc/grpc/issues/15734#issuecomment-396962671),
-/// gRPC stream may contain more than one message
-pub const GRPC_CHUNK_SIZE_LIMIT: u64 = 4 * 1024 * 1024;
-
-/// See [`GRPC_CHUNK_SIZE_LIMIT`] description for details
-pub const GRPC_METADATA_SIZE_PER_MESSAGE: u64 = 5;
 
 // HTTP constants
 
@@ -48,18 +28,6 @@ pub mod db {
   pub const ATTR_S3_PATH: &str = "s3_path";
   pub const ATTR_UNCHECKED: &str = "unchecked";
 }
-
-// old DynamoDB constants
-
-pub const BLOB_TABLE_NAME: &str = "blob-service-blob";
-pub const BLOB_TABLE_BLOB_HASH_FIELD: &str = "blobHash";
-pub const BLOB_TABLE_S3_PATH_FIELD: &str = "s3Path";
-pub const BLOB_TABLE_CREATED_FIELD: &str = "created";
-
-pub const BLOB_REVERSE_INDEX_TABLE_NAME: &str = "blob-service-reverse-index";
-pub const BLOB_REVERSE_INDEX_TABLE_HOLDER_FIELD: &str = "holder";
-pub const BLOB_REVERSE_INDEX_TABLE_BLOB_HASH_FIELD: &str = "blobHash";
-pub const BLOB_REVERSE_INDEX_TABLE_HASH_INDEX_NAME: &str = "blobHash-index";
 
 // Environment variables
 
