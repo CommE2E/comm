8c7226c8debbd8534e25673be37f31b401f11d3a
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2023-08-26 08:41:22 +0200

    [blob-service] Remove gRPC service, change default port
    Summary:
    In D8796 we removed last blob gRPC client. This diff removes gRPC server as well.
    
    First part of [[ https://linear.app/comm/issue/ENG-4297/blob-service-delete-grpc-interface | ENG-4297 ]].
    - Deletes all gRPC related code from Blob service.
    - Changes default HTTP port to 50053.
    
    Test Plan: HTTP service still works.
    
    Reviewers: michal, varun, patryk, jon
    
    Reviewed By: michal
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8959

--- services/blob/build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-  println!("cargo:rerun-if-changed=src/main.rs");
-
-  println!("cargo:rerun-if-changed=../../shared/protos/blob.proto");
-  tonic_build::compile_protos("../../shared/protos/blob.proto")
-    .expect("Failed to compile protobuf file");
-}
