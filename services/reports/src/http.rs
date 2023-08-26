b15e03a44d1161f7540c0655e9ba802cd4367486
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2023-08-26 18:02:26 +0200

    [reports-service] Add service error types
    Summary:
    Adds service error types to be used by the reports service and conversions to HTTP error responses.
    
    Depends on D8951
    
    Test Plan: Tested together with next diffs
    
    Reviewers: michal, patryk, varun, jon
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8968

--- services/reports/src/http.rs
+++ services/reports/src/http.rs
@@ -1,10 +1,15 @@
-use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::error::{
+  ErrorBadRequest, ErrorInternalServerError, ErrorServiceUnavailable,
+  ErrorUnsupportedMediaType,
+};
+use actix_web::{web, App, HttpResponse, HttpServer, ResponseError};
 use anyhow::Result;
-use tracing::info;
+use http::StatusCode;
+use tracing::{debug, error, info, trace, warn};
 
 use crate::config::CONFIG;
 use crate::constants::REQUEST_BODY_JSON_SIZE_LIMIT;
-use crate::service::ReportsService;
+use crate::service::{ReportsService, ReportsServiceError};
 
 pub async fn run_http_server(service: ReportsService) -> Result<()> {
   use actix_web::middleware::{Logger, NormalizePath};
@@ -33,4 +38,62 @@ pub async fn run_http_server(service: ReportsService) -> Result<()> {
   .await?;
 
   Ok(())
+}
+
+fn handle_reports_service_error(err: &ReportsServiceError) -> actix_web::Error {
+  use aws_sdk_dynamodb::Error as DynamoDBError;
+  use comm_services_lib::database::Error as DBError;
+
+  trace!("Handling reports service error: {:?}", err);
+  match err {
+    ReportsServiceError::UnsupportedReportType => {
+      ErrorUnsupportedMediaType("unsupported report type")
+    }
+    ReportsServiceError::SerdeError(err) => {
+      error!("Serde error: {0:?} - {0}", err);
+      ErrorInternalServerError("internal error")
+    }
+    ReportsServiceError::ParseError(err) => {
+      debug!("Parse error: {0:?} - {0}", err);
+      ErrorBadRequest("invalid input format")
+    }
+    ReportsServiceError::BlobError(err) => {
+      error!("Blob Service error: {0:?} - {0}", err);
+      ErrorInternalServerError("internal error")
+    }
+    ReportsServiceError::DatabaseError(db_err) => match db_err {
+      // retriable errors
+      DBError::MaxRetriesExceeded
+      | DBError::AwsSdk(
+        DynamoDBError::InternalServerError(_)
+        | DynamoDBError::ProvisionedThroughputExceededException(_)
+        | DynamoDBError::RequestLimitExceeded(_),
+      ) => {
+        warn!("AWS transient error occurred");
+        ErrorServiceUnavailable("please retry")
+      }
+      err => {
+        error!("Unexpected database error: {0:?} - {0}", err);
+        ErrorInternalServerError("internal error")
+      }
+    },
+    #[allow(unreachable_patterns)]
+    err => {
+      error!("Received an unexpected error: {0:?} - {0}", err);
+      ErrorInternalServerError("server error")
+    }
+  }
+}
+
+/// This allow us to `await?` blob service calls in HTTP handlers
+impl ResponseError for ReportsServiceError {
+  fn error_response(&self) -> HttpResponse {
+    handle_reports_service_error(self).error_response()
+  }
+
+  fn status_code(&self) -> StatusCode {
+    handle_reports_service_error(self)
+      .as_response_error()
+      .status_code()
+  }
 }
