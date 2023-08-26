e756ae3da5dba0d154ee0d9be687bb528653b3b4
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2023-08-26 08:41:22 +0200

    [blob-service] Remove gRPC service, change default port
    Summary:
    In D8796 we removed last blob gRPC client. This diff removes gRPC server as well.
    
    First part of [[ https://linear.app/comm/issue/ENG-4297/blob-service-delete-grpc-interface | ENG-4297 ]].
    - Deletes all gRPC related code from Blob service.
    - Changes default HTTP port to 50053.
    
    Test Plan: HTTP service still works.
    
    Reviewers: michal!, varun, patryk, jon
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8959

--- services/blob/src/main.rs
+++ services/blob/src/main.rs
@@ -1,7 +1,6 @@
 pub mod config;
 pub mod constants;
 pub mod database;
-pub mod grpc;
 pub mod http;
 pub mod s3;
 pub mod service;
@@ -29,21 +28,17 @@ async fn main() -> Result<()> {
   config::parse_cmdline_args()?;
 
   let aws_config = config::load_aws_config().await;
-  let db = database::old::DatabaseClient::new(&aws_config);
+  let db = database::DatabaseClient::new(&aws_config);
   let s3 = s3::S3Client::new(&aws_config);
 
-  let new_db = database::DatabaseClient::new(&aws_config);
   let service = service::BlobService::new(
-    new_db,
-    s3.clone(),
+    db,
+    s3,
     BlobServiceConfig {
       instant_delete_orphaned_blobs: true,
       ..Default::default()
     },
   );
 
-  tokio::select! {
-    http_result = crate::http::run_http_server(service) => http_result,
-    grpc_result = crate::grpc::run_grpc_server(db, s3) => grpc_result,
-  }
+  crate::http::run_http_server(service).await
 }
