ce80f84a905a72aa74cd06860c24a6b966a58d28
xsanm <kkurowski.kk@gmail.com>
2023-08-28 13:00:19 +0200

    [client-backup] implement function to encrypt backup
    Summary:
    Implementing backup encryption with `AES-256` in `GCM` mode.
    
    Depends on D8998
    
    Test Plan: Tested with next diff.
    
    Reviewers: michal, marcin, bartek
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8999

--- native/backup/encryption.js
+++ native/backup/encryption.js
@@ -1,7 +1,12 @@
 // @flow
 
+import { hexToUintArray } from 'lib/media/data-utils.js';
+import type { Backup, BackupEncrypted } from 'lib/types/backup-types.js';
+
+import { convertObjToBytes } from './conversion-utils.js';
 import { fetchNativeKeychainCredentials } from '../account/native-credentials.js';
 import { commCoreModule } from '../native-modules.js';
+import * as AES from '../utils/aes-crypto-module.js';
 
 async function getBackupKey(backupID: string): Promise<Uint8Array> {
   const nativeCredentials = await fetchNativeKeychainCredentials();
@@ -13,4 +18,17 @@ async function getBackupKey(backupID: string): Promise<Uint8Array> {
   return new Uint8Array(backupKey);
 }
 
-export { getBackupKey };
+async function encryptBackup(backup: Backup): Promise<BackupEncrypted> {
+  const { backupID, userKeys, userData } = backup;
+  const userKeysBytes = convertObjToBytes(userKeys);
+  const backupKey = await getBackupKey(backupID);
+  const ct1 = AES.encrypt(backupKey, userKeysBytes);
+
+  const userDataBytes = convertObjToBytes(userData);
+  const backupDataKey = hexToUintArray(userKeys.backupDataKey);
+  const ct2 = AES.encrypt(backupDataKey, userDataBytes);
+
+  return { backupID, userKeys: ct1, userData: ct2 };
+}
+
+export { getBackupKey, encryptBackup };
