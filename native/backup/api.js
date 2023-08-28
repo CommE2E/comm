df328a3d2a5d4f0b9e6f5f86d1a3ed0325ca9db7
xsanm <kkurowski.kk@gmail.com>
2023-08-28 12:41:06 +0200

    [client-backup] implement API call to upload backup
    Summary:
    Created multipart form data and uploads it with authentication.
    
    Depends on D8992
    
    Test Plan: Call this with mock data and check if works, call it with wrong data/auth to make sure it will be rejected.
    
    Reviewers: michal, marcin, bartek
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8993

---
+++ native/backup/api.js
@@ -0,0 +1,57 @@
+// @flow
+
+import base64 from 'base-64';
+
+import backupService from 'lib/facts/backup-service.js';
+import type { BackupAuth, BackupEncrypted } from 'lib/types/backup-types.js';
+import { makeBackupServiceEndpointURL } from 'lib/utils/backup-service.js';
+import { toBase64URL } from 'lib/utils/base64.js';
+
+import { commUtilsModule } from '../native-modules.js';
+
+function getBackupFormData(backup: BackupEncrypted): FormData {
+  const { backupID, userKeys, userData } = backup;
+  const userKeysHash = commUtilsModule.sha256(userKeys.buffer);
+  const userDataHash = commUtilsModule.sha256(userData.buffer);
+  const userKeysStr = commUtilsModule.base64EncodeBuffer(userKeys.buffer);
+  const userDataStr = commUtilsModule.base64EncodeBuffer(userData.buffer);
+
+  const formData = new FormData();
+  formData.append('backup_id', backupID);
+  formData.append('user_keys_hash', toBase64URL(userKeysHash));
+  formData.append('user_keys', userKeysStr);
+  formData.append('user_data_hash', toBase64URL(userDataHash));
+  formData.append('user_data', userDataStr);
+  formData.append('attachments', '');
+  return formData;
+}
+
+function getBackupAuthorizationHeader(auth: BackupAuth) {
+  const authStr = JSON.stringify(auth);
+  const authBase64 = base64.encode(authStr);
+  return `Bearer ${authBase64}`;
+}
+
+async function uploadBackup(backup: BackupEncrypted, auth: BackupAuth) {
+  const authHeader = getBackupAuthorizationHeader(auth);
+
+  const uploadBackupEndpoint = backupService.httpEndpoints.UPLOAD_BACKUP;
+  const sendBackupResponse = await fetch(
+    makeBackupServiceEndpointURL(uploadBackupEndpoint),
+    {
+      method: uploadBackupEndpoint.method,
+      body: getBackupFormData(backup),
+      headers: {
+        'Content-Type': 'multipart/form-data',
+        'Authorization': authHeader,
+      },
+    },
+  );
+
+  if (!sendBackupResponse.ok) {
+    const { status, statusText } = sendBackupResponse;
+    throw new Error(`Server responded with HTTP ${status}: ${statusText}`);
+  }
+}
+
+export { uploadBackup };
