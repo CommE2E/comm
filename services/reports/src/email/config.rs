c6a48c23f71d22c1d78eab596714da1b440bf1b7
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2023-08-28 15:21:59 +0200

    [reports-service] Parse email config from CLI args
    Summary:
    Introduced possibility to pass email config either via CLI args, or from a file (with path configurable from CLI).
    The Postmark API token can also be passed as an env variable.
    
    Config being None means that e-mails are disabled.
    
    Depends on D8978
    
    Test Plan:
    Unit tests, also printed the config object and confirmed that:
    - Config is None if no args passed and no `email-config.json` present
    - Config is Some if no args passed and valid `email-config.json` present
    - Config is None if no args passed and invalid `email-config.json` present
    - Config from `email-config.json` is overriden by inline json passed as `--email-config` arg
    - Error is raised if `--email-config` arg contains invalid JSON (malfolmed or missing fields)
    - Error is raised if both `--email-config` and `--email-config-file` are present at the same time
    - Error is raised if `--email-config-file` contains invalid path (e.g. file not exists)
    - Error is raised if `--email-config-file` contains path to incorrect JSON
    - Postmark token is read from the config file
    - If `POSTMARK_TOKEN` env is present, it overrides the file value
    - If the token isn't present in the config file or env, an warn is displayed and config is None
    
    Reviewers: michal, patryk, varun, jon
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8980

--- services/reports/src/email/config.rs
+++ services/reports/src/email/config.rs
@@ -1,9 +1,11 @@
-use std::{collections::HashMap, str::FromStr};
+use anyhow::{Context, Result};
+use std::{collections::HashMap, path::PathBuf, str::FromStr};
 use tracing::warn;
 
 use crate::report_types::ReportType;
 
 const ENV_POSTMARK_TOKEN: &str = "POSTMARK_TOKEN";
+const DEFAULT_CONFIG_PATH: &str = "./email-config.json";
 
 #[derive(Clone, Debug, Hash, Eq, PartialEq, serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -61,6 +63,56 @@ impl FromStr for EmailConfig {
   }
 }
 
+/// Email-related CLI arguments for clap
+#[derive(clap::Args)]
+#[group(multiple = false)]
+pub struct EmailArgs {
+  // these args are mutually exclusive
+  #[arg(long = "email-config")]
+  config_content: Option<EmailConfig>,
+
+  #[arg(
+    long = "email-config-file",
+    default_value = DEFAULT_CONFIG_PATH,
+    value_hint = clap::ValueHint::FilePath
+  )]
+  config_file: Option<PathBuf>,
+}
+
+impl EmailArgs {
+  pub fn parse(&self) -> Result<Option<EmailConfig>> {
+    if self
+      .config_content
+      .as_ref()
+      .is_some_and(|it| it.postmark_token.is_some())
+    {
+      return Ok(self.config_content.clone());
+    }
+
+    let Some(path) = self.config_file.as_ref() else {
+      return Ok(None);
+    };
+
+    let file_contents = match std::fs::read_to_string(path) {
+      Ok(contents) => contents,
+      Err(_) if path.to_str() == Some(DEFAULT_CONFIG_PATH) => {
+        // Failed to read but it's default path so we can skip
+        return Ok(None);
+      }
+      err => err.with_context(|| format!("Failed to read path: {path:?}"))?,
+    };
+
+    let cfg = EmailConfig::from_str(&file_contents)
+      .context("Failed to parse email config file")?;
+
+    // Don't provide emails config if token not present
+    if cfg.postmark_token.is_none() {
+      return Ok(None);
+    }
+    Ok(Some(cfg))
+  }
+}
+
 fn postmark_token_from_env() -> Option<String> {
   std::env::var(ENV_POSTMARK_TOKEN)
     .ok()
@@ -69,6 +121,8 @@ fn postmark_token_from_env() -> Option<String> {
 
 #[cfg(test)]
 mod tests {
+  use once_cell::sync::Lazy;
+
   use super::*;
 
   const RAW_CFG: &str = r#"{
@@ -78,6 +132,18 @@ mod tests {
     }"#;
   const RAW_CFG_NO_TOKEN: &str =
     r#"{"senderEmail": "a@b.c", "mailingGroups": {} }"#;
+
+  static EXAMPLE_CFG: Lazy<EmailConfig> = Lazy::new(|| EmailConfig {
+    postmark_token: Some("hello".to_string()),
+    sender_email: "foo@bar.com".to_string(),
+    mailing_groups: HashMap::new(),
+  });
+
+  static EXAMPLE_CFG_NO_TOKEN: Lazy<EmailConfig> = Lazy::new(|| EmailConfig {
+    postmark_token: None,
+    sender_email: "foo@bar.com".to_string(),
+    mailing_groups: HashMap::new(),
+  });
 
   #[test]
   fn test_postmark_token() {
@@ -103,5 +169,51 @@ mod tests {
       .postmark_token
       .as_ref()
       .is_some_and(|token| token == "world"));
+  }
+
+  #[test]
+  fn parse_args_no_token() {
+    // should return none when config exists but has no token
+    let mut args = EmailArgs {
+      config_content: Some(EXAMPLE_CFG_NO_TOKEN.clone()),
+      config_file: None,
+    };
+    let cfg = args.parse().expect("failed to parse");
+    assert!(cfg.is_none());
+
+    args.config_content = Some(EXAMPLE_CFG.clone());
+    let cfg = args.parse().expect("failed to parse");
+    assert!(cfg.is_some());
+  }
+
+  #[test]
+  fn parse_args_priority() {
+    // direct content should have higher priority than file
+    let args = EmailArgs {
+      config_content: Some(EXAMPLE_CFG.clone()),
+      config_file: Some("/hello.json".into()),
+    };
+    let cfg = args.parse().expect("failed to parse");
+    assert!(cfg.is_some());
+    assert_eq!(cfg.unwrap().sender_email, EXAMPLE_CFG.sender_email);
+  }
+
+  #[test]
+  fn parse_skips_default_path() {
+    let args = EmailArgs {
+      config_content: None,
+      config_file: Some("not/exists.json".into()),
+    };
+    args.parse().expect_err("parse should fail");
+
+    let args = EmailArgs {
+      config_content: None,
+      config_file: Some(DEFAULT_CONFIG_PATH.into()),
+    };
+    // if this fails, check if your email-config.json is correct
+    let _ = args.parse().expect("failed to parse");
+
+    // we cannot assert if parsed config is none because the actual file
+    // can exist on developer's machine
   }
 }
