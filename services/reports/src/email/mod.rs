a6aa509088ecfdfe100a8155e47ba83cbfd9306b
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2023-08-28 22:18:57 +0200

    [reports-service] Add functions to send e-mails
    Summary:
    This diff adds a Postmark client and two functions: One to compose an e-mail object and another to send all the emails in batch
    
    Depends on D8986
    
    Test Plan: Test plan in child diff D898 where these functions are called
    
    Reviewers: michal, patryk, jon, tomek
    
    Reviewed By: michal, tomek
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8987

--- services/reports/src/email/mod.rs
+++ services/reports/src/email/mod.rs
@@ -1,2 +1,73 @@
+use postmark::{
+  api::email::{self, SendEmailBatchRequest},
+  reqwest::{PostmarkClient, PostmarkClientError},
+  Query,
+};
+use tracing::{debug, trace, warn};
+
+use crate::{
+  config::CONFIG,
+  report_types::{ReportID, ReportInput, ReportType},
+};
+
 pub mod config;
 mod template;
+
+pub type EmailError = postmark::QueryError<PostmarkClientError>;
+
+pub struct ReportEmail {
+  report_type: ReportType,
+  rendered_message: String,
+  subject: String,
+}
+
+pub fn prepare_email(
+  report_input: &ReportInput,
+  report_id: &ReportID,
+  user_id: Option<&str>,
+) -> ReportEmail {
+  let message =
+    template::render_email_for_report(report_input, report_id, user_id);
+  let subject = template::subject_for_report(report_input, user_id);
+  ReportEmail {
+    report_type: report_input.report_type,
+    rendered_message: message,
+    subject,
+  }
+}
+
+pub async fn send_emails(
+  emails: impl IntoIterator<Item = ReportEmail>,
+) -> Result<(), EmailError> {
+  let Some(email_config) = CONFIG.email_config() else {
+    debug!("E-mail config unavailable. Skipping sending e-mails");
+    return Ok(());
+  };
+
+  // it's cheap to build this every time
+  let client = PostmarkClient::builder()
+    .token(&email_config.postmark_token)
+    .build();
+
+  let requests: SendEmailBatchRequest = emails
+    .into_iter()
+    .filter_map(|item| {
+      let Some(recipient) = email_config.recipient_for_report_type(&item.report_type) else {
+        warn!("Recipient E-mail for {:?} not configured. Skipping", &item.report_type);
+        return None;
+      };
+
+      let email = email::SendEmailRequest::builder()
+        .from(&email_config.sender_email)
+        .to(recipient)
+        .body(email::Body::html(item.rendered_message))
+        .subject(item.subject)
+        .build();
+      Some(email)
+    })
+    .collect();
+
+  let responses = requests.execute(&client).await?;
+  trace!(?responses, "E-mails sent successfully.");
+  Ok(())
+}
