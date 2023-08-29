19b681b7a77e24a6992825b3154aa9a532103c2c
Tomasz Palys <tomasz.palys@swmansion.com>
2023-08-29 16:31:13 +0200

    [lib] Extract shared specs to a lib
    Summary: A part of a spec will be used in a lib. We can't migrate the whole spec there because `fetch` function is server-specific.
    
    Depends on D9008
    
    https://linear.app/comm/issue/ENG-4813/migrate-getclientresponsesselector-to-a-spec
    
    Test Plan: Flow
    
    Reviewers: kamil, bartek, inka

--- keyserver/src/shared/state-sync/entries-state-sync-spec.js
+++ keyserver/src/shared/state-sync/entries-state-sync-spec.js
@@ -1,17 +1,18 @@
 // @flow
 
 import { serverEntryInfosObject } from 'lib/shared/entry-utils.js';
+import { entriesStateSyncSpec as libSpec } from 'lib/shared/state-sync/entries-state-sync-spec.js';
 import type { CalendarQuery, RawEntryInfos } from 'lib/types/entry-types.js';
 
-import type { StateSyncSpec } from './state-sync-spec.js';
+import type { ServerStateSyncSpec } from './state-sync-spec.js';
 import {
   fetchEntryInfos,
   fetchEntryInfosByID,
 } from '../../fetchers/entry-fetchers.js';
 import type { Viewer } from '../../session/viewer.js';
 
-export const entriesStateSyncSpec: StateSyncSpec<RawEntryInfos> = Object.freeze(
-  {
+export const entriesStateSyncSpec: ServerStateSyncSpec<RawEntryInfos> =
+  Object.freeze({
     async fetch(
       viewer: Viewer,
       query: $ReadOnlyArray<CalendarQuery>,
@@ -23,11 +24,5 @@ export const entriesStateSyncSpec: StateSyncSpec<RawEntryInfos> = Object.freeze(
       const entriesResult = await fetchEntryInfos(viewer, query);
       return serverEntryInfosObject(entriesResult.rawEntryInfos);
     },
-    hashKey: 'entryInfos',
-    innerHashSpec: {
-      hashKey: 'entryInfo',
-      deleteKey: 'deleteEntryIDs',
-      rawInfosKey: 'rawEntryInfos',
-    },
-  },
-);
+    ...libSpec,
+  });
