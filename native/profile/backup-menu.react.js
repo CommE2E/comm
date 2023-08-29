158f47905916262505d16cea2dc56353c45e9017
xsanm <kkurowski.kk@gmail.com>
2023-08-29 16:34:16 +0200

    [client-backup] add backup restore test
    Summary:
    We can do it automatically, but I don't want to slow the app more. Result should be presented in Alert.
    
    Depends on D9011
    
    Test Plan: Test both successful and failed restore.
    
    Reviewers: michal, marcin, bartek
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D9004

--- native/profile/backup-menu.react.js
+++ native/profile/backup-menu.react.js
@@ -1,22 +1,45 @@
 // @flow
 
 import * as React from 'react';
-import { Switch, Text, View } from 'react-native';
+import { Alert, Switch, Text, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { useDispatch } from 'react-redux';
 
+import { getMessageForException } from 'lib/utils/errors.js';
+import { entries } from 'lib/utils/objects.js';
+
+import { useClientBackup } from '../backup/use-client-backup.js';
+import Button from '../components/button.react.js';
 import { setLocalSettingsActionType } from '../redux/action-types.js';
 import { useSelector } from '../redux/redux-utils.js';
-import { useStyles } from '../themes/colors.js';
+import { useColors, useStyles } from '../themes/colors.js';
 
 // eslint-disable-next-line no-unused-vars
 function BackupMenu(props: { ... }): React.Node {
   const styles = useStyles(unboundStyles);
   const dispatch = useDispatch();
+  const colors = useColors();
 
+  const userStore = useSelector(state => state.userStore);
   const isBackupEnabled = useSelector(
     state => state.localSettings.isBackupEnabled,
   );
+
+  const { restoreBackupProtocol } = useClientBackup();
+
+  const testRestore = React.useCallback(async () => {
+    let message;
+    try {
+      const result = await restoreBackupProtocol({ userStore });
+      message = entries(result)
+        .map(([key, value]) => `${key}: ${value}`)
+        .join('\n');
+    } catch (e) {
+      console.error(`Backup uploading error: ${e}`);
+      message = `Backup restore error: ${String(getMessageForException(e))}`;
+    }
+    Alert.alert('Restore protocol result', message);
+  }, [restoreBackupProtocol, userStore]);
 
   const onBackupToggled = React.useCallback(
     value => {
@@ -39,6 +62,19 @@ function BackupMenu(props: { ... }): React.Node {
           <Text style={styles.submenuText}>Toggle automatic backup</Text>
           <Switch value={isBackupEnabled} onValueChange={onBackupToggled} />
         </View>
+      </View>
+
+      <Text style={styles.header}>ACTIONS</Text>
+      <View style={styles.section}>
+        <Button
+          onPress={testRestore}
+          style={styles.row}
+          iosFormat="highlight"
+          iosHighlightUnderlayColor={colors.panelIosHighlightUnderlay}
+          iosActiveOpacity={0.85}
+        >
+          <Text style={styles.submenuText}>Test backup restore protocol</Text>
+        </Button>
       </View>
     </ScrollView>
   );
@@ -76,6 +112,12 @@ const unboundStyles = {
     color: 'panelForegroundLabel',
     flex: 1,
     fontSize: 16,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 24,
+    paddingVertical: 14,
   },
 };
 
