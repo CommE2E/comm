57926caeb9697610e9667ab034169ddfc70417f9
Inka Sokołowska <inka.d.sokolowska@gmail.com>
2023-08-30 14:38:45 +0200

    [keyserver][lib][native][web] Remove connection field from redux
    Summary:
    issue: https://linear.app/comm/issue/ENG-4460/refactor-connection-field
    This diff is the same as D8932, but this time we don't need a migration because `connection` is not being persisted. This diff does make it temporarily be presisted, becuse all of
    keyserverStore is being persisted. I will fix this in the next diff
    
    Test Plan: ran `yarn flow-all`.
    
    Reviewers: michal, kamil
    
    Reviewed By: michal
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D9023

--- native/redux/state-types.js
+++ native/redux/state-types.js
@@ -15,7 +15,6 @@ import type { LoadingStatus } from 'lib/types/loading-types.js';
 import type { MessageStore } from 'lib/types/message-types.js';
 import type { UserPolicies } from 'lib/types/policy-types.js';
 import type { ReportStore } from 'lib/types/report-types.js';
-import type { ConnectionInfo } from 'lib/types/socket-types.js';
 import type { ThreadStore } from 'lib/types/thread-types.js';
 import type { CurrentUserInfo, UserStore } from 'lib/types/user-types.js';
 import type { NotifPermissionAlertInfo } from 'lib/utils/push-alerts.js';
@@ -41,7 +40,6 @@ export type AppState = {
   +dataLoaded: boolean,
   +customServer: ?string,
   +notifPermissionAlertInfo: NotifPermissionAlertInfo,
-  +connection: ConnectionInfo,
   +actualizedCalendarQuery: CalendarQuery,
   +watchedThreadIDs: $ReadOnlyArray<string>,
   +lifecycleState: LifecycleState,
