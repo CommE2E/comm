c3e67ba139cdc03e290695659c959c7d2423c21c
Patryk Klatka <patryk.klatka@gmail.com>
2023-08-30 13:52:21 +0200

    [native] Introduce markdown component for chat mentions
    Summary:
    Solution for [ENG-4560](https://linear.app/comm/issue/ENG-4560/bold-mentions-when-user-has-access-to-given-chat)
    This diff creates a new component for chat mentions which handles bolding mentions when user has access to given chat.
    
    Depends on D8844.
    
    Test Plan:
    Before testing, please apply the entire native stack.
    
    1. Create a community with three users.
    2. Create a private chat for two chosen users.
    3. Mention private chat in community chat (e. g. `@[[256|8282:default text]]`).
    For users assigned to previously created private chat, the mention should be bold and render current thread name:
    {F697982}
    For users who are not in private chat, the mention should be rendered with plain text message style:
    {F697980}
    
    Reviewers: tomek, inka, rohan
    
    Reviewed By: rohan
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8845

---
+++ native/markdown/markdown-chat-mention.react.js
@@ -0,0 +1,29 @@
+// @flow
+
+import * as React from 'react';
+import { Text } from 'react-native';
+
+import type { ResolvedThreadInfo } from 'lib/types/thread-types.js';
+
+import { useMarkdownOnPressUtils } from './markdown-utils.js';
+
+type TextProps = React.ElementConfig<typeof Text>;
+type Props = {
+  +threadInfo: ResolvedThreadInfo,
+  +hasAccessToChat: boolean,
+  +children: React.Node,
+  ...TextProps,
+};
+function MarkdownChatMention(props: Props): React.Node {
+  const { threadInfo, hasAccessToChat, ...rest } = props;
+  const { isRevealed, onLongPressHandler } = useMarkdownOnPressUtils();
+  const shouldBePressable = hasAccessToChat ? isRevealed : false;
+  return (
+    <Text
+      onLongPress={shouldBePressable ? onLongPressHandler : null}
+      {...rest}
+    />
+  );
+}
+
+export default MarkdownChatMention;
