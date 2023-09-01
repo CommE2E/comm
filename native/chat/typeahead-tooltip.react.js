5fb5cfa6d620d67af91240eca2620eca3ca90a6c
Patryk Klatka <patryk.klatka@gmail.com>
2023-09-01 11:45:07 +0200

    [web][native] Rename usernamePrefix to textPrefix in TypeaheadMatchedStrings type
    Summary:
    This diff renames `usernamePrefix` to `textPrefix` in `TypeaheadMatchedStrings` type. Since we will show chats in mention typeahead tooltip, it is necessary to rename utilities to make code more generic.
    
    Depends on D8853, D8849.
    
    Test Plan: Check if `TypeaheadTooltip` works properly both on native and web.
    
    Reviewers: tomek, inka, rohan
    
    Reviewed By: rohan
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8896

--- native/chat/typeahead-tooltip.react.js
+++ native/chat/typeahead-tooltip.react.js
@@ -30,7 +30,7 @@ function TypeaheadTooltip(props: TypeaheadTooltipProps): React.Node {
     focusAndUpdateTextAndSelection,
   } = props;
 
-  const { textBeforeAtSymbol, usernamePrefix } = matchedStrings;
+  const { textBeforeAtSymbol, query } = matchedStrings;
 
   const styles = useStyles(unboundStyles);
 
@@ -40,7 +40,7 @@ function TypeaheadTooltip(props: TypeaheadTooltipProps): React.Node {
         const { newText, newSelectionStart } = getNewTextAndSelection(
           textBeforeAtSymbol,
           text,
-          usernamePrefix,
+          query,
           item,
         );
 
@@ -64,7 +64,7 @@ function TypeaheadTooltip(props: TypeaheadTooltipProps): React.Node {
       styles.buttonLabel,
       textBeforeAtSymbol,
       text,
-      usernamePrefix,
+      query,
       focusAndUpdateTextAndSelection,
     ],
   );
