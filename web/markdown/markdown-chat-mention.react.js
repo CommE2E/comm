3cd683e28cedbc63f03ea44ff885e5e8e8aeadf1
Patryk Klatka <patryk.klatka@gmail.com>
2023-09-01 11:44:26 +0200

    [web] Make chat mentions clickable
    Summary:
    Solution for [ENG-4561](https://linear.app/comm/issue/ENG-4561/redirect-to-given-chat-when-mention-is-clicked).
    
    This diff uses `useOnClickThread` hook which redirects to chat when mention is clicked and uses it in `markdown-chat-mention.react.js`
    
    Depends on D8852.
    
    Test Plan:
    Before testing, please apply the entire web stack.
    
    Test if mentions inside a spoiler are properly rendered and are clickable only when spoiler is opened:
    {F698073}
    
    Reviewers: tomek, inka, rohan
    
    Reviewed By: inka
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8875

--- web/markdown/markdown-chat-mention.react.js
+++ web/markdown/markdown-chat-mention.react.js
@@ -2,15 +2,20 @@
 
 import * as React from 'react';
 
+import type { ResolvedThreadInfo } from 'lib/types/thread-types.js';
+
 import css from './markdown.css';
+import { useOnClickThread } from '../selectors/thread-selectors.js';
 
 type MarkdownChatMentionProps = {
+  +threadInfo: ResolvedThreadInfo,
   +hasAccessToChat: boolean,
   +text: string,
 };
 
 function MarkdownChatMention(props: MarkdownChatMentionProps): React.Node {
-  const { hasAccessToChat, text } = props;
+  const { threadInfo, hasAccessToChat, text } = props;
+  const onClick = useOnClickThread(threadInfo);
 
   if (!hasAccessToChat) {
     return text;
@@ -18,7 +23,9 @@ function MarkdownChatMention(props: MarkdownChatMentionProps): React.Node {
 
   return (
     <strong>
-      <a className={css.chatMention}>{text}</a>
+      <a className={css.chatMention} onClick={onClick}>
+        {text}
+      </a>
     </strong>
   );
 }
