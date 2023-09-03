2e822dac132be0e173ac8d81aeb4887d663135fd
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-09-03 18:28:58 -0400

    [native] introduce UserProfileRelationshipButton
    Summary: This diff introduces the `UserProfileRelationshipButton` component that will be rendered on a user profile. This button will be used to do relationship actions such as sending/withdrawing a friend request and adding/rejecting a friend request between the viewer and the user of the profile
    
    Test Plan: Please see the demo videos/screenshots below
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D9258

---
+++ native/components/user-profile-relationship-button.react.js
@@ -0,0 +1,185 @@
+// @flow
+
+import Icon from '@expo/vector-icons/FontAwesome5.js';
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+import { useRelationshipPrompt } from 'lib/hooks/relationship-prompt.js';
+import { userRelationshipStatus } from 'lib/types/relationship-types.js';
+import type { ThreadInfo } from 'lib/types/thread-types.js';
+import type { UserInfo } from 'lib/types/user-types';
+
+import Button from './button.react.js';
+import { useStyles, useColors } from '../themes/colors.js';
+import Alert from '../utils/alert.js';
+
+const onErrorCallback = () => {
+  Alert.alert('Unknown error', 'Uhh... try again?', [{ text: 'OK' }]);
+};
+
+type Props = {
+  +threadInfo: ThreadInfo,
+  +pendingPersonalThreadUserInfo?: UserInfo,
+};
+
+function UserProfileRelationshipButton(props: Props): React.Node {
+  const { threadInfo, pendingPersonalThreadUserInfo } = props;
+
+  const {
+    otherUserInfo,
+    callbacks: { friendUser, unfriendUser },
+  } = useRelationshipPrompt(
+    threadInfo,
+    onErrorCallback,
+    pendingPersonalThreadUserInfo,
+  );
+
+  const styles = useStyles(unboundStyles);
+  const colors = useColors();
+
+  const acceptFriendRequestButtonStyle = React.useMemo(
+    () => [
+      styles.buttonContainer,
+      styles.greenButton,
+      styles.incomingFriendRequestButton,
+    ],
+    [
+      styles.buttonContainer,
+      styles.greenButton,
+      styles.incomingFriendRequestButton,
+    ],
+  );
+
+  const rejectFriendRequestButtonStyle = React.useMemo(
+    () => [
+      styles.buttonContainer,
+      styles.redButton,
+      styles.incomingFriendRequestButton,
+    ],
+    [
+      styles.buttonContainer,
+      styles.incomingFriendRequestButton,
+      styles.redButton,
+    ],
+  );
+
+  const withdrawFriendRequestButtonStyle = React.useMemo(
+    () => [styles.buttonContainer, styles.redButton],
+    [styles.buttonContainer, styles.redButton],
+  );
+
+  const addFriendButtonStyle = React.useMemo(
+    () => [styles.buttonContainer, styles.greenButton],
+    [styles.buttonContainer, styles.greenButton],
+  );
+
+  if (
+    !otherUserInfo ||
+    !otherUserInfo.username ||
+    otherUserInfo.relationshipStatus === userRelationshipStatus.FRIEND
+  ) {
+    return null;
+  }
+
+  if (
+    otherUserInfo.relationshipStatus === userRelationshipStatus.REQUEST_RECEIVED
+  ) {
+    return (
+      <View style={styles.incomingFriendRequestContainer}>
+        <Text style={styles.incomingFriendRequestLabel}>
+          Incoming friend request
+        </Text>
+        <View style={styles.incomingFriendRequestButtonsContainer}>
+          <Button style={acceptFriendRequestButtonStyle} onPress={friendUser}>
+            <Icon
+              name="user-plus"
+              size={22}
+              color={colors.floatingButtonLabel}
+              style={styles.buttonIcon}
+            />
+            <Text style={styles.buttonText}>Accept</Text>
+          </Button>
+          <View style={styles.incomingFriendRequestButtonGap} />
+          <Button style={rejectFriendRequestButtonStyle} onPress={unfriendUser}>
+            <Icon
+              name="user-minus"
+              size={22}
+              color={colors.floatingButtonLabel}
+              style={styles.buttonIcon}
+            />
+            <Text style={styles.buttonText}>Reject</Text>
+          </Button>
+        </View>
+      </View>
+    );
+  }
+
+  if (
+    otherUserInfo.relationshipStatus === userRelationshipStatus.REQUEST_SENT
+  ) {
+    return (
+      <Button style={withdrawFriendRequestButtonStyle} onPress={unfriendUser}>
+        <Icon
+          name="user-minus"
+          size={22}
+          color={colors.floatingButtonLabel}
+          style={styles.buttonIcon}
+        />
+        <Text style={styles.buttonText}>Withdraw Friend Request</Text>
+      </Button>
+    );
+  }
+
+  return (
+    <Button style={addFriendButtonStyle} onPress={friendUser}>
+      <Icon
+        name="user-plus"
+        size={22}
+        color={colors.floatingButtonLabel}
+        style={styles.buttonIcon}
+      />
+      <Text style={styles.buttonText}>Add Friend</Text>
+    </Button>
+  );
+}
+
+const unboundStyles = {
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 8,
+    marginTop: 16,
+    borderRadius: 8,
+  },
+  buttonIcon: {
+    paddingRight: 8,
+  },
+  buttonText: {
+    color: 'floatingButtonLabel',
+  },
+  redButton: {
+    backgroundColor: 'vibrantRedButton',
+  },
+  greenButton: {
+    backgroundColor: 'vibrantGreenButton',
+  },
+  incomingFriendRequestContainer: {
+    marginTop: 24,
+  },
+  incomingFriendRequestLabel: {
+    color: 'modalForegroundLabel',
+  },
+  incomingFriendRequestButtonsContainer: {
+    flexDirection: 'row',
+  },
+  incomingFriendRequestButton: {
+    marginTop: 8,
+    flex: 1,
+  },
+  incomingFriendRequestButtonGap: {
+    width: 8,
+  },
+};
+
+export default UserProfileRelationshipButton;
