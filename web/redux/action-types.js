168c4bbee7c60eb80940b9fe48e42d64295c4642
Michal Gniadek <michal.gniadek@swmansion.com>
2023-09-05 15:23:14 +0200

    [web] Add setInitialReduxState action
    Summary:
    [ENG-4752](https://linear.app/comm/issue/ENG-4752)
    Depends on D9124
    
    Action for the previously introduced endpoint. It's defined inside `web` and doesn't have the `STARTED`, `SUCCESS`, `FAILED` actions because:
    - This is a web-specific endpoint that returns (and expects) web-specific data (e.g. urlInfo, web nav info)
    - To simulate the previous behaviour as much possible this action shouldn't interact much with other reducers and that's why it's inside of the web reducer
    - We don't really have use case for tracking progress and failure currently. The app can't function at all until this endpoint is called successfuly
    - Adding the `STARTED` etc. variants and `useDispatchActionPromise` would require moving it to lib and `BaseAction`. But it can't be moved to `lib` because it uses `web`-specific types
    
    If ever decide to expand it to `native`, it shouldn't be much of a problem to rework it (the added code is quite small).
    
    Test Plan:
    Call:
    ```
    const callGetInitialReduxState = useServerCall(getInitialReduxState);
    const urlInfo = infoFromURL(decodeURI(window.location.href));
    const payload = await callGetInitialReduxState(urlInfo);
    dispatch({ type: setInitialReduxState, payload });
    ```
    after the app loaded.
    - Check if the action contained the expected payload.
    - Check that diff after applying this action only contained `lastUserInteractionCalendar`, `lastPruned`, `updatesCurrentAsOf` etc. (the "static" data was the same)
    
    Reviewers: kamil, inka, atul
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D9140

--- web/redux/action-types.js
+++ web/redux/action-types.js
@@ -1,6 +1,39 @@
 // @flow
 
+import type { CallServerEndpoint } from 'lib/utils/call-server-endpoint.js';
+import type { URLInfo } from 'lib/utils/url-utils.js';
+
+import type { InitialReduxState } from '../types/redux-types.js';
+
 export const updateNavInfoActionType = 'UPDATE_NAV_INFO';
 export const updateWindowDimensionsActionType = 'UPDATE_WINDOW_DIMENSIONS';
 export const updateWindowActiveActionType = 'UPDATE_WINDOW_ACTIVE';
 export const setDeviceIDActionType = 'SET_DEVICE_ID';
+export const setInitialReduxState = 'SET_INITIAL_REDUX_STATE';
+
+const getInitialReduxState =
+  (
+    callServerEndpoint: CallServerEndpoint,
+  ): (URLInfo => Promise<InitialReduxState>) =>
+  async urlInfo => {
+    const response = await callServerEndpoint(
+      'get_initial_redux_state',
+      urlInfo,
+    );
+    return {
+      navInfo: response.navInfo,
+      currentUserInfo: response.currentUserInfo,
+      entryStore: response.entryStore,
+      threadStore: response.threadStore,
+      userInfos: response.userInfos,
+      actualizedCalendarQuery: response.actualizedCalendarQuery,
+      messageStore: response.messageStore,
+      dataLoaded: response.dataLoaded,
+      pushApiPublicKey: response.pushApiPublicKey,
+      commServicesAccessToken: response.commServicesAccessToken,
+      inviteLinksStore: response.inviteLinksStore,
+      keyserverInfo: response.keyserverInfo,
+    };
+  };
+
+export { getInitialReduxState };
