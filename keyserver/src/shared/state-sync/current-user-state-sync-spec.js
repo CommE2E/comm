3ace7d6b233a4df1ccda53716bd711414011657e
Michal Gniadek <michal.gniadek@swmansion.com>
2023-09-05 11:31:47 +0200

    [keyserver] Remove old current user info
    Summary:
    [ENG-4850](https://linear.app/comm/issue/ENG-4850)
    Depends on D9110
    We can remove old current user info and simplify the code. Reasoning copied from the task:
    1. As you can see in D1463 when OldCurrentUserInfo the version was 86, while our current minimal supported version is 128
    2. `fetchCurrentUserInfo` uses `fetchLoggedInUserInfo`, which if you look at the code, never actually creates `OldCurrentUserInfo` structure, it always created the newer type
    3. Other code always uses fetchCurrentUserInfo as the source of truth so we can also change their signatures
    
    Test Plan: `yarn flow`, analyze all changes and make sure that all data is in the end fetched using `fetchCurrentUserInfo` or from the clients
    
    Reviewers: kamil, inka, atul
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D9111

--- keyserver/src/shared/state-sync/current-user-state-sync-spec.js
+++ keyserver/src/shared/state-sync/current-user-state-sync-spec.js
@@ -1,18 +1,15 @@
 // @flow
 
 import { currentUserStateSyncSpec as libSpec } from 'lib/shared/state-sync/current-user-state-sync-spec.js';
-import type {
-  CurrentUserInfo,
-  OldCurrentUserInfo,
-} from 'lib/types/user-types.js';
+import type { CurrentUserInfo } from 'lib/types/user-types.js';
 
 import type { ServerStateSyncSpec } from './state-sync-spec.js';
 import { fetchCurrentUserInfo } from '../../fetchers/user-fetchers.js';
 import type { Viewer } from '../../session/viewer.js';
 
 export const currentUserStateSyncSpec: ServerStateSyncSpec<
-  OldCurrentUserInfo | CurrentUserInfo,
-  OldCurrentUserInfo | CurrentUserInfo,
+  CurrentUserInfo,
+  CurrentUserInfo,
 > = Object.freeze({
   fetch(viewer: Viewer) {
     return fetchCurrentUserInfo(viewer);
