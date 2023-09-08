074761606d14498ce100b75d0d7252ba0a864225
Jonathan Ringer <jonathan.ringer@comm.app>
2023-09-08 16:33:02 +0200

    [Keyserver] Upload new onetime keys to identity service when requested
    Summary:
    When receiving a "refreshKeysRequest" from tunnelbroker, keyserver
    should upload new onetime keys to identity service.
    
    https://linear.app/comm/issue/ENG-4602
    
    Depends on D8751
    
    Test Plan: Integration test to follow, wanted to keep this JS only
    
    Reviewers: marcin, ashoat, jon
    
    Reviewed By: marcin, ashoat
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8752

--- keyserver/src/socket/tunnelbroker.js
+++ keyserver/src/socket/tunnelbroker.js
@@ -2,10 +2,16 @@
 
 import WebSocket from 'ws';
 
-import { type TBKeyserverConnectionInitializationMessage } from 'lib/types/tunnelbroker-messages.js';
+import {
+  type TBKeyserverConnectionInitializationMessage,
+  type MessageFromTunnelbroker,
+  tunnelbrokerMessageTypes,
+} from 'lib/types/tunnelbroker-messages.js';
+import { ServerError } from 'lib/utils/errors.js';
 
 import { fetchOlmAccount } from '../updaters/olm-account-updater.js';
-import type { IdentityInfo } from '../user/identity.js';
+import { type IdentityInfo } from '../user/identity.js';
+import { uploadNewOneTimeKeys } from '../utils/olm-utils.js';
 
 async function createAndMaintainTunnelbrokerWebsocket(
   identityInfo: IdentityInfo,
@@ -18,6 +24,15 @@ async function createAndMaintainTunnelbrokerWebsocket(
     identityInfo.userId,
     identityInfo.accessToken,
   );
+}
+
+function handleTBMessageEvent(event: ArrayBuffer): Promise<void> {
+  const message: MessageFromTunnelbroker = JSON.parse(event.toString());
+
+  if (message.type === tunnelbrokerMessageTypes.REFRESH_KEYS_REQUEST) {
+    return uploadNewOneTimeKeys(message.numberOfKeys);
+  }
+  throw new ServerError('unsupported_tunnelbroker_message');
 }
 
 function openTunnelbrokerConnection(
@@ -48,6 +63,8 @@ function openTunnelbrokerConnection(
     tunnelbrokerSocket.on('error', (error: Error) => {
       console.error('Tunnelbroker socket error', error.message);
     });
+
+    tunnelbrokerSocket.on('message', handleTBMessageEvent);
   } catch {
     console.log('Failed to open connection with Tunnelbroker');
   }
