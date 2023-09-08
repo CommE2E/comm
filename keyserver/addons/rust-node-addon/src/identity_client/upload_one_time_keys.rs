57a84506b15a6e57ee7cb6f5e65321302a3db77b
Jonathan Ringer <jonathan.ringer@comm.app>
2023-09-08 16:21:08 +0200

    [Keyserver/rust] Implement upload_one_time_keys to identity service
    Summary:
    Allow for the rust node addon to upload keys to identity
    service.
    
    https://linear.app/comm/issue/ENG-4601
    
    Depends on D8749
    
    Test Plan:
    Tested end-to-end in later diff
    ```
    cd keyserver/addons/rust-node-addon
    yarn build
    ```
    
    Reviewers: bartek, varun
    
    Reviewed By: bartek
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8750

---
+++ keyserver/addons/rust-node-addon/src/identity_client/upload_one_time_keys.rs
@@ -0,0 +1,40 @@
+use super::*;
+
+use identity_client::UploadOneTimeKeysRequest;
+use tracing::{debug, error};
+
+#[napi]
+#[instrument(skip_all)]
+pub async fn upload_one_time_keys(
+  user_id: String,
+  device_id: String,
+  access_token: String,
+  content_one_time_pre_keys: Vec<String>,
+  notif_one_time_pre_keys: Vec<String>,
+) -> bool {
+  let channel = match get_identity_service_channel().await {
+    Ok(c) => c,
+    Err(e) => {
+      error!(
+        "Error when attempting to connect to Identity service: {}",
+        e
+      );
+      return false;
+    }
+  };
+
+  let mut identity_client = IdentityClientServiceClient::new(channel);
+
+  let upload_request = UploadOneTimeKeysRequest {
+    user_id,
+    device_id,
+    access_token,
+    content_one_time_pre_keys,
+    notif_one_time_pre_keys,
+  };
+
+  debug!("Sending one time keys to Identity service");
+  let result = identity_client.upload_one_time_keys(upload_request).await;
+
+  result.is_ok()
+}
