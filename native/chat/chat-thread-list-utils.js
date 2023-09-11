2ce8245fa6e25474adca83d9e12ead721655bbb9
Atul Madhugiri <atulm@berkeley.edu>
2023-09-11 18:19:16 -0400

    [native] Pull static fns out of `ChatThreadList` to `ChatThreadListUtils`
    Summary:
    There are a handful of static functions in the `ChatThreadList` component. Figured I'd move these out to a separate utils file.
    
    (I initially had a huge diff that converted `ChatThreadList` to a functional component and added various memoizations. However, I realized that diff would be pretty difficult to review so I'm breaking down the refactor into hopefully easier to review pieces.)
    
    This diff is just one step in the process of converting `ChatThreadList` into a functional component. When this work is done, we will be able to avoid a lot of re-rendering and hopefully improve performance quite a bit.
    
    In subsequent diffs will
    1. Lift up "search-related" JSX
    2. etc.. continue turning `ChatThreadList` into a functional component so we can more precisely memoize things, particularly `ChatThreadListItem`s.
    
    ---
    
    Depends on D9134
    
    Test Plan: Search experience continues to work as expected. Can search for users/threads/etc. as before.
    
    Reviewers: ginsu, tomek, rohan
    
    Reviewed By: ginsu
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D9135

---
+++ native/chat/chat-thread-list-utils.js
@@ -0,0 +1,58 @@
+// @flow
+
+import _sum from 'lodash/fp/sum.js';
+import { Platform } from 'react-native';
+
+import {
+  chatThreadListItemHeight,
+  spacerHeight,
+} from './chat-thread-list-item.react.js';
+import type { Item } from './chat-thread-list.react.js';
+import { sidebarHeight } from './sidebar-item.react.js';
+
+function keyExtractor(item: Item): string {
+  if (item.type === 'chatThreadItem') {
+    return item.threadInfo.id;
+  } else if (item.type === 'empty') {
+    return 'empty';
+  }
+  return 'search';
+}
+
+function itemHeight(item: Item): number {
+  if (item.type === 'search') {
+    return Platform.OS === 'ios' ? 54.5 : 55;
+  }
+
+  // itemHeight for emptyItem might be wrong because of line wrapping
+  // but we don't care because we'll only ever be rendering this item
+  // by itself and it should always be on-screen
+  if (item.type === 'empty') {
+    return 123;
+  }
+
+  const baseHeight = chatThreadListItemHeight;
+  const sidebarsHeight = item.sidebars.length * sidebarHeight;
+  const spacerHeightAdjustment = item.sidebars.length > 0 ? spacerHeight : 0;
+
+  return baseHeight + sidebarsHeight + spacerHeightAdjustment;
+}
+
+function heightOfItems(data: $ReadOnlyArray<Item>): number {
+  return _sum(data.map(itemHeight));
+}
+
+function getItemLayout(
+  data: ?$ReadOnlyArray<Item>,
+  index: number,
+): { length: number, offset: number, index: number } {
+  if (!data) {
+    return { length: 0, offset: 0, index };
+  }
+  const offset = heightOfItems(data.filter((_, i): boolean => i < index));
+  const item = data[index];
+  const length = item ? itemHeight(item) : 0;
+  return { length, offset, index };
+}
+
+export { keyExtractor, itemHeight, heightOfItems, getItemLayout };
