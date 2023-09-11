ac54b888a06dc6ed2943c8e6867f51005c5f2906
Atul Madhugiri <atulm@berkeley.edu>
2023-09-10 20:49:29 -0400

    [native] Convert `ConnectedSwipeable` to functional component
    Summary: I thought the changes here would prevent `SwipeableThread` from re-rendering, but it looks like I misinterpreted some profiling sessions... that said I already did this work so figured I'd put it up (assuming it isn't burdensome for reviewers to review, etc.)
    
    Test Plan: `SwipeableThread` continues to look and work as before.
    
    Reviewers: ginsu, tomek, rohan
    
    Reviewed By: tomek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D9118

--- native/components/swipeable.js
+++ native/components/swipeable.js
@@ -7,9 +7,9 @@ import SwipeableComponent from 'react-native-gesture-handler/Swipeable';
 import { useSelector } from 'react-redux';
 
 import Button from './button.react.js';
-import { type Colors, useColors, useStyles } from '../themes/colors.js';
+import { useStyles } from '../themes/colors.js';
 
-type BaseProps = {
+type Props = {
   +buttonWidth: number,
   +rightActions: $ReadOnlyArray<{
     +key: string,
@@ -23,68 +23,76 @@ type BaseProps = {
   },
   +children?: React.Node,
 };
-type Props = {
-  ...BaseProps,
-  +windowWidth: number,
-  +colors: Colors,
-  +styles: typeof unboundStyles,
-};
 
-class Swipeable extends React.PureComponent<Props> {
-  static defaultProps = {
-    rightActions: [],
-  };
+function Swipeable(props: Props): React.Node {
+  const styles = useStyles(unboundStyles);
+  const windowWidth = useSelector(state => state.dimensions.width);
 
-  renderRightActions = progress => {
-    const actions = this.props.rightActions.map(
-      ({ key, content, color, onPress }, i) => {
-        const translation = progress.interpolate({
-          inputRange: [0, 1],
-          outputRange: [
-            (this.props.rightActions.length - i) * this.props.buttonWidth,
-            0,
-          ],
-        });
-        return (
-          <Animated.View
-            key={key}
-            style={{
-              transform: [{ translateX: translation }],
-            }}
-          >
-            <Button
-              onPress={onPress}
-              style={[
-                this.props.styles.action,
-                {
-                  width: this.props.windowWidth + this.props.buttonWidth,
-                  marginRight: -this.props.windowWidth,
-                  paddingRight: this.props.windowWidth,
-                  backgroundColor: color,
-                },
-              ]}
+  const {
+    buttonWidth,
+    rightActions = [],
+    onSwipeableRightWillOpen,
+    innerRef,
+    children,
+  } = props;
+
+  const renderRightActions = React.useCallback(
+    progress => {
+      const actions = rightActions.map(
+        ({ key, content, color, onPress }, i) => {
+          const translation = progress.interpolate({
+            inputRange: [0, 1],
+            outputRange: [(rightActions.length - i) * buttonWidth, 0],
+          });
+          return (
+            <Animated.View
+              key={key}
+              style={{ transform: [{ translateX: translation }] }}
             >
-              {content}
-            </Button>
-          </Animated.View>
-        );
-      },
-    );
+              <Button
+                onPress={onPress}
+                style={[
+                  styles.action,
+                  {
+                    width: windowWidth + buttonWidth,
+                    marginRight: -windowWidth,
+                    paddingRight: windowWidth,
+                    backgroundColor: color,
+                  },
+                ]}
+              >
+                {content}
+              </Button>
+            </Animated.View>
+          );
+        },
+      );
 
-    return <View style={this.props.styles.actionsContainer}>{actions}</View>;
-  };
+      return <View style={styles.actionsContainer}>{actions}</View>;
+    },
+    [
+      buttonWidth,
+      rightActions,
+      styles.action,
+      styles.actionsContainer,
+      windowWidth,
+    ],
+  );
 
-  render() {
-    return (
+  const swipeable = React.useMemo(
+    () => (
       <SwipeableComponent
-        renderRightActions={this.renderRightActions}
-        ref={this.props.innerRef}
-        onSwipeableRightWillOpen={this.props.onSwipeableRightWillOpen}
+        renderRightActions={renderRightActions}
+        ref={innerRef}
+        onSwipeableRightWillOpen={onSwipeableRightWillOpen}
       >
-        {this.props.children}
+        {children}
       </SwipeableComponent>
-    );
-  }
+    ),
+    [children, innerRef, onSwipeableRightWillOpen, renderRightActions],
+  );
+
+  return swipeable;
 }
 
 const unboundStyles = {
@@ -98,20 +106,4 @@ const unboundStyles = {
   },
 };
 
-const ConnectedSwipeable: React.ComponentType<BaseProps> =
-  React.memo<BaseProps>(function ConnectedSwipeable(props: BaseProps) {
-    const styles = useStyles(unboundStyles);
-    const windowWidth = useSelector(state => state.dimensions.width);
-    const colors = useColors();
-
-    return (
-      <Swipeable
-        {...props}
-        styles={styles}
-        windowWidth={windowWidth}
-        colors={colors}
-      />
-    );
-  });
-
-export default ConnectedSwipeable;
+export default Swipeable;
