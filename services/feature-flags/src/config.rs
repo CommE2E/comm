159451c0327a4adeae5b8d71c50428b3678eb53f
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2023-09-12 08:56:02 +0200

    [feature-flags] Standardize localstack and http config
    Summary:
    - Replaced old `--sandbox` flag with `--localstack-endpoint` that we use in other services. More info in D8835
    - Also renamed `--port` to `--http-port` to match other services
    - Changed default port to 50055, see https://linear.app/comm/issue/ENG-3288/dont-bind-services-to-default-grpc-port
    
    Test Plan:
    `cargo run` - should start with default localstack endpoint (`LOCALSTACK_ENDPOINT` env is set by Nix). Default HTTP port is 50055.
    The `--http-port` and `--localstack-endpoint` arguments do override the defaults.
    
    Reviewers: michal, tomek, varun, jon
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D9138

--- services/feature-flags/src/config.rs
+++ services/feature-flags/src/config.rs
@@ -1,27 +1,21 @@
-use clap::{builder::FalseyValueParser, Parser};
+use clap::Parser;
 use once_cell::sync::Lazy;
 use tracing::info;
 
-use crate::constants::{
-  DEFAULT_LOCALSTACK_URL, HTTP_SERVER_DEFAULT_PORT, SANDBOX_ENV_VAR,
-};
+use crate::constants::HTTP_SERVER_DEFAULT_PORT;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 pub struct AppConfig {
-  /// Run the service in sandbox
-  #[arg(long = "sandbox", default_value_t = false)]
-  #[arg(env = SANDBOX_ENV_VAR)]
-  #[arg(value_parser = FalseyValueParser::new())]
-  pub is_sandbox: bool,
-  /// AWS Localstack service URL, applicable in sandbox mode
-  #[arg(long, default_value_t = DEFAULT_LOCALSTACK_URL.to_string())]
-  pub localstack_url: String,
-  #[arg(long = "port", default_value_t = HTTP_SERVER_DEFAULT_PORT)]
+  /// AWS Localstack service URL
+  #[arg(env = "LOCALSTACK_ENDPOINT")]
+  #[arg(long)]
+  pub localstack_endpoint: Option<String>,
+  #[arg(long, default_value_t = HTTP_SERVER_DEFAULT_PORT)]
   pub http_port: u16,
 }
 
-pub static CONFIG: Lazy<AppConfig> = Lazy::new(|| AppConfig::parse());
+pub static CONFIG: Lazy<AppConfig> = Lazy::new(AppConfig::parse);
 
 pub fn parse_cmdline_args() {
   Lazy::force(&CONFIG);
@@ -30,12 +24,9 @@ pub fn parse_cmdline_args() {
 pub async fn load_aws_config() -> aws_types::SdkConfig {
   let mut config_builder = aws_config::from_env();
 
-  if CONFIG.is_sandbox {
-    info!(
-      "Running in sandbox environment. Localstack URL: {}",
-      &CONFIG.localstack_url
-    );
-    config_builder = config_builder.endpoint_url(&CONFIG.localstack_url);
+  if let Some(endpoint) = &CONFIG.localstack_endpoint {
+    info!("Using Localstack. AWS endpoint URL: {}", endpoint);
+    config_builder = config_builder.endpoint_url(endpoint);
   }
 
   config_builder.load().await
