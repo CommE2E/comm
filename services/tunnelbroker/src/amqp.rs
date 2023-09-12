c0566a2b52108ead96729c51258835e1563cbf49
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2023-09-12 21:35:59 +0200

    [tunnelbroker] Allow configuring AMQP credentials
    Summary:
    We need a way to provide RabbitMQ credentials, without providing them in AMQP URI, since AWS doesn't expose authenticated URI.
    Also, made AMQP URI configurable via env vars
    
    Test Plan: Ran external RabbitMQ with custom credentials and configured tunnelbroker using environment variables
    
    Reviewers: kamil, michal, varun, jon
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D9153

--- services/tunnelbroker/src/amqp.rs
+++ services/tunnelbroker/src/amqp.rs
@@ -1,14 +1,29 @@
 use crate::CONFIG;
-use lapin::{Connection, ConnectionProperties};
+use lapin::{uri::AMQPUri, Connection, ConnectionProperties};
 use tracing::info;
 
 pub async fn connect() -> Connection {
-  let conn =
-    Connection::connect(&CONFIG.amqp_uri, ConnectionProperties::default())
-      .await
-      .expect("Unable to connect to amqp endpoint");
+  let mut amqp_uri = CONFIG
+    .amqp_uri
+    .parse::<AMQPUri>()
+    .expect("Invalid AMQP URI");
 
-  info!("Connected to amqp endpoint: {}", &CONFIG.amqp_uri);
+  // Allow set / override credentials using env vars
+  if let Some(amqp_user) = from_env("AMQP_USERNAME") {
+    amqp_uri.authority.userinfo.username = amqp_user;
+  }
+  if let Some(amqp_pass) = from_env("AMQP_PASSWORD") {
+    amqp_uri.authority.userinfo.password = amqp_pass;
+  }
 
-  return conn;
+  let conn = Connection::connect_uri(amqp_uri, ConnectionProperties::default())
+    .await
+    .expect("Unable to connect to AMQP endpoint");
+
+  info!("Connected to AMQP endpoint: {}", &CONFIG.amqp_uri);
+  conn
+}
+
+fn from_env(var_name: &str) -> Option<String> {
+  std::env::var(var_name).ok().filter(|s| !s.is_empty())
 }
