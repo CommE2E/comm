CommE2E/comm

commit 1c77e3b8170ba0987890dd68f0e9fd0742673c25

[client-backup] call C++ function for secure generating random string
Summary:
[ENG-4498](https://linear.app/comm/issue/ENG-4498/generate-backupid).

Placing this here to execute this on crypto thread.

Depends on D8886

Test Plan: Call `commCoreModule.generateRandomString`.

Reviewers: michal, marcin

Subscribers: ashoat, tomek

Differential Revision: https://phab.comm.dev/D8887

native/cpp/CommonCpp/NativeModules/CommCoreModule.cpp
@@ -845,4 +845,33 @@ jsi::Value CommCoreModule::computeBackupKey(
       });
 }
 
+jsi::Value CommCoreModule::generateRandomString(jsi::Runtime &rt, double size) {
+  return createPromiseAsJSIValue(
+      rt,
+      [this, &size](jsi::Runtime &innerRt, std::shared_ptr<Promise> promise) {
+        taskType job = [this, &innerRt, &size, promise]() {
+          std::string error;
+          std::string randomString;
+          try {
+            randomString = crypto::Tools::generateRandomString(size);
+          } catch (const std::exception &e) {
+            error = "Failed to generate random string for size " +
+                std::to_string(size) + ": " + e.what();
+          }
+
+          this->jsInvoker_->invokeAsync(
+              [&innerRt, error, randomString, promise]() {
+                if (error.size()) {
+                  promise->reject(error);
+                } else {
+                  jsi::String jsiRandomString =
+                      jsi::String::createFromUtf8(innerRt, randomString);
+                  promise->resolve(std::move(jsiRandomString));
+                }
+              });
+        };
+        this->cryptoThread->scheduleTask(job);
+      });
+}
+
 } // namespace comm

native/cpp/CommonCpp/NativeModules/CommCoreModule.h
@@ -89,6 +89,8 @@ class CommCoreModule : public facebook::react::CommCoreModuleSchemaCxxSpecJSI {
       jsi::Runtime &rt,
       jsi::String password,
       jsi::String backupID) override;
+  virtual jsi::Value
+  generateRandomString(jsi::Runtime &rt, double size) override;
 
 public:
   CommCoreModule(std::shared_ptr<facebook::react::CallInvoker> jsInvoker);

native/cpp/CommonCpp/_generated/commJSI-generated.cpp
@@ -110,6 +110,9 @@ static jsi::Value __hostFunction_CommCoreModuleSchemaCxxSpecJSI_reportDBOperatio
 static jsi::Value __hostFunction_CommCoreModuleSchemaCxxSpecJSI_computeBackupKey(jsi::Runtime &rt, TurboModule &turboModule, const jsi::Value* args, size_t count) {
   return static_cast<CommCoreModuleSchemaCxxSpecJSI *>(&turboModule)->computeBackupKey(rt, args[0].asString(rt), args[1].asString(rt));
 }
+static jsi::Value __hostFunction_CommCoreModuleSchemaCxxSpecJSI_generateRandomString(jsi::Runtime &rt, TurboModule &turboModule, const jsi::Value* args, size_t count) {
+  return static_cast<CommCoreModuleSchemaCxxSpecJSI *>(&turboModule)->generateRandomString(rt, args[0].asNumber());
+}
 
 CommCoreModuleSchemaCxxSpecJSI::CommCoreModuleSchemaCxxSpecJSI(std::shared_ptr<CallInvoker> jsInvoker)
   : TurboModule("CommTurboModule", jsInvoker) {
@@ -144,6 +147,7 @@ CommCoreModuleSchemaCxxSpecJSI::CommCoreModuleSchemaCxxSpecJSI(std::shared_ptr<C
   methodMap_["checkIfDatabaseNeedsDeletion"] = MethodMetadata {0, __hostFunction_CommCoreModuleSchemaCxxSpecJSI_checkIfDatabaseNeedsDeletion};
   methodMap_["reportDBOperationsFailure"] = MethodMetadata {0, __hostFunction_CommCoreModuleSchemaCxxSpecJSI_reportDBOperationsFailure};
   methodMap_["computeBackupKey"] = MethodMetadata {2, __hostFunction_CommCoreModuleSchemaCxxSpecJSI_computeBackupKey};
+  methodMap_["generateRandomString"] = MethodMetadata {1, __hostFunction_CommCoreModuleSchemaCxxSpecJSI_generateRandomString};
 }
 
 

native/cpp/CommonCpp/_generated/commJSI.h
@@ -51,6 +51,7 @@ public:
   virtual bool checkIfDatabaseNeedsDeletion(jsi::Runtime &rt) = 0;
   virtual void reportDBOperationsFailure(jsi::Runtime &rt) = 0;
   virtual jsi::Value computeBackupKey(jsi::Runtime &rt, jsi::String password, jsi::String backupID) = 0;
+  virtual jsi::Value generateRandomString(jsi::Runtime &rt, double size) = 0;
 
 };
 
@@ -319,6 +320,14 @@ private:
 
       return bridging::callFromJs<jsi::Value>(
           rt, &T::computeBackupKey, jsInvoker_, instance_, std::move(password), std::move(backupID));
+    }
+    jsi::Value generateRandomString(jsi::Runtime &rt, double size) override {
+      static_assert(
+          bridging::getParameterCount(&T::generateRandomString) == 2,
+          "Expected generateRandomString(...) to have 2 parameters");
+
+      return bridging::callFromJs<jsi::Value>(
+          rt, &T::generateRandomString, jsInvoker_, instance_, std::move(size));
     }
 
   private:

native/schema/CommCoreModuleSchema.js
@@ -80,6 +80,7 @@ interface Spec extends TurboModule {
   +checkIfDatabaseNeedsDeletion: () => boolean;
   +reportDBOperationsFailure: () => void;
   +computeBackupKey: (password: string, backupID: string) => Promise<Object>;
+  +generateRandomString: (size: number) => Promise<string>;
 }
 
 export interface CoreModuleSpec extends Spec {

commit 1088de4f1053f0410ae6a1332f509dff0487ed40

[CommCoreModule] capture primitive parameter by value
Summary:
Fixes regression introduced in D8887.

This is a solution for: [ENG-4926](https://linear.app/comm/issue/ENG-4926/client-backup-upload-issue).

At the time code gets to run, the outer function `generateRandomString` may have already returned. This means that `size` might have been destroyed, and there is undefined reference. That's why this function generates backup ID with length 0 and as an effect cause `Argon2` to throw `salt too short`.

On dev everything works, this happens only on dev when code is more optimized.

Also added casting to correct type.

Test Plan:
Test upload protocol in `Release mode`.

Reviewers: michal, bartek, marcin, tomek

native/cpp/CommonCpp/NativeModules/CommCoreModule.cpp
@@ -930,13 +930,13 @@ jsi::Value CommCoreModule::computeBackupKey(
 
 jsi::Value CommCoreModule::generateRandomString(jsi::Runtime &rt, double size) {
   return createPromiseAsJSIValue(
-      rt,
-      [this, &size](jsi::Runtime &innerRt, std::shared_ptr<Promise> promise) {
-        taskType job = [this, &innerRt, &size, promise]() {
+      rt, [=](jsi::Runtime &innerRt, std::shared_ptr<Promise> promise) {
+        taskType job = [=, &innerRt]() {
           std::string error;
           std::string randomString;
           try {
-            randomString = crypto::Tools::generateRandomString(size);
+            randomString =
+                crypto::Tools::generateRandomString(static_cast<size_t>(size));
           } catch (const std::exception &e) {
             error = "Failed to generate random string for size " +
                 std::to_string(size) + ": " + e.what();