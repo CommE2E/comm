254654f123c16b4f84362eedbc99231630d15fb9
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-09-22 02:10:29 -0400

    [native] introduce UserProfileMenuButton
    Summary:
    The last step here is to make this menu button a touchable and to navigate to `UserRelationshipTooltipModal` whenever the menu button gets pressed.
    
    Initially I had all this logic living in UserProfile; however, I didn't want to overcrowd this component, so I created a separate `UserProfileMenuButton` component to keep things better organized
    
    Here are the figma designs showing which options should be shown in different states:
    
    When user is a friend:
    {F792574}
    
    When viewer is blocked (couldn't find native designs but the concept is the same here):
    {F792580}
    
    When user is blocked by viewer:
    {F792576}
    
    When there is no relationship status between user/viewer, or there is a pending friend request in either direction:
    {F792578}
    
    This is step 5 in the list below
    Outlined below are the steps I will take to update `RelationshipListItemTooltipModal`:
    1. Add the block action to relationship list item tooltip menu (`RelationshipListItem` won't need this option, but User profiles will. This is shown in the figma screenshot above)
    2. Rename `RelationshipListItemTooltipModal` to something more generic like `UserRelationshipTooltipModal`
    3. Make `UserRelationshipTooltipButton` into a functional component (We need to use the `useColors` hook in a subsequent diff)
    4. Introduce the `tooltipButtonIcon` as a param to `RelationshipListItemTooltipModal` (`RelationshipListItem` will use a pencil icon and user profiles will use a kebab menu icon)
    5. Introduce UserProfileMenuButton (This component will handle navigating to `RelationshipListItemTooltipModal`
    
    Depends on D9381
    
    Test Plan:
    Please see the demo videos below
    
    When user is a friend:
    {F792582}
    
    When viewer is blocked:
    {F792583}
    
    When user is blocked by viewer:
    {F792585}
    
    When there is no relationship status between user/viewer, or there is a pending friend request in either direction:
    {F792586}
    
    {F792590}
    
    {F792589}
    
    Reviewers: atul, inka
    
    Reviewed By: atul
    
    Subscribers: ashoat, tomek, wyilio
    
    Differential Revision: https://phab.comm.dev/D9382

---
+++ native/user-profile/user-profile-menu-button.react.js
@@ -0,0 +1,147 @@
+// @flow
+
+import { useNavigation, useRoute } from '@react-navigation/native';
+import invariant from 'invariant';
+import * as React from 'react';
+import { View, TouchableOpacity } from 'react-native';
+
+import { useRelationshipPrompt } from 'lib/hooks/relationship-prompt.js';
+import { userRelationshipStatus } from 'lib/types/relationship-types.js';
+import type { ThreadInfo } from 'lib/types/thread-types.js';
+import type { UserInfo } from 'lib/types/user-types';
+
+import { userProfileMenuButtonHeight } from './user-profile-constants.js';
+import SWMansionIcon from '../components/swmansion-icon.react.js';
+import { OverlayContext } from '../navigation/overlay-context.js';
+import { UserRelationshipTooltipModalRouteName } from '../navigation/route-names.js';
+import { useStyles } from '../themes/colors.js';
+
+// We need to set onMenuButtonLayout in order to allow .measure()
+// to be on the ref
+const onMenuButtonLayout = () => {};
+
+type Props = {
+  +threadInfo: ThreadInfo,
+  +pendingPersonalThreadUserInfo: ?UserInfo,
+};
+
+function UserProfileMenuButton(props: Props): React.Node {
+  const { threadInfo, pendingPersonalThreadUserInfo } = props;
+
+  const { otherUserInfo } = useRelationshipPrompt(
+    threadInfo,
+    undefined,
+    pendingPersonalThreadUserInfo,
+  );
+
+  const { navigate } = useNavigation();
+  const route = useRoute();
+
+  const styles = useStyles(unboundStyles);
+
+  const overlayContext = React.useContext(OverlayContext);
+
+  const menuButtonRef = React.useRef();
+
+  const visibleTooltipActionEntryIDs = React.useMemo(() => {
+    const result = [];
+
+    if (otherUserInfo?.relationshipStatus === userRelationshipStatus.FRIEND) {
+      result.push('unfriend');
+      result.push('block');
+    } else if (
+      otherUserInfo?.relationshipStatus ===
+        userRelationshipStatus.BOTH_BLOCKED ||
+      otherUserInfo?.relationshipStatus ===
+        userRelationshipStatus.BLOCKED_BY_VIEWER
+    ) {
+      result.push('unblock');
+    } else {
+      result.push('block');
+    }
+
+    return result;
+  }, [otherUserInfo?.relationshipStatus]);
+
+  const onPressMenuButton = React.useCallback(() => {
+    invariant(
+      overlayContext,
+      'UserProfileMenuButton should have OverlayContext',
+    );
+    overlayContext.setScrollBlockingModalStatus('open');
+
+    const currentMenuButtonRef = menuButtonRef.current;
+    if (!currentMenuButtonRef || !otherUserInfo) {
+      return;
+    }
+
+    currentMenuButtonRef.measure((x, y, width, height, pageX, pageY) => {
+      const coordinates = {
+        x: pageX,
+        y: pageY,
+        width,
+        height,
+      };
+
+      const verticalBounds = {
+        height: userProfileMenuButtonHeight,
+        y: pageY,
+      };
+
+      const { relationshipStatus, ...restUserInfo } = otherUserInfo;
+      const relativeUserInfo = {
+        ...restUserInfo,
+        isViewer: false,
+      };
+      navigate<'UserRelationshipTooltipModal'>({
+        name: UserRelationshipTooltipModalRouteName,
+        params: {
+          presentedFrom: route.key,
+          initialCoordinates: coordinates,
+          verticalBounds,
+          visibleEntryIDs: visibleTooltipActionEntryIDs,
+          relativeUserInfo,
+          tooltipButtonIcon: 'menu',
+        },
+      });
+    });
+  }, [
+    navigate,
+    otherUserInfo,
+    overlayContext,
+    route.key,
+    visibleTooltipActionEntryIDs,
+  ]);
+
+  const userProfileMenuButton = React.useMemo(
+    () => (
+      <TouchableOpacity
+        onPress={onPressMenuButton}
+        style={styles.iconContainer}
+      >
+        <View ref={menuButtonRef} onLayout={onMenuButtonLayout}>
+          <SWMansionIcon
+            name="menu-vertical"
+            size={24}
+            style={styles.moreIcon}
+          />
+        </View>
+      </TouchableOpacity>
+    ),
+    [onPressMenuButton, styles.iconContainer, styles.moreIcon],
+  );
+
+  return userProfileMenuButton;
+}
+
+const unboundStyles = {
+  iconContainer: {
+    alignSelf: 'flex-end',
+  },
+  moreIcon: {
+    color: 'modalButtonLabel',
+    alignSelf: 'flex-end',
+  },
+};
+
+export default UserProfileMenuButton;
