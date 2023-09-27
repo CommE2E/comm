f481aafdeb2fdcc4bbeecc6f6837f1bd95dab5de
Patryk Klatka <patryk.klatka@gmail.com>
2023-09-27 16:47:06 +0200

    [native] Make chat mentions clickable
    Summary:
    Solution for [ENG-4561](https://linear.app/comm/issue/ENG-4561/redirect-to-given-chat-when-mention-is-clicked).
    
    This diff introduces new hook which redirects to chat when mention is clicked and uses it in `markdown-utils.js` and finally in `markdown-chat-mention.react.js`
    
    Depends on D8845.
    
    Test Plan:
    Before testing, please apply the entire native stack.
    
    1. Test if mentions inside a spoiler are properly rendered and are clickable only when spoiler is opened:
    {F697993}
    2. Test if mentions are clickable and if animation is changed when tooltip is opened:
    {F697994}
    
    Reviewers: tomek, inka, rohan
    
    Reviewed By: rohan
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D8873

--- native/markdown/markdown-chat-mention.react.js
+++ native/markdown/markdown-chat-mention.react.js
@@ -3,18 +3,28 @@
 import * as React from 'react';
 import { Text, StyleSheet } from 'react-native';
 
+import type { ResolvedThreadInfo } from 'lib/types/thread-types.js';
+
 import { useMarkdownOnPressUtils } from './markdown-utils.js';
+import { useNavigateToThreadWithFadeAnimation } from '../chat/message-list-types.js';
 
 type TextProps = React.ElementConfig<typeof Text>;
 type Props = {
+  +threadInfo: ResolvedThreadInfo,
   +children: React.Node,
   ...TextProps,
 };
 function MarkdownChatMention(props: Props): React.Node {
-  const { ...rest } = props;
-  const { isRevealed, onLongPressHandler } = useMarkdownOnPressUtils();
+  const { threadInfo, ...rest } = props;
+  const { messageKey, isRevealed, onLongPressHandler } =
+    useMarkdownOnPressUtils();
+  const onPressHandler = useNavigateToThreadWithFadeAnimation(
+    threadInfo,
+    messageKey,
+  );
   return (
     <Text
+      onPress={isRevealed ? onPressHandler : null}
       onLongPress={isRevealed ? onLongPressHandler : null}
       style={styles.mention}
       {...rest}
