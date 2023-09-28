a7591b8e678a099261ecefb9bbee6520f65b27c0
xsanm <kkurowski.kk@gmail.com>
2023-09-28 15:00:20 +0200

    [Tunnelbroker] always persist WebSocket messages in database
    Summary:
    - We need to make sure that each message is delivered to client.
    - We need to persist it immediately after receiving
    - Message will be removed after confirmation from client
    
    Depends on D9314
    
    Test Plan:
    1. Restart RabbitMQ
    2. Restart localstack
    3. Run this code:
    ```
    #[tokio::test]
    async fn send_websocket() {
      let device_info = create_device(None).await;
      let mut socket = create_socket(&device_info).await;
    
      let request = WebsocketMessageToDevice {
        device_id: device_info.device_id.to_string(),
        payload: "message from test".to_string(),
      };
    
      let serialized_request = serde_json::to_string(&request)
        .expect("Failed to serialize connection request");
      println!("request: {}", serialized_request);
    
      socket
        .send(Message::Text(serialized_request))
        .await
        .expect("Failed to send message");
    }
    ```
    4. Open RabbitMQ management console, there should be one dropped message (there is no device with `test_persist` ID)
    [ss]
    5. Open DynamoDB console, message should be persisted:
    [ss].
    6. Additionally, check TB logs for:
    ```
    2023-09-28T12:52:08.492774Z DEBUG tunnelbroker::websockets::session: Received message for ZXx1ADCFxFm6P+UmVhX0A1tuqUoBU7lYjig/gMzSEJI
    2023-09-28T12:52:08.492789Z DEBUG tunnelbroker::database: Persisting message to device: ZXx1ADCFxFm6P+UmVhX0A1tuqUoBU7lYjig/gMzSEJI
    ```
    7. Run:
    `cd services/commtest && cargo test --test tunnelbroker_persist_tests  -- --nocapture`
    
    Reviewers: michal, bartek, varun, jon
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D9316

---
+++ services/commtest/tests/tunnelbroker_persist_tests.rs
@@ -0,0 +1,95 @@
+mod proto {
+  tonic::include_proto!("tunnelbroker");
+}
+
+use commtest::identity::device::create_device;
+use commtest::identity::olm_account_infos::{
+  MOCK_CLIENT_KEYS_1, MOCK_CLIENT_KEYS_2,
+};
+use commtest::tunnelbroker::socket::create_socket;
+use futures_util::{SinkExt, StreamExt};
+use proto::tunnelbroker_service_client::TunnelbrokerServiceClient;
+use proto::MessageToDevice;
+use std::time::Duration;
+use tokio::time::sleep;
+use tokio_tungstenite::tungstenite::Message;
+use tunnelbroker_messages::{
+  MessageToDevice as WebsocketMessageToDevice, RefreshKeyRequest,
+};
+
+/// Tests that a message to an offline device gets pushed to dynamodb
+/// then recalled once a device connects
+
+#[tokio::test]
+async fn persist_grpc_messages() {
+  let device_info = create_device(None).await;
+
+  // Send request for keyserver to refresh keys (identity service)
+  let mut tunnelbroker_client =
+    TunnelbrokerServiceClient::connect("http://localhost:50051")
+      .await
+      .unwrap();
+
+  let refresh_request = RefreshKeyRequest {
+    device_id: device_info.device_id.to_string(),
+    number_of_keys: 5,
+  };
+
+  let payload = serde_json::to_string(&refresh_request).unwrap();
+  let request = MessageToDevice {
+    device_id: device_info.device_id.to_string(),
+    payload,
+  };
+  let grpc_message = tonic::Request::new(request);
+  tunnelbroker_client
+    .send_message_to_device(grpc_message)
+    .await
+    .unwrap();
+
+  // Wait a specified duration to ensure that message had time to persist
+  sleep(Duration::from_millis(100)).await;
+
+  let mut socket = create_socket(&device_info).await;
+  // Have keyserver receive any websocket messages
+  if let Some(Ok(response)) = socket.next().await {
+    // Check that message received by keyserver matches what identity server
+    // issued
+    let serialized_response: RefreshKeyRequest =
+      serde_json::from_str(&response.to_text().unwrap()).unwrap();
+    assert_eq!(serialized_response, refresh_request);
+  };
+}
+
+#[tokio::test]
+async fn persist_websocket_messages() {
+  let sender = create_device(Some(&MOCK_CLIENT_KEYS_1)).await;
+  let receiver = create_device(Some(&MOCK_CLIENT_KEYS_2)).await;
+
+  // Send message to not connected client
+  let payload = "persisted message";
+  let request = WebsocketMessageToDevice {
+    device_id: receiver.device_id.clone(),
+    payload: payload.to_string(),
+  };
+
+  let serialized_request = serde_json::to_string(&request)
+    .expect("Failed to serialize message to device");
+
+  let mut sender_socket = create_socket(&sender).await;
+  sender_socket
+    .send(Message::Text(serialized_request))
+    .await
+    .expect("Failed to send message");
+
+  // Wait a specified duration to ensure that message had time to persist
+  sleep(Duration::from_millis(100)).await;
+
+  // Connect receiver
+  let mut receiver_socket = create_socket(&receiver).await;
+
+  // Receive message
+  if let Some(Ok(response)) = receiver_socket.next().await {
+    let received_payload = response.to_text().unwrap();
+    assert_eq!(payload, received_payload);
+  };
+}
