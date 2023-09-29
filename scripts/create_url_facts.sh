a4fcfc0bc736688a2347d6042384b47a7375ec54
Michal Gniadek <michal.gniadek@swmansion.com>
2023-09-29 16:41:02 +0200

    [keyserver] Update CORS
    Summary:
    [ENG-4768](https://linear.app/comm/issue/ENG-4768)
    We want the web app to call squadcal instead of `web.comm.app` which means we need to setup new cors headers so the browser doesn't reject the request. We can use the express middleware from cors library to automatically handle adding cors headers to requests and handling preflight requests (where the browser first sends and OPTION request).
    
    Test Plan:
    Navigate to `test.localhost:3000/comm` which still resolves to the node server but has different origin.
    - without any changes I'm getting: `Access to fetch at 'http://localhost:3000/comm/get_initial_redux_state' from origin 'http://test.localhost:3000' has been blocked by CORS policy`
    - after these changes and changing the `facts/commapp_url.json` to `http://test.localhost:3000` the request went through
    
    Adding @ashoat as reviewer because of new dependency.
    
    Reviewers: inka, kamil, atul, ashoat
    
    Subscribers: tomek, wyilio, ashoat
    
    Differential Revision: https://phab.comm.dev/D9396

--- scripts/create_url_facts.sh
+++ scripts/create_url_facts.sh
@@ -8,6 +8,7 @@ KEYSERVER_FACTS_DIR="${ROOT_DIR}/keyserver/facts"
 COMMAPP_URL_PATH="${KEYSERVER_FACTS_DIR}/commapp_url.json"
 LANDING_URL_PATH="${KEYSERVER_FACTS_DIR}/landing_url.json"
 SQUADCAL_URL_PATH="${KEYSERVER_FACTS_DIR}/squadcal_url.json"
+WEBAPP_CORS_PATH="${KEYSERVER_FACTS_DIR}/webapp_cors.json"
 
 if [[ ! -d "$KEYSERVER_FACTS_DIR" ]]; then
   mkdir -p "$KEYSERVER_FACTS_DIR"
@@ -23,4 +24,8 @@ fi
 
 if [[ ! -f "$SQUADCAL_URL_PATH" ]]; then
   cp "$SCRIPT_DIR/templates/squadcal_url.json" "$SQUADCAL_URL_PATH"
+fi
+
+if [[ ! -f "$WEBAPP_CORS_PATH" ]]; then
+  cp "$SCRIPT_DIR/templates/webapp_cors.json" "$WEBAPP_CORS_PATH"
 fi
