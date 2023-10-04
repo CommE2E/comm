af83c75d68c78cad5ecc4d5538f201a7dd82b8a8
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-10-04 00:46:36 -0400

    [native] introduce contentDimensions prop to full screen view modal
    Summary: This is step 5
    
    Test Plan: confirmed that there were no regresssions with the content size + navigating to `ImageModal`
    
    Subscribers: ashoat, tomek, wyilio
    
    Differential Revision: https://phab.comm.dev/D9363

--- native/media/image-modal.react.js
+++ native/media/image-modal.react.js
@@ -4,7 +4,7 @@ import Clipboard from '@react-native-clipboard/clipboard';
 import invariant from 'invariant';
 import * as React from 'react';
 
-import { type MediaInfo } from 'lib/types/media-types.js';
+import type { MediaInfo, Dimensions } from 'lib/types/media-types.js';
 
 import Multimedia from './multimedia.react.js';
 import { useIntentionalSaveMedia } from './save-media.js';
@@ -12,6 +12,8 @@ import FullScreenViewModal from '../components/full-screen-view-modal.react.js';
 import { displayActionResultModal } from '../navigation/action-result-modal.js';
 import type { AppNavigationProp } from '../navigation/app-navigator.react.js';
 import type { NavigationRoute } from '../navigation/route-names.js';
+import { useSelector } from '../redux/redux-utils.js';
+import { derivedDimensionsInfoSelector } from '../selectors/dimensions-selectors.js';
 import type { ChatMultimediaMessageInfoItem } from '../types/chat-types.js';
 import {
   type VerticalBounds,
@@ -35,6 +37,8 @@ function ImageModal(props: Props): React.Node {
   const { navigation, route } = props;
   const { mediaInfo, item } = route.params;
 
+  const dimensionsInfo = useSelector(derivedDimensionsInfoSelector);
+
   const intentionalSaveMedia = useIntentionalSaveMedia();
 
   const onPressSave = React.useCallback(() => {
@@ -56,10 +60,45 @@ function ImageModal(props: Props): React.Node {
     });
   }, [mediaInfo]);
 
+  const imageDimensions: Dimensions = React.useMemo(() => {
+    const frame = {
+      width: dimensionsInfo.width,
+      height: dimensionsInfo.safeAreaHeight,
+    };
+
+    // Make space for the close button
+    let { height: maxHeight, width: maxWidth } = frame;
+    if (maxHeight > maxWidth) {
+      maxHeight -= 100;
+    } else {
+      maxWidth -= 100;
+    }
+
+    const { dimensions } = mediaInfo;
+    if (dimensions.height < maxHeight && dimensions.width < maxWidth) {
+      return dimensions;
+    }
+
+    const heightRatio = maxHeight / dimensions.height;
+    const widthRatio = maxWidth / dimensions.width;
+    if (heightRatio < widthRatio) {
+      return {
+        height: maxHeight,
+        width: dimensions.width * heightRatio,
+      };
+    } else {
+      return {
+        width: maxWidth,
+        height: dimensions.height * widthRatio,
+      };
+    }
+  }, [dimensionsInfo.safeAreaHeight, dimensionsInfo.width, mediaInfo]);
+
   return (
     <FullScreenViewModal
       navigation={navigation}
       route={route}
+      contentDimensions={imageDimensions}
       saveContentCallback={onPressSave}
       copyContentCallback={onPressCopy}
     >
