f90080f1a9a22e27aedf33ad0cefa5f392fe7fa9
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-10-03 23:21:08 -0400

    [native] introduce saveContentCallback and copyContentCallback props to full screen view modal
    Summary: This is step 4 in the list below
    
    Test Plan: Confirmed that copy and save funtionality still works as expected
    
    Reviewers:
    
    Subscribers:

--- native/media/image-modal.react.js
+++ native/media/image-modal.react.js
@@ -1,11 +1,15 @@
 // @flow
 
+import Clipboard from '@react-native-clipboard/clipboard';
+import invariant from 'invariant';
 import * as React from 'react';
 
 import { type MediaInfo } from 'lib/types/media-types.js';
 
 import Multimedia from './multimedia.react.js';
+import { useIntentionalSaveMedia } from './save-media.js';
 import FullScreenViewModal from '../components/full-screen-view-modal.react.js';
+import { displayActionResultModal } from '../navigation/action-result-modal.js';
 import type { AppNavigationProp } from '../navigation/app-navigator.react.js';
 import type { NavigationRoute } from '../navigation/route-names.js';
 import type { ChatMultimediaMessageInfoItem } from '../types/chat-types.js';
@@ -29,9 +33,36 @@ type Props = {
 
 function ImageModal(props: Props): React.Node {
   const { navigation, route } = props;
+  const { mediaInfo, item } = route.params;
+
+  const intentionalSaveMedia = useIntentionalSaveMedia();
+
+  const onPressSave = React.useCallback(() => {
+    invariant(
+      mediaInfo.type === 'photo' || mediaInfo.type === 'video',
+      'saving media of type ' + mediaInfo.type + ' is not supported',
+    );
+
+    const { id: uploadID, uri } = mediaInfo;
+    const { id: messageServerID, localID: messageLocalID } = item.messageInfo;
+    const ids = { uploadID, messageServerID, messageLocalID };
+    return intentionalSaveMedia(uri, ids);
+  }, [intentionalSaveMedia, item.messageInfo, mediaInfo]);
+
+  const onPressCopy = React.useCallback(() => {
+    const { uri } = mediaInfo;
+    Clipboard.setImageFromURL(uri, success => {
+      displayActionResultModal(success ? 'copied!' : 'failed to copy :(');
+    });
+  }, [mediaInfo]);
 
   return (
-    <FullScreenViewModal navigation={navigation} route={route}>
+    <FullScreenViewModal
+      navigation={navigation}
+      route={route}
+      saveContentCallback={onPressSave}
+      copyContentCallback={onPressCopy}
+    >
       <Multimedia mediaInfo={route.params.mediaInfo} spinnerColor="white" />
     </FullScreenViewModal>
   );
