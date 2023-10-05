4a9fcffb56fa67d991b2e0a2924e24f6f81d3d7c
Michal Gniadek <michal.gniadek@swmansion.com>
2023-10-05 10:30:57 +0200

    [nix] Create new url facts
    Summary:
    Part of [ENG-5153](https://linear.app/comm/issue/ENG-5153)
    Depends on D9397
    
    We would like to separate hosting web app on dev environment to `localhost:3000/webapp/`` and move the keyserver logic to `localhost:3000/keyserver/` (so in the end nothing will be hosted on `localhost:3000/`) to simplify the dev environment and allow for web <-> keyserver separation. We can also use this opportunity to cleanup the naming.  This diff automatically adds new files on nix start. They will be used in the later diff.
    
    Test Plan: Run `nix develop`, check if files are generated
    
    Reviewers: inka, kamil, atul
    
    Subscribers: ashoat, tomek, wyilio
    
    Differential Revision: https://phab.comm.dev/D9398

--- scripts/create_url_facts.sh
+++ scripts/create_url_facts.sh
@@ -5,25 +5,25 @@ set -euo pipefail
 SCRIPT_DIR=$(cd "$(dirname "$0")" || true; pwd -P)
 ROOT_DIR="${SCRIPT_DIR}/.."
 KEYSERVER_FACTS_DIR="${ROOT_DIR}/keyserver/facts"
-COMMAPP_URL_PATH="${KEYSERVER_FACTS_DIR}/commapp_url.json"
 LANDING_URL_PATH="${KEYSERVER_FACTS_DIR}/landing_url.json"
-SQUADCAL_URL_PATH="${KEYSERVER_FACTS_DIR}/squadcal_url.json"
+WEBAPP_URL_PATH="${KEYSERVER_FACTS_DIR}/webapp_url.json"
+KEYSERVER_URL_PATH="${KEYSERVER_FACTS_DIR}/keyserver_url.json"
 WEBAPP_CORS_PATH="${KEYSERVER_FACTS_DIR}/webapp_cors.json"
 
 if [[ ! -d "$KEYSERVER_FACTS_DIR" ]]; then
   mkdir -p "$KEYSERVER_FACTS_DIR"
 fi
 
-if [[ ! -f "$COMMAPP_URL_PATH" ]]; then
-  cp "$SCRIPT_DIR/templates/commapp_url.json" "$COMMAPP_URL_PATH"
-fi
-
 if [[ ! -f "$LANDING_URL_PATH" ]]; then
   cp "$SCRIPT_DIR/templates/landing_url.json" "$LANDING_URL_PATH"
 fi
 
-if [[ ! -f "$SQUADCAL_URL_PATH" ]]; then
-  cp "$SCRIPT_DIR/templates/squadcal_url.json" "$SQUADCAL_URL_PATH"
+if [[ ! -f "$WEBAPP_URL_PATH" ]]; then
+  cp "$SCRIPT_DIR/templates/webapp_url.json" "$WEBAPP_URL_PATH"
+fi
+
+if [[ ! -f "$KEYSERVER_URL_PATH" ]]; then
+  cp "$SCRIPT_DIR/templates/keyserver_url.json" "$KEYSERVER_URL_PATH"
 fi
 
 if [[ ! -f "$WEBAPP_CORS_PATH" ]]; then
