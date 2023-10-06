03feb5f41d1f7a33e02ae2ff42fa8aeb8cb33e5b
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-10-05 21:06:02 -0400

    [native] introduce avatar constants
    Summary: This diff address @atul's brittle code concerns in D9376
    
    Test Plan: confirmed that there were no regressions with the sizing of avatars
    
    Reviewers:
    
    Subscribers:

---
+++ native/avatars/avatar-constants.js
@@ -0,0 +1,13 @@
+// @flow
+
+export const xSmallAvatarSize = 16;
+
+export const smallAvatarSize = 24;
+
+export const mediumAvatarSize = 40;
+
+export const largeAvatarSize = 90;
+
+export const xLargeAvatarSize = 112;
+
+export const xxLargeAvatarSize = 224;
