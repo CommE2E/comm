6f99b8dbabd1c854cfd5bd8844d82150d63f3533
wyilio <williamwang749@gmail.com>
2023-10-06 00:30:12 -0400

    Implement Display and TryFrom traits for DeviceType
    Summary:
    Implemented Display and TryFrom impls to generated DeviceType object in grpc_clients/identity/src/device.rs.
    Being able to marshal the protobuf into a "more realized" object will likely be useful in the future for both devices and services.
    
    Test Plan: Replace usage of Device with DeviceType in services/identity
    
    Reviewers: varun
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D9384

---
+++ shared/grpc_clients/src/identity/device.rs
@@ -0,0 +1,33 @@
+use std::fmt::{Display, Formatter, Result as FmtResult};
+
+use crate::error::Error;
+use crate::identity::protos::client::DeviceType;
+
+impl TryFrom<i32> for DeviceType {
+  type Error = crate::error::Error;
+
+  fn try_from(value: i32) -> Result<Self, Self::Error> {
+    match value {
+      0 => Ok(DeviceType::Keyserver),
+      1 => Ok(DeviceType::Web),
+      2 => Ok(DeviceType::Ios),
+      3 => Ok(DeviceType::Android),
+      4 => Ok(DeviceType::Windows),
+      5 => Ok(DeviceType::MacOs),
+      _ => Err(Error::InvalidDeviceType),
+    }
+  }
+}
+
+impl Display for DeviceType {
+  fn fmt(&self, f: &mut Formatter) -> FmtResult {
+    match self {
+      DeviceType::Keyserver => write!(f, "keyserver"),
+      DeviceType::Web => write!(f, "web"),
+      DeviceType::Ios => write!(f, "ios"),
+      DeviceType::Android => write!(f, "android"),
+      DeviceType::Windows => write!(f, "windows"),
+      DeviceType::MacOs => write!(f, "macos"),
+    }
+  }
+}
