587e22d552188e5628632a278e13199df164ac10
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-10-07 05:09:20 -0400

    [web] introduce UserProfile component
    Summary: This diff introduces the barebones user profile component. This includes the user avatar, the username, and the copy username button (but w/o the functionality). That and the other elements of the user profile will come in subsequent diffs
    
    Test Plan: Please see screenshot below
    
    Subscribers: ashoat, tomek, wyilio
    
    Differential Revision: https://phab.comm.dev/D9409

---
+++ web/modals/user-profile/user-profile.react.js
@@ -0,0 +1,35 @@
+// @flow
+
+import * as React from 'react';
+
+import SWMansionIcon from 'lib/components/SWMansionIcon.react.js';
+import { stringForUserExplicit } from 'lib/shared/user-utils.js';
+import type { UserInfo } from 'lib/types/user-types';
+
+import css from './user-profile.css';
+import UserAvatar from '../../avatars/user-avatar.react.js';
+
+type Props = {
+  +userInfo: ?UserInfo,
+};
+
+function UserProfile(props: Props): React.Node {
+  const { userInfo } = props;
+
+  const usernameText = stringForUserExplicit(userInfo);
+
+  return (
+    <div className={css.container}>
+      <UserAvatar userID={userInfo?.id} size="L" />
+      <div className={css.usernameContainer}>
+        <div className={css.usernameText}>{usernameText}</div>
+        <div className={css.copyUsernameContainer}>
+          <SWMansionIcon icon="copy" size={16} />
+          <p className={css.copyUsernameText}>Copy username</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UserProfile;
