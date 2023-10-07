04ffaeeb536218340a039fd7b4bf2e3e3cc40593
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-10-07 17:07:10 -0400

    [web] introduce UserProfileMessageButton
    Summary:
    This diff introduces the `UserProfileMessageButton`. The `UserProfileMessageButtons` will be responsible for navigating to the personal thread between the user and the viewer. For all of this logic we can simply just use the `useUserProfileThreadInfo` hook
    
    https://linear.app/comm/issue/ENG-4966/introduceport-userprofilemessagebutton
    
    Depends on D9411
    
    Test Plan:
    Please see the demo video below
    
    {F804470}
    
    Anonymous user:
    {F804461}
    
    Block in either direction:
    {F804468}
    
    Reviewers: atul, inka
    
    Reviewed By: atul
    
    Subscribers: ashoat, tomek, wyilio
    
    Differential Revision: https://phab.comm.dev/D9412

---
+++ web/modals/user-profile/user-profile-message-button.react.js
@@ -0,0 +1,48 @@
+// @flow
+
+import * as React from 'react';
+
+import { useModalContext } from 'lib/components/modal-provider.react.js';
+import SWMansionIcon from 'lib/components/SWMansionIcon.react.js';
+import type { ThreadInfo } from 'lib/types/thread-types.js';
+
+import css from './user-profile.css';
+import Button from '../../components/button.react.js';
+import { useOnClickThread } from '../../selectors/thread-selectors.js';
+
+type Props = {
+  +threadInfo: ThreadInfo,
+};
+
+function UserProfileMessageButton(props: Props): React.Node {
+  const { threadInfo } = props;
+
+  const { clearModals } = useModalContext();
+  const onClickThread = useOnClickThread(threadInfo);
+
+  const onClickMessageButton = React.useCallback(
+    (event: SyntheticEvent<HTMLElement>) => {
+      clearModals();
+      onClickThread(event);
+    },
+    [clearModals, onClickThread],
+  );
+
+  const userProfileMessageButton = React.useMemo(
+    () => (
+      <Button
+        variant="filled"
+        onClick={onClickMessageButton}
+        className={css.actionButton}
+      >
+        <SWMansionIcon icon="send-2" size={22} />
+        <div>Message</div>
+      </Button>
+    ),
+    [onClickMessageButton],
+  );
+
+  return userProfileMessageButton;
+}
+
+export default UserProfileMessageButton;
