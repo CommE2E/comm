fa275e313fde3bc9fcdc2308f303a20d229b57e3
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-10-07 17:07:10 -0400

    [web] introduce UserProfileMessageButton
    Summary: This diff intorduces the `UserProfileMessageButton`. The `UserProfileMessageButtons` will be responsible for navigating to the personal thread between the user and the viewer. For all of this logic we can simply just use the `useUserProfileThreadInfo` hook
    
    Test Plan: Please see the demo video below
    
    Reviewers:
    
    Subscribers:

--- web/modals/user-profile/user-profile.react.js
+++ web/modals/user-profile/user-profile.react.js
@@ -3,10 +3,13 @@
 import * as React from 'react';
 
 import SWMansionIcon from 'lib/components/SWMansionIcon.react.js';
+import { relationshipBlockedInEitherDirection } from 'lib/shared/relationship-utils.js';
+import { useUserProfileThreadInfo } from 'lib/shared/thread-utils.js';
 import { stringForUserExplicit } from 'lib/shared/user-utils.js';
 import type { UserInfo } from 'lib/types/user-types';
 import sleep from 'lib/utils/sleep.js';
 
+import UserProfileMessageButton from './user-profile-message-button.react.js';
 import css from './user-profile.css';
 import UserAvatar from '../../avatars/user-avatar.react.js';
 
@@ -16,6 +19,8 @@ type Props = {
 
 function UserProfile(props: Props): React.Node {
   const { userInfo } = props;
+
+  const userProfileThreadInfo = useUserProfileThreadInfo(userInfo);
 
   const usernameText = stringForUserExplicit(userInfo);
 
@@ -32,21 +37,40 @@ function UserProfile(props: Props): React.Node {
     setUsernameCopied(false);
   }, [usernameCopied, usernameText]);
 
+  const messageButton = React.useMemo(() => {
+    if (
+      !userProfileThreadInfo ||
+      relationshipBlockedInEitherDirection(userInfo?.relationshipStatus)
+    ) {
+      return null;
+    }
+
+    return (
+      <UserProfileMessageButton threadInfo={userProfileThreadInfo.threadInfo} />
+    );
+  }, [userInfo?.relationshipStatus, userProfileThreadInfo]);
+
   return (
     <div className={css.container}>
-      <UserAvatar userID={userInfo?.id} size="L" />
-      <div className={css.usernameContainer}>
-        <div className={css.usernameText}>{usernameText}</div>
-        <div
-          className={css.copyUsernameContainer}
-          onClick={onClickCopyUsername}
-        >
-          <SWMansionIcon icon={!usernameCopied ? 'copy' : 'check'} size={16} />
-          <p className={css.copyUsernameText}>
-            {!usernameCopied ? 'Copy username' : 'Username copied!'}
-          </p>
+      <div className={css.userInfoContainer}>
+        <UserAvatar userID={userInfo?.id} size="L" />
+        <div className={css.usernameContainer}>
+          <div className={css.usernameText}>{usernameText}</div>
+          <div
+            className={css.copyUsernameContainer}
+            onClick={onClickCopyUsername}
+          >
+            <SWMansionIcon
+              icon={!usernameCopied ? 'copy' : 'check'}
+              size={16}
+            />
+            <p className={css.copyUsernameText}>
+              {!usernameCopied ? 'Copy username' : 'Username copied!'}
+            </p>
+          </div>
         </div>
       </div>
+      {messageButton}
     </div>
   );
 }
