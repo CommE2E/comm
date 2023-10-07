0309b8d313881e4c57f9bb9f394a2e9deb3a2b56
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-10-07 03:20:25 -0400

    [web] introduce XL and XXL sizes to the avatar component
    Summary: When building the user profiles on `native` we needed to introduce two new size variants: XL and XXL for our avatar component. We also need to do the same to our web avatar component
    
    Test Plan: Please see screenshots below
    
    Subscribers: ashoat, tomek, wyilio
    
    Differential Revision: https://phab.comm.dev/D9408

--- web/avatars/edit-thread-avatar.react.js
+++ web/avatars/edit-thread-avatar.react.js
@@ -37,7 +37,7 @@ function EditThreadAvatar(props: Props): React.Node {
     <div className={css.editThreadAvatarContainer}>
       <ThreadAvatar
         threadInfo={threadInfo}
-        size="L"
+        size="XL"
         showSpinner={threadAvatarSaveInProgress}
       />
       {editThreadAvatarMenu}
