29853fff9a1afd5db1fcd35af19a215e3266b963
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-10-07 03:20:25 -0400

    [web] introduce XL and XXL sizes to the avatar component
    Summary: When building the user profiles on `native` we needed to introduce two new size variants: XL and XXL for our avatar component. We also need to do the same to our web avatar component
    
    Test Plan: Please see screenshots below
    
    Subscribers: ashoat, tomek, wyilio
    
    Differential Revision: https://phab.comm.dev/D9408

--- web/sidebar/community-creation/community-creation-modal.react.js
+++ web/sidebar/community-creation/community-creation-modal.react.js
@@ -127,7 +127,7 @@ function CommunityCreationModal(): React.Node {
   if (avatarNodeEnabled) {
     avatarNode = (
       <div className={css.avatarContainer}>
-        <UserAvatar userID="256" size="L" />
+        <UserAvatar userID="256" size="XL" />
       </div>
     );
   }
