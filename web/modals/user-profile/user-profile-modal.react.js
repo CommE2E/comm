bad361dfbce32ac51c9cc2e9c345d65d0ea4e002
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-10-07 05:10:38 -0400

    [web] introduce UserProfileModal component
    Summary: As a followup to introducing the user profile component, we should also introduce its containing component, which will be a mobile. Just like `UserProfileBottomSheet` this compnent will select the `userInfo` based on the `userID` and handle any containing comopnent specific features (like onClose, etc)
    
    Test Plan: Please see the demo video below
    
    Subscribers: ashoat, tomek, wyilio
    
    Differential Revision: https://phab.comm.dev/D9410

---
+++ web/modals/user-profile/user-profile-modal.react.js
@@ -0,0 +1,32 @@
+// @flow
+
+import * as React from 'react';
+
+import { useModalContext } from 'lib/components/modal-provider.react.js';
+import type { UserInfo } from 'lib/types/user-types.js';
+
+import UserProfile from './user-profile.react.js';
+import { useSelector } from '../../redux/redux-utils.js';
+import Modal from '../modal.react.js';
+
+type Props = {
+  +userID: string,
+};
+
+function UserProfileModal(props: Props): React.Node {
+  const { userID } = props;
+
+  const { popModal } = useModalContext();
+
+  const userInfo: ?UserInfo = useSelector(
+    state => state.userStore.userInfos[userID],
+  );
+
+  return (
+    <Modal size="fit-content" onClose={popModal} name="">
+      <UserProfile userInfo={userInfo} />
+    </Modal>
+  );
+}
+
+export default UserProfileModal;
