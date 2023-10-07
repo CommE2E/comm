5f89c4ae182747f5fff3bc0132295688327366e9
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-10-07 15:39:17 -0400

    [web] introduce copy username functionality to user profiles
    Summary:
    As part of the scope for user profiles, we want to be able to copy the user's username onto your clipboard. This diff adds this functionality into the copy username div already on the user profile
    
    https://linear.app/comm/issue/ENG-4963/copy-username-functionality
    
    Depends on D9410
    
    Test Plan:
    Please see the demo video below
    
    {F804421}
    
    Reviewers: atul, inka
    
    Reviewed By: atul
    
    Subscribers: ashoat, tomek, wyilio
    
    Differential Revision: https://phab.comm.dev/D9411

--- web/modals/user-profile/user-profile.react.js
+++ web/modals/user-profile/user-profile.react.js
@@ -5,6 +5,7 @@ import * as React from 'react';
 import SWMansionIcon from 'lib/components/SWMansionIcon.react.js';
 import { stringForUserExplicit } from 'lib/shared/user-utils.js';
 import type { UserInfo } from 'lib/types/user-types';
+import sleep from 'lib/utils/sleep.js';
 
 import css from './user-profile.css';
 import UserAvatar from '../../avatars/user-avatar.react.js';
@@ -18,20 +19,41 @@ function UserProfile(props: Props): React.Node {
 
   const usernameText = stringForUserExplicit(userInfo);
 
+  const [usernameCopied, setUsernameCopied] = React.useState<boolean>(false);
+
+  const onClickCopyUsername = React.useCallback(async () => {
+    if (usernameCopied) {
+      return;
+    }
+
+    await navigator.clipboard.writeText(usernameText);
+    setUsernameCopied(true);
+    await sleep(3000);
+    setUsernameCopied(false);
+  }, [usernameCopied, usernameText]);
+
   const userProfile = React.useMemo(
     () => (
       <div className={css.container}>
         <UserAvatar userID={userInfo?.id} size="L" />
         <div className={css.usernameContainer}>
           <div className={css.usernameText}>{usernameText}</div>
-          <div className={css.copyUsernameContainer}>
-            <SWMansionIcon icon="copy" size={16} />
-            <p className={css.copyUsernameText}>Copy username</p>
+          <div
+            className={css.copyUsernameContainer}
+            onClick={onClickCopyUsername}
+          >
+            <SWMansionIcon
+              icon={!usernameCopied ? 'copy' : 'check'}
+              size={16}
+            />
+            <p className={css.copyUsernameText}>
+              {!usernameCopied ? 'Copy username' : 'Username copied!'}
+            </p>
           </div>
         </div>
       </div>
     ),
-    [userInfo?.id, usernameText],
+    [onClickCopyUsername, userInfo?.id, usernameCopied, usernameText],
   );
 
   return userProfile;
