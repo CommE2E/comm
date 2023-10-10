bca31a5b3ad1f7cacfaaa583908a4f5450429d9b
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-10-10 13:47:12 -0400

    [web] update RelationshipPromptButton to be more reusable
    Summary: I want to use `RelationshipPromptButton` in user profiles. Right now there are some styles that are making it difficult to do that. In this diff I moved/changed some of the styles around + snuck in a quick memoization improvement to this component
    
    Test Plan: Confirmed that `RelationshipPromptButton` looked and behaved the same
    
    Subscribers: ashoat, tomek, wyilio
    
    Differential Revision: https://phab.comm.dev/D9446

--- web/chat/relationship-prompt/relationship-prompt-button.js
+++ web/chat/relationship-prompt/relationship-prompt-button.js
@@ -2,6 +2,7 @@
 
 import { type IconDefinition } from '@fortawesome/fontawesome-common-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classnames from 'classnames';
 import * as React from 'react';
 
 import css from './relationship-prompt.css';
@@ -12,21 +13,29 @@ type Props = {
   +icon: IconDefinition,
   +buttonColor: ButtonColor,
   +onClick: () => void,
+  +className?: string,
 };
 function RelationshipPromptButton(props: Props): React.Node {
-  const { text, icon, buttonColor, onClick } = props;
+  const { text, icon, buttonColor, onClick, className } = props;
 
-  return (
-    <Button
-      variant="filled"
-      buttonColor={buttonColor}
-      onClick={onClick}
-      className={css.promptButton}
-    >
-      <FontAwesomeIcon icon={icon} className={css.promptIcon} />
-      <p className={css.promptText}>{text}</p>
-    </Button>
+  const buttonClassName = classnames(css.promptButton, className);
+
+  const relationshipPromptButton = React.useMemo(
+    () => (
+      <Button
+        variant="filled"
+        buttonColor={buttonColor}
+        onClick={onClick}
+        className={buttonClassName}
+      >
+        <FontAwesomeIcon icon={icon} />
+        <p className={css.promptText}>{text}</p>
+      </Button>
+    ),
+    [buttonClassName, buttonColor, icon, onClick, text],
   );
+
+  return relationshipPromptButton;
 }
 
 export default RelationshipPromptButton;
