CommE2E/comm

commit b5ce5b36217930f634540218158c58eca7ba9c4e

[keyserver] Use async version of brotliCompress
Summary:
It's generally better for performance to let this be async, especially if it's implemented natively and can take advantage of threading / parallelism.

Putting this as a separate diff because I think it has some separate risks.

Depends on D9294

Test Plan: See parent diff's test plan

Reviewers: atul, michal, kamil

Subscribers: tomek

Differential Revision: https://phab.comm.dev/D9298

keyserver/src/socket/socket.js
@@ -260,7 +260,11 @@ class Socket {
         handleAsyncPromise(extendCookieLifespan(viewer.cookieID));
       }
       for (const response of serverResponses) {
-        this.sendMessage(response);
+        // Normally it's an anti-pattern to await in sequence like this. But in
+        // this case, we have a requirement that this array of serverResponses
+        // is delivered in order. See here:
+        // https://github.com/CommE2E/comm/blob/101eb34481deb49c609bfd2c785f375886e52666/keyserver/src/socket/socket.js#L566-L568
+        await this.sendMessage(response);
       }
       if (clientSocketMessage.type === clientSocketMessageTypes.INITIAL) {
         this.onSuccessfulConnection();
@@ -277,7 +281,7 @@ class Socket {
           errorMessage.responseTo = responseTo;
         }
         this.markActivityOccurred();
-        this.sendMessage(errorMessage);
+        await this.sendMessage(errorMessage);
         return;
       }
       invariant(clientSocketMessage, 'should be set');
@@ -301,7 +305,7 @@ class Socket {
             },
           };
         }
-        this.sendMessage(authErrorMessage);
+        await this.sendMessage(authErrorMessage);
         this.ws.close(4100, error.message);
         return;
       } else if (error.message === 'client_version_unsupported') {
@@ -338,19 +342,19 @@ class Socket {
             },
           };
         }
-        this.sendMessage(authErrorMessage);
+        await this.sendMessage(authErrorMessage);
         this.ws.close(4101, error.message);
         return;
       }
       if (error.payload) {
-        this.sendMessage({
+        await this.sendMessage({
           type: serverSocketMessageTypes.ERROR,
           responseTo,
           message: error.message,
           payload: error.payload,
         });
       } else {
-        this.sendMessage({
+        await this.sendMessage({
           type: serverSocketMessageTypes.ERROR,
           responseTo,
           message: error.message,
@@ -379,7 +383,7 @@ class Socket {
     }
   };
 
-  sendMessage = (message: ServerServerSocketMessage) => {
+  sendMessage = async (message: ServerServerSocketMessage) => {
     invariant(
       this.ws.readyState > 0,
       "shouldn't send message until connection established",
@@ -405,7 +409,7 @@ class Socket {
       return;
     }
 
-    const compressionResult = compressMessage(stringMessage);
+    const compressionResult = await compressMessage(stringMessage);
     if (!compressionResult.compressed) {
       this.ws.send(stringMessage);
       return;
@@ -870,7 +874,7 @@ class Socket {
     });
     invariant(checkStateRequest, 'should be set');
 
-    this.sendMessage({
+    await this.sendMessage({
       type: serverSocketMessageTypes.REQUESTS,
       payload: { serverRequests: [checkStateRequest] },
     });

keyserver/src/utils/compress.js
@@ -1,5 +1,6 @@
 // @flow
 
+import { promisify } from 'util';
 import zlib from 'zlib';
 
 import type { CompressedData } from 'lib/types/compression-types.js';
@@ -11,15 +12,17 @@ const brotliOptions = {
 };
 const minimumSizeForCompression = 4096; // bytes
 
+const brotliCompress = promisify(zlib.brotliCompress);
+
 type CompressionResult =
   | { +compressed: true, +result: CompressedData }
   | { +compressed: false, +result: string };
-function compressMessage(message: string): CompressionResult {
+async function compressMessage(message: string): Promise<CompressionResult> {
   const bytesInMessage = new Blob([message]).size;
   if (bytesInMessage < minimumSizeForCompression) {
     return { compressed: false, result: message };
   }
-  const brotliResult = zlib.brotliCompressSync(message, brotliOptions);
+  const brotliResult = await brotliCompress(message, brotliOptions);
   const base64Encoded = brotliResult.toString('base64');
   const result = {
     algo: 'brotli+base64',

lib/utils/sequential-promise-resolver.js
@@ -1,11 +1,11 @@
 // @flow
 
 class SequentialPromiseResolver<Result> {
-  onResolve: Result => mixed;
+  onResolve: Result => Promise<mixed>;
   promises: Array<Promise<?Result>> = [];
   currentlySpinning: boolean = false;
 
-  constructor(onResolve: Result => mixed) {
+  constructor(onResolve: Result => Promise<mixed>) {
     this.onResolve = onResolve;
   }
 
@@ -22,9 +22,11 @@ class SequentialPromiseResolver<Result> {
 
     let currentPromise = this.promises.shift();
     while (currentPromise) {
+      // It's important that we await in sequence here as the messages must be
+      // delivered in order. For more context, see https://phab.comm.dev/D355
       const result = await currentPromise;
       if (result) {
-        this.onResolve(result);
+        await this.onResolve(result);
       }
       currentPromise = this.promises.shift();
     }

commit b80a73813786834c8639109999429a485aaa4316

[keyserver] Avoid "WebSocket is not open" error
Summary:
Linear issue: [ENG-5177](https://linear.app/comm/issue/ENG-5177/websocket-is-not-open)

This error is not really causing any problems. That said, its appearance can be considered a regression caused by D9298.

Before that diff, the check on `this.ws.readyState` at the start of `sendMessage` prevented the error. After that diff, there was a possibility of `this.ws.readyState` changing after the compression step.

This diff addresses the regression by introducing a second check after the compression step.

Test Plan: To be honest, this would be fairly expensive to test compared to the cost of making the diff, and the priority of the issue it's addressing. I think the diff is fairly safe... relying on Flow and CI to catch any issues.

Reviewers: atul, michal, kamil

Subscribers:

keyserver/src/socket/socket.js
@@ -407,6 +407,11 @@ class Socket {
     }
 
     const compressionResult = await compressMessage(stringMessage);
+
+    if (this.ws.readyState !== 1) {
+      return;
+    }
+
     if (!compressionResult.compressed) {
       this.ws.send(stringMessage);
       return;