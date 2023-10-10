27d48eb689e4499f9d7b86a8494c1376f99623b9
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-10-10 15:18:59 -0400

    [web] implement RelationshipPromptButtons into user profiles
    Summary: This diff adds the `RelationshipPromptButton` to the `UserProfileActionButtons` component. The `UserProfileActionButtons` now handles rendering the different layout of the action buttons on the user profile depending on what the condtion of the relationship status is
    
    Test Plan: Please see screenshots below
    
    Subscribers: ashoat, tomek, wyilio
    
    Differential Revision: https://phab.comm.dev/D9447

--- web/modals/user-profile/user-profile-action-buttons.react.js
+++ web/modals/user-profile/user-profile-action-buttons.react.js
@@ -1,11 +1,16 @@
 // @flow
 
+import { faUserMinus, faUserPlus } from '@fortawesome/free-solid-svg-icons';
 import * as React from 'react';
 
+import { useRelationshipPrompt } from 'lib/hooks/relationship-prompt.js';
+import { userRelationshipStatus } from 'lib/types/relationship-types.js';
 import type { ThreadInfo } from 'lib/types/thread-types';
 
 import UserProfileMessageButton from './user-profile-message-button.react.js';
 import css from './user-profile.css';
+import RelationshipPromptButton from '../../chat/relationship-prompt/relationship-prompt-button.js';
+import { buttonThemes } from '../../components/button.react.js';
 
 type Props = {
   +threadInfo: ThreadInfo,
@@ -14,14 +19,80 @@ type Props = {
 function UserProfileActionButtons(props: Props): React.Node {
   const { threadInfo } = props;
 
-  const userProfileActionButtons = React.useMemo(
-    () => (
-      <div className={css.buttonsContainer}>
+  const {
+    otherUserInfo,
+    callbacks: { friendUser, unfriendUser },
+  } = useRelationshipPrompt(threadInfo);
+
+  const userProfileActionButtons = React.useMemo(() => {
+    if (
+      !otherUserInfo ||
+      otherUserInfo.relationshipStatus === userRelationshipStatus.FRIEND
+    ) {
+      return <UserProfileMessageButton threadInfo={threadInfo} />;
+    }
+
+    if (
+      otherUserInfo.relationshipStatus ===
+      userRelationshipStatus.REQUEST_RECEIVED
+    ) {
+      return (
+        <>
+          <UserProfileMessageButton threadInfo={threadInfo} />
+          <div>
+            <p className={css.incomingFriendRequestText}>
+              Incoming friend request
+            </p>
+            <div className={css.multiButtonRowContainer}>
+              <RelationshipPromptButton
+                text="Accept"
+                icon={faUserPlus}
+                buttonColor={buttonThemes.success}
+                onClick={friendUser}
+                className={css.actionButton}
+              />
+              <RelationshipPromptButton
+                text="Reject"
+                icon={faUserMinus}
+                buttonColor={buttonThemes.danger}
+                onClick={unfriendUser}
+                className={css.actionButton}
+              />
+            </div>
+          </div>
+        </>
+      );
+    }
+
+    if (
+      otherUserInfo.relationshipStatus === userRelationshipStatus.REQUEST_SENT
+    ) {
+      return (
+        <div className={css.multiButtonRowContainer}>
+          <RelationshipPromptButton
+            text="Withdraw Friend Request"
+            icon={faUserMinus}
+            buttonColor={buttonThemes.danger}
+            onClick={unfriendUser}
+            className={css.actionButton}
+          />
+          <UserProfileMessageButton threadInfo={threadInfo} />
+        </div>
+      );
+    }
+    return (
+      <div className={css.multiButtonRowContainer}>
+        <RelationshipPromptButton
+          text="Add Friend"
+          icon={faUserPlus}
+          buttonColor={buttonThemes.success}
+          onClick={friendUser}
+          className={css.actionButton}
+        />
         <UserProfileMessageButton threadInfo={threadInfo} />
       </div>
-    ),
-    [threadInfo],
-  );
+    );
+  }, [otherUserInfo, friendUser, threadInfo, unfriendUser]);
 
   return userProfileActionButtons;
 }
