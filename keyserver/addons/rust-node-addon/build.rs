afe16842ca9913cefa7681c3ddca54e0dea762db
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2023-10-12 13:35:05 -0400

    [keyserver] clippy fixes
    Summary:
    just ran cargo clippy and fixed some small things
    
    Depends on D9470
    
    Test Plan: cargo build
    
    Reviewers: bartek, michal
    
    Subscribers: ashoat, tomek, wyilio
    
    Differential Revision: https://phab.comm.dev/D9471

--- keyserver/addons/rust-node-addon/build.rs
+++ keyserver/addons/rust-node-addon/build.rs
@@ -13,8 +13,7 @@ fn main() {
   println!("cargo:rerun-if-changed={}", VERSIONS_JS_PATH);
   let js_path = Path::new(VERSIONS_JS_PATH);
 
-  let content =
-    fs::read_to_string(&js_path).expect("Failed to read version.js");
+  let content = fs::read_to_string(js_path).expect("Failed to read version.js");
 
   let version_line = content
     .lines()
@@ -28,7 +27,7 @@ fn main() {
   // semicolon.
   let re = Regex::new(r"webAndKeyserverCodeVersion\s*=\s*(\d+);").unwrap();
   let version: u64 = re
-    .captures(&version_line)
+    .captures(version_line)
     .and_then(|cap| cap.get(1))
     .map_or_else(
       || panic!("Failed to capture version number"),
@@ -43,7 +42,7 @@ fn main() {
   let rust_path = Path::new(&out_dir).join("version.rs");
 
   fs::write(
-    &rust_path,
+    rust_path,
     format!("pub const CODE_VERSION: u64 = {};", version),
   )
   .expect("Failed to write version.rs");
