b4d426862da1e052dfdade6c054c08678fb7c932
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-10-12 17:36:21 -0400

    [web] factor out mediaModalItem from FullScreenModal
    Summary: This is step 2 in the list below
    
    Test Plan: Confirmed that there were no regressions with viewing a multimedia message in a full screen view
    
    Subscribers: ashoat, tomek, wyilio
    
    Differential Revision: https://phab.comm.dev/D9496

--- web/media/multimedia-modal.react.js
+++ web/media/multimedia-modal.react.js
@@ -1,13 +1,18 @@
 // @flow
 
+import invariant from 'invariant';
 import * as React from 'react';
 
+import { fetchableMediaURI } from 'lib/media/media-utils.js';
 import type {
   EncryptedMediaType,
   MediaType,
   Dimensions,
 } from 'lib/types/media-types.js';
 
+import EncryptedMultimedia from './encrypted-multimedia.react.js';
+import LoadableVideo from './loadable-video.react.js';
+import { usePlaceholder } from './media-utils.js';
 import FullScreenViewModal from '../modals/full-screen-view-modal.react.js';
 
 type MediaInfo =
@@ -33,7 +38,108 @@ type Props = {
 };
 
 function MultimediaModal(props: Props): React.Node {
-  return <FullScreenViewModal {...props} />;
+  const { media } = props;
+
+  const thumbHashEncryptionKey =
+    media.thumbnailEncryptionKey ?? media.encryptionKey;
+
+  const placeholderImage = usePlaceholder(
+    media.thumbHash,
+    thumbHashEncryptionKey,
+  );
+
+  const [dimensions, setDimensions] = React.useState<?Dimensions>(null);
+
+  const photo = React.useMemo(() => {
+    if (media.type !== 'photo') {
+      return null;
+    }
+    const uri = fetchableMediaURI(media.uri);
+    const style = {
+      backgroundImage: placeholderImage
+        ? `url(${placeholderImage})`
+        : undefined,
+    };
+
+    return <img src={uri} style={style} />;
+  }, [media.type, media.uri, placeholderImage]);
+
+  const video = React.useMemo(() => {
+    if (media.type !== 'video') {
+      return null;
+    }
+
+    const uri = fetchableMediaURI(media.uri);
+
+    const { thumbnailURI } = media;
+    invariant(thumbnailURI, 'video missing thumbnail');
+
+    return (
+      <LoadableVideo
+        uri={uri}
+        thumbnailSource={{ thumbnailURI }}
+        thumbHashDataURL={placeholderImage}
+      />
+    );
+  }, [media, placeholderImage]);
+
+  const encryptedMultimedia = React.useMemo(() => {
+    if (media.type !== 'encrypted_photo' && media.type !== 'encrypted_video') {
+      return null;
+    }
+
+    const {
+      type,
+      blobURI,
+      encryptionKey,
+      thumbnailBlobURI,
+      thumbnailEncryptionKey,
+    } = media;
+
+    const contentDimensions = dimensions ?? media.dimensions;
+    const elementStyle = contentDimensions
+      ? {
+          width: `${contentDimensions.width}px`,
+          height: `${contentDimensions.height}px`,
+        }
+      : undefined;
+
+    return (
+      <EncryptedMultimedia
+        type={type}
+        blobURI={blobURI}
+        encryptionKey={encryptionKey}
+        thumbnailBlobURI={thumbnailBlobURI}
+        thumbnailEncryptionKey={thumbnailEncryptionKey}
+        placeholderSrc={placeholderImage}
+        elementStyle={elementStyle}
+      />
+    );
+  }, [dimensions, media, placeholderImage]);
+
+  const mediaModalItem = React.useMemo(() => {
+    if (media.type === 'photo') {
+      return photo;
+    } else if (media.type === 'video') {
+      return video;
+    } else {
+      return encryptedMultimedia;
+    }
+  }, [encryptedMultimedia, media.type, photo, video]);
+
+  const multimediaModal = React.useMemo(
+    () => (
+      <FullScreenViewModal
+        contentDimensions={dimensions}
+        setContentDimensions={setDimensions}
+      >
+        {mediaModalItem}
+      </FullScreenViewModal>
+    ),
+    [dimensions, mediaModalItem],
+  );
+
+  return multimediaModal;
 }
 
 export default MultimediaModal;
