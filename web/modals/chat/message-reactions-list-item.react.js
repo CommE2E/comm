0cb28b625adfe9411284fa22e39782da3aac278b
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-10-12 23:08:55 -0400

    [web] introduce MessageReactionsListItem
    Summary: This diff simply factors out the logic of rendering the message reactions list item into it's own component. We want to do this so that we can use `usePushUserProfileModal` inside this component
    
    Test Plan: Confirmed that the message reactions modal still looks and behaves the same
    
    Subscribers: ashoat, tomek, wyilio
    
    Differential Revision: https://phab.comm.dev/D9485

---
+++ web/modals/chat/message-reactions-list-item.react.js
@@ -0,0 +1,28 @@
+// @flow
+
+import * as React from 'react';
+
+import { type MessageReactionListInfo } from 'lib/shared/reaction-utils.js';
+
+import css from './message-reactions-modal.css';
+import UserAvatar from '../../avatars/user-avatar.react.js';
+
+type Props = {
+  +messageReactionUser: MessageReactionListInfo,
+};
+
+function MessageReactionsListItem(props: Props): React.Node {
+  const { messageReactionUser } = props;
+
+  return (
+    <div className={css.userRowContainer}>
+      <div className={css.userInfoContainer}>
+        <UserAvatar size="S" userID={messageReactionUser.id} />
+        <div className={css.username}>{messageReactionUser.username}</div>
+      </div>
+      <div>{messageReactionUser.reaction}</div>
+    </div>
+  );
+}
+
+export default MessageReactionsListItem;
