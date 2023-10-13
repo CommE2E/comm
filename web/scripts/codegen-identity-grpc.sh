810ea54ea9e8dff71bebcda4ed5c6f0d37cb0392
Ashoat Tevosyan <ashoat@comm.app>
2023-10-13 15:48:21 -0400

    [web] Introduce codegen-identity-grpc script
    Summary:
    In order to update our `grpc-web` codegen, we need to run `protoc` with some specific parameters, and then rename the files. I figured it would be good to put this into a Yarn script so we don't have to remember it / pull it from some diff's test plan every time.
    
    Note that after running this script, some more work is necessary. There are a couple small modifications necessary to the `.cjs` files (update the imported filename and add `@generated` annotations), but the bulk of the work is in converting the TypeScript libraries to Flow.
    
    Depends on D9490
    
    Test Plan: Run the script
    
    Reviewers: varun, atul
    
    Reviewed By: varun
    
    Subscribers: tomek, wyilio
    
    Differential Revision: https://phab.comm.dev/D9491

---
+++ web/scripts/codegen-identity-grpc.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+protoc -I=../shared/protos/ identity_client.proto --js_out=import_style=commonjs:protobufs --grpc-web_out=import_style=commonjs+dts,mode=grpcwebtext:protobufs
+
+mv protobufs/identity_client_pb.js protobufs/identity-structs.cjs
+mv protobufs/identity_client_grpc_web_pb.js protobufs/identity-client.cjs
+mv protobufs/identity_client_pb.d.ts protobufs/identity-structs.cjs.flow
+mv protobufs/identity_client_grpc_web_pb.d.ts protobufs/identity-client.cjs.flow
+
+# This echo statement splits the string to ensure that Phabricator shows this file in reviews
+echo "Make sure to edit the files to correct import paths, reintroduce @""generated annotation, and convert TS to Flow!"
