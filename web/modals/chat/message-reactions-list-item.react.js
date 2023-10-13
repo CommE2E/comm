abc645d0d3d7593a6fab705e897fe8e73d65ef2c
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-10-13 02:25:49 -0400

    [web] make user profile accessible from message reactions list item
    Summary: This diff makes the message reactions list item  interactive. When a user clicks on this component, the user profile modal will be pushed
    
    Test Plan: Please see the demo video below
    
    Reviewers:
    
    Subscribers:

--- web/modals/chat/message-reactions-list-item.react.js
+++ web/modals/chat/message-reactions-list-item.react.js
@@ -6,6 +6,7 @@ import { type MessageReactionListInfo } from 'lib/shared/reaction-utils.js';
 
 import css from './message-reactions-modal.css';
 import UserAvatar from '../../avatars/user-avatar.react.js';
+import { usePushUserProfileModal } from '../user-profile/user-profile-utils.js';
 
 type Props = {
   +messageReactionUser: MessageReactionListInfo,
@@ -14,8 +15,10 @@ type Props = {
 function MessageReactionsListItem(props: Props): React.Node {
   const { messageReactionUser } = props;
 
+  const pushUserProfileModal = usePushUserProfileModal(messageReactionUser.id);
+
   return (
-    <div className={css.userRowContainer}>
+    <div className={css.userRowContainer} onClick={pushUserProfileModal}>
       <div className={css.userInfoContainer}>
         <UserAvatar size="S" userID={messageReactionUser.id} />
         <div className={css.username}>{messageReactionUser.username}</div>
