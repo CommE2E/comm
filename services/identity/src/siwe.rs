4a2d8b36af987b54c80cbc3690edaf1501e6064c
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2023-10-13 00:19:20 -0400

    [services] move eth address validation to siwe.rs
    Summary:
    just moving this to the right location...
    
    Depends on D9479
    
    Test Plan: cargo build
    
    Reviewers: bartek, michal, jon
    
    Subscribers: ashoat, tomek, wyilio
    
    Differential Revision: https://phab.comm.dev/D9480

--- services/identity/src/siwe.rs
+++ services/identity/src/siwe.rs
@@ -1,4 +1,5 @@
 use chrono::Utc;
+use regex::Regex;
 use siwe::Message;
 use tonic::Status;
 use tracing::error;
@@ -31,4 +32,53 @@ pub fn parse_and_verify_siwe_message(
     })?;
 
   Ok(siwe_message)
+}
+
+pub fn is_valid_ethereum_address(candidate: &str) -> bool {
+  let ethereum_address_regex = Regex::new(r"^0x[a-fA-F0-9]{40}$").unwrap();
+  ethereum_address_regex.is_match(candidate)
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_valid_ethereum_address() {
+    assert!(is_valid_ethereum_address(
+      "0x1234567890123456789012345678901234567890"
+    ),);
+    assert!(is_valid_ethereum_address(
+      "0xABCDEF123456789012345678901234567890ABCD"
+    ));
+    assert!(is_valid_ethereum_address(
+      "0xabcdef123456789012345678901234567890abcd"
+    ));
+  }
+
+  #[test]
+  fn test_invalid_ethereum_address() {
+    // Shorter than 42 characters
+    assert_eq!(
+      is_valid_ethereum_address("0x12345678901234567890123456789012345678"),
+      false
+    );
+    // Longer than 42 characters
+    assert_eq!(
+      is_valid_ethereum_address("0x123456789012345678901234567890123456789012"),
+      false
+    );
+    // Missing 0x prefix
+    assert_eq!(
+      is_valid_ethereum_address("1234567890123456789012345678901234567890"),
+      false
+    );
+    // Contains invalid characters
+    assert_eq!(
+      is_valid_ethereum_address("0x1234567890GHIJKL9012345678901234567890"),
+      false
+    );
+    // Empty string
+    assert_eq!(is_valid_ethereum_address(""), false);
+  }
 }
