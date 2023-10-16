CommE2E/comm

commit 1c9a613cd869ce96535018fb88009caae0e8d294

[keyserver] migration to publish prekeys to identity
Summary:
Issue: [ENG-3944](https://linear.app/comm/issue/ENG-3944/update-keyserver-prekey-lifecycle-code-to-upload-to-identity-service#comment-8beb6586).

Currently, prekeys on the prod keyserver are for sure marked as published, but yet published to Identity. To fix that uploading those via migration.

Keys are already marked as published, so not marking those again to keep the old publishing date, only uploading to the Identity service.

Depends on D9423

Test Plan:
1. Run migration.
2. Check for ` migration 46 succeeded`.
3. Check Identity logs.

Reviewers: varun, marcin, ashoat

Subscribers: tomek, wyilio, ashoat

Differential Revision: https://phab.comm.dev/D9427

keyserver/src/database/migration-config.js
@@ -11,10 +11,14 @@ import { dbQuery, SQL } from '../database/database.js';
 import { processMessagesInDBForSearch } from '../database/search-utils.js';
 import { deleteThread } from '../deleters/thread-deleters.js';
 import { createScriptViewer } from '../session/scripts.js';
+import { fetchOlmAccount } from '../updaters/olm-account-updater.js';
 import { updateRolesAndPermissionsForAllThreads } from '../updaters/thread-permission-updaters.js';
 import { updateThread } from '../updaters/thread-updaters.js';
 import { ensureUserCredentials } from '../user/checks.js';
-import { createPickledOlmAccount } from '../utils/olm-utils.js';
+import {
+  createPickledOlmAccount,
+  publishPrekeysToIdentity,
+} from '../utils/olm-utils.js';
 
 const botViewer = createScriptViewer(bots.commbot.userID);
 
@@ -556,6 +560,23 @@ const migrations: $ReadOnlyMap<number, () => Promise<mixed>> = new Map([
               CHARSET latin1 COLLATE latin1_swedish_ci NOT NULL;
         `,
       ),
+  ],
+  [
+    46,
+    async () => {
+      try {
+        const [content, notif] = await Promise.all([
+          fetchOlmAccount('content'),
+          fetchOlmAccount('notifications'),
+        ]);
+        await publishPrekeysToIdentity(content.account, notif.account);
+      } catch (e) {
+        console.warn('Encountered error while trying to publish prekeys', e);
+        if (process.env.ENV === 'prod') {
+          throw e;
+        }
+      }
+    },
   ],
 ]);
 const newDatabaseVersion: number = Math.max(...migrations.keys());

keyserver/src/utils/olm-utils.js
@@ -203,7 +203,11 @@ async function revalidateAccountPrekeys(
 ): Promise<void> {
   // Since keys are rotated synchronously, only check validity of one
   if (shouldRotatePrekey(contentAccount)) {
-    await publishNewPrekeys(contentAccount, notifAccount);
+    contentAccount.generate_prekey();
+    notifAccount.generate_prekey();
+    await publishPrekeysToIdentity(contentAccount, notifAccount);
+    contentAccount.mark_prekey_as_published();
+    notifAccount.mark_prekey_as_published();
   }
   if (shouldForgetPrekey(contentAccount)) {
     contentAccount.forget_old_prekey();
@@ -211,20 +215,16 @@ async function revalidateAccountPrekeys(
   }
 }
 
-async function publishNewPrekeys(
+async function publishPrekeysToIdentity(
   contentAccount: OlmAccount,
   notifAccount: OlmAccount,
 ): Promise<void> {
   const rustAPIPromise = getRustAPI();
   const fetchIdentityInfoPromise = fetchIdentityInfo();
-
   const deviceID = JSON.parse(contentAccount.identity_keys()).ed25519;
 
-  contentAccount.generate_prekey();
   const { prekey: contentPrekey, prekeySignature: contentPrekeySignature } =
     getAccountPrekeysSet(contentAccount);
-
-  notifAccount.generate_prekey();
   const { prekey: notifPrekey, prekeySignature: notifPrekeySignature } =
     getAccountPrekeysSet(notifAccount);
 
@@ -254,9 +254,6 @@ async function publishNewPrekeys(
     notifPrekey,
     notifPrekeySignature,
   );
-
-  contentAccount.mark_prekey_as_published();
-  notifAccount.mark_prekey_as_published();
 }
 
 export {
@@ -270,4 +267,5 @@ export {
   getContentSigningKey,
   getAccountPrekeysSet,
   revalidateAccountPrekeys,
+  publishPrekeysToIdentity,
 };

commit 89ca969890084914f67f082f0c82493b026e6998

[keyserver] fix production check while uploading keys to Identity
Summary: Fixing regression introduced in D9427.

This shouldn't affect anything, the only risk is that migration could make a silent fail on prod because the condition returns false always - but this is not what we want.

I discovered on my local setup that `process.env.NODE_ENV === 'production'` will not work because this env value is never set to production (this might be the fault of some missing `.env` files on my setup), but based on the code we only set `NODE_ENV` for dev, so changing this for safer solution.

Test Plan:
1. Terminate Identity
2. Test migration on prod (fail) / dev (only warning)

Reviewers: michal, marcin, ashoat

keyserver/src/database/migration-config.js
@@ -572,7 +572,7 @@ const migrations: $ReadOnlyMap<number, () => Promise<mixed>> = new Map([
         await publishPrekeysToIdentity(content.account, notif.account);
       } catch (e) {
         console.warn('Encountered error while trying to publish prekeys', e);
-        if (process.env.ENV === 'prod') {
+        if (process.env.NODE_ENV !== 'development') {
           throw e;
         }
       }