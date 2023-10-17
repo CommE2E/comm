52d969952bca9c84e3b250fd90342e7c6572df27
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2023-10-17 13:26:31 +0200

    [terraform] Localstack changes for commtest
    Summary:
    Minor changes to localstack terraform configuration.
    They're needed to be able to successfully destroy resources using `terraform apply -destroy`.
    Also made localstack endpoint configurable - this will be required later when running terraform from within a container.
    
    Test Plan:
    - TF plan shows no changes on remote config
    - On local dev config, doing:
    
      ```
      cd services; docker compose up -d localstack
      cd terraform/dev
      terraform apply -auto-approve
      terraform apply -destroy -auto-approve
      ````
    successfully initializes and then destroys all resources.
    
    Reviewers: varun, michal, kamil, jon
    
    Subscribers: ashoat, tomek, wyilio
    
    Differential Revision: https://phab.comm.dev/D9507

--- services/terraform/modules/shared/s3.tf
+++ services/terraform/modules/shared/s3.tf
@@ -6,6 +6,7 @@ variable "s3_bucket_names" {
 }
 
 resource "aws_s3_bucket" "comm_buckets" {
-  count  = length(var.s3_bucket_names)
-  bucket = "${var.s3_bucket_names[count.index]}${var.bucket_name_suffix}"
+  count         = length(var.s3_bucket_names)
+  bucket        = "${var.s3_bucket_names[count.index]}${var.bucket_name_suffix}"
+  force_destroy = var.is_dev ? true : false
 }
