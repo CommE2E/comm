414c8edd00cb0c9de2ad1c676f10a72db917d232
marcinwasowicz <marcinkwasowicz@gmail.com>
2023-10-18 15:10:42 +0200

    Refactor olm accounts lifecycle on web Differential Revision: https://phab.comm.dev/D9532

--- web/account/account-hooks.js
+++ web/account/account-hooks.js
@@ -1,35 +1,143 @@
 // @flow
 
+import olm from '@commapp/olm';
 import * as React from 'react';
+import { useDispatch } from 'react-redux';
+import uuid from 'uuid';
 
-import type { SignedIdentityKeysBlob } from 'lib/types/crypto-types.js';
+import type {
+  SignedIdentityKeysBlob,
+  CompleteCryptoStore,
+  IdentityKeysBlob,
+} from 'lib/types/crypto-types.js';
 
+import { initOlm } from '../olm/olm-utils.js';
+import {
+  setPrimaryIdentityKeys,
+  setNotificationIdentityKeys,
+  setPickledPrimaryAccount,
+  setPickledNotificationAccount,
+} from '../redux/crypto-store-reducer.js';
 import { useSelector } from '../redux/redux-utils.js';
-import { getSignedIdentityKeysBlobSelector } from '../selectors/socket-selectors.js';
 
-function useSignedIdentityKeysBlob(): ?SignedIdentityKeysBlob {
-  const getSignedIdentityKeysBlob: ?() => Promise<SignedIdentityKeysBlob> =
-    useSelector(getSignedIdentityKeysBlobSelector);
+function useGetOrCreateCryptoStore(): () => Promise<CompleteCryptoStore> {
+  const dispatch = useDispatch();
+  const currentCryptoStore = useSelector(state => state.cryptoStore);
 
-  const [signedIdentityKeysBlob, setSignedIdentityKeysBlob] =
-    React.useState<?SignedIdentityKeysBlob>(null);
+  return React.useCallback(async () => {
+    if (
+      currentCryptoStore.primaryAccount &&
+      currentCryptoStore.primaryIdentityKeys &&
+      currentCryptoStore.notificationAccount &&
+      currentCryptoStore.notificationIdentityKeys
+    ) {
+      return {
+        primaryAccount: currentCryptoStore.primaryAccount,
+        primaryIdentityKeys: currentCryptoStore.primaryIdentityKeys,
+        notificationAccount: currentCryptoStore.notificationAccount,
+        notificationIdentityKeys: currentCryptoStore.notificationIdentityKeys,
+      };
+    }
 
-  React.useEffect(() => {
-    (async () => {
-      if (
-        getSignedIdentityKeysBlob === null ||
-        getSignedIdentityKeysBlob === undefined
-      ) {
-        setSignedIdentityKeysBlob(null);
-        return;
-      }
-      const resolvedSignedIdentityKeysBlob: SignedIdentityKeysBlob =
-        await getSignedIdentityKeysBlob();
-      setSignedIdentityKeysBlob(resolvedSignedIdentityKeysBlob);
-    })();
-  }, [getSignedIdentityKeysBlob]);
+    await initOlm();
 
-  return signedIdentityKeysBlob;
+    const identityAccount = new olm.Account();
+    identityAccount.create();
+    const { ed25519: identityED25519, curve25519: identityCurve25519 } =
+      JSON.parse(identityAccount.identity_keys());
+
+    dispatch({
+      type: setPrimaryIdentityKeys,
+      payload: { ed25519: identityED25519, curve25519: identityCurve25519 },
+    });
+
+    const identityAccountPicklingKey = uuid.v4();
+    const pickledIdentityAccount = identityAccount.pickle(
+      identityAccountPicklingKey,
+    );
+
+    dispatch({
+      type: setPickledPrimaryAccount,
+      payload: {
+        picklingKey: identityAccountPicklingKey,
+        pickledAccount: pickledIdentityAccount,
+      },
+    });
+
+    const notificationAccount = new olm.Account();
+    notificationAccount.create();
+    const { ed25519: notificationED25519, curve25519: notificationCurve25519 } =
+      JSON.parse(notificationAccount.identity_keys());
+
+    dispatch({
+      type: setNotificationIdentityKeys,
+      payload: {
+        ed25519: notificationED25519,
+        curve25519: notificationCurve25519,
+      },
+    });
+
+    const notificationAccountPicklingKey = uuid.v4();
+    const pickledNotificationAccount = notificationAccount.pickle(
+      notificationAccountPicklingKey,
+    );
+
+    dispatch({
+      type: setPickledNotificationAccount,
+      payload: {
+        picklingKey: notificationAccountPicklingKey,
+        pickledAccount: pickledNotificationAccount,
+      },
+    });
+
+    return {
+      primaryAccount: {
+        picklingKey: identityAccountPicklingKey,
+        pickledAccount: pickledIdentityAccount,
+      },
+      primaryIdentityKeys: {
+        ed25519: identityED25519,
+        curve25519: identityCurve25519,
+      },
+      notificationAccount: {
+        picklingKey: notificationAccountPicklingKey,
+        pickledAccount: pickledNotificationAccount,
+      },
+      notificationIdentityKeys: {
+        ed25519: notificationED25519,
+        curve25519: notificationCurve25519,
+      },
+    };
+  }, [dispatch, currentCryptoStore]);
 }
 
-export { useSignedIdentityKeysBlob };
+function useGetSignedIdentityKeysBlob(): () => Promise<SignedIdentityKeysBlob> {
+  const getOrCreateCryptoStore = useGetOrCreateCryptoStore();
+
+  return React.useCallback(async () => {
+    const { primaryAccount, primaryIdentityKeys, notificationIdentityKeys } =
+      await getOrCreateCryptoStore();
+
+    await initOlm();
+    const primaryOLMAccount = new olm.Account();
+    primaryOLMAccount.unpickle(
+      primaryAccount.picklingKey,
+      primaryAccount.pickledAccount,
+    );
+
+    const identityKeysBlob: IdentityKeysBlob = {
+      primaryIdentityPublicKeys: primaryIdentityKeys,
+      notificationIdentityPublicKeys: notificationIdentityKeys,
+    };
+
+    const payloadToBeSigned: string = JSON.stringify(identityKeysBlob);
+    const signedIdentityKeysBlob: SignedIdentityKeysBlob = {
+      payload: payloadToBeSigned,
+      signature: primaryOLMAccount.sign(payloadToBeSigned),
+    };
+
+    return signedIdentityKeysBlob;
+  }, [getOrCreateCryptoStore]);
+}
+
+export { useGetSignedIdentityKeysBlob, useGetOrCreateCryptoStore };
