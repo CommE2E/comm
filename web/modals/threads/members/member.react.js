205d997a43ab14da163fbeacd03c5946058af119
Rohan Khanderia <rohan.khanderia@gmail.com>
2023-10-19 16:02:37 -0400

    [lib/web/native] Display community role for users in subchannels/sidebars
    Summary:
    Currently, the members modal will display the correct role for community roots (i.e. 'Members', 'Admins'), but will only display 'Members' for subchannels/sidebars. This is because of the way that
    `ThreadInfo` only contains the `Members` role for these threads.
    
    The solution here is to introspect into the community's threadInfo for that specific thread, and pull the roles from there for each member. This will ensure that we have access to all of the community roles.
    
    I noted in a code comment, but the special case is for `GENESIS`, where we don't readily have access to the community's members (and also roles are not really supported there), so in that case
    we'll just read the current `ThreadInfo` for the role (probably 'Members' for subchannels/sidebars).
    
    Resolves https://linear.app/comm/issue/ENG-5178/ashoat-shows-up-under-admins-column-but-with-members-pill
    
    Test Plan:
    Please see the test videos below (also tested for custom roles)
    
    Before:
    
    After:
    
    Reviewers: atul, ginsu, ashoat
    
    Subscribers: tomek, wyilio
    
    Differential Revision: https://phab.comm.dev/D9430

--- web/modals/threads/members/member.react.js
+++ web/modals/threads/members/member.react.js
@@ -19,6 +19,7 @@ import {
   useDispatchActionPromise,
   useServerCall,
 } from 'lib/utils/action-utils.js';
+import { useRoleNamesFromCommunityThreadInfo } from 'lib/utils/role-utils.js';
 
 import ChangeMemberRoleModal from './change-member-role-modal.react.js';
 import css from './members-modal.css';
@@ -41,8 +42,11 @@ function ThreadMember(props: Props): React.Node {
   const { memberInfo, threadInfo, setOpenMenu } = props;
   const { pushModal } = useModalContext();
   const userName = stringForUser(memberInfo);
-  const { roles } = threadInfo;
-  const { role } = memberInfo;
+
+  const roleNames = useRoleNamesFromCommunityThreadInfo(threadInfo, [
+    memberInfo,
+  ]);
+  const roleName = roleNames.get(memberInfo.id);
 
   const onMenuChange = React.useCallback(
     menuOpen => {
@@ -108,8 +112,6 @@ function ThreadMember(props: Props): React.Node {
     () => <SWMansionIcon icon="edit-1" size={17} />,
     [],
   );
-
-  const roleName = role && roles[role].name;
 
   const label = React.useMemo(
     () => <Label variant="grey">{roleName}</Label>,
