280df609d24f6be620c11764b7e85b7342cd1418
marcinwasowicz <marcinkwasowicz@gmail.com>
2023-10-19 12:23:29 +0200

    Initialize olm sessions for notifications on web and keyserver via socket requests
    Summary: edit in GUI
    
    Test Plan: edit in GUI
    
    Reviewers: michal, kamil, atul
    
    Subscribers: ashoat, tomek, wyilio
    
    Differential Revision: https://phab.comm.dev/D9535

--- web/selectors/socket-selectors.js
+++ web/selectors/socket-selectors.js
@@ -61,6 +61,7 @@ const sessionIdentificationSelector: (
 type WebGetClientResponsesSelectorInputType = {
   +state: AppState,
   +getSignedIdentityKeysBlob: () => Promise<SignedIdentityKeysBlob>,
+  +getInitialNotificationsEncryptedMessage: () => Promise<string>,
 };
 
 const webGetClientResponsesSelector: (
@@ -74,23 +75,26 @@ const webGetClientResponsesSelector: (
     input.getSignedIdentityKeysBlob,
   (input: WebGetClientResponsesSelectorInputType) =>
     input.state.navInfo.tab === 'calendar',
+  (input: WebGetClientResponsesSelectorInputType) =>
+    input.getInitialNotificationsEncryptedMessage,
   (
       getClientResponsesFunc: (
         calendarActive: boolean,
         oneTimeKeyGenerator: ?OneTimeKeyGenerator,
         getSignedIdentityKeysBlob: () => Promise<SignedIdentityKeysBlob>,
-        getInitialNotificationsEncryptedMessage: ?() => Promise<string>,
+        getInitialNotificationsEncryptedMessage: () => Promise<string>,
         serverRequests: $ReadOnlyArray<ClientServerRequest>,
       ) => Promise<$ReadOnlyArray<ClientClientResponse>>,
       getSignedIdentityKeysBlob: () => Promise<SignedIdentityKeysBlob>,
       calendarActive: boolean,
+      getInitialNotificationsEncryptedMessage: () => Promise<string>,
     ) =>
     (serverRequests: $ReadOnlyArray<ClientServerRequest>) =>
       getClientResponsesFunc(
         calendarActive,
         null,
         getSignedIdentityKeysBlob,
-        null,
+        getInitialNotificationsEncryptedMessage,
         serverRequests,
       ),
 );
