CommE2E/comm

commit a45eeb96a3b4d4dc207041e6bc8eb8dcc4b8833d

[web] Gate encrypted media uploads on Comm staff community
Summary:
Similiar to D7335 but for web.

Most of the component code passes `threadID` only so I had to add a separate selector for `threadInfo`.

Test Plan:
- In my local environment, created a community with ID from `lib/facts/commStaffCommunity` using the create-community script.
- Sent a photo to that community
- Sent the same photo to a chat in GENESIS community
- Ensured that the `uploads` table has two new entries: one containing `encryptionKey` and the other one doesn't. (also looked at the `thread` column)
- Created subchats and threads in both communities, repeated.

Reviewers: ashoat, atul

Reviewed By: ashoat

Subscribers: tomek

Differential Revision: https://phab.comm.dev/D7434

web/input/input-state-container.react.js
@@ -32,6 +32,7 @@ import {
   useModalContext,
   type PushModal,
 } from 'lib/components/modal-provider.react.js';
+import commStaffCommunity from 'lib/facts/comm-staff-community.js';
 import { getNextLocalUploadID } from 'lib/media/media-utils.js';
 import { pendingToRealizedThreadIDsSelector } from 'lib/selectors/thread-selectors.js';
 import {
@@ -46,6 +47,7 @@ import {
   threadIsPending,
   threadIsPendingSidebar,
   patchThreadInfoToIncludeMentionedMembersOfParent,
+  threadInfoInsideCommunity,
 } from 'lib/shared/thread-utils.js';
 import type { CalendarQuery } from 'lib/types/entry-types.js';
 import type {
@@ -71,6 +73,7 @@ import {
   type ClientNewThreadRequest,
   type NewThreadResult,
   type ThreadInfo,
+  type RawThreadInfo,
   threadTypes,
 } from 'lib/types/thread-types.js';
 import {
@@ -106,6 +109,7 @@ type Props = {
   +drafts: { +[key: string]: string },
   +viewerID: ?string,
   +messageStoreMessages: { +[id: string]: RawMessageInfo },
+  +threadStoreThreadInfos: { +[id: string]: RawThreadInfo },
   +pendingRealizedThreadIDs: $ReadOnlyMap<string, string>,
   +dispatch: Dispatch,
   +dispatchActionPromise: DispatchActionPromise,
@@ -163,8 +167,6 @@ class InputStateContainer extends React.PureComponent<Props, State> {
   };
   replyCallbacks: Array<(message: string) => void> = [];
   pendingThreadCreations = new Map<string, Promise<string>>();
-  // TODO: we want to send encrypted media if thread is in the Comm community
-  sendEncryptedMedia: boolean = false;
 
   // When the user sends a multimedia message that triggers the creation of a
   // sidebar, the sidebar gets created right away, but the message needs to wait
@@ -330,7 +332,7 @@ class InputStateContainer extends React.PureComponent<Props, State> {
           creatorID,
           media,
         },
-        { forceMultimediaMessageType: this.sendEncryptedMedia },
+        { forceMultimediaMessageType: this.shouldEncryptMedia(threadID) },
       );
       newMessageInfos.set(messageID, messageInfo);
     }
@@ -372,6 +374,14 @@ class InputStateContainer extends React.PureComponent<Props, State> {
       `rawMessageInfo ${localMessageID} should be multimedia`,
     );
     return rawMessageInfo;
+  }
+
+  shouldEncryptMedia(threadID: ?string): boolean {
+    if (!threadID) {
+      return false;
+    }
+    const threadInfo = this.props.threadStoreThreadInfos[threadID];
+    return threadInfoInsideCommunity(threadInfo, commStaffCommunity.id);
   }
 
   async sendMultimediaMessage(messageInfo: RawMultimediaMessageInfo) {
@@ -632,7 +642,7 @@ class InputStateContainer extends React.PureComponent<Props, State> {
     const { pushModal } = this.props;
 
     const appendResults = await Promise.all(
-      files.map(file => this.appendFile(file, selectionTime)),
+      files.map(file => this.appendFile(threadID, file, selectionTime)),
     );
 
     if (appendResults.some(({ result }) => !result.success)) {
@@ -682,6 +692,7 @@ class InputStateContainer extends React.PureComponent<Props, State> {
   }
 
   async appendFile(
+    threadID: ?string,
     file: File,
     selectTime: number,
   ): Promise<{
@@ -723,7 +734,7 @@ class InputStateContainer extends React.PureComponent<Props, State> {
     const { uri, file: fixedFile, mediaType, dimensions } = result;
 
     let encryptionResult;
-    if (this.sendEncryptedMedia) {
+    if (this.shouldEncryptMedia(threadID)) {
       let encryptionResponse;
       const encryptionStart = Date.now();
       try {
@@ -1502,6 +1513,9 @@ const ConnectedInputStateContainer: React.ComponentType<BaseProps> =
     const messageStoreMessages = useSelector(
       state => state.messageStore.messages,
     );
+    const threadStoreThreadInfos = useSelector(
+      state => state.threadStore.threadInfos,
+    );
     const pendingToRealizedThreadIDs = useSelector(state =>
       pendingToRealizedThreadIDsSelector(state.threadStore.threadInfos),
     );
@@ -1541,6 +1555,7 @@ const ConnectedInputStateContainer: React.ComponentType<BaseProps> =
         drafts={drafts}
         viewerID={viewerID}
         messageStoreMessages={messageStoreMessages}
+        threadStoreThreadInfos={threadStoreThreadInfos}
         pendingRealizedThreadIDs={pendingToRealizedThreadIDs}
         calendarQuery={calendarQuery}
         uploadMultimedia={callUploadMultimedia}

commit ea809efd79c2a27ff62bdf68a6788d0cf601470e

[web] Fix thread creation from media
Summary:
This solves [ENG-5145](https://linear.app/comm/issue/ENG-5145/creating-a-sidebar-by-sending-an-image-doesnt-work-on-prod-web), a regression introduced in D7434.

The issue is that pending threads don't appear in the `ThreadStore`. Instead of trying to pull the `ThreadInfo` from the `ThreadStore`, we should drill it through from the callsites.

Test Plan: I confirmed that I'm again able to create a thread from media in my local environment (the bug no longer repros)

Reviewers: bartek, atul, tomek

Subscribers:

web/chat/chat-input-bar.react.js
@@ -528,9 +528,10 @@ class ChatInputBar extends React.PureComponent<Props> {
   onMultimediaFileChange = async (
     event: SyntheticInputEvent<HTMLInputElement>,
   ) => {
-    const result = await this.props.inputState.appendFiles([
-      ...event.target.files,
-    ]);
+    const result = await this.props.inputState.appendFiles(
+      this.props.threadInfo,
+      [...event.target.files],
+    );
     if (!result && this.multimediaInput) {
       this.multimediaInput.value = '';
     }

web/chat/chat-message-list-container.react.js
@@ -36,11 +36,11 @@ function ChatMessageListContainer(props: Props): React.Node {
   const dispatch = useDispatch();
 
   React.useEffect(() => {
-    if (isChatCreation && activeChatThreadID !== threadInfo?.id) {
+    if (isChatCreation && activeChatThreadID !== threadInfo.id) {
       let payload = {
-        activeChatThreadID: threadInfo?.id,
+        activeChatThreadID: threadInfo.id,
       };
-      if (threadIsPending(threadInfo?.id)) {
+      if (threadIsPending(threadInfo.id)) {
         payload = {
           ...payload,
           pendingThread: threadInfo,
@@ -60,7 +60,7 @@ function ChatMessageListContainer(props: Props): React.Node {
     drop: item => {
       const { files } = item;
       if (inputState && files.length > 0) {
-        inputState.appendFiles(files);
+        inputState.appendFiles(threadInfo, files);
       }
     },
     collect: monitor => ({
@@ -91,9 +91,9 @@ function ChatMessageListContainer(props: Props): React.Node {
         return;
       }
       e.preventDefault();
-      inputState.appendFiles([...files]);
+      inputState.appendFiles(threadInfo, [...files]);
     },
-    [inputState],
+    [inputState, threadInfo],
   );
 
   React.useEffect(() => {

web/input/input-state-container.react.js
@@ -78,7 +78,6 @@ import {
   type ClientNewThreadRequest,
   type NewThreadResult,
   type ThreadInfo,
-  type RawThreadInfo,
 } from 'lib/types/thread-types.js';
 import {
   type DispatchActionPromise,
@@ -120,7 +119,6 @@ type Props = {
   +drafts: { +[key: string]: string },
   +viewerID: ?string,
   +messageStoreMessages: { +[id: string]: RawMessageInfo },
-  +threadStoreThreadInfos: { +[id: string]: RawThreadInfo },
   +pendingRealizedThreadIDs: $ReadOnlyMap<string, string>,
   +dispatch: Dispatch,
   +dispatchActionPromise: DispatchActionPromise,
@@ -292,10 +290,18 @@ class InputStateContainer extends React.PureComponent<Props, State> {
         ) {
           continue;
         }
+        const { shouldEncrypt } = upload;
         let assignedUploads = newlyAssignedUploads.get(messageID);
         if (!assignedUploads) {
-          assignedUploads = { threadID, uploads: [] };
+          assignedUploads = { threadID, shouldEncrypt, uploads: [] };
           newlyAssignedUploads.set(messageID, assignedUploads);
+        }
+        if (shouldEncrypt !== assignedUploads.shouldEncrypt) {
+          console.warn(
+            `skipping upload ${localUploadID} ` +
+              "because shouldEncrypt doesn't match",
+          );
+          continue;
         }
         assignedUploads.uploads.push(upload);
       }
@@ -303,7 +309,7 @@ class InputStateContainer extends React.PureComponent<Props, State> {
 
     const newMessageInfos = new Map();
     for (const [messageID, assignedUploads] of newlyAssignedUploads) {
-      const { uploads, threadID } = assignedUploads;
+      const { uploads, threadID, shouldEncrypt } = assignedUploads;
       const creatorID = this.props.viewerID;
       invariant(creatorID, 'need viewer ID in order to send a message');
       const media = uploads.map(
@@ -358,7 +364,7 @@ class InputStateContainer extends React.PureComponent<Props, State> {
           creatorID,
           media,
         },
-        { forceMultimediaMessageType: this.shouldEncryptMedia(threadID) },
+        { forceMultimediaMessageType: shouldEncrypt },
       );
       newMessageInfos.set(messageID, messageInfo);
     }
@@ -402,11 +408,7 @@ class InputStateContainer extends React.PureComponent<Props, State> {
     return rawMessageInfo;
   }
 
-  shouldEncryptMedia(threadID: ?string): boolean {
-    if (!threadID) {
-      return false;
-    }
-    const threadInfo = this.props.threadStoreThreadInfos[threadID];
+  shouldEncryptMedia(threadInfo: ThreadInfo): boolean {
     return threadInfoInsideCommunity(threadInfo, commStaffCommunity.id);
   }
 
@@ -602,8 +604,8 @@ class InputStateContainer extends React.PureComponent<Props, State> {
           assignedUploads,
           draft: draft ?? '',
           textCursorPosition: textCursorPosition ?? 0,
-          appendFiles: (files: $ReadOnlyArray<File>) =>
-            this.appendFiles(threadID, files),
+          appendFiles: (threadInfo: ThreadInfo, files: $ReadOnlyArray<File>) =>
+            this.appendFiles(threadInfo, files),
           cancelPendingUpload: (localUploadID: string) =>
             this.cancelPendingUpload(threadID, localUploadID),
           sendTextMessage: (
@@ -659,16 +661,14 @@ class InputStateContainer extends React.PureComponent<Props, State> {
   }
 
   async appendFiles(
-    threadID: ?string,
+    threadInfo: ThreadInfo,
     files: $ReadOnlyArray<File>,
   ): Promise<boolean> {
-    invariant(threadID, 'threadID should be set in appendFiles');
-
     const selectionTime = Date.now();
     const { pushModal } = this.props;
 
     const appendResults = await Promise.all(
-      files.map(file => this.appendFile(threadID, file, selectionTime)),
+      files.map(file => this.appendFile(threadInfo, file, selectionTime)),
     );
 
     if (appendResults.some(({ result }) => !result.success)) {
@@ -700,7 +700,7 @@ class InputStateContainer extends React.PureComponent<Props, State> {
     const newUploadsObject = _keyBy('localID')(newUploads);
     this.setState(
       prevState => {
-        const newThreadID = this.getRealizedOrPendingThreadID(threadID);
+        const newThreadID = this.getRealizedOrPendingThreadID(threadInfo.id);
         const prevUploads = prevState.pendingUploads[newThreadID];
         const mergedUploads = prevUploads
           ? { ...prevUploads, ...newUploadsObject }
@@ -712,13 +712,13 @@ class InputStateContainer extends React.PureComponent<Props, State> {
           },
         };
       },
-      () => this.uploadFiles(threadID, newUploads),
+      () => this.uploadFiles(threadInfo.id, newUploads),
     );
     return true;
   }
 
   async appendFile(
-    threadID: ?string,
+    threadInfo: ThreadInfo,
     file: File,
     selectTime: number,
   ): Promise<{
@@ -759,8 +759,10 @@ class InputStateContainer extends React.PureComponent<Props, State> {
     }
     const { uri, file: fixedFile, mediaType, dimensions } = result;
 
+    const shouldEncrypt = this.shouldEncryptMedia(threadInfo);
+
     let encryptionResult;
-    if (this.shouldEncryptMedia(threadID)) {
+    if (shouldEncrypt) {
       let encryptionResponse;
       const encryptionStart = Date.now();
       try {
@@ -813,6 +815,7 @@ class InputStateContainer extends React.PureComponent<Props, State> {
           abort: null,
           steps,
           selectTime,
+          shouldEncrypt,
         },
       },
     };
@@ -1607,9 +1610,6 @@ const ConnectedInputStateContainer: React.ComponentType<BaseProps> =
     const messageStoreMessages = useSelector(
       state => state.messageStore.messages,
     );
-    const threadStoreThreadInfos = useSelector(
-      state => state.threadStore.threadInfos,
-    );
     const pendingToRealizedThreadIDs = useSelector(state =>
       pendingToRealizedThreadIDsSelector(state.threadStore.threadInfos),
     );
@@ -1651,7 +1651,6 @@ const ConnectedInputStateContainer: React.ComponentType<BaseProps> =
         drafts={drafts}
         viewerID={viewerID}
         messageStoreMessages={messageStoreMessages}
-        threadStoreThreadInfos={threadStoreThreadInfos}
         pendingRealizedThreadIDs={pendingToRealizedThreadIDs}
         calendarQuery={calendarQuery}
         uploadMultimedia={callUploadMultimedia}

web/input/input-state.js
@@ -41,6 +41,7 @@ export type PendingMultimediaUpload = {
   +abort: ?() => void,
   +steps: MediaMissionStep[],
   +selectTime: number,
+  +shouldEncrypt: boolean,
 };
 
 export type TypeaheadState = {
@@ -63,7 +64,10 @@ export type InputState = {
   +draft: string,
   +textCursorPosition: number,
   +typeaheadState: TypeaheadState,
-  +appendFiles: (files: $ReadOnlyArray<File>) => Promise<boolean>,
+  +appendFiles: (
+    threadInfo: ThreadInfo,
+    files: $ReadOnlyArray<File>,
+  ) => Promise<boolean>,
   +cancelPendingUpload: (localUploadID: string) => void,
   +sendTextMessage: (
     messageInfo: RawTextMessageInfo,