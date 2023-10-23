a1b4b4885b4f5aec9a30200836904cbc0705fcd6
xsanm <kkurowski.kk@gmail.com>
2023-10-23 14:34:22 +0200

    [Tunnelbroker] add `MessageID` field to `MessageToDevice`
    Summary:
    Clients need to know `messageID` to confirm message receive.
    Keeping old in `commtest` struct to make tests more readable.
    
    Depends on D9571
    
    Test Plan:
    Run all Tunnelbroker tests (But overall this doesn't affect logic).
    
    Reviewers: michal, bartek, varun

--- services/commtest/tests/tunnelbroker_persist_tests.rs
+++ services/commtest/tests/tunnelbroker_persist_tests.rs
@@ -6,15 +6,15 @@ use commtest::identity::olm_account_infos::{
   MOCK_CLIENT_KEYS_1, MOCK_CLIENT_KEYS_2,
 };
 use commtest::service_addr;
-use commtest::tunnelbroker::socket::{create_socket, send_message};
+use commtest::tunnelbroker::socket::{
+  create_socket, send_message, WebSocketMessageToDevice,
+};
 use futures_util::StreamExt;
 use proto::tunnelbroker_service_client::TunnelbrokerServiceClient;
 use proto::MessageToDevice;
 use std::time::Duration;
 use tokio::time::sleep;
-use tunnelbroker_messages::{
-  MessageToDevice as WebSocketMessageToDevice, RefreshKeyRequest,
-};
+use tunnelbroker_messages::RefreshKeyRequest;
 
 /// Tests that a message to an offline device gets pushed to dynamodb
 /// then recalled once a device connects
