3e6b9187505968d00b9b188e3bca1d8b73054e50
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-10-23 02:09:28 -0400

    [native] introduce Add keyserver screen UI
    Summary: This diff introduces and implements the UI for the add keyserver screen
    
    Test Plan: Please see the demo video below
    
    Subscribers: ashoat, tomek, wyilio
    
    Differential Revision: https://phab.comm.dev/D9590

--- native/profile/add-keyserver.react.js
+++ native/profile/add-keyserver.react.js
@@ -1,10 +1,85 @@
 // @flow
 
+import { useNavigation } from '@react-navigation/native';
 import * as React from 'react';
+import { View, Text } from 'react-native';
+
+import TextInput from '../components/text-input.react.js';
+import HeaderRightTextButton from '../navigation/header-right-text-button.react.js';
+import { useStyles, useColors } from '../themes/colors.js';
 
 // eslint-disable-next-line no-unused-vars
 function AddKeyserver(props: { ... }): React.Node {
-  return null;
+  const { setOptions } = useNavigation();
+
+  const { panelForegroundTertiaryLabel } = useColors();
+  const styles = useStyles(unboundStyles);
+
+  const [urlInput, setUrlInput] = React.useState('');
+
+  const onPressSave = React.useCallback(() => {
+    // TODO
+  }, []);
+
+  React.useEffect(() => {
+    setOptions({
+      // eslint-disable-next-line react/display-name
+      headerRight: () => (
+        <HeaderRightTextButton label="Save" onPress={onPressSave} />
+      ),
+    });
+  }, [onPressSave, setOptions, styles.header]);
+
+  const onChangeText = React.useCallback(
+    (text: string) => setUrlInput(text),
+    [],
+  );
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>KEYSERVER URL</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          value={urlInput}
+          onChangeText={onChangeText}
+          placeholder="Keyserver URL"
+          placeholderTextColor={panelForegroundTertiaryLabel}
+          autoFocus={true}
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+      </View>
+    </View>
+  );
 }
+
+const unboundStyles = {
+  container: {
+    paddingTop: 8,
+  },
+  header: {
+    color: 'panelBackgroundLabel',
+    fontSize: 12,
+    fontWeight: '400',
+    paddingBottom: 3,
+    paddingHorizontal: 24,
+  },
+  inputContainer: {
+    backgroundColor: 'panelForeground',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 24,
+    paddingVertical: 13,
+  },
+  input: {
+    color: 'panelForegroundLabel',
+    flex: 1,
+    fontFamily: 'Arial',
+    fontSize: 16,
+    paddingVertical: 0,
+    borderBottomColor: 'transparent',
+  },
+};
 
 export default AddKeyserver;
