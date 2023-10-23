3c42ecffc737abffe1e765c7b1617d51b39b70c5
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-10-23 14:12:58 -0400

    [web] update AppearanceChangeModal buttons to use redux state
    Summary:
    Right now the updating the theme state is just localized to a `useState` in `AppearanceChangeModal`. Now that we have `globalThemeInfo` in our redux state we should update and use that instead.
    
    Part of https://linear.app/comm/issue/ENG-4958/update-theme-based-on-the-active-theme-redux-state
    
    Depends on D9553
    
    Test Plan:
    Please see the demo video below
    
    {F827012}
    
    Reviewers: atul, inka, rohan
    
    Reviewed By: atul
    
    Subscribers: ashoat, tomek, wyilio
    
    Differential Revision: https://phab.comm.dev/D9554

--- web/settings/appearance-change-modal.react.js
+++ web/settings/appearance-change-modal.react.js
@@ -4,56 +4,70 @@ import classNames from 'classnames';
 import * as React from 'react';
 
 import { useModalContext } from 'lib/components/modal-provider.react.js';
+import { useUpdateThemePreference } from 'lib/hooks/theme.js';
 
 import css from './appearance-change-modal.css';
 import Modal from '../modals/modal.react.js';
+import { useSelector } from '../redux/redux-utils.js';
 
 function AppearanceChangeModal(): React.Node {
+  const globalThemeInfo = useSelector(state => state.globalThemeInfo);
+  const updateThemePreference = useUpdateThemePreference();
+
   const { popModal } = useModalContext();
 
-  const [selectedMode, setSelectedMode] = React.useState('dark');
-
   const onClickLightModeButton = React.useCallback(
-    () => setSelectedMode('light'),
-    [],
+    () => updateThemePreference('light'),
+    [updateThemePreference],
   );
 
   const onClickDarkModeButton = React.useCallback(
-    () => setSelectedMode('dark'),
-    [],
+    () => updateThemePreference('dark'),
+    [updateThemePreference],
   );
 
   const lightModeButtonClassName = classNames({
     [css.button]: true,
-    [css.selectedButton]: selectedMode === 'light',
+    [css.selectedButton]: globalThemeInfo.preference === 'light',
   });
 
   const darkModeButtonClassName = classNames({
     [css.button]: true,
-    [css.selectedButton]: selectedMode === 'dark',
+    [css.selectedButton]: globalThemeInfo.preference === 'dark',
   });
 
-  return (
-    <Modal name="Appearance" onClose={popModal}>
-      <div className={css.container}>
-        <h1 className={css.header}>App theme</h1>
-        <div className={css.buttonContainer}>
-          <div
-            className={lightModeButtonClassName}
-            onClick={onClickLightModeButton}
-          >
-            Light
-          </div>
-          <div
-            className={darkModeButtonClassName}
-            onClick={onClickDarkModeButton}
-          >
-            Dark
+  const appearanceChangeModal = React.useMemo(
+    () => (
+      <Modal name="Appearance" onClose={popModal}>
+        <div className={css.container}>
+          <h1 className={css.header}>App theme</h1>
+          <div className={css.buttonContainer}>
+            <div
+              className={lightModeButtonClassName}
+              onClick={onClickLightModeButton}
+            >
+              Light
+            </div>
+            <div
+              className={darkModeButtonClassName}
+              onClick={onClickDarkModeButton}
+            >
+              Dark
+            </div>
           </div>
         </div>
-      </div>
-    </Modal>
+      </Modal>
+    ),
+    [
+      darkModeButtonClassName,
+      lightModeButtonClassName,
+      onClickDarkModeButton,
+      onClickLightModeButton,
+      popModal,
+    ],
   );
+
+  return appearanceChangeModal;
 }
 
 export default AppearanceChangeModal;
