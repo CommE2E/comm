9f292232c4d8bb78ede92a404476b06bee35d524
xsanm <kkurowski.kk@gmail.com>
2023-10-23 14:34:22 +0200

    [Tunnelbroker] add `MessageID` field to `MessageToDevice`
    Summary:
    Clients need to know `messageID` to confirm message receive.
    Keeping old in `commtest` struct to make tests more readable.
    
    Depends on D9571
    
    Test Plan: Run all Tunnelbroker tests (But overall this doesn't affect logic).
    
    Reviewers: michal, bartek, varun, jon
    
    Subscribers: ashoat, tomek, wyilio
    
    Differential Revision: https://phab.comm.dev/D9572

--- shared/tunnelbroker_messages/src/messages/message_to_device.rs
+++ shared/tunnelbroker_messages/src/messages/message_to_device.rs
@@ -8,6 +8,8 @@ pub struct MessageToDevice {
   #[serde(rename = "deviceID")]
   pub device_id: String,
   pub payload: String,
+  #[serde(rename = "messageID")]
+  pub message_id: String,
 }
 
 #[cfg(test)]
@@ -19,12 +21,14 @@ mod message_to_device_tests {
     let example_payload = r#"{
       "type": "MessageToDevice",
       "deviceID": "alice",
-      "payload": "message from Bob"
+      "payload": "message from Bob",
+      "messageID": "id234"
     }"#;
 
     let request =
       serde_json::from_str::<MessageToDevice>(example_payload).unwrap();
     assert_eq!(request.device_id, "alice");
     assert_eq!(request.payload, "message from Bob");
+    assert_eq!(request.message_id, "id234");
   }
 }
