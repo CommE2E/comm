a5d69348e54dfe77cddb07d0d64ca2fe867876ec
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-10-24 16:30:32 -0400

    [native] introduce barebones keyserver selection list screen
    Summary:
    This diff introduces the barebones keyserver selection list screen and all the navigation logic to navigate from the profile screen to this new screen
    
    Linear Task: https://linear.app/comm/issue/ENG-4911/introduce-keyserver-selection-screen-into-our-profile-app-navigation
    
    Depends on D9565
    
    Test Plan:
    Please see the demo video below
    
    {F834123}
    
    Reviewers: rohan, inka, michal
    
    Reviewed By: rohan
    
    Subscribers: ashoat, tomek, wyilio
    
    Differential Revision: https://phab.comm.dev/D9566

---
+++ native/profile/keyserver-selection-list.react.js
@@ -0,0 +1,10 @@
+// @flow
+
+import * as React from 'react';
+
+// eslint-disable-next-line no-unused-vars
+function KeyserverSelectionList(props: { ... }): React.Node {
+  return null;
+}
+
+export default KeyserverSelectionList;
