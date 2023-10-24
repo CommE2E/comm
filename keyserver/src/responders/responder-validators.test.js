ab6b2a27f29b4386a30117b7cd66c7985badc178
Atul Madhugiri <atulm@berkeley.edu>
2023-10-24 18:04:07 -0400

    Revert "[lib] Introduce basic `permissionsToBitmask` and `hasPermission`"
    This reverts commit c307122b3a4040938ee87cb430441530d002f674.

--- keyserver/src/responders/responder-validators.test.js
+++ keyserver/src/responders/responder-validators.test.js
@@ -96,6 +96,7 @@ describe('user responder validators', () => {
                 role: '83796',
                 permissions: {
                   know_of: { value: true, source: '1' },
+                  membership: { value: false, source: null },
                   visible: { value: true, source: '1' },
                   voiced: { value: true, source: '1' },
                   edit_entries: { value: true, source: '1' },
@@ -135,6 +136,7 @@ describe('user responder validators', () => {
               role: '83795',
               permissions: {
                 know_of: { value: true, source: '1' },
+                membership: { value: false, source: null },
                 visible: { value: true, source: '1' },
                 voiced: { value: false, source: null },
                 edit_entries: { value: false, source: null },
@@ -225,6 +227,7 @@ describe('user responder validators', () => {
                 role: '83796',
                 permissions: {
                   know_of: { value: true, source: '1' },
+                  membership: { value: false, source: null },
                   visible: { value: true, source: '1' },
                   voiced: { value: true, source: '1' },
                   edit_entries: { value: true, source: '1' },
@@ -250,6 +253,7 @@ describe('user responder validators', () => {
                 role: '83795',
                 permissions: {
                   know_of: { value: true, source: '1' },
+                  membership: { value: false, source: null },
                   visible: { value: true, source: '1' },
                   voiced: { value: false, source: null },
                   edit_entries: { value: false, source: null },
@@ -327,6 +331,7 @@ describe('user responder validators', () => {
               role: '83795',
               permissions: {
                 know_of: { value: true, source: '1' },
+                membership: { value: false, source: null },
                 visible: { value: true, source: '1' },
                 voiced: { value: false, source: null },
                 edit_entries: { value: false, source: null },
