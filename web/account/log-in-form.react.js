6c6b888c5bfdf9af77aff33d1d61ae8dcbb894eb
marcinwasowicz <marcinkwasowicz@gmail.com>
2023-10-25 18:18:08 +0200

    Refactor olm accounts lifecycle on web
    Summary:
    This differential does not introduce new functionality but refactors the code so that we have utilities to ensure that olm data (accounts, keys and sessions) are always there and we don't have to make olm-related function arguments optional. In particular this differential addresses the issue I pointed out in this comment: https://linear.app/comm/issue/ENG-5236/olm-accounts-on-web-not-created-anywhere-outside-of-loginform#comment-7a85dfed.
    
    On one hand this differential indeed introduces significant changes to the code. On the other I am not sure how severe is the issue pointed in the link above and how important this differential actually is. That said if there is noticeable objection from reviewers I am willing to drop this differential as introducing e2e notifs for web is technically possible without it. The only issue is that we might not be able to initialize olm sessions for users that have never logged out from web since February/March 2023 (at that time we introduced olm data in web).
    
    Test Plan:
    1. Log out and log in on web. Ensure (using Redux in Developer Tools) that new olm accounts are dispatched in redux.
    2. Using mariadb console set `signed_identity_keys` field in cookies table to NULL for the cookie associated with currently logged-in user on you r browser.
    3. Reload web app. Using mariadb console ensure that  `signed_identity_keys` field in cookies table to NULL for the cookie associated with currently logged-in user on you r browser is set to a proper JSON again.
    
    Steps above ensure that already existing functionality relying on olm data wasn't broken by current changes.
    
    Reviewers: atul, kamil, michal, ashoat
    
    Reviewed By: ashoat
    
    Subscribers: ashoat, tomek, wyilio
    
    Differential Revision: https://phab.comm.dev/D9532

--- web/account/log-in-form.react.js
+++ web/account/log-in-form.react.js
@@ -1,84 +1,33 @@
 // @flow
 
-import olm from '@commapp/olm';
 import { useConnectModal } from '@rainbow-me/rainbowkit';
 import * as React from 'react';
 import { useDispatch } from 'react-redux';
-import uuid from 'uuid';
 import { useWalletClient } from 'wagmi';
 
 import { isDev } from 'lib/utils/dev-utils.js';
 
+import { useGetOrCreateCryptoStore } from './account-hooks.js';
 import css from './log-in-form.css';
 import SIWEButton from './siwe-button.react.js';
 import SIWELoginForm from './siwe-login-form.react.js';
 import TraditionalLoginForm from './traditional-login-form.react.js';
 import Button from '../components/button.react.js';
 import OrBreak from '../components/or-break.react.js';
-import { initOlm } from '../olm/olm-utils.js';
 import { updateNavInfoActionType } from '../redux/action-types.js';
-import { setCryptoStore } from '../redux/crypto-store-reducer.js';
-import { useSelector } from '../redux/redux-utils.js';
 
 function LoginForm(): React.Node {
   const { openConnectModal } = useConnectModal();
   const { data: signer } = useWalletClient();
   const dispatch = useDispatch();
 
-  const cryptoStore = useSelector(state => state.cryptoStore);
+  const getOrCreateCryptoStore = useGetOrCreateCryptoStore();
 
   React.useEffect(() => {
     (async () => {
-      if (cryptoStore !== null && cryptoStore !== undefined) {
-        return;
-      }
-      await initOlm();
-
-      const identityAccount = new olm.Account();
-      identityAccount.create();
-      const { ed25519: identityED25519, curve25519: identityCurve25519 } =
-        JSON.parse(identityAccount.identity_keys());
-
-      const identityAccountPicklingKey = uuid.v4();
-      const pickledIdentityAccount = identityAccount.pickle(
-        identityAccountPicklingKey,
-      );
-
-      const notificationAccount = new olm.Account();
-      notificationAccount.create();
-      const {
-        ed25519: notificationED25519,
-        curve25519: notificationCurve25519,
-      } = JSON.parse(notificationAccount.identity_keys());
-
-      const notificationAccountPicklingKey = uuid.v4();
-      const pickledNotificationAccount = notificationAccount.pickle(
-        notificationAccountPicklingKey,
-      );
-
-      dispatch({
-        type: setCryptoStore,
-        payload: {
-          primaryAccount: {
-            picklingKey: identityAccountPicklingKey,
-            pickledAccount: pickledIdentityAccount,
-          },
-          primaryIdentityKeys: {
-            ed25519: identityED25519,
-            curve25519: identityCurve25519,
-          },
-          notificationAccount: {
-            picklingKey: notificationAccountPicklingKey,
-            pickledAccount: pickledNotificationAccount,
-          },
-          notificationIdentityKeys: {
-            ed25519: notificationED25519,
-            curve25519: notificationCurve25519,
-          },
-        },
-      });
+      await getOrCreateCryptoStore();
     })();
-  }, [dispatch, cryptoStore]);
+  }, [getOrCreateCryptoStore]);
 
   const onQRCodeLoginButtonClick = React.useCallback(() => {
     dispatch({
