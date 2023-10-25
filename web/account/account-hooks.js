44afefd5e2a239a8c180bbc1df3a3fd93c248ef2
marcinwasowicz <marcinkwasowicz@gmail.com>
2023-10-25 18:18:08 +0200

    Refactor olm accounts lifecycle on web
    Summary:
    This differential does not introduce new functionality but refactors the code so that we have utilities to ensure that olm data (accounts, keys and sessions) are always there and we don't have to make olm-related function arguments optional. In particular this differential addresses the issue I pointed out in this comment: https://linear.app/comm/issue/ENG-5236/olm-accounts-on-web-not-created-anywhere-outside-of-loginform#comment-7a85dfed.
    
    On one hand this differential indeed introduces significant changes to the code. On the other I am not sure how severe is the issue pointed in the link above and how important this differential actually is. That said if there is noticeable objection from reviewers I am willing to drop this differential as introducing e2e notifs for web is technically possible without it. The only issue is that we might not be able to initialize olm sessions for users that have never logged out from web since February/March 2023 (at that time we introduced olm data in web).
    
    Test Plan:
    1. Log out and log in on web. Ensure (using Redux in Developer Tools) that new olm accounts are dispatched in redux.
    2. Using mariadb console set `signed_identity_keys` field in cookies table to NULL for the cookie associated with currently logged-in user on you r browser.
    3. Reload web app. Using mariadb console ensure that  `signed_identity_keys` field in cookies table to NULL for the cookie associated with currently logged-in user on you r browser is set to a proper JSON again.
    
    Steps above ensure that already existing functionality relying on olm data wasn't broken by current changes.
    
    Reviewers: atul, kamil, michal, ashoat
    
    Reviewed By: ashoat
    
    Subscribers: ashoat, tomek, wyilio
    
    Differential Revision: https://phab.comm.dev/D9532

--- web/account/account-hooks.js
+++ web/account/account-hooks.js
@@ -1,35 +1,161 @@
 // @flow
 
+import olm from '@commapp/olm';
+import invariant from 'invariant';
 import * as React from 'react';
+import { useDispatch } from 'react-redux';
+import uuid from 'uuid';
 
-import type { SignedIdentityKeysBlob } from 'lib/types/crypto-types.js';
+import type {
+  SignedIdentityKeysBlob,
+  CryptoStore,
+  IdentityKeysBlob,
+  CryptoStoreContextType,
+} from 'lib/types/crypto-types.js';
 
+import { initOlm } from '../olm/olm-utils.js';
+import { setCryptoStore } from '../redux/crypto-store-reducer.js';
 import { useSelector } from '../redux/redux-utils.js';
-import { getSignedIdentityKeysBlobSelector } from '../selectors/socket-selectors.js';
 
-function useSignedIdentityKeysBlob(): ?SignedIdentityKeysBlob {
-  const getSignedIdentityKeysBlob: ?() => Promise<SignedIdentityKeysBlob> =
-    useSelector(getSignedIdentityKeysBlobSelector);
+const CryptoStoreContext: React.Context<?CryptoStoreContextType> =
+  React.createContext(null);
 
-  const [signedIdentityKeysBlob, setSignedIdentityKeysBlob] =
-    React.useState<?SignedIdentityKeysBlob>(null);
+type Props = {
+  +children: React.Node,
+};
 
-  React.useEffect(() => {
-    (async () => {
-      if (
-        getSignedIdentityKeysBlob === null ||
-        getSignedIdentityKeysBlob === undefined
-      ) {
-        setSignedIdentityKeysBlob(null);
-        return;
+function GetOrCreateCryptoStoreProvider(props: Props): React.Node {
+  const dispatch = useDispatch();
+  const createCryptoStore = React.useCallback(async () => {
+    await initOlm();
+
+    const identityAccount = new olm.Account();
+    identityAccount.create();
+    const { ed25519: identityED25519, curve25519: identityCurve25519 } =
+      JSON.parse(identityAccount.identity_keys());
+
+    const identityAccountPicklingKey = uuid.v4();
+    const pickledIdentityAccount = identityAccount.pickle(
+      identityAccountPicklingKey,
+    );
+
+    const notificationAccount = new olm.Account();
+    notificationAccount.create();
+    const { ed25519: notificationED25519, curve25519: notificationCurve25519 } =
+      JSON.parse(notificationAccount.identity_keys());
+
+    const notificationAccountPicklingKey = uuid.v4();
+    const pickledNotificationAccount = notificationAccount.pickle(
+      notificationAccountPicklingKey,
+    );
+
+    const newCryptoStore = {
+      primaryAccount: {
+        picklingKey: identityAccountPicklingKey,
+        pickledAccount: pickledIdentityAccount,
+      },
+      primaryIdentityKeys: {
+        ed25519: identityED25519,
+        curve25519: identityCurve25519,
+      },
+      notificationAccount: {
+        picklingKey: notificationAccountPicklingKey,
+        pickledAccount: pickledNotificationAccount,
+      },
+      notificationIdentityKeys: {
+        ed25519: notificationED25519,
+        curve25519: notificationCurve25519,
+      },
+    };
+
+    dispatch({ type: setCryptoStore, payload: newCryptoStore });
+    return newCryptoStore;
+  }, [dispatch]);
+
+  const currentCryptoStore = useSelector(state => state.cryptoStore);
+  const createCryptoStorePromiseRef = React.useRef<?Promise<CryptoStore>>(null);
+  const getCryptoStorePromise = React.useCallback(() => {
+    if (currentCryptoStore) {
+      return Promise.resolve(currentCryptoStore);
+    }
+
+    const currentCreateCryptoStorePromiseRef =
+      createCryptoStorePromiseRef.current;
+    if (currentCreateCryptoStorePromiseRef) {
+      return currentCreateCryptoStorePromiseRef;
+    }
+
+    const newCreateCryptoStorePromise = (async () => {
+      try {
+        return await createCryptoStore();
+      } catch (e) {
+        createCryptoStorePromiseRef.current = undefined;
+        throw e;
       }
-      const resolvedSignedIdentityKeysBlob: SignedIdentityKeysBlob =
-        await getSignedIdentityKeysBlob();
-      setSignedIdentityKeysBlob(resolvedSignedIdentityKeysBlob);
     })();
-  }, [getSignedIdentityKeysBlob]);
 
-  return signedIdentityKeysBlob;
+    createCryptoStorePromiseRef.current = newCreateCryptoStorePromise;
+    return newCreateCryptoStorePromise;
+  }, [createCryptoStore, currentCryptoStore]);
+
+  const isCryptoStoreSet = !!currentCryptoStore;
+  React.useEffect(() => {
+    if (!isCryptoStoreSet) {
+      createCryptoStorePromiseRef.current = undefined;
+    }
+  }, [isCryptoStoreSet]);
+
+  const contextValue = React.useMemo(
+    () => ({
+      getInitializedCryptoStore: getCryptoStorePromise,
+    }),
+    [getCryptoStorePromise],
+  );
+
+  return (
+    <CryptoStoreContext.Provider value={contextValue}>
+      {props.children}
+    </CryptoStoreContext.Provider>
+  );
 }
 
-export { useSignedIdentityKeysBlob };
+function useGetOrCreateCryptoStore(): () => Promise<CryptoStore> {
+  const context = React.useContext(CryptoStoreContext);
+  invariant(context, 'CryptoStoreContext not found');
+  return context.getInitializedCryptoStore;
+}
+
+function useGetSignedIdentityKeysBlob(): () => Promise<SignedIdentityKeysBlob> {
+  const getOrCreateCryptoStore = useGetOrCreateCryptoStore();
+
+  return React.useCallback(async () => {
+    const { primaryAccount, primaryIdentityKeys, notificationIdentityKeys } =
+      await getOrCreateCryptoStore();
+
+    await initOlm();
+    const primaryOLMAccount = new olm.Account();
+    primaryOLMAccount.unpickle(
+      primaryAccount.picklingKey,
+      primaryAccount.pickledAccount,
+    );
+
+    const identityKeysBlob: IdentityKeysBlob = {
+      primaryIdentityPublicKeys: primaryIdentityKeys,
+      notificationIdentityPublicKeys: notificationIdentityKeys,
+    };
+
+    const payloadToBeSigned: string = JSON.stringify(identityKeysBlob);
+    const signedIdentityKeysBlob: SignedIdentityKeysBlob = {
+      payload: payloadToBeSigned,
+      signature: primaryOLMAccount.sign(payloadToBeSigned),
+    };
+
+    return signedIdentityKeysBlob;
+  }, [getOrCreateCryptoStore]);
+}
+
+export {
+  useGetSignedIdentityKeysBlob,
+  useGetOrCreateCryptoStore,
+  GetOrCreateCryptoStoreProvider,
+};
