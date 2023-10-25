2c269eb010133d88725c0d3d7e685a6d0aee6b4a
xsanm <kkurowski.kk@gmail.com>
2023-10-25 12:39:05 +0200

    [Tunnelbroker] refactor initializing session to make it possible to send a response to the client on session creation failure
    Summary:
    This refactor is to make D9595 possible.
    The intention is to fix `outgoing`'s ownership issue when session creation fails (`send` forces `outgoing` to be mutable).
    Doesn't look like a very optimal pattern, but not sure if this can be improved are is there a more graceful way to handle this - if anyone has any suggestions I am happy to change this :smile:
    
    Creating separate diff to make it easier for reviewers, actual usage is in D9595.
    
    Depends on D9593
    
    Test Plan: Tunnelbroker tests (most important are `test_tunnelbroker_invalid_auth` and `test_tunnelbroker_valid_auth`).
    
    Reviewers: michal, bartek, varun, jon
    
    Reviewed By: michal, bartek
    
    Subscribers: ashoat, tomek, wyilio
    
    Differential Revision: https://phab.comm.dev/D9594

--- services/tunnelbroker/src/websockets/mod.rs
+++ services/tunnelbroker/src/websockets/mod.rs
@@ -1,7 +1,7 @@
 pub mod session;
 
 use crate::database::DatabaseClient;
-use crate::websockets::session::SessionError;
+use crate::websockets::session::initialize_amqp;
 use crate::CONFIG;
 use futures_util::stream::SplitSink;
 use futures_util::StreamExt;
@@ -19,6 +19,11 @@ use tracing::{debug, error, info};
 use tunnelbroker_messages::{MessageSentStatus, MessageToDeviceRequestStatus};
 
 type BoxedError = Box<dyn std::error::Error + Send + Sync + 'static>;
+
+pub type ErrorWithStreamHandle<S> = (
+  session::SessionError,
+  SplitSink<WebSocketStream<S>, Message>,
+);
 
 use self::session::WebsocketSession;
 
@@ -234,18 +239,18 @@ async fn initiate_session<S: AsyncRead + AsyncWrite + Unpin>(
   frame: Message,
   db_client: DatabaseClient,
   amqp_channel: lapin::Channel,
-) -> Result<WebsocketSession<S>, session::SessionError> {
-  let session = session::WebsocketSession::from_frame(
-    outgoing,
-    db_client.clone(),
-    frame,
-    &amqp_channel,
-  )
-  .await
-  .map_err(|_| {
-    error!("Device failed to send valid connection request.");
-    SessionError::InvalidMessage
-  })?;
+) -> Result<WebsocketSession<S>, ErrorWithStreamHandle<S>> {
+  let initialized_session =
+    initialize_amqp(db_client.clone(), frame, &amqp_channel).await;
 
-  Ok(session)
+  match initialized_session {
+    Ok((device_info, amqp_consumer)) => Ok(WebsocketSession::new(
+      outgoing,
+      db_client,
+      device_info,
+      amqp_channel,
+      amqp_consumer,
+    )),
+    Err(e) => Err((e, outgoing)),
+  }
 }
