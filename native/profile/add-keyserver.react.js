d979f2ce4b1ec0c5e8befc25cac9de0c4b3ffbe8
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-10-26 16:12:02 -0400

    [native] introduce KeyserverSelectionListAddButton
    Summary:
    This diff introduces the `KeyserverSelectionListAddButton` and all the necessary navigation logic to navigate to the add keyserver input screen from the keyserver selection list screen
    
    Linear task: https://linear.app/comm/issue/ENG-5408/introduce-keyserver-selection-list-header-right-button
    
    Depends on D9586
    
    Test Plan:
    Please see the demo video below
    
    {F835908}
    
    Reviewers: inka, rohan, michal
    
    Reviewed By: rohan
    
    Subscribers: ashoat, tomek, wyilio
    
    Differential Revision: https://phab.comm.dev/D9587

---
+++ native/profile/add-keyserver.react.js
@@ -0,0 +1,10 @@
+// @flow
+
+import * as React from 'react';
+
+// eslint-disable-next-line no-unused-vars
+function AddKeyserver(props: { ... }): React.Node {
+  return null;
+}
+
+export default AddKeyserver;
