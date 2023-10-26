2fd6845906098cca09a1d058fe50cee8a998078f
Rohan Khanderia <rohan.khanderia@gmail.com>
2023-10-26 11:55:03 -0400

    [native] Redux persist migration to remove descendant_open_voiced from threads
    Summary:
    Although we handled the keyserver DB in D9599, it'll be good to perform a 'synced migration' where we also update the client's threads to prevent an inconsistency report. This diff runs a persist migrtion on `native` to handle this. The expected outcome is the client's threads no longer have this permission.
    
    Followed migration 36 quite closely in terms of handling client DB threads
    
    Depends on D9599
    
    Resolves https://linear.app/comm/issue/ENG-5254/add-a-migration-to-fix-existing-communities-that-have-this-permission
    
    Test Plan:
    Performed two different tests:
    
    1. Manually tested the code that updated `threadInfos`:
    ```
    const threadStore = { ... } // threadStore contents here
    
    const updatedThreadInfos = {};
    const threadInfos = threadStore.threadInfos;
    for (const threadID in threadInfos) {
      const threadInfo = threadInfos[threadID];
      const { roles } = threadInfo;
    
      const updatedRoles = {};
      for (const roleID in roles) {
        const role = roles[roleID];
        const { permissions: rolePermissions } = role;
        const updatedPermissions = {};
        for (const permission in rolePermissions) {
            if (permission !== 'descendant_open_voiced') {
                updatedPermissions[permission] = rolePermissions[permission];
            }
        }
        updatedRoles[roleID] = { ...role, permissions: updatedPermissions };
      }
    
      const updatedThreadInfo = {
        ...threadInfo,
        roles: updatedRoles,
      };
    
      // Nothing should log here (testing: nothing logs)
      for (const roleID in updatedRoles) {
        const role = updatedRoles[roleID];
        const { permissions: rolePermissions } = role;
        for (const permission in rolePermissions) {
          if (permission === 'descendant_open_voiced') {
            console.log('descendant_open_voiced', roleID);
          }
        }
      }
    
      updatedThreadInfos[threadID] = updatedThreadInfo;
    }
    ```
    
    2. Confirmed the results of the migration itself:
    - Open `native` and run `yarn redux-devtools` to inspect `threadStore`
    - Verified that some roles had the `descendant_open_voiced` permission
    - Upgraded the persist version to trigger the migration
    - Confirmed that the migration succeeded
    - Checked the redux devtools for the same roles and confirmed that there was one less permission (introspecting into the permissions confirmed that `descendant_open_voiced` was removed
    - Waited a few minutes and confirmed that the client did not detect any inconsistencies
    
    Reviewers: atul!, ginsu
    
    Subscribers: ashoat, tomek, wyilio
    
    Differential Revision: https://phab.comm.dev/D9608

---
+++ native/redux/remove-select-role-permissions.test.js
@@ -0,0 +1,33 @@
+// @flow
+import type { RawThreadInfos } from 'lib/types/thread-types.js';
+import { deepDiff } from 'lib/utils/objects.js';
+
+import { persistMigrationToRemoveDescendantOpenVoiced } from './remove-select-role-permissions.js';
+import { threadStoreThreadsWithDescendantOpenVoiced } from './update-roles-and-permissions-test-data.js';
+
+describe('persistMigrationToRemoveDescendantOpenVoiced', () => {
+  it("should correctly remove 'descendant_open_voiced' from permissions", () => {
+    const migratedRawThreadInfos: RawThreadInfos =
+      persistMigrationToRemoveDescendantOpenVoiced(
+        threadStoreThreadsWithDescendantOpenVoiced,
+      );
+
+    // Only role 256|85027 in thread 256|85022 has
+    // 'descendant_open_voiced' permissions included
+    const threadDiff = deepDiff(
+      threadStoreThreadsWithDescendantOpenVoiced,
+      migratedRawThreadInfos,
+    );
+    expect(threadDiff).toStrictEqual({
+      '256|85022': {
+        roles: {
+          '256|85027': {
+            permissions: {
+              descendant_open_voiced: true,
+            },
+          },
+        },
+      },
+    });
+  });
+});
