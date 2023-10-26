297e5ed63235d27f4ed2dbee8a800fa684245bc2
xsanm <kkurowski.kk@gmail.com>
2023-10-26 19:01:15 +0200

    [web] implement screen for testing Tunnelbroker
    Summary:
    This is a screen for testing Tunnelbroker utilities. It might be helpful for testing on production.
    
    Styles/components are mostly copied from other places in the codebase which visually look similar. This is only for devs/staff and will be removed in the future.
    
    Depends on D9616
    
    Test Plan: See example [here](https://linear.app/comm/issue/ENG-5050/test-sending-messages-between-clients#comment-75a6e65d).
    
    Reviewers: michal, bartek, tomek, inka
    
    Reviewed By: michal
    
    Subscribers: ashoat, wyilio
    
    Differential Revision: https://phab.comm.dev/D9617

---
+++ web/settings/tunnelbroker-message-list.react.js
@@ -0,0 +1,50 @@
+// @flow
+
+import * as React from 'react';
+
+import type { TunnelbrokerSocketListener } from 'lib/tunnelbroker/tunnelbroker-context.js';
+import type { TunnelbrokerMessage } from 'lib/types/tunnelbroker/messages.js';
+
+import css from './tunnelbroker-message-list.css';
+import Modal from '../modals/modal.react.js';
+
+type Props = {
+  +addListener: (listener: TunnelbrokerSocketListener) => void,
+  +removeListener: (listener: TunnelbrokerSocketListener) => void,
+  +onClose: () => void,
+};
+
+function TunnelbrokerMessagesScreen(props: Props): React.Node {
+  const { addListener, onClose, removeListener } = props;
+  const [messages, setMessages] = React.useState<TunnelbrokerMessage[]>([]);
+
+  const listener = React.useCallback((msg: TunnelbrokerMessage) => {
+    setMessages(prev => [...prev, msg]);
+  }, []);
+
+  React.useEffect(() => {
+    addListener(listener);
+    return () => removeListener(listener);
+  }, [addListener, listener, removeListener]);
+
+  let messageList = (
+    <div className={css.messageRow}>
+      <div className={css.messageCol}>No messages</div>
+    </div>
+  );
+  if (messages.length) {
+    messageList = messages.map(message => (
+      <div key={message.messageID} className={css.messageRow}>
+        <div className={css.messageCol}>{JSON.stringify(message)}</div>
+      </div>
+    ));
+  }
+
+  return (
+    <Modal name="Tunnelbroker Messages" onClose={onClose} size="large">
+      <div className={css.messageList}>{messageList}</div>
+    </Modal>
+  );
+}
+
+export default TunnelbrokerMessagesScreen;
