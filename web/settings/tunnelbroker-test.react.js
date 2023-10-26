194c12faa0d568238332a3ad0e5b73e9db084b6d
xsanm <kkurowski.kk@gmail.com>
2023-10-26 19:01:15 +0200

    [web] implement screen for testing Tunnelbroker
    Summary:
    This is a screen for testing Tunnelbroker utilities. It might be helpful for testing on production.
    
    Styles/components are mostly copied from other places in the codebase which visually look similar. This is only for devs/staff and will be removed in the future.
    
    Depends on D9616
    
    Test Plan: See example [here](https://linear.app/comm/issue/ENG-5050/test-sending-messages-between-clients#comment-75a6e65d).
    
    Reviewers: michal, bartek, tomek
    
    Subscribers: ashoat, wyilio
    
    Differential Revision: https://phab.comm.dev/D9617

---
+++ web/settings/tunnelbroker-test.react.js
@@ -0,0 +1,94 @@
+// @flow
+
+import invariant from 'invariant';
+import * as React from 'react';
+
+import { type ClientMessageToDevice } from 'lib/tunnelbroker/tunnelbroker-context.js';
+
+import css from './tunnelbroker-test.css';
+import Button from '../components/button.react.js';
+import Input from '../modals/input.react.js';
+import Modal from '../modals/modal.react.js';
+
+type Props = {
+  +sendMessage: (message: ClientMessageToDevice) => Promise<void>,
+  +onClose: () => void,
+};
+
+function TunnelbrokerTestScreen(props: Props): React.Node {
+  const { sendMessage, onClose } = props;
+  const [recipient, setRecipient] = React.useState('');
+  const [message, setMessage] = React.useState('');
+  const [loading, setLoading] = React.useState(false);
+  const [errorMessage, setErrorMessage] = React.useState('');
+  const recipientInput = React.useRef(null);
+  const messageInput = React.useRef(null);
+
+  const onSubmit = React.useCallback(
+    async event => {
+      event.preventDefault();
+
+      setLoading(true);
+      try {
+        await sendMessage({ deviceID: recipient, payload: message });
+      } catch (e) {
+        setErrorMessage(e.message);
+      }
+      setLoading(false);
+    },
+    [message, recipient, sendMessage],
+  );
+
+  let errorMsg;
+  if (errorMessage) {
+    errorMsg = <div className={css.modalError}>{errorMessage}</div>;
+  }
+
+  return (
+    <Modal name="Send Tunnelbroker Message" onClose={onClose} size="large">
+      <div className={css.modalBody}>
+        <div className={css.content}>
+          <Input
+            type="text"
+            placeholder="deviceID"
+            value={recipient}
+            onChange={(event: SyntheticEvent<HTMLInputElement>) => {
+              const target = event.target;
+              invariant(target instanceof HTMLInputElement, 'target not input');
+              setRecipient(target.value);
+            }}
+            disabled={loading}
+            ref={recipientInput}
+            label="Recipient"
+          />
+          <Input
+            type="text"
+            placeholder="payload"
+            value={message}
+            onChange={(event: SyntheticEvent<HTMLInputElement>) => {
+              const target = event.target;
+              invariant(target instanceof HTMLInputElement, 'target not input');
+              setMessage(target.value);
+            }}
+            disabled={loading}
+            ref={messageInput}
+            label="Message"
+          />
+        </div>
+        <div className={css.footer}>
+          <Button
+            type="submit"
+            variant="filled"
+            onClick={onSubmit}
+            disabled={!recipient || !message || loading}
+          >
+            Send Message
+          </Button>
+          {errorMsg}
+        </div>
+      </div>
+    </Modal>
+  );
+}
+
+export default TunnelbrokerTestScreen;
