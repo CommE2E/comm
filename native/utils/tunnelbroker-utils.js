281926b3e62eddd8d95198e0b8a58a295d0e23d9
xsanm <kkurowski.kk@gmail.com>
2023-10-26 18:13:41 +0200

    [native] create Tunnelbroker session
    Summary:
    Create a diff that gathers the necessary data to establish a proper socket connection and begins to use the context.
    
    Depends on D9609
    
    Test Plan: Use mock credentials (credentials that I manually added to Identity as a valid user) and verify if a session was created.
    
    Reviewers: michal, bartek, tomek
    
    Subscribers: ashoat, wyilio
    
    Differential Revision: https://phab.comm.dev/D9610

---
+++ native/utils/tunnelbroker-utils.js
@@ -0,0 +1,36 @@
+// @flow
+
+import React from 'react';
+
+import type { ConnectionInitializationMessage } from 'lib/types/tunnelbroker/session-types.js';
+
+import { getContentSigningKey } from './crypto-utils.js';
+import { useSelector } from '../redux/redux-utils.js';
+
+function useTunnelbrokerInitMessage(): ?ConnectionInitializationMessage {
+  const [deviceID, setDeviceID] = React.useState<?string>();
+  const userID = useSelector(state => state.currentUserInfo?.id);
+  const accessToken = useSelector(state => state.commServicesAccessToken);
+
+  React.useEffect(() => {
+    (async () => {
+      const contentSigningKey = await getContentSigningKey();
+      setDeviceID(contentSigningKey);
+    })();
+  }, []);
+
+  return React.useMemo(() => {
+    if (!deviceID || !accessToken || !userID) {
+      return null;
+    }
+    return ({
+      type: 'ConnectionInitializationMessage',
+      deviceID,
+      accessToken,
+      userID,
+      deviceType: 'mobile',
+    }: ConnectionInitializationMessage);
+  }, [accessToken, deviceID, userID]);
+}
+
+export { useTunnelbrokerInitMessage };
