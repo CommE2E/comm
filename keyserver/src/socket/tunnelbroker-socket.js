07a683639c39fb0ca4ea0dcf84791157fc0b182c
xsanm <kkurowski.kk@gmail.com>
2023-10-27 18:57:51 +0200

    [keyserver] implement socket and update code to interact with Tunnelbroker API
    Summary:
    1. Updates old code to new API
    2. Implement Socket
    3. Remove old types
    
    There is some code duplication with what's in `lib/`. I'll try to refactor this later.
    
    Looks like `RefreshKeyRequest` is defined not properly in Rust codebase, should not be a separate message but part of `MessageToDevice`. I'll fix that as followup - shouldn't affect this code.
    
    Depends on D9596
    
    Test Plan:
    1. Check if the keyserver can connect to Tunnelbroker
    2. Check if the socket is properly closed when eg. Tunnelbroker is killed
    3. Check if socket issues don't cause the keyserver to crash
    4. Send `RefreshKeyRequest` and verify on keyserver and Identity logs that it is properly processed
    4. Kill keyserver, send `RefreshKeyRequest`, launch keyserver and check if the message was delivered and processed.
    
    Reviewers: michal, bartek, varun
    
    Subscribers: ashoat, tomek, wyilio
    
    Differential Revision: https://phab.comm.dev/D9623

---
+++ keyserver/src/socket/tunnelbroker-socket.js
@@ -0,0 +1,156 @@
+// @flow
+
+import uuid from 'uuid';
+import WebSocket from 'ws';
+
+import type { ClientMessageToDevice } from 'lib/tunnelbroker/tunnelbroker-context.js';
+import {
+  type RefreshKeyRequest,
+  refreshKeysRequestValidator,
+} from 'lib/types/tunnelbroker/keys-types.js';
+import type { MessageReceiveConfirmation } from 'lib/types/tunnelbroker/message-receive-confirmation-types.js';
+import type { MessageSentStatus } from 'lib/types/tunnelbroker/message-to-device-request-status-types.js';
+import type { MessageToDeviceRequest } from 'lib/types/tunnelbroker/message-to-device-request-types.js';
+import {
+  type TunnelbrokerMessage,
+  tunnelbrokerMessageTypes,
+  tunnelbrokerMessageValidator,
+} from 'lib/types/tunnelbroker/messages.js';
+import type { ConnectionInitializationMessage } from 'lib/types/tunnelbroker/session-types.js';
+
+import { uploadNewOneTimeKeys } from '../utils/olm-utils.js';
+
+type PromiseCallbacks = {
+  +resolve: () => void,
+  +reject: (error: string) => void,
+};
+type Promises = { [clientMessageID: string]: PromiseCallbacks };
+
+class TunnelbrokerSocket {
+  ws: WebSocket;
+  connected: boolean;
+  promises: Promises;
+
+  constructor(socketURL: string, initMessage: ConnectionInitializationMessage) {
+    this.connected = false;
+    this.promises = {};
+
+    const socket = new WebSocket(socketURL);
+
+    socket.on('open', () => {
+      socket.send(JSON.stringify(initMessage));
+    });
+
+    socket.on('close', async () => {
+      this.connected = false;
+      console.error('Connection to Tunnelbroker closed');
+    });
+
+    socket.on('error', (error: Error) => {
+      console.error('Tunnelbroker socket error:', error.message);
+    });
+
+    socket.on('message', this.onMessage);
+
+    this.ws = socket;
+  }
+
+  onMessage: (event: ArrayBuffer) => Promise<void> = async (
+    event: ArrayBuffer,
+  ) => {
+    let rawMessage;
+    try {
+      rawMessage = JSON.parse(event.toString());
+    } catch (e) {
+      console.error('error while parsing Tunnelbroker message:', e.message);
+      return;
+    }
+
+    if (!tunnelbrokerMessageValidator.is(rawMessage)) {
+      console.error('invalid TunnelbrokerMessage: ', rawMessage.toString());
+      return;
+    }
+    const message: TunnelbrokerMessage = rawMessage;
+
+    if (
+      message.type ===
+      tunnelbrokerMessageTypes.CONNECTION_INITIALIZATION_RESPONSE
+    ) {
+      if (message.status.type === 'Success' && !this.connected) {
+        this.connected = true;
+        console.info('session with Tunnelbroker created');
+      } else if (message.status.type === 'Success' && this.connected) {
+        console.info(
+          'received ConnectionInitializationResponse with status: Success for already connected socket',
+        );
+      } else {
+        this.connected = false;
+        console.error(
+          'creating session with Tunnelbroker error:',
+          message.status.data,
+        );
+      }
+    } else if (message.type === tunnelbrokerMessageTypes.MESSAGE_TO_DEVICE) {
+      const confirmation: MessageReceiveConfirmation = {
+        type: tunnelbrokerMessageTypes.MESSAGE_RECEIVE_CONFIRMATION,
+        messageIDs: [message.messageID],
+      };
+      this.ws.send(JSON.stringify(confirmation));
+
+      const { payload } = message;
+      try {
+        const messageToKeyserver = JSON.parse(payload);
+        if (refreshKeysRequestValidator.is(messageToKeyserver)) {
+          const request: RefreshKeyRequest = messageToKeyserver;
+          await uploadNewOneTimeKeys(request.numberOfKeys);
+        }
+      } catch (e) {
+        console.error(
+          'error while processing message to keyserver:',
+          e.message,
+        );
+      }
+    } else if (
+      message.type === tunnelbrokerMessageTypes.MESSAGE_TO_DEVICE_REQUEST_STATUS
+    ) {
+      for (const status: MessageSentStatus of message.clientMessageIDs) {
+        if (status.type === 'Success') {
+          this.promises[status.data]?.resolve();
+          delete this.promises[status.data];
+        } else if (status.type === 'Error') {
+          this.promises[status.data.id]?.reject(status.data.error);
+          delete this.promises[status.data.id];
+        } else if (status.type === 'SerializationError') {
+          console.error('SerializationError for message: ', status.data);
+        } else if (status.type === 'InvalidRequest') {
+          console.log('Tunnelbroker recorded InvalidRequest');
+        }
+      }
+    }
+  };
+
+  sendMessage: (message: ClientMessageToDevice) => Promise<void> = (
+    message: ClientMessageToDevice,
+  ) => {
+    if (!this.connected) {
+      throw new Error('Tunnelbroker not connected');
+    }
+    const clientMessageID = uuid.v4();
+    const messageToDevice: MessageToDeviceRequest = {
+      type: tunnelbrokerMessageTypes.MESSAGE_TO_DEVICE_REQUEST,
+      clientMessageID,
+      deviceID: message.deviceID,
+      payload: message.payload,
+    };
+
+    return new Promise((resolve, reject) => {
+      this.promises[clientMessageID] = {
+        resolve,
+        reject,
+      };
+      this.ws.send(JSON.stringify(messageToDevice));
+    });
+  };
+}
+
+export default TunnelbrokerSocket;
