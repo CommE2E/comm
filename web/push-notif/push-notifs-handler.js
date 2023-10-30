289422f63a72030d0bdcc7e3e4e27ffe28ab961d
Inka Sokołowska <inka.d.sokolowska@gmail.com>
2023-10-30 14:29:19 +0100

    [lib][web][native] Refactor deviceToken action
    Summary:
    issue: https://linear.app/comm/issue/ENG-5284/refactor-setdevicetoken-action
    I split the action into two actions to spare the calling code having to select all keyservers when it simply wants to call all of them
    
    Test Plan: tested that both action work, tested that
    
    Reviewers: michal, ginsu, kamil
    
    Subscribers: ashoat, tomek, wyilio
    
    Differential Revision: https://phab.comm.dev/D9628

--- web/push-notif/push-notifs-handler.js
+++ web/push-notif/push-notifs-handler.js
@@ -4,15 +4,12 @@ import * as React from 'react';
 import { useDispatch } from 'react-redux';
 
 import {
-  setDeviceToken,
+  useSetDeviceTokenFanout,
   setDeviceTokenActionTypes,
 } from 'lib/actions/device-actions.js';
 import { useModalContext } from 'lib/components/modal-provider.react.js';
 import { isLoggedIn } from 'lib/selectors/user-selectors.js';
-import {
-  useDispatchActionPromise,
-  useServerCall,
-} from 'lib/utils/action-utils.js';
+import { useDispatchActionPromise } from 'lib/utils/action-utils.js';
 import { convertNonPendingIDToNewSchema } from 'lib/utils/migration-utils.js';
 import {
   shouldSkipPushPermissionAlert,
@@ -27,7 +24,7 @@ import { useSelector } from '../redux/redux-utils.js';
 
 function useCreateDesktopPushSubscription() {
   const dispatchActionPromise = useDispatchActionPromise();
-  const callSetDeviceToken = useServerCall(setDeviceToken);
+  const callSetDeviceToken = useSetDeviceTokenFanout();
 
   React.useEffect(
     () =>
@@ -66,7 +63,7 @@ function useCreatePushSubscription(): () => Promise<void> {
   const publicKey = useSelector(state => state.pushApiPublicKey);
 
   const dispatchActionPromise = useDispatchActionPromise();
-  const callSetDeviceToken = useServerCall(setDeviceToken);
+  const callSetDeviceToken = useSetDeviceTokenFanout();
 
   return React.useCallback(async () => {
     if (!publicKey) {
