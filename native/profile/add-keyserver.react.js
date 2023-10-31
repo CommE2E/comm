27eb37ace7042d39c7f5f641865122486ee1fe50
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-10-31 00:56:58 -0400

    [native] implement onPressSave keyserver button callback
    Summary: This diff implements `onPressSave` keyserver button. This callback will create the payload necessary for this action and dispatch the add keyserver redux action we just introducted with the payload we just created. In a subsequent diff we will implement validation that will be used before we introduce the keyserver
    
    Test Plan: Please see the demo video below
    
    Subscribers: ashoat, tomek, wyilio
    
    Differential Revision: https://phab.comm.dev/D9665

--- native/profile/add-keyserver.react.js
+++ native/profile/add-keyserver.react.js
@@ -3,14 +3,24 @@
 import { useNavigation } from '@react-navigation/native';
 import * as React from 'react';
 import { View, Text } from 'react-native';
+import { useDispatch } from 'react-redux';
+
+import { addKeyserverActionType } from 'lib/actions/keyserver-actions.js';
+import type { KeyserverInfo } from 'lib/types/keyserver-types.js';
+import { defaultConnectionInfo } from 'lib/types/socket-types.js';
 
 import TextInput from '../components/text-input.react.js';
 import HeaderRightTextButton from '../navigation/header-right-text-button.react.js';
+import { useSelector } from '../redux/redux-utils.js';
 import { useStyles, useColors } from '../themes/colors.js';
 
 // eslint-disable-next-line no-unused-vars
 function AddKeyserver(props: { ... }): React.Node {
-  const { setOptions } = useNavigation();
+  const { goBack, setOptions } = useNavigation();
+
+  const dispatch = useDispatch();
+
+  const currentUserID = useSelector(state => state.currentUserInfo?.id);
 
   const { panelForegroundTertiaryLabel } = useColors();
   const styles = useStyles(unboundStyles);
@@ -18,8 +28,29 @@ function AddKeyserver(props: { ... }): React.Node {
   const [urlInput, setUrlInput] = React.useState('');
 
   const onPressSave = React.useCallback(() => {
-    // TODO
-  }, []);
+    if (!currentUserID || !urlInput) {
+      return;
+    }
+
+    const newKeyserverInfo: KeyserverInfo = {
+      cookie: null,
+      updatesCurrentAsOf: 0,
+      urlPrefix: urlInput,
+      connection: defaultConnectionInfo,
+      lastCommunicatedPlatformDetails: null,
+      deviceToken: null,
+    };
+
+    dispatch({
+      type: addKeyserverActionType,
+      payload: {
+        keyserverAdminUserID: currentUserID,
+        newKeyserverInfo,
+      },
+    });
+
+    goBack();
+  }, [currentUserID, dispatch, goBack, urlInput]);
 
   React.useEffect(() => {
     setOptions({
