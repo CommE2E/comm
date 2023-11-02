CommE2E/comm

commit 8199f294dea5c5363f130481945dc43076aa3336

[client-backup] add local settings to store
Summary:
Allow user (for now only staff) to enable in disable automatic backup.

Toggle will be introduced in next diff.

Depends on D9003

Test Plan: Flip value and check result

Reviewers: michal, marcin, bartek

Subscribers: ashoat, tomek

Differential Revision: https://phab.comm.dev/D9010

native/redux/redux-setup.js
@@ -43,6 +43,7 @@ import {
   setReduxStateActionType,
   setStoreLoadedActionType,
   type Action,
+  setLocalSettingsActionType,
 } from './action-types.js';
 import { remoteReduxDevServerConfig } from './dev-tools.js';
 import { defaultDimensionsInfo } from './dimensions-updater.react.js';
@@ -127,6 +128,9 @@ const defaultState = ({
         urlPrefix: defaultURLPrefix,
       },
     },
+  },
+  localSettings: {
+    isBackupEnabled: false,
   },
 }: AppState);
 
@@ -298,6 +302,11 @@ function reducer(state: AppState = defaultState, action: Action) {
       });
     }
     return state;
+  } else if (action.type === setLocalSettingsActionType) {
+    return {
+      ...state,
+      localSettings: { ...state.localSettings, ...action.payload },
+    };
   }
 
   if (action.type === setNewSessionActionType) {

native/backup/backup-handler.js
@@ -20,6 +20,9 @@ function BackupHandler(): null {
   const currentUserID = useSelector(
     state => state.currentUserInfo && state.currentUserInfo.id,
   );
+  const isBackupEnabled = useSelector(
+    state => state.localSettings.isBackupEnabled,
+  );
   const loggedIn = useSelector(isLoggedIn);
   const isStaff = useIsCurrentUserStaff();
 
@@ -27,7 +30,7 @@ function BackupHandler(): null {
 
   React.useEffect(() => {
     (async () => {
-      if (!loggedIn || !(isStaff || isDev)) {
+      if (!isBackupEnabled || !loggedIn || !(isStaff || isDev)) {
         return;
       }
 
@@ -54,7 +57,14 @@ function BackupHandler(): null {
         Alert.alert('Backup protocol result', message);
       }
     })();
-  }, [currentUserID, isStaff, loggedIn, uploadBackupProtocol, userStore]);
+  }, [
+    currentUserID,
+    isBackupEnabled,
+    isStaff,
+    loggedIn,
+    uploadBackupProtocol,
+    userStore,
+  ]);
 
   return null;
 }

native/redux/action-types.js
@@ -10,6 +10,7 @@ import type { DimensionsInfo } from './dimensions-updater.react.js';
 import type { AppState } from './state-types.js';
 import type { DeviceCameraInfo } from '../types/camera.js';
 import type { ConnectivityInfo } from '../types/connectivity.js';
+import type { LocalSettings } from '../types/local-settings-types.js';
 import type { GlobalThemeInfo } from '../types/themes.js';
 
 export const updateDimensionsActiveType = 'UPDATE_DIMENSIONS';
@@ -21,6 +22,7 @@ export const updateThreadLastNavigatedActionType =
   'UPDATE_THREAD_LAST_NAVIGATED';
 export const setStoreLoadedActionType = 'SET_STORE_LOADED';
 export const setReduxStateActionType = 'SET_REDUX_STATE';
+export const setLocalSettingsActionType = 'SET_LOCAL_SETTINGS';
 
 export const backgroundActionTypes: Set<string> = new Set([
   saveMessagesActionType,
@@ -62,4 +64,5 @@ export type Action =
     }
   | {
       +type: 'SET_STORE_LOADED',
-    };
+    }
+  | { +type: 'SET_LOCAL_SETTINGS', +payload: LocalSettings };

native/redux/state-types.js
@@ -24,6 +24,7 @@ import type { DimensionsInfo } from './dimensions-updater.react.js';
 import type { NavInfo } from '../navigation/default-state.js';
 import type { DeviceCameraInfo } from '../types/camera.js';
 import type { ConnectivityInfo } from '../types/connectivity.js';
+import type { LocalSettings } from '../types/local-settings-types.js';
 import type { GlobalThemeInfo } from '../types/themes.js';
 
 export type AppState = {
@@ -59,4 +60,5 @@ export type AppState = {
   +inviteLinksStore: InviteLinksStore,
   +lastCommunicatedPlatformDetails: LastCommunicatedPlatformDetails,
   +keyserverStore: KeyserverStore,
+  +localSettings: LocalSettings,
 };

native/types/local-settings-types.js
@@ -1,0 +1,5 @@
+// @flow
+
+export type LocalSettings = {
+  +isBackupEnabled: boolean,
+};

commit 75408b563e5c20296b2f03989649e8393093261d

[native] fix handling logout action
Summary:
Fix for a regression introduced in D9010.

The problem was that the previous code returned immediately which means `baseReducer` wasn't able to properly modify the state.

Not a huge fun of this solution but I saw this somewhere in codebase. As an alternative, I can suggest:
1. Creating a separate reducer and running it in the same place - don't think is much better, will require the same assignment.
2. Creating separate reduce and moving this to lib.

 Only 2 two seems better than this, but don't want to put native-specific data in lib.

Test Plan:
Using Redux DevTools make sure that logout is handled and data that needs to be cleared is cleared/set default where it needs to be.

Reviewers: ginsu, michal, tomek

native/redux/redux-setup.js
@@ -182,9 +182,11 @@ function reducer(state: AppState = defaultState, action: Action) {
     action.type === logOutActionTypes.success ||
     action.type === deleteAccountActionTypes.success
   ) {
-    return {
+    state = {
       ...state,
-      localSettings: { isBackupEnabled: false },
+      localSettings: {
+        isBackupEnabled: false,
+      },
     };
   }
 
@@ -290,6 +292,7 @@ type FixUnreadActiveThreadResult = {
   +state: AppState,
   +threadStoreOperations: $ReadOnlyArray<ThreadStoreOperation>,
 };
+
 function fixUnreadActiveThread(
   state: AppState,
   action: *,
@@ -336,6 +339,7 @@ function fixUnreadActiveThread(
 }
 
 let appLastBecameInactive = 0;
+
 function appBecameInactive() {
   appLastBecameInactive = Date.now();
 }