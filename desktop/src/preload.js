cfa6724437be03ae3d69f777308fa48fa2252d84
Ashoat Tevosyan <ashoat@comm.app>
2023-11-05 11:10:19 -0500

    [Flow202][desktop][skip-ci] Fix Flow issues in desktop codebase
    Summary:
    This one was pretty simple... just added a bunch of types in various places.
    
    NOTE: CI will fail on this diff. I considered the possibility of fixing Flow errors BEFORE upgrading Flow, but it wasn't possible... in some cases, the fixes to support the new version of Flow caused errors in the old version. I could have hidden these type errors with `$FlowFixMe` lines and then later revert those, but that seemed like too much busy work.
    
    Test Plan: Make sure all Flow errors in `desktop` are resolved
    
    Reviewers: michal
    
    Reviewed By: michal
    
    Subscribers: tomek, wyilio
    
    Differential Revision: https://phab.comm.dev/D9721

--- desktop/src/preload.js
+++ desktop/src/preload.js
@@ -1,5 +1,6 @@
 // @flow
 
+import type { IpcRendererEvent } from 'electron';
 // eslint-disable-next-line import/extensions
 import { contextBridge, ipcRenderer } from 'electron/renderer';
 
@@ -7,7 +8,8 @@ import type { ElectronBridge } from 'lib/types/electron-types.js';
 
 const bridge: ElectronBridge = {
   onNavigate: callback => {
-    const withEvent = (event, ...args) => callback(...args);
+    const withEvent = (event: IpcRendererEvent, ...args: $ReadOnlyArray<any>) =>
+      callback(...args);
     ipcRenderer.on('on-navigate', withEvent);
     return () => ipcRenderer.removeListener('on-navigate', withEvent);
   },
@@ -16,7 +18,8 @@ const bridge: ElectronBridge = {
   setBadge: value => ipcRenderer.send('set-badge', value),
   version: ipcRenderer.sendSync('get-version'),
   onNewVersionAvailable: callback => {
-    const withEvent = (event, ...args) => callback(...args);
+    const withEvent = (event: IpcRendererEvent, ...args: $ReadOnlyArray<any>) =>
+      callback(...args);
     ipcRenderer.on('on-new-version-available', withEvent);
     return () =>
       ipcRenderer.removeListener('on-new-version-available', withEvent);
@@ -24,13 +27,15 @@ const bridge: ElectronBridge = {
   updateToNewVersion: () => ipcRenderer.send('update-to-new-version'),
   platform: { win32: 'windows', darwin: 'macos' }[process.platform],
   onDeviceTokenRegistered: callback => {
-    const withEvent = (event, ...args) => callback(...args);
+    const withEvent = (event: IpcRendererEvent, ...args: $ReadOnlyArray<any>) =>
+      callback(...args);
     ipcRenderer.on('on-device-token-registered', withEvent);
     return () =>
       ipcRenderer.removeListener('on-device-token-registered', withEvent);
   },
   onNotificationClicked: callback => {
-    const withEvent = (event, ...args) => callback(...args);
+    const withEvent = (event: IpcRendererEvent, ...args: $ReadOnlyArray<any>) =>
+      callback(...args);
     ipcRenderer.on('on-notification-clicked', withEvent);
     return () =>
       ipcRenderer.removeListener('on-notification-clicked', withEvent);
