1f4710d060e09323ee219288361b0eae6881e7a0
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-11-06 15:58:59 -0500

    [native] introduce status indicator component
    Summary: In the keyserver selection bottom sheet ui, I will need to add a status indicator again. Rather than copy + paste all the logic necessary for this, I decided to factor out the logic into its own component that can be reused anytime
    
    Test Plan: `flow` and confirmed that the status indicator still looks and behaves the same in the keyserver selection list
    
    Reviewers:
    
    Subscribers:

--- native/profile/keyserver-selection-list-item.react.js
+++ native/profile/keyserver-selection-list-item.react.js
@@ -7,6 +7,7 @@ import type { KeyserverInfo } from 'lib/types/keyserver-types.js';
 
 import CommIcon from '../components/comm-icon.react.js';
 import Pill from '../components/pill.react.js';
+import StatusIndicator from '../components/status-indicator.react.js';
 import { useStyles, useColors } from '../themes/colors.js';
 
 type Props = {
@@ -31,30 +32,6 @@ function KeyserverSelectionListItem(props: Props): React.Node {
     [colors.panelForegroundLabel],
   );
 
-  const isConnected = keyserverInfo.connection.status === 'connected';
-
-  const { connectionIndicatorOuterStyle, connectionIndicatorInnerStyle } =
-    React.useMemo(() => {
-      const outerStyle = isConnected
-        ? styles.onlineIndicatorOuter
-        : styles.offlineIndicatorOuter;
-
-      const innerStyle = isConnected
-        ? styles.onlineIndicatorInner
-        : styles.offlineIndicatorInner;
-
-      return {
-        connectionIndicatorOuterStyle: outerStyle,
-        connectionIndicatorInnerStyle: innerStyle,
-      };
-    }, [
-      isConnected,
-      styles.offlineIndicatorInner,
-      styles.offlineIndicatorOuter,
-      styles.onlineIndicatorInner,
-      styles.onlineIndicatorOuter,
-    ]);
-
   const keyserverListItem = React.useMemo(
     () => (
       <View style={styles.keyserverListItemContainer}>
@@ -63,17 +40,14 @@ function KeyserverSelectionListItem(props: Props): React.Node {
           backgroundColor={colors.codeBackground}
           icon={cloudIcon}
         />
-        <View style={connectionIndicatorOuterStyle}>
-          <View style={connectionIndicatorInnerStyle} />
-        </View>
+        <StatusIndicator connectionInfo={keyserverInfo.connection} />
       </View>
     ),
     [
       cloudIcon,
       colors.codeBackground,
-      connectionIndicatorInnerStyle,
-      connectionIndicatorOuterStyle,
       keyserverAdminUsername,
+      keyserverInfo.connection,
       styles.keyserverListItemContainer,
     ],
   );
@@ -88,34 +62,6 @@ const unboundStyles = {
     alignItems: 'center',
     paddingHorizontal: 24,
     paddingVertical: 10,
-  },
-  onlineIndicatorOuter: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'greenIndicatorOuter',
-    width: 18,
-    height: 18,
-    borderRadius: 9,
-  },
-  onlineIndicatorInner: {
-    backgroundColor: 'greenIndicatorInner',
-    width: 9,
-    height: 9,
-    borderRadius: 4.5,
-  },
-  offlineIndicatorOuter: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'redIndicatorOuter',
-    width: 18,
-    height: 18,
-    borderRadius: 9,
-  },
-  offlineIndicatorInner: {
-    backgroundColor: 'redIndicatorInner',
-    width: 9,
-    height: 9,
-    borderRadius: 4.5,
   },
 };
 
