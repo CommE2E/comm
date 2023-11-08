b722994827ffb0e6fb41934fe99c36ee2f1ff1d3
Ashoat Tevosyan <ashoat@comm.app>
2023-11-08 10:46:06 -0500

    [Flow202][web] [7/x] Fix Flow issues with React hooks being underconstrained
    Summary:
    The new version of Flow is requiring declarations to provide explicit type params when Flow isn't able to infer them.
    
    This diff adds those declarations for `React.useRef` and `React.useState` calls.
    
    NOTE: CI will fail on this diff. I considered the possibility of fixing Flow errors BEFORE upgrading Flow, but it wasn't possible... in some cases, the fixes to support the new version of Flow caused errors in the old version. I could have hidden these type errors with `$FlowFixMe` lines and then later revert those, but that seemed like too much busy work.
    
    Depends on D9772
    
    Test Plan: Confirm the Flow errors go away
    
    Reviewers: atul
    
    Subscribers: tomek, wyilio
    
    Differential Revision: https://phab.comm.dev/D9773

--- web/push-notif/badge-handler.react.js
+++ web/push-notif/badge-handler.react.js
@@ -4,6 +4,7 @@ import * as React from 'react';
 
 import { connectionSelector } from 'lib/selectors/keyserver-selectors.js';
 import { unreadCount } from 'lib/selectors/thread-selectors.js';
+import type { ConnectionInfo } from 'lib/types/socket-types.js';
 import { ashoatKeyserverID } from 'lib/utils/validation-utils.js';
 
 import electron from '../electron.js';
@@ -12,7 +13,7 @@ import getTitle from '../title/getTitle.js';
 
 function useBadgeHandler() {
   const connection = useSelector(connectionSelector(ashoatKeyserverID));
-  const prevConnection = React.useRef();
+  const prevConnection = React.useRef<?ConnectionInfo>();
 
   const boundUnreadCount = useSelector(unreadCount);
   const prevUnreadCount = React.useRef(boundUnreadCount);
