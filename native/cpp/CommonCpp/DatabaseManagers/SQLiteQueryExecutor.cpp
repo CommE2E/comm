1d7b2f866df473b3697d1cf845e1b6be54d983d3
Michal Gniadek <michal.gniadek@swmansion.com>
2023-11-08 10:11:45 +0100

    [native] Make SecureStore methods static
    Summary:
    [ENG-1567](https://linear.app/comm/issue/ENG-1567)
    
    This will make it easier to expose SecureStore to Rust as our native Rust codebase as inherently stateless.
    
    Test Plan:
    Tested on Android and iOS physical devices:
    - Add log to `SQLiteQueryExecutor::initilize` that prints `maybeEncryptionKey` read from Secure Store
    - Open the app -> encryption key is logged
    - Log out and log in to trigger creation of a new encryption key
    - Restart the app -> a different encryption key is logged
    
    Reviewers: marcin, kamil
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D9923

--- native/cpp/CommonCpp/DatabaseManagers/SQLiteQueryExecutor.cpp
+++ native/cpp/CommonCpp/DatabaseManagers/SQLiteQueryExecutor.cpp
@@ -1372,9 +1372,8 @@ void SQLiteQueryExecutor::clearSensitiveData() {
 void SQLiteQueryExecutor::initialize(std::string &databasePath) {
   std::call_once(SQLiteQueryExecutor::initialized, [&databasePath]() {
     SQLiteQueryExecutor::sqliteFilePath = databasePath;
-    CommSecureStore commSecureStore{};
     folly::Optional<std::string> maybeEncryptionKey =
-        commSecureStore.get(SQLiteQueryExecutor::secureStoreEncryptionKeyID);
+        CommSecureStore::get(SQLiteQueryExecutor::secureStoreEncryptionKeyID);
 
     if (file_exists(databasePath) && maybeEncryptionKey) {
       SQLiteQueryExecutor::encryptionKey = maybeEncryptionKey.value();
@@ -1385,10 +1384,9 @@ void SQLiteQueryExecutor::initialize(std::string &databasePath) {
 }
 
 void SQLiteQueryExecutor::assign_encryption_key() {
-  CommSecureStore commSecureStore{};
   std::string encryptionKey = comm::crypto::Tools::generateRandomHexString(
       SQLiteQueryExecutor::sqlcipherEncryptionKeySize);
-  commSecureStore.set(
+  CommSecureStore::set(
       SQLiteQueryExecutor::secureStoreEncryptionKeyID, encryptionKey);
   SQLiteQueryExecutor::encryptionKey = encryptionKey;
 }
