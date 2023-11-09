af7bbe7815bcd6be5083f1b548c3310cc76adfd3
Ashoat Tevosyan <ashoat@comm.app>
2023-11-09 10:11:21 -0500

    [Flow202][keyserver][skip-ci] [1/x] Fix Flow issues with conditional changeRole passed to promiseAll
    Summary:
    The new Flow is much stricter about `promiseAll`. It's relaxed a bit in Flow 0.214, but for now we have to comply with the additional strictness.
    
    I separated out this diff because it required some minimal changes to `thread-permission-updates.js` in order to rename and export a type. This way the context for those changes is more clear.
    
    NOTE: CI will fail on this diff. I considered the possibility of fixing Flow errors BEFORE upgrading Flow, but it wasn't possible... in some cases, the fixes to support the new version of Flow caused errors in the old version. I could have hidden these type errors with `$FlowFixMe` lines and then later revert those, but that seemed like too much busy work.
    
    Depends on D9786
    
    Test Plan: Confirm the Flow errors go away
    
    Reviewers: tomek, rohan
    
    Reviewed By: tomek, rohan
    
    Differential Revision: https://phab.comm.dev/D9849

--- keyserver/src/updaters/thread-permission-updaters.js
+++ keyserver/src/updaters/thread-permission-updaters.js
@@ -68,7 +68,7 @@ type MembershipRowToDelete = {
   +oldRole: string,
 };
 type MembershipRow = MembershipRowToSave | MembershipRowToDelete;
-type Changeset = {
+export type MembershipChangeset = {
   +membershipRows: MembershipRow[],
   +relationshipChangeset: RelationshipChangeset,
 };
@@ -90,7 +90,7 @@ async function changeRole(
   userIDs: $ReadOnlyArray<string>,
   role: string | -1 | 0 | null,
   options?: ChangeRoleOptions,
-): Promise<Changeset> {
+): Promise<MembershipChangeset> {
   const intent = role === -1 || role === 0 ? 'leave' : 'join';
   const setNewMembersToUnread =
     options?.setNewMembersToUnread && intent === 'join';
@@ -415,7 +415,7 @@ type AncestorChanges = {
 };
 async function updateDescendantPermissions(
   initialChangedAncestor: ChangedAncestor,
-): Promise<Changeset> {
+): Promise<MembershipChangeset> {
   const membershipRows = [];
   const relationshipChangeset = new RelationshipChangeset();
 
@@ -708,7 +708,7 @@ type RecalculatePermissionsMemberInfo = {
 };
 async function recalculateThreadPermissions(
   threadID: string,
-): Promise<Changeset> {
+): Promise<MembershipChangeset> {
   const threadQuery = SQL`
     SELECT type, depth, parent_thread_id, containing_thread_id
     FROM threads
@@ -1069,7 +1069,7 @@ type ChangesetCommitResult = {
 };
 async function commitMembershipChangeset(
   viewer: Viewer,
-  changeset: Changeset,
+  changeset: MembershipChangeset,
   {
     changedThreadIDs = new Set(),
     calendarQuery,
