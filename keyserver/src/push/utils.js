8935312cf39889ad59ea14c0e057c5ee71b9c58e
marcinwasowicz <marcinkwasowicz@gmail.com>
2023-11-10 13:37:44 +0100

    Refactor Windows push code to match conventions for other platforms

--- keyserver/src/push/utils.js
+++ keyserver/src/push/utils.js
@@ -6,7 +6,6 @@ import invariant from 'invariant';
 import webpush from 'web-push';
 
 import type { PlatformDetails } from 'lib/types/device-types.js';
-import type { WNSNotification } from 'lib/types/notif-types.js';
 import { threadSubscriptions } from 'lib/types/subscription-types.js';
 import { threadPermissions } from 'lib/types/thread-permission-types.js';
 
@@ -22,6 +21,7 @@ import type {
   TargetedAPNsNotification,
   TargetedAndroidNotification,
   TargetedWebNotification,
+  TargetedWNSNotification,
 } from './types.js';
 import { dbQuery, SQL } from '../database/database.js';
 
@@ -282,13 +282,9 @@ type WNSPushResult = {
   +errors?: $ReadOnlyArray<WNSPushError>,
   +invalidTokens?: $ReadOnlyArray<string>,
 };
-async function wnsPush({
-  notification,
-  deviceTokens,
-}: {
-  +notification: WNSNotification,
-  +deviceTokens: $ReadOnlyArray<string>,
-}): Promise<WNSPushResult> {
+async function wnsPush(
+  targetedNotifications: $ReadOnlyArray<TargetedWNSNotification>,
+): Promise<WNSPushResult> {
   const token = await getWNSToken();
   if (!token && process.env.NODE_ENV === 'development') {
     console.log(`no keyserver/secrets/wns_config.json so ignoring notifs`);
@@ -296,11 +292,17 @@ async function wnsPush({
   }
   invariant(token, `keyserver/secrets/wns_config.json should exist`);
 
-  const notificationString = JSON.stringify(notification);
+  const pushResults = targetedNotifications.map(async targetedNotification => {
+    const notificationString = JSON.stringify(
+      targetedNotification.notification,
+    );
 
-  const pushResults = deviceTokens.map(async devicePushURL => {
     try {
-      return await wnsSinglePush(token, notificationString, devicePushURL);
+      return await wnsSinglePush(
+        token,
+        notificationString,
+        targetedNotification.deviceToken,
+      );
     } catch (error) {
       return { error };
     }
@@ -309,6 +311,9 @@ async function wnsPush({
   const errors = [];
   const notifIDs = [];
   const invalidTokens = [];
+  const deviceTokens = targetedNotifications.map(
+    ({ deviceToken }) => deviceToken,
+  );
   for (let i = 0; i < pushResults.length; i++) {
     const pushResult = await pushResults[i];
     if (pushResult.error) {
