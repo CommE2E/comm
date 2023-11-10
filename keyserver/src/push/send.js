694db6d6559aa29264a1d23f49e063b3371fd7cd
marcinwasowicz <marcinkwasowicz@gmail.com>
2023-11-10 13:37:44 +0100

    Refactor Windows push code to match conventions for other platforms
    Summary: This differential does not introduce new functionality but unifies typing and signature for push-related functions for all platforms.
    
    Test Plan: Ensure that Windows notifications work correctly.
    
    Reviewers: michal
    
    Reviewed By: michal
    
    Subscribers: ashoat, tomek, wyilio
    
    Differential Revision: https://phab.comm.dev/D9818

--- keyserver/src/push/send.js
+++ keyserver/src/push/send.js
@@ -59,6 +59,7 @@ import type {
   TargetedAPNsNotification,
   TargetedAndroidNotification,
   TargetedWebNotification,
+  TargetedWNSNotification,
 } from './types.js';
 import {
   apnPush,
@@ -435,8 +436,11 @@ async function sendPushNotif(input: {
           unreadCount,
           platformDetails,
         });
-        const deviceTokens = devices.map(({ deviceToken }) => deviceToken);
-        return await sendWNSNotification(notification, deviceTokens, {
+        const targetedNotifications = devices.map(({ deviceToken }) => ({
+          notification,
+          deviceToken,
+        }));
+        return await sendWNSNotification(targetedNotifications, {
           ...notificationInfo,
           codeVersion,
           stateVersion,
@@ -1338,17 +1342,15 @@ type WNSResult = {
   +invalidTokens?: $ReadOnlyArray<string>,
 };
 async function sendWNSNotification(
-  notification: WNSNotification,
-  deviceTokens: $ReadOnlyArray<string>,
+  targetedNotifications: $ReadOnlyArray<TargetedWNSNotification>,
   notificationInfo: NotificationInfo,
 ): Promise<WNSResult> {
   const { source, codeVersion, stateVersion } = notificationInfo;
 
-  const response = await wnsPush({
-    notification,
-    deviceTokens,
-  });
-
+  const response = await wnsPush(targetedNotifications);
+  const deviceTokens = targetedNotifications.map(
+    ({ deviceToken }) => deviceToken,
+  );
   const wnsIDs = response.wnsIDs ?? [];
   const delivery: WNSDelivery = {
     source,
