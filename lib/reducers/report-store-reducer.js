CommE2E/comm

commit 4200597741c515c7bb39ef4ed846df49146a7e30

[lib] refactor handling new inconsistencies in report store reducer for using operations
Summary:
Convert logic for ops approach.

Depends on D7932

Test Plan: Tests

Reviewers: tomek, michal

Subscribers: ashoat, atul

Differential Revision: https://phab.comm.dev/D7933

lib/reducers/report-store-reducer.js
@@ -43,13 +43,6 @@ export default function reduceReportStore(
     isReportEnabled(report, state.enabledReports),
   );
 
-  const updatedReports =
-    newInconsistencies.length > 0
-      ? [...state.queuedReports, ...newInconsistencies].filter(report =>
-          isReportEnabled(report, state.enabledReports),
-        )
-      : state.queuedReports;
-
   if (action.type === updateReportsEnabledActionType) {
     const newEnabledReports = { ...state.enabledReports, ...action.payload };
     const newFilteredReports = newReports.filter(report =>
@@ -151,10 +144,24 @@ export default function reduceReportStore(
       reportStoreOperations,
     };
   }
-  const reportStore =
-    updatedReports !== state.queuedReports
-      ? { ...state, queuedReports: updatedReports }
-      : state;
 
-  return { reportStore, reportStoreOperations: [] };
+  if (newReports) {
+    const reportStoreOperations: $ReadOnlyArray<ReportStoreOperation> =
+      convertReportsToReplaceReportOps(newReports);
+
+    const queuedReports = processReportStoreOperations(
+      state.queuedReports,
+      reportStoreOperations,
+    );
+
+    return {
+      reportStore: {
+        ...state,
+        queuedReports,
+      },
+      reportStoreOperations,
+    };
+  }
+
+  return { reportStore: state, reportStoreOperations: [] };
 }

lib/reducers/report-store-reducer.test.js
@@ -298,7 +298,10 @@ describe('new inconsistencies test', () => {
         },
       },
       defaultBaseAction,
-      [mockErrorReport, mockMediaReport],
+      [
+        { ...mockErrorReport, id: 'new-1' },
+        { ...mockMediaReport, id: 'new-2' },
+      ],
     );
     expect(reportStore.queuedReports).toHaveLength(2);
   });

commit f8c416c28088d594f527569c2f91a90d11c970d6

[lib] Fix persisting of privacy settings
Summary: This solves [ENG-5694](https://linear.app/comm/issue/ENG-5694/privacy-settings-arent-saved-when-app-is-killed), which is a regression introduced in D7933. The details are explained in that Linear task and diff.

Test Plan: Compiled a release build to my physical iOS device and confirmed that the issue is gone

Reviewers: kamil, tomek, michal, atul

Subscribers:

lib/reducers/report-store-reducer.js
@@ -173,7 +173,7 @@ export default function reduceReportStore(
     };
   }
 
-  if (newReports) {
+  if (newReports.length > 0) {
     const reportStoreOperations: $ReadOnlyArray<ReportStoreOperation> =
       convertReportsToReplaceReportOps(newReports);
 