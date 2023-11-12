62935ff4a29dd1a514603f2df23f79ecbbca56d3
Ashoat Tevosyan <ashoat@comm.app>
2023-11-12 18:44:33 -0500

    [Flow202][keyserver][skip-ci] [11/x] Address type errors with memberOfContainingThread
    Summary:
    The new version of Flow caught a type error here. `memberOfContainingThread` should be a `boolean`, but can be set to be nullable. This diff casts it to a `boolean` to avoid the type error.
    
    NOTE: CI will fail on this diff. I considered the possibility of fixing Flow errors BEFORE upgrading Flow, but it wasn't possible... in some cases, the fixes to support the new version of Flow caused errors in the old version. I could have hidden these type errors with `$FlowFixMe` lines and then later revert those, but that seemed like too much busy work.
    
    Depends on D9859
    
    Test Plan: Confirm the Flow errors go away
    
    Reviewers: tomek, rohan
    
    Reviewed By: rohan
    
    Differential Revision: https://phab.comm.dev/D9860

--- keyserver/src/updaters/thread-permission-updaters.js
+++ keyserver/src/updaters/thread-permission-updaters.js
@@ -229,7 +229,7 @@ async function changeRole(
     const ancestorMembership = ancestorMembershipInfo.get(userID);
     if (ancestorMembership) {
       permissionsFromParent = ancestorMembership.permissionsFromParent;
-      memberOfContainingThread = ancestorMembership.memberOfContainingThread;
+      memberOfContainingThread = !!ancestorMembership.memberOfContainingThread;
     }
     if (!hasContainingThreadID) {
       memberOfContainingThread = true;
