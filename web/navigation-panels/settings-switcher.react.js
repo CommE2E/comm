514ab049402b398ef2b9782f85a0f9bec6a99efe
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-11-13 17:03:14 -0500

    [web] introduce keyservers to the settings switcher
    Summary: This diff introduces a new "Keyservers" option in the settings switcher
    
    Test Plan: Please see screenshot below
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D9887

--- web/navigation-panels/settings-switcher.react.js
+++ web/navigation-panels/settings-switcher.react.js
@@ -7,6 +7,7 @@ import NavigationPanel from './navigation-panel.react.js';
 import css from './settings-switcher.css';
 import { updateNavInfoActionType } from '../redux/action-types.js';
 import { navSettingsSectionSelector } from '../selectors/nav-selectors.js';
+import { useStaffCanSee } from '../utils/staff-utils.js';
 
 function SettingsSwitcher(): React.Node {
   const dispatch = useDispatch();
@@ -29,6 +30,30 @@ function SettingsSwitcher(): React.Node {
     ),
     [onClickAccountSettings],
   );
+
+  const staffCanSee = useStaffCanSee();
+
+  const onClickKeyservers = React.useCallback(
+    (event: SyntheticEvent<HTMLAnchorElement>) => {
+      event.preventDefault();
+      dispatch({
+        type: updateNavInfoActionType,
+        payload: { tab: 'settings', settingsSection: 'keyservers' },
+      });
+    },
+    [dispatch],
+  );
+
+  const keyserversNavigationItem = React.useMemo(() => {
+    if (!staffCanSee) {
+      return null;
+    }
+    return (
+      <a className={css.navigationPanelTab} onClick={onClickKeyservers}>
+        <p>Keyservers</p>
+      </a>
+    );
+  }, [onClickKeyservers, staffCanSee]);
 
   const onClickDangerZone = React.useCallback(
     (event: SyntheticEvent<HTMLAnchorElement>) => {
@@ -53,6 +78,9 @@ function SettingsSwitcher(): React.Node {
     <NavigationPanel.Container tabSelector={navSettingsSectionSelector}>
       <NavigationPanel.Item tab="account">
         {accountSettingsNavigationItem}
+      </NavigationPanel.Item>
+      <NavigationPanel.Item tab="keyservers">
+        {keyserversNavigationItem}
       </NavigationPanel.Item>
       <NavigationPanel.Item tab="danger-zone">
         {dangerZoneNavigationItem}
