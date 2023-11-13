c715d038716d6bdf2d1cca8d51427f89e5db736a
Ashoat Tevosyan <ashoat@comm.app>
2023-11-13 16:21:14 -0500

    [Flow202][keyserver][skip-ci] [23/x] Address type errors in addSessionChangeInfoToResult
    Summary:
    The new version of Flow forced me to type the local variable `userInfos` here, which revealed some errors.
    
    From investigation I found that `userInfos` is definitely an array when set ([here](https://github.com/CommE2E/comm/blob/beb3df179ce2e61b80dc367d91d544e2fdc89700/keyserver/src/responders/user-responders.js#L337), [here](https://github.com/CommE2E/comm/blob/beb3df179ce2e61b80dc367d91d544e2fdc89700/keyserver/src/responders/user-responders.js#L389), and [here](https://github.com/CommE2E/comm/blob/f7df2484fa4a055b1fac648e5abfa69e00d6dce5/keyserver/src/creators/account-creator.js#L200)).
    
    This issue was never surfaced because it looks like if `Object.values` is passed an array, it just returns that same array.
    
    NOTE: CI will fail on this diff. I considered the possibility of fixing Flow errors BEFORE upgrading Flow, but it wasn't possible... in some cases, the fixes to support the new version of Flow caused errors in the old version. I could have hidden these type errors with `$FlowFixMe` lines and then later revert those, but that seemed like too much busy work.
    
    Depends on D9871
    
    Test Plan: Confirm the Flow errors go away
    
    Reviewers: michal
    
    Reviewed By: michal
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D9872

--- keyserver/src/session/cookies.js
+++ keyserver/src/session/cookies.js
@@ -28,7 +28,6 @@ import type { SIWESocialProof } from 'lib/types/siwe-types.js';
 import type { InitialClientSocketMessage } from 'lib/types/socket-types.js';
 import type { UserInfo } from 'lib/types/user-types.js';
 import { isDev } from 'lib/utils/dev-utils.js';
-import { values } from 'lib/utils/objects.js';
 
 import {
   isBcryptHash,
@@ -579,7 +578,7 @@ function addSessionChangeInfoToResult(
   result: Object,
 ) {
   let threadInfos = {},
-    userInfos = {};
+    userInfos: $ReadOnlyArray<UserInfo> = [];
   if (result.cookieChange) {
     ({ threadInfos, userInfos } = result.cookieChange);
   }
@@ -588,7 +587,7 @@ function addSessionChangeInfoToResult(
     sessionChange = ({
       cookieInvalidated: true,
       threadInfos,
-      userInfos: (values(userInfos).map(a => a): UserInfo[]),
+      userInfos,
       currentUserInfo: {
         anonymous: true,
       },
@@ -597,7 +596,7 @@ function addSessionChangeInfoToResult(
     sessionChange = ({
       cookieInvalidated: false,
       threadInfos,
-      userInfos: (values(userInfos).map(a => a): UserInfo[]),
+      userInfos,
     }: ServerSessionChange);
   }
   if (viewer.cookieSource === cookieSources.BODY) {
