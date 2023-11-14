f07c0efc064bb79bc5c1e9469d9c06a0f85cc0c8
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-11-14 17:09:26 -0500

    [web] introduce add keyserver modal
    Summary: This diff introduces the add keyserver modal. The focus of this diff is just the interface, and subsquent diffs will handle implementing the behavoir of adding a keyserver to the keyserver store
    
    Test Plan: Please see screenshot below
    
    Reviewers:
    
    Subscribers:

---
+++ web/modals/keyserver-selection/add-keyserver-modal.react.js
@@ -0,0 +1,59 @@
+// @flow
+
+import * as React from 'react';
+
+import { useModalContext } from 'lib/components/modal-provider.react.js';
+
+import css from './add-keyserver-modal.css';
+import Button, { buttonThemes } from '../../components/button.react.js';
+import Input from '../input.react.js';
+import Modal from '../modal.react.js';
+
+function AddKeyserverModal(): React.Node {
+  const { popModal } = useModalContext();
+
+  const [keyserverURL, setKeyserverURL] = React.useState<string>('');
+  console.log(setKeyserverURL);
+
+  const onChangeKeyserverURL = React.useCallback(
+    (event: SyntheticEvent<HTMLInputElement>) => {
+      setKeyserverURL(event.currentTarget.value);
+    },
+    [],
+  );
+
+  const onClickAddKeyserver = React.useCallback(() => {
+    // TODO
+  }, []);
+
+  const addKeyserverModal = React.useMemo(
+    () => (
+      <Modal size="large" onClose={popModal} name="Add keyserver">
+        <div className={css.container}>
+          <div className={css.inputTitle}>Keyserver URL</div>
+          <Input
+            type="text"
+            value={keyserverURL}
+            onChange={onChangeKeyserverURL}
+            placeholder="Keyserver URL"
+          />
+        </div>
+        <div className={css.buttonContainer}>
+          <Button
+            variant="filled"
+            buttonColor={buttonThemes.primary}
+            className={css.button}
+            onClick={onClickAddKeyserver}
+          >
+            Add keyserver
+          </Button>
+        </div>
+      </Modal>
+    ),
+    [keyserverURL, onChangeKeyserverURL, onClickAddKeyserver, popModal],
+  );
+
+  return addKeyserverModal;
+}
+
+export default AddKeyserverModal;
