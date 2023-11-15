cd80f5e3321d4e401f531cf332015df0f8f6a731
Ashoat Tevosyan <ashoat@comm.app>
2023-11-15 14:37:40 -0500

    [Flow202][native][skip-ci] [8/x] Fix incorrect ReactNav types in navigators & screens
    Summary:
    The new version of Flow is stricter with types, and exposed several errors in our typing of navigators and screens.
    
    Some of these are pretty bad, but the good news is that Flow should make sure we don't make the same mistakes again going forward.
    
    NOTE: CI will fail on this diff. I considered the possibility of fixing Flow errors BEFORE upgrading Flow, but it wasn't possible... in some cases, the fixes to support the new version of Flow caused errors in the old version. I could have hidden these type errors with `$FlowFixMe` lines and then later revert those, but that seemed like too much busy work.
    
    Depends on D9991
    
    Test Plan: Confirm the Flow errors go away
    
    Reviewers: ginsu, atul, tomek
    
    Differential Revision: https://phab.comm.dev/D9992

--- native/user-profile/user-profile-bottom-sheet.react.js
+++ native/user-profile/user-profile-bottom-sheet.react.js
@@ -4,9 +4,9 @@ import * as React from 'react';
 
 import type { UserInfo } from 'lib/types/user-types';
 
+import type { UserProfileBottomSheetNavigationProp } from './user-profile-bottom-sheet-navigator.react.js';
 import UserProfile from './user-profile.react.js';
 import BottomSheet from '../bottom-sheet/bottom-sheet.react.js';
-import type { RootNavigationProp } from '../navigation/root-navigator.react.js';
 import type { NavigationRoute } from '../navigation/route-names.js';
 import { useSelector } from '../redux/redux-utils.js';
 
@@ -15,7 +15,7 @@ export type UserProfileBottomSheetParams = {
 };
 
 type Props = {
-  +navigation: RootNavigationProp<'UserProfileBottomSheet'>,
+  +navigation: UserProfileBottomSheetNavigationProp<'UserProfileBottomSheet'>,
   +route: NavigationRoute<'UserProfileBottomSheet'>,
 };
 
