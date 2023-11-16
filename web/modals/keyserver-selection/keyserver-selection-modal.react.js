fc76ce5d35aedb39d468f86ab1441cde4e36250e
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-11-16 16:36:36 -0500

    [web] update the keyserver selection modal based on the connection status
    Summary: In the desgins for the keyserver selection modal, the text of the modal changes based on if you are currently connected to the selected keyserver or not.
    
    Test Plan: Please see the screenshots below
    
    Reviewers:
    
    Subscribers:

--- web/modals/keyserver-selection/keyserver-selection-modal.react.js
+++ web/modals/keyserver-selection/keyserver-selection-modal.react.js
@@ -22,6 +22,67 @@ function KeyserverSelectionModal(props: Props): React.Node {
 
   const { popModal } = useModalContext();
 
+  const { keyerverRemoveInfoText, keyserverRemoveButton } =
+    React.useMemo(() => {
+      if (keyserverInfo.connection.status !== 'connected') {
+        const removeInfoText = (
+          <>
+            <div>
+              You may delete offline keyserver from your keyserver list. When
+              you delete a keyserver, you will still remain in the associated
+              communities.
+            </div>
+            <div>
+              Any messages or content you have previously sent will remain on
+              the keyserver&rsquo;s communities after disconnecting or deleting.
+            </div>
+          </>
+        );
+
+        const removeButton = (
+          <Button
+            variant="filled"
+            buttonColor={buttonThemes.danger}
+            className={css.button}
+          >
+            Delete keyserver from list
+          </Button>
+        );
+
+        return {
+          keyerverRemoveInfoText: removeInfoText,
+          keyserverRemoveButton: removeButton,
+        };
+      }
+      const removeInfoText = (
+        <>
+          <div>
+            Disconnecting from this keyserver will remove you from its
+            associated communities.
+          </div>
+          <div>
+            Any messages or content you have previously sent will remain on the
+            keyserver.
+          </div>
+        </>
+      );
+
+      const removeButton = (
+        <Button
+          variant="filled"
+          buttonColor={buttonThemes.danger}
+          className={css.button}
+        >
+          Disconnect keyserver
+        </Button>
+      );
+
+      return {
+        keyerverRemoveInfoText: removeInfoText,
+        keyserverRemoveButton: removeButton,
+      };
+    }, [keyserverInfo.connection.status]);
+
   return (
     <Modal size="large" onClose={popModal} name="Keyserver details">
       <div className={css.container}>
@@ -35,26 +96,10 @@ function KeyserverSelectionModal(props: Props): React.Node {
           <div className={css.keyserverURL}>{keyserverInfo.urlPrefix}</div>
         </div>
         <div className={css.keyserverRemoveTextContainer}>
-          <div>
-            You may delete offline keyserver from your keyserver list. When you
-            delete a keyserver, you will still remain in the associated
-            communities.
-          </div>
-          <div>
-            Any messages or content you have previously sent will remain on the
-            keyserver&rsquo;s communities after disconnecting or deleting.
-          </div>
+          {keyerverRemoveInfoText}
         </div>
       </div>
-      <div className={css.buttonContainer}>
-        <Button
-          variant="filled"
-          buttonColor={buttonThemes.danger}
-          className={css.button}
-        >
-          Delete keyserver from list
-        </Button>
-      </div>
+      <div className={css.buttonContainer}>{keyserverRemoveButton}</div>
     </Modal>
   );
 }
