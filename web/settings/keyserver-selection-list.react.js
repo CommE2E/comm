434b680681023d72002b8a2d1f7507c18cfb075e
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-11-16 15:15:29 -0500

    [web] introduce add keyserver button
    Summary: This diff introduces the add keyserver button element on to the keyserver selection list screen. When a user clicks on this element we should push the add keyserver modal
    
    Test Plan: Please see the demo video below
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D9918

--- web/settings/keyserver-selection-list.react.js
+++ web/settings/keyserver-selection-list.react.js
@@ -2,11 +2,14 @@
 
 import * as React from 'react';
 
+import { useModalContext } from 'lib/components/modal-provider.react.js';
+import SWMansionIcon from 'lib/components/SWMansionIcon.react.js';
 import { selectedKeyserversSelector } from 'lib/selectors/keyserver-selectors.js';
 import type { SelectedKeyserverInfo } from 'lib/types/keyserver-types.js';
 
 import KeyserverSelectionListItem from './keyserver-selection-list-item.react.js';
 import css from './keyserver-selection-list.css';
+import AddKeyserverModal from '../modals/keyserver-selection/add-keyserver-modal.react.js';
 import { useSelector } from '../redux/redux-utils.js';
 
 function KeyserverSelectionList(): React.Node {
@@ -27,14 +30,29 @@ function KeyserverSelectionList(): React.Node {
     [selectedKeyserverInfos],
   );
 
+  const { pushModal } = useModalContext();
+
+  const onClickAddKeyserver = React.useCallback(() => {
+    pushModal(<AddKeyserverModal />);
+  }, [pushModal]);
+
   const keyserverSelectionList = React.useMemo(
     () => (
       <div className={css.container}>
-        <h4 className={css.header}>Keyservers</h4>
+        <div className={css.headerContainer}>
+          <h4 className={css.header}>Keyservers</h4>
+          <div onClick={onClickAddKeyserver}>
+            <SWMansionIcon
+              icon="plus-small"
+              size={26}
+              className={css.addButton}
+            />
+          </div>
+        </div>
         {selectedKeyservers}
       </div>
     ),
-    [selectedKeyservers],
+    [onClickAddKeyserver, selectedKeyservers],
   );
 
   return keyserverSelectionList;
