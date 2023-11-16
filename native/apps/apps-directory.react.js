abafe07a6d73100b38f89548f43ec3ab8b85ce7e
Ashoat Tevosyan <ashoat@comm.app>
2023-11-16 16:22:57 -0500

    [Flow202][native][skip-ci] [18/x] Type AppsDirectory FlatList
    Summary:
    Separating this out into its own diff since some extra changes were necessary. Will annotate inline.
    
    NOTE: CI will fail on this diff. I considered the possibility of fixing Flow errors BEFORE upgrading Flow, but it wasn't possible... in some cases, the fixes to support the new version of Flow caused errors in the old version. I could have hidden these type errors with `$FlowFixMe` lines and then later revert those, but that seemed like too much busy work.
    
    Depends on D10001
    
    Test Plan: Confirm the Flow errors go away
    
    Reviewers: atul
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D10002

--- native/apps/apps-directory.react.js
+++ native/apps/apps-directory.react.js
@@ -3,13 +3,23 @@
 import * as React from 'react';
 import { Text, FlatList, View } from 'react-native';
 
+import type { SupportedApps } from 'lib/types/enabled-apps.js';
+
 import AppListing from './app-listing.react.js';
 import type { NavigationRoute } from '../navigation/route-names.js';
 import type { TabNavigationProp } from '../navigation/tab-navigator.react.js';
 import { useSelector } from '../redux/redux-utils.js';
 import { useStyles } from '../themes/colors.js';
 
-const APP_DIRECTORY_DATA = [
+type Item = {
+  +id: SupportedApps | 'chat',
+  +alwaysEnabled: boolean,
+  +appName: string,
+  +appIcon: 'message-square' | 'calendar',
+  +appCopy: string,
+};
+
+const APP_DIRECTORY_DATA: $ReadOnlyArray<Item> = [
   {
     id: 'chat',
     alwaysEnabled: true,
@@ -36,10 +46,10 @@ function AppsDirectory(props: Props): React.Node {
   const enabledApps = useSelector(state => state.enabledApps);
 
   const renderAppCell = React.useCallback(
-    ({ item }) => (
+    ({ item }: { +item: Item, ... }) => (
       <AppListing
         id={item.id}
-        enabled={enabledApps[item.id]}
+        enabled={item.id === 'chat' ? true : enabledApps[item.id]}
         alwaysEnabled={item.alwaysEnabled}
         appName={item.appName}
         appIcon={item.appIcon}
@@ -48,7 +58,7 @@ function AppsDirectory(props: Props): React.Node {
     ),
     [enabledApps],
   );
-  const getItemID = React.useCallback(item => item.id, []);
+  const getItemID = React.useCallback((item: Item) => item.id, []);
 
   return (
     <View style={styles.view}>
