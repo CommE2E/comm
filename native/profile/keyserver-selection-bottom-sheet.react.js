f68ce52a93a23ecc07843844537d1fb25670142b
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-11-17 00:54:28 -0500

    [lib/native] lift set custom server action type into lib
    Summary:
    At the moment, the `customServer` redux field lives only in native. Since we want to use this state in web as well, we will need to lift the redux logic into lib. This diff handles lifting the set custom server action type into lib, and a subsequent diff will handle lifting the redux state into lib.
    
    Depends on D10089
    
    Test Plan: `flow` and confirmed that the custom server url on native still works as expected
    
    Reviewers: inka, michal, rohan
    
    Reviewed By: rohan
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10090

--- native/profile/keyserver-selection-bottom-sheet.react.js
+++ native/profile/keyserver-selection-bottom-sheet.react.js
@@ -5,6 +5,7 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
+import { setCustomServerActionType } from 'lib/actions/custom-server-actions.js';
 import { removeKeyserverActionType } from 'lib/actions/keyserver-actions.js';
 import type { KeyserverInfo } from 'lib/types/keyserver-types.js';
 import type { GlobalAccountUserInfo } from 'lib/types/user-types.js';
@@ -22,7 +23,6 @@ import { useColors, useStyles } from '../themes/colors.js';
 import type { BottomSheetRef } from '../types/bottom-sheet.js';
 import Alert from '../utils/alert.js';
 import { useStaffCanSee } from '../utils/staff-utils.js';
-import { setCustomServer } from '../utils/url-utils.js';
 
 export type KeyserverSelectionBottomSheetParams = {
   +keyserverAdminUserInfo: GlobalAccountUserInfo,
@@ -112,7 +112,7 @@ function KeyserverSelectionBottomSheet(props: Props): React.Node {
 
     if (staffCanSee) {
       dispatch({
-        type: setCustomServer,
+        type: setCustomServerActionType,
         payload: keyserverInfo.urlPrefix,
       });
     }
