50b6bf0349794cbbab41f2c81424347dd5169ece
Michal Gniadek <michal.gniadek@swmansion.com>
2023-11-20 16:24:41 +0100

    [native] Implement iOS encryption for Rust
    Summary:
    [ENG-5343](https://linear.app/comm/issue/ENG-5343)
    
    iOS implementation of the Rust encryption methods. Uses already existing functions.
    
    Depends on D9932
    
    Test Plan:
    Run this code on a physical iOS device:
    ```
    fn test(promise_id: u32) {
      RUNTIME.spawn(async move {
        let f = move || -> Result<String, cxx::Exception> {
          let key = &mut [0; constants::aes::KEY_SIZE];
          ffi::generate_key(key)?;
          let plaintext = &mut [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
          let sealed_data =
            &mut [0; 10 + constants::aes::IV_LENGTH + constants::aes::TAG_LENGTH];
          ffi::encrypt(key, plaintext, sealed_data)?;
          let plaintext2 = &mut [0; 10];
          ffi::decrypt(key, sealed_data, plaintext2)?;
    
          Ok(format!(
            "
        Key: {key:?}
        Plaintext: {plaintext:?}
        Sealed Data: {sealed_data:?}
        Plaintext2: {plaintext2:?}"
          ))
        };
        handle_string_result_as_callback(f(), promise_id);
      });
    }
    ```
    
    Example output:
    ```
        Key: [25, 165, 133, 47, 109, 170, 104, 88, 70, 47, 174, 67, 235, 86, 27, 48, 117, 195, 150, 178, 120, 34, 117, 120, 115, 73, 135, 50, 216, 152, 37, 40]
        Plaintext: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
        Sealed Data: [75, 154, 51, 64, 189, 35, 46, 50, 39, 181, 228, 130, 86, 208, 62, 48, 14, 103, 18, 191, 92, 117, 248, 186, 35, 8, 134, 109, 251, 54, 182, 192, 74, 244, 175, 46, 228, 120]
        Plaintext2: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
    ```
    
    Also I've run it a few times anc checked that the key was different each time. Also run the encryption with invalid length of `sealed_data` and decryption with invalid data and in both cases it failed gracefully with `Result::Err`.
    
    Reviewers: marcin, varun, kamil
    
    Subscribers:

--- native/ios/Comm/AESCrypto.mm
+++ native/ios/Comm/AESCrypto.mm
@@ -1,8 +1,17 @@
 #import "AESCrypto.h"
+#import "AESCryptoModuleObjCCompat.h"
+#import <Foundation/Foundation.h>
 
 namespace comm {
 
 std::string AESCrypto::generateKey(rust::Slice<uint8_t> buffer) {
+  NSError *keyGenerationError = nil;
+  [AESCryptoModuleObjCCompat generateKey:buffer.data()
+                       destinationLength:buffer.size()
+                               withError:&keyGenerationError];
+  if (keyGenerationError) {
+    return std::string([[keyGenerationError localizedDescription] UTF8String]);
+  }
   return std::string();
 }
 
@@ -10,6 +19,22 @@ std::string AESCrypto::encrypt(
     rust::Slice<uint8_t> key,
     rust::Slice<uint8_t> plaintext,
     rust::Slice<uint8_t> sealedData) {
+  NSData *keyBuffer = [NSData dataWithBytesNoCopy:key.data()
+                                           length:key.size()
+                                     freeWhenDone:NO];
+  NSData *plaintextBuffer = [NSData dataWithBytesNoCopy:plaintext.data()
+                                                 length:plaintext.size()
+                                           freeWhenDone:NO];
+  NSError *encryptError = nil;
+  [AESCryptoModuleObjCCompat encryptWithKey:keyBuffer
+                                  plaintext:plaintextBuffer
+                             destinationPtr:sealedData.data()
+                          destinationLength:sealedData.size()
+                                  withError:&encryptError];
+
+  if (encryptError) {
+    return std::string([[encryptError localizedDescription] UTF8String]);
+  }
   return std::string();
 }
 
@@ -17,6 +42,22 @@ std::string AESCrypto::decrypt(
     rust::Slice<uint8_t> key,
     rust::Slice<uint8_t> sealedData,
     rust::Slice<uint8_t> plaintext) {
+  NSData *keyBuffer = [NSData dataWithBytesNoCopy:key.data()
+                                           length:key.size()
+                                     freeWhenDone:NO];
+  NSData *sealedDataBuffer = [NSData dataWithBytesNoCopy:sealedData.data()
+                                                  length:sealedData.size()
+                                            freeWhenDone:NO];
+  NSError *decryptError = nil;
+  [AESCryptoModuleObjCCompat decryptWithKey:keyBuffer
+                                 sealedData:sealedDataBuffer
+                             destinationPtr:plaintext.data()
+                          destinationLength:plaintext.size()
+                                  withError:&decryptError];
+
+  if (decryptError) {
+    return std::string([[decryptError localizedDescription] UTF8String]);
+  }
   return std::string();
 }
 
--- native/ios/Comm/CommAESCryptoUtils/AESCryptoModuleObjCCompat.h
+++ native/ios/Comm/CommAESCryptoUtils/AESCryptoModuleObjCCompat.h
@@ -3,15 +3,28 @@
 @interface AESCryptoModuleObjCCompat : NSObject
 - (BOOL)generateKey:(NSMutableData *_Nonnull)destination
           withError:(NSError *_Nullable *_Nullable)error;
++ (BOOL)generateKey:(void *_Nonnull)destinationPtr
+    destinationLength:(NSInteger)destinationLength
+            withError:(NSError *_Nullable *_Nullable)error;
 - (NSInteger)encryptedLength:(NSData *_Nonnull)plaintext;
 - (BOOL)encryptWithKey:(NSData *_Nonnull)rawKey
              plaintext:(NSData *_Nonnull)plaintext
            destination:(NSMutableData *_Nonnull)destination
              withError:(NSError *_Nullable *_Nullable)error;
++ (BOOL)encryptWithKey:(NSData *_Nonnull)rawKey
+             plaintext:(NSData *_Nonnull)plaintext
+        destinationPtr:(void *_Nonnull)destinationPtr
+     destinationLength:(NSInteger)destinationLength
+             withError:(NSError *_Nullable *_Nullable)error;
 - (NSInteger)decryptedLength:(NSData *_Nonnull)sealedData;
 - (BOOL)decryptWithKey:(NSData *_Nonnull)rawKey
             sealedData:(NSData *_Nonnull)sealedData
            destination:(NSMutableData *_Nonnull)destination
+             withError:(NSError *_Nullable *_Nullable)error;
++ (BOOL)decryptWithKey:(NSData *_Nonnull)rawKey
+            sealedData:(NSData *_Nonnull)sealedData
+        destinationPtr:(void *_Nonnull)destinationPtr
+     destinationLength:(NSInteger)destinationLength
              withError:(NSError *_Nullable *_Nullable)error;
 - (nonnull instancetype)init;
 @end
