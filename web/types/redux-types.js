eb4b2ed4962a514e5322ea57d65ce884606122fd
xsanm <kkurowski.kk@gmail.com>
2023-11-20 16:10:05 +0100

    [web/keyserver] make InitialReduxStateResponder configurable
    Summary:
    The purpose is to start rely on data in DB.
    This change is allow endpoint to return only needed data, avoid fetching data persisted in DB.
    
    This is safe for old clients, as this endpoint is only called after refresh/initial load, and those two things are equal with getting the newest version of the webapp.
    
    This is only about changing endpoint, actual excluding data is implemented later in the stack.
    
    Depends on D9948
    
    Test Plan: Check if app still loads after refresh
    
    Reviewers: tomek, michal
    
    Reviewed By: tomek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D9949

--- web/types/redux-types.js
+++ web/types/redux-types.js
@@ -5,6 +5,7 @@ import type { InviteLinksStore } from 'lib/types/link-types.js';
 import type { MessageStore } from 'lib/types/message-types.js';
 import type { ThreadStore } from 'lib/types/thread-types.js';
 import type { CurrentUserInfo, UserInfos } from 'lib/types/user-types.js';
+import type { URLInfo } from 'lib/utils/url-utils.js';
 
 import type { NavInfo } from '../types/nav-types.js';
 
@@ -39,4 +40,13 @@ export type InitialReduxState = {
 export type InitialKeyserverInfo = {
   +sessionID: ?string,
   +updatesCurrentAsOf: number,
+};
+
+export type ExcludedData = {
+  +threadStore?: boolean,
+};
+
+export type InitialReduxStateRequest = {
+  +urlInfo: URLInfo,
+  +excludedData: ExcludedData,
 };
