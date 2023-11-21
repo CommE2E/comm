83481f348b9edbae755da0278f532d4898980a89
Michal Gniadek <michal.gniadek@swmansion.com>
2023-11-21 13:52:45 +0100

    [native] Implement Android encryption for Rust
    Summary:
    [ENG-5343](https://linear.app/comm/issue/ENG-5343)
    
    Android native implementation of Rust encryption functions. Uses the previosly refactored `ByteBuffer`s.
    
    Depends on D9932, D9938
    
    Test Plan:
    Run this code on a physical Android device:
    ```
    fn test(promise_id: u32) {
      RUNTIME.spawn(async move {
        let f = move || -> Result<String, cxx::Exception> {
          let key = &mut [0; constants::aes::KEY_SIZE];
          ffi::generate_key(key)?;
          let plaintext = &mut [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
          let sealed_data =
            &mut [0; 10 + constants::aes::IV_LENGTH + constants::aes::TAG_LENGTH];
          ffi::encrypt(key, plaintext, sealed_data)?;
          let plaintext2 = &mut [0; 10];
          ffi::decrypt(key, sealed_data, plaintext2)?;
    
          Ok(format!(
            "
        Key: {key:?}
        Plaintext: {plaintext:?}
        Sealed Data: {sealed_data:?}
        Plaintext2: {plaintext2:?}"
          ))
        };
        handle_string_result_as_callback(f(), promise_id);
      });
    }
    ```
    
    Example output:
    ```
    Key: [170, 59, 114, 153, 221, 12, 179, 127, 37, 56, 196, 135, 93, 180, 20, 162, 69, 11, 123, 65, 31, 19, 243, 182, 150, 75, 217, 232, 232, 126, 235, 77]
    Plaintext: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
    Sealed Data: [218, 85, 237, 92, 79, 91, 84, 15, 186, 180, 54, 92, 96, 95, 159, 4, 78, 248, 189, 252, 105, 64, 179, 135, 129, 155, 34, 250, 13, 151, 116, 157, 98, 122, 98, 234, 77, 16]
    Plaintext2: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
    ```
    
    Reviewers: marcin, bartek, kamil
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D9939

--- native/android/app/src/cpp/AESCrypto.cpp
+++ native/android/app/src/cpp/AESCrypto.cpp
@@ -1,20 +1,84 @@
+#include "jniHelpers.h"
 #include <Tools/AESCrypto.h>
+#include <fbjni/ByteBuffer.h>
+#include <fbjni/fbjni.h>
+
+using namespace facebook::jni;
+
+class AESCryptoJavaClass : public JavaClass<AESCryptoJavaClass> {
+public:
+  // app.comm.android.aescrypto.AESCryptoModuleCompat
+  static auto constexpr kJavaDescriptor =
+      "Lapp/comm/android/aescrypto/AESCryptoModuleCompat;";
+
+  static void generateKey(rust::Slice<uint8_t> buffer) {
+    local_ref<JByteBuffer> byteBuffer =
+        JByteBuffer::wrapBytes(buffer.data(), buffer.size());
+
+    static const auto cls = javaClassStatic();
+    static auto method =
+        cls->getStaticMethod<void(local_ref<JByteBuffer>)>("generateKey");
+    method(cls, byteBuffer);
+  }
+
+  static void encrypt(
+      rust::Slice<uint8_t> key,
+      rust::Slice<uint8_t> plaintext,
+      rust::Slice<uint8_t> sealedData) {
+    local_ref<JByteBuffer> keyBuffer =
+        JByteBuffer::wrapBytes(key.data(), key.size());
+    local_ref<JByteBuffer> plaintextBuffer =
+        JByteBuffer::wrapBytes(plaintext.data(), plaintext.size());
+    local_ref<JByteBuffer> sealedDataBuffer =
+        JByteBuffer::wrapBytes(sealedData.data(), sealedData.size());
+    static const auto cls = javaClassStatic();
+    static auto method = cls->getStaticMethod<void(
+        local_ref<JByteBuffer>,
+        local_ref<JByteBuffer>,
+        local_ref<JByteBuffer>)>("encrypt");
+    method(cls, keyBuffer, plaintextBuffer, sealedDataBuffer);
+  }
+
+  static void decrypt(
+      rust::Slice<uint8_t> key,
+      rust::Slice<uint8_t> sealedData,
+      rust::Slice<uint8_t> plaintext) {
+    local_ref<JByteBuffer> keyBuffer =
+        JByteBuffer::wrapBytes(key.data(), key.size());
+    local_ref<JByteBuffer> sealedDataBuffer =
+        JByteBuffer::wrapBytes(sealedData.data(), sealedData.size());
+    local_ref<JByteBuffer> plaintextBuffer =
+        JByteBuffer::wrapBytes(plaintext.data(), plaintext.size());
+    static const auto cls = javaClassStatic();
+    static auto method = cls->getStaticMethod<void(
+        local_ref<JByteBuffer>,
+        local_ref<JByteBuffer>,
+        local_ref<JByteBuffer>)>("decrypt");
+    method(cls, keyBuffer, sealedDataBuffer, plaintextBuffer);
+  }
+};
 
 namespace comm {
 
 void AESCrypto::generateKey(rust::Slice<uint8_t> buffer) {
+  NativeAndroidAccessProvider::runTask(
+      [&]() { AESCryptoJavaClass::generateKey(buffer); });
 }
 
 void AESCrypto::encrypt(
     rust::Slice<uint8_t> key,
     rust::Slice<uint8_t> plaintext,
     rust::Slice<uint8_t> sealedData) {
+  NativeAndroidAccessProvider::runTask(
+      [&]() { AESCryptoJavaClass::encrypt(key, plaintext, sealedData); });
 }
 
 void AESCrypto::decrypt(
     rust::Slice<uint8_t> key,
     rust::Slice<uint8_t> sealedData,
     rust::Slice<uint8_t> plaintext) {
+  NativeAndroidAccessProvider::runTask(
+      [&]() { AESCryptoJavaClass::decrypt(key, sealedData, plaintext); });
 }
 
 } // namespace comm
