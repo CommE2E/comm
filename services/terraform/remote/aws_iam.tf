c1c0b7820e43031cc2920f7a29c843a9b1bd640d
William Wang <williamwang749@gmail.com>
2023-11-21 16:10:02 -0500

    [terraform] [4/n] search_index_lambda terraform configuration options
    Summary:
    This adds support for search_index_lambda. This lambda consumes our DynamoDB stream for identity-users and then pushes these changes to OpenSearch
    
    Depends on D9936
    
    Test Plan: Verified terraform configuration on staging
    
    Reviewers: varun, bartek
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D9878

--- services/terraform/remote/aws_iam.tf
+++ services/terraform/remote/aws_iam.tf
@@ -196,6 +196,103 @@ resource "aws_iam_role" "reports_service" {
 }
 
 
+data "aws_iam_policy_document" "assume_identity_search_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "search_index_lambda_role" {
+  name               = "search_index_lambda_role"
+  assume_role_policy = data.aws_iam_policy_document.assume_identity_search_role.json
+
+  managed_policy_arns = [
+    aws_iam_policy.manage_cloudwatch_logs.arn,
+    aws_iam_policy.opensearch_domain_access.arn,
+    aws_iam_policy.read_identity_users_stream.arn,
+  ]
+}
+
+data "aws_iam_policy_document" "opensearch_domain_access" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "es:ESHttpHead",
+      "es:ESHttpPost",
+      "es:ESHttpGet",
+      "es:ESHttpDelete",
+      "es:ESHttpPut",
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "opensearch_domain_access" {
+  name        = "opensearch-domain-access"
+  path        = "/"
+  description = "IAM policy for accessing opensearch domain"
+  policy      =  data.aws_iam_policy_document.opensearch_domain_access.json
+}
+
+data "aws_iam_policy_document" "read_identity_users_stream" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "dynamodb:GetRecords",
+      "dynamodb:GetShardIterator",
+      "dynamodb:DescribeStream",
+      "dynamodb:ListStreams",
+    ]
+    resources = [
+      module.shared.dynamodb_tables["identity-users"].arn,
+      module.shared.dynamodb_tables["identity-users"].stream_arn,
+      "${module.shared.dynamodb_tables["identity-users"].arn}/stream/*",
+    ]
+  }
+}
+
+resource "aws_iam_policy" "read_identity_users_stream" {
+  name        = "read-identity-users-stream"
+  path        = "/"
+  description = "IAM policy for managing identity-users stream"
+  policy      = data.aws_iam_policy_document.read_identity_users_stream.json
+}
+
+data "aws_iam_policy_document" "manage_cloudwatch_logs" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = ["arn:aws:logs:*:*:*"]
+  }
+}
+
+resource "aws_iam_policy" "manage_cloudwatch_logs" {
+  name        = "manage-cloudwatch-logs"
+  path        = "/"
+  description = "IAM policy for managing cloudwatch logs"
+  policy      = data.aws_iam_policy_document.manage_cloudwatch_logs.json
+}
+
+resource "aws_iam_role_policy_attachment" "AWSLambdaVPCAccessExecutionRole" {
+    role       = "${aws_iam_role.search_index_lambda_role.name}"
+    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
+
 data "aws_iam_policy_document" "identity-search" {
   statement {
     effect = "Allow"
