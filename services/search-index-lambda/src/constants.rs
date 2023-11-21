dd3e95101e34e719a9092ae5e323a99323268457
William Wang <williamwang749@gmail.com>
2023-11-21 16:09:21 -0500

    [services] [3/n] Rust opensearch indexing lambda
    Summary:
    A lambda written in Rust that consumes a dynamodb stream for identity-users and indexes users into the identity-search OpenSearch instance.
    
    Depends on D9877
    
    Test Plan: Successfully built using `cargo lambda` and deployed with terraform. Confirmed manually the lambda is syncing the stream.
    
    Reviewers: varun, bartek
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D9936

---
+++ services/search-index-lambda/src/constants.rs
@@ -0,0 +1,4 @@
+pub const DYNAMODB_USER_ID_KEY: &str = "userID";
+pub const DYNAMODB_USERNAME_KEY: &str = "username";
+pub const LOG_LEVEL_ENV_VAR: &str =
+  tracing_subscriber::filter::EnvFilter::DEFAULT_ENV;
