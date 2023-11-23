8011c2ffcf597051857f4948cc50233be84e91b3
Ashoat Tevosyan <ashoat@comm.app>
2023-11-23 09:43:38 -0500

    [Flow202][native][skip-ci] [34/x] Type WritableAnimatedStyleObj in ReactionSelectionPopover and Tooltip
    Summary:
    Separating this out because it required some changes. After this change, `AnimatedStyleObj` includes all types from React Native's `View.style` prop.
    
    NOTE: CI will fail on this diff. I considered the possibility of fixing Flow errors BEFORE upgrading Flow, but it wasn't possible... in some cases, the fixes to support the new version of Flow caused errors in the old version. I could have hidden these type errors with `$FlowFixMe` lines and then later revert those, but that seemed like too much busy work.
    
    Depends on D10017
    
    Test Plan: Confirm the Flow errors go away
    
    Reviewers: ginsu
    
    Reviewed By: ginsu
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D10018

--- native/types/react-native.js
+++ native/types/react-native.js
@@ -3,6 +3,7 @@
 import AnimatedInterpolation from 'react-native/Libraries/Animated/nodes/AnimatedInterpolation.js';
 import type ReactNativeAnimatedValue from 'react-native/Libraries/Animated/nodes/AnimatedValue.js';
 import type { ViewToken } from 'react-native/Libraries/Lists/ViewabilityHelper.js';
+import type { ____ViewStyle_Internal } from 'react-native/Libraries/StyleSheet/StyleSheetTypes.js';
 
 export type {
   Layout,
@@ -43,3 +44,5 @@ export type ImagePasteEvent = {
 };
 
 export type { AnimatedInterpolation };
+
+export type ViewStyleObj = ____ViewStyle_Internal;
