CommE2E/comm

commit 226291ba6c44d0d798db268909bd5f264bd5b0b4

[web] start persisting `updatesCurrentAsOf`
Summary:
Some context is here: [ENG-5316](https://linear.app/comm/issue/ENG-5316/start-persisting-data-on-web-database#comment-767efe81).

We want to use the proper timestamp to allow the app to receive updates for data that will be stored in SQLite and longer received on initial load using `getInitialReduxStateResponder`.

This is a breaking change that could cause some issues, so launching this only for staff for now, putting this function in lib because later in the stack it is used also on a keyserver.

Test Plan:
1. For non-staff users make sure `updatesCurrentAsOf` is updated on each reload
2. For staff users, make sure it's persisted and updated after receiving an update
3. Check if the policies mechanism still works (it was connected, we used this to fetch all data after acknowledgment).

Reviewers: tomek, michal, inka

Subscribers: ashoat

Differential Revision: https://phab.comm.dev/D9948

web/redux/persist.js
@@ -275,8 +275,7 @@ const keyserverStoreTransform: Transform = createTransform(
   (state: KeyserverStore): PersistedKeyserverStore => {
     const keyserverInfos = {};
     for (const key in state.keyserverInfos) {
-      const { connection, updatesCurrentAsOf, sessionID, ...rest } =
-        state.keyserverInfos[key];
+      const { connection, sessionID, ...rest } = state.keyserverInfos[key];
       keyserverInfos[key] = rest;
     }
     return {
@@ -290,7 +289,6 @@ const keyserverStoreTransform: Transform = createTransform(
       keyserverInfos[key] = {
         ...state.keyserverInfos[key],
         connection: { ...defaultConnectionInfo },
-        updatesCurrentAsOf: 0,
         sessionID: null,
       };
     }

lib/shared/web-database.js
@@ -1,0 +1,13 @@
+// @flow
+
+import { isStaff } from './staff-utils.js';
+import { isDev } from '../utils/dev-utils.js';
+
+function canUseDatabaseOnWeb(userID: ?string): boolean {
+  if (!userID) {
+    return false;
+  }
+  return isDev || isStaff(userID);
+}
+
+export { canUseDatabaseOnWeb };

web/redux/redux-setup.js
@@ -12,6 +12,7 @@ import baseReducer from 'lib/reducers/master-reducer.js';
 import { mostRecentlyReadThreadSelector } from 'lib/selectors/thread-selectors.js';
 import { isLoggedIn } from 'lib/selectors/user-selectors.js';
 import { invalidSessionDowngrade } from 'lib/shared/session-utils.js';
+import { canUseDatabaseOnWeb } from 'lib/shared/web-database.js';
 import type { Shape } from 'lib/types/core.js';
 import type {
   CryptoStore,
@@ -128,9 +129,13 @@ export function reducer(oldState: AppState | void, action: Action): AppState {
     const { userInfos, keyserverInfos, ...rest } = action.payload;
     const newKeyserverInfos = { ...state.keyserverStore.keyserverInfos };
     for (const keyserverID in keyserverInfos) {
+      const newUpdatesCurrentAsOf = canUseDatabaseOnWeb(rest.currentUserInfo.id)
+        ? newKeyserverInfos[keyserverID].updatesCurrentAsOf
+        : keyserverInfos[keyserverID].updatesCurrentAsOf;
       newKeyserverInfos[keyserverID] = {
         ...newKeyserverInfos[keyserverID],
         ...keyserverInfos[keyserverID],
+        updatesCurrentAsOf: newUpdatesCurrentAsOf,
       };
     }
     return validateState(oldState, {

commit 9286ef7b154988118dffd33eb0c4a7a84fb09c45

[Flow202][web][skip-ci] [22/x] Add updatesCurrentAsOf to PersistedKeyserverInfo
Summary: This type error was introduced in D9948 when we started persisting `updatesCurrentAsOf`.

NOTE: CI will fail on this diff. I considered the possibility of fixing Flow errors BEFORE upgrading Flow, but it wasn't possible... in some cases, the fixes to support the new version of Flow caused errors in the old version. I could have hidden these type errors with `$FlowFixMe` lines and then later revert those, but that seemed like too much busy work.

Depends on D9984

Test Plan: Confirm the Flow errors go away

Reviewers: kamil, tomek, atul

web/redux/persist.js
@@ -264,7 +264,6 @@ type PersistedKeyserverInfo = $Diff<
   KeyserverInfo,
   {
     +connection: ConnectionInfo,
-    +updatesCurrentAsOf: number,
     +sessionID?: ?string,
   },
 >;