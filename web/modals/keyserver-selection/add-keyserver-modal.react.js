21fb6dc564baec1bce029de2d02edc1cf758f4e7
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-11-29 04:28:39 -0500

    [web] implement onClickAddKeyserver callback
    Summary: When a user clicks on the add keyserver button their keyserver should be stored in the keyserver store. `onClickAddKeyserver` handles this funtionality. Additionally, if the user is a staff or in dev mode, we want the input field to be pre-populated with the value of `customServer`
    
    Test Plan: Please see the demo video below
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10094

--- web/modals/keyserver-selection/add-keyserver-modal.react.js
+++ web/modals/keyserver-selection/add-keyserver-modal.react.js
@@ -2,17 +2,32 @@
 
 import * as React from 'react';
 
+import { addKeyserverActionType } from 'lib/actions/keyserver-actions.js';
 import { useModalContext } from 'lib/components/modal-provider.react.js';
+import type { KeyserverInfo } from 'lib/types/keyserver-types.js';
+import { defaultConnectionInfo } from 'lib/types/socket-types.js';
+import { useDispatch } from 'lib/utils/redux-utils.js';
 
 import css from './add-keyserver-modal.css';
 import Button, { buttonThemes } from '../../components/button.react.js';
+import { useSelector } from '../../redux/redux-utils.js';
+import { useStaffCanSee } from '../../utils/staff-utils.js';
 import Input from '../input.react.js';
 import Modal from '../modal.react.js';
 
 function AddKeyserverModal(): React.Node {
   const { popModal } = useModalContext();
 
-  const [keyserverURL, setKeyserverURL] = React.useState<string>('');
+  const dispatch = useDispatch();
+
+  const staffCanSee = useStaffCanSee();
+
+  const currentUserID = useSelector(state => state.currentUserInfo?.id);
+  const customServer = useSelector(state => state.customServer);
+
+  const [keyserverURL, setKeyserverURL] = React.useState<string>(
+    customServer && staffCanSee ? customServer : '',
+  );
 
   const onChangeKeyserverURL = React.useCallback(
     (event: SyntheticEvent<HTMLInputElement>) =>
@@ -21,8 +36,25 @@ function AddKeyserverModal(): React.Node {
   );
 
   const onClickAddKeyserver = React.useCallback(() => {
-    // TODO
-  }, []);
+    const newKeyserverInfo: KeyserverInfo = {
+      cookie: null,
+      updatesCurrentAsOf: 0,
+      urlPrefix: keyserverURL,
+      connection: defaultConnectionInfo,
+      lastCommunicatedPlatformDetails: null,
+      deviceToken: null,
+    };
+
+    dispatch({
+      type: addKeyserverActionType,
+      payload: {
+        keyserverAdminUserID: currentUserID,
+        newKeyserverInfo,
+      },
+    });
+
+    popModal();
+  }, [currentUserID, dispatch, keyserverURL, popModal]);
 
   const addKeyserverModal = React.useMemo(
     () => (
