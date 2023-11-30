cf9df5c696495390e9c3176ababe147e32ec9524
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2023-11-30 12:04:06 -0500

    [identity] increase number of tasks and enable stickiness on load balancer
    Summary:
    Resolves https://linear.app/comm/issue/ENG-5909/increase-number-of-identity-service-nodes-on-stagingprod
    
    We want to get used to thinking of the identity service as a distributed system. Enabling session stickiness lets us send _finish RPC requests to the same task as the corresponding _start requests by setting a cookie.
    
    Test Plan:
    https://gist.github.com/vdhanan/07cd60c9474f76d63fa10ef9d909b8ef
    
    used the lb cookie to send a bunch of requests in quick succession, saw in the staging logs that they went to the same task until the cookie expired, at which point they went to both tasks
    
    Reviewers: bartek!
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10102

--- services/terraform/remote/service_identity.tf
+++ services/terraform/remote/service_identity.tf
@@ -1,5 +1,5 @@
 locals {
-  identity_service_image_tag      = "0.3"
+  identity_service_image_tag      = "0.5"
   identity_service_server_image   = "commapp/identity-server:${local.identity_service_image_tag}"
   identity_service_container_name = "identity-server"
 
@@ -89,10 +89,7 @@ resource "aws_ecs_service" "identity_service" {
   task_definition      = aws_ecs_task_definition.identity_service.arn
   force_new_deployment = true
 
-  desired_count = 1
-  lifecycle {
-    ignore_changes = [desired_count]
-  }
+  desired_count = 2
 
   # Expose Identity service to other services in the cluster
   service_connect_configuration {
@@ -157,6 +154,12 @@ resource "aws_lb_target_group" "identity_service_grpc" {
 
   # The "bridge" network mode requires target type set to instance
   target_type = "instance"
+
+  stickiness {
+    type            = "lb_cookie"
+    cookie_duration = 10 # Duration in seconds
+    enabled         = true
+  }
 
   health_check {
     enabled             = true
