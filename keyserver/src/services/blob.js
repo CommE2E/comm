695703188b9f7c9110819acb35019775c0c322a6
Tomasz Palys <tomasz.palys@swmansion.com>
2023-12-01 12:58:48 +0100

    [keyserver] Change blob upload functions to return more usable results
    Summary:
    We would like to be able to differentiate between hash in use and a plain failure and to achieve that we need a different API
    
    Depends on D10099
    
    Test Plan: Run a script and check if both SUCCESS and HASH_IN_USE results are returned
    
    Reviewers: michal, bartek, rohan
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D10131

--- keyserver/src/services/blob.js
+++ keyserver/src/services/blob.js
@@ -5,14 +5,27 @@ import {
   getBlobFetchableURL,
   makeBlobServiceEndpointURL,
 } from 'lib/utils/blob-service.js';
-import { getMessageForException } from 'lib/utils/errors.js';
 
 type BlobDescriptor = {
   +hash: string,
   +holder: string,
 };
 
-async function uploadBlob(blob: Blob, hash: string): Promise<void> {
+type BlobOperationResult =
+  | {
+      +success: true,
+    }
+  | {
+      +success: false,
+      +reason: 'HASH_IN_USE' | 'OTHER',
+      +status: number,
+      +statusText: string,
+    };
+
+async function uploadBlob(
+  blob: Blob,
+  hash: string,
+): Promise<BlobOperationResult> {
   const formData = new FormData();
   formData.append('blob_hash', hash);
   formData.append('blob_data', blob);
@@ -27,11 +40,21 @@ async function uploadBlob(blob: Blob, hash: string): Promise<void> {
 
   if (!uploadBlobResponse.ok) {
     const { status, statusText } = uploadBlobResponse;
-    throw new Error(`Payload upload failed with HTTP ${status}: ${statusText}`);
+    const reason = status === 409 ? 'HASH_IN_USE' : 'OTHER';
+    return {
+      success: false,
+      reason,
+      status,
+      statusText,
+    };
   }
+
+  return { success: true };
 }
 
-async function assignHolder(params: BlobDescriptor): Promise<void> {
+async function assignHolder(
+  params: BlobDescriptor,
+): Promise<BlobOperationResult> {
   const { hash, holder } = params;
   const assignHolderResponse = await fetch(
     makeBlobServiceEndpointURL(blobService.httpEndpoints.ASSIGN_HOLDER),
@@ -49,23 +72,39 @@ async function assignHolder(params: BlobDescriptor): Promise<void> {
 
   if (!assignHolderResponse.ok) {
     const { status, statusText } = assignHolderResponse;
-    throw new Error(
-      `Holder assignment failed with HTTP ${status}: ${statusText}`,
-    );
+    return { success: false, reason: 'OTHER', status, statusText };
   }
+
+  return { success: true };
 }
 
-async function upload(blob: Blob, params: BlobDescriptor): Promise<void> {
+async function upload(
+  blob: Blob,
+  params: BlobDescriptor,
+): Promise<
+  | {
+      +success: true,
+    }
+  | {
+      +success: false,
+      +assignHolderResult: BlobOperationResult,
+      +uploadBlobResult: BlobOperationResult,
+    },
+> {
   const { hash, holder } = params;
-  try {
-    await Promise.all([assignHolder({ hash, holder }), uploadBlob(blob, hash)]);
-  } catch (e) {
-    throw new Error(
-      `Payload upload failed with: ${
-        getMessageForException(e) ?? 'unknown error'
-      }`,
-    );
+
+  const [holderResult, uploadResult] = await Promise.all([
+    assignHolder({ hash, holder }),
+    uploadBlob(blob, hash),
+  ]);
+  if (holderResult.success && uploadResult.success) {
+    return { success: true };
   }
+  return {
+    success: false,
+    assignHolderResult: holderResult,
+    uploadBlobResult: uploadResult,
+  };
 }
 
 export type BlobDownloadResult =
