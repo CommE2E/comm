3f4b64781e1008ee42c60f8b73a0b6d5a3898eb1
Ashoat Tevosyan <ashoat@comm.app>
2023-11-30 23:15:18 -0500

    [web] Don't crash if electron not defined in useCreateDesktopPushSubscription
    Summary:
    Context in [ENG-6000](https://linear.app/comm/issue/ENG-6000/fetchdevicetoken-is-not-a-function). This diff fixes a regression introduced in D9970.
    
    Test Plan: I deployed this fix to my keyserver in production and it resolved the issue described on Linear.
    
    Reviewers: marcin, michal

--- web/push-notif/push-notifs-handler.js
+++ web/push-notif/push-notifs-handler.js
@@ -48,7 +48,7 @@ function useCreateDesktopPushSubscription() {
   );
 
   React.useEffect(() => {
-    electron?.fetchDeviceToken();
+    electron?.fetchDeviceToken?.();
   }, []);
 
   React.useEffect(
