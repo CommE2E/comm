CommE2E/comm

commit 7c941860b36b73fbbbdbf418b1c3c42a90b9b75f

Fix MacOS app loosing device token in undeterministic manner Differential Revision: https://phab.comm.dev/D9970

web/push-notif/push-notifs-handler.js
@@ -44,6 +44,10 @@ function useCreateDesktopPushSubscription() {
     [callSetDeviceToken, dispatchActionPromise],
   );
 
+  React.useEffect(() => {
+    electron?.fetchDeviceToken();
+  }, []);
+
   const dispatch = useDispatch();
 
   React.useEffect(

desktop/src/main.js
@@ -253,10 +253,16 @@ const show = (urlPath?: string) => {
       main.show();
 
       if (app.isPackaged) {
-        (async () => {
+        const sendDeviceTokenToWebApp = async () => {
           const token = await registerForNotifications();
           main.webContents.send('on-device-token-registered', token);
-        })();
+        };
+
+        sendDeviceTokenToWebApp();
+
+        ipcMain.on('fetch-device-token', () => {
+          sendDeviceTokenToWebApp();
+        });
       }
     }
   });

desktop/src/preload.js
@@ -35,6 +35,7 @@ const bridge: ElectronBridge = {
     return () =>
       ipcRenderer.removeListener('on-notification-clicked', withEvent);
   },
+  fetchDeviceToken: () => ipcRenderer.send('fetch-device-token'),
 };
 
 contextBridge.exposeInMainWorld('electronContextBridge', bridge);

lib/types/electron-types.js
@@ -24,4 +24,5 @@ export type ElectronBridge = {
   +platform?: 'windows' | 'macos',
   +onDeviceTokenRegistered?: OnDeviceTokenRegisteredListener => () => void,
   +onNotificationClicked?: OnNotificationClickedListener => () => void,
+  +fetchDeviceToken: () => void,
 };

commit 4b2eabc69d4aa379c4a3d58f80bb1b4091978b85

[web] Don't crash if electron.fetchDeviceToken not defined in useCreateDesktopPushSubscription
Summary: Context in [ENG-6000](https://linear.app/comm/issue/ENG-6000/fetchdevicetoken-is-not-a-function). This diff fixes a regression introduced in D9970.

Test Plan: I deployed this fix to my keyserver in production and it resolved the issue described on Linear.

Reviewers: marcin, michal

Reviewed By: marcin

Subscribers: tomek

Differential Revision: https://phab.comm.dev/D10129

web/push-notif/push-notifs-handler.js
@@ -48,7 +48,7 @@ function useCreateDesktopPushSubscription() {
   );
 
   React.useEffect(() => {
-    electron?.fetchDeviceToken();
+    electron?.fetchDeviceToken?.();
   }, []);
 
   React.useEffect(