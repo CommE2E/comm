3328a732bdfe580343f978099744f464ee97bf27
Ashoat Tevosyan <ashoat@comm.app>
2023-12-02 16:41:55 -0500

    [keyserver] Deprecate streamJSON
    Summary:
    The only usages of this function were removed in D9145. Removing it from the codebase allows us to rip out three NPM packages.
    
    Depends on D10165
    
    Test Plan: Flow
    
    Reviewers: michal, kamil
    
    Reviewed By: kamil
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D10166

--- keyserver/src/utils/json-stream.js
+++ keyserver/src/utils/json-stream.js
@@ -1,50 +1,9 @@
 // @flow
 
-import type { $Response } from 'express';
-import JSONStream from 'JSONStream';
-import replaceStream from 'replacestream';
-import Combine from 'stream-combiner';
-
-function streamJSON(
-  res: $Response,
-  input: { +[key: string]: mixed },
-): stream$Readable {
-  const jsonStream = Combine(
-    JSONStream.stringifyObject('{', ',', '}'),
-    replaceStream(/</g, '\\u003c'),
-  );
-  jsonStream.pipe(res, { end: false });
-  resolvePromisesToStream(jsonStream, input);
-  return jsonStream;
-}
-
-function resolvePromisesToStream(
-  stream: { +write: ([string, mixed]) => mixed, +end: () => mixed, ... },
-  input: { +[key: string]: mixed },
-) {
-  const blocking = [];
-  for (const key in input) {
-    const value = input[key];
-    if (value instanceof Promise) {
-      blocking.push(
-        (async () => {
-          const result = await value;
-          stream.write([key, result]);
-        })(),
-      );
-    } else {
-      stream.write([key, value]);
-    }
-  }
-  Promise.all(blocking).then(() => {
-    stream.end();
-  });
-}
-
 function waitForStream(readable: stream$Readable): Promise<void> {
   return new Promise(r => {
     readable.on('end', r);
   });
 }
 
-export { streamJSON, waitForStream };
+export { waitForStream };
