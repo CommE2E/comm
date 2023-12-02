aac431ec1679290dc308a1ec9ceca00b63045ec9
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2023-12-02 18:49:51 -0500

    [keyserver] try to get identity config from file if env var is not present
    Summary:
    Depends on D10147
    
    keep the env var logic in place for the Docker workflow, but now also try to get the JSON from a file for normal `yarn dev` workflow
    
    Test Plan: tried both Docker and `yarn dev` workflows, saw the expected logs and confirmed that the socket address was the fake one i set in the config
    
    Reviewers: bartek, ashoat
    
    Reviewed By: bartek
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10148

---
+++ keyserver/addons/rust-node-addon/src/identity_client/config.rs
@@ -0,0 +1,77 @@
+use super::IdentityServiceConfig;
+use serde::Deserialize;
+use serde_json;
+use std::env;
+use std::fs;
+use std::path::{Path, PathBuf};
+use tracing::warn;
+
+pub(super) fn get_identity_service_config(
+) -> Result<IdentityServiceConfig, Box<dyn std::error::Error>> {
+  const IDENTITY_SERVICE_CONFIG_NAME: ConfigName = ConfigName {
+    folder: ConfigFolder::Secrets,
+    name: "identity_service_config",
+  };
+  let config = get_config(&IDENTITY_SERVICE_CONFIG_NAME)?;
+  Ok(config)
+}
+
+#[derive(Debug)]
+enum ConfigFolder {
+  Secrets,
+  #[allow(dead_code)]
+  Facts,
+}
+
+impl ConfigFolder {
+  fn as_str(&self) -> &'static str {
+    match self {
+      ConfigFolder::Secrets => "secrets",
+      ConfigFolder::Facts => "facts",
+    }
+  }
+}
+
+struct ConfigName {
+  folder: ConfigFolder,
+  name: &'static str,
+}
+
+impl ConfigName {
+  fn get_key_for_config_name(&self) -> String {
+    format!("COMM_JSONCONFIG_{}_{}", self.folder.as_str(), self.name)
+  }
+
+  fn get_path_for_config_name(&self) -> PathBuf {
+    Path::new(self.folder.as_str()).join(format!("{}.json", self.name))
+  }
+}
+
+fn get_config<T: for<'de> Deserialize<'de>>(
+  config_name: &ConfigName,
+) -> Result<T, Box<dyn std::error::Error>> {
+  let key = config_name.get_key_for_config_name();
+
+  match env::var(&key) {
+    Ok(env_value) => match serde_json::from_str::<T>(&env_value) {
+      Ok(config) => return Ok(config),
+      Err(e) => {
+        warn!("Failed to deserialize env value '{}': {}", env_value, e);
+      }
+    },
+    Err(e) => {
+      warn!("Failed to read config from env var '{}': {}", key, e);
+    }
+  }
+
+  let path = config_name.get_path_for_config_name();
+  let file_content = fs::read_to_string(&path).map_err(|e| {
+    warn!("Failed to read config file '{}': {}", path.display(), e);
+    e
+  })?;
+
+  serde_json::from_str::<T>(&file_content).map_err(|e| {
+    warn!("Failed to deserialize file content: {}", e);
+    e.into()
+  })
+}
