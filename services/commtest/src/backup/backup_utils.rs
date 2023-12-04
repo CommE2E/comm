1cadde3e5aa5820dc9d45969b564a084165f0bc1
Michal Gniadek <michal.gniadek@swmansion.com>
2023-12-04 14:34:45 +0100

    [commtest] Move backup client to shared
    Summary:
    [ENG-5555](https://linear.app/comm/issue/ENG-5555) and [ENG-5554](https://linear.app/comm/issue/ENG-5554)
    
    Move the backup client to shared so that it can be used on native. There are a few changes that I will annotate in inline comments. The total amount of moved logic is pretty small but if it's hard to review, let me know and I will split this into more diffs.
    
    Test Plan:
    - `cargo run` in backup and blob
    - `yarn run-integration-tests backup`
    - `yarn run-performance-tests blob`
    
    Reviewers: bartek, varun, kamil
    
    Reviewed By: bartek, varun
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10260

--- services/commtest/src/backup/backup_utils.rs
+++
@@ -1,9 +0,0 @@
-#[derive(Debug, Clone)]
-pub struct BackupData {
-  pub backup_id: String,
-  pub user_keys_hash: String,
-  pub user_keys: Vec<u8>,
-  pub user_data_hash: String,
-  pub user_data: Vec<u8>,
-  pub attachments: Vec<String>,
-}
