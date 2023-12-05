95bd063a612f8b9937534a057afced1b353d00b5
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2023-12-05 00:59:54 -0500

    [native] use build configuration to set socket addr
    Summary:
    Depends on D10186
    
    Set the default socket address depending on the build configuration. We use staging for debug builds and prod for release builds. This mirrors the defaults on keyserver.
    
    Test Plan: the newly added unit tests passed. ran them with `cargo test` and `cargo test --release` to trigger both tests.
    
    Reviewers: bartek
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10187

--- native/native_rust_library/src/constants.rs
+++ native/native_rust_library/src/constants.rs
@@ -4,3 +4,34 @@ pub mod aes {
   pub const IV_LENGTH: usize = 12; // bytes - unique Initialization Vector (nonce)
   pub const TAG_LENGTH: usize = 16; // bytes - GCM auth tag
 }
+
+#[cfg(debug_assertions)]
+pub const DEFAULT_SOCKET_ADDR: &str =
+  "https://identity.staging.commtechnologies.org:50054";
+
+#[cfg(not(debug_assertions))]
+pub const DEFAULT_SOCKET_ADDR: &str =
+  "https://identity.commtechnologies.org:50054";
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[cfg(debug_assertions)]
+  #[test]
+  fn test_default_socket_addr_debug() {
+    assert_eq!(
+      DEFAULT_SOCKET_ADDR,
+      "https://identity.staging.commtechnologies.org:50054"
+    );
+  }
+
+  #[cfg(not(debug_assertions))]
+  #[test]
+  fn test_default_socket_addr_release() {
+    assert_eq!(
+      DEFAULT_SOCKET_ADDR,
+      "https://identity.commtechnologies.org:50054"
+    );
+  }
+}
