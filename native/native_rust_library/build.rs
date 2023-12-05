5f27e4d5441eea599867ff77c8b5f30f9b7ccc55
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2023-12-05 00:59:54 -0500

    [native] use build configuration to set socket addr
    Summary:
    Depends on D10186
    
    Set the default socket address depending on the build configuration. We use staging for debug builds and prod for release builds. This mirrors the defaults on keyserver.
    
    Test Plan: the newly added unit tests passed. ran them with `cargo test` and `cargo test --release` to trigger both tests.
    
    Reviewers: bartek
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10187

--- native/native_rust_library/build.rs
+++ native/native_rust_library/build.rs
@@ -1,7 +1,29 @@
 use regex::Regex;
+use serde::{Deserialize, Serialize};
 use std::env;
 use std::fs;
 use std::path::Path;
+
+const DEFAULT_DEBUG_IDENTITY_SOCKET_ADDR: &str =
+  "https://identity.staging.commtechnologies.org:50054";
+const DEFAULT_RELEASE_IDENTITY_SOCKET_ADDR: &str =
+  "https://identity.commtechnologies.org:50054";
+const IDENTITY_SERVICE_CONFIG_PATH: &str =
+  "../facts/identity_service_config.json";
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct IdentityServiceConfig {
+  identity_socket_addr: String,
+}
+
+fn get_identity_service_config(
+) -> Result<IdentityServiceConfig, Box<dyn std::error::Error>> {
+  let path = Path::new(IDENTITY_SERVICE_CONFIG_PATH);
+  let file_content = fs::read_to_string(path)?;
+
+  serde_json::from_str(&file_content).map_err(|e| e.into())
+}
 
 fn main() {
   let _cxx_build =
@@ -17,14 +39,14 @@ fn main() {
     .lines()
     .find(|line| line.contains("const int codeVersion"))
     .expect("Failed to find codeVersion line");
-  println!("Version line: {}", version_line);
 
   // The regex searches for the string "const int codeVersion", followed by any
   // number of whitespace characters, an escaped opening curly brace, more
   // optional whitespace, a series of one or more digits (which it captures),
   // some more optional whitespace, an escaped closing curly brace, and finally
   // a semicolon.
-  let re = Regex::new(r"const int codeVersion\s*\{\s*(\d+)\s*\};").unwrap();
+  let re = Regex::new(r"const int codeVersion\s*\{\s*(\d+)\s*\};")
+    .expect("Failed to compile regular expression");
   let version: u64 = re
     .captures(version_line)
     .and_then(|cap| cap.get(1))
@@ -37,15 +59,40 @@ fn main() {
       },
     );
 
-  let out_dir = env::var("OUT_DIR").unwrap();
-  let rust_path = Path::new(&out_dir).join("version.rs");
+  let out_dir = env::var("OUT_DIR").expect("Error fetching OUT_DIR env var");
+  let version_path = Path::new(&out_dir).join("version.rs");
 
   fs::write(
-    rust_path,
+    version_path,
     format!("pub const CODE_VERSION: u64 = {};", version),
   )
   .expect("Failed to write version.rs");
 
+  let identity_socket_addr = match get_identity_service_config() {
+    Ok(config) => config.identity_socket_addr,
+    Err(_) => {
+      let profile =
+        env::var("PROFILE").expect("Error fetching PROFILE env var");
+      if profile == "release" {
+        DEFAULT_RELEASE_IDENTITY_SOCKET_ADDR.to_string()
+      } else {
+        DEFAULT_DEBUG_IDENTITY_SOCKET_ADDR.to_string()
+      }
+    }
+  };
+
+  let socket_config_path = Path::new(&out_dir).join("socket_config.rs");
+
+  fs::write(
+    socket_config_path,
+    format!(
+      "pub const IDENTITY_SOCKET_ADDR: &str = \"{}\";",
+      identity_socket_addr
+    ),
+  )
+  .expect("Failed to write socket_config.rs");
+
   println!("cargo:rerun-if-changed=src/lib.rs");
   println!("cargo:rerun-if-changed={}", HEADER_PATH);
+  println!("cargo:rerun-if-changed={}", IDENTITY_SERVICE_CONFIG_PATH);
 }
