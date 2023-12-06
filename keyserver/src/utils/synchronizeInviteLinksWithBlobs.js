c5004531217e4d53d172f9689836180d5765d888
Tomasz Palys <tomasz.palys@swmansion.com>
2023-12-06 10:40:44 +0100

    [keyserver] Add a function that synchronizes keyserver's invite links with blobs
    Summary:
    In this script we try to create blobs for links where a holder is missing or a holder doesn't point to an existing blob. This function will be used in a migration and in a script that periodically synchronizes data.
    
    Depends on D10150
    
    Test Plan: Run the function as a script and check if it creates blobs for all the links that don't have them.
    
    Reviewers: michal, bartek, rohan
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D10205

---
+++ keyserver/src/utils/synchronizeInviteLinksWithBlobs.js
@@ -0,0 +1,44 @@
+// @flow
+
+import uuid from 'uuid';
+
+import { inviteLinkBlobHash } from 'lib/shared/invite-links.js';
+import type { InviteLinkWithHolder } from 'lib/types/link-types.js';
+
+import {
+  getInviteLinkBlob,
+  uploadInviteLinkBlob,
+} from '../creators/invite-link-creator.js';
+import { fetchAllPrimaryInviteLinks } from '../fetchers/link-fetchers.js';
+import { setLinkHolder } from '../updaters/link-updaters.js';
+
+async function synchronizeInviteLinksWithBlobs() {
+  const links = await fetchAllPrimaryInviteLinks();
+  const promises = [];
+  for (const link: InviteLinkWithHolder of links) {
+    promises.push(
+      (async () => {
+        const isHolderPresent = !!link.blobHolder;
+        const holder = link.blobHolder ?? uuid.v4();
+        if (isHolderPresent) {
+          const blobFetchResult = await getInviteLinkBlob(
+            inviteLinkBlobHash(link.name),
+          );
+          if (blobFetchResult.found) {
+            return;
+          }
+        }
+        const uploadResult = await uploadInviteLinkBlob(
+          inviteLinkBlobHash(link.name),
+          holder,
+        );
+        if (uploadResult.success && !isHolderPresent) {
+          await setLinkHolder(link.name, holder);
+        }
+      })(),
+    );
+  }
+  await Promise.all(promises);
+}
+
+export { synchronizeInviteLinksWithBlobs };
