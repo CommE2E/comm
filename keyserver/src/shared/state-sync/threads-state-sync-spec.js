4d0c307cc1e460e9116c1ee96be2490721e99b5a
Atul Madhugiri <atulm@berkeley.edu>
2023-12-06 17:38:00 -0500

    [lib] Introduce `rawThreadInfoValidator` and consume in `threads-state-sync-spec`
    Summary:
    Introduce validator that can handle both `LegacyRawThreadInfo` and `MinimallyEncodedRawThreadInfo` because `keyserver` will be handling both.
    
    Updated validator was required to update `threads-state-sync-spec` which will allow us to update rest of thread update/thread "ops" code.
    
    ---
    
    Depends on D10210
    
    Test Plan:
    Flow/CI/validator will fail during testing if not defined correctly.
    
    Reviewers:
    ashoat, ginsu, tomek, rohan

--- keyserver/src/shared/state-sync/threads-state-sync-spec.js
+++ keyserver/src/shared/state-sync/threads-state-sync-spec.js
@@ -1,11 +1,13 @@
 // @flow
 
+import { rawThreadInfoValidator } from 'lib/permissions/minimally-encoded-thread-permissions-validators.js';
 import { threadsStateSyncSpec as libSpec } from 'lib/shared/state-sync/threads-state-sync-spec.js';
 import type { ClientThreadInconsistencyReportCreationRequest } from 'lib/types/report-types.js';
 import {
   type LegacyRawThreadInfos,
   type LegacyRawThreadInfo,
-  legacyRawThreadInfoValidator,
+  type RawThreadInfo,
+  type RawThreadInfos,
 } from 'lib/types/thread-types.js';
 import { hash, combineUnorderedHashes, values } from 'lib/utils/objects.js';
 
@@ -40,10 +42,10 @@ async function fetch(viewer: Viewer, ids?: $ReadOnlySet<string>) {
   return result.threadInfos;
 }
 
-function getServerInfosHash(infos: LegacyRawThreadInfos) {
+function getServerInfosHash(infos: RawThreadInfos) {
   return combineUnorderedHashes(values(infos).map(getServerInfoHash));
 }
 
-function getServerInfoHash(info: LegacyRawThreadInfo) {
-  return hash(validateOutput(null, legacyRawThreadInfoValidator, info));
+function getServerInfoHash(info: RawThreadInfo) {
+  return hash(validateOutput(null, rawThreadInfoValidator, info));
 }
