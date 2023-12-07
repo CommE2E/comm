b819c205ec80d1948f6f49f9bdbb21c02e4179f6
xsanm <kkurowski.kk@gmail.com>
2023-12-07 17:09:09 +0100

    [web] add missing `userID` param
    Summary: Context in [ENG-5910](https://linear.app/comm/issue/ENG-5910/web-threadstore-missing-threads-causes-crash).
    
    Test Plan:
    1. Stop processing thread store ops from reducer (to make sure logout does not fill state).
    2. Make sure table is empty.
    3. Reload app and check if threads are fetched from keyserver and persisted in DB.
    4. Reload again and threads should be read from DB.
    
    Reviewers: tomek, michal
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D10222

--- web/redux/initial-state-gate.js
+++ web/redux/initial-state-gate.js
@@ -70,7 +70,7 @@ function InitialReduxStateGate(props: Props): React.Node {
           });
 
           const currentLoggedInUserID = payload.currentUserInfo?.anonymous
-            ? undefined
+            ? null
             : payload.currentUserInfo?.id;
           const useDatabase = canUseDatabaseOnWeb(currentLoggedInUserID);
 
@@ -105,13 +105,16 @@ function InitialReduxStateGate(props: Props): React.Node {
               },
             }));
 
-            await processDBStoreOperations({
-              threadStoreOperations,
-              draftStoreOperations: [],
-              messageStoreOperations: [],
-              reportStoreOperations: [],
-              userStoreOperations: [],
-            });
+            await processDBStoreOperations(
+              {
+                threadStoreOperations,
+                draftStoreOperations: [],
+                messageStoreOperations: [],
+                reportStoreOperations: [],
+                userStoreOperations: [],
+              },
+              currentLoggedInUserID,
+            );
           }
           dispatch({ type: setInitialReduxState, payload });
         } catch (err) {
