73655e8bb06cf4b3279aacf304ca5dce3681393a
Tomasz Palys <tomasz.palys@swmansion.com>
2023-12-08 18:05:39 +0100

    [lib] Modify link verification to call the proper keyserver
    Summary:
    This diff allows calling any keyserver to check if a link is valid.
    
    https://linear.app/comm/issue/ENG-5423/join-a-community-on-a-given-keyserver
    
    Depends on D10206
    
    Test Plan: Check if clicking an invite link opens a modal on web. Also checked if providing an explicit keyserver id works.
    
    Reviewers: michal, bartek, rohan, inka
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D10290

--- native/navigation/deep-links-context-provider.react.js
+++ native/navigation/deep-links-context-provider.react.js
@@ -6,7 +6,7 @@ import * as React from 'react';
 import { Linking, Platform } from 'react-native';
 
 import {
-  verifyInviteLink,
+  useVerifyInviteLink,
   verifyInviteLinkActionTypes,
 } from 'lib/actions/link-actions.js';
 import {
@@ -16,7 +16,6 @@ import {
 } from 'lib/facts/links.js';
 import { isLoggedIn } from 'lib/selectors/user-selectors.js';
 import type { SetState } from 'lib/types/hook-types.js';
-import { useLegacyAshoatKeyserverCall } from 'lib/utils/action-utils.js';
 import { useDispatchActionPromise } from 'lib/utils/redux-promise-utils.js';
 
 import {
@@ -82,7 +81,7 @@ function DeepLinksContextProvider(props: Props): React.Node {
 
   const loggedIn = useSelector(isLoggedIn);
   const dispatchActionPromise = useDispatchActionPromise();
-  const validateLink = useLegacyAshoatKeyserverCall(verifyInviteLink);
+  const validateLink = useVerifyInviteLink();
   const navigation = useNavigation();
   React.useEffect(() => {
     void (async () => {
