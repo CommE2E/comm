bc7527bef2138db5c240d3b52d1e44c273133dd6
xsanm <kkurowski.kk@gmail.com>
2023-12-12 19:57:56 +0100

    [native] update Tunnelbroker init message to rely on Identity `userID`
    Summary:
    To authenticate we need to use ID assigned by identity.
    
    Test Plan:
    1. Login to staging Identity (`loginPasswordUser`).
    2. Call `setCommServicesAuthMetadata`.
    3. Dispatch action with new access token.
    4. Check it connection with staging Tunnelbroker was created.
    
    Reviewers: marcin, varun, michal

--- native/utils/tunnelbroker-utils.js
+++ native/utils/tunnelbroker-utils.js
@@ -5,19 +5,23 @@ import * as React from 'react';
 import type { ConnectionInitializationMessage } from 'lib/types/tunnelbroker/session-types.js';
 
 import { getContentSigningKey } from './crypto-utils.js';
+import { commCoreModule } from '../native-modules.js';
 import { useSelector } from '../redux/redux-utils.js';
 
 function useTunnelbrokerInitMessage(): ?ConnectionInitializationMessage {
   const [deviceID, setDeviceID] = React.useState<?string>();
-  const userID = useSelector(state => state.currentUserInfo?.id);
+  const [userID, setUserID] = React.useState<?string>();
   const accessToken = useSelector(state => state.commServicesAccessToken);
 
   React.useEffect(() => {
     void (async () => {
-      const contentSigningKey = await getContentSigningKey();
+      const [contentSigningKey, { userID: identityUserID }] = await Promise.all(
+        [getContentSigningKey(), commCoreModule.getCommServicesAuthMetadata()],
+      );
       setDeviceID(contentSigningKey);
+      setUserID(identityUserID);
     })();
-  }, []);
+  }, [accessToken]);
 
   return React.useMemo(() => {
     if (!deviceID || !accessToken || !userID) {
