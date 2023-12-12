85106eb6307400b013ff477b9c2c11241e4b0186
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-12-12 04:05:50 -0500

    [web] cleanup thread settings media gallery modal
    Summary: This diff cleans up the one off styles in the thread settings media gallery modal and replaces it with styles of the redesigned modal.
    
    Test Plan: Please see screenshots below
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10324

--- web/modals/threads/gallery/thread-settings-media-gallery.react.js
+++ web/modals/threads/gallery/thread-settings-media-gallery.react.js
@@ -120,6 +120,14 @@ function ThreadSettingsMediaGalleryModal(
       );
     }
 
+    if (filteredMediaInfos.length === 0) {
+      return (
+        <div className={css.noMedia}>
+          No {tab === 'All' ? 'media' : tab.toLowerCase()} in this chat.
+        </div>
+      );
+    }
+
     return filteredMediaInfos.map((media, i) => {
       let imageSource;
       if (media.type === 'photo') {
@@ -197,8 +205,7 @@ function ThreadSettingsMediaGalleryModal(
 
   const threadSettingsMediaGalleryModal = React.useMemo(
     () => (
-      <Modal name={modalName} onClose={onClose} size="large">
-        {tabs}
+      <Modal name={modalName} subheader={tabs} onClose={onClose} size="large">
         {tabContent}
       </Modal>
     ),
