5c9641ec16f6409a35e13b09b806cd3c4e22b4bc
xsanm <kkurowski.kk@gmail.com>
2023-12-12 19:57:56 +0100

    [native] update Tunnelbroker init message to rely on Identity `userID`
    Summary:
    To authenticate we need to use ID assigned by identity.
    I added `accessToken` as a dependency to fetch new Services Metadata each time CSAT changes (it should be caused by the event emitter that @marcin is implementing) so I think this should be safe and avoid any race conditions.
    
    Depends on D10327
    
    Test Plan:
    1. Login to staging Identity (`loginPasswordUser`).
    2. Call `setCommServicesAuthMetadata`.
    3. Dispatch action with the new access token.
    4. Check if the connection with staging Tunnelbroker was created.
    
    Reviewers: marcin, varun, michal

--- native/utils/tunnelbroker-utils.js
+++ native/utils/tunnelbroker-utils.js
@@ -4,20 +4,22 @@ import * as React from 'react';
 
 import type { ConnectionInitializationMessage } from 'lib/types/tunnelbroker/session-types.js';
 
-import { getContentSigningKey } from './crypto-utils.js';
+import { commCoreModule } from '../native-modules.js';
 import { useSelector } from '../redux/redux-utils.js';
 
 function useTunnelbrokerInitMessage(): ?ConnectionInitializationMessage {
   const [deviceID, setDeviceID] = React.useState<?string>();
-  const userID = useSelector(state => state.currentUserInfo?.id);
+  const [userID, setUserID] = React.useState<?string>();
   const accessToken = useSelector(state => state.commServicesAccessToken);
 
   React.useEffect(() => {
     void (async () => {
-      const contentSigningKey = await getContentSigningKey();
+      const { userID: identityUserID, deviceID: contentSigningKey } =
+        await commCoreModule.getCommServicesAuthMetadata();
       setDeviceID(contentSigningKey);
+      setUserID(identityUserID);
     })();
-  }, []);
+  }, [accessToken]);
 
   return React.useMemo(() => {
     if (!deviceID || !accessToken || !userID) {
