494d7575b1a5b4edb9d3343027cecc244b674f24
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-12-12 02:18:13 -0500

    [web] cleanup account delete modal
    Summary: This diff cleans up the one off styles in the account delete modal and replaces it with styles of the redesigned modal.
    
    Test Plan: Please see the screenshots below
    
    Reviewers:
    
    Subscribers:

--- web/settings/account-delete-modal.react.js
+++ web/settings/account-delete-modal.react.js
@@ -34,10 +34,13 @@ const AccountDeleteModal: React.ComponentType<{}> = React.memo<{}>(
 
     const [errorMessage, setErrorMessage] = React.useState('');
 
-    let errorMsg;
-    if (errorMessage) {
-      errorMsg = <div className={css.form_error}>{errorMessage}</div>;
-    }
+    const errorMsg = React.useMemo(() => {
+      if (!errorMessage) {
+        return <div className={css.errorPlaceholder} />;
+      }
+
+      return <div className={css.form_error}>{errorMessage}</div>;
+    }, [errorMessage]);
 
     const deleteAction = React.useCallback(async () => {
       try {
@@ -58,12 +61,33 @@ const AccountDeleteModal: React.ComponentType<{}> = React.memo<{}>(
           deleteKeyserverAccountActionTypes,
           deleteAction(),
         );
+        setErrorMessage('unknown error');
       },
-      [dispatchActionPromise, deleteAction],
+      [deleteAction, dispatchActionPromise],
+    );
+
+    const primaryButton = React.useMemo(
+      () => (
+        <Button
+          variant="filled"
+          buttonColor={buttonThemes.danger}
+          type="submit"
+          onClick={onDelete}
+          disabled={inputDisabled}
+        >
+          Delete Account
+        </Button>
+      ),
+      [inputDisabled, onDelete],
     );
 
     return (
-      <Modal name="Delete Account" onClose={popModal} size="large">
+      <Modal
+        name="Delete Account"
+        onClose={popModal}
+        size="large"
+        primaryButton={primaryButton}
+      >
         <div className={css.modal_body}>
           <form method="POST">
             <SWMansionIcon icon="warning-circle" size={22} />
@@ -71,18 +95,7 @@ const AccountDeleteModal: React.ComponentType<{}> = React.memo<{}>(
               Your account will be permanently deleted. There is no way to
               reverse this.
             </p>
-            <div className={css.form_footer}>
-              <Button
-                variant="filled"
-                buttonColor={buttonThemes.danger}
-                type="submit"
-                onClick={onDelete}
-                disabled={inputDisabled}
-              >
-                Delete Account
-              </Button>
-              {errorMsg}
-            </div>
+            {errorMsg}
           </form>
         </div>
       </Modal>
