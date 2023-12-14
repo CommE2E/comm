bdc3eb27e0de37fe17c2afb21f5daed3518fb893
Michal Gniadek <michal.gniadek@swmansion.com>
2023-12-14 13:45:24 +0100

    [backup] Update LogItem struct
    Summary:
    Update the current log item implementation:
    - Changed log id to number (the only dynamodb change)
    - Used `BlobOrDBContent` for content and `Vec<BlobInfo>` for attachments
    - Added `ensure_size_constraints` function
    - Implemented `From` and `TryFrom` for `AttributeValue`
    - Removed existing db operations for log items (the new improved version will be included in the later diffs)
    - Improved constants
    
    Depends on D10405
    
    Test Plan:
    Created log item:
    ```
    LogItem {
      backup_id: "backup_id".to_string(),
      log_id: 124,
      content: BlobOrDBContent::new(vec![0; 2000]),
      attachments: vec![],
    };
    ```
    Called `ensure_size_constraints`, uploaded it to dynamodb and downloaded it again.
    - Checked that blob service wasn't used in this case
    - Checked that the downloaded item was the same
    Changed the dynamodb item size limit to a smaller value (`200` bytes) and repeated the test. This time the content was uploaded to blob service.
    
    Reviewers: bartek, kamil
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10406

--- services/backup/src/database/mod.rs
+++ services/backup/src/database/mod.rs
@@ -1,25 +1,15 @@
 pub mod backup_item;
 pub mod log_item;
 
-use std::collections::HashMap;
-
+use self::backup_item::{BackupItem, OrderedBackupItem};
+use crate::constants::backup_table;
 use aws_sdk_dynamodb::{
   operation::get_item::GetItemOutput,
   types::{AttributeValue, ReturnValue},
 };
 use comm_lib::database::Error;
+use std::collections::HashMap;
 use tracing::{error, trace, warn};
-
-use crate::constants::{
-  backup_table, LOG_TABLE_FIELD_ATTACHMENT_HOLDERS, LOG_TABLE_FIELD_BACKUP_ID,
-  LOG_TABLE_FIELD_DATA_HASH, LOG_TABLE_FIELD_LOG_ID,
-  LOG_TABLE_FIELD_PERSISTED_IN_BLOB, LOG_TABLE_FIELD_VALUE, LOG_TABLE_NAME,
-};
-
-use self::{
-  backup_item::{BackupItem, OrderedBackupItem},
-  log_item::{parse_log_item, LogItem},
-};
 
 #[derive(Clone)]
 pub struct DatabaseClient {
@@ -226,139 +216,5 @@ impl DatabaseClient {
         AttributeValue::S(backup_id.to_string()),
       ),
     ])
-  }
-
-  // log item
-  pub async fn put_log_item(&self, log_item: LogItem) -> Result<(), Error> {
-    let item = HashMap::from([
-      (
-        LOG_TABLE_FIELD_BACKUP_ID.to_string(),
-        AttributeValue::S(log_item.backup_id),
-      ),
-      (
-        LOG_TABLE_FIELD_LOG_ID.to_string(),
-        AttributeValue::S(log_item.log_id),
-      ),
-      (
-        LOG_TABLE_FIELD_PERSISTED_IN_BLOB.to_string(),
-        AttributeValue::Bool(log_item.persisted_in_blob),
-      ),
-      (
-        LOG_TABLE_FIELD_VALUE.to_string(),
-        AttributeValue::S(log_item.value),
-      ),
-      (
-        LOG_TABLE_FIELD_DATA_HASH.to_string(),
-        AttributeValue::S(log_item.data_hash),
-      ),
-      (
-        LOG_TABLE_FIELD_ATTACHMENT_HOLDERS.to_string(),
-        AttributeValue::S(log_item.attachment_holders),
-      ),
-    ]);
-
-    self
-      .client
-      .put_item()
-      .table_name(LOG_TABLE_NAME)
-      .set_item(Some(item))
-      .send()
-      .await
-      .map_err(|e| {
-        error!("DynamoDB client failed to put log item");
-        Error::AwsSdk(e.into())
-      })?;
-
-    Ok(())
-  }
-
-  pub async fn find_log_item(
-    &self,
-    backup_id: &str,
-    log_id: &str,
-  ) -> Result<Option<LogItem>, Error> {
-    let item_key = HashMap::from([
-      (
-        LOG_TABLE_FIELD_BACKUP_ID.to_string(),
-        AttributeValue::S(backup_id.to_string()),
-      ),
-      (
-        LOG_TABLE_FIELD_LOG_ID.to_string(),
-        AttributeValue::S(log_id.to_string()),
-      ),
-    ]);
-
-    match self
-      .client
-      .get_item()
-      .table_name(LOG_TABLE_NAME)
-      .set_key(Some(item_key))
-      .send()
-      .await
-      .map_err(|e| {
-        error!("DynamoDB client failed to find log item");
-        Error::AwsSdk(e.into())
-      })? {
-      GetItemOutput {
-        item: Some(item), ..
-      } => {
-        let log_item = parse_log_item(item)?;
-        Ok(Some(log_item))
-      }
-      _ => Ok(None),
-    }
-  }
-
-  pub async fn find_log_items_for_backup(
-    &self,
-    backup_id: &str,
-  ) -> Result<Vec<LogItem>, Error> {
-    let response = self
-      .client
-      .query()
-      .table_name(LOG_TABLE_NAME)
-      .key_condition_expression("#backupID = :valueToMatch")
-      .expression_attribute_names("#backupID", LOG_TABLE_FIELD_BACKUP_ID)
-      .expression_attribute_values(
-        ":valueToMatch",
-        AttributeValue::S(backup_id.to_string()),
-      )
-      .send()
-      .await
-      .map_err(|e| {
-        error!("DynamoDB client failed to find log items for backup");
-        Error::AwsSdk(e.into())
-      })?;
-
-    if response.count == 0 {
-      return Ok(Vec::new());
-    }
-
-    let mut results: Vec<LogItem> =
-      Vec::with_capacity(response.count() as usize);
-    for item in response.items.unwrap_or_default() {
-      let log_item = parse_log_item(item)?;
-      results.push(log_item);
-    }
-    Ok(results)
-  }
-
-  pub async fn remove_log_item(&self, log_id: &str) -> Result<(), Error> {
-    self
-      .client
-      .delete_item()
-      .table_name(LOG_TABLE_NAME)
-      .key(
-        LOG_TABLE_FIELD_LOG_ID,
-        AttributeValue::S(log_id.to_string()),
-      )
-      .send()
-      .await
-      .map_err(|e| {
-        error!("DynamoDB client failed to remove log item");
-        Error::AwsSdk(e.into())
-      })?;
-
-    Ok(())
   }
 }
