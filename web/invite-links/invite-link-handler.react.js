2d73f0cfadad6c7836a82a89f2d3c63be8505980
Tomasz Palys <tomasz.palys@swmansion.com>
2023-12-14 15:49:42 +0100

    [web][native] Download invite link blob and use it as an override
    Summary:
    Download a blob, parse it and use its content as a keyserver override.
    
    Depends on D10311
    
    Test Plan: Open a link for which a blob is downloaded and check if a blob is downloaded and its content is used correctly.
    
    Reviewers: michal, bartek, rohan, inka
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D10338

--- web/invite-links/invite-link-handler.react.js
+++ web/invite-links/invite-link-handler.react.js
@@ -8,6 +8,8 @@ import {
 } from 'lib/actions/link-actions.js';
 import { useModalContext } from 'lib/components/modal-provider.react.js';
 import { isLoggedIn } from 'lib/selectors/user-selectors.js';
+import { getKeyserverOverrideForAnInviteLink } from 'lib/shared/invite-links.js';
+import type { KeyserverOverride } from 'lib/shared/invite-links.js';
 import { useDispatchActionPromise } from 'lib/utils/redux-promise-utils.js';
 import { useDispatch } from 'lib/utils/redux-utils.js';
 
@@ -17,45 +19,80 @@ import { useSelector } from '../redux/redux-utils.js';
 
 function InviteLinkHandler(): null {
   const inviteSecret = useSelector(state => state.navInfo.inviteSecret);
+  const inviteLinkSecret = React.useRef<?string>(null);
+  const [keyserverOverride, setKeyserverOverride] =
+    React.useState<?KeyserverOverride>(undefined);
   const loggedIn = useSelector(isLoggedIn);
 
   const dispatchActionPromise = useDispatchActionPromise();
   const dispatch = useDispatch();
-  const validateLink = useVerifyInviteLink();
   const { pushModal } = useModalContext();
   React.useEffect(() => {
-    if (!inviteSecret || !loggedIn) {
-      return;
-    }
-    dispatch({
-      type: updateNavInfoActionType,
-      payload: { inviteSecret: null },
-    });
-    const validateLinkPromise = validateLink({ secret: inviteSecret });
-    void dispatchActionPromise(
-      verifyInviteLinkActionTypes,
-      validateLinkPromise,
-    );
     void (async () => {
-      const result = await validateLinkPromise;
-      if (result.status === 'already_joined') {
+      if (!inviteSecret || !loggedIn) {
         return;
       }
+      dispatch({
+        type: updateNavInfoActionType,
+        payload: { inviteSecret: null },
+      });
+      setKeyserverOverride(undefined);
+      inviteLinkSecret.current = inviteSecret;
+
+      try {
+        const newKeyserverOverride =
+          await getKeyserverOverrideForAnInviteLink(inviteSecret);
+        setKeyserverOverride(newKeyserverOverride);
+      } catch (e) {
+        console.error('Error while downloading an invite link blob', e);
+        pushModal(
+          <AcceptInviteModal
+            verificationResponse={{
+              status: 'invalid',
+            }}
+            inviteSecret={inviteSecret}
+          />,
+        );
+      }
+    })();
+  }, [dispatch, inviteSecret, loggedIn, pushModal]);
+
+  const validateLink = useVerifyInviteLink(keyserverOverride);
+  React.useEffect(() => {
+    const secret = inviteLinkSecret.current;
+    if (keyserverOverride === undefined || !secret) {
+      return;
+    }
+    setKeyserverOverride(undefined);
+
+    void (async () => {
+      let result;
+      try {
+        const validateLinkPromise = validateLink({ secret });
+        void dispatchActionPromise(
+          verifyInviteLinkActionTypes,
+          validateLinkPromise,
+        );
+
+        result = await validateLinkPromise;
+        if (result.status === 'already_joined') {
+          return;
+        }
+      } catch (e) {
+        console.error('Error while verifying an invite link', e);
+        result = {
+          status: 'invalid',
+        };
+      }
+
       pushModal(
         <AcceptInviteModal
           verificationResponse={result}
-          inviteSecret={inviteSecret}
+          inviteSecret={secret}
         />,
       );
     })();
-  }, [
-    dispatch,
-    dispatchActionPromise,
-    inviteSecret,
-    loggedIn,
-    pushModal,
-    validateLink,
-  ]);
+  }, [dispatchActionPromise, keyserverOverride, pushModal, validateLink]);
 
   return null;
 }
