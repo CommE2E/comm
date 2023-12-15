33b1a01873c6884fc40dc747492de80d0ad44c72
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2023-12-15 07:12:16 -0500

    [terraform] bump identity service to 0.6
    Summary: i've retagged 0.6-staging as 0.6 on docker hub. will run terraform apply on both staging and prod when i land this diff.
    
    Test Plan: confirmed that the new image is good by calling the DeleteUser RPC which was recently moved to the auth proto.
    
    Reviewers: bartek!
    
    Subscribers:

--- services/terraform/remote/service_identity.tf
+++ services/terraform/remote/service_identity.tf
@@ -1,5 +1,5 @@
 locals {
-  identity_service_image_tag      = "0.5"
+  identity_service_image_tag      = "0.6"
   identity_service_server_image   = "commapp/identity-server:${local.identity_service_image_tag}"
   identity_service_container_name = "identity-server"
 
