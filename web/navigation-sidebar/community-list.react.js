845969a02f4bf15635394881002c1746a162fb3f
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-12-14 23:05:18 -0500

    [web] introduce community list
    Summary:
    This diff introduces the community list component that will be used in the navigation sidebar component (which will be introduced in a future diff)
    
    For context on what this should look like here is a screenshot from the figma:
    {F954850}
    
    Part of https://linear.app/comm/issue/ENG-5947/list-of-all-top-level-communites-communities
    
    Depends on D10359
    
    Test Plan:
    Please see the demo videos below
    
    User with many communities:
    {F954893}
    
    user with not many communities:
    {F954899}
    
    Reviewers: atul, rohan, kamil
    
    Reviewed By: rohan
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10360

---
+++ web/navigation-sidebar/community-list.react.js
@@ -0,0 +1,29 @@
+// @flow
+
+import * as React from 'react';
+
+import { communityThreadSelector } from 'lib/selectors/thread-selectors.js';
+import { useAppendCommunitySuffix } from 'lib/utils/drawer-utils.react.js';
+import { useResolvedThreadInfos } from 'lib/utils/entity-helpers.js';
+
+import CommunityListItem from './community-list-item.react.js';
+import css from './community-list.css';
+import { useSelector } from '../redux/redux-utils.js';
+
+function CommunityList(): React.Node {
+  const communities = useSelector(communityThreadSelector);
+  const resolvedCommunities = useResolvedThreadInfos(communities);
+  const communitiesSuffixed = useAppendCommunitySuffix(resolvedCommunities);
+
+  const communityList = React.useMemo(
+    () =>
+      communitiesSuffixed.map(community => (
+        <CommunityListItem key={community.id} threadInfo={community} />
+      )),
+    [communitiesSuffixed],
+  );
+
+  return <div className={css.container}>{communityList}</div>;
+}
+
+export default CommunityList;
