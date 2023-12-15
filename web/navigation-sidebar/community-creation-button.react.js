ba092a03ef1cd56e2525ad880b1bc0658ffaacde
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-12-14 23:51:54 -0500

    [web] introduce community creation button for navigation sidebar
    Summary: This diff introduces the community creation button that will be rendered in the navigation sidebar footer
    
    Test Plan: Please see the demo video below
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10363

---
+++ web/navigation-sidebar/community-creation-button.react.js
@@ -0,0 +1,35 @@
+// @flow
+
+import * as React from 'react';
+
+import { useModalContext } from 'lib/components/modal-provider.react.js';
+import SWMansionIcon from 'lib/components/SWMansionIcon.react.js';
+
+import css from './community-creation-button.css';
+import CommunityCreationModal from '../sidebar/community-creation/community-creation-modal.react.js';
+import { useNavigationSidebarTooltip } from '../utils/tooltip-action-utils.js';
+
+function CommunityCreationButton(): React.Node {
+  const { pushModal } = useModalContext();
+
+  const onPressCommunityCreationButton = React.useCallback(() => {
+    pushModal(<CommunityCreationModal />);
+  }, [pushModal]);
+
+  const { onMouseEnter, onMouseLeave } = useNavigationSidebarTooltip({
+    tooltipLabel: 'Create community',
+  });
+
+  return (
+    <div
+      className={css.container}
+      onClick={onPressCommunityCreationButton}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      <SWMansionIcon icon="plus-small" size={22} />
+    </div>
+  );
+}
+
+export default CommunityCreationButton;
