09e151fd13106366f39140e8b664ccacbfee5fab
xsanm <kkurowski.kk@gmail.com>
2023-12-17 16:51:23 +0100

    [native] implement accepting `olm` session
    Summary: Code which receive response from Tunnelbroker with session creation request, calls Identity to get Inbound keys and creates session.
    
    Depends on D10379
    
    Test Plan: Call this method and verify result
    
    Reviewers: marcin, varun, michal

--- native/utils/crypto-utils.js
+++ native/utils/crypto-utils.js
@@ -1,9 +1,14 @@
 // @flow
 
-import type { OLMIdentityKeys } from 'lib/types/crypto-types';
-import type { OlmSessionInitializationInfo } from 'lib/types/request-types';
+import type {
+  IdentityKeysBlob,
+  OLMIdentityKeys,
+} from 'lib/types/crypto-types.js';
+import type { InboundKeyInfoResponse } from 'lib/types/identity-service-types.js';
+import type { OlmSessionInitializationInfo } from 'lib/types/request-types.js';
+import type { OutboundSessionCreation } from 'lib/types/tunnelbroker/peer-to-peer-message-types.js';
 
-import { commCoreModule } from '../native-modules.js';
+import { commCoreModule, commRustModule } from '../native-modules.js';
 
 function nativeNotificationsSessionCreator(
   notificationsIdentityKeys: OLMIdentityKeys,
@@ -27,4 +32,51 @@ async function getContentSigningKey(): Promise<string> {
   return ed25519;
 }
 
-export { getContentSigningKey, nativeNotificationsSessionCreator };
+async function nativeInboundContentSessionCreator(
+  message: OutboundSessionCreation,
+): Promise<string> {
+  const { senderInfo, encryptedContent } = message;
+
+  const authMetadata = await commCoreModule.getCommServicesAuthMetadata();
+  const { userID, deviceID, accessToken } = authMetadata;
+  if (!userID || !deviceID || !accessToken) {
+    throw new Error('CommServicesAuthMetadata is missing');
+  }
+
+  const keysResponse = await commRustModule.getInboundKeysForUser(
+    userID,
+    deviceID,
+    accessToken,
+    senderInfo.userID,
+  );
+
+  const inboundKeys: InboundKeyInfoResponse[] = JSON.parse(keysResponse);
+  const deviceKeys: ?InboundKeyInfoResponse = inboundKeys.find(keys => {
+    const keysPayload: IdentityKeysBlob = JSON.parse(keys.payload);
+    return (
+      keysPayload.primaryIdentityPublicKeys.ed25519 === senderInfo.deviceID
+    );
+  });
+
+  if (!deviceKeys) {
+    throw new Error(
+      `No keys for the device that requested creating a session, deviceID: ${senderInfo.deviceID}`,
+    );
+  }
+  const keysPayload: IdentityKeysBlob = JSON.parse(deviceKeys.payload);
+  const identityKeys = JSON.stringify({
+    curve25519: keysPayload.primaryIdentityPublicKeys.curve25519,
+    ed25519: keysPayload.primaryIdentityPublicKeys.ed25519,
+  });
+  return commCoreModule.initializeContentInboundSession(
+    identityKeys,
+    encryptedContent,
+    keysPayload.primaryIdentityPublicKeys.ed25519,
+  );
+}
+
+export {
+  getContentSigningKey,
+  nativeNotificationsSessionCreator,
+  nativeInboundContentSessionCreator,
+};
