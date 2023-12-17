49e35f952869b0076b8008d91a5f2857530f0f14
xsanm <kkurowski.kk@gmail.com>
2023-12-17 16:51:23 +0100

    [native] implement accepting `olm` session
    Summary:
    Code which receive response from Tunnelbroker with session creation request, calls Identity to get Inbound keys and creates session.
    
    Depends on D10379
    
    Test Plan: Call this method and verify result
    
    Reviewers: marcin, varun, michal
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10380

--- native/handlers/peer-to-peer-message-handler.js
+++ native/handlers/peer-to-peer-message-handler.js
@@ -5,11 +5,22 @@ import {
   peerToPeerMessageTypes,
 } from 'lib/types/tunnelbroker/peer-to-peer-message-types.js';
 
+import { nativeInboundContentSessionCreator } from '../utils/crypto-utils.js';
+
 async function peerToPeerMessageHandler(
   message: PeerToPeerMessage,
 ): Promise<void> {
   if (message.type === peerToPeerMessageTypes.OUTBOUND_SESSION_CREATION) {
-    console.log('Received session creation request');
+    try {
+      const result = await nativeInboundContentSessionCreator(message);
+      console.log(
+        `Created inbound session with device ${message.senderInfo.deviceID}: ${result}`,
+      );
+    } catch (e) {
+      console.log(
+        `Error creating inbound session with device ${message.senderInfo.deviceID}: ${e.message}`,
+      );
+    }
   } else if (message.type === peerToPeerMessageTypes.ENCRYPTED_MESSAGE) {
     console.log('Received encrypted message');
   }
