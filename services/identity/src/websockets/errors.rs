6f1d8674fbe30afe1a3c77950339cfb6af19d052
William Wang <williamwang749@gmail.com>
2023-12-18 17:13:11 -0500

    [5/n] prefix search websocket server
    Summary: This diff implements a websocket server on the identity service that queries users on the opensearch domain with prefixes.
    
    Test Plan: Tested on staging with a custom image.
    
    Reviewers: O1 Nix
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10392

---
+++ services/identity/src/websockets/errors.rs
@@ -0,0 +1,11 @@
+pub type BoxedError = Box<dyn std::error::Error + Send + Sync + 'static>;
+
+#[derive(
+  Debug, derive_more::Display, derive_more::From, derive_more::Error,
+)]
+pub enum WebsocketError {
+  InvalidMessage,
+  SendError,
+  SearchError,
+  SerializationError,
+}
