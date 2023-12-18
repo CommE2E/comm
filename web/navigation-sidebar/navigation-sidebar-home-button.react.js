715fbe4f37d13ac29712b7c0789997db01c41be5
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-12-18 17:14:26 -0500

    [web] show total unreads in the navigation sidebar home button
    Summary: This diff introuces the unread badge in the navigation sidebar home button component. Here we want to show the user the total number of unread messages they have on Comm across all of their communites
    
    Test Plan: Please see the screenshots below
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10391

--- web/navigation-sidebar/navigation-sidebar-home-button.react.js
+++ web/navigation-sidebar/navigation-sidebar-home-button.react.js
@@ -3,11 +3,16 @@
 import * as React from 'react';
 
 import SWMansionIcon from 'lib/components/SWMansionIcon.react.js';
+import { unreadCount } from 'lib/selectors/thread-selectors.js';
 
 import css from './navigation-sidebar-home-button.css';
+import UnreadBadge from '../components/unread-badge.react.js';
+import { useSelector } from '../redux/redux-utils.js';
 import { useNavigationSidebarTooltip } from '../utils/tooltip-action-utils.js';
 
 function NavigationSidebarHomeButton(): React.Node {
+  const unreadCountValue = useSelector(unreadCount);
+
   const { onMouseEnter, onMouseLeave } = useNavigationSidebarTooltip({
     tooltipLabel: 'Home',
   });
@@ -19,6 +24,9 @@ function NavigationSidebarHomeButton(): React.Node {
         onMouseEnter={onMouseEnter}
         onMouseLeave={onMouseLeave}
       >
+        <div className={css.unreadBadgeContainer}>
+          <UnreadBadge unreadCount={unreadCountValue} />
+        </div>
         <SWMansionIcon icon="home-1" size={24} />
       </div>
     </div>
