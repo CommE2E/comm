3f8ae70414a858c5ba6837fd84bc0055874fc550
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-12-18 17:39:02 -0500

    [web] show unreads for each community in the community list
    Summary: This diff introduces the unread badge to the community list in the navigation sidebar. Here we want to show the unreads broken down by each community
    
    Test Plan: Please see the screenshots/demo video
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10393

--- web/navigation-sidebar/community-list-item.react.js
+++ web/navigation-sidebar/community-list-item.react.js
@@ -2,10 +2,14 @@
 
 import * as React from 'react';
 
+import { threadInHomeChatList } from 'lib/shared/thread-utils.js';
 import type { ResolvedThreadInfo } from 'lib/types/thread-types.js';
+import { values } from 'lib/utils/objects.js';
 
 import css from './community-list-item.css';
 import ThreadAvatar from '../avatars/thread-avatar.react.js';
+import UnreadBadge from '../components/unread-badge.react.js';
+import { useSelector } from '../redux/redux-utils.js';
 import { useNavigationSidebarTooltip } from '../utils/tooltip-action-utils.js';
 
 type Props = {
@@ -14,6 +18,32 @@ type Props = {
 
 function CommunityListItem(props: Props): React.Node {
   const { threadInfo } = props;
+  const { id: threadID } = threadInfo;
+
+  const threadInfos = useSelector(state => state.threadStore.threadInfos);
+
+  const unreadCountValue = React.useMemo(
+    () =>
+      values(threadInfos).filter(
+        communityInfo =>
+          threadInHomeChatList(communityInfo) &&
+          communityInfo.currentUser.unread &&
+          (threadID === communityInfo.community ||
+            threadID === communityInfo.id),
+      ).length,
+    [threadID, threadInfos],
+  );
+
+  const unreadBadge = React.useMemo(() => {
+    if (unreadCountValue === 0) {
+      return null;
+    }
+    return (
+      <div className={css.unreadBadgeContainer}>
+        <UnreadBadge unreadCount={unreadCountValue} />
+      </div>
+    );
+  }, [unreadCountValue]);
 
   const { onMouseEnter, onMouseLeave } = useNavigationSidebarTooltip({
     tooltipLabel: threadInfo.uiName,
@@ -26,6 +56,7 @@ function CommunityListItem(props: Props): React.Node {
       onMouseLeave={onMouseLeave}
     >
       <ThreadAvatar size="S" threadInfo={threadInfo} />
+      {unreadBadge}
     </div>
   );
 }
