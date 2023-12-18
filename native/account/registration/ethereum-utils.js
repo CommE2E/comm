2524f7c2001ce68020678e9c0ec6de6eb398704d
Ashoat Tevosyan <ashoat@comm.app>
2023-12-18 11:20:35 -0800

    [native] Extract useGetEthereumAccountFromSIWEResult
    Summary:
    We want to reuse this in `FullscreenSIWEPanel` for [ENG-4074](https://linear.app/comm/issue/ENG-4074/make-sure-main-siwe-button-redirects-to-registration-flow-if-no).
    
    Depends on D10413
    
    Test Plan:
    I tested this diff stack as follows:
    
    1. Delete test ETH account 1. Attempt to do main SIWE login with ETH account 1. Confirm it redirects to registration flow. Complete registration flow and confirm account successfully created
    2. Same as above, but go back after reaching the avatar screen, and then forward to see the Ethereum screen. Choose the option to use the already connected ETH account, then complete registration flow and confirm account successfully created
    3. Same as above, except in the Ethereum screen, choose the option to "skip" the Ethereum login. Confirm password account is successfully created
    4. Same as above, except in the Ethereum screen, select another Ethereum wallet that already has a Comm account. Confirm that we hit `ExistingEthereumAccount`, and that we're able to login directly with the existing account
    5. Same as above, except in the Ethereum screen, select another Ethereum wallet that doesn't already have a Comm account. Confirm that the account is successfully created using the second Ethereum wallet and not the first
    6. Same as above, except in the Ethereum screen, go through the auth flow again for the exact same wallet. Confirm that the account is successfully created
    
    Note that I needed to apply [this patch](https://gist.github.com/Ashoat/9b8037bb8dbf5f039a94b8868a40958f) to work around [ENG-6097](https://linear.app/comm/issue/ENG-6097/set-new-session-action-for-new-keyserver-corrupts-keyserverstore).
    
    Reviewers: atul, ginsu
    
    Reviewed By: atul
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D10414

---
+++ native/account/registration/ethereum-utils.js
@@ -0,0 +1,60 @@
+// @flow
+
+import invariant from 'invariant';
+import * as React from 'react';
+
+import { ENSCacheContext } from 'lib/components/ens-cache-provider.react.js';
+import type { SIWEResult } from 'lib/types/siwe-types.js';
+
+import { RegistrationContext } from './registration-context.js';
+import {
+  type EthereumAccountSelection,
+  ensAvatarSelection,
+} from './registration-types.js';
+
+function useGetEthereumAccountFromSIWEResult(): (
+  result: SIWEResult,
+) => Promise<EthereumAccountSelection> {
+  const registrationContext = React.useContext(RegistrationContext);
+  invariant(registrationContext, 'registrationContext should be set');
+  const { setCachedSelections } = registrationContext;
+
+  const cacheContext = React.useContext(ENSCacheContext);
+  const { ensCache } = cacheContext;
+
+  return React.useCallback(
+    async result => {
+      // We want to figure out if the user has an ENS avatar now
+      // so that we can default to the ENS avatar in AvatarSelection
+      let avatarURI: ?string = null;
+      if (ensCache) {
+        avatarURI = await ensCache.getAvatarURIForAddress(result.address);
+      }
+
+      const ethereumAccount = {
+        accountType: 'ethereum',
+        ...result,
+        avatarURI,
+      };
+
+      setCachedSelections(oldUserSelections => {
+        const base = {
+          ...oldUserSelections,
+          ethereumAccount,
+        };
+        if (base.avatarData || !avatarURI) {
+          return base;
+        }
+        return {
+          ...base,
+          avatarData: ensAvatarSelection,
+        };
+      });
+
+      return ethereumAccount;
+    },
+    [ensCache, setCachedSelections],
+  );
+}
+
+export { useGetEthereumAccountFromSIWEResult };
