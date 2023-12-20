bea7a81bb9b9db4d183b287d3213439891a49f84
Ashoat Tevosyan <ashoat@comm.app>
2023-12-20 14:27:04 -0500

    [native] Redirect to registration flow when ETH login attempted without registered account
    Summary:
    Linear task: [ENG-4074](https://linear.app/comm/issue/ENG-4074/make-sure-main-siwe-button-redirects-to-registration-flow-if-no)
    
    Depends on D10414
    
    Test Plan:
    Here is a screenshot of the new screen:
    
    {F994709}
    
    Additionally, I tested this diff stack as follows:
    
    1. Delete test ETH account 1. Attempt to do main SIWE login with ETH account 1. Confirm it redirects to registration flow. Complete registration flow and confirm account successfully created
    2. Same as above, but go back after reaching the avatar screen, and then forward to see the Ethereum screen. Choose the option to use the already connected ETH account, then complete registration flow and confirm account successfully created
    3. Same as above, except in the Ethereum screen, choose the option to "skip" the Ethereum login. Confirm password account is successfully created
    4. Same as above, except in the Ethereum screen, select another Ethereum wallet that already has a Comm account. Confirm that we hit `ExistingEthereumAccount`, and that we're able to login directly with the existing account
    5. Same as above, except in the Ethereum screen, select another Ethereum wallet that doesn't already have a Comm account. Confirm that the account is successfully created using the second Ethereum wallet and not the first
    6. Same as above, except in the Ethereum screen, go through the auth flow again for the exact same wallet. Confirm that the account is successfully created
    
    Note that I needed to apply [this patch](https://gist.github.com/Ashoat/9b8037bb8dbf5f039a94b8868a40958f) to work around [ENG-6097](https://linear.app/comm/issue/ENG-6097/set-new-session-action-for-new-keyserver-corrupts-keyserverstore).
    
    Reviewers: atul, ginsu
    
    Reviewed By: atul
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D10415

---
+++ native/account/registration/account-does-not-exist.react.js
@@ -0,0 +1,79 @@
+// @flow
+
+import * as React from 'react';
+import { Text, View, Image } from 'react-native';
+
+import RegistrationButtonContainer from './registration-button-container.react.js';
+import RegistrationButton from './registration-button.react.js';
+import RegistrationContainer from './registration-container.react.js';
+import RegistrationContentContainer from './registration-content-container.react.js';
+import type { RegistrationNavigationProp } from './registration-navigator.react.js';
+import commSwooshSource from '../../img/comm-swoosh.png';
+import {
+  type NavigationRoute,
+  CoolOrNerdModeSelectionRouteName,
+} from '../../navigation/route-names.js';
+import { useStyles } from '../../themes/colors.js';
+
+type Props = {
+  +navigation: RegistrationNavigationProp<'AccountDoesNotExist'>,
+  +route: NavigationRoute<'AccountDoesNotExist'>,
+};
+function AccountDoesNotExist(props: Props): React.Node {
+  const { navigate } = props.navigation;
+  const onSubmit = React.useCallback(() => {
+    navigate(CoolOrNerdModeSelectionRouteName);
+  }, [navigate]);
+
+  const styles = useStyles(unboundStyles);
+  return (
+    <RegistrationContainer>
+      <RegistrationContentContainer style={styles.scrollViewContentContainer}>
+        <Text style={styles.header}>New Comm account</Text>
+        <Text style={styles.body}>
+          It looks like this is your first time logging into Comm.
+        </Text>
+        <Text style={styles.body}>
+          Let&rsquo;s get started with registering your Ethereum account!
+        </Text>
+        <View style={styles.commSwooshContainer}>
+          <Image source={commSwooshSource} style={styles.commSwoosh} />
+        </View>
+      </RegistrationContentContainer>
+      <RegistrationButtonContainer>
+        <RegistrationButton onPress={onSubmit} label="Next" variant="enabled" />
+      </RegistrationButtonContainer>
+    </RegistrationContainer>
+  );
+}
+
+const unboundStyles = {
+  scrollViewContentContainer: {
+    flexGrow: 1,
+  },
+  header: {
+    fontSize: 24,
+    color: 'panelForegroundLabel',
+    paddingBottom: 16,
+  },
+  body: {
+    fontFamily: 'Arial',
+    fontSize: 15,
+    lineHeight: 20,
+    color: 'panelForegroundSecondaryLabel',
+    paddingBottom: 16,
+  },
+  commSwooshContainer: {
+    flexGrow: 1,
+    flexShrink: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  commSwoosh: {
+    resizeMode: 'center',
+    width: '100%',
+    height: '100%',
+  },
+};
+
+export default AccountDoesNotExist;
