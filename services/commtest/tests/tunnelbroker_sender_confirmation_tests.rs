544d89dd0b813e701755af9853c8c5cf8144ac3a
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2023-12-21 09:12:57 +0100

    [commtest] Rename create_device to register_user_device
    Summary:
    The `create_device` wasn't very accurate name. What the function does is:
    - Register a new user
    - Upload keys for its primary device
    
    Also added a `DeviceType` optional argument to the function which will be needed late optional argument to the function which will be needed later.
    
    Depends on D10404
    
    Test Plan: Commtest builds, CommTest CI
    
    Reviewers: kamil, michal, varun
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10420

--- services/commtest/tests/tunnelbroker_sender_confirmation_tests.rs
+++ services/commtest/tests/tunnelbroker_sender_confirmation_tests.rs
@@ -1,4 +1,4 @@
-use commtest::identity::device::create_device;
+use commtest::identity::device::register_user_device;
 use commtest::identity::olm_account_infos::{
   DEFAULT_CLIENT_KEYS, MOCK_CLIENT_KEYS_1, MOCK_CLIENT_KEYS_2,
 };
@@ -14,8 +14,8 @@ use tunnelbroker_messages::{
 
 #[tokio::test]
 async fn get_confirmation() {
-  let sender = create_device(Some(&MOCK_CLIENT_KEYS_1)).await;
-  let receiver = create_device(Some(&MOCK_CLIENT_KEYS_2)).await;
+  let sender = register_user_device(Some(&MOCK_CLIENT_KEYS_1), None).await;
+  let receiver = register_user_device(Some(&MOCK_CLIENT_KEYS_2), None).await;
 
   let client_message_id = "mockID".to_string();
 
@@ -53,7 +53,7 @@ async fn get_confirmation() {
 
 #[tokio::test]
 async fn get_serialization_error() {
-  let sender = create_device(Some(&DEFAULT_CLIENT_KEYS)).await;
+  let sender = register_user_device(Some(&DEFAULT_CLIENT_KEYS), None).await;
   let message = "some bad json".to_string();
 
   let mut sender_socket = create_socket(&sender).await.unwrap();
@@ -74,7 +74,7 @@ async fn get_serialization_error() {
 
 #[tokio::test]
 async fn get_invalid_request_error() {
-  let sender = create_device(Some(&DEFAULT_CLIENT_KEYS)).await;
+  let sender = register_user_device(Some(&DEFAULT_CLIENT_KEYS), None).await;
 
   let mut sender_socket = create_socket(&sender).await.unwrap();
   sender_socket
