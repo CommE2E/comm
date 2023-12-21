d298ff762cc74bca837606de0d5cad2b7b17e1af
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2023-12-21 09:12:57 +0100

    [commtest] Rename create_device to register_user_device
    Summary:
    The `create_device` wasn't very accurate name. What the function does is:
    - Register a new user
    - Upload keys for its primary device
    
    Also added a `DeviceType` optional argument to the function which will be needed late optional argument to the function which will be needed later.
    
    Depends on D10404
    
    Test Plan: Commtest builds, CommTest CI
    
    Reviewers: kamil, michal, varun
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10420

--- services/commtest/tests/tunnelbroker_integration_tests.rs
+++ services/commtest/tests/tunnelbroker_integration_tests.rs
@@ -2,7 +2,7 @@ mod proto {
   tonic::include_proto!("tunnelbroker");
 }
 
-use commtest::identity::device::create_device;
+use commtest::identity::device::register_user_device;
 use commtest::identity::olm_account_infos::{
   MOCK_CLIENT_KEYS_1, MOCK_CLIENT_KEYS_2,
 };
@@ -21,7 +21,7 @@ use tunnelbroker_messages::RefreshKeyRequest;
 #[tokio::test]
 async fn send_refresh_request() {
   // Create session as a keyserver
-  let device_info = create_device(None).await;
+  let device_info = register_user_device(None, None).await;
   let mut socket = create_socket(&device_info).await.unwrap();
 
   // Send request for keyserver to refresh keys (identity service)
@@ -59,8 +59,8 @@ async fn send_refresh_request() {
 
 #[tokio::test]
 async fn test_messages_order() {
-  let sender = create_device(Some(&MOCK_CLIENT_KEYS_1)).await;
-  let receiver = create_device(Some(&MOCK_CLIENT_KEYS_2)).await;
+  let sender = register_user_device(Some(&MOCK_CLIENT_KEYS_1), None).await;
+  let receiver = register_user_device(Some(&MOCK_CLIENT_KEYS_2), None).await;
 
   let messages = vec![
     WebSocketMessageToDevice {
