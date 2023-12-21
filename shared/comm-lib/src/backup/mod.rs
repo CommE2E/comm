bfb1cd5fd778f0b9479d27ccbc82ed42f23bc740
Michal Gniadek <michal.gniadek@swmansion.com>
2023-12-21 16:49:54 +0100

    [backup] Log upload endpoint
    Summary:
    [ENG-5329 : Add endpoints for uploading a log](https://linear.app/comm/issue/ENG-5329/add-endpoints-for-uploading-a-log)
    
    Add the actual logic for uploading a log
    
    Depends on D10453
    
    Test Plan:
    - Modify the maximum size that we can keep in dynamodb to 200
    - Upload two logs:
    ```
    UploadLogRequest {
      log_id: 11111,
      content: vec![1; 10],
      attachments: None,
    }
    UploadLogRequest {
      log_id: 22222,
      content: vec![2; 10000],
      attachments: Some(vec!["attachment".into()]),
    }
    ```
    - Checked that I got two upload respones
    - Checked the db:
    	- `11111` has `content` column with bytes
    	- `22222` has `attachments` column with one `BlobInfo` and `blobInfo` column with a `BlobInfo` for content
    - Checked blob logs:
    	- "Assign holder request" for hash "attachment"
    	- Assign holder request and "Upload blob request" for content of the second request
    
    Reviewers: bartek, kamil
    
    Subscribers:

--- shared/comm-lib/src/backup/mod.rs
+++ shared/comm-lib/src/backup/mod.rs
@@ -5,3 +5,21 @@ pub struct LatestBackupIDResponse {
   #[serde(rename = "backupID")]
   pub backup_id: String,
 }
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct UploadLogRequest {
+  pub log_id: usize,
+  pub content: Vec<u8>,
+  pub attachments: Option<Vec<String>>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum LogWSRequest {
+  UploadLog(UploadLogRequest),
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum LogWSResponse {
+  LogUploaded { log_id: usize },
+  ServerError,
+}
