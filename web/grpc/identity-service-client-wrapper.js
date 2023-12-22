ca7fb7438e11d66fc63eea4e6b8df88b4611c263
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2023-12-22 09:55:26 -0600

    [web] client method for login password user
    Summary: this method handles all of the logic around session ids (for sticky sessions on identity service) and OPAQUE. we return a string because that's what we do in CommRustModule, and we would like to have a single interface for the two clients.
    
    Test Plan: called loginPasswordUser from root.js and got back a string, from which userID and access token could be parsed
    
    Reviewers: tomek, inka, ashoat
    
    Differential Revision: https://phab.comm.dev/D10455

--- web/grpc/identity-service-client-wrapper.js
+++ web/grpc/identity-service-client-wrapper.js
@@ -1,12 +1,24 @@
 // @flow
 
+import { Login } from '@commapp/opaque-ke-wasm';
+
 import identityServiceConfig from 'lib/facts/identity-service.js';
 import type { IdentityServiceAuthLayer } from 'lib/types/identity-service-types.js';
 
 import { VersionInterceptor, AuthInterceptor } from './interceptor.js';
+import { initOpaque } from '../crypto/opaque-utils.js';
 import * as IdentityAuthClient from '../protobufs/identity-auth-client.cjs';
-import { Empty } from '../protobufs/identity-unauth-structs.cjs';
+import {
+  DeviceKeyUpload,
+  Empty,
+  IdentityKeyInfo,
+  OpaqueLoginFinishRequest,
+  OpaqueLoginStartRequest,
+  Prekey,
+} from '../protobufs/identity-unauth-structs.cjs';
 import * as IdentityClient from '../protobufs/identity-unauth.cjs';
+
+const webDeviceType = 1;
 
 class IdentityServiceClientWrapper {
   authClient: ?IdentityAuthClient.IdentityClientServicePromiseClient;
@@ -84,6 +96,87 @@ class IdentityServiceClientWrapper {
     } else {
       throw new Error('Identity service client is not initialized');
     }
+  };
+
+  loginUser: (
+    username: string,
+    password: string,
+    keyPayload: string,
+    keyPayloadSignature: string,
+    contentPrekey: string,
+    contentPrekeySignature: string,
+    notifPrekey: string,
+    notifPrekeySignature: string,
+    contentOneTimeKeys: Array<string>,
+    notifOneTimeKeys: Array<string>,
+  ) => Promise<string> = async (
+    username: string,
+    password: string,
+    keyPayload: string,
+    keyPayloadSignature: string,
+    contentPrekey: string,
+    contentPrekeySignature: string,
+    notifPrekey: string,
+    notifPrekeySignature: string,
+    contentOneTimeKeys: Array<string>,
+    notifOneTimeKeys: Array<string>,
+  ): Promise<string> => {
+    if (!this.unauthorizedClient) {
+      await this.initUnauthorizedClient();
+    }
+
+    const client = this.unauthorizedClient;
+    if (!client) {
+      throw new Error('Failed to initialize identity service client');
+    }
+
+    await initOpaque();
+    const opaqueLogin = new Login();
+    const startRequestBytes = opaqueLogin.start(password);
+
+    const identityKeyInfo = new IdentityKeyInfo();
+    identityKeyInfo.setPayload(keyPayload);
+    identityKeyInfo.setPayloadSignature(keyPayloadSignature);
+
+    const contentPrekeyUpload = new Prekey();
+    contentPrekeyUpload.setPrekey(contentPrekey);
+    contentPrekeyUpload.setPrekeySignature(contentPrekeySignature);
+
+    const notifPrekeyUpload = new Prekey();
+    notifPrekeyUpload.setPrekey(notifPrekey);
+    notifPrekeyUpload.setPrekeySignature(notifPrekeySignature);
+
+    const deviceKeyUpload = new DeviceKeyUpload();
+    deviceKeyUpload.setDeviceKeyInfo(identityKeyInfo);
+    deviceKeyUpload.setContentUpload(contentPrekeyUpload);
+    deviceKeyUpload.setNotifUpload(notifPrekeyUpload);
+    deviceKeyUpload.setOneTimeContentPrekeysList(contentOneTimeKeys);
+    deviceKeyUpload.setOneTimeNotifPrekeysList(notifOneTimeKeys);
+    deviceKeyUpload.setDeviceType(webDeviceType);
+
+    const loginStartRequest = new OpaqueLoginStartRequest();
+    loginStartRequest.setUsername(username);
+    loginStartRequest.setOpaqueLoginRequest(startRequestBytes);
+    loginStartRequest.setDeviceKeyUpload(deviceKeyUpload);
+
+    const logInStartResponse =
+      await client.logInPasswordUserStart(loginStartRequest);
+
+    const finishRequestBytes = opaqueLogin.finish(
+      logInStartResponse.getOpaqueLoginResponse_asU8(),
+    );
+
+    const logInFinishRequest = new OpaqueLoginFinishRequest();
+    logInFinishRequest.setSessionId(logInStartResponse.getSessionId());
+    logInFinishRequest.setOpaqueLoginUpload(finishRequestBytes);
+
+    const logInFinishResponse =
+      await client.logInPasswordUserFinish(logInFinishRequest);
+
+    const userID = logInFinishResponse.getUserId();
+    const accessToken = logInFinishResponse.getAccessToken();
+
+    return JSON.stringify({ userID, accessToken });
   };
 }
 
