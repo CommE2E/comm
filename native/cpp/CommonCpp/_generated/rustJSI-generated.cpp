c25b5d3a5bdb415ddd6753fd1122408fc2fbbbc4
Tomasz Palys <tomasz.palys@swmansion.com>
2023-12-22 16:11:42 +0100

    [native] Expose get keyserver keys Identity RPC
    Summary:
    Add Rust and C++ code that allows calling this function.
    
    Depends on D10403
    
    Test Plan: Call this function in a code introduced by D10327 and check if the keys are returned.
    
    Reviewers: inka, varun, bartek, michal
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D10449

--- native/cpp/CommonCpp/_generated/rustJSI-generated.cpp
+++ native/cpp/CommonCpp/_generated/rustJSI-generated.cpp
@@ -36,6 +36,9 @@ static jsi::Value __hostFunction_CommRustModuleSchemaCxxSpecJSI_getOutboundKeysF
 static jsi::Value __hostFunction_CommRustModuleSchemaCxxSpecJSI_versionSupported(jsi::Runtime &rt, TurboModule &turboModule, const jsi::Value* args, size_t count) {
   return static_cast<CommRustModuleSchemaCxxSpecJSI *>(&turboModule)->versionSupported(rt);
 }
+static jsi::Value __hostFunction_CommRustModuleSchemaCxxSpecJSI_getKeyserverKeys(jsi::Runtime &rt, TurboModule &turboModule, const jsi::Value* args, size_t count) {
+  return static_cast<CommRustModuleSchemaCxxSpecJSI *>(&turboModule)->getKeyserverKeys(rt, args[0].asString(rt), args[1].asString(rt), args[2].asString(rt), args[3].asString(rt));
+}
 
 CommRustModuleSchemaCxxSpecJSI::CommRustModuleSchemaCxxSpecJSI(std::shared_ptr<CallInvoker> jsInvoker)
   : TurboModule("CommRustTurboModule", jsInvoker) {
@@ -47,6 +50,7 @@ CommRustModuleSchemaCxxSpecJSI::CommRustModuleSchemaCxxSpecJSI(std::shared_ptr<C
   methodMap_["deleteUser"] = MethodMetadata {3, __hostFunction_CommRustModuleSchemaCxxSpecJSI_deleteUser};
   methodMap_["getOutboundKeysForUserDevice"] = MethodMetadata {5, __hostFunction_CommRustModuleSchemaCxxSpecJSI_getOutboundKeysForUserDevice};
   methodMap_["versionSupported"] = MethodMetadata {0, __hostFunction_CommRustModuleSchemaCxxSpecJSI_versionSupported};
+  methodMap_["getKeyserverKeys"] = MethodMetadata {4, __hostFunction_CommRustModuleSchemaCxxSpecJSI_getKeyserverKeys};
 }
 
 
