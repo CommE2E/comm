c5fd3cf9cf40473f387b5c3b81d1b774b9fc5ce1
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2023-12-22 09:55:26 -0600

    [web] client method for login password user
    Summary: this method handles all of the logic around session ids (for sticky sessions on identity service) and OPAQUE. we return a string because that's what we do in CommRustModule, and we would like to have a single interface for the two clients.
    
    Test Plan: called loginPasswordUser from root.js and got back a string, from which userID and access token could be parsed
    
    Reviewers: tomek, inka, ashoat
    
    Differential Revision: https://phab.comm.dev/D10455

--- web/grpc/identity-service-client-wrapper.js
+++ web/grpc/identity-service-client-wrapper.js
@@ -1,12 +1,24 @@
 // @flow
 
+import { Login } from '@commapp/opaque-ke-wasm';
+
 import identityServiceConfig from 'lib/facts/identity-service.js';
 import type { IdentityServiceAuthLayer } from 'lib/types/identity-service-types.js';
 
 import { VersionInterceptor, AuthInterceptor } from './interceptor.js';
+import { initOpaque } from '../crypto/opaque-utils.js';
 import * as IdentityAuthClient from '../protobufs/identity-auth-client.cjs';
 import * as IdentityClient from '../protobufs/identity-client.cjs';
-import { Empty } from '../protobufs/identity-structs.cjs';
+import {
+  DeviceKeyUpload,
+  Empty,
+  IdentityKeyInfo,
+  OpaqueLoginFinishRequest,
+  OpaqueLoginStartRequest,
+  PreKey,
+} from '../protobufs/identity-structs.cjs';
+
+const webDeviceType = Object.freeze(1);
 
 class IdentityServiceClientWrapper {
   authClient: ?IdentityAuthClient.IdentityClientServicePromiseClient;
@@ -21,7 +33,9 @@ class IdentityServiceClientWrapper {
     return process.env.IDENTITY_SOCKET_ADDR ?? identityServiceConfig.defaultURL;
   }
 
-  async initAuthClient(authLayer: IdentityServiceAuthLayer): Promise<void> {
+  initAuthClient(
+    authLayer: IdentityServiceAuthLayer,
+  ): IdentityAuthClient.IdentityClientServicePromiseClient {
     const { userID, deviceID, commServicesAccessToken } = authLayer;
 
     const identitySocketAddr = this.determineSocketAddr();
@@ -42,9 +56,11 @@ class IdentityServiceClientWrapper {
       null,
       authClientOpts,
     );
+
+    return this.authClient;
   }
 
-  async initUnauthorizedClient(): Promise<void> {
+  initUnauthorizedClient(): IdentityClient.IdentityClientServicePromiseClient {
     const identitySocketAddr = this.determineSocketAddr();
 
     const versionInterceptor = new VersionInterceptor<Request, Response>();
@@ -59,6 +75,8 @@ class IdentityServiceClientWrapper {
         null,
         unauthorizedClientOpts,
       );
+
+    return this.unauthorizedClient;
   }
 
   deleteUser: (
@@ -70,20 +88,100 @@ class IdentityServiceClientWrapper {
     deviceID: string,
     accessToken: string,
   ): Promise<void> => {
-    if (!this.authClient) {
-      const authLayer: IdentityServiceAuthLayer = {
+    const authClient =
+      this.authClient ||
+      this.initAuthClient({
         userID,
         deviceID,
         commServicesAccessToken: accessToken,
-      };
-      await this.initAuthClient(authLayer);
+      });
+
+    await authClient.deleteUser(new Empty());
+  };
+
+  loginPasswordUser: (
+    username: string,
+    password: string,
+    keyPayload: string,
+    keyPayloadSignature: string,
+    contentPrekey: string,
+    contentPrekeySignature: string,
+    notifPrekey: string,
+    notifPrekeySignature: string,
+    contentOneTimeKeys: Array<string>,
+    notifOneTimeKeys: Array<string>,
+  ) => Promise<string> = async (
+    username: string,
+    password: string,
+    keyPayload: string,
+    keyPayloadSignature: string,
+    contentPrekey: string,
+    contentPrekeySignature: string,
+    notifPrekey: string,
+    notifPrekeySignature: string,
+    contentOneTimeKeys: Array<string>,
+    notifOneTimeKeys: Array<string>,
+  ): Promise<string> => {
+    const client = this.unauthorizedClient || this.initUnauthorizedClient();
+
+    await initOpaque();
+    const opaqueLogin = new Login();
+    const startRequestBytes = opaqueLogin.start(password);
+
+    const identityKeyInfo = new IdentityKeyInfo();
+    identityKeyInfo.setPayload(keyPayload);
+    identityKeyInfo.setPayloadsignature(keyPayloadSignature);
+
+    const contentPrekeyUpload = new PreKey();
+    contentPrekeyUpload.setPrekey(contentPrekey);
+    contentPrekeyUpload.setPrekeysignature(contentPrekeySignature);
+
+    const notifPrekeyUpload = new PreKey();
+    notifPrekeyUpload.setPrekey(notifPrekey);
+    notifPrekeyUpload.setPrekeysignature(notifPrekeySignature);
+
+    const deviceKeyUpload = new DeviceKeyUpload();
+    deviceKeyUpload.setDevicekeyinfo(identityKeyInfo);
+    deviceKeyUpload.setContentupload(contentPrekeyUpload);
+    deviceKeyUpload.setNotifupload(notifPrekeyUpload);
+    deviceKeyUpload.setOnetimecontentprekeysList(contentOneTimeKeys);
+    deviceKeyUpload.setOnetimenotifprekeysList(notifOneTimeKeys);
+    deviceKeyUpload.setDevicetype(webDeviceType);
+
+    const loginStartRequest = new OpaqueLoginStartRequest();
+    loginStartRequest.setUsername(username);
+    loginStartRequest.setOpaqueloginrequest(startRequestBytes);
+    loginStartRequest.setDevicekeyupload(deviceKeyUpload);
+
+    let loginStartResponse;
+    try {
+      loginStartResponse =
+        await client.loginPasswordUserStart(loginStartRequest);
+    } catch (startError) {
+      startError.method = 'loginPasswordUserStart';
+      throw startError;
+    }
+    const finishRequestBytes = opaqueLogin.finish(
+      loginStartResponse.getOpaqueloginresponse_asU8(),
+    );
+
+    const loginFinishRequest = new OpaqueLoginFinishRequest();
+    loginFinishRequest.setSessionid(loginStartResponse.getSessionid());
+    loginFinishRequest.setOpaqueloginupload(finishRequestBytes);
+
+    let loginFinishResponse;
+    try {
+      loginFinishResponse =
+        await client.loginPasswordUserFinish(loginFinishRequest);
+    } catch (finishError) {
+      finishError.method = 'loginPasswordUserFinish';
+      throw finishError;
     }
 
-    if (this.authClient) {
-      await this.authClient.deleteUser(new Empty());
-    } else {
-      throw new Error('Identity service client is not initialized');
-    }
+    const userID = loginFinishResponse.getUserid();
+    const accessToken = loginFinishResponse.getAccesstoken();
+
+    return JSON.stringify({ userID, accessToken });
   };
 }
 
