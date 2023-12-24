c1c75ff46e2f7ac6986e4ede6576ea04ef5829e2
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-12-23 21:01:02 -0500

    [web] introduce community header button
    Summary: This diff introduces the community header button. This will be used by the two community buttons we will have in the header.
    
    Test Plan: Please see the demo video below
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10497

---
+++ web/community-header/community-header-button.react.js
@@ -0,0 +1,40 @@
+// @flow
+
+import * as React from 'react';
+
+import SWMansionIcon, {
+  type Icon,
+} from 'lib/components/SWMansionIcon.react.js';
+
+import css from './community-header-button.css';
+import { useLabelTooltip } from '../tooltips/tooltip-action-utils.js';
+import { tooltipPositions } from '../tooltips/tooltip-utils.js';
+
+type Props = {
+  +icon: Icon,
+  +label: string,
+  +onClick: () => mixed,
+};
+
+function CommunityHeaderButton(props: Props): React.Node {
+  const { icon, label, onClick } = props;
+
+  const { onMouseEnter, onMouseLeave } = useLabelTooltip({
+    tooltipLabel: label,
+    position: tooltipPositions.BOTTOM,
+    tooltipMargin: 16,
+  });
+
+  return (
+    <div
+      className={css.container}
+      onClick={onClick}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      <SWMansionIcon icon={icon} size={20} className={css.addButton} />
+    </div>
+  );
+}
+
+export default CommunityHeaderButton;
