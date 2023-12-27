037487f4658eaca727a77ea04691caab8d141de6
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-12-27 02:54:05 -0500

    [web] introduce card component
    Summary: This component introduces the "Card" component. I built the "Card" component as the new UI is moslty made up of these "Cards" to house various parts of Comm, and I wanted to make sure we could easily enforce the style rules that every "Card" will need. The "Card" component is made up of two parts: the header and the body.
    
    Test Plan: Please see the following screenshots
    
    Reviewers:
    
    Subscribers:

---
+++ web/components/card.react.js
@@ -0,0 +1,64 @@
+// @flow
+
+import classnames from 'classnames';
+import * as React from 'react';
+
+import css from './card.css';
+
+type CardItemProps = {
+  +header: React.Node,
+  +body: React.Node,
+  +className?: string,
+};
+
+function CardItem(props: CardItemProps): React.Node {
+  const { header, body } = props;
+
+  return (
+    <>
+      <div className={css.headerShadow}>{header}</div>
+      {body}
+    </>
+  );
+}
+
+type CardContainerProps = {
+  +children: React.ChildrenArray<React.Element<typeof CardItem>>,
+};
+
+function CardContainer(props: CardContainerProps): React.Node {
+  const { children } = props;
+
+  const items = React.useMemo(
+    () =>
+      React.Children.map(children, (child, index) => {
+        if (!child) {
+          return null;
+        }
+
+        const numOfCards = React.Children.count(children);
+
+        const className = classnames(
+          {
+            [css.itemContainer]: true,
+            [css.secondaryItemContainer]: index % 2 === 1,
+            [css.firstItemContainer]: numOfCards > 1 && index === 0,
+            [css.lastItemContainer]: numOfCards > 1 && index === numOfCards - 1,
+          },
+          child.props.className,
+        );
+
+        return <div className={className}>{child}</div>;
+      }),
+    [children],
+  );
+
+  return <div className={css.container}>{items}</div>;
+}
+
+const Card = {
+  Container: CardContainer,
+  Item: CardItem,
+};
+
+export default Card;
