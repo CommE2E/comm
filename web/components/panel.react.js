ab09a35fc272d5afd13322a8b073d6f3ff222679
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-12-27 02:54:05 -0500

    [web] introduce panel component
    Summary: This component introduces the "Card" component. I built the "Card" component as the new UI is moslty made up of these "Cards" to house various parts of Comm, and I wanted to make sure we could easily enforce the style rules that every "Card" will need. The "Card" component is made up of two parts: the header and the body.
    
    Test Plan: Please see the following screenshots
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10500

---
+++ web/components/panel.react.js
@@ -0,0 +1,69 @@
+// @flow
+
+import classnames from 'classnames';
+import * as React from 'react';
+
+import css from './panel.css';
+
+export type PanelData = {
+  +header: React.Node,
+  +body: React.Node,
+  +classNameOveride?: string,
+};
+
+type PanelItemProps = {
+  ...PanelData,
+  +index: number,
+  +numOfItems: number,
+};
+
+function PanelItem(props: PanelItemProps): React.Node {
+  const { header, body, index, numOfItems, classNameOveride } = props;
+
+  const className = classnames(
+    {
+      [css.itemContainer]: true,
+      [css.secondaryItemContainer]: index % 2 === 1,
+      [css.singleItemContainer]: numOfItems === 1,
+      [css.firstMultiItemContainer]: numOfItems > 1 && index === 0,
+      [css.lastMultiItemContainer]: numOfItems > 1 && index === numOfItems - 1,
+    },
+    classNameOveride,
+  );
+
+  return (
+    <div className={className}>
+      <div className={css.headerContainer}>{header}</div>
+      {body}
+    </div>
+  );
+}
+
+type PanelProps = {
+  +panelItems: $ReadOnlyArray<PanelData>,
+};
+
+function Panel(props: PanelProps): React.Node {
+  const { panelItems } = props;
+
+  const items = React.useMemo(
+    () =>
+      panelItems.map((item, index) => {
+        return (
+          <PanelItem
+            key={index}
+            header={item.header}
+            body={item.body}
+            classNameOveride={item.classNameOveride}
+            index={index}
+            numOfItems={panelItems.length}
+          />
+        );
+      }),
+    [panelItems],
+  );
+
+  return <div className={css.container}>{items}</div>;
+}
+
+export default Panel;
