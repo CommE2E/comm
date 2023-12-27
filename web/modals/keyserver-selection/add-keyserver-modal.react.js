703376b0c0813c57056342218ec2f8a012feb052
Inka Sokołowska <inka.d.sokolowska@gmail.com>
2023-12-27 13:27:23 +0100

    [lib][web][native] Create defaultKeyserverInfo
    Summary:
    issue: https://linear.app/comm/issue/ENG-6299/save-new-keyserver-on-set-new-session
    I will need this value in yet another place so I think it makes sense to extrac it
    
    Test Plan: Tested that add it's still possible to add a keyserver, and the entry looks like it did before
    
    Reviewers: ginsu, michal, kamil
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10484

--- web/modals/keyserver-selection/add-keyserver-modal.react.js
+++ web/modals/keyserver-selection/add-keyserver-modal.react.js
@@ -6,7 +6,7 @@ import { addKeyserverActionType } from 'lib/actions/keyserver-actions.js';
 import { useModalContext } from 'lib/components/modal-provider.react.js';
 import { useIsKeyserverURLValid } from 'lib/shared/keyserver-utils.js';
 import type { KeyserverInfo } from 'lib/types/keyserver-types.js';
-import { defaultConnectionInfo } from 'lib/types/socket-types.js';
+import { defaultKeyserverInfo } from 'lib/types/keyserver-types.js';
 import { useDispatch } from 'lib/utils/redux-utils.js';
 
 import css from './add-keyserver-modal.css';
@@ -53,12 +53,8 @@ function AddKeyserverModal(): React.Node {
     }
 
     const newKeyserverInfo: KeyserverInfo = {
-      cookie: null,
-      updatesCurrentAsOf: 0,
+      ...defaultKeyserverInfo,
       urlPrefix: keyserverURL,
-      connection: defaultConnectionInfo,
-      lastCommunicatedPlatformDetails: null,
-      deviceToken: null,
     };
 
     dispatch({
