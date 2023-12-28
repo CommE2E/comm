570a8b715d9166cba56930c8bee15e59bf5147db
William Wang <williamwang749@gmail.com>
2023-12-28 11:56:37 -0500

    [identity] [lib] [keyserver] [7/n] Add userID to reserved_usernames table in DDB
    Summary:
    Adding userID to reserved_usernames table. This is to enable future work streaming usernames to opensearch instance.
    
    Depends on D10438
    
    Test Plan: Test on local dev. Registered new user on ios and successfully persisted userID attribute in dynamoDB.
    
    Reviewers: varun, bartek
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10480

--- keyserver/src/cron/update-identity-reserved-usernames.js
+++ keyserver/src/cron/update-identity-reserved-usernames.js
@@ -4,19 +4,19 @@ import { getRustAPI } from 'rust-node-addon';
 
 import type { ReservedUsernameMessage } from 'lib/types/crypto-types.js';
 
-import { fetchAllUsernames } from '../fetchers/user-fetchers.js';
+import { fetchAllUserDetails } from '../fetchers/user-fetchers.js';
 import { fetchOlmAccount } from '../updaters/olm-account-updater.js';
 
 async function updateIdentityReservedUsernames(): Promise<void> {
-  const [usernames, rustAPI, accountInfo] = await Promise.all([
-    fetchAllUsernames(),
+  const [userDetails, rustAPI, accountInfo] = await Promise.all([
+    fetchAllUserDetails(),
     getRustAPI(),
     fetchOlmAccount('content'),
   ]);
   const issuedAt = new Date().toISOString();
   const reservedUsernameMessage: ReservedUsernameMessage = {
     statement: 'Add the following usernames to reserved list',
-    payload: usernames,
+    payload: userDetails,
     issuedAt,
   };
   const stringifiedMessage = JSON.stringify(reservedUsernameMessage);
