1e7bfece5f6a31056d035b403d2b88a1c7d7d150
Ginsu Eddy <ginsu.eddy@gmail.com>
2023-12-27 23:04:33 -0500

    [web] extend navigation sidebar tooltip api
    Summary:
    In the new community header/topbar, we will be introducing some buttons that when hovered, a tooltip will be rendered below the element at a smaller margin (16px instead of 24px). At the moment, the only direction the navigation sidebar tooltip can go is right with 24px of margin. So this diff extends the api of the navigation sidebar tooltip to take in a tooltip position and a margin size and will render the navigation sidebar tooltip based off these new parameters.
    
    Also please note, I am aware naming this component a navigation sidebar tooltip is probably now a bit dated, but in the very next diff I will update the name of this component to be something more generic
    
    For context here is what the buttons on the community header/topbar will look like:
    {F1010681}
    
    Depends on D10471
    
    Test Plan:
    Please see the screenshots below. Also witch each example below I confirmed that the tooltip size returned by `calculateNavigationSidebarTooltipSize` correctly matched the size of the element
    
    right w/ 16px of margin
    {F1010686}
    
    left w/24 px of margin
    {F1010687}
    
    top w/ 24px of margin
    {F1010689}
    
    bottom w/ 16px of margin
    {F1010688}
    
    Reviewers: atul, rohan, kamil
    
    Reviewed By: atul
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10472

--- web/navigation-sidebar/navigation-sidebar-toolitp.react.js
+++ web/navigation-sidebar/navigation-sidebar-toolitp.react.js
@@ -1,26 +1,44 @@
 // @flow
 
+import classNames from 'classnames';
 import * as React from 'react';
 
-import {
-  navigationSidebarTooltipContainerStyle,
-  navigationSidebarTooltipStyle,
-} from './navigation-sidebar-constants.js';
+import { navigationSidebarTooltipStyle } from './navigation-sidebar-constants.js';
 import css from './navigation-sidebar-tooltip.css';
+import {
+  tooltipPositions,
+  type TooltipPosition,
+} from '../utils/tooltip-utils.js';
 
 type Props = {
   +tooltipLabel: string,
+  +position: TooltipPosition,
+  +tooltipMargin: number,
 };
 
 function NavigationSidebarTooltip(props: Props): React.Node {
-  const { tooltipLabel } = props;
+  const { tooltipLabel, position, tooltipMargin } = props;
+
+  const tooltipMarginStyle = React.useMemo(
+    () => ({
+      marginLeft: position === tooltipPositions.RIGHT ? tooltipMargin : 0,
+      marginRight: position === tooltipPositions.LEFT ? tooltipMargin : 0,
+      marginTop: position === tooltipPositions.BOTTOM ? tooltipMargin : 0,
+      marginBottom: position === tooltipPositions.TOP ? tooltipMargin : 0,
+    }),
+    [position, tooltipMargin],
+  );
+
+  const arrowClassName = classNames(css.arrow, {
+    [css.arrowLeft]: position === tooltipPositions.RIGHT,
+    [css.arrowRight]: position === tooltipPositions.LEFT,
+    [css.arrowTop]: position === tooltipPositions.BOTTOM,
+    [css.arrowBottom]: position === tooltipPositions.TOP,
+  });
 
   return (
-    <div
-      className={css.container}
-      style={navigationSidebarTooltipContainerStyle}
-    >
-      <div className={css.arrowLeft} />
+    <div className={css.container} style={tooltipMarginStyle}>
+      <div className={arrowClassName} />
       <div className={css.tooltipLabel} style={navigationSidebarTooltipStyle}>
         {tooltipLabel}
       </div>
