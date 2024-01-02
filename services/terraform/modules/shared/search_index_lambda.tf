313dad67615bc47edfd220030d6e7ffff0edb791
William Wang <williamwang749@gmail.com>
2024-01-02 00:04:42 -0500

    [terraform] [services] [8/n] Add lambda trigger for reserved usernames
    Summary: This adds an event trigger to have the lambda function add userID and usernames
    
    Test Plan: Tested on staging with successful insertion
    
    Reviewers: varun, bartek
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10501

--- services/terraform/modules/shared/search_index_lambda.tf
+++ services/terraform/modules/shared/search_index_lambda.tf
@@ -41,6 +41,12 @@ resource "aws_lambda_event_source_mapping" "identity_users_trigger" {
   starting_position = "LATEST"
 }
 
+resource "aws_lambda_event_source_mapping" "identity_reserved_usernames_trigger" {
+  event_source_arn  = aws_dynamodb_table.identity-reserved-usernames.stream_arn
+  function_name     = aws_lambda_function.search_index_lambda.arn
+  starting_position = "LATEST"
+}
+
 resource "aws_security_group" "search_index_lambda" {
   name   = "search_index_lambda_sg"
   vpc_id = var.vpc_id
