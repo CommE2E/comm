9b9d3a542cb8dc75623c20f195d8d627b216faf4
marcinwasowicz <marcinkwasowicz@gmail.com>
2024-01-02 10:25:21 +0100

    Implement tools to manage backup files
    Summary:
    This differential introduces API that allows to get the path to a backup file associated with particular backup ID and to remove backup files directory at once during
    log-out.
    
    Test Plan:
    1. Call `PlatformSpecificTools::getBackupFilePath()` in C++ code which execution can be easily triggered manually (e.x. draft update).
    2. Trigger execution of the code above.
    3. Download application container from XCode or examine Device File Explorer in Android studio to ensure that empty backup directory was created.
    4. Log out.
    5. Examine application file system again. Ensure directory was deleted.
    
    Reviewers: kamil, michal, bartek
    
    Reviewed By: kamil
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10502

--- native/android/app/src/main/java/app/comm/android/fbjni/PlatformSpecificTools.java
+++ native/android/app/src/main/java/app/comm/android/fbjni/PlatformSpecificTools.java
@@ -3,9 +3,12 @@ package app.comm.android.fbjni;
 import android.content.Context;
 import android.util.Log;
 import app.comm.android.MainApplication;
+import java.io.File;
+import java.lang.SecurityException;
 import java.nio.charset.StandardCharsets;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+
 public class PlatformSpecificTools {
   static SecureRandom secureRandom = new SecureRandom();
 
@@ -25,5 +28,74 @@ public class PlatformSpecificTools {
     return mainApplicationContext
         .getFileStreamPath("comm_notifications_crypto_account")
         .getPath();
+  }
+
+  public static String getBackupDirectoryPath() {
+    Context mainApplicationContext =
+        MainApplication.getMainApplicationContext();
+    if (mainApplicationContext == null) {
+      throw new RuntimeException(
+          "Failed to resolve backup path - main application context not initialized.");
+    }
+
+    String filesDirPath = mainApplicationContext.getFilesDir().getPath();
+    String backupDirPath = String.join(File.separator, filesDirPath, "backup");
+
+    try {
+      File backupDirectory = new File(backupDirPath);
+      if (!backupDirectory.exists() && !backupDirectory.mkdirs()) {
+        throw new RuntimeException("Failed to create backup directory.");
+      }
+      return backupDirPath;
+    } catch (SecurityException | NullPointerException e) {
+      throw new RuntimeException(
+          "Failed to check if backup directory exists or to attempt its creation. Details: " +
+          e.getMessage());
+    }
+  }
+
+  public static String
+  getBackupFilePath(String backupID, boolean isAttachments) {
+    String backupDirPath = PlatformSpecificTools.getBackupDirectoryPath();
+
+    String filename;
+    if (isAttachments) {
+      filename = String.join("_", "backup", backupID, "attachments");
+    } else {
+      filename = String.join("_", "backup", backupID);
+    }
+    return String.join(File.separator, backupDirPath, filename);
+  }
+
+  public static void removeBackupDirectory() {
+    String backupDirPath = PlatformSpecificTools.getBackupDirectoryPath();
+    try {
+      File backupDirectory = new File(backupDirPath);
+      if (!backupDirectory.exists()) {
+        return;
+      }
+
+      File[] files = backupDirectory.listFiles();
+      if (files == null && !backupDirectory.delete()) {
+        throw new RuntimeException("Failed to remove backup directory.");
+      } else if (files == null) {
+        return;
+      }
+
+      // Backup directory structure is supposed to be flat.
+      for (File file : files) {
+        if (!file.delete()) {
+          throw new RuntimeException(
+              "Failed to remove backup file at path: " + file.getPath());
+        }
+      }
+
+      if (!backupDirectory.delete()) {
+        throw new RuntimeException("Failed to remove backup directory.");
+      }
+    } catch (NullPointerException | SecurityException e) {
+      throw new RuntimeException(
+          "Failed to remove backup directory. Details: " + e.getMessage());
+    }
   }
 }
