4a6e4c7fbdd1d38b3d2e15fc33ceca192ebf1eff
marcinwasowicz <marcinkwasowicz@gmail.com>
2024-01-02 10:25:21 +0100

    Implement tools to manage backup files

--- native/ios/Comm/PlatformSpecificTools.mm
+++ native/ios/Comm/PlatformSpecificTools.mm
@@ -37,4 +37,80 @@ std::string PlatformSpecificTools::getNotificationsCryptoAccountPath() {
               .path UTF8String]);
 }
 
+std::string PlatformSpecificTools::getBackupFilePath(
+    std::string backupID,
+    bool isAttachments) {
+
+  NSError *err = nil;
+  NSURL *documentsUrl =
+      [NSFileManager.defaultManager URLForDirectory:NSDocumentDirectory
+                                           inDomain:NSUserDomainMask
+                                  appropriateForURL:nil
+                                             create:false
+                                              error:&err];
+  if (err) {
+    NSLog(@"Error: %@", err);
+    throw std::runtime_error(
+        "Failed to resolve backup path - could not find documentsUrl");
+  }
+
+  NSURL *backupDir = [documentsUrl URLByAppendingPathComponent:@"backup"];
+
+  NSError *backupDirCreateError = nil;
+  if (![NSFileManager.defaultManager fileExistsAtPath:backupDir.path]) {
+    [NSFileManager.defaultManager createDirectoryAtURL:backupDir
+                           withIntermediateDirectories:YES
+                                            attributes:nil
+                                                 error:&backupDirCreateError];
+  }
+  if (backupDirCreateError) {
+    throw std::runtime_error(
+        "Failed to create backup directory. Details: " +
+        std::string([backupDirCreateError.localizedDescription UTF8String]));
+  }
+
+  NSString *backupIDObjC = [NSString stringWithCString:backupID.c_str()
+                                              encoding:NSUTF8StringEncoding];
+  if (isAttachments) {
+    return [[backupDir
+                URLByAppendingPathComponent:
+                    [@"backup-id-attachments-"
+                        stringByAppendingString:backupIDObjC]].path UTF8String];
+  }
+  return [[backupDir
+              URLByAppendingPathComponent:
+                  [@"backup-id-"
+                      stringByAppendingString:backupIDObjC]].path UTF8String];
+}
+
+void PlatformSpecificTools::removeBackupDirectory() {
+  NSError *err = nil;
+  NSURL *documentsUrl =
+      [NSFileManager.defaultManager URLForDirectory:NSDocumentDirectory
+                                           inDomain:NSUserDomainMask
+                                  appropriateForURL:nil
+                                             create:false
+                                              error:&err];
+  if (err) {
+    NSLog(@"Error: %@", err);
+    throw std::runtime_error(
+        "Failed to resolve backup path - could not find documentsUrl");
+  }
+
+  NSURL *backupDir = [documentsUrl URLByAppendingPathComponent:@"backup"];
+  if (![NSFileManager.defaultManager fileExistsAtPath:backupDir.path]) {
+    return;
+  }
+
+  NSError *backupDirRemovalError = nil;
+  [NSFileManager.defaultManager removeItemAtURL:backupDir
+                                          error:&backupDirRemovalError];
+
+  if (backupDirRemovalError) {
+    throw std::runtime_error(
+        "Failed to remove backup directory. Details: " +
+        std::string([backupDirRemovalError.localizedDescription UTF8String]));
+  }
+}
+
 }; // namespace comm
