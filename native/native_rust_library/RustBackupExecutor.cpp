9afe7036d3bf9dd27680a7896e153cdb521f8cb9
marcinwasowicz <marcinkwasowicz@gmail.com>
2024-01-03 17:51:33 +0100

    Enable backup creation and restoration from Rust
    Summary: This differential provides boilerplate code so that we can call `BackupOperationsExecutor` from Rust.
    
    Test Plan: edit in web
    
    Reviewers: kamil, michal
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10512

--- native/native_rust_library/RustBackupExecutor.cpp
+++ native/native_rust_library/RustBackupExecutor.cpp
@@ -14,4 +14,16 @@ rust::String getBackupFilePath(rust::String backupID, bool isAttachments) {
   return rust::String(PlatformSpecificTools::getBackupFilePath(
       std::string(backupID), isAttachments));
 }
+
+void createMainCompaction(rust::String backupID) {
+  BackupOperationsExecutor::createMainCompaction(std::string(backupID));
+}
+
+void restoreFromMainCompaction(
+    rust::String mainCompactionPath,
+    rust::String mainCompactionEncryptionKey) {
+  BackupOperationsExecutor::restoreFromMainCompaction(
+      std::string(mainCompactionPath),
+      std::string(mainCompactionEncryptionKey));
+}
 } // namespace comm
