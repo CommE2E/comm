db000882ad8246040785458e928fa1a287793f58
Ginsu Eddy <ginsu.eddy@gmail.com>
2024-01-04 01:29:13 -0500

    [web] house the thread list in a panel component
    Summary: This diff updates the chat component to use the panel component I introduced earlier. To make things easy to review I only included the changes for the thread list; however, in subsequent diffs I will bring back the message list and house it in this same panel component
    
    Test Plan: Please see a screenshot below
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10528

--- web/chat/chat.react.js
+++ web/chat/chat.react.js
@@ -5,42 +5,42 @@ import * as React from 'react';
 import ThreadDraftUpdater from 'lib/components/thread-draft-updater.react.js';
 import { isLoggedIn } from 'lib/selectors/user-selectors.js';
 
-import ChatMessageListContainer from './chat-message-list-container.react.js';
 import ChatTabs from './chat-tabs.react.js';
+import ChatThreadList from './chat-thread-list.react.js';
+import css from './chat.css';
 import { ThreadListProvider } from './thread-list-provider.js';
+import Panel, { type PanelData } from '../components/panel.react.js';
 import { useSelector } from '../redux/redux-utils.js';
 
 function Chat(): React.Node {
   const loggedIn = useSelector(isLoggedIn);
-  const activeChatThreadID = useSelector(
-    state => state.navInfo.activeChatThreadID,
-  );
-  const chatModeIsCreate = useSelector(
-    state => state.navInfo.chatMode === 'create',
-  );
-  const chatList = React.useMemo(
-    () => (
-      <ThreadListProvider>
-        <ChatTabs />
-      </ThreadListProvider>
-    ),
-    [],
-  );
-  const messageList = React.useMemo(() => {
-    if (!activeChatThreadID && !chatModeIsCreate) {
-      return null;
-    }
-    return <ChatMessageListContainer activeChatThreadID={activeChatThreadID} />;
-  }, [activeChatThreadID, chatModeIsCreate]);
+
+  const chatList = React.useMemo(() => <ChatThreadList />, []);
 
   let threadDraftUpdater = null;
   if (loggedIn) {
     threadDraftUpdater = <ThreadDraftUpdater />;
   }
+
+  const panelData: $ReadOnlyArray<PanelData> = React.useMemo(
+    () => [
+      {
+        header: <ChatTabs />,
+        body: chatList,
+        classNameOveride: css.threadListPanel,
+      },
+    ],
+    [chatList],
+  );
+
+  const chatPanel = React.useMemo(
+    () => <Panel panelItems={panelData} />,
+    [panelData],
+  );
+
   return (
     <>
-      {chatList}
-      {messageList}
+      <ThreadListProvider>{chatPanel}</ThreadListProvider>
       {threadDraftUpdater}
     </>
   );
