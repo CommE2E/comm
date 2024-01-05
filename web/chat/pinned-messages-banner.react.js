185ca5ce200568d9858ca3c13dc6ea8ad3e28044
Ginsu Eddy <ginsu.eddy@gmail.com>
2024-01-04 21:58:45 -0500

    [web] factor out pinned messages banner into its own component
    Summary:
    We will be using the thread top bar component as the panel header for the message list panel item. In order to accomplish this we will need to decouple the pinned messages banner from the top bar. To make the move a bit easier/more organized, I took a second to make the pinned messages banner into its own component.
    
    Additionally, I snuck in some color changes that have the pinned messages banner utilize the new color design system.
    
    Test Plan:
    `flow` and confirmed the pinned messages banner still works as expected. Also please see the screenshots below:
    
    dark:
    {F1055725}
    
    light:
    {F1055726}
    
    Reviewers: atul, inka
    
    Reviewed By: atul
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10548

---
+++ web/chat/pinned-messages-banner.react.js
@@ -0,0 +1,55 @@
+// @flow
+
+import * as React from 'react';
+import { ChevronRight } from 'react-feather';
+
+import { useModalContext } from 'lib/components/modal-provider.react.js';
+import type { ThreadInfo } from 'lib/types/thread-types.js';
+import { pinnedMessageCountText } from 'lib/utils/message-pinning-utils.js';
+
+import css from './pinned-messages-banner.css';
+import { InputStateContext } from '../input/input-state.js';
+import PinnedMessagesModal from '../modals/chat/pinned-messages-modal.react.js';
+
+type Props = {
+  +threadInfo: ThreadInfo,
+};
+
+function PinnedMessagesBanner(props: Props): React.Node {
+  const { threadInfo } = props;
+
+  const { pushModal } = useModalContext();
+
+  const inputState = React.useContext(InputStateContext);
+
+  const pushThreadPinsModal = React.useCallback(() => {
+    pushModal(
+      <InputStateContext.Provider value={inputState}>
+        <PinnedMessagesModal threadInfo={threadInfo} />
+      </InputStateContext.Provider>,
+    );
+  }, [pushModal, inputState, threadInfo]);
+
+  const pinnedMessagesBanner = React.useMemo(() => {
+    const bannerText =
+      !!threadInfo.pinnedCount &&
+      pinnedMessageCountText(threadInfo.pinnedCount);
+
+    if (!bannerText) {
+      return null;
+    }
+
+    return (
+      <div className={css.container}>
+        <a className={css.pinnedCountText} onClick={pushThreadPinsModal}>
+          {bannerText}
+          <ChevronRight size={14} className={css.chevronRight} />
+        </a>
+      </div>
+    );
+  }, [pushThreadPinsModal, threadInfo.pinnedCount]);
+
+  return pinnedMessagesBanner;
+}
+
+export default PinnedMessagesBanner;
