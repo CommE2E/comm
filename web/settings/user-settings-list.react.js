c484c6b93f3d13cb7450b37debc4b2ce61640ba8
Ginsu Eddy <ginsu.eddy@gmail.com>
2024-01-08 15:40:09 -0500

    [web] introduce user settings list
    Summary: This diff introduces the user settings list component. This component will be used to switch between the various comm user settings.
    
    Test Plan: Please see the screenshot below
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10612

---
+++ web/settings/user-settings-list.react.js
@@ -0,0 +1,92 @@
+// @flow
+
+import * as React from 'react';
+
+import { useDispatch } from 'lib/utils/redux-utils.js';
+
+import UserSettingsListItem from './user-settings-list-item.react.js';
+import css from './user-settings-list.css';
+import PanelHeader from '../components/panel-header.react.js';
+import Panel, { type PanelData } from '../components/panel.react.js';
+import { updateNavInfoActionType } from '../redux/action-types.js';
+import { useStaffCanSee } from '../utils/staff-utils.js';
+
+function UserSettingsList(): React.Node {
+  const dispatch = useDispatch();
+
+  const staffCanSee = useStaffCanSee();
+
+  const onClickAccountSettings = React.useCallback(() => {
+    dispatch({
+      type: updateNavInfoActionType,
+      payload: { tab: 'settings', settingsSection: 'account' },
+    });
+  }, [dispatch]);
+
+  const onClickKeyservers = React.useCallback(() => {
+    dispatch({
+      type: updateNavInfoActionType,
+      payload: { tab: 'settings', settingsSection: 'keyservers' },
+    });
+  }, [dispatch]);
+
+  const onClickDangerZone = React.useCallback(() => {
+    dispatch({
+      type: updateNavInfoActionType,
+      payload: { tab: 'settings', settingsSection: 'danger-zone' },
+    });
+  }, [dispatch]);
+
+  const keyserverSettingsListItem = React.useMemo(() => {
+    if (!staffCanSee) {
+      return null;
+    }
+
+    return (
+      <UserSettingsListItem
+        id="keyservers"
+        name="Keyservers"
+        onClick={onClickKeyservers}
+      />
+    );
+  }, [onClickKeyservers, staffCanSee]);
+
+  const userSettingsListBody = React.useMemo(
+    () => (
+      <div className={css.userSettingsList}>
+        <UserSettingsListItem
+          id="account"
+          name="My account"
+          onClick={onClickAccountSettings}
+        />
+        {keyserverSettingsListItem}
+        <UserSettingsListItem
+          id="danger-zone"
+          name="Danger zone"
+          onClick={onClickDangerZone}
+        />
+      </div>
+    ),
+    [keyserverSettingsListItem, onClickAccountSettings, onClickDangerZone],
+  );
+
+  const panelData: $ReadOnlyArray<PanelData> = React.useMemo(
+    () => [
+      {
+        header: <PanelHeader headerLabel="Settings" />,
+        body: userSettingsListBody,
+        classNameOveride: css.container,
+      },
+    ],
+    [userSettingsListBody],
+  );
+
+  const userSettingsList = React.useMemo(
+    () => <Panel panelItems={panelData} />,
+    [panelData],
+  );
+
+  return userSettingsList;
+}
+
+export default UserSettingsList;
