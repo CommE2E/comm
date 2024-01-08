97953d2ccfd9ba5e8b9d4070f4850e217453e278
Ginsu Eddy <ginsu.eddy@gmail.com>
2024-01-08 00:09:27 -0500

    [web] introduce chat message list panel item
    Summary: This diff reintroduces the chat message list back into the chat component and now houses it inside the panel component.
    
    Test Plan: Please see the screenshots below
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10567

--- web/chat/chat.react.js
+++ web/chat/chat.react.js
@@ -1,21 +1,44 @@
 // @flow
 
+import invariant from 'invariant';
 import * as React from 'react';
 
 import ThreadDraftUpdater from 'lib/components/thread-draft-updater.react.js';
 import { isLoggedIn } from 'lib/selectors/user-selectors.js';
 
+import ChatMessageListContainer from './chat-message-list-container.react.js';
 import ChatTabs from './chat-tabs.react.js';
 import ChatThreadList from './chat-thread-list.react.js';
 import css from './chat.css';
 import { ThreadListProvider } from './thread-list-provider.js';
+import ThreadTopBar from './thread-top-bar.react.js';
 import Panel, { type PanelData } from '../components/panel.react.js';
 import { useSelector } from '../redux/redux-utils.js';
+import { useThreadInfoForPossiblyPendingThread } from '../utils/thread-utils.js';
 
 function Chat(): React.Node {
   const loggedIn = useSelector(isLoggedIn);
+  const activeChatThreadID = useSelector(
+    state => state.navInfo.activeChatThreadID,
+  );
+  const chatModeIsCreate = useSelector(
+    state => state.navInfo.chatMode === 'create',
+  );
+
+  const threadInfo = useThreadInfoForPossiblyPendingThread(activeChatThreadID);
+  invariant(threadInfo, 'threadInfo should be set');
 
   const chatList = React.useMemo(() => <ChatThreadList />, []);
+  const messageList = React.useMemo(() => {
+    if (!activeChatThreadID && !chatModeIsCreate) {
+      return null;
+    }
+    return (
+      <>
+        <ChatMessageListContainer activeChatThreadID={activeChatThreadID} />
+      </>
+    );
+  }, [activeChatThreadID, chatModeIsCreate]);
 
   let threadDraftUpdater = null;
   if (loggedIn) {
@@ -29,8 +52,13 @@ function Chat(): React.Node {
         body: chatList,
         classNameOveride: css.threadListPanel,
       },
+      {
+        header: <ThreadTopBar threadInfo={threadInfo} />,
+        body: messageList,
+        classNameOveride: css.messageListPanel,
+      },
     ],
-    [chatList],
+    [chatList, messageList, threadInfo],
   );
 
   const chatPanel = React.useMemo(
