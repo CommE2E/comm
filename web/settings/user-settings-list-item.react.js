20a5f76447d999e757ff41f7c7b4cbe15331132b
Ginsu Eddy <ginsu.eddy@gmail.com>
2024-01-08 15:30:46 -0500

    [web] introduce user settings list item
    Summary:
    This diff introduces the settings list item. This component will be used in the settings list to swtich between the different user settings which I will introduce in a subsequent diff.
    
    For context here is what the user settings list looks like in the figma:
    {F1056021}
    
    Linear task: https://linear.app/comm/issue/ENG-5952/settings-list-ui
    
    Depends on D10610
    
    Test Plan:
    Please see the demo video below:
    
    {F1056022}
    
    Reviewers: atul, inka
    
    Reviewed By: atul
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10611

---
+++ web/settings/user-settings-list-item.react.js
@@ -0,0 +1,38 @@
+// @flow
+
+import classNames from 'classnames';
+import * as React from 'react';
+
+import css from './user-settings-list-item.css';
+import { useSelector } from '../redux/redux-utils.js';
+import { navSettingsSectionSelector } from '../selectors/nav-selectors.js';
+import type { NavigationSettingsSection } from '../types/nav-types.js';
+
+type Props = {
+  +id: NavigationSettingsSection,
+  +name: string,
+  +onClick: () => mixed,
+};
+
+function UserSettingsListItem(props: Props): React.Node {
+  const { id, name, onClick } = props;
+
+  const currentSelectedSettings = useSelector(navSettingsSectionSelector);
+
+  const className = classNames(css.container, {
+    [css.selected]: currentSelectedSettings === id,
+  });
+
+  const userSettingsListItem = React.useMemo(
+    () => (
+      <div className={className} onClick={onClick}>
+        {name}
+      </div>
+    ),
+    [className, name, onClick],
+  );
+
+  return userSettingsListItem;
+}
+
+export default UserSettingsListItem;
