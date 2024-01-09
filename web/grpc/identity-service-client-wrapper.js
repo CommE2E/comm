34f749191af5947631aae8a4468f867adf4ed67e
Tomasz Palys <tomasz.palys@swmansion.com>
2024-01-09 16:28:27 +0100

    [lib][native][web] Expose identity API by using a context
    Summary:
    Create a context that exposes the API client and recreates it on every user change or token change. Simplify the client by avoiding a need to provide credentials on every call to the API.
    
    Depends on D10449
    
    Test Plan: TODO
    
    Reviewers: inka, varun, kamil, bartek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D10584

--- web/grpc/identity-service-client-wrapper.js
+++ web/grpc/identity-service-client-wrapper.js
@@ -3,6 +3,7 @@
 import identityServiceConfig from 'lib/facts/identity-service.js';
 import type {
   IdentityServiceAuthLayer,
+  IdentityServiceClient,
   OutboundKeyInfoResponse,
 } from 'lib/types/identity-service-types.js';
 
@@ -12,23 +13,30 @@ import * as IdentityAuthStructs from '../protobufs/identity-auth-structs.cjs';
 import * as IdentityClient from '../protobufs/identity-client.cjs';
 import { Empty } from '../protobufs/identity-structs.cjs';
 
-class IdentityServiceClientWrapper {
+class IdentityServiceClientWrapper implements IdentityServiceClient {
   authClient: ?IdentityAuthClient.IdentityClientServicePromiseClient;
-  unauthorizedClient: ?IdentityClient.IdentityClientServicePromiseClient;
+  unauthorizedClient: IdentityClient.IdentityClientServicePromiseClient;
 
-  constructor() {
-    this.authClient = null;
-    this.unauthorizedClient = null;
+  constructor(authLayer: ?IdentityServiceAuthLayer) {
+    if (authLayer) {
+      this.authClient =
+        IdentityServiceClientWrapper.createAuthClient(authLayer);
+    }
+    this.unauthorizedClient =
+      IdentityServiceClientWrapper.createUnauthorizedClient();
   }
 
-  determineSocketAddr(): string {
+  static determineSocketAddr(): string {
     return process.env.IDENTITY_SOCKET_ADDR ?? identityServiceConfig.defaultURL;
   }
 
-  async initAuthClient(authLayer: IdentityServiceAuthLayer): Promise<void> {
+  static createAuthClient(
+    authLayer: IdentityServiceAuthLayer,
+  ): IdentityAuthClient.IdentityClientServicePromiseClient {
     const { userID, deviceID, commServicesAccessToken } = authLayer;
 
-    const identitySocketAddr = this.determineSocketAddr();
+    const identitySocketAddr =
+      IdentityServiceClientWrapper.determineSocketAddr();
 
     const versionInterceptor = new VersionInterceptor<Request, Response>();
     const authInterceptor = new AuthInterceptor<Request, Response>(
@@ -41,15 +49,16 @@ class IdentityServiceClientWrapper {
       unaryInterceptors: [versionInterceptor, authInterceptor],
     };
 
-    this.authClient = new IdentityAuthClient.IdentityClientServicePromiseClient(
+    return new IdentityAuthClient.IdentityClientServicePromiseClient(
       identitySocketAddr,
       null,
       authClientOpts,
     );
   }
 
-  async initUnauthorizedClient(): Promise<void> {
-    const identitySocketAddr = this.determineSocketAddr();
+  static createUnauthorizedClient(): IdentityClient.IdentityClientServicePromiseClient {
+    const identitySocketAddr =
+      IdentityServiceClientWrapper.determineSocketAddr();
 
     const versionInterceptor = new VersionInterceptor<Request, Response>();
 
@@ -57,83 +66,55 @@ class IdentityServiceClientWrapper {
       unaryInterceptors: [versionInterceptor],
     };
 
-    this.unauthorizedClient =
-      new IdentityClient.IdentityClientServicePromiseClient(
-        identitySocketAddr,
-        null,
-        unauthorizedClientOpts,
-      );
+    return new IdentityClient.IdentityClientServicePromiseClient(
+      identitySocketAddr,
+      null,
+      unauthorizedClientOpts,
+    );
   }
 
-  async deleteUser(
-    userID: string,
-    deviceID: string,
-    accessToken: string,
-  ): Promise<void> {
+  deleteUser: () => Promise<void> = async () => {
     if (!this.authClient) {
-      const authLayer: IdentityServiceAuthLayer = {
-        userID,
-        deviceID,
-        commServicesAccessToken: accessToken,
-      };
-      await this.initAuthClient(authLayer);
-    }
-
-    if (this.authClient) {
-      await this.authClient.deleteUser(new Empty());
-    } else {
       throw new Error('Identity service client is not initialized');
     }
-  }
+    await this.authClient.deleteUser(new Empty());
+  };
 
-  async getKeyserverKeys(
-    userID: string,
-    deviceID: string,
-    accessToken: string,
-    keyserverID: string,
-  ): Promise<?OutboundKeyInfoResponse> {
-    if (!this.authClient) {
-      const authLayer: IdentityServiceAuthLayer = {
-        userID,
-        deviceID,
-        commServicesAccessToken: accessToken,
+  getKeyserverKeys: (keyserverID: string) => Promise<?OutboundKeyInfoResponse> =
+    async (keyserverID: string) => {
+      const client = this.authClient;
+      if (!client) {
+        throw new Error('Identity service client is not initialized');
+      }
+
+      const request = new IdentityAuthStructs.OutboundKeysForUserRequest();
+      request.setUserid(keyserverID);
+      const response = await client.getKeyserverKeys(request);
+      const keyserverInfo = response.getKeyserverinfo();
+      if (!response.hasKeyserverinfo() || !keyserverInfo) {
+        return null;
+      }
+
+      const identityInfo = keyserverInfo.getIdentityinfo();
+      const contentPreKey = keyserverInfo.getContentprekey();
+      const notifPreKey = keyserverInfo.getNotifprekey();
+
+      if (!identityInfo || !contentPreKey || !notifPreKey) {
+        return null;
+      }
+
+      return {
+        payload: identityInfo.getPayload(),
+        payloadSignature: identityInfo.getPayloadsignature(),
+        socialProof: identityInfo.getSocialproof(),
+        contentPrekey: contentPreKey.getPrekey(),
+        contentPrekeySignature: contentPreKey.getPrekeysignature(),
+        notifPrekey: notifPreKey.getPrekey(),
+        notifPrekeySignature: notifPreKey.getPrekeysignature(),
+        oneTimeContentPrekey: keyserverInfo.getOnetimecontentprekey(),
+        oneTimeNotifPrekey: keyserverInfo.getOnetimenotifprekey(),
       };
-      await this.initAuthClient(authLayer);
-    }
-
-    const client = this.authClient;
-    if (!client) {
-      throw new Error('Identity service client is not initialized');
-    }
-
-    const request = new IdentityAuthStructs.OutboundKeysForUserRequest();
-    request.setUserid(keyserverID);
-    const response = await client.getKeyserverKeys(request);
-    const keyserverInfo = response.getKeyserverinfo();
-    if (!response.hasKeyserverinfo() || !keyserverInfo) {
-      return null;
-    }
-
-    const identityInfo = keyserverInfo.getIdentityinfo();
-    const contentPreKey = keyserverInfo.getContentprekey();
-    const notifPreKey = keyserverInfo.getNotifprekey();
-
-    if (!identityInfo || !contentPreKey || !notifPreKey) {
-      return null;
-    }
-
-    return {
-      payload: identityInfo.getPayload(),
-      payloadSignature: identityInfo.getPayloadsignature(),
-      socialProof: identityInfo.getSocialproof(),
-      contentPrekey: contentPreKey.getPrekey(),
-      contentPrekeySignature: contentPreKey.getPrekeysignature(),
-      notifPrekey: notifPreKey.getPrekey(),
-      notifPrekeySignature: notifPreKey.getPrekeysignature(),
-      oneTimeContentPrekey: keyserverInfo.getOnetimecontentprekey(),
-      oneTimeNotifPrekey: keyserverInfo.getOnetimenotifprekey(),
     };
-  }
 }
 
 export { IdentityServiceClientWrapper };
