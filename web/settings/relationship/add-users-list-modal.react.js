287351de86e2709b33751974518a748c259700c1
Ginsu Eddy <ginsu.eddy@gmail.com>
2024-01-10 16:47:34 -0500

    [web] move the send friend request button to add users list modal
    Summary: With the new modals API introduced in D10228 we need to bring the logic for the send friend request button to the `AddUsersListModal` so that we can pass it in as the `primaryButton` prop
    
    Test Plan: `flow`
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10719

--- web/settings/relationship/add-users-list-modal.react.js
+++ web/settings/relationship/add-users-list-modal.react.js
@@ -2,14 +2,29 @@
 
 import * as React from 'react';
 
+import {
+  updateRelationships,
+  updateRelationshipsActionTypes,
+} from 'lib/actions/relationship-actions.js';
+import { createLoadingStatusSelector } from 'lib/selectors/loading-selectors.js';
 import type {
   UserRelationshipStatus,
   RelationshipAction,
 } from 'lib/types/relationship-types.js';
+import { useLegacyAshoatKeyserverCall } from 'lib/utils/action-utils.js';
+import { useDispatchActionPromise } from 'lib/utils/redux-promise-utils.js';
 
+import css from './add-users-list.css';
 import AddUsersList from './add-users-list.react.js';
 import type { ButtonColor } from '../../components/button.react.js';
+import Button from '../../components/button.react.js';
+import LoadingIndicator from '../../loading-indicator.react.js';
 import SearchModal from '../../modals/search-modal.react.js';
+import { useSelector } from '../../redux/redux-utils.js';
+
+const loadingStatusSelector = createLoadingStatusSelector(
+  updateRelationshipsActionTypes,
+);
 
 type Props = {
   +closeModal: () => void,
@@ -30,25 +45,79 @@ function AddUsersListModal(props: Props): React.Node {
     relationshipAction,
   } = props;
 
+  const [errorMessage, setErrorMessage] = React.useState('');
+
   const addUsersListChildGenerator = React.useCallback(
     (searchText: string) => (
       <AddUsersList
         searchText={searchText}
         excludedStatuses={excludedStatuses}
-        confirmButtonContent={confirmButtonContent}
-        confirmButtonColor={confirmButtonColor}
-        relationshipAction={relationshipAction}
-        closeModal={closeModal}
+        errorMessage={errorMessage}
       />
     ),
-    [
-      excludedStatuses,
-      confirmButtonContent,
-      confirmButtonColor,
-      relationshipAction,
-      closeModal,
-    ],
+    [excludedStatuses, errorMessage],
   );
+
+  const callUpdateRelationships =
+    useLegacyAshoatKeyserverCall(updateRelationships);
+
+  const dispatchActionPromise = useDispatchActionPromise();
+
+  const updateRelationshipsPromiseCreator = React.useCallback(async () => {
+    try {
+      setErrorMessage('');
+      const result = await callUpdateRelationships({
+        action: relationshipAction,
+        userIDs: [], // TODO: re-add pending users
+      });
+      closeModal();
+      return result;
+    } catch (e) {
+      setErrorMessage('unknown error');
+      throw e;
+    }
+  }, [callUpdateRelationships, closeModal, relationshipAction]);
+
+  const confirmSelection = React.useCallback(
+    () =>
+      dispatchActionPromise(
+        updateRelationshipsActionTypes,
+        updateRelationshipsPromiseCreator(),
+      ),
+    [dispatchActionPromise, updateRelationshipsPromiseCreator],
+  );
+
+  const loadingStatus = useSelector(loadingStatusSelector);
+
+  const primaryButton = React.useMemo(() => {
+    let buttonContent = confirmButtonContent;
+
+    if (loadingStatus === 'loading') {
+      buttonContent = (
+        <>
+          <div className={css.hidden}>{confirmButtonContent}</div>
+          <LoadingIndicator status="loading" />
+        </>
+      );
+    }
+
+    return (
+      <Button
+        onClick={confirmSelection}
+        // TODO: re-add 0 users check
+        disabled={true || loadingStatus === 'loading'}
+        variant="filled"
+        buttonColor={confirmButtonColor}
+      >
+        <div className={css.confirmButtonContainer}>{buttonContent}</div>
+      </Button>
+    );
+  }, [
+    confirmButtonColor,
+    confirmButtonContent,
+    confirmSelection,
+    loadingStatus,
+  ]);
 
   return (
     <SearchModal
@@ -56,6 +125,7 @@ function AddUsersListModal(props: Props): React.Node {
       onClose={closeModal}
       size="large"
       searchPlaceholder="Search by name"
+      primaryButton={primaryButton}
     >
       {addUsersListChildGenerator}
     </SearchModal>
