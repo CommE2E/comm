f32353e0670a230b43cb1b95d7283eeede4fa7af
Ginsu Eddy <ginsu.eddy@gmail.com>
2024-01-10 23:05:17 -0500

    [web] update add users list item ui
    Summary: The add users modal is getting a brand new ui + ux. This diff updates the add users list item ui to match the new checkbox ui/ux.
    
    Test Plan: Please see the screenshot below
    
    Reviewers:
    
    Subscribers:

--- web/settings/relationship/add-users-list-item.react.js
+++ web/settings/relationship/add-users-list-item.react.js
@@ -1,28 +1,40 @@
 // @flow
 
+import classnames from 'classnames';
 import * as React from 'react';
 
 import type { AccountUserInfo } from 'lib/types/user-types.js';
 
 import css from './add-users-list.css';
-import Button from '../../components/button.react.js';
+import UserAvatar from '../../avatars/user-avatar.react.js';
+import Checkbox from '../../components/checkbox.react.js';
 
 type Props = {
   +userInfo: AccountUserInfo,
-  +selectUser: (userID: string) => mixed,
+  +userSelected: boolean,
+  +onToggleUser: (userID: string) => mixed,
 };
 
 function AddUsersListItem(props: Props): React.Node {
-  const { userInfo, selectUser } = props;
-  const addUser = React.useCallback(
-    () => selectUser(userInfo.id),
-    [selectUser, userInfo.id],
+  const { userInfo, userSelected, onToggleUser } = props;
+
+  const toggleUser = React.useCallback(
+    () => onToggleUser(userInfo.id),
+    [onToggleUser, userInfo.id],
   );
+
+  const usernameClassname = classnames(css.username, {
+    [css.selectedUsername]: userSelected,
+  });
+
   return (
-    <Button className={css.addUserButton} onClick={addUser}>
-      <div className={css.addUserButtonUsername}>{userInfo.username}</div>
-      Add
-    </Button>
+    <div className={css.userListItemContainer} onClick={toggleUser}>
+      <Checkbox checked={userSelected} />
+      <div className={css.avatarContainer}>
+        <UserAvatar userID={userInfo.id} size="S" />
+      </div>
+      <div className={usernameClassname}>{userInfo.username}</div>
+    </div>
   );
 }
 
