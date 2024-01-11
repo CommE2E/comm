65cf384519faa189e18fa25defb9daebf73901dd
William Wang <williamwang749@gmail.com>
2024-01-11 11:49:45 -0500

    [services] Downgrade aws provider to 4.67.0 on local dev
    Summary:
    Opensearch service apply/modify/destroy have delays starting from version 5.0.0. This diff downgrades our local dev environment to version 4.67.0, removing these delays.
    
    https://linear.app/comm/issue/ENG-6393/investigate-reducing-opensearch-local-and-ci-deployment-times#comment-3a151b96
    
    Test Plan:
    Successful `terraform apply` on local dev environment
    
    Reviewers:
    bartek,
    varun
    
    Subscribers:

--- services/terraform/dev/providers.tf
+++ services/terraform/dev/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.7.0"
+      version = "4.67.0"
     }
     random = {
       source  = "hashicorp/random"
