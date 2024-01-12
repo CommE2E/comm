c0cd77ac4a13503bb7087d5b5c39024e77b66b14
Michal Gniadek <michal.gniadek@swmansion.com>
2024-01-12 16:00:54 +0100

    [native] Backup compaction upload
    Summary:
    [ENG-6217 : Procedure for handling compaction/log files](https://linear.app/comm/issue/ENG-6217/procedure-for-handling-compactionlog-files)
    [ENG-6218 : Upload logic for compactions](https://linear.app/comm/issue/ENG-6218/upload-logic-for-compactions)
    
    General logic for file (compaction and log) upload and compaction specific upload. After enabling the backup handler and each time `TRIGGER_BACKUP_FILE_UPLOAD` is triggered we want to iterate over all files in backup directory and upload data. A compaction should consist of 3 files: `backup-XXX` which contains the compaction data (created in C++ database code), `backup-123-userkeys` which contains the **UserKeys** in JSON (this will be created from Rust in a later diff) and an optional `backup-123-attachments` file which contains newline-separated list of attachment hashes (files containing sensitive data will are encrypted). The files names are parsed with regex to get e.g. `backupID`. The data is uploaded and after a succesfull upload, files are deleted.
    
    Depends on D10630
    
    Test Plan: Create `backup-123`, `backup-123-attachments`, `backup-123-userkeys` files. Enable backup upload. Make sure all data was successfuly uploaded to the backup service and the files were removed.
    
    Reviewers: marcin, bartek, kamil
    
    Reviewed By: bartek
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10632

---
+++ native/native_rust_library/src/backup/file_info.rs
@@ -0,0 +1,69 @@
+use lazy_static::lazy_static;
+use regex::Regex;
+use std::path::PathBuf;
+
+lazy_static! {
+  static ref BACKUP_DATA_FILE_REGEX: Regex = Regex::new(
+      r"^backup-(?<backup_id>[^-]*)(?:-log-(?<log_id>\d*))?(?<additional_data>-userkeys|-attachments)?$"
+    )
+    .expect("Regex compilation failed");
+}
+
+#[derive(Debug)]
+pub struct BackupFileInfo {
+  pub backup_id: String,
+  pub log_id: Option<usize>,
+  pub additional_data: Option<String>,
+}
+
+impl TryFrom<PathBuf> for BackupFileInfo {
+  type Error = ();
+
+  fn try_from(value: PathBuf) -> Result<Self, Self::Error> {
+    let Some(file_name) = value.file_name() else {
+      return Err(());
+    };
+    let file_name = file_name.to_string_lossy();
+
+    let Some(captures) = BACKUP_DATA_FILE_REGEX.captures(&file_name) else {
+      return Err(());
+    };
+
+    let Some(backup_id) = captures
+      .name("backup_id")
+      .map(|re_match| re_match.as_str().to_string())
+    else {
+      // Should never happen happen because regex matched the filename
+      println!(
+        "Couldn't parse 'backup_id' from backup filename: {file_name:?}"
+      );
+      return Err(());
+    };
+
+    let log_id = match captures
+      .name("log_id")
+      .map(|re_match| re_match.as_str().parse::<usize>())
+    {
+      None => None,
+      Some(Ok(log_id)) => Some(log_id),
+      Some(Err(err)) => {
+        // Should never happen happen because regex matched the filename
+        println!(
+          "Couldn't parse 'log_id' from backup filename: {file_name:?}. \
+          Error: {err:?}"
+        );
+        return Err(());
+      }
+    };
+
+    let additional_data = captures
+      .name("additional_data")
+      .map(|m| m.as_str().to_string());
+
+    Ok(Self {
+      backup_id,
+      log_id,
+      additional_data,
+    })
+  }
+}
