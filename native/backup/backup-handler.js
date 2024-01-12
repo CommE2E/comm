4be3573b83c59039ceb427235845e33e072fa7d5
Michal Gniadek <michal.gniadek@swmansion.com>
2024-01-12 15:47:38 +0100

    [native] Start/stop backup client from JS
    Summary:
    [ENG-6213 : Create long lived backup client](https://linear.app/comm/issue/ENG-6213/create-long-lived-backup-client)
    
    Start and stop the rust backup client handler from JS if a user is a logged in staff and enables it in the settings.
    
    Depends on D10621
    
    Test Plan:
    Monitor backup service logs and:
    - Enable the backup handler in menu -> the websocket in connected
    - Check that the websocket is disconnected after disabling the backup handler in menu or logging out
    - Check that the websocket automatically reconnects after app is closed and opened (without logging out) and when backup service is killed (simulating network issues)
    
    Reviewers: marcin, bartek, kamil
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10629

--- native/backup/backup-handler.js
+++ native/backup/backup-handler.js
@@ -1,25 +1,15 @@
 // @flow
 
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as React from 'react';
 
 import { isLoggedIn } from 'lib/selectors/user-selectors.js';
 import { accountHasPassword } from 'lib/shared/account-utils.js';
-import { getMessageForException } from 'lib/utils/errors.js';
 
-import { BACKUP_HASH_STORAGE_KEY } from './constants.js';
-import { convertObjToBytes } from './conversion-utils.js';
-import { useClientBackup } from './use-client-backup.js';
-import { commUtilsModule } from '../native-modules.js';
+import { commCoreModule } from '../native-modules.js';
 import { useSelector } from '../redux/redux-utils.js';
-import Alert from '../utils/alert.js';
 import { useStaffCanSee } from '../utils/staff-utils.js';
 
 function BackupHandler(): null {
-  const userStore = useSelector(state => state.userStore);
-  const currentUserID = useSelector(
-    state => state.currentUserInfo && state.currentUserInfo.id,
-  );
   const isBackupEnabled = useSelector(
     state => state.localSettings.isBackupEnabled,
   );
@@ -29,62 +19,27 @@ function BackupHandler(): null {
     accountHasPassword(state.currentUserInfo),
   );
 
-  const { uploadBackupProtocol } = useClientBackup();
-
   React.useEffect(() => {
-    if (!isBackupEnabled) {
-      AsyncStorage.removeItem(BACKUP_HASH_STORAGE_KEY);
+    if (!staffCanSee || !isAccountWithPassword) {
+      return;
     }
-  }, [isBackupEnabled]);
 
-  React.useEffect(() => {
-    void (async () => {
-      if (
-        !isBackupEnabled ||
-        !loggedIn ||
-        !staffCanSee ||
-        !isAccountWithPassword
-      ) {
-        return;
+    if (isBackupEnabled && loggedIn) {
+      try {
+        commCoreModule.startBackupHandler();
+      } catch (err) {
+        console.log('Error starting backup handler:', err);
       }
-
-      const userData = { userStore };
-      const userDataBytes = convertObjToBytes(userData);
-      const currentBackupHash = commUtilsModule.sha256(userDataBytes.buffer);
-
-      const mostRecentlyUploadedBackupHash = await AsyncStorage.getItem(
-        BACKUP_HASH_STORAGE_KEY,
-      );
-
-      if (
-        !mostRecentlyUploadedBackupHash ||
-        currentBackupHash !== mostRecentlyUploadedBackupHash
-      ) {
+    } else {
+      void (async () => {
         try {
-          await uploadBackupProtocol(userData);
-          await AsyncStorage.setItem(
-            BACKUP_HASH_STORAGE_KEY,
-            currentBackupHash,
-          );
-        } catch (e) {
-          const message = String(getMessageForException(e));
-          console.error(`Backup uploading error: ${message}`);
-          Alert.alert(
-            'Backup protocol info',
-            `Backup uploading error: ${message}`,
-          );
+          await commCoreModule.stopBackupHandler();
+        } catch (err) {
+          console.log('Error stopping backup handler:', err);
         }
-      }
-    })();
-  }, [
-    currentUserID,
-    isBackupEnabled,
-    staffCanSee,
-    loggedIn,
-    uploadBackupProtocol,
-    userStore,
-    isAccountWithPassword,
-  ]);
+      })();
+    }
+  }, [isBackupEnabled, staffCanSee, loggedIn, isAccountWithPassword]);
 
   return null;
 }
