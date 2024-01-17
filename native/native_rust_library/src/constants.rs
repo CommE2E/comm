0f4e5cd0b4b17a66c441d137f0376e461b3b78b7
Michal Gniadek <michal.gniadek@swmansion.com>
2024-01-17 17:40:37 +0100

    [native] Create and upload backup compaction
    Summary:
    Update the JS and Rust code to upload sqlite backup compaction. First Rust saves the encrypted user keys data to a file and schedules a C++ code the to also save the database compaction. After both of these are done it uploads them to the backup service. If all operations succeed the JS promise is resolved otherwise it's rejected.
    
    Depends on D10858
    
    Test Plan: Click the test button in JS, make sure the backup is uploaded to the backup service and files are removed. [with later diffs] tested that the backup can be restored. Tested that errors are propageated to the JS promise.
    
    Reviewers: marcin, bartek, kamil
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10859

--- native/native_rust_library/src/constants.rs
+++ native/native_rust_library/src/constants.rs
@@ -1,16 +1,19 @@
 use std::time::Duration;
 
+#[allow(unused)]
 pub mod aes {
   pub const KEY_SIZE: usize = 32; // bytes
   pub const IV_LENGTH: usize = 12; // bytes - unique Initialization Vector (nonce)
   pub const TAG_LENGTH: usize = 16; // bytes - GCM auth tag
 }
 
-/// Should match constants defined in `CommSecureStore.h`
 pub mod secure_store {
+  /// Should match constants defined in `CommSecureStore.h`
   pub const COMM_SERVICES_ACCESS_TOKEN: &str = "accessToken";
   pub const USER_ID: &str = "userID";
   pub const DEVICE_ID: &str = "deviceID";
+  /// Should match constant defined in `SQLiteQueryExecutor.h`
+  pub const SECURE_STORE_ENCRYPTION_KEY_ID: &str = "comm.encryptionKey";
 }
 
 pub const BACKUP_SERVICE_CONNECTION_RETRY_DELAY: Duration =
--- native/native_rust_library/src/lib.rs
+++ native/native_rust_library/src/lib.rs
@@ -250,12 +250,11 @@ mod ffi {
     fn trigger_backup_file_upload();
 
     #[cxx_name = "createBackup"]
-    fn create_backup_sync(
+    fn create_backup(
       backup_id: String,
       backup_secret: String,
       pickle_key: String,
       pickled_account: String,
-      user_data: String,
       promise_id: u32,
     );
 
@@ -281,18 +280,15 @@ mod ffi {
   unsafe extern "C++" {
     include!("RustBackupExecutor.h");
 
-    #[allow(unused)]
     #[cxx_name = "getBackupDirectoryPath"]
     fn get_backup_directory_path() -> Result<String>;
 
-    #[allow(unused)]
     #[cxx_name = "getBackupFilePath"]
     fn get_backup_file_path(
       backup_id: &str,
       is_attachments: bool,
     ) -> Result<String>;
 
-    #[allow(unused)]
     #[cxx_name = "getBackupLogFilePath"]
     fn get_backup_log_file_path(
       backup_id: &str,
@@ -300,7 +296,6 @@ mod ffi {
       is_attachments: bool,
     ) -> Result<String>;
 
-    #[allow(unused)]
     #[cxx_name = "getBackupUserKeysFilePath"]
     fn get_backup_user_keys_file_path(backup_id: &str) -> Result<String>;
 
