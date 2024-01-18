fb7fe6bbf148e0ee41b58405859d2b0492ab6b02
marcinwasowicz <marcinkwasowicz@gmail.com>
2024-01-18 18:06:10 +0100

    Prepare tests to cover missing SQLiteQueryExecutor queries
    Summary:
    IGNORE: this differential is not expected to be landed. I created it in order to provide unit tests
    that can serve as a test plan for previous differentials in this stack since most of them break the app.
    
    Test Plan: Execute implemented tests.
    
    Reviewers: kamil, michal
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10711

---
+++ web/database/queries/olm-persist-data-queries.test.js
@@ -0,0 +1,47 @@
+// @flow
+
+import { getDatabaseModule } from '../db-module.js';
+import { clearSensitiveData } from '../utils/db-utils.js';
+
+const FILE_PATH = 'test.sqlite';
+
+describe('Draft Store queries', () => {
+  let queryExecutor;
+  let dbModule;
+
+  beforeAll(async () => {
+    dbModule = getDatabaseModule();
+  });
+
+  beforeEach(() => {
+    queryExecutor = new dbModule.SQLiteQueryExecutor(FILE_PATH);
+    queryExecutor.storeOlmPersistAccount('accountData');
+
+    queryExecutor.storeOlmPersistSession({
+      targetUserID: '1',
+      sessionData: '1',
+    });
+    queryExecutor.storeOlmPersistSession({
+      targetUserID: '2',
+      sessionData: '2',
+    });
+    queryExecutor.storeOlmPersistSession({
+      targetUserID: '3',
+      sessionData: '3',
+    });
+  });
+
+  afterEach(() => {
+    clearSensitiveData(dbModule, FILE_PATH, queryExecutor);
+  });
+
+  it('should return olm account data', () => {
+    const olmAccount = queryExecutor.getOlmPersistAccountDataWeb();
+    expect(olmAccount.value).toBe('accountData');
+  });
+
+  it('should return all olm sessions', () => {
+    const olmSessions = queryExecutor.getOlmPersistSessionsData();
+    expect(olmSessions.length).toBe(3);
+  });
+});
