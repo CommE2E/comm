f4947c6e40713986de50e7fe9fadc44a06f287fb
marcinwasowicz <marcinkwasowicz@gmail.com>
2024-01-18 14:33:11 +0100

    Refactor all 'getAll...'-like queries not to use ORM. Differential Revision: https://phab.comm.dev/D10704

--- native/cpp/CommonCpp/DatabaseManagers/entities/Message.h
+++ native/cpp/CommonCpp/DatabaseManagers/entities/Message.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <sqlite3.h>
 #include <memory>
 #include <string>
 
@@ -14,6 +15,30 @@ struct Message {
   std::unique_ptr<int> future_type;
   std::unique_ptr<std::string> content;
   int64_t time;
+
+  static Message fromSQLResult(sqlite3_stmt *sqlRow, int idx) {
+    const char *local_id =
+        reinterpret_cast<const char *>(sqlite3_column_text(sqlRow, idx + 1));
+    const char *content =
+        reinterpret_cast<const char *>(sqlite3_column_text(sqlRow, idx + 6));
+
+    std::unique_ptr<int> future_type;
+    if (sqlite3_column_type(sqlRow, idx + 5) == SQLITE_NULL) {
+      future_type = nullptr;
+    } else {
+      future_type = std::make_unique<int>(sqlite3_column_int(sqlRow, idx + 5));
+    }
+
+    return Message{
+        reinterpret_cast<const char *>(sqlite3_column_text(sqlRow, idx)),
+        local_id ? std::make_unique<std::string>(local_id) : nullptr,
+        reinterpret_cast<const char *>(sqlite3_column_text(sqlRow, idx + 2)),
+        reinterpret_cast<const char *>(sqlite3_column_text(sqlRow, idx + 3)),
+        sqlite3_column_int(sqlRow, idx + 4),
+        std::move(future_type),
+        content ? std::make_unique<std::string>(content) : nullptr,
+        sqlite3_column_int64(sqlRow, idx + 7)};
+  }
 };
 
 } // namespace comm
--- native/cpp/CommonCpp/DatabaseManagers/entities/OlmPersistAccount.h
+++ native/cpp/CommonCpp/DatabaseManagers/entities/OlmPersistAccount.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <sqlite3.h>
 #include <string>
 
 namespace comm {
@@ -7,6 +8,12 @@ namespace comm {
 struct OlmPersistAccount {
   int id;
   std::string account_data;
+
+  static OlmPersistAccount fromSQLResult(sqlite3_stmt *sqlRow, int idx) {
+    return OlmPersistAccount{
+        sqlite3_column_int(sqlRow, idx),
+        reinterpret_cast<const char *>(sqlite3_column_text(sqlRow, idx + 1))};
+  }
 };
 
 } // namespace comm
