601cdee978c0a622a60ec19a12416a4c279543bf
marcinwasowicz <marcinkwasowicz@gmail.com>
2024-01-18 14:33:11 +0100

    Refactor all 'getAll...'-like queries not to use ORM.
    Summary: edit in web
    
    Test Plan: edit in web
    
    Reviewers: michal, kamil
    
    Subscribers:

--- native/cpp/CommonCpp/DatabaseManagers/entities/MessageStoreThread.h
+++ native/cpp/CommonCpp/DatabaseManagers/entities/MessageStoreThread.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <sqlite3.h>
 #include <string>
 
 namespace comm {
@@ -7,6 +8,13 @@ namespace comm {
 struct MessageStoreThread {
   std::string id;
   int start_reached;
+
+  static MessageStoreThread fromSQLResult(sqlite3_stmt *sqlRow, int idx) {
+    return MessageStoreThread{
+        reinterpret_cast<const char *>(sqlite3_column_text(sqlRow, idx)),
+        sqlite3_column_int(sqlRow, idx + 1),
+    };
+  }
 };
 
 } // namespace comm
