cd52c5154ab6a526bff4ec10d14c9d60419daecd
marcinwasowicz <marcinkwasowicz@gmail.com>
2024-01-18 15:20:13 +0100

    Refactor all replace and update queries not to user ORM.
    Summary: edit in web
    
    Test Plan: edit in web
    
    Reviewers: kamil, michal
    
    Subscribers:

--- native/cpp/CommonCpp/DatabaseManagers/entities/OlmPersistAccount.h
+++ native/cpp/CommonCpp/DatabaseManagers/entities/OlmPersistAccount.h
@@ -14,6 +14,12 @@ struct OlmPersistAccount {
         sqlite3_column_int(sqlRow, idx),
         reinterpret_cast<const char *>(sqlite3_column_text(sqlRow, idx + 1))};
   }
+
+  int bindToSQL(sqlite3_stmt *sql, int idx) const {
+    sqlite3_bind_int(sql, idx, id);
+    return sqlite3_bind_text(
+        sql, idx + 1, account_data.c_str(), -1, SQLITE_STATIC);
+  }
 };
 
 } // namespace comm
--- native/cpp/CommonCpp/DatabaseManagers/entities/UserInfo.h
+++ native/cpp/CommonCpp/DatabaseManagers/entities/UserInfo.h
@@ -13,7 +13,13 @@ struct UserInfo {
     return UserInfo{
         reinterpret_cast<const char *>(sqlite3_column_text(sqlRow, idx)),
         reinterpret_cast<const char *>(sqlite3_column_text(sqlRow, idx + 1))};
-  };
+  }
+
+  int bindToSQL(sqlite3_stmt *sql, int idx) const {
+    sqlite3_bind_text(sql, idx, id.c_str(), -1, SQLITE_STATIC);
+    return sqlite3_bind_text(
+        sql, idx + 1, user_info.c_str(), -1, SQLITE_STATIC);
+  }
 };
 
 } // namespace comm
