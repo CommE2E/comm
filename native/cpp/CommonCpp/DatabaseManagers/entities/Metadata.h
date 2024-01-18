163dd89b82b0d1d11eed9ab740ea36ee9962f555
marcinwasowicz <marcinkwasowicz@gmail.com>
2024-01-18 14:47:23 +0100

    Refactor all 'get entity by primary key'-like queries not to use ORM Differential Revision: https://phab.comm.dev/D10705

--- native/cpp/CommonCpp/DatabaseManagers/entities/Metadata.h
+++ native/cpp/CommonCpp/DatabaseManagers/entities/Metadata.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "SQLiteDataConverters.h"
+#include <sqlite3.h>
 #include <string>
 
 namespace comm {
@@ -7,6 +9,11 @@ namespace comm {
 struct Metadata {
   std::string name;
   std::string data;
+
+  static Metadata fromSQLResult(sqlite3_stmt *sqlRow, int idx) {
+    return Metadata{
+        getStringFromSQLRow(sqlRow, idx), getStringFromSQLRow(sqlRow, idx + 1)};
+  }
 };
 
 } // namespace comm
