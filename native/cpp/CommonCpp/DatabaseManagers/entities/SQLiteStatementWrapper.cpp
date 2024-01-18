629825b6112ba57367e94290659842f8700964ec
marcinwasowicz <marcinkwasowicz@gmail.com>
2024-01-18 14:33:11 +0100

    Refactor all 'getAll...'-like queries not to use ORM. Differential Revision: https://phab.comm.dev/D10704

---
+++ native/cpp/CommonCpp/DatabaseManagers/entities/SQLiteStatementWrapper.cpp
@@ -0,0 +1,38 @@
+#include "SQLiteStatementWrapper.h"
+
+#include <sstream>
+#include <stdexcept>
+#include <system_error>
+
+namespace comm {
+SQLiteStatementWrapper::SQLiteStatementWrapper(
+    sqlite3 *db,
+    std::string sql,
+    std::string onLastStepFailureMessage) {
+  int prepareSQLResult =
+      sqlite3_prepare_v2(db, sql.c_str(), -1, &preparedSQLPtr, nullptr);
+
+  if (prepareSQLResult != SQLITE_OK) {
+    std::stringstream error_message;
+    error_message << "Failed to prepare SQL statement. Details: "
+                  << sqlite3_errstr(prepareSQLResult) << std::endl;
+    throw std::runtime_error(error_message.str());
+  }
+  onLastStepFailureMessage = onLastStepFailureMessage;
+}
+
+SQLiteStatementWrapper::~SQLiteStatementWrapper() {
+  int lastStepResult = sqlite3_finalize(preparedSQLPtr);
+  if (lastStepResult != SQLITE_OK) {
+    std::stringstream error_message;
+    error_message << onLastStepFailureMessage
+                  << " Details: " << sqlite3_errstr(lastStepResult)
+                  << std::endl;
+    throw std::runtime_error(error_message.str());
+  }
+}
+
+SQLiteStatementWrapper::operator sqlite3_stmt *() {
+  return preparedSQLPtr;
+}
+} // namespace comm
