b611d008ad3154dab311e6005ea5d08f9a4fcf2f
marcinwasowicz <marcinkwasowicz@gmail.com>
2024-01-18 14:47:23 +0100

    Refactor all 'get entity by primary key'-like queries not to use ORM Differential Revision: https://phab.comm.dev/D10705

--- native/cpp/CommonCpp/DatabaseManagers/entities/PersistItem.h
+++ native/cpp/CommonCpp/DatabaseManagers/entities/PersistItem.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <sqlite3.h>
 #include <string>
 
 namespace comm {
@@ -7,6 +8,12 @@ namespace comm {
 struct PersistItem {
   std::string key;
   std::string item;
+
+  static PersistItem fromSQLResult(sqlite3_stmt *sqlRow, int idx) {
+    return PersistItem{
+        reinterpret_cast<const char *>(sqlite3_column_text(sqlRow, idx)),
+        reinterpret_cast<const char *>(sqlite3_column_text(sqlRow, idx + 1))};
+  }
 };
 
 } // namespace comm
