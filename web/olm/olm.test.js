bc9dbc7e3a6e9f035b44bd34060c6deeca6ac934
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2024-01-20 00:05:43 -0500

    [web] add test for one-time keys
    Summary: before calling the olm account methods related to managing one-time keys, i wanted to make sure the library worked as expected with a simple unit test.
    
    Test Plan: test passes
    
    Reviewers: ashoat!
    
    Subscribers:

--- web/olm/olm.test.js
+++ web/olm/olm.test.js
@@ -15,4 +15,14 @@ describe('olm.Account', () => {
     account.generate_prekey();
     expect(account.prekey()).toBeDefined();
   });
+  it('should be able to generate and return one-time keys', async () => {
+    await olm.init();
+    const account = new olm.Account();
+    account.create();
+    account.generate_one_time_keys(5);
+    const oneTimeKeysObject = JSON.parse(account.one_time_keys());
+    expect(oneTimeKeysObject).toBeDefined();
+    const oneTimeKeys = oneTimeKeysObject.curve25519;
+    expect(Object.keys(oneTimeKeys).length).toBe(5);
+  });
 });
