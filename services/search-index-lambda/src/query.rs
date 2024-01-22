6eed41f61e40bda8f1be1f2f8369fd63ab7891fd
William Wang <williamwang749@gmail.com>
2024-01-22 13:04:41 -0500

    [services] Reorganize lambda function structs into query.rs and payload.rs
    Summary:
    This moves the structs for querying and deserializing dynamodb stream records.
    
    Test Plan:
    Successfully compiled with `cargo lambda`
    
    Reviewers:
    varun,
    bartek
    
    Subscribers:

---
+++ services/search-index-lambda/src/query.rs
@@ -0,0 +1,36 @@
+use serde::Serialize;
+
+#[derive(Serialize)]
+pub struct UpdateByQuery {
+  pub query: Query,
+
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub script: Option<Script>,
+}
+
+#[derive(Serialize)]
+pub struct Script {
+  pub source: String,
+  pub lang: String,
+}
+
+#[derive(Serialize)]
+pub struct Query {
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub r#match: Option<Match>,
+
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub term: Option<Term>,
+}
+
+#[derive(Serialize)]
+pub struct Match {
+  #[serde(rename = "userID")]
+  pub user_id: String,
+}
+
+#[derive(Serialize)]
+pub struct Term {
+  #[serde(rename = "userID")]
+  pub user_id: String,
+}
