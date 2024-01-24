38defdd5a246479bb6bbacd7b94dd1ebc9f276d2
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2024-01-24 10:29:51 +0100

    [identity] Scaffold RPC for device list updates
    Summary:
    I need only basic part of it for [[ https://linear.app/comm/issue/ENG-5425/primary-device-sends-the-identity-service-the-updated-device-list | ENG-5425 ]].
    Proper implementation is tracked by [[ https://linear.app/comm/issue/ENG-5101/new-rpc-to-update-devicelist | ENG-5101 ]].
    
    Test Plan: Compiled Identity Service. Implementation and integration tests are done in subsequent diffs.
    
    Reviewers: varun, will, ashoat, michal
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D10797

--- web/protobufs/identity-auth-client.cjs
+++ web/protobufs/identity-auth-client.cjs
@@ -749,5 +749,66 @@ proto.identity.auth.IdentityClientServicePromiseClient.prototype.getDeviceListFo
 };
 
 
+/**
+ * @const
+ * @type {!grpc.web.MethodDescriptor<
+ *   !proto.identity.auth.UpdateDeviceListRequest,
+ *   !proto.identity.unauth.Empty>}
+ */
+const methodDescriptor_IdentityClientService_UpdateDeviceListForUser = new grpc.web.MethodDescriptor(
+  '/identity.auth.IdentityClientService/UpdateDeviceListForUser',
+  grpc.web.MethodType.UNARY,
+  proto.identity.auth.UpdateDeviceListRequest,
+  identity_unauth_pb.Empty,
+  /**
+   * @param {!proto.identity.auth.UpdateDeviceListRequest} request
+   * @return {!Uint8Array}
+   */
+  function(request) {
+    return request.serializeBinary();
+  },
+  identity_unauth_pb.Empty.deserializeBinary
+);
+
+
+/**
+ * @param {!proto.identity.auth.UpdateDeviceListRequest} request The
+ *     request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @param {function(?grpc.web.RpcError, ?proto.identity.unauth.Empty)}
+ *     callback The callback function(error, response)
+ * @return {!grpc.web.ClientReadableStream<!proto.identity.unauth.Empty>|undefined}
+ *     The XHR Node Readable Stream
+ */
+proto.identity.auth.IdentityClientServiceClient.prototype.updateDeviceListForUser =
+    function(request, metadata, callback) {
+  return this.client_.rpcCall(this.hostname_ +
+      '/identity.auth.IdentityClientService/UpdateDeviceListForUser',
+      request,
+      metadata || {},
+      methodDescriptor_IdentityClientService_UpdateDeviceListForUser,
+      callback);
+};
+
+
+/**
+ * @param {!proto.identity.auth.UpdateDeviceListRequest} request The
+ *     request proto
+ * @param {?Object<string, string>=} metadata User defined
+ *     call metadata
+ * @return {!Promise<!proto.identity.unauth.Empty>}
+ *     Promise that resolves to the response
+ */
+proto.identity.auth.IdentityClientServicePromiseClient.prototype.updateDeviceListForUser =
+    function(request, metadata) {
+  return this.client_.unaryCall(this.hostname_ +
+      '/identity.auth.IdentityClientService/UpdateDeviceListForUser',
+      request,
+      metadata || {},
+      methodDescriptor_IdentityClientService_UpdateDeviceListForUser);
+};
+
+
 module.exports = proto.identity.auth;
 
