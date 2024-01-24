7a8574c25ad613b6b935a53a354d3abcd2a2e2c7
William Wang <williamwang749@gmail.com>
2024-01-24 16:21:56 -0500

    Add identity_search_messages to shared and use in identity
    Summary:
    This introduces identity_search_messages into shared for usage by identity's prefix search websocket services and commtest (in later diffs).
    
    Depends on D10802
    
    Test Plan: cargo fmt and build. Successfully running identity service
    
    Reviewers: varun, bartek
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10804

---
+++ shared/identity_search_messages/src/messages/mod.rs
@@ -0,0 +1,18 @@
+//! Messages sent from client to Identity Search Server
+
+pub mod auth_messages;
+
+pub use auth_messages::*;
+
+use serde::{Deserialize, Serialize};
+pub use websocket_messages::{
+  ConnectionInitializationResponse, ConnectionInitializationStatus, Heartbeat,
+};
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum Messages {
+  AuthMessage(AuthMessage),
+  Heartbeat(Heartbeat),
+  ConnectionInitializationResponse(ConnectionInitializationResponse),
+}
