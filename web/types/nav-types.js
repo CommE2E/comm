77b2ea59dc16581a508357dd570486ae6c3b21ed
Atul Madhugiri <atulm@berkeley.edu>
2024-01-25 00:32:46 -0500

    Get rid of `minimallyEncoded*` prefix for validators
    Summary:
    As of D10805, we no longer have any `MinimallyEncoded*` types. In some of the diffs that removed the prefix from the types I also removed the prefix from the corresponding validator name.. in some cases I didn't. This diff removes the prefix from validators where it remains.
    
    ---
    
    Depends on D10805
    
    Test Plan: `flow`
    
    Reviewers: ashoat, ginsu, tomek
    
    Differential Revision: https://phab.comm.dev/D10806

--- web/types/nav-types.js
+++ web/types/nav-types.js
@@ -3,7 +3,7 @@
 import type { TInterface } from 'tcomb';
 import t from 'tcomb';
 
-import { minimallyEncodedThreadInfoValidator } from 'lib/permissions/minimally-encoded-thread-permissions-validators.js';
+import { threadInfoValidator } from 'lib/permissions/minimally-encoded-thread-permissions-validators.js';
 import type { ThreadInfo } from 'lib/types/minimally-encoded-thread-permissions-types.js';
 import { type BaseNavInfo } from 'lib/types/nav-types.js';
 import {
@@ -51,7 +51,7 @@ export const navInfoValidator: TInterface<NavInfo> = tShape<$Exact<NavInfo>>({
   endDate: t.String,
   tab: navigationTabValidator,
   activeChatThreadID: t.maybe(tID),
-  pendingThread: t.maybe(minimallyEncodedThreadInfoValidator),
+  pendingThread: t.maybe(threadInfoValidator),
   settingsSection: t.maybe(navigationSettingsSectionValidator),
   selectedUserList: t.maybe(t.list(accountUserInfoValidator)),
   chatMode: t.maybe(navigationChatModeValidator),
