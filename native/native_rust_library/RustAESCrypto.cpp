899245016479c70eb6f294e16bc3a1680b377996
marcinwasowicz <marcinkwasowicz@gmail.com>
2024-01-26 11:51:42 +0100

    Implement log encryption using native AES utilities
    Summary:
    This differential:
    1. Refactors our native AES crypto API so that it can work both with `std::vector<std::uint8_t>&` and `rust::Slice<std::uint8_t>`.
    2. Uses AES crypto API to encrypt log files.
    3. Introduces additional encryption key to encrypt log files. This key lifecycle is tightly coupled with encryption key lifecycle used to encrypt latest compaction.
    
    Test Plan: Same as for parent differential but this time ensure that logs don't have any human-readable content (not attachments - they remain unencrypted)
    
    Reviewers: michal, kamil
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10838

--- native/native_rust_library/RustAESCrypto.cpp
+++ native/native_rust_library/RustAESCrypto.cpp
@@ -4,21 +4,21 @@
 namespace comm {
 
 void aesGenerateKey(rust::Slice<uint8_t> buffer) {
-  AESCrypto::generateKey(buffer);
+  AESCrypto<rust::Slice<uint8_t>>::generateKey(buffer);
 }
 
 void aesEncrypt(
     rust::Slice<uint8_t> key,
     rust::Slice<uint8_t> plaintext,
     rust::Slice<uint8_t> sealedData) {
-  AESCrypto::encrypt(key, plaintext, sealedData);
+  AESCrypto<rust::Slice<uint8_t>>::encrypt(key, plaintext, sealedData);
 }
 
 void aesDecrypt(
     rust::Slice<uint8_t> key,
     rust::Slice<uint8_t> sealedData,
     rust::Slice<uint8_t> plaintext) {
-  AESCrypto::decrypt(key, sealedData, plaintext);
+  AESCrypto<rust::Slice<uint8_t>>::decrypt(key, sealedData, plaintext);
 }
 
 } // namespace comm
