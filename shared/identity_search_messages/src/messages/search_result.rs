4fe66c00f5cfc350b94ef322608f0da5892922ff
William Wang <williamwang749@gmail.com>
2024-01-26 17:35:31 -0500

    [identity] [5/n] Create search result message type for identity search
    Summary:
    This creates a search result type in shared and uses it in the identity search websocket server
    
    Depends on 10848
    
    Test Plan: Successfully tested on local dev. Prefix queries return correct response
    
    Reviewers: varun, bartek
    
    Reviewed By: bartek
    
    Subscribers: kamil, ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10851

---
+++ shared/identity_search_messages/src/messages/search_result.rs
@@ -0,0 +1,16 @@
+//! Search Result Messages sent by Identity Search via WebSocket.
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct User {
+  #[serde(rename = "userID")]
+  pub user_id: String,
+  pub username: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(tag = "type")]
+pub struct SearchResult {
+  pub hits: Vec<User>,
+}
