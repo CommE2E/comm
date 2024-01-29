b5e2e6cb8ae2378b9b158eeffe18d02d8bbcc5be
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2024-01-29 13:08:42 +0100

    [identity] Implement RPC for secondary device login
    Summary:
    Implementation of RPC from D10861.
    - Created function to put device data into device list (device key upload)
    - Called this function along with access token creation from the RPC handler
    - Performed basic device list checks - the RPC fails if the device is not on the device list.
    
    Depends on D10861
    
    Test Plan:
    Tested both scenarios: device on device list and device not on device list.
    - If the device is not on the list, fails with `permission denied` status
    - When the device is on the list, the RPC succeeds and returns the access token. Used `awslocal dynamodb scan` to make sure device keys are put in the devices table under `userID|device-{deviceID}` primary key.
    
    Reviewers: varun, michal
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10862

--- services/identity/src/client_service.rs
+++ services/identity/src/client_service.rs
@@ -583,7 +583,69 @@ impl IdentityClientService for ClientService {
     &self,
     request: tonic::Request<SecondaryDeviceLoginRequest>,
   ) -> Result<tonic::Response<SecondaryDeviceLoginResponse>, tonic::Status> {
-    Err(tonic::Status::unimplemented("unimplemented"))
+    let code_version = get_code_version(&request);
+    let message = request.into_inner();
+
+    let flattened_device_key_upload =
+      construct_flattened_device_key_upload(&message)?;
+    let user_id = message.user_id;
+    let device_id = flattened_device_key_upload.device_id_key.clone();
+
+    let Some(device_list) = self
+      .client
+      .get_current_device_list(&user_id)
+      .await
+      .map_err(handle_db_error)?
+    else {
+      warn!("User {} does not have valid device list. Secondary device auth impossible.", user_id);
+      return Err(tonic::Status::aborted("device list error"));
+    };
+
+    if !device_list.device_ids.contains(&device_id) {
+      return Err(tonic::Status::permission_denied(
+        "device not in device list",
+      ));
+    }
+
+    let login_time = chrono::Utc::now();
+
+    // generate access token
+    let user_identifier = self
+      .client
+      .get_user_identifier(&user_id)
+      .await
+      .map_err(handle_db_error)?;
+    let token = AccessTokenData::with_created_time(
+      user_id.clone(),
+      device_id,
+      login_time,
+      user_identifier.into(),
+      &mut OsRng,
+    );
+    let access_token = token.access_token.clone();
+    self
+      .client
+      .put_access_token_data(token)
+      .await
+      .map_err(handle_db_error)?;
+
+    // put device data (device key upload)
+    self
+      .client
+      .put_device_data(
+        &user_id,
+        flattened_device_key_upload,
+        code_version,
+        login_time,
+      )
+      .await
+      .map_err(handle_db_error)?;
+
+    let response = SecondaryDeviceLoginResponse {
+      user_id,
+      access_token,
+    };
+    Ok(Response::new(response))
   }
 
   async fn generate_nonce(
