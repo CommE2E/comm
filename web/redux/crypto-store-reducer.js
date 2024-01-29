5a2c819a3c3b703d5f5151c3c15fb47ddc9d6279
Inka Sokołowska <inka.d.sokolowska@gmail.com>
2024-01-29 15:01:11 +0100

    [web] Remove logout / delete account / identity login logic from specific reducers
    Summary:
    issue: [[https://linear.app/comm/issue/ENG-6424/delete-state-on-identity-login-logout-delete-account | ENG-6424 ]]
    From web specific fields, we only remove in `resetUserSpecificStateOnIdentityActions` `cryptoStore` and `communityPickerStore`. `communityPickerStore` wan't being cleared on any of those actions before, but it should have been. But that means I only need to update `reduceCryptoStore`
    
    Test Plan: Tested that `cryptoStore` is still cleared on logout
    
    Reviewers: michal, kamil, ginsu
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10869

--- web/redux/crypto-store-reducer.js
+++ web/redux/crypto-store-reducer.js
@@ -1,9 +1,5 @@
 // @flow
 
-import {
-  logOutActionTypes,
-  deleteAccountActionTypes,
-} from 'lib/actions/user-actions.js';
 import { setNewSessionActionType } from 'lib/keyserver-conn/keyserver-conn-types.js';
 import type { CryptoStore } from 'lib/types/crypto-types.js';
 import { usingCommServicesAccessToken } from 'lib/utils/services-utils.js';
@@ -15,11 +11,6 @@ const setCryptoStore = 'SET_CRYPTO_STORE';
 function reduceCryptoStore(state: ?CryptoStore, action: Action): ?CryptoStore {
   if (action.type === setCryptoStore) {
     return action.payload;
-  } else if (
-    action.type === logOutActionTypes.success ||
-    action.type === deleteAccountActionTypes.success
-  ) {
-    return null;
   } else if (
     action.type === setNewSessionActionType &&
     action.payload.sessionChange.cookieInvalidated &&
