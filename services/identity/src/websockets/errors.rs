f5ae0f430dc6fce474e71c0ae66ade532bd44649
William Wang <williamwang749@gmail.com>
2024-01-29 14:46:02 -0500

    [identity] [6/n] Requires a Search Query json message from client for user identity search
    Summary:
    This requires the client to send a Search Query json object when using identity search. This makes it clear the user search type and query (i.e. prefix vs exact).
    
    As an example, after authenticating with the websocket server, to prefix search, you would send:
    ```
    {"prefix": "..."}
    ```
    
    Depends on D10851
    
    Test Plan: tested locally after authenticating.
    
    Reviewers: varun, bartek
    
    Reviewed By: bartek
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10873

--- services/identity/src/websockets/errors.rs
+++ services/identity/src/websockets/errors.rs
@@ -1,3 +1,5 @@
+use tracing::error;
+
 pub type BoxedError = Box<dyn std::error::Error + Send + Sync + 'static>;
 
 #[derive(
@@ -5,9 +7,24 @@ pub type BoxedError = Box<dyn std::error::Error + Send + Sync + 'static>;
 )]
 pub enum WebsocketError {
   InvalidMessage,
+  InvalidSearchQuery,
   UnauthorizedDevice,
   SendError,
   SearchError,
   AuthError,
   SerializationError,
+}
+
+impl From<serde_json::Error> for WebsocketError {
+  fn from(err: serde_json::Error) -> Self {
+    tracing::error!("Error serializing: {}", err);
+    WebsocketError::SerializationError
+  }
+}
+
+impl From<reqwest::Error> for WebsocketError {
+  fn from(err: reqwest::Error) -> Self {
+    tracing::error!("Error with search request: {}", err);
+    WebsocketError::SearchError
+  }
 }
