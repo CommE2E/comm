22572c7a040fe1caec8ff41a37547c350a7e96bb
Atul Madhugiri <atulm@berkeley.edu>
2024-01-31 14:04:52 -0500

    [lib] Move `WebInitialReduxStateResponse` to `lib`
    Summary:
    Move `WebInitialReduxStateResponse` (and `WebInitialKeyserverInfo`) to `lib`.
    
    We will then:
    1. Split into `Client` and `Server` variants
    2. Make the client one take `ThreadStore` and server can take `LegacyThreadStore`
    
    ---
    
    Depends on D10894
    
    Test Plan: CI + `flow` + close reading of changes. Used IDE `Refactor -> Move` tool to make this change.
    
    Reviewers: ashoat, ginsu, tomek
    
    Reviewed By: ginsu
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10895

--- web/types/redux-types.js
+++ web/types/redux-types.js
@@ -4,22 +4,10 @@ import type { EntryStore, CalendarQuery } from 'lib/types/entry-types.js';
 import type { InviteLinksStore } from 'lib/types/link-types.js';
 import type { MessageStore } from 'lib/types/message-types.js';
 import type { WebNavInfo } from 'lib/types/nav-types.js';
-import type { LegacyThreadStore, ThreadStore } from 'lib/types/thread-types.js';
+import type { WebInitialKeyserverInfo } from 'lib/types/redux-types.js';
+import type { ThreadStore } from 'lib/types/thread-types.js';
 import type { CurrentUserInfo, UserInfos } from 'lib/types/user-types.js';
 import type { URLInfo } from 'lib/utils/url-utils.js';
-
-export type WebInitialReduxStateResponse = {
-  +navInfo: WebNavInfo,
-  +currentUserInfo: CurrentUserInfo,
-  +entryStore: EntryStore,
-  +threadStore: LegacyThreadStore,
-  +userInfos: UserInfos,
-  +messageStore: MessageStore,
-  +pushApiPublicKey: ?string,
-  +commServicesAccessToken: null,
-  +inviteLinksStore: InviteLinksStore,
-  +keyserverInfo: WebInitialKeyserverInfo,
-};
 
 export type InitialReduxState = {
   +navInfo: WebNavInfo,
@@ -34,11 +22,6 @@ export type InitialReduxState = {
   +dataLoaded: boolean,
   +actualizedCalendarQuery: CalendarQuery,
   +keyserverInfos: { +[keyserverID: string]: WebInitialKeyserverInfo },
-};
-
-export type WebInitialKeyserverInfo = {
-  +sessionID: ?string,
-  +updatesCurrentAsOf: number,
 };
 
 export type ExcludedData = {
