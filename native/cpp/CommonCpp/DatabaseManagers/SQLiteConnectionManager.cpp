770f827e93e1df433dec0632972c8d1e1812199f
marcinwasowicz <marcinkwasowicz@gmail.com>
2024-01-31 23:38:56 +0100

    Implement restore from backup logs
    Summary: This differential implements restore from backup log function on web and native
    
    Test Plan: Work in progress
    
    Reviewers: kamil, michal
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10905

--- native/cpp/CommonCpp/DatabaseManagers/SQLiteConnectionManager.cpp
+++ native/cpp/CommonCpp/DatabaseManagers/SQLiteConnectionManager.cpp
@@ -1,5 +1,6 @@
 #include "SQLiteConnectionManager.h"
 
+#include "Logger.h"
 #include <sstream>
 #include <stdexcept>
 #include <string>
@@ -53,5 +54,59 @@ void SQLiteConnectionManager::closeConnection() {
 
 SQLiteConnectionManager::~SQLiteConnectionManager() {
   closeConnectionInternal();
+}
+
+void SQLiteConnectionManager::restoreFromBackupLog(
+    const std::vector<std::uint8_t> &backupLog) {
+  if (!dbConnection) {
+    throw std::runtime_error(
+        "Programmer error: attempt to restore from backup log but database "
+        "connection is not initialized.");
+  }
+
+  static auto backupLogRestoreConflictHandler =
+      [](void *, int conflictReason, sqlite3_changeset_iter *changesetIter) {
+        const char *tableName;
+        int columnsNumber;
+        int operationType;
+
+        int getOperationResult = sqlite3changeset_op(
+            changesetIter, &tableName, &columnsNumber, &operationType, 0);
+
+        if (getOperationResult != SQLITE_OK) {
+          Logger::log(
+              "Failed to extract operation from log iterator. Details: " +
+              std::string(sqlite3_errstr(getOperationResult)));
+          return SQLITE_CHANGESET_OMIT;
+        }
+
+        std::stringstream conflictMessage;
+        conflictMessage << "Conflict of type " << conflictReason
+                        << " occurred for operation of type " << operationType
+                        << " for table " << tableName
+                        << " during backup log application";
+        Logger::log(conflictMessage.str());
+
+        if (operationType == SQLITE_INSERT &&
+            conflictReason == SQLITE_CHANGESET_CONFLICT) {
+          return SQLITE_CHANGESET_REPLACE;
+        }
+
+        return SQLITE_CHANGESET_OMIT;
+      };
+
+  int applyChangesetResult = sqlite3changeset_apply(
+      dbConnection,
+      backupLog.size(),
+      (void *)backupLog.data(),
+      nullptr,
+      backupLogRestoreConflictHandler,
+      nullptr);
+
+  if (applyChangesetResult != SQLITE_OK) {
+    throw std::runtime_error(
+        "Failed to apply backup log. Details: " +
+        std::string(sqlite3_errstr(applyChangesetResult)));
+  }
 }
 } // namespace comm
