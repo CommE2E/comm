bf2665f0adb884f5d02859c3ed9b19517be7b9ff
Ginsu Eddy <ginsu.eddy@gmail.com>
2024-02-01 01:35:29 -0500

    [web] introduce add users list provider
    Summary:
    **PLEASE NOTE THAT THIS DIFF AND SUBSEQUENT DIFFS IN THIS STACK WILL NOT BE LANDED UNTIL MORE OF THE REDESIGN IS READY SINCE THIS WILL CAUSE REGRESSIONS IN PROD**
    
    In order to use the add users list for both add friends + add members, we need to make this component completely generic. Right now there is a lot of business logic for searching for any user to add as a friend that needs to be factored out.
    
    As I was attempting to lift the rest of the add friends specific logic from `AddUsersList`, I noticed that I needed to drill a ton of props in a bunch of places to handle the state needed for `AddUsersList`. This seemed a bit like a red flag to me, and I thought it would be a lot cleaner/better if I introduced a new provider to handle the state needed in `AddUsersList` and any relevant components
    
    Depends on D10936
    
    Test Plan: `flow` + confirmed the add friends modal continues to work as expected
    
    Reviewers: atul, inka
    
    Reviewed By: atul
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10937

--- web/settings/relationship/add-friends-modal.react.js
+++ web/settings/relationship/add-friends-modal.react.js
@@ -8,6 +8,7 @@ import {
 } from 'lib/types/relationship-types.js';
 
 import AddUsersListModal from './add-users-list-modal.react.js';
+import { AddUsersListProvider } from './add-users-list-provider.react.js';
 
 const excludedStatuses = new Set([
   userRelationshipStatus.FRIEND,
@@ -18,14 +19,21 @@ const excludedStatuses = new Set([
 ]);
 
 function AddFriendsModal(): React.Node {
-  return (
-    <AddUsersListModal
-      name="Add friends"
-      excludedStatuses={excludedStatuses}
-      confirmButtonContent="Send friend requests"
-      relationshipAction={relationshipActions.FRIEND}
-    />
+  const addFriendsModal = React.useMemo(
+    () => (
+      <AddUsersListProvider>
+        <AddUsersListModal
+          name="Add friends"
+          excludedStatuses={excludedStatuses}
+          confirmButtonContent="Send friend requests"
+          relationshipAction={relationshipActions.FRIEND}
+        />
+      </AddUsersListProvider>
+    ),
+    [],
   );
+
+  return addFriendsModal;
 }
 
 export default AddFriendsModal;
