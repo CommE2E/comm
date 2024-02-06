4811444492dcfec7f472e3b68aa02f2b3ce03951
Atul Madhugiri <atulm@berkeley.edu>
2024-02-06 14:16:57 -0500

    tmp

--- keyserver/src/fetchers/role-fetchers.js
+++ keyserver/src/fetchers/role-fetchers.js
@@ -1,26 +1,27 @@
 // @flow
 
 import { specialRoles } from 'lib/permissions/special-roles.js';
-import type { ClientLegacyRoleInfo } from 'lib/types/thread-types.js';
+import type { ServerLegacyRoleInfo } from 'lib/types/thread-types.js';
 
 import { dbQuery, SQL } from '../database/database.js';
 
-async function fetchRoles(threadID: string): Promise<ClientLegacyRoleInfo[]> {
+async function fetchRoles(threadID: string): Promise<ServerLegacyRoleInfo[]> {
   const query = SQL`
-    SELECT id, name, permissions, 
+    SELECT id, name, permissions, special_role,
       special_role = ${specialRoles.DEFAULT_ROLE} AS is_default
     FROM roles
     WHERE thread = ${threadID}
   `;
   const [result] = await dbQuery(query);
 
-  const roles: Array<ClientLegacyRoleInfo> = [];
+  const roles: Array<ServerLegacyRoleInfo> = [];
   for (const row of result) {
     roles.push({
       id: row.id.toString(),
       name: row.name,
       permissions: JSON.parse(row.permissions),
       isDefault: Boolean(row.is_default),
+      specialRole: row.special_role,
     });
   }
   return roles;
