d25040720da1f518c077b91da72dbfdf4be7f4d1
Ginsu Eddy <ginsu.eddy@gmail.com>
2024-02-05 22:20:18 -0500

    DRAFT [web] add navigation sidebar tab indicator to nav sidebar

--- web/navigation-sidebar/account-settings-button.react.js
+++ web/navigation-sidebar/account-settings-button.react.js
@@ -7,6 +7,7 @@ import { useDispatch } from 'lib/utils/redux-utils.js';
 
 import css from './account-settings-button.css';
 import { navigationSidebarLabelTooltipMargin } from './navigation-sidebar-constants.js';
+import NavigationSidebarTabIndicator from './navigation-sidebar-tab-indicator.react.js';
 import { updateNavInfoActionType } from '../redux/action-types.js';
 import { useLabelTooltip } from '../tooltips/tooltip-action-utils.js';
 import { tooltipPositions } from '../tooltips/tooltip-utils.js';
@@ -33,13 +34,16 @@ function AccountSettingsButton(): React.Node {
   });
 
   return (
-    <div
-      className={css.container}
-      onClick={openAccountSettings}
-      onMouseEnter={onMouseEnter}
-      onMouseLeave={onMouseLeave}
-    >
-      <SWMansionIcon icon="settings" size={20} />
+    <div>
+      <NavigationSidebarTabIndicator isActive={false} />
+      <div
+        className={css.container}
+        onClick={openAccountSettings}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+      >
+        <SWMansionIcon icon="settings" size={20} />
+      </div>
     </div>
   );
 }
