0d04a2740b5737ca4240d746f4591f5a8e297f09
Atul Madhugiri <atulm@berkeley.edu>
2024-02-06 16:12:36 -0500

    [lib] Add `shouldIncludeSpecialRoleFieldInRoles` to `RawThreadInfoOptions`
    Summary:
    Setting the minimum code version for all platforms to `FUTURE_CODE_VERSION` for the timebeing.
    
    ---
    
    Depends on D10971
    
    Test Plan: Will be tested implicitly by subsequent diffs in stack.
    
    Reviewers:
    ashoat, ginsu, tomek

--- keyserver/src/fetchers/thread-fetchers.js
+++ keyserver/src/fetchers/thread-fetchers.js
@@ -9,7 +9,10 @@ import {
   getContainingThreadID,
   getCommunity,
 } from 'lib/shared/thread-utils.js';
-import { hasMinCodeVersion } from 'lib/shared/version-utils.js';
+import {
+  FUTURE_CODE_VERSION,
+  hasMinCodeVersion,
+} from 'lib/shared/version-utils.js';
 import type { AvatarDBContent, ClientAvatar } from 'lib/types/avatar-types.js';
 import type { RawMessageInfo, MessageInfo } from 'lib/types/message-types.js';
 import type { RawThreadInfo } from 'lib/types/minimally-encoded-thread-permissions-types.js';
@@ -285,6 +288,11 @@ function rawThreadInfosFromServerThreadInfos(
     viewer.platformDetails,
     { native: 301, web: 56 },
   );
+  const futureCodeVersion = hasMinCodeVersion(viewer.platformDetails, {
+    native: FUTURE_CODE_VERSION,
+    web: FUTURE_CODE_VERSION,
+    majorDesktop: FUTURE_CODE_VERSION,
+  });
 
   const threadInfos: {
     [string]: LegacyRawThreadInfo | RawThreadInfo,
@@ -300,6 +308,7 @@ function rawThreadInfosFromServerThreadInfos(
         filterManageInviteLinksPermission: codeVersionBelow221,
         filterVoicedInAnnouncementChannelsPermission: codeVersionBelow283,
         minimallyEncodePermissions: minimallyEncodedPermissionsSupported,
+        includeSpecialRoleFieldInRoles: futureCodeVersion,
       },
     );
     if (threadInfo) {
