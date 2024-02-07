ebbda709e475b283c70bd8cec66ff7248742f174
William Wang <williamwang749@gmail.com>
2024-02-07 18:22:22 -0500

    [identity] Remove usage of sending raw error messages to client
    Summary: This removes usage of raw error messages that are not part of identity search message types.
    
    Test Plan: ran and verified on local dev through cargo run
    
    Reviewers: varun, bartek
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D10996

--- services/identity/src/websockets/send.rs
+++ services/identity/src/websockets/send.rs
@@ -8,32 +8,8 @@ use hyper_tungstenite::tungstenite::Message;
 use hyper_tungstenite::WebSocketStream;
 use tracing::error;
 
-use crate::websockets::errors;
-
 pub type WebsocketSink =
   Arc<Mutex<SplitSink<WebSocketStream<Upgraded>, Message>>>;
-
-pub async fn send_error_response(
-  error: errors::WebsocketError,
-  outgoing: WebsocketSink,
-) {
-  let response_msg = serde_json::json!({
-    "action": "errorMessage",
-    "error": format!("{}", error)
-  });
-
-  match serde_json::to_string(&response_msg) {
-    Ok(serialized_response) => {
-      send_message(Message::Text(serialized_response), outgoing).await;
-    }
-    Err(serialize_error) => {
-      error!(
-        "Failed to serialize the error response: {:?}",
-        serialize_error
-      );
-    }
-  }
-}
 
 pub async fn send_message(message: Message, outgoing: WebsocketSink) {
   if let Err(e) = outgoing.lock().await.send(message).await {
