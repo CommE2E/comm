7e43d837aabcc114d4222cdb65bc101a7cefcead
Michal Gniadek <michal.gniadek@swmansion.com>
2024-02-09 15:34:48 +0100

    [web] Restore compaction
    Summary:
    Restore compaction on web. Because web keeps redux persist storage in the database, and native doesn't, we must also:
    - save redux persist date before restoration
    - put it back after it's restored
    We can do that before applying logs, before we can be sure that they won't touch this table.
    
    Moved a few persist constants to a new file because otherwise webworker imported something that used `window`, which isn't available there.
    
    Depends on D11042
    
    Test Plan:
    - Add logging to the native client so that it logs backupID and encryption keys for compaction
    - Upload a new compaction from native with a draft
    - Send restore message
    - Reload web app
    - Check that the draft from native exists now on web. Check that the enabled apps are the same as before restore (redux persist didn't change)
    
    Reviewers: marcin, bartek, kamil
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D11043

--- web/database/worker/db-worker.js
+++ web/database/worker/db-worker.js
@@ -3,6 +3,7 @@
 import initBackupClientModule from 'backup-client-wasm';
 import localforage from 'localforage';
 
+import { restoreBackup } from './backup.js';
 import {
   getClientStoreFromQueryExecutor,
   processDBStoreOperations,
@@ -243,6 +244,14 @@ async function processAppRequest(
     message.type === workerRequestMessageTypes.REMOVE_PERSIST_STORAGE_ITEM
   ) {
     sqliteQueryExecutor.removePersistStorageItem(message.key);
+  } else if (message.type === workerRequestMessageTypes.BACKUP_RESTORE) {
+    await restoreBackup(
+      sqliteQueryExecutor,
+      dbModule,
+      message.authMetadata,
+      message.backupID,
+      message.backupDataKey,
+    );
   }
 
   persistNeeded = true;
