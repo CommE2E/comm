3ab7e42917ba9d0f9b8ff6d1c446274ca6b1a8e9
Ginsu Eddy <ginsu.eddy@gmail.com>
2024-02-09 17:38:31 -0500

    [web] introduce thread settings utils
    Summary: Move diff, I created this file to house all the business logic for the thread settings modal. We need to do this because we need to use this logic outside of this component and use it in a "`primaryButton`" prop that we will pass to the modal
    
    Test Plan: `flow` + confirmed that the thread settings modal still works as expected
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D11031

---
+++ web/modals/threads/settings/thread-settings-utils.js
@@ -0,0 +1,211 @@
+// @flow
+
+import * as React from 'react';
+
+import {
+  changeThreadSettingsActionTypes,
+  useChangeThreadSettings,
+  deleteThreadActionTypes,
+  useDeleteThread,
+} from 'lib/actions/thread-actions.js';
+import { useModalContext } from 'lib/components/modal-provider.react.js';
+import { containedThreadInfos } from 'lib/selectors/thread-selectors.js';
+import { type SetState } from 'lib/types/hook-types.js';
+import type { ThreadInfo } from 'lib/types/minimally-encoded-thread-permissions-types.js';
+import { type ThreadChanges } from 'lib/types/thread-types.js';
+import { useDispatchActionPromise } from 'lib/utils/redux-promise-utils.js';
+
+import ThreadDeleteConfirmationModal from './thread-settings-delete-confirmation-modal.react.js';
+import { useSelector } from '../../../redux/redux-utils.js';
+
+type UseOnSaveGeneralThreadSettingsParams = {
+  +threadInfo: ThreadInfo,
+  +queuedChanges: ThreadChanges,
+  +setQueuedChanges: SetState<ThreadChanges>,
+  +setErrorMessage: SetState<?string>,
+};
+
+function useOnSaveGeneralThreadSettings(
+  params: UseOnSaveGeneralThreadSettingsParams,
+): (event: SyntheticEvent<HTMLElement>) => mixed {
+  const { threadInfo, queuedChanges, setQueuedChanges, setErrorMessage } =
+    params;
+
+  const dispatchActionPromise = useDispatchActionPromise();
+  const callChangeThreadSettings = useChangeThreadSettings();
+
+  const changeThreadSettingsAction = React.useCallback(async () => {
+    try {
+      setErrorMessage('');
+      return await callChangeThreadSettings({
+        threadID: threadInfo.id,
+        changes: queuedChanges,
+      });
+    } catch (e) {
+      setErrorMessage('unknown_error');
+      throw e;
+    } finally {
+      setQueuedChanges(Object.freeze({}));
+    }
+  }, [
+    callChangeThreadSettings,
+    queuedChanges,
+    setErrorMessage,
+    setQueuedChanges,
+    threadInfo.id,
+  ]);
+
+  const onSubmit = React.useCallback(
+    (event: SyntheticEvent<HTMLElement>) => {
+      event.preventDefault();
+      void dispatchActionPromise(
+        changeThreadSettingsActionTypes,
+        changeThreadSettingsAction(),
+      );
+    },
+    [changeThreadSettingsAction, dispatchActionPromise],
+  );
+
+  return onSubmit;
+}
+
+type UseOnSavePrivacySettingsParams = {
+  +threadInfo: ThreadInfo,
+  +queuedChanges: ThreadChanges,
+  +setQueuedChanges: SetState<ThreadChanges>,
+  +setErrorMessage: SetState<?string>,
+};
+
+function useOnSavePrivacyThreadSettings(
+  params: UseOnSavePrivacySettingsParams,
+): (event: SyntheticEvent<HTMLElement>) => mixed {
+  const { threadInfo, queuedChanges, setQueuedChanges, setErrorMessage } =
+    params;
+
+  const modalContext = useModalContext();
+  const dispatchActionPromise = useDispatchActionPromise();
+  const callChangeThreadSettings = useChangeThreadSettings();
+
+  const changeThreadSettingsAction = React.useCallback(async () => {
+    try {
+      setErrorMessage('');
+      const response = await callChangeThreadSettings({
+        threadID: threadInfo.id,
+        changes: queuedChanges,
+      });
+      modalContext.popModal();
+      return response;
+    } catch (e) {
+      setErrorMessage('unknown_error');
+      setQueuedChanges(Object.freeze({}));
+      throw e;
+    }
+  }, [
+    callChangeThreadSettings,
+    modalContext,
+    queuedChanges,
+    setErrorMessage,
+    setQueuedChanges,
+    threadInfo.id,
+  ]);
+
+  const onSubmit = React.useCallback(
+    (event: SyntheticEvent<HTMLElement>) => {
+      event.preventDefault();
+      void dispatchActionPromise(
+        changeThreadSettingsActionTypes,
+        changeThreadSettingsAction(),
+      );
+    },
+    [changeThreadSettingsAction, dispatchActionPromise],
+  );
+
+  return onSubmit;
+}
+
+type UseOnDeleteParams = {
+  +threadInfo: ThreadInfo,
+  +setErrorMessage: SetState<?string>,
+};
+
+function useOnDeleteThread(
+  params: UseOnDeleteParams,
+): (event: SyntheticEvent<HTMLElement>) => mixed {
+  const { threadInfo, setErrorMessage } = params;
+
+  const modalContext = useModalContext();
+  const dispatchActionPromise = useDispatchActionPromise();
+  const callDeleteThread = useDeleteThread();
+  const containedThreads = useSelector(
+    state => containedThreadInfos(state)[threadInfo.id],
+  );
+
+  const shouldUseDeleteConfirmationModal = React.useMemo(
+    () => containedThreads?.length > 0,
+    [containedThreads?.length],
+  );
+
+  const popThreadDeleteConfirmationModal = React.useCallback(() => {
+    if (shouldUseDeleteConfirmationModal) {
+      modalContext.popModal();
+    }
+  }, [modalContext, shouldUseDeleteConfirmationModal]);
+
+  const deleteThreadAction = React.useCallback(async () => {
+    try {
+      setErrorMessage('');
+      const response = await callDeleteThread({ threadID: threadInfo.id });
+      popThreadDeleteConfirmationModal();
+      modalContext.popModal();
+      return response;
+    } catch (e) {
+      popThreadDeleteConfirmationModal();
+      setErrorMessage(
+        e.message === 'invalid_credentials'
+          ? 'permission not granted'
+          : 'unknown error',
+      );
+      throw e;
+    }
+  }, [
+    callDeleteThread,
+    modalContext,
+    popThreadDeleteConfirmationModal,
+    setErrorMessage,
+    threadInfo.id,
+  ]);
+
+  const dispatchDeleteThreadAction = React.useCallback(() => {
+    void dispatchActionPromise(deleteThreadActionTypes, deleteThreadAction());
+  }, [dispatchActionPromise, deleteThreadAction]);
+
+  const onDelete = React.useCallback(
+    (event: SyntheticEvent<HTMLElement>) => {
+      event.preventDefault();
+      if (shouldUseDeleteConfirmationModal) {
+        modalContext.pushModal(
+          <ThreadDeleteConfirmationModal
+            threadInfo={threadInfo}
+            onConfirmation={dispatchDeleteThreadAction}
+          />,
+        );
+      } else {
+        dispatchDeleteThreadAction();
+      }
+    },
+    [
+      dispatchDeleteThreadAction,
+      modalContext,
+      shouldUseDeleteConfirmationModal,
+      threadInfo,
+    ],
+  );
+
+  return onDelete;
+}
+
+export {
+  useOnSaveGeneralThreadSettings,
+  useOnSavePrivacyThreadSettings,
+  useOnDeleteThread,
+};
