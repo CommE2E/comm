32621a6b3dde746c1a0ce917426754c35f45c657
Ginsu Eddy <ginsu.eddy@gmail.com>
2024-02-09 15:15:29 -0500

    [web] cleanup UnsavedChangesModal
    Summary: This diff cleans up the onse off styles in the UnsavedChangesModal and updates the modal to follow the conventions of the new modal api
    
    Test Plan: Please see the screenshot below
    
    Reviewers:
    
    Subscribers:

--- web/modals/unsaved-changes-modal.react.js
+++ web/modals/unsaved-changes-modal.react.js
@@ -9,7 +9,6 @@ import css from './unsaved-changes-modal.css';
 import Button, { buttonThemes } from '../components/button.react.js';
 
 function UnsavedChangesModal(): React.Node {
-  const modalName = '';
   const { popModal } = useModalContext();
 
   const onCloseModalClick = React.useCallback(() => {
@@ -18,36 +17,52 @@ function UnsavedChangesModal(): React.Node {
     popModal();
   }, [popModal]);
 
-  return (
-    <Modal
-      name={modalName}
-      onClose={popModal}
-      withCloseButton={false}
-      size="large"
-    >
-      <div className={css.unsavedChangesModalText}>
-        You have unsaved changes, are you sure you want to close the modal? You
-        will lose all your progress.
-      </div>
-      <div className={css.unsavedChangesModalButtons}>
-        <Button
-          variant="outline"
-          className={css.unsavedChangesBackButton}
-          onClick={popModal}
-        >
-          Back
-        </Button>
-        <Button
-          variant="filled"
-          className={css.unsavedChangesCloseModalButton}
-          buttonColor={buttonThemes.danger}
-          onClick={onCloseModalClick}
-        >
-          Close Modal
-        </Button>
-      </div>
-    </Modal>
+  const primaryButton = React.useMemo(
+    () => (
+      <Button
+        variant="filled"
+        className={css.unsavedChangesCloseModalButton}
+        buttonColor={buttonThemes.danger}
+        onClick={onCloseModalClick}
+      >
+        Close Modal
+      </Button>
+    ),
+    [onCloseModalClick],
   );
+
+  const secondaryButton = React.useMemo(
+    () => (
+      <Button
+        variant="outline"
+        className={css.unsavedChangesBackButton}
+        onClick={popModal}
+      >
+        Back
+      </Button>
+    ),
+    [popModal],
+  );
+
+  const unsavedChangesModal = React.useMemo(
+    () => (
+      <Modal
+        onClose={popModal}
+        withCloseButton={false}
+        size="large"
+        primaryButton={primaryButton}
+        secondaryButton={secondaryButton}
+      >
+        <div className={css.unsavedChangesModalText}>
+          You have unsaved changes, are you sure you want to close the modal?
+          You will lose all your progress.
+        </div>
+      </Modal>
+    ),
+    [popModal, primaryButton, secondaryButton],
+  );
+
+  return unsavedChangesModal;
 }
 
 export default UnsavedChangesModal;
