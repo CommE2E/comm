d058a9d75654e632ab7edc88c290afba6b109321
Ashoat Tevosyan <ashoat@comm.app>
2024-02-09 21:46:08 -0500

    [lib][native][web] Refactor useInitialNotificationsEncryptedMessage to get Olm session creator from context
    Summary: Now that we have a cross-platform context exposing this value, we can get it from there and simplify the API for `useInitialNotificationsEncryptedMessage` a little bit.
    
    Depends on D11218
    
    Test Plan: In addition to the plan described here: https://gist.github.com/Ashoat/75ab690d5c53cdd68a51b02e03e27c58
    
    I also made sure that after session recovery, than an entry in the `olm_sessions` MariaDB table existed for the new cookie with `is_content = 0`
    
    Reviewers: inka, tomek
    
    Subscribers:

--- native/data/sqlite-data-handler.js
+++ native/data/sqlite-data-handler.js
@@ -30,7 +30,6 @@ import { commCoreModule } from '../native-modules.js';
 import { setStoreLoadedActionType } from '../redux/action-types.js';
 import { useSelector } from '../redux/redux-utils.js';
 import Alert from '../utils/alert.js';
-import { nativeNotificationsSessionCreator } from '../utils/crypto-utils.js';
 import { isTaskCancelledError } from '../utils/error-handling.js';
 import { useStaffCanSee } from '../utils/staff-utils.js';
 
@@ -61,7 +60,7 @@ function SQLiteDataHandler(): React.Node {
   );
   const mediaCacheContext = React.useContext(MediaCacheContext);
   const getInitialNotificationsEncryptedMessage =
-    useInitialNotificationsEncryptedMessage(nativeNotificationsSessionCreator);
+    useInitialNotificationsEncryptedMessage(authoritativeKeyserverID);
 
   const callFetchNewCookieFromNativeCredentials = React.useCallback(
     async (source: RecoveryActionSource) => {
