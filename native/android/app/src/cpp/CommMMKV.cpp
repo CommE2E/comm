72d55be450afe970dc272ebba7691bfc2172bb10
marcinwasowicz <marcinkwasowicz@gmail.com>
2024-02-12 14:53:25 +0100

    Add getter and setter for integers, getter for all keys and method to remove subset of keys to MMKV
    Summary: This differential MMKV API with integer getter and setter and function to retrieve all keys in the db.
    
    Test Plan: edit in web
    
    Reviewers: tomek, bartek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D11047

--- native/android/app/src/cpp/CommMMKV.cpp
+++ native/android/app/src/cpp/CommMMKV.cpp
@@ -37,6 +37,53 @@ public:
     }
     return std::nullopt;
   }
+
+  static bool setInt(std::string key, int value) {
+    static const auto cls = javaClassStatic();
+    static auto method =
+        cls->getStaticMethod<jboolean(std::string, int)>("setInt");
+    return method(cls, key, value);
+  }
+
+  static std::optional<int> getInt(std::string key, int noValue) {
+    static const auto cls = javaClassStatic();
+    static auto method =
+        cls->getStaticMethod<JInteger(std::string, int)>("getInt");
+    const auto result = method(cls, key, noValue);
+    if (result) {
+      return result->value();
+    }
+    return std::nullopt;
+  }
+
+  static std::vector<std::string> getAllKeys() {
+    static const auto cls = javaClassStatic();
+    static auto method =
+        cls->getStaticMethod<JArrayClass<JString>()>("getAllKeys");
+    auto methodResult = method(cls);
+
+    std::vector<std::string> result;
+    for (int i = 0; i < methodResult->size(); i++) {
+      result.push_back(methodResult->getElement(i)->toStdString());
+    }
+
+    return result;
+  }
+
+  static void removeKeys(const std::vector<std::string> &keys) {
+    static const auto cls = javaClassStatic();
+    static auto method =
+        cls->getStaticMethod<void(local_ref<JArrayClass<JString>>)>(
+            "removeKeys");
+
+    local_ref<JArrayClass<JString>> keysJava =
+        JArrayClass<JString>::newArray(keys.size());
+
+    for (int i = 0; i < keys.size(); i++) {
+      keysJava->setElement(i, *make_jstring(keys[i]));
+    }
+    method(cls, keysJava);
+  }
 };
 
 namespace comm {
@@ -65,4 +112,29 @@ std::optional<std::string> CommMMKV::getString(std::string key) {
   return result;
 }
 
+bool CommMMKV::setInt(std::string key, int value) {
+  bool result;
+  NativeAndroidAccessProvider::runTask(
+      [&]() { result = CommMMKVJavaClass::setInt(key, value); });
+  return result;
+}
+
+std::optional<int> CommMMKV::getInt(std::string key, int noValue) {
+  std::optional<int> result;
+  NativeAndroidAccessProvider::runTask(
+      [&]() { result = CommMMKVJavaClass::getInt(key, noValue); });
+  return result;
+}
+
+std::vector<std::string> CommMMKV::getAllKeys() {
+  std::vector<std::string> result;
+  NativeAndroidAccessProvider::runTask(
+      [&]() { result = CommMMKVJavaClass::getAllKeys(); });
+  return result;
+}
+
+void CommMMKV::removeKeys(const std::vector<std::string> &keys) {
+  NativeAndroidAccessProvider::runTask(
+      [&]() { CommMMKVJavaClass::removeKeys(keys); });
+}
 } // namespace comm
--- native/cpp/CommonCpp/Tools/CommMMKV.h
+++ native/cpp/CommonCpp/Tools/CommMMKV.h
@@ -2,6 +2,7 @@
 
 #include <optional>
 #include <string>
+#include <vector>
 
 namespace comm {
 class CommMMKV {
@@ -11,5 +12,19 @@ public:
   static void clearSensitiveData();
   static bool setString(std::string key, std::string value);
   static std::optional<std::string> getString(std::string key);
+
+  static bool setInt(std::string key, int value);
+
+  // MMKV API can't return null when we try to get integer that
+  // doesn't exist. It allows us to set default value that is
+  // returned instead in case the integer isn't present. The
+  // developer should pass as `noValue` the value that they
+  // know should never be set under certain key. Implementation
+  // will pass `noValue` as default value and return `std::nullopt`
+  // in case MMKV returns default value.
+  static std::optional<int> getInt(std::string key, int noValue);
+
+  static std::vector<std::string> getAllKeys();
+  static void removeKeys(const std::vector<std::string> &keys);
 };
 } // namespace comm
