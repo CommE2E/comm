6f1c83ebad4e85fc5b9a87bb5c8331828e44fdb5
marcinwasowicz <marcinkwasowicz@gmail.com>
2024-02-12 14:53:25 +0100

    Add getter and setter for integers, getter for all keys and method to remove subset of keys to MMKV
    Summary: This differential MMKV API with integer getter and setter and function to retrieve all keys in the db.
    
    Test Plan: edit in web
    
    Reviewers: tomek, bartek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D11047

--- native/cpp/CommonCpp/Tools/CommMMKV.h
+++ native/cpp/CommonCpp/Tools/CommMMKV.h
@@ -2,6 +2,7 @@
 
 #include <optional>
 #include <string>
+#include <vector>
 
 namespace comm {
 class CommMMKV {
@@ -11,5 +12,11 @@ public:
   static void clearSensitiveData();
   static bool setString(std::string key, std::string value);
   static std::optional<std::string> getString(std::string key);
+
+  static bool setInt(std::string key, int value);
+  static std::optional<int> getInt(std::string key, int noValue);
+
+  static std::vector<std::string> getAllKeys();
+  static void removeKeys(const std::vector<std::string> &keys);
 };
 } // namespace comm
