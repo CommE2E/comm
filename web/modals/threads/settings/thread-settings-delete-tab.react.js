ebc48810d3abb45c0f958f3795ca0b701f825419
Ginsu Eddy <ginsu.eddy@gmail.com>
2024-02-12 01:01:58 -0500

    [web] cleanup thread settings modal delete tab
    Summary: This diff cleans up the delete tab in the thread settings modal to follow the conventions of the new modal api. Subsquent diffs will handle the other tabs in the thread settings modal
    
    Test Plan: Please see the screenshot below
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D11032

--- web/modals/threads/settings/thread-settings-delete-tab.react.js
+++ web/modals/threads/settings/thread-settings-delete-tab.react.js
@@ -3,61 +3,21 @@
 import * as React from 'react';
 
 import SWMansionIcon from 'lib/components/SWMansionIcon.react.js';
-import { type SetState } from 'lib/types/hook-types.js';
-import type { ThreadInfo } from 'lib/types/minimally-encoded-thread-permissions-types.js';
 
-import SubmitSection from './submit-section.react.js';
 import css from './thread-settings-delete-tab.css';
-import { useOnDeleteThread } from './thread-settings-utils.js';
-import { buttonThemes } from '../../../components/button.react.js';
 
-type ThreadSettingsDeleteTabProps = {
-  +threadSettingsOperationInProgress: boolean,
-  +threadInfo: ThreadInfo,
-  +setErrorMessage: SetState<?string>,
-  +errorMessage?: ?string,
-};
-
-function ThreadSettingsDeleteTab(
-  props: ThreadSettingsDeleteTabProps,
-): React.Node {
-  const {
-    threadSettingsOperationInProgress,
-    threadInfo,
-    setErrorMessage,
-    errorMessage,
-  } = props;
-
-  const onDeleteThread = useOnDeleteThread({
-    threadInfo,
-    setErrorMessage,
-  });
-
+function ThreadSettingsDeleteTab(): React.Node {
   return (
-    <form method="POST" className={css.container}>
-      <div>
-        <div className={css.warning_container}>
-          <SWMansionIcon
-            icon="warning-circle"
-            className={css.warning_icon}
-            size={26}
-          />
-          <p className={css.deletion_warning}>
-            Your chat will be permanently deleted. There is no way to reverse
-            this.
-          </p>
-        </div>
-      </div>
-      <SubmitSection
-        errorMessage={errorMessage}
-        onClick={onDeleteThread}
-        variant="filled"
-        buttonColor={buttonThemes.danger}
-        disabled={threadSettingsOperationInProgress}
-      >
-        Delete
-      </SubmitSection>
-    </form>
+    <div className={css.warning_container}>
+      <SWMansionIcon
+        icon="warning-circle"
+        className={css.warning_icon}
+        size={26}
+      />
+      <p className={css.deletion_warning}>
+        Your chat will be permanently deleted. There is no way to reverse this.
+      </p>
+    </div>
   );
 }
 
