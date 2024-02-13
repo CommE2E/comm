444ff10b18c837a0115716004a9f871e620105de
Atul Madhugiri <atulm@berkeley.edu>
2024-02-13 16:58:25 -0500

    [native] Deprecate and move `updateClientDBThreadStoreThreadInfos` and `createUpdateDBOpsForThreadStoreThreadInfos`
    Summary:
    `createUpdateDBOpsForThreadStoreThreadInfos` consumes `deprecatedConvertClientDBThreadInfoToRawThreadInfo`, but based on the name `createUpdateDBOpsForThreadStoreThreadInfos` it's not clear that the function is only for handling "Legacy" `RawThreadInfo`s. I should've made this more clear when I was doing the refactoring work.
    
    We **could** modify these functions to handle both (previously known as) `LegacyRawThreadInfo`s and `MinimallyEncodedRawThreadInfo`s, but that would complicate the logic and make things tricky with `flow` types (because we want to support Legacy -> Legacy, Legacy -> MinimallyEncoded, and MinimallyEncoded -> MinimallyEncoded migration functions.
    
    It would also be good to keep these existing utilities "frozen" so we don't have to go back and test old migrations pre-minimalEncoding... which would be tedious and time consuming.
    
    Personally think it makes sense to keep these utilities "frozen" in a `deprecated-*` file and introduce new functions that only handle `MinimallyEncoded->MinimallyEncoded` going forward.
    
    This is kind of an opinionated decision, so adding @ashoat as reviewer to get his thoughts.
    
    ---
    
    Depends on D11056
    
    Test Plan:
    Simple rename and move, trusting `flow`.
    
    Reviewers:
    ashoat, ginsu, tomek

---
+++ native/redux/deprecated-update-client-db-thread-store-thread-infos.js
@@ -0,0 +1,94 @@
+// @flow
+
+import type { ClientDBThreadStoreOperation } from 'lib/ops/thread-store-ops.js';
+import type { RawThreadInfo } from 'lib/types/minimally-encoded-thread-permissions-types.js';
+import type {
+  ClientDBThreadInfo,
+  LegacyRawThreadInfo,
+  MixedRawThreadInfos,
+} from 'lib/types/thread-types.js';
+import { values } from 'lib/utils/objects.js';
+import {
+  convertRawThreadInfoToClientDBThreadInfo,
+  deprecatedConvertClientDBThreadInfoToRawThreadInfo,
+} from 'lib/utils/thread-ops-utils.js';
+
+import type { AppState } from './state-types.js';
+import { commCoreModule } from '../native-modules.js';
+
+function deprecatedUpdateClientDBThreadStoreThreadInfos(
+  state: AppState,
+  migrationFunc: MixedRawThreadInfos => MixedRawThreadInfos,
+  handleMigrationFailure?: AppState => AppState,
+): AppState {
+  // Get threads from SQLite `threads` table.
+  const clientDBThreadInfos = commCoreModule.getAllThreadsSync();
+
+  const operations = deprecatedCreateUpdateDBOpsForThreadStoreThreadInfos(
+    clientDBThreadInfos,
+    migrationFunc,
+  );
+
+  // Try processing `ClientDBThreadStoreOperation`s and log out if
+  //    `processThreadStoreOperationsSync(...)` throws an exception.
+  try {
+    commCoreModule.processThreadStoreOperationsSync(operations);
+  } catch (exception) {
+    console.log(exception);
+    if (handleMigrationFailure) {
+      return handleMigrationFailure(state);
+    }
+    return ({ ...state, cookie: null }: any);
+  }
+
+  return state;
+}
+
+function deprecatedCreateUpdateDBOpsForThreadStoreThreadInfos(
+  clientDBThreadInfos: $ReadOnlyArray<ClientDBThreadInfo>,
+  migrationFunc: MixedRawThreadInfos => MixedRawThreadInfos,
+): $ReadOnlyArray<ClientDBThreadStoreOperation> {
+  // Translate `ClientDBThreadInfo`s to `RawThreadInfo`s.
+  const rawThreadInfos = clientDBThreadInfos.map(
+    deprecatedConvertClientDBThreadInfoToRawThreadInfo,
+  );
+
+  // Convert `rawThreadInfo`s to a map of `threadID` => `threadInfo`.
+  const threadIDToThreadInfo = rawThreadInfos.reduce(
+    (
+      acc: { [string]: LegacyRawThreadInfo | RawThreadInfo },
+      threadInfo: LegacyRawThreadInfo | RawThreadInfo,
+    ) => {
+      acc[threadInfo.id] = threadInfo;
+      return acc;
+    },
+    {},
+  );
+
+  // Apply `migrationFunc` to `threadInfo`s.
+  const updatedThreadIDToThreadInfo = migrationFunc(threadIDToThreadInfo);
+
+  // Convert the updated `threadInfo`s back into an array.
+  const updatedRawThreadInfos: $ReadOnlyArray<
+    LegacyRawThreadInfo | RawThreadInfo,
+  > = values(updatedThreadIDToThreadInfo);
+
+  // Translate `RawThreadInfo`s to `ClientDBThreadInfo`s.
+  const convertedClientDBThreadInfos: $ReadOnlyArray<ClientDBThreadInfo> =
+    updatedRawThreadInfos.map(convertRawThreadInfoToClientDBThreadInfo);
+
+  // Construct `ClientDBThreadStoreOperation`s to clear SQLite `threads`
+  //    table and repopulate with `ClientDBThreadInfo`s.
+  return [
+    {
+      type: 'remove_all',
+    },
+    ...convertedClientDBThreadInfos.map((thread: ClientDBThreadInfo) => ({
+      type: 'replace',
+      payload: thread,
+    })),
+  ];
+}
+
+export { deprecatedCreateUpdateDBOpsForThreadStoreThreadInfos };
+export { deprecatedUpdateClientDBThreadStoreThreadInfos };
