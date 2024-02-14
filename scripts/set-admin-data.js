86b7b5e18a4c31f7badfdb47079c0d0f43cdb494
Inka Sokołowska <inka.d.sokolowska@gmail.com>
2024-02-14 15:16:23 +0100

    [scripts] Create a script for configuring user credentials
    Summary: issue: [[ https://linear.app/comm/issue/ENG-6615/create-a-script-for-making-authoritative-keyserver-id-configurable | ENG-6615 ]]
    
    Test Plan:
    I added `node $PRJ_ROOT/scripts/set-admin-data.js` to dev-shell.nix:
    {F1152224}
    
    Reviewers: tomek, michal, ashoat
    
    Reviewed By: tomek, ashoat
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D11075

---
+++ scripts/set-admin-data.js
@@ -0,0 +1,84 @@
+/* eslint-disable flowtype/require-valid-file-annotation */
+
+const fs = require('fs');
+const readline = require('readline');
+
+// Create an interface for reading input
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const data = {};
+
+rl.question('username: ', value1 => {
+  data.username = value1;
+
+  rl.question('password: ', value2 => {
+    data.password = value2;
+
+    rl.question('user id: ', value3 => {
+      data.id = value3;
+
+      writeFiles(data);
+
+      // Close the readline interface
+      rl.close();
+    });
+  });
+});
+
+function writeFiles(credentials) {
+  try {
+    const userCredentials = {
+      username: credentials.username,
+      password: credentials.password,
+      usingIdentityCredentials: true,
+    };
+    const userCredentialsJSON = JSON.stringify(userCredentials, null, 2);
+    const keyserverSecrets = 'keyserver/secrets';
+    if (!fs.existsSync(keyserverSecrets)) {
+      fs.mkdirSync(keyserverSecrets);
+    }
+    fs.writeFileSync(
+      'keyserver/secrets/user_credentials.json',
+      userCredentialsJSON,
+    );
+
+    // Create configs containing authoritative keyservers data.
+    // Because we have different mechanisms for fetching configs,
+    // we need this config in two places
+
+    const authoritativeKeyserver = {
+      authoritativeKeyserverID: credentials.id,
+    };
+    const authoritativeKeyserverJSON = JSON.stringify(
+      authoritativeKeyserver,
+      null,
+      2,
+    );
+    const nativeFacts = 'native/facts';
+    if (!fs.existsSync(nativeFacts)) {
+      fs.mkdirSync(nativeFacts);
+    }
+    fs.writeFileSync(
+      'native/facts/authoritative_keyserver.json',
+      authoritativeKeyserverJSON,
+    );
+
+    const keyserverFacts = 'keyserver/facts';
+    if (!fs.existsSync(keyserverFacts)) {
+      fs.mkdirSync(keyserverFacts);
+    }
+    fs.writeFileSync(
+      'keyserver/facts/authoritative_keyserver.json',
+      authoritativeKeyserverJSON,
+    );
+  } catch (e) {
+    console.error(
+      'Failure creating configuration files: ' +
+        'admin data could not be correctly written',
+    );
+    throw e;
+  }
+}
