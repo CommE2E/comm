c220e550084a1b5b7f59b37ba767dec5e6b50636
Michal Gniadek <michal.gniadek@swmansion.com>
2024-02-19 15:55:01 +0100

    [web] Introduce useWebLock
    Summary:
    [ENG-6667 : Implement "primary" tab lock](https://linear.app/comm/issue/ENG-6667)
    
    We want to only have a singular tunnelbroker connection across all tabs on web. One way to achieve this is to use the WebLockAPI. The `useWebLock` hook exposes a nicer interface to it. It returns lock status and `releaseLock` function. When the status is equal to `'should-release'` the caller is expected to run cleanup (e.g. disconnecting from the tunnelbroker) and call `releaseLock`.
    
    Only the foreground tabs will try and acquire a lock. After a tab is backgrounded it should release the lock if it has one.
    
    Some information about the web lock api:
    - It allows to request a lock with a specific name
    - The lock can be held only "in one place"/"one call to the `request` method" across all tabs of the same origin
      - If we make sure to only call it once in the app, we can get the required locking behaviour between tabs
      - (this is assuming the `exclusive` mode, which is the default)
    - If the lock is already taken, the `request` call goes into a queue, and will wait until the lock is released
      - Request in queue can be aborted with an `AbortSignall`
    - Call to `request` returns a promise and expects an async callback as an argument. When it acquired the lock it calls the provided callback. The promise returned by the `request` call resolves when the the promise returned from the callback resolves.
    - Locks are released automatically after the tab is closed
    
    Additionally our version of flow doesn't type the `LockManager` (this was also the case for e.g. service worker and shared worker types) so I had to do it myself.
    
    Test Plan:
    Add this code:
    
    ```
    const { lockStatus, releaseLock } = useWebLock('lock');
    React.useEffect(() => {
       document.title = lockStatus;
    
       if(lockStatus === 'should-release') {
         // Add some delay to simulate closing the tunnelbroker socket
         setTimeout(releaseLock, 1000);
       }
     }, [lockStatus]);
    ```
    
    Try switching between different tabs quickly, try displaying two tabs at the same time. Check that at all times the only tab holding the lock is one of the tabs in the foreground (except for the 1s delay before call to `releaseLock`). Check that if no tab is selected the lock is released.
    
    Reviewers: kamil, inka, bartek
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D11113

---
+++ web/web-lock.js
@@ -0,0 +1,130 @@
+// @flow
+
+import * as React from 'react';
+
+import { useIsAppForegrounded } from 'lib/shared/lifecycle-utils.js';
+
+const lockStatuses = Object.freeze({
+  ACQUIRED: 'acquired',
+  SHOULD_RELEASE: 'should-release',
+  REQUESTED: 'requested',
+  RELEASED: 'released',
+});
+export type LockStatus = $Values<typeof lockStatuses>;
+const ABORT_REASON_BACKGROUNDED = 'backgrounded';
+
+function useWebLock(name: string): {
+  lockStatus: LockStatus,
+  releaseLockOrAbortRequest: () => void,
+} {
+  const [lockRequest, setLockRequest] = React.useState<null | {
+    controller: AbortController,
+    resolveCallback: ?() => void,
+  }>(null);
+
+  const releaseLockOrAbortRequest = React.useCallback(() => {
+    if (!lockRequest) {
+      return;
+    }
+
+    // Abort lock request if we don't have it
+    lockRequest.controller.abort(ABORT_REASON_BACKGROUNDED);
+    // Release lock if we have it
+    lockRequest?.resolveCallback?.();
+    // The `navigator.locks.request` promise should now resolve
+    // and the lock will be set to null
+  }, [lockRequest]);
+
+  const isVisible = useIsAppForegrounded();
+  React.useEffect(() => {
+    if (!isVisible) {
+      // If the tab isn't visible and we haven't acquired the lock
+      // we want to abort the request
+      if (lockRequest && !lockRequest.resolveCallback) {
+        releaseLockOrAbortRequest();
+      }
+      return;
+    }
+
+    if (lockRequest) {
+      return;
+    }
+
+    const controller = new AbortController();
+    setLockRequest({
+      controller,
+      resolveCallback: null,
+    });
+
+    void (async () => {
+      try {
+        const lockManager: LockManager = (navigator: any).locks;
+        // This promise will resolve after we get a lock
+        // and the inner Promise resolves. It will reject
+        // if we abort the request or there is an error.
+        await lockManager.request(
+          name,
+          { signal: controller.signal },
+          () =>
+            new Promise(resolve => {
+              setLockRequest({ controller, resolveCallback: resolve });
+            }),
+        );
+      } catch (err) {
+        if (err !== ABORT_REASON_BACKGROUNDED) {
+          console.log('Error when requesting a lock', err);
+        }
+      }
+
+      setLockRequest(null);
+    })();
+  }, [isVisible, lockRequest, name, releaseLockOrAbortRequest]);
+
+  const lockStatus = React.useMemo(() => {
+    if (!lockRequest) {
+      return lockStatuses.RELEASED;
+    }
+    if (!lockRequest?.resolveCallback) {
+      return lockStatuses.REQUESTED;
+    }
+    if (!isVisible) {
+      return lockStatuses.SHOULD_RELEASE;
+    }
+
+    return lockStatuses.ACQUIRED;
+  }, [lockRequest, isVisible]);
+
+  return { lockStatus, releaseLockOrAbortRequest };
+}
+
+type LockManager = {
+  +request: <T>(
+    name: string,
+    options: {
+      +mode?: LockMode,
+      +ifAvailable?: boolean,
+      +steal?: boolean,
+      +signal?: AbortSignal,
+    },
+    callback: (Lock) => Promise<T>,
+  ) => Promise<T>,
+  +query: () => Promise<{
+    +held: $ReadOnlyArray<LockInfo>,
+    +pending: $ReadOnly<LockInfo>,
+  }>,
+};
+
+type Lock = {
+  +name: string,
+  +mode: LockMode,
+};
+
+type LockInfo = {
+  +name: string,
+  +mode: LockMode,
+  +clientID: string,
+};
+
+type LockMode = 'exclusive' | 'shared';
+
+export { useWebLock };
