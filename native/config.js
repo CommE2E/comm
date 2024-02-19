7c8ff03ff6bb7e6b2c319149ca0f70d775141335
xsanm <kkurowski.kk@gmail.com>
2024-02-19 17:59:35 +0100

    [lib] introduce stateless `olm` API
    Summary:
    The point of this code is to introduce `olm` interface shared across all platforms, which is stateless:
    - on native we have `CryptoModule` which takes care of each action and persist state
    - on web there is ongoing @michal's work to achieve the same [project](https://linear.app/comm/project/[tab-synchronization]-move-cryptostore-operations-to-shared-worker-d86b8dc7215b).
    
    This will allow us to:
    - use `olm` API from `/lib` (we can also expand it to `keyserver`)
    - move code like [peerToPeerMessageHandler](https://github.com/CommE2E/comm/blob/0f6b3d510c4141beda5aaf0fedd4f7b78ccdfa70/native/handlers/peer-to-peer-message-handler.js#L12) to `/lib`
    - completely move taking care of state/transactions/atomicity to different place and from business logic perspective only call methods
    
    Right now, on web it will cause some code duplication with [OlmSessionCreatorContext](https://github.com/CommE2E/comm/blob/0f6b3d510c4141beda5aaf0fedd4f7b78ccdfa70/web/account/account-hooks.js#L406), but after [ENG-6462](https://linear.app/comm/issue/ENG-6462/browser-tab-synchronization) this context won't have any state and we can remove it and just call methods.
    
    This also helps @michal to implement moving responsibility to worker - right now app still works with context and whene everything is ready we can just "flip-the-switch to use this API".
    
    This apporach was discussed with @michal, @tomek and @marcin.
    
    Depends on D11110
    
    Test Plan: Tested encrypt/decrypt methods on web with properly generated accounts/sessions.
    
    Reviewers: marcin, varun, tomek!, michal!
    
    Subscribers: ashoat, tomek, marcin, michal
    
    Differential Revision: https://phab.comm.dev/D11111

--- native/config.js
+++ native/config.js
@@ -5,6 +5,7 @@ import { Platform } from 'react-native';
 import { registerConfig } from 'lib/utils/config.js';
 
 import { resolveKeyserverSessionInvalidationUsingNativeCredentials } from './account/legacy-recover-keyserver-session.js';
+import { olmAPI } from './crypto/olm-api.js';
 import { persistConfig, codeVersion } from './redux/persist.js';
 
 registerConfig({
@@ -16,4 +17,5 @@ registerConfig({
     codeVersion,
     stateVersion: persistConfig.version,
   },
+  olmAPI,
 });
