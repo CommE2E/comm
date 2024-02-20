cce2bfa85260569541aad1449422e0e5d0a0f203
Atul Madhugiri <atulm@berkeley.edu>
2024-02-20 14:45:14 -0500

    [native] Reintroduce `createUpdateDBOpsForThreadStoreThreadInfos` for new `RawThreadInfo` encoding
    Summary:
    Discussed in D11057, but we want to "freeze" the previous implementation of `createUpdateDBOpsForThreadStoreThreadInfos` in place to support old migrations while re-introducing a new version that only handles `MinimallyEncoded -> MinimallyEncoded` migrations. We will only have `MinimallyEncoded -> MinimallyEncoded` migrations in the future, so IMO it doesn't make sense to clutter up `createUpdateDBOpsForThreadStoreThreadInfos` to be able to handle
    
    - LegacyRawThreadInfos => LegacyRawThreadInfos
    - LegacyRawThreadInfos => MinimallyEncodedRawThreadInfos
    - MinimallyEncodedRawThreadInfos => MinimallyEncodedRawThreadInfos
    
    It also lets us avoid having to go back and test some of the old migrations when we tweak `createUpdateDBOpsForThreadStoreThreadInfos`.
    
    ---
    
    Depends on D11057
    
    Test Plan: This gets consumed for the first time in the upcoming specialRoles migration where it will be tested implicitly.
    
    Reviewers: ginsu, tomek, kamil, michal
    
    Reviewed By: tomek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D11095

--- native/redux/client-db-utils.js
+++ native/redux/client-db-utils.js
@@ -1,11 +1,18 @@
 // @flow
 
+import _keyBy from 'lodash/fp/keyBy.js';
+
 import type { ClientDBMessageStoreOperation } from 'lib/ops/message-store-ops.js';
+import type { ClientDBThreadStoreOperation } from 'lib/ops/thread-store-ops.js';
 import type {
   RawMessageInfo,
   ClientDBMessageInfo,
   ClientDBThreadMessageInfo,
 } from 'lib/types/message-types.js';
+import type {
+  ClientDBThreadInfo,
+  RawThreadInfos,
+} from 'lib/types/thread-types.js';
 import {
   translateClientDBMessageInfoToRawMessageInfo,
   translateRawMessageInfoToClientDBMessageInfo,
@@ -13,7 +20,44 @@ import {
   translateThreadMessageInfoToClientDBThreadMessageInfo,
   type TranslatedThreadMessageInfos,
 } from 'lib/utils/message-ops-utils.js';
-import { entries } from 'lib/utils/objects.js';
+import { entries, values } from 'lib/utils/objects.js';
+import {
+  convertClientDBThreadInfoToRawThreadInfo,
+  convertRawThreadInfoToClientDBThreadInfo,
+} from 'lib/utils/thread-ops-utils.js';
+
+function createUpdateDBOpsForThreadStoreThreadInfos(
+  clientDBThreadInfos: $ReadOnlyArray<ClientDBThreadInfo>,
+  migrationFunc: RawThreadInfos => RawThreadInfos,
+): $ReadOnlyArray<ClientDBThreadStoreOperation> {
+  // 1. Translate `ClientDBThreadInfo`s to `RawThreadInfo`s.
+  const rawThreadInfos = clientDBThreadInfos.map(
+    convertClientDBThreadInfoToRawThreadInfo,
+  );
+
+  // 2. Convert `RawThreadInfo`s to a map of `threadID` => `threadInfo`.
+  const keyedRawThreadInfos = _keyBy('id')(rawThreadInfos);
+
+  // 3. Apply `migrationFunc` to `ThreadInfo`s.
+  const updatedKeyedRawThreadInfos = migrationFunc(keyedRawThreadInfos);
+
+  // 4. Convert the updated `RawThreadInfos` back into an array.
+  const updatedKeyedRawThreadInfosArray = values(updatedKeyedRawThreadInfos);
+
+  // 5. Translate `RawThreadInfo`s back to `ClientDBThreadInfo`s.
+  const updatedClientDBThreadInfos = updatedKeyedRawThreadInfosArray.map(
+    convertRawThreadInfoToClientDBThreadInfo,
+  );
+
+  // 6. Construct `replace` `ClientDBThreadStoreOperation`s.
+  const replaceThreadOperations = updatedClientDBThreadInfos.map(thread => ({
+    type: 'replace',
+    payload: thread,
+  }));
+
+  // 7. Prepend `replaceThreadOperations` with `remove_all` op and return.
+  return [{ type: 'remove_all' }, ...replaceThreadOperations];
+}
 
 function createUpdateDBOpsForMessageStoreMessages(
   clientDBMessageInfos: $ReadOnlyArray<ClientDBMessageInfo>,
@@ -69,6 +113,7 @@ function createUpdateDBOpsForMessageStoreThreads(
 }
 
 export {
+  createUpdateDBOpsForThreadStoreThreadInfos,
   createUpdateDBOpsForMessageStoreMessages,
   createUpdateDBOpsForMessageStoreThreads,
 };
