d63cd0cf9e5205792e4b431fe3691cd7799aa4d6
William Wang <williamwang749@gmail.com>
2024-02-23 13:21:30 -0500

    [terraform] Move blob scheduler code to aws_iam.tf
    Summary:
    This moves the scheduler role and policy to a shared location in aws_iam.tf for usage by other scheduled tasks.
    
    Test Plan:
    terraform fmt and plan
    
    Reviewers:
    varun
    
    Subscribers:

--- services/terraform/remote/aws_iam.tf
+++ services/terraform/remote/aws_iam.tf
@@ -319,3 +319,53 @@ resource "aws_opensearch_domain_policy" "opensearch_domain_access" {
   domain_name     = module.shared.opensearch_domain_identity.domain_name
   access_policies = data.aws_iam_policy_document.opensearch_domain_access.json
 }
+
+resource "aws_iam_role" "scheduler" {
+  name = "cron-scheduler-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = ["scheduler.amazonaws.com"]
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "scheduler" {
+  policy_arn = aws_iam_policy.scheduler.arn
+  role       = aws_iam_role.scheduler.name
+}
+
+resource "aws_iam_policy" "scheduler" {
+  name = "cron-scheduler-policy"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      # Allow scheduler to execute the task
+      {
+
+        Effect = "Allow",
+        Action = [
+          "ecs:RunTask"
+        ]
+        Resource = aws_ecs_task_definition.blob_cleanup.arn_without_revision
+      },
+      # Allow scheduler to set the IAM roles of the ECS task
+      {
+        Effect = "Allow",
+        Action = [
+          "iam:PassRole"
+        ]
+        Resource = [
+          aws_ecs_task_definition.blob_cleanup.execution_role_arn,
+          aws_ecs_task_definition.blob_cleanup.task_role_arn
+        ]
+      },
+    ]
+  })
+}
