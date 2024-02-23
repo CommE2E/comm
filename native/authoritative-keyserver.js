3f492de73aac6e1ff8ce136a328a5856ddb25858
Inka Sokołowska <inka.d.sokolowska@gmail.com>
2024-02-23 16:05:07 +0100

    [native] Fix hermes throwing on nonexistant files
    Summary: issue: https://linear.app/comm/issue/ENG-6929/sqlite-database-deletion-was-triggered-by-detecting-corrupted-database
    
    Test Plan:
    tested that if the file doesn't exist `authoritativeKeyserverID` is `256` and it is possible to logout and back in
    Tested that if the file exists and `authoritativeKeyserverID` is correctly set
    
    Checked that
    ```
    const {
        authoritativeKeyserverID: keyserverID,
        // $FlowExpectedError: file might not exist
      } = require('./facts/authoritative_keyserver.json');
    ```
    throws if the file doesn't exist, so `authoritativeKeyserverID` is not being set to `undefined`
    
    Reviewers: michal, tomek
    
    Reviewed By: tomek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D11157

--- native/authoritative-keyserver.js
+++ native/authoritative-keyserver.js
@@ -2,18 +2,13 @@
 
 import { ashoatKeyserverID } from 'lib/utils/validation-utils.js';
 
-function getAuthoritativeKeyserverID(): string {
-  try {
-    const {
-      authoritativeKeyserverID: keyserverID,
-      // $FlowExpectedError: file might not exist
-    } = require('./facts/authoritative_keyserver.json');
-    return keyserverID;
-  } catch {
-    return ashoatKeyserverID;
-  }
-}
-
-const authoritativeKeyserverID: string = getAuthoritativeKeyserverID();
+let authoritativeKeyserverID: string = ashoatKeyserverID;
+try {
+  const {
+    authoritativeKeyserverID: keyserverID,
+    // $FlowExpectedError: file might not exist
+  } = require('./facts/authoritative_keyserver.json');
+  authoritativeKeyserverID = keyserverID;
+} catch {}
 
 export { authoritativeKeyserverID };
