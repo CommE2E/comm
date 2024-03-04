08380a84539ad1bcf61df4c3b20af3a16939c54f
marcinwasowicz <marcinkwasowicz@gmail.com>
2024-03-04 11:16:22 +0100

    Implement notifications sessions storage on top of MMKV. Migrate existing notifs account to SQLite
    Summary:
    This differential deprecates old way of keeping notifications sessions and builds new one on top of MMKV. Now sessions are kept in MMKV under keys that are
    constructed from relevant keyserver id. Pickling key for sessions is also stored in MMKV. Additionally this differential implements SQLite migration that transfers
    existing notifs account and session from old storage to SQLite and MMKV respectively.
    
    Test Plan:
    1. Build the app before applying this diff and log in to create session.
    2. Apply this diff and buld the app again.
    3. Ensure the notifs are still working. This tests migration.
    4. Log out and log in and ensure notifs are stil working.
    
    Reviewers: tomek, bartek, kamil, michal
    
    Reviewed By: michal
    
    Subscribers: michal, inka, ashoat
    
    Differential Revision: https://phab.comm.dev/D11234

--- native/cpp/CommonCpp/Notifications/BackgroundDataStorage/NotificationsCryptoModule.h
+++ native/cpp/CommonCpp/Notifications/BackgroundDataStorage/NotificationsCryptoModule.h
@@ -7,59 +7,65 @@
 
 namespace comm {
 class NotificationsCryptoModule {
-  const static std::string secureStoreNotificationsAccountDataKey;
   const static std::string notificationsCryptoAccountID;
-  const static std::string keyserverHostedNotificationsID;
-  const static std::string initialEncryptedMessageContent;
 
-  static std::string getPicklingKey();
-  static void serializeAndFlushCryptoModule(
-      std::unique_ptr<crypto::CryptoModule> cryptoModule,
-      const std::string &path,
-      const std::string &picklingKey,
-      const std::string &callingProcessName);
+  // Used for migration of legacy notifications sessions
   static std::unique_ptr<crypto::CryptoModule> deserializeCryptoModule(
       const std::string &path,
       const std::string &picklingKey);
-  static void callCryptoModule(
-      std::function<void(
-          const std::unique_ptr<crypto::CryptoModule> &cryptoModule)> caller,
-      const std::string &callingProcessName);
+  static std::string
+  getKeyserverNotificationsSessionKey(const std::string &keyserverID);
+  static std::string serializeNotificationsSession(
+      std::shared_ptr<crypto::Session> session,
+      std::string picklingKey);
+  static std::pair<std::unique_ptr<crypto::Session>, std::string>
+  deserializeNotificationsSession(const std::string &serializedSession);
+  static void persistNotificationsSessionInternal(
+      const std::string &keyserverID,
+      const std::string &picklingKey,
+      std::shared_ptr<crypto::Session> session);
+  static std::pair<std::unique_ptr<crypto::Session>, std::string>
+  fetchNotificationsSession(const std::string &keyserverID);
 
 public:
+  const static std::string initialEncryptedMessageContent;
   const static int olmEncryptedTypeMessage;
-  static void
-  initializeNotificationsCryptoAccount(const std::string &callingProcessName);
+
+  static std::unique_ptr<crypto::CryptoModule>
+  migrateLegacyNotificationsCryptoModule();
   static void clearSensitiveData();
-  static crypto::EncryptedData initializeNotificationsSession(
-      const std::string &identityKeys,
-      const std::string &prekey,
-      const std::string &prekeySignature,
-      const std::string &oneTimeKeys,
-      const std::string &callingProcessName);
-  static bool
-  isNotificationsSessionInitialized(const std::string &callingProcessName);
+  static void persistNotificationsSession(
+      const std::string &keyserverID,
+      std::shared_ptr<crypto::Session> keyserverNotificationsSession);
+  static bool isNotificationsSessionInitialized(const std::string &keyserverID);
   static std::string decrypt(
+      const std::string &keyserverID,
       const std::string &data,
-      const size_t messageType,
-      const std::string &callingProcessName);
+      const size_t messageType);
 
   class StatefulDecryptResult {
     StatefulDecryptResult(
-        std::unique_ptr<crypto::CryptoModule> cryptoModule,
+        std::unique_ptr<crypto::Session> session,
+        std::string keyserverID,
+        std::string picklingKey,
         std::string decryptedData);
-    std::unique_ptr<crypto::CryptoModule> cryptoModuleState;
+    std::unique_ptr<crypto::Session> sessionState;
+    std::string keyserverID;
+    std::string picklingKey;
     std::string decryptedData;
     friend NotificationsCryptoModule;
 
   public:
     std::string getDecryptedData();
+    std::string getKeyserverID();
+    std::string getPicklingKey();
   };
 
-  static std::unique_ptr<StatefulDecryptResult>
-  statefulDecrypt(const std::string &data, const size_t messageType);
-  static void flushState(
-      std::unique_ptr<StatefulDecryptResult> statefulDecryptResult,
-      const std::string &callingProcessName);
+  static std::unique_ptr<StatefulDecryptResult> statefulDecrypt(
+      const std::string &keyserverID,
+      const std::string &data,
+      const size_t messageType);
+  static void
+  flushState(std::unique_ptr<StatefulDecryptResult> statefulDecryptResult);
 };
 } // namespace comm
