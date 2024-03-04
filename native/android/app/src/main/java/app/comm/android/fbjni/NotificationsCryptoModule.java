fd5931c1f5959f2fc202f1e64cb246019b4ea477
marcinwasowicz <marcinkwasowicz@gmail.com>
2024-03-04 11:16:22 +0100

    Implement notifications sessions storage on top of MMKV. Migrate existing notifs account to SQLite Differential Revision: https://phab.comm.dev/D11234

--- native/android/app/src/main/java/app/comm/android/fbjni/NotificationsCryptoModule.java
+++ native/android/app/src/main/java/app/comm/android/fbjni/NotificationsCryptoModule.java
@@ -3,5 +3,5 @@ package app.comm.android.fbjni;
 public class NotificationsCryptoModule {
   public static native int olmEncryptedTypeMessage();
   public static native String
-  decrypt(String data, int messageType, String callingProcessName);
+  decrypt(String keyserverID, String data, int messageType);
 }
