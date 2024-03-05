646bd76b3aaa160831a42f6a765f950352d666c5
Inka Sokołowska <inka.d.sokolowska@gmail.com>
2024-03-05 17:37:32 +0100

    [scripts] Add scrips for setting up a new authoritative keyserver
    Summary:
    issues: [[https://linear.app/comm/issue/ENG-6613/create-new-db | ENG-6613]] and [[https://linear.app/comm/issue/ENG-6612/move-old-db | ENG-6612]]
    These scripts
    1. check if `usingIdentityCredentials` flag is set to `true` in `user_credentials.json`
    2. if so check if metadata table is correctly filled in
    3. if either of these is not fulfilled asks the dev if they want to set up a new authoritative keyserver
    4. if they agree, the db is moved to a backup and the dev is prompted for admin credentials (username and password)
    
    I found using bash for operations on the db, and using js for writing files to be the most readable. I also had the js code already written as part of my February goal. But if this is too messy I can also write everything in bash. Please let me know what you think
    
    Test Plan:
    ran `$PRJ_ROOT/scripts/set-up-authoritative-keyserver.sh` from `dev-shell.nix`
    ran nix and checked that if the dev aborts the process they can still use their old setup
    ran from nix, and checked that if the dev agrees to proceed and gives correct credentials, the db is moved and `user_credentials.json` is filled in. It is also possible to run the keyserver and the db is correctly set up
    ran from nix, and checked that if the dev agrees to proceed but gives incorrect credentials, then the keyserver fails. Then, the next time nix is run, the script correctly recognises that something is wrong, and asks again if the dev wants to set up the keyserver
    (some logs explaining that nix has to be run again will be added in [[https://linear.app/comm/issue/ENG-6618/make-yarn-dev-print-an-error-informing-the-user-that-they-need-to | ENG-6618]])
    
    Reviewers:
    tomek, michal, ashoat
    Subscribers:

---
+++ scripts/set-up-authoritative-keyserver.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+PRJ_ROOT="$(git rev-parse --show-toplevel)"
+
+user_credentials_file="$PRJ_ROOT/keyserver/secrets/user_credentials.json"
+# user_credentials_file="keyserver/secrets/user_credentials.json"
+
+set_up_or_abort() {
+  read -r -p "Do you want to set up a new authoritative keyserver? (y/N) " user_input
+    
+  if [[ $user_input != "Y" && $user_input != "y" ]]; then
+      exit 1
+  fi
+
+  timestamp=$(date +%s)
+  db_version_name="comm_bakup$timestamp"
+  echo "backup db name: $db_version_name"
+
+  #Create backup db and move all tables from comm
+  mysql -u "$USER" -Bse "CREATE DATABASE $db_version_name;GRANT ALL ON $db_version_name"'.*'" TO comm@localhost;"
+
+  for table in $(mysql -u "$USER" -Bse "USE comm;SHOW tables FROM comm;"); do 
+    mysql -u "$USER" -Bse "USE comm;RENAME TABLE comm.$table TO $db_version_name.$table;"; 
+  done;
+
+  node "$PRJ_ROOT"/scripts/set-user-credentials.js "$PRJ_ROOT"
+}
+
+if grep -q '"usingIdentityCredentials":.*true' "$user_credentials_file"; then    
+    if (mysql -u "$USER" -Bse "USE comm; SELECT * FROM metadata" 2>/dev/null | grep "db_version"); then
+      echo "'usingIdentityCredentials' is set to true in user_credentials.json, and the db is set up."
+    else
+      echo -e "'usingIdentityCredentials' is set to true, but the database is not set up.\n" \
+      "This was likely caused by the keyserver failing to login with the provided credentials,"\
+      "or the keyserver never being run"
+      set_up_or_abort
+    fi
+else
+    echo "'usingIdentityCredentials' is missing or set to false in user_credentials.json."
+    set_up_or_abort
+fi
