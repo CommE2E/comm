062fc055d8873a3c82a1e208a5862d868b8e09db
marcinwasowicz <marcinkwasowicz@gmail.com>
2024-03-14 14:16:54 +0100

    Implement multiple keyserver unread count on web and desktop on top of IndexedDB
    Summary: This differential implements unread count handling from multiple keyservers
    
    Test Plan:
    1. Ensure that unread count is working properly.
    2. Toggle developer tools and ensure that there is relevant entry in IndexDB the corresponds to unread count for the keyserver that we logged in to.
    
    Reviewers: michal
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D11328

--- web/push-notif/badge-handler.react.js
+++ web/push-notif/badge-handler.react.js
@@ -2,35 +2,59 @@
 
 import * as React from 'react';
 
-import { connectionSelector } from 'lib/selectors/keyserver-selectors.js';
-import { unreadCount } from 'lib/selectors/thread-selectors.js';
-import type { ConnectionInfo } from 'lib/types/socket-types.js';
+import { allConnectionInfosSelector } from 'lib/selectors/keyserver-selectors.js';
+import { allUnreadCounts } from 'lib/selectors/thread-selectors.js';
 
-import { authoritativeKeyserverID } from '../authoritative-keyserver.js';
+import {
+  updateNotifsUnreadCountStorage,
+  queryNotifsUnreadCountStorage,
+} from './notif-crypto-utils.js';
 import electron from '../electron.js';
 import { useSelector } from '../redux/redux-utils.js';
 import getTitle from '../title/get-title.js';
 
 function useBadgeHandler() {
-  const connection = useSelector(connectionSelector(authoritativeKeyserverID));
-  const prevConnection = React.useRef<?ConnectionInfo>();
-
-  const boundUnreadCount = useSelector(unreadCount);
-  const prevUnreadCount = React.useRef(boundUnreadCount);
+  const connection = useSelector(allConnectionInfosSelector);
+  const unreadCount = useSelector(allUnreadCounts);
 
   React.useEffect(() => {
-    if (
-      connection?.status === 'connected' &&
-      (prevConnection.current?.status !== 'connected' ||
-        boundUnreadCount !== prevUnreadCount.current)
-    ) {
-      document.title = getTitle(boundUnreadCount);
-      electron?.setBadge(boundUnreadCount === 0 ? null : boundUnreadCount);
-    }
+    void (async () => {
+      const unreadCountUpdates: {
+        [keyserverID: string]: number,
+      } = {};
+      const unreadCountQueries: Array<string> = [];
 
-    prevConnection.current = connection;
-    prevUnreadCount.current = boundUnreadCount;
-  }, [boundUnreadCount, connection]);
+      for (const keyserverID in unreadCount) {
+        if (connection[keyserverID]?.status !== 'connected') {
+          unreadCountQueries.push(keyserverID);
+          continue;
+        }
+        unreadCountUpdates[keyserverID] = unreadCount[keyserverID];
+      }
+
+      let queriedUnreadCounts: { +[keyserverID: string]: ?number } = {};
+      [queriedUnreadCounts] = await Promise.all([
+        queryNotifsUnreadCountStorage(unreadCountQueries),
+        updateNotifsUnreadCountStorage(unreadCountUpdates),
+      ]);
+
+      let totalUnreadCount = 0;
+      for (const keyserverID in unreadCountUpdates) {
+        totalUnreadCount += unreadCountUpdates[keyserverID];
+      }
+
+      for (const keyserverID in queriedUnreadCounts) {
+        if (!queriedUnreadCounts[keyserverID]) {
+          totalUnreadCount += unreadCount[keyserverID];
+          continue;
+        }
+        totalUnreadCount += queriedUnreadCounts[keyserverID];
+      }
+
+      document.title = getTitle(totalUnreadCount);
+      electron?.setBadge(totalUnreadCount === 0 ? null : totalUnreadCount);
+    })();
+  }, [unreadCount, connection]);
 }
 
 export default useBadgeHandler;
