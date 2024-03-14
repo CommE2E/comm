2758cdaa062e36299a82028ff2038c1c3b83b4c4
Michal Gniadek <michal.gniadek@swmansion.com>
2024-03-14 16:43:55 +0100

    [web] Olm session creation between own devices
    Summary:
    [ENG-7221 : Tunnelbroker session creation on web](https://linear.app/comm/issue/ENG-7221)
    
    Now that we have a proper olm session persistance on web we can implement testing screens for peer to peer message sending, and creating sessions between own devices.
    
    `getContentSigningKey` and `createOlmSessionsWithOwnDevices` functions have been moved from native to lib and updated to use the cross-platform APIs.
    
    Depends on D11340
    
    Test Plan: Created sessions from web to native and tried sending messages both ways. Repeated with native creating the session to web. In both cases the messages where successfully ecnrypted/decrypted.
    
    Reviewers: kamil, tomek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D11341

--- native/utils/crypto-utils.js
+++ native/utils/crypto-utils.js
@@ -1,18 +1,9 @@
 // @flow
 
-import { type ClientMessageToDevice } from 'lib/tunnelbroker/tunnelbroker-context.js';
-import type {
-  IdentityKeysBlob,
-  OLMIdentityKeys,
-} from 'lib/types/crypto-types.js';
-import type { OutboundKeyInfoResponse } from 'lib/types/identity-service-types';
+import type { OLMIdentityKeys } from 'lib/types/crypto-types.js';
 import type { OlmSessionInitializationInfo } from 'lib/types/request-types.js';
-import {
-  type OutboundSessionCreation,
-  peerToPeerMessageTypes,
-} from 'lib/types/tunnelbroker/peer-to-peer-message-types.js';
 
-import { commCoreModule, commRustModule } from '../native-modules.js';
+import { commCoreModule } from '../native-modules.js';
 
 function nativeNotificationsSessionCreator(
   notificationsIdentityKeys: OLMIdentityKeys,
@@ -28,14 +19,6 @@ function nativeNotificationsSessionCreator(
     oneTimeKey,
     keyserverID,
   );
-}
-
-async function getContentSigningKey(): Promise<string> {
-  await commCoreModule.initializeCryptoAccount();
-  const {
-    primaryIdentityPublicKeys: { ed25519 },
-  } = await commCoreModule.getUserPublicKey();
-  return ed25519;
 }
 
 function nativeOutboundContentSessionCreator(
@@ -58,75 +41,7 @@ function nativeOutboundContentSessionCreator(
   );
 }
 
-async function createOlmSessionsWithOwnDevices(
-  sendMessage: (message: ClientMessageToDevice) => Promise<void>,
-): Promise<void> {
-  const authMetadata = await commCoreModule.getCommServicesAuthMetadata();
-  const { userID, deviceID, accessToken } = authMetadata;
-  if (!userID || !deviceID || !accessToken) {
-    throw new Error('CommServicesAuthMetadata is missing');
-  }
-
-  await commCoreModule.initializeCryptoAccount();
-  const keysResponse = await commRustModule.getOutboundKeysForUser(
-    userID,
-    deviceID,
-    accessToken,
-    userID,
-  );
-
-  const outboundKeys: OutboundKeyInfoResponse[] = JSON.parse(keysResponse);
-
-  for (const deviceKeys: OutboundKeyInfoResponse of outboundKeys) {
-    const keysPayload: IdentityKeysBlob = JSON.parse(deviceKeys.payload);
-
-    if (keysPayload.primaryIdentityPublicKeys.ed25519 === deviceID) {
-      continue;
-    }
-    const recipientDeviceID = keysPayload.primaryIdentityPublicKeys.ed25519;
-    if (!deviceKeys.oneTimeContentPrekey) {
-      console.log(`One-time key is missing for device ${recipientDeviceID}`);
-      continue;
-    }
-    try {
-      const encryptedContent = await nativeOutboundContentSessionCreator(
-        keysPayload.primaryIdentityPublicKeys,
-        {
-          prekey: deviceKeys.contentPrekey,
-          prekeySignature: deviceKeys.contentPrekeySignature,
-          oneTimeKey: deviceKeys.oneTimeContentPrekey,
-        },
-        recipientDeviceID,
-      );
-
-      const sessionCreationMessage: OutboundSessionCreation = {
-        type: peerToPeerMessageTypes.OUTBOUND_SESSION_CREATION,
-        senderInfo: {
-          userID,
-          deviceID,
-        },
-        encryptedContent,
-      };
-
-      await sendMessage({
-        deviceID: recipientDeviceID,
-        payload: JSON.stringify(sessionCreationMessage),
-      });
-      console.log(
-        `Request to create a session with device ${recipientDeviceID} sent.`,
-      );
-    } catch (e) {
-      console.log(
-        'Error creating outbound session with ' +
-          `device ${recipientDeviceID}: ${e.message}`,
-      );
-    }
-  }
-}
-
 export {
-  getContentSigningKey,
   nativeNotificationsSessionCreator,
-  createOlmSessionsWithOwnDevices,
   nativeOutboundContentSessionCreator,
 };
