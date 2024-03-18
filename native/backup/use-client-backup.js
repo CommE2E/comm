c0e96428f4c24fe9814e8c60a25642fe380d8c7c
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2024-03-18 12:23:55 +0100

    [lib][native] Call backup restore protocol after QR auth
    Summary:
    - On primary device, replaced stub values with actual backup keys
    - On secondary device, added a function to call backup restore protocol when received backup keys from primary device
    
    Depends on D11345
    
    Test Plan:
    - Verified that backup keys are retrieved and sent to the secondary device
    - Verified that backup data is downloaded on the secondary device
    
    Reviewers: michal, kamil
    
    Reviewed By: kamil
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D11346

--- native/backup/use-client-backup.js
+++ native/backup/use-client-backup.js
@@ -73,4 +73,4 @@ function useClientBackup(): ClientBackup {
   return { uploadBackupProtocol, restoreBackupProtocol };
 }
 
-export { useClientBackup };
+export { getBackupSecret, useClientBackup };
