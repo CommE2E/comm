e45f601398e1a30d159de57abb61635cf5865cd3
Tomasz Palys <tomasz.palys@swmansion.com>
2024-03-20 21:53:04 +0100

    [native][web] Remove processing the ops from reducers
    Summary: We no longer need to process the ops in reducers because the new handler handles that.
    
    https://linear.app/comm/issue/ENG-7099/stop-processing-the-ops-in-master-reducers
    
    Depends on D11367
    
    Test Plan: Check if the changes are persisted in the db - by e.g. checking if the drafts remain present after restarting the app.
    
    Reviewers: kamil, inka, will

--- native/redux/redux-setup.js
+++ native/redux/redux-setup.js
@@ -48,7 +48,6 @@ import { defaultState } from './default-state.js';
 import { remoteReduxDevServerConfig } from './dev-tools.js';
 import { persistConfig, setPersistor } from './persist.js';
 import { onStateDifference } from './redux-debug-utils.js';
-import { processDBStoreOperations } from './redux-utils.js';
 import { nonUserSpecificFieldsNative } from './state-types.js';
 import type { AppState } from './state-types.js';
 import { getGlobalNavContext } from '../navigation/icky-global.js';
@@ -313,8 +312,6 @@ function reducer(state: AppState = defaultState, inputAction: Action) {
     ...state,
     dbOpsStore: queueDBOps(state.dbOpsStore, action.actionID, ops),
   };
-
-  void processDBStoreOperations(ops);
 
   return state;
 }
