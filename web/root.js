dd5a59afcc150bf44e322e7e90422ef3891fcb92
Tomasz Palys <tomasz.palys@swmansion.com>
2024-03-20 20:49:07 +0100

    [lib] Handle processing of the ops
    Summary:
    We're taking the ops from the front of the queue, process it, and dispatch an action that removes it after we're done.
    
    https://linear.app/comm/issue/ENG-7102/reduce-the-action-this-should-filter-the-ops-out-from-the-array
    
    Depends on D11366
    
    Test Plan: Added a sleep to the handler after calling `processDBStoreOperations` and observed in Redux debugger that the ops were processed one-by-one and that the queue length was decreasing.
    
    Reviewers: kamil, inka, will
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D11367

--- web/root.js
+++ web/root.js
@@ -13,7 +13,6 @@ import IntegrityHandler from 'lib/components/integrity-handler.react.js';
 import KeyserverConnectionsHandler from 'lib/components/keyserver-connections-handler.js';
 import PrekeysHandler from 'lib/components/prekeys-handler.react.js';
 import ReportHandler from 'lib/components/report-handler.react.js';
-import { DBOpsHandler } from 'lib/handlers/db-ops-handler.react.js';
 import { CallKeyserverEndpointProvider } from 'lib/keyserver-conn/call-keyserver-endpoint-provider.react.js';
 import { reduxLoggerMiddleware } from 'lib/utils/action-logger.js';
 
@@ -22,6 +21,7 @@ import {
   OlmSessionCreatorProvider,
 } from './account/account-hooks.js';
 import App from './app.react.js';
+import { DBOpsHandler } from './components/db-ops-handler.react.js';
 import ErrorBoundary from './error-boundary.react.js';
 import IdentityServiceContextProvider from './grpc/identity-service-context-provider.react.js';
 import { defaultWebState } from './redux/default-state.js';
