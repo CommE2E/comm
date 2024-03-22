8e31ee0ab76c87a0b427c756d12a773cd623b641
Ginsu Eddy <ginsu.eddy@gmail.com>
2024-03-22 04:01:20 -0400

    introduce farcast account component

--- native/account/registration/connect-farcaster.react.js
+++ native/account/registration/connect-farcaster.react.js
@@ -1,25 +1,17 @@
 // @flow
 
-import invariant from 'invariant';
 import * as React from 'react';
-import { View, Text, Linking } from 'react-native';
-import WebView from 'react-native-webview';
+import { View } from 'react-native';
 
-import { FIDContext } from 'lib/components/fid-provider.react.js';
-
-import RegistrationButtonContainer from './registration-button-container.react.js';
 import RegistrationButton from './registration-button.react.js';
 import RegistrationContainer from './registration-container.react.js';
-import RegistrationContentContainer from './registration-content-container.react.js';
 import type { RegistrationNavigationProp } from './registration-navigator.react.js';
 import type { CoolOrNerdMode } from './registration-types.js';
+import FarcasterAccount from '../../components/farcast-account.react.js';
 import {
   type NavigationRoute,
   UsernameSelectionRouteName,
 } from '../../navigation/route-names.js';
-import { useStyles } from '../../themes/colors.js';
-import { defaultLandingURLPrefix } from '../../utils/url-utils.js';
-import FarcasterLogo from '../../vectors/farcaster-logo.react.js';
 
 export type ConnectFarcasterParams = {
   +userSelections: {
@@ -28,41 +20,16 @@ export type ConnectFarcasterParams = {
   },
 };
 
-type FarcasterWebViewMessage =
-  | {
-      +type: 'farcaster_url',
-      +url: string,
-    }
-  | {
-      +type: 'farcaster_data',
-      +fid: string,
-    };
-
-type WebViewMessageEvent = {
-  +nativeEvent: {
-    +data: string,
-    ...
-  },
-  ...
-};
-
-type WebViewState = 'closed' | 'opening';
-
 type Props = {
   +navigation: RegistrationNavigationProp<'ConnectFarcaster'>,
   +route: NavigationRoute<'ConnectFarcaster'>,
 };
-
-const commConnectFarcasterURL = `${defaultLandingURLPrefix}/connect-farcaster`;
 
 function ConnectFarcaster(prop: Props): React.Node {
   const { navigation, route } = prop;
 
   const { navigate } = navigation;
   const { params } = route;
-
-  const [webViewState, setWebViewState] =
-    React.useState<WebViewState>('closed');
 
   const goToNextStep = React.useCallback(() => {
     navigate<'UsernameSelection'>({
@@ -71,108 +38,24 @@ function ConnectFarcaster(prop: Props): React.Node {
     });
   }, [navigate, params]);
 
-  const setFID = React.useContext(FIDContext)?.setFID;
-  invariant(setFID, 'FIDContext is missing');
-
-  const handleMessage = React.useCallback(
-    (event: WebViewMessageEvent) => {
-      const data: FarcasterWebViewMessage = JSON.parse(event.nativeEvent.data);
-
-      if (data.type === 'farcaster_url') {
-        void Linking.openURL(data.url);
-      } else if (data.type === 'farcaster_data') {
-        setFID(data.fid);
-        goToNextStep();
-      }
-    },
-    [setFID, goToNextStep],
-  );
-
-  const styles = useStyles(unboundStyles);
-
-  const webView = React.useMemo(() => {
-    if (webViewState === 'closed') {
-      return null;
-    }
-
-    return (
-      <View style={styles.webViewContainer}>
-        <WebView
-          source={{ uri: commConnectFarcasterURL }}
-          style={styles.webView}
-          onMessage={handleMessage}
-          incognito={true}
-          originWhitelist={['*']}
-        />
-      </View>
-    );
-  }, [handleMessage, webViewState, styles.webViewContainer, styles.webView]);
-
-  const onPressConnectFarcaster = React.useCallback(() => {
-    setWebViewState('opening');
-  }, []);
-
-  const connectButtonVariant =
-    webViewState === 'opening' ? 'loading' : 'enabled';
   return (
     <RegistrationContainer>
-      <RegistrationContentContainer style={styles.scrollViewContentContainer}>
-        <Text style={styles.header}>
-          Do you want to connect your Farcaster account?
-        </Text>
-        <Text style={styles.body}>
-          Connecting your Farcaster account lets you see your mutual follows on
-          Comm. We&rsquo;ll also surface communities based on your Farcaster
-          channels.
-        </Text>
-        <View style={styles.farcasterLogoContainer}>
-          <FarcasterLogo />
-        </View>
-        {webView}
-      </RegistrationContentContainer>
-      <RegistrationButtonContainer>
-        <RegistrationButton
-          onPress={onPressConnectFarcaster}
-          label="Connect Farcaster account"
-          variant={connectButtonVariant}
-        />
+      <FarcasterAccount onSuccess={goToNextStep} />
+      <View style={styles.secondaryButtonContainer}>
         <RegistrationButton
           onPress={goToNextStep}
           label="Do not connect"
           variant="outline"
         />
-      </RegistrationButtonContainer>
+      </View>
     </RegistrationContainer>
   );
 }
 
-const unboundStyles = {
-  scrollViewContentContainer: {
-    flexGrow: 1,
-  },
-  header: {
-    fontSize: 24,
-    color: 'panelForegroundLabel',
-    paddingBottom: 16,
-  },
-  body: {
-    fontFamily: 'Arial',
-    fontSize: 15,
-    lineHeight: 20,
-    color: 'panelForegroundSecondaryLabel',
-    paddingBottom: 16,
-  },
-  farcasterLogoContainer: {
-    flexGrow: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  webViewContainer: {
-    height: 0,
-    overflow: 'hidden',
-  },
-  webView: {
-    height: 0,
+const styles = {
+  secondaryButtonContainer: {
+    marginHorizontal: 16,
+    marginBottom: 8,
   },
 };
 
---
+++ native/components/farcast-account.react.js
@@ -0,0 +1,157 @@
+// @flow
+
+import invariant from 'invariant';
+import * as React from 'react';
+import { View, Text, Linking } from 'react-native';
+import WebView from 'react-native-webview';
+
+import { FIDContext } from 'lib/components/fid-provider.react.js';
+
+import RegistrationButton from '../account/registration/registration-button.react.js';
+import RegistrationContentContainer from '../account/registration/registration-content-container.react.js';
+import { useStyles } from '../themes/colors.js';
+import { defaultLandingURLPrefix } from '../utils/url-utils.js';
+import FarcasterLogo from '../vectors/farcaster-logo.react.js';
+
+type FarcasterWebViewMessage =
+  | {
+      +type: 'farcaster_url',
+      +url: string,
+    }
+  | {
+      +type: 'farcaster_data',
+      +fid: string,
+    };
+
+type WebViewMessageEvent = {
+  +nativeEvent: {
+    +data: string,
+    ...
+  },
+  ...
+};
+
+type WebViewState = 'closed' | 'opening';
+
+const commConnectFarcasterURL = `${defaultLandingURLPrefix}/connect-farcaster`;
+
+type Props = {
+  +onSuccess: () => mixed,
+};
+
+function FarcastAccount(props: Props): React.Node {
+  const { onSuccess } = props;
+
+  const setFID = React.useContext(FIDContext)?.setFID;
+  invariant(setFID, 'FIDContext is missing');
+
+  const styles = useStyles(unboundStyles);
+
+  const [webViewState, setWebViewState] =
+    React.useState<WebViewState>('closed');
+
+  const onPressConnectFarcaster = React.useCallback(() => {
+    setWebViewState('opening');
+  }, []);
+
+  const handleMessage = React.useCallback(
+    (event: WebViewMessageEvent) => {
+      const data: FarcasterWebViewMessage = JSON.parse(event.nativeEvent.data);
+
+      if (data.type === 'farcaster_url') {
+        void Linking.openURL(data.url);
+      } else if (data.type === 'farcaster_data') {
+        setFID(data.fid.toString());
+        onSuccess();
+      }
+    },
+    [onSuccess, setFID],
+  );
+
+  const webView = React.useMemo(() => {
+    if (webViewState === 'closed') {
+      return null;
+    }
+
+    return (
+      <View style={styles.webViewContainer}>
+        <WebView
+          source={{ uri: commConnectFarcasterURL }}
+          style={styles.webView}
+          onMessage={handleMessage}
+          incognito={true}
+          originWhitelist={['*']}
+        />
+      </View>
+    );
+  }, [handleMessage, webViewState, styles.webViewContainer, styles.webView]);
+
+  const connectButtonVariant =
+    webViewState === 'opening' ? 'loading' : 'enabled';
+
+  return (
+    <View style={styles.container}>
+      <RegistrationContentContainer style={styles.scrollViewContentContainer}>
+        <Text style={styles.header}>
+          Do you want to connect your Farcaster account?
+        </Text>
+        <Text style={styles.body}>
+          Connecting your Farcaster account lets you see your mutual follows on
+          Comm. We&rsquo;ll also surface communities based on your Farcaster
+          channels.
+        </Text>
+        <View style={styles.farcasterLogoContainer}>
+          <FarcasterLogo />
+        </View>
+        {webView}
+      </RegistrationContentContainer>
+      <View style={styles.connectButtonContainer}>
+        <RegistrationButton
+          onPress={onPressConnectFarcaster}
+          label="Connect Farcaster account"
+          variant={connectButtonVariant}
+        />
+      </View>
+    </View>
+  );
+}
+
+const unboundStyles = {
+  container: {
+    flex: 1,
+    backgroundColor: 'panelBackground',
+    justifyContent: 'space-between',
+  },
+  scrollViewContentContainer: {
+    flexGrow: 1,
+  },
+  header: {
+    fontSize: 24,
+    color: 'panelForegroundLabel',
+    paddingBottom: 16,
+  },
+  body: {
+    fontFamily: 'Arial',
+    fontSize: 15,
+    lineHeight: 20,
+    color: 'panelForegroundSecondaryLabel',
+    paddingBottom: 16,
+  },
+  farcasterLogoContainer: {
+    flexGrow: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  webViewContainer: {
+    height: 0,
+    overflow: 'hidden',
+  },
+  webView: {
+    height: 0,
+  },
+  connectButtonContainer: {
+    marginHorizontal: 16,
+  },
+};
+
+export default FarcastAccount;
