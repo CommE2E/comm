1b161120eacf24cd7d25870eb1eec93a5b6c956e
Ashoat Tevosyan <ashoat@comm.app>
2024-03-21 22:39:40 -0400

    [native] Make ConnectFarcaster look nice

--- native/account/registration/connect-farcaster.react.js
+++ native/account/registration/connect-farcaster.react.js
@@ -1,8 +1,11 @@
 // @flow
 
+import invariant from 'invariant';
 import * as React from 'react';
 import { View, Text, Linking } from 'react-native';
 import WebView from 'react-native-webview';
+
+import { FIDContext } from 'lib/components/fid-provider.react.js';
 
 import RegistrationButtonContainer from './registration-button-container.react.js';
 import RegistrationButton from './registration-button.react.js';
@@ -13,7 +16,9 @@ import {
   type NavigationRoute,
   UsernameSelectionRouteName,
 } from '../../navigation/route-names.js';
+import { useStyles } from '../../themes/colors.js';
 import { defaultLandingURLPrefix } from '../../utils/url-utils.js';
+import FarcasterLogo from '../../vectors/farcaster-logo.react.js';
 
 type FarcasterWebViewMessage =
   | {
@@ -52,29 +57,33 @@ function ConnectFarcaster(prop: Props): React.Node {
   const [webViewState, setWebViewState] =
     React.useState<WebViewState>('closed');
 
-  const [farcasterAccountFID, setFarcasterAccountFID] =
-    React.useState<?string>(null);
-
-  const onSkip = React.useCallback(() => {
+  const goToNextStep = React.useCallback(() => {
     navigate<'UsernameSelection'>({
       name: UsernameSelectionRouteName,
       params,
     });
   }, [navigate, params]);
 
-  const handleMessage = React.useCallback((event: WebViewMessageEvent) => {
-    console.log('handing message from webview');
-    const data: FarcasterWebViewMessage = JSON.parse(event.nativeEvent.data);
+  const setFID = React.useContext(FIDContext)?.setFID;
+  invariant(setFID, 'FIDContext is missing');
 
-    if (data.type === 'farcaster_url') {
-      console.log('url', data.url);
-      void Linking.openURL(data.url);
-    } else if (data.type === 'farcaster_data') {
-      setFarcasterAccountFID(data.fid);
-    }
-  }, []);
+  const handleMessage = React.useCallback(
+    (event: WebViewMessageEvent) => {
+      const data: FarcasterWebViewMessage = JSON.parse(event.nativeEvent.data);
 
-  const webview = React.useMemo(() => {
+      if (data.type === 'farcaster_url') {
+        void Linking.openURL(data.url);
+      } else if (data.type === 'farcaster_data') {
+        setFID(data.fid);
+        goToNextStep();
+      }
+    },
+    [setFID, goToNextStep],
+  );
+
+  const styles = useStyles(unboundStyles);
+
+  const webView = React.useMemo(() => {
     if (webViewState === 'closed') {
       return null;
     }
@@ -83,73 +92,80 @@ function ConnectFarcaster(prop: Props): React.Node {
       <View style={styles.webViewContainer}>
         <WebView
           source={{ uri: commConnectFarcasterURL }}
-          style={styles.webview}
+          style={styles.webView}
           onMessage={handleMessage}
           incognito={true}
           originWhitelist={['*']}
         />
       </View>
     );
-  }, [handleMessage, webViewState]);
+  }, [handleMessage, webViewState, styles.webViewContainer, styles.webView]);
 
-  const farcasterAccountDetails = React.useMemo(() => {
-    if (!farcasterAccountFID) {
-      return null;
-    }
-    return (
-      <View>
-        <Text style={styles.text}>Farcaster fid: {farcasterAccountFID}</Text>
-      </View>
-    );
-  }, [farcasterAccountFID]);
+  const onPressConnectFarcaster = React.useCallback(() => {
+    setWebViewState('opening');
+  }, []);
 
-  const buttons = React.useMemo(() => {
-    if (farcasterAccountFID) {
-      return null;
-    }
-
-    return (
+  const connectButtonVariant =
+    webViewState === 'opening' ? 'loading' : 'enabled';
+  return (
+    <RegistrationContainer>
+      <RegistrationContentContainer style={styles.scrollViewContentContainer}>
+        <Text style={styles.header}>
+          Do you want to connect your Farcaster account?
+        </Text>
+        <Text style={styles.body}>
+          Connecting your Farcaster account lets you see your mutual follows on
+          Comm. We&rsquo;ll also surface communities based on your Farcaster
+          channels.
+        </Text>
+        <View style={styles.farcasterLogoContainer}>
+          <FarcasterLogo />
+        </View>
+        {webView}
+      </RegistrationContentContainer>
       <RegistrationButtonContainer>
         <RegistrationButton
-          onPress={() => {
-            setWebViewState('opening');
-          }}
+          onPress={onPressConnectFarcaster}
           label="Connect Farcaster account"
-          variant="enabled"
+          variant={connectButtonVariant}
         />
         <RegistrationButton
-          onPress={onSkip}
+          onPress={goToNextStep}
           label="Do not connect"
           variant="outline"
         />
       </RegistrationButtonContainer>
-    );
-  }, [farcasterAccountFID, onSkip]);
-
-  return (
-    <>
-      <RegistrationContainer>
-        <RegistrationContentContainer>
-          <Text style={styles.text}>Connect to Farcaster?</Text>
-          {farcasterAccountDetails}
-          {buttons}
-        </RegistrationContentContainer>
-      </RegistrationContainer>
-      {webview}
-    </>
+    </RegistrationContainer>
   );
 }
 
-const styles = {
+const unboundStyles = {
+  scrollViewContentContainer: {
+    flexGrow: 1,
+  },
+  header: {
+    fontSize: 24,
+    color: 'panelForegroundLabel',
+    paddingBottom: 16,
+  },
+  body: {
+    fontFamily: 'Arial',
+    fontSize: 15,
+    lineHeight: 20,
+    color: 'panelForegroundSecondaryLabel',
+    paddingBottom: 16,
+  },
+  farcasterLogoContainer: {
+    flexGrow: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   webViewContainer: {
     height: 0,
     overflow: 'hidden',
   },
-  webview: {
+  webView: {
     height: 0,
-  },
-  text: {
-    color: 'white',
   },
 };
 
