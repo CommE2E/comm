965612b870031e8636de188f4104cd63490f47c4
William Wang <williamwang749@gmail.com>
2024-03-22 11:27:28 -0400

    [sqlite] add aux users table for aux user store data
    Summary: Creates the sqlite database table for aux user store
    
    Test Plan: Tested on native and made sure aux user table was being initialized
    
    Reviewers: ginsu, kamil, atul
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D11507

--- native/cpp/CommonCpp/DatabaseManagers/SQLiteQueryExecutor.cpp
+++ native/cpp/CommonCpp/DatabaseManagers/SQLiteQueryExecutor.cpp
@@ -578,6 +578,15 @@ bool migrate_notifs_crypto_account(sqlite3 *db) {
 #endif
 }
 
+bool create_aux_user_table(sqlite3 *db) {
+  std::string query =
+      "CREATE TABLE IF NOT EXISTS aux_users ("
+      "  id TEXT UNIQUE PRIMARY KEY NOT NULL,"
+      "  aux_user_info TEXT NOT NULL"
+      ");";
+  return create_table(db, query, "aux_users");
+}
+
 bool create_schema(sqlite3 *db) {
   char *error;
   sqlite3_exec(
@@ -684,6 +693,11 @@ bool create_schema(sqlite3 *db) {
       "CREATE TABLE IF NOT EXISTS integrity_store ("
       "   id TEXT UNIQUE PRIMARY KEY NOT NULL,"
       "   thread_hash TEXT NOT NULL"
+      ");"
+
+      "CREATE TABLE IF NOT EXISTS aux_users ("
+      "  id TEXT UNIQUE PRIMARY KEY NOT NULL,"
+      "  aux_user_info TEXT NOT NULL"
       ");"
 
       "CREATE INDEX IF NOT EXISTS media_idx_container"
@@ -933,7 +947,8 @@ std::vector<std::pair<unsigned int, SQLiteMigration>> migrations{
      {35, {create_communities_table, true}},
      {36, {create_messages_to_device_table, true}},
      {37, {create_integrity_table, true}},
-     {38, {migrate_notifs_crypto_account, true}}}};
+     {38, {migrate_notifs_crypto_account, true}},
+     {39, {create_aux_user_table, true}}}};
 
 enum class MigrationResult { SUCCESS, FAILURE, NOT_APPLIED };
 
