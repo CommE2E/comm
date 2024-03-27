432994ae09e0b4fa7a855101671701f4276247d7
Ginsu Eddy <ginsu.eddy@gmail.com>
2024-03-27 14:35:34 -0400

    [landing] introduce ConnectFarcaster
    Summary: introduce `ConnectFarcaster` component. This component should return null as the purpose of this component is to call the `useSignIn` hook from `@farcaster/auth-kit` and return it to the react native webview component
    
    Test Plan: Confirmed that I was able to get both the `farcaster_url` and the `farcaster_data` back in the native message webview whenever this component was rendered in the native webview component + tested the entire sign in flow during the hackathon
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D11428

---
+++ landing/connect-farcaster.react.js
@@ -0,0 +1,96 @@
+// @flow
+
+import { AuthKitProvider, useSignIn } from '@farcaster/auth-kit';
+import * as React from 'react';
+
+type FarcasterWebViewMessage =
+  | {
+      +type: 'farcaster_url',
+      +url: string,
+    }
+  | {
+      +type: 'farcaster_data',
+      +fid: string,
+    };
+
+const config = {
+  domain: 'Comm',
+  siweURL: 'https://comm.app/connect-farcaster',
+  rpcURL: 'https://mainnet.optimism.io',
+  relay: 'https://relay.farcaster.xyz',
+};
+
+function postMessageToNativeWebView(message: FarcasterWebViewMessage) {
+  window.ReactNativeWebView?.postMessage?.(JSON.stringify(message));
+}
+
+type OnSuccessCallbackArgs = {
+  fid: string,
+};
+function onSuccessCallback({ fid }: OnSuccessCallbackArgs) {
+  postMessageToNativeWebView({
+    type: 'farcaster_data',
+    fid,
+  });
+}
+
+function ConnectFarcaster(): React.Node {
+  const signInState = useSignIn({ onSuccess: onSuccessCallback });
+
+  const {
+    signIn,
+    connect,
+    reconnect,
+    isSuccess,
+    isError,
+    channelToken,
+    url,
+    validSignature,
+  } = signInState;
+
+  React.useEffect(() => {
+    if (!channelToken) {
+      connect();
+    }
+  }, [channelToken, connect]);
+
+  const messageSentRef = React.useRef<boolean>(false);
+  const authenticated = isSuccess && validSignature;
+
+  React.useEffect(() => {
+    if (authenticated) {
+      return;
+    }
+    if (isError) {
+      reconnect();
+    }
+
+    signIn();
+
+    if (url && messageSentRef.current === false) {
+      messageSentRef.current = true;
+
+      postMessageToNativeWebView({
+        type: 'farcaster_url',
+        url: url.toString(),
+      });
+    }
+  }, [authenticated, isError, reconnect, signIn, url]);
+
+  return null;
+}
+
+function ConnectFarcasterWrapper(): React.Node {
+  const connectFarcasterWrapper = React.useMemo(
+    () => (
+      <AuthKitProvider config={config}>
+        <ConnectFarcaster />
+      </AuthKitProvider>
+    ),
+    [],
+  );
+
+  return connectFarcasterWrapper;
+}
+
+export default ConnectFarcasterWrapper;
