aabf91724a80a67fb55db5928995fa165965f5e0
Ginsu Eddy <ginsu.eddy@gmail.com>
2024-03-27 15:09:46 -0400

    [landing/lib/native] introduce FarcasterWebView
    Summary:
    This component will be used to talk to `BASE_LANDING_URL/connect-farcaster` route that was just introduced prior in this stack. Based on the `FarcasterWebViewMessage` type this webview will either redirect the user to the farcaster app if they have it, safari with a link to download the app if they don't have farcaster, or return the user's `fid` to the `onSuccess` callback if the farcaster user data is returned back to the webview
    
    Also in this diff I snuck in factoring out some shared types into their own file rather than copying + pasting the same type across different files
    
    Depends on D11429
    
    Test Plan: Tested the entire sign in flow during the hackathon
    
    Reviewers: atul
    
    Reviewed By: atul
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D11432

---
+++ native/components/farcaster-web-view.react.js
@@ -0,0 +1,71 @@
+// @flow
+
+import * as React from 'react';
+import { View, Linking } from 'react-native';
+import WebView from 'react-native-webview';
+
+import type { FarcasterWebViewMessage } from 'lib/types/farcaster-types.js';
+
+import type { WebViewMessageEvent } from '../types/web-view-types.js';
+import { defaultLandingURLPrefix } from '../utils/url-utils.js';
+
+const commConnectFarcasterURL = `${defaultLandingURLPrefix}/connect-farcaster`;
+
+const webViewSource = { uri: commConnectFarcasterURL };
+
+const webViewOriginWhitelist = ['*'];
+
+export type FarcasterWebViewState = 'closed' | 'opening';
+
+type Props = {
+  +onSuccess: (fid: string) => mixed,
+  +webViewState: FarcasterWebViewState,
+};
+
+function FarcasterWebView(props: Props): React.Node {
+  const { onSuccess, webViewState } = props;
+
+  const handleMessage = React.useCallback(
+    (event: WebViewMessageEvent) => {
+      const data: FarcasterWebViewMessage = JSON.parse(event.nativeEvent.data);
+
+      if (data.type === 'farcaster_url') {
+        void Linking.openURL(data.url);
+      } else if (data.type === 'farcaster_data') {
+        onSuccess(data.fid);
+      }
+    },
+    [onSuccess],
+  );
+
+  const webView = React.useMemo(() => {
+    if (webViewState === 'closed') {
+      return null;
+    }
+
+    return (
+      <View style={styles.webViewContainer}>
+        <WebView
+          source={webViewSource}
+          onMessage={handleMessage}
+          incognito={true}
+          originWhitelist={webViewOriginWhitelist}
+        />
+      </View>
+    );
+  }, [handleMessage, webViewState]);
+
+  return webView;
+}
+
+const styles = {
+  webViewContainer: {
+    height: 0,
+    overflow: 'hidden',
+  },
+  connectButtonContainer: {
+    marginHorizontal: 16,
+  },
+};
+
+export default FarcasterWebView;
