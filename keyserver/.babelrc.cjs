62b3601c95c90670ea5a3addee9f31c72a3992a6
Ginsu Eddy <ginsu.eddy@gmail.com>
2024-03-27 14:26:44 -0400

    [keyserver/landing] add @babel/plugin-transform-private-methods to list of plugins
    Summary:
    The day before the hackathon @ashoat and I were debugging an issue where the farcaster app signin redirect `url` was not being returned with the `WebView`. As we were working on a solution where we need to run `yarn prod` in `landing` + `yarn prod-build && yarn prod` in `keyserver` we were getting some babel issues that were resolved by adding the plugins in this diff
    
    Linear task w/ discussion about the issue @ashoat and I were trying to solve: https://linear.app/comm/issue/ENG-7325/connect-to-farcaster-wo-siwn
    
    Added the babel plugins + ran `yarn cleaninstall`
    
    Made @ashoat blocking since this diff adds a dependency + new babel plugins
    
    Depends on D11426
    
    Test Plan: N/A + was able to test the entire SIWF flow during the hackathon + was able to run `yarn prod` in `landing` + `yarn prod-build && yarn prod` in `keyserver` w/o getting any errors
    
    Reviewers: ashoat, atul
    
    Reviewed By: ashoat, atul
    
    Subscribers: tomek, ashoat
    
    Differential Revision: https://phab.comm.dev/D11427

--- keyserver/.babelrc.cjs
+++ keyserver/.babelrc.cjs
@@ -5,6 +5,7 @@ module.exports = {
     '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-syntax-dynamic-import',
     '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-transform-private-methods',
     '@babel/plugin-proposal-nullish-coalescing-operator',
     ['@babel/plugin-transform-runtime', { useESModules: true }],
   ],
--- landing/.babelrc.cjs
+++ landing/.babelrc.cjs
@@ -4,6 +4,7 @@ module.exports = {
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-transform-private-methods',
     '@babel/plugin-proposal-nullish-coalescing-operator',
     ['@babel/plugin-transform-runtime', { useESModules: true }],
   ],
