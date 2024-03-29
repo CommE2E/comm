7fe20f8bb37977298cfba18d21d614333f6a6649
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2024-03-29 06:34:49 +0100

    [commtest] Add test for LogInExistingDevice RPC
    Summary:
    Added a simple test doing the test plan from D11466
    
    Depends on D11466, D11467
    
    Test Plan: The test should pass
    
    Reviewers: varun
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D11468

--- services/commtest/tests/identity_access_tokens_tests.rs
+++ services/commtest/tests/identity_access_tokens_tests.rs
@@ -1,7 +1,11 @@
 use commtest::identity::device::{
   register_user_device, DEVICE_TYPE, PLACEHOLDER_CODE_VERSION,
 };
+use commtest::identity::SigningCapableAccount;
 use commtest::service_addr;
+use grpc_clients::identity::protos::unauth::{
+  Empty, ExistingDeviceLoginRequest,
+};
 use grpc_clients::identity::{
   get_unauthenticated_client, protos::unauth::VerifyUserAccessTokenRequest,
 };
@@ -19,16 +23,66 @@ async fn verify_access_token() {
   .await
   .expect("Couldn't connect to identity service");
 
-  let verify_request = VerifyUserAccessTokenRequest {
-    user_id: device_info.user_id,
-    device_id: device_info.device_id,
-    access_token: device_info.access_token,
-  };
-
+  let verify_request = VerifyUserAccessTokenRequest::from(&device_info);
   let response = identity_client
     .verify_user_access_token(verify_request)
     .await
     .unwrap();
 
   assert!(response.into_inner().token_valid);
+}
+
+#[tokio::test]
+async fn refresh_token_test() {
+  let identity_grpc_endpoint = service_addr::IDENTITY_GRPC.to_string();
+  let mut client = get_unauthenticated_client(
+    &identity_grpc_endpoint,
+    PLACEHOLDER_CODE_VERSION,
+    DEVICE_TYPE.to_string(),
+  )
+  .await
+  .expect("Couldn't connect to identity service");
+
+  let mut account = SigningCapableAccount::new();
+  let client_keys = account.public_keys();
+  let user = register_user_device(Some(&client_keys), None).await;
+
+  // refresh session
+  let nonce = client
+    .generate_nonce(Empty {})
+    .await
+    .expect("failed to generate nonce")
+    .into_inner()
+    .nonce;
+  let challenge_response = account.sign_nonce(nonce);
+  let new_credentials = client
+    .log_in_existing_device(ExistingDeviceLoginRequest {
+      user_id: user.user_id.clone(),
+      device_id: user.device_id.clone(),
+      challenge_response,
+    })
+    .await
+    .expect("LogInExistingDevice call failed")
+    .into_inner();
+
+  // old token should now be invalid
+  let old_token_result = client
+    .verify_user_access_token(VerifyUserAccessTokenRequest::from(&user))
+    .await
+    .expect("failed to verify token")
+    .into_inner();
+  assert!(!old_token_result.token_valid);
+
+  // new token should be valid
+  let new_token_result = client
+    .verify_user_access_token(VerifyUserAccessTokenRequest {
+      user_id: new_credentials.user_id,
+      access_token: new_credentials.access_token,
+      device_id: user.device_id,
+    })
+    .await
+    .expect("failed to verify token")
+    .into_inner();
+
+  assert!(new_token_result.token_valid);
 }
