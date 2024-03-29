2dd87c9a5003b83fbcc465035596f32cd48e1b13
Ginsu Eddy <ginsu.eddy@gmail.com>
2024-03-29 16:32:09 -0400

    [native] update FarcasterPrompt based on if the fid is set or not
    Summary: This diff updates the text content of `FarcasterPrompt` based on wheter or not the user is logged into farcaster or not (we determine this by whether or not the comm user has an `fid` set)
    
    Test Plan: Please see the screenshots below
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D11484

--- native/components/farcaster-prompt.react.js
+++ native/components/farcaster-prompt.react.js
@@ -1,31 +1,49 @@
 // @flow
 
+import invariant from 'invariant';
 import * as React from 'react';
 import { View, Text } from 'react-native';
+
+import { FIDContext } from 'lib/components/fid-provider.react.js';
 
 import { useStyles } from '../themes/colors.js';
 import FarcasterLogo from '../vectors/farcaster-logo.react.js';
 
 function FarcasterPrompt(): React.Node {
+  const fidContext = React.useContext(FIDContext);
+  invariant(fidContext, 'FIDContext is missing');
+
+  const { fid } = fidContext;
+
   const styles = useStyles(unboundStyles);
+
+  const headerText = fid
+    ? 'Disconnect from Farcaster'
+    : 'Do you want to connect your Farcaster account';
+
+  const bodyText = fid
+    ? 'You can disconnect your Farcaster account at any time.'
+    : 'Connecting your Farcaster account lets you see your mutual follows ' +
+      'on Comm. We’ll also surface communities based on your Farcaster ' +
+      'channels.';
 
   const farcasterPrompt = React.useMemo(
     () => (
       <>
-        <Text style={styles.header}>
-          Do you want to connect your Farcaster account?
-        </Text>
-        <Text style={styles.body}>
-          Connecting your Farcaster account lets you see your mutual follows on
-          Comm. We&rsquo;ll also surface communities based on your Farcaster
-          channels.
-        </Text>
+        <Text style={styles.header}>{headerText}</Text>
+        <Text style={styles.body}>{bodyText}</Text>
         <View style={styles.farcasterLogoContainer}>
           <FarcasterLogo />
         </View>
       </>
     ),
-    [styles.body, styles.farcasterLogoContainer, styles.header],
+    [
+      bodyText,
+      headerText,
+      styles.body,
+      styles.farcasterLogoContainer,
+      styles.header,
+    ],
   );
 
   return farcasterPrompt;
