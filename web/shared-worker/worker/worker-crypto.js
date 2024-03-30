CommE2E/comm

commit 9a1a84aea24fcc7a435c19f3cc3270cba15d216e

[keyserver] Update NEXT_CODE_VERSION to 12 for desktop

web/shared-worker/worker/worker-crypto.js
@@ -5,10 +5,7 @@ import localforage from 'localforage';
 import uuid from 'uuid';
 
 import { initialEncryptedMessageContent } from 'lib/shared/crypto-utils.js';
-import {
-  hasMinCodeVersion,
-  NEXT_CODE_VERSION,
-} from 'lib/shared/version-utils.js';
+import { hasMinCodeVersion } from 'lib/shared/version-utils.js';
 import {
   olmEncryptedMessageTypes,
   type OLMIdentityKeys,
@@ -505,7 +502,7 @@ const olmAPI: OlmAPI = {
     let notifsOlmDataEncryptionKeyDBLabel;
 
     if (
-      hasMinCodeVersion(platformDetails, { majorDesktop: NEXT_CODE_VERSION })
+      hasMinCodeVersion(platformDetails, { majorDesktop: 12 })
     ) {
       notifsOlmDataEncryptionKeyDBLabel = getOlmEncryptionKeyDBLabelForCookie(
         cookie,

web/push-notif/push-notifs-handler.js
@@ -8,10 +8,7 @@ import {
 } from 'lib/actions/device-actions.js';
 import { useModalContext } from 'lib/components/modal-provider.react.js';
 import { isLoggedIn } from 'lib/selectors/user-selectors.js';
-import {
-  hasMinCodeVersion,
-  NEXT_CODE_VERSION,
-} from 'lib/shared/version-utils.js';
+import { hasMinCodeVersion } from 'lib/shared/version-utils.js';
 import { isDesktopPlatform } from 'lib/types/device-types.js';
 import { getConfig } from 'lib/utils/config.js';
 import { convertNonPendingIDToNewSchema } from 'lib/utils/migration-utils.js';
@@ -45,7 +42,7 @@ function useCreateDesktopPushSubscription() {
   React.useEffect(() => {
     if (
       !isDesktopPlatform(platformDetails.platform) ||
-      !hasMinCodeVersion(platformDetails, { majorDesktop: NEXT_CODE_VERSION })
+      !hasMinCodeVersion(platformDetails, { majorDesktop: 12 })
     ) {
       return;
     }
@@ -72,7 +69,7 @@ function useCreateDesktopPushSubscription() {
 
   React.useEffect(() => {
     if (
-      hasMinCodeVersion(platformDetails, { majorDesktop: NEXT_CODE_VERSION }) &&
+      hasMinCodeVersion(platformDetails, { majorDesktop: 12 }) &&
       !notifsOlmSessionMigrated
     ) {
       return undefined;

commit 5e7349fa7fed30452eafe4bb1dcd4421dd797bca

[web] ESLint fix for NEXT_CODE_VERSION commit below
Fixes an ESLint error introduced in 9a1a84aea24fcc7a435c19f3cc3270cba15d216e. Not sure why it wasn't caught by `lint-staged`...

web/shared-worker/worker/worker-crypto.js
@@ -501,9 +501,7 @@ const olmAPI: OlmAPI = {
     let notifsOlmDataContentKey;
     let notifsOlmDataEncryptionKeyDBLabel;
 
-    if (
-      hasMinCodeVersion(platformDetails, { majorDesktop: 12 })
-    ) {
+    if (hasMinCodeVersion(platformDetails, { majorDesktop: 12 })) {
       notifsOlmDataEncryptionKeyDBLabel = getOlmEncryptionKeyDBLabelForCookie(
         cookie,
         keyserverID,