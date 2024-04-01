63128ef16af16e630e7208bb2fbd83774e10c738
William Wang <williamwang749@gmail.com>
2024-04-01 15:52:22 -0400

    [lib] Introduce aux user store reducer and action types
    Summary: Introduces the aux user store reducer and fid setter action type
    
    Test Plan: Ran the following code
    
    Reviewers: atul, ginsu
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D11528

--- native/redux/state-types.js
+++ native/redux/state-types.js
@@ -3,6 +3,7 @@
 import type { Orientations } from 'react-native-orientation-locker';
 import type { PersistState } from 'redux-persist/es/types.js';
 
+import type { AuxUserStore } from 'lib/types/aux-user-types.js';
 import type { CommunityStore } from 'lib/types/community-types.js';
 import type { DBOpsStore } from 'lib/types/db-ops-types';
 import type { DraftStore } from 'lib/types/draft-types.js';
@@ -81,6 +82,7 @@ export type AppState = {
   +communityStore: CommunityStore,
   +dbOpsStore: DBOpsStore,
   +syncedMetadataStore: SyncedMetadataStore,
+  +auxUserStore: AuxUserStore,
 };
 
 export { nonUserSpecificFieldsNative };
