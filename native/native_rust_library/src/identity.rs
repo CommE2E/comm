0231fec257bdd9906687f46a99787448b9d659e1
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2024-04-01 08:57:01 +0200

    [native_rust_library] Create ffi submodules for identity API
    Summary:
    Part of [[ https://linear.app/comm/issue/ENG-6713/split-native-rust-library-librs-into-smaller-files | ENG-6713 ]].
    
    - Created a base module for Identity Service and moved first APIs there.
    - Followed the convention from Backup module having the `ffi` submodule and further submodule tree
    
    Depends on D11517
    
    Test Plan: The same test plan for the whole stack: each commit compiles on both platforms. At the final commit - played a bit with these APIs, made sure they're callable from JS
    
    Reviewers: varun!, michal
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D11518

--- native/native_rust_library/src/identity.rs
+++ native/native_rust_library/src/identity.rs
@@ -1,3 +1,69 @@
+use grpc_clients::identity::get_unauthenticated_client;
+use grpc_clients::identity::protos::unauth::Empty;
+
+use crate::utils::jsi_callbacks::{
+  handle_bool_result_as_callback, handle_string_result_as_callback,
+};
+use crate::{Error, RUNTIME};
+use crate::{CODE_VERSION, DEVICE_TYPE, IDENTITY_SOCKET_ADDR};
+
 pub mod exact_user_search;
 pub mod farcaster;
 pub mod wallet_registration;
+
+pub mod ffi {
+  use super::*;
+
+  pub use exact_user_search::ffi::*;
+  pub use farcaster::ffi::*;
+  pub use wallet_registration::ffi::*;
+
+  pub fn generate_nonce(promise_id: u32) {
+    RUNTIME.spawn(async move {
+      let result = fetch_nonce().await;
+      handle_string_result_as_callback(result, promise_id);
+    });
+  }
+
+  pub fn version_supported(promise_id: u32) {
+    RUNTIME.spawn(async move {
+      let result = version_supported_helper().await;
+      handle_bool_result_as_callback(result, promise_id);
+    });
+  }
+}
+
+async fn fetch_nonce() -> Result<String, Error> {
+  let mut identity_client = get_unauthenticated_client(
+    IDENTITY_SOCKET_ADDR,
+    CODE_VERSION,
+    DEVICE_TYPE.as_str_name().to_lowercase(),
+  )
+  .await?;
+  let nonce = identity_client
+    .generate_nonce(Empty {})
+    .await?
+    .into_inner()
+    .nonce;
+  Ok(nonce)
+}
+
+async fn version_supported_helper() -> Result<bool, Error> {
+  let mut identity_client = get_unauthenticated_client(
+    IDENTITY_SOCKET_ADDR,
+    CODE_VERSION,
+    DEVICE_TYPE.as_str_name().to_lowercase(),
+  )
+  .await?;
+  let response = identity_client.ping(Empty {}).await;
+  match response {
+    Ok(_) => Ok(true),
+    Err(e) => {
+      if grpc_clients::error::is_version_unsupported(&e) {
+        Ok(false)
+      } else {
+        Err(e.into())
+      }
+    }
+  }
+}
