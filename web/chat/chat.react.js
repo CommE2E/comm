6bf3a3e6223eb565825fd84c707ff3a3f3e102d6
Michal Gniadek <michal.gniadek@swmansion.com>
2024-04-02 17:40:31 +0200

    [web/native/lib] Use message prunner on web
    Summary:
    [ENG-3475 : Implement message pruner for the web](https://linear.app/comm/issue/ENG-3475)
    Now that messages will be persisted on web we should prun them occasionaly. This moves `MessageStorePruner`, `nextMessagePruneTimeSelector` and `pruneThreadIDsSelector` from native to lib and removes the platform-specific code, which now lives in `native`/`web`.
    
    Also fixes an issue mentioned in [this comment](https://linear.app/comm/issue/ENG-3475/implement-message-pruner-for-the-web#comment-507d92af) by ignoring threads with less than 20 messages in `nextMessagePruneTimeSelector`.
    
    Depends on D11544
    
    Test Plan: Change the `msInHour` constant to a lower value so pruning happens more often. Test on both web and native that the threads with <20 messages get pruned. Test that this doesn't cause issues on web (e.g. the messages can still be fetched from keyserver).
    
    Reviewers: atul, kamil, marcin
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D11545

--- web/chat/chat.react.js
+++ web/chat/chat.react.js
@@ -2,6 +2,7 @@
 
 import * as React from 'react';
 
+import MessageStorePruner from 'lib/components/message-store-pruner.react.js';
 import ThreadDraftUpdater from 'lib/components/thread-draft-updater.react.js';
 import { isLoggedIn } from 'lib/selectors/user-selectors.js';
 
@@ -9,6 +10,7 @@ import ChatMessageListContainer from './chat-message-list-container.react.js';
 import ChatTabs from './chat-tabs.react.js';
 import { ThreadListProvider } from './thread-list-provider.js';
 import { useSelector } from '../redux/redux-utils.js';
+import { activeThreadSelector } from '../selectors/nav-selectors.js';
 
 function Chat(): React.Node {
   const loggedIn = useSelector(isLoggedIn);
@@ -37,11 +39,15 @@ function Chat(): React.Node {
   if (loggedIn) {
     threadDraftUpdater = <ThreadDraftUpdater />;
   }
+
+  const activeThreadID = useSelector(activeThreadSelector);
+
   return (
     <>
       {chatList}
       {messageList}
       {threadDraftUpdater}
+      <MessageStorePruner activeThreadID={activeThreadID} />
     </>
   );
 }
