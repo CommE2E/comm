9a7c9bac9bb4c8295dc78d53f2cb7d4e4bc47550
Ginsu Eddy <ginsu.eddy@gmail.com>
2024-04-04 13:37:04 -0400

    [native] implement connect farcaster bottom sheet ui
    Summary:
    This diff introduces the connect farcaster bottom sheet ui
    
    Please note that the focus of this diff was to introduce the UI. Subsequent diffs will polish/finish up the connect farcaster bottom sheet on successfully connect functionality, fix the snap points, etc.
    
    Linear task: https://linear.app/comm/issue/ENG-7519/implement-ui-for-connectfarcasterbottomsheet
    
    Depends on D11559
    
    Test Plan:
    Please see screenshots below
    
    {F1466853}
    
    Reviewers: atul, inka
    
    Reviewed By: atul
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D11560

--- native/components/connect-farcaster-bottom-sheet.react.js
+++ native/components/connect-farcaster-bottom-sheet.react.js
@@ -1,7 +1,13 @@
 // @flow
 
 import * as React from 'react';
+import { View, StyleSheet } from 'react-native';
 
+import FarcasterPrompt from './farcaster-prompt.react.js';
+import FarcasterWebView, {
+  type FarcasterWebViewState,
+} from './farcaster-web-view.react.js';
+import RegistrationButton from '../account/registration/registration-button.react.js';
 import BottomSheet from '../bottom-sheet/bottom-sheet.react.js';
 import type { RootNavigationProp } from '../navigation/root-navigator.react.js';
 import type { NavigationRoute } from '../navigation/route-names.js';
@@ -18,11 +24,60 @@ function ConnectFarcasterBottomSheet(props: Props): React.Node {
 
   const bottomSheetRef = React.useRef(null);
 
-  return (
-    <BottomSheet ref={bottomSheetRef} onClosed={goBack}>
-      {null}
-    </BottomSheet>
+  const [webViewState, setWebViewState] =
+    React.useState<FarcasterWebViewState>('closed');
+
+  const onSuccessfulConnect = React.useCallback(() => {
+    // TODO: Update fid
+    bottomSheetRef.current?.close();
+  }, []);
+
+  const onPressConnect = React.useCallback(() => {
+    setWebViewState('opening');
+  }, []);
+
+  const connectButtonVariant =
+    webViewState === 'opening' ? 'loading' : 'enabled';
+
+  const connectFarcasterBottomSheet = React.useMemo(
+    () => (
+      <BottomSheet ref={bottomSheetRef} onClosed={goBack}>
+        <View style={styles.container}>
+          <View style={styles.promptContainer}>
+            <FarcasterPrompt />
+          </View>
+          <RegistrationButton
+            onPress={onPressConnect}
+            label="Connect Farcaster account"
+            variant={connectButtonVariant}
+          />
+        </View>
+        <FarcasterWebView
+          onSuccess={onSuccessfulConnect}
+          webViewState={webViewState}
+        />
+      </BottomSheet>
+    ),
+    [
+      connectButtonVariant,
+      goBack,
+      onPressConnect,
+      onSuccessfulConnect,
+      webViewState,
+    ],
   );
+
+  return connectFarcasterBottomSheet;
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+  },
+  promptContainer: {
+    marginBottom: 40,
+  },
+});
 
 export default ConnectFarcasterBottomSheet;
