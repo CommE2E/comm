e2f3e8f3225f3bb54c743e13b4918d77c3eff2a9
William Wang <williamwang749@gmail.com>
2024-04-08 08:00:55 -0400

    [sqlite/native] Make thread activity sqlite ops available to native
    Summary: This makes the thread activity sqlite operations available on native
    
    Test Plan:
    Ran the following code:
    
    Depends on D11581
    
    Reviewers: kamil, atul, ginsu
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D11584

--- native/cpp/CommonCpp/NativeModules/CommCoreModule.cpp
+++ native/cpp/CommonCpp/NativeModules/CommCoreModule.cpp
@@ -385,6 +385,13 @@ jsi::Value CommCoreModule::processAuxUserStoreOperations(
   return this->auxUserStore.processStoreOperations(rt, std::move(operations));
 }
 
+jsi::Value CommCoreModule::processThreadActivityStoreOperations(
+    jsi::Runtime &rt,
+    jsi::Array operations) {
+  return this->threadActivityStore.processStoreOperations(
+      rt, std::move(operations));
+}
+
 void CommCoreModule::terminate(jsi::Runtime &rt) {
   TerminateApp::terminate();
 }
@@ -1366,7 +1373,8 @@ CommCoreModule::CommCoreModule(
       communityStore(jsInvoker),
       integrityStore(jsInvoker),
       syncedMetadataStore(jsInvoker),
-      auxUserStore(jsInvoker) {
+      auxUserStore(jsInvoker),
+      threadActivityStore(jsInvoker) {
   GlobalDBSingleton::instance.enableMultithreading();
 }
 
