48a8258baba8bf6f1af293fa11eac0754f3e2600
William Wang <williamwang749@gmail.com>
2024-04-08 08:00:55 -0400

    [sqlite/native] Make thread activity sqlite ops available to native
    Summary: This makes the thread activity sqlite operations available on native
    
    Test Plan:
    Ran the following code:
    
    Depends on D11581
    
    Reviewers: kamil, atul, ginsu
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D11584

---
+++ native/cpp/CommonCpp/NativeModules/PersistentStorageUtilities/DataStores/ThreadActivityStore.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "../../../DatabaseManagers/entities/ThreadActivityEntry.h"
+#include "BaseDataStore.h"
+#include "ThreadActivityStoreOperations.h"
+
+#include <jsi/jsi.h>
+
+namespace comm {
+
+class ThreadActivityStore : public BaseDataStore<
+                                ThreadActivityStoreOperationBase,
+                                ThreadActivityEntry> {
+private:
+  static OperationType REMOVE_OPERATION;
+  static OperationType REMOVE_ALL_OPERATION;
+  static OperationType REPLACE_OPERATION;
+
+public:
+  ThreadActivityStore(std::shared_ptr<facebook::react::CallInvoker> jsInvoker);
+
+  std::vector<std::unique_ptr<ThreadActivityStoreOperationBase>>
+  createOperations(jsi::Runtime &rt, const jsi::Array &operations)
+      const override;
+
+  jsi::Array parseDBDataStore(
+      jsi::Runtime &rt,
+      std::shared_ptr<std::vector<ThreadActivityEntry>> dataVectorPtr)
+      const override;
+};
+
+} // namespace comm
