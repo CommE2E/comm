d050bb1223f613f7622ba6ab1e26ba0b8eb3138a
William Wang <williamwang749@gmail.com>
2024-04-09 16:35:03 -0400

    [native] migrate threadActivityStore to SQLite
    Summary: This migrates threadActivityStore to SQLite
    
    Test Plan:
    Test plan is to populate redux persist with threadActivity entries on a lower migration version and then upgrade the version.
    
    This migration will currently fail as is as there is a possible bug with redux persist thread activity not storing `lastPruned`. This is tracked by:
    https://linear.app/comm/issue/ENG-7663/missing-lastpruned-field-in-current-thread-activity-redux-persist
    
    By providing a default `lastPruned` value in the reducer, this migration works, but will need to fix ENG-7663 before landing.
    
    Will not land until test plan updated and issue resolved
    
    Reviewers: atul, kamil
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D11616

--- native/redux/persist.js
+++ native/redux/persist.js
@@ -36,6 +36,11 @@ import {
   convertReportsToReplaceReportOps,
   reportStoreOpsHandlers,
 } from 'lib/ops/report-store-ops.js';
+import {
+  type ClientDBThreadActivityStoreOperation,
+  threadActivityStoreOpsHandlers,
+  type ReplaceThreadActivityEntryOperation,
+} from 'lib/ops/thread-activity-store-ops.js';
 import type { ClientDBThreadStoreOperation } from 'lib/ops/thread-store-ops.js';
 import { threadStoreOpsHandlers } from 'lib/ops/thread-store-ops.js';
 import {
@@ -1208,6 +1213,32 @@ const migrations = {
     const { userStore, ...rest } = state;
     return rest;
   },
+  [69]: async (state: AppState) => {
+    const replaceOps: $ReadOnlyArray<ReplaceThreadActivityEntryOperation> =
+      entries(state.threadActivityStore).map(([threadID, entry]) => ({
+        type: 'replace_thread_activity_entry',
+        payload: {
+          id: threadID,
+          threadActivityStoreEntry: entry,
+        },
+      }));
+
+    const dbOperations: $ReadOnlyArray<ClientDBThreadActivityStoreOperation> =
+      threadActivityStoreOpsHandlers.convertOpsToClientDBOps([
+        { type: 'remove_all_thread_activity_entries' },
+        ...replaceOps,
+      ]);
+
+    try {
+      await commCoreModule.processThreadActivityStoreOperations(dbOperations);
+    } catch (exception) {
+      if (isTaskCancelledError(exception)) {
+        return state;
+      }
+      return handleReduxMigrationFailure(state);
+    }
+    return state;
+  },
 };
 
 type PersistedReportStore = $Diff<
@@ -1229,7 +1260,7 @@ const persistConfig = {
   storage: AsyncStorage,
   blacklist: persistBlacklist,
   debug: __DEV__,
-  version: 68,
+  version: 69,
   transforms: [
     messageStoreMessagesBlocklistTransform,
     reportStoreTransform,
