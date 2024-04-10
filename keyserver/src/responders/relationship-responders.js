42a27c9cdc0b3fd09f41be4486caed52c308b1e0
Atul Madhugiri <atulm@berkeley.edu>
2024-04-10 01:16:29 -0400

    [lib] Update `updateRelationshipInputValidator` to be union of `[traditional/farcaster]RelationshipRequestValidator`
    Summary:
    Update this validator so it supports requests of type `TraditionalRelationshipRequest` and `FarcasterRelationshipRequest`
    
    Also renamed `updateFarcasterRelationshipInputValidator` to `farcasterRelationshipRequestValidator` for clarity and more consistent naming.
    
    ---
    
    Depends on D11611
    
    Test Plan: Send both `TraditionalRelationshipRequest` and `FarcasterRelationshipRequest` requests and ensure that they are validated by `keyserver`. Confirmed with log statements.
    
    Reviewers: ginsu, tomek, varun, will
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D11612

--- keyserver/src/responders/relationship-responders.js
+++ keyserver/src/responders/relationship-responders.js
@@ -1,19 +1,20 @@
 // @flow
 
-import t, { type TInterface } from 'tcomb';
+import t, { type TInterface, type TUnion } from 'tcomb';
 
 import {
   type TraditionalRelationshipRequest,
   type RelationshipErrors,
   traditionalRelationshipActionsList,
   type RelationshipRequest,
+  farcasterRelationshipRequestValidator,
 } from 'lib/types/relationship-types.js';
 import { tShape } from 'lib/utils/validation-utils.js';
 
 import type { Viewer } from '../session/viewer.js';
 import { updateRelationships } from '../updaters/relationship-updaters.js';
 
-export const updateRelationshipInputValidator: TInterface<TraditionalRelationshipRequest> =
+export const traditionalRelationshipRequestValidator: TInterface<TraditionalRelationshipRequest> =
   tShape<TraditionalRelationshipRequest>({
     action: t.enums.of(
       traditionalRelationshipActionsList,
@@ -21,6 +22,12 @@ export const updateRelationshipInputValidator: TInterface<TraditionalRelationshi
     ),
     userIDs: t.list(t.String),
   });
+
+export const updateRelationshipInputValidator: TUnion<RelationshipRequest> =
+  t.union([
+    traditionalRelationshipRequestValidator,
+    farcasterRelationshipRequestValidator,
+  ]);
 
 export const relationshipErrorsValidator: TInterface<RelationshipErrors> =
   tShape<RelationshipErrors>({
