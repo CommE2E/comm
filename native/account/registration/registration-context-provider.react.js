278724c3e14b8dd88d3f12980c57c4fe94b99d03
marcinwasowicz <marcinkwasowicz@gmail.com>
2024-04-11 11:14:35 +0200

    Persist SIWE backup secrets to SQLite after succesfull registration process
    Summary:
    This differential saves generated backup secrets in registration context which then persists it into SQLite when the regiatration is complete and database is
    correctly re-created.
    
    Test Plan:
    1. Add the following gist: . It creates a button that enables to print siwe backup secrets to the console.
    2. Execute registration flow.
    3. Tap the button. See siwe backup secrets in the console.
    4. Log out. Don't kill the app!
    5. Log in to the same SIWE account.
    6. Tap the button ensure that there is no backup secret in the db.
    
    Reviewers: tomek, inka, atul
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D11638

--- native/account/registration/registration-context-provider.react.js
+++ native/account/registration/registration-context-provider.react.js
@@ -1,10 +1,16 @@
 // @flow
 
+import _isEmpty from 'lodash/fp/isEmpty.js';
 import * as React from 'react';
+
+import { isLoggedIn } from 'lib/selectors/user-selectors.js';
 
 import { RegistrationContext } from './registration-context.js';
 import { useRegistrationServerCall } from './registration-server-call.js';
 import type { CachedUserSelections } from './registration-types.js';
+import { commCoreModule } from '../../native-modules.js';
+import { useSelector } from '../../redux/redux-utils.js';
+import Alert from '../../utils/alert.js';
 
 const emptyObj: CachedUserSelections = Object.freeze({});
 
@@ -37,6 +43,40 @@ function RegistrationContextProvider(props: Props): React.Node {
       setSkipEthereumLoginOnce,
     ],
   );
+
+  const loggedIn = useSelector(isLoggedIn);
+  const clearCachedSelectionsPromise = React.useRef<?Promise<void>>(null);
+
+  React.useEffect(() => {
+    if (clearCachedSelectionsPromise.current) {
+      return;
+    }
+
+    if (_isEmpty(cachedSelections) || !loggedIn) {
+      return;
+    }
+
+    clearCachedSelectionsPromise.current = (async () => {
+      try {
+        if (cachedSelections.siweBackupSecrets) {
+          await commCoreModule.setSIWEBackupSecrets(
+            cachedSelections.siweBackupSecrets,
+          );
+        }
+      } catch (e) {
+        console.log(e);
+        if (__DEV__) {
+          Alert.alert(
+            'Failed to persist SIWE backup secrets to SQLite.',
+            e.message,
+          );
+        }
+      } finally {
+        setCachedSelections({});
+        clearCachedSelectionsPromise.current = null;
+      }
+    })();
+  }, [cachedSelections, loggedIn]);
 
   return (
     <RegistrationContext.Provider value={contextValue}>
