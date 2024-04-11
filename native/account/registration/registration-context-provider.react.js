3458d61f9bd87a169998c50402acf120457ed4ac
marcinwasowicz <marcinkwasowicz@gmail.com>
2024-04-11 11:14:35 +0200

    Persist SIWE backup secrets to SQLite after succesfull registration process
    Summary: This differential saves generated backup secrets in registration context which then persists it into SQLite when the regiatration is complete and database is
    correctly re-created.
    
    Test Plan:
    1. Add the following gist: . It creates a button that enables to print siwe backup secrets to the console.
    2. Execute registration flow.
    3. Tap the button. See siwe backup secrets in the console.
    4. Log out. Don't kill the app!
    5. Log in to the same SIWE account.
    6. Tap the button ensure that there is no backup secret in the db.
    
    Reviewers: tomek, inka, atul
    
    Subscribers:

--- native/account/registration/registration-context-provider.react.js
+++ native/account/registration/registration-context-provider.react.js
@@ -2,9 +2,14 @@
 
 import * as React from 'react';
 
+import { isLoggedIn } from 'lib/selectors/user-selectors.js';
+import type { SIWEBackupSecrets } from 'lib/types/siwe-types';
+
 import { RegistrationContext } from './registration-context.js';
 import { useRegistrationServerCall } from './registration-server-call.js';
 import type { CachedUserSelections } from './registration-types.js';
+import { commCoreModule } from '../../native-modules.js';
+import { useSelector } from '../../redux/redux-utils.js';
 
 const emptyObj: CachedUserSelections = Object.freeze({});
 
@@ -20,6 +25,8 @@ function RegistrationContextProvider(props: Props): React.Node {
   const setSkipEthereumLoginOnce = React.useCallback((skip: boolean) => {
     baseSetSkipEthereumLoginOnce(skip || undefined);
   }, []);
+  const [siweBackupSecrets, setSIWEBackupSecrets] =
+    React.useState<?SIWEBackupSecrets>(undefined);
 
   const registrationServerCall = useRegistrationServerCall();
   const contextValue = React.useMemo(
@@ -29,14 +36,34 @@ function RegistrationContextProvider(props: Props): React.Node {
       setCachedSelections,
       skipEthereumLoginOnce,
       setSkipEthereumLoginOnce,
+      siweBackupSecrets,
+      setSIWEBackupSecrets,
     }),
     [
       registrationServerCall,
       cachedSelections,
       skipEthereumLoginOnce,
       setSkipEthereumLoginOnce,
+      siweBackupSecrets,
+      setSIWEBackupSecrets,
     ],
   );
+  const storeLoaded = useSelector(state => state.storeLoaded);
+  const loggedIn = useSelector(isLoggedIn);
+  const lastPersistedBackupSecret = React.useRef<?SIWEBackupSecrets>(undefined);
+
+  React.useEffect(() => {
+    void (async () => {
+      if (!siweBackupSecrets || !storeLoaded || !loggedIn) {
+        return;
+      }
+      if (lastPersistedBackupSecret.current === siweBackupSecrets) {
+        return;
+      }
+      lastPersistedBackupSecret.current = siweBackupSecrets;
+      await commCoreModule.setSIWEBackupSecrets(siweBackupSecrets);
+    })();
+  }, [storeLoaded, siweBackupSecrets, loggedIn]);
 
   return (
     <RegistrationContext.Provider value={contextValue}>
