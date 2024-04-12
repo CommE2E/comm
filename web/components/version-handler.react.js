0fe9f9ba575150a8a5132b8bbe9fe1f8fa758753
Inka Sokołowska <inka.d.sokolowska@gmail.com>
2024-04-12 10:36:27 +0200

    [web] Show a popup when client_version_unsupported
    Summary:
    issue: [[https://linear.app/comm/issue/ENG-6128/[web]-add-a-ui-element-to-inform-the-user-that-the-client-version-is | ENG-6128]]
    The text is based on the text shown on native:
    ```
    'App out of date',
    message:
      'Your app version is pretty old, and the server doesn’t know how ' +
      `to speak to it anymore. Please use the ${platformStore} to update!`,
    ```
    
    Test Plan: Tested that if the server sends `client_version_unsupported` the modal shows up
    
    Reviewers: tomek, ginsu
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D11643

---
+++ web/components/version-handler.react.js
@@ -0,0 +1,68 @@
+// @flow
+
+import * as React from 'react';
+
+import { useModalContext } from 'lib/components/modal-provider.react.js';
+import { isDesktopPlatform } from 'lib/types/device-types.js';
+import { getConfig } from 'lib/utils/config.js';
+
+import css from './version-handler.css';
+import Modal from '../modals/modal.react.js';
+import { useSelector } from '../redux/redux-utils.js';
+
+function VersionUnsupportedModal(): React.Node {
+  const { popModal } = useModalContext();
+
+  const actionRequestMessage = isDesktopPlatform(
+    getConfig().platformDetails.platform,
+  )
+    ? 'Please reload the app'
+    : 'Please refresh the page';
+
+  return (
+    <Modal name="App version unsupported" onClose={popModal} size="large">
+      <div className={css.modalContent}>
+        Your app version is pretty old, and the server doesn’t know how to speak
+        to it anymore. {actionRequestMessage}.
+      </div>
+    </Modal>
+  );
+}
+
+type Props = {
+  +keyserverID: string,
+};
+
+function SingleKeyserverMinVersionHandler(props: Props): null {
+  const { keyserverID } = props;
+
+  const connectionIssue = useSelector(
+    state =>
+      state.keyserverStore.keyserverInfos[keyserverID].connection
+        .connectionIssue,
+  );
+
+  const { pushModal } = useModalContext();
+
+  React.useEffect(() => {
+    if (connectionIssue === 'client_version_unsupported') {
+      pushModal(<VersionUnsupportedModal />);
+    }
+  }, [connectionIssue, keyserverID, pushModal]);
+
+  return null;
+}
+const MemoizedSingleKeyserverMinVersionHandler: React.ComponentType<Props> =
+  React.memo<Props>(SingleKeyserverMinVersionHandler);
+
+function MinVersionHandler(): React.Node {
+  const keyserverIDs = useSelector(state =>
+    Object.keys(state.keyserverStore.keyserverInfos),
+  );
+
+  return keyserverIDs.map(id => (
+    <MemoizedSingleKeyserverMinVersionHandler keyserverID={id} key={id} />
+  ));
+}
+
+export default MinVersionHandler;
