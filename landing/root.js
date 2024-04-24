5024c6007dda8ecc374c4f46bbe8687fa348581d
marcinwasowicz <marcinkwasowicz@gmail.com>
2024-04-24 14:07:12 +0200

    [SIWE] Differentiate content for social proof and backup message.
    Summary: This differential introduces different SIWE message content for social proof and backup message.
    
    Test Plan:
    1. Ensure that `siwe-utils.test.js` pass during commit.
    2. I executed SIWE login flow and ensured that messages that appear in rainbow app match expected content.
    
    Reviewers: ashoat
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D11757

--- landing/root.js
+++ landing/root.js
@@ -3,13 +3,15 @@
 import * as React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import { type SIWEMessageType } from 'lib/types/siwe-types.js';
+
 import Landing from './landing.react.js';
 import { SIWEContext } from './siwe-context.js';
 
 declare var routerBasename: string;
 declare var siweNonce: ?string;
 declare var siwePrimaryIdentityPublicKey: ?string;
-declare var siweMessageType: ?string;
+declare var siweMessageType: ?SIWEMessageType;
 
 function RootComponent(): React.Node {
   const siweContextValue = React.useMemo(
--- landing/siwe-context.js
+++ landing/siwe-context.js
@@ -2,10 +2,12 @@
 
 import * as React from 'react';
 
+import { type SIWEMessageType } from 'lib/types/siwe-types.js';
+
 export type SIWEContextType = {
   +siweNonce: ?string,
   +siwePrimaryIdentityPublicKey: ?string,
-  +siweMessageType: ?string,
+  +siweMessageType: ?SIWEMessageType,
 };
 
 const SIWEContext: React.Context<SIWEContextType> = React.createContext({
