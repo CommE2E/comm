5a19bd32d3c4d24e3bc826549d2ea86ba836f3fa
Ginsu Eddy <ginsu.eddy@gmail.com>
2024-04-25 16:29:58 -0400

    [keyserver] introduce barebones createOrUpdateFarcasterChannelTag
    Summary:
    This diff introduces the barebones `createOrUpdateFarcasterChannelTag` function. For the scope of this project we just want this function to upload a mapping of farcaster channel => comm community to the blob service for now, with the intention that all the other necessary parts for this function like (permissions, inserting the blob holder in the keyserver will be addressed and added to this function as a [[ https://linear.app/comm/issue/ENG-7713/introduce-communities-table-to-keyserver | follow up project ]] that I will start shortly)
    
    Linear task: https://linear.app/comm/issue/ENG-7575/introduce-uploadfarcasterchannelblob-function and https://linear.app/comm/issue/ENG-7581/introduce-getfarcasterchannelblob-function
    
    Test Plan:
    In my local stack I was able to use `createOrUpdateFarcasterChannelTag` to upload the farcaster channel => comm community blob and got the expected response back.
    
    I also confirmed that when I tried to reuse the same farcaster channel tag I would trigger the `already_in_use` error which is expected
    
    Reviewers: bartek, atul, tomek
    
    Reviewed By: tomek
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D11772

---
+++ keyserver/src/creators/farcaster-channel-tag-creator.js
@@ -0,0 +1,89 @@
+// @flow
+
+import uuid from 'uuid';
+
+import type {
+  CreateOrUpdateFarcasterChannelTagRequest,
+  CreateOrUpdateFarcasterChannelTagResponse,
+} from 'lib/types/community-types.js';
+import { ServerError } from 'lib/utils/errors.js';
+
+import {
+  uploadBlob,
+  assignHolder,
+  download,
+  type BlobOperationResult,
+  type BlobDownloadResult,
+} from '../services/blob.js';
+import { Viewer } from '../session/viewer.js';
+
+async function createOrUpdateFarcasterChannelTag(
+  viewer: Viewer,
+  request: CreateOrUpdateFarcasterChannelTagRequest,
+): Promise<CreateOrUpdateFarcasterChannelTagResponse> {
+  const { commCommunityID, farcasterChannelID } = request;
+
+  const blobDownload = await getFarcasterChannelTagBlob(farcasterChannelID);
+
+  if (blobDownload.found) {
+    throw new ServerError('already_in_use');
+  }
+
+  const blobHolder = uuid.v4();
+
+  const blobResult = await uploadFarcasterChannelTagBlob(
+    commCommunityID,
+    farcasterChannelID,
+    blobHolder,
+  );
+
+  if (!blobResult.success) {
+    if (blobResult.reason === 'HASH_IN_USE') {
+      throw new ServerError('already_in_use');
+    } else {
+      throw new ServerError('unknown_error');
+    }
+  }
+
+  return {
+    commCommunityID,
+    blobHolder,
+  };
+}
+
+function farcasterChannelTagBlobHash(secret: string): string {
+  return `farcaster_channel_tag_${secret}`;
+}
+
+function getFarcasterChannelTagBlob(
+  secret: string,
+): Promise<BlobDownloadResult> {
+  const hash = farcasterChannelTagBlobHash(secret);
+
+  return download(hash);
+}
+
+async function uploadFarcasterChannelTagBlob(
+  commCommunityID: string,
+  farcasterChannelID: string,
+  holder: string,
+): Promise<BlobOperationResult> {
+  const payload = {
+    commCommunityID,
+    farcasterChannelID,
+  };
+  const payloadString = JSON.stringify(payload);
+
+  const hash = farcasterChannelTagBlobHash(farcasterChannelID);
+  const blob = new Blob([payloadString]);
+
+  const uploadResult = await uploadBlob(blob, hash);
+
+  if (!uploadResult.success) {
+    return uploadResult;
+  }
+
+  return await assignHolder({ holder, hash });
+}
+
+export { createOrUpdateFarcasterChannelTag, uploadFarcasterChannelTagBlob };
