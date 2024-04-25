389935dc0fba56caf553c8b97f64cc64812dd62b
Atul Madhugiri <atulm@berkeley.edu>
2024-04-25 13:55:57 -0400

    [native] Replace `threadHasPermission` with `useThreadHasPermission` in `CommunityActionsButton`
    Summary: Similar to D11786, but for `CommunityActionsButton`
    
    ---
    
    Depends on D11786
    
    Test Plan:
    
    `flow` + close reading
    
    Reviewers: ashoat, ginsu, tomek
    
    Subscribers:

--- native/components/community-actions-button.react.js
+++ native/components/community-actions-button.react.js
@@ -7,7 +7,7 @@ import { TouchableOpacity, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import { primaryInviteLinksSelector } from 'lib/selectors/invite-links-selectors.js';
-import { threadHasPermission } from 'lib/shared/thread-utils.js';
+import { useThreadHasPermission } from 'lib/shared/thread-utils.js';
 import type { ThreadInfo } from 'lib/types/minimally-encoded-thread-permissions-types.js';
 import { threadPermissions } from 'lib/types/thread-permission-types.js';
 import { threadTypes } from 'lib/types/thread-types-enum.js';
@@ -82,6 +82,15 @@ function CommunityActionsButton(props: Props): React.Node {
   const activeTheme = useSelector(state => state.globalThemeInfo.activeTheme);
   const styles = useStyles(unboundStyles);
 
+  const canManageLinks = useThreadHasPermission(
+    community,
+    threadPermissions.MANAGE_INVITE_LINKS,
+  );
+  const canChangeRoles = useThreadHasPermission(
+    community,
+    threadPermissions.CHANGE_ROLE,
+  );
+
   const { showActionSheetWithOptions } = useActionSheet();
   const actions = React.useMemo(() => {
     if (!community) {
@@ -89,10 +98,6 @@ function CommunityActionsButton(props: Props): React.Node {
     }
 
     const result = [];
-    const canManageLinks = threadHasPermission(
-      community,
-      threadPermissions.MANAGE_INVITE_LINKS,
-    );
     if (canManageLinks) {
       result.push({
         label: 'Manage invite links',
@@ -107,10 +112,6 @@ function CommunityActionsButton(props: Props): React.Node {
       });
     }
 
-    const canChangeRoles = threadHasPermission(
-      community,
-      threadPermissions.CHANGE_ROLE,
-    );
     if (canChangeRoles) {
       result.push({
         label: 'Manage roles',
@@ -136,7 +137,9 @@ function CommunityActionsButton(props: Props): React.Node {
     return null;
   }, [
     community,
+    canManageLinks,
     inviteLink,
+    canChangeRoles,
     fid,
     navigateToManagePublicLinkView,
     navigateToInviteLinksView,
