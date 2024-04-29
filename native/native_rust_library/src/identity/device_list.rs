760b1b52559e7495490be3a34cbaee44ad31590a
xsanm <kkurowski.kk@gmail.com>
2024-04-29 16:23:34 +0200

    [native] implement `GetDeviceListsForUsers` RPC in `native_rust_library`
    Summary: Added client method calling the RPC to native rust library, exposed it to C++.
    
    Test Plan: Tested in D11834
    
    Reviewers: tomek, bartek
    
    Reviewed By: tomek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D11832

--- native/native_rust_library/src/identity/device_list.rs
+++ native/native_rust_library/src/identity/device_list.rs
@@ -1,6 +1,6 @@
 use grpc_clients::identity::get_auth_client;
 use grpc_clients::identity::protos::auth::{
-  GetDeviceListRequest, UpdateDeviceListRequest,
+  GetDeviceListRequest, PeersDeviceListsRequest, UpdateDeviceListRequest,
 };
 
 use crate::identity::AuthInfo;
@@ -30,6 +30,24 @@ pub mod ffi {
       let result =
         get_device_list_for_user_helper(auth_info, user_id, since_timestamp)
           .await;
+      handle_string_result_as_callback(result, promise_id);
+    });
+  }
+
+  pub fn get_device_lists_for_users(
+    auth_user_id: String,
+    auth_device_id: String,
+    auth_access_token: String,
+    user_ids: Vec<String>,
+    promise_id: u32,
+  ) {
+    RUNTIME.spawn(async move {
+      let auth_info = AuthInfo {
+        access_token: auth_access_token,
+        user_id: auth_user_id,
+        device_id: auth_device_id,
+      };
+      let result = get_device_lists_for_users_helper(auth_info, user_ids).await;
       handle_string_result_as_callback(result, promise_id);
     });
   }
@@ -77,6 +95,29 @@ async fn get_device_list_for_user_helper(
     .into_inner();
 
   let payload = serde_json::to_string(&response.device_list_updates)?;
+  Ok(payload)
+}
+
+async fn get_device_lists_for_users_helper(
+  auth_info: AuthInfo,
+  user_ids: Vec<String>,
+) -> Result<String, Error> {
+  let mut identity_client = get_auth_client(
+    IDENTITY_SOCKET_ADDR,
+    auth_info.user_id,
+    auth_info.device_id,
+    auth_info.access_token,
+    CODE_VERSION,
+    DEVICE_TYPE.as_str_name().to_lowercase(),
+  )
+  .await?;
+
+  let response = identity_client
+    .get_device_lists_for_users(PeersDeviceListsRequest { user_ids })
+    .await?
+    .into_inner();
+
+  let payload = serde_json::to_string(&response.users_device_lists)?;
   Ok(payload)
 }
 
