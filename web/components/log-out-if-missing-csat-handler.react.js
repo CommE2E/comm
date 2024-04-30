4dcf358d2c319f9c6178fac6dde099255dd075b1
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2024-04-30 12:34:16 -0400

    [web] display modal explaining why user was logged out
    Summary:
    when web clients receive the first code version with usingCommServicesAccessToken set to true, we will need to log them out so that they can register/log in to the identity service and get a CSAT. when we log them out, we should display a modal explaining what has happened.
    
    Depends on D11705
    
    Test Plan:
    set the usingCSAT flag to true and refreshed the app. confirmed that modal was displayed and the link worked
    
    {F1683797}
    
    Reviewers: ashoat, tomek
    
    Reviewed By: ashoat
    
    Differential Revision: https://phab.comm.dev/D11844

--- web/components/log-out-if-missing-csat-handler.react.js
+++ web/components/log-out-if-missing-csat-handler.react.js
@@ -3,9 +3,41 @@
 import * as React from 'react';
 
 import { logOutActionTypes, useLogOut } from 'lib/actions/user-actions.js';
+import { useModalContext } from 'lib/components/modal-provider.react.js';
 import { useDispatchActionPromise } from 'lib/utils/redux-promise-utils.js';
 import { useSelector } from 'lib/utils/redux-utils.js';
 import { usingCommServicesAccessToken } from 'lib/utils/services-utils.js';
+
+import css from './version-handler.css';
+import Modal from '../modals/modal.react.js';
+
+function MissingCSATModal(): React.Node {
+  const { popModal } = useModalContext();
+
+  return (
+    <Modal
+      name="We&rsquo;re improving our password security"
+      onClose={popModal}
+      size="large"
+    >
+      <div className={css.modalContent}>
+        <p>
+          Unfortunately, we must log you out in order to generate a PAKE-derived
+          secret. You can learn more about PAKEs and the protocol we use
+          (OPAQUE){' '}
+          <a
+            href="https://blog.cryptographyengineering.com/2018/10/19/lets-talk-about-pake/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            here
+          </a>
+          .
+        </p>
+      </div>
+    </Modal>
+  );
+}
 
 function LogOutIfMissingCSATHandler() {
   const dispatchActionPromise = useDispatchActionPromise();
@@ -14,11 +46,20 @@ function LogOutIfMissingCSATHandler() {
   const hasAccessToken = useSelector(state => !!state.commServicesAccessToken);
   const dataLoaded = useSelector(state => state.dataLoaded);
 
+  const { pushModal } = useModalContext();
+
   React.useEffect(() => {
     if (!hasAccessToken && dataLoaded && usingCommServicesAccessToken) {
       void dispatchActionPromise(logOutActionTypes, callLogOut());
+      pushModal(<MissingCSATModal />);
     }
-  }, [callLogOut, dataLoaded, dispatchActionPromise, hasAccessToken]);
+  }, [
+    callLogOut,
+    dataLoaded,
+    dispatchActionPromise,
+    hasAccessToken,
+    pushModal,
+  ]);
 }
 
 export default LogOutIfMissingCSATHandler;
