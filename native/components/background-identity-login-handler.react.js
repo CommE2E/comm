4974b613b77755700f8ba78b371b07d1f053beee
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2024-05-01 12:29:02 -0400

    [native] handler that just logs out if missing CSAT and no password
    Summary: this is similar to the LogOutIfMissingCSATHandler but we only log out wallet users since we can't auth in the background for them
    
    Test Plan: as a wallet user on native, with usingCommServicesAccessToken set to true, i cleared the CSAT from redux and confirmed that i was logged out
    
    Reviewers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D11906

---
+++ native/components/background-identity-login-handler.react.js
@@ -0,0 +1,48 @@
+// @flow
+
+import * as React from 'react';
+
+import { logOutActionTypes, useLogOut } from 'lib/actions/user-actions.js';
+import { useModalContext } from 'lib/components/modal-provider.react.js';
+import { accountHasPassword } from 'lib/shared/account-utils.js';
+import { useDispatchActionPromise } from 'lib/utils/redux-promise-utils.js';
+import { useSelector } from 'lib/utils/redux-utils.js';
+import { usingCommServicesAccessToken } from 'lib/utils/services-utils.js';
+
+import Alert from '../utils/alert.js';
+
+function BackgroundIdentityLoginHandler() {
+  const dispatchActionPromise = useDispatchActionPromise();
+  const callLogOut = useLogOut();
+
+  const isAccountWithPassword = useSelector(state =>
+    accountHasPassword(state.currentUserInfo),
+  );
+  const hasAccessToken = useSelector(state => !!state.commServicesAccessToken);
+  const dataLoaded = useSelector(state => state.dataLoaded);
+
+  const { pushModal } = useModalContext();
+
+  React.useEffect(() => {
+    if (hasAccessToken || !dataLoaded || !usingCommServicesAccessToken) {
+      return;
+    }
+    if (!isAccountWithPassword) {
+      void dispatchActionPromise(logOutActionTypes, callLogOut());
+      Alert.alert(
+        'Security update',
+        'Unfortunately, we must log you out as we perform an update to our system.',
+        [{ text: 'OK' }],
+      );
+    }
+  }, [
+    callLogOut,
+    dataLoaded,
+    dispatchActionPromise,
+    hasAccessToken,
+    isAccountWithPassword,
+    pushModal,
+  ]);
+}
+
+export default BackgroundIdentityLoginHandler;
