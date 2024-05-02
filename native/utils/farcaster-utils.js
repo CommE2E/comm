c6d7b95517e7362114638f8d903cd2f77d042aec
Ginsu Eddy <ginsu.eddy@gmail.com>
2024-05-02 13:23:00 -0400

    [lib/native] introduce useLinkFID hook
    Summary:
    This diff introduces `useLinkFID` which is a hook that returns a callback that calls the `linkFarcasterAccount` identity rpc and then sets the `fid` into the synced metadata client store
    
    Depends on D11873
    
    Test Plan:
    Confirmed that the `fid` was being set into the synced metadata client store + confirmed in meeting with @varun that I was correctly calling and getting the expected behavior of `linkFarcasterAccount` RPC.
    
    Confirmed that loading + error states worked as expected
    
    Reviewers: will, varun, ashoat
    
    Reviewed By: ashoat
    
    Subscribers: ashoat, tomek, varun
    
    Differential Revision: https://phab.comm.dev/D11863

---
+++ native/utils/farcaster-utils.js
@@ -0,0 +1,41 @@
+// @flow
+
+import * as React from 'react';
+import { Alert } from 'react-native';
+
+import { getMessageForException } from 'lib/utils/errors.js';
+import { useLinkFID } from 'lib/utils/farcaster-utils.js';
+
+import {
+  getFarcasterAccountAlreadyLinkedAlertDetails,
+  UnknownErrorAlertDetails,
+} from './alert-messages.js';
+
+function useTryLinkFID(): (newFID: string) => Promise<void> {
+  const linkFID = useLinkFID();
+
+  return React.useCallback(
+    async (newFID: string) => {
+      try {
+        await linkFID(newFID);
+      } catch (e) {
+        if (
+          getMessageForException(e) ===
+          'farcaster ID already associated with different user'
+        ) {
+          const { title, message } =
+            getFarcasterAccountAlreadyLinkedAlertDetails();
+          Alert.alert(title, message);
+        } else {
+          Alert.alert(
+            UnknownErrorAlertDetails.title,
+            UnknownErrorAlertDetails.message,
+          );
+        }
+      }
+    },
+    [linkFID],
+  );
+}
+
+export { useTryLinkFID };
