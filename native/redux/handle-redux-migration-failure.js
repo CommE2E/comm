d966f48c6daa4ad7b6a055422172e78674f74c10
Ashoat Tevosyan <ashoat@comm.app>
2024-05-04 19:39:11 -0700

    [native] Factor out handleReduxMigrationFailure
    Summary:
    This is the `native` equivalent of D11884.
    
    In a later diff I want to call this from another file, and need to extract it in order to avoid an import cycle.
    
    Pulling `persistBlacklist` in as well to avoid an import cycle.
    
    Depends on D11885
    
    Test Plan: Flow, and the testing done elsewhere in the stack
    
    Reviewers: ginsu
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D11886

---
+++ native/redux/handle-redux-migration-failure.js
@@ -0,0 +1,41 @@
+// @flow
+
+import { wipeKeyserverStore } from 'lib/utils/keyserver-store-utils.js';
+import { resetUserSpecificState } from 'lib/utils/reducers-utils.js';
+
+import { defaultState } from './default-state.js';
+import { nonUserSpecificFieldsNative, type AppState } from './state-types.js';
+
+const persistBlacklist = [
+  'loadingStatuses',
+  'lifecycleState',
+  'dimensions',
+  'draftStore',
+  'connectivity',
+  'deviceOrientation',
+  'frozen',
+  'threadStore',
+  'storeLoaded',
+  'dbOpsStore',
+  'syncedMetadataStore',
+  'userStore',
+  'auxUserStore',
+  'commServicesAccessToken',
+];
+
+function handleReduxMigrationFailure(oldState: AppState): AppState {
+  const persistedNonUserSpecificFields = nonUserSpecificFieldsNative.filter(
+    field => !persistBlacklist.includes(field) || field === '_persist',
+  );
+  const stateAfterReset = resetUserSpecificState(
+    oldState,
+    defaultState,
+    persistedNonUserSpecificFields,
+  );
+  return {
+    ...stateAfterReset,
+    keyserverStore: wipeKeyserverStore(stateAfterReset.keyserverStore),
+  };
+}
+
+export { persistBlacklist, handleReduxMigrationFailure };
