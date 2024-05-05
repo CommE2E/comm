880b51cbda0be5743f53b432316c83581b686965
Ashoat Tevosyan <ashoat@comm.app>
2024-05-04 19:28:37 -0700

    [web] Factor out handleReduxMigrationFailure
    Summary:
    In a later diff I want to call this from another file, and need to extract it in order to avoid an import cycle.
    
    Pulling `persistWhitelist` in as well to avoid an import cycle.
    
    Depends on D11883
    
    Test Plan: Flow, and the testing done elsewhere in the stack
    
    Reviewers: ginsu
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D11884

---
+++ web/redux/handle-redux-migration-failure.js
@@ -0,0 +1,34 @@
+// @flow
+
+import { wipeKeyserverStore } from 'lib/utils/keyserver-store-utils.js';
+import { resetUserSpecificState } from 'lib/utils/reducers-utils.js';
+
+import { defaultWebState } from './default-state.js';
+import { nonUserSpecificFieldsWeb, type AppState } from './redux-setup.js';
+
+const persistWhitelist = [
+  'enabledApps',
+  'alertStore',
+  'commServicesAccessToken',
+  'keyserverStore',
+  'globalThemeInfo',
+  'customServer',
+  'messageStore',
+];
+
+function handleReduxMigrationFailure(oldState: AppState): AppState {
+  const persistedNonUserSpecificFields = nonUserSpecificFieldsWeb.filter(
+    field => persistWhitelist.includes(field) || field === '_persist',
+  );
+  const stateAfterReset = resetUserSpecificState(
+    oldState,
+    defaultWebState,
+    persistedNonUserSpecificFields,
+  );
+  return {
+    ...stateAfterReset,
+    keyserverStore: wipeKeyserverStore(stateAfterReset.keyserverStore),
+  };
+}
+
+export { persistWhitelist, handleReduxMigrationFailure };
