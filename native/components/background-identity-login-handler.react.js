79827e1baff4b33c3e0f9559ee0ffbc33fd76706
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2024-05-06 22:55:40 +0200

    [native] try to log in if native user is missing CSAT
    Summary: if a native password user is missing a CSAT (and usingCommServicesAccessToken is true), we should try to log in the identity and get a new CSAT
    
    Test Plan:
    using this [patch](https://gist.github.com/vdhanan/e1768cdec753add5d64a2d4027d8f177), i cleared the access token on native and confirmed that i was logged in to identity again and received a new access token. i confirmed that if identity is unavailable, i get logged out and the alert displays explaining why i was logged out.
    
    also confirmed that my avatar doesn't get removed, since we perform keyserver auth as part of the `usePasswordLogIn` hook
    
    Reviewers: ashoat, tomek
    
    Reviewed By: ashoat
    
    Differential Revision: https://phab.comm.dev/D12128

--- native/components/background-identity-login-handler.react.js
+++ native/components/background-identity-login-handler.react.js
@@ -3,13 +3,14 @@
 import * as React from 'react';
 
 import { logOutActionTypes, useLogOut } from 'lib/actions/user-actions.js';
-import { useModalContext } from 'lib/components/modal-provider.react.js';
+import { usePasswordLogIn } from 'lib/hooks/login-hooks.js';
 import { accountHasPassword } from 'lib/shared/account-utils.js';
 import { securityUpdateLogoutText } from 'lib/types/alert-types.js';
 import { useDispatchActionPromise } from 'lib/utils/redux-promise-utils.js';
 import { useSelector } from 'lib/utils/redux-utils.js';
 import { usingCommServicesAccessToken } from 'lib/utils/services-utils.js';
 
+import { fetchNativeKeychainCredentials } from '../account/native-credentials.js';
 import Alert from '../utils/alert.js';
 
 function BackgroundIdentityLoginHandler() {
@@ -22,26 +23,51 @@ function BackgroundIdentityLoginHandler() {
   const hasAccessToken = useSelector(state => !!state.commServicesAccessToken);
   const dataLoaded = useSelector(state => state.dataLoaded);
 
-  const { pushModal } = useModalContext();
+  const callIdentityPasswordLogIn = usePasswordLogIn();
 
-  React.useEffect(() => {
+  const handleLogOutAndAlert = React.useCallback(() => {
+    void dispatchActionPromise(logOutActionTypes, callLogOut());
+    Alert.alert('Security update', securityUpdateLogoutText, [{ text: 'OK' }]);
+  }, [dispatchActionPromise, callLogOut]);
+
+  const logInIfPossibleElseLogOut = React.useCallback(async () => {
     if (hasAccessToken || !dataLoaded || !usingCommServicesAccessToken) {
       return;
     }
+
     if (!isAccountWithPassword) {
-      void dispatchActionPromise(logOutActionTypes, callLogOut());
-      Alert.alert('Security update', securityUpdateLogoutText, [
-        { text: 'OK' },
-      ]);
+      handleLogOutAndAlert();
+      return;
+    }
+
+    const nativeCredentials = await fetchNativeKeychainCredentials();
+    if (!nativeCredentials) {
+      console.log(
+        'Native credentials missing. Cannot log in to identity service',
+      );
+      handleLogOutAndAlert();
+      return;
+    }
+
+    try {
+      await callIdentityPasswordLogIn(
+        nativeCredentials.username,
+        nativeCredentials.password,
+      );
+    } catch (e) {
+      handleLogOutAndAlert();
     }
   }, [
-    callLogOut,
+    callIdentityPasswordLogIn,
     dataLoaded,
-    dispatchActionPromise,
+    handleLogOutAndAlert,
     hasAccessToken,
     isAccountWithPassword,
-    pushModal,
   ]);
+
+  React.useEffect(() => {
+    void logInIfPossibleElseLogOut();
+  }, [logInIfPossibleElseLogOut]);
 }
 
 export default BackgroundIdentityLoginHandler;
