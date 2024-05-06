d25bcae02167a0d0e0248b54963e0c250baba81c
marcinwasowicz <marcinkwasowicz@gmail.com>
2024-05-06 12:50:39 +0200

    [Terraform] Give backup service AWS IAM role necessary to query and update backup service logs table
    Summary: Staging and production backup service doesn't have necessary permissions to update backup service logs. This is the reason why log upload and download on iOS is broken. This differential adds necessary permission so that the logs functionality is fixed on iOS.
    
    Test Plan:
    Prior to the test plan I had to delete and recreate backup-service-logs table since its schema was out of sync with TF and service source code (`logID` was still considered a string)
    1. Run terraform apply.
    2. Upload backup from iOS device.
    3. Start typing draft after uploading to trigger log upload.
    4. Log out.
    5. Log in.
    6. Restore.
    7. Ensure that draft is restored.
    
    Reviewers: bartek
    
    Reviewed By: bartek
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D11900

--- services/terraform/modules/shared/outputs.tf
+++ services/terraform/modules/shared/outputs.tf
@@ -2,6 +2,7 @@ locals {
   exported_dynamodb_tables = [
     aws_dynamodb_table.feature-flags,
     aws_dynamodb_table.backup-service-backup,
+    aws_dynamodb_table.backup-service-log,
     aws_dynamodb_table.reports-service-reports,
     aws_dynamodb_table.tunnelbroker-undelivered-messages,
     aws_dynamodb_table.identity-users,
