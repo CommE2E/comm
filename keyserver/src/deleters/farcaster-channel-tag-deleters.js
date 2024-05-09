a36a56f70ae6cef25ad027267bae3ab4f25103a1
Ginsu Eddy <ginsu.eddy@gmail.com>
2024-05-09 13:50:31 -0400

    [keyserver/lib] update deleteFarcasterChannelTag to use communites table
    Summary: Now that we have a communities table that was introduced in D11903, we can use it to better improve our deleteFarcasterChannelTag method
    
    Test Plan: Removed the farcaster channel tag and confirmed that the farcaster channel id and the blob holder were removed in the correct row of the communites table + also confirmed that the blob holding the farcaster channel <> comm community association was deleted
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D11978

--- keyserver/src/deleters/farcaster-channel-tag-deleters.js
+++ keyserver/src/deleters/farcaster-channel-tag-deleters.js
@@ -7,6 +7,7 @@ import {
 import type { DeleteFarcasterChannelTagRequest } from 'lib/types/community-types';
 import { ServerError } from 'lib/utils/errors.js';
 
+import { dbQuery, SQL } from '../database/database.js';
 import { deleteBlob } from '../services/blob.js';
 import type { Viewer } from '../session/viewer';
 
@@ -14,19 +15,47 @@ async function deleteFarcasterChannelTag(
   viewer: Viewer,
   request: DeleteFarcasterChannelTagRequest,
 ): Promise<void> {
-  const { farcasterChannelID, blobHolder } = request;
-
   if (DISABLE_TAGGING_FARCASTER_CHANNEL) {
     throw new ServerError('internal_error');
   }
 
-  await deleteBlob(
-    {
-      hash: farcasterChannelTagBlobHash(farcasterChannelID),
-      holder: blobHolder,
-    },
-    true,
-  );
+  const query = SQL`
+    START TRANSACTION;
+
+    SELECT blob_holder INTO @currentBlobHolder
+    FROM communities
+    WHERE id = ${request.commCommunityID}
+      AND farcaster_channel_id = ${request.farcasterChannelID}
+    FOR UPDATE;
+
+    UPDATE communities
+    SET
+      farcaster_channel_id = NULL,
+      blob_holder = NULL
+    WHERE id = ${request.commCommunityID}
+      AND farcaster_channel_id = ${request.farcasterChannelID};
+    
+    COMMIT;
+
+    SELECT @currentBlobHolder AS blobHolder;
+  `;
+
+  const [transactionResult] = await dbQuery(query, {
+    multipleStatements: true,
+  });
+
+  const selectResult = transactionResult.pop();
+  const [row] = selectResult;
+
+  if (row?.blobHolder) {
+    await deleteBlob(
+      {
+        hash: farcasterChannelTagBlobHash(request.farcasterChannelID),
+        holder: row.blobHolder,
+      },
+      true,
+    );
+  }
 }
 
 export { deleteFarcasterChannelTag };
