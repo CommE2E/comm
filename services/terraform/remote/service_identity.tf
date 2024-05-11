beb6293c9be8cfa16d5576494d724843ec79fda9
William Wang <williamwang749@gmail.com>
2024-05-11 13:28:39 -0400

    [terraform] display comm-lib debug logs
    Summary:
    We should include `comm-lib` as part of our debug logs for identity
    
    Test Plan:
    terraform apply
    
    Reviewers:
    bartek,
    kamil
    
    Subscribers:

--- services/terraform/remote/service_identity.tf
+++ services/terraform/remote/service_identity.tf
@@ -66,7 +66,7 @@ resource "aws_ecs_task_definition" "identity_service" {
       environment = [
         {
           name  = "RUST_LOG"
-          value = local.is_staging ? "info,identity=debug" : "info"
+          value = local.is_staging ? "info,identity=debug,comm-lib=debug" : "info"
         },
         {
           name  = "KEYSERVER_PUBLIC_KEY"
