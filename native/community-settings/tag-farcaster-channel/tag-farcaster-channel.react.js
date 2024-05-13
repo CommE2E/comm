c7ab17c0ddf9dccf10e3e858c30d2c953140baa5
Ginsu Eddy <ginsu.eddy@gmail.com>
2024-05-13 12:06:28 -0400

    [native] introduce TagChannelButton
    Summary: In subsquent diffs, I plan to introduce other logic + UI elements to the `TagFarcasterChannel` screen. In preperation for that I wanted to factor out all the tag channel button logic into it's own seperate component so that `TagFarcasterChannel` isn't too saturated
    
    Test Plan: `flow` + confirmed everything still works as expected
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D12024

--- native/community-settings/tag-farcaster-channel/tag-farcaster-channel.react.js
+++ native/community-settings/tag-farcaster-channel/tag-farcaster-channel.react.js
@@ -1,28 +1,12 @@
 // @flow
 
-import { useActionSheet } from '@expo/react-native-action-sheet';
-import invariant from 'invariant';
 import * as React from 'react';
-import { View, Text, Platform } from 'react-native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { View, Text } from 'react-native';
 
-import {
-  createOrUpdateFarcasterChannelTagActionTypes,
-  useCreateOrUpdateFarcasterChannelTag,
-} from 'lib/actions/community-actions.js';
-import { NeynarClientContext } from 'lib/components/neynar-client-provider.react.js';
-import type { NeynarChannel } from 'lib/types/farcaster-types.js';
-import { useCurrentUserFID } from 'lib/utils/farcaster-utils.js';
-import { useDispatchActionPromise } from 'lib/utils/redux-promise-utils.js';
-
+import TagChannelButton from './tag-channel-button.react.js';
 import type { TagFarcasterChannelNavigationProp } from './tag-farcaster-channel-navigator.react.js';
 import { tagFarcasterChannelErrorMessages } from './tag-farcaster-channel-utils.js';
-import RegistrationButton from '../../account/registration/registration-button.react.js';
-import {
-  TagFarcasterChannelByNameRouteName,
-  type NavigationRoute,
-} from '../../navigation/route-names.js';
-import { useSelector } from '../../redux/redux-utils.js';
+import { type NavigationRoute } from '../../navigation/route-names.js';
 import { useStyles } from '../../themes/colors.js';
 
 export type TagFarcasterChannelParams = {
@@ -35,130 +19,13 @@ type Props = {
 };
 
 function TagFarcasterChannel(props: Props): React.Node {
-  const { navigation, route } = props;
+  const { route } = props;
 
-  const { navigate } = navigation;
   const { communityID } = route.params;
 
   const styles = useStyles(unboundStyles);
 
-  const fid = useCurrentUserFID();
-  invariant(fid, 'FID should be set');
-
-  const [channelOptions, setChannelOptions] = React.useState<
-    $ReadOnlyArray<NeynarChannel>,
-  >([]);
-
   const [error, setError] = React.useState<?string>(null);
-
-  const neynarClientContext = React.useContext(NeynarClientContext);
-  invariant(neynarClientContext, 'NeynarClientContext is missing');
-
-  const { client } = neynarClientContext;
-
-  React.useEffect(() => {
-    void (async () => {
-      const channels = await client.fetchFollowedFarcasterChannels(fid);
-
-      setChannelOptions(channels);
-    })();
-  }, [client, fid]);
-
-  const activeTheme = useSelector(state => state.globalThemeInfo.activeTheme);
-
-  const { showActionSheetWithOptions } = useActionSheet();
-
-  const insets = useSafeAreaInsets();
-
-  const dispatchActionPromise = useDispatchActionPromise();
-
-  const createOrUpdateFarcasterChannelTag =
-    useCreateOrUpdateFarcasterChannelTag();
-
-  const createCreateOrUpdateActionPromise = React.useCallback(
-    async (channelID: string) => {
-      try {
-        return await createOrUpdateFarcasterChannelTag({
-          commCommunityID: communityID,
-          farcasterChannelID: channelID,
-        });
-      } catch (e) {
-        setError(e.message);
-        throw e;
-      }
-    },
-    [communityID, createOrUpdateFarcasterChannelTag],
-  );
-
-  const onOptionSelected = React.useCallback(
-    (selectedIndex: ?number) => {
-      if (
-        selectedIndex === undefined ||
-        selectedIndex === null ||
-        selectedIndex > channelOptions.length
-      ) {
-        return;
-      }
-
-      setError(null);
-
-      // This is the "Other" option
-      if (selectedIndex === channelOptions.length) {
-        navigate<'TagFarcasterChannelByName'>({
-          name: TagFarcasterChannelByNameRouteName,
-          params: { communityID },
-        });
-
-        return;
-      }
-
-      const channel = channelOptions[selectedIndex];
-
-      void dispatchActionPromise(
-        createOrUpdateFarcasterChannelTagActionTypes,
-        createCreateOrUpdateActionPromise(channel.id),
-      );
-    },
-    [
-      channelOptions,
-      communityID,
-      createCreateOrUpdateActionPromise,
-      dispatchActionPromise,
-      navigate,
-    ],
-  );
-
-  const onPressTag = React.useCallback(() => {
-    const channelNames = [
-      ...channelOptions.map(channel => `/${channel.id}`),
-      'Other',
-    ];
-
-    const options =
-      Platform.OS === 'ios' ? [...channelNames, 'Cancel'] : channelNames;
-
-    const cancelButtonIndex = Platform.OS === 'ios' ? options.length - 1 : -1;
-
-    const containerStyle = {
-      paddingBottom: insets.bottom,
-    };
-
-    showActionSheetWithOptions(
-      {
-        options,
-        cancelButtonIndex,
-        containerStyle,
-        userInterfaceStyle: activeTheme ?? 'dark',
-      },
-      onOptionSelected,
-    );
-  }, [
-    activeTheme,
-    channelOptions,
-    insets.bottom,
-    onOptionSelected,
-    showActionSheetWithOptions,
-  ]);
 
   const errorMessage = React.useMemo(() => {
     if (!error) {
@@ -182,11 +49,7 @@ function TagFarcasterChannel(props: Props): React.Node {
         </View>
         <Text style={styles.sectionHeaderText}>FARCASTER CHANNEL</Text>
         <View style={styles.panelSectionContainer}>
-          <RegistrationButton
-            onPress={onPressTag}
-            label="Tag channel"
-            variant="enabled"
-          />
+          <TagChannelButton communityID={communityID} setError={setError} />
         </View>
         <View style={styles.errorContainer}>{errorMessage}</View>
       </View>
@@ -196,7 +59,7 @@ function TagFarcasterChannel(props: Props): React.Node {
       styles.sectionText,
       styles.sectionHeaderText,
       styles.errorContainer,
-      onPressTag,
+      communityID,
       errorMessage,
     ],
   );
