de9150e6b21268260076727082a1f988e1fe9a9c
Ashoat Tevosyan <ashoat@comm.app>
2024-05-13 18:23:05 -0400

    [keyserver] Script to print largest Farcaster communities
    Summary:
    Rahul needed this for some of his work so I whipped up a quick script. Wanted to commit it to the codebase in case we need to re-run it later to get an updated list.
    
    Details in [OPS-283](https://linear.app/comm/issue/OPS-283/write-script-to-determine-largest-farcaster-channels-by-follower-count).
    
    Test Plan: I ran the script. Results are in the Linear task
    
    Reviewers: ginsu
    
    Reviewed By: ginsu
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D12020

---
+++ keyserver/src/scripts/biggest-farcaster-channels.js
@@ -0,0 +1,25 @@
+// @flow
+
+import invariant from 'invariant';
+
+import type { NeynarChannel } from 'lib/types/farcaster-types.js';
+
+import { main } from './utils.js';
+import { neynarClient } from '../utils/fc-cache.js';
+
+async function fetchAllFarcasterChannelsAndPrintSortedByFollowerCount() {
+  invariant(neynarClient, 'neynarClient should be defined');
+  const allChannels = await neynarClient.getAllChannels();
+  allChannels.sort(
+    (channelA: NeynarChannel, channelB: NeynarChannel) =>
+      channelB.follower_count - channelA.follower_count,
+  );
+  const simplifiedChannelInfo = allChannels.map(({ id, follower_count }) => ({
+    id,
+    follower_count,
+  }));
+  simplifiedChannelInfo.splice(1000);
+  console.log(JSON.stringify(simplifiedChannelInfo, undefined, '  '));
+}
+
+main([fetchAllFarcasterChannelsAndPrintSortedByFollowerCount]);
--- keyserver/src/scripts/utils.js
+++ keyserver/src/scripts/utils.js
@@ -3,6 +3,7 @@
 import { endPool } from '../database/database.js';
 import { endFirebase, endAPNs } from '../push/providers.js';
 import { publisher } from '../socket/redis.js';
+import { initFCCache } from '../utils/fc-cache.js';
 import { prefetchAllURLFacts } from '../utils/urls.js';
 
 function endScript() {
@@ -15,6 +16,7 @@ function endScript() {
 function main(functions: $ReadOnlyArray<() => Promise<mixed>>) {
   void (async () => {
     await prefetchAllURLFacts();
+    await initFCCache();
     try {
       for (const f of functions) {
         await f();
