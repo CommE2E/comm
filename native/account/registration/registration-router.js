de04b8878079f2f0a57163481a8cbfe275efeef4
Ashoat Tevosyan <ashoat@comm.app>
2024-05-13 15:52:54 -0400

    [native] Refocus ConnectEthereum if registration fails due to expired nonce
    Summary:
    Prior to this diff stack, when registration fails due to an expired nonce we would dismiss the entire `RegistrationNavigator`.
    
    This diff addresses [ENG-4087](https://linear.app/comm/issue/ENG-4087/refocus-connectethereum-if-nonce-is-expired-by-the-time-registration) by making sure we refocus the `ConnectEthereum` screen when we get an expired nonce error.
    
    I had to introduce `RegistrationRouter` here so that we could intelligently refocus `ConnectEthereum`, even if it's missing from the navigation stack.
    
    Depends on D12018
    
    Test Plan:
    I tested this diff as follows:
    
    1. Be in a multi-keyserver environment, testing SIWE with iOS simulator
    2. Do a SIWE and then wait 2 minutes to let the nonce expire in the following screens:
      - `FullscreenSIWEPanel` for an account that doesn't exist yet when the new registration flow is disabled
      - `FullscreenSIWEPanel` for an account that doesn't exist yet when the new registration flow is enabled
      - `FullscreenSIWEPanel` for an account that does exist
      - New registration flow for an account that doesn't exist yet (`RegistrationTerms`)
      - New registration flow for an account that does exist (`ExistingEthereumAccount`)
    3. Make sure there are no duplicate `Alert`s, that in all cases an `Alert` is shown, and that the "back" action activates when the user confirms the `Alert`
    
    Reviewers: inka
    
    Subscribers: varun

---
+++ native/account/registration/registration-router.js
@@ -0,0 +1,104 @@
+// @flow
+
+import type {
+  StackAction,
+  Route,
+  Router,
+  StackRouterOptions,
+  StackNavigationState,
+  RouterConfigOptions,
+} from '@react-navigation/core';
+import { StackRouter } from '@react-navigation/native';
+
+import type { ConnectEthereumParams } from './connect-ethereum.react.js';
+import { reconnectEthereumActionType } from '../../navigation/action-types.js';
+import { removeScreensFromStack } from '../../navigation/navigation-utils.js';
+import {
+  ConnectEthereumRouteName,
+  ConnectFarcasterRouteName,
+} from '../../navigation/route-names.js';
+
+type ReconnectEthereumAction = {
+  +type: 'RECONNECT_ETHEREUM',
+  +payload: {
+    +params: ConnectEthereumParams,
+  },
+};
+export type RegistrationRouterNavigationAction =
+  | StackAction
+  | ReconnectEthereumAction;
+
+export type RegistrationRouterExtraNavigationHelpers = {
+  +reconnectEthereum: ConnectEthereumParams => void,
+};
+
+function RegistrationRouter(
+  routerOptions: StackRouterOptions,
+): Router<StackNavigationState, RegistrationRouterNavigationAction> {
+  const {
+    getStateForAction: baseGetStateForAction,
+    actionCreators: baseActionCreators,
+    ...rest
+  } = StackRouter(routerOptions);
+  return {
+    ...rest,
+    getStateForAction: (
+      lastState: StackNavigationState,
+      action: RegistrationRouterNavigationAction,
+      options: RouterConfigOptions,
+    ) => {
+      if (action.type === reconnectEthereumActionType) {
+        // First, we look to see if ConnectEthereum is already in the stack.
+        // If it is, we'll pop all later screens and navigate to it.
+        const routeNames = lastState.routes.map(({ name }) => name);
+        if (routeNames.includes(ConnectEthereumRouteName)) {
+          const newState = removeScreensFromStack(
+            lastState,
+            (route: Route<>) =>
+              route.name === ConnectEthereumRouteName ? 'break' : 'remove',
+          );
+          const connectEthereumRoute = newState.routes[newState.index];
+          const restRoutes = newState.routes.slice(0, -1);
+          return {
+            ...newState,
+            routes: [
+              ...restRoutes,
+              {
+                ...connectEthereumRoute,
+                params: action.payload.params,
+              },
+            ],
+          };
+        }
+        // If it's not in the stack, we'll pop up to ConnectFarcaster
+        // and then push a new route
+        const newState = removeScreensFromStack(lastState, (route: Route<>) =>
+          route.name === ConnectFarcasterRouteName ? 'break' : 'remove',
+        );
+        return {
+          ...newState,
+          routes: [
+            ...newState.routes,
+            {
+              name: ConnectEthereumRouteName,
+              key: 'ReconnectEthereum',
+              params: action.payload.params,
+            },
+          ],
+          index: newState.index + 1,
+        };
+      } else {
+        return baseGetStateForAction(lastState, action, options);
+      }
+    },
+    actionCreators: {
+      ...baseActionCreators,
+      reconnectEthereum: (params: ConnectEthereumParams) => ({
+        type: reconnectEthereumActionType,
+        payload: { params },
+      }),
+    },
+  };
+}
+
+export default RegistrationRouter;
