88cb88c4c7ee1fce278fff14b0af3b3c3aa63c6b
Ginsu Eddy <ginsu.eddy@gmail.com>
2024-05-15 03:01:18 -0400

    [native] introduce AutoJoinCommunityHandler
    Summary: `AutoJoinCommunityHandler` handles the logic for getting the user to automatically join communites that are tagged to farcaster channels that the user follows. In a subsquent diff we will update this logic so we background these communites/threads by default
    
    Test Plan: Tagged a channel and then created a new account (who had my fid hardcoded), saw that the new account able to join the community that was tagged
    
    Reviewers:
    
    Subscribers:

--- native/root.react.js
+++ native/root.react.js
@@ -46,6 +46,7 @@ import { BottomSheetProvider } from './bottom-sheet/bottom-sheet-provider.react.
 import ChatContextProvider from './chat/chat-context-provider.react.js';
 import MessageEditingContextProvider from './chat/message-editing-context-provider.react.js';
 import AccessTokenHandler from './components/access-token-handler.react.js';
+import { AutoJoinCommunityHandler } from './components/auto-join-community-handler.react.js';
 import ConnectFarcasterAlertHandler from './components/connect-farcaster-alert-handler.react.js';
 import { FeatureFlagsProvider } from './components/feature-flags-provider.react.js';
 import PersistedStateGate from './components/persisted-state-gate.js';
@@ -340,6 +341,7 @@ function Root() {
                                                 <PrekeysHandler />
                                                 <ReportHandler />
                                                 <FarcasterDataHandler />
+                                                <AutoJoinCommunityHandler />
                                               </PersistedStateGate>
                                               {navigation}
                                             </RegistrationContextProvider>
