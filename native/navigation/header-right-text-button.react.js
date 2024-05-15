5a7e335edc3b6c9307456ab89626d6b75a421d57
Ashoat Tevosyan <ashoat@comm.app>
2024-05-15 11:48:06 -0400

    [native] Disable AddKeyserver button while keyserver check is in progress
    Summary:
    We should never ever introduce an interface without considering loading state. This was a huge omission in the initial work on this project.
    
    Depends on D12054
    
    Test Plan:
    Added a sleep for this video:
    
    {F1769557}
    
    Reviewers: ginsu
    
    Reviewed By: ginsu
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D12055

--- native/navigation/header-right-text-button.react.js
+++ native/navigation/header-right-text-button.react.js
@@ -8,16 +8,18 @@ import { useStyles } from '../themes/colors.js';
 type Props = {
   +label: string,
   +onPress: () => mixed,
+  +disabled?: boolean,
 };
 
 function HeaderRightTextButton(props: Props): React.Node {
-  const { label, onPress } = props;
+  const { label, onPress, disabled } = props;
 
   const styles = useStyles(unboundStyles);
+  const textStyle = disabled ? styles.disabledTextStyle : styles.textStyle;
 
   return (
-    <TouchableOpacity onPress={onPress}>
-      <Text style={styles.textStyle}>{label}</Text>
+    <TouchableOpacity onPress={onPress} disabled={disabled}>
+      <Text style={textStyle}>{label}</Text>
     </TouchableOpacity>
   );
 }
@@ -25,6 +27,11 @@ function HeaderRightTextButton(props: Props): React.Node {
 const unboundStyles = {
   textStyle: {
     color: 'purpleLink',
+    fontSize: 16,
+    marginRight: 10,
+  },
+  disabledTextStyle: {
+    color: 'disabledButtonText',
     fontSize: 16,
     marginRight: 10,
   },
