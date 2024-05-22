c1b774a3938620dda70a8690ffc3134b330602a5
Ashoat Tevosyan <ashoat@comm.app>
2024-05-22 11:42:20 +0200

    [native] Move StateContainer in LoggedOutModal to function component
    Summary: As a first step towards replacing the Reanimated 1 syntax in `LoggedOutModal` with Reanimated 2 syntax ([ENG-8146](https://linear.app/comm/issue/ENG-8146/convert-loggedoutmodal-to-reanimated-2-syntax)), I'm converting the class component to a function component.
    
    Test Plan:
    I tested this diff stack by playing around with `LoggedOutModal` on my iOS simulator in my local environment:
    
    1. I navigated to `LogInPanel` and back
    2. I navigated to `LegacyRegisterPanel` and back
    3. I navigated to `FullscreenSIWEPanel` and back
    4. I navigated to `QRCodeSignInNavigator` and back
    5. I navigated to `Registration` and back
    
    Reviewers: inka
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D12180

--- native/utils/state-container.js
+++ native/utils/state-container.js
@@ -1,27 +1,13 @@
 // @flow
 
-export type SimpleStateSetter<S: { ... }> = (
+type SimpleStateSetter<S: { ... }> = (
   newState: Partial<S>,
   callback?: () => mixed,
 ) => void;
 
 export type StateChange<S: { ... }> = Partial<S> | (S => Partial<S>);
-type StateSetter<S: { ... }> = (
-  newState: StateChange<S>,
-  callback?: () => mixed,
-) => void;
 
 export type StateContainer<S: { ... }> = {
   state: S,
   setState: SimpleStateSetter<S>,
 };
-
-function setStateForContainer<FullState: { ... }, OurContainer: { ... }>(
-  setState: StateSetter<FullState>,
-  reverseSelector: (ourChange: Partial<OurContainer>) => StateChange<FullState>,
-): SimpleStateSetter<OurContainer> {
-  return (ourChange: Partial<OurContainer>, callback?: () => mixed) =>
-    setState(reverseSelector(ourChange), callback);
-}
-
-export { setStateForContainer };
