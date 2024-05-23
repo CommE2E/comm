84ed5f6bbfd44b14a1d5d9ac37adbcb7dde82db6
xsanm <kkurowski.kk@gmail.com>
2024-05-23 13:05:28 +0200

    [native] update `olm` API to allow creating session without OTKs
    Summary:
    Modifying `CryptoModule` API.
    
    Depends on D12201
    
    Test Plan: Tested in D12204.
    
    Reviewers: marcin, tomek
    
    Reviewed By: tomek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D12202

--- native/cpp/CommonCpp/NativeModules/CommCoreModule.h
+++ native/cpp/CommonCpp/NativeModules/CommCoreModule.h
@@ -102,7 +102,7 @@ class CommCoreModule : public facebook::react::CommCoreModuleSchemaCxxSpecJSI {
       jsi::String identityKeys,
       jsi::String prekey,
       jsi::String prekeySignature,
-      jsi::String oneTimeKeys,
+      std::optional<jsi::String> oneTimeKey,
       jsi::String keyserverID) override;
   virtual jsi::Value
   isNotificationsSessionInitialized(jsi::Runtime &rt) override;
@@ -120,7 +120,7 @@ class CommCoreModule : public facebook::react::CommCoreModuleSchemaCxxSpecJSI {
       jsi::String identityKeys,
       jsi::String prekey,
       jsi::String prekeySignature,
-      jsi::String oneTimeKeys,
+      std::optional<jsi::String> oneTimeKey,
       jsi::String deviceID) override;
   virtual jsi::Value initializeContentInboundSession(
       jsi::Runtime &rt,
--- native/schema/CommCoreModuleSchema.js
+++ native/schema/CommCoreModuleSchema.js
@@ -64,7 +64,7 @@ interface Spec extends TurboModule {
     identityKeys: string,
     prekey: string,
     prekeySignature: string,
-    oneTimeKey: string,
+    oneTimeKey: ?string,
     keyserverID: string,
   ) => Promise<string>;
   +isNotificationsSessionInitialized: () => Promise<boolean>;
@@ -81,7 +81,7 @@ interface Spec extends TurboModule {
     identityKeys: string,
     prekey: string,
     prekeySignature: string,
-    oneTimeKey: string,
+    oneTimeKey: ?string,
     deviceID: string,
   ) => Promise<OutboundSessionCreationResult>;
   +initializeContentInboundSession: (
