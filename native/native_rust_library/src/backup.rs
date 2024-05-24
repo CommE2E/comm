469592410cf989bcdf553e41bc8ac7df8ff0a306
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2024-05-24 16:33:40 +0200

    [native_rust_library] fix serialization of BackupKeysResult
    Summary:
    the validator in secondary-device-qr-code-scanner-react.js expects the field names to be camelCase
    
    Depends on D12211
    
    Test Plan:
    ```
          const backupKeys = assertWithValidator<BackupKeys>(
            JSON.parse(backupKeysResponse),
            backupKeysValidator,
          );
    ```
    this assertion in secondary-device-qr-code-scanner-react.js was failing, but now succeeds
    
    Reviewers: bartek, kamil
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D12212

--- native/native_rust_library/src/backup.rs
+++ native/native_rust_library/src/backup.rs
@@ -439,6 +439,7 @@ fn get_user_identity_from_secure_store() -> Result<UserIdentity, cxx::Exception>
   })
 }
 
+// This struct should match `BackupKeys` in `lib/types/backup-types.js`
 #[derive(Debug, Serialize)]
 #[serde(rename_all = "camelCase")]
 struct BackupKeysResult {
