20642f71b64d78f392344bc3d56c4c13f193541f
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2024-05-29 23:01:25 +0200

    [identity][protos] Add RPC to update PlatformDetails
    Summary:
    
    Addresses [[ https://linear.app/comm/issue/ENG-8280/implement-identity-service-endoint-to-update-platformdetails | ENG-8280 ]].
    This RPC is going to be called when client has been updated.
    
    No inputs or outputs are needed - all required data is sent in gRPC metadata.
    
    Depends on D12273
    
    Test Plan:
    
    Patched native_rust_library to pass custom code version value, instead of `CODE_VERSION` constant. After each of the following actions,
    verified DDB for code version changes:
    - Called other RPCs (important: for already existing devices)- no change
    - Called `SyncPlatformDetails` RPC - the value was updated
    
    Reviewers: ashoat!, marcin, varun
    
    Subscribers:

--- web/protobufs/identity-auth-client.cjs
+++ web/protobufs/identity-auth-client.cjs
@@ -1176,5 +1176,66 @@ proto.identity.auth.IdentityClientServicePromiseClient.prototype.findUserIdentit
 };
 
 
+/**
+ * @const
+ * @type {!grpc.web.MethodDescriptor<
+ *   !proto.identity.unauth.Empty,
+ *   !proto.identity.unauth.Empty>}
+ */
+const methodDescriptor_IdentityClientService_SyncPlatformDetails = new grpc.web.MethodDescriptor(
+  '/identity.auth.IdentityClientService/SyncPlatformDetails',
+  grpc.web.MethodType.UNARY,
+  identity_unauth_pb.Empty,
+  identity_unauth_pb.Empty,
+  /**
+   * @param {!proto.identity.unauth.Empty} request
+   * @return {!Uint8Array}
+   */
+  function(request) {
+    return request.serializeBinary();
+  },
+  identity_unauth_pb.Empty.deserializeBinary
+);
+
+
+/**
+ * @param {!proto.identity.unauth.Empty} request The
+ *     request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @param {function(?grpc.web.RpcError, ?proto.identity.unauth.Empty)}
+ *     callback The callback function(error, response)
+ * @return {!grpc.web.ClientReadableStream<!proto.identity.unauth.Empty>|undefined}
+ *     The XHR Node Readable Stream
+ */
+proto.identity.auth.IdentityClientServiceClient.prototype.syncPlatformDetails =
+    function(request, metadata, callback) {
+  return this.client_.rpcCall(this.hostname_ +
+      '/identity.auth.IdentityClientService/SyncPlatformDetails',
+      request,
+      metadata || {},
+      methodDescriptor_IdentityClientService_SyncPlatformDetails,
+      callback);
+};
+
+
+/**
+ * @param {!proto.identity.unauth.Empty} request The
+ *     request proto
+ * @param {?Object<string, string>=} metadata User defined
+ *     call metadata
+ * @return {!Promise<!proto.identity.unauth.Empty>}
+ *     Promise that resolves to the response
+ */
+proto.identity.auth.IdentityClientServicePromiseClient.prototype.syncPlatformDetails =
+    function(request, metadata) {
+  return this.client_.unaryCall(this.hostname_ +
+      '/identity.auth.IdentityClientService/SyncPlatformDetails',
+      request,
+      metadata || {},
+      methodDescriptor_IdentityClientService_SyncPlatformDetails);
+};
+
+
 module.exports = proto.identity.auth;
 
