cf047ef1cab7db1e1bad32a4f9eeb8e644fbde20
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2024-06-02 11:55:37 +0200

    [identity] Add /inbound_keys HTTP handler
    Summary:
    Added HTTP endpoint for inbound keys. Request and response format described in [[ https://linear.app/comm/issue/ENG-8290/call-the-http-endpoint-to-fetch-inbound-keys-from-the-nse#comment-c6deb0c3 | ENG-8290 ]].
    
    Added some utility traits to leave handler with only business logic.
    
    Depends on D12300, D12303
    
    Test Plan:
    Registered a user with a few devices.
    - Called `GetInboundKeysForUser` RPC
    - Called `/inbound_keys` HTTP endpoint
    Compared results.
    
    Reviewers: marcin!, varun, will
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D12304

--- services/identity/src/http/mod.rs
+++ services/identity/src/http/mod.rs
@@ -1,7 +1,8 @@
-use hyper::{Body, Request, Response};
+use hyper::{Body, Method, Request, Response};
 
 mod errors;
 mod handlers;
+mod utils;
 
 type HttpRequest = Request<Body>;
 type HttpResponse = Response<Body>;
@@ -12,14 +13,23 @@ type ErrorResponse = Result<HttpResponse, errors::BoxedError>;
 #[tracing::instrument(skip_all, name = "http_request", fields(request_id))]
 pub(super) async fn handle_http_request(
   req: HttpRequest,
-  _db_client: crate::DatabaseClient,
+  db_client: crate::DatabaseClient,
 ) -> Result<HttpResponse, crate::websockets::errors::BoxedError> {
+  use utils::IntoServerResponse;
+
   tracing::Span::current()
     .record("request_id", uuid::Uuid::new_v4().to_string());
 
   let response = match req.uri().path() {
     "/health" => Response::new(Body::from("OK")),
+    "/device_inbound_keys" => match req.method() {
+      &Method::GET => handlers::inbound_keys_handler(req, db_client)
+        .await
+        .into_response()?,
+      _ => errors::http405()?,
+    },
     _ => errors::http404("Not found")?,
   };
+
   Ok(response)
 }
