4ea5a44a36bcd2ddc645b560dbb36d1159564644
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2024-06-02 11:55:37 +0200

    [identity] Add /inbound_keys HTTP handler
    Summary:
    Added HTTP endpoint for inbound keys. Request and response format described in [[ https://linear.app/comm/issue/ENG-8290/call-the-http-endpoint-to-fetch-inbound-keys-from-the-nse#comment-c6deb0c3 | ENG-8290 ]].
    
    Added some utility traits to leave handler with only business logic.
    
    Depends on D12300, D12303
    
    Test Plan:
    Registered a user with a few devices.
    - Called `GetInboundKeysForUser` RPC
    - Called `/inbound_keys` HTTP endpoint
    Compared results.
    
    Reviewers: marcin!, varun, will
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D12304

--- services/identity/src/http/handlers.rs
+++ services/identity/src/http/handlers.rs
@@ -1,11 +1,42 @@
 use super::{
-  errors::{create_error_response, http400},
-  ErrorResponse, HttpRequest,
+  errors::{create_error_response, http400, http404},
+  utils::{RequestExt, ResponseExt},
+  ErrorResponse, HttpRequest, HttpResponse,
 };
 use comm_lib::auth::UserIdentity;
 use hyper::header::AUTHORIZATION;
 use hyper::StatusCode;
+use serde_json::json;
 use tracing::error;
+
+#[tracing::instrument(skip_all)]
+pub async fn inbound_keys_handler(
+  req: HttpRequest,
+  db_client: crate::DatabaseClient,
+) -> Result<HttpResponse, ErrorResponse> {
+  verify_csat(&req, &db_client).await?;
+
+  let query_args = req.query_string_args();
+  let user_id = query_args
+    .get("user_id")
+    .ok_or_else(|| http400("missing user_id query param"))?;
+
+  let devices_map = db_client
+    .get_keys_for_user(user_id, false)
+    .await?
+    .ok_or_else(|| http404("user not found"))?;
+
+  let identifier = db_client
+    .get_user_identity(user_id)
+    .await?
+    .ok_or_else(|| http404("user not found"))?;
+
+  let response_json = json!({
+    "devices": devices_map,
+    "identity": identifier
+  });
+  HttpResponse::from_json(&response_json)
+}
 
 #[tracing::instrument(skip_all)]
 async fn verify_csat(
