1d99533bbd8b1b414c677bb1c7ef9a082f115154
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2024-06-02 11:55:37 +0200

    [identity] Add /inbound_keys HTTP handler
    Summary:
    
    Added HTTP endpoint for inbound keys. Request and response format described in [[ https://linear.app/comm/issue/ENG-8290/call-the-http-endpoint-to-fetch-inbound-keys-from-the-nse#comment-c6deb0c3 | ENG-8290 ]].
    
    Added some utility traits to leave handler with only business logic.
    
    Depends on D12300, D12303
    
    Test Plan:
    
    Registered a user with a few devices.
    - Called `GetInboundKeysForUser` RPC
    - Called `/inbound_keys` HTTP endpoint
    Compared results.
    
    Reviewers: marcin!, varun, will
    
    Subscribers:

---
+++ services/identity/src/http/utils.rs
@@ -0,0 +1,64 @@
+use hyper::{header::CONTENT_TYPE, Body, Response};
+use std::collections::HashMap;
+use tracing::error;
+
+use super::{
+  errors::{http500, BoxedError},
+  ErrorResponse, HttpRequest, HttpResponse,
+};
+
+pub trait RequestExt {
+  fn query_string_args(&self) -> HashMap<String, String>;
+}
+
+pub trait ResponseExt {
+  fn from_json<T: ?Sized + serde::Serialize>(
+    response: &T,
+  ) -> Result<HttpResponse, ErrorResponse>;
+}
+
+impl RequestExt for HttpRequest {
+  fn query_string_args(&self) -> HashMap<String, String> {
+    let Some(uri_str) = self.uri().query() else {
+      return HashMap::new();
+    };
+    let params: HashMap<_, _> = url::form_urlencoded::parse(uri_str.as_bytes())
+      .into_owned()
+      .collect();
+
+    tracing::trace!("Found query string args: {:?}", params);
+    params
+  }
+}
+
+impl ResponseExt for Response<Body> {
+  fn from_json<T: ?Sized + serde::Serialize>(
+    body: &T,
+  ) -> Result<HttpResponse, ErrorResponse> {
+    let json_string = serde_json::to_string(&body).map_err(|err| {
+      error!("JSON serialization error: {err:?}");
+      http500()
+    })?;
+    let response = Response::builder()
+      .header(CONTENT_TYPE, "application/json")
+      .body(Body::from(json_string))
+      .map_err(|err| ErrorResponse::Err(Box::new(err)))?;
+    Ok(response)
+  }
+}
+
+pub trait IntoServerResponse {
+  /// Convenience helper for converting handler return value
+  /// into response returned by server future
+  fn into_response(self) -> Result<HttpResponse, BoxedError>;
+}
+
+impl IntoServerResponse for Result<HttpResponse, ErrorResponse> {
+  fn into_response(self) -> Result<HttpResponse, BoxedError> {
+    let response = match self {
+      Ok(ok_response) => ok_response,
+      Err(err_response) => err_response?,
+    };
+    Ok(response)
+  }
+}
