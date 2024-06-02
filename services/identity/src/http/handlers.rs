a2cf0abbb89e255e404ea3e34334d0a2849740ee
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2024-06-02 11:55:37 +0200

    [identity] Add /inbound_keys HTTP handler
    Summary:
    Added HTTP endpoint for inbound keys. Request and response format described in [[ https://linear.app/comm/issue/ENG-8290/call-the-http-endpoint-to-fetch-inbound-keys-from-the-nse#comment-c6deb0c3 | ENG-8290 ]].
    
    Added some utility traits to leave handler with only business logic.
    
    Depends on D12300, D12303
    
    Test Plan:
    Registered a user with a few devices.
    - Called `GetInboundKeysForUser` RPC
    - Called `/inbound_keys` HTTP endpoint
    Compared results.
    
    Reviewers: marcin, varun, will
    
    Reviewed By: marcin, varun
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D12304

--- services/identity/src/http/handlers.rs
+++ services/identity/src/http/handlers.rs
@@ -1,11 +1,34 @@
 use super::{
-  errors::{create_error_response, http400},
-  ErrorResponse, HttpRequest,
+  errors::{create_error_response, http400, http404},
+  utils::{RequestExt, ResponseExt},
+  ErrorResponse, HttpRequest, HttpResponse,
 };
 use comm_lib::auth::UserIdentity;
 use hyper::header::AUTHORIZATION;
 use hyper::StatusCode;
 use tracing::error;
+
+#[tracing::instrument(skip_all)]
+pub async fn inbound_keys_handler(
+  req: HttpRequest,
+  db_client: crate::DatabaseClient,
+) -> Result<HttpResponse, ErrorResponse> {
+  verify_csat(&req, &db_client).await?;
+
+  // Device ID query string arg is base64-url encoded
+  let query_args = req.query_string_args();
+  let device_id_param = query_args
+    .get("device_id")
+    .ok_or_else(|| http400("missing device_id query param"))?;
+  let device_id = device_id_param.replace('-', "+").replace('_', "/");
+
+  let device_data = db_client
+    .find_device_by_id(&device_id)
+    .await?
+    .ok_or_else(|| http404("device not found"))?;
+
+  HttpResponse::json(&device_data)
+}
 
 #[tracing::instrument(skip_all)]
 async fn verify_csat(
