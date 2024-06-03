575fbcf657ce2b2bcf89f11e87eac3ae90ed9e15
William Wang <williamwang749@gmail.com>
2024-06-03 09:19:22 -0400

    [terraform] configure intial sops setup
    Summary:
    This sets up sops for our self-host keyserver config.
    secrets are stored in `keyserver_secrets.json`
    
    Test Plan:
    `terraform apply` and outputted secrets from `keyserver_secrets.json` like so:
    ```
    output "my_secret" {
      value = nonsensitive(local.secrets["mariaDB"])
    }
    ```
    
    Depends on D12277
    
    Reviewers: bartek, varun
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D12297

--- services/terraform/self-host/main.tf
+++ services/terraform/self-host/main.tf
@@ -7,6 +7,16 @@ terraform {
   }
 }
 
+provider "sops" {}
+
+data "sops_file" "keyserver_secrets_json" {
+  source_file = "keyserver_secrets.json"
+}
+
+locals {
+  secrets = jsondecode(data.sops_file.keyserver_secrets_json.raw)
+}
+
 provider "aws" {
   region = "us-east-2"
 
