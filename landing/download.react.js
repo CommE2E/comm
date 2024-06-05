17b03857e48fb40682ac41f5ea0278d80e049309
Ginsu Eddy <ginsu.eddy@gmail.com>
2024-06-05 10:00:55 -0700

    [landing] introduce download page
    Summary: This diff introduces the download page where a user can press the button of the platform they are using to download the app on to thier device
    
    Test Plan: Please see the screenshots below
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D12583

---
+++ landing/download.react.js
@@ -0,0 +1,96 @@
+// @flow
+
+import {
+  faAppStoreIos,
+  faGooglePlay,
+  faApple,
+  faWindows,
+} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames';
+import * as React from 'react';
+
+import Button from './button.react.js';
+import css from './download.css';
+import typography from './typography.css';
+
+const onClickiOS = () => {
+  window.open(
+    'https://apps.apple.com/us/app/comm-messenger/id1574433435',
+    '_blank',
+  );
+};
+
+const onClickAndroid = () => {
+  window.open(
+    'https://play.google.com/store/apps/details?id=app.comm.android',
+    '_blank',
+  );
+};
+
+const onClickMacOS = () => {
+  window.open(
+    'https://electron-update.commtechnologies.org/download/dmg',
+    '_blank',
+  );
+};
+
+const onClickWindows = () => {
+  window.open(
+    'https://electron-update.commtechnologies.org/download/windows',
+    '_blank',
+  );
+};
+
+function Download(): React.Node {
+  const headerClassName = classNames([typography.heading1, css.heading]);
+  const subheaderClassName = classNames([
+    typography.subheading2,
+    css.subheading,
+  ]);
+  const platformHeaderTextClassName = classNames([
+    typography.heading4,
+    css.platfromHeader,
+  ]);
+
+  return (
+    <div className={css.container}>
+      <h1 className={headerClassName}>Download Comm</h1>
+      <h2 className={subheaderClassName}>
+        Note that account creation is only possible on mobile.
+      </h2>
+      <p className={platformHeaderTextClassName}>Mobile</p>
+      <div className={css.buttonsContainer}>
+        <Button onClick={onClickiOS}>
+          <div className={css.buttonContentContainer}>
+            <FontAwesomeIcon icon={faAppStoreIos} size="lg" />
+            <div className={typography.paragraph1}>iOS</div>
+          </div>
+        </Button>
+        <Button onClick={onClickAndroid}>
+          <div className={css.buttonContentContainer}>
+            <FontAwesomeIcon icon={faGooglePlay} size="lg" />
+            <div className={typography.paragraph1}>Android</div>
+          </div>
+        </Button>
+      </div>
+      <p className={platformHeaderTextClassName}>Desktop</p>
+      <div className={css.buttonsContainer}>
+        <Button onClick={onClickMacOS}>
+          <div className={css.buttonContentContainer}>
+            <FontAwesomeIcon icon={faApple} size="lg" />
+            <div className={typography.paragraph1}>macOS</div>
+          </div>
+        </Button>
+        <Button onClick={onClickWindows}>
+          <div className={css.buttonContentContainer}>
+            <FontAwesomeIcon icon={faWindows} size="lg" />
+            <div className={typography.paragraph1}>Windows</div>
+          </div>
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default Download;
