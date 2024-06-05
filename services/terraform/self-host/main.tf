cf0b369f6505a71734064a5dbcd55d09b8d4e3fb
William Wang <williamwang749@gmail.com>
2024-06-05 19:17:14 -0400

    [terraform] remove usage of s3 bucket for terraform config
    Summary: After discussing with Ashoat, we initially don't want to set up with an S3 bucket storing terraform state.
    
    Test Plan: terraform apply was successful after a reinit of terraform
    
    Reviewers: ashoat, varun, bartek
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D12323

--- services/terraform/self-host/main.tf
+++ services/terraform/self-host/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  backend "s3" {
-    region  = "us-east-2"
-    key     = "terraform.tfstate"
-    bucket  = "self-host-keyserver-terraform"
-    encrypt = true
-  }
-}
-
 provider "sops" {}
 
 data "sops_file" "keyserver_secrets_json" {
