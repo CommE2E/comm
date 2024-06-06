a55bfbb5cf802b9a5e0a9cc127f03daabe561172
William Wang <williamwang749@gmail.com>
2024-06-05 20:02:54 -0400

    [terraform] Replace all config options with .tfvars and remove sops
    Summary:
    Bartek pointed out that SOPS was overkill for self-hosting. Additionally, several configuration options required the user to make terraform changes.
    
    In this diff, I introduce the usage of terraform variables to
    1. configure mariadb username/password that was previously configured through sops
    2. configure the aws region, subnets, and user ip
    
    Test Plan: terraform apply and successfully connected `mysql` client to rds endpoint
    
    Reviewers: bartek, varun
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D12324

--- services/terraform/self-host/aws_vpc.tf
+++ services/terraform/self-host/aws_vpc.tf
@@ -6,17 +6,17 @@ resource "aws_vpc" "default" {
 }
 
 # Public Subnets
-resource "aws_subnet" "public_a" {
+resource "aws_subnet" "public_1" {
   vpc_id                  = aws_vpc.default.id
   cidr_block              = "172.31.0.0/20"
-  availability_zone       = "us-east-2a"
+  availability_zone       = var.availability_zone_1
   map_public_ip_on_launch = true
 }
 
-resource "aws_subnet" "public_b" {
+resource "aws_subnet" "public_2" {
   vpc_id                  = aws_vpc.default.id
   cidr_block              = "172.31.16.0/20"
-  availability_zone       = "us-east-2b"
+  availability_zone       = var.availability_zone_2
   map_public_ip_on_launch = true
 }
 
@@ -30,25 +30,25 @@ resource "aws_route_table" "public_igw_route_table" {
   vpc_id = aws_vpc.default.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block = "${var.allowed_ip}/32"
     gateway_id = aws_internet_gateway.default.id
   }
 }
 
-resource "aws_route_table_association" "public_a_igw_route_association" {
-  subnet_id      = aws_subnet.public_a.id
+resource "aws_route_table_association" "public_1_igw_route_association" {
+  subnet_id      = aws_subnet.public_1.id
   route_table_id = aws_route_table.public_igw_route_table.id
 }
 
-resource "aws_route_table_association" "public_b_igw_route_association" {
-  subnet_id      = aws_subnet.public_b.id
+resource "aws_route_table_association" "public_2_igw_route_association" {
+  subnet_id      = aws_subnet.public_2.id
   route_table_id = aws_route_table.public_igw_route_table.id
 }
 
 # DB Subnet Group
 resource "aws_db_subnet_group" "public_db_subnet_group" {
   name       = "public-db-subnet-group"
-  subnet_ids = [aws_subnet.public_a.id, aws_subnet.public_b.id]
+  subnet_ids = [aws_subnet.public_1.id, aws_subnet.public_2.id]
 
   tags = {
     Name = "DB subnet group associated with private vpc subnet"
--- services/terraform/self-host/main.tf
+++ services/terraform/self-host/main.tf
@@ -1,15 +1,5 @@
-provider "sops" {}
-
-data "sops_file" "keyserver_secrets_json" {
-  source_file = "keyserver_secrets.json"
-}
-
-locals {
-  secrets = jsondecode(data.sops_file.keyserver_secrets_json.raw)
-}
-
 provider "aws" {
-  region = "us-east-2"
+  region = var.region
 
   default_tags {
     tags = {
