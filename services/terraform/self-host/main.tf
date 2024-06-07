5391d6ca6ca6a4d676bcc26db64275b059d5a3ce
William Wang <williamwang749@gmail.com>
2024-06-06 22:41:24 -0400

    [terraform] Use default vpc, subnets, and internet gateway for self-host
    Summary:
    This removes the need to specify availability zones by using the vpc, subnets, and internet gateway default to each region.
    
    Depends on D12324
    
    Test Plan: Deployed set up to region specified in config. Mariadb database was accessible.
    
    Reviewers: bartek, varun
    
    Reviewed By: bartek
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D12339

--- services/terraform/self-host/main.tf
+++ services/terraform/self-host/main.tf
@@ -1,3 +1,8 @@
+locals {
+  vpc_id      = var.user_created_vpc ? aws_vpc.default[0].id : data.aws_vpc.default.id
+  vpc_subnets = var.user_created_vpc ? [aws_subnet.public_1[0].id, aws_subnet.public_2[0].id] : [data.aws_subnets.default.ids[0], data.aws_subnets.default.ids[1]]
+}
+
 provider "aws" {
   region = var.region
 
