bd2084ac0481d1bf7303d44167941c5eea333762
Ginsu Eddy <ginsu.eddy@gmail.com>
2024-06-07 11:59:09 -0700

    [landing] introduce differenation section
    Summary: This diff intorduces the new differenation section on the landing page. Since the copy for this section is not ready atm, I gated this section behind a flag that can easily be enabled/disabled
    
    Test Plan: Please see the demo video below
    
    Reviewers:
    
    Subscribers:

--- landing/app-landing.react.js
+++ landing/app-landing.react.js
@@ -4,10 +4,17 @@ import * as React from 'react';
 
 import { imageAssetMetaData, assetMetaData } from './asset-meta-data.js';
 import CompetitorComparison from './competitor-comparison.react.js';
+import DifferentiationInfo from './differentiation-info.react.js';
 import Hero from './hero.react.js';
 import InfoBlock from './info-block.react.js';
 import RequestAccess from './request-access.react.js';
 import usePreloadImageAssets from './use-pre-load-image-assets.react.js';
+
+const DISABLE_DIFFERENTIATION_INFO = false;
+
+const differentiationInfo = DISABLE_DIFFERENTIATION_INFO ? null : (
+  <DifferentiationInfo />
+);
 
 function AppLanding(): React.Node {
   usePreloadImageAssets(imageAssetMetaData);
@@ -17,6 +24,7 @@ function AppLanding(): React.Node {
   return (
     <main>
       <Hero {...hero} />
+      {differentiationInfo}
       <CompetitorComparison />
       <InfoBlock {...keyserver} />
       <InfoBlock {...team} />
