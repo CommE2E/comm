4c41696fb8641c0b8df558ac173bbf109a365420
Tomasz Palys <tomasz.palys@swmansion.com>
2024-06-07 16:25:16 +0200

    [keyserver] Add a thread column to invite links
    Summary:
    This column is used to support links to chats
    
    https://linear.app/comm/issue/ENG-8248/modify-invite-links-table-on-keyserver-to-include-an-optional-thread
    
    Test Plan:
    Run the migration and check if the column was added. Create an empty DB and check if the column was created.
    Also, checked if the index was created in both cases.
    
    Reviewers: inka, ginsu
    
    Reviewed By: ginsu
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D12351

--- keyserver/src/database/setup-db.js
+++ keyserver/src/database/setup-db.js
@@ -279,7 +279,9 @@ async function createTables() {
         expiration_time bigint(20),
         limit_of_uses int UNSIGNED,
         number_of_uses int UNSIGNED NOT NULL DEFAULT 0,
-        blob_holder char(36) CHARSET latin1
+        blob_holder char(36) CHARSET latin1,
+        thread bigint(20) DEFAULT NULL,
+        thread_role bigint(20) DEFAULT NULL
       ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
       CREATE TABLE olm_sessions (
@@ -429,7 +431,8 @@ async function createTables() {
       ALTER TABLE invite_links
         ADD PRIMARY KEY (id),
         ADD UNIQUE KEY (name),
-        ADD INDEX community_primary (community, \`primary\`);
+        ADD INDEX community_primary (community, \`primary\`),
+        ADD INDEX community_thread (community, thread);
 
       ALTER TABLE olm_sessions
         ADD PRIMARY KEY (cookie_id, is_content);
