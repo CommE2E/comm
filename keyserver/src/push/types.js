cd2d381015d8c7a5f966c9d373dbcec380d680fd
marcinwasowicz <marcinkwasowicz@gmail.com>
2024-06-11 17:05:59 +0200

    Rename cookieID -> cryptoID and deviceToken -> deliveryID
    Summary: This differential renames cookieID -> cryptoID and deviceToken -> deliveryID in keyserver and lib push related files. After moving notif generation and encryption to lib `deviceToken` and `cookieID` are no longer valid they should be replaced with something more generic.
    
    Test Plan:
    1. Flow
    2. Test all notifs for all platforms and large notifs as well.
    
    Reviewers: kamil, tomek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D12398

--- keyserver/src/push/types.js
+++ keyserver/src/push/types.js
@@ -4,7 +4,7 @@ import apn from '@parse/node-apn';
 
 export type TargetedAPNsNotification = {
   +notification: apn.Notification,
-  +deviceToken: string,
+  +deliveryID: string,
   +encryptedPayloadHash?: string,
   +encryptionOrder?: number,
 };
