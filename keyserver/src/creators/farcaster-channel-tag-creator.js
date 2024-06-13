1332d8b975dacac68e138730cda45e2bbd882914
Ginsu Eddy <ginsu.eddy@gmail.com>
2024-06-13 00:59:50 -0700

    [keyserver/lib] update farcaster channel tag blob payload to include keyserver id
    Summary:
    While working on the auto-join communities feature, I discovered an issue with the Farcaster channel tag blob hash method. The current setup allows the download of a Farcaster channel tag blob for a community not part of the keyserver.
    
    The Farcaster channel tag blob hash was composed solely of the farcasterChannelID. This means that if a user in Keyserver A creates a Farcaster channel tag for the /comm Farcaster channel, a user in Keyserver B, who also follows the /comm Farcaster channel, would receive the community info. However, this community would not be part of Keyserver B.
    
    Since communities are localized to their keyserver, we should include the keyserver ID as part of the blob hash to ensure proper download blob functionality in a multikeyserver environment.
    
    Depends on D12062
    
    Test Plan: `flow` + confirmed that the creating + updating + deleting farcaster tag all still worked
    
    Reviewers: tomek, kamil, ashoat
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D12063

--- keyserver/src/creators/farcaster-channel-tag-creator.js
+++ keyserver/src/creators/farcaster-channel-tag-creator.js
@@ -27,6 +27,7 @@ import {
   type BlobDownloadResult,
 } from '../services/blob.js';
 import { Viewer } from '../session/viewer.js';
+import { thisKeyserverID } from '../user/identity.js';
 
 async function createOrUpdateFarcasterChannelTag(
   viewer: Viewer,
@@ -36,9 +37,10 @@ async function createOrUpdateFarcasterChannelTag(
     throw new ServerError('internal_error');
   }
 
-  const [communityInfos, blobDownload] = await Promise.all([
+  const [communityInfos, blobDownload, keyserverID] = await Promise.all([
     fetchCommunityInfos(viewer, [request.commCommunityID]),
     getFarcasterChannelTagBlob(request.farcasterChannelID),
+    thisKeyserverID(),
   ]);
 
   if (communityInfos.length !== 1) {
@@ -49,10 +51,11 @@ async function createOrUpdateFarcasterChannelTag(
     throw new ServerError('already_in_use');
   }
 
+  const communityID = `${keyserverID}|${request.commCommunityID}`;
   const blobHolder = uuid.v4();
 
   const blobResult = await uploadFarcasterChannelTagBlob(
-    request.commCommunityID,
+    communityID,
     request.farcasterChannelID,
     blobHolder,
   );
