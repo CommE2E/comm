27505f18c69563372a906ac7c2120b46ac35da97
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2024-06-14 09:11:47 +0200

    [identity][websocket] Call DB client directly to verify CSAT
    Summary:
    Noticed that we're doing an unnecessary round-trip with search WebSocket auth:
    Identity WS, when received auth message, connected to itself via gRPC just to verify CSAT.
    
    Replaced this with a direct DDB call.
    
    Test Plan: Verified that identity search is still working on local clients (with local Identity).
    
    Reviewers: will, varun
    
    Reviewed By: will
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D12447

--- services/identity/src/websockets/auth.rs
+++ services/identity/src/websockets/auth.rs
@@ -1,89 +1,44 @@
-use client_proto::VerifyUserAccessTokenRequest;
-use grpc_clients::identity::{self, PlatformMetadata};
-use grpc_clients::tonic::Request;
-use identity::get_unauthenticated_client;
-use identity::protos::unauthenticated as client_proto;
 use identity_search_messages::IdentitySearchAuthMessage;
 use tracing::{debug, error};
 
-use crate::constants::{error_types, DEFAULT_IDENTITY_ENDPOINT};
+use crate::constants::error_types;
 use crate::websockets::errors::WebsocketError;
 
-const PLACEHOLDER_CODE_VERSION: u64 = 0;
-const DEVICE_TYPE: &str = "service";
-
 #[tracing::instrument(skip_all)]
-async fn verify_user_access_token(
-  user_id: &str,
-  device_id: &str,
-  access_token: &str,
-) -> Result<bool, WebsocketError> {
-  let grpc_client = get_unauthenticated_client(
-    DEFAULT_IDENTITY_ENDPOINT,
-    PlatformMetadata::new(PLACEHOLDER_CODE_VERSION, DEVICE_TYPE),
-  )
-  .await;
+pub async fn handle_auth_message(
+  db_client: &crate::DatabaseClient,
+  message: &str,
+) -> Result<(), WebsocketError> {
+  let Ok(auth_message) = serde_json::from_str(message.trim()) else {
+    error!(
+      errorType = error_types::SEARCH_LOG,
+      "Failed to parse auth message"
+    );
+    return Err(WebsocketError::InvalidMessage);
+  };
 
-  let mut grpc_client = match grpc_client {
-    Ok(grpc_client) => grpc_client,
-    Err(e) => {
+  let IdentitySearchAuthMessage {
+    user_id,
+    device_id,
+    access_token,
+  } = auth_message;
+
+  let is_valid_token = db_client
+    .verify_access_token(user_id.clone(), device_id, access_token)
+    .await
+    .map_err(|err| {
       error!(
         errorType = error_types::SEARCH_LOG,
-        "Failed to get unauthenticated client: {}", e
+        "Failed to verify user access token: {:?}", err
       );
-      return Err(WebsocketError::AuthError);
-    }
-  };
-
-  let message = VerifyUserAccessTokenRequest {
-    user_id: user_id.to_string(),
-    device_id: device_id.to_string(),
-    access_token: access_token.to_string(),
-  };
-
-  let request = Request::new(message);
-  let response = match grpc_client.verify_user_access_token(request).await {
-    Ok(response) => response,
-    Err(_) => {
-      error!(
-        errorType = error_types::SEARCH_LOG,
-        "Failed to verify user access token"
-      );
-      return Err(WebsocketError::AuthError);
-    }
-  };
-
-  Ok(response.into_inner().token_valid)
-}
-
-#[tracing::instrument(skip_all)]
-pub async fn handle_auth_message(message: &str) -> Result<(), WebsocketError> {
-  let auth_message = serde_json::from_str(message.trim());
-
-  let auth_message: IdentitySearchAuthMessage = match auth_message {
-    Ok(auth_message) => auth_message,
-    Err(_) => {
-      error!(
-        errorType = error_types::SEARCH_LOG,
-        "Failed to parse auth message"
-      );
-      return Err(WebsocketError::InvalidMessage);
-    }
-  };
-
-  let user_id = auth_message.user_id;
-  let device_id = auth_message.device_id;
-  let access_token = auth_message.access_token;
-
-  let is_valid_token =
-    verify_user_access_token(&user_id, &device_id, &access_token).await?;
+      WebsocketError::AuthError
+    })?;
 
   if is_valid_token {
     debug!("User {} authenticated", user_id);
+    Ok(())
   } else {
     debug!("User {} not authenticated", user_id);
-    return Err(WebsocketError::UnauthorizedDevice);
+    Err(WebsocketError::UnauthorizedDevice)
   }
-
-  Ok(())
 }
