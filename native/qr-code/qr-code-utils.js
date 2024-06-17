f5d67a66b7ccb6c63c985d04b913d59b75abc122
xsanm <kkurowski.kk@gmail.com>
2024-06-17 11:55:57 +0200

    [web][native] extract QR code login functions to separate files
    Summary:
    [ENG-8385](https://linear.app/comm/issue/ENG-8385/qr-code-login-is-not-working-on-master).
    
    QR code login is not working right now. As described [here](https://linear.app/comm/issue/ENG-8385/qr-code-login-is-not-working-on-master#comment-a8a82224) (Point 3) and [here](https://linear.app/comm/issue/ENG-8385/qr-code-login-is-not-working-on-master#comment-c4e8125c) the issue is, because of the fact that `useQRAuth` is unmounting before it can send `SECONDARY_DEVICE_REGISTRATION_SUCCESS`. This stack is refactoring the code to move logic to context (see [here](https://linear.app/comm/issue/ENG-8385/qr-code-login-is-not-working-on-master#comment-647d811a)).
    
    This diff purpose is to move some functions to separate places to later pass it as props to a context.
    
    Depends on D12440
    
    Test Plan: Flow
    
    Reviewers: bartek
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D12445

--- native/qr-code/qr-code-utils.js
+++ native/qr-code/qr-code-utils.js
@@ -1,6 +1,7 @@
 // @flow
 
 import { hexToUintArray } from 'lib/media/data-utils.js';
+import type { BackupKeys } from 'lib/types/backup-types.js';
 import {
   peerToPeerMessageTypes,
   type QRCodeAuthMessage,
@@ -9,13 +10,20 @@ import {
   qrCodeAuthMessagePayloadValidator,
   type QRCodeAuthMessagePayload,
 } from 'lib/types/tunnelbroker/qr-code-auth-message-types.js';
+import { getMessageForException } from 'lib/utils/errors.js';
 
 import {
   convertBytesToObj,
   convertObjToBytes,
 } from '../backup/conversion-utils.js';
-import { commUtilsModule } from '../native-modules.js';
+import { commCoreModule, commUtilsModule } from '../native-modules.js';
+import { persistConfig } from '../redux/persist.js';
 import * as AES from '../utils/aes-crypto-module.js';
+import {
+  appOutOfDateAlertDetails,
+  unknownErrorAlertDetails,
+} from '../utils/alert-messages.js';
+import Alert from '../utils/alert.js';
 
 function composeTunnelbrokerQRAuthMessage(
   encryptionKey: string,
@@ -52,4 +60,38 @@ function parseTunnelbrokerQRAuthMessage(
   return Promise.resolve(payload);
 }
 
-export { composeTunnelbrokerQRAuthMessage, parseTunnelbrokerQRAuthMessage };
+function handleSecondaryDeviceRegistrationError(error: mixed): void {
+  console.error('Secondary device registration error:', error);
+  const messageForException = getMessageForException(error);
+  if (
+    messageForException === 'client_version_unsupported' ||
+    messageForException === 'unsupported_version'
+  ) {
+    Alert.alert(
+      appOutOfDateAlertDetails.title,
+      appOutOfDateAlertDetails.message,
+    );
+  } else {
+    Alert.alert(
+      unknownErrorAlertDetails.title,
+      unknownErrorAlertDetails.message,
+    );
+  }
+}
+
+function performBackupRestore(backupKeys: BackupKeys): Promise<void> {
+  const { backupID, backupDataKey, backupLogDataKey } = backupKeys;
+  return commCoreModule.restoreBackupData(
+    backupID,
+    backupDataKey,
+    backupLogDataKey,
+    persistConfig.version.toString(),
+  );
+}
+
+export {
+  composeTunnelbrokerQRAuthMessage,
+  parseTunnelbrokerQRAuthMessage,
+  handleSecondaryDeviceRegistrationError,
+  performBackupRestore,
+};
