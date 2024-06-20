eb8b32fa7dbd0cadbcd685cdd1c9e1fbbdccfae5
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2024-06-20 08:32:55 +0200

    [comm-lib][reports] Extract 'authenticated' service trait
    Summary:
    > This diff is large, but smaller parts make no sense without each other.
    
    Introduced two entities:
    - `HttpAuthenticatedService` trait - to be implemented by services like `ReportsService` or `BlobServiceClient`.
      They do require either CSAM (UserIdentity) or a service-to-service token to work (e.g. make blob calls).
      But they can still be used in unauthenticated endpoints.
    - `Authenticated<S>` extractor. This is to be used in HTTP handler functions to get an instance of an aforementioned service.
    
    The business logic code (`FromRequest` impl) was already implemented for Reports service. I extracted it into
    the above trait and made more flexible. Added lots of in-code comments to better understand.
    
    In the next diff, this will be implemented for Backup service.
    
    Depends on D12506
    
    Test Plan:
    Local reports service:
    - Upload report with valid CSAT - use it to get User ID and for blob service requests
    - Upload report with invalid CSAT - fallback to services-token
    - Upload report with no CSAT - service-to-service token was retrieved.
    
    All reports were large enough to trigger Blob service uploads.
    
    Reviewers: kamil, varun, will
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D12507

--- services/reports/src/http/service.rs
+++ services/reports/src/http/service.rs
@@ -1,12 +1,23 @@
 use actix_web::FromRequest;
-use comm_lib::auth::{
-  is_csat_verification_disabled, AuthService, AuthorizationCredential,
+use comm_lib::{
+  auth::AuthorizationCredential,
+  http::auth_service::{Authenticated, HttpAuthenticatedService},
 };
 use std::{future::Future, pin::Pin};
-use tracing::{error, warn};
 
 use crate::service::ReportsService;
 
+impl HttpAuthenticatedService for ReportsService {
+  fn make_authenticated(
+    self,
+    auth_credential: AuthorizationCredential,
+  ) -> Self {
+    self.with_authentication(auth_credential)
+  }
+}
+
+// helper implementation to let use `ReportsService` directly in handlers,
+// without having to use `Authenticated<ReportsService>` extractor
 impl FromRequest for ReportsService {
   type Error = actix_web::Error;
   type Future = Pin<Box<dyn Future<Output = Result<Self, actix_web::Error>>>>;
@@ -16,67 +27,12 @@ impl FromRequest for ReportsService {
     req: &actix_web::HttpRequest,
     payload: &mut actix_web::dev::Payload,
   ) -> Self::Future {
-    use actix_web::error::{ErrorForbidden, ErrorInternalServerError};
-
-    let base_service =
-      req.app_data::<ReportsService>().cloned().ok_or_else(|| {
-        tracing::error!(
-          "FATAL! Failed to extract ReportsService from actix app_data. \
-      Check HTTP server configuration"
-        );
-        ErrorInternalServerError("Internal server error")
-      });
-
-    let auth_service = AuthService::from_request(req, payload).into_inner();
-    let request_auth_value =
-      AuthorizationCredential::from_request(req, payload).into_inner();
+    let service_future =
+      Authenticated::<ReportsService>::from_request(req, payload);
 
     Box::pin(async move {
-      let auth_service = auth_service?;
-      let base_service = base_service?;
-
-      let credential = request_auth_value.ok();
-
-      // This is Some if the request contains valid Authorization header
-      let auth_token = match credential {
-        Some(token @ AuthorizationCredential::UserToken(_)) => {
-          let token_valid = auth_service
-            .verify_auth_credential(&token)
-            .await
-            .map_err(|err| {
-            error!("Failed to verify access token: {err}");
-            ErrorInternalServerError("Internal server error")
-          })?;
-          if token_valid || is_csat_verification_disabled() {
-            token
-          } else {
-            warn!("Posting report with invalid credentials! Defaulting to ServicesToken...");
-            get_services_token_credential(&auth_service).await?
-          }
-        }
-        Some(_) => {
-          // Reports service shouldn't be called by other services
-          warn!("Reports service requires user authorization");
-          return Err(ErrorForbidden("Forbidden"));
-        }
-        None => {
-          // Unauthenticated requests get a service-to-service token
-          get_services_token_credential(&auth_service).await?
-        }
-      };
-      let service = base_service.with_authentication(auth_token);
-      Ok(service)
+      let service = service_future.await?;
+      Ok(service.into_inner())
     })
   }
-}
-
-async fn get_services_token_credential(
-  auth_service: &AuthService,
-) -> Result<AuthorizationCredential, actix_web::Error> {
-  let services_token =
-    auth_service.get_services_token().await.map_err(|err| {
-      error!("Failed to get services token: {err}");
-      actix_web::error::ErrorInternalServerError("Internal server error")
-    })?;
-  Ok(AuthorizationCredential::ServicesToken(services_token))
 }
