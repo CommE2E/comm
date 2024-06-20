f8d2af60a271d214e34b5667cad6045230c67613
Ginsu Eddy <ginsu.eddy@gmail.com>
2024-06-20 16:18:32 -0400

    [web] introduce TagFarcasterChannelModal
    Summary: This diff introduces the `TagFarcasterChannelModal` component. This modal will show the selected farcaster channel if there is one tag otherwise it will let the user create a tag
    
    Test Plan: Please see the screenshot below
    
    Reviewers:
    
    Subscribers:

---
+++ web/tag-farcaster-channel/tag-farcaster-channel-modal.react.js
@@ -0,0 +1,57 @@
+// @flow
+
+import * as React from 'react';
+
+import { useModalContext } from 'lib/components/modal-provider.react.js';
+import type { CommunityInfo } from 'lib/types/community-types.js';
+
+import css from './tag-farcaster-channel-modal.css';
+import Modal from '../modals/modal.react.js';
+import { useSelector } from '../redux/redux-utils.js';
+
+type Props = {
+  +communityID: string,
+};
+
+function TagFarcasterChannelModal(props: Props): React.Node {
+  const { communityID } = props;
+
+  const { popModal } = useModalContext();
+
+  const communityInfo: ?CommunityInfo = useSelector(
+    state => state.communityStore.communityInfos[communityID],
+  );
+
+  const channelNameTextContent = React.useMemo(() => {
+    if (!communityInfo?.farcasterChannelID) {
+      return (
+        <div className={css.noChannelText}>No Farcaster channel tagged</div>
+      );
+    }
+
+    return (
+      <div className={css.farcasterChannelText}>
+        /{communityInfo.farcasterChannelID}
+      </div>
+    );
+  }, [communityInfo?.farcasterChannelID]);
+
+  const tagFarcasterChannelModal = React.useMemo(
+    () => (
+      <Modal name="Tag a Farcaster channel" onClose={popModal} size="large">
+        <div className={css.modalDescription}>
+          Tag a Farcaster channel so followers can find your Comm community!
+        </div>
+        <div className={css.farcasterChannelTitle}>
+          Selected Farcaster channel:
+        </div>
+        {channelNameTextContent}
+      </Modal>
+    ),
+    [channelNameTextContent, popModal],
+  );
+
+  return tagFarcasterChannelModal;
+}
+
+export default TagFarcasterChannelModal;
