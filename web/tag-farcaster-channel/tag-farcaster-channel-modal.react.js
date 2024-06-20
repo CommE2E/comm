4e71b1f6864edfb7a9a0dd562502f5b087239e3d
Ginsu Eddy <ginsu.eddy@gmail.com>
2024-06-20 18:11:44 -0400

    [web] introduce RemoveTagButton
    Summary: This diff introduces the `RemoveTagButton`. This button is used to remove a farcaster channel tag from a community
    
    Test Plan: Confirmed that when I clicked on the button the tag was removed
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D12534

--- web/tag-farcaster-channel/tag-farcaster-channel-modal.react.js
+++ web/tag-farcaster-channel/tag-farcaster-channel-modal.react.js
@@ -5,6 +5,7 @@ import * as React from 'react';
 import { useModalContext } from 'lib/components/modal-provider.react.js';
 import type { CommunityInfo } from 'lib/types/community-types.js';
 
+import RemoveTagButton from './remove-tag-button.react.js';
 import css from './tag-farcaster-channel-modal.css';
 import Modal from '../modals/modal.react.js';
 import { useSelector } from '../redux/redux-utils.js';
@@ -22,6 +23,8 @@ function TagFarcasterChannelModal(props: Props): React.Node {
     state => state.communityStore.communityInfos[communityID],
   );
 
+  const [removeTagError, setRemoveTagError] = React.useState<?string>();
+
   const channelNameTextContent = React.useMemo(() => {
     if (!communityInfo?.farcasterChannelID) {
       return (
@@ -36,17 +39,37 @@ function TagFarcasterChannelModal(props: Props): React.Node {
     );
   }, [communityInfo?.farcasterChannelID]);
 
+  const primaryButton = React.useMemo(() => {
+    if (communityInfo?.farcasterChannelID) {
+      return (
+        <RemoveTagButton
+          communityID={communityID}
+          channelID={communityInfo.farcasterChannelID}
+          setError={setRemoveTagError}
+        />
+      );
+    }
+    // TODO: Implement TagChannelButton
+    return null;
+  }, [communityID, communityInfo?.farcasterChannelID]);
+
   const tagFarcasterChannelModal = React.useMemo(
     () => (
-      <Modal name="Tag a Farcaster channel" onClose={popModal} size="large">
+      <Modal
+        name="Tag a Farcaster channel"
+        onClose={popModal}
+        size="large"
+        primaryButton={primaryButton}
+      >
         <div className={css.modalDescription}>
           Tag a Farcaster channel so followers can find your Comm community!
         </div>
         <div className={css.farcasterChannelTitle}>Selected channel:</div>
         {channelNameTextContent}
+        <div className={css.errorMessage}>{removeTagError}</div>
       </Modal>
     ),
-    [channelNameTextContent, popModal],
+    [channelNameTextContent, popModal, primaryButton, removeTagError],
   );
 
   return tagFarcasterChannelModal;
