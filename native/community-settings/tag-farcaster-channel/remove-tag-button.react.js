85783c157c21a9a0f9375ed24409b62c84d5b4c2
Ginsu Eddy <ginsu.eddy@gmail.com>
2024-06-20 17:00:23 -0400

    [lib/native] introduce useRemoveFarcasterChannelTag
    Summary: We are going to want this logic on both `web` and `native`. Created this shared hook that factors out all the shared logic for removing a farcaster channel tag
    
    Test Plan: `flow` + confirmed no regressions with removing a farcaster channel tag on `native`
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D12532

--- native/community-settings/tag-farcaster-channel/remove-tag-button.react.js
+++ native/community-settings/tag-farcaster-channel/remove-tag-button.react.js
@@ -3,21 +3,11 @@
 import * as React from 'react';
 import { Text, ActivityIndicator } from 'react-native';
 
-import {
-  deleteFarcasterChannelTagActionTypes,
-  useDeleteFarcasterChannelTag,
-} from 'lib/actions/community-actions.js';
-import { createLoadingStatusSelector } from 'lib/selectors/loading-selectors.js';
+import { useRemoveFarcasterChannelTag } from 'lib/shared/community-utils.js';
 import type { SetState } from 'lib/types/hook-types.js';
-import { useDispatchActionPromise } from 'lib/utils/redux-promise-utils.js';
 
 import Button from '../../components/button.react.js';
-import { useSelector } from '../../redux/redux-utils.js';
 import { useStyles, useColors } from '../../themes/colors.js';
-
-const deleteFarcasterChannelTagStatusSelector = createLoadingStatusSelector(
-  deleteFarcasterChannelTagActionTypes,
-);
 
 type Props = {
   +communityID: string,
@@ -31,56 +21,24 @@ function RemoveTagButton(props: Props): React.Node {
   const styles = useStyles(unboundStyles);
   const colors = useColors();
 
-  const dispatchActionPromise = useDispatchActionPromise();
-
-  const deleteFarcasterChannelTag = useDeleteFarcasterChannelTag();
-
-  const createDeleteActionPromise = React.useCallback(async () => {
-    try {
-      return await deleteFarcasterChannelTag({
-        commCommunityID: communityID,
-        farcasterChannelID: channelID,
-      });
-    } catch (e) {
-      setError(e.message);
-      throw e;
-    }
-  }, [channelID, communityID, deleteFarcasterChannelTag, setError]);
-
-  const onPressRemoveTag = React.useCallback(() => {
-    void dispatchActionPromise(
-      deleteFarcasterChannelTagActionTypes,
-      createDeleteActionPromise(),
-    );
-  }, [createDeleteActionPromise, dispatchActionPromise]);
-
-  const deleteFarcasterChannelTagStatus = useSelector(
-    deleteFarcasterChannelTagStatusSelector,
+  const { removeTag, isLoading } = useRemoveFarcasterChannelTag(
+    communityID,
+    channelID,
+    setError,
   );
 
-  const isLoadingDeleteFarcasterChannelTag =
-    deleteFarcasterChannelTagStatus === 'loading';
-
   const buttonContent = React.useMemo(() => {
-    if (isLoadingDeleteFarcasterChannelTag) {
+    if (isLoading) {
       return (
         <ActivityIndicator size="small" color={colors.panelForegroundLabel} />
       );
     }
 
     return <Text style={styles.buttonText}>Remove tag</Text>;
-  }, [
-    colors.panelForegroundLabel,
-    isLoadingDeleteFarcasterChannelTag,
-    styles.buttonText,
-  ]);
+  }, [colors.panelForegroundLabel, isLoading, styles.buttonText]);
 
   return (
-    <Button
-      style={styles.button}
-      disabled={isLoadingDeleteFarcasterChannelTag}
-      onPress={onPressRemoveTag}
-    >
+    <Button style={styles.button} disabled={isLoading} onPress={removeTag}>
       {buttonContent}
     </Button>
   );
