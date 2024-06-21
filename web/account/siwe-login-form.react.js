CommE2E/comm

commit 2c6cd81938721ddcee59b2726a41e1f4b5ad434c

[lib][native][web] Introduce useWalletLogIn hook
Summary:
This addresses [ENG-7680](https://linear.app/comm/issue/ENG-7680/introduce-hook-for-wallet-identitykeyserver-login), [ENG-7670](https://linear.app/comm/issue/ENG-7670/call-keyserverauth-from-fullscreensiwepanel-on-native), and [ENG-7672](https://linear.app/comm/issue/ENG-7672/call-keyserverauth-from-siweloginform-on-web).

This furthers the goal of the parent task, which is to move authoritative keyserver auth from `KeyserverConnectionHandler` to the individual components responsible for login.

Depends on D11767

Test Plan:
This whole stack was tested with the following steps:

1. On native, attempting to log in with an ETH account that hasn’t been registered yet
2. On native, attemping to register with an ETH account that has already been registered
3. On native, log in with an ETH account that has already been registered
4. On native, register with an ETH account that hasn’t already been registered
5. On native, register with a password account
6. On native, log in with a password account
7. On web, attempting to log in with an ETH account that hasn’t been registered yet
8. On web, log in with an ETH account that has already been registered
9. On web, log in with a password account

Reviewers: tomek, inka

Differential Revision: https://phab.comm.dev/D11768

web/account/siwe-login-form.react.js
@@ -16,12 +16,11 @@ import {
 import {
   identityGenerateNonceActionTypes,
   useIdentityGenerateNonce,
-  identityLogInActionTypes,
-  useIdentityWalletLogIn,
 } from 'lib/actions/user-actions.js';
 import ConnectedWalletInfo from 'lib/components/connected-wallet-info.react.js';
 import SWMansionIcon from 'lib/components/swmansion-icon.react.js';
 import stores from 'lib/facts/stores.js';
+import { useWalletLogIn } from 'lib/hooks/login-hooks.js';
 import { useLegacyAshoatKeyserverCall } from 'lib/keyserver-conn/legacy-keyserver-call.js';
 import { logInExtraInfoSelector } from 'lib/selectors/account-selectors.js';
 import { createLoadingStatusSelector } from 'lib/selectors/loading-selectors.js';
@@ -79,7 +78,7 @@ function SIWELoginForm(props: SIWELoginFormProps): React.Node {
   const legacySiweAuthCall = useLegacyAshoatKeyserverCall(siweAuth);
   const logInExtraInfo = useSelector(logInExtraInfoSelector);
 
-  const identityWalletLogIn = useIdentityWalletLogIn();
+  const identityWalletLogIn = useWalletLogIn();
 
   const [siweNonce, setSIWENonce] = React.useState<?string>(null);
 
@@ -157,26 +156,25 @@ function SIWELoginForm(props: SIWELoginFormProps): React.Node {
   );
 
   const attemptIdentityWalletLogIn = React.useCallback(
-    (walletAddress: string, siweMessage: string, siweSignature: string) => {
-      return dispatchActionPromise(
-        identityLogInActionTypes,
-        (async () => {
-          try {
-            return await identityWalletLogIn(
-              walletAddress,
-              siweMessage,
-              siweSignature,
-            );
-          } catch (e) {
-            if (getMessageForException(e) === 'user not found') {
-              setError('account_does_not_exist');
-            }
-            throw e;
-          }
-        })(),
-      );
+    async (
+      walletAddress: string,
+      siweMessage: string,
+      siweSignature: string,
+    ) => {
+      try {
+        return await identityWalletLogIn(
+          walletAddress,
+          siweMessage,
+          siweSignature,
+        );
+      } catch (e) {
+        if (getMessageForException(e) === 'user not found') {
+          setError('account_does_not_exist');
+        }
+        throw e;
+      }
     },
-    [dispatchActionPromise, identityWalletLogIn],
+    [identityWalletLogIn],
   );
 
   const dispatch = useDispatch();

lib/hooks/login-hooks.js
@@ -5,6 +5,7 @@ import * as React from 'react';
 import {
   identityLogInActionTypes,
   useIdentityPasswordLogIn,
+  useIdentityWalletLogIn,
 } from '../actions/user-actions.js';
 import { useKeyserverAuth } from '../keyserver-conn/keyserver-auth.js';
 import { logInActionSources } from '../types/account-types.js';
@@ -27,21 +28,42 @@ type CurrentStep =
 
 const inactiveStep = { step: 'inactive' };
 
-function usePasswordLogIn(): (
-  username: string,
-  password: string,
-) => Promise<void> {
+type LogInInputs =
+  | {
+      +accountType: 'username',
+      +username: string,
+      +password: string,
+    }
+  | {
+      +accountType: 'ethereum',
+      +walletAddress: string,
+      +siweMessage: string,
+      +siweSignature: string,
+    };
+
+function useLogIn(): LogInInputs => Promise<void> {
   const [currentStep, setCurrentStep] =
     React.useState<CurrentStep>(inactiveStep);
 
   const identityPasswordLogIn = useIdentityPasswordLogIn();
+  const identityWalletLogIn = useIdentityWalletLogIn();
   const dispatchActionPromise = useDispatchActionPromise();
   const returnedFunc = React.useCallback(
-    (username: string, password: string) =>
+    (logInInputs: LogInInputs) =>
       new Promise<void>(
         // eslint-disable-next-line no-async-promise-executor
         async (resolve, reject) => {
-          const action = identityPasswordLogIn(username, password);
+          const action =
+            logInInputs.accountType === 'username'
+              ? identityPasswordLogIn(
+                  logInInputs.username,
+                  logInInputs.password,
+                )
+              : identityWalletLogIn(
+                  logInInputs.walletAddress,
+                  logInInputs.siweMessage,
+                  logInInputs.siweSignature,
+                );
           void dispatchActionPromise(identityLogInActionTypes, action);
           try {
             await action;
@@ -55,7 +77,7 @@ function usePasswordLogIn(): (
           }
         },
       ),
-    [dispatchActionPromise, identityPasswordLogIn],
+    [dispatchActionPromise, identityPasswordLogIn, identityWalletLogIn],
   );
 
   const keyserverAuth = useKeyserverAuth(authoritativeKeyserverID());
@@ -101,4 +123,38 @@ function usePasswordLogIn(): (
   return returnedFunc;
 }
 
-export { usePasswordLogIn };
+function usePasswordLogIn(): (
+  username: string,
+  password: string,
+) => Promise<void> {
+  const logIn = useLogIn();
+  return React.useCallback(
+    (username: string, password: string) =>
+      logIn({
+        accountType: 'username',
+        username,
+        password,
+      }),
+    [logIn],
+  );
+}
+
+function useWalletLogIn(): (
+  walletAddress: string,
+  siweMessage: string,
+  siweSignature: string,
+) => Promise<void> {
+  const logIn = useLogIn();
+  return React.useCallback(
+    (walletAddress: string, siweMessage: string, siweSignature: string) =>
+      logIn({
+        accountType: 'ethereum',
+        walletAddress,
+        siweMessage,
+        siweSignature,
+      }),
+    [logIn],
+  );
+}
+
+export { usePasswordLogIn, useWalletLogIn };

native/account/fullscreen-siwe-panel.react.js
@@ -6,6 +6,7 @@ import * as React from 'react';
 import { ActivityIndicator, View } from 'react-native';
 
 import { setDataLoadedActionType } from 'lib/actions/client-db-store-actions.js';
+import { useWalletLogIn } from 'lib/hooks/login-hooks.js';
 import { type SIWEResult, SIWEMessageTypes } from 'lib/types/siwe-types.js';
 import { ServerError } from 'lib/utils/errors.js';
 import { useDispatch } from 'lib/utils/redux-utils.js';
@@ -14,10 +15,7 @@ import { usingCommServicesAccessToken } from 'lib/utils/services-utils.js';
 import { useGetEthereumAccountFromSIWEResult } from './registration/ethereum-utils.js';
 import { RegistrationContext } from './registration/registration-context.js';
 import { enableNewRegistrationMode } from './registration/registration-types.js';
-import {
-  useLegacySIWEServerCall,
-  useIdentityWalletLogInCall,
-} from './siwe-hooks.js';
+import { useLegacySIWEServerCall } from './siwe-hooks.js';
 import SIWEPanel from './siwe-panel.react.js';
 import { commRustModule } from '../native-modules.js';
 import {
@@ -71,7 +69,7 @@ function FullscreenSIWEPanel(props: Props): React.Node {
   );
 
   const legacySiweServerCall = useLegacySIWEServerCall();
-  const identityWalletLogInCall = useIdentityWalletLogInCall();
+  const identityWalletLogInCall = useWalletLogIn();
   const successRef = React.useRef(false);
   const dispatch = useDispatch();
   const onSuccess = React.useCallback(
@@ -82,7 +80,11 @@ function FullscreenSIWEPanel(props: Props): React.Node {
           const findUserIDResponse =
             await commRustModule.findUserIDForWalletAddress(result.address);
           if (JSON.parse(findUserIDResponse).userID) {
-            await identityWalletLogInCall(result);
+            await identityWalletLogInCall(
+              result.address,
+              result.message,
+              result.signature,
+            );
           } else if (enableNewRegistrationMode) {
             await onAccountDoesNotExist(result);
           } else {

native/account/registration/existing-ethereum-account.react.js
@@ -6,6 +6,7 @@ import { Text, View } from 'react-native';
 import { setDataLoadedActionType } from 'lib/actions/client-db-store-actions.js';
 import { siweAuthActionTypes } from 'lib/actions/siwe-actions.js';
 import { useENSName } from 'lib/hooks/ens-cache.js';
+import { useWalletLogIn } from 'lib/hooks/login-hooks.js';
 import { createLoadingStatusSelector } from 'lib/selectors/loading-selectors.js';
 import type { SIWEResult } from 'lib/types/siwe-types.js';
 import { useDispatch } from 'lib/utils/redux-utils.js';
@@ -21,10 +22,7 @@ import { useSelector } from '../../redux/redux-utils.js';
 import { useStyles } from '../../themes/colors.js';
 import { UnknownErrorAlertDetails } from '../../utils/alert-messages.js';
 import Alert from '../../utils/alert.js';
-import {
-  useIdentityWalletLogInCall,
-  useLegacySIWEServerCall,
-} from '../siwe-hooks.js';
+import { useLegacySIWEServerCall } from '../siwe-hooks.js';
 
 const siweAuthLoadingStatusSelector =
   createLoadingStatusSelector(siweAuthActionTypes);
@@ -37,14 +35,18 @@ type Props = {
 };
 function ExistingEthereumAccount(props: Props): React.Node {
   const legacySiweServerCall = useLegacySIWEServerCall();
-  const identityWalletLogInCall = useIdentityWalletLogInCall();
+  const identityWalletLogInCall = useWalletLogIn();
 
   const { params } = props.route;
   const dispatch = useDispatch();
   const onProceedToLogIn = React.useCallback(async () => {
     if (usingCommServicesAccessToken) {
       try {
-        await identityWalletLogInCall(params);
+        await identityWalletLogInCall(
+          params.address,
+          params.message,
+          params.signature,
+        );
       } catch (e) {
         Alert.alert(
           UnknownErrorAlertDetails.title,

native/account/siwe-hooks.js
@@ -4,8 +4,6 @@ import * as React from 'react';
 
 import { siweAuth, siweAuthActionTypes } from 'lib/actions/siwe-actions.js';
 import {
-  identityLogInActionTypes,
-  useIdentityWalletLogIn,
   identityRegisterActionTypes,
   useIdentityWalletRegister,
 } from 'lib/actions/user-actions.js';
@@ -16,10 +14,7 @@ import type {
   LogInStartingPayload,
   LogInExtraInfo,
 } from 'lib/types/account-types.js';
-import type {
-  SIWEResult,
-  IdentityWalletRegisterInput,
-} from 'lib/types/siwe-types.js';
+import type { IdentityWalletRegisterInput } from 'lib/types/siwe-types.js';
 import { useDispatchActionPromise } from 'lib/utils/redux-promise-utils.js';
 
 import { authoritativeKeyserverID } from '../authoritative-keyserver.js';
@@ -102,20 +97,6 @@ function useLegacySIWEServerCall(): (
   );
 }
 
-function useIdentityWalletLogInCall(): SIWEResult => Promise<void> {
-  const identityWalletLogIn = useIdentityWalletLogIn();
-  const dispatchActionPromise = useDispatchActionPromise();
-  return React.useCallback(
-    async ({ address, message, signature }) => {
-      const siwePromise = identityWalletLogIn(address, message, signature);
-      void dispatchActionPromise(identityLogInActionTypes, siwePromise);
-
-      await siwePromise;
-    },
-    [dispatchActionPromise, identityWalletLogIn],
-  );
-}
-
 function useIdentityWalletRegisterCall(): IdentityWalletRegisterInput => Promise<void> {
   const identityWalletRegister = useIdentityWalletRegister();
   const dispatchActionPromise = useDispatchActionPromise();
@@ -135,8 +116,4 @@ function useIdentityWalletRegisterCall(): IdentityWalletRegisterInput => Promise
   );
 }
 
-export {
-  useLegacySIWEServerCall,
-  useIdentityWalletLogInCall,
-  useIdentityWalletRegisterCall,
-};
+export { useLegacySIWEServerCall, useIdentityWalletRegisterCall };

commit c8f17efc7168744fe81b2748cdeca742300f36d3

[web] fix uncaught exception bug
Summary: Depends on D12547

Looks like this bug was introduced in D11768. We don't need to re-throw here.

Test Plan: no longer getting an uncaught exception error when SIWE fails

Reviewers: inka, ginsu

Subscribers:

web/account/siwe-login-form.react.js
@@ -174,7 +174,7 @@ function SIWELoginForm(props: SIWELoginFormProps): React.Node {
       siweSignature: string,
     ) => {
       try {
-        return await walletLogIn(walletAddress, siweMessage, siweSignature);
+        await walletLogIn(walletAddress, siweMessage, siweSignature);
       } catch (e) {
         const messageForException = getMessageForException(e);
         if (messageForException === 'user_not_found') {
@@ -187,7 +187,6 @@ function SIWELoginForm(props: SIWELoginFormProps): React.Node {
         } else if (messageForException === 'retry_from_native') {
           setError('retry_from_native');
         }
-        throw e;
       }
     },
     [walletLogIn],