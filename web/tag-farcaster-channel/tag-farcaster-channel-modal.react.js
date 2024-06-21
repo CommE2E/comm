858f886d7de2a41684fba5c62cbdabdd399e15b1
Ginsu Eddy <ginsu.eddy@gmail.com>
2024-06-20 23:14:30 -0400

    [web] introduce CreateFarcasterChannelTagModal
    Summary: This diff introduces a new modal component called `CreateFarcasterChannelTagModal`. `CreateFarcasterChannelTagModal` handles the user flow of creating a farcaster channel tag based on the farcaster channels you are following. Once the tag is sucuessfully created then the modal will close and return the user to the `TagFarcasterChannelModal` modal
    
    Test Plan: Please see the demo video below
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D12566

--- web/tag-farcaster-channel/tag-farcaster-channel-modal.react.js
+++ web/tag-farcaster-channel/tag-farcaster-channel-modal.react.js
@@ -5,8 +5,10 @@ import * as React from 'react';
 import { useModalContext } from 'lib/components/modal-provider.react.js';
 import type { CommunityInfo } from 'lib/types/community-types.js';
 
+import CreateFarcasterChannelTagModal from './create-farcaster-channel-tag-modal.react.js';
 import RemoveTagButton from './remove-tag-button.react.js';
 import css from './tag-farcaster-channel-modal.css';
+import Button, { buttonThemes } from '../components/button.react.js';
 import Modal from '../modals/modal.react.js';
 import { useSelector } from '../redux/redux-utils.js';
 
@@ -17,13 +19,19 @@ type Props = {
 function TagFarcasterChannelModal(props: Props): React.Node {
   const { communityID } = props;
 
-  const { popModal } = useModalContext();
+  const { popModal, pushModal } = useModalContext();
 
   const communityInfo: ?CommunityInfo = useSelector(
     state => state.communityStore.communityInfos[communityID],
   );
 
   const [removeTagError, setRemoveTagError] = React.useState<?string>();
+
+  const openCreateFarcasterChannelTagModal = React.useCallback(
+    () =>
+      pushModal(<CreateFarcasterChannelTagModal communityID={communityID} />),
+    [communityID, pushModal],
+  );
 
   const channelNameTextContent = React.useMemo(() => {
     if (!communityInfo?.farcasterChannelID) {
@@ -49,9 +57,21 @@ function TagFarcasterChannelModal(props: Props): React.Node {
         />
       );
     }
-    // TODO: Implement TagChannelButton
-    return null;
-  }, [communityID, communityInfo?.farcasterChannelID]);
+
+    return (
+      <Button
+        variant="filled"
+        buttonColor={buttonThemes.standard}
+        onClick={openCreateFarcasterChannelTagModal}
+      >
+        Tag channel
+      </Button>
+    );
+  }, [
+    communityID,
+    communityInfo?.farcasterChannelID,
+    openCreateFarcasterChannelTagModal,
+  ]);
 
   const tagFarcasterChannelModal = React.useMemo(
     () => (
