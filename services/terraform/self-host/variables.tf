f94e2931681154e0e0073acf00b86adf84a3a4b2
William Wang <williamwang749@gmail.com>
2024-06-20 20:04:40 -0400

    [terraform] initial ecs setup for keyserver primary
    Summary: Drafting this for some initial feedback. This sets up ecs cluster with primary keyserver task def
    
    Test Plan: Successfully running the the keyserver task in a docker container
    
    Reviewers: varun, bartek
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D12531

--- services/terraform/self-host/variables.tf
+++ services/terraform/self-host/variables.tf
@@ -23,6 +23,21 @@ variable "allowed_ip" {
 
 variable "user_created_vpc" {
   description = "Use non-default vpc and subnets"
+}
+
+variable "keyserver_username" {
+  description = "Keyserver username"
+  type        = string
+}
+
+variable "keyserver_password" {
+  description = "Keyserver password"
+  type        = string
+  sensitive   = true
+}
+
+variable "using_identity_credentials" {
+  description = "Whether to use identity credentials to login"
   type        = bool
   default     = false
 }
@@ -37,4 +52,10 @@ variable "availability_zone_2" {
   description = "Second availability zone for vpc subnet if user created vpc"
   type        = string
   default     = "us-west-1c"
+}
+
+variable "identity_socket_address" {
+  description = "The socket address to access the identity service"
+  type        = string
+  default     = "https://identity.commtechnologies.org:50054"
 }
