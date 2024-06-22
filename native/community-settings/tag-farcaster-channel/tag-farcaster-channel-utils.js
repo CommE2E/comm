dd8efa661e3d0b7ab543d561daec95ae1245fd52
Ginsu Eddy <ginsu.eddy@gmail.com>
2024-06-22 17:41:39 -0400

    [lib/native] lift tagFarcasterChannelErrorMessages into lib
    Summary: We will need this in `web` as well
    
    Test Plan: `flow`
    
    Reviewers:
    
    Subscribers:

--- native/community-settings/tag-farcaster-channel/tag-farcaster-channel-utils.js
+++
@@ -1,8 +0,0 @@
-// @flow
-
-const tagFarcasterChannelErrorMessages: { +[string]: string } = {
-  already_in_use: 'This Farcaster channel is already tagged to a community.',
-  channel_not_found: 'Could not find a channel with the provided name.',
-};
-
-export { tagFarcasterChannelErrorMessages };
