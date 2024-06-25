27e70b61d732ce2032fd126d154df3ac653f383f
William Wang <williamwang749@gmail.com>
2024-06-24 21:32:35 -0400

    [terraform] set up keyserver load balancer
    Summary:
    This implements the load balancer for the keyserver ecs cluster, distributing keyserver requests to keyserver tasks
    
    Depends on D12559
    
    Test Plan:
    Successful load balancer health check on the primary ecs task, meaning traffic is properly sent to our fargate tasks.
    
    Was also able to send requests through my domain configured to point to the load balancer.
    
    Reviewers: bartek, kamil
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D12560

--- services/terraform/self-host/variables.tf
+++ services/terraform/self-host/variables.tf
@@ -1,3 +1,8 @@
+variable "domain_name" {
+  description = "Domain name for your keyserver"
+  type        = string
+}
+
 variable "mariadb_username" {
   description = "MariaDB username"
   type        = string
