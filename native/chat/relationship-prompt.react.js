7d85a6e1f547801e6833804405c39609ecc328af
Ginsu Eddy <ginsu.eddy@gmail.com>
2024-06-27 15:13:08 -0400

    [native] introduce loading logic to RelationshipPrompt
    Summary: This diff updates `RelationshipPrompt` to use the `RelationshipButton` component rather than the current inline buttons
    
    Test Plan: Confirmed that the relationship prompt buttons in the relationship prompt show a loading spinner when the action is in progress
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D12600

--- native/chat/relationship-prompt.react.js
+++ native/chat/relationship-prompt.react.js
@@ -2,7 +2,7 @@
 
 import Icon from '@expo/vector-icons/FontAwesome5.js';
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, ActivityIndicator } from 'react-native';
 
 import { useRelationshipPrompt } from 'lib/hooks/relationship-prompt.js';
 import type { ThreadInfo } from 'lib/types/minimally-encoded-thread-permissions-types.js';
@@ -10,9 +10,50 @@ import { userRelationshipStatus } from 'lib/types/relationship-types.js';
 import type { UserInfo } from 'lib/types/user-types.js';
 
 import Button from '../components/button.react.js';
-import { useStyles } from '../themes/colors.js';
+import { useStyles, useColors } from '../themes/colors.js';
 import { unknownErrorAlertDetails } from '../utils/alert-messages.js';
 import Alert from '../utils/alert.js';
+
+type ButtonContentContainerProps = {
+  +children: React.Node,
+  +isLoading: boolean,
+};
+
+function ButtonContentContainer(props: ButtonContentContainerProps) {
+  const { children, isLoading } = props;
+
+  const styles = useStyles(unboundStyles);
+  const colors = useColors();
+
+  const buttonContentContainerStyles = React.useMemo(() => {
+    const result = [styles.buttonContentContainer];
+
+    if (isLoading) {
+      result.push(styles.buttonLoading);
+    }
+
+    return result;
+  }, [isLoading, styles.buttonContentContainer, styles.buttonLoading]);
+
+  const loadingSpinner = React.useMemo(
+    () =>
+      isLoading ? (
+        <ActivityIndicator
+          size="small"
+          color={colors.whiteText}
+          style={styles.loadingSpinner}
+        />
+      ) : null,
+    [colors.whiteText, isLoading, styles.loadingSpinner],
+  );
+
+  return (
+    <>
+      <View style={buttonContentContainerStyles}>{children}</View>
+      {loadingSpinner}
+    </>
+  );
+}
 
 type Props = {
   +pendingPersonalThreadUserInfo: ?UserInfo,
@@ -34,6 +75,12 @@ const RelationshipPrompt: React.ComponentType<Props> = React.memo<Props>(
     const {
       otherUserInfo,
       callbacks: { blockUser, unblockUser, friendUser, unfriendUser },
+      loadingState: {
+        isLoadingBlockUser,
+        isLoadingUnblockUser,
+        isLoadingFriendUser,
+        isLoadingUnfriendUser,
+      },
     } = useRelationshipPrompt(
       threadInfo,
       onErrorCallback,
@@ -54,9 +101,15 @@ const RelationshipPrompt: React.ComponentType<Props> = React.memo<Props>(
     ) {
       return (
         <View style={styles.container}>
-          <Button style={[styles.button, styles.redButton]} onPress={blockUser}>
-            <Icon name="user-shield" size={12} color="white" />
-            <Text style={styles.buttonText}>Block User</Text>
+          <Button
+            style={[styles.button, styles.redButton]}
+            onPress={blockUser}
+            disabled={isLoadingBlockUser}
+          >
+            <ButtonContentContainer isLoading={isLoadingBlockUser}>
+              <Icon name="user-shield" size={12} color="white" />
+              <Text style={styles.buttonText}>Block user</Text>
+            </ButtonContentContainer>
           </Button>
         </View>
       );
@@ -73,9 +126,12 @@ const RelationshipPrompt: React.ComponentType<Props> = React.memo<Props>(
           <Button
             style={[styles.button, styles.greenButton]}
             onPress={unblockUser}
+            disabled={isLoadingUnblockUser}
           >
-            <Icon name="user-shield" size={12} color="white" />
-            <Text style={styles.buttonText}>Unblock User</Text>
+            <ButtonContentContainer isLoading={isLoadingUnblockUser}>
+              <Icon name="user-shield" size={12} color="white" />
+              <Text style={styles.buttonText}>Unblock user</Text>
+            </ButtonContentContainer>
           </Button>
         </View>
       );
@@ -90,16 +146,22 @@ const RelationshipPrompt: React.ComponentType<Props> = React.memo<Props>(
           <Button
             style={[styles.button, styles.greenButton]}
             onPress={friendUser}
+            disabled={isLoadingFriendUser}
           >
-            <Icon name="user-plus" size={12} color="white" />
-            <Text style={styles.buttonText}>Accept Friend Request</Text>
+            <ButtonContentContainer isLoading={isLoadingFriendUser}>
+              <Icon name="user-plus" size={12} color="white" />
+              <Text style={styles.buttonText}>Accept friend request</Text>
+            </ButtonContentContainer>
           </Button>
           <Button
             style={[styles.button, styles.redButton]}
             onPress={unfriendUser}
+            disabled={isLoadingUnfriendUser}
           >
-            <Icon name="user-slash" size={12} color="white" />
-            <Text style={styles.buttonText}>Reject Friend Request</Text>
+            <ButtonContentContainer isLoading={isLoadingUnfriendUser}>
+              <Icon name="user-slash" size={12} color="white" />
+              <Text style={styles.buttonText}>Reject friend request</Text>
+            </ButtonContentContainer>
           </Button>
         </View>
       );
@@ -113,9 +175,12 @@ const RelationshipPrompt: React.ComponentType<Props> = React.memo<Props>(
           <Button
             style={[styles.button, styles.redButton]}
             onPress={unfriendUser}
+            disabled={isLoadingUnfriendUser}
           >
-            <Icon name="user-minus" size={12} color="white" />
-            <Text style={styles.buttonText}>Withdraw Friend Request</Text>
+            <ButtonContentContainer isLoading={isLoadingUnfriendUser}>
+              <Icon name="user-minus" size={12} color="white" />
+              <Text style={styles.buttonText}>Withdraw friend request</Text>
+            </ButtonContentContainer>
           </Button>
         </View>
       );
@@ -126,13 +191,22 @@ const RelationshipPrompt: React.ComponentType<Props> = React.memo<Props>(
         <Button
           style={[styles.button, styles.greenButton]}
           onPress={friendUser}
+          disabled={isLoadingFriendUser}
         >
-          <Icon name="user-plus" size={12} color="white" />
-          <Text style={styles.buttonText}>Add Friend</Text>
+          <ButtonContentContainer isLoading={isLoadingFriendUser}>
+            <Icon name="user-plus" size={12} color="white" />
+            <Text style={styles.buttonText}>Add friend</Text>
+          </ButtonContentContainer>
         </Button>
-        <Button style={[styles.button, styles.redButton]} onPress={blockUser}>
-          <Icon name="user-shield" size={12} color="white" />
-          <Text style={styles.buttonText}>Block User</Text>
+        <Button
+          style={[styles.button, styles.redButton]}
+          onPress={blockUser}
+          disabled={isLoadingBlockUser}
+        >
+          <ButtonContentContainer isLoading={isLoadingBlockUser}>
+            <Icon name="user-shield" size={12} color="white" />
+            <Text style={styles.buttonText}>Block user</Text>
+          </ButtonContentContainer>
         </Button>
       </View>
     );
@@ -152,6 +226,7 @@ const unboundStyles = {
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'center',
+    alignItems: 'center',
     marginHorizontal: 5,
   },
   greenButton: {
@@ -160,12 +235,22 @@ const unboundStyles = {
   redButton: {
     backgroundColor: 'vibrantRedButton',
   },
+  buttonContentContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  buttonLoading: {
+    opacity: 0,
+  },
   buttonText: {
     fontSize: 11,
     color: 'white',
     fontWeight: 'bold',
     textAlign: 'center',
     marginLeft: 5,
+  },
+  loadingSpinner: {
+    position: 'absolute',
   },
 };
 
