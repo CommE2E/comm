8e1656408028a2824b4d3527360e683cfe01069e
xsanm <kkurowski.kk@gmail.com>
2024-06-28 13:58:16 +0200

    [Tunnelbroker] implement creating APNs client
    Summary:
    Implementing APNs client which will handle sending notifs.
    
    After some research I discovered that opening new connections for every request could be treated as DoS attack. However, `reqwest` can handle that, if we use the same `Client` and when making request it will use keep-alive connection pooling.
    
    Depends on D12610
    
    Test Plan: Test creating client with adding config args from CMD.
    
    Reviewers: bartek, marcin
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D12611

--- services/tunnelbroker/src/notifs/apns/mod.rs
+++ services/tunnelbroker/src/notifs/apns/mod.rs
@@ -1,7 +1,8 @@
+use crate::notifs::apns::config::APNsConfig;
 use crate::notifs::apns::token::APNsToken;
-
+use std::time::Duration;
 pub mod config;
-mod error;
+pub mod error;
 pub mod token;
 
 #[derive(Clone)]
@@ -9,4 +10,23 @@ pub struct APNsClient {
   http2_client: reqwest::Client,
   token: APNsToken,
   is_prod: bool,
+}
+
+impl APNsClient {
+  pub fn new(config: &APNsConfig) -> Result<Self, error::Error> {
+    let token_ttl = Duration::from_secs(60 * 55);
+    let token = APNsToken::new(config, token_ttl)?;
+
+    let http2_client = reqwest::Client::builder()
+      .http2_prior_knowledge()
+      .http2_keep_alive_interval(Some(Duration::from_secs(5)))
+      .http2_keep_alive_while_idle(true)
+      .build()?;
+
+    Ok(APNsClient {
+      http2_client,
+      token,
+      is_prod: config.production,
+    })
+  }
 }
