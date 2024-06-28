493327ac8540031d3c1874d21b3f240da00f729a
xsanm <kkurowski.kk@gmail.com>
2024-06-28 14:18:05 +0200

    [Tunnelbroker] implement creating APNs headers
    Summary:
    Implementing building request headers.
    
    Apple docs: [Send a POST request to APNs](https://developer.apple.com/documentation/usernotifications/sending-notification-requests-to-apns#Send-a-POST-request-to-APNs).
    
    Not sure about `PushType` - perhaps we can just use `String` but I prefer more detailed type definition. (cc. @marcin).
    
    Depends on D12611
    
    Test Plan: Tested calling this function with different params
    
    Reviewers: bartek, marcin
    
    Reviewed By: bartek, marcin
    
    Subscribers: ashoat, tomek, marcin
    
    Differential Revision: https://phab.comm.dev/D12612

--- services/tunnelbroker/src/notifs/apns/mod.rs
+++ services/tunnelbroker/src/notifs/apns/mod.rs
@@ -1,8 +1,11 @@
 use crate::notifs::apns::config::APNsConfig;
+use crate::notifs::apns::headers::{NotificationHeaders, PushType};
 use crate::notifs::apns::token::APNsToken;
+use reqwest::header::{HeaderMap, HeaderValue, AUTHORIZATION};
 use std::time::Duration;
 pub mod config;
 pub mod error;
+mod headers;
 pub mod token;
 
 #[derive(Clone)]
@@ -28,5 +31,58 @@ impl APNsClient {
       token,
       is_prod: config.production,
     })
+  }
+
+  async fn build_headers(
+    &self,
+    notif_headers: NotificationHeaders,
+  ) -> Result<HeaderMap, error::Error> {
+    let mut headers = HeaderMap::new();
+
+    headers.insert(
+      reqwest::header::CONTENT_TYPE,
+      HeaderValue::from_static("application/json"),
+    );
+
+    let bearer = self.token.get_bearer().await?;
+    let token = format!("bearer {bearer}");
+    headers.insert(AUTHORIZATION, HeaderValue::from_str(&token)?);
+
+    if let Some(apns_topic) = &notif_headers.apns_topic {
+      headers.insert("apns-topic", HeaderValue::from_str(apns_topic)?);
+    }
+
+    if let Some(apns_id) = &notif_headers.apns_id {
+      headers.insert("apns-id", HeaderValue::from_str(apns_id)?);
+    }
+
+    if let Some(push_type) = &notif_headers.apns_push_type {
+      let push_type_str = match push_type {
+        PushType::Alert => "alert",
+        PushType::Background => "background",
+        PushType::Location => "location",
+        PushType::Voip => "voip",
+        PushType::Complication => "complication",
+        PushType::FileProvider => "fileprovider",
+        PushType::Mdm => "mdm",
+        PushType::LiveActivity => "live",
+        PushType::PushToTalk => "pushtotalk",
+      };
+      headers.insert("apns-push-type", HeaderValue::from_static(push_type_str));
+    }
+
+    if let Some(expiration) = notif_headers.apns_expiration {
+      headers.insert("apns-expiration", HeaderValue::from(expiration));
+    }
+
+    if let Some(priority) = notif_headers.apns_priority {
+      headers.insert("apns-priority", HeaderValue::from(priority));
+    }
+
+    if let Some(collapse_id) = &notif_headers.apns_collapse_id {
+      headers.insert("apns-collapse-id", HeaderValue::from_str(collapse_id)?);
+    }
+
+    Ok(headers)
   }
 }
