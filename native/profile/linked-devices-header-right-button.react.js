63899045a3195533b006c7f96f40f227c723d7b3
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2024-06-28 15:45:58 -0400

    [native] modify SecondaryDeviceQRCodeScanner to support emulator
    Summary:
    if we're trying to add a device from an iOS simulator or android emulator, we should just display a TextInput where the user can paste the QR Code URL
    
    Depends on D12619
    
    Test Plan:
    if device is added successfully, we are navigated back upon acknowledging the alert
    
    if device can't be added successfully, we are navigated back when the alert fires
    
    adding keyserver as secondary device works -- appears in DDB correctly
    
    Reviewers: ashoat, bartek
    
    Reviewed By: ashoat
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D12620

--- native/profile/linked-devices-header-right-button.react.js
+++ native/profile/linked-devices-header-right-button.react.js
@@ -5,22 +5,11 @@ import * as React from 'react';
 
 import HeaderRightTextButton from '../navigation/header-right-text-button.react.js';
 import { SecondaryDeviceQRCodeScannerRouteName } from '../navigation/route-names.js';
-import Alert from '../utils/alert.js';
-import { deviceIsEmulator } from '../utils/url-utils.js';
 
 function LinkedDevicesHeaderRightButton(): React.Node {
   const { navigate } = useNavigation();
 
   const navigateToQRCodeScanner = React.useCallback(() => {
-    if (deviceIsEmulator) {
-      Alert.alert(
-        'Unsupported device',
-        "You can't access the QR code scanner on a simulator.",
-        [{ text: 'OK' }],
-        { cancelable: false },
-      );
-      return;
-    }
     navigate(SecondaryDeviceQRCodeScannerRouteName);
   }, [navigate]);
 
