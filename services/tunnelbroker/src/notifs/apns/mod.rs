abdc1a18775ff2de18a7dd7755ac8161e54999c3
xsanm <kkurowski.kk@gmail.com>
2024-06-28 13:45:08 +0200

    [Tunnelbroker] implement APNs Token generation and lifetime
    Summary:
    Implementing what is described in Apple's [Establishing a token-based connection to APNs](https://developer.apple.com/documentation/usernotifications/establishing-a-token-based-connection-to-apns).
    
    Config in tests is randomly generated.
    
    Depends on D12609
    
    Test Plan:
    Tests, later in the task tested actual communication with APNs.
    
    Reviewers: bartek, marcin

--- services/tunnelbroker/src/notifs/apns/mod.rs
+++ services/tunnelbroker/src/notifs/apns/mod.rs
@@ -1,7 +1,12 @@
+use crate::notifs::apns::token::APNsToken;
+
 pub mod config;
+mod error;
+pub mod token;
 
 #[derive(Clone)]
 pub struct APNsClient {
   http2_client: reqwest::Client,
+  token: APNsToken,
   is_prod: bool,
 }
