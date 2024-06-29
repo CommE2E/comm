c0692399434d95dff5f332d3c6ccd4014ba70a6f
Ashoat Tevosyan <ashoat@comm.app>
2024-06-29 14:34:36 -0400

    [native] Add CommSecureStore.get on main thread to resolve race with DB thread
    Summary: The code comment explains why we need it. This resolves [ENG-8069](https://linear.app/comm/issue/ENG-8069/investigate-why-securestore-can-get-corrupted-and-how-to-heal).
    
    Test Plan:
    1. Test on physical Android device. Confirm that before this diff it can reproduce [ENG-8069](https://linear.app/comm/issue/ENG-8069/investigate-why-securestore-can-get-corrupted-and-how-to-heal)
    2. Delete Comm app from phone. Deploy a release build with `yarn react-native run-android --variant=release` (may need to make sure `native/facts/authoritative_keyserver.json` is not set)
    3. Repeat the following steps 10 times:
      - Open the app and wait for LoggedOutModal to render
      - Kill the app
      - Open the app again and see if it crashes
      - Between attempts, clear the app's internal storage via settings
    
    Reviewers: marcin, tomek
    
    Subscribers:

--- native/android/app/src/main/java/app/comm/android/CommCoreJSIModulePackage.java
+++ native/android/app/src/main/java/app/comm/android/CommCoreJSIModulePackage.java
@@ -26,6 +26,13 @@ public class CommCoreJSIModulePackage implements JSIModulePackage {
     CommSecureStore.getInstance().initialize(secureStoreModuleSupplier);
     CommHybrid.initHybrid(reactApplicationContext);
 
+    // We issue a useless set on CommSecureStore here to force it to initialize
+    // prior to scheduling initializeDatabaseManager on the DB thread below.
+    // This avoids a race condition where the DB thread and main thread both
+    // attempt to initialize CommSecureStore at the same time, which can cause
+    // a crash loop as described in ENG-7696 and ENG-8069.
+    CommSecureStore.set("comm.secure_store_initialization_complete", "1");
+
     File sqliteFile = reactApplicationContext.getDatabasePath("comm.sqlite");
     GlobalDBSingleton.scheduleOrRun(() -> {
       DatabaseInitializer.initializeDatabaseManager(sqliteFile.getPath());
