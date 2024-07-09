ad7e35ae8b71dde2de1580f3f934af853dcc80ce
William Wang <williamwang749@gmail.com>
2024-07-09 16:41:58 -0400

    [terraform] Introduce secondary keyserver service
    Summary:
    This introduces the secondary keyserver service. The task is mostly identical to the primary except `COMM_NODE_ROLE` being set to
    secondary.
    
    Depends on D12710
    
    Test Plan:
    I added a temporary keyserver route that printed true/false for whether it was primary. Load balancer properly distributed traffic
    to both primary and secondary requests and was able to curl https://wyilio.com/role to get both true and false responses
    
    Reviewers: varun, bartek
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D12711

--- services/terraform/self-host/keyserver_secondary.tf
+++ services/terraform/self-host/keyserver_secondary.tf
@@ -1,21 +1,17 @@
 locals {
-  keyserver_service_image_tag      = "1.0"
-  keyserver_service_server_image   = "wyilio/keyserver:${local.keyserver_service_image_tag}"
-  keyserver_primary_container_name = "keyserver-primary"
+  keyserver_secondary_container_name = "keyserver-secondary"
 }
 
-resource "aws_cloudwatch_log_group" "keyserver_primary_service" {
-  name              = "/ecs/keyserver-primary-task-def"
+resource "aws_cloudwatch_log_group" "keyserver_secondary_service" {
+  name              = "/ecs/keyserver-secondary-task-def"
   retention_in_days = 7
 }
 
-output "mariadb_address" {
-  value = aws_db_instance.mariadb.address
-}
+resource "aws_ecs_task_definition" "keyserver_secondary_service" {
+  depends_on = [aws_ecs_service.keyserver_primary_service]
 
-resource "aws_ecs_task_definition" "keyserver_primary_service" {
   network_mode             = "awsvpc"
-  family                   = "keyserver-primary-task-def"
+  family                   = "keyserver-secondary-task-def"
   requires_compatibilities = ["FARGATE"]
   task_role_arn            = aws_iam_role.ecs_task_role.arn
   execution_role_arn       = aws_iam_role.ecs_task_execution.arn
@@ -28,7 +24,7 @@ resource "aws_ecs_task_definition" "keyserver_primary_service" {
 
   container_definitions = jsonencode([
     {
-      name      = local.keyserver_primary_container_name
+      name      = local.keyserver_secondary_container_name
       image     = local.keyserver_service_server_image
       essential = true
       portMappings = [
@@ -47,7 +43,7 @@ resource "aws_ecs_task_definition" "keyserver_primary_service" {
         },
         {
           name  = "COMM_NODE_ROLE"
-          value = "primary"
+          value = "secondary"
         },
         {
           name  = "COMM_LISTEN_ADDR"
@@ -82,12 +78,6 @@ resource "aws_ecs_task_definition" "keyserver_primary_service" {
           })
         },
         {
-          name = "COMM_JSONCONFIG_facts_webapp_cors"
-          value = jsonencode({
-            "domain" : "https://web.comm.app"
-          })
-        },
-        {
           name = "COMM_JSONCONFIG_facts_keyserver_url"
           value = jsonencode({
             "baseDomain" : "https://${var.domain_name}",
@@ -95,6 +85,12 @@ resource "aws_ecs_task_definition" "keyserver_primary_service" {
             "baseRoutePath" : "/",
             "https" : false,
             "proxy" : "none"
+          })
+        },
+        {
+          name = "COMM_JSONCONFIG_facts_webapp_cors"
+          value = jsonencode({
+            "domain" : "https://web.comm.app"
           })
         },
         {
@@ -114,7 +110,7 @@ resource "aws_ecs_task_definition" "keyserver_primary_service" {
         "logDriver" = "awslogs"
         "options" = {
           "awslogs-create-group"  = "true"
-          "awslogs-group"         = aws_cloudwatch_log_group.keyserver_primary_service.name
+          "awslogs-group"         = aws_cloudwatch_log_group.keyserver_secondary_service.name
           "awslogs-stream-prefix" = "ecs"
           "awslogs-region"        = "${var.region}"
         }
@@ -133,20 +129,17 @@ resource "aws_ecs_task_definition" "keyserver_primary_service" {
   skip_destroy = false
 }
 
-resource "aws_ecs_service" "keyserver_primary_service" {
-  depends_on = [null_resource.create_comm_database]
+resource "aws_ecs_service" "keyserver_secondary_service" {
+  depends_on = [aws_ecs_service.keyserver_primary_service]
 
-  name                               = "keyserver-primary-service"
-  cluster                            = aws_ecs_cluster.keyserver_cluster.id
-  task_definition                    = aws_ecs_task_definition.keyserver_primary_service.arn
-  launch_type                        = "FARGATE"
-  enable_execute_command             = true
-  enable_ecs_managed_tags            = true
-  force_new_deployment               = true
-  desired_count                      = 1
-  deployment_maximum_percent         = 100
-  deployment_minimum_healthy_percent = 0
-
+  name                    = "keyserver-secondary-service"
+  cluster                 = aws_ecs_cluster.keyserver_cluster.id
+  task_definition         = aws_ecs_task_definition.keyserver_secondary_service.arn
+  launch_type             = "FARGATE"
+  enable_execute_command  = true
+  enable_ecs_managed_tags = true
+  force_new_deployment    = true
+  desired_count           = 1
 
   network_configuration {
     subnets          = local.vpc_subnets
@@ -156,7 +149,7 @@ resource "aws_ecs_service" "keyserver_primary_service" {
 
   load_balancer {
     target_group_arn = aws_lb_target_group.keyserver_service.arn
-    container_name   = local.keyserver_primary_container_name
+    container_name   = local.keyserver_secondary_container_name
     container_port   = 3000
   }
 
@@ -165,38 +158,3 @@ resource "aws_ecs_service" "keyserver_primary_service" {
     rollback = true
   }
 }
-
-resource "aws_security_group" "keyserver_service" {
-  name   = "keyserver-service-ecs-sg"
-  vpc_id = local.vpc_id
-
-  # Allow all inbound traffic  on port 3000
-  ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description      = "Allow inbound traffic from any IPv6 address"
-    from_port        = 3000
-    to_port          = 3000
-    protocol         = "tcp"
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  # Allow all outbound traffic
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-
