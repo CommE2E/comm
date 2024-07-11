9add6098fd8bc0b8d0d77302308939af6c98f4e5
xsanm <kkurowski.kk@gmail.com>
2024-07-11 14:39:13 +0200

    [Tunnelbroker] implement FCM config
    Summary:
    Config to FCM similar to keyserver.
    
    Keyserver config: [link](https://github.com/CommE2E/comm/blob/ea0aac2608af297a9d96e467ed80cb2ab8f42361/keyserver/src/push/providers.js#L62).
    
    Depends on D12751
    
    Test Plan:
    1. Add config var from commandline and log it
    2. For staging going to test after deploying
    
    Reviewers: bartek, marcin
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D12752

--- services/tunnelbroker/src/notifs/mod.rs
+++ services/tunnelbroker/src/notifs/mod.rs
@@ -1,8 +1,11 @@
 use crate::notifs::apns::APNsClient;
+use crate::notifs::fcm::FCMClient;
 
 pub mod apns;
+pub mod fcm;
 
 #[derive(Clone)]
 pub struct NotifClient {
   pub(crate) apns: Option<APNsClient>,
+  pub(crate) fcm: Option<FCMClient>,
 }
