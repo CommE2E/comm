0d0e2fe5c9d5fa261ad55e1fe7e780dd804d9555
xsanm <kkurowski.kk@gmail.com>
2024-07-15 17:25:30 +0200

    [Tunnelbroker] implement OAuth 2.0 JWT token encoding
    Summary:
    Implementing what is described in Google's OAuth 2.0 for Server to Server Applications, [Forming the JWT claim set
    ](https://developers.google.com/identity/protocols/oauth2/service-account#authorizingrequests).
    
    `get_auth_bearer` is only public method, will handle generating and refreshing token.
    
    Depends on D12753
    
    Test Plan: Call `get_jwt_token` and test if return correct jwt token in format
    ```
    {Base64url encoded header}.{Base64url encoded claim set}
    ```.
    Later in the stack this token is used to make HTTP call to get access token for FCM push service.
    
    Reviewers: bartek, marcin

--- services/tunnelbroker/src/notifs/fcm/mod.rs
+++ services/tunnelbroker/src/notifs/fcm/mod.rs
@@ -1,20 +1,29 @@
 use crate::notifs::fcm::config::FCMConfig;
+use crate::notifs::fcm::token::FCMToken;
+use std::time::Duration;
 
 pub mod config;
 mod error;
+mod token;
 
 #[derive(Clone)]
 pub struct FCMClient {
   http_client: reqwest::Client,
   config: FCMConfig,
+  token: FCMToken,
 }
 
 impl FCMClient {
   pub fn new(config: &FCMConfig) -> Result<Self, error::Error> {
     let http_client = reqwest::Client::builder().build()?;
+
+    let token_ttl = Duration::from_secs(60 * 55);
+    let token = FCMToken::new(&config.clone(), token_ttl)?;
+
     Ok(FCMClient {
       http_client,
       config: config.clone(),
+      token,
     })
   }
 }
