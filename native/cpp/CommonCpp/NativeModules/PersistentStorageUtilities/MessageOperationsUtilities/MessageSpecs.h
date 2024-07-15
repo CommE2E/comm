a74646ef5299747e4175abab4c1ec066fd01e4d5
Ashoat Tevosyan <ashoat@comm.app>
2024-07-15 16:09:42 -0400

    [native] Extract MessageType to DatabaseManagers
    Summary:
    I want to reuse this from the `DatabaseManagers` codebase, which is isolated from `NativeModules` so that it can be compiled to WASM for `web`.
    
    As such, I'm moving it into that folder, where it can be accessed from the whole codebase.
    
    Depends on D12760
    
    Test Plan: Make sure it compiles
    
    Reviewers: kamil, tomek, marcin
    
    Subscribers:

--- native/cpp/CommonCpp/NativeModules/PersistentStorageUtilities/MessageOperationsUtilities/MessageSpecs.h
+++ native/cpp/CommonCpp/NativeModules/PersistentStorageUtilities/MessageOperationsUtilities/MessageSpecs.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "../../../DatabaseManagers/entities/MessageType.h"
 #include "MessageSpecs/ChangeRoleMessageSpec.h"
 #include "MessageSpecs/ChangeSettingsMessageSpec.h"
 #include "MessageSpecs/CreateEntryMessageSpec.h"
@@ -21,32 +22,6 @@
 #include <map>
 
 namespace comm {
-
-enum class MessageType {
-  TEXT,
-  CREATE_THREAD,
-  ADD_MEMBERS,
-  CREATE_SUB_THREAD,
-  CHANGE_SETTINGS,
-  REMOVE_MEMBERS,
-  CHANGE_ROLE,
-  LEAVE_THREAD,
-  JOIN_THREAD,
-  CREATE_ENTRY,
-  EDIT_ENTRY,
-  DELETE_ENTRY,
-  RESTORE_ENTRY,
-  UNSUPPORTED,
-  IMAGES,
-  MULTIMEDIA,
-  LEGACY_UPDATE_RELATIONSHIP,
-  SIDEBAR_SOURCE,
-  CREATE_SIDEBAR,
-  REACTION,
-  EDIT_MESSAGE,
-  TOGGLE_PIN,
-  UPDATE_RELATIONSHIP,
-};
 
 const std::map<MessageType, std::unique_ptr<MessageSpec>> messageSpecsHolder =
     []() {
