6ba99121f34c17be0c895834d2ba41554770fd34
xsanm <kkurowski.kk@gmail.com>
2024-07-15 17:22:08 +0200

    [Tunnelbroker] implement creating FCM client
    Summary:
    Implementing FCM client which will handle sending notifs.
    
    Depends on D12752
    
    Test Plan: Test creating client.
    
    Reviewers: bartek, marcin
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D12753

--- services/tunnelbroker/src/notifs/fcm/mod.rs
+++ services/tunnelbroker/src/notifs/fcm/mod.rs
@@ -1,6 +1,20 @@
+use crate::notifs::fcm::config::FCMConfig;
+
 pub mod config;
+mod error;
 
 #[derive(Clone)]
 pub struct FCMClient {
-  http2_client: reqwest::Client,
+  http_client: reqwest::Client,
+  config: FCMConfig,
+}
+
+impl FCMClient {
+  pub fn new(config: &FCMConfig) -> Result<Self, error::Error> {
+    let http_client = reqwest::Client::builder().build()?;
+    Ok(FCMClient {
+      http_client,
+      config: config.clone(),
+    })
+  }
 }
