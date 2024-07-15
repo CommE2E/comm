666c07b51945700d3e8ec1c233597ed2f1804df4
xsanm <kkurowski.kk@gmail.com>
2024-07-15 18:07:54 +0200

    [Tunnelbroker] implement sending FCM message
    Summary:
    Sending Firebase Message according to [docs](https://firebase.google.com/docs/reference/fcm/rest/v1/projects.messages/send).
    
    Depends on D12768
    
    Test Plan: Run this method with different params and check both error statuses and if notif is delivered to physical Android device.
    
    Reviewers: bartek, marcin
    
    Reviewed By: bartek
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D12769

--- services/tunnelbroker/src/notifs/fcm/mod.rs
+++ services/tunnelbroker/src/notifs/fcm/mod.rs
@@ -1,6 +1,12 @@
 use crate::notifs::fcm::config::FCMConfig;
+use crate::notifs::fcm::error::Error::FCMError;
+use crate::notifs::fcm::firebase_message::{FCMMessage, FCMMessageWrapper};
+use crate::notifs::fcm::response::FCMErrorResponse;
 use crate::notifs::fcm::token::FCMToken;
+use reqwest::header::{HeaderMap, HeaderValue, AUTHORIZATION};
+use reqwest::StatusCode;
 use std::time::Duration;
+use tracing::{debug, error};
 
 pub mod config;
 mod error;
@@ -29,5 +35,54 @@ impl FCMClient {
       config: config.clone(),
       token,
     })
+  }
+
+  pub async fn send(&self, message: FCMMessage) -> Result<(), error::Error> {
+    let token = message.token.clone();
+    debug!("Sending FCM notif to {}", token);
+
+    let mut headers = HeaderMap::new();
+    headers.insert(
+      reqwest::header::CONTENT_TYPE,
+      HeaderValue::from_static("application/json"),
+    );
+
+    let bearer = self.token.get_auth_bearer().await?;
+    headers.insert(AUTHORIZATION, HeaderValue::from_str(&bearer)?);
+
+    let url = format!(
+      "https://fcm.googleapis.com/v1/projects/{}/messages:send",
+      self.config.project_id
+    );
+
+    let msg_wrapper = FCMMessageWrapper { message };
+    let payload = serde_json::to_string(&msg_wrapper).unwrap();
+
+    let response = self
+      .http_client
+      .post(&url)
+      .headers(headers)
+      .body(payload)
+      .send()
+      .await?;
+
+    match response.status() {
+      StatusCode::OK => {
+        debug!("Successfully sent FCM notif to {}", token);
+        Ok(())
+      }
+      error_status => {
+        let body = response
+          .text()
+          .await
+          .unwrap_or_else(|error| format!("Error occurred: {}", error));
+        error!(
+          "Failed sending FCM notification to: {}. Status: {}. Body: {}",
+          token, error_status, body
+        );
+        let fcm_error = FCMErrorResponse::from_status(error_status, body);
+        Err(FCMError(fcm_error))
+      }
+    }
   }
 }
