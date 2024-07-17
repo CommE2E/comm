78594cba0d4cd5b7a7041b2005642a0911b5a756
Inka Sokołowska <inka.d.sokolowska@gmail.com>
2024-07-17 16:55:32 +0200

    [native] Add message search store
    Summary: issue: [[https://linear.app/comm/issue/ENG-8831/add-messagesearchstore | ENG-8831]]
    
    Test Plan: Tested with the next diff. Added an "add search message" operation on the js side and tested that updateMessageSearchIndex was run and no errors showed up in Metro nor in XCode console
    
    Reviewers: tomek, kamil, marcin
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D12795

---
+++ native/cpp/CommonCpp/NativeModules/PersistentStorageUtilities/DataStores/MessageSearchStore.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "../../../DatabaseManagers/entities/MessageSearchResult.h"
+#include "../../DBOperationBase.h"
+#include "../../MessageSearchStoreOperations.h"
+#include "BaseDataStore.h"
+
+#include <jsi/jsi.h>
+
+namespace comm {
+
+class MessageSearchStore
+    : public BaseDataStore<DBOperationBase, MessageSearchResult> {
+private:
+  static OperationType INSERT_OPERATION;
+
+public:
+  MessageSearchStore(std::shared_ptr<facebook::react::CallInvoker> jsInvoker);
+
+  std::vector<std::unique_ptr<DBOperationBase>> createOperations(
+      jsi::Runtime &rt,
+      const jsi::Array &operations) const override;
+
+  jsi::Array parseDBDataStore(
+      jsi::Runtime &rt,
+      std::shared_ptr<std::vector<MessageSearchResult>> dataVectorPtr)
+      const override;
+};
+
+} // namespace comm
