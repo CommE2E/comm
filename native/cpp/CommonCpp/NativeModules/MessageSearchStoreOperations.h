265e918720cdfbb363811f4d240eab1f79c9dcd5
Inka Sokołowska <inka.d.sokolowska@gmail.com>
2024-07-17 14:42:39 +0200

    [native] Add MessageSearchStoreOperations
    Summary:
    issue: [[https://linear.app/comm/issue/ENG-8832/add-insertsearchmessagesoperation | ENG-8832]]
    
    Test Plan:
    Tested with following diffs. Added an "add search message" operation on the js side and tested that `updateMessageSearchIndex` was run and no errors showed up in Metro nor in XCode console
    
    Reviewers:
    tomek, kamil, marcin
    Subscribers:

---
+++ native/cpp/CommonCpp/NativeModules/MessageSearchStoreOperations.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "DatabaseManager.h"
+#include <jsi/jsi.h>
+
+namespace comm {
+
+namespace jsi = facebook::jsi;
+
+class InsertSearchMessagesOperation : public DBOperationBase {
+public:
+  InsertSearchMessagesOperation(jsi::Runtime &rt, const jsi::Object &payload)
+      : originalMessageID{payload.getProperty(rt, "originalMessageID")
+                              .asString(rt)
+                              .utf8(rt)},
+        messageID{payload.getProperty(rt, "messageID").asString(rt).utf8(rt)},
+        content{payload.getProperty(rt, "content").asString(rt).utf8(rt)} {
+  }
+
+  virtual void execute() override {
+    DatabaseManager::getQueryExecutor().updateMessageSearchIndex(
+        this->originalMessageID, this->messageID, this->content);
+  }
+
+private:
+  std::string originalMessageID;
+  std::string messageID;
+  std::string content;
+};
+
+} // namespace comm
