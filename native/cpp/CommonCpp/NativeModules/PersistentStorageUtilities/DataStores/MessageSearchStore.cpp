9a5544edeb592230ee0e892b7d1eec5517c16964
Inka Sokołowska <inka.d.sokolowska@gmail.com>
2024-07-17 16:55:32 +0200

    [native] Add message search store
    Summary: issue: [[https://linear.app/comm/issue/ENG-8831/add-messagesearchstore | ENG-8831]]
    
    Test Plan: Tested with the next diff. Added an "add search message" operation on the js side and tested that updateMessageSearchIndex was run and no errors showed up in Metro nor in XCode console
    
    Reviewers: tomek, kamil, marcin
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D12795

---
+++ native/cpp/CommonCpp/NativeModules/PersistentStorageUtilities/DataStores/MessageSearchStore.cpp
@@ -0,0 +1,44 @@
+#include "MessageSearchStore.h"
+
+#include "../../DBOperationBase.h"
+#include <ReactCommon/TurboModuleUtils.h>
+#include <jsi/jsi.h>
+
+namespace comm {
+
+OperationType MessageSearchStore::INSERT_OPERATION = "insert_search_messages";
+
+MessageSearchStore::MessageSearchStore(
+    std::shared_ptr<facebook::react::CallInvoker> jsInvoker)
+    : BaseDataStore(jsInvoker) {
+}
+
+jsi::Array MessageSearchStore::parseDBDataStore(
+    jsi::Runtime &rt,
+    std::shared_ptr<std::vector<MessageSearchResult>> vectorPtr) const {
+  jsi::Array jsiSearchMessages = jsi::Array(rt, 0);
+
+  return jsiSearchMessages;
+}
+
+std::vector<std::unique_ptr<DBOperationBase>>
+MessageSearchStore::createOperations(
+    jsi::Runtime &rt,
+    const jsi::Array &operations) const {
+  std::vector<std::unique_ptr<DBOperationBase>> messageSearchStoreOps;
+  for (auto idx = 0; idx < operations.size(rt); idx++) {
+    auto op = operations.getValueAtIndex(rt, idx).asObject(rt);
+    auto op_type = op.getProperty(rt, "type").asString(rt).utf8(rt);
+
+    auto payload_obj = op.getProperty(rt, "payload").asObject(rt);
+    if (op_type == INSERT_OPERATION) {
+      messageSearchStoreOps.push_back(
+          std::make_unique<InsertSearchMessagesOperation>(rt, payload_obj));
+    } else {
+      throw std::runtime_error("unsupported operation: " + op_type);
+    }
+  }
+  return messageSearchStoreOps;
+}
+
+} // namespace comm
