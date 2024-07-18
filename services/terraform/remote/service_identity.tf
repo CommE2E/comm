2f2e98ec0869e06c431b40375f8dc6898a271241
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2024-07-18 14:00:07 -0400

    [terraform] Increase Identity memory to 2048
    Summary:
    - Updated launch template to `t3.xlarge` (16GB RAM) - can consider also `t3.large` (8GB), I'll be experimenting with it on staging
    - Updated Identity task definition to use 2048 MB memory
    
    Test Plan: Applied on staging, then prod.
    
    Reviewers: will, ashoat, bartek
    
    Reviewed By: will, ashoat
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D12792

--- services/terraform/remote/service_identity.tf
+++ services/terraform/remote/service_identity.tf
@@ -114,7 +114,7 @@ resource "aws_ecs_task_definition" "identity_service" {
   execution_role_arn       = aws_iam_role.ecs_task_execution.arn
   network_mode             = "bridge"
   cpu                      = "512"
-  memory                   = "512"
+  memory                   = "2048"
   requires_compatibilities = ["EC2"]
 
   # Set this to true if you want to keep old revisions
