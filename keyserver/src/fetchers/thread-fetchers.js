3cd205451a3214b2200a898253369633154983af
Atul Madhugiri <atulm@berkeley.edu>
2024-07-18 17:29:45 -0400

    [keyserver] Strip `member.permissions` for `FUTURE_CODE_VERSION` clients
    Summary:
    Add `stripMemberPermissions` option to `rawThreadInfoFromServerThreadInfo` to strip member permissions for new clients.
    
    ---
    
    Depends on D12763
    
    Test Plan:
    Did some basic testing where I hardcoded `stripMemberPermissions` being passed to `rawThreadInfoFromServerThreadInfo` and set breakpoints to check output.
    
    Will do additional testing with validators + after getting basic client migration working.
    
    With `stripMemberPermissions=false`:
    {F2296839}
    
    (can observe that each of the two members have `permissions` field set to string)
    
    With `stripMemberPermissions=true`:
    {F2296843}
    
    Reviewers: ashoat, tomek
    
    Reviewed By: ashoat
    
    Differential Revision: https://phab.comm.dev/D12779

--- keyserver/src/fetchers/thread-fetchers.js
+++ keyserver/src/fetchers/thread-fetchers.js
@@ -10,7 +10,10 @@ import {
   getContainingThreadID,
   getCommunity,
 } from 'lib/shared/thread-utils.js';
-import { hasMinCodeVersion } from 'lib/shared/version-utils.js';
+import {
+  FUTURE_CODE_VERSION,
+  hasMinCodeVersion,
+} from 'lib/shared/version-utils.js';
 import type { AvatarDBContent, ClientAvatar } from 'lib/types/avatar-types.js';
 import type { RawMessageInfo, MessageInfo } from 'lib/types/message-types.js';
 import type { ThinRawThreadInfo } from 'lib/types/minimally-encoded-thread-permissions-types.js';
@@ -305,6 +308,10 @@ function rawThreadInfosFromServerThreadInfos(
       web: 88,
     },
   );
+  const stripMemberPermissions = hasMinCodeVersion(viewer.platformDetails, {
+    native: FUTURE_CODE_VERSION,
+    web: FUTURE_CODE_VERSION,
+  });
 
   const threadInfos: {
     [string]: LegacyThinRawThreadInfo | ThinRawThreadInfo,
@@ -324,6 +331,7 @@ function rawThreadInfosFromServerThreadInfos(
         allowAddingUsersToCommunityRoot: addingUsersToCommunityRootSupported,
         filterManageFarcasterChannelTagsPermission:
           manageFarcasterChannelTagsPermissionUnsupported,
+        stripMemberPermissions: stripMemberPermissions,
       },
     );
     if (threadInfo) {
