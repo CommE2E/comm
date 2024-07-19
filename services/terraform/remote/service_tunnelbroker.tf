00abc2d7371f2a40c0389410e4e1efaa1ade0858
xsanm <kkurowski.kk@gmail.com>
2024-07-19 16:09:16 +0200

    [terraform] Deploy Tunnelbroker 0.11 to staging
    Summary: Deploying FCM work.
    
    Test Plan: Run `terraform plan -target aws_ecs_service.tunnelbroker`
    
    Reviewers: bartek, marcin
    
    Reviewed By: marcin
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D12802

--- services/terraform/remote/service_tunnelbroker.tf
+++ services/terraform/remote/service_tunnelbroker.tf
@@ -1,7 +1,7 @@
 locals {
   tunnelbroker_config = {
     docker_image = "commapp/tunnelbroker"
-    docker_tag   = local.is_staging ? "0.10-staging" : "0.9"
+    docker_tag   = local.is_staging ? "0.11-staging" : "0.9"
 
     grpc_port      = 50051
     websocket_port = 51001
