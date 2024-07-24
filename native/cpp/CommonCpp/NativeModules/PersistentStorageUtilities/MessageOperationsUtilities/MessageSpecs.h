196cb7a97ff0c6e8f6ccd56214fb1c777295c2db
Inka Sokołowska <inka.d.sokolowska@gmail.com>
2024-07-24 16:48:55 +0200

    [native][web] Fill target_message column for sidebar source
    Summary: issue: [[https://linear.app/comm/issue/ENG-8891/fill-target-message-for-sidebar-source-on-clients | ENG-8891]]
    
    Test Plan:
    Tested that in a newly created db the target_message is filled in for sidebar source, by searching in a sidebar and getting a result for the first message of that siedbar
    Tested that the migration succeeds and after it is run it is also possible to search for the first message in a sidebar
    
    Reviewers: tomek, kamil, marcin
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D12869

--- native/cpp/CommonCpp/NativeModules/PersistentStorageUtilities/MessageOperationsUtilities/MessageSpecs.h
+++ native/cpp/CommonCpp/NativeModules/PersistentStorageUtilities/MessageOperationsUtilities/MessageSpecs.h
@@ -17,36 +17,11 @@
 #include "MessageSpecs/TextMessageSpec.h"
 #include "MessageSpecs/UnsupportedMessageSpec.h"
 #include "MessageSpecs/UpdateRelationshipMessageSpec.h"
+#include "MessageTypeEnum.h"
 
 #include <map>
 
 namespace comm {
-
-enum class MessageType {
-  TEXT,
-  CREATE_THREAD,
-  ADD_MEMBERS,
-  CREATE_SUB_THREAD,
-  CHANGE_SETTINGS,
-  REMOVE_MEMBERS,
-  CHANGE_ROLE,
-  LEAVE_THREAD,
-  JOIN_THREAD,
-  CREATE_ENTRY,
-  EDIT_ENTRY,
-  DELETE_ENTRY,
-  RESTORE_ENTRY,
-  UNSUPPORTED,
-  IMAGES,
-  MULTIMEDIA,
-  LEGACY_UPDATE_RELATIONSHIP,
-  SIDEBAR_SOURCE,
-  CREATE_SIDEBAR,
-  REACTION,
-  EDIT_MESSAGE,
-  TOGGLE_PIN,
-  UPDATE_RELATIONSHIP,
-};
 
 const std::map<MessageType, std::unique_ptr<MessageSpec>> messageSpecsHolder =
     []() {
