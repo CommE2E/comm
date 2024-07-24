bf435750f31410d2d4807cb31702e943e17ca068
William Wang <williamwang749@gmail.com>
2024-07-24 15:16:01 -0400

    [terraform] up maximum deployment and minimum deployment percents
    Summary:
    This ensures that there is always at least one landing and webapp node
    
    Depends on D12868
    
    Test Plan: terraform apply
    
    Reviewers: bartek, varun
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D12874

--- services/terraform/self-host/webapp.tf
+++ services/terraform/self-host/webapp.tf
@@ -88,8 +88,8 @@ resource "aws_ecs_service" "webapp_service" {
   enable_ecs_managed_tags            = true
   force_new_deployment               = true
   desired_count                      = 2
-  deployment_maximum_percent         = 100
-  deployment_minimum_healthy_percent = 0
+  deployment_maximum_percent         = 200
+  deployment_minimum_healthy_percent = 100
 
 
   network_configuration {
