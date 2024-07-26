6bc6b545b15281b461faa206c943eaf0219c561e
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2024-07-26 13:24:22 +0200

    [tunnelbroker] Implement WebPush client adapter
    Summary:
    Implemented wrapper over `web_push` client, that handles VAPID signing and proper payload creation for us.
    Configured it to use `hyper` backend, which is the same what we use for other push services (reqwest uses it under the hood).
    
    Test Plan:
    Cargo build.
    Tested together with the rest of the stack
    
    Reviewers: kamil!, marcin
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D12893

--- services/tunnelbroker/src/notifs/web_push/error.rs
+++ services/tunnelbroker/src/notifs/web_push/error.rs
@@ -2,14 +2,13 @@ use derive_more::{Display, Error, From};
 
 #[derive(Debug, From, Display, Error)]
 pub enum Error {
-  JWTError,
-  ReqwestError(reqwest::Error),
-  InvalidHeaderValue(reqwest::header::InvalidHeaderValue),
+  Jwt,
+  WebPush(web_push::WebPushError),
   SerdeJson(serde_json::Error),
 }
 
 impl From<jsonwebtoken::errors::Error> for Error {
   fn from(_: jsonwebtoken::errors::Error) -> Self {
-    Self::JWTError
+    Self::Jwt
   }
 }
