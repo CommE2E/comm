3cdfbdebdc6935f805af7ad8099e7347adcb5f80
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2024-07-28 13:50:57 +0200

    [shared][rust] Add proc-macro to deserialize tagged enum variants
    Summary:
    Detailed description of why we need this: [[ https://linear.app/comm/issue/ENG-8901/fix-serde-parsing-incorrect-tunnelbroker-message-types | ENG-8901 ]].
    
    Unfortunately, proc-macros need to be defined in a separate crate ([[ https://users.rust-lang.org/t/why-do-we-need-procedural-macro-separation-on-a-crate-level/107295/4 | see why]]), so I created a `util-macros` crate in our Cargo workspace.
    
    Test Plan:
    - Added rustdoc usage example
    - Rust unit tests added in the next diff
    - Used and tested in in D12895
    - [[ https://play.rust-lang.org/?version=stable&mode=debug&edition=2021&gist=08ae6d1f1810a2e47c32ba60c6d2ae53 | Rust playground ]] - see Linear description
    
    Reviewers: varun, will, kamil
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D12901

---
+++ shared/util_macros/src/tag_aware_deserialize.rs
@@ -0,0 +1,41 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{Attribute, Lit, Meta, NestedMeta};
+
+pub fn impl_tag_aware_deserialize_macro(ast: &syn::DeriveInput) -> TokenStream {
+  let name = &ast.ident;
+
+  let gen = quote! {
+    impl<'de> serde::de::Deserialize<'de> for #name {
+      fn deserialize<D>(deserializer: D) -> std::result::Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+      {
+        use serde::de::Error;
+
+        let this = serde_json::Value::deserialize(deserializer)?;
+        if let Some(found_tag) = this.get("type") {
+          if found_tag == stringify!(#name) {
+            // now we can run _original_ deserialize
+            return match #name::deserialize(this) {
+              Ok(object) => Ok(object),
+              Err(err) => Err(Error::custom(err)),
+            };
+          }
+        }
+        Err(Error::custom(format!("Deserialized object is not a '{}'", stringify!(#name))))
+      }
+    }
+
+    // We need a dummy `Serialize` impl for `remote = "Self"` to work
+    impl Serialize for #name {
+      fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+      where
+        S: serde::Serializer,
+      {
+        Self::serialize(self, serializer)
+      }
+    }
+  };
+  gen.into()
+}
