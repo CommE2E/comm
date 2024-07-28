CommE2E/comm

commit 3cdfbdebdc6935f805af7ad8099e7347adcb5f80

[shared][rust] Add proc-macro to deserialize tagged enum variants
Summary:
Detailed description of why we need this: [[ https://linear.app/comm/issue/ENG-8901/fix-serde-parsing-incorrect-tunnelbroker-message-types | ENG-8901 ]].

Unfortunately, proc-macros need to be defined in a separate crate ([[ https://users.rust-lang.org/t/why-do-we-need-procedural-macro-separation-on-a-crate-level/107295/4 | see why]]), so I created a `util-macros` crate in our Cargo workspace.

Test Plan:
- Added rustdoc usage example
- Rust unit tests added in the next diff
- Used and tested in in D12895
- [[ https://play.rust-lang.org/?version=stable&mode=debug&edition=2021&gist=08ae6d1f1810a2e47c32ba60c6d2ae53 | Rust playground ]] - see Linear description

Reviewers: varun, will, kamil

Subscribers: ashoat, tomek

Differential Revision: https://phab.comm.dev/D12901

shared/util_macros/src/tag_aware_deserialize.rs
@@ -1,0 +1,41 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{Attribute, Lit, Meta, NestedMeta};
+
+pub fn impl_tag_aware_deserialize_macro(ast: &syn::DeriveInput) -> TokenStream {
+  let name = &ast.ident;
+
+  let gen = quote! {
+    impl<'de> serde::de::Deserialize<'de> for #name {
+      fn deserialize<D>(deserializer: D) -> std::result::Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+      {
+        use serde::de::Error;
+
+        let this = serde_json::Value::deserialize(deserializer)?;
+        if let Some(found_tag) = this.get("type") {
+          if found_tag == stringify!(#name) {
+            // now we can run _original_ deserialize
+            return match #name::deserialize(this) {
+              Ok(object) => Ok(object),
+              Err(err) => Err(Error::custom(err)),
+            };
+          }
+        }
+        Err(Error::custom(format!("Deserialized object is not a '{}'", stringify!(#name))))
+      }
+    }
+
+    // We need a dummy `Serialize` impl for `remote = "Self"` to work
+    impl Serialize for #name {
+      fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+      where
+        S: serde::Serializer,
+      {
+        Self::serialize(self, serializer)
+      }
+    }
+  };
+  gen.into()
+}

shared/util_macros/Cargo.toml
@@ -1,0 +1,15 @@
+[package]
+name = "util_macros"
+version = "0.1.0"
+edition.workspace = true
+license.workspace = true
+homepage.workspace = true
+
+[lib]
+proc-macro = true
+
+[dependencies]
+serde = { workspace = true, features = ["derive"] }
+serde_json = { workspace = true }
+syn = "1.0"
+quote = "1.0"

shared/util_macros/src/lib.rs
@@ -1,0 +1,62 @@
+extern crate proc_macro;
+use proc_macro::TokenStream;
+
+mod tag_aware_deserialize;
+
+/// Fixes [`serde::Deserialize`] implementation for deserializing
+/// untagged enums of internally tagged structs. Original implementation
+/// totally ignores the `tag` attribute when deserializing enum variants.
+///
+/// This derive requires two serde attributes to be present:
+/// `#[serde(tag = "type", remote = "Self")]`
+///
+/// ### Example
+/// ```
+/// use serde::{Deserialize, Serialize};
+/// use util_macros::TagAwareDeserialize;
+///
+/// // Note that FirstStruct and SecondStruct have identical fields
+/// // They're only distinguishable by 'tag'
+/// #[derive(Debug, Serialize, Deserialize, TagAwareDeserialize)]
+/// #[serde(tag = "type", remote = "Self")]
+/// struct FirstStruct {
+///   foo: String,
+///   bar: String,
+/// }
+/// #[derive(Debug, Serialize, Deserialize, TagAwareDeserialize)]
+/// #[serde(tag = "type", remote = "Self")]
+/// struct SecondStruct {
+///   foo: String,
+///   bar: String,
+/// }
+///
+/// #[derive(Debug, Serialize, Deserialize)]
+/// #[serde(untagged)]
+/// enum EitherStruct {
+///   // Note that FirstStruct is BEFORE SecondStruct
+///   FirstStruct(FirstStruct),
+///   SecondStruct(SecondStruct),
+/// }
+///
+/// let input = SecondStruct {
+///   foo: "a".to_string(),
+///   bar: "b".to_string(),
+/// };
+/// let serialized = serde_json::to_string(&input).unwrap();
+///
+/// let deserialized: EitherStruct = serde_json::from_str(&serialized).unwrap();
+/// match deserialized {
+///   EitherStruct::SecondStruct(result) => {
+///     println!("Successfully deserialized {:?}", result);
+///   },
+///   other => {
+///     println!("Wrong variant was deserialized: {:?}", other);
+///   }
+/// };
+///
+/// ```
+#[proc_macro_derive(TagAwareDeserialize)]
+pub fn tag_aware_deserialize_macro_derive(input: TokenStream) -> TokenStream {
+  let ast = syn::parse(input).unwrap();
+  tag_aware_deserialize::impl_tag_aware_deserialize_macro(&ast)
+}

commit 7abe990ac85c1e0aadc4902780877faec3d6d9cf

[util-macros] Add tests for serde and TagAwareDeserialize behavior
Summary:

Added tests to demonstrate both serde issue described in [[ https://linear.app/comm/issue/ENG-8901/fix-serde-parsing-incorrect-tunnelbroker-message-types | ENG-8901 ]] and the macro introduced in D12901

Depends on D12901

Test Plan:

`cargo test`

Reviewers: varun, will, kamil

Subscribers:

shared/util_macros/tests/tag_aware_deserialize.rs
@@ -1,0 +1,138 @@
+// Structs with plain serde derives
+mod serde_deserialize_example {
+  use serde::{Deserialize, Serialize};
+
+  // Note that FirstStruct and SecondStruct have identical fields
+  #[derive(Debug, Serialize, Deserialize)]
+  #[serde(tag = "type")]
+  pub struct FirstStruct {
+    pub foo: String,
+    pub bar: String,
+  }
+  #[derive(Debug, Serialize, Deserialize)]
+  #[serde(tag = "type")]
+  pub struct SecondStruct {
+    pub foo: String,
+    pub bar: String,
+  }
+  #[derive(Debug, Serialize, Deserialize)]
+  #[serde(tag = "type")]
+  pub struct OtherStruct {
+    pub baz: String,
+  }
+
+  #[derive(Debug, Serialize, Deserialize)]
+  #[serde(untagged)]
+  #[allow(clippy::enum_variant_names)]
+  pub enum EitherStruct {
+    // Note that FirstStruct is BEFORE SecondStruct
+    OtherStruct(OtherStruct),
+    FirstStruct(FirstStruct),
+    SecondStruct(SecondStruct),
+  }
+}
+
+// Structs with serde and `TagAwareDeserialize` derive
+mod tag_aware_deserialize_example {
+  use serde::{Deserialize, Serialize};
+  use util_macros::TagAwareDeserialize;
+
+  // Note that FirstStruct and SecondStruct have identical fields
+  #[derive(Debug, Serialize, Deserialize, TagAwareDeserialize)]
+  #[serde(tag = "type", remote = "Self")]
+  pub struct FirstStruct {
+    pub foo: String,
+    pub bar: String,
+  }
+  #[derive(Debug, Serialize, Deserialize, TagAwareDeserialize)]
+  #[serde(tag = "type", remote = "Self")]
+  pub struct SecondStruct {
+    pub foo: String,
+    pub bar: String,
+  }
+  #[derive(Debug, Serialize, Deserialize, TagAwareDeserialize)]
+  #[serde(tag = "type", remote = "Self")]
+  pub struct OtherStruct {
+    pub baz: String,
+  }
+
+  #[derive(Debug, Serialize, Deserialize)]
+  #[serde(untagged)]
+  #[allow(clippy::enum_variant_names)]
+  pub enum EitherStruct {
+    // Note that FirstStruct is BEFORE SecondStruct
+    OtherStruct(OtherStruct),
+    FirstStruct(FirstStruct),
+    SecondStruct(SecondStruct),
+  }
+}
+
+#[test]
+fn serde_deserialize_ignores_tag() {
+  use serde_deserialize_example::OtherStruct;
+
+  let with_tag = r#"{"type":"OtherStruct","baz":"abc"}"#;
+  let without_tag = r#"{"baz":"abc"}"#;
+
+  let deserialized_with_tag = serde_json::from_str::<OtherStruct>(with_tag);
+  let deserialized_without_tag =
+    serde_json::from_str::<OtherStruct>(without_tag);
+
+  assert!(deserialized_with_tag.is_ok());
+  assert!(deserialized_without_tag.is_ok());
+}
+
+#[test]
+fn tag_aware_deserialize_requires_tag() {
+  use tag_aware_deserialize_example::OtherStruct;
+
+  let with_tag = r#"{"type":"OtherStruct","baz":"abc"}"#;
+  let without_tag = r#"{"baz":"abc"}"#;
+
+  let deserialized_with_tag = serde_json::from_str::<OtherStruct>(with_tag);
+  let deserialized_without_tag =
+    serde_json::from_str::<OtherStruct>(without_tag);
+
+  assert!(deserialized_with_tag.is_ok());
+  assert!(deserialized_without_tag.is_err());
+}
+
+#[test]
+#[ignore = "this test is expected to fail until serde fixes it"]
+fn serde_deserializes_wrong_variant_demo() {
+  use serde_deserialize_example::*;
+
+  let input = SecondStruct {
+    foo: "a".to_string(),
+    bar: "b".to_string(),
+  };
+  let serialized = serde_json::to_string(&input).unwrap();
+
+  let deserialized: EitherStruct = serde_json::from_str(&serialized).unwrap();
+  match deserialized {
+    EitherStruct::SecondStruct(_) => (), // should be this
+    other => {
+      // It fails here, `FirstStruct` is deserialized instead
+      std::panic::panic_any(format!("Wrong struct deserialized: {:?}", other))
+    }
+  };
+}
+
+#[test]
+fn tag_aware_deserialize_returns_correct_variant() {
+  use tag_aware_deserialize_example::*;
+
+  let input = SecondStruct {
+    foo: "a".to_string(),
+    bar: "b".to_string(),
+  };
+  let serialized = serde_json::to_string(&input).unwrap();
+
+  let deserialized: EitherStruct = serde_json::from_str(&serialized).unwrap();
+  match deserialized {
+    EitherStruct::SecondStruct(_) => (), // should be this
+    other => {
+      std::panic::panic_any(format!("Wrong struct deserialized: {:?}", other))
+    }
+  };
+}