01183efa86c29ed26c5bfb61d8c9a95fb081f691
William Wang <williamwang749@gmail.com>
2024-07-29 19:56:24 -0400

    [keyserver] Run webapp through comm services cluster
    Summary: Run webapp on the comm services cluster. Will not land until web.comm.app has been properly set up on prod
    
    Test Plan: terraform apply. Switched `comm.software` to point to web app load balancer and accessed the working web app
    
    Reviewers: bartek, varun
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D12928

---
+++ services/terraform/remote/service_webapp.tf
@@ -0,0 +1,54 @@
+locals {
+  webapp_image_tag      = "1.0.102"
+  webapp_service_image  = "commapp/keyserver:${local.webapp_image_tag}"
+  webapp_container_name = "webapp"
+
+  webapp_run_server_config = jsonencode({
+    runKeyserver = false
+    runWebApp    = true
+    runLanding   = false
+  })
+
+  webapp_landing_environment_vars = local.secrets["webappLandingEnvVars"]
+
+  webapp_landing_environment_vars_encoded = {
+    for key, value in local.webapp_landing_environment_vars : key => jsonencode(value)
+  }
+
+  stage_specific_environment_vars = (local.is_staging ?
+    local.secrets["webappLandingStagingEnvVars"] :
+  local.secrets["webappLandingProdEnvVars"])
+
+  stage_specific_environment_vars_encoded = {
+    for key, value in local.stage_specific_environment_vars : key => jsonencode(value)
+  }
+
+  webapp_environment_vars = merge(
+    local.webapp_landing_environment_vars_encoded,
+    local.stage_specific_environment_vars_encoded,
+    {
+      "COMM_LISTEN_ADDR"                        = "0.0.0.0",
+      "COMM_NODE_ROLE"                          = "webapp",
+      "COMM_JSONCONFIG_facts_run_server_config" = local.webapp_run_server_config
+  })
+}
+
+module "webapp_service" {
+  source = "../modules/node_service"
+
+  container_name              = "webapp"
+  image                       = local.webapp_service_image
+  service_name                = "webapp"
+  cluster_id                  = aws_ecs_cluster.comm_services.id
+  domain_name                 = local.is_staging ? "comm.software" : "web.comm.app"
+  vpc_id                      = aws_vpc.default.id
+  vpc_subnets                 = [aws_subnet.public_a.id, aws_subnet.public_b.id]
+  region                      = "us-east-2"
+  environment_vars            = local.webapp_environment_vars
+  ecs_task_role_arn           = aws_iam_role.ecs_task_role.arn
+  ecs_task_execution_role_arn = aws_iam_role.ecs_task_execution.arn
+}
+
+output "webapp_service_load_balancer_dns_name" {
+  value = module.webapp_service.service_load_balancer_dns_name
+}
