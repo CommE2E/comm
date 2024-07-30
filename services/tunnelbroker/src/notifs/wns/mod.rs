0f1ee85d4f624003e8539032d9fa78a2b7580744
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2024-07-30 02:45:17 -0400

    [Tunnelbroker] send push notifs to windows devices
    Summary: Make it possible to send push notifs to windows devices through tunnelbroker
    
    Depends on D12934
    
    Test Plan: https://gist.github.com/vdhanan/3921680cde51e92a8c7787771b36d19a
    
    successfully sent push notifs to my windows desktop app
    
    Reviewers: kamil, bartek, marcin
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D12935

--- services/tunnelbroker/src/notifs/wns/mod.rs
+++ services/tunnelbroker/src/notifs/wns/mod.rs
@@ -1,4 +1,7 @@
 use crate::notifs::wns::config::WNSConfig;
+use error::WNSTokenError;
+use reqwest::StatusCode;
+use response::WNSErrorResponse;
 use std::{
   sync::{Arc, RwLock},
   time::{Duration, SystemTime},
@@ -6,11 +9,18 @@ use std::{
 
 pub mod config;
 mod error;
+mod response;
 
 #[derive(Debug, Clone)]
 pub struct WNSAccessToken {
   token: String,
   expires: SystemTime,
+}
+
+#[derive(Debug, Clone)]
+pub struct WNSNotif {
+  pub device_token: String,
+  pub payload: String,
 }
 
 #[derive(Clone)]
@@ -30,9 +40,45 @@ impl WNSClient {
     })
   }
 
-  pub async fn get_wns_token(
-    &mut self,
-  ) -> Result<Option<String>, error::Error> {
+  pub async fn send(&self, notif: WNSNotif) -> Result<(), error::Error> {
+    let wns_access_token = self.get_wns_token().await?;
+
+    let url = notif.device_token;
+
+    // Send the notification
+    let response = self
+      .http_client
+      .post(&url)
+      .header("Content-Type", "application/octet-stream")
+      .header("X-WNS-Type", "wns/raw")
+      .bearer_auth(wns_access_token)
+      .body(notif.payload)
+      .send()
+      .await?;
+
+    match response.status() {
+      StatusCode::OK => {
+        tracing::debug!("Successfully sent WNS notif to {}", &url);
+        Ok(())
+      }
+      error_status => {
+        let body = response
+          .text()
+          .await
+          .unwrap_or_else(|error| format!("Error occurred: {}", error));
+        tracing::error!(
+          "Failed sending WNS notification to: {}. Status: {}. Body: {}",
+          &url,
+          error_status,
+          body
+        );
+        let fcm_error = WNSErrorResponse::from_status(error_status, body);
+        Err(error::Error::WNSNotification(fcm_error))
+      }
+    }
+  }
+
+  pub async fn get_wns_token(&self) -> Result<String, error::Error> {
     const EXPIRY_WINDOW: Duration = Duration::from_secs(10);
 
     {
@@ -42,7 +88,7 @@ impl WNSClient {
         .map_err(|_| error::Error::ReadLock)?;
       if let Some(ref token) = *read_guard {
         if token.expires >= SystemTime::now() - EXPIRY_WINDOW {
-          return Ok(Some(token.token.clone()));
+          return Ok(token.token.clone());
         }
       }
     }
@@ -68,17 +114,17 @@ impl WNSClient {
         .await
         .unwrap_or_else(|_| String::from("<failed to read body>"));
       tracing::error!(status, "Failure when getting the WNS token: {}", body);
-      return Ok(None);
+      return Err(error::Error::WNSToken(WNSTokenError::Unknown(status)));
     }
 
     let response_json: serde_json::Value = response.json().await?;
     let token = response_json["access_token"]
       .as_str()
-      .ok_or(error::Error::TokenNotFound)?
+      .ok_or(error::Error::WNSToken(WNSTokenError::TokenNotFound))?
       .to_string();
     let expires_in = response_json["expires_in"]
       .as_u64()
-      .ok_or(error::Error::ExpiryNotFound)?;
+      .ok_or(error::Error::WNSToken(WNSTokenError::ExpiryNotFound))?;
 
     let expires = SystemTime::now() + Duration::from_secs(expires_in);
 
@@ -92,6 +138,6 @@ impl WNSClient {
         expires,
       });
     }
-    Ok(Some(token))
+    Ok(token)
   }
 }
