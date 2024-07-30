515ae6d224f5bf50a4f65e8a894a0446a2390058
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2024-07-29 23:26:34 -0400

    [Tunnelbroker] implement WNS config
    Summary: WNS config for tunnelbroker, similar to keyserver [config](https://github.com/CommE2E/comm/blob/fa607e0f4348154e1d331327cdd410e3dbf6525f/keyserver/src/push/providers.js#L143)
    
    Depends on D12931
    
    Test Plan: Add config var from commandline and log it
    
    Reviewers: kamil, bartek, marcin
    
    Subscribers:

--- services/tunnelbroker/src/notifs/mod.rs
+++ services/tunnelbroker/src/notifs/mod.rs
@@ -1,14 +1,17 @@
 use crate::notifs::apns::APNsClient;
 use crate::notifs::fcm::FCMClient;
 use crate::notifs::web_push::WebPushClient;
+use crate::notifs::wns::WNSClient;
 
 pub mod apns;
 pub mod fcm;
 pub mod web_push;
+pub mod wns;
 
 #[derive(Clone)]
 pub struct NotifClient {
   pub(crate) apns: Option<APNsClient>,
   pub(crate) fcm: Option<FCMClient>,
   pub(crate) web_push: Option<WebPushClient>,
+  pub(crate) wns: Option<WNSClient>,
 }
