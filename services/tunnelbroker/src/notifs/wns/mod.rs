358ada5313b66c27b63cf33d49ec75f50a4a3006
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2024-07-30 00:53:12 -0400

    [Tunnelbroker] Implement client method to get WNS token
    Summary: We need an oauth2 access token to supply as the bearer token when we send push notifs
    
    Depends on D12933
    
    Test Plan: successfully logged access token from WNS server
    
    Reviewers: kamil, bartek, marcin
    
    Subscribers:

--- services/tunnelbroker/src/notifs/wns/mod.rs
+++ services/tunnelbroker/src/notifs/wns/mod.rs
@@ -1,12 +1,23 @@
 use crate::notifs::wns::config::WNSConfig;
+use std::{
+  sync::{Arc, RwLock},
+  time::{Duration, SystemTime},
+};
 
 pub mod config;
 mod error;
+
+#[derive(Debug, Clone)]
+pub struct WNSAccessToken {
+  token: String,
+  expires: SystemTime,
+}
 
 #[derive(Clone)]
 pub struct WNSClient {
   http_client: reqwest::Client,
   config: WNSConfig,
+  access_token: Arc<RwLock<Option<WNSAccessToken>>>,
 }
 
 impl WNSClient {
@@ -15,6 +26,69 @@ impl WNSClient {
     Ok(WNSClient {
       http_client,
       config: config.clone(),
+      access_token: Arc::new(RwLock::new(None)),
     })
+  }
+
+  pub async fn get_wns_token(
+    &mut self,
+  ) -> Result<Option<String>, error::Error> {
+    let expiry_window_in_secs = 10;
+
+    {
+      let read_guard = self
+        .access_token
+        .read()
+        .map_err(|_| error::Error::ReadLock)?;
+      if let Some(ref token) = *read_guard {
+        if token.expires
+          >= SystemTime::now() - Duration::from_secs(expiry_window_in_secs)
+        {
+          return Ok(Some(token.token.clone()));
+        }
+      }
+    }
+
+    let params = [
+      ("grant_type", "client_credentials"),
+      ("client_id", &self.config.app_id),
+      ("client_secret", &self.config.secret),
+      ("scope", "https://wns.windows.com/.default"),
+    ];
+
+    let url = format!(
+      "https://login.microsoftonline.com/{}/oauth2/v2.0/token",
+      self.config.tenant_id
+    );
+
+    let response = self.http_client.post(&url).form(&params).send().await?;
+
+    if !response.status().is_success() {
+      tracing::error!("Failure when getting the WNS token: {:?}", response);
+      return Ok(None);
+    }
+
+    let response_json: serde_json::Value = response.json().await?;
+    let token = response_json["access_token"]
+      .as_str()
+      .ok_or(error::Error::TokenNotFound)?
+      .to_string();
+    let expires_in = response_json["expires_in"]
+      .as_u64()
+      .ok_or(error::Error::ExpiryNotFound)?;
+
+    let expires = SystemTime::now() + Duration::from_secs(expires_in);
+
+    {
+      let mut write_guard = self
+        .access_token
+        .write()
+        .map_err(|_| error::Error::WriteLock)?;
+      *write_guard = Some(WNSAccessToken {
+        token: token.clone(),
+        expires,
+      });
+    }
+    Ok(Some(token))
   }
 }
