cd94f644a6ba81b8154870fc3bce8582e3215aab
William Wang <williamwang749@gmail.com>
2024-07-30 00:14:39 -0400

    [terraform] deploy landing on comm services cluster
    Summary:
    This puts up landing on our comm services cluster. This will require a new keyserver image with
    https://phab.comm.dev/D12927
    
    Test Plan: Tested with custom image and accessed landing page at comm.app
    
    Reviewers: varun, bartek
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D12930

---
+++ services/terraform/remote/shared_webapp_landing.tf
@@ -0,0 +1,22 @@
+locals {
+  keyserver_image_tag = "1.0.103"
+  keyserver_image     = "commapp/keyserver:${local.keyserver_image_tag}"
+
+  shared_keyserver_environment_vars = {
+    "COMM_LISTEN_ADDR" = "0.0.0.0",
+  }
+
+  webapp_landing_environment_vars = local.secrets["webappLandingEnvVars"]
+
+  webapp_landing_environment_vars_encoded = {
+    for key, value in local.webapp_landing_environment_vars : key => jsonencode(value)
+  }
+
+  stage_specific_environment_vars = (local.is_staging ?
+    local.secrets["webappLandingStagingEnvVars"] :
+  local.secrets["webappLandingProdEnvVars"])
+
+  stage_specific_environment_vars_encoded = {
+    for key, value in local.stage_specific_environment_vars : key => jsonencode(value)
+  }
+}
