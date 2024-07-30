85f76347f76d3c4789a7f8f1cd56b8727f413bc5
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2024-07-30 00:07:58 -0400

    [Tunnelbroker] implement WNS client
    Summary:
    Implementation for WNS client which will handle sending notifs to Windows devices
    
    Depends on D12932
    
    Test Plan: Test creating client from main
    
    Reviewers: bartek, kamil, marcin
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D12933

--- services/tunnelbroker/src/notifs/wns/mod.rs
+++ services/tunnelbroker/src/notifs/wns/mod.rs
@@ -1,6 +1,20 @@
+use crate::notifs::wns::config::WNSConfig;
+
 pub mod config;
+mod error;
 
 #[derive(Clone)]
 pub struct WNSClient {
   http_client: reqwest::Client,
+  config: WNSConfig,
+}
+
+impl WNSClient {
+  pub fn new(config: &WNSConfig) -> Result<Self, error::Error> {
+    let http_client = reqwest::Client::builder().build()?;
+    Ok(WNSClient {
+      http_client,
+      config: config.clone(),
+    })
+  }
 }
