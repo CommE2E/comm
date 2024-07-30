8a400c03172b410d16386fb4d9b0b47ec2f50adc
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2024-07-30 00:53:12 -0400

    [Tunnelbroker] Implement client method to get WNS token
    Summary:
    We need an oauth2 access token to supply as the bearer token when we send push notifs
    
    Depends on D12933
    
    Test Plan: successfully logged access token from WNS server
    
    Reviewers: kamil, bartek, marcin
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D12934

--- services/tunnelbroker/src/notifs/wns/error.rs
+++ services/tunnelbroker/src/notifs/wns/error.rs
@@ -3,4 +3,13 @@ use derive_more::{Display, Error, From};
 #[derive(Debug, From, Display, Error)]
 pub enum Error {
   Reqwest(reqwest::Error),
+  SerdeJson(serde_json::Error),
+  #[display(fmt = "Token not found in response")]
+  TokenNotFound,
+  #[display(fmt = "Expiry time not found in response")]
+  ExpiryNotFound,
+  #[display(fmt = "Failed to acquire read lock")]
+  ReadLock,
+  #[display(fmt = "Failed to acquire write lock")]
+  WriteLock,
 }
