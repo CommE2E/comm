b68f0896137984842c0365e29e6d03747d08cca8
William Wang <williamwang749@gmail.com>
2024-07-30 00:14:39 -0400

    [terraform] deploy landing on comm services cluster
    Summary:
    This puts up landing on our comm services cluster. This will require a new keyserver image with
    https://phab.comm.dev/D12927
    
    Depends on D12929
    
    Test Plan: Tested with custom image and accessed landing page at comm.app
    
    Reviewers: varun, bartek
    
    Reviewed By: varun
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D12930

--- services/terraform/remote/service_landing.tf
+++ services/terraform/remote/service_landing.tf
@@ -1,38 +1,38 @@
 locals {
-  webapp_container_name = "webapp"
+  landing_container_name = "landing"
 
-  webapp_run_server_config = jsonencode({
+  landing_run_server_config = jsonencode({
     runKeyserver = false
-    runWebApp    = true
-    runLanding   = false
+    runWebApp    = false
+    runLanding   = true
   })
 
-  webapp_environment_vars = merge(
+  landing_environment_vars = merge(
     local.webapp_landing_environment_vars_encoded,
     local.stage_specific_environment_vars_encoded,
     local.shared_keyserver_environment_vars,
     {
-      "COMM_NODE_ROLE"                          = "webapp",
-      "COMM_JSONCONFIG_facts_run_server_config" = local.webapp_run_server_config
+      "COMM_NODE_ROLE"                          = "landing",
+      "COMM_JSONCONFIG_facts_run_server_config" = local.landing_run_server_config
   })
 }
 
-module "webapp_service" {
+module "landing_service" {
   source = "../modules/keyserver_node_service"
 
-  container_name              = "webapp"
+  container_name              = "landing"
   image                       = local.keyserver_image
-  service_name                = "webapp"
+  service_name                = "landing"
   cluster_id                  = aws_ecs_cluster.comm_services.id
-  domain_name                 = local.is_staging ? "comm.software" : "web.comm.app"
+  domain_name                 = local.is_staging ? "comm.engineer" : "comm.app"
   vpc_id                      = aws_vpc.default.id
   vpc_subnets                 = [aws_subnet.public_a.id, aws_subnet.public_b.id]
   region                      = "us-east-2"
-  environment_vars            = local.webapp_environment_vars
+  environment_vars            = local.landing_environment_vars
   ecs_task_role_arn           = aws_iam_role.keyserver_node_ecs_task_role.arn
   ecs_task_execution_role_arn = aws_iam_role.ecs_task_execution.arn
 }
 
-output "webapp_service_load_balancer_dns_name" {
-  value = module.webapp_service.service_load_balancer_dns_name
+output "landing_service_load_balancer_dns_name" {
+  value = module.landing_service.service_load_balancer_dns_name
 }
