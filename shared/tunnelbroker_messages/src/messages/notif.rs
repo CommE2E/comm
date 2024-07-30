be878af07b094824c14c065b4715117af66e5195
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2024-07-30 02:45:17 -0400

    [WIP][Tunnelbroker] send push notifs to windows devices
    Summary:
    DRAFT
    
    Depends on D12934
    
    Test Plan: TBD
    
    Reviewers: kamil, bartek, marcin
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D12935

--- shared/tunnelbroker_messages/src/messages/notif.rs
+++ shared/tunnelbroker_messages/src/messages/notif.rs
@@ -35,3 +35,14 @@ pub struct WebPushNotif {
   pub device_id: String,
   pub payload: String,
 }
+
+/// WNS notif built on client.
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+#[serde(tag = "type", rename_all = "camelCase")]
+pub struct WNSNotif {
+  #[serde(rename = "clientMessageID")]
+  pub client_message_id: String,
+  #[serde(rename = "deviceID")]
+  pub device_id: String,
+  pub payload: String,
+}
