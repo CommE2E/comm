9a3e066465ae3fbd66acf43e58605a949fc5c5eb
Inka Sokołowska <inka.d.sokolowska@gmail.com>
2024-08-02 11:59:23 +0200

    [native] Add nux context
    Summary:
    issue: [[https://linear.app/comm/issue/ENG-8618/find-coordinates-and-verticalbounds-for-tooltip | ENG-8618]]
    To be able to get coordinest of buttons, we need to let them render first. Then, they will register their coordinates in this context. A handler component will select those coordinates, and navigate to screens passing the coordinates in params
    
    Test Plan: Tested with following diffs. Testeds that after a button registares their coordinates the handler effect is fired and nux tip appears on the screen, rendering the button in the cprrect place
    
    Reviewers: tomek, will
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D12963

---
+++ native/components/nux-tips-context.react.js
@@ -0,0 +1,72 @@
+// @flow
+
+import * as React from 'react';
+
+import { values } from 'lib/utils/objects.js';
+
+const nuxTip = Object.freeze({
+  MUTED: 'muted',
+  COMMUNITY_DRAWER: 'community_drawer',
+});
+
+type NUXTip = $Values<typeof nuxTip>;
+
+type TipProps = {
+  +x: ?number,
+  +y: ?number,
+  +width: ?number,
+  +height: ?number,
+  +pageX: ?number,
+  +pageY: ?number,
+};
+
+export type NUXTipsContextType = {
+  +registerTipButton: (type: NUXTip, tipProps: TipProps) => void,
+  +getTipsProps: () => ?{ [type: NUXTip]: TipProps },
+};
+
+const NUXTipsContext: React.Context<?NUXTipsContextType> =
+  React.createContext<?NUXTipsContextType>();
+
+type Props = {
+  +children: React.Node,
+};
+
+function NUXTipsContextProvider(props: Props): React.Node {
+  const { children } = props;
+
+  const tipsProps = React.useRef<{ [tip: NUXTip]: TipProps }>({});
+
+  const registerTipButton = React.useCallback(
+    (type: NUXTip, tipProps: TipProps) => {
+      tipsProps.current[type] = {
+        ...tipsProps.current[type],
+        ...tipProps,
+      };
+    },
+    [],
+  );
+
+  const getTipsProps = React.useCallback(() => {
+    for (const type of values(nuxTip)) {
+      if (!tipsProps.current[type]) {
+        return null;
+      }
+    }
+    return tipsProps.current;
+  }, []);
+
+  const value = React.useMemo(
+    () => ({
+      registerTipButton,
+      getTipsProps,
+    }),
+    [getTipsProps, registerTipButton],
+  );
+
+  return (
+    <NUXTipsContext.Provider value={value}>{children}</NUXTipsContext.Provider>
+  );
+}
+
+export { NUXTipsContext, NUXTipsContextProvider, nuxTip };
