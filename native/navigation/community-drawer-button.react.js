1cffd23bcbc669cd5a7957c031e56af63f876f0e
Inka Sokołowska <inka.d.sokolowska@gmail.com>
2024-08-02 12:03:47 +0200

    [native] Register CommunityDrawerButton in nux context
    Summary: issue: [[https://linear.app/comm/issue/ENG-8618/find-coordinates-and-verticalbounds-for-tooltip | ENG-8618]]
    
    Test Plan: Tested with following diffs. Testeds that after a button registares their coordinates the handler effect is fired and nux tip appears on the screen, rendering the button in the cprrect place
    
    Reviewers: tomek, will
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D12964

--- native/navigation/community-drawer-button.react.js
+++ native/navigation/community-drawer-button.react.js
@@ -1,11 +1,16 @@
 // @flow
 
 import Icon from '@expo/vector-icons/Feather.js';
+import invariant from 'invariant';
 import * as React from 'react';
 import { TouchableOpacity } from 'react-native';
 
 import type { CommunityDrawerNavigationProp } from './community-drawer-navigator.react.js';
 import type { TabNavigationProp } from './tab-navigator.react.js';
+import {
+  NUXTipsContext,
+  nuxTip,
+} from '../components/nux-tips-context.react.js';
 import { useStyles } from '../themes/colors.js';
 
 type Props = {
@@ -18,6 +23,16 @@ type Props = {
 function CommunityDrawerButton(props: Props): React.Node {
   const styles = useStyles(unboundStyles);
   const { navigation } = props;
+
+  const tipsContext = React.useContext(NUXTipsContext);
+  invariant(tipsContext, 'NUXTipsContext should be defined');
+  const { registerTipButton } = tipsContext;
+
+  React.useEffect(() => {
+    return () => {
+      registerTipButton(nuxTip.COMMUNITY_DRAWER, null);
+    };
+  }, [registerTipButton]);
 
   return (
     <TouchableOpacity onPress={navigation.openDrawer}>
