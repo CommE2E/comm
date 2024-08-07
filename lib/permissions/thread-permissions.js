CommE2E/comm

commit 442fb06e886d4417fc89408e5713ad9e5b1e2de4

[lib] Prevent editing role permissions from adding unexpected permissions
Summary:
Full context is in the comment in comment in [[ https://linear.app/comm/issue/ENG-5622/unify-the-role-creation-and-role-modification-logic#comment-2d98a2cd | ENG-5622 ]], but basically there was not a clear 1:1 match of permissions before and after editing a role sometimes. This is because the user-surfaced permissions provided some `descendant_*` permissions that `Members`, upon creation, did not originally have.

This diff fixes that so there's a clear equality between the two when permissions are edited.

Addresses [[ https://linear.app/comm/issue/ENG-5622/unify-the-role-creation-and-role-modification-logic#comment-2d98a2cd | ENG-5622 ]]

After this, I'll need to update the MariaDB migration in [[ https://phab.comm.dev/D9599 | D9599 ]] and the redux persist migration in [[ https://phab.comm.dev/D9608 | D9608 ]] to remove all of these permissions. That's why I've added them to an array in `lib` called `permissionsToRemoveInMigration` so I can share one common object, rather than copy and pasting the huge collection of strings in both migrations.

Adding @ashoat as a reviewer here since I think he may have the most context on my proposed solution

Depends on D9493

Test Plan:
Created some communities, edited some permissions for the `Members` role and then removed them, and created some custom roles with some random permissions.

Ran `yarn script dist/scripts/validate-role-permissions.js` to verify that there are no discrepencies. Before this diff, the permissions blob would have unexpected permissions like
```
deepDiff(existingRolePermissions, baseExpectedPermissionBlob) = {
  "join_thread": true,
  "descendant_react_to_message": true,
  "descendant_edit_message": true,
  "descendant_add_members": true,
  "descendant_edit_entries": true,
  "descendant_edit_thread": true,
  "descendant_edit_thread_description": true,
  "descendant_edit_thread_color": true,

lib/types/thread-permission-types.js
@@ -124,10 +124,7 @@ const editCalendarPermission = {
   userSurfacedPermission: userSurfacedPermissions.EDIT_CALENDAR,
 };
 const editEntries = threadPermissions.EDIT_ENTRIES;
-const descendantEditEntries =
-  threadPermissionPropagationPrefixes.DESCENDANT +
-  threadPermissions.EDIT_ENTRIES;
-const editCalendarPermissions = new Set([editEntries, descendantEditEntries]);
+const editCalendarPermissions = new Set([editEntries]);
 
 const knowOfSecretChannelsPermission = {
   title: 'Know of secret channels',
@@ -170,42 +167,16 @@ const createAndEditChannelsPermission = {
   userSurfacedPermission: userSurfacedPermissions.CREATE_AND_EDIT_CHANNELS,
 };
 const editThreadName = threadPermissions.EDIT_THREAD_NAME;
-const descendantEditThreadName =
-  threadPermissionPropagationPrefixes.DESCENDANT +
-  threadPermissions.EDIT_THREAD_NAME;
 const editThreadDescription = threadPermissions.EDIT_THREAD_DESCRIPTION;
-const descendantEditThreadDescription =
-  threadPermissionPropagationPrefixes.DESCENDANT +
-  threadPermissions.EDIT_THREAD_DESCRIPTION;
 const editThreadColor = threadPermissions.EDIT_THREAD_COLOR;
-const descendantEditThreadColor =
-  threadPermissionPropagationPrefixes.DESCENDANT +
-  threadPermissions.EDIT_THREAD_COLOR;
 const createSubchannels = threadPermissions.CREATE_SUBCHANNELS;
-const descendantCreateSubchannels =
-  threadPermissionPropagationPrefixes.DESCENDANT +
-  threadPermissionFilterPrefixes.TOP_LEVEL +
-  threadPermissions.CREATE_SUBCHANNELS;
 const editThreadAvatar = threadPermissions.EDIT_THREAD_AVATAR;
-const descendantEditThreadAvatar =
-  threadPermissionPropagationPrefixes.DESCENDANT +
-  threadPermissions.EDIT_THREAD_AVATAR;
-const descendantTopLevelCreateSidebars =
-  threadPermissionPropagationPrefixes.DESCENDANT +
-  threadPermissionFilterPrefixes.TOP_LEVEL +
-  threadPermissions.CREATE_SIDEBARS;
 const createAndEditChannelsPermissions = new Set([
   editThreadName,
-  descendantEditThreadName,
   editThreadDescription,
-  descendantEditThreadDescription,
   editThreadColor,
-  descendantEditThreadColor,
   createSubchannels,
-  descendantCreateSubchannels,
   editThreadAvatar,
-  descendantEditThreadAvatar,
-  descendantTopLevelCreateSidebars,
 ]);
 
 const deleteChannelsPermission = {
@@ -214,13 +185,7 @@ const deleteChannelsPermission = {
   userSurfacedPermission: userSurfacedPermissions.DELETE_CHANNELS,
 };
 const deleteThread = threadPermissions.DELETE_THREAD;
-const descendantDeleteThread =
-  threadPermissionPropagationPrefixes.DESCENDANT +
-  threadPermissions.DELETE_THREAD;
-const deleteChannelsPermissions = new Set([
-  deleteThread,
-  descendantDeleteThread,
-]);
+const deleteChannelsPermissions = new Set([deleteThread]);
 
 const addMembersPermission = {
   title: 'Add members',
@@ -228,18 +193,11 @@ const addMembersPermission = {
   userSurfacedPermission: userSurfacedPermissions.ADD_MEMBERS,
 };
 const addMembers = threadPermissions.ADD_MEMBERS;
-const descendantAddMembers =
-  threadPermissionPropagationPrefixes.DESCENDANT +
-  threadPermissions.ADD_MEMBERS;
 const childOpenAddMembers =
   threadPermissionPropagationPrefixes.CHILD +
   threadPermissionFilterPrefixes.OPEN +
   threadPermissions.ADD_MEMBERS;
-const addMembersPermissions = new Set([
-  addMembers,
-  descendantAddMembers,
-  childOpenAddMembers,
-]);
+const addMembersPermissions = new Set([addMembers, childOpenAddMembers]);
 
 const removeMembersPermission = {
   title: 'Remove members',
@@ -247,13 +205,7 @@ const removeMembersPermission = {
   userSurfacedPermission: userSurfacedPermissions.REMOVE_MEMBERS,
 };
 const removeMembers = threadPermissions.REMOVE_MEMBERS;
-const descendantRemoveMembers =
-  threadPermissionPropagationPrefixes.DESCENDANT +
-  threadPermissions.REMOVE_MEMBERS;
-const removeMembersPermissions = new Set([
-  removeMembers,
-  descendantRemoveMembers,
-]);
+const removeMembersPermissions = new Set([removeMembers]);
 
 const changeRolePermission = {
   title: 'Change roles',
@@ -261,10 +213,7 @@ const changeRolePermission = {
   userSurfacedPermission: userSurfacedPermissions.CHANGE_ROLES,
 };
 const changeRole = threadPermissions.CHANGE_ROLE;
-const descendantChangeRole =
-  threadPermissionPropagationPrefixes.DESCENDANT +
-  threadPermissions.CHANGE_ROLE;
-const changeRolePermissions = new Set([changeRole, descendantChangeRole]);
+const changeRolePermissions = new Set([changeRole]);
 
 const editVisibilityPermission = {
   title: 'Edit visibility',
@@ -272,13 +221,7 @@ const editVisibilityPermission = {
   userSurfacedPermission: userSurfacedPermissions.EDIT_VISIBILITY,
 };
 const editPermissions = threadPermissions.EDIT_PERMISSIONS;
-const descendantEditPermissions =
-  threadPermissionPropagationPrefixes.DESCENDANT +
-  threadPermissions.EDIT_PERMISSIONS;
-const editVisibilityPermissions = new Set([
-  editPermissions,
-  descendantEditPermissions,
-]);
+const editVisibilityPermissions = new Set([editPermissions]);
 
 const managePinsPermission = {
   title: 'Manage pins',
@@ -286,10 +229,7 @@ const managePinsPermission = {
   userSurfacedPermission: userSurfacedPermissions.MANAGE_PINS,
 };
 const managePins = threadPermissions.MANAGE_PINS;
-const descendantManagePins =
-  threadPermissionPropagationPrefixes.DESCENDANT +
-  threadPermissions.MANAGE_PINS;
-const managePinsPermissions = new Set([managePins, descendantManagePins]);
+const managePinsPermissions = new Set([managePins]);
 
 const reactToMessagePermission = {
   title: 'React to messages',
@@ -297,13 +237,7 @@ const reactToMessagePermission = {
   userSurfacedPermission: userSurfacedPermissions.REACT_TO_MESSAGES,
 };
 const reactToMessage = threadPermissions.REACT_TO_MESSAGE;
-const descendantReactToMessage =
-  threadPermissionPropagationPrefixes.DESCENDANT +
-  threadPermissions.REACT_TO_MESSAGE;
-const reactToMessagePermissions = new Set([
-  reactToMessage,
-  descendantReactToMessage,
-]);
+const reactToMessagePermissions = new Set([reactToMessage]);
 
 const editMessagePermission = {
   title: 'Edit messages',
@@ -311,10 +245,7 @@ const editMessagePermission = {
   userSurfacedPermission: userSurfacedPermissions.EDIT_MESSAGES,
 };
 const editMessage = threadPermissions.EDIT_MESSAGE;
-const descendantEditMessage =
-  threadPermissionPropagationPrefixes.DESCENDANT +
-  threadPermissions.EDIT_MESSAGE;
-const editMessagePermissions = new Set([editMessage, descendantEditMessage]);
+const editMessagePermissions = new Set([editMessage]);
 
 const manageInviteLinksPermission = {
   title: 'Manage invite links',
@@ -322,13 +253,7 @@ const manageInviteLinksPermission = {
   userSurfacedPermission: userSurfacedPermissions.MANAGE_INVITE_LINKS,
 };
 const manageInviteLinks = threadPermissions.MANAGE_INVITE_LINKS;
-const descendantManageInviteLinks =
-  threadPermissionPropagationPrefixes.DESCENDANT +
-  threadPermissions.MANAGE_INVITE_LINKS;
-const manageInviteLinksPermissions = new Set([
-  manageInviteLinks,
-  descendantManageInviteLinks,
-]);
+const manageInviteLinksPermissions = new Set([manageInviteLinks]);
 
 export type UserSurfacedPermissionOption = {
   +title: string,
@@ -388,8 +313,7 @@ export const universalCommunityPermissions: $ReadOnlyArray<string> = [
     threadPermissionFilterPrefixes.OPEN +
     threadPermissions.VISIBLE,
 
-  // join_thread | child_open_join_thread | descendant_opentoplevel_join_thread
-  threadPermissions.JOIN_THREAD,
+  // child_open_join_thread | descendant_opentoplevel_join_thread
   threadPermissionPropagationPrefixes.CHILD +
     threadPermissionFilterPrefixes.OPEN +
     threadPermissions.JOIN_THREAD,

lib/types/thread-permission-types.test.js
@@ -4,12 +4,11 @@ import {
   configurableCommunityPermissions,
   universalCommunityPermissions,
   userSurfacedPermissions,
-  type UserSurfacedPermission,
   threadPermissions,
 } from './thread-permission-types.js';
 import { getRolePermissionBlobs } from '../permissions/thread-permissions.js';
 import { threadTypes } from '../types/thread-types-enum.js';
-import { deepDiff, values } from '../utils/objects.js';
+import { deepDiff } from '../utils/objects.js';
 
 describe('Community Announcement Root', () => {
   it('should find Member permissions from getRolePermissionBlobs and user-surfaced permissions to be equal', () => {
@@ -34,30 +33,9 @@ describe('Community Announcement Root', () => {
     expect(
       deepDiff(membersPermissionBlob, membersPermissionsConstructedBlob),
     ).toEqual({});
-  });
-
-  it('should find Admin permissions from getRolePermissionBlobs and user-surfaced permissions to be equal', () => {
-    const adminsPermissionBlob =
-      getRolePermissionBlobs(threadTypes.COMMUNITY_ANNOUNCEMENT_ROOT).Admins ??
-      {};
-
-    const adminsPermissionsConstructed = [
-      ...values(userSurfacedPermissions)
-        .map((permission: UserSurfacedPermission) => [
-          ...configurableCommunityPermissions[permission],
-        ])
-        .flat(),
-      ...universalCommunityPermissions,
-    ];
-
-    const adminsPermissionsConstructedBlob = Object.fromEntries(
-      adminsPermissionsConstructed.map(permission => [permission, true]),
-    );
-
-    // Context: https://phab.comm.dev/D8478#inline-55680
     expect(
-      deepDiff(adminsPermissionBlob, adminsPermissionsConstructedBlob),
-    ).toEqual({ descendant_voiced: true });
+      deepDiff(membersPermissionsConstructedBlob, membersPermissionBlob),
+    ).toEqual({});
   });
 });
 
@@ -90,6 +68,9 @@ describe('Community Root', () => {
 
     expect(
       deepDiff(membersPermissionBlob, membersPermissionsConstructedBlob),
+    ).toEqual({});
+    expect(
+      deepDiff(membersPermissionsConstructedBlob, membersPermissionBlob),
     ).toEqual({});
   });
 });

lib/utils/migration-utils.js
@@ -130,6 +130,50 @@ const permissionsToRemoveInMigration: $ReadOnlyArray<string> = [
   threadPermissionPropagationPrefixes.DESCENDANT +
     threadPermissionFilterPrefixes.OPEN +
     threadPermissions.VOICED,
+
+  threadPermissions.JOIN_THREAD,
+
+  threadPermissionPropagationPrefixes.DESCENDANT +
+    threadPermissions.EDIT_ENTRIES,
+
+  threadPermissionPropagationPrefixes.DESCENDANT +
+    threadPermissions.EDIT_THREAD_NAME,
+  threadPermissionPropagationPrefixes.DESCENDANT +
+    threadPermissions.EDIT_THREAD_DESCRIPTION,
+  threadPermissionPropagationPrefixes.DESCENDANT +
+    threadPermissions.EDIT_THREAD_COLOR,
+  threadPermissionPropagationPrefixes.DESCENDANT +
+    threadPermissionFilterPrefixes.TOP_LEVEL +
+    threadPermissions.CREATE_SUBCHANNELS,
+  threadPermissionPropagationPrefixes.DESCENDANT +
+    threadPermissions.EDIT_THREAD_AVATAR,
+  threadPermissionPropagationPrefixes.DESCENDANT +
+    threadPermissionFilterPrefixes.TOP_LEVEL +
+    threadPermissions.CREATE_SIDEBARS,
+
+  threadPermissionPropagationPrefixes.DESCENDANT +
+    threadPermissions.ADD_MEMBERS,
+
+  threadPermissionPropagationPrefixes.DESCENDANT +
+    threadPermissions.REMOVE_MEMBERS,
+
+  threadPermissionPropagationPrefixes.DESCENDANT +
+    threadPermissions.CHANGE_ROLE,
+
+  threadPermissionPropagationPrefixes.DESCENDANT +
+    threadPermissions.EDIT_PERMISSIONS,
+
+  threadPermissionPropagationPrefixes.DESCENDANT +
+    threadPermissions.MANAGE_PINS,
+
+  threadPermissionPropagationPrefixes.DESCENDANT +
+    threadPermissions.REACT_TO_MESSAGE,
+
+  threadPermissionPropagationPrefixes.DESCENDANT +
+    threadPermissions.EDIT_MESSAGE,
+
+  threadPermissionPropagationPrefixes.DESCENDANT +
+    threadPermissions.MANAGE_INVITE_LINKS,
 ];
 
 export {

commit ba63257e269eabbdec25fab82b2d0774dd374d93

[lib] Fix role permissions for channels
Summary:
This diff addresses [ENG-8953](https://linear.app/comm/issue/ENG-8953/role-permissions-only-affect-community-root). I went through each broken permission listed [here](https://linear.app/comm/issue/ENG-8953/role-permissions-only-affect-community-root#comment-ad79bb28).

1. The changes in `lib/types/thread-permission-types.js` update the mapping from `UserSurfacedPermission` to role permission strings, the representation used in MariaDB. We add permission strings that guarantee each user-surfaced permission propagates to descendants in the right way.
2. The changes in `lib/permissions/thread-permissions.js` removes these permissions from being assigned based on `ThreadType`. The user-surfaced permissions are responsible for these permissions, so we need to make sure they don't appear if the admin didn't grant them.

Depends on D13016

Test Plan:
The whole stack was tested as follows:

1. Unit tests from D9686, which toggle user-surfaced permissions on and off and make sure no difference is caught. This ensures that the original issue introduced in D9686 isn't reintrod
uced
2. Careful review of each descendant permission removed in D9686
3. Create a community as `userA` and add `userB`. Grant tagging permissions to all members. Make sure `userB` can tag inside non-root channels
4. Do above, then create a channel without `userB`, and make sure `userB` can't tag there either (or do anything other than view). This is the repro described [here](https://linear.app/c
omm/issue/ENG-8953/role-permissions-only-affect-community-root#comment-94d7f10c)
5. Do above, but also create a thread inside the channel (as `userA`) and make sure `userB` can't do anything inside the thread other than view, until they join the parent channel

Reviewers: tomek, inka

lib/permissions/thread-permissions.js
@@ -346,17 +346,6 @@ function getRolePermissionBlobsForCommunityRoot(
   };
 }
 
-const nonCommunityVoicedPermissions = {
-  [threadPermissions.VOICED]: true,
-  [threadPermissions.EDIT_ENTRIES]: true,
-  [threadPermissions.EDIT_THREAD_NAME]: true,
-  [threadPermissions.EDIT_THREAD_COLOR]: true,
-  [threadPermissions.EDIT_THREAD_DESCRIPTION]: true,
-  [threadPermissions.EDIT_THREAD_AVATAR]: true,
-  [threadPermissions.CREATE_SUBCHANNELS]: true,
-  [threadPermissions.ADD_MEMBERS]: true,
-};
-
 function getRolePermissionBlobs(threadType: ThreadType): RolePermissionBlobs {
   if (threadTypeIsThick(threadType)) {
     const thickThreadType = assertThickThreadType(threadType);
@@ -371,15 +360,6 @@ function getRolePermissionBlobs(threadType: ThreadType): RolePermissionBlobs {
   if (thinThreadType === threadTypes.SIDEBAR) {
     const memberPermissions = {
       [threadPermissions.VOICED]: true,
-      [threadPermissions.REACT_TO_MESSAGE]: true,
-      [threadPermissions.EDIT_MESSAGE]: true,
-      [threadPermissions.EDIT_THREAD_NAME]: true,
-      [threadPermissions.EDIT_THREAD_COLOR]: true,
-      [threadPermissions.EDIT_THREAD_DESCRIPTION]: true,
-      [threadPermissions.EDIT_THREAD_AVATAR]: true,
-      [threadPermissions.ADD_MEMBERS]: true,
-      [threadPermissions.EDIT_PERMISSIONS]: true,
-      [threadPermissions.REMOVE_MEMBERS]: true,
       [threadPermissions.LEAVE_THREAD]: true,
     };
     return {
@@ -396,12 +376,7 @@ function getRolePermissionBlobs(threadType: ThreadType): RolePermissionBlobs {
       [threadPermissions.KNOW_OF]: true,
       [threadPermissions.VISIBLE]: true,
       [threadPermissions.VOICED]: true,
-      [threadPermissions.REACT_TO_MESSAGE]: true,
-      [threadPermissions.EDIT_MESSAGE]: true,
-      [threadPermissions.EDIT_THREAD_COLOR]: true,
-      [threadPermissions.EDIT_THREAD_DESCRIPTION]: true,
       [threadPermissions.CREATE_SIDEBARS]: true,
-      [threadPermissions.EDIT_ENTRIES]: true,
       [openDescendantKnowOf]: true,
       [openDescendantVisible]: true,
       [openChildJoinThread]: true,
@@ -417,12 +392,6 @@ function getRolePermissionBlobs(threadType: ThreadType): RolePermissionBlobs {
         [threadPermissions.KNOW_OF]: true,
         [threadPermissions.VISIBLE]: true,
         [threadPermissions.VOICED]: true,
-        [threadPermissions.REACT_TO_MESSAGE]: true,
-        [threadPermissions.EDIT_MESSAGE]: true,
-        [threadPermissions.EDIT_ENTRIES]: true,
-        [threadPermissions.EDIT_THREAD_NAME]: true,
-        [threadPermissions.EDIT_THREAD_COLOR]: true,
-        [threadPermissions.EDIT_THREAD_DESCRIPTION]: true,
         [threadPermissions.CREATE_SIDEBARS]: true,
         [openDescendantKnowOf]: true,
         [openDescendantVisible]: true,
@@ -437,8 +406,6 @@ function getRolePermissionBlobs(threadType: ThreadType): RolePermissionBlobs {
   const subthreadBasePermissions = {
     [threadPermissions.KNOW_OF]: true,
     [threadPermissions.VISIBLE]: true,
-    [threadPermissions.REACT_TO_MESSAGE]: true,
-    [threadPermissions.EDIT_MESSAGE]: true,
     [threadPermissions.CREATE_SIDEBARS]: true,
     [threadPermissions.LEAVE_THREAD]: true,
     [openDescendantKnowOf]: true,
@@ -452,10 +419,8 @@ function getRolePermissionBlobs(threadType: ThreadType): RolePermissionBlobs {
     thinThreadType === threadTypes.COMMUNITY_SECRET_SUBTHREAD
   ) {
     const memberPermissions = {
-      [threadPermissions.REMOVE_MEMBERS]: true,
-      [threadPermissions.EDIT_PERMISSIONS]: true,
       ...subthreadBasePermissions,
-      ...nonCommunityVoicedPermissions,
+      [threadPermissions.VOICED]: true,
     };
     return {
       Members: memberPermissions,

lib/types/thread-permission-types.js
@@ -150,7 +150,11 @@ const editCalendarPermission = {
   userSurfacedPermission: userSurfacedPermissions.EDIT_CALENDAR,
 };
 const editEntries = threadPermissions.EDIT_ENTRIES;
-const editCalendarPermissions = new Set([editEntries]);
+const descendantEditEntries =
+  threadPermissionPropagationPrefixes.DESCENDANT +
+  threadPermissionMembershipPrefixes.MEMBER +
+  threadPermissions.EDIT_ENTRIES;
+const editCalendarPermissions = new Set([editEntries, descendantEditEntries]);
 
 const knowOfSecretChannelsPermission = {
   title: 'Know of secret channels',
@@ -201,16 +205,42 @@ const createAndEditChannelsPermission = {
   userSurfacedPermission: userSurfacedPermissions.CREATE_AND_EDIT_CHANNELS,
 };
 const editThreadName = threadPermissions.EDIT_THREAD_NAME;
+const descendantEditThreadName =
+  threadPermissionPropagationPrefixes.DESCENDANT +
+  threadPermissionMembershipPrefixes.MEMBER +
+  threadPermissions.EDIT_THREAD_NAME;
 const editThreadDescription = threadPermissions.EDIT_THREAD_DESCRIPTION;
+const descendantEditThreadDescription =
+  threadPermissionPropagationPrefixes.DESCENDANT +
+  threadPermissionMembershipPrefixes.MEMBER +
+  threadPermissions.EDIT_THREAD_DESCRIPTION;
 const editThreadColor = threadPermissions.EDIT_THREAD_COLOR;
+const descendantEditThreadColor =
+  threadPermissionPropagationPrefixes.DESCENDANT +
+  threadPermissionMembershipPrefixes.MEMBER +
+  threadPermissions.EDIT_THREAD_COLOR;
 const createSubchannels = threadPermissions.CREATE_SUBCHANNELS;
+const descendantCreateSubchannels =
+  threadPermissionPropagationPrefixes.DESCENDANT +
+  threadPermissionFilterPrefixes.TOP_LEVEL +
+  threadPermissionMembershipPrefixes.MEMBER +
+  threadPermissions.CREATE_SUBCHANNELS;
 const editThreadAvatar = threadPermissions.EDIT_THREAD_AVATAR;
+const descendantEditThreadAvatar =
+  threadPermissionPropagationPrefixes.DESCENDANT +
+  threadPermissionMembershipPrefixes.MEMBER +
+  threadPermissions.EDIT_THREAD_AVATAR;
 const createAndEditChannelsPermissions = new Set([
   editThreadName,
+  descendantEditThreadName,
   editThreadDescription,
+  descendantEditThreadDescription,
   editThreadColor,
+  descendantEditThreadColor,
   createSubchannels,
+  descendantCreateSubchannels,
   editThreadAvatar,
+  descendantEditThreadAvatar,
 ]);
 
 const deleteChannelsPermission = {
@@ -219,18 +249,26 @@ const deleteChannelsPermission = {
   userSurfacedPermission: userSurfacedPermissions.DELETE_CHANNELS,
 };
 const deleteThread = threadPermissions.DELETE_THREAD;
-const deleteChannelsPermissions = new Set([deleteThread]);
+const descendantDeleteThread =
+  threadPermissionPropagationPrefixes.DESCENDANT +
+  threadPermissionMembershipPrefixes.MEMBER +
+  threadPermissions.DELETE_THREAD;
+const deleteChannelsPermissions = new Set([
+  deleteThread,
+  descendantDeleteThread,
+]);
 
 const addMembersPermission = {
   title: 'Add members',
   description: 'Allows members to add other members to channels',
   userSurfacedPermission: userSurfacedPermissions.ADD_MEMBERS,
 };
-const childOpenAddMembers =
-  threadPermissionPropagationPrefixes.CHILD +
-  threadPermissionFilterPrefixes.OPEN +
+const descendantAddMembers =
+  threadPermissionPropagationPrefixes.DESCENDANT +
+  threadPermissionFilterPrefixes.OPEN_TOP_LEVEL +
+  threadPermissionMembershipPrefixes.MEMBER +
   threadPermissions.ADD_MEMBERS;
-const addMembersPermissions = new Set([childOpenAddMembers]);
+const addMembersPermissions = new Set([descendantAddMembers]);
 
 const removeMembersPermission = {
   title: 'Remove members',
@@ -238,7 +276,14 @@ const removeMembersPermission = {
   userSurfacedPermission: userSurfacedPermissions.REMOVE_MEMBERS,
 };
 const removeMembers = threadPermissions.REMOVE_MEMBERS;
-const removeMembersPermissions = new Set([removeMembers]);
+const descendantRemoveMembers =
+  threadPermissionPropagationPrefixes.DESCENDANT +
+  threadPermissionMembershipPrefixes.MEMBER +
+  threadPermissions.REMOVE_MEMBERS;
+const removeMembersPermissions = new Set([
+  removeMembers,
+  descendantRemoveMembers,
+]);
 
 const changeRolePermission = {
   title: 'Change roles',
@@ -254,7 +299,14 @@ const editVisibilityPermission = {
   userSurfacedPermission: userSurfacedPermissions.EDIT_VISIBILITY,
 };
 const editPermissions = threadPermissions.EDIT_PERMISSIONS;
-const editVisibilityPermissions = new Set([editPermissions]);
+const descendantEditPermissions =
+  threadPermissionPropagationPrefixes.DESCENDANT +
+  threadPermissionMembershipPrefixes.MEMBER +
+  threadPermissions.EDIT_PERMISSIONS;
+const editVisibilityPermissions = new Set([
+  editPermissions,
+  descendantEditPermissions,
+]);
 
 const managePinsPermission = {
   title: 'Manage pins',
@@ -262,7 +314,11 @@ const managePinsPermission = {
   userSurfacedPermission: userSurfacedPermissions.MANAGE_PINS,
 };
 const managePins = threadPermissions.MANAGE_PINS;
-const managePinsPermissions = new Set([managePins]);
+const descendantManagePins =
+  threadPermissionPropagationPrefixes.DESCENDANT +
+  threadPermissionMembershipPrefixes.MEMBER +
+  threadPermissions.MANAGE_PINS;
+const managePinsPermissions = new Set([managePins, descendantManagePins]);
 
 const reactToMessagePermission = {
   title: 'React to messages',
@@ -270,7 +326,14 @@ const reactToMessagePermission = {
   userSurfacedPermission: userSurfacedPermissions.REACT_TO_MESSAGES,
 };
 const reactToMessage = threadPermissions.REACT_TO_MESSAGE;
-const reactToMessagePermissions = new Set([reactToMessage]);
+const descendantReactToMessage =
+  threadPermissionPropagationPrefixes.DESCENDANT +
+  threadPermissionMembershipPrefixes.MEMBER +
+  threadPermissions.REACT_TO_MESSAGE;
+const reactToMessagePermissions = new Set([
+  reactToMessage,
+  descendantReactToMessage,
+]);
 
 const editMessagePermission = {
   title: 'Edit messages',
@@ -278,7 +341,11 @@ const editMessagePermission = {
   userSurfacedPermission: userSurfacedPermissions.EDIT_MESSAGES,
 };
 const editMessage = threadPermissions.EDIT_MESSAGE;
-const editMessagePermissions = new Set([editMessage]);
+const descendantEditMessage =
+  threadPermissionPropagationPrefixes.DESCENDANT +
+  threadPermissionMembershipPrefixes.MEMBER +
+  threadPermissions.EDIT_MESSAGE;
+const editMessagePermissions = new Set([editMessage, descendantEditMessage]);
 
 const manageInviteLinksPermission = {
   title: 'Manage invite links',
@@ -286,7 +353,14 @@ const manageInviteLinksPermission = {
   userSurfacedPermission: userSurfacedPermissions.MANAGE_INVITE_LINKS,
 };
 const manageInviteLinks = threadPermissions.MANAGE_INVITE_LINKS;
-const manageInviteLinksPermissions = new Set([manageInviteLinks]);
+const descendantManageInviteLinks =
+  threadPermissionPropagationPrefixes.DESCENDANT +
+  threadPermissionMembershipPrefixes.MEMBER +
+  threadPermissions.MANAGE_INVITE_LINKS;
+const manageInviteLinksPermissions = new Set([
+  manageInviteLinks,
+  descendantManageInviteLinks,
+]);
 
 const manageFarcasterChannelTagsPermission = {
   title: 'Manage Farcaster channel tags',