34a969388cdf15e05404b50394e1d9c01efbe0f2
Inka Sokołowska <inka.d.sokolowska@gmail.com>
2024-08-14 16:04:17 +0200

    [native] Create muted tab tip
    Summary: issue: [[https://linear.app/comm/issue/ENG-8544/create-background-menu | ENG-8544]]
    
    Test Plan: Please see the video:
    
    Reviewers: tomek, will
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D13084

---
+++ native/navigation/muted-tab-tip.react.js
@@ -0,0 +1,42 @@
+// @flow
+
+import * as React from 'react';
+
+import { useCurrentUserFID } from 'lib/utils/farcaster-utils.js';
+
+import ChatTabBarButton from '../chat/chat-tab-bar-button.react.js';
+import { backgroundChatThreadListOptions } from '../chat/chat.react.js';
+import {
+  type NUXTipsOverlayProps,
+  createNUXTipsOverlay,
+} from '../tooltip/nux-tips-overlay.react.js';
+
+const mutedTabTipTextBase =
+  'When you mute a chat, it automatically gets moved to this tab.';
+const mutedTabTipTextIfFID =
+  ' When Comm automatically adds you to a community associated with a ' +
+  'Farcaster channel you follow, we mute them by default.';
+
+function WrappedChatTabBarButton(): React.Node {
+  const { title, tabBarIcon } = backgroundChatThreadListOptions;
+  return <ChatTabBarButton title={title} tabBarIcon={tabBarIcon} />;
+}
+
+function MutedTabTip(props: NUXTipsOverlayProps<'MutedTabTip'>): React.Node {
+  const fid = useCurrentUserFID();
+  console.log(fid);
+  const text = React.useMemo(() => {
+    if (!fid) {
+      return mutedTabTipTextBase;
+    }
+    return mutedTabTipTextBase + mutedTabTipTextIfFID;
+  }, [fid]);
+
+  const MutedTabTipComponentType: React.ComponentType<
+    NUXTipsOverlayProps<'MutedTabTip'>,
+  > = createNUXTipsOverlay(WrappedChatTabBarButton, text);
+
+  return <MutedTabTipComponentType {...props} />;
+}
+
+export default MutedTabTip;
