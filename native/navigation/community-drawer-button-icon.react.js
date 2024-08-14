04ec7c793cef80d7cefb3fca432d6486ba2bf3b5
Inka Sokołowska <inka.d.sokolowska@gmail.com>
2024-08-14 14:42:19 +0200

    [native] Extract community drawer button for nux tips
    Summary:
    issue: [[https://linear.app/comm/issue/ENG-8542/create-navigation-side-drawer-tip-button | ENG-8542]]
    We need to be able to display the button without it navigating to the drawer and without the effect being run
    
    Test Plan: Tested that the community drawer button looks like it used to
    
    Reviewers: tomek, will
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D13081

---
+++ native/navigation/community-drawer-button-icon.react.js
@@ -0,0 +1,19 @@
+// @flow
+
+import Icon from '@expo/vector-icons/Feather.js';
+import * as React from 'react';
+
+import { useStyles } from '../themes/colors.js';
+
+// eslint-disable-next-line no-unused-vars
+export default function CommunityDrawerButtonIcon(props: { ... }): React.Node {
+  const styles = useStyles(unboundStyles);
+  return <Icon name="menu" size={26} style={styles.drawerButton} />;
+}
+
+const unboundStyles = {
+  drawerButton: {
+    color: 'listForegroundSecondaryLabel',
+    marginLeft: 16,
+  },
+};
---
+++ native/navigation/community-drawer-tip.react.js
@@ -0,0 +1,20 @@
+// @flow
+
+import * as React from 'react';
+
+import CommunityDrawerButtonIcon from './community-drawer-button-icon.react.js';
+import {
+  type NUXTipsOverlayProps,
+  createNUXTipsOverlay,
+} from '../tooltip/nux-tips-overlay.react.js';
+
+const communityDrawerText =
+  'You can use this view to explore the tree of channels ' +
+  'inside a community. This shows you all of the channels you can see, ' +
+  "including ones you haven't joined.";
+
+const CommunityDrawerTip: React.ComponentType<
+  NUXTipsOverlayProps<'CommunityDrawerTip'>,
+> = createNUXTipsOverlay(CommunityDrawerButtonIcon, communityDrawerText);
+
+export default CommunityDrawerTip;
