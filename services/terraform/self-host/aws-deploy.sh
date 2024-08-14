9793890fb05b7f3b3983fca3bcd866b02e14ad89
William Wang <williamwang749@gmail.com>
2024-08-14 15:42:05 -0400

    [terraform] remove unnecessary aws deploy script commands to adjust ingress rules to keyserver load balancer
    Summary:
    Running the function to restore ingress rules is unnecessary as running terraform apply already takes care of this. The script is not broken with this function, but will
    output unnecessary output noting that authorizing the general ip access cidr of 0.0.0.0/0 already exists and there is no personal ip ingress rule to unauthorize.
    
    Test Plan:
    Tested by running script and was able to access the keyserver
    
    Reviewers:
    bartek,
    varun
    
    Subscribers:

--- services/terraform/self-host/aws-deploy.sh
+++ services/terraform/self-host/aws-deploy.sh
@@ -186,9 +186,6 @@ aws ecs update-service --cluster "$cluster_name" --service "$primary_service_nam
 echo "Waiting for health check at $health_check_domain to return status 200 OK"
 check_health
 
-echo "Primary node successfully running. Re-enabling public access to load balancer"
-enable_lb_traffic
-
 echo "Setting desired count of secondary service to $num_desired_secondary_nodes".
 aws ecs update-service --cluster "$cluster_name" --service "$secondary_service_name" --desired-count "$num_desired_secondary_nodes" --force-new-deployment > /dev/null
 
