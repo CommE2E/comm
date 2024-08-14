30047630da0e2f31e5d6921972b68c488d53cb24
Inka Sokołowska <inka.d.sokolowska@gmail.com>
2024-08-14 14:38:37 +0200

    [native] Add <next> button to nux tips
    Summary:
    issue: [[https://linear.app/comm/issue/ENG-8617/create-nux-tooltip-item | ENG-8617]]
    This button will mavigete to the next tip and dismiss the tips in the last tip.
    {F2471216}
    
    Test Plan: Tested with following diffs, tested that the button navigates to the next tip and dismisses on the last tip
    
    Reviewers: tomek, will
    
    Reviewed By: tomek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D13080

--- native/tooltip/nux-tips-overlay.react.js
+++ native/tooltip/nux-tips-overlay.react.js
@@ -13,9 +13,11 @@ import Animated, {
 } from 'react-native-reanimated';
 
 import {
+  getNUXTipParams,
   NUXTipsContext,
   type NUXTip,
 } from '../components/nux-tips-context.react.js';
+import PrimaryButton from '../components/primary-button.react.js';
 import type { AppNavigationProp } from '../navigation/app-navigator.react.js';
 import { OverlayContext } from '../navigation/overlay-context.js';
 import type {
@@ -52,6 +54,7 @@ const unboundStyles = {
     backgroundColor: 'tooltipBackground',
     borderRadius: 5,
     overflow: 'hidden',
+    padding: 20,
   },
   triangleUp: {
     borderBottomColor: 'tooltipBackground',
@@ -84,7 +87,11 @@ const unboundStyles = {
   tipText: {
     color: 'panelForegroundLabel',
     fontSize: 20,
-    padding: 15,
+    marginBottom: 10,
+  },
+  buttonContainer: {
+    width: 100,
+    alignSelf: 'flex-end',
   },
 };
 
@@ -346,6 +353,28 @@ function createNUXTipsOverlay<Route: NUXTipRouteNames>(
       triangleUp = <View style={[styles.triangleUp, triangleStyle]} />;
     }
 
+    const callbackParams = getNUXTipParams(route.params.tipKey);
+
+    const onPressOk = React.useCallback(() => {
+      const {
+        nextTip,
+        tooltipLocation: nextLocation,
+        nextRouteName,
+      } = callbackParams;
+      goBackOnce();
+
+      if (!nextTip || !nextRouteName) {
+        return;
+      }
+      navigation.navigate<NUXTipRouteNames>({
+        name: nextRouteName,
+        params: {
+          tipKey: nextTip,
+          tooltipLocation: nextLocation,
+        },
+      });
+    }, [callbackParams, goBackOnce, navigation]);
+
     return (
       <TouchableWithoutFeedback onPress={goBackOnce}>
         <View style={styles.container}>
@@ -371,6 +400,13 @@ function createNUXTipsOverlay<Route: NUXTipRouteNames>(
             {triangleUp}
             <View style={styles.items}>
               <Text style={styles.tipText}>{tipText}</Text>
+              <View style={styles.buttonContainer}>
+                <PrimaryButton
+                  onPress={onPressOk}
+                  label="Next"
+                  variant="enabled"
+                />
+              </View>
             </View>
             {triangleDown}
           </AnimatedView>
