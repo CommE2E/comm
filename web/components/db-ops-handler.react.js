f022b32a30c9b3881b47df17f4975480a524bbd5
xsanm <kkurowski.kk@gmail.com>
2024-08-14 16:54:17 +0200

    [SQLite] enable DB on web for all users
    Summary:
    [ENG-8075](https://linear.app/comm/issue/ENG-8075/enable-db-on-web-for-all-users).
    
    Removing `canUseDatabaseOnWeb`.
    
    Test Plan:
    1. Make `canUseDatabaseOnWeb` return false and use the app (data is returned from the keyserver).
    2. Apply this diff
    3. Refresh first time: data is returned from the keyserver but ops are processed on DB.
    4. Refresh a second time: data is not returned from the keyserver but read from DB.
    
    Reviewers: tomek, inka, marcin
    
    Reviewed By: tomek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D13108

--- web/components/db-ops-handler.react.js
+++ web/components/db-ops-handler.react.js
@@ -5,14 +5,12 @@ import * as React from 'react';
 import { DBOpsHandler as BaseHandler } from 'lib/handlers/db-ops-handler.react.js';
 import type { StoreOperations } from 'lib/types/store-ops-types.js';
 
-import { useSelector } from '../redux/redux-utils.js';
 import { processDBStoreOperations } from '../shared-worker/utils/store.js';
 
 function DBOpsHandler(): React.Node {
-  const currentUserID = useSelector(state => state.currentUserInfo?.id ?? null);
   const processOperations = React.useCallback(
-    (ops: StoreOperations) => processDBStoreOperations(ops, currentUserID),
-    [currentUserID],
+    (ops: StoreOperations) => processDBStoreOperations(ops),
+    [],
   );
   return <BaseHandler processDBStoreOperations={processOperations} />;
 }
