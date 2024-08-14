c2d92250cf2a6ceaab125dc544a341f09baa68e0
Inka Sokołowska <inka.d.sokolowska@gmail.com>
2024-08-14 15:50:49 +0200

    [native] Create custom top tabs component
    Summary:
    issue: [[https://linear.app/comm/issue/ENG-8541/create-background-tip-button | ENG-8541]]
    We need this to be able to display the button for the muted tab tip.
    For now this results in some changes to the style: the indicator doesn't smoothly move from one tab to the second, and there is some on press feedback that wans't present before. Ill address those changes in [[https://linear.app/comm/issue/ENG-9041/update-tabs-style | ENG-9041]]
    
    Test Plan: Please see the video:
    
    Reviewers: tomek, will
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D13082

---
+++ native/chat/chat-tab-bar.react.js
@@ -0,0 +1,73 @@
+// @flow
+
+import type {
+  MaterialTopTabBarProps,
+  Route,
+  TabBarItemProps,
+} from '@react-navigation/core';
+import { MaterialTopTabBar } from '@react-navigation/material-top-tabs';
+import invariant from 'invariant';
+import * as React from 'react';
+import { View } from 'react-native';
+import { TabBarItem } from 'react-native-tab-view';
+
+import {
+  nuxTip,
+  NUXTipsContext,
+} from '../components/nux-tips-context.react.js';
+import {
+  HomeChatThreadListRouteName,
+  BackgroundChatThreadListRouteName,
+} from '../navigation/route-names.js';
+
+const ButtonTitleToTip = Object.freeze({
+  [BackgroundChatThreadListRouteName]: nuxTip.MUTED,
+  [HomeChatThreadListRouteName]: nuxTip.HOME,
+});
+
+function TabBarButton(props: TabBarItemProps<Route<>>) {
+  const tipsContext = React.useContext(NUXTipsContext);
+  invariant(tipsContext, 'NUXTipsContext should be defined');
+
+  const viewRef = React.useRef<?React.ElementRef<typeof View>>();
+  const onLayout = React.useCallback(() => {
+    const button = viewRef.current;
+    if (!button) {
+      return;
+    }
+
+    const tipType = ButtonTitleToTip[props.route.name];
+    if (!tipType) {
+      return;
+    }
+    button.measure((x, y, width, height, pageX, pageY) => {
+      tipsContext.registerTipButton(tipType, {
+        x,
+        y,
+        width,
+        height,
+        pageX,
+        pageY,
+      });
+    });
+  }, [props.route.name, tipsContext]);
+
+  return (
+    <View ref={viewRef} onLayout={onLayout}>
+      <TabBarItem {...props} />
+    </View>
+  );
+}
+
+export default function TabBarTop(
+  props: MaterialTopTabBarProps<Route<>>,
+): React.Node {
+  const renderTabBarItem = React.useCallback(
+    (innerProps: $ReadOnly<{ ...TabBarItemProps<Route<>>, key: string }>) => (
+      <TabBarButton {...innerProps} />
+    ),
+    [],
+  );
+
+  return <MaterialTopTabBar {...props} renderTabBarItem={renderTabBarItem} />;
+}
