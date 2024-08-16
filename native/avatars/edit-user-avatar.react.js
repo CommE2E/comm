a67f3a61c4fb5d0217d760a36d63b422b76a3f4c
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2024-08-16 00:11:27 -0400

    [native] let user choose farcaster avatar
    Summary:
    This diff makes it possible for a new user to select a farcaster avatar during registration if they have connected their farcaster account. it also makes it possible for existing users to set the farcaster avatar from the profile tab if there's a connected farcaster account.
    
    Depends on D13100
    
    Test Plan: tested at the end of the stack by successfully selecting farcaster avatar during registration. avatar is displayed immediately upon selection and appears in profile after registration is complete
    
    Reviewers: ashoat, tomek
    
    Reviewed By: ashoat
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D13101

--- native/avatars/edit-user-avatar.react.js
+++ native/avatars/edit-user-avatar.react.js
@@ -7,6 +7,7 @@ import { ActivityIndicator, TouchableOpacity, View } from 'react-native';
 
 import { EditUserAvatarContext } from 'lib/components/edit-user-avatar-provider.react.js';
 import { useENSAvatar } from 'lib/hooks/ens-cache.js';
+import { useFarcasterAvatarURL } from 'lib/hooks/fc-cache.js';
 import { getETHAddressForUserInfo } from 'lib/shared/account-utils.js';
 import type { GenericUserInfoWithAvatar } from 'lib/types/avatar-types.js';
 
@@ -27,11 +28,13 @@ import { useSelector } from '../redux/redux-utils.js';
 import { useStyles } from '../themes/colors.js';
 
 type Props =
-  | { +userID: ?string, +disabled?: boolean }
+  | { +userID: ?string, +disabled?: boolean, +fid: ?string }
   | {
       +userInfo: ?GenericUserInfoWithAvatar,
       +disabled?: boolean,
-      +prefetchedAvatarURI: ?string,
+      +prefetchedENSAvatarURI: ?string,
+      +prefetchedFarcasterAvatarURL: ?string,
+      +fid: ?string,
     };
 function EditUserAvatar(props: Props): React.Node {
   const editUserAvatarContext = React.useContext(EditUserAvatarContext);
@@ -53,7 +56,12 @@ function EditUserAvatar(props: Props): React.Node {
     [userInfo],
   );
   const fetchedENSAvatarURI = useENSAvatar(ethAddress);
-  const ensAvatarURI = fetchedENSAvatarURI ?? props.prefetchedAvatarURI;
+  const ensAvatarURI = fetchedENSAvatarURI ?? props.prefetchedENSAvatarURI;
+
+  const fid = props.fid;
+  const fetchedFarcasterAvatarURL = useFarcasterAvatarURL(fid);
+  const farcasterAvatarURL =
+    fetchedFarcasterAvatarURL ?? props.prefetchedFarcasterAvatarURL;
 
   const { navigate } = useNavigation();
 
@@ -82,6 +90,11 @@ function EditUserAvatar(props: Props): React.Node {
     [nativeSetUserAvatar],
   );
 
+  const setFarcasterUserAvatar = React.useCallback(
+    () => nativeSetUserAvatar({ type: 'farcaster' }),
+    [nativeSetUserAvatar],
+  );
+
   const removeUserAvatar = React.useCallback(
     () => nativeSetUserAvatar({ type: 'remove' }),
     [nativeSetUserAvatar],
@@ -99,19 +112,25 @@ function EditUserAvatar(props: Props): React.Node {
       configOptions.push({ id: 'ens', onPress: setENSUserAvatar });
     }
 
+    if (farcasterAvatarURL) {
+      configOptions.push({ id: 'farcaster', onPress: setFarcasterUserAvatar });
+    }
+
     if (hasCurrentAvatar) {
       configOptions.push({ id: 'remove', onPress: removeUserAvatar });
     }
 
     return configOptions;
   }, [
-    hasCurrentAvatar,
-    ensAvatarURI,
-    navigateToCamera,
     navigateToEmojiSelection,
-    removeUserAvatar,
-    setENSUserAvatar,
     selectFromGalleryAndUpdateUserAvatar,
+    navigateToCamera,
+    ensAvatarURI,
+    farcasterAvatarURL,
+    hasCurrentAvatar,
+    setENSUserAvatar,
+    setFarcasterUserAvatar,
+    removeUserAvatar,
   ]);
 
   const showAvatarActionSheet = useShowAvatarActionSheet(actionSheetConfig);
@@ -131,7 +150,7 @@ function EditUserAvatar(props: Props): React.Node {
   const userAvatar = userID ? (
     <UserAvatar userID={userID} size="XL" />
   ) : (
-    <UserAvatar userInfo={userInfo} size="XL" />
+    <UserAvatar userInfo={userInfo} fid={fid} size="XL" />
   );
 
   const { disabled } = props;
