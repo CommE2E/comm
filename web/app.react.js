014d19b71b674fd662722391202e11dddc742040
Ashoat Tevosyan <ashoat@comm.app>
2024-08-19 12:47:29 -0700

    [web][keyserver] codeVersion -> 108

--- web/app.react.js
+++ web/app.react.js
@@ -117,7 +117,7 @@ registerConfig({
   calendarRangeInactivityLimit: null,
   platformDetails: {
     platform: electron?.platform ?? 'web',
-    codeVersion: 107,
+    codeVersion: 108,
     stateVersion: persistConfig.version,
     ...desktopDetails,
   },
