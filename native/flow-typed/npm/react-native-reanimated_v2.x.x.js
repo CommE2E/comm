CommE2E/comm

commit 032ab74877dbed96265849419bf546b7dbeaa09b

[native] introduced flow type for reanimated layout animations
Summary:
To use Reanimated's new layout animations we need to add libdefs for the different animations. This diff just handles the `SlideInDown` and `SlideOutDown` layout animations along with all the necessary classes/types needed to support these layout animations. I will annotate inline with where the changes/types came from.

---

Linear Task: [[ https://linear.app/comm/issue/ENG-2165/flow-type-reanimated-layout-animation | ENG-2165 ]]

Test Plan: All the testing for this diff came from its use in D5557. In that diff I was able to implement the layout actions using the appropriate type defs that I created in this diff, and I received no flow errors which I believe to be a good indicator that everything is correct.

Reviewers: atul, ashoat

Reviewed By: atul, ashoat

Subscribers: ashoat, tomek, atul, abosh

Differential Revision: https://phab.comm.dev/D5556

native/flow-typed/npm/react-native-reanimated_v2.x.x.js
@@ -14,6 +14,26 @@
  */
 
 declare module 'react-native-reanimated' {
+  // This was taken from the flow typed library definitions of bottom-tabs_v6
+  declare type StyleObj =
+    | null
+    | void
+    | number
+    | false
+    | ''
+    | $ReadOnlyArray<StyleObj>
+    | { [name: string]: any, ... };
+
+  declare type ViewStyleProp = StyleObj;
+  declare type TextStyleProp = StyleObj;
+
+  declare type StyleProps = {|
+    ...ViewStyleProp,
+    ...TextStyleProp,
+    +originX?: number,
+    +originY?: number,
+    +[key: string]: any,
+  |};
   
   declare class Node { }
   
@@ -123,6 +143,7 @@ declare module 'react-native-reanimated' {
     ...
   };
   declare export var EasingNode: EasingModule;
+  declare type EasingFn = (t: number) => number;
 
   declare export type TimingState = {
     +finished: Value,
@@ -212,6 +233,165 @@ declare module 'react-native-reanimated' {
       config: DecayConfig,
     ): Node,
   |};
+
+  declare type LayoutAnimation = {|
+    +initialValues: StyleProps,
+    +animations: StyleProps,
+    +callback?: (finished: boolean) => void,
+  |};
+
+  declare type AnimationFunction = (a?: any, b?: any, c?: any) => any;
+
+  declare type EntryAnimationsValues = {|
+    +targetOriginX: number,
+    +targetOriginY: number,
+    +targetWidth: number,
+    +targetHeight: number,
+    +targetGlobalOriginX: number,
+    +targetGlobalOriginY: number,
+  |};
+
+  declare type ExitAnimationsValues = {|
+    +currentOriginX: number,
+    +currentOriginY: number,
+    +currentWidth: number,
+    +currentHeight: number,
+    +currentGlobalOriginX: number,
+    +currentGlobalOriginY: number,
+  |};
+
+  declare export type EntryExitAnimationFunction = (
+    targetValues: EntryAnimationsValues | ExitAnimationsValues,
+  ) => LayoutAnimation;
+
+  declare type AnimationConfigFunction<T> = (
+    targetValues: T,
+  ) => LayoutAnimation;
+
+  declare type LayoutAnimationsValues = {|
+    +currentOriginX: number,
+    +currentOriginY: number,
+    +currentWidth: number,
+    +currentHeight: number,
+    +currentGlobalOriginX: number,
+    +currentGlobalOriginY: number,
+    +targetOriginX: number,
+    +targetOriginY: number,
+    +targetWidth: number,
+    +targetHeight: number,
+    +targetGlobalOriginX: number,
+    +argetGlobalOriginY: number,
+    +windowWidth: number,
+    +windowHeight: number,
+  |};
+
+  declare type LayoutAnimationFunction = (
+    targetValues: LayoutAnimationsValues,
+  ) => LayoutAnimation;
+
+  declare type BaseLayoutAnimationConfig = {|
+    +duration?: number,
+    +easing?: EasingFn,
+    +type?: AnimationFunction,
+    +damping?: number,
+    +mass?: number,
+    +stiffness?: number,
+    +overshootClamping?: number,
+    +restDisplacementThreshold?: number,
+    +restSpeedThreshold?: number,
+  |};
+
+  declare type BaseBuilderAnimationConfig = {|
+    ...BaseLayoutAnimationConfig,
+    rotate?: number | string,
+  |};
+
+  declare type LayoutAnimationAndConfig = [
+    AnimationFunction,
+    BaseBuilderAnimationConfig,
+  ];
+
+  declare export class BaseAnimationBuilder {
+    static duration(durationMs: number): BaseAnimationBuilder;
+    duration(durationMs: number): BaseAnimationBuilder;
+
+    static delay(delayMs: number): BaseAnimationBuilder;
+    delay(delayMs: number): BaseAnimationBuilder;
+
+    static withCallback(
+      callback: (finished: boolean) => void,
+    ): BaseAnimationBuilder;
+    withCallback(callback: (finished: boolean) => void): BaseAnimationBuilder;
+
+    static getDuration(): number;
+    getDuration(): number;
+
+    static randomDelay(): BaseAnimationBuilder;
+    randomDelay(): BaseAnimationBuilder;
+
+    getDelay(): number;
+    getDelayFunction(): AnimationFunction;
+
+    static build(): EntryExitAnimationFunction | LayoutAnimationFunction;
+  }
+
+  declare export type ReanimatedAnimationBuilder =
+    | Class<BaseAnimationBuilder>
+    | BaseAnimationBuilder;
+
+  declare export class ComplexAnimationBuilder extends BaseAnimationBuilder {
+    static easing(easingFunction: EasingFn): ComplexAnimationBuilder;
+    easing(easingFunction: EasingFn): ComplexAnimationBuilder;
+
+    static rotate(degree: string): ComplexAnimationBuilder;
+    rotate(degree: string): ComplexAnimationBuilder;
+
+    static springify(): ComplexAnimationBuilder;
+    springify(): ComplexAnimationBuilder;
+
+    static damping(damping: number): ComplexAnimationBuilder;
+    damping(damping: number): ComplexAnimationBuilder;
+
+    static mass(mass: number): ComplexAnimationBuilder;
+    mass(mass: number): ComplexAnimationBuilder;
+
+    static stiffness(stiffness: number): ComplexAnimationBuilder;
+    stiffness(stiffness: number): ComplexAnimationBuilder;
+
+    static overshootClamping(
+      overshootClamping: number,
+    ): ComplexAnimationBuilder;
+    overshootClamping(overshootClamping: number): ComplexAnimationBuilder;
+
+    static restDisplacementThreshold(
+      restDisplacementThreshold: number,
+    ): ComplexAnimationBuilder;
+    restDisplacementThreshold(
+      restDisplacementThreshold: number,
+    ): ComplexAnimationBuilder;
+
+    static restSpeedThreshold(
+      restSpeedThreshold: number,
+    ): ComplexAnimationBuilder;
+    restSpeedThreshold(restSpeedThreshold: number): ComplexAnimationBuilder;
+
+    static withInitialValues(values: StyleProps): BaseAnimationBuilder;
+    withInitialValues(values: StyleProps): BaseAnimationBuilder;
+
+    getAnimationAndConfig(): LayoutAnimationAndConfig;
+  }
+
+  declare export class SlideInDown extends ComplexAnimationBuilder {
+    static createInstance(): SlideInDown;
+
+    build(): AnimationConfigFunction<EntryAnimationsValues>;
+  }
+
+  declare export class SlideOutDown extends ComplexAnimationBuilder {
+    static createInstance(): SlideOutDown;
+
+    build(): AnimationConfigFunction<ExitAnimationsValues>;
+  }
 
   declare type $SyntheticEvent<T: { ... }> = {
     +nativeEvent: $ReadOnly<$Exact<T>>,

commit c5c10d514ca63becb1b2b5349754ae3209f8163e

[native] Fix broken StyleProps type for react-native-reanimated libdef
Summary:
This issue was first introduced in D5556, and led to a `$FlowFixMe` being introduced in D13066.

It's not possible to spread a non-object. I remove the spreads here because the only relevant part of the union is the object form (`{ [name: string]: any, ... }`), which is already included in the `StyleProps` type anyways.

This diff addresses part of [ENG-9039](https://linear.app/comm/issue/ENG-9039/investigate-flow-errors).

Test Plan: Flow

Reviewers: inka, tomek

Subscribers:

native/flow-typed/npm/react-native-reanimated_v2.x.x.js
@@ -28,8 +28,6 @@ declare module 'react-native-reanimated' {
   declare type TextStyleProp = StyleObj;
 
   declare type StyleProps = $ReadOnly<{|
-    ...ViewStyleProp,
-    ...TextStyleProp,
     +originX?: number,
     +originY?: number,
     +[key: string]: any,

native/tooltip/nux-tips-overlay.react.js
@@ -338,7 +338,6 @@ function createNUXTipsOverlay<Route: NUXTipRouteNames>(
         <View style={styles.container}>
           <AnimatedView
             style={styles.backdrop}
-            // $FlowFixMe
             entering={opacityEnteringAnimation}
             exiting={opacityExitingAnimation}
           />