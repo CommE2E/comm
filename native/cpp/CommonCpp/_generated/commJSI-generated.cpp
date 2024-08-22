b90a5e732138946302495cd0b91c9de9bd8db97c
Tomasz Palys <tomasz.palys@swmansion.com>
2024-08-22 19:40:08 +0200

    [native] Expose messages fetching function through JSI
    Summary:
    Expose the function through JSI so that it can be used on native clients.
    
    https://linear.app/comm/issue/ENG-8701/create-c-functions-that-fetch-a-subset-of-messages
    
    Depends on D13140
    
    Test Plan: TODO
    
    Reviewers: kamil, inka
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D13141

--- native/cpp/CommonCpp/_generated/commJSI-generated.cpp
+++ native/cpp/CommonCpp/_generated/commJSI-generated.cpp
@@ -235,6 +235,9 @@ static jsi::Value __hostFunction_CommCoreModuleSchemaCxxSpecJSI_getRelatedMessag
 static jsi::Value __hostFunction_CommCoreModuleSchemaCxxSpecJSI_searchMessages(jsi::Runtime &rt, TurboModule &turboModule, const jsi::Value* args, size_t count) {
   return static_cast<CommCoreModuleSchemaCxxSpecJSI *>(&turboModule)->searchMessages(rt, args[0].asString(rt), args[1].asString(rt), args[2].isNull() || args[2].isUndefined() ? std::nullopt : std::make_optional(args[2].asString(rt)), args[3].isNull() || args[3].isUndefined() ? std::nullopt : std::make_optional(args[3].asString(rt)));
 }
+static jsi::Value __hostFunction_CommCoreModuleSchemaCxxSpecJSI_fetchMessages(jsi::Runtime &rt, TurboModule &turboModule, const jsi::Value* args, size_t count) {
+  return static_cast<CommCoreModuleSchemaCxxSpecJSI *>(&turboModule)->fetchMessages(rt, args[0].asString(rt), args[1].asNumber(), args[2].asNumber());
+}
 
 CommCoreModuleSchemaCxxSpecJSI::CommCoreModuleSchemaCxxSpecJSI(std::shared_ptr<CallInvoker> jsInvoker)
   : TurboModule("CommTurboModule", jsInvoker) {
@@ -310,6 +313,7 @@ CommCoreModuleSchemaCxxSpecJSI::CommCoreModuleSchemaCxxSpecJSI(std::shared_ptr<C
   methodMap_["markPrekeysAsPublished"] = MethodMetadata {0, __hostFunction_CommCoreModuleSchemaCxxSpecJSI_markPrekeysAsPublished};
   methodMap_["getRelatedMessages"] = MethodMetadata {1, __hostFunction_CommCoreModuleSchemaCxxSpecJSI_getRelatedMessages};
   methodMap_["searchMessages"] = MethodMetadata {4, __hostFunction_CommCoreModuleSchemaCxxSpecJSI_searchMessages};
+  methodMap_["fetchMessages"] = MethodMetadata {3, __hostFunction_CommCoreModuleSchemaCxxSpecJSI_fetchMessages};
 }
 
 
