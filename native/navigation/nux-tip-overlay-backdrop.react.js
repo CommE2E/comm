ecae90e10de723999de33f87ee023db2659c81e8
Inka Sokołowska <inka.d.sokolowska@gmail.com>
2024-08-22 09:54:45 +0200

    [native] Fix blinking overlay background
    Summary: issue: [[https://linear.app/comm/issue/ENG-9058/update-tip-style | ENG-9058]]
    
    Test Plan: Please see the video:
    
    Reviewers: tomek, will
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D13136

---
+++ native/navigation/nux-tip-overlay-backdrop.react.js
@@ -0,0 +1,105 @@
+// @flow
+
+import invariant from 'invariant';
+import * as React from 'react';
+import { withTiming } from 'react-native-reanimated';
+
+import type { AppNavigationProp } from './app-navigator.react.js';
+import { OverlayContext } from './overlay-context.js';
+import type { NUXTipRouteNames, NavigationRoute } from './route-names';
+import {
+  firstNUXTipKey,
+  getNUXTipParams,
+} from '../components/nux-tips-context.react.js';
+import { useStyles } from '../themes/colors.js';
+import { animationDuration } from '../tooltip/nux-tips-overlay.react.js';
+import { AnimatedView } from '../types/styles.js';
+
+type Props = {
+  +navigation: AppNavigationProp<'NUXTipOverlayBackdrop'>,
+  +route: NavigationRoute<'NUXTipOverlayBackdrop'>,
+};
+
+function NUXTipOverlayBackdrop(props: Props): React.Node {
+  const overlayContext = React.useContext(OverlayContext);
+  invariant(overlayContext, 'NUXTipsOverlay should have OverlayContext');
+  const { shouldRenderScreenContent } = overlayContext;
+
+  return shouldRenderScreenContent ? (
+    <NUXTipOverlayBackdropInner {...props} />
+  ) : null;
+}
+
+function opacityEnteringAnimation() {
+  'worklet';
+
+  return {
+    animations: {
+      opacity: withTiming(0.7, { duration: animationDuration }),
+    },
+    initialValues: {
+      opacity: 0,
+    },
+  };
+}
+
+function NUXTipOverlayBackdropInner(props: Props): React.Node {
+  const overlayContext = React.useContext(OverlayContext);
+  invariant(overlayContext, 'NUXTipsOverlay should have OverlayContext');
+  const { onExitFinish } = overlayContext;
+
+  const styles = useStyles(unboundStyles);
+
+  const opacityExitingAnimation = React.useCallback(() => {
+    'worklet';
+
+    return {
+      animations: {
+        opacity: withTiming(0, { duration: animationDuration }),
+      },
+      initialValues: {
+        opacity: 0.7,
+      },
+      callback: onExitFinish,
+    };
+  }, [onExitFinish]);
+
+  const { nextTip, tooltipLocation, nextRouteName } =
+    getNUXTipParams(firstNUXTipKey);
+  invariant(nextRouteName && nextTip, 'first nux tip should be defined');
+
+  React.useEffect(
+    () =>
+      props.navigation.navigate<NUXTipRouteNames>({
+        name: nextRouteName,
+        params: {
+          tipKey: nextTip,
+          tooltipLocation,
+        },
+      }),
+    // We want this effect to run exactly once, when this component is mounted
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [],
+  );
+
+  return (
+    <AnimatedView
+      style={styles.backdrop}
+      entering={opacityEnteringAnimation}
+      exiting={opacityExitingAnimation}
+    />
+  );
+}
+
+const unboundStyles = {
+  backdrop: {
+    backgroundColor: 'black',
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+    right: 0,
+    top: 0,
+  },
+};
+
+export default NUXTipOverlayBackdrop;
