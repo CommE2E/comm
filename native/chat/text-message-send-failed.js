cbc9d64d41032a50366720a410af3bd17f95a436
xsanm <kkurowski.kk@gmail.com>
2024-08-23 11:33:39 +0200

    [lib][web][native] implement detecting DM messages as failed
    Summary:
    [ENG-8423](https://linear.app/comm/issue/ENG-8423/support-e2ee-text-messages).
    
    Further simplification is complicated because of different types on different platforms.
    
    Depends on D13157
    
    Test Plan:
    Message visible in UI as failed.
    
    Note: This stack is tested end-to-end according to the process described [here](https://linear.app/comm/issue/ENG-8423/support-e2ee-text-messages#comment-b409cae7).
    
    Reviewers: tomek, inka
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D13158

--- native/chat/text-message-send-failed.js
+++ native/chat/text-message-send-failed.js
@@ -1,18 +1,12 @@
 // @flow
 
+import { textMessageSendFailed as sharedTextMessageSendFailed } from 'lib/shared/chat-utils.js';
+
 import type { ChatTextMessageInfoItemWithHeight } from '../types/chat-types.js';
 
 export default function textMessageSendFailed(
   item: ChatTextMessageInfoItemWithHeight,
 ): boolean {
-  const {
-    id,
-    creator: { isViewer },
-  } = item.messageInfo;
-  return !!(
-    isViewer &&
-    (id === null || id === undefined) &&
-    item.localMessageInfo &&
-    item.localMessageInfo.sendFailed
-  );
+  const { messageInfo, localMessageInfo } = item;
+  return sharedTextMessageSendFailed(messageInfo, localMessageInfo);
 }
--- web/chat/text-message-send-failed.js
+++ web/chat/text-message-send-failed.js
@@ -1,21 +1,17 @@
 // @flow
 
 import type { ChatMessageInfoItem } from 'lib/selectors/chat-selectors.js';
+import { textMessageSendFailed as sharedTextMessageSendFailed } from 'lib/shared/chat-utils.js';
 import { messageTypes } from 'lib/types/message-types-enum.js';
 
 export default function textMessageSendFailed(
   item: ChatMessageInfoItem,
 ): boolean {
-  const {
-    id,
-    creator: { isViewer },
-    type,
-  } = item.messageInfo;
-  return !!(
-    isViewer &&
-    type === messageTypes.TEXT &&
-    (id === null || id === undefined) &&
-    item.localMessageInfo &&
-    item.localMessageInfo.sendFailed
-  );
+  const { messageInfo, localMessageInfo } = item;
+
+  if (messageInfo.type !== messageTypes.TEXT) {
+    return false;
+  }
+
+  return sharedTextMessageSendFailed(messageInfo, localMessageInfo);
 }
