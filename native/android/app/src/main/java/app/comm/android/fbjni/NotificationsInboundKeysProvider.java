abfec25850181ae68ecc4314f9ac85457d2c033e
Marcin Wąsowicz <marcinwasowicz@MacBook-Pro-Marcin.local>
2024-08-26 13:55:40 +0200

    Return single curve25519 string from NotificationsInboundKeysProvider
    Summary: This differential returns only curve25519 key string from native code and creates JSON in C++ with folly instead of creating JSON in native code. The rieason for this change is that we saw issues with the content of the JSON returned from Java - Android app was unable to create session as inbound since sender identity key was corrupted. This change fixed the issue.
    
    Test Plan:
    1. Before this diff creating notifs session with Android device as inboud would fail each (> 90%) of the times.
    2. After this diff it always works.
    
    Reviewers: kamil
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D13165

--- native/android/app/src/main/java/app/comm/android/fbjni/NotificationsInboundKeysProvider.java
+++ native/android/app/src/main/java/app/comm/android/fbjni/NotificationsInboundKeysProvider.java
@@ -1,5 +1,6 @@
 package app.comm.android.fbjni;
 
+import android.util.Log;
 import app.comm.android.commservices.CommAndroidServicesClient;
 import java.io.IOException;
 import org.json.JSONException;
@@ -12,13 +13,6 @@ public class NotificationsInboundKeysProvider {
         CommAndroidServicesClient.getInstance()
             .getNotifsInboundKeysForDeviceSync(deviceID);
     String curve25519 = notifInboundKeys.getString("curve25519");
-    // There are several reason to return JSON with curve25519 only:
-    //    1. We only need curve25519 to create inbound session.
-    //    2. In Session.cpp there is a convention to pass curve25519
-    //       key as JSON and then add offset length to advance
-    //       the string pointer.
-    //    3. There is a risk that stringification might not preserve
-    //       the order.
-    return new JSONObject().put("curve25519", curve25519).toString();
+    return curve25519;
   }
 }
