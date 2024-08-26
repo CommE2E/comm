83d450c76b829a4429a4f97b265dc6773900e298
Marcin Wąsowicz <marcinwasowicz@MacBook-Pro-Marcin.local>
2024-08-26 13:55:40 +0200

    Return single curve25519 string from NotificationsInboundKeysProvider
    Summary: This differential returns only curve25519 key string from native code and creates JSON in C++ with folly instead of creating JSON in native code. The rieason for this change is that we saw issues with the content of the JSON returned from Java - Android app was unable to create session as inbound since sender identity key was corrupted. This change fixed the issue.
    
    Test Plan:
    1. Before this diff creating notifs session with Android device as inboud would fail each (> 90%) of the times.
    2. After this diff it always works.
    
    Reviewers: kamil
    
    Subscribers:

--- native/ios/Comm/NotificationsInboundKeysProvider.mm
+++ native/ios/Comm/NotificationsInboundKeysProvider.mm
@@ -19,13 +19,6 @@ std::string NotificationsInboundKeysProvider::getNotifsInboundKeysForDeviceID(
 
   std::string curve25519 =
       std::string([notifInboundKeys[@"curve25519"] UTF8String]);
-  // There are several reason to return JSON with curve25519 only:
-  //    1. We only need curve25519 to create inbound session.
-  //    2. In Session.cpp there is a convention to pass curve25519
-  //       key as JSON and then add offset length to advance
-  //       the string pointer.
-  //    3. There is a risk that stringification might not preserve
-  //       the order.
-  return folly::toJson(folly::dynamic::object("curve25519", curve25519));
+  return curve25519;
 }
 } // namespace comm
