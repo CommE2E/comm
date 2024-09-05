ed95ad5e51522dd904413c91216e194a68e14ebe
Marcin Wąsowicz <marcinkwasowicz@gmail.com>
2024-09-05 15:08:03 +0200

    Implement DMActivityHandler
    Summary: This differential implements DMActivityHandler component. Its role is the same as ActivityHandler but it works thick threads. I opted for new compoment instead of modifying ActivityHandler since we only need one component for thick threads while there are many ActivityHandlers (one for each keyserver). Therefore I couldn't see an easy way to reuse ActivityHandler.
    
    Test Plan:
    1. Create thick thread between users A and B.
    2. Log in as user A on two devices (on web and on native) and log in as user B on third device.
    3. Send message from B to A. Open the thread as A on web. Ensure rescinding happens on native.
    4. Open the thread as A on native. Change opened thread as A on web. Send messages from B to A. Ensure that thread remains read on web.
    
    Reviewers: tomek, kamil, inka
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D13246

--- web/app.react.js
+++ web/app.react.js
@@ -55,6 +55,7 @@ import { EditModalProvider } from './chat/edit-message-provider.js';
 import { MemberListSidebarProvider } from './chat/member-list-sidebar/member-list-sidebar-provider.react.js';
 import { AutoJoinCommunityHandler } from './components/auto-join-community-handler.react.js';
 import CommunitiesRefresher from './components/communities-refresher.react.js';
+import DMActivityHandler from './components/dm-activity-handler.react.js';
 import LogOutIfMissingCSATHandler from './components/log-out-if-missing-csat-handler.react.js';
 import NavigationArrows from './components/navigation-arrows.react.js';
 import MinVersionHandler from './components/version-handler.react.js';
@@ -256,6 +257,7 @@ class App extends React.PureComponent<Props> {
                       <TunnelbrokerDeviceTokenHandler />
                       <FarcasterDataHandler />
                       <AutoJoinCommunityHandler />
+                      <DMActivityHandler />
                       {content}
                     </ChatMentionContextProvider>
                   </MessageSearchStateProvider>
