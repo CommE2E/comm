74252216be5635185d27882058a8c2a55dac577f
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2024-09-06 16:05:05 +0200

    [web][native] Enable blob uploads for thick threads in input-state-container
    Summary:
    Part of [[ https://linear.app/comm/issue/ENG-8708/make-sure-we-dont-send-multimedia-metadata-for-e2ee-dms | ENG-8708 ]].
    
    Updated media upload logic in both input-state-containers to:
    - Always use Blob service for thick threads (regardless of the hardcoded flag)
    - Don't upload metadata to keyserver in thick threads.
    
    Depends on D13280
    
    Test Plan: TBD
    
    Reviewers: kamil
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D13281

--- web/input/input-state-container.react.js
+++ web/input/input-state-container.react.js
@@ -441,7 +441,10 @@ class InputStateContainer extends React.PureComponent<Props, State> {
   }
 
   shouldEncryptMedia(threadInfo: ThreadInfo): boolean {
-    return threadInfoInsideCommunity(threadInfo, commStaffCommunity.id);
+    return (
+      threadTypeIsThick(threadInfo.type) ||
+      threadInfoInsideCommunity(threadInfo, commStaffCommunity.id)
+    );
   }
 
   async sendMultimediaMessage(
@@ -757,7 +760,7 @@ class InputStateContainer extends React.PureComponent<Props, State> {
           },
         };
       },
-      () => this.uploadFiles(threadInfo.id, newUploads),
+      () => this.uploadFiles(threadInfo, newUploads),
     );
     return true;
   }
@@ -867,16 +870,17 @@ class InputStateContainer extends React.PureComponent<Props, State> {
   }
 
   uploadFiles(
-    threadID: string,
+    threadInfo: ThreadInfo,
     uploads: $ReadOnlyArray<PendingMultimediaUpload>,
   ): Promise<mixed> {
     return Promise.all(
-      uploads.map(upload => this.uploadFile(threadID, upload)),
+      uploads.map(upload => this.uploadFile(threadInfo, upload)),
     );
   }
 
-  async uploadFile(threadID: string, upload: PendingMultimediaUpload) {
+  async uploadFile(threadInfo: ThreadInfo, upload: PendingMultimediaUpload) {
     const { selectTime, localID, encryptionKey } = upload;
+    const threadID = threadInfo.id;
     const isEncrypted =
       !!encryptionKey &&
       (upload.mediaType === 'encrypted_photo' ||
@@ -918,8 +922,10 @@ class InputStateContainer extends React.PureComponent<Props, State> {
         abortHandler: (abort: () => void) =>
           this.handleAbortCallback(threadID, localID, abort),
       };
+      const isThickThread = threadTypeIsThick(threadInfo.type);
+      const useBlobService = isThickThread || this.useBlobServiceUploads;
       if (
-        this.useBlobServiceUploads &&
+        useBlobService &&
         (upload.mediaType === 'encrypted_photo' ||
           upload.mediaType === 'encrypted_video')
       ) {
@@ -941,7 +947,7 @@ class InputStateContainer extends React.PureComponent<Props, State> {
             loop: false,
             thumbHash,
           },
-          keyserverOrThreadID: threadID,
+          keyserverOrThreadID: isThickThread ? null : threadID,
           callbacks,
         });
       } else {
@@ -1645,7 +1651,7 @@ class InputStateContainer extends React.PureComponent<Props, State> {
       };
     });
 
-    void this.uploadFiles(threadInfo.id, uploadsToRetry);
+    void this.uploadFiles(threadInfo, uploadsToRetry);
   }
 
   addReply = (message: string) => {
