8715a71d0f2b6ba867a83dd4422563d8050b1658
Inka Sokołowska <inka.d.sokolowska@gmail.com>
2024-09-06 12:35:20 +0200

    [lib][web][native] Update createPendingThread usages
    Summary:
    issue: [[https://linear.app/comm/issue/ENG-8974/update-all-the-places-where-creatependingthread-is-used | ENG-8974]]
    I went through all places where `createPendingThread` is used and checked what needs to be done for them. Some places still need to be updated, but they are related to `useExistingThreadInfoFinder`, which is a different task.
    
    Test Plan:
    1. Tested that `ComposeThreadButton` creates a thick thread now. It's the button used on iOS for creating a new chat from the chat tab
    2. `composeThread` in `ChatThreadList` is the same as `ComposeThreadButton`, but for Android instead of iOS. Assuming it works since iOS works
    3. Checked that `Top/bar` which uses `useThreadInfoForPossiblyPendingThread` correctly shows the name of the user the ucrrent user is not friends with. Checked that it correctly shows the name of a chat the user is a member of
    4. `createPendingPersonalThread` is used in
      - `createPendingThreadItem` used to create an item in the thread list, but only for personal chats with users the current user has not yet messaged ever. Checked that their names are shown correctly in those items.
      - `useUserProfileThreadInfo` used to display profile info of users the user doesn't yet know. Thested that their name and friendship buttons are shown correctly
    
    Reviewers: tomek, kamil
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D13253

--- native/chat/chat-thread-list.react.js
+++ native/chat/chat-thread-list.react.js
@@ -137,7 +137,7 @@ function ChatThreadList(props: BaseProps): React.Node {
     }
     const threadInfo = createPendingThread({
       viewerID: loggedInUserInfo.id,
-      threadType: threadTypes.GENESIS_PRIVATE,
+      threadType: threadTypes.PRIVATE,
       members: [loggedInUserInfo],
     });
     navigateToThread({ threadInfo, searching: true });
