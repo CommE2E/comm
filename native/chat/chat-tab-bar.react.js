CommE2E/comm

commit 3a5db7a16094f8b6d472ed7dbcd43170e5a69805

[native] Add renderTabBarItem to MaterialTopTabBarProps
Summary: issue: [[https://linear.app/comm/issue/ENG-9041/update-tabs-style | ENG-9041]]

Test Plan: flow

Reviewers: tomek, will

Subscribers: ashoat

Differential Revision: https://phab.comm.dev/D13175

native/flow-typed/npm/@react-navigation/core_v6.x.x.js
@@ -1978,11 +1978,66 @@ declare module '@react-navigation/core' {
     +descriptors: {| +[key: string]: MaterialTopTabDescriptor |},
   |};
 
-  declare export type MaterialTopTabBarProps = {|
+  declare export type TabDescriptor<T: Route<>> = {|
+    +accessibilityLabel?: string;
+    +accessible?: boolean;
+    +testID?: string;
+    +labelText?: string;
+    +labelAllowFontScaling?: boolean;
+    +href?: string;
+    +label?: (props: {
+      +route: T;
+      +labelText?: string;
+      +focused: boolean;
+      +color: string;
+      +allowFontScaling?: boolean;
+      +style?: TextStyleProp;
+    }) => React$Node;
+    +icon?: (props: {
+      +route: T;
+      +focused: boolean;
+      +color: string;
+      +size: number;
+    }) => React$Node;
+    +badge?: (props: { route: T }) => React$Node;
+  |};
+
+  declare export type OpaqueColorValue = Symbol & {|+__TYPE__: 'Color'|};
+  declare export type ColorValue = string | OpaqueColorValue;
+
+  declare export interface PressableAndroidRippleConfig {
+    +color?: ?ColorValue;
+    +borderless?: ?boolean;
+    +radius?: ?number;
+    +foreground?: ?boolean;
+  }
+  
+
+  declare export type TabBarItemProps<T: Route<>> = TabDescriptor<T> & {
+    +position: AnimatedInterpolation;
+    +route: T;
+    +navigationState: NavigationState;
+    +activeColor?: string;
+    +inactiveColor?: string;
+    +pressColor?: string;
+    +pressOpacity?: number;
+    +onLayout?: (event: LayoutEvent) => void;
+    +onPress: () => void;
+    +onLongPress: () => void;
+    +defaultTabWidth?: number;
+    +labelStyle?: TextStyleProp;
+    +style: ViewStyleProp;
+    +android_ripple?: PressableAndroidRippleConfig;
+  };
+
+  declare export type MaterialTopTabBarProps<T: Route<>> = {|
     ...MaterialTopTabNavigationBuilderResult,
     +layout: {| +width: number, +height: number |},
     +position: any, // Reanimated.Node<number>
     +jumpTo: string => void,
+    +renderTabBarItem:  (
+      props: TabBarItemProps<T> & { key: string }
+    ) => React$Node,
   |};
 
   declare export type MaterialTopTabNavigationConfig = {|
@@ -1996,7 +2051,7 @@ declare module '@react-navigation/core' {
     +style?: ViewStyleProp,
     +gestureHandlerProps?: PanGestureHandlerProps,
     +pager?: MaterialTopTabPagerProps => React$Node,
-    +tabBar?: MaterialTopTabBarProps => React$Node,
+    +tabBar?: MaterialTopTabBarProps<Route <>> => React$Node,
   |};
 
   declare export type ExtraMaterialTopTabNavigatorProps = {|

native/flow-typed/npm/@react-navigation/material-top-tabs_v6.x.x.js
@@ -12,6 +12,7 @@ declare module '@react-navigation/material-top-tabs' {
     MaterialTopTabNavigationConfig,
     MaterialTopTabNavigationBuilderResult,
     MaterialTopTabBarProps,
+    Route,
   } from '@react-navigation/core';
 
   /**
@@ -42,7 +43,7 @@ declare module '@react-navigation/material-top-tabs' {
    */
 
   declare export var MaterialTopTabBar: React$ComponentType<
-    MaterialTopTabBarProps,
+    MaterialTopTabBarProps<Route<>>,
   >;
 
 }

commit b34e714a4a2fc3a76866ffeaa5fdb83817f3309d

[native] Fix up issues from D13175
Summary:
1. Prettier does not run on `flow-typed` to avoid applying changes to libdefs that come from upstream. As such, it's important that we manually fix up whitespace issues.
2. In libdefs, we should always be specific between `{| |}` and `{ ... }`, and never just use `{}`
3. We should prefer `type` to `interface` whenver possible to avoid complex inheritance-based typing
4. We should maintain consistent style across the file. This diff replaces the inconsistent `;` that was introduced in D13175 with `,` that matches the rest of the file
5. We should always makes types read-only unless they need to be written to

Test Plan: Flow

Reviewers: inka!, tomek!

Subscribers:

native/chat/chat-tab-bar.react.js
@@ -63,9 +63,9 @@ export default function TabBarTop(
   props: MaterialTopTabBarProps<Route<>>,
 ): React.Node {
   const renderTabBarItem = React.useCallback(
-    (innerProps: $ReadOnly<{ ...TabBarItemProps<Route<>>, key: string }>) => (
-      <TabBarButton {...innerProps} />
-    ),
+    (
+      innerProps: $ReadOnly<{ ...TabBarItemProps<Route<>>, +key: string, ... }>,
+    ) => <TabBarButton {...innerProps} />,
     [],
   );
 

native/flow-typed/npm/@react-navigation/core_v6.x.x.js
@@ -1979,56 +1979,59 @@ declare module '@react-navigation/core' {
   |};
 
   declare export type TabDescriptor<T: Route<>> = {|
-    +accessibilityLabel?: string;
-    +accessible?: boolean;
-    +testID?: string;
-    +labelText?: string;
-    +labelAllowFontScaling?: boolean;
-    +href?: string;
+    +accessibilityLabel?: string,
+    +accessible?: boolean,
+    +testID?: string,
+    +labelText?: string,
+    +labelAllowFontScaling?: boolean,
+    +href?: string,
     +label?: (props: {
-      +route: T;
-      +labelText?: string;
-      +focused: boolean;
-      +color: string;
-      +allowFontScaling?: boolean;
-      +style?: TextStyleProp;
-    }) => React$Node;
+      +route: T,
+      +labelText?: string,
+      +focused: boolean,
+      +color: string,
+      +allowFontScaling?: boolean,
+      +style?: TextStyleProp,
+      ...
+    }) => React$Node,
     +icon?: (props: {
       +route: T;
-      +focused: boolean;
-      +color: string;
-      +size: number;
-    }) => React$Node;
-    +badge?: (props: { route: T }) => React$Node;
+      +focused: boolean,
+      +color: string,
+      +size: number,
+      ...
+    }) => React$Node,
+    +badge?: (props: { +route: T, ... }) => React$Node,
   |};
 
-  declare export type OpaqueColorValue = Symbol & {|+__TYPE__: 'Color'|};
+  declare export type OpaqueColorValue = Symbol & {| +__TYPE__: 'Color' |};
   declare export type ColorValue = string | OpaqueColorValue;
 
-  declare export interface PressableAndroidRippleConfig {
-    +color?: ?ColorValue;
-    +borderless?: ?boolean;
-    +radius?: ?number;
-    +foreground?: ?boolean;
-  }
-  
+  declare export type PressableAndroidRippleConfig = {
+    +color?: ?ColorValue,
+    +borderless?: ?boolean,
+    +radius?: ?number,
+    +foreground?: ?boolean,
+    ...
+  };
 
   declare export type TabBarItemProps<T: Route<>> = $ReadOnly<{
     ...TabDescriptor<T>, 
-    +position: AnimatedInterpolation;
-    +route: T;
-    +navigationState: NavigationState;
-    +activeColor?: string;
-    +inactiveColor?: string;
-    +pressColor?: string;
-    +pressOpacity?: number;
-    +onLayout?: (event: LayoutEvent) => void;
-    +onPress: () => void;
-    +onLongPress: () => void;
-    +defaultTabWidth?: number;
-    +labelStyle?: TextStyleProp;
-    +style: ViewStyleProp;
-    +android_ripple?: PressableAndroidRippleConfig;
+    +position: AnimatedInterpolation,
+    +route: T,
+    +navigationState: NavigationState,
+    +activeColor?: string,
+    +inactiveColor?: string,
+    +pressColor?: string,
+    +pressOpacity?: number,
+    +onLayout?: (event: LayoutEvent) => void,
+    +onPress: () => void,
+    +onLongPress: () => void,
+    +defaultTabWidth?: number,
+    +labelStyle?: TextStyleProp,
+    +style: ViewStyleProp,
+    +android_ripple?: PressableAndroidRippleConfig,
+    ...
   }>;
 
   declare export type MaterialTopTabBarProps<T: Route<>> = {|
@@ -2037,7 +2040,7 @@ declare module '@react-navigation/core' {
     +position: any, // Reanimated.Node<number>
     +jumpTo: string => void,
     +renderTabBarItem:  (
-      props: $ReadOnly<{...TabBarItemProps<T>, key: string }>,
+      props: $ReadOnly<{ ...TabBarItemProps<T>, +key: string, ... }>,
     ) => React$Node,
   |};
 
@@ -2052,7 +2055,7 @@ declare module '@react-navigation/core' {
     +style?: ViewStyleProp,
     +gestureHandlerProps?: PanGestureHandlerProps,
     +pager?: MaterialTopTabPagerProps => React$Node,
-    +tabBar?: MaterialTopTabBarProps<Route <>> => React$Node,
+    +tabBar?: MaterialTopTabBarProps<Route<>> => React$Node,
   |};
 
   declare export type ExtraMaterialTopTabNavigatorProps = {|