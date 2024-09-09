b6ed9687c08b61fdd8e3f20f6146cb03a5c68940
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2024-09-09 22:07:02 +0200

    [terraform] bump prod identity to 0.35
    Summary:
    we probably can't deploy this until monday at this point, but preparing the diff now so that we're not blocked on getting it approved later.
    
    confirmed that @ashoat tagged and pushed 0.35 to dockerhub after the apparent issues pushing 0.35-staging to dockerhub yesterday.
    
    won't land until we're ready to deploy
    
    Test Plan: terraform plan
    
    Reviewers: will, ashoat, bartek
    
    Reviewed By: will
    
    Subscribers: tomek, ashoat
    
    Differential Revision: https://phab.comm.dev/D13252

--- services/terraform/remote/service_identity.tf
+++ services/terraform/remote/service_identity.tf
@@ -1,5 +1,5 @@
 locals {
-  identity_service_image_tag      = local.is_staging ? "0.35-staging" : "0.34"
+  identity_service_image_tag      = local.is_staging ? "0.35-staging" : "0.35"
   identity_service_server_image   = "commapp/identity-server:${local.identity_service_image_tag}"
   identity_service_container_name = "identity-server"
 
