c7d91b798f22e7574ea1205ff37f9759254275a5
xsanm <kkurowski.kk@gmail.com>
2024-09-13 10:39:04 +0200

    [web][native][lib] deprecate `sendDMActionTypes` and simplify hooks
    Summary:
    [ENG-8424](https://linear.app/comm/issue/ENG-8424/update-inputstatecontainer-to-support-multimedia-messages-being-sent).
    
    This removes code, now we use dedicated actions for composable messages. Some more context about why this is better in D13290.
    
    Depends on D13329
    
    Test Plan: Flow
    
    Reviewers: tomek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D13330

--- web/chat/failed-send.react.js
+++ web/chat/failed-send.react.js
@@ -5,16 +5,13 @@ import * as React from 'react';
 
 import { type ChatMessageInfoItem } from 'lib/selectors/chat-selectors.js';
 import { threadInfoSelector } from 'lib/selectors/thread-selectors.js';
-import { useRetrySendDMOperation } from 'lib/shared/dm-ops/process-dm-ops.js';
 import { messageID } from 'lib/shared/message-utils.js';
 import { messageTypes } from 'lib/types/message-types-enum.js';
 import {
   assertComposableMessageType,
-  type LocalMessageInfo,
   type RawComposableMessageInfo,
 } from 'lib/types/message-types.js';
 import type { ThreadInfo } from 'lib/types/minimally-encoded-thread-permissions-types.js';
-import { threadTypeIsThick } from 'lib/types/thread-types-enum.js';
 
 import css from './chat-message-list.css';
 import multimediaMessageSendFailed from './multimedia-message-send-failed.js';
@@ -32,10 +29,6 @@ type Props = {
   +rawMessageInfo: RawComposableMessageInfo,
   +inputState: ?InputState,
   +parentThreadInfo: ?ThreadInfo,
-  +retrySendDMOperation: (
-    messageID: string,
-    localMessageInfo: LocalMessageInfo,
-  ) => Promise<void>,
 };
 class FailedSend extends React.PureComponent<Props> {
   retryingText = false;
@@ -113,17 +106,6 @@ class FailedSend extends React.PureComponent<Props> {
         return;
       }
       this.retryingText = true;
-      if (threadTypeIsThick(this.props.threadInfo.type)) {
-        const failedMessageID = this.props.rawMessageInfo.id;
-        invariant(failedMessageID, 'failedMessageID should be set for DMs');
-        const localMessageInfo = this.props.item.localMessageInfo;
-        invariant(
-          localMessageInfo,
-          'localMessageInfo should be set for failed message',
-        );
-        void this.props.retrySendDMOperation(failedMessageID, localMessageInfo);
-        return;
-      }
       void inputState.sendTextMessage(
         {
           ...rawMessageInfo,
@@ -167,7 +149,6 @@ const ConnectedFailedSend: React.ComponentType<BaseProps> =
     const parentThreadInfo = useSelector(state =>
       parentThreadID ? threadInfoSelector(state)[parentThreadID] : null,
     );
-    const retrySendDMOperation = useRetrySendDMOperation();
 
     return (
       <FailedSend
@@ -175,7 +156,6 @@ const ConnectedFailedSend: React.ComponentType<BaseProps> =
         rawMessageInfo={rawMessageInfo}
         inputState={inputState}
         parentThreadInfo={parentThreadInfo}
-        retrySendDMOperation={retrySendDMOperation}
       />
     );
   });
