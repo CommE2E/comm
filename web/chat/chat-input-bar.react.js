c5f34caf9676f5e9a2de863a4ce42926e41760a6
Tomasz Palys <tomasz.palys@swmansion.com>
2024-09-13 22:37:22 +0200

    [web] Fix broken pending threads
    Summary:
    In D13282 we modified the logic so that instead of an active thread ID we use active thread info that is taken from the thread store. Unfortunately, this change broke the pending threads which aren't present in the store. What we should do instead is to let the component provide the thread store for us.
    
    https://linear.app/comm/issue/ENG-9268/pending-threads-are-broken-on-master-on-the-web
    
    Test Plan: Check if it is possible to create a new thread by sending a text message in a pending thread.
    
    Reviewers: kamil, bartek, ashoat
    
    Reviewed By: bartek, ashoat
    
    Differential Revision: https://phab.comm.dev/D13338

--- web/chat/chat-input-bar.react.js
+++ web/chat/chat-input-bar.react.js
@@ -85,6 +85,7 @@ type Props = {
   +currentUserIsVoiced: boolean,
   +currentUserCanJoinThread: boolean,
   +threadFrozen: boolean,
+  +cancelPendingUpload: (uploadID: string) => void,
 };
 
 class ChatInputBar extends React.PureComponent<Props> {
@@ -240,7 +241,7 @@ class ChatInputBar extends React.PureComponent<Props> {
       );
     }
 
-    const { pendingUploads, cancelPendingUpload } = this.props.inputState;
+    const { pendingUploads } = this.props.inputState;
     const multimediaPreviews = pendingUploads.map(pendingUpload => {
       const { uri, mediaType, thumbHash, dimensions } = pendingUpload;
       let mediaSource = { thumbHash, dimensions };
@@ -270,7 +271,7 @@ class ChatInputBar extends React.PureComponent<Props> {
         <Multimedia
           mediaSource={mediaSource}
           pendingUpload={pendingUpload}
-          remove={cancelPendingUpload}
+          remove={this.props.cancelPendingUpload}
           multimediaCSSClass={css.multimedia}
           multimediaImageCSSClass={css.multimediaImage}
           key={pendingUpload.localID}
@@ -684,6 +685,14 @@ const ConnectedChatInputBar: React.ComponentType<BaseProps> =
         [suggestedUsers, suggestedChats],
       );
 
+    const baseCancelPendingUpload = props.inputState.cancelPendingUpload;
+    const cancelPendingUpload = React.useCallback(
+      (uploadID: string) => {
+        baseCancelPendingUpload(props.threadInfo, uploadID);
+      },
+      [baseCancelPendingUpload, props.threadInfo],
+    );
+
     return (
       <ChatInputBar
         {...props}
@@ -701,6 +710,7 @@ const ConnectedChatInputBar: React.ComponentType<BaseProps> =
         currentUserIsVoiced={currentUserIsVoiced}
         currentUserCanJoinThread={currentUserCanJoinThread}
         threadFrozen={threadFrozen}
+        cancelPendingUpload={cancelPendingUpload}
       />
     );
   });
