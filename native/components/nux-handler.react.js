e5e9e22e1e84e83d52aef0bdd1914a0c819f3c00
Tomasz Palys <tomasz.palys@swmansion.com>
2024-09-13 17:08:35 +0200

    [lib] Enable NUX for staff
    Summary:
    Turn the flag on for staff and dev.
    
    https://linear.app/comm/issue/ENG-9111/remove-the-flag-once-we-are-ready-to-launch-nux
    
    Test Plan: Open the app and check if NUX is performed.
    
    Reviewers: kamil, inka
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D13337

--- native/components/nux-handler.react.js
+++ native/components/nux-handler.react.js
@@ -9,13 +9,13 @@ import { isLoggedIn } from 'lib/selectors/user-selectors.js';
 import { NUXTipsContext } from './nux-tips-context.react.js';
 import { useSelector } from '../redux/redux-utils.js';
 import { useOnFirstLaunchEffect } from '../utils/hooks.js';
-
-const showTipsFlag = false;
+import { useStaffCanSee } from '../utils/staff-utils.js';
 
 function NUXHandler(): React.Node {
   const nuxTipsContext = React.useContext(NUXTipsContext);
   invariant(nuxTipsContext, 'nuxTipsContext should be defined');
   const { tipsProps } = nuxTipsContext;
+  const showTipsFlag = useStaffCanSee();
 
   const loggedIn = useSelector(isLoggedIn);
 
