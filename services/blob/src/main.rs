9988340db0b35cba19c5a7bd2dadb9e22998835b
William Wang <williamwang749@gmail.com>
2024-09-18 13:17:07 -0400

    [blob] use json logs with tracing for blob service
    Summary: Similar to what we do on the identity service, this enables json logs with the blob service
    
    Test Plan: applied to staging and confirmed json logs in cloudwatch
    
    Reviewers: bartek, varun
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D13393

--- services/blob/src/main.rs
+++ services/blob/src/main.rs
@@ -9,18 +9,34 @@ pub mod tools;
 use anyhow::Result;
 use comm_lib::auth::AuthService;
 use config::Command;
+use constants::COMM_SERVICES_USE_JSON_LOGS;
+use std::env;
 use tracing_subscriber::filter::{EnvFilter, LevelFilter};
 
 use crate::service::BlobServiceConfig;
 
 fn configure_logging() -> Result<()> {
+  let use_json_logs: bool = env::var(COMM_SERVICES_USE_JSON_LOGS)
+    .unwrap_or("false".to_string())
+    .parse()
+    .unwrap_or_default();
+
   let filter = EnvFilter::builder()
     .with_default_directive(LevelFilter::INFO.into())
     .with_env_var(constants::LOG_LEVEL_ENV_VAR)
     .from_env_lossy();
 
-  let subscriber = tracing_subscriber::fmt().with_env_filter(filter).finish();
-  tracing::subscriber::set_global_default(subscriber)?;
+  if use_json_logs {
+    let subscriber = tracing_subscriber::fmt()
+      .json()
+      .with_env_filter(filter)
+      .finish();
+    tracing::subscriber::set_global_default(subscriber)?;
+  } else {
+    let subscriber = tracing_subscriber::fmt().with_env_filter(filter).finish();
+    tracing::subscriber::set_global_default(subscriber)?;
+  }
+
   Ok(())
 }
 
