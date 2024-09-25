d10b1a64cef98693d3d9af27485592ba8bad6679
Marcin Wąsowicz <marcinkwasowicz@gmail.com>
2024-09-25 18:59:41 +0200

    Add AES cryptography to EncryptedNotifUtilsAPI Differential Revision: https://phab.comm.dev/D13468

--- keyserver/src/push/encrypted-notif-utils-api.js
+++ keyserver/src/push/encrypted-notif-utils-api.js
@@ -6,6 +6,7 @@ import type { EncryptedNotifUtilsAPI } from 'lib/types/notif-types.js';
 
 import { blobServiceUpload } from './utils.js';
 import { encryptAndUpdateOlmSession } from '../updaters/olm-session-updater.js';
+import { encrypt, generateKey } from '../utils/aes-crypto-utils.js';
 import { getOlmUtility } from '../utils/olm-utils.js';
 
 const encryptedNotifUtilsAPI: EncryptedNotifUtilsAPI = {
@@ -54,6 +55,20 @@ const encryptedNotifUtilsAPI: EncryptedNotifUtilsAPI = {
     Buffer.byteLength(serializedPayload),
   getEncryptedNotifHash: async (serializedNotification: string) =>
     getOlmUtility().sha256(serializedNotification),
+  getBlobHash: async (blob: Uint8Array) => {
+    return getOlmUtility().sha256(new Uint8Array(blob.buffer));
+  },
+  generateAESKey: async () => {
+    const aesKeyBytes = await generateKey();
+    return Buffer.from(aesKeyBytes).toString('base64');
+  },
+  encryptWithAESKey: async (encryptionKey: string, unencryptedData: string) => {
+    const encryptionKeyBytes = new Uint8Array(
+      Buffer.from(encryptionKey, 'base64'),
+    );
+    const unencryptedDataBytes = new TextEncoder().encode(unencryptedData);
+    return await encrypt(encryptionKeyBytes, unencryptedDataBytes);
+  },
 };
 
 export default encryptedNotifUtilsAPI;
