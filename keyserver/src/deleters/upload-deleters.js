8e09d6fbd23c3b90993d9f526c6743bc3cbbed6f
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2024-09-30 11:04:06 +0200

    [keyserver] Remove holders in upload deleters
    Summary:
    Address [[ https://linear.app/comm/issue/ENG-9352/delete-holder-when-deleting-upload | ENG-9352 ]].
    Before calling `DELETE FROM uploads`, we need to select the `extra` column first and remove blob holders if present in the column JSON.
    
    Depends on D13511
    
    Test Plan: TBD
    
    Reviewers: tomek, ashoat
    
    Differential Revision: https://phab.comm.dev/D13512

--- keyserver/src/deleters/upload-deleters.js
+++ keyserver/src/deleters/upload-deleters.js
@@ -3,7 +3,9 @@
 import { ServerError } from 'lib/utils/errors.js';
 
 import { dbQuery, SQL } from '../database/database.js';
+import { deleteBlob, removeBlobHolders } from '../services/blob.js';
 import type { Viewer } from '../session/viewer.js';
+import { blobHoldersFromUploadRows } from '../uploads/media-utils.js';
 
 async function deleteUpload(viewer: Viewer, id: string): Promise<void> {
   if (!viewer.loggedIn) {
@@ -11,7 +13,7 @@ async function deleteUpload(viewer: Viewer, id: string): Promise<void> {
   }
 
   const fetchQuery = SQL`
-    SELECT uploader, container, user_container AS userContainer
+    SELECT uploader, container, user_container AS userContainer, extra
     FROM uploads
     WHERE id = ${id}
   `;
@@ -21,7 +23,7 @@ async function deleteUpload(viewer: Viewer, id: string): Promise<void> {
     throw new ServerError('invalid_parameters');
   }
   const [row] = result;
-  const { uploader, container, userContainer } = row;
+  const { uploader, container, userContainer, extra } = row;
 
   if (
     uploader.toString() !== viewer.userID ||
@@ -31,26 +33,47 @@ async function deleteUpload(viewer: Viewer, id: string): Promise<void> {
     throw new ServerError('invalid_parameters');
   }
 
+  const { blobHash, blobHolder } = JSON.parse(extra);
+  let holderRemovalPromise;
+  if (blobHash && blobHolder) {
+    holderRemovalPromise = deleteBlob({
+      hash: blobHash,
+      holder: blobHolder,
+    });
+  }
+
   const deleteQuery = SQL`
     DELETE u, i
     FROM uploads u
     LEFT JOIN ids i ON i.id = u.id
     WHERE u.id = ${id}
   `;
-  await dbQuery(deleteQuery);
+  await Promise.all([dbQuery(deleteQuery), holderRemovalPromise]);
 }
 
 const maxUnassignedUploadAge = 24 * 60 * 60 * 1000;
 async function deleteUnassignedUploads(): Promise<void> {
   const oldestUnassignedUploadToKeep = Date.now() - maxUnassignedUploadAge;
-  await dbQuery(SQL`
+
+  const holdersQuery = SQL`
+    SELECT extra
+    FROM uploads
+    WHERE container IS NULL
+      AND user_container IS NULL
+      AND creation_time < ${oldestUnassignedUploadToKeep}
+  `;
+  const [rows] = await dbQuery(holdersQuery);
+  const blobHolders = blobHoldersFromUploadRows(rows);
+
+  const deletionQuery = SQL`
     DELETE u, i
     FROM uploads u
     LEFT JOIN ids i ON i.id = u.id
     WHERE u.container IS NULL
       AND u.user_container IS NULL
       AND creation_time < ${oldestUnassignedUploadToKeep}
-  `);
+  `;
+  await Promise.all([dbQuery(deletionQuery), removeBlobHolders(blobHolders)]);
 }
 
 export { deleteUpload, deleteUnassignedUploads };
