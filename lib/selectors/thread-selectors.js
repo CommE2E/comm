CommE2E/comm

commit bb020e4f67b1e4b7ab7953ac29a3d69332bea2a7

[lib] Create a thin thread when some users don't support thick threads
Summary:
Check if all the users support thick threads and chose thread type accordingly.

https://linear.app/comm/issue/ENG-9257/limit-chat-candidates-to-users-who-are-registered-with-identity

Test Plan: On both native and web checked if the correct thread type is created, when sending text message, depending on the presence of members in `AuxUserStore`

Reviewers: kamil, will

Subscribers: ashoat

Differential Revision: https://phab.comm.dev/D13404

lib/selectors/thread-selectors.js
@@ -33,6 +33,7 @@ import {
   roleIsAdminRole,
   threadIsPending,
   getPendingThreadID,
+  pendingThreadType,
 } from '../shared/thread-utils.js';
 import type { ClientAvatar, ClientEmojiAvatar } from '../types/avatar-types';
 import type { EntryInfo } from '../types/entry-types.js';
@@ -461,17 +462,24 @@ const pendingToRealizedThreadIDsSelector: (
       const actualMemberIDs = rawThreadInfo.members
         .filter(member => member.role)
         .map(member => member.id);
-      const pendingThreadID = getPendingThreadID(
-        rawThreadInfo.type,
-        actualMemberIDs,
-        rawThreadInfo.sourceMessageID,
-      );
-      const existingResult = result.get(pendingThreadID);
-      if (
-        !existingResult ||
-        rawThreadInfos[existingResult].creationTime > rawThreadInfo.creationTime
-      ) {
-        result.set(pendingThreadID, threadID);
+      const possiblePendingThreadTypes = [
+        pendingThreadType(actualMemberIDs.length - 1, 'thin', true),
+        pendingThreadType(actualMemberIDs.length - 1, 'thick', true),
+      ];
+      for (const type of possiblePendingThreadTypes) {
+        const pendingThreadID = getPendingThreadID(
+          type,
+          actualMemberIDs,
+          rawThreadInfo.sourceMessageID,
+        );
+        const existingResult = result.get(pendingThreadID);
+        if (
+          !existingResult ||
+          rawThreadInfos[existingResult].creationTime >
+            rawThreadInfo.creationTime
+        ) {
+          result.set(pendingThreadID, threadID);
+        }
       }
     }
     return result;

lib/shared/thread-actions-utils.js
@@ -12,6 +12,7 @@ import {
   removeUsersFromThreadActionTypes,
   type RemoveUsersFromThreadInput,
 } from '../actions/thread-actions.js';
+import type { AuxUserInfos } from '../types/aux-user-types.js';
 import type { CalendarQuery } from '../types/entry-types.js';
 import type {
   RelativeMemberInfo,
@@ -23,6 +24,7 @@ import {
   assertThickThreadType,
   threadTypeIsThick,
 } from '../types/thread-types-enum.js';
+import type { ThreadType } from '../types/thread-types-enum.js';
 import type {
   ChangeThreadSettingsPayload,
   ClientNewThinThreadRequest,
@@ -60,6 +62,7 @@ type CreateRealThreadParameters = {
   +handleError?: () => mixed,
   +calendarQuery: CalendarQuery,
   +usingOlmViaTunnelbrokerForDMs: boolean,
+  +auxUserInfos: AuxUserInfos,
 };
 
 async function createRealThreadFromPendingThread({
@@ -71,12 +74,20 @@ async function createRealThreadFromPendingThread({
   viewerID,
   calendarQuery,
   usingOlmViaTunnelbrokerForDMs,
-}: CreateRealThreadParameters): Promise<string> {
+  auxUserInfos,
+}: CreateRealThreadParameters): Promise<{
+  +threadID: string,
+  +threadType: ThreadType,
+}> {
   if (!threadIsPending(threadInfo.id)) {
-    return threadInfo.id;
+    return {
+      threadID: threadInfo.id,
+      threadType: threadInfo.type,
+    };
   }
 
   let newThreadID;
+  let newThreadType = threadInfo.type;
 
   const otherMemberIDs = threadOtherMembers(threadInfo.members, viewerID).map(
     member => member.id,
@@ -125,7 +136,12 @@ async function createRealThreadFromPendingThread({
       otherMemberIDs.length > 0,
       'otherMemberIDs should not be empty for threads',
     );
-    if (threadTypeIsThick(threadInfo.type)) {
+    const allUsersSupportThickThreads = otherMemberIDs.every(
+      memberID =>
+        auxUserInfos[memberID]?.deviceList &&
+        auxUserInfos[memberID].deviceList.devices.length > 0,
+    );
+    if (threadTypeIsThick(threadInfo.type) && allUsersSupportThickThreads) {
       const type = assertThickThreadType(
         pendingThreadType(
           otherMemberIDs.length,
@@ -144,6 +160,7 @@ async function createRealThreadFromPendingThread({
         initialMemberIDs: otherMemberIDs,
         color: threadInfo.color,
       });
+      newThreadType = type;
     } else {
       const type = assertThinThreadType(
         pendingThreadType(
@@ -166,9 +183,13 @@ async function createRealThreadFromPendingThread({
       void dispatchActionPromise(newThreadActionTypes, resultPromise);
       const result = await resultPromise;
       newThreadID = result.newThreadID;
+      newThreadType = type;
     }
   }
-  return newThreadID;
+  return {
+    threadID: newThreadID,
+    threadType: newThreadType,
+  };
 }
 
 export { removeMemberFromThread, createRealThreadFromPendingThread };

native/input/input-state-container.react.js
@@ -57,6 +57,7 @@ import {
   threadIsPending,
   threadIsPendingSidebar,
 } from 'lib/shared/thread-utils.js';
+import type { AuxUserInfos } from 'lib/types/aux-user-types';
 import type { CalendarQuery } from 'lib/types/entry-types.js';
 import type {
   Media,
@@ -85,6 +86,7 @@ import {
   threadTypeIsThick,
   threadTypeIsSidebar,
 } from 'lib/types/thread-types-enum.js';
+import type { ThreadType } from 'lib/types/thread-types-enum.js';
 import {
   type ClientNewThinThreadRequest,
   type NewThreadResult,
@@ -170,6 +172,7 @@ type Props = {
   +newThickThread: (request: NewThickThreadRequest) => Promise<string>,
   +textMessageCreationSideEffectsFunc: CreationSideEffectsFunc<RawTextMessageInfo>,
   +usingOlmViaTunnelbrokerForDMs: boolean,
+  +auxUserInfos: AuxUserInfos,
 };
 type State = {
   +pendingUploads: PendingMultimediaUploads,
@@ -185,7 +188,13 @@ class InputStateContainer extends React.PureComponent<Props, State> {
     (params: EditInputBarMessageParameters) => void,
   > = [];
   scrollToMessageCallbacks: Array<(messageID: string) => void> = [];
-  pendingThreadCreations: Map<string, Promise<string>> = new Map();
+  pendingThreadCreations: Map<
+    string,
+    Promise<{
+      +threadID: string,
+      +threadType: ThreadType,
+    }>,
+  > = new Map();
   pendingThreadUpdateHandlers: Map<string, (ThreadInfo) => mixed> = new Map();
   // TODO: flip the switch
   // Note that this enables Blob service for encrypted media only
@@ -349,7 +358,8 @@ class InputStateContainer extends React.PureComponent<Props, State> {
         // again.
         throw new Error('Thread creation failed');
       }
-      newThreadID = await threadCreationPromise;
+      const result = await threadCreationPromise;
+      newThreadID = result.threadID;
     } catch (e) {
       const copy = cloneError(e);
       copy.localID = messageInfo.localID;
@@ -505,9 +515,9 @@ class InputStateContainer extends React.PureComponent<Props, State> {
       }
     }
 
-    let newThreadID = null;
+    let threadCreationResult = null;
     try {
-      newThreadID = await this.startThreadCreation(threadInfo);
+      threadCreationResult = await this.startThreadCreation(threadInfo);
     } catch (e) {
       const copy = cloneError(e);
       copy.localID = messageInfo.localID;
@@ -524,13 +534,14 @@ class InputStateContainer extends React.PureComponent<Props, State> {
 
     const newMessageInfo = {
       ...messageInfo,
-      threadID: newThreadID,
+      threadID: threadCreationResult?.threadID,
       time: Date.now(),
     };
 
     const newThreadInfo = {
       ...threadInfo,
-      id: newThreadID,
+      id: threadCreationResult?.threadID,
+      type: threadCreationResult?.threadType ?? threadInfo.type,
     };
 
     void this.props.dispatchActionPromise(
@@ -545,9 +556,14 @@ class InputStateContainer extends React.PureComponent<Props, State> {
     );
   };
 
-  startThreadCreation(threadInfo: ThreadInfo): Promise<string> {
+  startThreadCreation(
+    threadInfo: ThreadInfo,
+  ): Promise<{ +threadID: string, +threadType: ThreadType }> {
     if (!threadIsPending(threadInfo.id)) {
-      return Promise.resolve(threadInfo.id);
+      return Promise.resolve({
+        threadID: threadInfo.id,
+        threadType: threadInfo.type,
+      });
     }
     let threadCreationPromise = this.pendingThreadCreations.get(threadInfo.id);
     if (!threadCreationPromise) {
@@ -561,6 +577,7 @@ class InputStateContainer extends React.PureComponent<Props, State> {
         viewerID: this.props.viewerID,
         calendarQuery,
         usingOlmViaTunnelbrokerForDMs: this.props.usingOlmViaTunnelbrokerForDMs,
+        auxUserInfos: this.props.auxUserInfos,
       });
       this.pendingThreadCreations.set(threadInfo.id, threadCreationPromise);
     }
@@ -1773,6 +1790,7 @@ const ConnectedInputStateContainer: React.ComponentType<BaseProps> =
     const textMessageCreationSideEffectsFunc =
       useMessageCreationSideEffectsFunc<RawTextMessageInfo>(messageTypes.TEXT);
     const usingOlmViaTunnelbrokerForDMs = useAllowOlmViaTunnelbrokerForDMs();
+    const auxUserInfos = useSelector(state => state.auxUserStore.auxUserInfos);
 
     return (
       <InputStateContainer
@@ -1794,6 +1812,7 @@ const ConnectedInputStateContainer: React.ComponentType<BaseProps> =
         staffCanSee={staffCanSee}
         textMessageCreationSideEffectsFunc={textMessageCreationSideEffectsFunc}
         usingOlmViaTunnelbrokerForDMs={usingOlmViaTunnelbrokerForDMs}
+        auxUserInfos={auxUserInfos}
       />
     );
   });

web/input/input-state-container.react.js
@@ -59,6 +59,7 @@ import {
   threadIsPending,
   threadIsPendingSidebar,
 } from 'lib/shared/thread-utils.js';
+import type { AuxUserInfos } from 'lib/types/aux-user-types.js';
 import type { CalendarQuery } from 'lib/types/entry-types.js';
 import type {
   MediaMission,
@@ -83,6 +84,7 @@ import {
   threadTypeIsSidebar,
   threadTypeIsThick,
 } from 'lib/types/thread-types-enum.js';
+import type { ThreadType } from 'lib/types/thread-types-enum.js';
 import {
   type ClientNewThinThreadRequest,
   type NewThreadResult,
@@ -170,6 +172,7 @@ type Props = {
   +textMessageCreationSideEffectsFunc: CreationSideEffectsFunc<RawTextMessageInfo>,
   +identityContext: ?IdentityClientContextType,
   +usingOlmViaTunnelbrokerForDMs: boolean,
+  +auxUserInfos: AuxUserInfos,
 };
 type WritableState = {
   pendingUploads: {
@@ -201,9 +204,18 @@ class InputStateContainer extends React.PureComponent<Props, State> {
     },
   };
   replyCallbacks: Array<(message: string) => void> = [];
-  pendingThreadCreations: Map<string, Promise<string>> = new Map<
+  pendingThreadCreations: Map<
     string,
-    Promise<string>,
+    Promise<{
+      +threadID: string,
+      +threadType: ThreadType,
+    }>,
+  > = new Map<
+    string,
+    Promise<{
+      +threadID: string,
+      +threadType: ThreadType,
+    }>,
   >();
   // TODO: flip the switch
   // Note that this enables Blob service for encrypted media only
@@ -481,7 +493,8 @@ class InputStateContainer extends React.PureComponent<Props, State> {
         // again.
         throw new Error('Thread creation failed');
       }
-      newThreadID = await threadCreationPromise;
+      const result = await threadCreationPromise;
+      newThreadID = result.threadID;
     } catch (e) {
       const copy = cloneError(e);
       copy.localID = messageInfo.localID;
@@ -577,9 +590,15 @@ class InputStateContainer extends React.PureComponent<Props, State> {
     }
   }
 
-  startThreadCreation(threadInfo: ThreadInfo): Promise<string> {
+  startThreadCreation(threadInfo: ThreadInfo): Promise<{
+    +threadID: string,
+    +threadType: ThreadType,
+  }> {
     if (!threadIsPending(threadInfo.id)) {
-      return Promise.resolve(threadInfo.id);
+      return Promise.resolve({
+        threadID: threadInfo.id,
+        threadType: threadInfo.type,
+      });
     }
     let threadCreationPromise = this.pendingThreadCreations.get(threadInfo.id);
     if (!threadCreationPromise) {
@@ -593,6 +612,7 @@ class InputStateContainer extends React.PureComponent<Props, State> {
         viewerID: this.props.viewerID,
         calendarQuery,
         usingOlmViaTunnelbrokerForDMs: this.props.usingOlmViaTunnelbrokerForDMs,
+        auxUserInfos: this.props.auxUserInfos,
       });
       this.pendingThreadCreations.set(threadInfo.id, threadCreationPromise);
     }
@@ -1323,9 +1343,9 @@ class InputStateContainer extends React.PureComponent<Props, State> {
       }
     }
 
-    let newThreadID = null;
+    let threadCreationResult = null;
     try {
-      newThreadID = await this.startThreadCreation(threadInfo);
+      threadCreationResult = await this.startThreadCreation(threadInfo);
     } catch (e) {
       const copy = cloneError(e);
       copy.localID = messageInfo.localID;
@@ -1342,13 +1362,14 @@ class InputStateContainer extends React.PureComponent<Props, State> {
 
     const newMessageInfo = {
       ...messageInfo,
-      threadID: newThreadID,
+      threadID: threadCreationResult?.threadID,
       time: Date.now(),
     };
 
     const newThreadInfo = {
       ...threadInfo,
-      id: newThreadID,
+      id: threadCreationResult?.threadID,
+      type: threadCreationResult?.threadType ?? threadInfo.type,
     };
     void this.props.dispatchActionPromise(
       sendTextMessageActionTypes,
@@ -1709,6 +1730,7 @@ const ConnectedInputStateContainer: React.ComponentType<BaseProps> =
     const textMessageCreationSideEffectsFunc =
       useMessageCreationSideEffectsFunc<RawTextMessageInfo>(messageTypes.TEXT);
     const usingOlmViaTunnelbrokerForDMs = useAllowOlmViaTunnelbrokerForDMs();
+    const auxUserInfos = useSelector(state => state.auxUserStore.auxUserInfos);
 
     return (
       <InputStateContainer
@@ -1735,6 +1757,7 @@ const ConnectedInputStateContainer: React.ComponentType<BaseProps> =
         textMessageCreationSideEffectsFunc={textMessageCreationSideEffectsFunc}
         identityContext={identityContext}
         usingOlmViaTunnelbrokerForDMs={usingOlmViaTunnelbrokerForDMs}
+        auxUserInfos={auxUserInfos}
       />
     );
   });

commit 0fe1e6c5aa2072b2777eeef7024e18ae4d38cdc6

[lib] Match thread types in pendingToRealizedThreadIDsSelector
Summary:
This addresses [ENG-9451](https://linear.app/comm/issue/ENG-9451/[web]-pressing-on-a-chat-in-the-chat-list-navigates-to-a-different), which was caused by D13404.

Before that diff, we would only match a pending thread ID to a realized thread if the thread type matched.

In that diff, we wanted to relax this condition a little bit so that a pending thick thread would match a realized thin thread, and vice versa.

However, we didn't consider a scenario where somebody creates a `LOCAL` thread and then somebody else leaves. In this case, the use of `pendingThreadType(actualMemberIDs.length - 1, thickOrThin)` would return a different thread type than the current thread's.

This diff replaces the usage of `pendingThreadType`, and brings back the old behavior of checking the realized thread type. However, we make sure to include both thin and thick thread types for each realized thread.

Depends on D13553

Test Plan: I recreated the test case in the Linear task as follows:

1. Create three accounts A, B, and C
2. Make A friends with B and C
3. Have A create a chat with B and C
4. Have B leave the chat
5. Have A search for C. Confirm that a pending personal thick thread appears in the results, and when it's pressed we no longer navigate to the test chat mentioned above

Reviewers: inka, tomek, kamil

Subscribers:

lib/selectors/thread-selectors.js
@@ -33,7 +33,6 @@ import {
   roleIsAdminRole,
   threadIsPending,
   getPendingThreadID,
-  pendingThreadType,
 } from '../shared/thread-utils.js';
 import type { ClientAvatar, ClientEmojiAvatar } from '../types/avatar-types';
 import type { EntryInfo } from '../types/entry-types.js';
@@ -49,7 +48,10 @@ import {
   threadTypeIsThick,
   threadTypeIsCommunityRoot,
   type ThreadType,
+  threadTypes,
   threadTypeIsSidebar,
+  threadTypeIsPrivate,
+  threadTypeIsPersonal,
 } from '../types/thread-types-enum.js';
 import type {
   MixedRawThreadInfos,
@@ -459,17 +461,38 @@ const pendingToRealizedThreadIDsSelector: (
       ) {
         continue;
       }
+
       const actualMemberIDs = rawThreadInfo.members
         .filter(member => member.role)
         .map(member => member.id);
+
       // In this function we're generating possible pending thread IDs that
       // could become `rawThreadInfos`. It is possible that a thick pending
       // thread becomes a thin thread, so we're including it twice in a map -
       // for each possible pending thread ID.
-      const possiblePendingThreadTypes = [
-        pendingThreadType(actualMemberIDs.length - 1, 'thin'),
-        pendingThreadType(actualMemberIDs.length - 1, 'thick'),
-      ];
+      let possiblePendingThreadTypes;
+      if (threadTypeIsPersonal(rawThreadInfo.type)) {
+        possiblePendingThreadTypes = [
+          threadTypes.PERSONAL,
+          threadTypes.GENESIS_PERSONAL,
+        ];
+      } else if (threadTypeIsPrivate(rawThreadInfo.type)) {
+        possiblePendingThreadTypes = [
+          threadTypes.PRIVATE,
+          threadTypes.GENESIS_PRIVATE,
+        ];
+      } else if (threadTypeIsSidebar(rawThreadInfo.type)) {
+        possiblePendingThreadTypes = [
+          threadTypes.THICK_SIDEBAR,
+          threadTypes.SIDEBAR,
+        ];
+      } else {
+        possiblePendingThreadTypes = [
+          threadTypes.LOCAL,
+          threadTypes.COMMUNITY_SECRET_SUBTHREAD,
+        ];
+      }
+
       for (const type of possiblePendingThreadTypes) {
         const pendingThreadID = getPendingThreadID(
           type,