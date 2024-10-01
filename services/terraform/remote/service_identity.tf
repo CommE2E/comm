f6e53d8f8c854e49eaded585ee36ae6a5a204a86
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2024-10-01 13:28:14 -0400

    [terraform] bump prod identity to 0.39
    Summary: should resolve some of the alarms we're getting on prod
    
    haven't seen any regressions on staging since we deployed 0.39 there
    
    Test Plan: terraform plan
    
    Reviewers: bartek, will, ashoat
    
    Subscribers:

--- services/terraform/remote/service_identity.tf
+++ services/terraform/remote/service_identity.tf
@@ -1,5 +1,5 @@
 locals {
-  identity_service_image_tag      = local.is_staging ? "0.39" : "0.37"
+  identity_service_image_tag      = local.is_staging ? "0.39" : "0.39"
   identity_service_server_image   = "commapp/identity-server:${local.identity_service_image_tag}"
   identity_service_container_name = "identity-server"
 
