CommE2E/comm

commit ef9b7221ed6ba0ac981f18dff53e7229d00ce7de

[tunnelbroker] Use AMQP wrapper in servers
Summary:
TBD

Depends on D13603

Test Plan: TBD

Reviewers: kamil, varun, will

Subscribers: ashoat, tomek

Differential Revision: https://phab.comm.dev/D13604

services/tunnelbroker/src/grpc/mod.rs
@@ -11,13 +11,14 @@ use tonic::transport::Server;
 use tracing::debug;
 use tunnelbroker_messages::MessageToDevice;
 
+use crate::amqp::AmqpConnection;
 use crate::constants::{CLIENT_RMQ_MSG_PRIORITY, WS_SESSION_CLOSE_AMQP_MSG};
 use crate::database::{handle_ddb_error, DatabaseClient};
 use crate::{constants, CONFIG};
 
 struct TunnelbrokerGRPC {
   client: DatabaseClient,
-  amqp_channel: lapin::Channel,
+  amqp: AmqpConnection,
 }
 
 pub fn handle_amqp_error(error: lapin::Error) -> tonic::Status {
@@ -57,7 +58,10 @@ impl TunnelbrokerService for TunnelbrokerGRPC {
       .map_err(|_| tonic::Status::invalid_argument("Invalid argument"))?;
 
     self
-      .amqp_channel
+      .amqp
+      .channel("grpc")
+      .await
+      .map_err(handle_amqp_error)?
       .basic_publish(
         "",
         &message.device_id,
@@ -81,7 +85,10 @@ impl TunnelbrokerService for TunnelbrokerGRPC {
     debug!("Connection close request for device {}", &message.device_id);
 
     self
-      .amqp_channel
+      .amqp
+      .channel("grpc")
+      .await
+      .map_err(handle_amqp_error)?
       .basic_publish(
         "",
         &message.device_id,
@@ -122,16 +129,11 @@ impl TunnelbrokerService for TunnelbrokerGRPC {
 
 pub async fn run_server(
   client: DatabaseClient,
-  ampq_connection: &lapin::Connection,
+  amqp_connection: &AmqpConnection,
 ) -> Result<(), tonic::transport::Error> {
   let addr = format!("[::]:{}", CONFIG.grpc_port)
     .parse()
     .expect("Unable to parse gRPC address");
-
-  let amqp_channel = ampq_connection
-    .create_channel()
-    .await
-    .expect("Unable to create amqp channel");
 
   tracing::info!("gRPC server listening on {}", &addr);
   Server::builder()
@@ -139,7 +141,7 @@ pub async fn run_server(
     .http2_keepalive_timeout(Some(constants::GRPC_KEEP_ALIVE_PING_TIMEOUT))
     .add_service(TunnelbrokerServiceServer::new(TunnelbrokerGRPC {
       client,
-      amqp_channel,
+      amqp: amqp_connection.clone(),
     }))
     .serve(addr)
     .await

services/tunnelbroker/src/websockets/mod.rs
@@ -1,5 +1,6 @@
 pub mod session;
 
+use crate::amqp::AmqpConnection;
 use crate::constants::{SOCKET_HEARTBEAT_TIMEOUT, WS_SESSION_CLOSE_AMQP_MSG};
 use crate::database::DatabaseClient;
 use crate::notifs::NotifClient;
@@ -39,7 +40,7 @@ use self::session::WebsocketSession;
 /// It also handles regular HTTP requests (currently health check)
 struct WebsocketService {
   addr: SocketAddr,
-  channel: lapin::Channel,
+  amqp: AmqpConnection,
   db_client: DatabaseClient,
   notif_client: NotifClient,
 }
@@ -62,7 +63,7 @@ impl hyper::service::Service<Request<Body>> for WebsocketService {
   fn call(&mut self, mut req: Request<Body>) -> Self::Future {
     let addr = self.addr;
     let db_client = self.db_client.clone();
-    let channel = self.channel.clone();
+    let amqp = self.amqp.clone();
     let notif_client = self.notif_client.clone();
 
     let future = async move {
@@ -72,7 +73,7 @@ impl hyper::service::Service<Request<Body>> for WebsocketService {
 
         // Spawn a task to handle the websocket connection.
         tokio::spawn(async move {
-          accept_connection(websocket, addr, db_client, channel, notif_client)
+          accept_connection(websocket, addr, db_client, amqp, notif_client)
             .await;
         });
 
@@ -101,7 +102,7 @@ impl hyper::service::Service<Request<Body>> for WebsocketService {
 
 pub async fn run_server(
   db_client: DatabaseClient,
-  amqp_connection: &lapin::Connection,
+  amqp_connection: &AmqpConnection,
   notif_client: NotifClient,
 ) -> Result<(), BoxedError> {
   let addr = env::var("COMM_TUNNELBROKER_WEBSOCKET_ADDR")
@@ -115,15 +116,12 @@ pub async fn run_server(
   http.http1_keep_alive(true);
 
   while let Ok((stream, addr)) = listener.accept().await {
-    let channel = amqp_connection
-      .create_channel()
-      .await
-      .expect("Failed to create AMQP channel");
+    let amqp = amqp_connection.clone();
     let connection = http
       .serve_connection(
         stream,
         WebsocketService {
-          channel,
+          amqp,
           db_client: db_client.clone(),
           addr,
           notif_client: notif_client.clone(),
@@ -169,10 +167,18 @@ async fn accept_connection(
   hyper_ws: HyperWebsocket,
   addr: SocketAddr,
   db_client: DatabaseClient,
-  amqp_channel: lapin::Channel,
+  amqp_connection: AmqpConnection,
   notif_client: NotifClient,
 ) {
   debug!("Incoming connection from: {}", addr);
+
+  let amqp_channel = match amqp_connection.channel(addr).await {
+    Ok(channel) => channel,
+    Err(err) => {
+      tracing::warn!("Failed to create AMQP channel for {addr}: {err:?}.");
+      return;
+    }
+  };
 
   let ws_stream = match hyper_ws.await {
     Ok(stream) => stream,

services/tunnelbroker/src/main.rs
@@ -48,7 +48,9 @@ async fn main() -> Result<()> {
   config::parse_cmdline_args()?;
   let aws_config = config::load_aws_config().await;
   let db_client = database::DatabaseClient::new(&aws_config);
-  let amqp_connection = amqp::connect().await;
+  let amqp_connection = amqp::AmqpConnection::connect()
+    .await
+    .expect("Failed to create AMQP connection");
 
   let apns_config = CONFIG.apns_config.clone();
 

commit 6441e49dd4fe6536d355bd4018e82437e8493ab7

[tunnelbroker] Implement lazy AMQP channel wrapper
Summary:

This cancels minor quality regression introduced earlier in D13604.
- For websocket, we store the channel in the `WebsocketSession` struct and reuse it unless it's broken
- However for gRPC, after opted out of channel pool, a new short-lived channel is created for every single call, which is against what RabbitMQ [recommends](https://www.rabbitmq.com/docs/channels#high-channel-churn).

Created a simple wrapper that lazily-initializes the channel upon first use, and creates a new one only if the previous was closed.

Depends on D13611

Test Plan:

Added some additional logging and ran Commtest

Reviewers: kamil

Subscribers:

services/tunnelbroker/src/amqp.rs
@@ -3,7 +3,7 @@ use lapin::{uri::AMQPUri, ConnectionProperties};
 use once_cell::sync::Lazy;
 use std::sync::Arc;
 use std::time::Duration;
-use tokio::sync::RwLock;
+use tokio::sync::{Mutex, RwLock};
 use tracing::{debug, error, info, warn};
 
 use crate::constants::error_types;
@@ -124,6 +124,40 @@ impl AmqpConnection {
   pub fn maybe_reconnect_in_background(&self) {
     let this = self.clone();
     tokio::spawn(async move { this.reset_conn().await });
+  }
+}
+
+/// Wrapper over [`lapin::Channel`] that automatically recreates AMQP channel
+/// in case of errors. The channel is initialized on first use.
+///
+/// TODO: Add support for restoring channel topology (queues and consumers)
+/// (`lapin` has this built-in, but it's internal crate feature)
+pub struct AmqpChannel {
+  conn: AmqpConnection,
+  channel: Arc<Mutex<Option<lapin::Channel>>>,
+}
+
+impl AmqpChannel {
+  pub fn new(amqp_connection: &AmqpConnection) -> Self {
+    let channel = Arc::new(Mutex::new(None));
+    Self {
+      conn: amqp_connection.clone(),
+      channel,
+    }
+  }
+
+  pub async fn get(&self) -> Result<lapin::Channel, lapin::Error> {
+    let mut channel = self.channel.lock().await;
+    match channel.as_ref() {
+      Some(ch) if ch.status().connected() => Ok(ch.clone()),
+      _ => {
+        let new_channel = self.conn.new_channel().await?;
+        let channel_id = new_channel.id();
+        debug!(channel_id, "Instantiated lazy AMQP channel.");
+        *channel = Some(new_channel.clone());
+        Ok(new_channel)
+      }
+    }
   }
 }
 

services/tunnelbroker/src/grpc/mod.rs
@@ -11,14 +11,14 @@ use tonic::transport::Server;
 use tracing::debug;
 use tunnelbroker_messages::MessageToDevice;
 
-use crate::amqp::AmqpConnection;
+use crate::amqp::{AmqpChannel, AmqpConnection};
 use crate::constants::{CLIENT_RMQ_MSG_PRIORITY, WS_SESSION_CLOSE_AMQP_MSG};
 use crate::database::{handle_ddb_error, DatabaseClient};
 use crate::{constants, CONFIG};
 
 struct TunnelbrokerGRPC {
   client: DatabaseClient,
-  amqp: AmqpConnection,
+  amqp_channel: AmqpChannel,
 }
 
 pub fn handle_amqp_error(error: lapin::Error) -> tonic::Status {
@@ -58,8 +58,8 @@ impl TunnelbrokerService for TunnelbrokerGRPC {
       .map_err(|_| tonic::Status::invalid_argument("Invalid argument"))?;
 
     self
-      .amqp
-      .new_channel()
+      .amqp_channel
+      .get()
       .await
       .map_err(handle_amqp_error)?
       .basic_publish(
@@ -85,8 +85,8 @@ impl TunnelbrokerService for TunnelbrokerGRPC {
     debug!("Connection close request for device {}", &message.device_id);
 
     self
-      .amqp
-      .new_channel()
+      .amqp_channel
+      .get()
       .await
       .map_err(handle_amqp_error)?
       .basic_publish(
@@ -141,7 +141,7 @@ pub async fn run_server(
     .http2_keepalive_timeout(Some(constants::GRPC_KEEP_ALIVE_PING_TIMEOUT))
     .add_service(TunnelbrokerServiceServer::new(TunnelbrokerGRPC {
       client,
-      amqp: amqp_connection.clone(),
+      amqp_channel: AmqpChannel::new(amqp_connection),
     }))
     .serve(addr)
     .await