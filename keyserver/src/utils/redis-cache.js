1fc5a0a1ee805d1a8856c0cffe28075fa05a0a82
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2024-10-08 14:29:45 -0400

    [keyserver] redis cache
    Summary:
    this cache is used in the next diff to try to retrieve info about a particular fc channel without making a neynar API call.
    
    Depends on D13635
    
    Test Plan: tested in next diff
    
    Reviewers: ashoat
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D13636

---
+++ keyserver/src/utils/redis-cache.js
@@ -0,0 +1,51 @@
+// @flow
+
+import type { RedisClient } from 'redis';
+import redis from 'redis';
+
+import type { NeynarChannel } from 'lib/types/farcaster-types.js';
+
+import { redisConfig } from '../socket/redis.js';
+
+class RedisCache {
+  cacheClient: RedisClient;
+
+  constructor() {
+    this.cacheClient = redis.createClient(redisConfig);
+  }
+
+  setChannelInfo(fcChannelID: string, fcChannel: NeynarChannel): Promise<void> {
+    const stringifiedChannelInfo = JSON.stringify(fcChannel);
+    return new Promise((resolve, reject) => {
+      this.cacheClient.set(
+        `channel:${fcChannelID}`,
+        stringifiedChannelInfo,
+        'EX',
+        600, // item expires after 10 minutes
+        err => {
+          if (err) {
+            return reject(err);
+          }
+          return resolve();
+        },
+      );
+    });
+  }
+
+  getChannelInfo(fcChannelID: string): Promise<?NeynarChannel> {
+    return new Promise((resolve, reject) => {
+      this.cacheClient.get(`channel:${fcChannelID}`, (err, result) => {
+        if (err) {
+          return reject(err);
+        }
+        // Reset the expiration when the cached data is successfully retrieved
+        this.cacheClient.expire(`channel:${fcChannelID}`, 600);
+        return resolve(result ? JSON.parse(result) : null);
+      });
+    });
+  }
+}
+
+const redisCache: RedisCache = new RedisCache();
+
+export { redisCache };
