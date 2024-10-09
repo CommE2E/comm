e05bf1e59f98731559f96c7d201f52d96b9ba329
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2024-10-08 23:24:26 -0400

    [keyserver] extend redis cache to store user identities returned by findUserIdentities
    Summary: new methods to get and set user identities in the cache
    
    Test Plan: tested in next diff
    
    Reviewers: ashoat
    
    Subscribers:

--- keyserver/src/utils/redis-cache.js
+++ keyserver/src/utils/redis-cache.js
@@ -4,6 +4,7 @@ import type { RedisClient } from 'redis';
 import redis from 'redis';
 
 import type { NeynarChannel } from 'lib/types/farcaster-types.js';
+import type { Identity } from 'lib/types/identity-service-types.js';
 
 import { redisConfig } from '../socket/redis.js';
 
@@ -40,6 +41,37 @@ class RedisCache {
         }
         // Reset the expiration when the cached data is successfully retrieved
         this.cacheClient.expire(`channel:${fcChannelID}`, 600);
+        return resolve(result ? JSON.parse(result) : null);
+      });
+    });
+  }
+
+  setUserIdentity(userID: string, userIdentity: Identity): Promise<void> {
+    const stringifiedUserIdentity = JSON.stringify(userIdentity);
+    return new Promise((resolve, reject) => {
+      this.cacheClient.set(
+        `user:${userID}`,
+        stringifiedUserIdentity,
+        'EX',
+        60, // item expires after 1 minute
+        err => {
+          if (err) {
+            return reject(err);
+          }
+          return resolve();
+        },
+      );
+    });
+  }
+
+  getUserIdentity(userID: string): Promise<?Identity> {
+    return new Promise((resolve, reject) => {
+      this.cacheClient.get(`user:${userID}`, (err, result) => {
+        if (err) {
+          return reject(err);
+        }
+        // Reset the expiration when the cached data is successfully retrieved
+        this.cacheClient.expire(`user:${userID}`, 60);
         return resolve(result ? JSON.parse(result) : null);
       });
     });
