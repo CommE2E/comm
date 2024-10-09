8dbdc0d06f431fa4be717834b63f68d981bb053f
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2024-10-08 22:56:43 -0400

    [keyserver] delete all fc channel tags
    Summary: i had tagged a bunch of communities with farcaster channels and needed a way to remove the tags in a way that not only clean up my keyserver but also clean up dynamodb and s3. this script may be helpful to others, so putting up a diff
    
    Test Plan: successfully removed all the farcaster tags i had created from my mariadb communities table, ddb, and s3
    
    Reviewers: ashoat
    
    Subscribers:

---
+++ keyserver/src/scripts/delete-all-fc-channel-tags.js
@@ -0,0 +1,29 @@
+// @flow
+
+import { main } from './utils.js';
+import { deleteFarcasterChannelTag } from '../deleters/farcaster-channel-tag-deleters.js';
+import { fetchAllCommunityInfosWithNames } from '../fetchers/community-fetchers.js';
+import { createScriptViewer } from '../session/scripts.js';
+import { thisKeyserverAdmin } from '../user/identity.js';
+
+async function deleteAllFCChannelTags() {
+  const admin = await thisKeyserverAdmin();
+  const adminViewer = createScriptViewer(admin.id);
+
+  const allCommunityInfosWithNames = await fetchAllCommunityInfosWithNames();
+
+  const deleteFarcasterChannelTagPromises = allCommunityInfosWithNames
+    .map(communityInfoWithName => {
+      if (!communityInfoWithName.farcasterChannelID) {
+        return null;
+      }
+      return deleteFarcasterChannelTag(adminViewer, {
+        commCommunityID: communityInfoWithName.id,
+        farcasterChannelID: communityInfoWithName.farcasterChannelID,
+      });
+    })
+    .filter(Boolean);
+  await Promise.all(deleteFarcasterChannelTagPromises);
+}
+
+main([deleteAllFCChannelTags]);
