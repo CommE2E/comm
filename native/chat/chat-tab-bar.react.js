a3a0777d7dc16f6887f398c3830715da03492853
Inka Sokołowska <inka.d.sokolowska@gmail.com>
2024-10-11 11:35:00 +0200

    [native] Fix NUX Android problem
    Summary:
    On some Androids, we cannot measure a button that is not visible on the screen. `undefineds` are returned. So we have to wait of the login screen to be dismissed, before we call `measure`.
    I wanted to pass the `measure` function itseft through conxtext, but that results in `Cannot read property '_nativeTag' of undefined` error. Not sure what that means, but I don't think passing the ref is much worse.
    `measure` asynchronously measures the component and then calls the callback. This is by I need states and an effect, instead of a memo.
    
    `buttonMeasured` flag ensures the tip doesn't jump after the `measure` is done. `measure` returns quickly and I don't think this slight delay is a problem.
    
    Test Plan:
    Tested on Android 11 physical device, and iOS 17.4 simulator. Tesdted by updating `NUXHandlerInner` to run the effect on every app reaload.
    Checked that on both Android and iOS the NUX tips are displayed in the correct position. Checked thare are no odd visual effects, like jumping (thanks to `buttonMeasured`)
    
    Reviewers: tomek, kamil
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D13697

--- native/chat/chat-tab-bar.react.js
+++ native/chat/chat-tab-bar.react.js
@@ -9,7 +9,10 @@ import { MaterialTopTabBar } from '@react-navigation/material-top-tabs';
 import invariant from 'invariant';
 import * as React from 'react';
 import { View } from 'react-native';
+import type { MeasureOnSuccessCallback } from 'react-native/Libraries/Renderer/shims/ReactNativeTypes';
 import { TabBarItem } from 'react-native-tab-view';
+
+import type { ReactRefSetter } from 'lib/types/react-types.js';
 
 import {
   nuxTip,
@@ -25,35 +28,29 @@ const ButtonTitleToTip = Object.freeze({
   [HomeChatThreadListRouteName]: nuxTip.HOME,
 });
 
+const onLayout = () => {};
+
 function TabBarButton(props: TabBarItemProps<Route<>>) {
   const tipsContext = React.useContext(NUXTipsContext);
   invariant(tipsContext, 'NUXTipsContext should be defined');
+  const { registerTipButton } = tipsContext;
 
-  const viewRef = React.useRef<?React.ElementRef<typeof View>>();
-  const onLayout = React.useCallback(() => {
-    const button = viewRef.current;
-    if (!button) {
-      return;
-    }
-
-    const tipType = ButtonTitleToTip[props.route.name];
-    if (!tipType) {
-      return;
-    }
-    button.measure((x, y, width, height, pageX, pageY) => {
-      tipsContext.registerTipButton(tipType, {
-        x,
-        y,
-        width,
-        height,
-        pageX,
-        pageY,
-      });
-    });
-  }, [props.route.name, tipsContext]);
+  const registerRef: ReactRefSetter<React.ElementRef<typeof View>> =
+    React.useCallback(
+      element => {
+        const tipType = ButtonTitleToTip[props.route.name];
+        if (!tipType) {
+          return;
+        }
+        const measure = (callback: MeasureOnSuccessCallback) =>
+          element?.measure(callback);
+        registerTipButton(tipType, measure);
+      },
+      [props.route.name, registerTipButton],
+    );
 
   return (
-    <View ref={viewRef} onLayout={onLayout}>
+    <View ref={registerRef} onLayout={onLayout}>
       <TabBarItem {...props} />
     </View>
   );
