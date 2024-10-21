d75e1e886753efd6d7785972947825eacad74630
William Wang <williamwang749@gmail.com>
2024-10-21 00:45:05 -0400

    [keyserver] verify webhook event with webhook secret
    Summary:
    This verifies the webhook event with `X-Neynar-Signature` and the webhook secret to verify it's coming from Neynar.
    
    Depends on D13755
    
    Test Plan:
    Tested by triggering a webhook event on warpcast and verifying that `isValid` was true.
    Verified that a webhook event with a differing body resulted in `isValid` evaluating as false
    
    Reviewers: ashoat, varun
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D13756

--- keyserver/src/responders/farcaster-webhook-responders.js
+++ keyserver/src/responders/farcaster-webhook-responders.js
@@ -1,18 +1,37 @@
 // @flow
 
+import { createHmac } from 'crypto';
 import type { $Request } from 'express';
 
+import { type NeynarWebhookCastCreatedEvent } from 'lib/types/farcaster-types.js';
 import { neynarWebhookCastCreatedEventValidator } from 'lib/types/validators/farcaster-webhook-validators.js';
+import { ServerError } from 'lib/utils/errors.js';
 import { assertWithValidator } from 'lib/utils/validation-utils.js';
+
+import { getNeynarConfig } from '../utils/neynar-utils.js';
 
 const taggedCommFarcasterInputValidator =
   neynarWebhookCastCreatedEventValidator;
 
-async function taggedCommFarcasterResponder(request: $Request): Promise<void> {
-  const event = assertWithValidator(
-    request.body,
-    taggedCommFarcasterInputValidator,
-  );
+async function verifyNeynarWebhookSignature(
+  signature: string,
+  event: NeynarWebhookCastCreatedEvent,
+): Promise<boolean> {
+  const neynarSecret = await getNeynarConfig();
+  if (!neynarSecret?.neynarWebhookSecret) {
+    throw new ServerError('missing_webhook_secret');
+  }
+
+  const hmac = createHmac('sha512', neynarSecret.neynarWebhookSecret);
+  hmac.update(JSON.stringify(event));
+
+  return hmac.digest('hex') === signature;
+}
+
+async function taggedCommFarcasterResponder(req: $Request): Promise<void> {
+  const { body } = req;
+
+  const event = assertWithValidator(body, taggedCommFarcasterInputValidator);
 
   const threadHashTagRegex: RegExp = new RegExp('#createathread', 'i');
   const foundCreateThreadHashTag = threadHashTagRegex.test(event.data.text);
@@ -20,7 +39,15 @@ async function taggedCommFarcasterResponder(request: $Request): Promise<void> {
     return;
   }
 
-  console.log(event);
+  const signature = req.header('X-Neynar-Signature');
+  if (!signature) {
+    throw new ServerError('missing_neynar_signature');
+  }
+
+  const isValidSignature = await verifyNeynarWebhookSignature(signature, event);
+  if (!isValidSignature) {
+    throw new ServerError('invalid_webhook_signature');
+  }
 }
 
 export { taggedCommFarcasterResponder, taggedCommFarcasterInputValidator };
