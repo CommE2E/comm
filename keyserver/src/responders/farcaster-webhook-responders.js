d8bf02561ee641bded560723a8376c520a2302c4
William Wang <williamwang749@gmail.com>
2024-10-21 00:45:05 -0400

    [keyserver] verify webhook event with webhook secret
    Summary:
    This verifies the webhook event with `X-Neynar-Signature` and the webhook secret to verify it's coming from Neynar.
    
    Depends on D13755
    
    Test Plan:
    Tested by triggering a webhook event on warpcast and verifying that `isValid` was true.
    Verified that a webhook event with a differing body resulted in `isValid` evaluating as false
    
    Reviewers:
    ashoat,
    varun
    
    Subscribers:

--- keyserver/src/responders/farcaster-webhook-responders.js
+++ keyserver/src/responders/farcaster-webhook-responders.js
@@ -1,20 +1,40 @@
 // @flow
 
+import { createHmac } from 'crypto';
 import type { $Request } from 'express';
 
 import { neynarWebhookCastCreatedEventValidator } from 'lib/types/validators/farcaster-webhook-validators.js';
+import { ServerError } from 'lib/utils/errors.js';
 import { assertWithValidator } from 'lib/utils/validation-utils.js';
+
+import { getNeynarConfig } from '../utils/fc-cache.js';
 
 const taggedCommFarcasterInputValidator =
   neynarWebhookCastCreatedEventValidator;
 
-async function taggedCommFarcasterResponder(request: $Request): Promise<void> {
-  const event = assertWithValidator(
-    request.body,
-    taggedCommFarcasterInputValidator,
-  );
+async function taggedCommFarcasterResponder(req: $Request): Promise<void> {
+  const body = req.body;
 
-  console.log(event);
+  const event = assertWithValidator(body, taggedCommFarcasterInputValidator);
+
+  const sig = req.header('X-Neynar-Signature');
+  if (!sig) {
+    throw new ServerError('missing_neynar_signature');
+  }
+
+  const neynarSecret = await getNeynarConfig();
+  if (!neynarSecret?.neynarWebhookSecret) {
+    throw new Error('missing_webhook_secret');
+  }
+
+  const hmac = createHmac('sha512', neynarSecret.neynarWebhookSecret);
+  hmac.update(JSON.stringify(event));
+
+  const generatedSignature = hmac.digest('hex');
+  const isValid = generatedSignature === sig;
+  if (!isValid) {
+    throw new ServerError('invalid_webhook_signature');
+  }
 }
 
 export { taggedCommFarcasterResponder, taggedCommFarcasterInputValidator };
