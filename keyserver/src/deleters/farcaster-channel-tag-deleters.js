dc12de02244eafd1101228b4359d54123db46f8a
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2024-10-23 00:46:52 -0400

    reset avatar when deleteFarcasterChannelTag is called
    Summary:
    if a tagged community has a farcaster avatar, we should reset it to NULL when the tag is removed
    
    Depends on D13778
    
    Test Plan:
    Tested in conjunction with previous diff
    
    id	name	avatar	description
    Created community
    
    84426	Oct23	NULL	NULL
    Tagged community with spam channel. NULL description was replaced with description from neynar. (also confirmed that avatar and description were updated on client)
    
    84426	Oct23	{"type":"farcaster"}	Dedicated channel for everything $SPAM
    Removed tag. avatar was reset to NULL. description remained
    
    84426	Oct23	NULL	Dedicated channel for everything $SPAM
    Removed description from native app. It was set to an empty string
    
    84426	Oct23	NULL
    Added a new description from app.
    
    84426	Oct23	NULL	Asdf
    Tagged community with a different channel. Avatar changed but description remained the same.
    
    84426	Oct23	{"type":"farcaster"}	Asdf
    Removed tag
    
    84426	Oct23	NULL	Asdf
    Removed description
    
    84426	Oct23	NULL
    Tagged community with a different channel. empty string description was replaced with description from neynar
    
    84426	Oct23	{"type":"farcaster"}	your musings, thoughts & dreams; welcome here
    
    Reviewers: ashoat
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D13779

--- keyserver/src/deleters/farcaster-channel-tag-deleters.js
+++ keyserver/src/deleters/farcaster-channel-tag-deleters.js
@@ -4,11 +4,14 @@ import { farcasterChannelTagBlobHash } from 'lib/shared/community-utils.js';
 import type { DeleteFarcasterChannelTagRequest } from 'lib/types/community-types.js';
 import { threadPermissions } from 'lib/types/thread-permission-types.js';
 import { ServerError } from 'lib/utils/errors.js';
+import { ignorePromiseRejections } from 'lib/utils/promises.js';
 
 import { dbQuery, SQL } from '../database/database.js';
+import { fetchServerThreadInfos } from '../fetchers/thread-fetchers.js';
 import { checkThreadPermission } from '../fetchers/thread-permission-fetchers.js';
 import { deleteBlob } from '../services/blob.js';
 import type { Viewer } from '../session/viewer';
+import { updateThread } from '../updaters/thread-updaters.js';
 
 async function deleteFarcasterChannelTag(
   viewer: Viewer,
@@ -39,7 +42,6 @@ async function deleteFarcasterChannelTag(
       blob_holder = NULL
     WHERE id = ${request.commCommunityID}
       AND farcaster_channel_id = ${request.farcasterChannelID};
-    
     COMMIT;
 
     SELECT @currentBlobHolder AS blobHolder;
@@ -61,6 +63,25 @@ async function deleteFarcasterChannelTag(
       true,
     );
   }
+
+  // In the background, we fetch information about the community thread. If the
+  // thread's avatar type is `farcaster`, we remove it.
+  ignorePromiseRejections(
+    (async () => {
+      const serverThreadInfos = await fetchServerThreadInfos({
+        threadID: request.commCommunityID,
+      });
+      const { avatar } = serverThreadInfos.threadInfos[request.commCommunityID];
+      if (avatar?.type !== 'farcaster') {
+        return;
+      }
+
+      await updateThread(viewer, {
+        threadID: request.commCommunityID,
+        changes: { avatar: { type: 'remove' } },
+      });
+    })(),
+  );
 }
 
 export { deleteFarcasterChannelTag };
