7d7ebb0032250ffffde9d19dfee32675ee8f5cb5
William Wang <williamwang749@gmail.com>
2024-10-23 00:28:44 -0400

    [keyserver] add community creation logic to farcaster webhook responder
    Summary:
    This adds the logic for communtity creation to the farcaster webhook responder. We either decide to early return or continue to sidebar creation with the resulting `channelCommunityID`.
    
    In the case that `AUTHORITATIVE_FARCASTER_BOT` is set, we create new communities if they haven't already been created and tagged through the blob service or default to the no channel community if no channel was specified.
    
    Test Plan:
    I used rahul's testingthings channel for testing this. I created a webhook event by tagging myself with `#createathread`. I then copied this webhook event and used it to trigger the responder.
    
    On staging, I created a dummy user with rahul's fid as well as a dummy user with my fid. In triggering the responder with `AUTHORITATIVE_FARCASTER_BOT` set to true, confirmed that the community was created with rahul set to admin.
    
    Reset everything to prior and ensured that if rahul's fid didn't exist on staging identity, retriggering the responder resulted in the same community but with the tagger (my fid) as the admin.
    
    Verified community creation through iOS simulator.
    
    Retriggered responder to verify that it then picked up on the community already existing and returning the proper threadID.
    
    Depends on D13774
    
    Reviewers: ashoat, varun
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D13775

--- keyserver/src/responders/farcaster-webhook-responders.js
+++ keyserver/src/responders/farcaster-webhook-responders.js
@@ -2,15 +2,115 @@
 
 import { createHmac } from 'crypto';
 import type { $Request } from 'express';
+import { getRustAPI } from 'rust-node-addon';
 
+import bots from 'lib/facts/bots.js';
+import { extractKeyserverIDFromID } from 'lib/keyserver-conn/keyserver-call-utils.js';
+import { threadTypes } from 'lib/types/thread-types-enum.js';
+import { type NewThreadResponse } from 'lib/types/thread-types.js';
 import { neynarWebhookCastCreatedEventValidator } from 'lib/types/validators/farcaster-webhook-validators.js';
 import { ServerError } from 'lib/utils/errors.js';
 import { assertWithValidator } from 'lib/utils/validation-utils.js';
 
-import { getNeynarConfig } from '../utils/fc-cache.js';
+import {
+  getFarcasterChannelTagBlob,
+  createOrUpdateFarcasterChannelTag,
+  farcasterChannelTagBlobValidator,
+} from '../creators/farcaster-channel-tag-creator.js';
+import { createThread } from '../creators/thread-creator.js';
+import { fetchThreadInfos } from '../fetchers/thread-fetchers.js';
+import { verifyUserIDs } from '../fetchers/user-fetchers.js';
+import { createBotViewer } from '../session/bots.js';
+import { updateRole } from '../updaters/thread-updaters.js';
+import { thisKeyserverAdmin, thisKeyserverID } from '../user/identity.js';
+import { getFarcasterBotConfig } from '../utils/farcaster-bot.js';
+import { getNeynarConfig, neynarClient } from '../utils/fc-cache.js';
 
 const taggedCommFarcasterInputValidator =
   neynarWebhookCastCreatedEventValidator;
+
+const noChannelCommunityID = '80887273';
+const { commbot } = bots;
+
+async function createTaggedFarcasterCommunity(
+  channelID: string,
+  taggerFID: string,
+): Promise<NewThreadResponse> {
+  const neynarChannel =
+    await neynarClient?.fetchFarcasterChannelByID(channelID);
+  if (!neynarChannel) {
+    throw new ServerError('channel_not_found');
+  }
+
+  const keyserverAdmin = await thisKeyserverAdmin();
+  let communityAdminID = keyserverAdmin.id;
+  const leadFID = neynarChannel.lead.fid.toString();
+
+  const rustAPI = await getRustAPI();
+  const farcasterUsers = await rustAPI.getFarcasterUsers([leadFID, taggerFID]);
+
+  const validUserIDs = await verifyUserIDs(
+    farcasterUsers.map(user => user.userID),
+  );
+
+  const validFarcasterUsers = farcasterUsers.filter(user =>
+    validUserIDs.includes(user.userID),
+  );
+
+  if (validFarcasterUsers.length > 0) {
+    communityAdminID =
+      validFarcasterUsers.find(user => user.farcasterID === leadFID)?.userID ||
+      validFarcasterUsers[0].userID;
+  }
+
+  const commbotViewer = createBotViewer(commbot.userID);
+  const initialMemberIDs = validUserIDs.includes(communityAdminID)
+    ? validUserIDs
+    : [...validUserIDs, communityAdminID];
+
+  const newThreadResponse = await createThread(
+    commbotViewer,
+    {
+      type: threadTypes.COMMUNITY_ROOT,
+      name: neynarChannel.name,
+      initialMemberIDs,
+    },
+    {
+      forceAddMembers: true,
+    },
+  );
+
+  const { newThreadID } = newThreadResponse;
+  const fetchThreadResult = await fetchThreadInfos(commbotViewer, {
+    threadID: newThreadResponse.newThreadID,
+  });
+  const threadInfo = fetchThreadResult.threadInfos[newThreadID];
+
+  if (!threadInfo) {
+    throw new ServerError('fetch_failed');
+  }
+
+  const adminRoleID = Object.keys(threadInfo.roles).find(
+    roleID => threadInfo.roles[roleID].name === 'Admins',
+  );
+
+  if (!adminRoleID) {
+    throw new ServerError('community_missing_admin');
+  }
+
+  await updateRole(commbotViewer, {
+    threadID: newThreadID,
+    memberIDs: [communityAdminID],
+    role: adminRoleID,
+  });
+
+  await createOrUpdateFarcasterChannelTag(commbotViewer, {
+    commCommunityID: newThreadResponse.newThreadID,
+    farcasterChannelID: channelID,
+  });
+
+  return newThreadResponse;
+}
 
 async function taggedCommFarcasterResponder(req: $Request): Promise<void> {
   const body = req.body;
@@ -35,6 +135,51 @@ async function taggedCommFarcasterResponder(req: $Request): Promise<void> {
   if (!isValid) {
     throw new ServerError('invalid_webhook_signature');
   }
+
+  const farcasterBotConfig = await getFarcasterBotConfig();
+  const isAuthoritativeFarcasterBot =
+    farcasterBotConfig.authoritativeFarcasterBot ?? false;
+  const eventChannelID = event.data.channel?.id;
+  const eventTaggerFID = event.data.author.fid;
+
+  if (!eventChannelID && !isAuthoritativeFarcasterBot) {
+    return;
+  }
+
+  let channelCommunityID = noChannelCommunityID;
+  if (eventChannelID) {
+    const blobDownload = await getFarcasterChannelTagBlob(eventChannelID);
+    if (blobDownload.found) {
+      const blobText = await blobDownload.blob.text();
+      const blobObject = JSON.parse(blobText);
+      const farcasterChannelTagBlob = assertWithValidator(
+        blobObject,
+        farcasterChannelTagBlobValidator,
+      );
+
+      const { commCommunityID } = farcasterChannelTagBlob;
+      const commCommunityKeyserverID =
+        extractKeyserverIDFromID(commCommunityID);
+      const keyserverID = await thisKeyserverID();
+
+      if (keyserverID !== commCommunityKeyserverID) {
+        return;
+      }
+      channelCommunityID = commCommunityID.split('|')[1];
+    } else if (!blobDownload.found && blobDownload.status === 404) {
+      if (!isAuthoritativeFarcasterBot) {
+        return;
+      }
+      const newThreadResponse = await createTaggedFarcasterCommunity(
+        eventChannelID,
+        eventTaggerFID.toString(),
+      );
+      channelCommunityID = newThreadResponse.newThreadID;
+    } else {
+      throw new ServerError('blob_fetch_failed');
+    }
+  }
+  console.log(channelCommunityID);
 }
 
 export { taggedCommFarcasterResponder, taggedCommFarcasterInputValidator };
