cad0197a05f563c08a4e49b86034b761139adaec
Ashoat Tevosyan <ashoat@comm.app>
2024-10-29 13:16:58 -0400

    [web] Convert activeChatThreadItem to a hook
    Summary:
    This will make the next diff a little easier.
    
    Depends on D13821
    
    Test Plan:
    Flow, and tested in combination with the rest of the stack:
    
    1. Make sure membership operations (user joining / leaving) don't appear in `MessagePreview`
    2. Make sure reactions to the viewer's messages still appear in `MessagePreview`
    3. Make sure reactions to other user's messages don't appear in `MessagePreview`
    4. Test fresh login to thread with only one message in the last 14 days, which is a reaction to a non-viewer message. Make sure the reaction initially appears in `MessagePreview`, but then is replaced after more messages are fetched by the client
    
    Reviewers: tomek
    
    Reviewed By: tomek
    
    Differential Revision: https://phab.comm.dev/D13822

--- web/avatars/web-edit-thread-avatar-provider.react.js
+++ web/avatars/web-edit-thread-avatar-provider.react.js
@@ -4,15 +4,14 @@ import * as React from 'react';
 
 import { BaseEditThreadAvatarProvider } from 'lib/components/base-edit-thread-avatar-provider.react.js';
 
-import { useSelector } from '../redux/redux-utils.js';
-import { activeChatThreadItem as activeChatThreadItemSelector } from '../selectors/chat-selectors.js';
+import { useActiveChatThreadItem } from '../selectors/chat-selectors.js';
 
 type Props = {
   +children: React.Node,
 };
 function WebEditThreadAvatarProvider(props: Props): React.Node {
   const { children } = props;
-  const activeChatThreadItem = useSelector(activeChatThreadItemSelector);
+  const activeChatThreadItem = useActiveChatThreadItem();
   const activeThreadID = activeChatThreadItem?.threadInfo?.id ?? '';
 
   return (
