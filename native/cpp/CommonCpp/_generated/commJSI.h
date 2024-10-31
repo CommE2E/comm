7b32c682204fd49dc5c9922b6b8b5b2d967a7f1a
xsanm <kkurowski.kk@gmail.com>
2024-10-31 13:33:22 +0100

    [backup-client] make `backupID` mandatory when restoring backup
    Summary:
    [ENG-6145](https://linear.app/comm/issue/ENG-6145/change-backup-restore-endpoint-to-expect-username-or-wallet-address).
    
    Alternative, was to pass `username`/`wallet` to `restoreBackup` - but each time using this we should already have `latestBackupInfo` as it's needed during restore, so this better matches out protocol.
    
    Depends on D13839
    
    Test Plan: Tested in TBD
    
    Reviewers: varun, bartek

--- native/cpp/CommonCpp/_generated/commJSI.h
+++ native/cpp/CommonCpp/_generated/commJSI.h
@@ -76,7 +76,7 @@ public:
   virtual void stopBackupHandler(jsi::Runtime &rt) = 0;
   virtual jsi::Value createNewBackup(jsi::Runtime &rt, jsi::String backupSecret) = 0;
   virtual jsi::Value createNewSIWEBackup(jsi::Runtime &rt, jsi::String backupSecret, jsi::String siweBackupMsg) = 0;
-  virtual jsi::Value restoreBackup(jsi::Runtime &rt, jsi::String backupSecret, jsi::String maxVersion) = 0;
+  virtual jsi::Value restoreBackup(jsi::Runtime &rt, jsi::String backupSecret, jsi::String maxVersion, jsi::String backupID) = 0;
   virtual jsi::Value restoreSIWEBackup(jsi::Runtime &rt, jsi::String backupSecret, jsi::String backupID, jsi::String maxVersion) = 0;
   virtual jsi::Value restoreBackupData(jsi::Runtime &rt, jsi::String backupID, jsi::String backupDataKey, jsi::String backupLogDataKey, jsi::String maxVersion) = 0;
   virtual jsi::Value retrieveBackupKeys(jsi::Runtime &rt, jsi::String backupSecret) = 0;
@@ -565,13 +565,13 @@ private:
       return bridging::callFromJs<jsi::Value>(
           rt, &T::createNewSIWEBackup, jsInvoker_, instance_, std::move(backupSecret), std::move(siweBackupMsg));
     }
-    jsi::Value restoreBackup(jsi::Runtime &rt, jsi::String backupSecret, jsi::String maxVersion) override {
+    jsi::Value restoreBackup(jsi::Runtime &rt, jsi::String backupSecret, jsi::String maxVersion, jsi::String backupID) override {
       static_assert(
-          bridging::getParameterCount(&T::restoreBackup) == 3,
-          "Expected restoreBackup(...) to have 3 parameters");
+          bridging::getParameterCount(&T::restoreBackup) == 4,
+          "Expected restoreBackup(...) to have 4 parameters");
 
       return bridging::callFromJs<jsi::Value>(
-          rt, &T::restoreBackup, jsInvoker_, instance_, std::move(backupSecret), std::move(maxVersion));
+          rt, &T::restoreBackup, jsInvoker_, instance_, std::move(backupSecret), std::move(maxVersion), std::move(backupID));
     }
     jsi::Value restoreSIWEBackup(jsi::Runtime &rt, jsi::String backupSecret, jsi::String backupID, jsi::String maxVersion) override {
       static_assert(
