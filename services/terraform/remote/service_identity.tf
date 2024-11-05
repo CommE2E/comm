eb63a576897610ac80904986ce8ff186f82045bd
Ashoat Tevosyan <ashoat@comm.app>
2024-11-05 15:10:46 -0500

    [services] Bump staging identity to 0.45
    Summary: This includes D13883.
    
    Test Plan: N/A
    
    Reviewers: varun, will, bartek
    
    Reviewed By: varun
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D13884

--- services/terraform/remote/service_identity.tf
+++ services/terraform/remote/service_identity.tf
@@ -1,5 +1,5 @@
 locals {
-  identity_service_image_tag      = local.is_staging ? "0.44" : "0.44"
+  identity_service_image_tag      = local.is_staging ? "0.45" : "0.44"
   identity_service_server_image   = "commapp/identity-server:${local.identity_service_image_tag}"
   identity_service_container_name = "identity-server"
 
