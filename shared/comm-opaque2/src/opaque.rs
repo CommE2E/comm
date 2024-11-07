1c988013abf8f97f476e9aad424a930da434f1cd
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2024-11-07 14:50:03 -0500

    [shared] bump opaque-ke to 2.1.0-pre.1
    Summary: This version of opaque-ke uses the latest version of voprf. The previous version of opaque-ke was using an older version of voprf that no longer works with the latest version of Rust. Hence, in order to upgrade to the latest version of Rust, we need to first upgrade opaque-ke.
    
    Next diff will bump our Rust version from 1.76 to 1.82 (latest stable version)
    
    Test Plan:
    
    1. deployed custom identity image to staging with these changes
    2. checked out master branch
    3. successfully logged in to existing account on staging identity from native, web, keyserver
    4. successfully registered and logged in to staging from native
    5. switched back to feature branch
    6. successfully logged in to existing account on staging identity from native, web, keyserver
    7. successfully registered and logged in to staging from native
    
    Reviewers: ashoat, will, bartek
    
    Subscribers:

--- shared/comm-opaque2/src/opaque.rs
+++ shared/comm-opaque2/src/opaque.rs
@@ -1,5 +1,6 @@
-use argon2::Argon2;
+use generic_array::{ArrayLength, GenericArray};
 use opaque_ke::ciphersuite::CipherSuite;
+use opaque_ke::errors::InternalError;
 
 pub struct Cipher;
 
@@ -7,5 +8,28 @@ impl CipherSuite for Cipher {
   type OprfCs = opaque_ke::Ristretto255;
   type KeGroup = opaque_ke::Ristretto255;
   type KeyExchange = opaque_ke::key_exchange::tripledh::TripleDh;
-  type Ksf = Argon2<'static>;
+  type Ksf = Argon2_4096_3_1;
+}
+
+pub struct Argon2_4096_3_1 {
+  inner: argon2::Argon2<'static>,
+}
+impl Default for Argon2_4096_3_1 {
+  fn default() -> Self {
+    Self {
+      inner: argon2::Argon2::new(
+        argon2::Algorithm::default(),
+        argon2::Version::default(),
+        argon2::Params::new(4096, 3, 1, None).unwrap(),
+      ),
+    }
+  }
+}
+impl opaque_ke::ksf::Ksf for Argon2_4096_3_1 {
+  fn hash<L: ArrayLength<u8>>(
+    &self,
+    input: GenericArray<u8, L>,
+  ) -> Result<GenericArray<u8, L>, InternalError> {
+    self.inner.hash(input)
+  }
 }
