b63b09d62c78ace204dd68fcd5d0b2a79a16cc59
William Wang <williamwang749@gmail.com>
2024-11-13 14:57:09 -0500

    [keyserver] frog hello world example
    Summary: This diff implements a hello world Frog application and serves it on port 3001.
    
    Test Plan:
    `GET` requests to the keyserver at `localhost:3001` contain valid HTML document in the response.
    
    Used `ngrok` to create a publicly accessible url forwarding requests to `locahost:3001`. Used this url to validate the frame with the warpcast frame validator at https://warpcast.com/~/developers/frames?url=https://81fb-69-201-171-11.ngrok-free.app
    
    Reviewers: ashoat
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D13947

---
+++ keyserver/src/frog/frog.js
@@ -0,0 +1,34 @@
+// @flow
+
+/** @jsxImportSource hono/jsx */
+
+/* eslint-disable react/react-in-jsx-scope */
+
+import { serve } from '@hono/node-server';
+import { Button, Frog } from 'frog';
+
+function startFrogHonoServer() {
+  const frogApp = new Frog({ title: 'frog app' });
+
+  frogApp.frame('/', c => {
+    return c.res({
+      image: (
+        <div style={{ color: 'white', display: 'flex', fontSize: 60 }}>
+          Hello World!
+        </div>
+      ),
+      intents: [
+        <Button key="invite" value="Invite Link">
+          Join chat
+        </Button>,
+      ],
+    });
+  });
+
+  serve({
+    fetch: frogApp.fetch,
+    port: parseInt(process.env.FROG_PORT, 10) || 3001,
+  });
+}
+
+export { startFrogHonoServer };
