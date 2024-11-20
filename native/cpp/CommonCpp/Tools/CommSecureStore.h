1899766705d813665e10c77d3357c6a5fdf0a68c
xsanm <kkurowski.kk@gmail.com>
2024-11-20 15:27:05 +0100

    [CommCoreModule][native_rust_library] move constants to one place
    Summary:
    Part of [ENG-9656](https://linear.app/comm/issue/ENG-9656/native-changes-for-minimal-version-of-backup)
    
    Not sure why previously it was defined in two places, makes it hard to maintain so cleaning this up. Added missing comment.
    
    Depends on D13937
    
    Test Plan: Tested creating backup and secondary device login
    
    Reviewers: bartek, tomek

--- native/cpp/CommonCpp/Tools/CommSecureStore.h
+++ native/cpp/CommonCpp/Tools/CommSecureStore.h
@@ -10,9 +10,13 @@ class CommSecureStore {
 public:
   static void set(const std::string key, const std::string value);
   static folly::Optional<std::string> get(const std::string key);
+  // Should match constant defined in `native_rust_library/src/constants.rs`
   inline static const std::string commServicesAccessToken = "accessToken";
   inline static const std::string userID = "userID";
   inline static const std::string deviceID = "deviceID";
+  inline static const std::string encryptionKey = "comm.encryptionKey";
+  inline static const std::string backupLogsEncryptionKey =
+      "comm.backupLogsEncryptionKey";
 };
 
 } // namespace comm
