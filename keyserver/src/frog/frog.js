9102fea9ba1426f7e1cb5784b137dabd59d41709
William Wang <williamwang749@gmail.com>
2024-11-20 00:14:38 -0500

    [keyserver] implement invite link button to frog hono route
    Summary:
    This implements the invite link button in the frame. We take a invite link parameter in the route. The button will redirect to this link
    
    We also check if the invite link parameter is a valid Comm invite link by checking if it is prefixed by `https://comm.app/invite/`. If not, we default to `https://comm.app`
    
    Depends on D13947
    
    Test Plan:
    Used the warpcast frame validator https://warpcast.com/~/developers to display the frame and confirmed button redirected to invite link provided as the parameter with the following url in the warpcast frame validator: https://<keyserver url>/https%3A%2F%2Fcomm.app%2Finvite%2Fcryptoleft
    
    Used a different link than a Comm link and confirmed it still defaults to `https://comm.app`
    
    Reviewers: varun, ashoat
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D13971

--- keyserver/src/frog/frog.js
+++ keyserver/src/frog/frog.js
@@ -7,10 +7,21 @@
 import { serve } from '@hono/node-server';
 import { Button, Frog } from 'frog';
 
+import { inviteLinkURL } from 'lib/facts/links.js';
+
 function startFrogHonoServer() {
   const frogApp = new Frog({ title: 'Comm Frame App' });
 
-  frogApp.frame('/', c => {
+  frogApp.frame('/:inviteLink', c => {
+    const { inviteLink } = c.req.param();
+
+    let buttonLink = 'https://comm.app';
+    const inviteLinkURLPrefix = inviteLinkURL('');
+
+    if (inviteLink.startsWith(inviteLinkURLPrefix)) {
+      buttonLink = inviteLink;
+    }
+
     return c.res({
       image: (
         <div style={{ color: 'white', display: 'flex', fontSize: 60 }}>
@@ -18,9 +29,9 @@ function startFrogHonoServer() {
         </div>
       ),
       intents: [
-        <Button key="invite" value="Invite Link">
+        <Button.Link key="invite" href={buttonLink}>
           Join chat
-        </Button>,
+        </Button.Link>,
       ],
     });
   });
