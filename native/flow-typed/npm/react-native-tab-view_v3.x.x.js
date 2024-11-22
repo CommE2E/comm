1ac9181141714a52d8c390d3e9648d15d0e584b1
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2024-11-21 22:43:10 -0500

    [native] community joiner modal
    Summary:
    Depends on D13995
    
    we'll navigate to this modal from the community drawer button in D13987 and from the bottom sheet in D13986
    
    Test Plan: tested that the tabs work as expected and all the community threads display in the right tab. tested the join/leave behavior in previous diff, which also has a video with the modal
    
    Reviewers: ashoat
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D13996

---
+++ native/flow-typed/npm/react-native-tab-view_v3.x.x.js
@@ -0,0 +1,61 @@
+// flow-typed signature: a7123e367e1cd3cb25dcd811f56eea57
+// flow-typed version: <<STUB>>/react-native-tab-view_v3.3.0/flow_v0.202.1
+
+declare module 'react-native-tab-view' {
+  import type { Route } from '@react-navigation/core';
+  import type {
+    AnimatedValue,
+    ViewStyle,
+  } from 'react-native-gesture-handler/@react-native';
+
+  declare type Layout = {
+    +width: number,
+    +height: number,
+  };
+  declare type SceneRendererProps = {
+    +layout: Layout,
+    +jumpTo: (key: string) => void,
+    ...
+  };
+
+  declare export var TabBarItem: React$ComponentType<{
+    +position: AnimatedValue,
+    +route: Route<>,
+    +navigationState: { +index: number, ... },
+    +onPress: () => void,
+    +onLongPress: () => void,
+    +style: ViewStyle,
+    +getLabelText?: () => string,
+    +getAccessible?: () => void,
+    +getAccessibilityLabel?: () => void,
+    +renderIcon?: ({ color: string, ... }) => React$Node,
+    +getTestID?: () => void,
+    +activeColor?: string,
+    ...
+  }>;
+
+  declare export var TabView: React$ComponentType<{
+    +navigationState: { +index: number, ... },
+    +renderScene: (props: SceneRendererProps & { route: Route<> }) => React$Node,
+    +onIndexChange: (number) => void,
+    +initialLayout: Partial<Layout>,
+    +renderTabBar: (TabBarProps) => React$Node,
+    ...
+  }>;
+
+  declare export function SceneMap(scenes: {
+    [key: string]: () => React$Node,
+  }): (props: SceneRendererProps & { route: Route<> }) => React$Node;
+
+  declare export var TabBar: React$ComponentType<{
+    +style: ViewStyle,
+    +indicatorStyle: ViewStyle,
+    ...
+  }>;
+
+  declare export type TabBarProps = {
+    +style: ViewStyle,
+    +indicatorStyle: ViewStyle,
+    ...
+  }
+}
