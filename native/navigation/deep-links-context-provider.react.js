ce616cfd2327d2593383e469c0bbac1e7b0db30b
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2024-11-25 12:07:08 +0100

    [native] Show version unsupported when opened QR screen
    Summary:
    The QR screen is now for dev-only, but it's theoretically possible to access it via deep link.
    
    For now, the clients aren't able to do it because there are no such QR codes to scan, but in the future after device list launch, they might try doing it on old client versions.
    We should display the unsupported version message.
    
    Depends on D14036
    
    Test Plan: Changed isDev condition and opened the QR scanner screen. I was logged out and a message was displayed.
    
    Reviewers: kamil, angelika
    
    Reviewed By: kamil
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D14037

--- native/navigation/deep-links-context-provider.react.js
+++ native/navigation/deep-links-context-provider.react.js
@@ -25,6 +25,7 @@ import { useDispatchActionPromise } from 'lib/utils/redux-promise-utils.js';
 import {
   errorMessageIsInvalidCSAT,
   usingCommServicesAccessToken,
+  usingRestoreFlow,
 } from 'lib/utils/services-utils.js';
 
 import {
@@ -32,7 +33,10 @@ import {
   SecondaryDeviceQRCodeScannerRouteName,
 } from './route-names.js';
 import { useSelector } from '../redux/redux-utils.js';
-import { useOnFirstLaunchEffect } from '../utils/hooks.js';
+import {
+  useShowVersionUnsupportedAlert,
+  useOnFirstLaunchEffect,
+} from '../utils/hooks.js';
 
 type DeepLinksContextType = {
   +setCurrentLinkUrl: SetState<?string>,
@@ -97,6 +101,7 @@ function DeepLinksContextProvider(props: Props): React.Node {
   const { getAuthMetadata } = identityContext;
 
   const invalidTokenLogOut = useInvalidCSATLogOut();
+  const showVersionUnsupportedAlert = useShowVersionUnsupportedAlert(false);
   const loggedIn = useSelector(isLoggedIn);
   const dispatchActionPromise = useDispatchActionPromise();
   const validateLink = useVerifyInviteLink(keyserverOverride);
@@ -146,10 +151,21 @@ function DeepLinksContextProvider(props: Props): React.Node {
           });
         }
       } else if (parsedData.type === 'qr-code') {
+        if (!usingRestoreFlow) {
+          showVersionUnsupportedAlert();
+          return;
+        }
         navigation.navigate(SecondaryDeviceQRCodeScannerRouteName);
       }
     })();
-  }, [currentLink, getAuthMetadata, loggedIn, navigation, invalidTokenLogOut]);
+  }, [
+    currentLink,
+    getAuthMetadata,
+    loggedIn,
+    navigation,
+    invalidTokenLogOut,
+    showVersionUnsupportedAlert,
+  ]);
 
   React.useEffect(() => {
     const secret = inviteLinkSecret.current;
