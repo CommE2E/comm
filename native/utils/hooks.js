e2d3ee6520526f189520254c3f00795a0d4e6197
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2024-11-25 07:47:04 +0100

    [native] Extract showing version unsupported alert to separate hook
    Summary:
    This alert is going to be also shown when use_new_flow is returned. Made logout-upon-confirm configurable
    
    Depends on D14034
    
    Test Plan: Mocked `versionSupported()` always return false. The modal was displayed and I was logged out afterwards.
    
    Reviewers: kamil, varun
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D14035

--- native/utils/hooks.js
+++ native/utils/hooks.js
@@ -3,6 +3,14 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as React from 'react';
 
+import { useLogOut, logOutActionTypes } from 'lib/actions/user-actions.js';
+import { isLoggedIn } from 'lib/selectors/user-selectors.js';
+import { useDispatchActionPromise } from 'lib/utils/redux-promise-utils.js';
+
+import { useSelector } from '../redux/redux-utils.js';
+import { appOutOfDateAlertDetails } from '../utils/alert-messages.js';
+import Alert from '../utils/alert.js';
+
 function useOnFirstLaunchEffect(uniqueKey: string, effect: () => mixed) {
   const [started, setStarted] = React.useState(false);
 
@@ -26,4 +34,30 @@ function useOnFirstLaunchEffect(uniqueKey: string, effect: () => mixed) {
   }, [effect, started, uniqueKey]);
 }
 
-export { useOnFirstLaunchEffect };
+function useShowVersionUnsupportedAlert(shouldCallLogOut: boolean): () => void {
+  const loggedIn = useSelector(isLoggedIn);
+  const dispatchActionPromise = useDispatchActionPromise();
+  const callLogOut = useLogOut();
+
+  const onUsernameAlertAcknowledged = React.useCallback(() => {
+    if (loggedIn && shouldCallLogOut) {
+      void dispatchActionPromise(logOutActionTypes, callLogOut());
+    }
+  }, [callLogOut, dispatchActionPromise, loggedIn, shouldCallLogOut]);
+
+  return React.useCallback(() => {
+    Alert.alert(
+      appOutOfDateAlertDetails.title,
+      appOutOfDateAlertDetails.message,
+      [
+        {
+          text: 'OK',
+          onPress: onUsernameAlertAcknowledged,
+        },
+      ],
+      { cancelable: false },
+    );
+  }, [onUsernameAlertAcknowledged]);
+}
+
+export { useOnFirstLaunchEffect, useShowVersionUnsupportedAlert };
