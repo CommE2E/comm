6340aabf2ad2bf81ed392f9c4fd42c4a6b4433c9
xsanm <kkurowski.kk@gmail.com>
2024-11-27 17:01:24 +0100

    [native] upload User Keys backup when user is staff
    Summary:
    Part of [ENG-9604](https://linear.app/comm/issue/ENG-9604/update-the-backup-handler-lifecycle-for-minimal-version-of-backup) but only for staff.
    
    This is also needed for [ENG-9711](https://linear.app/comm/issue/ENG-9711/add-handler-for-staff-to-attempt-restore-after-attempting-backup).
    
    Depends on D14056
    
    Test Plan:
    1. Check if backup is uploaded when user has no backup
    2. Log out/in and check if backup is uploaded
    
    Reviewers: bartek, tomek
    
    Reviewed By: bartek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D14059

--- native/backup/backup-handler.js
+++ native/backup/backup-handler.js
@@ -2,11 +2,14 @@
 
 import * as React from 'react';
 
+import { createUserKeysBackupActionTypes } from 'lib/actions/backup-actions.js';
 import { useCheckIfPrimaryDevice } from 'lib/hooks/primary-device-hooks.js';
 import { isLoggedIn } from 'lib/selectors/user-selectors.js';
 import { useStaffAlert } from 'lib/shared/staff-utils.js';
 import { getMessageForException } from 'lib/utils/errors.js';
+import { useDispatchActionPromise } from 'lib/utils/redux-promise-utils.js';
 
+import { useClientBackup } from './use-client-backup.js';
 import { commCoreModule } from '../native-modules.js';
 import { useSelector } from '../redux/redux-utils.js';
 import { useStaffCanSee } from '../utils/staff-utils.js';
@@ -20,6 +23,13 @@ function BackupHandler(): null {
   const canPerformBackupOperation = loggedIn && !isBackground;
   const checkIfPrimaryDevice = useCheckIfPrimaryDevice();
   const { showAlertToStaff } = useStaffAlert();
+  const latestBackupInfo = useSelector(
+    state => state.backupStore.latestBackupInfo,
+  );
+  const dispatchActionPromise = useDispatchActionPromise();
+  const { createUserKeysBackup } = useClientBackup();
+  const backupUploadInProgress = React.useRef<boolean>(false);
+  const [handlerStarted, setHandlerStarted] = React.useState(false);
 
   React.useEffect(() => {
     if (!staffCanSee) {
@@ -32,17 +42,21 @@ function BackupHandler(): null {
         return;
       }
 
-      if (canPerformBackupOperation) {
+      if (!handlerStarted && canPerformBackupOperation) {
         try {
           commCoreModule.startBackupHandler();
+          setHandlerStarted(true);
         } catch (err) {
           const message = getMessageForException(err) ?? 'unknown error';
           showAlertToStaff('Error starting backup handler', message);
           console.log('Error starting backup handler:', message);
         }
-      } else {
+      }
+
+      if (handlerStarted && !canPerformBackupOperation) {
         try {
           commCoreModule.stopBackupHandler();
+          setHandlerStarted(false);
         } catch (err) {
           const message = getMessageForException(err) ?? 'unknown error';
           showAlertToStaff('Error stopping backup handler', message);
@@ -51,12 +65,58 @@ function BackupHandler(): null {
       }
     })();
   }, [
-    staffCanSee,
-    loggedIn,
-    isBackground,
-    checkIfPrimaryDevice,
     canPerformBackupOperation,
+    checkIfPrimaryDevice,
+    handlerStarted,
     showAlertToStaff,
+    staffCanSee,
+  ]);
+
+  React.useEffect(() => {
+    if (
+      !staffCanSee ||
+      !canPerformBackupOperation ||
+      !handlerStarted ||
+      backupUploadInProgress.current ||
+      !!latestBackupInfo
+    ) {
+      return;
+    }
+
+    void (async () => {
+      backupUploadInProgress.current = true;
+      const isPrimaryDevice = await checkIfPrimaryDevice();
+      if (!isPrimaryDevice) {
+        backupUploadInProgress.current = false;
+        return;
+      }
+
+      try {
+        const promise = (async () => {
+          const backupID = await createUserKeysBackup();
+          return {
+            backupID,
+            timestamp: Date.now(),
+          };
+        })();
+        void dispatchActionPromise(createUserKeysBackupActionTypes, promise);
+        await promise;
+      } catch (err) {
+        const message = getMessageForException(err) ?? 'unknown error';
+        showAlertToStaff('Error creating User Keys backup', message);
+        console.log('Error creating User Keys backup:', message);
+      }
+      backupUploadInProgress.current = false;
+    })();
+  }, [
+    canPerformBackupOperation,
+    checkIfPrimaryDevice,
+    createUserKeysBackup,
+    dispatchActionPromise,
+    handlerStarted,
+    latestBackupInfo,
+    showAlertToStaff,
+    staffCanSee,
   ]);
 
   return null;
