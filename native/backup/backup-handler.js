5e33fbea67ff6d213c9072aa2377a3648ff3c946
xsanm <kkurowski.kk@gmail.com>
2024-11-27 17:45:24 +0100

    [native] add handler for staff to attempt backup restore
    Summary:
    [ENG-9711](https://linear.app/comm/issue/ENG-9711/add-handler-for-staff-to-attempt-restore-after-attempting-backup).
    
    Depends on D14059
    
    Test Plan: Update `millisecondsPerDay` to be lower value and test if this works.
    
    Reviewers: bartek, tomek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D14060

--- native/backup/backup-handler.js
+++ native/backup/backup-handler.js
@@ -9,10 +9,13 @@ import { getMessageForException } from 'lib/utils/errors.js';
 import { useDispatchActionPromise } from 'lib/utils/redux-promise-utils.js';
 
 import { useClientBackup } from './use-client-backup.js';
+import { useGetBackupSecretForLoggedInUser } from './use-get-backup-secret.js';
 import { commCoreModule } from '../native-modules.js';
 import { useSelector } from '../redux/redux-utils.js';
 import Alert from '../utils/alert.js';
 import { useStaffCanSee } from '../utils/staff-utils.js';
+
+const millisecondsPerDay = 24 * 60 * 60 * 1000;
 
 function BackupHandler(): null {
   const loggedIn = useSelector(isLoggedIn);
@@ -26,7 +29,9 @@ function BackupHandler(): null {
     state => state.backupStore.latestBackupInfo,
   );
   const dispatchActionPromise = useDispatchActionPromise();
-  const { createUserKeysBackup } = useClientBackup();
+  const { createUserKeysBackup, retrieveLatestBackupInfo, getBackupUserKeys } =
+    useClientBackup();
+  const getBackupSecret = useGetBackupSecretForLoggedInUser();
   const backupUploadInProgress = React.useRef<boolean>(false);
   const [handlerStarted, setHandlerStarted] = React.useState(false);
 
@@ -73,13 +78,58 @@ function BackupHandler(): null {
     handlerStarted,
   ]);
 
+  const testUserKeysRestore = React.useCallback(async () => {
+    if (!latestBackupInfo?.backupID) {
+      return;
+    }
+    const {
+      latestBackupInfo: { backupID },
+      userIdentifier,
+    } = await retrieveLatestBackupInfo();
+
+    const backupSecret = await getBackupSecret();
+    const {
+      backupDataKey: backupDataKeyFromBackup,
+      backupLogDataKey: backupLogDataKeyFromBackup,
+    } = await getBackupUserKeys(userIdentifier, backupSecret, backupID);
+
+    const {
+      backupID: localBackupID,
+      backupDataKey: localBackupDataKey,
+      backupLogDataKey: localBackupLogDataKey,
+    } = await commCoreModule.getQRAuthBackupData();
+
+    const backupIDCheck =
+      latestBackupInfo.backupID === backupID && backupID === localBackupID;
+    const keysCheck =
+      backupDataKeyFromBackup === localBackupDataKey &&
+      backupLogDataKeyFromBackup === localBackupLogDataKey;
+
+    if (!backupIDCheck || !keysCheck) {
+      throw new Error(
+        '\n' +
+          `backupID: ${backupID}\n` +
+          `latestBackupInfo.backupID: ${latestBackupInfo.backupID}\n` +
+          `localBackupID: ${localBackupID}\n` +
+          `backupDataKeyFromBackup: ${backupDataKeyFromBackup}\n` +
+          `backupLogDataKeyFromBackup: ${backupLogDataKeyFromBackup}\n` +
+          `localBackupDataKey: ${localBackupDataKey}\n` +
+          `localBackupLogDataKey: ${localBackupLogDataKey}\n`,
+      );
+    }
+  }, [
+    getBackupSecret,
+    getBackupUserKeys,
+    latestBackupInfo?.backupID,
+    retrieveLatestBackupInfo,
+  ]);
+
   React.useEffect(() => {
     if (
       !staffCanSee ||
       !canPerformBackupOperation ||
       !handlerStarted ||
-      backupUploadInProgress.current ||
-      !!latestBackupInfo
+      backupUploadInProgress.current
     ) {
       return;
     }
@@ -90,6 +140,22 @@ function BackupHandler(): null {
       if (!isPrimaryDevice) {
         backupUploadInProgress.current = false;
         return;
+      }
+
+      if (latestBackupInfo) {
+        const timestamp = latestBackupInfo.timestamp;
+        // If last upload one less than 24h ago ignore it
+        if (timestamp >= Date.now() - millisecondsPerDay) {
+          return;
+        }
+
+        try {
+          await testUserKeysRestore();
+        } catch (err) {
+          const message = getMessageForException(err) ?? 'unknown error';
+          Alert.alert('Error restoring User Keys backup', message);
+          console.log('Error restoring User Keys backup:', message);
+        }
       }
 
       try {
@@ -111,6 +177,7 @@ function BackupHandler(): null {
     handlerStarted,
     latestBackupInfo,
     staffCanSee,
+    testUserKeysRestore,
   ]);
 
   return null;
