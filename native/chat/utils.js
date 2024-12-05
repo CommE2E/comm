0c0df6b501e11d88822b22f56965c7ae60fba0bb
Angelika Serwa <angelika.serwa@swmansion.com>
2024-12-05 01:04:10 +0100

    [native] Migrate threadColorOverride in useAnimatedMessageTooltipButton to Reanimate V2 API
    Summary:
    Before threadColorOverride was a reanimated node or null. It returned null if the message is not a text message or there is no transition.
     It contained a node with a block:
    - if progress is 1 then call `setThreadColorBrightness`
    - return interpolated color based on progress value
    By using `useDerivedValue` I achieved something similar. Now it’s a SharedValue that:
    - contains null if the message is not a text message or there is no transition
    - if progress is 1 then `setThreadColorBrightness` is called on JS thread
    - contains an interpolated color if there is a transition and it’s a text message
    So the behaviour is the same but now it returns `SharedValue<string | null>` instead of `Node | null`.
    
    `threadColorOverride` is used by `InnerTextMessage` component so I needed to migrate it too but it’s very straightforward, just add `useAnimatedStyle` instead of `useMemo` and use a shared value instead of a node
    
    Test Plan: Have a thread with a different thread color than the parent thread. Open up a tooltip on a text message and navigate to the child thread. Verify that the message background color is interpolated correctly
    
    Reviewers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D14120

--- native/chat/utils.js
+++ native/chat/utils.js
@@ -5,7 +5,10 @@ import * as React from 'react';
 import Animated, {
   type SharedValue,
   interpolate,
+  runOnJS,
   useAnimatedStyle,
+  useDerivedValue,
+  interpolateColor,
 } from 'react-native-reanimated';
 
 import { useLoggedInUserInfo } from 'lib/hooks/account-hooks.js';
@@ -42,17 +45,7 @@ import type {
 } from '../types/layout-types.js';
 import type { AnimatedViewStyle } from '../types/styles.js';
 
-const {
-  Node,
-  Extrapolate,
-  interpolateNode,
-  interpolateColors,
-  block,
-  call,
-  eq,
-  cond,
-  sub,
-} = Animated;
+const { Node, Extrapolate, interpolateNode, sub } = Animated;
 
 function textMessageItemHeight(
   item: ChatTextMessageInfoItemWithHeight,
@@ -190,12 +183,11 @@ function useAnimatedMessageTooltipButton({
   sourceMessage,
   initialCoordinates,
   messageListVerticalBounds,
-  progress,
   progressV2,
   targetInputBarHeight,
 }: AnimatedMessageArgs): {
   +style: AnimatedViewStyle,
-  +threadColorOverride: ?Node,
+  +threadColorOverride: SharedValue<string | null>,
   +isThreadColorDarkOverride: ?boolean,
 } {
   const chatContext = React.useContext(ChatContext);
@@ -261,26 +253,23 @@ function useAnimatedMessageTooltipButton({
     }
   }, [sourceMessage.threadInfo.color, targetColor]);
 
-  const threadColorOverride = React.useMemo(() => {
+  const threadColorOverride = useDerivedValue(() => {
     if (
       sourceMessage.messageShapeType !== 'text' ||
       !currentTransitionSidebarSourceID
     ) {
       return null;
     }
-    return block([
-      cond(eq(progress, 1), call([], setThreadColorBrightness)),
-      interpolateColors(progress, {
-        inputRange: [0, 1],
-        outputColorRange: [
-          `#${targetColor}`,
-          `#${sourceMessage.threadInfo.color}`,
-        ],
-      }),
-    ]);
+    if (progressV2.value === 1) {
+      runOnJS(setThreadColorBrightness)();
+    }
+    return interpolateColor(
+      progressV2.value,
+      [0, 1],
+      [`#${targetColor}`, `#${sourceMessage.threadInfo.color}`],
+    );
   }, [
     currentTransitionSidebarSourceID,
-    progress,
     setThreadColorBrightness,
     sourceMessage.messageShapeType,
     sourceMessage.threadInfo.color,
