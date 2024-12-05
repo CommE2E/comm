301b105247cebaeb1549cb3161d76b56dac5c8c9
Tomasz Palys <tomasz.palys@swmansion.com>
2024-12-05 18:00:55 +0100

    [native] Render SIWE restore panel
    Summary:
    Show a panel where users can restore their account using SIWE.
    
    https://linear.app/comm/issue/ENG-8203/implement-navigation-flow-for-new-siwe-log-in
    
    Depends on D14084
    
    Test Plan: Click the restore with SIWE button and check if the panel is shown correctly.
    
    Reviewers: kamil, angelika, bartek, ashoat
    
    Differential Revision: https://phab.comm.dev/D14085

--- native/account/restore-prompt-screen.react.js
+++ native/account/restore-prompt-screen.react.js
@@ -8,6 +8,8 @@ import RegistrationButtonContainer from './registration/registration-button-cont
 import RegistrationContainer from './registration/registration-container.react.js';
 import RegistrationContentContainer from './registration/registration-content-container.react.js';
 import type { SignInNavigationProp } from './sign-in-navigator.react';
+import { useSIWEPanelState } from './siwe-hooks.js';
+import SIWEPanel from './siwe-panel.react.js';
 import type { NavigationRoute } from '../navigation/route-names';
 import { RestorePasswordAccountScreenRouteName } from '../navigation/route-names.js';
 import { useStyles } from '../themes/colors.js';
@@ -18,6 +20,10 @@ type Props = {
   +route: NavigationRoute<'RestorePromptScreen'>,
 };
 
+const siweSignatureRequestData = {
+  messageType: 'msg_auth',
+};
+
 function RestorePromptScreen(props: Props): React.Node {
   const styles = useStyles(unboundStyles);
 
@@ -25,43 +31,67 @@ function RestorePromptScreen(props: Props): React.Node {
     props.navigation.navigate(RestorePasswordAccountScreenRouteName);
   }, [props.navigation]);
 
+  const {
+    panelState,
+    openPanel,
+    onPanelClosed,
+    onPanelClosing,
+    siwePanelSetLoading,
+  } = useSIWEPanelState();
+  let siwePanel;
+  if (panelState !== 'closed') {
+    siwePanel = (
+      <SIWEPanel
+        onClosing={onPanelClosing}
+        onClosed={onPanelClosed}
+        closing={panelState === 'closing'}
+        onSuccessfulWalletSignature={() => {}}
+        siweSignatureRequestData={siweSignatureRequestData}
+        setLoading={siwePanelSetLoading}
+      />
+    );
+  }
+
   return (
-    <RegistrationContainer>
-      <RegistrationContentContainer style={styles.scrollViewContentContainer}>
-        <Text style={styles.header}>Restore account</Text>
-        <Text style={styles.section}>
-          If you’ve lost access to your primary device, you can try recovering
-          your Comm account.
-        </Text>
-        <Text style={styles.section}>
-          To proceed, select the same login method that you used during
-          registration.
-        </Text>
-        <Text style={styles.section}>
-          Note that after completing the recovery flow, you will be logged out
-          from all of your other devices.
-        </Text>
-        <View style={styles.iconContainer}>
-          <RestoreIcon />
-        </View>
-      </RegistrationContentContainer>
-      <RegistrationButtonContainer>
-        <View style={styles.buttonContainer}>
-          <PromptButton
-            text="Restore with Ethereum"
-            onPress={() => {}}
-            variant="siwe"
-          />
-        </View>
-        <View style={styles.buttonContainer}>
-          <PromptButton
-            text="Restore with password"
-            onPress={openPasswordRestoreScreen}
-            variant="enabled"
-          />
-        </View>
-      </RegistrationButtonContainer>
-    </RegistrationContainer>
+    <>
+      <RegistrationContainer>
+        <RegistrationContentContainer style={styles.scrollViewContentContainer}>
+          <Text style={styles.header}>Restore account</Text>
+          <Text style={styles.section}>
+            If you’ve lost access to your primary device, you can try recovering
+            your Comm account.
+          </Text>
+          <Text style={styles.section}>
+            To proceed, select the same login method that you used during
+            registration.
+          </Text>
+          <Text style={styles.section}>
+            Note that after completing the recovery flow, you will be logged out
+            from all of your other devices.
+          </Text>
+          <View style={styles.iconContainer}>
+            <RestoreIcon />
+          </View>
+        </RegistrationContentContainer>
+        <RegistrationButtonContainer>
+          <View style={styles.buttonContainer}>
+            <PromptButton
+              text="Restore with Ethereum"
+              onPress={openPanel}
+              variant={panelState === 'opening' ? 'loading' : 'siwe'}
+            />
+          </View>
+          <View style={styles.buttonContainer}>
+            <PromptButton
+              text="Restore with password"
+              onPress={openPasswordRestoreScreen}
+              variant="enabled"
+            />
+          </View>
+        </RegistrationButtonContainer>
+      </RegistrationContainer>
+      {siwePanel}
+    </>
   );
 }
 
