3418cc2626ade91840e5606208dfec4aabcf927a
xsanm <kkurowski.kk@gmail.com>
2024-12-05 13:30:44 +0100

    [lib] add Identity Restore action type
    Summary:
    [ENG-8195](https://linear.app/comm/issue/ENG-8195/implement-new-log-in-for-usernamepassword-users).
    
    Action type to restore, similar to other Identity-auth actions.
    
    I went through all places where `identityLogInActionTypes` and `identityRegisterActionTypes` are reduced to make sure `identityRestoreActionTypes` is handled in a similar way
    
    Depends on D14080
    
    Test Plan: Flow, logic tested later in the stack
    
    Reviewers: bartek, tomek, varun
    
    Reviewed By: bartek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D14081

--- web/redux/services-access-token-reducer.js
+++ web/redux/services-access-token-reducer.js
@@ -3,6 +3,7 @@
 import {
   identityLogInActionTypes,
   identityRegisterActionTypes,
+  restoreUserActionTypes,
 } from 'lib/actions/user-actions.js';
 
 import type { Action } from './redux-setup.js';
@@ -10,6 +11,7 @@ import type { Action } from './redux-setup.js';
 function reduceServicesAccessToken(state: ?string, action: Action): ?string {
   if (
     action.type === identityLogInActionTypes.success ||
+    action.type === restoreUserActionTypes.success ||
     action.type === identityRegisterActionTypes.success
   ) {
     return action.payload.accessToken;
