5811d1942814f93bbaa98077cee25873dd1ed0b5
xsanm <kkurowski.kk@gmail.com>
2024-12-11 16:55:17 +0100

    [native] implement screen when scanning QR code a non-primary device
    Summary:
    [ENG-9959](https://linear.app/comm/issue/ENG-9959/improve-scanning-qr-code-flow).
    
    Copy is taken from [ENG-9936](https://linear.app/comm/issue/ENG-9936/show-a-specific-error-when-scanning-a-qr-code-on-a-secondary-device)
    
    Styles are taken from similar register screens, and there is a chance that those will duplicate with other QR Auth screens - if so I am going to refactor this but for now I prefer to keep this like this if there are any changes needed specific to a screen.
    
    Also, after this is done maybe worth renaming the `<Registration*` components to `<Auth*` components to avoid confusion.
    
    In [ENG-9968](https://linear.app/comm/issue/ENG-9968/final-testing-task) there is an example of what the flow looks like.
    
    {F3446213}
    
    Depends on D14105
    
    Test Plan: Navigate to screen
    
    Reviewers: ashoat, tomek
    
    Reviewed By: ashoat
    
    Differential Revision: https://phab.comm.dev/D14114

---
+++ native/account/qr-auth/qr-auth-not-primary-device.react.js
@@ -0,0 +1,80 @@
+// @flow
+
+import type {
+  StackNavigationEventMap,
+  StackNavigationState,
+  StackOptions,
+} from '@react-navigation/core';
+import * as React from 'react';
+import { Text } from 'react-native';
+
+import { type QRAuthNavigationProp } from './qr-auth-navigator.react.js';
+import RegistrationButtonContainer from '../../account/registration/registration-button-container.react.js';
+import RegistrationContainer from '../../account/registration/registration-container.react.js';
+import RegistrationContentContainer from '../../account/registration/registration-content-container.react.js';
+import PrimaryButton from '../../components/primary-button.react.js';
+import type { RootNavigationProp } from '../../navigation/root-navigator.react.js';
+import {
+  type NavigationRoute,
+  type ScreenParamList,
+} from '../../navigation/route-names.js';
+import { useStyles } from '../../themes/colors.js';
+
+type Props = {
+  +navigation: QRAuthNavigationProp<'QRAuthNotPrimaryDevice'>,
+  +route: NavigationRoute<'QRAuthNotPrimaryDevice'>,
+};
+
+function QRAuthNotPrimaryDevice(prop: Props): React.Node {
+  const { navigation } = prop;
+  const styles = useStyles(unboundStyles);
+
+  const onPressGoBack = React.useCallback(() => {
+    navigation
+      .getParent<
+        ScreenParamList,
+        'LinkedDevicesBottomSheet',
+        StackNavigationState,
+        StackOptions,
+        StackNavigationEventMap,
+        RootNavigationProp<'LinkedDevicesBottomSheet'>,
+      >()
+      ?.goBack();
+  }, [navigation]);
+
+  return (
+    <RegistrationContainer>
+      <RegistrationContentContainer>
+        <Text style={styles.header}>Device not primary</Text>
+        <Text style={styles.body}>
+          This mobile device is not your primary device, and cannot be used to
+          authorize new devices. Please try your other mobile device(s).
+        </Text>
+      </RegistrationContentContainer>
+      <RegistrationButtonContainer>
+        <PrimaryButton
+          onPress={onPressGoBack}
+          label="Go back"
+          variant="enabled"
+        />
+      </RegistrationButtonContainer>
+    </RegistrationContainer>
+  );
+}
+
+const unboundStyles = {
+  header: {
+    fontSize: 24,
+    color: 'panelForegroundLabel',
+    paddingBottom: 16,
+  },
+  body: {
+    fontFamily: 'Arial',
+    fontSize: 15,
+    lineHeight: 20,
+    color: 'panelForegroundSecondaryLabel',
+    paddingBottom: 16,
+  },
+};
+
+export default QRAuthNotPrimaryDevice;
