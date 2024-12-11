8a23992d6ba222953054ab2c64c476b9932cbdbd
xsanm <kkurowski.kk@gmail.com>
2024-12-11 13:30:31 +0100

    [native] make sure `start_backup_handler` is called only once in `BackupHandler`
    Summary:
    [ENG-10001](https://linear.app/comm/issue/ENG-10001/user-sometimes-gets-logged-out-just-after-logging-in).
    
    The asynchronous nature of this effect caused this promise to be spawned a couple of times, resulting in the starting handler multiple times.
    
    Depends on D14095
    
    Test Plan:
    Login -> logout -> login = no issues about backup upload, user is not logged out automatically.
    
    Added some logs to make sure there is only one call to `startBackupHandler`
    
    Reviewers: bartek, tomek
    
    Reviewed By: bartek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D14096

--- native/backup/backup-handler.js
+++ native/backup/backup-handler.js
@@ -35,16 +35,19 @@ function BackupHandler(): null {
     useClientBackup();
   const getBackupSecret = useGetBackupSecretForLoggedInUser();
   const backupUploadInProgress = React.useRef<boolean>(false);
+  const startingBackupHandlerInProgress = React.useRef<boolean>(false);
   const [handlerStarted, setHandlerStarted] = React.useState(false);
 
   React.useEffect(() => {
-    if (!staffCanSee) {
+    if (!staffCanSee || startingBackupHandlerInProgress.current) {
       return;
     }
 
     void (async () => {
+      startingBackupHandlerInProgress.current = true;
       const isPrimaryDevice = await checkIfPrimaryDevice();
       if (!isPrimaryDevice) {
+        startingBackupHandlerInProgress.current = false;
         return;
       }
 
@@ -69,6 +72,7 @@ function BackupHandler(): null {
           console.log('Error stopping backup handler:', message);
         }
       }
+      startingBackupHandlerInProgress.current = false;
     })();
   }, [
     canPerformBackupOperation,
