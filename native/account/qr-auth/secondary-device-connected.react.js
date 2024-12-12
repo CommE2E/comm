ac13fccad6ae71ec8e8631d2830c44d1aa2a0efb
xsanm <kkurowski.kk@gmail.com>
2024-12-12 19:23:48 +0100

    [native] implement screen after secondary device is connected
    Summary:
    [ENG-9959](https://linear.app/comm/issue/ENG-9959/improve-scanning-qr-code-flow).
    
    Final screen when adding secondary device succeded. There is no option to go back, because it could be strange for the user to back on connecting screen.
    
    In [ENG-9968](https://linear.app/comm/issue/ENG-9968/final-testing-task) there is example of how this looks like
    
    Depends on D14116
    
    Test Plan: Navigate to screen
    
    Reviewers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D14149

---
+++ native/account/qr-auth/secondary-device-connected.react.js
@@ -0,0 +1,73 @@
+// @flow
+
+import type {
+  StackNavigationEventMap,
+  StackNavigationState,
+  StackOptions,
+} from '@react-navigation/core';
+import * as React from 'react';
+import { Text } from 'react-native';
+
+import type { QRAuthNavigationProp } from './qr-auth-navigator.react.js';
+import RegistrationButtonContainer from '../../account/registration/registration-button-container.react.js';
+import RegistrationContainer from '../../account/registration/registration-container.react.js';
+import RegistrationContentContainer from '../../account/registration/registration-content-container.react.js';
+import PrimaryButton from '../../components/primary-button.react.js';
+import type { RootNavigationProp } from '../../navigation/root-navigator.react.js';
+import {
+  type NavigationRoute,
+  type ScreenParamList,
+} from '../../navigation/route-names.js';
+import { useStyles } from '../../themes/colors.js';
+
+type Props = {
+  +navigation: QRAuthNavigationProp<'SecondaryDeviceConnected'>,
+  +route: NavigationRoute<'SecondaryDeviceConnected'>,
+};
+
+function SecondaryDeviceConnected(prop: Props): React.Node {
+  const { navigation } = prop;
+  const styles = useStyles(unboundStyles);
+
+  const onPress = React.useCallback(() => {
+    navigation
+      .getParent<
+        ScreenParamList,
+        'LinkedDevicesBottomSheet',
+        StackNavigationState,
+        StackOptions,
+        StackNavigationEventMap,
+        RootNavigationProp<'LinkedDevicesBottomSheet'>,
+      >()
+      ?.goBack();
+  }, [navigation]);
+
+  return (
+    <RegistrationContainer>
+      <RegistrationContentContainer>
+        <Text style={styles.header}>Device added</Text>
+        <Text style={styles.body}>Device registered successfully</Text>
+      </RegistrationContentContainer>
+      <RegistrationButtonContainer>
+        <PrimaryButton onPress={onPress} label="OK" variant="enabled" />
+      </RegistrationButtonContainer>
+    </RegistrationContainer>
+  );
+}
+
+const unboundStyles = {
+  header: {
+    fontSize: 24,
+    color: 'panelForegroundLabel',
+    paddingBottom: 16,
+  },
+  body: {
+    fontFamily: 'Arial',
+    fontSize: 15,
+    lineHeight: 20,
+    color: 'panelForegroundSecondaryLabel',
+    paddingBottom: 16,
+  },
+};
+
+export default SecondaryDeviceConnected;
