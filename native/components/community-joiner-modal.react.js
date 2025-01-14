96f159b5d3a1fb6730797af86d0f7719434930bc
Varun Dhananjaya <varun.dhananjaya@gmail.com>
2025-01-14 17:30:46 -0500

    [native] fix avatars in directory
    Summary:
    Resolves https://linear.app/comm/issue/ENG-9834/get-farcaster-thread-avatars-to-render-correctly-in-new-modals
    
    We prop drill the farcasterChannelID to the ThreadAvatar component because the corresponding community will likely not be in the user's community store
    
    Depends on D14181
    
    Test Plan: thread avatars now display in directory for communities the user hasn't yet joined
    
    Reviewers: ashoat
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D14204

--- native/components/community-joiner-modal.react.js
+++ native/components/community-joiner-modal.react.js
@@ -65,30 +65,45 @@ function CommunityJoinerModal(props: Props): React.Node {
     [communities],
   );
 
-  const generateThreadInfos = React.useCallback(
+  const generateThreadInfosAndFCChannelIDs = React.useCallback(
     (communityList: $ReadOnlyArray<ClientCommunityInfoWithCommunityName>) =>
       communityList
-        .map(community =>
-          community.threadInfo
-            ? threadInfoFromRawThreadInfo(
-                community.threadInfo,
-                viewerID,
-                userInfos,
-              )
-            : null,
-        )
+        .map(community => {
+          const { farcasterChannelID, threadInfo } = community;
+          if (!threadInfo) {
+            return null;
+          }
+          return {
+            threadInfo: threadInfoFromRawThreadInfo(
+              threadInfo,
+              viewerID,
+              userInfos,
+            ),
+            farcasterChannelID,
+          };
+        })
         .filter(Boolean),
     [userInfos, viewerID],
   );
 
+  const generalThreadInfosAndFCChannelIDs = React.useMemo(
+    () => generateThreadInfosAndFCChannelIDs(generalCommunities),
+    [generateThreadInfosAndFCChannelIDs, generalCommunities],
+  );
+
+  const cryptoThreadInfosAndFCChannelIDs = React.useMemo(
+    () => generateThreadInfosAndFCChannelIDs(cryptoCommunities),
+    [generateThreadInfosAndFCChannelIDs, cryptoCommunities],
+  );
+
   const generalThreadInfos = React.useMemo(
-    () => generateThreadInfos(generalCommunities),
-    [generateThreadInfos, generalCommunities],
+    () => generalThreadInfosAndFCChannelIDs.map(item => item.threadInfo),
+    [generalThreadInfosAndFCChannelIDs],
   );
 
   const cryptoThreadInfos = React.useMemo(
-    () => generateThreadInfos(cryptoCommunities),
-    [generateThreadInfos, cryptoCommunities],
+    () => cryptoThreadInfosAndFCChannelIDs.map(item => item.threadInfo),
+    [cryptoThreadInfosAndFCChannelIDs],
   );
 
   const generalIndex = useThreadSearchIndex(generalThreadInfos);
@@ -97,23 +112,23 @@ function CommunityJoinerModal(props: Props): React.Node {
   const renderGeneralTab = React.useCallback(
     () => (
       <CommunityList
-        threadInfos={generalThreadInfos}
+        threadInfosAndFCChannelIDs={generalThreadInfosAndFCChannelIDs}
         itemStyle={styles.threadListItem}
         searchIndex={generalIndex}
       />
     ),
-    [generalIndex, generalThreadInfos, styles.threadListItem],
+    [generalIndex, generalThreadInfosAndFCChannelIDs, styles.threadListItem],
   );
 
   const renderCryptoTab = React.useCallback(
     () => (
       <CommunityList
-        threadInfos={cryptoThreadInfos}
+        threadInfosAndFCChannelIDs={cryptoThreadInfosAndFCChannelIDs}
         itemStyle={styles.threadListItem}
         searchIndex={cryptoIndex}
       />
     ),
-    [cryptoIndex, cryptoThreadInfos, styles.threadListItem],
+    [cryptoIndex, cryptoThreadInfosAndFCChannelIDs, styles.threadListItem],
   );
 
   const [index, setIndex] = React.useState(0);
