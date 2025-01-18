e87482e507c5b1480248fa4936a99fcdfa691eeb
Angelika Serwa <angelika.serwa@swmansion.com>
2025-01-18 01:26:42 +0100

    [native] Migrate containerStyle in CameraModal to functional component
    Summary:
    https://linear.app/comm/issue/ENG-8152/convert-cameramodal-to-reanimated-2-syntax
    
    A simple migration to `useAnimatedStyle`
    
    Depends on D14238
    
    Test Plan: Verify opening and closing camera modal works.
    
    Reviewers: tomek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D14239

--- native/media/camera-modal.react.js
+++ native/media/camera-modal.react.js
@@ -55,7 +55,7 @@ import { useSelector } from '../redux/redux-utils.js';
 import { colors } from '../themes/colors.js';
 import { type DeviceCameraInfo } from '../types/camera.js';
 import type { NativeMethods } from '../types/react-native.js';
-import { type ViewStyle, type AnimatedViewStyle } from '../types/styles.js';
+import { type ViewStyle } from '../types/styles.js';
 import { clampV2 } from '../utils/animation-utils.js';
 
 const maxZoom = 16;
@@ -178,20 +178,10 @@ type Props = {
   +overlayStyle: ViewStyle,
   +sendButtonProgress: Animated.Value,
   +sendButtonStyle: ViewStyle,
+  +containerStyle: ViewStyle,
 };
 
 class CameraModal extends React.PureComponent<Props> {
-  get containerStyle(): AnimatedViewStyle {
-    const { overlayContext } = this.props;
-    if (!overlayContext) {
-      return styles.container;
-    }
-    return {
-      ...styles.container,
-      opacity: overlayContext.position,
-    };
-  }
-
   renderCamera = ({
     camera,
     status,
@@ -334,7 +324,7 @@ class CameraModal extends React.PureComponent<Props> {
       ? RNCamera.Constants.Type.front
       : RNCamera.Constants.Type.back;
     return (
-      <Reanimated.View style={this.containerStyle}>
+      <Reanimated.View style={this.props.containerStyle}>
         {statusBar}
         <RNCamera
           type={type}
@@ -1054,6 +1044,20 @@ const ConnectedCameraModal: React.ComponentType<BaseProps> =
       prevPendingPhotoCapture.current = pendingPhotoCapture;
     }, [pendingPhotoCapture]);
 
+    const containerAnimatedStyle = useAnimatedStyle(
+      () => ({
+        opacity: overlayContext?.positionV2?.value,
+      }),
+      [overlayContext],
+    );
+
+    const containerStyle = React.useMemo(() => {
+      if (!overlayContext) {
+        return styles.container;
+      }
+      return [styles.container, containerAnimatedStyle];
+    }, [containerAnimatedStyle, overlayContext]);
+
     return (
       <CameraModal
         {...props}
@@ -1099,6 +1103,7 @@ const ConnectedCameraModal: React.ComponentType<BaseProps> =
         overlayStyle={overlayStyle}
         sendButtonProgress={sendButtonProgress.current}
         sendButtonStyle={sendButtonStyle}
+        containerStyle={containerStyle}
       />
     );
   });
