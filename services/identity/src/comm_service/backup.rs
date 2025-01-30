15135e36afff8b37999f0e91036792a944cba3a7
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2025-01-30 09:58:32 +0100

    [identity] Backup client method for preparing user keys
    Summary:
    TBD
    
    Depends on D14276
    
    Test Plan: TBD
    
    Reviewers: kamil, tomek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D14277

--- services/identity/src/comm_service/backup.rs
+++ services/identity/src/comm_service/backup.rs
@@ -1,4 +1,7 @@
-use comm_lib::auth::AuthService;
+use comm_lib::{auth::AuthService, backup::database::BackupItem};
+use hex::ToHex;
+use reqwest::multipart::Part;
+use sha2::{Digest, Sha256};
 
 use crate::{
   config::CONFIG,
@@ -81,4 +84,71 @@ pub async fn user_has_backup(
       )
     }
   }
+}
+
+pub async fn prepare_user_keys(
+  auth_service: &AuthService,
+  user_id: &str,
+  backup_id: &str,
+  user_keys: Vec<u8>,
+  siwe_backup_msg: Option<String>,
+) -> Result<BackupItem, crate::error::Error> {
+  let path = "/utils/prepare_user_keys";
+  let url = CONFIG
+    .backup_service_url
+    .join(path)
+    .expect("failed to construct backup service URL");
+
+  let services_token = auth_service
+    .get_services_token()
+    .await
+    .map_err(|err| {
+      tracing::error!(
+        errorType = error_types::HTTP_LOG,
+        "Failed to retrieve service-to-service token: {err:?}",
+      );
+      tonic::Status::aborted(tonic_status_messages::UNEXPECTED_ERROR)
+    })?
+    .as_authorization_token()?;
+
+  let client = reqwest::Client::builder().build()?;
+  let mut form = reqwest::multipart::Form::new()
+    .text("user_id", user_id.to_string())
+    .text("backup_id", backup_id.to_string())
+    .text(
+      "user_keys_hash",
+      Sha256::digest(&user_keys).encode_hex::<String>(),
+    )
+    .part("user_keys", Part::bytes(user_keys));
+
+  if let Some(siwe_backup_msg_value) = siwe_backup_msg {
+    form = form.text("siwe_backup_msg", siwe_backup_msg_value);
+  }
+
+  let response = client
+    .post(url)
+    .bearer_auth(services_token)
+    .multipart(form)
+    .send()
+    .await?;
+
+  if let Err(err) = response.error_for_status_ref() {
+    let response_status = response.status();
+    let response_body = response
+      .text()
+      .await
+      .unwrap_or("[failed to get response text]".to_string());
+    tracing::error!(
+      errorType = error_types::HTTP_LOG,
+      "Backup service failed to prepare user keys: {} - {}",
+      response_status,
+      response_body,
+    );
+
+    return Err(err.into());
+  }
+
+  let backup_item: BackupItem = response.json().await?;
+
+  Ok(backup_item)
 }
