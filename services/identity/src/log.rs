0c29dbf3474a3e8154998f215354cb04f4ef9ce7
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2025-02-13 09:01:05 +0100

    [identity] Skip redacting specific values
    Summary:
    Part of [[ https://linear.app/comm/issue/ENG-10178/improve-logging-for-identity-and-backup | ENG-10178 ]].
    We [[ https://linear.app/comm/issue/ENG-10170/ashoat-is-locked-out-of-ios-in-production#comment-274e9177 | agreed ]] to omit this value when redacting sensitive data.
    
    Depends on D14369
    
    Test Plan: None
    
    Reviewers: kamil, tomek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D14370

--- services/identity/src/log.rs
+++ services/identity/src/log.rs
@@ -1,6 +1,13 @@
 use crate::config::CONFIG;
 
 pub fn redact_sensitive_data(sensitive_data: &str) -> &str {
+  const ALLOWED_VALUES: [&str; 1] =
+    [crate::constants::staff::AUTHORITATIVE_KEYSERVER_OWNER_USER_ID];
+
+  if ALLOWED_VALUES.contains(&sensitive_data) {
+    return sensitive_data;
+  }
+
   if CONFIG.redact_sensitive_data {
     "REDACTED"
   } else {
