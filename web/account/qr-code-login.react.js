680b5f5fa416aa5b9e4ed4a4d9e30792ede25c3e
Tomasz Palys <tomasz.palys@swmansion.com>
2025-02-17 14:32:40 +0100

    [web] Extract the common logic from the QR auth
    Summary:
    The same logic is used in two places, which means we can extract it. We can't simply share the logic with native, where a `focusEffect` is used, but this isn't a big issue because native isn't affected by the bug we're fixing.
    
    https://linear.app/comm/issue/ENG-10200/secondary-logout-in-signed-device-list-is-not-working-on-web
    
    Test Plan: Test if logging in using the QR code works.
    
    Reviewers: kamil, bartek
    
    Reviewed By: kamil
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D14375

--- web/account/qr-code-login.react.js
+++ web/account/qr-code-login.react.js
@@ -3,33 +3,13 @@
 import { QRCodeSVG } from 'qrcode.react';
 import * as React from 'react';
 
-import { useSecondaryDeviceQRAuthContext } from 'lib/components/secondary-device-qr-auth-context-provider.react.js';
-import { qrCodeLinkURL } from 'lib/facts/links.js';
-import { platformToIdentityDeviceType } from 'lib/types/identity-service-types.js';
-import { getConfig } from 'lib/utils/config.js';
+import { useSecondaryDeviceQRAuthURL } from 'lib/components/secondary-device-qr-auth-context-provider.react.js';
 
 import css from './qr-code-login.css';
 import LoadingIndicator from '../loading-indicator.react.js';
 
 function QRCodeLogin(): React.Node {
-  const { qrData, openSecondaryQRAuth, closeSecondaryQRAuth } =
-    useSecondaryDeviceQRAuthContext();
-
-  React.useEffect(() => {
-    void openSecondaryQRAuth();
-    return closeSecondaryQRAuth;
-  }, [closeSecondaryQRAuth, openSecondaryQRAuth]);
-
-  const { platform } = getConfig().platformDetails;
-  const qrCodeURL = React.useMemo(() => {
-    if (!qrData) {
-      return undefined;
-    }
-
-    const identityDeviceType = platformToIdentityDeviceType[platform];
-
-    return qrCodeLinkURL(qrData.aesKey, qrData.deviceID, identityDeviceType);
-  }, [platform, qrData]);
+  const qrCodeURL = useSecondaryDeviceQRAuthURL();
 
   const qrCodeComponent = React.useMemo(() => {
     if (qrCodeURL) {
