2bee4fdd7c9693266e73636ee86f18260d69c629
Angelika Serwa <angelika.serwa@swmansion.com>
2025-02-18 14:38:43 +0100

    [web] Add colon in qr code login copy
    Summary:
    Add colon according to this comment: https://phab.comm.dev/D14368#inline-79925
    
    Test Plan:
    open qr code login page  and verify the colon is here
    
    Reviewers:
    tomek
    
    Subscribers:

--- web/account/qr-code-login.react.js
+++ web/account/qr-code-login.react.js
@@ -34,7 +34,7 @@ function QRCodeLogin(): React.Node {
     <div className={css.qrContainer}>
       <div className={css.title}>Log in to Comm</div>
       <div className={css.scanInstructions}>
-        Open the Comm app on your phone and scan the QR code below
+        Open the Comm app on your phone and scan the QR code below:
       </div>
       <div className={css.qrCodeContainer}>
         <QRCodeSVG value={qrCodeURL} size={300} level="L" />
