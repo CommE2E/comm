b6b12e3d0ae3c284963db2348e260dc0e3eca564
Angelika Serwa <angelika.serwa@swmansion.com>
2025-02-22 05:43:50 +0100

    [native] Remove position from overlay navigator
    Summary:
    https://linear.app/comm/issue/ENG-10006/rename-positionv2-variables-in-overlaynavigator-to-position
    
    The position variable in OverlayNavigator is deprecated and no longer used anywhere, so we can delete it.
    
    Test Plan: flow & run the app
    
    Reviewers: tomek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D14406

--- native/navigation/overlay-context.js
+++ native/navigation/overlay-context.js
@@ -2,13 +2,10 @@
 
 import * as React from 'react';
 import type { SharedValue } from 'react-native-reanimated';
-import Animated from 'react-native-reanimated';
 
 export type VisibleOverlay = {
   +routeKey: string,
   +routeName: string,
-  // position is deprecated - use positionV2 and Reanimated V2 API
-  +position: ?Animated.Value,
   +positionV2: ?SharedValue<number>,
   +shouldRenderScreenContent: boolean,
   +onExitFinish?: () => void,
@@ -19,8 +16,6 @@ export type ScrollBlockingModalStatus = 'open' | 'closed' | 'closing';
 
 export type OverlayContextType = {
   // position and isDismissing are local to the current route
-  // position is deprecated - use positionV2 and Reanimated V2 API
-  +position: ?Animated.Node,
   +positionV2: ?SharedValue<number>,
   +shouldRenderScreenContent: boolean,
   +onExitFinish?: () => void,
