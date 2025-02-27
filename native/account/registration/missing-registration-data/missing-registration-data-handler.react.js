d0bfc7bcdddffbf585ecc69675eceb3490e0078f
Tomasz Palys <tomasz.palys@swmansion.com>
2025-02-27 15:16:42 +0100

    [native] Avoid running the handler multiple times
    Summary:
    The effect contains some async logic that is run based on a condition that is later modified in the effect. This means that it is possible to run this effect multiple times before the condition is noticed to be true (without this change the logic runs usually 4 times).
    
    https://linear.app/comm/issue/ENG-10271/generating-backups-for-siwe-users-who-used-v1-fallback-doesnt-work
    
    Test Plan: Check if the logic is run just once, by adding some console logs. This code can be run when a SIWE user doesn't have a backup.
    
    Reviewers: kamil, bartek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D14416

--- native/account/registration/missing-registration-data/missing-registration-data-handler.react.js
+++ native/account/registration/missing-registration-data/missing-registration-data-handler.react.js
@@ -36,12 +36,17 @@ function MissingRegistrationDataHandler(): React.Node {
   );
   const dispatch = useDispatch();
 
+  const lastAlertInfo = React.useRef(createSIWEBAckupMessageAlertInfo);
+  React.useEffect(() => {
+    lastAlertInfo.current = createSIWEBAckupMessageAlertInfo;
+  }, [createSIWEBAckupMessageAlertInfo]);
+
   React.useEffect(() => {
     if (
       !loggedIn ||
       accountHasPassword(currentUserInfo) ||
       cachedSelections.siweBackupSecrets ||
-      shouldSkipCreateSIWEBackupMessageAlert(createSIWEBAckupMessageAlertInfo)
+      shouldSkipCreateSIWEBackupMessageAlert(lastAlertInfo.current)
     ) {
       return;
     }
@@ -55,7 +60,10 @@ function MissingRegistrationDataHandler(): React.Node {
       const nativeSIWEBackupSecrets =
         await commCoreModule.getSIWEBackupSecrets();
 
-      if (nativeSIWEBackupSecrets) {
+      if (
+        nativeSIWEBackupSecrets ||
+        shouldSkipCreateSIWEBackupMessageAlert(lastAlertInfo.current)
+      ) {
         return;
       }
 
@@ -67,6 +75,11 @@ function MissingRegistrationDataHandler(): React.Node {
         alertType: alertTypes.SIWE_BACKUP_MESSAGE,
         time: Date.now(),
       };
+      lastAlertInfo.current = {
+        alertType: alertTypes.SIWE_BACKUP_MESSAGE,
+        totalAlerts: lastAlertInfo.current + 1,
+        lastAlertTime: payload.time,
+      };
 
       dispatch({
         type: recordAlertActionType,
@@ -76,7 +89,6 @@ function MissingRegistrationDataHandler(): React.Node {
   }, [
     cachedSelections.siweBackupSecrets,
     checkIfPrimaryDevice,
-    createSIWEBAckupMessageAlertInfo,
     currentUserInfo,
     dispatch,
     loggedIn,
