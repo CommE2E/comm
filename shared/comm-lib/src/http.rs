8fd5dea490253acb5c968b8b9d89b7ed1c791902
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2025-03-06 10:13:42 +0100

    [comm-lib] Read CORS allow-origin from env
    Summary:
    [[ https://linear.app/comm/issue/ENG-10297/make-cors-configurable-from-env-for-http-services | ENG-10297 ]]
    I had this implemented some time ago and stashed, publishing a diff.
    
    Test Plan: I tested this on staging with my personal domain - added it to CORS via Terraform
    
    Reviewers: kamil
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D15351

--- shared/comm-lib/src/http.rs
+++ shared/comm-lib/src/http.rs
@@ -2,10 +2,23 @@ pub mod auth;
 pub mod auth_service;
 pub mod multipart;
 
+use std::collections::HashSet;
+
 use crate::tools::BoxedError;
 use actix_cors::Cors;
 use actix_web::web::Bytes;
 use futures_core::Stream;
+
+use once_cell::sync::Lazy;
+
+static ALLOWED_ORIGINS: Lazy<HashSet<String>> = Lazy::new(|| {
+  std::env::var("ALLOW_ORIGIN_LIST")
+    .unwrap_or_default()
+    .split(',')
+    .map(|it| it.trim().to_string())
+    .filter(|it| !it.is_empty())
+    .collect()
+});
 
 pub fn cors_config(is_sandbox: bool) -> Cors {
   // For local development, use relaxed CORS config
@@ -18,6 +31,10 @@ pub fn cors_config(is_sandbox: bool) -> Cors {
   Cors::default()
     .allowed_origin("https://web.comm.app")
     .allowed_origin("https://comm.software")
+    .allowed_origin_fn(|origin_header, _| match origin_header.to_str() {
+      Ok(origin) => ALLOWED_ORIGINS.contains(origin),
+      _ => false,
+    })
     // for local development using prod service
     .allowed_origin("http://localhost:3000")
     .allowed_methods(vec!["GET", "POST", "PUT", "DELETE", "OPTIONS"])
