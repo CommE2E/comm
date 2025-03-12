c64eaf40f230f6622abf638cd171a22fe9b33931
Angelika Serwa <angelika.serwa@swmansion.com>
2025-03-12 13:34:34 +0100

    [native] Add MediaModule expo module
    Summary: https://linear.app/comm/issue/ENG-10314/migrate-comm-app-to-use-new-media-module
    
    Test Plan: Build the app
    
    Reviewers: bartek
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D14468

---
+++ native/expo-modules/comm-expo-package/android/src/main/java/app/comm/android/media/MediaModule.kt
@@ -0,0 +1,18 @@
+package app.comm.android.media
+
+import expo.modules.kotlin.modules.Module
+import expo.modules.kotlin.modules.ModuleDefinition
+
+class MediaModule : Module() {
+  override fun definition() = ModuleDefinition {
+    Name("MediaModule")
+  }
+
+  // region Function implementations
+
+
+  // endregion
+}
+// region Exception definitions
+
+// endregion
