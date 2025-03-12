45ba7ca20f613e6ec1124ea127de28c0e45f8c10
Angelika Serwa <angelika.serwa@swmansion.com>
2025-03-12 13:34:34 +0100

    [native] Add MediaModule expo module

---
+++ native/expo-modules/comm-expo-package/ios/MediaModule.swift
@@ -0,0 +1,13 @@
+import ExpoModulesCore
+
+public class MediaModule: Module {
+  public func definition() -> ModuleDefinition {
+    Name("MediaModule")
+  }
+}
+
+// MARK: - Function implementations
+
+
+// MARK: - Exception definitions
+
