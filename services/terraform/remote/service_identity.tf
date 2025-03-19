ecd049135f1e7db43e8da4bfb2924d64bc6ae048
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2025-03-19 15:23:49 +0100

    [terraform] Bump staging Identity to 0.51.2
    Summary:
    
    Built image with prekey verification to test for a few days on staging before publishing to prod
    
    Test Plan:
    
    terraform plan
    
    Reviewers: kamil, tomek
    
    Subscribers:

--- services/terraform/remote/service_identity.tf
+++ services/terraform/remote/service_identity.tf
@@ -1,5 +1,5 @@
 locals {
-  identity_service_image_tag      = local.is_staging ? "0.51.1" : "0.51.1"
+  identity_service_image_tag      = local.is_staging ? "0.51.2" : "0.51.1"
   identity_service_server_image   = "commapp/identity-server:${local.identity_service_image_tag}"
   identity_service_container_name = "identity-server"
 
