79eabafa2fa65ed862977cd54974bc102e590b75
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2025-03-20 11:45:00 +0100

    [grpc-clients] Add helpers for services grpc client
    Summary:
    
    Added helpers to instantiate Identity auth client with service-to-service token, similiar to `create_auth_client()` for CSAT requests
    
    Depends on D14465
    
    Test Plan:
    
    cargo build. Client is used in the next diff
    
    Reviewers: kamil
    
    Subscribers:

--- shared/grpc_clients/src/identity/authenticated.rs
+++ shared/grpc_clients/src/identity/authenticated.rs
@@ -19,6 +19,10 @@ pub struct AuthLayer {
   access_token: String,
 }
 
+pub struct ServicesAuthLayer {
+  services_token: String,
+}
+
 impl ToMetadataValueAscii for str {
   fn parse_to_ascii(&self) -> Result<MetadataValue<Ascii>, Status> {
     self.parse().map_err(|_: InvalidMetadataValue| {
@@ -38,8 +42,24 @@ impl Interceptor for AuthLayer {
   }
 }
 
+impl Interceptor for ServicesAuthLayer {
+  fn call(&mut self, mut request: Request<()>) -> Result<Request<()>, Status> {
+    request
+      .metadata_mut()
+      .insert("services_token", self.services_token.parse_to_ascii()?);
+
+    Ok(request)
+  }
+}
+
 pub type ChainedInterceptedAuthClient = AuthClient<
   InterceptedService<Channel, ChainedInterceptor<AuthLayer, CodeVersionLayer>>,
+>;
+pub type ChainedInterceptedServicesAuthClient = AuthClient<
+  InterceptedService<
+    Channel,
+    ChainedInterceptor<ServicesAuthLayer, CodeVersionLayer>,
+  >,
 >;
 
 pub async fn get_auth_client(
@@ -58,6 +78,27 @@ pub async fn get_auth_client(
     device_id,
     access_token,
   };
+
+  let version_interceptor = CodeVersionLayer::from(platform_metadata);
+
+  let chained = ChainedInterceptor {
+    first: auth_interceptor,
+    second: version_interceptor,
+  };
+
+  Ok(AuthClient::with_interceptor(channel, chained))
+}
+
+pub async fn get_services_auth_client(
+  url: &str,
+  services_token: String,
+  platform_metadata: PlatformMetadata,
+) -> Result<ChainedInterceptedServicesAuthClient, Error> {
+  use crate::get_grpc_service_channel;
+
+  let channel = get_grpc_service_channel(url).await?;
+
+  let auth_interceptor = ServicesAuthLayer { services_token };
 
   let version_interceptor = CodeVersionLayer::from(platform_metadata);
 
