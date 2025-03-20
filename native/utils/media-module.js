085ad9283c285de875c9bc707cfb216f06e5f778
Angelika Serwa <angelika.serwa@swmansion.com>
2025-03-20 10:36:36 +0100

    [native] Use transcodeVideo() from MediaModule instead of ffmpeg
    Summary:
    https://linear.app/comm/issue/ENG-10314/migrate-comm-app-to-use-new-media-module
    
    Depends on D14479
    
    Test Plan: Send some videos on some devices and verify they're transcoded correctly
    
    Reviewers: bartek
    
    Reviewed By: bartek
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D14480

--- native/utils/media-module.js
+++ native/utils/media-module.js
@@ -1,6 +1,10 @@
 // @flow
 
-import { requireNativeModule } from 'expo-modules-core';
+import {
+  requireNativeModule,
+  NativeModulesProxy,
+  EventEmitter,
+} from 'expo-modules-core';
 
 type VideoInfo = {
   +duration: number, // seconds
@@ -10,11 +14,39 @@ type VideoInfo = {
   +format: string,
 };
 
+export type H264Profile = 'baseline' | 'main' | 'high';
+
+export type TranscodeOptions = {
+  +width: number,
+  +height: number,
+  +bitrate?: number,
+  +profile?: H264Profile,
+};
+
+type ProgressCallback = (progress: number) => void;
+
+type TranscodeProgressEvent = {
+  +progress: number,
+};
+
+type TranscodeStats = {
+  +size: number,
+  +duration: number,
+  +speed: number,
+};
+
 const MediaModule: {
   +getVideoInfo: (path: string) => Promise<VideoInfo>,
   +hasMultipleFrames: (path: string) => Promise<boolean>,
   +generateThumbnail: (inputPath: string, outputPath: string) => Promise<void>,
+  +transcodeVideo: (
+    inputPath: string,
+    outputPath: string,
+    options: TranscodeOptions,
+  ) => Promise<TranscodeStats>,
 } = requireNativeModule('MediaModule');
+
+const emitter = new EventEmitter(MediaModule ?? NativeModulesProxy.MediaModule);
 
 export function getVideoInfo(path: string): Promise<VideoInfo> {
   return MediaModule.getVideoInfo(path);
@@ -29,4 +61,21 @@ export function generateThumbnail(
   outputPath: string,
 ): Promise<void> {
   return MediaModule.generateThumbnail(inputPath, outputPath);
+}
+
+export async function transcodeVideo(
+  inputPath: string,
+  outputPath: string,
+  options: TranscodeOptions,
+  progressCallback?: ProgressCallback,
+): Promise<TranscodeStats> {
+  const listener = (event: TranscodeProgressEvent) => {
+    progressCallback?.(event.progress);
+  };
+  const subscription = emitter.addListener('onTranscodeProgress', listener);
+  try {
+    return await MediaModule.transcodeVideo(inputPath, outputPath, options);
+  } finally {
+    subscription.remove();
+  }
 }
