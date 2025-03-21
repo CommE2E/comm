813b6d8ffedb26f1f8c2e814fe4dd80d766ff1e3
Ashoat Tevosyan <ashoat@gmail.com>
2025-03-20 22:26:10 -0400

    [native] Script to bump native version
    Summary: Claude helped generate this. Will save me some time during release processes.
    
    Test Plan: Ran the script and it worked!
    
    Reviewers: tomek, angelika
    
    Differential Revision: https://phab.comm.dev/D14486

--- native/cpp/CommonCpp/NativeModules/CommCoreModule.h
+++ native/cpp/CommonCpp/NativeModules/CommCoreModule.h
@@ -30,6 +30,7 @@ namespace comm {
 namespace jsi = facebook::jsi;
 
 class CommCoreModule : public facebook::react::CommCoreModuleSchemaCxxSpecJSI {
+  // Version is now defined in native/version.js and injected during build
   const int codeVersion{470};
   std::unique_ptr<WorkerThread> cryptoThread;
 
---
+++ native/version.js
@@ -0,0 +1,4 @@
+// @flow
+
+export const codeVersion = 471;
+export const codeVersionName = '1.0.471';
