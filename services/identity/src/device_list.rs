e2e1cddaf466d469f64ef80a625f3315d7b2ad83
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2025-03-24 12:10:08 +0100

    [identity] Support tuples in device list verification
    Summary:
    Addresses [[ https://linear.app/comm/issue/ENG-10232/update-logoutprimarydevice-rpc-to-accept-tuple-instead-of-singleton | ENG-10232 ]] and verification part of [[ https://linear.app/comm/issue/ENG-10239/update-restore-primary-device-and-get-csat-to-handle-keyserver | ENG-10239 ]].
    Updated verification function to allow tuples if keyserver device ID is some.
    
    Original "strict form" breaks existing clients if they're keyserver owners (so breaks dev experience) so implemented an opt-in flag, of which removal is tracked in [[ https://linear.app/comm/issue/ENG-10414/enable-strict-device-list-checks-for-singletonstuples | ENG-10414 ]].
    
    Test Plan:
    Both strict/non-strict tested on staging:
    - No behavior change when no keyserver present
    - When keyserver is present, devce lists of length 2 are allowed
      - Additionally in strict form, keyserver device ID must match and singletons are no longer allowed
    
    Reviewers: kamil
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D14490

--- services/identity/src/device_list.rs
+++ services/identity/src/device_list.rs
@@ -229,10 +229,12 @@ pub fn verify_singleton_device_list(
   // expected primary device ID for "lastPrimarySignature".
   // Use `None` if the device list isn't expected to contain last signature.
   expected_previous_primary_device_id: Option<&String>,
+  expected_keyserver_device_id: Option<&String>,
 ) -> Result<(), tonic::Status> {
   use tonic::Status;
   use tonic_status_messages::INVALID_DEVICE_LIST_UPDATE as INVALID_DEVICE_LIST;
 
+  // Verify lastPrimarySignature
   match (
     &device_list.last_primary_signature,
     expected_previous_primary_device_id,
@@ -255,6 +257,7 @@ pub fn verify_singleton_device_list(
     }
   };
 
+  // verify curPrimarySignature
   let Some(signature) = &device_list.current_primary_signature else {
     debug!("Missing curPrimarySignature for singleton device list");
     return Err(Status::invalid_argument(INVALID_DEVICE_LIST));
@@ -266,11 +269,7 @@ pub fn verify_singleton_device_list(
     signature,
   )?;
 
-  if device_list.devices.len() != 1 {
-    debug!("Invalid device list length");
-    return Err(Status::invalid_argument(INVALID_DEVICE_LIST));
-  }
-
+  // verify primary device ID
   if device_list
     .devices
     .first()
@@ -281,6 +280,49 @@ pub fn verify_singleton_device_list(
     return Err(Status::invalid_argument(INVALID_DEVICE_LIST));
   }
 
+  // NOTE: for better dev experience, we can be less strict for now,
+  // allowing both singleton/tuple device list when keyserver is present
+  let strict_device_list_checks_enabled =
+    std::env::var("COMM_SERVICES_STRICT_DEVICE_LIST_CHECKS")
+      .ok()
+      .is_some();
+
+  if !strict_device_list_checks_enabled {
+    let max_expected_device_list_length =
+      if expected_keyserver_device_id.is_some() {
+        2
+      } else {
+        1
+      };
+
+    if device_list.devices.is_empty()
+      || device_list.devices.len() > max_expected_device_list_length
+    {
+      debug!("Invalid device list length");
+      return Err(Status::invalid_argument(INVALID_DEVICE_LIST));
+    }
+  } else {
+    // verify keyserver device ID and device list length
+    if let Some(keyserver_device_id) = expected_keyserver_device_id {
+      if device_list.devices.len() != 2 {
+        debug!("Invalid device list length");
+        return Err(Status::invalid_argument(INVALID_DEVICE_LIST));
+      }
+
+      if device_list
+        .devices
+        .last()
+        .filter(|it| *it == keyserver_device_id)
+        .is_none()
+      {
+        debug!("Invalid primary device ID for singleton device list");
+        return Err(Status::invalid_argument(INVALID_DEVICE_LIST));
+      }
+    } else if device_list.devices.len() != 1 {
+      debug!("Invalid device list length");
+      return Err(Status::invalid_argument(INVALID_DEVICE_LIST));
+    }
+  }
   Ok(())
 }
 
