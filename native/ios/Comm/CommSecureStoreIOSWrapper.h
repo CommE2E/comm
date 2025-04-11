01c809cf11de3710a7c592314947e27a1f191a94
Angelika Serwa <angelika.serwa@swmansion.com>
2025-04-11 16:31:06 +0200

    ios

--- native/ios/Comm/CommSecureStoreIOSWrapper.h
+++ native/ios/Comm/CommSecureStoreIOSWrapper.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#import <EXSecureStore/EXSecureStore.h>
+//#import <EXSecureStore/EXSecureStore.h>
 
 #import <Foundation/Foundation.h>
 
--- native/ios/Comm/CommSecureStoreIOSWrapper.mm
+++ native/ios/Comm/CommSecureStoreIOSWrapper.mm
@@ -4,19 +4,19 @@
 #import <ExpoModulesCore/EXModuleRegistryProvider.h>
 
 @interface CommSecureStoreIOSWrapper ()
-@property(nonatomic, strong) EXSecureStore *secureStore;
+//@property(nonatomic, strong) EXSecureStore *secureStore;
 @property(nonatomic, strong) NSDictionary *options;
 @end
 
-@interface EXSecureStore (CommEXSecureStore)
-- (BOOL)_setValue:(NSString *)value
-          withKey:(NSString *)key
-      withOptions:(NSDictionary *)options
-            error:(NSError **)error;
-- (NSString *)_getValueWithKey:(NSString *)key
-                   withOptions:(NSDictionary *)options
-                         error:(NSError **)error;
-@end
+//@interface EXSecureStore (CommEXSecureStore)
+//- (BOOL)_setValue:(NSString *)value
+//          withKey:(NSString *)key
+//      withOptions:(NSDictionary *)options
+//            error:(NSError **)error;
+//- (NSString *)_getValueWithKey:(NSString *)key
+//                   withOptions:(NSDictionary *)options
+//                         error:(NSError **)error;
+//@end
 
 @implementation CommSecureStoreIOSWrapper
 
@@ -27,12 +27,12 @@
   static dispatch_once_t onceToken;
   dispatch_once(&onceToken, ^{
     shared = [[self alloc] init];
-    EXModuleRegistryProvider *moduleRegistryProvider =
-        [[EXModuleRegistryProvider alloc] init];
-    EXSecureStore *secureStore =
-        (EXSecureStore *)[[moduleRegistryProvider moduleRegistry]
-            getExportedModuleOfClass:EXSecureStore.class];
-    shared.secureStore = secureStore;
+//    EXModuleRegistryProvider *moduleRegistryProvider =
+//        [[EXModuleRegistryProvider alloc] init];
+//    EXSecureStore *secureStore =
+//        (EXSecureStore *)[[moduleRegistryProvider moduleRegistry]
+//            getExportedModuleOfClass:EXSecureStore.class];
+//    shared.secureStore = secureStore;
   });
   return shared;
 }
@@ -40,15 +40,15 @@
 - (void)set:(NSString *)key
           value:(NSString *)value
     withOptions:(NSDictionary *)options {
-  if ([self secureStore] == nil) {
-    [NSException raise:@"secure store error"
-                format:@"secure store has not been initialized"];
-  }
+//  if ([self secureStore] == nil) {
+//    [NSException raise:@"secure store error"
+//                format:@"secure store has not been initialized"];
+//  }
   NSError *error;
-  [[self secureStore] _setValue:value
-                        withKey:key
-                    withOptions:options
-                          error:&error];
+//  [[self secureStore] _setValue:value
+//                        withKey:key
+//                    withOptions:options
+//                          error:&error];
   if (error != nil) {
     [NSException raise:@"secure store error"
                 format:@"error occured when setting data"];
@@ -56,29 +56,31 @@
 }
 
 - (void)set:(NSString *)key value:(NSString *)value {
-  [self set:key
-            value:value
-      withOptions:@{
-        @"keychainAccessible" : @(EXSecureStoreAccessibleAfterFirstUnlock)
-      }];
+//  [self set:key
+//            value:value
+//      withOptions:@{
+//        @"keychainAccessible" : @(EXSecureStoreAccessibleAfterFirstUnlock)
+//      }];
 }
 
 - (NSString *)get:(NSString *)key withOptions:(NSDictionary *)options {
-  if ([self secureStore] == nil) {
-    [NSException raise:@"secure store error"
-                format:@"secure store has not been initialized"];
-  }
+//  if ([self secureStore] == nil) {
+//    [NSException raise:@"secure store error"
+//                format:@"secure store has not been initialized"];
+//  }
   NSError *error;
-  return [[self secureStore] _getValueWithKey:key
-                                  withOptions:options
-                                        error:&error];
+//  return [[self secureStore] _getValueWithKey:key
+//                                  withOptions:options
+//                                        error:&error];
+  return @"";
 }
 
 - (NSString *)get:(NSString *)key {
-  return [self get:key
-       withOptions:@{
-         @"keychainAccessible" : @(EXSecureStoreAccessibleAfterFirstUnlock)
-       }];
+//  return [self get:key
+//       withOptions:@{
+//         @"keychainAccessible" : @(EXSecureStoreAccessibleAfterFirstUnlock)
+//       }];
+  return @"";
 }
 
 @end
