6dcc2c5bc25824e0ca6773820e0216f842cca0c8
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2025-04-29 14:54:40 +0200

    [native] Add c++ util to parse ed25519 key from JSON blob
    Summary:
    
    Added an utility function that takes identity keys JSON blob from Olm account and extracts ed25519 signing public key.
    The function will be used for prekey verification.
    Code was inspired by the `CommCoreModule::getUserPublicKey()` (however it was not possible to easily deduplicate code, that function does other stuff too).
    
    Depends on D14619
    
    Test Plan:
    
    iOS and Android build. The function is used and tested in the next diff.
    
    Reviewers: kamil, tomek
    
    Subscribers:

--- native/cpp/CommonCpp/Tools/OlmUtils.h
+++ native/cpp/CommonCpp/Tools/OlmUtils.h
@@ -24,4 +24,12 @@ jsi::Object parseOLMOneTimeKeys(jsi::Runtime &rt, std::string oneTimeKeysBlob);
  */
 std::string parseOLMPrekey(std::string prekeyBlob);
 
+/**
+ * Extracts the ed25519 signing public key from an identity keys JSON blob
+ * @param identityKeysBlob JSON string with identity keys
+ * @return The ed25519 public key string
+ * @throws std::runtime_error if parsing fails
+ */
+std::string getSigningPublicKey(const std::string &identityKeysBlob);
+
 } // namespace comm
