a6095a57d06a87f04c26fc7f280e81c35ee35c28
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2025-04-29 14:54:40 +0200

    [native] Add c++ util to parse ed25519 key from JSON blob
    Summary:
    Added an utility function that takes identity keys JSON blob from Olm account and extracts ed25519 signing public key.
    The function will be used for prekey verification.
    Code was inspired by the `CommCoreModule::getUserPublicKey()` (however it was not possible to easily deduplicate code, that function does other stuff too).
    
    Depends on D14619
    
    Test Plan: iOS and Android build. The function is used and tested in the next diff.
    
    Reviewers: kamil, tomek
    
    Reviewed By: kamil
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D14620

--- native/cpp/CommonCpp/Tools/OlmUtils.cpp
+++ native/cpp/CommonCpp/Tools/OlmUtils.cpp
@@ -33,4 +33,25 @@ std::string parseOLMPrekey(std::string prekeyBlob) {
   return parsedPrekey["curve25519"].values().begin()->asString();
 }
 
+std::string getSigningPublicKey(const std::string &identityKeysBlob) {
+  folly::dynamic parsedKeys;
+  try {
+    parsedKeys = folly::parseJson(identityKeysBlob);
+  } catch (const folly::json::parse_error &e) {
+    std::string errorMessage{
+        "parsing identity keys failed with: " + std::string(e.what())};
+    Logger::log(errorMessage);
+    throw std::runtime_error(errorMessage);
+  }
+
+  if (!parsedKeys.count("ed25519") || !parsedKeys["ed25519"].isString()) {
+    std::string errorMessage{
+        "parsing identity keys failed: ed25519 key missing or malformed"};
+    Logger::log(errorMessage);
+    throw std::runtime_error(errorMessage);
+  }
+
+  return parsedKeys["ed25519"].asString();
+}
+
 } // namespace comm
