93d8aed2531302eb7b3f56de41498ad786bd5837
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2025-05-05 12:15:53 +0200

    [blob] Implement endpoint to get blob sizes
    Summary:
    Part of [[ https://linear.app/comm/issue/ENG-10730/add-endpoint-to-fetch-blob-sizes | ENG-10730 ]].
    Added implementation to the HTTP endpoint for fetching blob sizes:
    - Gets size from DDB if present
    - Asks S3 otherwise
    
    Depends on D14645
    
    Test Plan:
    Request `POST <staging_endpoint>/`
    ```
    lang=json
    {
        "blobHashes": [
            "e3e00439135c82417aae9f2225bfd0f734339a0f045950f3d4d1a9a114e014ab",
            "O8vMTKJjbMRwoJ0Y4nuPQoVl78xcpI7jGF6ToqmAxRw",
            "2ttb0Biep5dFhF3j6UG79JtFEbiUDSvBAzrIE8ZNbhw",
            "f25c66aea4f70333cb04da9598edb5b8f99b1f7e7a60da7d66e1338a6e23b548",
            "5dmD0dcTOHK3KyMTBVKNpknOEIVFpPDJjzyXcJp8ovI",
            "blob6"
        ]
    }
    ```
    Response:
    ```
    lang=json
    {
        "blobSizes": {
            "5dmD0dcTOHK3KyMTBVKNpknOEIVFpPDJjzyXcJp8ovI": 11178,
            "f25c66aea4f70333cb04da9598edb5b8f99b1f7e7a60da7d66e1338a6e23b548": 184320,
            "e3e00439135c82417aae9f2225bfd0f734339a0f045950f3d4d1a9a114e014ab": 690996,
            "blob6": 398,
            "2ttb0Biep5dFhF3j6UG79JtFEbiUDSvBAzrIE8ZNbhw": 820028,
            "O8vMTKJjbMRwoJ0Y4nuPQoVl78xcpI7jGF6ToqmAxRw": 8038920
        }
    }
    ```
    
    Reviewers: kamil
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D14646

--- services/blob/src/http/handlers/metadata.rs
+++ services/blob/src/http/handlers/metadata.rs
@@ -1,7 +1,8 @@
 use crate::{http::utils::verify_caller_is_service, service::BlobService};
 use actix_web::{web, HttpResponse};
 use comm_lib::{
-  auth::AuthorizationCredential, blob::types::http::BlobSizesRequest,
+  auth::AuthorizationCredential,
+  blob::types::http::{BlobSizesRequest, BlobSizesResponse},
 };
 use tracing::instrument;
 
@@ -13,5 +14,9 @@ pub async fn get_blob_size(
 ) -> actix_web::Result<HttpResponse> {
   verify_caller_is_service(&requesting_identity)?;
 
-  Ok(HttpResponse::NotImplemented().body("Not implemented yet"))
+  let request = payload.into_inner();
+  let blob_hashes = request.blob_hashes.into_iter().collect();
+  let blob_sizes = service.query_blob_sizes(blob_hashes).await?;
+  let response = BlobSizesResponse { blob_sizes };
+  Ok(HttpResponse::Ok().json(response))
 }
