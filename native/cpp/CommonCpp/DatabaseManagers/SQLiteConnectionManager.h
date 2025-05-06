619a20a34fe7e118859862e0bddd22b9e13ae9ee
xsanm <kkurowski.kk@gmail.com>
2025-05-06 16:07:27 +0200

    [SQLite] move connection creation to manager class and remove `default_on_db_open_callback` and
    Summary:
    Implemented as part of [ENG-10603](https://linear.app/comm/issue/ENG-10603/biggest-risk-factor-out-code-from-databasequeryexecutor)
    
    `default_on_db_open_callback` was a huge blocker to move some code around and to make some fields non-static. It was needed only on native, so it was moved to `NativeSQLiteConnectionManager` as `onDatabaseOpen`.
    
    `initializeConnection` was converted to be pure virtual to provide platform-specific implementation on each platform (they're different). To avoid code duplication, `createConnection` was introduced, but note that this is only an utility; caching `dbConnection` was moved to derived classes.
    
    Depends on D14662
    
    Test Plan: Login/logout on web and native, perform some operations on db
    
    Reviewers: bartek, tomek, angelika
    
    Reviewed By: bartek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D14663

--- native/cpp/CommonCpp/DatabaseManagers/SQLiteConnectionManager.h
+++ native/cpp/CommonCpp/DatabaseManagers/SQLiteConnectionManager.h
@@ -18,14 +18,29 @@ protected:
       int expectedResultCode = SQLITE_OK);
   void closeConnectionInternal();
 
+  // Shared implementation of creating a connection used by derived classes.
+  sqlite3 *createConnection(std::string sqliteFilePath);
+
 public:
   SQLiteConnectionManager();
-  sqlite3 *getConnection();
+  virtual ~SQLiteConnectionManager();
+
+  // Creates a SQLite connection that is returned, but it is the caller's
+  // responsibility to manage and close it. It is important to use this method,
+  // not the `sqlite3` API, because creating a connection might be different
+  // depending on the platform. Custom behaviour is implemented in derived
+  // classes.
+  virtual sqlite3 *getEphemeralConnection(
+      std::string sqliteFilePath,
+      std::string sqliteEncryptionKey) = 0;
+  // Creates a SQLite connection that is cached and stored as an attribute. It
+  // can be accessed using `getConnection` and closed using `closeConnection`
   virtual void initializeConnection(
       std::string sqliteFilePath,
-      std::function<void(sqlite3 *)> on_db_open_callback);
+      std::string sqliteEncryptionKey) = 0;
+  sqlite3 *getConnection();
   virtual void closeConnection() = 0;
-  virtual ~SQLiteConnectionManager();
+
   virtual void restoreFromBackupLog(const std::vector<std::uint8_t> &backupLog);
 };
 } // namespace comm
