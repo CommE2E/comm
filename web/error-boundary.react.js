036abbfadb6f1483dde819ba0254882edebc4874
xsanm <kkurowski.kk@gmail.com>
2025-05-06 11:49:45 +0200

    [web] handle unsupported version in ErrorBoundary
    Summary:
    [ENG-10627](https://linear.app/comm/issue/ENG-10627/something-went-wrong-on-web-when-version-is-unsupported).
    
    The error from task description was thrown in `InitialReduxStateGate` because client was unable to talk to keyserver. It is not possible to use `VersionUnsupportedModal` because error in `InitialReduxStateGate` causes `initialStateLoaded` to be false and as a result Modals are not shown and only `<Loading />` is visible.
    
    This is more general approach that should handle all cases, not only errors from `InitialReduxStateGate`.
    
    Also note, that this scenario shouldn't really happen, also while testing D14592. Code from `InitialReduxStateGate` is executed on app refresh, which means that user should be already running on a newest version. We should only handle this error when `MinCodeVersion` changes while app is already loaded and is communicating with keyserver - this is already covered in `MinVersionHandler`, or during login in `useHandleSecondaryDeviceLogInError`.
    
    Test Plan: Similar to D14592, bump the version and reload the app
    
    Reviewers: tomek, bartek

--- web/error-boundary.react.js
+++ web/error-boundary.react.js
@@ -3,8 +3,10 @@
 import * as React from 'react';
 
 import type { ErrorInfo, ErrorData } from 'lib/types/report-types.js';
+import { getMessageForException } from 'lib/utils/errors.js';
 
 import css from './error-boundary.css';
+import { getVersionUnsupportedError } from './utils/version-utils.js';
 
 type Props = {
   +children: React.Node,
@@ -29,9 +31,22 @@ class ErrorBoundary extends React.PureComponent<Props, State> {
 
   render(): React.Node {
     if (this.state.errorData.length > 0) {
+      const versionUnsupportedError = this.state.errorData.find(e => {
+        const messageForException = getMessageForException(e.error);
+        return (
+          messageForException === 'client_version_unsupported' ||
+          messageForException === 'unsupported_version'
+        );
+      });
+
+      let message = 'Something has gone wrong, please reload the page';
+      if (versionUnsupportedError) {
+        message = getVersionUnsupportedError();
+      }
+
       return (
         <div className={css.container}>
-          <h3>Something has gone wrong, please reload the page</h3>
+          <h3>{message}</h3>
         </div>
       );
     }
