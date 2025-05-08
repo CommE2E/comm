29a8fd871266f30661d5d17849d6c74e5eaf8b40
xsanm <kkurowski.kk@gmail.com>
2025-05-08 15:23:26 +0200

    [SQLite] convert smart pointers to `std::optional` to represent nullable value in entities
    Summary:
    [ENG-5719](https://linear.app/comm/issue/ENG-5719/improve-pointers-handling#comment-3768ecd3).
    
    Before C++17 we had to use pointers to simulate nullable values. After update and implementing `std::optional` marshalling in D12736 we can use more appropriate `std::optional (C++) <-> undefined (JS)` conversion and avoid implementation differences between web and native.
    
    Depends on D14665
    
    Test Plan:
    `cd web & yarn jest`, build native app
    
    Reviewers: bartek, tomek

--- native/cpp/CommonCpp/DatabaseManagers/entities/SQLiteDataConverters.h
+++ native/cpp/CommonCpp/DatabaseManagers/entities/SQLiteDataConverters.h
@@ -2,26 +2,27 @@
 
 #include <sqlite3.h>
 #include <memory>
+#include <optional>
 #include <string>
 
 namespace comm {
 // getting data from SQL statement result row
 std::string getStringFromSQLRow(sqlite3_stmt *sqlRow, int idx);
 int getIntFromSQLRow(sqlite3_stmt *sqlRow, int idx);
-std::unique_ptr<std::string>
-getStringPtrFromSQLRow(sqlite3_stmt *sqlRow, int idx);
-std::unique_ptr<int> getIntPtrFromSQLRow(sqlite3_stmt *sqlRow, int idx);
+std::optional<std::string>
+getOptionalStringFromSQLRow(sqlite3_stmt *sqlRow, int idx);
+std::optional<int> getOptionalIntFromSQLRow(sqlite3_stmt *sqlRow, int idx);
 int64_t getInt64FromSQLRow(sqlite3_stmt *sqlRow, int idx);
 
 // binding data to SQL statement
 int bindStringToSQL(const std::string &data, sqlite3_stmt *sql, int idx);
-int bindStringPtrToSQL(
-    const std::unique_ptr<std::string> &data,
+int bindOptionalStringToSQL(
+    const std::optional<std::string> &data,
     sqlite3_stmt *sql,
     int idx);
 int bindIntToSQL(int data, sqlite3_stmt *sql, int idx);
-int bindIntPtrToSQL(
-    const std::unique_ptr<int> &data,
+int bindOptionalIntToSQL(
+    const std::optional<int> &data,
     sqlite3_stmt *sql,
     int idx);
 int bindInt64ToSQL(int64_t data, sqlite3_stmt *sql, int idx);
