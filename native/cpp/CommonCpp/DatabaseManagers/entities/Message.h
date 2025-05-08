574be510c93f0ac2188b467270b125c8c75e15a4
xsanm <kkurowski.kk@gmail.com>
2025-05-08 15:23:26 +0200

    [SQLite] convert smart pointers to `std::optional` to represent nullable value in entities
    Summary:
    [ENG-5719](https://linear.app/comm/issue/ENG-5719/improve-pointers-handling#comment-3768ecd3).
    
    Before C++17 we had to use pointers to simulate nullable values. After update and implementing `std::optional` marshalling in D12736 we can use more appropriate `std::optional (C++) <-> undefined (JS)` conversion and avoid implementation differences between web and native.
    
    Depends on D14665
    
    Test Plan: `cd web & yarn jest`, build native app
    
    Reviewers: bartek, tomek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D14666

--- native/cpp/CommonCpp/DatabaseManagers/entities/Message.h
+++ native/cpp/CommonCpp/DatabaseManagers/entities/Message.h
@@ -4,6 +4,7 @@
 #include "Nullable.h"
 #include <sqlite3.h>
 #include <memory>
+#include <optional>
 #include <string>
 #include <vector>
 
@@ -11,34 +12,34 @@ namespace comm {
 
 struct Message {
   std::string id;
-  std::unique_ptr<std::string> local_id;
+  std::optional<std::string> local_id;
   std::string thread;
   std::string user;
   int type;
-  std::unique_ptr<int> future_type;
-  std::unique_ptr<std::string> content;
+  std::optional<int> future_type;
+  std::optional<std::string> content;
   int64_t time;
 
   static Message fromSQLResult(sqlite3_stmt *sqlRow, int idx) {
     return Message{
         getStringFromSQLRow(sqlRow, idx),
-        getStringPtrFromSQLRow(sqlRow, idx + 1),
+        getOptionalStringFromSQLRow(sqlRow, idx + 1),
         getStringFromSQLRow(sqlRow, idx + 2),
         getStringFromSQLRow(sqlRow, idx + 3),
         getIntFromSQLRow(sqlRow, idx + 4),
-        getIntPtrFromSQLRow(sqlRow, idx + 5),
-        getStringPtrFromSQLRow(sqlRow, idx + 6),
+        getOptionalIntFromSQLRow(sqlRow, idx + 5),
+        getOptionalStringFromSQLRow(sqlRow, idx + 6),
         getInt64FromSQLRow(sqlRow, idx + 7)};
   }
 
   int bindToSQL(sqlite3_stmt *sql, int idx) const {
     bindStringToSQL(id, sql, idx);
-    bindStringPtrToSQL(local_id, sql, idx + 1);
+    bindOptionalStringToSQL(local_id, sql, idx + 1);
     bindStringToSQL(thread, sql, idx + 2);
     bindStringToSQL(user, sql, idx + 3);
     bindIntToSQL(type, sql, idx + 4);
-    bindIntPtrToSQL(future_type, sql, idx + 5);
-    bindStringPtrToSQL(content, sql, idx + 6);
+    bindOptionalIntToSQL(future_type, sql, idx + 5);
+    bindOptionalStringToSQL(content, sql, idx + 6);
     return bindInt64ToSQL(time, sql, idx + 7);
   }
 };
