9766bca6af076883669425128277bba59b3244ee
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2025-05-10 16:21:29 +0200

    [backup] Refactor HTTP handler utility functions
    Summary:
    Address [[ https://linear.app/comm/issue/ENG-10683/refactor-code-in-backup-service-http-handler | ENG-10683 ]].
    This mod was messy - HTTP handlers/responders were mixed with helper functions with misleading names.
    Also, it was getting tough with references lifetimes, which could make trouble in next diffs with copying logs.
    
    - Moved Blob/Holder-specific functions to a submodule. Renamed them to be more accurate
    - Moved Multipart-processing utilities to a trait with impl for Multipart object. This made syntax cleaner.
    
    Depends on D14681
    
    Test Plan: cargo build. Backup integration tests
    
    Reviewers: kamil
    
    Reviewed By: kamil
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D14682

--- services/backup/src/http/handlers/backup.rs
+++ services/backup/src/http/handlers/backup.rs
@@ -39,26 +39,18 @@ pub async fn upload(
 
   info!("Backup data upload started");
 
-  let (user_keys_blob_info, user_keys_revoke) = forward_field_to_blob(
-    &mut multipart,
-    &blob_client,
-    "user_keys_hash",
-    "user_keys",
-  )
-  .await?;
+  let (user_keys_blob_info, user_keys_revoke) = multipart
+    .forward_field_to_blob(&blob_client, "user_keys_hash", "user_keys")
+    .await?;
 
-  let (user_data_blob_info, user_data_revoke) = forward_field_to_blob(
-    &mut multipart,
-    &blob_client,
-    "user_data_hash",
-    "user_data",
-  )
-  .await?;
+  let (user_data_blob_info, user_data_revoke) = multipart
+    .forward_field_to_blob(&blob_client, "user_data_hash", "user_data")
+    .await?;
 
   let (attachments, attachments_revokes) =
-    process_attachments(&mut multipart, &blob_client).await?;
+    multipart.process_attachmens_field(&blob_client).await?;
 
-  let siwe_backup_msg = get_siwe_backup_msg(&mut multipart).await?;
+  let siwe_backup_msg = multipart.get_siwe_backup_msg().await?;
 
   let item = BackupItem::new(
     user.user_id.clone(),
@@ -168,16 +160,12 @@ pub async fn upload_user_data(
 
   info!("Backup User Data upload started");
 
-  let (user_data_blob_info, user_data_revoke) = forward_field_to_blob(
-    &mut multipart,
-    &blob_client,
-    "user_data_hash",
-    "user_data",
-  )
-  .await?;
+  let (user_data_blob_info, user_data_revoke) = multipart
+    .forward_field_to_blob(&blob_client, "user_data_hash", "user_data")
+    .await?;
 
   let (attachments, attachments_revokes) =
-    process_attachments(&mut multipart, &blob_client).await?;
+    multipart.process_attachmens_field(&blob_client).await?;
 
   let existing_backup_item = db_client
     .find_backup_item(&user.user_id, &backup_id)
@@ -214,155 +202,6 @@ pub async fn upload_user_data(
   Ok(HttpResponse::Ok().finish())
 }
 
-#[instrument(skip_all, fields(hash_field_name, data_field_name))]
-async fn forward_field_to_blob<'revoke, 'blob: 'revoke>(
-  multipart: &mut actix_multipart::Multipart,
-  blob_client: &'blob BlobServiceClient,
-  hash_field_name: &str,
-  data_field_name: &str,
-) -> actix_web::Result<(BlobInfo, Defer<'revoke>)> {
-  trace!("Reading blob fields: {hash_field_name:?}, {data_field_name:?}");
-
-  let blob_hash = get_named_text_field(hash_field_name, multipart).await?;
-
-  let Some(mut field) = multipart.try_next().await? else {
-    warn!("Malformed request: expected a field.");
-    return Err(ErrorBadRequest("Bad request"))?;
-  };
-  if field.name() != data_field_name {
-    warn!(
-      hash_field_name,
-      "Malformed request: '{data_field_name}' data field expected."
-    );
-    return Err(ErrorBadRequest("Bad request"))?;
-  }
-
-  let blob_info = BlobInfo {
-    blob_hash,
-    holder: uuid::Uuid::new_v4().to_string(),
-  };
-
-  // [`actix_multipart::Multipart`] isn't [`std::marker::Send`], and so we cannot pass it to the blob client directly.
-  // Instead we have to forward it to a channel and create stream from the receiver.
-  let (tx, rx) = tokio::sync::mpsc::channel(1);
-  let receive_promise = async move {
-    trace!("Receiving blob data");
-    // [`actix_multipart::MultipartError`] isn't [`std::marker::Send`] so we return it here, and pass [`Infallible`]
-    // as the error to the channel
-    while let Some(chunk) = field.try_next().await? {
-      if let Err(err) = tx.send(Result::<Bytes, Infallible>::Ok(chunk)).await {
-        warn!("Error when sending data through a channel: '{err}'");
-        // Error here means that the channel has been closed from the blob client side. We don't want to return an error
-        // here, because `tokio::try_join!` only returns the first error it receives and we want to prioritize the backup
-        // client error.
-        break;
-      }
-    }
-    trace!("Finished receiving blob data");
-    Result::<(), actix_web::Error>::Ok(())
-  };
-
-  let data_stream = ReceiverStream::new(rx);
-  let send_promise = async {
-    blob_client
-      .simple_put(&blob_info.blob_hash, &blob_info.holder, data_stream)
-      .await
-      .map_err(BackupError::from)?;
-
-    Ok(())
-  };
-
-  tokio::try_join!(receive_promise, send_promise)?;
-
-  let revoke_info = blob_info.clone();
-  let revoke_holder = Defer::new(|| {
-    blob_client
-      .schedule_revoke_holder(revoke_info.blob_hash, revoke_info.holder)
-  });
-
-  Ok((blob_info, revoke_holder))
-}
-
-#[instrument(skip_all)]
-async fn create_holder<'revoke, 'blob: 'revoke>(
-  hash: &str,
-  blob_client: &'blob BlobServiceClient,
-) -> Result<(BlobInfo, Defer<'revoke>), BackupError> {
-  let holder = uuid::Uuid::new_v4().to_string();
-
-  if !blob_client
-    .assign_holder(hash, &holder)
-    .await
-    .map_err(BackupError::from)?
-  {
-    warn!("Blob with hash {hash:?} doesn't exist");
-  }
-
-  let revoke_hash = hash.to_string();
-  let revoke_holder = holder.clone();
-  let revoke_holder = Defer::new(|| {
-    blob_client.schedule_revoke_holder(revoke_hash, revoke_holder)
-  });
-
-  let blob_info = BlobInfo {
-    blob_hash: hash.to_string(),
-    holder,
-  };
-
-  Ok((blob_info, revoke_holder))
-}
-
-async fn process_blob_hashes<'revoke, 'blob: 'revoke>(
-  hashes: Vec<String>,
-  blob_client: &'blob BlobServiceClient,
-) -> Result<(Vec<BlobInfo>, Vec<Defer<'revoke>>), BackupError> {
-  let mut blob_infos = Vec::new();
-  let mut revokes = Vec::new();
-  for hash in hashes {
-    let (blob_info, revoke) = create_holder(&hash, blob_client).await?;
-    blob_infos.push(blob_info);
-    revokes.push(revoke);
-  }
-
-  Ok((blob_infos, revokes))
-}
-
-#[instrument(skip_all)]
-async fn process_attachments<'revoke, 'blob: 'revoke>(
-  multipart: &mut actix_multipart::Multipart,
-  blob_client: &'blob BlobServiceClient,
-) -> Result<(Vec<BlobInfo>, Vec<Defer<'revoke>>), BackupError> {
-  let attachments_hashes: Vec<String> = match get_text_field(multipart).await {
-    Ok(Some((name, attachments))) => {
-      if name != "attachments" {
-        warn!(
-          name,
-          "Malformed request: 'attachments' text field expected."
-        );
-        return Err(BackupError::BadRequest);
-      }
-
-      attachments.lines().map(ToString::to_string).collect()
-    }
-    Ok(None) => Vec::new(),
-    Err(_) => return Err(BackupError::BadRequest),
-  };
-
-  process_blob_hashes(attachments_hashes, blob_client).await
-}
-
-#[instrument(skip_all)]
-pub async fn get_siwe_backup_msg(
-  multipart: &mut actix_multipart::Multipart,
-) -> actix_web::Result<Option<String>> {
-  Ok(
-    get_text_field(multipart)
-      .await?
-      .filter(|(name, _)| name == "siwe_backup_msg")
-      .map(|(_, siwe_backup_msg)| siwe_backup_msg),
-  )
-}
-
 #[instrument(skip_all, fields(backup_id = %path))]
 pub async fn download_user_keys(
   user: UserIdentity,
@@ -372,7 +211,7 @@ pub async fn download_user_keys(
 ) -> actix_web::Result<HttpResponse> {
   info!("Download user keys request");
   let backup_id = path.into_inner();
-  download_user_blob(
+  blob_utils::download_user_blob(
     |item| Ok(&item.user_keys),
     &user.user_id,
     &backup_id,
@@ -391,7 +230,7 @@ pub async fn download_user_data(
 ) -> actix_web::Result<HttpResponse> {
   info!("Download user data request");
   let backup_id = path.into_inner();
-  download_user_blob(
+  blob_utils::download_user_blob(
     |item| item.user_data.as_ref().ok_or(BackupError::NoUserData),
     &user.user_id,
     &backup_id,
@@ -399,33 +238,6 @@ pub async fn download_user_data(
     db_client,
   )
   .await
-}
-
-pub async fn download_user_blob(
-  data_extractor: impl FnOnce(&BackupItem) -> Result<&BlobInfo, BackupError>,
-  user_id: &str,
-  backup_id: &str,
-  blob_client: BlobServiceClient,
-  db_client: web::Data<DatabaseClient>,
-) -> actix_web::Result<HttpResponse> {
-  let backup_item = db_client
-    .find_backup_item(user_id, backup_id)
-    .await
-    .map_err(BackupError::from)?
-    .ok_or(BackupError::NoBackup)?;
-
-  let blob_info = data_extractor(&backup_item)?;
-
-  let stream = blob_client
-    .get(&blob_info.blob_hash)
-    .await
-    .map_err(BackupError::from)?;
-
-  Ok(
-    HttpResponse::Ok()
-      .content_type("application/octet-stream")
-      .streaming(stream),
-  )
 }
 
 #[instrument(skip_all, fields(username = %path))]
@@ -509,15 +321,11 @@ async fn upload_userkeys_and_create_backup_item<'revoke, 'blob: 'revoke>(
   tracing::Span::current().record("backup_id", &backup_id);
   info!("Backup User Keys upload started");
 
-  let (user_keys_blob_info, user_keys_revoke) = forward_field_to_blob(
-    &mut multipart,
-    blob_client,
-    "user_keys_hash",
-    "user_keys",
-  )
-  .await?;
+  let (user_keys_blob_info, user_keys_revoke) = multipart
+    .forward_field_to_blob(blob_client, "user_keys_hash", "user_keys")
+    .await?;
 
-  let siwe_backup_msg = get_siwe_backup_msg(&mut multipart).await?;
+  let siwe_backup_msg = multipart.get_siwe_backup_msg().await?;
   let ordered_backup_item = db_client
     .find_last_backup_item(user_id)
     .await
@@ -545,7 +353,11 @@ async fn upload_userkeys_and_create_backup_item<'revoke, 'blob: 'revoke>(
         .collect();
 
       let (attachments, attachments_revokes) =
-        process_blob_hashes(attachments_hashes, blob_client).await?;
+        blob_utils::create_holders_for_blob_hashes(
+          attachments_hashes,
+          blob_client,
+        )
+        .await?;
 
       revokes.extend(attachments_revokes);
 
@@ -553,7 +365,11 @@ async fn upload_userkeys_and_create_backup_item<'revoke, 'blob: 'revoke>(
         None => (None, attachments),
         Some(data) => {
           let (blob_infos, defers) =
-            process_blob_hashes(vec![data.blob_hash], blob_client).await?;
+            blob_utils::create_holders_for_blob_hashes(
+              vec![data.blob_hash],
+              blob_client,
+            )
+            .await?;
 
           let blob_info = blob_infos
             .into_iter()
@@ -615,4 +431,215 @@ async fn get_total_backup_size(
 
   let total_backup_size = total_blobs_size + ddb_logs_size;
   Ok(total_backup_size)
+}
+
+trait BackupRequestInput {
+  /// Consumes two next fields: blob hash and blob data
+  /// and uploads content to Blob Service, returning a BlobInfo
+  /// and a revoke handle for deleting the uploaded blob
+  /// in case of further failure. The revoke has to be later
+  /// canceled.
+  async fn forward_field_to_blob<'revoke, 'blob: 'revoke>(
+    &mut self,
+    blob_client: &'blob BlobServiceClient,
+    hash_field_name: &str,
+    data_field_name: &str,
+  ) -> actix_web::Result<(BlobInfo, Defer<'revoke>)>;
+
+  /// Consumes a text field named `siwe_backup_msg`
+  async fn get_siwe_backup_msg(&mut self) -> actix_web::Result<Option<String>>;
+
+  /// Consumes a text field named `attachments`.
+  /// For each attachment hash, establishes a new blob holder.
+  /// Returns BlobInfos and a revoke handle for removing the holder
+  /// in case of further failure. All revokes have to be later
+  /// canceled.
+  async fn process_attachmens_field<'revoke, 'blob: 'revoke>(
+    &mut self,
+    blob_client: &'blob BlobServiceClient,
+  ) -> Result<(Vec<BlobInfo>, Vec<Defer<'revoke>>), BackupError>;
+}
+
+impl BackupRequestInput for actix_multipart::Multipart {
+  #[instrument(skip_all, fields(hash_field_name, data_field_name))]
+  async fn forward_field_to_blob<'revoke, 'blob: 'revoke>(
+    &mut self,
+    blob_client: &'blob BlobServiceClient,
+    hash_field_name: &str,
+    data_field_name: &str,
+  ) -> actix_web::Result<(BlobInfo, Defer<'revoke>)> {
+    trace!("Reading blob fields: {hash_field_name:?}, {data_field_name:?}");
+
+    let multipart = self;
+    let blob_hash = get_named_text_field(hash_field_name, multipart).await?;
+
+    let Some(mut field) = multipart.try_next().await? else {
+      warn!("Malformed request: expected a field.");
+      return Err(ErrorBadRequest("Bad request"))?;
+    };
+    if field.name() != data_field_name {
+      warn!(
+        hash_field_name,
+        "Malformed request: '{data_field_name}' data field expected."
+      );
+      return Err(ErrorBadRequest("Bad request"))?;
+    }
+
+    let blob_info = BlobInfo {
+      blob_hash,
+      holder: uuid::Uuid::new_v4().to_string(),
+    };
+
+    // [`actix_multipart::Multipart`] isn't [`std::marker::Send`], and so we cannot pass it to the blob client directly.
+    // Instead we have to forward it to a channel and create stream from the receiver.
+    let (tx, rx) = tokio::sync::mpsc::channel(1);
+    let receive_promise = async move {
+      trace!("Receiving blob data");
+      // [`actix_multipart::MultipartError`] isn't [`std::marker::Send`] so we return it here, and pass [`Infallible`]
+      // as the error to the channel
+      while let Some(chunk) = field.try_next().await? {
+        if let Err(err) = tx.send(Result::<Bytes, Infallible>::Ok(chunk)).await
+        {
+          warn!("Error when sending data through a channel: '{err}'");
+          // Error here means that the channel has been closed from the blob client side. We don't want to return an error
+          // here, because `tokio::try_join!` only returns the first error it receives and we want to prioritize the backup
+          // client error.
+          break;
+        }
+      }
+      trace!("Finished receiving blob data");
+      Result::<(), actix_web::Error>::Ok(())
+    };
+
+    let data_stream = ReceiverStream::new(rx);
+    let send_promise = async {
+      blob_client
+        .simple_put(&blob_info.blob_hash, &blob_info.holder, data_stream)
+        .await
+        .map_err(BackupError::from)?;
+
+      Ok(())
+    };
+
+    tokio::try_join!(receive_promise, send_promise)?;
+
+    let revoke_info = blob_info.clone();
+    let revoke_holder = Defer::new(|| {
+      blob_client
+        .schedule_revoke_holder(revoke_info.blob_hash, revoke_info.holder)
+    });
+
+    Ok((blob_info, revoke_holder))
+  }
+
+  #[instrument(skip_all)]
+  async fn get_siwe_backup_msg(&mut self) -> actix_web::Result<Option<String>> {
+    Ok(
+      get_text_field(self)
+        .await?
+        .filter(|(name, _)| name == "siwe_backup_msg")
+        .map(|(_, siwe_backup_msg)| siwe_backup_msg),
+    )
+  }
+
+  #[instrument(skip_all)]
+  async fn process_attachmens_field<'revoke, 'blob: 'revoke>(
+    &mut self,
+    blob_client: &'blob BlobServiceClient,
+  ) -> Result<(Vec<BlobInfo>, Vec<Defer<'revoke>>), BackupError> {
+    let attachments_hashes: Vec<String> = match get_text_field(self).await {
+      Ok(Some((name, attachments))) => {
+        if name != "attachments" {
+          warn!(
+            name,
+            "Malformed request: 'attachments' text field expected."
+          );
+          return Err(BackupError::BadRequest);
+        }
+
+        attachments.lines().map(ToString::to_string).collect()
+      }
+      Ok(None) => Vec::new(),
+      Err(_) => return Err(BackupError::BadRequest),
+    };
+
+    blob_utils::create_holders_for_blob_hashes(attachments_hashes, blob_client)
+      .await
+  }
+}
+
+mod blob_utils {
+  use super::*;
+
+  pub async fn download_user_blob(
+    data_extractor: impl FnOnce(&BackupItem) -> Result<&BlobInfo, BackupError>,
+    user_id: &str,
+    backup_id: &str,
+    blob_client: BlobServiceClient,
+    db_client: web::Data<DatabaseClient>,
+  ) -> actix_web::Result<HttpResponse> {
+    let backup_item = db_client
+      .find_backup_item(user_id, backup_id)
+      .await
+      .map_err(BackupError::from)?
+      .ok_or(BackupError::NoBackup)?;
+
+    let blob_info = data_extractor(&backup_item)?;
+
+    let stream = blob_client
+      .get(&blob_info.blob_hash)
+      .await
+      .map_err(BackupError::from)?;
+
+    Ok(
+      HttpResponse::Ok()
+        .content_type("application/octet-stream")
+        .streaming(stream),
+    )
+  }
+
+  pub async fn create_holders_for_blob_hashes<'revoke, 'blob: 'revoke>(
+    hashes: Vec<String>,
+    blob_client: &'blob BlobServiceClient,
+  ) -> Result<(Vec<BlobInfo>, Vec<Defer<'revoke>>), BackupError> {
+    let mut blob_infos = Vec::new();
+    let mut revokes = Vec::new();
+    for hash in hashes {
+      let (blob_info, revoke) =
+        create_holder_for_hash(&hash, blob_client).await?;
+      blob_infos.push(blob_info);
+      revokes.push(revoke);
+    }
+
+    Ok((blob_infos, revokes))
+  }
+
+  #[instrument(skip_all)]
+  pub async fn create_holder_for_hash<'revoke, 'blob: 'revoke>(
+    hash: &str,
+    blob_client: &'blob BlobServiceClient,
+  ) -> Result<(BlobInfo, Defer<'revoke>), BackupError> {
+    let holder = uuid::Uuid::new_v4().to_string();
+
+    if !blob_client
+      .assign_holder(hash, &holder)
+      .await
+      .map_err(BackupError::from)?
+    {
+      warn!("Blob with hash {hash:?} doesn't exist");
+    }
+
+    let revoke_hash = hash.to_string();
+    let revoke_holder = holder.clone();
+    let revoke_holder = Defer::new(|| {
+      blob_client.schedule_revoke_holder(revoke_hash, revoke_holder)
+    });
+
+    let blob_info = BlobInfo {
+      blob_hash: hash.to_string(),
+      holder,
+    };
+
+    Ok((blob_info, revoke_holder))
+  }
 }
