842f9fd0b0e040fc8b1c31a6b4bbbc70b22d49b9
xsanm <kkurowski.kk@gmail.com>
2025-05-13 18:59:54 +0200

    [SQLite] move key generation to `DatabaseManager`
    Summary:
    Implemented as part of [ENG-10603](https://linear.app/comm/issue/ENG-10603/biggest-risk-factor-out-code-from-databasequeryexecutor)
    
    We need to move it higher to be able to create multiple `SQLiteQueryExecutor`'s. Logic should remain unchanged; later in the stack, there are more improvements to avoid having static props and to remove `SQLiteQueryExecutor::initialize`.
    
    `initializeSQLiteQueryExecutorProperties` implementation is mostly copy-paste of old SQLiteQueryExecutor::initialize.
    
    Depends on D14717
    
    Test Plan: Build the app and check that database is still there and can be used without logging out user. Then logout to check if generation still works.
    
    Reviewers: bartek, tomek, angelika
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D14718

--- native/cpp/CommonCpp/DatabaseManagers/DatabaseManager.h
+++ native/cpp/CommonCpp/DatabaseManagers/DatabaseManager.h
@@ -13,9 +13,21 @@ class DatabaseManager {
   // which is identical to finishing the migration process and having a fully
   // operational database that can be used by application logic.
   static std::once_flag queryExecutorCreationIndicated;
+  static void indicateQueryExecutorCreation();
+
+  // Indicate that all properties needed to create an instance of
+  // SQLiteQueryExecutor were initialized in a thread-safe way (keys were read
+  // from SecureStore or generated).
+  static std::once_flag sqliteQueryExecutorPropertiesInitialized;
+  static void
+  initializeSQLiteQueryExecutorProperties(std::string &databasePath);
+
+  // Generate and persist a backup key used as a database encryption key.
+  static std::string generateBackupDataKey();
+  // Generate and persist key used for encrypt backup logs.
+  static std::string generateBackupLogDataKey();
 
   static void setDatabaseStatusAsWorkable();
-  static void indicateQueryExecutorCreation();
 
 public:
   static const DatabaseQueryExecutor &getQueryExecutor();
