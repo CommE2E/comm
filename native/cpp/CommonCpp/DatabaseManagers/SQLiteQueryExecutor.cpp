55d9dbdf6ca973b82c4f754a4e02913a06412302
xsanm <kkurowski.kk@gmail.com>
2025-05-13 18:49:26 +0200

    [SQLite] fix and improve `generateBackupDataKey`
    Summary:
    Implemented as part of [ENG-10603](https://linear.app/comm/issue/ENG-10603/biggest-risk-factor-out-code-from-databasequeryexecutor)
    
    `generateBackupDataKey` was currently generating two keys, which is extremely misleading. In the next diffs, those methods are going to be moved to `DatabaseManager`, so first, a refactor is in place to avoid confusing reviewers.
    
    Added some comments to make it clear.
    
    Depends on D14716
    
    Test Plan: Build the app and check that database is still there and can be used without logging out user. Then logout to check if generation still works.
    
    Reviewers: bartek, tomek, angelika
    
    Reviewed By: bartek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D14717

--- native/cpp/CommonCpp/DatabaseManagers/SQLiteQueryExecutor.cpp
+++ native/cpp/CommonCpp/DatabaseManagers/SQLiteQueryExecutor.cpp
@@ -1667,6 +1667,7 @@ void SQLiteQueryExecutor::clearSensitiveData() {
     throw std::system_error(errno, std::generic_category(), errorStream.str());
   }
   SQLiteQueryExecutor::generateBackupDataKey();
+  SQLiteQueryExecutor::generateBackupLogDataKey();
   SQLiteQueryExecutor::migrate();
 }
 
@@ -1678,17 +1679,21 @@ void SQLiteQueryExecutor::initialize(std::string &databasePath) {
     folly::Optional<std::string> maybeBackupLogDataKey =
         CommSecureStore::get(CommSecureStore::backupLogDataKey);
 
-    if (SQLiteUtils::fileExists(databasePath) && maybeBackupDataKey &&
-        maybeBackupLogDataKey) {
+    // In case of a non-existent database file, we always want to generate fresh
+    // key.
+    if (!SQLiteUtils::fileExists(databasePath) || !maybeBackupDataKey) {
+      SQLiteQueryExecutor::generateBackupDataKey();
+    } else {
       SQLiteQueryExecutor::backupDataKey = maybeBackupDataKey.value();
-      SQLiteQueryExecutor::backupLogDataKey = maybeBackupLogDataKey.value();
-      return;
-    } else if (SQLiteUtils::fileExists(databasePath) && maybeBackupDataKey) {
-      SQLiteQueryExecutor::backupDataKey = maybeBackupDataKey.value();
-      SQLiteQueryExecutor::generateBackupLogDataKey();
-      return;
     }
-    SQLiteQueryExecutor::generateBackupDataKey();
+
+    // In case of a non-existent database file, we always want to generate fresh
+    // key.
+    if (!SQLiteUtils::fileExists(databasePath) || !maybeBackupLogDataKey) {
+      SQLiteQueryExecutor::generateBackupLogDataKey();
+    } else {
+      SQLiteQueryExecutor::backupLogDataKey = maybeBackupLogDataKey.value();
+    }
   });
 }
 
@@ -1819,7 +1824,6 @@ void SQLiteQueryExecutor::generateBackupDataKey() {
       SQLiteQueryExecutor::backupDataKeySize);
   CommSecureStore::set(CommSecureStore::backupDataKey, backupDataKey);
   SQLiteQueryExecutor::backupDataKey = backupDataKey;
-  SQLiteQueryExecutor::generateBackupLogDataKey();
 }
 
 void SQLiteQueryExecutor::generateBackupLogDataKey() {
