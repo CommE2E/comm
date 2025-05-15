70e51fd149c1cf5a67a5bbeea7c03d3ad882ceb4
xsanm <kkurowski.kk@gmail.com>
2025-05-15 17:01:54 +0200

    [SQLite] move `triggerBackupFileUpload` to `DatabaseManager`
    Summary:
    Implemented as part of [ENG-10603](https://linear.app/comm/issue/ENG-10603/biggest-risk-factor-out-code-from-databasequeryexecutor)
    
    It is not executing any queries, it is daabase management.
    
    It should be const because it changes connectionManager
    Note that this IS SAVE because I am only moving static to a different class.
    
    Depends on TBD
    
    Test Plan: Build app
    
    Reviewers: bartek, tomek, angelika

--- native/cpp/CommonCpp/DatabaseManagers/SQLiteQueryExecutor.h
+++ native/cpp/CommonCpp/DatabaseManagers/SQLiteQueryExecutor.h
@@ -210,7 +210,6 @@ public:
       std::string &backupDataKey,
       std::string &backupLogDataKey);
   void createMainCompaction(std::string backupID) const override;
-  void triggerBackupFileUpload() const override;
   void setUserDataKeys(
       const std::string &backupDataKey,
       const std::string &backupLogDataKey) const override;
