dd6e54433f83a1ebdd869f4013bbe7a7cb380238
xsanm <kkurowski.kk@gmail.com>
2025-05-19 13:46:58 +0200

    [SQLite] make `connectionManager` to be local prop `SQLiteQueryExecutor` and move management to `DatabaseManager`
    Summary:
    Implemented as part of [ENG-10603](https://linear.app/comm/issue/ENG-10603/biggest-risk-factor-out-code-from-databasequeryexecutor)
    
    TBD
    
    Depends on D14733
    
    Test Plan: TBD
    
    Reviewers: bartek, tomek, angelika
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D14734

--- native/cpp/CommonCpp/DatabaseManagers/SQLiteQueryExecutor.cpp
+++ native/cpp/CommonCpp/DatabaseManagers/SQLiteQueryExecutor.cpp
@@ -35,14 +35,8 @@ std::string SQLiteQueryExecutor::backupDataKey;
 
 std::string SQLiteQueryExecutor::backupLogDataKey;
 
-#ifndef EMSCRIPTEN
-NativeSQLiteConnectionManager SQLiteQueryExecutor::connectionManager;
-#else
-WebSQLiteConnectionManager SQLiteQueryExecutor::connectionManager;
-#endif
-
 void SQLiteQueryExecutor::migrate() const {
-  SQLiteQueryExecutor::connectionManager.validateEncryption(
+  this->connectionManager->validateEncryption(
       SQLiteQueryExecutor::sqliteFilePath, SQLiteQueryExecutor::backupDataKey);
 
   std::stringstream db_path;
@@ -50,7 +44,7 @@ void SQLiteQueryExecutor::migrate() const {
           << std::endl;
   Logger::log(db_path.str());
 
-  sqlite3 *db = SQLiteQueryExecutor::connectionManager.getEphemeralConnection(
+  sqlite3 *db = this->connectionManager->getEphemeralConnection(
       SQLiteQueryExecutor::sqliteFilePath, SQLiteQueryExecutor::backupDataKey);
 
   auto db_version = SQLiteUtils::getDatabaseVersion(db);
@@ -75,34 +69,38 @@ void SQLiteQueryExecutor::migrate() const {
   sqlite3_close(db);
 }
 
-SQLiteQueryExecutor::SQLiteQueryExecutor() {
-  this->migrate();
 #ifndef EMSCRIPTEN
+SQLiteQueryExecutor::SQLiteQueryExecutor(
+    std::shared_ptr<NativeSQLiteConnectionManager> connectionManager)
+    : connectionManager(std::move(connectionManager)) {
+  this->migrate();
+  SQLiteQueryExecutor::connectionManager->initializeConnection(
+      SQLiteQueryExecutor::sqliteFilePath, SQLiteQueryExecutor::backupDataKey);
   std::string currentBackupID = this->getMetadata("backupID");
   if (!ServicesUtils::fullBackupSupport || !currentBackupID.size()) {
     return;
   }
-  SQLiteQueryExecutor::connectionManager.setLogsMonitoring(true);
-#endif
+  SQLiteQueryExecutor::connectionManager->setLogsMonitoring(true);
 }
 
-SQLiteQueryExecutor::SQLiteQueryExecutor(std::string sqliteFilePath) {
+#else
+SQLiteQueryExecutor::SQLiteQueryExecutor(std::string sqliteFilePath)
+    : connectionManager(std::make_unique<WebSQLiteConnectionManager>()) {
   SQLiteQueryExecutor::sqliteFilePath = sqliteFilePath;
   this->migrate();
+  SQLiteQueryExecutor::connectionManager->initializeConnection(
+      SQLiteQueryExecutor::sqliteFilePath, SQLiteQueryExecutor::backupDataKey);
 }
+#endif
 
 sqlite3 *SQLiteQueryExecutor::getConnection() const {
-  SQLiteQueryExecutor::connectionManager.initializeConnection(
+  this->connectionManager->initializeConnection(
       SQLiteQueryExecutor::sqliteFilePath, SQLiteQueryExecutor::backupDataKey);
-  return SQLiteQueryExecutor::connectionManager.getConnection();
-}
-
-void SQLiteQueryExecutor::closeConnection() {
-  SQLiteQueryExecutor::connectionManager.closeConnection();
+  return this->connectionManager->getConnection();
 }
 
 SQLiteQueryExecutor::~SQLiteQueryExecutor() {
-  SQLiteQueryExecutor::closeConnection();
+  this->connectionManager->closeConnection();
 }
 
 std::string SQLiteQueryExecutor::getDraft(std::string key) const {
@@ -1619,7 +1617,7 @@ void SQLiteQueryExecutor::restoreFromMainCompaction(
 
 void SQLiteQueryExecutor::restoreFromBackupLog(
     const std::vector<std::uint8_t> &backupLog) const {
-  SQLiteQueryExecutor::connectionManager.restoreFromBackupLog(backupLog);
+  this->connectionManager->restoreFromBackupLog(backupLog);
 }
 
 } // namespace comm
