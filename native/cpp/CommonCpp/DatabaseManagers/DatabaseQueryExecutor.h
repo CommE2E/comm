104a40e466accfaa9218fb7be0b89daf4567fc98
xsanm <kkurowski.kk@gmail.com>
2025-05-19 13:46:58 +0200

    [SQLite] make `connectionManager` to be local prop SQLiteQueryExecutor
    Summary:
    Implemented as part of [ENG-10603](https://linear.app/comm/issue/ENG-10603/biggest-risk-factor-out-code-from-databasequeryexecutor)
    
    Cleaning up code
    
    Depends on TBD
    
    Test Plan: Build app
    
    Reviewers: bartek, tomek, angelika

--- native/cpp/CommonCpp/DatabaseManagers/DatabaseQueryExecutor.h
+++ native/cpp/CommonCpp/DatabaseManagers/DatabaseQueryExecutor.h
@@ -148,8 +148,6 @@ public:
       std::string mainCompactionPath,
       std::string mainCompactionEncryptionKey,
       std::string maxVersion) const = 0;
-  virtual void
-  restoreFromBackupLog(const std::vector<std::uint8_t> &backupLog) const = 0;
   virtual void addOutboundP2PMessages(
       const std::vector<OutboundP2PMessage> &messages) const = 0;
   virtual std::vector<OutboundP2PMessage>
