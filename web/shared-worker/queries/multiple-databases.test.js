74ad3556c8264168dd0f39451b1ffb62b528565c
xsanm <kkurowski.kk@gmail.com>
2025-05-21 12:43:34 +0200

    [SQLite] extract method to copy content between databases
    Summary:
    [ENG-10616](https://linear.app/comm/issue/ENG-10616/implement-copying-content-between-two-databases).
    
    Depends on D14739
    
    Test Plan: Tests
    
    Reviewers: bartek, tomek, angelika
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D14741

---
+++ web/shared-worker/queries/multiple-databases.test.js
@@ -0,0 +1,117 @@
+// @flow
+
+import { getDatabaseModule } from '../db-module.js';
+import { clearSensitiveData } from '../utils/db-utils.js';
+
+const MAIN_FILE_PATH = 'main.sqlite';
+const BACKUP_FILE_PATH = 'backup.sqlite';
+
+const METADATA_KEY = 'test_key';
+const METADATA_DEFAULT_VALUE_BACKUP = 'default_value';
+const METADATA_DEFAULT_VALUE_MAIN = '';
+
+describe('Multiple databases', () => {
+  let mainQueryExecutor, backupQueryExecutor;
+  let dbModule;
+
+  beforeAll(async () => {
+    dbModule = getDatabaseModule();
+  });
+
+  beforeEach(() => {
+    if (!dbModule) {
+      throw new Error('Database module is missing');
+    }
+    mainQueryExecutor = new dbModule.SQLiteQueryExecutor(MAIN_FILE_PATH);
+    if (!mainQueryExecutor) {
+      throw new Error('Main SQLiteQueryExecutor is missing');
+    }
+
+    backupQueryExecutor = new dbModule.SQLiteQueryExecutor(BACKUP_FILE_PATH);
+    if (!backupQueryExecutor) {
+      throw new Error('Backup SQLiteQueryExecutor is missing');
+    }
+
+    backupQueryExecutor.setMetadata(
+      METADATA_KEY,
+      METADATA_DEFAULT_VALUE_BACKUP,
+    );
+  });
+
+  afterEach(() => {
+    if (!dbModule) {
+      return;
+    }
+    if (mainQueryExecutor) {
+      clearSensitiveData(dbModule, MAIN_FILE_PATH, mainQueryExecutor);
+    }
+    if (backupQueryExecutor) {
+      clearSensitiveData(dbModule, BACKUP_FILE_PATH, backupQueryExecutor);
+    }
+  });
+
+  it('changes in one database should not affect other', () => {
+    const customValue = 'custom_value;';
+    mainQueryExecutor?.setMetadata(METADATA_KEY, customValue);
+
+    expect(mainQueryExecutor.getMetadata(METADATA_KEY)).toBe(customValue);
+
+    // set in `beforeEach`
+    expect(backupQueryExecutor.getMetadata(METADATA_KEY)).toBe(
+      METADATA_DEFAULT_VALUE_BACKUP,
+    );
+  });
+
+  it('copies content between databases', () => {
+    expect(mainQueryExecutor.getMetadata(METADATA_KEY)).toBe(
+      METADATA_DEFAULT_VALUE_MAIN,
+    );
+
+    // should not copy content that is not in `tablesAllowlist`
+    mainQueryExecutor.copyContentFromDatabase(BACKUP_FILE_PATH);
+    expect(mainQueryExecutor.getMetadata(METADATA_KEY)).toBe(
+      METADATA_DEFAULT_VALUE_MAIN,
+    );
+
+    const draftKey = 'conversation_id';
+    const draftContent = 'typed_message';
+    backupQueryExecutor.updateDraft(draftKey, draftContent);
+
+    let draft;
+    // present in backup
+    draft = backupQueryExecutor.getAllDrafts().find(d => d.key === draftKey);
+    expect(draft?.text).toBe(draftContent);
+    // missing in main
+    draft = mainQueryExecutor.getAllDrafts().find(d => d.key === draftKey);
+    expect(draft).toBeUndefined();
+
+    mainQueryExecutor.copyContentFromDatabase(BACKUP_FILE_PATH);
+    // present in backup
+    draft = backupQueryExecutor.getAllDrafts().find(d => d.key === draftKey);
+    expect(draft?.text).toBe(draftContent);
+    // present in backup
+    draft = mainQueryExecutor.getAllDrafts().find(d => d.key === draftKey);
+    expect(draft?.text).toBe(draftContent);
+  });
+
+  it('do not override content from main database', () => {
+    const draftKey = 'conversation_id';
+    const mainDraftContent = 'main_draft';
+    const backupDraftContent = 'backup_draft';
+    mainQueryExecutor.updateDraft(draftKey, mainDraftContent);
+    backupQueryExecutor.updateDraft(draftKey, backupDraftContent);
+
+    mainQueryExecutor.copyContentFromDatabase(BACKUP_FILE_PATH);
+
+    const mainDraft = mainQueryExecutor
+      .getAllDrafts()
+      .find(d => d.key === draftKey);
+
+    expect(mainDraft?.text).toBe(mainDraftContent);
+
+    const backupDraft = backupQueryExecutor
+      .getAllDrafts()
+      .find(d => d.key === draftKey);
+    expect(backupDraft?.text).toBe(backupDraftContent);
+  });
+});
