87894bcd5cc10f4aef2fcbbb24a0dcd8038efd9c
xsanm <kkurowski.kk@gmail.com>
2025-05-22 12:22:45 +0200

    [web] bind `SQLiteQueryExecutor::migrate`
    Summary:
    [ENG-8358](https://linear.app/comm/issue/ENG-8358/run-the-migrations-during-backup-restoration)
    
    This method should be available on web
    
    Depends on D14748
    
    Test Plan: Tested in TBD
    
    Reviewers: bartek, tomek, angelika

--- web/shared-worker/types/sqlite-query-executor.js
+++ web/shared-worker/types/sqlite-query-executor.js
@@ -53,6 +53,8 @@ export type MessageEntity = {
 declare export class SQLiteQueryExecutor {
   constructor(sqliteFilePath: string, skipMigration: boolean): void;
 
+  migrate(): void;
+
   updateDraft(key: string, text: string): void;
   moveDraft(oldKey: string, newKey: string): boolean;
   getAllDrafts(): ClientDBDraftInfo[];
