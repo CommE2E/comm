0f3c64c9c68f15df0f55e3eb8be3982737d9b1d3
Angelika Serwa <angelika.serwa@swmansion.com>
2025-05-24 05:59:59 +0200

    [native][RN79][skip-ci] Add CommInitializerModule [android]
    Summary:
    https://linear.app/comm/issue/ENG-10286/migrate-jsi-related-stuff
    
    See the previous diff for context
    
    Depends on D14971
    
    Test Plan: Build and run the app
    
    Reviewers: bartek
    
    Reviewed By: bartek
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D14972

---
+++ native/android/app/src/main/java/app/comm/android/comminitializermodule/CommInitializerModuleSpec.kt
@@ -0,0 +1,24 @@
+package app.comm.android.comminitializermodule
+
+import com.facebook.proguard.annotations.DoNotStrip
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.turbomodule.core.interfaces.TurboModule
+import javax.annotation.Nonnull
+
+abstract class CommInitializerModuleSpec(reactContext: ReactApplicationContext?) :
+  ReactContextBaseJavaModule(reactContext), TurboModule {
+  @Nonnull
+  override fun getName(): String {
+    return NAME
+  }
+
+  @ReactMethod(isBlockingSynchronousMethod = true)
+  @DoNotStrip
+  abstract fun initializeComm(): Boolean
+
+  companion object {
+    const val NAME: String = "CommInitializerModule"
+  }
+}
