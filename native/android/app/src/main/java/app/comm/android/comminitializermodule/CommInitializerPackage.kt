b45cc3ca79f838c43d7334c2e68bfb8a43f7f5ff
Angelika Serwa <angelika.serwa@swmansion.com>
2025-05-24 05:59:59 +0200

    [native] RN 79: Add CommInitializerModule [android]

---
+++ native/android/app/src/main/java/app/comm/android/comminitializermodule/CommInitializerPackage.kt
@@ -0,0 +1,51 @@
+package app.comm.android.comminitializermodule
+
+import com.facebook.react.BaseReactPackage
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.module.annotations.ReactModule
+import com.facebook.react.module.annotations.ReactModuleList
+import com.facebook.react.module.model.ReactModuleInfo
+import com.facebook.react.module.model.ReactModuleInfoProvider
+import java.util.Objects
+
+@ReactModuleList(
+    nativeModules = [CommInitializerModule::class
+    ]
+)
+class CommInitializerPackage : BaseReactPackage(), ReactPackage {
+    override fun getModule(
+        name: String, reactContext: ReactApplicationContext
+    ): NativeModule? {
+        return when (name) {
+            CommInitializerModuleSpec.NAME -> CommInitializerModule(reactContext)
+            else -> null
+        }
+    }
+
+    override fun getReactModuleInfoProvider(): ReactModuleInfoProvider {
+        val moduleList: Array<Class<*>> = arrayOf(CommInitializerModule::class.java)
+
+        val reactModuleInfoMap: MutableMap<String, ReactModuleInfo> = HashMap()
+        for (moduleClass in moduleList) {
+            val reactModule =
+                Objects.requireNonNull(
+                    moduleClass.getAnnotation(
+                        ReactModule::class.java
+                    )
+                )
+
+            reactModuleInfoMap[reactModule.name] = ReactModuleInfo(
+                reactModule.name,
+                moduleClass.name,
+                reactModule.canOverrideExistingModule,
+                reactModule.needsEagerInit,
+                reactModule.isCxxModule,
+                true
+            )
+        }
+
+        return ReactModuleInfoProvider { reactModuleInfoMap }
+    }
+}
