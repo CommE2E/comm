ea6dc40f34ce7f215940dc7baa8afc0c1c8fb80c
xsanm <kkurowski.kk@gmail.com>
2025-05-26 17:59:37 +0200

    [web] implement persisting restored database
    Summary:
    [ENG-10607](https://linear.app/comm/issue/ENG-10607/web-refactor-persisting-to-handle-more-sqlitequeryexecutor-objects)
    
    Added comment to explain how it works.
    
    Depends on D14764
    
    Test Plan: Test restore on web, test that persistence works
    
    Reviewers: bartek, tomek

--- web/shared-worker/utils/constants.js
+++ web/shared-worker/utils/constants.js
@@ -3,6 +3,7 @@
 import localforage from 'localforage';
 
 export const SQLITE_CONTENT = 'sqliteFileContent';
+export const RESTORED_SQLITE_CONTENT = 'restoredSQLiteContent';
 export const SQLITE_ENCRYPTION_KEY = 'encryptionKey';
 
 export const SQLITE_STAMPED_USER_ID_KEY = 'current_user_id';
