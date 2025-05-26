334195976ce67085ceb3ce1b0223a1871f4da467
Angelika Serwa <angelika.serwa@swmansion.com>
2025-05-26 12:08:29 +0200

    [native][RN79][skip-ci] Migrate AppDelegate to Swift
    Summary:
    https://linear.app/comm/issue/ENG-10275/apply-the-diff-from-react-native-upgrade-helper
    
    In RN 79 the AppDelegate is rewritten to Swift. Some custom parts of our AppDelegate are not easily rewritten into swift, so they got moved to DBInit file without any changes.
    Then the AppDelegate calls functions from DBInit and other objc functions.
    
    Depends on D14976
    
    Test Plan: https://linear.app/comm/issue/ENG-10985/final-testing
    
    Reviewers: bartek
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D14977

--- native/ios/Comm-Bridging-Header.h
+++ native/ios/Comm-Bridging-Header.h
@@ -2,3 +2,7 @@
 //  Use this file to import your target's public headers that you would like to
 //  expose to Swift.
 //
+#import "CommIOSNotifications.h"
+#import "Comm/DBInit.h"
+#import "Comm/Tools.h"
+#import "CommIOSServicesClient.h"
