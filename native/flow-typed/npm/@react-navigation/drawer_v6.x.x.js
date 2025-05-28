2da5d56c8cb2c1c4ea563da73bc68f4945ce9845
Angelika Serwa <angelika.serwa@swmansion.com>
2025-05-28 12:09:47 +0200

    [Flow262] Replace React$Ref -> React.ElementRef

--- native/flow-typed/npm/@react-navigation/drawer_v6.x.x.js
+++ native/flow-typed/npm/@react-navigation/drawer_v6.x.x.js
@@ -91,7 +91,7 @@ declare module '@react-navigation/drawer' {
    * DrawerGestureContext
    */
 
-  declare type GestureHandlerRef = React$Ref<
+  declare type GestureHandlerRef = React.ElementRef<
     React$ComponentType<PanGestureHandlerProps>,
   >;
   declare export var DrawerGestureContext: React$Context<?GestureHandlerRef>;
