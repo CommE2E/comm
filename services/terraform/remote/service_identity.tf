80b85c5561f265909e91706409ab452f2c03855c
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2025-05-28 15:07:33 +0200

    [terraform] Bump staging Identity to 0.53.0
    Summary:
    Built an image with [[ https://linear.app/comm/issue/ENG-10782/add-rpc-allowing-fetching-outbound-keys-for-users-with-whitelisted | ENG-10782 ]] included.
    Bumped minor because of new APIs
    
    Test Plan: terraform plan
    
    Reviewers: kamil, tomek
    
    Reviewed By: kamil
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D14781

--- services/terraform/remote/service_identity.tf
+++ services/terraform/remote/service_identity.tf
@@ -1,5 +1,5 @@
 locals {
-  identity_service_image_tag      = local.is_staging ? "0.52.2" : "0.52.2"
+  identity_service_image_tag      = local.is_staging ? "0.53.0" : "0.52.2"
   identity_service_server_image   = "commapp/identity-server:${local.identity_service_image_tag}"
   identity_service_container_name = "identity-server"
 
