201bdae9f6f29936435d7ed4780ea9234b5c449f
xsanm <kkurowski.kk@gmail.com>
2025-06-03 15:17:09 +0200

    [lib][web][native] implement log filtering
    Summary:
    [ENG-10765](https://linear.app/comm/issue/ENG-10765/update-extensive-olm-operations-logging).
    
    TBD
    
    Depends on D14809
    
    Test Plan: Tested later in the stack
    
    Reviewers: tomek!, bartek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D14810

--- web/settings/debug-logs-modal.react.js
+++ web/settings/debug-logs-modal.react.js
@@ -2,15 +2,18 @@
 
 import * as React from 'react';
 
-import { useDebugLogs } from 'lib/components/debug-logs-context.js';
+import { logTypes, useDebugLogs } from 'lib/components/debug-logs-context.js';
+import type { LogType } from 'lib/components/debug-logs-context.js';
 import { useModalContext } from 'lib/components/modal-provider.react.js';
+import { values } from 'lib/utils/objects.js';
 
 import css from './debug-logs-modal.css';
 import Button, { buttonThemes } from '../components/button.react.js';
+import EnumSettingsOption from '../components/enum-settings-option.react.js';
 import Modal from '../modals/modal.react.js';
 
 function DebugLogsModal(): React.Node {
-  const { logs, clearLogs } = useDebugLogs();
+  const { logs, clearLogs, logsFilter, setFilter } = useDebugLogs();
   const { popModal } = useModalContext();
 
   const messageList = React.useMemo(
@@ -33,13 +36,35 @@ function DebugLogsModal(): React.Node {
     await navigator.clipboard.writeText(JSON.stringify(logs, null, 2));
   }, [logs]);
 
+  const toggleLogsFilter = React.useCallback(
+    (logType: LogType) => setFilter(logType, !logsFilter.has(logType)),
+    [logsFilter, setFilter],
+  );
+
+  const logTypesList = React.useMemo(
+    () =>
+      values(logTypes).map(logType => (
+        <EnumSettingsOption
+          key={logType}
+          selected={!!logsFilter.has(logType)}
+          onSelect={() => toggleLogsFilter(logType)}
+          icon={null}
+          title={logType}
+          type="checkbox"
+          statements={[{ statement: logType }]}
+        />
+      )),
+    [logsFilter, toggleLogsFilter],
+  );
+
   return (
     <Modal name="Debug Logs" onClose={popModal} size="large">
       <div className={css.container}>
+        <div> {logTypesList}</div>
         <div className={css.logsList}>{messageList}</div>
         <div className={css.buttons}>
           <Button variant="filled" className={css.button} onClick={copyLogs}>
-            Copy logs
+            Copy filtered logs
           </Button>
           <Button
             variant="filled"
@@ -47,7 +72,7 @@ function DebugLogsModal(): React.Node {
             className={css.button}
             onClick={clearLogs}
           >
-            Clear logs
+            Clear filtered logs
           </Button>
         </div>
       </div>
