7db3176e68305428176ea4d010eaacba68db44f1
Angelika Serwa <angelika.serwa@swmansion.com>
2025-06-07 00:25:57 +0200

    [Flow262][skip-ci] React.Element -> React.Node/React.MixedElement
    Summary:
    https://linear.app/comm/issue/ENG-10205/update-flow-to-02691
    
    React.Element was removed in flow 0.245.0. According to docs: https://flow.org/en/docs/react/types/#toc-react-element it should be replaced with
    React.MixedElement or React.Node.
    
    But it looks like cloneElement still takes React.Element https://github.com/facebook/flow/blob/6712ab6ac50f820f66669b1d71a767fd340c919c/lib/react.js#L163
    so I silenced the error.
    
    Also MixedElement doesn't have `props` property and we still use it, so I silenced the error by converting to any. Accessing props property on children looks like a hack to me anyway.
    
    Depends on D15036
    
    Test Plan: Flow
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D15037

--- web/navigation-panels/navigation-panel.react.js
+++ web/navigation-panels/navigation-panel.react.js
@@ -8,50 +8,53 @@ import horizontalCSS from './topbar.css';
 import type { AppState } from '../redux/redux-setup.js';
 import { useSelector } from '../redux/redux-utils.js';
 
+type NavigationPanelContextType = {
+  +currentTab: ?string,
+  +css: { [key: string]: string },
+};
+
+const NavigationPanelContext = React.createContext<NavigationPanelContextType>({
+  currentTab: null,
+  css: {},
+});
+
 type NavigationPanelItemProps = {
   +tab: string,
   +children: React.Node,
 };
 
 function NavigationPanelItem(props: NavigationPanelItemProps): React.Node {
-  const { children } = props;
-  return children;
+  const { children, tab } = props;
+  const { currentTab, css } = React.useContext(NavigationPanelContext);
+  return (
+    <div
+      className={classNames({
+        [css.current_tab]: currentTab === tab,
+      })}
+    >
+      {children}
+    </div>
+  );
 }
 
-type NavigationPanelContainerProps<T> = {
+type NavigationPanelContainerProps<T: ?string> = {
   +tabSelector: AppState => T,
-  +children: React.ChildrenArray<?React.Element<typeof NavigationPanelItem>>,
+  +children: React.ChildrenArray<?React.MixedElement>,
   +horizontal?: boolean,
 };
 
-function NavigationPanelContainer<T>(
+function NavigationPanelContainer<T: ?string>(
   props: NavigationPanelContainerProps<T>,
 ): React.Node {
   const { children, tabSelector, horizontal = false } = props;
   const currentTab = useSelector(tabSelector);
   const css = horizontal ? horizontalCSS : verticalCSS;
 
-  const items = React.useMemo(
-    () =>
-      React.Children.map(children, child => {
-        if (!child) {
-          return null;
-        }
-        return (
-          <div
-            key={child.props.tab}
-            className={classNames({
-              [css.current_tab]: currentTab === child.props.tab,
-            })}
-          >
-            {child}
-          </div>
-        );
-      }),
-    [children, css.current_tab, currentTab],
+  return (
+    <NavigationPanelContext.Provider value={{ currentTab, css }}>
+      <div className={css.navigationPanelContainer}>{children}</div>
+    </NavigationPanelContext.Provider>
   );
-
-  return <div className={css.navigationPanelContainer}>{items}</div>;
 }
 
 const NavigationPanel = {
