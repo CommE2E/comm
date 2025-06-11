22a8863aef94dfe5e22997aae74309ef62f27857
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2025-06-11 17:12:52 +0200

    [native_rust_lib] Remove compaction file after restoring
    Summary:
    Address [[ https://linear.app/comm/issue/ENG-10697/clear-not-uploaded-logs-or-compaction | ENG-10697 ]].
    After restoring (or failing), downloaded compaction file should be removed.
    Logs are downloaded and applied in-memory, so there's no need to clean up.
    
    Depends on D14831
    
    Test Plan: Restore flow. Observed iOS simulator app directory and confirmed that the `restore_backup` file is created and then removed.
    
    Reviewers: kamil
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D14832

--- native/native_rust_library/src/utils.rs
+++ native/native_rust_library/src/utils.rs
@@ -1,2 +1,13 @@
+use std::path::{Path, PathBuf};
+
 pub mod future_manager;
 pub mod jsi_callbacks;
+
+pub fn schedule_remove_file(path: &Path) {
+  let path = PathBuf::from(path);
+  tokio::spawn(async move {
+    if let Err(err) = tokio::fs::remove_file(&path).await {
+      println!("Failed to remove file {0}: {err:?}", path.to_string_lossy());
+    }
+  });
+}
