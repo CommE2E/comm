7d8bfe5246037eae7661eaed4eb9da4b029543a7
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2025-06-12 13:08:13 +0200

    [native] Rename C++ functions for logs
    Summary:
    
    Previous names were confusing, these functions create only single log from last DB activity.
    
    Depends on D14834
    
    Test Plan: native build
    
    Reviewers: kamil
    
    Subscribers:

--- native/cpp/CommonCpp/DatabaseManagers/DatabaseManager.h
+++ native/cpp/CommonCpp/DatabaseManagers/DatabaseManager.h
@@ -65,7 +65,7 @@ public:
       const std::string &backupLogDataKey);
 
   // Backup methods
-  static void captureBackupLogs();
+  static void captureBackupLogForLastOperation();
   static void triggerBackupFileUpload();
   static void createMainCompaction(
       std::string backupID,
