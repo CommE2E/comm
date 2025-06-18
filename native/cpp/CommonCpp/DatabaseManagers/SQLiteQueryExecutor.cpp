ba2dd44a0f3467cf0c1ee8ca7c7d4f911f4310a0
xsanm <kkurowski.kk@gmail.com>
2025-06-18 13:07:46 +0200

    [SQLite] update `searchMessages` and `getRelatedMessagesForSearch` to support regular and backup tables
    Summary:
    [ENG-10860]https://linear.app/comm/issue/ENG-10860/update-getrelatedmessagesforsearch-to-support-multiple-tables) and [ENG-10859](https://linear.app/comm/issue/ENG-10859/update-searchmessages-to-support-multiple-tables)
    
    Depends on D14857
    
    Test Plan: Experiments with `EXPLAIN QUERY PLAN` to check if indexes are used and performance is not worse. Tests
    
    Reviewers: tomek, bartek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D14858

--- native/cpp/CommonCpp/DatabaseManagers/SQLiteQueryExecutor.cpp
+++ native/cpp/CommonCpp/DatabaseManagers/SQLiteQueryExecutor.cpp
@@ -1509,18 +1509,28 @@ std::vector<MessageEntity> SQLiteQueryExecutor::searchMessages(
     std::string threadID,
     std::optional<std::string> timestampCursor,
     std::optional<std::string> messageIDCursor) const {
+  bool backupItem = !threadIDMatchesKeyserverProtocol(threadID);
+  std::string messagesTable = backupItem ? "backup_messages" : "messages";
+  std::string mediaTable = backupItem ? "backup_media" : "media";
+
   std::stringstream searchMessagesSQL;
   searchMessagesSQL
       << "SELECT "
          "  m.id, m.local_id, m.thread, m.user, m.type, m.future_type, "
-         "  m.content, m.time, media.id, media.container, media.thread, "
-         "  media.uri, media.type, media.extras "
+         "  m.content, m.time, md.id, md.container, md.thread, "
+         "  md.uri, md.type, md.extras "
          "FROM message_search AS s "
-         "LEFT JOIN messages AS m "
+         "LEFT JOIN "
+      << messagesTable
+      << " AS m "
          "  ON m.id = s.original_message_id "
-         "LEFT JOIN media "
-         "  ON m.id = media.container "
-         "LEFT JOIN messages AS m2 "
+         "LEFT JOIN "
+      << mediaTable
+      << " AS md "
+         "  ON m.id = md.container "
+         "LEFT JOIN "
+      << messagesTable
+      << " AS m2 "
          "  ON m2.target_message = m.id "
          "  AND m2.type = ? AND m2.thread = ? "
          "WHERE s.processed_content MATCH ? "
@@ -1560,7 +1570,7 @@ std::vector<MessageEntity> SQLiteQueryExecutor::searchMessages(
     messageIDs.push_back(message.message.id);
   }
   std::vector<MessageEntity> relatedMessages =
-      this->getRelatedMessagesForSearch(messageIDs);
+      this->getRelatedMessagesForSearch(messageIDs, backupItem);
 
   for (auto &entity : relatedMessages) {
     messages.push_back(std::move(entity));
@@ -1569,17 +1579,24 @@ std::vector<MessageEntity> SQLiteQueryExecutor::searchMessages(
 }
 
 std::vector<MessageEntity> SQLiteQueryExecutor::getRelatedMessagesForSearch(
-    const std::vector<std::string> &messageIDs) const {
+    const std::vector<std::string> &messageIDs,
+    bool backupItem) const {
+  std::string messagesTable = backupItem ? "backup_messages" : "messages";
+  std::string mediaTable = backupItem ? "backup_media" : "media";
   std::stringstream selectRelatedMessagesSQLStream;
   selectRelatedMessagesSQLStream << "SELECT "
                                     "  m.id, m.local_id, m.thread, m.user, "
                                     "  m.type, m.future_type, m.content, "
-                                    "  m.time, media.id, media.container, "
-                                    "  media.thread, media.uri, media.type, "
-                                    "  media.extras "
-                                    "FROM messages AS m "
-                                    "LEFT JOIN media "
-                                    "  ON m.id = media.container "
+                                    "  m.time, md.id, md.container, "
+                                    "  md.thread, md.uri, md.type, "
+                                    "  md.extras "
+                                    "FROM "
+                                 << messagesTable
+                                 << " AS m "
+                                    "LEFT JOIN "
+                                 << mediaTable
+                                 << " AS md "
+                                    "  ON m.id = md.container "
                                     "WHERE m.target_message IN "
                                  << getSQLStatementArray(messageIDs.size())
                                  << "ORDER BY m.time DESC";
