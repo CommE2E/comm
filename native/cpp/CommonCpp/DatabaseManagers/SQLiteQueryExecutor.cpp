73cbfb0ef3937c76e12569dcb9e842acb6b04484
xsanm <kkurowski.kk@gmail.com>
2025-06-18 15:31:44 +0200

    [native] update `updateSQLiteUnreadStatus` to check for thread type
    Summary:
    [ENG-10953](https://linear.app/comm/issue/ENG-10953/update-updatesqliteunreadstatus-to-check-for-thread-type).
    
    Depends on D14858
    
    Test Plan: Carefully reading the code. Unable to test actual functionality because of [ENG-10959](https://linear.app/comm/issue/ENG-10959/received-malformed-notification-missing-key-id)
    
    Reviewers: tomek, bartek
    
    Reviewed By: tomek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D14859

--- native/cpp/CommonCpp/DatabaseManagers/SQLiteQueryExecutor.cpp
+++ native/cpp/CommonCpp/DatabaseManagers/SQLiteQueryExecutor.cpp
@@ -115,9 +115,14 @@ std::string SQLiteQueryExecutor::getDraft(std::string key) const {
 
 std::unique_ptr<Thread>
 SQLiteQueryExecutor::getThread(std::string threadID) const {
-  static std::string getThreadByPrimaryKeySQL =
+  bool dataIsBackedUp = !threadIDMatchesKeyserverProtocol(threadID);
+  std::string threadsTable = dataIsBackedUp ? "backup_threads" : "threads";
+
+  std::string getThreadByPrimaryKeySQL =
       "SELECT * "
-      "FROM threads "
+      "FROM " +
+      threadsTable +
+      " "
       "WHERE id = ?;";
   return getEntityByPrimaryKey<Thread>(
       this->getConnection(), getThreadByPrimaryKeySQL, threadID);
