61c9a576a04d2f2f539c9174a73400e6dbdeb67c
Angelika Serwa <angelika.serwa@swmansion.com>
2025-06-25 12:25:41 +0200

    [Flow 262] Make useSelector work with MonitorActionState

--- native/redux/redux-utils.js
+++ native/redux/redux-utils.js
@@ -4,8 +4,8 @@ import { useSelector as reactReduxUseSelector } from 'react-redux';
 
 import type { AppState } from './state-types.js';
 
-function useSelector<SS>(
-  selector: (state: AppState) => SS,
+function useSelector<SS, T = AppState>(
+  selector: (state: T) => SS,
   equalityFn?: (a: SS, b: SS) => boolean,
 ): SS {
   return reactReduxUseSelector(selector, equalityFn);
