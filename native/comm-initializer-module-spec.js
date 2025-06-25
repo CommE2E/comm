285c646b64f07dde3262aedc1899a0413b842a9c
Angelika Serwa <angelika.serwa@swmansion.com>
2025-06-25 11:47:28 +0200

    Fix initializer module types (squash)

--- native/comm-initializer-module-spec.js
+++ native/comm-initializer-module-spec.js
@@ -1,10 +1,10 @@
 //@flow
 
-import type { TurboModule } from 'react-native';
+import type { TurboModule } from 'react-native//Libraries/TurboModule/RCTExport.js';
 import { TurboModuleRegistry } from 'react-native';
 
 interface Spec extends TurboModule {
   initializeComm: () => boolean;
 }
 
-export default (TurboModuleRegistry.get<Spec>('CommInitializerModule'): Spec);
+export default (TurboModuleRegistry.get<Spec>('CommInitializerModule'): ?Spec);
--- native/native-modules.js
+++ native/native-modules.js
@@ -5,7 +5,7 @@ import type { CoreModuleSpec } from './schema/CommCoreModuleSchema.js';
 import type { Spec as RustModuleSpec } from './schema/CommRustModuleSchema.js';
 import type { UtilsModuleSpec } from './schema/CommUtilsModuleSchema.js';
 
-CommInitializerModule.initializeComm();
+CommInitializerModule?.initializeComm();
 
 export const commCoreModule: CoreModuleSpec = global.CommCoreModule;
 export const commUtilsModule: UtilsModuleSpec = global.CommUtilsModule;
