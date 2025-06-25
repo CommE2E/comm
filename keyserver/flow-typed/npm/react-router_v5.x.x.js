0662acd94cc1e84b85a86c9116095dc74e3e3c61
Angelika Serwa <angelika.serwa@swmansion.com>
2025-06-25 12:43:43 +0200

    [Flow 262] * -> any (squash)

--- keyserver/flow-typed/npm/react-router_v5.x.x.js
+++ keyserver/flow-typed/npm/react-router_v5.x.x.js
@@ -104,7 +104,7 @@ declare module "react-router" {
 
 
   declare export class Route extends React$Component<{|
-    component?: React$ComponentType<*>,
+    component?: React$ComponentType<any>,
     render?: (router: ContextRouter) => React$Node,
     children?: React$ComponentType<ContextRouter> | React$Node,
     path?: string | Array<string>,
