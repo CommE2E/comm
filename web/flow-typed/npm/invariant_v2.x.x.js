ca7fb91a89f3a9e4dbd7ac3c404bb2cc841db5af
Angelika Serwa <angelika.serwa@swmansion.com>
2025-06-28 02:43:24 +0200

    [flow] delete definitions

--- web/flow-typed/npm/invariant_v2.x.x.js
+++
@@ -1,6 +0,0 @@
-// flow-typed signature: b8efdbfe8d3c7df1e968c260529ae86e
-// flow-typed version: c6154227d1/invariant_v2.x.x/flow_>=v0.33.x <=v0.103.x
-
-declare module invariant {
-  declare module.exports: (condition: boolean, message: string) => void;
-}
