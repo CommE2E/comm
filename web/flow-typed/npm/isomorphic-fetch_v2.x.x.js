a5b99c77e91089f1fe423952a01f93410e65a790
Angelika Serwa <angelika.serwa@swmansion.com>
2025-06-28 02:43:24 +0200

    [Flow269][skip-ci] Delete environments definitions in web, native, landing, desktop, keyserver
    Summary:
    https://linear.app/comm/issue/ENG-10205/update-flow-to-02691
    
    Now flow sees them as duplicates with lib/ environments definitions.
    
    Depends on D15106
    
    Test Plan: Flow
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D15107

--- web/flow-typed/npm/isomorphic-fetch_v2.x.x.js
+++
@@ -1,9 +0,0 @@
-// flow-typed signature: 87ecff52383db025a798afd90d2a1d56
-// flow-typed version: c6154227d1/isomorphic-fetch_v2.x.x/flow_>=v0.25.x <=v0.103.x
-
-declare module "isomorphic-fetch" {
-  declare module.exports: (
-    input: string | Request | URL,
-    init?: RequestOptions
-  ) => Promise<Response>;
-}
