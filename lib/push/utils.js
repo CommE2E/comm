CommE2E/comm

commit e23434a3ecddeff5f4819b6220adfd492e57b477

[keyserver] Refactor latest message determination logic
Summary:
This diff is a no-op; just refactoring things to prepare for later diffs.

1. Use the new `messages` returned from `fetchMessageNotifyType` instead of `threadsToMessageIndices` and `messageInfos`. In a later diff, this has the benefit of including `messageNotifyType`
2. Get rid of `flattenLatestMessagesPerUser` by simplifying logic
3. Simplify types

Depends on D14335

Test Plan: Tested in combination with later diffs, to confirm that it's possible for some message types to not set a thread to unread

Reviewers: tomek, angelika

Differential Revision: https://phab.comm.dev/D14336

lib/push/utils.js
@@ -100,7 +100,7 @@ type FetchMessageNotifyTypeInputData = {
   +fetchMessageInfoByID: (messageID: string) => Promise<any>,
   +userID: string,
 };
-type FetchMessageNotifyTypeReturnInstance = {
+export type FetchMessageNotifyTypeReturnInstance = {
   +messageNotifyType: MessageNotifyType,
   +messageInfo: RawMessageInfo,
   +messageData: MessageData,

keyserver/src/creators/message-creator.js
@@ -8,6 +8,7 @@ import { type Device, type PushUserInfo } from 'lib/push/send-utils.js';
 import {
   fetchMessageNotifyType,
   generateNotifUserInfo,
+  type FetchMessageNotifyTypeReturnInstance,
 } from 'lib/push/utils.js';
 import {
   rawMessageInfoFromMessageData,
@@ -58,23 +59,12 @@ type UserThreadInfo = {
 };
 
 type LatestMessageInfo = {
+  +userID: string,
+  +threadID: string,
   +latestMessage: string,
   +latestMessageForUnreadCheck: ?string,
   +latestReadMessage: ?string,
 };
-
-type LatestMessagesPerUser = Map<
-  string,
-  $ReadOnlyMap<string, LatestMessageInfo>,
->;
-
-type LatestMessages = $ReadOnlyArray<
-  $ReadOnly<{
-    ...LatestMessageInfo,
-    +userID: string,
-    +threadID: string,
-  }>,
->;
 
 // Does not do permission checks! (checkThreadPermission)
 async function createMessages(
@@ -297,7 +287,7 @@ async function postMessageSend(
   messageDatas: MessageData[],
   updatesForCurrentSession: UpdatesForCurrentSession,
 ) {
-  const processForSearch = processMessagesForSearch(messageInfos);
+  const processForSearchPromise = processMessagesForSearch(messageInfos);
 
   let joinIndex = 0;
   let subthreadSelects = '';
@@ -408,7 +398,7 @@ async function postMessageSend(
   const messageInfosPerUser: {
     [userID: string]: $ReadOnlyArray<RawMessageInfo>,
   } = {};
-  const latestMessagesPerUser: LatestMessagesPerUser = new Map();
+  const latestMessagePromises: Array<Promise<Array<LatestMessageInfo>>> = [];
   const userPushInfoPromises: { [string]: Promise<?PushUserInfo> } = {};
   const userRescindInfoPromises: { [string]: Promise<?PushUserInfo> } = {};
 
@@ -439,14 +429,15 @@ async function postMessageSend(
       userID,
     });
 
-    latestMessagesPerUser.set(
-      userID,
-      determineLatestMessagesPerThread(
-        preUserPushInfo,
-        userID,
-        threadsToMessageIndices,
-        messageInfos,
-      ),
+    latestMessagePromises.push(
+      (async () => {
+        const messages = await messagesPromise;
+        return determineLatestMessagesPerThread(
+          messages,
+          preUserPushInfo,
+          userID,
+        );
+      })(),
     );
 
     const userDevices = [...preUserPushInfo.devices.values()];
@@ -477,20 +468,26 @@ async function postMessageSend(
     userRescindInfoPromises[userID] = userRescindInfoPromise;
   }
 
-  const latestMessages = flattenLatestMessagesPerUser(latestMessagesPerUser);
+  const latestMessageUpdatesPromise = (async () => {
+    const unflattenedLatestMessages = await Promise.all(latestMessagePromises);
+    const latestMessages = unflattenedLatestMessages.flat();
+    await Promise.all([
+      createReadStatusUpdates(latestMessages),
+      updateLatestMessages(latestMessages),
+    ]);
+  })();
 
   const [pushInfo, rescindInfo] = await Promise.all([
     promiseAll(userPushInfoPromises),
     promiseAll(userRescindInfoPromises),
-    createReadStatusUpdates(latestMessages),
     redisPublish(viewer, messageInfosPerUser, updatesForCurrentSession),
-    updateLatestMessages(latestMessages),
-    processForSearch,
+    latestMessageUpdatesPromise,
   ]);
 
   await Promise.all([
     sendPushNotifs(_pickBy(Boolean)(pushInfo)),
     sendRescindNotifs(_pickBy(Boolean)(rescindInfo)),
+    processForSearchPromise,
   ]);
 }
 
@@ -531,19 +528,23 @@ async function redisPublish(
 }
 
 function determineLatestMessagesPerThread(
+  messages: $ReadOnlyMap<
+    string,
+    $ReadOnlyArray<FetchMessageNotifyTypeReturnInstance>,
+  >,
   preUserPushInfo: UserThreadInfo,
   userID: string,
-  threadsToMessageIndices: $ReadOnlyMap<string, $ReadOnlyArray<number>>,
-  messageInfos: $ReadOnlyArray<RawMessageInfo>,
-): $ReadOnlyMap<string, LatestMessageInfo> {
+): Array<LatestMessageInfo> {
   const { threadIDs, notFocusedThreadIDs, subthreadsCanSetToUnread } =
     preUserPushInfo;
   const latestMessagesPerThread = new Map<string, LatestMessageInfo>();
   for (const threadID of threadIDs) {
-    const messageIndices = threadsToMessageIndices.get(threadID);
-    invariant(messageIndices, `indices should exist for thread ${threadID}`);
-    for (const messageIndex of messageIndices) {
-      const messageInfo = messageInfos[messageIndex];
+    const threadMessages = messages.get(threadID);
+    if (!threadMessages) {
+      continue;
+    }
+    for (const message of threadMessages) {
+      const { messageInfo } = message;
       if (
         messageInfo.type === messageTypes.CREATE_SUB_THREAD &&
         !subthreadsCanSetToUnread.has(messageInfo.childThreadID)
@@ -578,34 +579,20 @@ function determineLatestMessagesPerThread(
       ).toString();
 
       latestMessagesPerThread.set(threadID, {
+        userID,
+        threadID,
         latestMessage,
         latestMessageForUnreadCheck: latestMessage,
         latestReadMessage,
       });
     }
   }
-  return latestMessagesPerThread;
+  return [...latestMessagesPerThread.values()];
 }
 
-function flattenLatestMessagesPerUser(
-  latestMessagesPerUser: LatestMessagesPerUser,
-): LatestMessages {
-  const result = [];
-  for (const [userID, latestMessagesPerThread] of latestMessagesPerUser) {
-    for (const [threadID, latestMessages] of latestMessagesPerThread) {
-      result.push({
-        userID,
-        threadID,
-        latestMessage: latestMessages.latestMessage,
-        latestMessageForUnreadCheck: latestMessages.latestMessageForUnreadCheck,
-        latestReadMessage: latestMessages.latestReadMessage,
-      });
-    }
-  }
-  return result;
-}
-
-async function createReadStatusUpdates(latestMessages: LatestMessages) {
+async function createReadStatusUpdates(
+  latestMessages: $ReadOnlyArray<LatestMessageInfo>,
+) {
   const now = Date.now();
   const readStatusUpdates = latestMessages
     .filter(
@@ -627,7 +614,9 @@ async function createReadStatusUpdates(latestMessages: LatestMessages) {
   await createUpdates(readStatusUpdates);
 }
 
-async function updateLatestMessages(latestMessages: LatestMessages) {
+async function updateLatestMessages(
+  latestMessages: $ReadOnlyArray<LatestMessageInfo>,
+) {
   if (latestMessages.length === 0) {
     return;
   }

commit a1d794623c8cc3ac038b9f360285ce69d75b7525

[lib] Return own messages from fetchMessageNotifyType
Summary: This addresses [ENG-10962](https://linear.app/comm/issue/ENG-10962/older-daily-updates-threads-dont-show-ashoats-responses), a regression caused by D14336.

Test Plan:
1. I tested by patching this on my personal keyserver and sending messages before and after the changes. I confirmed that after the changes, the `last_message` column in the `memberships` table was correctly updated.
2. I read through the code and analyzed all codepaths to confirm that other places where `fetchMessageNotifyType` is used won't be affected.

Reviewers: tomek

Subscribers:

lib/push/utils.js
@@ -122,16 +122,14 @@ async function fetchMessageNotifyType(
     promises.push(
       ...messageIndices.map(async messageIndex => {
         const messageInfo = newMessageInfos[messageIndex];
-        if (messageInfo.creatorID === userID) {
-          // We don't need to notify the message author about their message
-          return undefined;
-        }
-
         const { type } = messageInfo;
         const { getMessageNotifyType } = messageSpecs[type];
 
         let messageNotifyType = messageNotifyTypes.SET_UNREAD;
-        if (getMessageNotifyType) {
+        if (messageInfo.creatorID === userID) {
+          // We don't need to notify the message author about their message
+          messageNotifyType = messageNotifyTypes.NONE;
+        } else if (getMessageNotifyType) {
           messageNotifyType = await getMessageNotifyType(messageInfo, {
             notifTargetUserID: userID,
             userNotMemberOfSubthreads,