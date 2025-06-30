a504294d926fb581291393314455905b22a66680
Angelika Serwa <angelika.serwa@swmansion.com>
2025-06-30 15:50:51 +0200

    [Flow269][skip-ci] Migrate from $Diff
    Summary:
    https://linear.app/comm/issue/ENG-10205/update-flow-to-02691
    
    v0.268.0: $Diff support is removed.
    They provided a polyfill but it's not working well. `Omit` type works better.
    
    Depends on D15109
    
    Test Plan: Flow
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D15110

--- native/redux/persist.js
+++ native/redux/persist.js
@@ -1398,10 +1398,7 @@ const legacyMigrations = {
   },
 };
 
-type PersistedReportStore = $Diff<
-  ReportStore,
-  { +queuedReports: $ReadOnlyArray<ClientReportCreationRequest> },
->;
+type PersistedReportStore = Omit<ReportStore, 'queuedReports'>;
 const reportStoreTransform: Transform = createTransform(
   (state: ReportStore): PersistedReportStore => {
     return { enabledReports: state.enabledReports };
