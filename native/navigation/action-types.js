0d38f3a1d0bdca15138d70f4e0e9ac586d12bebb
Angelika Serwa <angelika.serwa@swmansion.com>
2025-07-01 14:50:41 +0200

    [Flow269][skip-ci] Fix literals exports
    Summary:
    https://linear.app/comm/issue/ENG-10205/update-flow-to-02691
    
    Depends on D15115
    
    Test Plan: Flow
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D15116

--- native/navigation/action-types.js
+++ native/navigation/action-types.js
@@ -1,20 +1,20 @@
 // @flow
 
 // RootRouter
-export const logInActionType = 'LOG_IN';
-export const logOutActionType = 'LOG_OUT';
-export const clearRootModalsActionType = 'CLEAR_ROOT_MODALS';
-export const setNavStateActionType = 'SET_NAV_STATE';
+export const logInActionType = 'LOG_IN' as const;
+export const logOutActionType = 'LOG_OUT' as const;
+export const clearRootModalsActionType = 'CLEAR_ROOT_MODALS' as const;
+export const setNavStateActionType = 'SET_NAV_STATE' as const;
 
 // OverlayRouter
-export const clearOverlayModalsActionType = 'CLEAR_OVERLAY_MODALS';
-export const setRouteParamsActionType = 'SET_ROUTE_PARAMS';
+export const clearOverlayModalsActionType = 'CLEAR_OVERLAY_MODALS' as const;
+export const setRouteParamsActionType = 'SET_ROUTE_PARAMS' as const;
 
 // ChatRouter
-export const clearScreensActionType = 'CLEAR_SCREENS';
-export const replaceWithThreadActionType = 'REPLACE_WITH_THREAD';
-export const clearThreadsActionType = 'CLEAR_THREADS';
-export const pushNewThreadActionType = 'PUSH_NEW_THREAD';
+export const clearScreensActionType = 'CLEAR_SCREENS' as const;
+export const replaceWithThreadActionType = 'REPLACE_WITH_THREAD' as const;
+export const clearThreadsActionType = 'CLEAR_THREADS' as const;
+export const pushNewThreadActionType = 'PUSH_NEW_THREAD' as const;
 
 // RegistrationRouter
-export const reconnectEthereumActionType = 'RECONNECT_ETHEREUM';
+export const reconnectEthereumActionType = 'RECONNECT_ETHEREUM' as const;
