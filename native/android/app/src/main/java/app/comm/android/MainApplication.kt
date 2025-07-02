e951d13c70679b64b14f6684e747fd11ab6959b5
Angelika Serwa <angelika.serwa@swmansion.com>
2025-07-02 14:46:39 +0200

    [native][RN80][skip-ci] Native files changes
    Summary:
    https://linear.app/comm/issue/ENG-10275/apply-the-diff-from-react-native-upgrade-helper
    
    Depends on D15103
    
    Test Plan:
    https://linear.app/comm/issue/ENG-10985/final-testing
    
    Reviewers:
    bartek
    
    Subscribers:

--- native/android/app/src/main/java/app/comm/android/MainApplication.kt
+++ native/android/app/src/main/java/app/comm/android/MainApplication.kt
@@ -8,15 +8,13 @@ import app.comm.android.commservices.CommServicesPackage
 import app.comm.android.notifications.CommAndroidNotificationsPackage
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
+import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.ReactHost
 import com.facebook.react.ReactInstanceEventListener
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.ReactContext
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.react.soloader.OpenSourceMergedSoMapping
-import com.facebook.soloader.SoLoader
 import com.wix.reactnativekeyboardinput.KeyboardInputPackage
 import expo.modules.ApplicationLifecycleDispatcher
 import expo.modules.ReactNativeHostWrapper
@@ -61,11 +59,7 @@ class MainApplication : Application(), ReactApplication {
 
   override fun onCreate() {
     super.onCreate()
-    SoLoader.init(this, OpenSourceMergedSoMapping)
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
-      load()
-    }
+    loadReactNative(this)
     ApplicationLifecycleDispatcher.onApplicationCreate(this)
     context = applicationContext
   }
