ce11f9fcc134fc0ecd05fb24feec27476e3d8209
Angelika Serwa <angelika.serwa@swmansion.com>
2025-07-02 14:41:24 +0200

    [flow] Bump babel packages

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,10 +1,12 @@
 module.exports = {
   presets: ['@babel/preset-react', '@babel/preset-flow'],
   plugins: [
+    '@babel/plugin-transform-flow-strip-types',
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-proposal-optional-chaining',
     '@babel/plugin-proposal-nullish-coalescing-operator',
+    'babel-plugin-syntax-hermes-parser',
     ['@babel/plugin-transform-runtime', { useESModules: true }],
   ],
   env: {
--- keyserver/.babelrc.cjs
+++ keyserver/.babelrc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   presets: [
     ['@babel/preset-react', { runtime: 'automatic' }],
-    '@babel/preset-flow'
+    '@babel/preset-flow',
   ],
   plugins: [
     '@babel/plugin-proposal-class-properties',
@@ -10,6 +10,7 @@ module.exports = {
     '@babel/plugin-proposal-optional-chaining',
     '@babel/plugin-transform-private-methods',
     '@babel/plugin-proposal-nullish-coalescing-operator',
+    'babel-plugin-syntax-hermes-parser',
     ['@babel/plugin-transform-runtime', { useESModules: true }],
   ],
   env: {
