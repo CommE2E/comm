9e978e46ceb3ebf4c394ed559b8a6d900ed546fa
Angelika Serwa <angelika.serwa@swmansion.com>
2025-07-02 14:46:39 +0200

    [native][RN80][skip-ci] Native files changes
    Summary:
    https://linear.app/comm/issue/ENG-10275/apply-the-diff-from-react-native-upgrade-helper
    
    Depends on D15103
    
    Test Plan: https://linear.app/comm/issue/ENG-10985/final-testing
    
    Reviewers: bartek
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D15104

--- native/android/app/src/main/java/app/comm/android/MainApplication.kt
+++ native/android/app/src/main/java/app/comm/android/MainApplication.kt
@@ -13,13 +13,11 @@ import app.comm.android.commservices.CommServicesPackage
 import app.comm.android.notifications.CommAndroidNotificationsPackage
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
+import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.react.soloader.OpenSourceMergedSoMapping
-import com.facebook.soloader.SoLoader
 import com.wix.reactnativekeyboardinput.KeyboardInputPackage
 import expo.modules.ApplicationLifecycleDispatcher
 import expo.modules.ReactNativeHostWrapper
@@ -84,11 +82,7 @@ class MainApplication : Application(), ReactApplication {
     super.onCreate()
     Security.insertProviderAt(OpenSSLProvider(), 1)
     
-    SoLoader.init(this, OpenSourceMergedSoMapping)
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
-      load()
-    }
+    loadReactNative(this)
     ApplicationLifecycleDispatcher.onApplicationCreate(this)
     try {
       val field: Field = CursorWindow::class.java.getDeclaredField("sCursorWindowSize")
