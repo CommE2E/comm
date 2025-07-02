8bcd47f6a43187b9b831e8b57fad3c7ed1e6f261
Angelika Serwa <angelika.serwa@swmansion.com>
2025-07-02 14:41:24 +0200

    [flow] Bump babel packages

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   presets: ['@babel/preset-react', '@babel/preset-flow'],
   plugins: [
+    '@babel/plugin-transform-flow-strip-types',
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-proposal-optional-chaining',
