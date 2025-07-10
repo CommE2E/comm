895a22c1a8f6193c4a4a8dc922f9e75d0ecb301f
Angelika Serwa <angelika.serwa@swmansion.com>
2025-07-10 23:26:00 +0200

    [native][RN80][skip-ci] Fix login screen flickering on startup
    Summary:
    https://linear.app/comm/issue/ENG-10980/login-screen-flicker-on-startup-on-android
    
    In AppNavigator we wait for interactions before hiding the splash screen. After RN upgrade it no longer works - waitForInteractions() resolves
    before navigation navigates to the main screen.
    So we wait for navigation to navigate before hiding the splash screen.
    
    Depends on D15130
    
    Test Plan:
    Test in release and dev mode.
    Start the app without any data.
    Start the app with logged in user.
    
    Reviewers: bartek
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D15131

--- native/root.react.js
+++ native/root.react.js
@@ -133,6 +133,7 @@ function Root() {
         | (PossiblyStaleNavigationState => GenericNavigationAction),
     ) => void>();
   const navStateInitializedRef = React.useRef(false);
+  const hasNavigatedToInitialState = React.useRef(false);
 
   // We call this here to start the loading process
   // We gate the UI on the fonts loading in AppNavigator
@@ -150,6 +151,7 @@ function Root() {
     const updatedNavContext = {
       state: navStateRef.current,
       dispatch: navDispatchRef.current,
+      hasNavigatedToInitialState: hasNavigatedToInitialState.current,
     };
     setNavContext(updatedNavContext);
     setGlobalNavContext(updatedNavContext);
@@ -164,6 +166,7 @@ function Root() {
     void (async () => {
       let loadedState = initialState;
       if (__DEV__) {
+        hasNavigatedToInitialState.current = true;
         try {
           const navStateString = await AsyncStorage.getItem(
             navStateAsyncStorageKey,
@@ -215,6 +218,7 @@ function Root() {
       invariant(state, 'nav state should be non-null');
       const prevState = navStateRef.current;
       navStateRef.current = state;
+      hasNavigatedToInitialState.current = true;
       updateNavContext();
 
       const queuedActions = queuedActionsRef.current;
