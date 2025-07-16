a784864402a95175b03baddd709748f042561881
Angelika Serwa <angelika.serwa@swmansion.com>
2025-07-16 11:09:43 +0200

    [native][RN80][skip-ci] Fix orientation on iOS
    Summary:
    https://linear.app/comm/issue/ENG-10256/upgrade-react-native-to-080-and-expo-to-sdk-53
    
    Used code from here: https://github.com/wonday/react-native-orientation-locker/issues/307
    
    Depends on D15131
    
    Test Plan: Change orientation when using camera on ios
    
    Reviewers: bartek
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D15132

--- native/ios/Comm-Bridging-Header.h
+++ native/ios/Comm-Bridging-Header.h
@@ -2,7 +2,8 @@
 //  Use this file to import your target's public headers that you would like to
 //  expose to Swift.
 //
-#import "CommIOSNotifications.h"
 #import "Comm/DBInit.h"
 #import "Comm/Tools.h"
+#import "CommIOSNotifications.h"
 #import "CommIOSServicesClient.h"
+#import "Orientation.h"
