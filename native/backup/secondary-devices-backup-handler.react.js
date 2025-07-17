65c9f079be0964de247a6eebde2128f65680ee51
xsanm <kkurowski.kk@gmail.com>
2025-07-17 15:33:38 +0200

    [lib][native] implement sending backup keys to existing secondary devices
    Summary:
    [ENG-10632](https://linear.app/comm/issue/ENG-10632/biggest-risk-implementing-a-handler-in-react-that-will-perform-a-one).
    
    This should work smoothly and handle all the issues like encryption/decryption issues, network issues, killing the app in the middle, etc., because we use the same infrastructure as for DM messages, where we solved all the problems.
    
    Additionally, an existing client without this change will decrypt the message and store in the Inbound Messages table, and process it automatically after updating the app.
    
    Depends on D14948
    
    Test Plan:
    1. Restore on primary device
    2. Add some secondary devices
    3. Flip `fullBackupSupport`
    4. User Data uploaded without any additional changes
    5. List of secondary devices is in the store
    6. Message with keys is send to all secondary devices, only once. In case of missing session or encryption issue it works too.
    
    Reviewers: bartek
    
    Reviewed By: bartek
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D14949

---
+++ native/backup/secondary-devices-backup-handler.react.js
@@ -0,0 +1,120 @@
+// @flow
+
+import * as React from 'react';
+
+import { sendBackupDataToSecondaryActionTypes } from 'lib/actions/backup-actions.js';
+import { getOwnPeerDevices } from 'lib/selectors/user-selectors.js';
+import { IdentityClientContext } from 'lib/shared/identity-client-context.js';
+import { usePeerToPeerCommunication } from 'lib/tunnelbroker/peer-to-peer-context.js';
+import { useTunnelbroker } from 'lib/tunnelbroker/tunnelbroker-context.js';
+import {
+  type BackupDataP2PMessage,
+  userActionsP2PMessageTypes,
+} from 'lib/types/tunnelbroker/user-actions-peer-to-peer-message-types.js';
+import { useDispatchActionPromise } from 'lib/utils/redux-promise-utils.js';
+import { fullBackupSupport } from 'lib/utils/services-utils.js';
+
+import { commCoreModule } from '../native-modules.js';
+import { useSelector } from '../redux/redux-utils.js';
+
+function SecondaryDevicesBackupHandler(): React.Node {
+  const identityContext = React.useContext(IdentityClientContext);
+  if (!identityContext) {
+    throw new Error('Identity service client is not initialized');
+  }
+  const { getAuthMetadata } = identityContext;
+
+  const ownDevicesWithoutBackup = useSelector(
+    state => state.backupStore.ownDevicesWithoutBackup,
+  );
+  const ownDevices = useSelector(getOwnPeerDevices);
+  const { broadcastEphemeralMessage } = usePeerToPeerCommunication();
+  const dispatchActionPromise = useDispatchActionPromise();
+  const { socketState } = useTunnelbroker();
+  const restoreBackupState = useSelector(state => state.restoreBackupState);
+
+  const sendBackupDataToSecondaryDevices = React.useCallback(async (): Promise<
+    $ReadOnlyArray<string>,
+  > => {
+    if (!ownDevicesWithoutBackup || !ownDevicesWithoutBackup.length) {
+      return [];
+    }
+
+    // Avoid sending to devices that are removed since last backup upload
+    const removedDevices = ownDevicesWithoutBackup.filter(
+      deviceID =>
+        !ownDevices.find(ownDevice => ownDevice.deviceID === deviceID),
+    );
+
+    const backupData = await commCoreModule.getQRAuthBackupData();
+
+    const authMetadata = await getAuthMetadata();
+    const { userID: thisUserID, deviceID: thisDeviceID } = authMetadata;
+    if (!thisDeviceID || !thisUserID) {
+      throw new Error('No auth metadata');
+    }
+
+    const backupDataP2PMessage: BackupDataP2PMessage = {
+      type: userActionsP2PMessageTypes.BACKUP_DATA,
+      userID: thisUserID,
+      primaryDeviceID: thisDeviceID,
+      backupData,
+    };
+    const rawPayload = JSON.stringify(backupDataP2PMessage);
+
+    const recipients =
+      ownDevicesWithoutBackup
+        ?.filter(deviceID => !removedDevices.includes(deviceID))
+        ?.map(deviceID => ({
+          deviceID,
+          userID: thisUserID,
+        })) ?? [];
+
+    const devicesThatReceivedMessage = await broadcastEphemeralMessage(
+      rawPayload,
+      recipients,
+      authMetadata,
+    );
+    return [...removedDevices, ...devicesThatReceivedMessage];
+  }, [
+    broadcastEphemeralMessage,
+    getAuthMetadata,
+    ownDevices,
+    ownDevicesWithoutBackup,
+  ]);
+
+  const executed = React.useRef(false);
+  React.useEffect(() => {
+    if (
+      !fullBackupSupport ||
+      executed.current ||
+      !socketState.isAuthorized ||
+      !ownDevicesWithoutBackup ||
+      !ownDevicesWithoutBackup.length
+    ) {
+      return;
+    }
+
+    // This condition implies that this device is primary.
+    if (restoreBackupState.status !== 'user_data_backup_success') {
+      return;
+    }
+
+    void dispatchActionPromise(
+      sendBackupDataToSecondaryActionTypes,
+      sendBackupDataToSecondaryDevices(),
+    );
+
+    executed.current = true;
+  }, [
+    dispatchActionPromise,
+    ownDevicesWithoutBackup,
+    restoreBackupState.status,
+    sendBackupDataToSecondaryDevices,
+    socketState.isAuthorized,
+  ]);
+
+  return null;
+}
+
+export { SecondaryDevicesBackupHandler };
