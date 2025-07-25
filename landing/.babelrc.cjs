a441ed545a8009008600ed7222b7a65e8649c37b
Angelika Serwa <angelika.serwa@swmansion.com>
2025-07-25 10:51:50 +0200

    [ci] CI fixes
    Summary:
    CI fixes after rn upgrade
    
    Depends on D15134
    
    Test Plan: Run CI
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D15137

--- landing/.babelrc.cjs
+++ landing/.babelrc.cjs
@@ -6,6 +6,7 @@ module.exports = {
     '@babel/plugin-proposal-optional-chaining',
     '@babel/plugin-transform-private-methods',
     '@babel/plugin-proposal-nullish-coalescing-operator',
+    'babel-plugin-syntax-hermes-parser',
     ['@babel/plugin-transform-runtime', { useESModules: true }],
   ],
 };
