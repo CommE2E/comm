8918c1d9d1f19fcb59751ee7fcc3eab5e057310c
xsanm <kkurowski.kk@gmail.com>
2025-07-28 11:58:32 +0200

    [native] implement C++ ops for QueuedDMOperations
    Summary:
    [ENG-10709](https://linear.app/comm/issue/ENG-10709/implement-c-ops-for-queueddmoperations).
    
    Depends on D15156
    
    Test Plan:
    1. Test processing ops
    2. Read entire store and check if dm ops are there
    
    Reviewers: tomek
    
    Reviewed By: tomek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D15157

--- native/cpp/CommonCpp/NativeModules/DMOperationStoreOperations.h
+++ native/cpp/CommonCpp/NativeModules/DMOperationStoreOperations.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../DatabaseManagers/entities/DMOperation.h"
+#include "../DatabaseManagers/entities/QueuedDMOperation.h"
 #include "DBOperationBase.h"
 #include "DatabaseManager.h"
 #include <vector>
@@ -38,6 +39,50 @@ public:
   virtual void execute(DatabaseIdentifier id) override {
     DatabaseManager::getQueryExecutor(id).removeAllDMOperations();
   }
+};
+class AddQueuedDMOperationOperation : public DBOperationBase {
+public:
+  AddQueuedDMOperationOperation(QueuedDMOperation &&operation)
+      : operation{std::move(operation)} {
+  }
+
+  virtual void execute(DatabaseIdentifier id) override {
+    DatabaseManager::getQueryExecutor(id).addQueuedDMOperation(this->operation);
+  }
+
+private:
+  QueuedDMOperation operation;
+};
+
+class ClearQueuedDMOperationsOperation : public DBOperationBase {
+public:
+  ClearQueuedDMOperationsOperation(std::string queueType, std::string queueKey)
+      : queueType{queueType}, queueKey{queueKey} {
+  }
+
+  virtual void execute(DatabaseIdentifier id) override {
+    DatabaseManager::getQueryExecutor(id).clearQueuedDMOperations(
+        this->queueType, this->queueKey);
+  }
+
+private:
+  std::string queueType;
+  std::string queueKey;
+};
+
+class PruneQueuedDMOperationsOperation : public DBOperationBase {
+public:
+  PruneQueuedDMOperationsOperation(std::string timestamp)
+      : timestamp{timestamp} {
+  }
+
+  virtual void execute(DatabaseIdentifier id) override {
+    DatabaseManager::getQueryExecutor(id).removeQueuedDMOperationsOlderThan(
+        this->timestamp);
+  }
+
+private:
+  std::string timestamp;
 };
 
 } // namespace comm
