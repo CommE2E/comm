6e8eb8481b1673e14b1fc74af2ef0a1d77d52f8c
xsanm <kkurowski.kk@gmail.com>
2025-08-05 12:50:27 +0200

    [tunnelbroker] implement Farcaster client
    Summary:
    [ENG-10869](https://linear.app/comm/issue/ENG-10869/farcaster-communication-layer).
    
    Initial version of client, later in the stack requests are built on clients, but after changing it to a different approach where this is handled by Tunnelbroker, it could be reused, only `payload` can change to something with stronger types.
    
    Additionally, here the token is read on each request, which makes it slow but resistant to race conditions and token changes. After working on what @tomek suggested in [here](https://linear.app/comm/issue/ENG-11060/consider-tunnelbroker-role-in-farcaster-dc-integration#comment-56be1678)this could be improved and cached
    
    Depends on D15181
    
    Test Plan: Tested using some local tests (with mocked DC token and user ID), it was also tested with client changes in the actual app, example [here](https://linear.app/comm/issue/ENG-10870/minimal-client-protocol-implementation#comment-d66114c4).
    
    Reviewers: bartek, tomek
    
    Reviewed By: bartek
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D15182

---
+++ services/tunnelbroker/src/farcaster/mod.rs
@@ -0,0 +1,107 @@
+use crate::constants::FARCASTER_REQUEST_TIMEOUT;
+use crate::database::DatabaseClient;
+use crate::farcaster::error::Error::MissingFarcasterToken;
+use reqwest::header::{HeaderMap, HeaderValue, AUTHORIZATION};
+use tracing::{debug, error};
+pub mod error;
+
+#[derive(Debug)]
+pub enum APIMethod {
+  PUT,
+  GET,
+  STREAM,
+}
+
+pub struct FarcasterAPIRequest {
+  pub request_id: String,
+  pub user_id: String,
+  /// API version, examples: "v2", "fc"
+  pub api_version: String,
+  pub endpoint: String,
+  pub method: APIMethod,
+  /// query, body, or stream message
+  pub payload: String,
+}
+
+#[derive(Clone)]
+pub struct FarcasterClient {
+  farcaster_api_url: reqwest::Url,
+  http_client: reqwest::Client,
+  db_client: DatabaseClient,
+}
+
+impl FarcasterClient {
+  pub fn new(
+    farcaster_api_url: reqwest::Url,
+    db_client: DatabaseClient,
+  ) -> Result<Self, error::Error> {
+    let http_client = reqwest::Client::builder()
+      .timeout(FARCASTER_REQUEST_TIMEOUT)
+      .build()?;
+
+    Ok(FarcasterClient {
+      farcaster_api_url,
+      http_client,
+      db_client,
+    })
+  }
+
+  pub async fn api_request(
+    &self,
+    request: FarcasterAPIRequest,
+  ) -> Result<(reqwest::StatusCode, String), error::Error> {
+    debug!(
+      "Received Farcaster {:?} {} {} request from {}",
+      request.method, request.api_version, request.endpoint, request.user_id
+    );
+
+    let farcaster_dc_token_response = self
+      .db_client
+      .get_farcaster_token(&request.user_id)
+      .await
+      .map_err(error::Error::DatabaseError)?;
+
+    let farcaster_dc_token = match farcaster_dc_token_response {
+      Some(token) => token,
+      None => return Err(MissingFarcasterToken),
+    };
+
+    let mut headers = HeaderMap::new();
+    let bearer = format!("Bearer {}", farcaster_dc_token);
+    let mut bearer_header = HeaderValue::from_str(&bearer)?;
+    bearer_header.set_sensitive(true);
+    headers.insert(AUTHORIZATION, bearer_header);
+    headers.insert(
+      reqwest::header::CONTENT_TYPE,
+      HeaderValue::from_static("application/json"),
+    );
+
+    let mut url = self.farcaster_api_url.clone();
+
+    // Append path segments
+    url
+      .path_segments_mut()
+      .expect("base URL cannot be base")
+      .extend(&[&request.api_version, &request.endpoint]);
+
+    let request_builder = match request.method {
+      APIMethod::PUT => self
+        .http_client
+        .put(url)
+        .headers(headers)
+        .body(request.payload),
+      APIMethod::GET => {
+        // Directly append entire string of params
+        url.set_query(Some(&request.payload));
+        self.http_client.get(url).headers(headers)
+      }
+      APIMethod::STREAM => {
+        error!("Received STREAM API for {}", request.endpoint);
+        return Err(error::Error::InvalidRequest);
+      }
+    };
+
+    let response = request_builder.send().await?;
+    Ok((response.status(), response.text().await?))
+  }
+}
