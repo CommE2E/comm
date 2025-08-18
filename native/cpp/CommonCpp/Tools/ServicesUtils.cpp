67f102d98fe485a5c1e6b69f4eb3855df3481157
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2025-08-18 13:21:15 +0200

    Flip the switch to enable fullBackupSupport
    Summary:
    [[ https://linear.app/comm/issue/ENG-10549/flip-the-switch-to-release-fullbackupsupport-to-public | ENG-10549 ]]
    Removed staff checks, kept original function signatures for easy revert.
    
    Test Plan: Build and run the app on all platforms.
    
    Reviewers: kamil, tomek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D15211

--- native/cpp/CommonCpp/Tools/ServicesUtils.cpp
+++ native/cpp/CommonCpp/Tools/ServicesUtils.cpp
@@ -1,12 +1,7 @@
 #include "ServicesUtils.h"
-#include "StaffUtils.h"
 
 namespace comm {
 bool ServicesUtils::fullBackupSupport() {
-#if DEBUG
   return true;
-#else
-  return StaffUtils::isStaffRelease();
-#endif
 }
 } // namespace comm
