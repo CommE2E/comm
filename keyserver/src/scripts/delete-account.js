4362d1f918ee10ff01cc90bcaff9f877a3abfd07
Ashoat Tevosyan <ashoat@gmail.com>
2025-08-19 21:41:56 -0700

    [keyserver] Make it easier to use delete-account.js script
    Summary:
    Two improvements:
    
    1. Take a username directly instead of a user ID, so I don't have to look up the user ID. I am doing this via authoritative keyserver SQL query for now since it's easy. When we stop relying on the authoritative keyserver I'll need to update it to query the identity service, but this is just a script so it's not very important to get it perfect.
    2. Take the parameter as input via CLI, so I don't need to generate a new Docker image every time I want to delete a user (this takes a while).
    
    Test Plan:
    1. I deployed this change to my personal keyserver
    2. I created a dummy test account in production
    3. I tested that the script successfully deleted the dummy test account in production
    
    Reviewers: tomek
    
    Reviewed By: tomek
    
    Differential Revision: https://phab.comm.dev/D15219

--- keyserver/src/scripts/delete-account.js
+++ keyserver/src/scripts/delete-account.js
@@ -2,13 +2,24 @@
 
 import { main } from './utils.js';
 import { deleteAccount } from '../deleters/account-deleters.js';
+import { searchForUser } from '../search/users.js';
 import { createScriptViewer } from '../session/scripts.js';
 import { privilegedDeleteUsers } from '../utils/identity-utils.js';
 
 async function deleteTargetAccount() {
-  const targetUserID = '';
-  await privilegedDeleteUsers([targetUserID]);
-  const viewer = createScriptViewer(targetUserID);
+  const targetUsername = process.argv[2];
+  if (!targetUsername) {
+    console.warn('no username');
+    return;
+  }
+  const user = await searchForUser(targetUsername);
+  if (!user) {
+    console.warn(`could not find user with username ${targetUsername}`);
+    return;
+  }
+  const { id } = user;
+  await privilegedDeleteUsers([id]);
+  const viewer = createScriptViewer(id);
   await deleteAccount(viewer);
 }
 
