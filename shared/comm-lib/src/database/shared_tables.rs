d80fe62cd7a24f26900e2085959a868c8c4f3fd0
xsanm <kkurowski.kk@gmail.com>
2025-08-22 10:30:23 +0200

    [identity][terraform] update farcaster_tokens indexes
    Summary: In D15207 I've mad some bad assumptions and previous indexes where not optimal for two reasons:
    1. For unassigned tokens, we can't efficiently query for "attribute_not_exists" on the assignedInstance-index because GSI doesn't include items where the hash key attribute doesn't exist
    2. For expired heartbeats, we can use lastHeartbeat-index but it's not ideal since lastHeartbeat as hash key means we can't do range queries efficiently
    
    This diff fixes this
    
    Test Plan: terraform plan
    
    Reviewers: bartek, tomek

--- shared/comm-lib/src/database/shared_tables.rs
+++ shared/comm-lib/src/database/shared_tables.rs
@@ -8,6 +8,7 @@
 /// - assignedInstance: Instance ID (Tunnelbroker process) that owns this token
 /// - assignmentTimestamp: When the token was assigned to instance
 /// - lastHeartbeat: When the assigned instance last sent heartbeat
+/// - unassigned: Sparse attribute set to "true" for unassigned tokens
 pub mod farcaster_tokens {
   pub const TABLE_NAME: &str = "farcaster-tokens";
   pub const PARTITION_KEY: &str = "userID";
@@ -16,7 +17,9 @@ pub mod farcaster_tokens {
   pub const ASSIGNED_INSTANCE: &str = "assignedInstance";
   pub const ASSIGNMENT_TIMESTAMP: &str = "assignmentTimestamp";
   pub const LAST_HEARTBEAT: &str = "lastHeartbeat";
+  pub const UNASSIGNED: &str = "unassigned";
 
-  pub const ASSIGNED_INSTANCE_INDEX: &str = "assignedInstance-index";
-  pub const LAST_HEARTBEAT_INDEX: &str = "lastHeartbeat-index";
+  pub const ASSIGNED_INSTANCE_LAST_HEARTBEAT_INDEX: &str =
+    "assignedInstance-lastHeartbeat-index";
+  pub const UNASSIGNED_INDEX: &str = "unassigned-index";
 }
