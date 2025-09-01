bd233b3831d6bdb59c2d5464d4555ae46273f7ca
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2025-09-01 13:09:14 +0200

    [terraform] Deploy Identity 0.54.3 to staging

--- services/terraform/remote/service_identity.tf
+++ services/terraform/remote/service_identity.tf
@@ -1,5 +1,5 @@
 locals {
-  identity_service_image_tag      = local.is_staging ? "0.54.2" : "0.54.0"
+  identity_service_image_tag      = local.is_staging ? "0.54.3" : "0.54.0"
   identity_service_server_image   = "commapp/identity-server:${local.identity_service_image_tag}"
   identity_service_container_name = "identity-server"
 
