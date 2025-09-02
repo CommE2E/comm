bd3996a7cd6240e9e290805f220d628555dda5b1
Angelika Serwa <angelika.serwa@swmansion.com>
2025-09-02 10:43:27 +0200

    [native] Fix iOS crash when typing a message
    Summary:
    https://linear.app/comm/issue/ENG-11166/crashes-when-typing-text-messages-on-ios
    
    Currently there is a trick with switching two TextInputs when sending a message. There is no context in git history about why
    it's needed (https://github.com/CommE2E/comm/commit/153a3537ab097d01a8cff6b1cc742e18799e13d6) but removing it seems to fix
    the crash.
    There is one regression though (probably the reason why we need two text inputs):
    - type a message
    - send it
    - quickly continue typing
    The message is sent, but the text input is not cleared and the next message will start with the previous message.
    This can be improved by improving app performance when sending a message. I haven't measured anything, but on dev build it's
    laggy.
    
    Test Plan:
    Try to reproduce the crash and play with text input.
    
    Reviewers:
    tomek, ashoat, bartek
    
    Subscribers:

--- native/components/clearable-text-input.react.ios.js
+++ native/components/clearable-text-input.react.ios.js
@@ -1,97 +1,14 @@
 // @flow
 
-import invariant from 'invariant';
 import * as React from 'react';
 import { TextInput as BaseTextInput, View, StyleSheet } from 'react-native';
 
 import type { ClearableTextInputProps } from './clearable-text-input.js';
 import TextInput from './text-input.react.js';
-import type { TextInputKeyPressEvent } from '../types/react-native.js';
 
-type State = {
-  +textInputKey: number,
-};
-class ClearableTextInput extends React.PureComponent<
-  ClearableTextInputProps,
-  State,
-> {
-  state: State = {
-    textInputKey: 0,
-  };
-  pendingMessage: ?{ value: string, resolve: (value: string) => void };
-  lastKeyPressed: ?string;
-  lastTextInputSent: number = -1;
-  currentTextInput: ?React.ElementRef<typeof BaseTextInput>;
-  focused: boolean = false;
-
-  sendMessage() {
-    if (this.pendingMessageSent) {
-      return;
-    }
-    const { pendingMessage } = this;
-    invariant(pendingMessage, 'cannot send an empty message');
-    pendingMessage.resolve(pendingMessage.value);
-
-    const textInputSent = this.state.textInputKey - 1;
-    if (textInputSent > this.lastTextInputSent) {
-      this.lastTextInputSent = textInputSent;
-    }
-  }
-
-  get pendingMessageSent(): boolean {
-    return this.lastTextInputSent >= this.state.textInputKey - 1;
-  }
-
-  onOldInputChangeText: (text: string) => void = text => {
-    const { pendingMessage, lastKeyPressed } = this;
-    invariant(
-      pendingMessage,
-      'onOldInputChangeText should have a pendingMessage',
-    );
-
-    if (
-      !this.pendingMessageSent &&
-      lastKeyPressed &&
-      lastKeyPressed.length > 1
-    ) {
-      // This represents an autocorrect event on blur
-      pendingMessage.value = text;
-    }
-    this.lastKeyPressed = null;
-
-    this.sendMessage();
-  };
-
-  onOldInputKeyPress: (event: TextInputKeyPressEvent) => void = event => {
-    const { key } = event.nativeEvent;
-    if (this.lastKeyPressed && this.lastKeyPressed.length > key.length) {
-      return;
-    }
-    this.lastKeyPressed = key;
-    this.props.onKeyPress && this.props.onKeyPress(event);
-  };
-
-  onOldInputBlur: () => void = () => {
-    this.sendMessage();
-  };
-
-  onOldInputFocus: () => void = () => {
-    // It's possible for the user to press the old input after the new one
-    // appears. We can prevent that with pointerEvents="none", but that causes a
-    // blur event when we set it, which makes the keyboard briefly pop down
-    // before popping back up again when textInputRef is called below. Instead
-    // we try to catch the focus event here and refocus the currentTextInput
-    if (this.currentTextInput) {
-      this.currentTextInput.focus();
-    }
-  };
-
+class ClearableTextInput extends React.PureComponent<ClearableTextInputProps> {
   textInputRef: (textInput: ?React.ElementRef<typeof BaseTextInput>) => void =
     textInput => {
-      if (this.focused && textInput) {
-        textInput.focus();
-      }
-      this.currentTextInput = textInput;
       this.props.textInputRef(textInput);
     };
 
@@ -106,73 +23,29 @@ class ClearableTextInput extends React.PureComponent<
     };
     this.props.onSelectionChange?.(fakeSelectionEvent);
 
+    const { value } = this.props;
+
     this.props.onChangeText('');
 
-    const { value } = this.props;
-    if (!this.focused) {
-      return value;
-    }
-    return await new Promise(resolve => {
-      this.pendingMessage = { value, resolve };
-      this.setState(prevState => ({
-        textInputKey: prevState.textInputKey + 1,
-      }));
-    });
+    return value;
   }
-
-  onFocus: () => void = () => {
-    this.focused = true;
-  };
-
-  onBlur: () => void = () => {
-    this.focused = false;
-    if (this.pendingMessage) {
-      // This is to catch a race condition where somebody hits the send button
-      // and then blurs the TextInput before the textInputKey increment can
-      // rerender this component. With this.focused set to false, the new
-      // TextInput won't focus, and the old TextInput won't blur, which means
-      // nothing will call sendMessage unless we do it right here.
-      this.sendMessage();
-    }
-  };
 
   render(): React.Node {
     const { textInputRef, ...props } = this.props;
 
-    const textInputs = [];
-    if (this.state.textInputKey > 0) {
-      textInputs.push(
+    return (
+      <View style={styles.textInputContainer}>
         <TextInput
           {...props}
-          style={[props.style, styles.invisibleTextInput]}
-          onChangeText={this.onOldInputChangeText}
-          onKeyPress={this.onOldInputKeyPress}
-          onBlur={this.onOldInputBlur}
-          onFocus={this.onOldInputFocus}
-          key={this.state.textInputKey - 1}
-        />,
-      );
-    }
-    textInputs.push(
-      <TextInput
-        {...props}
-        onFocus={this.onFocus}
-        onBlur={this.onBlur}
-        onChangeText={this.props.onChangeText}
-        key={this.state.textInputKey}
-        ref={this.textInputRef}
-      />,
+          onChangeText={this.props.onChangeText}
+          ref={this.textInputRef}
+        />
+      </View>
     );
-
-    return <View style={styles.textInputContainer}>{textInputs}</View>;
   }
 }
 
 const styles = StyleSheet.create({
-  invisibleTextInput: {
-    opacity: 0,
-    position: 'absolute',
-  },
   textInputContainer: {
     flex: 1,
   },
