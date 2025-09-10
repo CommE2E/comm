76d34529ff63d9779d237528c4fcb7c271b731e8
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2025-09-10 19:15:02 +0200

    [blob] Add endpoint to fetch media
    Summary:
    Endpoint that is going to be called by both Comm and Farcaster clients to download and view multimedia. Thus it has to be unauthenticated.
    It allows downloading only blobs that are multimedia blobs (have the media info attribute). Also, it takes care of setting correct response content type.
    
    Depends on D15317
    
    Test Plan: Tested on staging and in later diffs. Verified that only multimedia blobs are reachable by it
    
    Reviewers: kamil
    
    Subscribers: ashoat, tomek
    
    Differential Revision: https://phab.comm.dev/D15318

--- services/blob/src/http/handlers/media.rs
+++ services/blob/src/http/handlers/media.rs
@@ -1,5 +1,6 @@
 use crate::database::types::MediaInfo;
 use crate::http::errors::handle_blob_service_error;
+use crate::http::utils::parse_range_header;
 use crate::service::BlobService;
 
 use actix_web::error::ErrorBadRequest;
@@ -8,9 +9,71 @@ use async_stream::try_stream;
 use comm_lib::blob::types::http::BlobUploadMultimediaResponse;
 use comm_lib::blob::types::BlobInfo;
 use comm_lib::http::multipart;
+use http::uri::Scheme;
 use tokio_stream::StreamExt;
 use tracing::{info, instrument, trace, warn};
 use tracing_futures::Instrument;
+
+#[instrument(
+  name = "get_media",
+  skip_all,
+  fields(media_id = %params.as_ref().as_str(), s3_path))
+]
+pub async fn get_media_handler(
+  service: web::Data<BlobService>,
+  params: web::Path<String>,
+  range_header: Option<web::Header<Range>>,
+) -> actix_web::Result<HttpResponse> {
+  info!("Get media request");
+  let media_id = params.into_inner();
+  validate_media_id(&media_id)?;
+
+  trace!("Initializing download session");
+  let (mut download, media_info) =
+    service.create_media_download(&media_id).await?;
+
+  let total_size = download.blob_size;
+  let (range_start, range_end): (u64, u64) =
+    parse_range_header(&range_header, total_size)?;
+  download.set_byte_range(range_start..=range_end);
+  let content_length = download.download_size();
+
+  let stream = download
+    .into_stream()
+    .map(|data| match data {
+      Ok(bytes) => Ok(web::Bytes::from(bytes)),
+      Err(err) => {
+        warn!("Error during download stream: {:?}", err);
+        Err(handle_blob_service_error(&err))
+      }
+    })
+    .in_current_span();
+
+  let content_type = media_info
+    .content_type
+    .as_deref()
+    .unwrap_or("application/octet-stream");
+
+  if range_header.is_some() {
+    return Ok(
+      HttpResponse::PartialContent()
+        .content_type(content_type)
+        .append_header(("Content-Length", content_length))
+        .append_header((
+          "Content-Range",
+          format!("bytes {}-{}/{}", range_start, range_end, total_size),
+        ))
+        .streaming(Box::pin(stream)),
+    );
+  }
+
+  Ok(
+    HttpResponse::Ok()
+      .content_type(content_type)
+      .append_header(("Content-Length", content_length))
+      .streaming(Box::pin(stream)),
+  )
+}
 
 #[instrument(skip_all, name = "upload_media", fields(blob_hash))]
 pub async fn upload_media_handler(
@@ -87,4 +150,23 @@ pub async fn upload_media_handler(
     metadata,
   };
   Ok(HttpResponse::Created().json(response))
+}
+
+fn validate_media_id(media_id: &str) -> Result<(), actix_web::Error> {
+  if comm_lib::tools::is_valid_identifier(media_id) {
+    return Ok(());
+  }
+
+  if let Ok(decoded_url) = urlencoding::decode(media_id) {
+    if decoded_url.parse::<http::Uri>().is_ok_and(|uri| {
+      // TODO: maybe additional validation here
+      uri.scheme().is_some_and(|scheme| {
+        *scheme == Scheme::HTTP || *scheme == Scheme::HTTPS
+      })
+    }) {
+      return Ok(());
+    }
+  }
+
+  Err(ErrorBadRequest("bad request"))
 }
