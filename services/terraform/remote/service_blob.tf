d6899ce7aa49b0ed5a58e46209733c76a2b4fbed
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2025-09-14 12:33:10 +0200

    [terraform] Bump staging Blob to 1.4.0

--- services/terraform/remote/service_blob.tf
+++ services/terraform/remote/service_blob.tf
@@ -1,5 +1,5 @@
 locals {
-  blob_service_image_tag      = local.is_staging ? "1.3.1" : "1.3.1"
+  blob_service_image_tag      = local.is_staging ? "1.4.0" : "1.3.1"
   blob_service_container_name = "blob-service-server"
   blob_service_server_image   = "commapp/blob-server:${local.blob_service_image_tag}"
 
