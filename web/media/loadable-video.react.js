058dec375720b56e1fb73752a1ee15c5c0fbca76
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2025-09-14 10:11:40 +0200

    [web] Use video.js to support HLS video playback
    Summary:
    [[ https://linear.app/comm/issue/ENG-11252/support-viewing-hls-videos-on-web | ENG-11252 ]].
    Attempt to support HLS videos on web, based on [[ https://dev.to/indranilchutia/how-to-implement-hls-video-streaming-in-a-react-app-2cki | this article ]].
    
    Depends on D15343
    
    Test Plan: Farcaster video message is now shown and playable on both Chrome and Safari. Still, some styling/responsiveness could be improved.
    
    Reviewers: kamil, tomek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D15344

--- web/media/loadable-video.react.js
+++ web/media/loadable-video.react.js
@@ -2,10 +2,18 @@
 
 import invariant from 'invariant';
 import * as React from 'react';
+import videojs, {
+  type Player,
+  type PlayerReadyCallback,
+  type VideoJsOptions,
+} from 'video.js';
+import 'video.js/dist/video-js.css';
+
+import type { Dimensions } from 'lib/types/media-types.js';
 
 import { useFetchAndDecryptMedia } from './encryption-utils.js';
 import { preloadImage } from './media-utils.js';
-import type { CSSStyle } from '../types/styles';
+import type { CSSStyle } from '../types/styles.js';
 
 type ThumbnailSource =
   | {
@@ -21,6 +29,8 @@ type Props = {
   +thumbHashDataURL?: ?string,
   +elementStyle?: ?Partial<CSSStyle>,
   +multimediaClassName?: string,
+  +loop?: boolean,
+  +dimensions?: ?Dimensions,
 };
 
 function LoadableVideo(
@@ -33,6 +43,8 @@ function LoadableVideo(
     thumbnailSource,
     elementStyle,
     multimediaClassName,
+    loop,
+    dimensions,
   } = props;
   const { thumbnailURI, thumbnailBlobURI, thumbnailEncryptionKey } =
     thumbnailSource;
@@ -82,11 +94,44 @@ function LoadableVideo(
     fetchAndDecryptMedia,
   ]);
 
+  const poster = thumbnailImage ?? thumbHashDataURL;
+
+  const hlsPlayer = React.useMemo(() => {
+    if (!uri || !uri.endsWith('.m3u8')) {
+      return null;
+    }
+    const videoJsOptions = {
+      autoplay: false,
+      controls: true,
+      responsive: true,
+      fluid: false,
+      loop,
+      width: Math.max(dimensions?.width ?? 0, 160),
+      height: dimensions?.height,
+      poster,
+      sources: [
+        {
+          src: uri,
+          type: 'application/x-mpegURL',
+        },
+      ],
+    };
+    return (
+      <HlsVideo
+        options={videoJsOptions}
+        multimediaClassName={multimediaClassName}
+      />
+    );
+  }, [uri, multimediaClassName, loop, poster, dimensions]);
+
+  if (hlsPlayer) {
+    return hlsPlayer;
+  }
+
   let videoSource;
   if (uri) {
     videoSource = <source src={uri} />;
   }
-  const poster = thumbnailImage ?? thumbHashDataURL;
   return (
     <video
       controls
@@ -97,6 +142,54 @@ function LoadableVideo(
     >
       {videoSource}
     </video>
+  );
+}
+
+type HlsVideoProps = {
+  +options: VideoJsOptions,
+  +onReady?: PlayerReadyCallback,
+  +multimediaClassName?: string,
+};
+
+function HlsVideo(props: HlsVideoProps): React.Node {
+  const containerRef = React.useRef<?HTMLDivElement>(null);
+  const playerRef = React.useRef<?Player>(null);
+  const { options, onReady, multimediaClassName } = props;
+
+  React.useEffect(() => {
+    if (!playerRef.current) {
+      const videoElement: Element = document.createElement('video-js');
+      containerRef.current?.appendChild(videoElement);
+
+      const player: Player = videojs(videoElement, options, () => {
+        onReady?.(player);
+      });
+      playerRef.current = player;
+    } else {
+      const player = playerRef.current;
+
+      player.autoplay(options.autoplay);
+      player.src(options.sources);
+      player.loop(options.loop);
+      options.poster && player.poster(options.poster);
+    }
+  }, [options, onReady]);
+
+  React.useEffect(() => {
+    const player = playerRef.current;
+
+    return () => {
+      if (player && !player.isDisposed()) {
+        player.dispose();
+        playerRef.current = null;
+      }
+    };
+  }, []);
+
+  return (
+    <div data-vjs-player>
+      <div ref={containerRef} className={multimediaClassName} />
+    </div>
   );
 }
 
