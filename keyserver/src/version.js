3210136dda42e93db12390e7f98520d274649d9f
Ashoat Tevosyan <ashoat@gmail.com>
2025-09-15 13:13:12 -0400

    [web][keyserver] codeVersion -> 187

--- keyserver/src/version.js
+++ keyserver/src/version.js
@@ -1,3 +1,3 @@
 // @flow
 
-export const keyserverCodeVersion = 186;
+export const keyserverCodeVersion = 187;
--- web/app.react.js
+++ web/app.react.js
@@ -131,7 +131,7 @@ registerConfig({
   calendarRangeInactivityLimit: null,
   platformDetails: {
     platform: electron?.platform ?? 'web',
-    codeVersion: 186,
+    codeVersion: 187,
     stateVersion: persistConfig.version,
     ...desktopDetails,
   },
