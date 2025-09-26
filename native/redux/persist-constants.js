58a1c32854d9d4b5deb66e3c13555c18230debcc
Ashoat Tevosyan <ashoat@gmail.com>
2025-09-26 15:52:00 -0400

    [lib][native][web] Track connecting Farcaster DC alert separate from connecting Farcaster alert
    Summary:
    We currently use the same `alertTypes` constant for both of these. This has a risk that a non-staff user with a new build would see the "connect Farcaster" alert, choose to enable it, and then never see the "connect Farcaster DC" alert because `shouldSkipConnectFarcasterAlert` filters after you see the alert once.
    
    To add this new `alertTypes` constant we need to make sure it's present in the Redux store on `native`.
    
    Test Plan: Careful code inspection and Flow
    
    Reviewers: tomek, kamil
    
    Reviewed By: tomek
    
    Differential Revision: https://phab.comm.dev/D15387

--- native/redux/persist-constants.js
+++ native/redux/persist-constants.js
@@ -5,6 +5,6 @@ const rootKey = 'root';
 // NOTE: renaming this constant requires updating
 // `native/native_rust_library/build.rs` to correctly
 // scrap Redux state version from this file.
-const storeVersion = 97;
+const storeVersion = 98;
 
 export { rootKey, storeVersion };
--- web/redux/persist-constants.js
+++ web/redux/persist-constants.js
@@ -3,6 +3,6 @@
 const rootKey = 'root';
 const rootKeyPrefix = 'persist:';
 const completeRootKey = `${rootKeyPrefix}${rootKey}`;
-const storeVersion = 97;
+const storeVersion = 98;
 
 export { rootKey, rootKeyPrefix, completeRootKey, storeVersion };
