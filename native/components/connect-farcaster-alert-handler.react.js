CommE2E/comm

commit 0139cd067e9deab0fda9ab678fb7f8f113799f7c

[native] Show a modal asking about DCs connection
Summary:
https://linear.app/comm/issue/ENG-10993/show-a-modal-for-connected-farcaster-users-asking-about-dcs-connection

Modify the Farcaster connection modal to introduce a new step where users can connect their Farcaster account.

The fact that we can have users, who:
1. Didn't see the modal yet
2. Saw a modal in the past but dismissed it
3. Saw a modal and connected
4. Dismissed a modal and connected in the profile
And that we want to make the user experience good when they connect Farcaster and then DC using the modal, makes it really complicated to introduce the right logic to handle all of the options.

This diff is a reasonable tradeoff between functionality and complexity. The idea is that we reset the alert state for the Farcaster modal. This means that users who dismissed the modal in the past would need to do it again after they receive an update with the DC support - this could be both annoying and desirable, depending on the point of view.

The consequence of this diff is that:
1. Users who dismissed the modal in the past will see it again with an option to connect Farcaster first
2. Users who connected will see it again, but immediately on the DC step
3. Between the `supportsFarcasterDCs` flag is enabled and releasing this diff, users won't see the modal. It is a consequence of the migration we're doing - we clear the alert state, but we don't want to show the modal without DCs connection option.

Depends on D15093

Test Plan: Check if a modal is shown when an account isn't connected and connecting both levels is possible.
Check if a modal appears for existing Farcaster users who can now connect DCs.

Reviewers: kamil, bartek

Subscribers: ashoat

Differential Revision: https://phab.comm.dev/D15148

native/components/connect-farcaster-alert-handler.react.js
@@ -11,10 +11,12 @@ import {
 } from 'lib/types/alert-types.js';
 import {
   useCurrentUserFID,
+  useCurrentUserSupportsDCs,
   useSetLocalFID,
 } from 'lib/utils/farcaster-utils.js';
 import { shouldSkipConnectFarcasterAlert } from 'lib/utils/push-alerts.js';
 import { useDispatch } from 'lib/utils/redux-utils.js';
+import { supportsFarcasterDCs } from 'lib/utils/services-utils.js';
 import sleep from 'lib/utils/sleep.js';
 
 import { ConnectFarcasterBottomSheetRouteName } from '../navigation/route-names.js';
@@ -28,6 +30,7 @@ function ConnectFarcasterAlertHandler(): React.Node {
   const loggedIn = useIsLoggedInToIdentityAndAuthoritativeKeyserver();
 
   const fid = useCurrentUserFID();
+  const currentUserSupportsDCs = useCurrentUserSupportsDCs();
 
   const setLocalFID = useSetLocalFID();
 
@@ -38,11 +41,16 @@ function ConnectFarcasterAlertHandler(): React.Node {
   const dispatch = useDispatch();
 
   React.useEffect(() => {
+    const shouldShowForDCs = fid && !currentUserSupportsDCs;
+    const shouldShowForInitialConnection = !fid;
+
     if (
       !loggedIn ||
       !isActive ||
       shouldSkipConnectFarcasterAlert(connectFarcasterAlertInfo, fid) ||
-      connectFarcasterAlertInfo.coldStartCount < 2
+      connectFarcasterAlertInfo.coldStartCount < 2 ||
+      (!shouldShowForInitialConnection && !shouldShowForDCs) ||
+      !supportsFarcasterDCs
     ) {
       return;
     }
@@ -54,7 +62,9 @@ function ConnectFarcasterAlertHandler(): React.Node {
       // again. We set it here, rather than in the bottom sheet itself, to avoid
       // the scenario where the user connects their Farcaster account but we
       // accidentally overwrite the FID on close and set it to null.
-      setLocalFID(null);
+      if (!fid) {
+        setLocalFID(null);
+      }
       navigate(ConnectFarcasterBottomSheetRouteName);
 
       const payload: RecordAlertActionPayload = {
@@ -69,6 +79,7 @@ function ConnectFarcasterAlertHandler(): React.Node {
     })();
   }, [
     connectFarcasterAlertInfo,
+    currentUserSupportsDCs,
     dispatch,
     fid,
     isActive,

lib/components/farcaster-data-handler.react.js
@@ -198,8 +198,8 @@ function FarcasterDataHandler(props: Props): React.Node {
         await neynarClient.checkIfCurrentUserFIDIsValid(fid);
       if (!isCurrentUserFIDValid) {
         await unlinkFID();
+        return;
       }
-      return;
     }
 
     if (identityFID) {

lib/types/tunnelbroker/user-actions-peer-to-peer-message-types.js
@@ -70,13 +70,13 @@ export const backupDataP2PMessageValidator: TInterface<BackupDataP2PMessage> =
 export type FarcasterConnectionUpdated = {
   +type: 'FARCASTER_CONNECTION_UPDATED',
   +farcasterID: ?string,
-  +hasDCsToken: ?boolean,
+  +hasDCsToken: boolean,
 };
 export const farcasterConnectionUpdatedValidator: TInterface<FarcasterConnectionUpdated> =
   tShape<FarcasterConnectionUpdated>({
     type: tString(userActionsP2PMessageTypes.FARCASTER_CONNECTION_UPDATED),
     farcasterID: t.maybe(t.String),
-    hasDCsToken: t.maybe(t.Boolean),
+    hasDCsToken: t.Boolean,
   });
 
 export type UserActionP2PMessage =

lib/utils/farcaster-utils.js
@@ -21,7 +21,6 @@ import { updateTypes } from '../types/update-types-enum.js';
 
 const DISABLE_CONNECT_FARCASTER_ALERT = false;
 const NO_FID_METADATA = 'NONE';
-const NO_DCS_SUPPORT_METADATA = 'NONE';
 
 function useCurrentUserFID(): ?string {
   // There is a distinction between null & undefined for the fid value.
@@ -42,21 +41,13 @@ function useCurrentUserFID(): ?string {
   return currentUserFID;
 }
 
-function useCurrentUserSupportsDCs(): ?boolean {
-  // There is a distinction between null & undefined for the fid DCs value.
-  // If the fid DCs is null this means that the user has decided NOT to set
-  // a Farcaster DCs association. If the fid DCs is undefined this means that
-  // the user has not yet been prompted to set a Farcaster DCs association.
+function useCurrentUserSupportsDCs(): boolean {
   const currentUserFIDDCs = useSelector(
     state =>
       state.syncedMetadataStore.syncedMetadata[
         syncedMetadataNames.CURRENT_USER_SUPPORTS_DCS
       ] ?? undefined,
   );
-
-  if (currentUserFIDDCs === NO_DCS_SUPPORT_METADATA) {
-    return null;
-  }
 
   return currentUserFIDDCs === 'true';
 }
@@ -96,16 +87,13 @@ function useSetLocalFID(): (fid: ?string) => void {
   );
 }
 
-function useSetLocalCurrentUserSupportsDCs(): (connected: ?boolean) => void {
+function useSetLocalCurrentUserSupportsDCs(): (connected: boolean) => void {
   const dispatch = useDispatch();
   const { invalidateCacheForUser } = useUserIdentityCache();
   const currentUserID = useSelector(state => state.currentUserInfo?.id);
   return React.useCallback(
     (connected: ?boolean) => {
-      // If we're unsetting the DCs support, we should set it to
-      // NO_DCS_SUPPORT_METADATA to avoid prompting the user for it again
-      const connectionStatus =
-        connected === null ? NO_DCS_SUPPORT_METADATA : String(connected);
+      const connectionStatus = String(connected);
       dispatch({
         type: setSyncedMetadataEntryActionType,
         payload: {
@@ -152,7 +140,7 @@ function useLinkFID(): (fid: string) => Promise<void> {
     async (fid: string) => {
       await linkFarcasterAccount(fid);
       setLocalFID(fid);
-      await broadcastConnectionStatus(fid, null);
+      await broadcastConnectionStatus(fid, false);
     },
     [linkFarcasterAccount, setLocalFID, broadcastConnectionStatus],
   );
@@ -199,9 +187,9 @@ function useUnlinkFID(): () => Promise<void> {
 
   return React.useCallback(async () => {
     await unlinkFarcasterAccount();
-    setLocalFID(null);
-    setLocalDCsSupport(null);
-    await broadcastConnectionStatus(null, null);
+    setLocalFID(undefined);
+    setLocalDCsSupport(false);
+    await broadcastConnectionStatus(null, false);
     clearFarcasterThreads();
   }, [
     unlinkFarcasterAccount,
@@ -243,7 +231,7 @@ function useBroadcastUpdateFarcasterConnectionStatus() {
   const currentUserID = useSelector(state => state.currentUserInfo?.id);
   const userDevices = useSelector(getOwnPeerDevices);
   return React.useCallback(
-    async (farcasterID: ?string, hasDCsToken: ?boolean) => {
+    async (farcasterID: ?string, hasDCsToken: boolean) => {
       if (!currentUserID) {
         return;
       }
@@ -304,7 +292,6 @@ Issued At: ${new Date().toISOString()}`
 export {
   DISABLE_CONNECT_FARCASTER_ALERT,
   NO_FID_METADATA,
-  NO_DCS_SUPPORT_METADATA,
   useCurrentUserFID,
   useCurrentUserSupportsDCs,
   useFarcasterDCsLoaded,

lib/utils/push-alerts.js
@@ -1,8 +1,8 @@
 // @flow
 
+import { isDev } from './dev-utils.js';
+import { DISABLE_CONNECT_FARCASTER_ALERT } from './farcaster-utils.js';
 import type { AlertInfo } from '../types/alert-types.js';
-import { isDev } from '../utils/dev-utils.js';
-import { DISABLE_CONNECT_FARCASTER_ALERT } from '../utils/farcaster-utils.js';
 
 const msInDay = 24 * 60 * 60 * 1000;
 
@@ -26,7 +26,7 @@ function shouldSkipConnectFarcasterAlert(
   return (
     isDev ||
     DISABLE_CONNECT_FARCASTER_ALERT ||
-    fid !== undefined ||
+    fid === null ||
     alertInfo.totalAlerts > 0
   );
 }

native/bottom-sheet/bottom-sheet.react.js
@@ -52,6 +52,8 @@ function ForwardedBottomSheet(
       backdropComponent={BottomSheetBackdrop}
       onChange={onChange}
       enablePanDownToClose={true}
+      keyboardBehavior="interactive"
+      keyboardBlurBehavior="restore"
     >
       {children}
     </GorhomBottomSheet>

native/components/connect-farcaster-bottom-sheet.react.js
@@ -6,7 +6,11 @@ import { View, StyleSheet } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import { useIsAppForegrounded } from 'lib/shared/lifecycle-utils.js';
-import { useCurrentUserFID } from 'lib/utils/farcaster-utils.js';
+import {
+  useCurrentUserFID,
+  useCurrentUserSupportsDCs,
+} from 'lib/utils/farcaster-utils.js';
+import { supportsFarcasterDCs } from 'lib/utils/services-utils.js';
 
 import FarcasterPrompt from './farcaster-prompt.react.js';
 import FarcasterWebView, {
@@ -17,6 +21,7 @@ import { BottomSheetContext } from '../bottom-sheet/bottom-sheet-provider.react.
 import BottomSheet from '../bottom-sheet/bottom-sheet.react.js';
 import type { RootNavigationProp } from '../navigation/root-navigator.react.js';
 import type { NavigationRoute } from '../navigation/route-names.js';
+import ConnectFarcasterDCs from '../profile/connect-farcaster-dcs.react.js';
 import { useTryLinkFID } from '../utils/farcaster-utils.js';
 
 const farcasterPromptHeight = 350;
@@ -37,8 +42,10 @@ function ConnectFarcasterBottomSheet(props: Props): React.Node {
     React.useState<FarcasterWebViewState>('closed');
 
   const [isLoadingLinkFID, setIsLoadingLinkFID] = React.useState(false);
+  const [showConnectDCs, setShowConnectDCs] = React.useState(false);
 
   const fid = useCurrentUserFID();
+  const currentUserSupportsDCs = useCurrentUserSupportsDCs();
 
   const tryLinkFID = useTryLinkFID();
 
@@ -48,6 +55,9 @@ function ConnectFarcasterBottomSheet(props: Props): React.Node {
 
       try {
         await tryLinkFID(newFID);
+        if (supportsFarcasterDCs) {
+          setShowConnectDCs(true);
+        }
       } finally {
         setIsLoadingLinkFID(false);
       }
@@ -71,19 +81,45 @@ function ConnectFarcasterBottomSheet(props: Props): React.Node {
 
   React.useEffect(() => {
     if (fid && isAppForegrounded) {
-      goBack();
+      if (currentUserSupportsDCs || !supportsFarcasterDCs) {
+        goBack();
+      } else if (supportsFarcasterDCs && !showConnectDCs) {
+        setShowConnectDCs(true);
+      }
     }
-  }, [fid, goBack, isAppForegrounded]);
+  }, [fid, goBack, isAppForegrounded, currentUserSupportsDCs, showConnectDCs]);
 
   const onPressConnect = React.useCallback(() => {
     setIsLoadingLinkFID(true);
     setWebViewState('opening');
   }, []);
 
+  const onConnectDCsSuccess = React.useCallback(() => {
+    setShowConnectDCs(false);
+    goBack();
+  }, [goBack]);
+
+  const onConnectDCsCancel = React.useCallback(() => {
+    setShowConnectDCs(false);
+    goBack();
+  }, [goBack]);
+
   const connectButtonVariant = isLoadingLinkFID ? 'loading' : 'enabled';
 
-  const connectFarcasterBottomSheet = React.useMemo(
-    () => (
+  const connectFarcasterBottomSheet = React.useMemo(() => {
+    if (showConnectDCs) {
+      return (
+        <BottomSheet onClosed={goBack}>
+          <ConnectFarcasterDCs
+            onSuccess={onConnectDCsSuccess}
+            onCancel={onConnectDCsCancel}
+            useBottomSheetTextInput={true}
+          />
+        </BottomSheet>
+      );
+    }
+
+    return (
       <BottomSheet onClosed={goBack}>
         <View style={styles.container}>
           <View style={styles.promptContainer}>
@@ -97,9 +133,17 @@ function ConnectFarcasterBottomSheet(props: Props): React.Node {
         </View>
         <FarcasterWebView onSuccess={onSuccess} webViewState={webViewState} />
       </BottomSheet>
-    ),
-    [goBack, onPressConnect, connectButtonVariant, onSuccess, webViewState],
-  );
+    );
+  }, [
+    showConnectDCs,
+    goBack,
+    onConnectDCsSuccess,
+    onConnectDCsCancel,
+    onPressConnect,
+    connectButtonVariant,
+    onSuccess,
+    webViewState,
+  ]);
 
   return connectFarcasterBottomSheet;
 }

native/profile/connect-farcaster-dcs.react.js
@@ -1,5 +1,6 @@
 // @flow
 
+import { BottomSheetTextInput } from '@gorhom/bottom-sheet';
 import * as React from 'react';
 import { ScrollView, View } from 'react-native';
 
@@ -13,19 +14,25 @@ import FarcasterPrompt from '../components/farcaster-prompt.react.js';
 import PrimaryButton from '../components/primary-button.react.js';
 import { FarcasterAuthContextProvider } from '../farcaster-auth/farcaster-auth-context-provider.react.js';
 import { useGetAuthToken } from '../farcaster-auth/farcaster-auth-utils.js';
-import { useStyles } from '../themes/colors.js';
+import {
+  useStyles,
+  useColors,
+  useKeyboardAppearance,
+} from '../themes/colors.js';
 import Alert from '../utils/alert.js';
 
 type Props = {
   +onSuccess: () => void,
   +onCancel: () => void,
+  +useBottomSheetTextInput?: boolean,
 };
 
 function InnerConnectFarcasterDCs(props: Props): React.Node {
-  const { onSuccess, onCancel } = props;
+  const { onSuccess, onCancel, useBottomSheetTextInput = false } = props;
 
   const [mnemonic, setMnemonic] = React.useState<?string>(null);
   const [signingInProgress, setSigningInProgress] = React.useState(false);
+  const [focused, setFocused] = React.useState(false);
 
   const scrollViewRef =
     React.useRef<?React.ElementRef<typeof ScrollView>>(null);
@@ -58,12 +65,17 @@ function InnerConnectFarcasterDCs(props: Props): React.Node {
   }, []);
 
   const onInputFocus = React.useCallback(() => {
+    setFocused(true);
     // Scroll to make the input fully visible when focused
     setTimeout(() => {
       if (scrollViewRef.current) {
         scrollViewRef.current.scrollToEnd({ animated: true });
       }
     }, 100);
+  }, []);
+
+  const onInputBlur = React.useCallback(() => {
+    setFocused(false);
   }, []);
 
   let buttonVariant = 'enabled';
@@ -74,9 +86,71 @@ function InnerConnectFarcasterDCs(props: Props): React.Node {
   }
 
   const styles = useStyles(unboundStyles);
+  const colors = useColors();
+  const keyboardAppearance = useKeyboardAppearance();
+
+  const textInputStyle = React.useMemo(
+    () =>
+      focused
+        ? [styles.textInput, styles.focusedTextInput]
+        : [styles.textInput],
+    [focused, styles.textInput, styles.focusedTextInput],
+  );
+
+  const textInput = React.useMemo(() => {
+    if (useBottomSheetTextInput) {
+      return (
+        <BottomSheetTextInput
+          autoCapitalize="none"
+          autoComplete="off"
+          autoCorrect={false}
+          editable={!signingInProgress}
+          keyboardType="default"
+          onChangeText={onChangeMnemonicText}
+          onFocus={onInputFocus}
+          onBlur={onInputBlur}
+          onSubmitEditing={onConnect}
+          placeholder="Wallet mnemonic"
+          placeholderTextColor={colors.panelSecondaryForegroundBorder}
+          returnKeyType="go"
+          secureTextEntry={true}
+          value={mnemonic}
+          style={textInputStyle}
+          keyboardAppearance={keyboardAppearance}
+        />
+      );
+    }
+    return (
+      <RegistrationTextInput
+        autoCapitalize="none"
+        autoComplete="off"
+        autoCorrect={false}
+        editable={!signingInProgress}
+        keyboardType="default"
+        onChangeText={onChangeMnemonicText}
+        onFocus={onInputFocus}
+        onSubmitEditing={onConnect}
+        placeholder="Wallet mnemonic"
+        returnKeyType="go"
+        secureTextEntry={true}
+        value={mnemonic}
+      />
+    );
+  }, [
+    colors.panelSecondaryForegroundBorder,
+    keyboardAppearance,
+    mnemonic,
+    onChangeMnemonicText,
+    onConnect,
+    onInputBlur,
+    onInputFocus,
+    signingInProgress,
+    textInputStyle,
+    useBottomSheetTextInput,
+  ]);
 
   return (
-    <View style={styles.container}>
+    <>
       <ScrollView
         ref={scrollViewRef}
         style={styles.scrollView}
@@ -85,22 +159,7 @@ function InnerConnectFarcasterDCs(props: Props): React.Node {
       >
         <View style={styles.contentContainer}>
           <FarcasterPrompt textType="connect_DC" />
-          <View style={styles.inputContainer}>
-            <RegistrationTextInput
-              autoCapitalize="none"
-              autoComplete="off"
-              autoCorrect={false}
-              editable={!signingInProgress}
-              keyboardType="default"
-              onChangeText={onChangeMnemonicText}
-              onFocus={onInputFocus}
-              onSubmitEditing={onConnect}
-              placeholder="Wallet mnemonic"
-              returnKeyType="go"
-              secureTextEntry={true}
-              value={mnemonic}
-            />
-          </View>
+          <View style={styles.inputContainer}>{textInput}</View>
         </View>
       </ScrollView>
       <View style={styles.buttonContainer}>
@@ -111,16 +170,11 @@ function InnerConnectFarcasterDCs(props: Props): React.Node {
         />
         <PrimaryButton onPress={onCancel} label="Cancel" variant="outline" />
       </View>
-    </View>
+    </>
   );
 }
 
 const unboundStyles = {
-  container: {
-    flex: 1,
-    backgroundColor: 'panelBackground',
-    paddingBottom: 16,
-  },
   scrollView: {
     flex: 1,
   },
@@ -137,6 +191,16 @@ const unboundStyles = {
   buttonContainer: {
     marginVertical: 8,
     marginHorizontal: 16,
+  },
+  textInput: {
+    color: 'panelForegroundLabel',
+    borderColor: 'panelSecondaryForegroundBorder',
+    borderWidth: 1,
+    borderRadius: 4,
+    padding: 12,
+  },
+  focusedTextInput: {
+    borderColor: 'panelForegroundLabel',
   },
 };
 

commit 45ed67d2accb2d15100af8f8c1704ffa6823b0bd

[native] Don't break ConnectFarcasterAlertHandler for new FC users
Summary:
D15349 turned off this handler for non-staff users, which means they will never get prompted to connect FC.

Also attempting to fix some issues introduced in D15148.

Depends on D15387

Test Plan: Careful code inspection and Flow

Reviewers: tomek, kamil

Subscribers:

native/components/connect-farcaster-alert-handler.react.js
@@ -39,41 +39,60 @@ function ConnectFarcasterAlertHandler(): React.Node {
   const connectFarcasterAlertInfo = useSelector(
     state => state.alertStore.alertInfos[alertTypes.CONNECT_FARCASTER],
   );
+  const connectFarcasterDCsAlertInfo = useSelector(
+    state => state.alertStore.alertInfos[alertTypes.CONNECT_FARCASTER_DCS],
+  );
 
   const dispatch = useDispatch();
 
   React.useEffect(() => {
-    const shouldShowForDCs = fid && !currentUserSupportsDCs;
-    const shouldShowForInitialConnection = !fid;
+    const shouldShowForDCs =
+      isFarcasterDCsIntegrationEnabled && fid && !currentUserSupportsDCs;
+    const shouldShowForInitialConnection = fid === undefined;
 
+    if (!loggedIn || !isActive) {
+      return;
+    }
+
+    let reasonToShow: void | 'initial_connection' | 'dcs';
     if (
-      !loggedIn ||
-      !isActive ||
-      shouldSkipConnectFarcasterAlert(connectFarcasterAlertInfo, fid) ||
-      connectFarcasterAlertInfo.coldStartCount < 2 ||
-      (!shouldShowForInitialConnection && !shouldShowForDCs) ||
-      !isFarcasterDCsIntegrationEnabled
+      shouldShowForInitialConnection &&
+      !shouldSkipConnectFarcasterAlert(connectFarcasterAlertInfo, fid) &&
+      connectFarcasterAlertInfo.coldStartCount >= 2
     ) {
+      reasonToShow = 'initial_connection';
+    } else if (
+      shouldShowForDCs &&
+      !shouldSkipConnectFarcasterAlert(connectFarcasterDCsAlertInfo, fid)
+    ) {
+      reasonToShow = 'dcs';
+    }
+
+    if (!reasonToShow) {
       return;
     }
 
     void (async () => {
       await sleep(1000);
 
-      // We set the local FID to null to prevent the prompt from being displayed
-      // again. We set it here, rather than in the bottom sheet itself, to avoid
-      // the scenario where the user connects their Farcaster account but we
-      // accidentally overwrite the FID on close and set it to null.
-      if (!fid) {
+      // If the user hasn't been prompted to connect Farcaster yet, then their
+      // FID will be undefined. We set it to null here to avoid a race
+      // condition. If they reject the prompt, then it will be the same result.
+      // But if they accept the prompt, this avoids a race if they accept and
+      // then close out of the bottom sheet.
+      if (fid === undefined) {
         setLocalFID(null);
       }
+
       navigate(ConnectFarcasterBottomSheetRouteName);
 
       const payload: RecordAlertActionPayload = {
-        alertType: alertTypes.CONNECT_FARCASTER,
+        alertType:
+          reasonToShow === 'dcs'
+            ? alertTypes.CONNECT_FARCASTER_DCS
+            : alertTypes.CONNECT_FARCASTER,
         time: Date.now(),
       };
-
       dispatch({
         type: recordAlertActionType,
         payload,
@@ -81,6 +100,7 @@ function ConnectFarcasterAlertHandler(): React.Node {
     })();
   }, [
     connectFarcasterAlertInfo,
+    connectFarcasterDCsAlertInfo,
     currentUserSupportsDCs,
     dispatch,
     fid,