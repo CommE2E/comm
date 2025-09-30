22f8d1bdab38ff5ef1f38c85c11ed43152668846
xsanm <kkurowski.kk@gmail.com>
2025-09-30 10:56:47 +0200

    [native] show `restoreBackupState` in `BackupMenu`
    Summary:
    [ENG-11313](https://linear.app/comm/issue/ENG-11313/investigate-why-ashoat-had-no-compactions-between-september-1st-and).
    
    Later, this could be helpful during investigation similar issues.
    
    Test Plan: Check if backup state is visible
    
    Reviewers: tomek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D15394

--- native/profile/backup-menu.react.js
+++ native/profile/backup-menu.react.js
@@ -149,11 +149,24 @@ function BackupMenu(props: Props): React.Node {
     userIdentifier,
   ]);
 
+  const restoreBackupState = useSelector(state => state.restoreBackupState);
+  const restoreBackupStateStr = React.useMemo(
+    () => JSON.stringify(restoreBackupState, null, 2),
+    [restoreBackupState],
+  );
+
   return (
     <ScrollView
       contentContainerStyle={styles.scrollViewContentContainer}
       style={styles.scrollView}
     >
+      <Text style={styles.header}>RESTORE BACKUP STATE</Text>
+      <View style={styles.section}>
+        <View style={styles.submenuButton}>
+          <Text style={styles.submenuText}>{restoreBackupStateStr}</Text>
+        </View>
+      </View>
+
       <Text style={styles.header}>ACTIONS</Text>
       <View style={styles.section}>
         <Button
@@ -231,6 +244,12 @@ const unboundStyles = {
     color: 'panelForegroundLabel',
     flex: 1,
     fontSize: 16,
+  },
+  submenuButton: {
+    flexDirection: 'row',
+    paddingHorizontal: 24,
+    paddingVertical: 10,
+    alignItems: 'center',
   },
   row: {
     flexDirection: 'row',
