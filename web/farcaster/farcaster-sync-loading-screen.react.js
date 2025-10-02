9082386a0dbf5da97e17b8802fce5259a0def050
Tomasz Palys <tomasz.palys@swmansion.com>
2025-10-02 16:35:52 +0200

    [lib] Add more info to the sync screens
    Summary:
    Explain the protocol differences and note the option to sync manually
    
    {F15318553}
    
    {F15318554}
    
    https://linear.app/comm/issue/ENG-11234/inform-users-about-differences-between-protocols
    
    Test Plan: Check how the UI looks on both platforms
    
    Reviewers: kamil, ashoat
    
    Reviewed By: ashoat
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D15413

--- web/farcaster/farcaster-sync-loading-screen.react.js
+++ web/farcaster/farcaster-sync-loading-screen.react.js
@@ -35,11 +35,23 @@ function FarcasterSyncLoadingScreen(): React.Node {
           <div className={css.content}>
             <h2 className={css.header}>Fetching Farcaster conversations</h2>
             <div className={css.separator} />
-            <p className={css.description}>
-              Comm is fetching all of your Farcaster messages so they can be
-              backed up. This can take a while, depending on how many
-              conversations you have.
-            </p>
+            <ol className={css.list}>
+              <li className={css.listItem}>
+                <strong>Fetching in progress</strong>: Comm is fetching all of
+                your Farcaster messages so they can be backed up. This can take
+                a while, depending on how many chats you have.
+              </li>
+              <li className={css.listItem}>
+                <strong>No E2E encryption</strong>: Please note that Farcaster
+                messages are not end-to-end encrypted, which means the Farcaster
+                team can see them. For better security, consider using Comm DMs.
+              </li>
+              <li className={css.listItem}>
+                <strong>Manual refresh</strong>: If you ever notice any missing
+                messages, you can manually refresh all Farcaster chats from the
+                settings page, or refresh an individual chat from its settings.
+              </li>
+            </ol>
             {progressComponent}
           </div>
           <div className={css.spinner}>
