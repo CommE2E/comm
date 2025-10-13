858af98f9b34e8f7ef7a7cbc5d29b90a5c8b417b
xsanm <kkurowski.kk@gmail.com>
2025-10-13 14:39:29 +0200

    [terraform] Bump resources for Blob and Tunnelbroker
    Summary: [ENG-11359](https://linear.app/comm/issue/ENG-11359/migrate-to-fargate-or-make-sure-were-using-all-of-the-resources-on-the).
    
    Test Plan: Apply this on prod and check if deployment passed
    
    Reviewers: tomek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D15453

--- services/terraform/remote/service_tunnelbroker.tf
+++ services/terraform/remote/service_tunnelbroker.tf
@@ -158,7 +158,7 @@ resource "aws_ecs_task_definition" "tunnelbroker" {
   execution_role_arn       = aws_iam_role.ecs_task_execution.arn
   network_mode             = "bridge"
   cpu                      = "256"
-  memory                   = "256"
+  memory                   = local.is_staging ? "256" : "1024"
   requires_compatibilities = ["EC2"]
 
   # Set this to true if you want to keep old revisions
