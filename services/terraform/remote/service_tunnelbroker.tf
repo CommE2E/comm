01f4a3fbb5774529c986ed0027611f3cba38544a
xsanm <kkurowski.kk@gmail.com>
2025-10-13 15:58:06 +0200

    [terraform] Deploy Tunnelbroker to production

--- services/terraform/remote/service_tunnelbroker.tf
+++ services/terraform/remote/service_tunnelbroker.tf
@@ -1,7 +1,7 @@
 locals {
   tunnelbroker_config = {
     docker_image = "commapp/tunnelbroker"
-    docker_tag   = local.is_staging ? "0.19.9" : "0.19.8"
+    docker_tag   = local.is_staging ? "0.19.9" : "0.19.9"
 
     grpc_port      = 50051
     websocket_port = 51001
