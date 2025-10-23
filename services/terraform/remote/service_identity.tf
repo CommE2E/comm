7ddc3df5cdfe7cf35e0aac54aa57e88a9dbf3e44
xsanm <kkurowski.kk@gmail.com>
2025-10-23 17:40:08 +0200

    [terraform] route Production network to Fargate
    Summary:
    [ENG-11488]https://linear.app/comm/issue/ENG-11488/route-production-network-to-fargate).
    
    Depends on D15518
    
    Test Plan: Terraform plan
    
    Reviewers: tomek
    
    Subscribers: ashoat
    
    Differential Revision: https://phab.comm.dev/D15519

--- services/terraform/remote/service_identity.tf
+++ services/terraform/remote/service_identity.tf
@@ -286,12 +286,12 @@ resource "aws_lb_listener" "identity_service_ws" {
     forward {
       target_group {
         arn    = aws_lb_target_group.identity_service_ws.arn
-        weight = local.is_staging ? 0 : 100 # Staging: 0% EC2, Prod: 100% EC2
+        weight = 0 # 0% EC2
       }
 
       target_group {
         arn    = aws_lb_target_group.identity_service_ws_fargate.arn
-        weight = local.is_staging ? 100 : 0 # Staging: 100% Fargate, Prod: 0% Fargate
+        weight = 100 # 100% Fargate
       }
 
       stickiness {
@@ -321,12 +321,12 @@ resource "aws_lb_listener" "identity_service_grpc" {
     forward {
       target_group {
         arn    = aws_lb_target_group.identity_service_grpc.arn
-        weight = local.is_staging ? 0 : 100 # Staging: 0% EC2, Prod: 100% EC2
+        weight = 0 # 0% EC2
       }
 
       target_group {
         arn    = aws_lb_target_group.identity_service_grpc_fargate.arn
-        weight = local.is_staging ? 100 : 0 # Staging: 100% Fargate, Prod: 0% Fargate
+        weight = 100 # 100% Fargate
       }
 
       stickiness {
