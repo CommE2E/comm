CommE2E/comm

commit d167294fbc2ca8adf048f2c1f1622ca4812f5448

[lib] Support pinning messages
Summary:
Support the pinning. It requires introducing a new property to threads where the pinned message IDs are stored. Handling the C++ side is placed later in the stack.

https://linear.app/comm/issue/ENG-11220/handle-message-pinning

Test Plan: Tested in combination with the rest of the stack: both on native and web, test if pinning and unpinning works. Also tested if the pins persist app reopens. Checked if the pins are correctly saved on Farcaster.

Reviewers: kamil

Subscribers: ashoat

Differential Revision: https://phab.comm.dev/D15498

lib/selectors/chat-selectors.js
@@ -29,7 +29,10 @@ import {
   type SidebarItem,
 } from '../shared/sidebar-item-utils.js';
 import { threadInChatList, threadIsPending } from '../shared/thread-utils.js';
-import { threadTypeIsSidebar } from '../shared/threads/thread-specs.js';
+import {
+  threadSpecs,
+  threadTypeIsSidebar,
+} from '../shared/threads/thread-specs.js';
 import { messageTypes } from '../types/message-types-enum.js';
 import {
   type ComposableMessageInfo,
@@ -450,22 +453,29 @@ function createChatMessageItems(
     targetMessageEditMap.set(messageInfo.targetMessageID, messageInfo.text);
   }
 
+  const threadInfo = threadInfos[threadID];
   const targetMessagePinStatusMap = new Map<string, boolean>();
-  // Once again, we iterate backwards to put the order of messages in
-  // chronological order (i.e. oldest to newest) to handle pinned messages.
-  // This is important because we want to make sure that the most recent pin
-  // action is the one that is used to determine whether a message
-  // is pinned or not.
-  for (let i = messages.length - 1; i >= 0; i--) {
-    const messageInfo = messages[i];
-    if (messageInfo.type !== messageTypes.TOGGLE_PIN) {
-      continue;
-    }
+  if (threadSpecs[threadInfo.type].protocol().pinsStoredOnServer) {
+    // Once again, we iterate backwards to put the order of messages in
+    // chronological order (i.e. oldest to newest) to handle pinned messages.
+    // This is important because we want to make sure that the most recent pin
+    // action is the one that is used to determine whether a message
+    // is pinned or not.
+    for (let i = messages.length - 1; i >= 0; i--) {
+      const messageInfo = messages[i];
+      if (messageInfo.type !== messageTypes.TOGGLE_PIN) {
+        continue;
+      }
 
-    targetMessagePinStatusMap.set(
-      messageInfo.targetMessageID,
-      messageInfo.action === 'pin',
-    );
+      targetMessagePinStatusMap.set(
+        messageInfo.targetMessageID,
+        messageInfo.action === 'pin',
+      );
+    }
+  } else {
+    for (const messageID of threadInfo.pinnedMessageIDs ?? []) {
+      targetMessagePinStatusMap.set(messageID, true);
+    }
   }
 
   const targetMessageDeleteStatusMap = new Map<string, boolean>();
@@ -602,7 +612,6 @@ function createChatMessageItems(
       return result;
     })();
 
-    const threadInfo = threadInfos[threadID];
     const parentThreadInfo = threadInfo?.parentThreadID
       ? threadInfos[threadInfo.parentThreadID]
       : null;

lib/shared/farcaster/farcaster-api.js
@@ -925,6 +925,40 @@ function useAcceptOneOnOneInvite(): (
   );
 }
 
+export type PinMessageInput = {
+  +conversationId: string,
+  +messageId: string,
+};
+
+function usePinMessage(): (input: PinMessageInput) => Promise<void> {
+  const { sendFarcasterRequest } = useTunnelbroker();
+  const { addLog } = useDebugLogs();
+  return React.useCallback(
+    async (input: PinMessageInput) => {
+      try {
+        await sendFarcasterRequest({
+          apiVersion: 'v2',
+          endpoint: 'direct-cast-pin-message',
+          method: { type: 'POST' },
+          payload: JSON.stringify(input),
+        });
+      } catch (error) {
+        addLog(
+          'Farcaster API: Failed to pin a message',
+          JSON.stringify({
+            conversationId: input.conversationId,
+            messageId: input.messageId,
+            error: getMessageForException(error),
+          }),
+          new Set([logTypes.FARCASTER, logTypes.ERROR]),
+        );
+        throw error;
+      }
+    },
+    [addLog, sendFarcasterRequest],
+  );
+}
+
 export {
   useSendFarcasterTextMessage,
   useFetchFarcasterMessages,
@@ -942,4 +976,5 @@ export {
   useAcceptInvite,
   useFetchFarcasterConversationInvites,
   useAcceptOneOnOneInvite,
+  usePinMessage,
 };

lib/shared/farcaster/farcaster-hooks.js
@@ -429,6 +429,8 @@ type ConversationFetchResult = {
   +farcasterConversation: FarcasterConversation,
   +thread: FarcasterRawThreadInfo,
   +threadMembers: Array<MemberInfoSansPermissions>,
+  +pinnedMessages: Array<RawMessageInfo>,
+  +userIDs: Array<string>,
 };
 async function fetchAndProcessConversation(
   conversationID: string,
@@ -553,10 +555,23 @@ async function fetchAndProcessConversation(
     members: threadMembers,
   };
 
+  const userFIDs = farcasterConversation.pinnedMessages.flatMap(message =>
+    extractFarcasterIDsFromPayload(farcasterMessageValidator, message),
+  );
+  const fetchedUserInfos = await fetchUsersByFIDs(userFIDs);
+  const pinnedMessages = farcasterConversation.pinnedMessages.flatMap(message =>
+    convertFarcasterMessageToCommMessages(message, fetchedUserInfos, addLog),
+  );
+  const userIDs = Array.from(fetchedUserInfos.entries()).map(
+    ([fid, user]) => user?.userID ?? userIDFromFID(fid),
+  );
+
   return {
     farcasterConversation,
     thread,
     threadMembers,
+    pinnedMessages,
+    userIDs,
   };
 }
 
@@ -615,6 +630,8 @@ function useFetchConversationWithBatching(): (
           threadMembers.forEach(member => batchedUpdates.addUserID(member.id));
         }
         batchedUpdates.addUpdateInfo(update);
+        batchedUpdates.addAdditionalMessageInfos(result.pinnedMessages);
+        batchedUpdates.addUserIDs(result.userIDs);
 
         return farcasterConversation;
       } catch (e) {
@@ -735,6 +752,8 @@ function useFetchConversationWithMessages(): (
           rawEntryInfos: [],
         };
         batchedUpdates.addUpdateInfo(update);
+        batchedUpdates.addAdditionalMessageInfos(result.pinnedMessages);
+        batchedUpdates.addUserIDs(result.userIDs);
 
         return farcasterConversation;
       } catch (e) {

lib/shared/thread-utils.js
@@ -952,6 +952,14 @@ function threadInfoFromRawThreadInfo(
   if (pinnedCount) {
     threadInfo = { ...threadInfo, pinnedCount };
   }
+
+  if (rawThreadInfo.pinnedMessageIDs) {
+    threadInfo = {
+      ...threadInfo,
+      pinnedMessageIDs: rawThreadInfo.pinnedMessageIDs,
+    };
+  }
+
   return threadInfo;
 }
 

lib/shared/threads/protocols/dm-thread-protocol.js
@@ -954,6 +954,7 @@ const dmThreadProtocol: ThreadProtocol<MinimallyEncodedThickMemberInfo> =
     supportsThreadRefreshing: false,
     supportsMessageEdit: true,
     supportsRelationships: true,
+    pinsStoredOnServer: false,
   });
 
 function pendingThreadType(numberOfOtherMembers: number) {

lib/shared/threads/protocols/farcaster-thread-protocol.js
@@ -726,6 +726,13 @@ const farcasterThreadProtocol: ThreadProtocol<MemberInfoSansPermissions> = {
       };
     }
 
+    if (clientDBThreadInfo.pinnedMessageIDs) {
+      rawThreadInfo = {
+        ...rawThreadInfo,
+        pinnedMessageIDs: JSON.parse(clientDBThreadInfo.pinnedMessageIDs),
+      };
+    }
+
     return rawThreadInfo;
   },
 
@@ -1032,10 +1039,11 @@ const farcasterThreadProtocol: ThreadProtocol<MemberInfoSansPermissions> = {
   supportsThreadRefreshing: true,
   temporarilyDisabledFeatures: {
     changingThreadAvatar: true,
-    pinningMessages: true,
+    pinningMessages: false,
   },
   supportsMessageEdit: false,
   supportsRelationships: false,
+  pinsStoredOnServer: false,
 };
 
 function pendingThreadType(numberOfOtherMembers: number) {

lib/shared/threads/protocols/keyserver-thread-protocol.js
@@ -757,6 +757,7 @@ const keyserverThreadProtocol: ThreadProtocol<MemberInfoSansPermissions> =
     supportsThreadRefreshing: false,
     supportsMessageEdit: true,
     supportsRelationships: true,
+    pinsStoredOnServer: true,
   });
 
 function pendingThreadType(numberOfOtherMembers: number) {

lib/shared/threads/thread-spec.js
@@ -559,6 +559,7 @@ export type ThreadProtocol<
   },
   +supportsMessageEdit: boolean,
   +supportsRelationships: boolean,
+  +pinsStoredOnServer: boolean,
 };
 
 export type ThreadSpec<

commit ab6ec57d1bb9461df3d9216d24e590281729254c

[lib] Unbreak pending threads
Broken in D15498, context in [ENG-11511](https://linear.app/comm/issue/ENG-11511/app-crash-when-message-selected).

lib/selectors/chat-selectors.js
@@ -455,7 +455,10 @@ function createChatMessageItems(
 
   const threadInfo = threadInfos[threadID];
   const targetMessagePinStatusMap = new Map<string, boolean>();
-  if (threadSpecs[threadInfo.type].protocol().pinsStoredOnServer) {
+  if (
+    threadInfo &&
+    threadSpecs[threadInfo.type].protocol().pinsStoredOnServer
+  ) {
     // Once again, we iterate backwards to put the order of messages in
     // chronological order (i.e. oldest to newest) to handle pinned messages.
     // This is important because we want to make sure that the most recent pin
@@ -472,7 +475,7 @@ function createChatMessageItems(
         messageInfo.action === 'pin',
       );
     }
-  } else {
+  } else if (threadInfo) {
     for (const messageID of threadInfo.pinnedMessageIDs ?? []) {
       targetMessagePinStatusMap.set(messageID, true);
     }