8a8611d1eeb7a108f5fa5934ed5ab060b6cf2c5e
Ashoat Tevosyan <ashoat@gmail.com>
2025-10-23 17:51:08 -0400

    [terraform] Bump keyserver image to 1.0.204
    This is for web & landing as well as keyserver (`self-host` directory, running on personal AWS)

--- services/terraform/remote/shared_webapp_landing.tf
+++ services/terraform/remote/shared_webapp_landing.tf
@@ -1,5 +1,5 @@
 locals {
-  keyserver_image_tag = "1.0.203"
+  keyserver_image_tag = "1.0.204"
   keyserver_image     = "commapp/keyserver:${local.keyserver_image_tag}"
 
   shared_keyserver_environment_vars = {
