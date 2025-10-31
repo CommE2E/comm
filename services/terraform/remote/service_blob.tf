1b7b9a56214ef9619255a117973f7179581874fe
xsanm <kkurowski.kk@gmail.com>
2025-10-31 16:41:28 +0100

    [terraform] remove EC2-based resources
    Summary: [ENG-11486](https://linear.app/comm/issue/ENG-11486/shut-down-ec2-services).
    
    All EC2-based ECS resources for services are now gone (EC2 service, target group, and task definition)
    
    Test Plan: terraform apply and test staging
    
    Reviewers: tomek, ashoat

--- services/terraform/remote/service_blob.tf
+++ services/terraform/remote/service_blob.tf
@@ -21,105 +21,7 @@ locals {
   blob_service_s3_bucket           = "commapp-blob${local.s3_bucket_name_suffix}"
 }
 
-resource "aws_ecs_task_definition" "blob_service" {
-  family = "blob-service-task-def"
-  container_definitions = jsonencode([
-    {
-      name      = local.blob_service_container_name
-      image     = local.blob_service_server_image
-      essential = true
-      portMappings = [
-        {
-          name          = local.blob_sc_port_name
-          containerPort = local.blob_service_container_http_port
-          protocol      = "tcp"
-          appProtocol   = "http"
-        }
-      ]
-      environment = [
-        {
-          name  = "RUST_LOG"
-          value = local.is_staging ? "info,blob=debug,comm_lib=debug" : "info"
-        },
-        {
-          name  = "BLOB_S3_BUCKET_NAME",
-          value = local.blob_service_s3_bucket
-        },
-        {
-          name  = "IDENTITY_SERVICE_ENDPOINT",
-          value = local.identity_local_url
-        },
-        {
-          name  = "COMM_SERVICES_DISABLE_CSAT_VERIFICATION",
-          value = local.is_staging ? "false" : "true"
-        },
-        {
-          name  = "COMM_SERVICES_USE_JSON_LOGS",
-          value = local.comm_services_use_json_logs
-        }
-      ]
-      logConfiguration = {
-        "logDriver" = "awslogs"
-        "options" = {
-          "awslogs-create-group"  = "true"
-          "awslogs-group"         = "/ecs/blob-service-task-def"
-          "awslogs-region"        = "us-east-2"
-          "awslogs-stream-prefix" = "ecs"
-        }
-      }
-    }
-  ])
-  task_role_arn            = aws_iam_role.services_ddb_full_access.arn
-  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
-  network_mode             = "bridge"
-  cpu                      = "512"
-  memory                   = local.is_staging ? "512" : "2048"
-  requires_compatibilities = ["EC2"]
 
-  # Set this to true if you want to keep old revisions
-  # when this definition is changed
-  skip_destroy = false
-}
-
-resource "aws_ecs_service" "blob_service" {
-  name        = "blob-service"
-  cluster     = aws_ecs_cluster.comm_services.id
-  launch_type = "EC2"
-
-  task_definition      = aws_ecs_task_definition.blob_service.arn
-  force_new_deployment = true
-
-  desired_count = 0
-
-  lifecycle {
-    ignore_changes = [desired_count]
-  }
-
-  # Expose Blob service to other services in the cluster
-  service_connect_configuration {
-    enabled = true
-    service {
-      discovery_name = local.blob_sc_dns_name
-      port_name      = local.blob_sc_port_name
-      client_alias {
-        port     = local.blob_service_container_http_port
-        dns_name = local.blob_sc_dns_name
-      }
-    }
-  }
-
-  # HTTP
-  load_balancer {
-    target_group_arn = aws_lb_target_group.blob_service_http.arn
-    container_name   = local.blob_service_container_name
-    container_port   = local.blob_service_container_http_port
-  }
-
-  deployment_circuit_breaker {
-    enable   = true
-    rollback = true
-  }
-}
 
 # Security group to configure access to the service
 resource "aws_security_group" "blob_service" {
@@ -147,25 +49,6 @@ resource "aws_security_group" "blob_service" {
   }
 }
 
-resource "aws_lb_target_group" "blob_service_http" {
-  name     = "blob-service-ecs-http-tg"
-  port     = local.blob_service_container_http_port
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.default.id
-
-  # ECS Fargate requires target type set to IP
-  target_type = "instance"
-
-  health_check {
-    enabled             = true
-    healthy_threshold   = 2
-    unhealthy_threshold = 3
-
-    protocol = "HTTP"
-    path     = "/health"
-    matcher  = "200-499"
-  }
-}
 
 # Load Balancer
 resource "aws_lb" "blob_service" {
