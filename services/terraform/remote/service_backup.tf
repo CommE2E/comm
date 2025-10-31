5fcce8aae5a1302062de1cc1b886f805558372fb
xsanm <kkurowski.kk@gmail.com>
2025-10-31 16:41:28 +0100

    [terraform] remove EC2-based resources
    Summary: [ENG-11486](https://linear.app/comm/issue/ENG-11486/shut-down-ec2-services).
    
    All EC2-based ECS resources for services are now gone (EC2 service, target group, and task definition)
    
    Depends on 15537
    
    Test Plan: terraform apply and test staging
    
    Reviewers: tomek, ashoat

--- services/terraform/remote/service_backup.tf
+++ services/terraform/remote/service_backup.tf
@@ -17,108 +17,7 @@ locals {
   backup_fargate_url = "http://${local.backup_sc_dns_name}-fargate:${local.backup_service_container_http_port}"
 }
 
-resource "aws_ecs_task_definition" "backup_service" {
-  family = "backup-service-task-def"
-  container_definitions = jsonencode([
-    {
-      name      = local.backup_service_container_name
-      image     = local.backup_service_server_image
-      essential = true
-      portMappings = [
-        {
-          name          = local.backup_sc_port_name
-          containerPort = local.backup_service_container_http_port
-          protocol      = "tcp"
-          appProtocol   = "http"
-        },
-      ]
-      environment = [
-        {
-          name  = "RUST_LOG"
-          value = local.is_staging ? "info,backup=debug,comm_lib=debug" : "info"
-        },
-        {
-          name  = "BLOB_SERVICE_URL",
-          value = local.blob_local_url
-          # If this ever fails, we can fallback to blob public URL:
-          # "https://${local.blob_service_domain_name}"
-        },
-        {
-          name  = "IDENTITY_SERVICE_ENDPOINT",
-          value = local.identity_local_url
-        },
-        {
-          name  = "COMM_SERVICES_DISABLE_CSAT_VERIFICATION",
-          value = local.is_staging ? "false" : "true"
-        },
-        {
-          name  = "COMM_SERVICES_USE_JSON_LOGS",
-          value = local.comm_services_use_json_logs
-        }
-      ]
-      logConfiguration = {
-        "logDriver" = "awslogs"
-        "options" = {
-          "awslogs-create-group"  = "true"
-          "awslogs-group"         = "/ecs/backup-service-task-def"
-          "awslogs-region"        = "us-east-2"
-          "awslogs-stream-prefix" = "ecs"
-        }
-      }
-    }
-  ])
-  task_role_arn            = aws_iam_role.backup_service.arn
-  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
-  network_mode             = "bridge"
-  cpu                      = "1024"
-  memory                   = "4096"
-  requires_compatibilities = ["EC2"]
 
-  # Set this to true if you want to keep old revisions
-  # when this definition is changed
-  skip_destroy = false
-}
-
-resource "aws_ecs_service" "backup_service" {
-  name        = "backup-service"
-  cluster     = aws_ecs_cluster.comm_services.id
-  launch_type = "EC2"
-
-  task_definition      = aws_ecs_task_definition.backup_service.arn
-  force_new_deployment = true
-
-  desired_count = 0
-  lifecycle {
-    ignore_changes = [desired_count]
-  }
-
-  service_connect_configuration {
-    enabled = true
-    service {
-      discovery_name = local.backup_sc_dns_name
-      port_name      = local.backup_sc_port_name
-      client_alias {
-        port     = local.backup_service_container_http_port
-        dns_name = local.backup_sc_dns_name
-      }
-    }
-  }
-
-  # HTTP
-  load_balancer {
-    target_group_arn = aws_lb_target_group.backup_service_http.arn
-    container_name   = local.backup_service_container_name
-    container_port   = local.backup_service_container_http_port
-  }
-
-  deployment_circuit_breaker {
-    enable   = true
-    rollback = true
-  }
-
-  enable_execute_command  = true
-  enable_ecs_managed_tags = true
-}
 
 # Security group to configure access to the service
 resource "aws_security_group" "backup_service" {
@@ -146,24 +45,6 @@ resource "aws_security_group" "backup_service" {
   }
 }
 
-resource "aws_lb_target_group" "backup_service_http" {
-  name     = "backup-service-ecs-http-tg"
-  port     = local.backup_service_container_http_port
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.default.id
-
-  target_type = "instance"
-
-  health_check {
-    enabled             = true
-    healthy_threshold   = 2
-    unhealthy_threshold = 3
-
-    protocol = "HTTP"
-    path     = "/health"
-    matcher  = "200-204"
-  }
-}
 
 # Load Balancer
 resource "aws_lb" "backup_service" {
