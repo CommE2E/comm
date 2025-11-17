61113c608df582db56d25ca8ec06ba4ae1eaeb7e
xsanm <kkurowski.kk@gmail.com>
2025-11-17 11:11:10 +0100

    [native][lib] remove feature flag service client code
    Summary:
    [ENG-11543](https://linear.app/comm/issue/ENG-11543/feature-flags-service-is-down).
    
    As part of [ENG-11521](https://linear.app/comm/issue/ENG-11521/consider-lowering-fargate-resource-usage-on-prod) I shut down Feature Flag service because it is not used at all, we should also remove client code to make it cleaner and avoid network calls.
    
    Test Plan: Flow
    
    Reviewers: ashoat
    
    Reviewed By: ashoat
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D15544

--- native/utils/crash-utils.js
+++ native/utils/crash-utils.js
@@ -4,7 +4,6 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import sleep from 'lib/utils/sleep.js';
 
-import { featureFlagsStorageKey } from '../components/feature-flags-provider.react.js';
 import { clearSensitiveData } from '../data/sqlite-data-handler.js';
 import { commCoreModule } from '../native-modules.js';
 import { navStateAsyncStorageKey } from '../navigation/persistance.js';
@@ -14,7 +13,6 @@ async function wipeAndExit() {
   await Promise.all([
     __DEV__ ? AsyncStorage.removeItem(navStateAsyncStorageKey) : null,
     AsyncStorage.removeItem('ANDROID_REFERRER'),
-    AsyncStorage.removeItem(featureFlagsStorageKey),
     clearSensitiveData(),
   ]);
   await getPersistor().purge();
