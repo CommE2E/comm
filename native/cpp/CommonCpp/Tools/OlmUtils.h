b943500c76a22130887eb8b7952b236facfb8c18
xsanm <kkurowski.kk@gmail.com>
2025-11-24 13:12:08 +0100

    [native] replace Olm with Vodozemac's Olm
    Summary:
    [ENG-11531](https://linear.app/comm/issue/ENG-11531/migrate-the-full-crypto-api-to-vodozemac).
    
    Making vodozemac available on native.
    
    Depends on D15549
    
    Test Plan: Test encrypt/decrypt and creating session with Olm and Vodozemac.
    
    Reviewers: ashoat
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D15550

--- native/cpp/CommonCpp/Tools/OlmUtils.h
+++ native/cpp/CommonCpp/Tools/OlmUtils.h
@@ -5,14 +5,6 @@
 namespace comm {
 
 /**
- * Parses a OLM prekey string from JSON format
- * @param prekeyBlob JSON string with prekey
- * @return The prekey string extracted from the JSON
- * @throws std::runtime_error if parsing fails
- */
-std::string parseOLMPrekey(std::string prekeyBlob);
-
-/**
  * Extracts the ed25519 signing public key from an identity keys JSON blob
  * @param identityKeysBlob JSON string with identity keys
  * @return The ed25519 public key string
