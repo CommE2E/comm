381031cea2a05fc39df95c993c8a90dd35e2e0c8
Ashoat Tevosyan <ashoat@gmail.com>
2026-01-18 16:31:48 -0800

    [identity] Bump min client version to require Vodozemac
    Summary: Part 1 of [ENG-11554](https://linear.app/comm/issue/ENG-11554/deprecate-supporting-device-with-olm).
    
    Test Plan: N/A
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D15557

--- services/identity/src/constants.rs
+++ services/identity/src/constants.rs
@@ -269,8 +269,8 @@ pub const ONE_TIME_KEY_REFRESH_NUMBER: u32 = 5;
 
 // Minimum supported code versions
 
-pub const MIN_SUPPORTED_NATIVE_VERSION: u64 = 446;
-pub const MIN_SUPPORTED_WEB_VERSION: u64 = 146;
+pub const MIN_SUPPORTED_NATIVE_VERSION: u64 = 559;
+pub const MIN_SUPPORTED_WEB_VERSION: u64 = 208;
 
 // Request metadata
 
