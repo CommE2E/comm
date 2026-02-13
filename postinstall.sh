90065e203156dd4afd93bdbdf97b40dc52f755dc
Ashoat Tevosyan <ashoat@gmail.com>
2026-02-13 12:16:11 -0500

    Reduce Buildkite CI flakiness
    Summary:
    1. Increase default `BUNDLE_TIMEOUT` from 10 seconds to 30 seconds
    2. Get rid of `pod repo update` in Buildkite jobs since we ended up adding this to `postinstall.sh` anyways
    3. Instead of always doing `pod repo update` in `postinstall.sh`, do it as a fallback if the first attempt fails
    4. Add `--deployment` so `Podfile.lock` isn't mutated by `postinstall.sh`
    
    Test Plan: We'll see if it works
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D15573

--- postinstall.sh
+++ postinstall.sh
@@ -13,5 +13,40 @@ yarn flow-mono create-symlinks native
 yarn workspace native jetify
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  (cd native/ios && bundle install && MACOSX_DEPLOYMENT_TARGET='' DEVELOPER_DIR=$(/usr/bin/xcode-select -p) bundle exec pod install --repo-update)
+  (
+    cd native/ios
+
+    export BUNDLE_TIMEOUT="${BUNDLE_TIMEOUT:-60}"
+    export BUNDLE_RETRY="${BUNDLE_RETRY:-6}"
+    export MACOSX_DEPLOYMENT_TARGET=''
+    DEVELOPER_DIR=$(/usr/bin/xcode-select -p)
+    export DEVELOPER_DIR
+
+    bundle_install_attempt=1
+    max_bundle_install_attempts=3
+    while true; do
+      if bundle install; then
+        break
+      fi
+
+      bundle_install_exit_code=$?
+      if [[ $bundle_install_attempt -ge $max_bundle_install_attempts ]]; then
+        exit "$bundle_install_exit_code"
+      fi
+
+      sleep_seconds=$((bundle_install_attempt * 15))
+      echo "bundle install failed (exit ${bundle_install_exit_code}), retrying in ${sleep_seconds}s..."
+      sleep "$sleep_seconds"
+      bundle_install_attempt=$((bundle_install_attempt + 1))
+    done
+
+    pod_install_exit_code=0
+    bundle exec pod install --deployment || pod_install_exit_code=$?
+
+    if [[ $pod_install_exit_code -ne 31 ]]; then
+      exit "$pod_install_exit_code"
+    fi
+
+    bundle exec pod install --repo-update --deployment
+  )
 fi
