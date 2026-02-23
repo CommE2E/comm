63e298cff0767d3328140f1e238d5fe2762567d3
Ashoat Tevosyan <ashoat@gmail.com>
2026-02-22 19:29:54 -0700

    [native] Extract AndroidAbiHelpers.groovy from native/android/app/build.gradle
    Summary: Depends on D15582
    
    Test Plan: CI
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D15583

---
+++ native/android/buildSrc/src/main/groovy/app/comm/gradle/AndroidAbiHelpers.groovy
@@ -0,0 +1,43 @@
+package app.comm.gradle
+
+import java.io.ByteArrayOutputStream
+import org.gradle.api.Project
+
+class AndroidAbiHelpers {
+  private static List<String> reactNativeArchitectures(Project project) {
+    def value = project.getProperties().get('reactNativeArchitectures')
+    value ? (value.split(',') as List<String>) : ['armeabi-v7a', 'x86_64', 'arm64-v8a']
+  }
+
+  static List<String> getBuildTypeABIs(Project project) {
+    if (System.getenv('BUILDKITE') == 'true') {
+      return ['arm64-v8a']
+    }
+
+    def isBundleRelease = project.gradle.startParameter.taskRequests.any {
+      !it.args.isEmpty() && it.args.first().contains('bundleRelease')
+    }
+    if (isBundleRelease) {
+      // All of the supported ABIs
+      // https://developer.android.com/ndk/guides/abis.html#sa
+      final allAbis = ['armeabi-v7a', 'arm64-v8a', 'x86_64']
+      project.logger.info("Using all architectures to build: ${allAbis}")
+      return allAbis
+    }
+
+    def nativeArchitectures = reactNativeArchitectures(project)
+    if (nativeArchitectures) {
+      return nativeArchitectures as List<String>
+    }
+
+    // Get current 'adb devices' architectures
+    def commandOutput = new ByteArrayOutputStream()
+    project.exec {
+      commandLine './bash/detect_abis.sh'
+      standardOutput = commandOutput
+    }
+    final detectedAbis = commandOutput.toString('UTF-8').trim().tokenize()
+    project.logger.info("Detected architectures to build: ${detectedAbis}")
+    detectedAbis
+  }
+}
