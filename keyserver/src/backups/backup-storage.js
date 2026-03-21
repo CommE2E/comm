a6a16b19c82d473ecd1799854c70a830aab1b59a
Ashoat Tevosyan <ashoat@gmail.com>
2026-03-20 22:57:14 -0400

    [keyserver] Add local backup storage adapter
    Summary:
    Part of [ENG-11578](https://linear.app/comm/issue/ENG-11578/set-up-dropbox-backups-of-keyserver-db-on-personal-server) and [ENG-11579](https://linear.app/comm/issue/ENG-11579/set-up-dropbox-backups-of-phabricator-db-on-personal-server).
    
    Depends on D15607
    
    Test Plan: Will test on prod
    
    Subscribers: tomek
    
    Differential Revision: https://phab.comm.dev/D15608

---
+++ keyserver/src/backups/backup-storage.js
@@ -0,0 +1,30 @@
+// @flow
+
+import { LocalBackupStorageAdapter } from './local-backup-storage-adapter.js';
+
+export type StoredFileInfo = {
+  +filename: string,
+  +lastModifiedTime: number,
+  +byteCount: number,
+};
+
+export type LocalStorageConfig = {
+  +type: 'local',
+  +directory: string,
+};
+
+export type StorageConfig = LocalStorageConfig;
+
+export type BackupStorageAdapter = {
+  +listFiles: () => Promise<$ReadOnlyArray<StoredFileInfo>>,
+  +deleteFile: (filename: string) => Promise<void>,
+  +createWriteStream: (filename: string) => Promise<stream$Writable>,
+};
+
+function createBackupStorageAdapter(
+  config: StorageConfig,
+): BackupStorageAdapter {
+  return new LocalBackupStorageAdapter(config);
+}
+
+export { createBackupStorageAdapter };
---
+++ keyserver/src/backups/local-backup-storage-adapter.js
@@ -0,0 +1,56 @@
+// @flow
+
+import fs from 'fs';
+import path from 'path';
+import { promisify } from 'util';
+
+import {
+  type LocalStorageConfig,
+  type StoredFileInfo,
+} from './backup-storage.js';
+
+const readdir = promisify(fs.readdir);
+const lstat = promisify(fs.lstat);
+const unlink = promisify(fs.unlink);
+
+class LocalBackupStorageAdapter {
+  +directory: string;
+
+  constructor(config: LocalStorageConfig) {
+    this.directory = config.directory;
+  }
+
+  async listFiles(): Promise<$ReadOnlyArray<StoredFileInfo>> {
+    const filenames = await readdir(this.directory);
+    const files = await Promise.all(
+      filenames.map(async filename => {
+        const stats = await lstat(path.join(this.directory, filename));
+        if (stats.isDirectory()) {
+          return null;
+        }
+        return {
+          filename,
+          lastModifiedTime: stats.mtime.getTime(),
+          byteCount: stats.size,
+        };
+      }),
+    );
+    return ((files.filter(Boolean): any): StoredFileInfo[]);
+  }
+
+  async deleteFile(filename: string): Promise<void> {
+    try {
+      await unlink(path.join(this.directory, filename));
+    } catch (error) {
+      if (error.code !== 'ENOENT') {
+        throw error;
+      }
+    }
+  }
+
+  async createWriteStream(filename: string): Promise<stream$Writable> {
+    return fs.createWriteStream(path.join(this.directory, filename));
+  }
+}
+
+export { LocalBackupStorageAdapter };
