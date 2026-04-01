5cf2a556e7718dd27debbb9416ff5772ac3f9eb6
Ashoat Tevosyan <ashoat@gmail.com>
2026-04-01 00:50:15 -0400

    [terraform] Clean up AWS Cloud Map HTTP namespace configs
    Summary:
    1. We don't need it on `self-host` at all because we don't do service-to-service communication
    2. We need it on `remote`, but we don't need the vestigial `AmazonECSManaged` label
    
    Test Plan: `terraform apply` in production
    
    Reviewers:
    
    Subscribers:

--- services/terraform/self-host/keyserver_cluster.tf
+++ services/terraform/self-host/keyserver_cluster.tf
@@ -16,15 +16,6 @@ resource "aws_ecs_cluster" "keyserver_cluster" {
   }
 }
 
-# Namespace for services to be able to communicate with each other
-# by their hostnames. Similar to docker compose network.
-resource "aws_service_discovery_http_namespace" "keyserver_cluster" {
-  name = "keyserver-cluster-http-namespace"
-  tags = {
-    "AmazonECSManaged" = "true"
-  }
-}
-
 resource "aws_ecs_cluster_capacity_providers" "keyserver_cluster" {
   cluster_name       = aws_ecs_cluster.keyserver_cluster.name
   capacity_providers = ["FARGATE"]
