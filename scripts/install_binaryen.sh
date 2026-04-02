a6ecf994e71ea4ea0360671a46a12607a3d161d9
Ashoat Tevosyan <ashoat@gmail.com>
2026-04-01 22:34:44 -0400

    [keyserver] Add ARM support for Binaryen installer
    Summary: D15632 moved us to ARM environment for building ARM version of keyserver (rather than emulation), but this Binaryen installer script was hardcoded to only support x86_64.
    
    Test Plan: I'll land this and then try another release
    
    Reviewers:
    
    Subscribers:

--- scripts/install_binaryen.sh
+++ scripts/install_binaryen.sh
@@ -2,12 +2,35 @@
 
 set -euo pipefail
 
-BINARYEN_VERSION=116
+BINARYEN_VERSION=128
 
-wget https://github.com/WebAssembly/binaryen/releases/download/version_${BINARYEN_VERSION}/binaryen-version_${BINARYEN_VERSION}-x86_64-linux.tar.gz
+ARCHITECTURE="$(uname -m)"
+case "${ARCHITECTURE}" in
+  x86_64)
+    BINARYEN_TARGET='x86_64-linux'
+    ;;
+  aarch64 | arm64)
+    BINARYEN_TARGET='aarch64-linux'
+    ;;
+  *)
+    echo "Unsupported architecture: ${ARCHITECTURE}" >&2
+    exit 1
+    ;;
+esac
 
-tar -xzf binaryen-version_${BINARYEN_VERSION}-x86_64-linux.tar.gz
+ARCHIVE="binaryen-version_${BINARYEN_VERSION}-${BINARYEN_TARGET}.tar.gz"
+RELEASE_URL="https://github.com/WebAssembly/binaryen/releases/download/version_${BINARYEN_VERSION}"
 
-cp -r binaryen-version_${BINARYEN_VERSION}/bin/* /usr/local/bin/
+wget "${RELEASE_URL}/${ARCHIVE}"
+wget "${RELEASE_URL}/${ARCHIVE}.sha256"
 
-rm -rf binaryen-version_${BINARYEN_VERSION} binaryen-version_${BINARYEN_VERSION}-x86_64-linux.tar.gz
+sha256sum -c "${ARCHIVE}.sha256"
+
+tar -xzf "${ARCHIVE}"
+
+cp -r "binaryen-version_${BINARYEN_VERSION}/bin/"* /usr/local/bin/
+
+rm -rf \
+  "binaryen-version_${BINARYEN_VERSION}" \
+  "${ARCHIVE}" \
+  "${ARCHIVE}.sha256"
