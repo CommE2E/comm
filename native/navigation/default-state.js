CommE2E/comm

commit 9b41f78dfae52d9f6d733f1e7d5955838c8ad7ed

[lib][native][web] Deprecate apps concept
Summary: We never implemented a second app beyond calendar. We can always bring this back later, but for now we're going to just default the calendar to always be on.

Test Plan: Tested both web and native locally

Subscribers: tomek

Differential Revision: https://phab.comm.dev/D15622

native/navigation/default-state.js
@@ -9,13 +9,13 @@ import {
   AppRouteName,
   TabNavigatorRouteName,
   LoggedOutModalRouteName,
+  CalendarRouteName,
   ProfileRouteName,
   ProfileScreenRouteName,
   ChatRouteName,
   ChatThreadListRouteName,
   HomeChatThreadListRouteName,
   BackgroundChatThreadListRouteName,
-  AppsRouteName,
   CommunityDrawerNavigatorRouteName,
 } from './route-names.js';
 
@@ -64,6 +64,9 @@ const defaultNavigationState: StaleNavigationState = {
                         },
                       },
                       {
+                        name: CalendarRouteName,
+                      },
+                      {
                         name: ProfileRouteName,
                         state: {
                           type: 'stack',
@@ -71,7 +74,6 @@ const defaultNavigationState: StaleNavigationState = {
                           routes: [{ name: ProfileScreenRouteName }],
                         },
                       },
-                      { name: AppsRouteName },
                     ],
                   },
                 },

native/redux/default-state.js
@@ -4,7 +4,6 @@ import { Platform } from 'react-native';
 import Orientation from 'react-native-orientation-locker';
 
 import { defaultAlertInfos } from 'lib/types/alert-types.js';
-import { defaultEnabledApps } from 'lib/types/enabled-apps.js';
 import { defaultCalendarFilters } from 'lib/types/filter-types.js';
 import { defaultKeyserverInfo } from 'lib/types/keyserver-types.js';
 import { defaultGlobalThemeInfo } from 'lib/types/theme-types.js';
@@ -48,7 +47,6 @@ const defaultState = ({
   },
   watchedThreadIDs: [],
   lifecycleState: 'active',
-  enabledApps: defaultEnabledApps,
   reportStore: {
     enabledReports: {
       crashReports: __DEV__,

web/redux/default-state.js
@@ -1,7 +1,6 @@
 // @flow
 
 import { defaultAlertInfos } from 'lib/types/alert-types.js';
-import { defaultWebEnabledApps } from 'lib/types/enabled-apps.js';
 import { defaultCalendarFilters } from 'lib/types/filter-types.js';
 import { defaultKeyserverInfo } from 'lib/types/keyserver-types.js';
 import { defaultGlobalThemeInfo } from 'lib/types/theme-types.js';
@@ -49,7 +48,6 @@ const defaultWebState: AppState = Object.freeze({
   },
   watchedThreadIDs: [],
   lifecycleState: 'active',
-  enabledApps: defaultWebEnabledApps,
   reportStore: {
     enabledReports: {
       crashReports: false,

lib/backup/persist-shared-migrations.js
@@ -8,6 +8,8 @@ import type { StoreOperations } from '../types/store-ops-types.js';
 import { syncedMetadataNames } from '../types/synced-metadata-types.js';
 import { getConfig } from '../utils/config.js';
 
+const deprecatedEnabledAppsSyncedMetadata = 'enabled_apps';
+
 export type SharedMigrationFunction = (
   databaseIdentifier: DatabaseIdentifier,
 ) => Promise<StoreOperations>;
@@ -87,6 +89,33 @@ const sharedMigrations: SharedMigrationsManifest = {
           syncedMetadataNames.ALERT_STORE,
           JSON.stringify(updatedAlertStore),
         ),
+      ];
+
+    return {
+      syncedMetadataStoreOperations,
+    };
+  }: SharedMigrationFunction),
+  [99]: (async (databaseIdentifier: DatabaseIdentifier) => {
+    const { sqliteAPI } = getConfig();
+
+    const clientStoreToMigrate =
+      await sqliteAPI.getClientDBStore(databaseIdentifier);
+
+    if (
+      !clientStoreToMigrate.syncedMetadata ||
+      !clientStoreToMigrate.syncedMetadata[deprecatedEnabledAppsSyncedMetadata]
+    ) {
+      return {};
+    }
+
+    const syncedMetadataStoreOperations: $ReadOnlyArray<ClientDBSyncedMetadataStoreOperation> =
+      [
+        {
+          type: 'remove_synced_metadata',
+          payload: {
+            names: [deprecatedEnabledAppsSyncedMetadata],
+          },
+        },
       ];
 
     return {

lib/reducers/enabled-apps-reducer.js
@@ -1,78 +1,0 @@
-// @flow
-
-import { setClientDBStoreActionType } from '../actions/client-db-store-actions.js';
-import { setNewSessionActionType } from '../keyserver-conn/keyserver-conn-types.js';
-import type { SyncedMetadataStoreOperation } from '../ops/synced-metadata-store-ops.js';
-import { createReplaceSyncedMetadataOperation } from '../ops/synced-metadata-store-ops.js';
-import type { EnabledApps } from '../types/enabled-apps.js';
-import {
-  defaultEnabledApps,
-  defaultWebEnabledApps,
-} from '../types/enabled-apps.js';
-import type { BaseAction } from '../types/redux-types.js';
-import { syncedMetadataNames } from '../types/synced-metadata-types.js';
-import { authoritativeKeyserverID } from '../utils/authoritative-keyserver.js';
-import { relyingOnAuthoritativeKeyserver } from '../utils/services-utils.js';
-
-export const enableAppActionType = 'ENABLE_APP';
-export const disableAppActionType = 'DISABLE_APP';
-
-type ReduceEnabledAppsResult = {
-  +enabledApps: EnabledApps,
-  +syncedMetadataStoreOperations: $ReadOnlyArray<SyncedMetadataStoreOperation>,
-};
-
-function getUpdatedEnabledAppsResult(
-  enabledApps: EnabledApps,
-): ReduceEnabledAppsResult {
-  return {
-    enabledApps,
-    syncedMetadataStoreOperations: [
-      createReplaceSyncedMetadataOperation(
-        syncedMetadataNames.ENABLED_APPS,
-        JSON.stringify(enabledApps),
-      ),
-    ],
-  };
-}
-
-export default function reduceEnabledApps(
-  state: EnabledApps,
-  action: BaseAction,
-): ReduceEnabledAppsResult {
-  if (action.type === enableAppActionType && action.payload === 'calendar') {
-    return getUpdatedEnabledAppsResult({ ...state, calendar: true });
-  } else if (
-    action.type === disableAppActionType &&
-    action.payload === 'calendar'
-  ) {
-    return getUpdatedEnabledAppsResult({ ...state, calendar: false });
-  } else if (action.type === setClientDBStoreActionType) {
-    let enabledApps = { ...state };
-    const enabledAppsString =
-      action.payload.syncedMetadata?.[syncedMetadataNames.ENABLED_APPS];
-    if (enabledAppsString) {
-      enabledApps = {
-        ...enabledApps,
-        ...JSON.parse(enabledAppsString),
-      };
-    }
-    return {
-      enabledApps,
-      syncedMetadataStoreOperations: [],
-    };
-  } else if (
-    action.type === setNewSessionActionType &&
-    action.payload.sessionChange.cookieInvalidated &&
-    action.payload.keyserverID === authoritativeKeyserverID() &&
-    relyingOnAuthoritativeKeyserver
-  ) {
-    return getUpdatedEnabledAppsResult(
-      process.env.BROWSER ? defaultWebEnabledApps : defaultEnabledApps,
-    );
-  }
-  return {
-    enabledApps: state,
-    syncedMetadataStoreOperations: [],
-  };
-}

lib/reducers/master-reducer.js
@@ -10,7 +10,6 @@ import reduceDataLoaded from './data-loaded-reducer.js';
 import { reduceDBOpsStore } from './db-ops-reducer.js';
 import { reduceDMOperationsQueue } from './dm-operations-queue-reducer.js';
 import { reduceDraftStore } from './draft-reducer.js';
-import reduceEnabledApps from './enabled-apps-reducer.js';
 import { reduceEntryInfos } from './entry-reducer.js';
 import { reduceHolderStore } from './holder-reducer.js';
 import { reduceIntegrityStore } from './integrity-reducer.js';
@@ -174,9 +173,6 @@ export default function baseReducer<N: BaseNavInfo, T: BaseAppState<N>>(
     threadStoreOperations,
   );
 
-  const { enabledApps, syncedMetadataStoreOperations: enabledAppsOps } =
-    reduceEnabledApps(state.enabledApps, action);
-
   const { globalThemeInfo, syncedMetadataStoreOperations: globalThemeInfoOps } =
     reduceGlobalThemeInfo(state.globalThemeInfo, action);
 
@@ -184,7 +180,6 @@ export default function baseReducer<N: BaseNavInfo, T: BaseAppState<N>>(
     reduceAlertStore(state.alertStore, action);
 
   const predefinedSyncedMetadataStoreOperations = [
-    ...enabledAppsOps,
     ...globalThemeInfoOps,
     ...alertStoreOps,
   ];
@@ -266,7 +261,6 @@ export default function baseReducer<N: BaseNavInfo, T: BaseAppState<N>>(
       ),
       alertStore,
       lifecycleState: reduceLifecycleState(state.lifecycleState, action),
-      enabledApps,
       reportStore,
       dataLoaded: reduceDataLoaded(state.dataLoaded, action),
       userPolicies: policiesReducer(state.userPolicies, action),

lib/types/enabled-apps.js
@@ -1,24 +1,0 @@
-// @flow
-
-export type EnabledApps = {
-  +calendar: boolean,
-  +wiki: boolean,
-  +tasks: boolean,
-  +files: boolean,
-};
-
-export type SupportedApps = $Keys<EnabledApps>;
-
-export const defaultEnabledApps: EnabledApps = {
-  calendar: false,
-  wiki: false,
-  tasks: false,
-  files: false,
-};
-
-export const defaultWebEnabledApps: EnabledApps = {
-  calendar: true,
-  wiki: false,
-  tasks: false,
-  files: false,
-};

lib/types/redux-types.js
@@ -57,7 +57,6 @@ import type {
   QueueDMOpsCondition,
 } from './dm-ops.js';
 import type { DraftStore } from './draft-types.js';
-import type { EnabledApps, SupportedApps } from './enabled-apps.js';
 import type {
   RawEntryInfo,
   EntryStore,
@@ -204,7 +203,6 @@ export type BaseAppState<NavInfo: BaseNavInfo = BaseNavInfo> = {
   +alertStore: AlertStore,
   +watchedThreadIDs: $ReadOnlyArray<string>,
   +lifecycleState: LifecycleState,
-  +enabledApps: EnabledApps,
   +reportStore: ReportStore,
   +dataLoaded: boolean,
   +userPolicies: UserPolicies,
@@ -949,14 +947,6 @@ export type BaseAction = $ReadOnly<{
     | {
         +type: 'UPDATE_LIFECYCLE_STATE',
         +payload: LifecycleState,
-      }
-    | {
-        +type: 'ENABLE_APP',
-        +payload: SupportedApps,
-      }
-    | {
-        +type: 'DISABLE_APP',
-        +payload: SupportedApps,
       }
     | {
         +type: 'UPDATE_REPORTS_ENABLED',

commit 2b9fe2c19faa8e903cc5d8a98904a44854df5060

[native] Fix default navigation state for Calendar
Summary: Broken in D15622.

Test Plan: Confirmed issue no longer repros

Reviewers:

Subscribers:

native/navigation/default-state.js
@@ -10,6 +10,7 @@ import {
   TabNavigatorRouteName,
   LoggedOutModalRouteName,
   CalendarRouteName,
+  CalendarScreenRouteName,
   ProfileRouteName,
   ProfileScreenRouteName,
   ChatRouteName,
@@ -65,6 +66,11 @@ const defaultNavigationState: StaleNavigationState = {
                       },
                       {
                         name: CalendarRouteName,
+                        state: {
+                          type: 'stack',
+                          index: 0,
+                          routes: [{ name: CalendarScreenRouteName }],
+                        },
                       },
                       {
                         name: ProfileRouteName,