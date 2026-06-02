f447dff5bc82702c1285059a38f565cbaa6d15ff
Ashoat Tevosyan <ashoat@gmail.com>
2026-06-02 00:28:53 -0400

    [yarn] Tighten Windows postinstall guard

--- postinstall.sh
+++ postinstall.sh
@@ -3,8 +3,7 @@
 set -Eeuo pipefail
 
 # Skip Windows
-echo "RUNNER_OS=${RUNNER_OS:-} OSTYPE=$OSTYPE"
-if [[ "$OSTYPE" == msys* || "$OSTYPE" == cygwin* ]]; then
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then
   exit 0
 fi
 
